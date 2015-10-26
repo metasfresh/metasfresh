15be97a318befd5209b2834f2b5a3eb70b883fd0
metas-dev <dev@metas-fresh.com>
2015-10-26 13:39:08 +0200

    bugfix: 185 - Fix message for Event.RemoteEndpointDisconnected

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5431240_sys_Fix_msg_Event_RemoteEndpointConnected_Disconnected.sql
@@ -0,0 +1,20 @@
+-- 26.10.2015 13:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Connection to global notifications is down.',Updated=TO_TIMESTAMP('2015-10-26 13:36:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=543623
+;
+
+-- 26.10.2015 13:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='N' WHERE AD_Message_ID=543623
+;
+
+-- 26.10.2015 13:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Connection to global notifications is up',Updated=TO_TIMESTAMP('2015-10-26 13:36:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=543624
+;
+
+-- 26.10.2015 13:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='N' WHERE AD_Message_ID=543624
+;
+
