a5d390c746c1cbd42ab52d27f1af927b9f007581
metas-dev <dev@metas-fresh.com>
2015-11-05 14:55:37 +0100

    adding a fix that sort-of got lost in uat->it

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5432440_sys_09421_fix_x_fresh_qtyonhand_ondate.sql
@@ -0,0 +1,10 @@
+
+
+ALTER TABLE "de.metas.fresh".x_fresh_qtyonhand_ondate DROP CONSTRAINT x_fresh_qtyonhand_ondate_pkey;
+
+DROP INDEX IF eXISTS "de.metas.fresh".x_fresh_qtyonhand_ondate_tuple;
+
+CREATE INDEX x_fresh_qtyonhand_ondate_tuple
+  ON "de.metas.fresh".x_fresh_qtyonhand_ondate
+  USING btree
+  (movementdate , m_product_id , m_attributesetinstance_id );
