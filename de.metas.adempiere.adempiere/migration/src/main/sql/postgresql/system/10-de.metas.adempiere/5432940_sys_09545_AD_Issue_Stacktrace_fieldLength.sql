63d9fe334b653cc25367599fe74bed922c71958a
metas-dev <dev@metas-fresh.com>
2015-11-11 10:42:48 +0200

    09545 Extend the AD Issue.Stacktrace length from 2000 to 4000 or even more (107425830415): integrate

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5432940_sys_09545_AD_Issue_Stacktrace_fieldLength.sql
@@ -0,0 +1,6 @@
+-- NOTE: in application dictionary, the field was already Reference=Text, FieldLength=999999
+-- 11.11.2015 10:35
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_issue','StackTrace','TEXT',null,'NULL')
+;
+
