9c2eb46ae65fee086ee1d70c0851c8a8515f998a
metas-ts <t.schoeneberg@metas-fresh.com>
2015-12-03 21:50:52 +0100

    enforcer/banDuplicateClases: adding artifacts to dependency mgmt (hopefully i now found a reliable workaround that

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/shipmentschedule/api/impl/HUShipmentScheduleBL.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/shipmentschedule/api/impl/HUShipmentScheduleBL.java
@@ -10,12 +10,12 @@ package de.metas.handlingunits.shipmentschedule.api.impl;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -667,7 +667,6 @@ public class HUShipmentScheduleBL implements IHUShipmentScheduleBL
 		I_M_HU_PI_Item_Product piItemProduct_Effective = shipmentSchedule.getM_HU_PI_Item_Product_Calculated();
 
 		if (!InterfaceWrapperHelper.isNull(shipmentSchedule, I_M_ShipmentSchedule.COLUMNNAME_QtyTU_Override))
-
 		{
 			qtyTU_Effective = shipmentSchedule.getQtyTU_Override();
 		}
