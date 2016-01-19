3f15e30672e719c3fc2ccd3c4bbe611284bdd53b
metas-cg <c.ghita@metas-fresh.com>
2016-01-19 07:20:15 +0100

    09687 DD order Copy with details (101015490340): merge us->uat

---
+++ de.metas.adempiere.libero.libero/src/main/sql/postgresql/system/5436680_sys_09687_FixIsCalculated.sql
@@ -0,0 +1,174 @@
+-- 14.01.2016 14:37
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:37:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53884
+;
+
+-- 14.01.2016 14:37
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:37:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53925
+;
+
+-- 14.01.2016 14:37
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:37:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53923
+;
+
+-- 14.01.2016 14:38
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:38:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53917
+;
+
+-- 14.01.2016 14:38
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:38:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53918
+;
+
+-- 14.01.2016 14:39
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:39:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53909
+;
+
+-- 14.01.2016 14:39
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:39:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53907
+;
+
+-- 14.01.2016 14:39
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:39:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53902
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53904
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53901
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53900
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53898
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53897
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53885
+;
+
+-- 14.01.2016 14:40
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:40:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53892
+;
+
+-- 14.01.2016 14:41
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:41:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53891
+;
+
+-- 14.01.2016 14:41
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:41:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53887
+;
+
+-- 14.01.2016 14:41
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 14:41:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53886
+;
+
+-- 14.01.2016 15:03
+-- URL zum Konzept
+UPDATE C_Async_Batch SET Processed='Y',Updated=TO_TIMESTAMP('2016-01-14 15:03:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=2187913 WHERE C_Async_Batch_ID=1000720
+;
+
+-- 14.01.2016 16:16
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:16:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53934
+;
+
+-- 14.01.2016 16:16
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:16:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53938
+;
+
+-- 14.01.2016 16:16
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:16:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53939
+;
+
+-- 14.01.2016 16:16
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:16:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53940
+;
+
+-- 14.01.2016 16:16
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:16:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53945
+;
+
+-- 14.01.2016 16:16
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:16:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53951
+;
+
+-- 14.01.2016 16:17
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:17:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53952
+;
+
+-- 14.01.2016 16:17
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:17:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53953
+;
+
+-- 14.01.2016 16:17
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:17:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53927
+;
+
+-- 14.01.2016 16:17
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-01-14 16:17:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53955
+;
+
+
+-- fix default values ---
+
+
+-- 14.01.2016 16:21
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2016-01-14 16:21:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53900
+;
+
+-- 14.01.2016 16:21
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2016-01-14 16:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53901
+;
+
+-- 14.01.2016 16:21
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2016-01-14 16:21:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53902
+;
+
+-- 14.01.2016 16:22
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2016-01-14 16:22:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53945
+;
+
+-- 14.01.2016 16:22
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2016-01-14 16:22:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53952
+;
+
