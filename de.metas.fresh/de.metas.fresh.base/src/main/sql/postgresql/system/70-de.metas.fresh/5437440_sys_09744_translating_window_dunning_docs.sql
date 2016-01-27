2f7a9344cbf6e474f7fc2f4e726330fabfb21539
metas-nw <n.wessel@metas.com>
2016-01-27 12:05:56 +0100

    09744 Mahnung Report and UI changes (100433003670): adding french CH translation

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5437440_sys_09744_translating_window_dunning_docs.sql
@@ -0,0 +1,71 @@
+-- 25.01.2016 13:10
+-- URL zum Konzept
+UPDATE AD_Window SET Name='Mahnungen',Updated=TO_TIMESTAMP('2016-01-25 13:10:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540155
+;
+
+-- 25.01.2016 13:10
+-- URL zum Konzept
+UPDATE AD_Window_Trl SET IsTranslated='N' WHERE AD_Window_ID=540155
+;
+
+-- 25.01.2016 13:10
+-- URL zum Konzept
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Mahnungen',Updated=TO_TIMESTAMP('2016-01-25 13:10:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540399
+;
+
+-- 25.01.2016 13:10
+-- URL zum Konzept
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=540399
+;
+
+
+-- 25.01.2016 13:12
+-- URL zum Konzept
+UPDATE AD_Tab SET Name='Mahnungen',Updated=TO_TIMESTAMP('2016-01-25 13:12:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540430
+;
+
+-- 25.01.2016 13:12
+-- URL zum Konzept
+UPDATE AD_Tab_Trl SET IsTranslated='N' WHERE AD_Tab_ID=540430
+;
+
+-- 25.01.2016 13:12
+-- URL zum Konzept
+UPDATE AD_Tab SET Name='Zeile',Updated=TO_TIMESTAMP('2016-01-25 13:12:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540431
+;
+
+-- 25.01.2016 13:12
+-- URL zum Konzept
+UPDATE AD_Tab_Trl SET IsTranslated='N' WHERE AD_Tab_ID=540431
+;
+
+-- 25.01.2016 13:13
+-- URL zum Konzept
+UPDATE AD_Tab SET Name='Quelle',Updated=TO_TIMESTAMP('2016-01-25 13:13:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540432
+;
+
+-- 25.01.2016 13:13
+-- URL zum Konzept
+UPDATE AD_Tab_Trl SET IsTranslated='N' WHERE AD_Tab_ID=540432
+;
+
+-- 25.01.2016 13:14
+-- URL zum Konzept
+UPDATE AD_Field SET Name='Organisation',Updated=TO_TIMESTAMP('2016-01-25 13:14:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=550668
+;
+
+-- 25.01.2016 13:14
+-- URL zum Konzept
+UPDATE AD_Field_Trl SET IsTranslated='N' WHERE AD_Field_ID=550668
+;
+
+-- 25.01.2016 13:14
+-- URL zum Konzept
+UPDATE AD_Field SET Name='Mahndatum',Updated=TO_TIMESTAMP('2016-01-25 13:14:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=550662
+;
+
+-- 25.01.2016 13:14
+-- URL zum Konzept
+UPDATE AD_Field_Trl SET IsTranslated='N' WHERE AD_Field_ID=550662
+;
+
