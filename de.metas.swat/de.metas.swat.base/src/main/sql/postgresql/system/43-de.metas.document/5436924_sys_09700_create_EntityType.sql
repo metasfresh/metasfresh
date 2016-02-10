dfd13134427b3affcc0f7e0d55de85a80b8b2290
metas-ts <t.schoeneberg@metas-fresh.com>
2016-02-10 10:54:46 +0100

    SQL rollout fix

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/43-de.metas.document/5436924_sys_09700_create_EntityType.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/43-de.metas.document/5436924_sys_09700_create_EntityType.sql
@@ -1,5 +1,5 @@
 
 -- 19.01.2016 15:42
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_EntityType (AD_Client_ID,AD_EntityType_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,ModelPackage,Name,Processing,Updated,UpdatedBy) VALUES (0,540079,0,TO_TIMESTAMP('2016-01-19 15:42:29','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.document','Y','de.metas.document.model','de.metas.document','N',TO_TIMESTAMP('2016-01-19 15:42:29','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_EntityType (AD_Client_ID,AD_EntityType_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,ModelPackage,Name,Processing,Updated,UpdatedBy) VALUES (0,5479,0,TO_TIMESTAMP('2016-01-19 15:42:29','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.document','Y','de.metas.document.model','de.metas.document','N',TO_TIMESTAMP('2016-01-19 15:42:29','YYYY-MM-DD HH24:MI:SS'),100)
 ;
