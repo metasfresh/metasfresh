89b911ee5a44e9496b37971c217b996da1a055e0
metas-ts <t.schoeneberg@metas-fresh.com>
2016-02-10 10:58:39 +0100

    SQL rollout fix

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/43-de.metas.product/5436920_sys_09700_create_EntityType.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/43-de.metas.product/5436920_sys_09700_create_EntityType.sql
@@ -1,5 +1,5 @@
 
 -- 19.01.2016 14:43
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_EntityType (AD_Client_ID,AD_EntityType_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,ModelPackage,Name,Processing,Updated,UpdatedBy) VALUES (0,5478,0,TO_TIMESTAMP('2016-01-19 14:43:03','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.product','Y','de.metas.product.model','de.metas.product','N',TO_TIMESTAMP('2016-01-19 14:43:03','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_EntityType (AD_Client_ID,AD_EntityType_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,ModelPackage,Name,Processing,Updated,UpdatedBy) VALUES (0,54078,0,TO_TIMESTAMP('2016-01-19 14:43:03','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.product','Y','de.metas.product.model','de.metas.product','N',TO_TIMESTAMP('2016-01-19 14:43:03','YYYY-MM-DD HH24:MI:SS'),100)
 ;
