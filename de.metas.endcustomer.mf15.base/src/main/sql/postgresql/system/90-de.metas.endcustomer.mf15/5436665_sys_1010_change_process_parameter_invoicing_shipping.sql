39bf576e41277145b3dfeca52f60f5981186b8c0
metas-ts <t.schoeneberg@metas-fresh.com>
2016-02-15 17:03:43 +0100

    rollout SQL fix

--- de.metas.endcustomer.mf15.base/src/main/sql/postgresql/system/90-de.metas.endcustomer.mf15/5436665_sys_1010_change_process_parameter_invoicing_shipping.sql
+++ de.metas.endcustomer.mf15.base/src/main/sql/postgresql/system/90-de.metas.endcustomer.mf15/5436665_sys_1010_change_process_parameter_invoicing_shipping.sql
@@ -1,4 +1,4 @@
-﻿-- 01.12.2015 13:31
+-- 01.12.2015 13:31
 -- URL zum Konzept
 UPDATE AD_Process_Para SET DefaultValue='Y',Updated=TO_TIMESTAMP('2015-12-01 13:31:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=540703
 ;
