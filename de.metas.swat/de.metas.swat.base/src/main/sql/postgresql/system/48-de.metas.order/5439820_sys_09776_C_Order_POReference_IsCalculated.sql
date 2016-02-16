037aa4e7562224ef7b3e66cce95fcbe0d59c6715
metas-ts <t.schoeneberg@metas-fresh.com>
2016-02-16 10:40:38 +0100

    FRESH-9: 09776 EDI - Receiver without ORDERS (100584995833): fixes

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.order/5439820_sys_09776_C_Order_POReference_IsCalculated.sql
@@ -0,0 +1,10 @@
+
+--
+-- we don't want the POReference to be clopied blindly.
+-- note that when create a PO from a SO, it is still copied to the the PO
+--
+-- 16.02.2016 10:14
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2016-02-16 10:14:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3045
+;
+
