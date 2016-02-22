ae5eaa9678e3830ad82bd6d195c3e2bd75ef3b8e
metas-rc <r.craciunescu@metas-fresh.com>
2016-02-22 13:36:19 +0100

    09281 create report for packaging material balance (106483495857): integrate

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5440400_sys_09281_Gebindesaldo_Processes_ToReports.sql
@@ -0,0 +1,10 @@
+-- 22.02.2016 11:57
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET JasperReport='@PREFIX@de/metas/reports/hubalancegeneral/report.jasper', JasperReport_Tabular='@PREFIX@de/metas/reports/hubalancegeneral/report_details_tabular.jasper',Updated=TO_TIMESTAMP('2016-02-22 11:57:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540651
+;
+
+-- 22.02.2016 11:57
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET JasperReport='@PREFIX@de/metas/reports/hubalance/report.jasper',Updated=TO_TIMESTAMP('2016-02-22 11:57:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540467
+;
+
