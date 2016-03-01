d89f2ab4f2804f1280eb30b15388c295b5aba8a3
metas-nw <n.wessel@metas-fresh.com>
2016-03-01 18:33:40 +0100

    adding additional translations

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5441356_system_trl_en_US_ad_window_role_quickstart.sql
@@ -0,0 +1,11 @@
+﻿
+update ad_window_trl set name = 'Boxing Type' ,istranslated = 'Y' where ad_window_id = 540192 and ad_language = 'en_US';
+update ad_window_trl set name = 'Business Partner Group' ,istranslated = 'Y' where ad_window_id = 192 and ad_language = 'en_US';
+update ad_window_trl set name = 'Business Partner' ,istranslated = 'Y' where ad_window_id = 123 and ad_language = 'en_US';
+update ad_window_trl set name = 'Invoice (Customer)' ,istranslated = 'Y' where ad_window_id = 167 and ad_language = 'en_US';
+update ad_window_trl set name = 'Packing Instruction' ,istranslated = 'Y' where ad_window_id = 540188 and ad_language = 'en_US';
+update ad_window_trl set name = 'Payment Term' ,istranslated = 'Y' where ad_window_id = 141 and ad_language = 'en_US';
+update ad_window_trl set name = 'Product Category' ,istranslated = 'Y' where ad_window_id = 144 and ad_language = 'en_US';
+update ad_window_trl set name = 'Product' ,istranslated = 'Y' where ad_window_id = 140 and ad_language = 'en_US';
+update ad_window_trl set name = 'Sales Order' ,istranslated = 'Y' where ad_window_id = 143 and ad_language = 'en_US';
+update ad_window_trl set name = 'Shipment (Customer)' ,istranslated = 'Y' where ad_window_id = 169 and ad_language = 'en_US';
