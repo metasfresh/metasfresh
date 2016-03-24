a11c447a8a1e4f7c2ec8e5aa54a583a8a1ceee8f
metas-ts <t.schoeneberg@metas-fresh.com>
2016-03-24 14:28:46 +0100

    minor, unrelated

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5443370_sys_rename_bestellkontrolle process.sql
@@ -0,0 +1,11 @@
+
+-- 24.03.2016 14:25
+-- URL zum Konzept
+UPDATE AD_Process SET Description='Erstellt und druckt zu den Auftragspositionen Bestellkontroll-Berichte für die jeweiligen Produktionsverantwortlichen sowie für die Spedition.', Name='Bestellkontrollen zum Auftrag',Updated=TO_TIMESTAMP('2016-03-24 14:25:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540603
+;
+
+-- 24.03.2016 14:25
+-- URL zum Konzept
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=540603
+;
+
