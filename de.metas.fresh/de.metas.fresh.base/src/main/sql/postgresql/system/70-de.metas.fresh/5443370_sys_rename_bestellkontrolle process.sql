17bf578cc998352dfcb8fcc480060d855f4e5843
metas-ts <t.schoeneberg@metas-fresh.com>
2016-03-26 06:44:55 +0100

    sql encoding fix

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5443370_sys_rename_bestellkontrolle process.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5443370_sys_rename_bestellkontrolle process.sql
@@ -1,7 +1,7 @@
-
+ï»¿
 -- 24.03.2016 14:25
 -- URL zum Konzept
-UPDATE AD_Process SET Description='Erstellt und druckt zu den Auftragspositionen Bestellkontroll-Berichte für die jeweiligen Produktionsverantwortlichen sowie für die Spedition.', Name='Bestellkontrollen zum Auftrag',Updated=TO_TIMESTAMP('2016-03-24 14:25:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540603
+UPDATE AD_Process SET Description='Erstellt und druckt zu den Auftragspositionen Bestellkontroll-Berichte fÃ¼r die jeweiligen Produktionsverantwortlichen sowie fÃ¼r die Spedition.', Name='Bestellkontrollen zum Auftrag',Updated=TO_TIMESTAMP('2016-03-24 14:25:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540603
 ;
 
 -- 24.03.2016 14:25
