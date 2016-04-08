103f1ad5367ba482e1e13a923c6f616cc5830afb
metas-rc <r.craciunescu@metas-fresh.com>
2016-04-08 12:47:48 +0200

    FRESH-128 Provide Basic CRM Features: integrating

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5444350_sys_Fresh_128_Request_Salesrep_Autocomplete.sql
@@ -0,0 +1,5 @@
+-- 08.04.2016 13:27
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsAutocomplete='Y',Updated=TO_TIMESTAMP('2016-04-08 13:27:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=5432
+;
+
