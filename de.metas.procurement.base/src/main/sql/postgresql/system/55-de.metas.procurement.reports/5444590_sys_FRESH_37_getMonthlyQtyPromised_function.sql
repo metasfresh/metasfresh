c14bfda5bd034bc415c7b9fc8e6d46a36c7f325f
metas-lc <l.cotuna@metas-fresh.com>
2016-04-20 07:37:25 +0200

    FRESH-37 09955 excel Report Procurement Statistics (105226320154)

---
+++ de.metas.procurement.base/src/main/sql/postgresql/system/55-de.metas.procurement.reports/5444590_sys_FRESH_37_getMonthlyQtyPromised_function.sql
@@ -0,0 +1,60 @@
+drop function if exists de_metas_procurement.getMonthlyQtyPromised
+(
+	p_MonthDate timestamp -- 1
+	, p_C_BPartner_ID numeric -- 2
+	, p_M_Product_ID numeric -- 3
+	, p_M_AttributeSetInstance_ID numeric -- 4
+	, p_C_Flatrate_DataEntry_ID numeric -- 5
+);
+
+create or replace function de_metas_procurement.getMonthlyQtyPromised
+(
+	p_MonthDate timestamp -- 1
+	, p_C_BPartner_ID numeric -- 2
+	, p_M_Product_ID numeric -- 3
+	, p_M_AttributeSetInstance_ID numeric -- 4
+	, p_C_Flatrate_DataEntry_ID numeric -- 5
+)
+returns numeric
+as
+$BODY$
+	select COALESCE(SUM(qtypromised), 0)
+	from PMM_Balance b
+	where true
+	and b.C_BPartner_ID=$2
+	and b.M_Product_ID=$3
+	and ($4 is null or b.M_AttributeSetInstance_ID=$4)
+	and b.MonthDate=date_trunc('month', $1)
+	and b.WeekDate is null
+	limit 1
+
+$BODY$
+LANGUAGE sql STABLE;
+
+
+
+/* TEST:
+select * from (
+	select
+		b.MonthDate
+		, b.WeekDate
+		,b.C_BPartner_ID
+		,b.M_Product_ID
+		,b.M_AttributeSetInstance_ID
+		,b.C_Flatrate_DataEntry_ID
+		, b.qtypromised
+		, de_metas_procurement.getMonthlyQtyPromised(
+			p_MonthDate := b.MonthDate
+			, p_C_BPartner_ID := b.C_BPartner_ID
+			, p_M_Product_ID := b.M_Product_ID
+			, p_M_AttributeSetInstance_ID := b.M_AttributeSetInstance_ID
+			, p_C_Flatrate_DataEntry_ID := b.C_Flatrate_DataEntry_ID
+		) as QtyPromised_Calc
+	from PMM_Balance b
+	where true
+	and b.WeekDate is null
+) t
+where true
+and t.qtypromised is distinct from t.QtyPromised_Calc
+;
+*/
