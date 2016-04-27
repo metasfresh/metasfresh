763a70605b50e6531f7f37ddad7e03db54b55f3d
metas-lc <liana.cotuna@metasfresh.com>
2016-04-27 17:47:27 +0300

    Customer individual Inout show Abladeort
    added unload bpartner address in customer individual report

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5445010_sys_FRESH-255_docs_unloading_bpartner_address_function.sql
@@ -0,0 +1,21 @@
+-- Function: de_metas_endcustomer_fresh_reports.docs_unloading_bpartner_address_report(numeric)
+
+-- DROP FUNCTION de_metas_endcustomer_fresh_reports.docs_unloading_bpartner_address_report(numeric);
+
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.docs_unloading_bpartner_address_report(record_id numeric)
+  RETURNS  TABLE ( 
+		unload_bpartner_name character varying,
+		unload_bpartner_address character varying
+	) AS 
+$$
+SELECT
+	bploc.Name, bploc.Address
+FROM
+	M_InOut io
+JOIN C_Order o ON io.c_Order_ID = o.C_Order_ID	
+JOIN C_BPartner_Location bploc ON o.Handover_Location_ID = bploc.C_BPartner_Location_ID
+
+WHERE io.M_InOut_ID = $1 
+$$
+LANGUAGE sql STABLE;
+
