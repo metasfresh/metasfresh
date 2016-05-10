dab6d42910aa754edc40df863c75b306dc8fba53
metas-lc <liana.cotuna@metasfresh.com>
2016-05-10 10:24:17 +0300

    FRESH-272 hubalance general flatrate filter
    refactoring, added exists_flatrateterm_for_product_and_bpartner function

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5445180_sys_FRESH-272_exists_flatrateterm_for_product_and_bpartner_function.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.exists_flatrateterm_for_product_and_bpartner(IN m_product_id numeric, IN c_bpartner_id numeric, IN startdate DATE, IN EndDate DATE )
+RETURNS BOOLEAN 
+AS
+$$
+select exists(select 1 from C_FLatrate_Term ft
+	JOIN C_FLatrate_Data fd on ft.C_Flatrate_Data_ID = fd.C_Flatrate_Data_ID
+	JOIN C_FLatrate_Conditions fc on ft.C_FLatrate_Conditions_ID = fc.C_FLatrate_Conditions_ID and fc.Type_Conditions = 'Refundable'
+	JOIN C_Flatrate_Matching fm on fm.C_FLatrate_Conditions_ID = fc.C_FLatrate_Conditions_ID
+	JOIN M_Product p on p.m_product_id = $1
+	WHERE fd.C_BPartner_ID = $2 AND
+				(fm.M_Product_ID = p.M_Product_ID OR ft.M_Product_ID = p.M_Product_ID OR (fm.M_Product_Category_Matching_ID  = p. M_Product_Category_ID AND fm.M_Product_ID IS NULL))
+				AND COALESCE( ft.EndDate >= $3, true )
+				AND COALESCE( ft.StartDate <= $4, true )
+				AND ft.isActive = 'Y'
+				AND ft.docstatus IN ('CO','CL'))
+
+$$
+LANGUAGE sql STABLE;
