cb5b2039534e0eaea01d7db2870f9f637616ec2d
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-05-12 17:38:08 +0300

    FRESH-305 Reduce Warehouse Dropdown List in Wareneingang (POS)
    Validation  for the eligible destinaiton warehouses in Wareneingang pos

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/adempiere/form/terminal/context/ITerminalContext.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/adempiere/form/terminal/context/ITerminalContext.java
@@ -13,15 +13,14 @@ package de.metas.adempiere.form.terminal.context;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-
 
 import java.awt.Dimension;
 import java.util.Properties;
@@ -51,6 +50,14 @@ public interface ITerminalContext extends IContextAware, IPropertiesContainer
 {
 	@Override
 	public Properties getCtx();
+
+	/**
+	 * Set a particular property
+	 * 
+	 * @param contextName
+	 * @param valueInt
+	 */
+	void setContextValue(String contextName, int valueInt);
 
 	/**
 	 * @return always return {@link ITrx#TRXNAME_None}.
