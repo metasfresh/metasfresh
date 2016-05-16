ca6b777019de74897219e40b60f40cd33b79b53b
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-05-16 18:28:44 +0300

    FRESH-307: Flag IsApplySecuritySetting in AD_Process
    I added the flag in the AD_Process tablke and also set the flag on true
    for the porcesses that I already know that are suitable for it.

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5445450_sys_FRESH-307_Set_Processes_For_Security_Setting.sql
@@ -0,0 +1,52 @@
+
+
+-- 16.05.2016 18:16
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:16:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540564
+;
+
+-- 16.05.2016 18:17
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:17:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540582
+;
+
+-- 16.05.2016 18:17
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:17:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540588
+;
+
+-- 16.05.2016 18:17
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:17:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540672
+;
+
+-- 16.05.2016 18:18
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:18:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540595
+;
+
+-- 16.05.2016 18:19
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:19:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540636
+;
+
+-- 16.05.2016 18:20
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:20:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540678
+;
+
+-- 16.05.2016 18:20
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:20:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540643
+;
+
+-- 16.05.2016 18:20
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:20:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540646
+;
+
+-- 16.05.2016 18:21
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsApplySecuritySettings='Y',Updated=TO_TIMESTAMP('2016-05-16 18:21:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540662
+;
+
