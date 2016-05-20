cf12166f8e87e38d2648295963e063a0f11c9d24
Teo Sarca <teo.sarca@gmail.com>
2016-05-20 18:03:11 +0300

    PropertyValuesDTO: validate value type

--- src/main/java/de/metas/ui/web/vaadin/window/model/PropertyValuesDTO.java
+++ src/main/java/de/metas/ui/web/vaadin/window/model/PropertyValuesDTO.java
@@ -1,14 +1,22 @@
 package de.metas.ui.web.vaadin.window.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableList;
 import com.google.gwt.thirdparty.guava.common.collect.ImmutableMap;
 
+import de.metas.ui.web.vaadin.window.GridRowId;
 import de.metas.ui.web.vaadin.window.PropertyName;
+import de.metas.ui.web.vaadin.window.editor.LookupValue;
 import de.metas.ui.web.vaadin.window.editor.NullValue;
 
 /*
@@ -110,6 +118,18 @@ public final class PropertyValuesDTO implements Serializable
 
 	public static final class Builder
 	{
+		private static final ImmutableList<Class<?>> allowedValueTypes = ImmutableList.of( //
+				NullValue.class //
+				, GridRowId.class //
+				, String.class //
+				, Integer.class, BigDecimal.class //
+				, Date.class, Timestamp.class // FIXME: i think we shall avoid Timestamp
+				, Boolean.class //
+				, SqlLazyLookupDataSource.class // FIXME: tolerate it for now
+				, ArrayList.class // FIXME: tolerate it for now => introduce a new datatype for List<PropertyValuesDTO>
+				, LookupValue.class //
+		);
+
 		private final ImmutableMap.Builder<PropertyName, Object> valuesBuilder = ImmutableMap.builder();
 
 		private Builder()
@@ -129,6 +149,31 @@ public final class PropertyValuesDTO implements Serializable
 
 		public Builder put(final PropertyName propertyName, final Object value)
 		{
+			//
+			// Validate the value type
+			// NOTE: the aim is to have primitive or well known values types because, later, we want to serialize to JSON
+			if (value != null)
+			{
+				final Class<? extends Object> valueClass = value.getClass();
+				if (allowedValueTypes.contains(valueClass))
+				{
+					// ok
+				}
+				else if (Supplier.class.isAssignableFrom(valueClass))
+				{
+					// tolerate it for now
+
+					// e.g.
+					// Suppliers.memoize(de.metas.ui.web.vaadin.window.datasource.sql.SqlModelDataSource$$Lambda$34/1396214905@5e071179)
+					// see de.metas.ui.web.vaadin.window.datasource.sql.SqlModelDataSource.retrieveRecordsSupplier(ModelDataSourceQuery)
+				}
+				else
+				{
+					throw new IllegalArgumentException("Invalid value class: " + valueClass
+							+ "\n Allowed types are: " + allowedValueTypes);
+				}
+			}
+
 			final Object valueNotNull = NullValue.valueOrNull(value);
 			valuesBuilder.put(propertyName, valueNotNull);
 			return this;
