0c0bc65d6c3ba9fc15bd81a0467232a839cfe710
Teo Sarca <teo.sarca@gmail.com>
2016-05-25 17:05:08 +0300

    introduce PropertyPath

---
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/window/shared/datatype/PropertyPath.java
@@ -0,0 +1,126 @@
+package de.metas.ui.web.window.shared.datatype;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+
+import de.metas.ui.web.window.PropertyName;
+
+/*
+ * #%L
+ * metasfresh-webui-api
+ * %%
+ * Copyright (C) 2016 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+@SuppressWarnings("serial")
+public final class PropertyPath implements Serializable
+{
+	public static final PropertyPath of(final PropertyName propertyName)
+	{
+		final PropertyName gridPropertyName = null;
+		final Object rowId = null;
+		return new PropertyPath(gridPropertyName, rowId, propertyName);
+	}
+
+	public static final PropertyPath of(final PropertyName gridPropertyName, final Object rowId, final PropertyName propertyName)
+	{
+		return new PropertyPath(gridPropertyName, rowId, propertyName);
+	}
+
+	@JsonProperty("gridPropertyName")
+	private final PropertyName gridPropertyName;
+	@JsonProperty("rowId")
+	private final Object rowId;
+	@JsonProperty("propertyName")
+	private final PropertyName propertyName;
+
+	private PropertyPath(
+			@JsonProperty("gridPropertyName") final PropertyName gridPropertyName //
+			, @JsonProperty("rowId") final Object rowId //
+			, @JsonProperty("propertyName") final PropertyName propertyName)
+	{
+		super();
+		this.gridPropertyName = gridPropertyName;
+		this.rowId = rowId;
+		this.propertyName = propertyName;
+	}
+
+	@Override
+	public String toString()
+	{
+		return MoreObjects.toStringHelper(this)
+				.add("gridPropertyName", gridPropertyName)
+				.add("rowId", rowId)
+				.add("propertyName", propertyName)
+				.toString();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(gridPropertyName, rowId, propertyName);
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+
+		if (getClass() != obj.getClass())
+		{
+			return false;
+		}
+
+		final PropertyPath other = (PropertyPath)obj;
+		return Objects.equals(this.gridPropertyName, other.gridPropertyName)
+				&& Objects.equals(this.rowId, other.rowId)
+				&& Objects.equals(this.propertyName, other.propertyName);
+	}
+
+	public PropertyName getPropertyName()
+	{
+		return propertyName;
+	}
+
+	public PropertyName getGridPropertyName()
+	{
+		return gridPropertyName;
+	}
+
+	public Object getRowId()
+	{
+		return rowId;
+	}
+
+	@JsonIgnore
+	public boolean isGridProperty()
+	{
+		return gridPropertyName != null;
+	}
+}
