d43de69c5fccbca94a8426fabf0d9139fc80e2d5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-06-13 13:41:20 +0200

    FRESH-386 fixing the mapped-products tab in the product window

--- de.metas.business/src/main/sql/postgresql/ddl/views/M_Product_Mapped_V.sql
+++ de.metas.business/src/main/sql/postgresql/ddl/views/M_Product_Mapped_V.sql
@@ -2,17 +2,22 @@
 DROP VIEW If EXISTS M_Product_Mapped_V;
 CREATE VIEW M_Product_Mapped_V AS
 SELECT 
-	p.AD_Client_ID, 
-	p.AD_Org_ID,
-	p.Created,
-	p.CreatedBy,
-	p.IsActive,
-	p.Updated,
-	p.UpdatedBy,
-	p.M_Product_ID,
-	p.M_Product_ID AS M_Product_Mapped_V_ID,
-	m.M_Product_Mapping_ID
-FROM M_Product_Mapping m
-	JOIN M_Product p ON p.M_Product_Mapping_ID=m.M_Product_Mapping_ID
+DISTINCT
+	p.M_Product_Mapping_ID,
+	p2.M_Product_ID,
+	p2.M_Product_ID AS M_Product_Mapped_V_ID,
+	p2.AD_Client_ID, 
+	p2.AD_Org_ID,
+	p2.Created,
+	p2.CreatedBy,
+	p2.IsActive,
+	p2.Updated,
+	p2.UpdatedBy
+FROM 
+	M_Product p 
+	JOIN M_Product p2 ON p2.M_Product_Mapping_ID=p.M_Product_Mapping_ID
+	
+	/* only joining M_Product_Mapping because we want to exclude inactive mappings */
+	JOIN M_Product_Mapping m ON m.M_Product_Mapping_ID=p.M_Product_Mapping_ID 
 WHERE true
 	AND m.IsActive='Y';
