ed01a2c5cf98afa387d19a023661a2a38c7d0f5c
Teo Sarca <teo.sarca@gmail.com>
2016-06-25 10:50:16 +0300

    FRESH-402: introduce flatrateterm fields to RfQ

--- de.metas.procurement.base/src/main/java/de/metas/procurement/base/rfq/model/I_C_RfQResponseLine.java
+++ de.metas.procurement.base/src/main/java/de/metas/procurement/base/rfq/model/I_C_RfQResponseLine.java
@@ -1,5 +1,6 @@
 package de.metas.procurement.base.rfq.model;
 
+import de.metas.flatrate.model.I_C_Flatrate_DataEntry;
 import de.metas.procurement.base.model.I_PMM_Product;
 
 /*
@@ -15,21 +16,29 @@ import de.metas.procurement.base.model.I_PMM_Product;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-public interface I_C_RfQResponseLine extends de.metas.rfq.model.I_C_RfQResponseLine 
+public interface I_C_RfQResponseLine extends de.metas.rfq.model.I_C_RfQResponseLine
 {
 	//@formatter:off
-	public static final String COLUMNNAME_PMM_Product_ID = "PMM_Product_ID";
+	String COLUMNNAME_PMM_Product_ID = "PMM_Product_ID";
 	int getPMM_Product_ID();
 	I_PMM_Product getPMM_Product();
 	void setPMM_Product(I_PMM_Product PMM_Product);
+	//@formatter:on
+
+	//@formatter:off
+	String COLUMNNAME_C_Flatrate_Term_ID = "C_Flatrate_Term_ID";
+	org.adempiere.model.ModelColumn<I_C_Flatrate_DataEntry, de.metas.flatrate.model.I_C_Flatrate_Term> COLUMN_C_Flatrate_Term_ID = new org.adempiere.model.ModelColumn<I_C_Flatrate_DataEntry, de.metas.flatrate.model.I_C_Flatrate_Term>(I_C_Flatrate_DataEntry.class, "C_Flatrate_Term_ID", de.metas.flatrate.model.I_C_Flatrate_Term.class);
+	int getC_Flatrate_Term_ID();
+	de.metas.procurement.base.model.I_C_Flatrate_Term getC_Flatrate_Term();
+	void setC_Flatrate_Term(de.metas.flatrate.model.I_C_Flatrate_Term C_Flatrate_Term);
 	//@formatter:on
 }
