bbece56188607eb28df629d72380c5b1c69471f3
Teo Sarca <teo.sarca@gmail.com>
2016-06-30 13:35:02 +0300

    FRESH-402: minor

--- de.metas.rfq/src/main/java/de/metas/rfq/process/C_RfQ_Publish.java
+++ de.metas.rfq/src/main/java/de/metas/rfq/process/C_RfQ_Publish.java
@@ -23,12 +23,12 @@ import de.metas.rfq.model.I_C_RfQResponse;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -37,7 +37,7 @@ import de.metas.rfq.model.I_C_RfQResponse;
 
 /**
  * Publish RfQ bidding invitations.
- * 
+ *
  * @author metas-dev <dev@metas-fresh.com>
  *
  */
@@ -48,7 +48,7 @@ public class C_RfQ_Publish extends SvrProcess implements ISvrProcessPrecondition
 	private final transient IRfqDAO rfqDAO = Services.get(IRfqDAO.class);
 
 	@Override
-	public boolean isPreconditionApplicable(GridTab gridTab)
+	public boolean isPreconditionApplicable(final GridTab gridTab)
 	{
 		final I_C_RfQ rfq = InterfaceWrapperHelper.create(gridTab, I_C_RfQ.class);
 		return rfqBL.isCompleted(rfq);
@@ -63,7 +63,7 @@ public class C_RfQ_Publish extends SvrProcess implements ISvrProcessPrecondition
 		final IRfQConfiguration rfqConfiguration = Services.get(IRfQConfiguration.class);
 		final IRfQResponsePublisher rfqPublisher = rfqConfiguration.getRfQResponsePublisher();
 
-		for (I_C_RfQResponse rfqResponse : rfqDAO.retrieveAllResponses(rfq))
+		for (final I_C_RfQResponse rfqResponse : rfqDAO.retrieveAllResponses(rfq))
 		{
 			if (!rfqBL.isDraft(rfqResponse))
 			{
