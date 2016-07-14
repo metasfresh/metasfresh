a30b431d80e53a19f8a2ae490ef9fd5160e74de6
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-07-14 17:06:03 +0300

    FRESH-531 #212 MRPProductInfo Display Conference with Sys Config

--- de.metas.fresh/de.metas.fresh.base/src/main/java/de/metas/fresh/gui/search/MRPInfoWindow.java
+++ de.metas.fresh/de.metas.fresh.base/src/main/java/de/metas/fresh/gui/search/MRPInfoWindow.java
@@ -13,11 +13,11 @@ package de.metas.fresh.gui.search;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
@@ -76,6 +76,10 @@ public class MRPInfoWindow extends InfoSimple
 
 	private static final String SYSCONFIG_DAYS_RANGE = "de.metas.fresh.MRPProductInfo.dayRange";
 
+	// FRESH-531
+	// Possibility to switch the conference displaying on and off
+	private static final String SYSCONFIG_DisplayConferenceFlag = "de.metas.fresh.MRPProductInfo.DisplayConferenceFlag";
+
 	private static final String MSG_ResetSortFilters = "de.metas.fresh.MRPProductInfo.ResetSortFilters";
 
 	private CButton mrpButton = null;
@@ -131,29 +135,35 @@ public class MRPInfoWindow extends InfoSimple
 		//
 		// Conference Sort Preferences CheckBox
 		{
-			isConferenceSortPreferencesCheckBox = new CCheckBox(msgBL.translate(ctx, I_AD_User_SortPref_Hdr.COLUMNNAME_IsConference));
-			isConferenceSortPreferencesCheckBox.addActionListener(new ActionListener()
+			final boolean isDisplayConference = Services.get(ISysConfigBL.class).getBooleanValue(SYSCONFIG_DisplayConferenceFlag, false);
+
+			// FRESH-531: Only display the Conference flag if the DisplayConferenceFlag Sys Config's value is on true
+			if (isDisplayConference)
 			{
-				@Override
-				public void actionPerformed(final ActionEvent e)
+				isConferenceSortPreferencesCheckBox = new CCheckBox(msgBL.translate(ctx, I_AD_User_SortPref_Hdr.COLUMNNAME_IsConference));
+				isConferenceSortPreferencesCheckBox.addActionListener(new ActionListener()
 				{
-					//
-					// Set conference mode on/off
-					final Boolean isConferenceSortPreferences = (Boolean)isConferenceSortPreferencesCheckBox.getValue();
-					setConferenceSortPreferences(isConferenceSortPreferences);
+					@Override
+					public void actionPerformed(final ActionEvent e)
+					{
+						//
+						// Set conference mode on/off
+						final Boolean isConferenceSortPreferences = (Boolean)isConferenceSortPreferencesCheckBox.getValue();
+						setConferenceSortPreferences(isConferenceSortPreferences);
 
-					//
-					// Set initial sorting preferences to conference/user
-					initSortingPreferences(ctx);
+						//
+						// Set initial sorting preferences to conference/user
+						initSortingPreferences(ctx);
 
-					//
-					// Clear and refresh sort options
-					p_table.setReloadOriginalSorting(true); // shall clear user sorting
-					p_table.clearSortCriteria(); // reset possible user sorting and sort by our preferences
-					repaint();
-				}
-			});
-			parameterPanel.add(isConferenceSortPreferencesCheckBox);
+						//
+						// Clear and refresh sort options
+						p_table.setReloadOriginalSorting(true); // shall clear user sorting
+						p_table.clearSortCriteria(); // reset possible user sorting and sort by our preferences
+						repaint();
+					}
+				});
+				parameterPanel.add(isConferenceSortPreferencesCheckBox);
+			}
 		}
 		//
 		// Sort Filters Button
