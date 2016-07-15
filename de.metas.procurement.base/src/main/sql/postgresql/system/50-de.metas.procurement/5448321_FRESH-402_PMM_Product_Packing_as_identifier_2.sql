31871d5fe9c7aa4b407e4c0e016b9c18496195eb
Teo Sarca <teo.sarca@gmail.com>
2016-07-15 11:54:47 +0300

    FRESH-402: PMM_Product: use packing instructions as identifier

---
+++ de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5448321_FRESH-402_PMM_Product_Packing_as_identifier_2.sql
@@ -0,0 +1,5 @@
+-- 14.07.2016 18:09
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsIdentifier='Y', SeqNo=30,Updated=TO_TIMESTAMP('2016-07-14 18:09:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=554216
+;
+
