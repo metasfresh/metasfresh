7b364a5fad2729e9e06212773c40029b1f0e7531
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-07-15 18:49:16 +0300

    FRESH-487 #152 Name updated

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/43-de.metas.document/5448460_sys_FRESH_487_MissingCounterDocuments_NameUpdated.sql
@@ -0,0 +1,72 @@
+-- 15.07.2016 18:09
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Fehlender/Unvollständiger Gegenbeleg',Updated=TO_TIMESTAMP('2016-07-15 18:09:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540742
+;
+
+-- 15.07.2016 18:09
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET IsTranslated='N' WHERE AD_Tab_ID=540742
+;
+
+
+
+-- 15.07.2016 18:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Name='toUpdate',Updated=TO_TIMESTAMP('2016-07-15 18:36:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Name='Fehlender/Unvollständiger Gegenbeleg',Updated=TO_TIMESTAMP('2016-07-15 18:36:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:36
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='toUpdate',Updated=TO_TIMESTAMP('2016-07-15 18:37:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540296
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET IsTranslated='N' WHERE AD_Window_ID=540296
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='toUpdate',Updated=TO_TIMESTAMP('2016-07-15 18:37:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='Fehlender/Unvollständiger Gegenbeleg',Updated=TO_TIMESTAMP('2016-07-15 18:37:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540296
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET IsTranslated='N' WHERE AD_Window_ID=540296
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Fehlender/Unvollständiger Gegenbeleg',Updated=TO_TIMESTAMP('2016-07-15 18:37:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540714
+;
+
+-- 15.07.2016 18:37
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=540714
+;
+
