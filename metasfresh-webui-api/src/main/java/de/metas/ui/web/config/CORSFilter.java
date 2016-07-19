6eeb272ca4af70c4de5f43a0b0e908369d8503c0
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-07-19 10:36:44 +0200

    FRESH-112: adding one more allowed header as requested by @damianprzygodzki to fix..
    ...the error "Content-Type is not allowed by
    Access-Control-Allow-Headers"

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/config/CORSFilter.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/config/CORSFilter.java
@@ -22,12 +22,12 @@ import org.springframework.stereotype.Component;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -53,12 +53,16 @@ public class CORSFilter implements Filter
 		if (response instanceof HttpServletResponse)
 		{
 			// FIXME: allow CORS for the whole application !!!
-			
+
 			HttpServletResponse httpResponse = (HttpServletResponse)response;
 			httpResponse.setHeader("Access-Control-Allow-Origin", "*");
 			httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 			httpResponse.setHeader("Access-Control-Max-Age", "3600");
-			httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+
+			// adding one more allowed header as requested by @damianprzygodzki to fix the error
+			// "Content-Type is not allowed by Access-Control-Allow-Headers"
+			// httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+			httpResponse.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
 		}
 		chain.doFilter(request, response);
 	}
