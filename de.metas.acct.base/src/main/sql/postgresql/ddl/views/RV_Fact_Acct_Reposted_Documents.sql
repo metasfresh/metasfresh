90886a3e3c6ac9f0cbb9ba0d76104cb09efe0f47
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-07-26 17:38:19 +0300

    FRESH-539 #225 SQL Part

---
+++ de.metas.acct.base/src/main/sql/postgresql/ddl/views/RV_Fact_Acct_Reposted_Documents.sql
@@ -0,0 +1,27 @@
+DROP VIEW IF EXISTS de_metas_acct.Reposted_Documents ;
+
+
+
+CREATE OR REPLACE VIEW de_metas_acct.Reposted_Documents 
+AS 
+SELECT 	
+
+pinst.AD_Client_ID,
+pinst.AD_Org_ID,
+pinst.Created,
+pinst.CreatedBy,
+pinst.Updated,
+pinst.UpdatedBy,
+pinst.IsActive,
+
+
+to_number (regexp_matches(p_msg, '\d+')::text, '99999999999999999') AS table_ID ,
+to_number (regexp_matches(substr(p_msg, position('Record_ID =' IN p_msg) + length('Record_ID =')), '\d+')::text, '99999999999999999' ) AS record_ID 
+	
+FROM AD_PInstance_Log pilog
+
+JOIN AD_PInstance pinst ON pilog.AD_PInstance_ID = pinst.AD_PInstance_ID
+JOIN AD_Process process ON pinst.AD_Process_ID = process.AD_Process_ID 
+
+WHERE process.value = 'Documents_FactAcct_Creation_For_Posted'
+;
