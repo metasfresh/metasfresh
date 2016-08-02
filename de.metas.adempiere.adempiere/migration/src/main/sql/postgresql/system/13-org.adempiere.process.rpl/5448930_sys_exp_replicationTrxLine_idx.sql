6c39827e259bb0bdf95ee4c49b9c6d4863e1532b
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-08-02 11:26:26 +0200

    minor: solving a perf problem with a large number of
    exp_replicationtrxline records

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/13-org.adempiere.process.rpl/5448930_sys_exp_replicationTrxLine_idx.sql
@@ -0,0 +1,5 @@
+﻿
+DROP INDEX IF EXISTS exp_replicationtrxline_exp_replicationtrx_id;
+
+CREATE INDEX exp_replicationtrxline_exp_replicationtrx_id
+   ON exp_replicationtrxline (exp_replicationtrx_id ASC NULLS LAST);
