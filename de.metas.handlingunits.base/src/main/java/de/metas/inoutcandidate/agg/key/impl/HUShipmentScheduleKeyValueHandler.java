ac9958817ef49e4090c95badc8f0209e32c2333f
Teo Sarca <teo.sarca@gmail.com>
2016-08-11 20:24:12 +0300

    FRESH-578 #275 consider HU attributes when aggregating shipment lines

--- de.metas.handlingunits.base/src/main/java/de/metas/inoutcandidate/agg/key/impl/HUShipmentScheduleKeyValueHandler.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/inoutcandidate/agg/key/impl/HUShipmentScheduleKeyValueHandler.java
@@ -59,6 +59,8 @@ public class HUShipmentScheduleKeyValueHandler implements IAggregationKeyValueHa
 
 		final I_M_HU luHU = schedWithHU.getM_LU_HU();
 		values.addAll(getValues(luHU));
+		
+		values.add(schedWithHU.getAttributesAggregationKey());
 
 		return values;
 	}
