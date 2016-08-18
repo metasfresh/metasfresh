986e8d39ee38d82310b75edef60850f89f5075f6
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-08-18 10:15:47 +0200

    FRESH-582 #279: moving this script to endcustomer-mf15

---
+++ de.metas.endcustomer.mf15.base/src/main/sql/postgresql/system/90-de.metas.endcustomer.mf15/5449000_cli_FRESH-582_doctype_purchaseorder_default.sql
@@ -0,0 +1,5 @@
+-- 09.08.2016 16:33
+-- URL zum Konzept
+INSERT INTO AD_Preference (AD_Preference_ID, AD_Client_ID, AD_Org_ID, IsActive, Created,CreatedBy,Updated,UpdatedBy,AD_Window_ID, AD_User_ID, Attribute, Value) VALUES (545308,1000000,0, 'Y',now(),100,now(),100, 181,NULL,'C_DocTypeTarget_ID','1000016')
+;
+
