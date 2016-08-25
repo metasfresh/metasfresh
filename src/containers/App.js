42dbf31fe3d849be1c1171af22eda0805c050ec9
damian.przygodzki <damian.przygodzki@jazzy.pro>
2016-08-25 09:51:55 +0200

    connection lost handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,10 +9,25 @@ import config from '../config.js';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Router, Route, browserHistory } from 'react-router';
 
+import {
+    noConnection
+} from '../actions/WindowActions'
+
 const store = configureStore(browserHistory);
 const history = syncHistoryWithStore(browserHistory, store);
 
 axios.defaults.withCredentials = true;
+
+axios.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    if(!error.response){
+        store.dispatch(noConnection());
+    }
+
+    return Promise.reject(error);
+});
+
 
 export default class App extends Component {
     render() {
