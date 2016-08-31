fed2098387de894f34198c5eadc19601e380f72a
Teo Sarca <teo.sarca@gmail.com>
2016-08-31 17:07:34 +0300

    FRESH-112: swagger: include only /window API for now

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/config/SwaggerConfig.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package de.metas.ui.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import de.metas.ui.web.window.controller.WindowRestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -40,7 +41,7 @@ public class SwaggerConfig
 	{
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage(WindowRestController.class.getPackage().getName()))
 				.paths(PathSelectors.any())
 				.build();
 	}
