df2575be73b6ea5f65725ff48e8342556c432f7e
metas-lc <liana.cotuna@metasfresh.com>
2016-09-02 11:39:02 +0300

    FRESH-567 #262
    more docu

--- de.metas.handlingunits.base/src/main/sql/postgresql/ddl/functions/recursive_hu_trace.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/ddl/functions/recursive_hu_trace.sql
@@ -1,5 +1,5 @@
 /* 
- use: it takes an M_HU_Trx_Line and returns all HUs and transactions which come from splitting the HU from the given M_HU_Trx_Line.
+ use: it takes an M_HU_Trx_Line and returns all HUs and transactions which come from splitting the HU from the given M_HU_Trx_Line. Note that it will return HU duplicates so you might want to select distinct if you only need HUs
  you can uncomment "RAISE NOTICE" to debug (but don't forget to comment them back when done)
  this function calls recursively "de.metas.handlingunits".recursive_hu_trace_sub function
  first it calls the function with the given parameter, and then with the set of results (rez) of the same function (if function returns 001 and 002, we call the function with [001, 002])
@@ -34,7 +34,7 @@ CREATE OR REPLACE FUNCTION "de.metas.handlingunits".recursive_hu_trace(trxline i
   BEGIN
 	while(0 != any(rez))
 	LOOP
-		for r in (SELECT m_hu_trx_line_id::integer, m_hu_id::integer FROM "de.metas.handlingunits".recursive_hu_trace_sub(rez)) loop
+		for r in (SELECT m_hu_trx_line_id::integer, m_hu_id::integer FROM "de.metas.handlingunits".recursive_hu_trace_sub(rez)) loop 
 			if (Array[r.m_hu_trx_line_id] <@ all_trx) 
 			then last_trx := last_trx;
 			--RAISE NOTICE 'removed %',r.m_hu_trx_line_id;
@@ -55,3 +55,61 @@ CREATE OR REPLACE FUNCTION "de.metas.handlingunits".recursive_hu_trace(trxline i
     RETURN query execute v_sql || array_to_string(all_trx,',') || ']::integer[]);' ;
 END;
 $$ LANGUAGE plpgsql;
+
+/*
+Testcase to check the function. It also helps if we need to modify recursive_hu_trace or recursive_hu_trace_sub
+
+I did a purchase order with 10 IFCOx5. Went to receipt pos and moved the HUs in an empty warehouse (for visibility purpose). 
+Went to Handling Units pos, opened my warehouse and split my HUs 3 times as following:  
+
+My HUs: (5 x 10 x 1)
+LU 1000650 (active)
+TU 1000651,1000653,1000655,1000657,1000659, (destroyed by split)
+   1000661,1000663,1000665,1000667,1000669 (active)
+
+1st Split (5 x 5 x 1 from 1000650)
+LU 1000671 (active)
+TU 1000672,1000674,1000676 (destroyed by split)
+   1000678,1000680 (active)
+
+2nd Split (5 x 3 x 1 from 1000671)
+LU 1000682 (destroyed by split)
+TU 1000683, 1000685, 1000687 (destroyed by split)
+
+3rd Split (5 x 2 x 2 from 1000682)
+LU 1000689 (active)
+TU 1000690, 1000692 (active)
+
+LU 1000694 (active)
+TU 1000695 (active)
+------------------------
+
+Run the function for the M_HU_TRX_Line(s) of my HUs 
+find the trx for e.g. TU 1000651 : select m_hu_trx_line_id,* from m_hu_trx_line where m_hu_id = 1000651
+select * from "de.metas.handlingunits".recursive_hu_trace(m_hu_trx_line_id)
+
+LU 1000650: 1000650
+
+TU:
+1000651: 1000651, 1000672, 1000683, 1000690 
+1000653: 1000653, 1000674, 1000685, 1000692, 1000671
+1000655: 1000655, 1000676, 1000687, 1000695
+1000657: 1000657, 1000678, 1000671
+1000659: 1000659, 1000680, 1000671
+1000661: 1000661, 1000671
+1000663: 1000663
+1000665: 1000665
+1000667: 1000667
+1000669: 1000669
+
+Run the function for the M_HU_TRX_Line(s) of last split HUs (the other way around)
+
+LU 1000689: 1000655, 1000676, 1000687, 1000695, 1000653, 1000674, 1000685, 1000692, 1000651, 1000672, 1000683, 1000690
+
+TU
+1000690: 1000651, 1000672, 1000683, 1000690
+1000692: 1000653, 1000674, 1000685, 1000692
+
+(note: here i put only the distinct HUs. They can appear more because of multiple transaction lines)
+
+*/
