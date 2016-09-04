f4e22365a2a2e9e4372d158ac14ee5c2fd874930
Teo Sarca <teo.sarca@gmail.com>
2016-09-04 12:01:02 +0300

    FRESH-112 update SwaggerConfig

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/config/SwaggerConfig.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/config/SwaggerConfig.java
@@ -1,9 +1,18 @@
 package de.metas.ui.web.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+import de.metas.ui.web.debug.DebugRestController;
+import de.metas.ui.web.menu.MenuRestController;
 import de.metas.ui.web.window.controller.WindowRestController;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -20,12 +29,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -41,8 +50,20 @@ public class SwaggerConfig
 	{
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage(WindowRestController.class.getPackage().getName()))
+				.apis(basePackages(WindowRestController.class, MenuRestController.class, DebugRestController.class))
 				.paths(PathSelectors.any())
 				.build();
+	}
+
+	private static final Predicate<RequestHandler> basePackages(final Class<?>... classes)
+	{
+		final Set<Predicate<RequestHandler>> predicates = new HashSet<>(classes.length);
+		for (final Class<?> clazz : classes)
+		{
+			final String packageName = clazz.getPackage().getName();
+			predicates.add(RequestHandlerSelectors.basePackage(packageName));
+		}
+
+		return Predicates.or(predicates);
 	}
 }
