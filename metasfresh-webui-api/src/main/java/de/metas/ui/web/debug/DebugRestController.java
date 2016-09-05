fbaaa33ac6a815411c8032b20bf86be02667cdfd
Teo Sarca <teo.sarca@gmail.com>
2016-09-05 11:49:15 +0300

    FRESH-112 DebugRestController: AD_Language

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/debug/DebugRestController.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/debug/DebugRestController.java
@@ -1,13 +1,17 @@
 package de.metas.ui.web.debug;
 
 import org.compiere.util.CacheMgt;
+import org.compiere.util.DisplayType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.metas.ui.web.config.WebConfig;
 import de.metas.ui.web.menu.MenuTreeRepository;
+import de.metas.ui.web.session.UserSession;
+import de.metas.ui.web.window.datatypes.json.JSONFilteringOptions;
 import de.metas.ui.web.window.model.DocumentCollection;
 
 /*
@@ -20,12 +24,12 @@ import de.metas.ui.web.window.model.DocumentCollection;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -39,8 +43,11 @@ public class DebugRestController
 	public static final String ENDPOINT = WebConfig.ENDPOINT_ROOT + "/debug";
 
 	@Autowired
+	private UserSession userSession;
+
+	@Autowired
 	private DocumentCollection documentCollection;
-	
+
 	@Autowired
 	private MenuTreeRepository menuTreeRepo;
 
@@ -50,6 +57,27 @@ public class DebugRestController
 		CacheMgt.get().reset(); // FIXME: debugging - while debugging is useful to reset all caches
 		documentCollection.cacheReset();
 		menuTreeRepo.cacheReset();
+	}
+
+	private static final void logResourceValueChanged(final String name, final Object value, final Object valueOld)
+	{
+		System.out.println("*********************************************************************************************");
+		System.out.println("Changed " + name + " " + valueOld + " -> " + value);
+		System.out.println("*********************************************************************************************");
+
+	}
+
+	@RequestMapping(value = "/language", method = RequestMethod.PUT)
+	public void setAD_Language(@RequestBody final String adLanguage)
+	{
+		final String adLanguageOld = userSession.setAD_Language(adLanguage);
+		logResourceValueChanged("AD_Language", adLanguage, adLanguageOld);
+	}
+
+	@RequestMapping(value = "/language", method = RequestMethod.GET)
+	public String getAD_Language()
+	{
+		return userSession.getAD_Language();
 	}
 
 }
