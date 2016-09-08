5ee5451ee7ac173262f2a57428f972b19b12d629
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2016-09-08 14:24:41 +0200

    FRESH-567 #262 move documentation into comment

--- de.metas.handlingunits.base/src/main/sql/postgresql/ddl/functions/recursive_hu_trace_sub.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/ddl/functions/recursive_hu_trace_sub.sql
@@ -1,15 +1,3 @@
-/* 
- this function is currently used in "de.metas.handlingunits".recursive_hu_trace function
- use: it takes an array of M_HU_Trx_Line(s) and returns for each M_HU_Trx_Line the directly related M_HU_Trx_Line(s) which come from splitting an HU.
- in case an HU was split, you will have at least 2 M_HU_Trx_Line(s) with qty!=0 (one with + and one with -) which have on parent_hu_trx_line_id the M_HU_Trx_Line_ID of each other. The new split HU lies in it's parent.    
- if the new HU is split again, then there is another M_HU_Trx_Line (a third one) with this HU. 
- that's why first we search for the "latest" M_HU_Trx_Line, and if there is none we stay with the parent.
- You can picture this function in layers:
- Layer 1 is the transaction you have + the parent. Parent is the split HU. 
- Layer 2 is the third transaction, with the same HU from Layer 1, and it announces a second split. If there is no second split, then there is just 1 layer. This is where this function stops.
- To get to layer 3, you just have to call this function with the transaction(s) from Layer 2. And so on, until the HU is not split any more 
-*/
-
 DROP FUNCTION IF EXISTS "de.metas.handlingunits".recursive_hu_trace_sub(trxline integer[]);
 CREATE OR REPLACE FUNCTION "de.metas.handlingunits".recursive_hu_trace_sub(trxline integer[]) RETURNS TABLE
 (
@@ -44,3 +32,16 @@ BEGIN
     RETURN query execute v_sql || array_to_string(new_trxline,',') || ']);' ;
 END;
 $$ LANGUAGE plpgsql;
+COMMENT ON FUNCTION "de.metas.handlingunits".recursive_hu_trace_sub(integer[]) IS '
+ this function is currently used in "de.metas.handlingunits".recursive_hu_trace function
+ use: it takes an array of M_HU_Trx_Line(s) and returns for each M_HU_Trx_Line the directly related M_HU_Trx_Line(s) which come from splitting an HU.
+ in case an HU was split, you will have at least 2 M_HU_Trx_Line(s) with qty!=0 (one with + and one with -) which have on parent_hu_trx_line_id the M_HU_Trx_Line_ID of each other. The new split HU lies in it''s parent.    
+ if the new HU is split again, then there is another M_HU_Trx_Line (a third one) with this HU. 
+ that''s why first we search for the "latest" M_HU_Trx_Line, and if there is none we stay with the parent.
+ You can picture this function in layers:
+ Layer 1 is the transaction you have + the parent. Parent is the split HU. 
+ Layer 2 is the third transaction, with the same HU from Layer 1, and it announces a second split. If there is no second split, then there is just 1 layer. This is where this function stops.
+ To get to layer 3, you just have to call this function with the transaction(s) from Layer 2. And so on, until the HU is not split any more 
+
+SEE https://github.com/metasfresh/metasfresh/issues/262
+';
