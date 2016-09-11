5174ffeb2a2d72f9d957718c7024f96095014363
Teo Sarca <teo.sarca@gmail.com>
2016-09-11 16:30:04 +0300

    FRESH-112 fix compile error

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/window/descriptor/factory/standard/GridTabVOElementsFactory.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/window/descriptor/factory/standard/GridTabVOElementsFactory.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
