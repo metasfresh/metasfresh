293daad156be6a93bb17c562a3f26cc29b845334
Teo Sarca <teo.sarca@gmail.com>
2016-09-12 18:11:17 +0300

    FRESH-657 optimize CalloutExecutor.executeAll
    * don't execute callouts which were already executed
    * collect execution statistics (if tracing)

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/callout/api/impl/CalloutExecutor.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/callout/api/impl/CalloutExecutor.java
@@ -3,11 +3,13 @@ package org.adempiere.ad.callout.api.impl;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.adempiere.ad.callout.api.ICalloutExecutor;
@@ -25,6 +27,7 @@ import org.compiere.util.Env;
 import org.slf4j.Logger;
 
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Stopwatch;
 
 import de.metas.logging.LogManager;
 
@@ -37,12 +40,19 @@ public final class CalloutExecutor implements ICalloutExecutor
 
 	private static final Logger logger = LogManager.getLogger(CalloutExecutor.class);
 
+	//
+	// Configuration (sharable data)
 	private final String tableName;
 	private final TableCalloutsMap tableCalloutsMap;
+	/** Set of callout IDs which are banned from execution because they failed on initialization */
 	private final Set<String> executionBlackListIds;
 
 	//
+	// Current state data (don't share between CalloutExecutor instances)
+	/** A set of callouts which are currently running */
 	private final Set<ICalloutInstance> activeCalloutInstances = new HashSet<>();
+	/** When enabled, contains a set of already executed callouts */
+	private volatile Set<ICalloutInstance> executedCalloutsTrace = null;
 
 	private CalloutExecutor(
 			final String tableName,
@@ -103,28 +113,73 @@ public final class CalloutExecutor implements ICalloutExecutor
 	@Override
 	public void executeAll(final Function<String, ICalloutField> calloutFieldsSupplier)
 	{
-		for (final Map.Entry<String, Collection<ICalloutInstance>> columnNameAndCallouts : tableCalloutsMap.getColumnNamesAndCalloutsEntries())
-		{
-			//
-			// Get the callout field from supplier
-			final String columnName = columnNameAndCallouts.getKey();
-			final ICalloutField field = calloutFieldsSupplier.apply(columnName);
-			if (field == null)
-			{
-				logger.trace("Skip executing all callouts for {} because the supplied field was null", columnName);
-			}
-			if (!isEligibleForExecuting(field))
-			{
-				logger.trace("Skip executing callouts for {} because it's not eligible", field);
-				return;
-			}
+		Check.assumeNotNull(calloutFieldsSupplier, "Parameter calloutFieldsSupplier is not null");
 
-			//
-			// Execute all callouts for current callout field
-			final Collection<ICalloutInstance> callouts = columnNameAndCallouts.getValue();
-			for (final ICalloutInstance callout : callouts)
+		logger.trace("executeAll: Begin executing all callouts for {} using fields supplier: {}", this, calloutFieldsSupplier);
+
+		//
+		// Enabled executed callouts tracing.
+		// We will track all indirectly executed callouts and we won't execute them again here.
+		final Set<ICalloutInstance> executedCalloutsTrace = new HashSet<>();
+		if (this.executedCalloutsTrace != null)
+		{
+			throw new IllegalStateException("executedCalloutsTrace is not null. Is there another executeAll currently running?"); // shall not happen
+		}
+		this.executedCalloutsTrace = executedCalloutsTrace;
+
+		//
+		// Statistics
+		StatisticsCollector statisticsCollector = logger.isTraceEnabled() ? new StatisticsCollector() : null;
+
+		try
+		{
+			for (final Map.Entry<String, Collection<ICalloutInstance>> columnNameAndCallouts : tableCalloutsMap.getColumnNamesAndCalloutsEntries())
 			{
-				execute(callout, field);
+				//
+				// Get the callout field from supplier
+				final String columnName = columnNameAndCallouts.getKey();
+				final ICalloutField field = calloutFieldsSupplier.apply(columnName);
+				if (field == null)
+				{
+					statisticsCollector = statisticsCollector == null ? null : statisticsCollector.collectFieldSkipped(columnName, "supplied field was null");
+					continue;
+				}
+				if (!isEligibleForExecuting(field))
+				{
+					statisticsCollector = statisticsCollector == null ? null : statisticsCollector.collectFieldSkipped(field, "not eligible");
+					continue;
+				}
+
+				//
+				// Execute all callouts for current callout field
+				final Collection<ICalloutInstance> callouts = columnNameAndCallouts.getValue();
+				for (final ICalloutInstance callout : callouts)
+				{
+					// Optimization: don't fire this callout if it was indirectly already fired.
+					if (executedCalloutsTrace.contains(callout))
+					{
+						statisticsCollector = statisticsCollector == null ? null : statisticsCollector.collectCalloutSkipped(columnName, callout, "it was already executed");
+						continue;
+					}
+
+					final Stopwatch calloutDuration = statisticsCollector == null ? null : Stopwatch.createStarted();
+
+					execute(callout, field);
+
+					statisticsCollector = statisticsCollector == null ? null : statisticsCollector.collectCalloutExecuted(columnName, callout, calloutDuration);
+				}
+			}
+		}
+		finally
+		{
+			// Disable executed callouts tracing
+			this.executedCalloutsTrace = null;
+
+			logger.trace("executeAll: Finished executing all callouts for {} using fields supplier: {}", this, calloutFieldsSupplier);
+			if (statisticsCollector != null)
+			{
+				statisticsCollector.stop();
+				logger.trace("{}", statisticsCollector.getSummary());
 			}
 		}
 	}
@@ -144,7 +199,13 @@ public final class CalloutExecutor implements ICalloutExecutor
 			return false;
 		}
 
-		return field.isTriggerCalloutAllowed();
+		if (!field.isTriggerCalloutAllowed())
+		{
+			logger.trace("Field {} does not allow callouts triggering", field);
+			return false;
+		}
+
+		return true;
 	}
 
 	private void execute(final ICalloutInstance callout, final ICalloutField field)
@@ -173,6 +234,13 @@ public final class CalloutExecutor implements ICalloutExecutor
 			}
 
 			callout.execute(this, field);
+
+			// Trace executed callout if required
+			final Set<ICalloutInstance> executedCalloutsTrace = this.executedCalloutsTrace;
+			if (executedCalloutsTrace != null)
+			{
+				executedCalloutsTrace.add(callout);
+			}
 		}
 		catch (final CalloutInitException e)
 		{
@@ -273,6 +341,142 @@ public final class CalloutExecutor implements ICalloutExecutor
 		public ICalloutProvider getDefaultCalloutProvider()
 		{
 			return Services.get(ICalloutFactory.class).getProvider();
+		}
+	}
+
+	/**
+	 * Internal object for collecting statistics when we run {@link CalloutExecutor#executeAll(Function)}.
+	 *
+	 * @author metas-dev <dev@metasfresh.com>
+	 *
+	 */
+	private static final class StatisticsCollector
+	{
+		private final Stopwatch durationTotal = Stopwatch.createStarted();
+		private int countFieldsSkipped = 0;
+		private int countCalloutsExecuted = 0;
+		private int countCalloutsSkipped = 0;
+		private final LinkedHashMap<ICalloutInstance, CalloutStatisticsEntry> calloutStatistics = new LinkedHashMap<>();
+
+		private StatisticsCollector()
+		{
+			super();
+		}
+
+		public String getSummary()
+		{
+			final StringBuilder sb = new StringBuilder();
+			sb.append("\n Executed " + countCalloutsExecuted + " callouts in " + durationTotal);
+			if (countCalloutsSkipped > 0)
+			{
+				sb.append("\n Skipped " + countCalloutsSkipped + " callouts which were called indirectly");
+			}
+			if (countFieldsSkipped > 0)
+			{
+				sb.append("\n Skipped " + countFieldsSkipped + " fields because they are no eligible");
+			}
+
+			final Collection<CalloutStatisticsEntry> calloutStatsEntries = calloutStatistics.values();
+			if (!calloutStatsEntries.isEmpty())
+			{
+				sb.append("\n Following callouts were executed: ");
+				calloutStatsEntries
+						.stream()
+						.sorted((s1, s2) -> (int)(s2.getDurationNanos() - s1.getDurationNanos())) // descending by duration
+						.forEach((s) -> sb.append("\n * " + s));
+			}
+
+			return sb.toString();
+		}
+
+		public StatisticsCollector stop()
+		{
+			durationTotal.stop();
+			return this;
+		}
+
+		public StatisticsCollector collectFieldSkipped(final String columnName, final String reason)
+		{
+			logger.trace("Skip executing all callouts for {} because {}", columnName, reason);
+			countFieldsSkipped++;
+			return this;
+		}
+
+		public StatisticsCollector collectFieldSkipped(final ICalloutField calloutField, final String reason)
+		{
+			logger.trace("Skip executing all callouts for {} because {}", calloutField, reason);
+			countFieldsSkipped++;
+			return this;
+		}
+
+		public StatisticsCollector collectCalloutSkipped(final String columnName, final ICalloutInstance callout, final String reason)
+		{
+			logger.trace("Skip executing callout {}: {} because {}", columnName, callout, reason);
+			countCalloutsSkipped++;
+			calloutStatistics.put(callout, CalloutStatisticsEntry.skipped(columnName, callout, reason));
+			return this;
+		}
+
+		public StatisticsCollector collectCalloutExecuted(final String columnName, final ICalloutInstance callout, final Stopwatch calloutDuration)
+		{
+			countCalloutsExecuted++;
+			calloutStatistics.put(callout, CalloutStatisticsEntry.executed(columnName, callout, calloutDuration));
+			return this;
+		}
+
+		/**
+		 * Per {@link ICalloutInstance} statistics.
+		 *
+		 * @author metas-dev <dev@metasfresh.com>
+		 *
+		 */
+		private static final class CalloutStatisticsEntry
+		{
+			public static final CalloutStatisticsEntry executed(final String columnName, final ICalloutInstance callout, final Stopwatch calloutDuration)
+			{
+				final long durationNanos;
+				final String durationStr;
+				if (calloutDuration != null)
+				{
+					calloutDuration.stop();
+					durationStr = calloutDuration.toString();
+					durationNanos = calloutDuration.elapsed(TimeUnit.NANOSECONDS);
+				}
+				else
+				{
+					durationStr = "???";
+					durationNanos = Long.MAX_VALUE; // just to jump in developer's eye!
+				}
+				final String summary = "EXECUTED " + columnName + " - " + durationStr + " - " + callout;
+				return new CalloutStatisticsEntry(summary, durationNanos);
+			}
+
+			public static final CalloutStatisticsEntry skipped(final String columnName, final ICalloutInstance callout, final String skipReason)
+			{
+				final String summary = "SKIPPED  " + columnName + " - " + skipReason + " - " + callout;
+				return new CalloutStatisticsEntry(summary, 0);
+			}
+
+			private final String summary;
+			private final long durationNanos;
+
+			private CalloutStatisticsEntry(final String summary, final long durationNanos)
+			{
+				super();
+				this.summary = summary;
+				this.durationNanos = durationNanos;
+			}
+
+			@Override
+			public String toString()
+			{
+				return summary;
+			}
+
+			public long getDurationNanos()
+			{
+				return durationNanos;
+			}
 		}
 	}
 }
