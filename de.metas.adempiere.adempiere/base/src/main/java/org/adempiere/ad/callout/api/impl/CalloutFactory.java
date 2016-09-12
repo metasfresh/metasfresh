fe8a58d31225829fa7db2724ffd6cd310c1d6b04
Teo Sarca <teo.sarca@gmail.com>
2016-09-12 13:43:48 +0300

    FRESH-657 made CompositeCalloutProvider immutable

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/callout/api/impl/CalloutFactory.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/callout/api/impl/CalloutFactory.java
@@ -10,38 +10,39 @@ package org.adempiere.ad.callout.api.impl;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-
 import java.util.List;
 import java.util.Properties;
 
 import org.adempiere.ad.callout.api.ICalloutFactory;
 import org.adempiere.ad.callout.api.TableCalloutsMap;
+import org.adempiere.ad.callout.spi.CompositeCalloutProvider;
 import org.adempiere.ad.callout.spi.ICalloutProvider;
 import org.adempiere.ad.callout.spi.IDefaultCalloutProvider;
-import org.adempiere.ad.callout.spi.impl.CompositeCalloutProvider;
+import org.adempiere.ad.callout.spi.impl.NullCalloutProvider;
 import org.adempiere.util.Services;
 import org.slf4j.Logger;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 
 import de.metas.logging.LogManager;
 
 public class CalloutFactory implements ICalloutFactory
 {
 	private static final transient Logger logger = LogManager.getLogger(CalloutFactory.class);
-	private final CompositeCalloutProvider providers = new CompositeCalloutProvider();
+	private ICalloutProvider providers = ICalloutProvider.NULL;
 
 	public CalloutFactory()
 	{
@@ -53,13 +54,18 @@ public class CalloutFactory implements ICalloutFactory
 	}
 
 	@Override
-	public void registerCalloutProvider(final ICalloutProvider provider)
+	public void registerCalloutProvider(final ICalloutProvider providerToAdd)
 	{
-		final boolean added = providers.addCalloutProvider(provider);
-		if (added)
+		final ICalloutProvider providersOld = providers;
+		final ICalloutProvider providersNew = CompositeCalloutProvider.compose(providersOld, providerToAdd);
+
+		if (providersNew == providersOld)
 		{
-			logger.info("Registered provider: {}", provider);
+			return;
 		}
+
+		providers = providersNew;
+		logger.info("Registered provider: {}", providerToAdd);
 	}
 
 	/**
@@ -69,7 +75,19 @@ public class CalloutFactory implements ICalloutFactory
 	@VisibleForTesting
 	final List<ICalloutProvider> getCalloutProvidersList()
 	{
-		return providers.getProviders();
+		final ICalloutProvider providers = this.providers;
+		if (providers instanceof CompositeCalloutProvider)
+		{
+			return ((CompositeCalloutProvider)providers).getProvidersList();
+		}
+		else if (NullCalloutProvider.isNull(providers))
+		{
+			return ImmutableList.of();
+		}
+		else
+		{
+			return ImmutableList.of(providers);
+		}
 	}
 
 	@Override
@@ -77,9 +95,9 @@ public class CalloutFactory implements ICalloutFactory
 	{
 		return providers;
 	}
-	
+
 	@Override
-	public TableCalloutsMap getCallouts(Properties ctx, final String tableName)
+	public TableCalloutsMap getCallouts(final Properties ctx, final String tableName)
 	{
 		return providers.getCallouts(ctx, tableName);
 	}
