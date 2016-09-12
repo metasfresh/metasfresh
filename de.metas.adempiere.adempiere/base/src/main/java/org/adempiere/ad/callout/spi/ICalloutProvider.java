61db4eafcca8d0091fe36ef40993e21704b5885f
Teo Sarca <teo.sarca@gmail.com>
2016-09-12 13:39:56 +0300

    FRESH-657 introduced NullCalloutProvider

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/callout/spi/ICalloutProvider.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/callout/spi/ICalloutProvider.java
@@ -3,9 +3,13 @@ package org.adempiere.ad.callout.spi;
 import java.util.Properties;
 
 import org.adempiere.ad.callout.api.TableCalloutsMap;
+import org.adempiere.ad.callout.spi.impl.NullCalloutProvider;
 
 @FunctionalInterface
 public interface ICalloutProvider
 {
+	/** A {@link ICalloutProvider} which supplies no callouts */
+	ICalloutProvider NULL = NullCalloutProvider.instance;
+
 	TableCalloutsMap getCallouts(final Properties ctx, final String tableName);
 }
