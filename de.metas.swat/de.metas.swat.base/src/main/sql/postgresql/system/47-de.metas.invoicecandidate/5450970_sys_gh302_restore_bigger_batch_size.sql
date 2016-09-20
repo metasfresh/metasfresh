6dffec6cd1f890c2c018f690ab2349227ad4ba0f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-09-20 21:41:55 +0200

    FRESH-599 #302 fix SQL

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5450970_sys_gh302_restore_bigger_batch_size.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5450970_sys_gh302_restore_bigger_batch_size.sql
@@ -3,11 +3,12 @@
 --
 UPDATE AD_SysConfig
 SET Description='Maximum number of invoice candidates to be updated in one workpackage. If there are more ICs to update, then another workpackage will be created for them.', 
-	Updated=now(), Updatedby=99
-WHERE name='de.metas.invoicecandidate.async.spi.impl.UpdateInvalidInvoiceCandidatesWorkpackageProcessor.MaxInvoiceCandidatesToUpdate'
+	Updated=now(), UpdatedBy=99
+WHERE Name='de.metas.invoicecandidate.async.spi.impl.UpdateInvalidInvoiceCandidatesWorkpackageProcessor.MaxInvoiceCandidatesToUpdate';
 
 UPDATE AD_SysConfig
 SET Description='How many invoice candidates shall be updated in one transaction; Note that the number of ICs to process is generally limited by the AD_Sysconfig-"de.metas.invoicecandidate.async.spi.impl.UpdateInvalidInvoiceCandidatesWorkpackageProcessor.MaxInvoiceCandidatesToUpdate" value',
+	Updated=now(), UpdatedBy=99
 WHERE Name='de.metas.invoicecandidate.api.impl.InvoiceCandInvalidUpdater.ItemsPerBatch';
 
 --
@@ -17,4 +18,4 @@ UPDATE AD_SysConfig
 SET Value='100', 
 	Updated=now(), Updatedby=99
 WHERE Name='de.metas.invoicecandidate.api.impl.InvoiceCandInvalidUpdater.ItemsPerBatch'
-AND Value='1'
+AND Value='1';
