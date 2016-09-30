c552a7f373c72acc26a3ef814234b93fbd0f2d6a
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-09-30 16:20:37 +0200

    FRESH-658 #370 Double click needed for weighing machine and occasional
    minor improvements while doing QA

--- de.metas.fresh/de.metas.fresh.base/src/main/java/de/metas/fresh/picking/form/ProductTableRowSearchSelectionMatcher.java
+++ de.metas.fresh/de.metas.fresh.base/src/main/java/de/metas/fresh/picking/form/ProductTableRowSearchSelectionMatcher.java
@@ -10,12 +10,12 @@ package de.metas.fresh.picking.form;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -45,10 +45,10 @@ import de.metas.handlingunits.model.I_EDI_M_Product_Lookup_UPC_v;
 
 /**
  * Matchers all rows for given barcode. M_Product.UPC and M_Product.Value are matched.
- * 
+ *
  * @author tsa
  * @task http://dewiki908/mediawiki/index.php/06821_Kommissionier_Terminal_Extension_%28104171338645%29
- * 
+ *
  */
 public class ProductTableRowSearchSelectionMatcher implements ITableRowSearchSelectionMatcher
 {
@@ -110,16 +110,16 @@ public class ProductTableRowSearchSelectionMatcher implements ITableRowSearchSel
 
 	/**
 	 * Called by {@link #init()} to load underlying data.
-	 * 
+	 *
 	 * This method shall call setters like {@link #setC_BPartner_ID(int)}, {@link #setProductIds(Set)} to configure this matcher correctly.
-	 * 
+	 *
 	 * @return true if it was correctly initialized; if this method returns false then {@link #isValid()} will return false
 	 */
 	private boolean load()
 	{
 		final IQueryBL queryBL = Services.get(IQueryBL.class);
 
-		final List<I_EDI_M_Product_Lookup_UPC_v> upcProductCandidates = 
+		final List<I_EDI_M_Product_Lookup_UPC_v> upcProductCandidates =
 				queryBL.createQueryBuilder(I_EDI_M_Product_Lookup_UPC_v.class, ctx, ITrx.TRXNAME_None)
 				.addOnlyActiveRecordsFilter()
 				.addEqualsFilter(I_EDI_M_Product_Lookup_UPC_v.COLUMNNAME_UPC, barcode)
@@ -311,4 +311,12 @@ public class ProductTableRowSearchSelectionMatcher implements ITableRowSearchSel
 				.append(productId, other.productId)
 				.isEqual();
 	}
+
+	@Override
+	public String toString()
+	{
+		return "ProductTableRowSearchSelectionMatcher [initialized=" + initialized + ", barcode=" + barcode + ", matchingType=" + matchingType + ", bpartnerId2productId=" + bpartnerId2productId + ", bpartnerId=" + bpartnerId + ", productId=" + productId + ", valid=" + valid + "]";
+	}
+
+
 }
