700db8afab6c04911d5f391e199399bc03bbb206
damian.przygodzki <damian.przygodzki@jazzy.pro>
2016-10-03 13:17:08 +0200

    Optimizing doclist

--- src/containers/DocList.js
+++ src/containers/DocList.js
@@ -37,20 +37,7 @@ class DocList extends Component {
     componentWillReceiveProps(props) {
         const {dispatch, windowType} = props;
 
-        dispatch(viewLayoutRequest(windowType, "grid")).then(response => {
-            this.setState(Object.assign({}, this.state, {
-                layout: response.data,
-                filters: response.data.filters
-            }), () => {
-                dispatch(createViewRequest(windowType, "grid", 20, [])).then((response) => {
-                    this.setState(Object.assign({}, this.state, {
-                        data: response.data
-                    }), () => {
-                        this.getView();
-                    })
-                })
-            })
-        });
+        this.updateData();
     }
 
     updateData = () => {
