9ad1df07310987d5f296d3496af65689a1a5bcbf
Katarzyna Dunikowska <Katarzyna Dunikowska>
2016-10-13 16:21:29 +0200

    Add filter component

---
+++ src/components/app/Filters.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Filters extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                Filters
+            </div>
+
+        )
+    }
+}
+
+export default Filters
