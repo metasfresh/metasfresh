4e87a541fefc870b185e4e3218e662a6dbee3f49
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-10-24 19:31:05 +0300

    FRESH-783 #500 Set DateDelivered in R_Request when M_InOut is set
    (cherry picked from commit c26531128ab847c6b3c29dddc0daf0ef810269c4)

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/request/model/validator/R_Request.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/request/model/validator/R_Request.java
@@ -6,10 +6,13 @@ import org.adempiere.ad.callout.spi.IProgramaticCalloutProvider;
 import org.adempiere.ad.modelvalidator.annotations.Init;
 import org.adempiere.ad.modelvalidator.annotations.ModelChange;
 import org.adempiere.ad.modelvalidator.annotations.Validator;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
 import org.compiere.model.I_R_Request;
 import org.compiere.model.I_R_RequestType;
 import org.compiere.model.ModelValidator;
+
+import de.metas.inout.model.I_M_InOut;
 
 /*
  * #%L
@@ -38,13 +41,12 @@ import org.compiere.model.ModelValidator;
 public class R_Request
 {
 
-	
 	@Init
 	public void registerCallout()
 	{
 		Services.get(IProgramaticCalloutProvider.class).registerAnnotatedCallout(this);
 	}
-	
+
 	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_NEW, ModelValidator.TYPE_BEFORE_CHANGE }, ifColumnsChanged = I_R_Request.COLUMNNAME_R_RequestType_ID)
 	@CalloutMethod(columnNames = { I_R_Request.COLUMNNAME_R_RequestType_ID })
 	public void onRequestTypeChange(final I_R_Request request)
@@ -63,4 +65,20 @@ public class R_Request
 			request.setR_RequestType_InternalName(requestType.getInternalName());
 		}
 	}
+
+	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_NEW, ModelValidator.TYPE_BEFORE_CHANGE }, ifColumnsChanged = I_R_Request.COLUMNNAME_M_InOut_ID)
+	@CalloutMethod(columnNames = { I_R_Request.COLUMNNAME_M_InOut_ID })
+	public void onMInOutSet(final de.metas.request.model.I_R_Request request)
+	{
+		final I_M_InOut inout = InterfaceWrapperHelper.create(request.getM_InOut(), I_M_InOut.class);
+
+		if (inout == null)
+		{
+			// in case the inout was removed or is not set, the DateDelivered shall also be not set
+			request.setDateDelivered(null);
+		}
+		request.setDateDelivered(inout.getMovementDate());
+
+	}
+
 }
