8a376822a18252b853cd58b6c2ff281a50b2efee
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-10-26 16:30:17 +0300

    FRESH-783 #500 new request type C_ConsumerComplaint

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5452450_sys_FRESH-783_R_Request_NewRequestType.sql
@@ -0,0 +1,5 @@
+
+-- 26.10.2016 14:49
+-- URL zum Konzept
+INSERT INTO R_RequestType (AD_Client_ID,AD_Org_ID,AutoDueDateDays,CCM_Default,ConfidentialType,Created,CreatedBy,DueDateTolerance,InternalName,IsActive,IsAutoChangeRequest,IsConfidentialInfo,IsDefault,IsDefaultForEMail,IsDefaultForLetter,IsEMailWhenDue,IsEMailWhenOverdue,IsIndexed,IsInvoiced,IsSelfService,Name,R_RequestType_ID,R_StatusCategory_ID,Updated,UpdatedBy) VALUES (0,0,0,'N','C',TO_TIMESTAMP('2016-10-26 14:49:41','YYYY-MM-DD HH24:MI:SS'),100,7,'C_ConsumerComplaint','Y','N','N','N','N','N','N','N','N','N','Y','Konsumentenreklamation',540007,1000001,TO_TIMESTAMP('2016-10-26 14:49:41','YYYY-MM-DD HH24:MI:SS'),100)
+;
