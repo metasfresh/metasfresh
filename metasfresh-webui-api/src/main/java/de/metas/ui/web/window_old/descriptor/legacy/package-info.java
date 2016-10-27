4929b68b1f8ad6ba63fb319044f1b8253be165f3
Teo Sarca <teo.sarca@gmail.com>
2016-10-27 16:49:17 +0300

    FRESH-112 removed old classes
    ... from initial prototype

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/window_old/descriptor/legacy/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package to provide {@link de.metas.ui.web.window_old.descriptor.PropertyDescriptor}'s based on the old-school {@link org.compiere.model.GridWindowVO} and it's related classes.
- */
-package de.metas.ui.web.window_old.descriptor.legacy;
