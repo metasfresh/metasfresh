085dc783aabb080e8e07c36cf4315ddd80323fc3
Teo Sarca <teo.sarca@gmail.com>
2016-10-28 12:29:41 +0300

    FRESH-794 #505 fix M_Material_Tracking.NumberOfQualityInspections virtual column's SQL (2)

--- de.metas.handlingunits.base/src/main/sql/postgresql/system/5452740_sys_FRESH-794_fix_M_MaterialTracking_NumberOfQualityInspections_virtual_column.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5452740_sys_FRESH-794_fix_M_MaterialTracking_NumberOfQualityInspections_virtual_column.sql
@@ -5,7 +5,7 @@ UPDATE AD_Column SET ColumnSQL='(
 	from RV_M_Material_Tracking_HU_Details hu
 	where hu.M_Material_Tracking_ID=M_Material_Tracking.M_Material_Tracking_ID
 	and hu.IsActive=''Y''
-	and hu.IsQualityInspection=''Y'' or hu.QualityInspectionCycle is not null
+	and (hu.IsQualityInspection=''Y'' or hu.QualityInspectionCycle is not null)
 )
 ',Updated=TO_TIMESTAMP('2016-10-28 12:23:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=552797
 ;
