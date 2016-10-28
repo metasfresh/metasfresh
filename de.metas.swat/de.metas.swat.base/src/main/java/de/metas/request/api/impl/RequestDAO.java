f1da0a0292cfd775e85575b7ff97d1ff56b5cee4
Teo Sarca <teo.sarca@gmail.com>
2016-10-28 17:01:11 +0200

    FRESH-783 #500 Migration: Create Requests for all inout lines with
    ..quality issues
    
    manually synced with origin/FRESH-783..

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/request/api/impl/RequestDAO.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/request/api/impl/RequestDAO.java
@@ -29,12 +29,12 @@ import de.metas.request.model.I_R_Request;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -45,6 +45,7 @@ public class RequestDAO implements IRequestDAO
 {
 	public static final String MSG_R_Request_From_InOut_Summary = "R_Request_From_InOut_Summary";
 
+	public static final String ATTR_NAME_QualityNotice = "QualityNotice";
 
 	@Override
 	public void createRequestFromInOutLine(final I_M_InOutLine line)
@@ -75,12 +76,16 @@ public class RequestDAO implements IRequestDAO
 		// data from line
 		request.setM_InOut_ID(inOutID);
 		request.setM_Product_ID(line.getM_Product_ID());
-		request.setQualityNote(line.getQualityNote());
+
+		final String qualityNoteName = line.getQualityNote();
+
+		// set QualityNote based on the string provided in the inout line
+		final Properties ctx = InterfaceWrapperHelper.getCtx(line);
 
 		final I_M_QualityNote qualityNoteForName = Services.get(IQualityNoteDAO.class).retrieveQualityNoteForName(ctx, qualityNoteName);
 
 		request.setM_QualityNote(qualityNoteForName);
-		
+
 		if(qualityNoteForName != null)
 		{
 			// in case there is a qualitynote set, also set the Performance type based on it
