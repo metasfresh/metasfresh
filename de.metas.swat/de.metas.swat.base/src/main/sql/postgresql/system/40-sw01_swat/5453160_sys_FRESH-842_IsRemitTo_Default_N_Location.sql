865d1bd8c7fcf2db7b65829b0c80431c4c582774
metas-rc <ruxandra.craciunescu@metasfresh.com>
2016-11-09 11:34:21 +0200

    FRESH-842 #555 RemitTo default false in C_BPartnerLocation

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5453160_sys_FRESH-842_IsRemitTo_Default_N_Location.sql
@@ -0,0 +1,5 @@
+-- 09.11.2016 11:16
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2016-11-09 11:16:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3093
+;
+
