17e785248ee6094028d9ccd3b1bdfee2b4dfcbf1
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-11-12 12:33:24 +0100

    FRESH-771 #489 update function DDL
    FRESH-771 #489 Implement DLM within single logical tables

--- de.metas.dlm/src/main/sql/postgresql/ddl/dlm/functions/remove_table_from_dlm.sql
+++ de.metas.dlm/src/main/sql/postgresql/ddl/dlm/functions/remove_table_from_dlm.sql
@@ -9,31 +9,25 @@ BEGIN
 	EXECUTE 'DROP INDEX IF EXISTS ' || p_table_name || '_DLM_Level;';
 
 	/* drop the triggers first, because they depends on the DLM_Level column (important in case of p_retain_dlm_column=false). */
-	FOR v_trigger_view_row IN 
-		EXECUTE 'SELECT * FROM dlm.triggers v WHERE lower(v.foreign_table_name) = lower('''|| p_table_name ||'_tbl'')'
-	LOOP
-		EXECUTE v_trigger_view_row.drop_dlm_trigger_ddl;
-	
-		RAISE NOTICE 'Dropped dlm trigger analog to FK constraint %', v_trigger_view_row.constraint_name;
-	END LOOP;
+ 	PERFORM dlm.drop_dlm_triggers(p_table_name);
 	
 	IF p_retain_dlm_column = false
 	THEN
 		EXECUTE 'ALTER TABLE ' || p_table_name || '_Tbl DROP COLUMN IF EXISTS DLM_Level;';
 		EXECUTE 'ALTER TABLE ' || p_table_name || '_Tbl DROP COLUMN IF EXISTS DLM_Partition_ID;';
-		RAISE NOTICE 'Dropped columns DLM_Level and DLM_Partition_ID from table % (if they existed)', p_table_name;
+		RAISE NOTICE 'remove_table_from_dlm - %: Dropped columns DLM_Level and DLM_Partition_ID from table % (if they existed)', p_table_name, p_table_name;
 		
 	ELSE
-		RAISE NOTICE 'Retained columns DLM_Level and DLM_Partition_ID of table %', p_table_name;
+		RAISE NOTICE 'remove_table_from_dlm - %: Retained columns DLM_Level and DLM_Partition_ID of table %', p_table_name, p_table_name;
 	END IF;
 	
 	EXECUTE 'ALTER TABLE ' || p_table_name || '_Tbl RENAME TO ' || p_table_name || ';';
-	RAISE NOTICE 'Renamed table % back to % ', p_table_name||'_Tbl', p_table_name;
+	RAISE NOTICE 'remove_table_from_dlm - %: Renamed table % back to % ', p_table_name, p_table_name||'_Tbl', p_table_name;
  END;
 $BODY$
   LANGUAGE plpgsql VOLATILE;
 COMMENT ON FUNCTION dlm.remove_table_from_dlm(text, boolean) IS 'gh #235, #489: Un-DLMs the given table:
 * drops the view and removes the "_tbl" suffix from the table name
 * drops partial indices
-* drops the tiggers and triggerfunctions that were created using the dlm.triggers view.
+* invokes dlm.drop_dlm_triggers with the given table name.
 * optionally drops the DLM_Level and DLM_Partition_ID column, if told so explicitly with the p_retain_dlm_column parameter set to false.';
