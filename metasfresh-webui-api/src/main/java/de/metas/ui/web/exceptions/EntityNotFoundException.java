0f82ced6b32455c6ac46a10a0aa953a06116b7bb
Teo Sarca <teo.sarca@gmail.com>
2016-11-30 18:27:01 +0200

    FRESH-946 #76 GET /documentView/{viewId} should return 404

---
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/exceptions/EntityNotFoundException.java
@@ -0,0 +1,51 @@
+package de.metas.ui.web.exceptions;
+
+import org.adempiere.exceptions.AdempiereException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+ * #%L
+ * metasfresh-webui-api
+ * %%
+ * Copyright (C) 2016 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+/**
+ * Exception thrown when some entity (document, process etc) was not found.
+ *
+ * NOTE: this exceptions binds to HTTP 404
+ *
+ * @author metas-dev <dev@metasfresh.com>
+ *
+ */
+@SuppressWarnings("serial")
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends AdempiereException
+{
+	public EntityNotFoundException(final String message, final Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public EntityNotFoundException(final String message)
+	{
+		super(message);
+	}
+
+}
