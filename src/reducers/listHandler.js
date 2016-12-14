3893354790d5c02f4f80d61ff6977c7e678cfd0d
Katarzyna Dunikowska <katarzyna.dunikowska@jazzy.pro>
2016-12-14 12:39:03 +0100

    Fix clearing page after view changed

--- src/reducers/listHandler.js
+++ src/reducers/listHandler.js
@@ -13,7 +13,10 @@ const initialState = {
         dir: null,
         windowType: null
     },
-    page: 1,
+    pagination: {
+        page: 1,
+        windowType: null
+    },
     viewId: ""
 }
 
@@ -27,8 +30,10 @@ export default function listHandler(state = initialState, action) {
 
         case types.SET_LIST_PAGINATION:
             return Object.assign({}, state, {
-                page: action.page,
-                windowType: action.windowType
+                pagination: Object.assign({}, state.pagination, {
+                    page: action.page,
+                    windowType: action.windowType
+                })
             })
 
         case types.SET_LIST_SORTING:
@@ -43,9 +48,12 @@ export default function listHandler(state = initialState, action) {
         case types.CLEAR_LIST_PROPS:
             return Object.assign({}, state, {
                 filters: [],
-                page: 1,
                 sorting: {},
-                windowType: null
+                windowType: null,
+                pagination: Object.assign({}, state.pagination, {
+                    page: 1,
+                    windowType: null
+                })
             })
 
         case types.INIT_FILTERS_PARAMETERS:
