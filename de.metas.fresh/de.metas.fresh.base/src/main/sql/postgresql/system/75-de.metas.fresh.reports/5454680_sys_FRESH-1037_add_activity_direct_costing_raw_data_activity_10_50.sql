db85b2af3f213f1a2a00331e0fdd42f3cb476bc4
metas-lc <liana.cotuna@metasfresh.com>
2016-12-16 15:57:15 +0200

    FRESH-1037 #714
    remove percentages from activity 10-50 reports and migrationscripts

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5454680_sys_FRESH-1037_add_activity_direct_costing_raw_data_activity_10_50.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5454680_sys_FRESH-1037_add_activity_direct_costing_raw_data_activity_10_50.sql
@@ -1,19 +1,46 @@
 
-DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Direct_Costing_Raw_Data_Remainings_Activity_10_50 (Year Date, AD_Org_ID numeric(10,0) );
-
-CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Direct_Costing_Raw_Data_Remainings_Activity_10_50 (Year Date, AD_Org_ID numeric(10,0) ) RETURNS TABLE
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Direct_Costing_Raw_Data_Activity_10_50 (Year Date, AD_Org_ID numeric(10,0));
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Direct_Costing_Raw_Data_Activity_10_50 (Year Date, AD_Org_ID numeric(10,0)) RETURNS TABLE
 	(
-	l_Value Character Varying, L_Name Character Varying,
-	Balance_10 numeric, Balance_20 numeric, Balance_30 numeric, Balance_40 numeric, Balance_50 numeric, Balance_90 numeric, Balance_Other numeric, Balance numeric, 
-	Budget_10 numeric, Budget_20 numeric, Budget_30 numeric, Budget_40 numeric, Budget_50 numeric, Budget_90 numeric, Budget numeric, 
-	L_Multiplicator numeric,
-	ad_org_id numeric (10,0)
+	Margin text, 
+	l1_Value Character Varying, 
+	L1_Name Character Varying, 
+	
+	L2_Value Character Varying, 
+	L2_Name Character Varying, 
+	
+	L3_Value Character Varying, 
+	L3_Name Character Varying, 
+	
+	Balance_10 numeric, 
+	Balance_20 numeric, 
+	Balance_30 numeric, 
+	Balance_40 numeric, 
+	Balance_50 numeric, 
+	Balance_90 numeric, 
+	Balance_Other numeric, 
+	Balance numeric, 
+	
+	Budget_10 numeric, 
+	Budget_20 numeric, 
+	Budget_30 numeric, 
+	Budget_40 numeric, 
+	Budget_50 numeric, 
+	Budget_90 numeric, 
+	Budget numeric, 
+	
+	L1_Multiplicator numeric, 
+	L2_Multiplicator numeric, 
+	L3_Multiplicator numeric, 
+	Seq text,
+	
+	ad_org_id numeric
 	)
 AS 
 $BODY$
 SELECT
-	
-	l_value, l_name,
+	margin, 
+	l1_value, l1_name, l2_value, l2_name, l3_value, l3_name,
 	Balance_10 AS Balance_10,
 	Balance_20 AS Balance_20,
 	Balance_30 AS Balance_30,
@@ -25,15 +52,17 @@ SELECT
 	Budget_10 AS Budget_10,
 	Budget_20 AS Budget_20,
 	Budget_30 AS Budget_30,
-	Budget_40 AS Budget_40,
+	Budget_40 AS Budget_40,	
 	Budget_50 AS Budget_50,
 	Budget_90 AS Budget_90,
-	Budget_10 + Budget_20 + Budget_30 + + Budget_40 + Budget_50 + Budget_90 + Budget_Other AS Budget,
-	acctBalance(l_ElementValue_ID, 0, 1) AS l_Multiplicator,
+	Budget_10 + Budget_20 + Budget_30 + Budget_40 + Budget_50 + Budget_90 + Budget_Other AS Budget,
+	acctBalance(l1_ElementValue_ID, 0, 1) AS l1_Multiplicator,
+	acctBalance(l2_ElementValue_ID, 0, 1) AS l2_Multiplicator,
+	acctBalance(l3_ElementValue_ID, 0, 1) AS l3_Multiplicator,
+	SeqNo AS Seq,
 	fa.ad_org_id
-
 FROM
-	de_metas_endcustomer_fresh_reports.Direct_Costing_selection_Remainings s
+	de_metas_endcustomer_fresh_reports.Direct_Costing_selection s
 	LEFT OUTER JOIN (
 		SELECT
 			Account_ID,
@@ -54,15 +83,14 @@ FROM
 			SUM( CASE WHEN a.Value = '90' THEN Budget ELSE 0 END ) AS Budget_90,
 			SUM( CASE WHEN a.Value IS NULL OR (a.Value != '10' AND a.Value != '20' AND a.Value != '30' AND a.Value != '40' AND a.Value != '50' AND a.Value != '90') 
 				THEN Budget ELSE 0 END ) AS Budget_Other,
-
 			fa.ad_org_id
 		FROM
 			(
 				SELECT fa.Account_ID
 					, COALESCE(ap.C_Activity_ID, a.C_Activity_ID, aev.C_Activity_ID) as C_Activity_ID
-					, CASE WHEN postingtype IN ('A','Y') THEN AmtAcctCr - AmtAcctDr ELSE 0 END AS Balance
-					, CASE WHEN postingtype = 'B' THEN AmtAcctCr - AmtAcctDr ELSE 0 END AS Budget
-					, fa.ad_org_id
+					, CASE WHEN postingtype in ('A','Y') THEN AmtAcctCr - AmtAcctDr ELSE 0 END AS Balance
+					, CASE WHEN postingtype = 'B' THEN AmtAcctCr - AmtAcctDr ELSE 0 END AS Budget,
+					fa.ad_org_id
 				FROM Fact_Acct fa
 				left outer join C_Activity a on (a.C_Activity_ID=fa.C_Activity_ID) AND a.isActive = 'Y'
 				left outer join C_Activity ap on (ap.C_Activity_ID=a.Parent_Activity_ID) AND ap.isActive = 'Y'
@@ -70,7 +98,8 @@ FROM
 				-- in case of c_activity in fact_acct is null, to take the default one from c_elementvalue (FRESH-845)
 				left outer join C_ElementValue ev ON fa.Account_ID = ev.C_ElementValue_ID AND ev.isActive = 'Y'
 				left outer join C_Activity aev ON ev.C_Activity_ID = aev.C_Activity_ID AND aev.isActive = 'Y'
-				WHERE	CASE WHEN postingtype = 'B' THEN
+				WHERE
+				CASE WHEN postingtype = 'B' THEN
 					dateacct::date <= (select enddate from c_period where c_period_id=report.Get_Period( 1000000, $1 ) AND isActive = 'Y')
 				ELSE 
 					(dateacct::date <= $1) END
@@ -78,15 +107,17 @@ FROM
 						SELECT MIN( StartDate )::Date FROM C_Period 
 						WHERE C_Year_ID = (SELECT C_Year_ID FROM C_Period WHERE C_Period_ID = report.Get_Period( 1000000, $1 ) AND isActive = 'Y') AND isActive = 'Y'
 					)
-				AND fa.ad_org_id = $2
-				AND fa.isActive = 'Y'
+				AND fa.isActive = 'Y'	
+
 			) fa
 			LEFT OUTER JOIN C_Activity a ON fa.C_Activity_ID = a.C_Activity_ID AND a.isActive = 'Y'
-
 		GROUP BY Account_ID, fa.ad_org_id
-	) fa ON fa.Account_ID = s.L_ElementValue_ID
+	) fa ON fa.Account_ID = s.L3_ElementValue_ID 
 WHERE fa.ad_org_id = $2
+ORDER BY
+	SeqNo
 $BODY$
 LANGUAGE sql STABLE
 ;
+
 
