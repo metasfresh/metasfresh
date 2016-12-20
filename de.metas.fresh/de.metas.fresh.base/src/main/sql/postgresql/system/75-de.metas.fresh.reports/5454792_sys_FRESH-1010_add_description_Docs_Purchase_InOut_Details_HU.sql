4abbae056742dcf16f3e0ad0a9d3b65b5e57e06d
metas-lc <liana.cotuna@metasfresh.com>
2016-12-20 12:42:35 +0200

    FRESH-1010 #696 add multi line description per order line
    add line description in order and inout jaspers
    show description in sales order

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5454792_sys_FRESH-1010_add_description_Docs_Purchase_InOut_Details_HU.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5454792_sys_FRESH-1010_add_description_Docs_Purchase_InOut_Details_HU.sql
@@ -1,4 +1,4 @@
---DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Purchase_InOut_Details_HU ( IN Record_ID numeric, IN AD_Language Character Varying (6) );
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Purchase_InOut_Details_HU ( IN Record_ID numeric, IN AD_Language Character Varying (6) );
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Purchase_InOut_Details_HU ( IN Record_ID numeric, IN AD_Language Character Varying (6) )
 RETURNS TABLE 
 (
