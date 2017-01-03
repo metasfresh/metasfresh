092d67a5fd119d5756ec240633c62c96b76483dd
Katarzyna Dunikowska <katarzyna.dunikowska@jazzy.pro>
2017-01-03 09:57:29 +0100

    Fix spaces

--- src/components/shortcuts/DocumentListContextShortcuts.js
+++ src/components/shortcuts/DocumentListContextShortcuts.js
@@ -2,45 +2,46 @@ import React, { Component, PropTypes } from 'react';
 import { Shortcuts } from 'react-shortcuts';
  
 class DocumentListContextShortcuts extends Component {
-  constructor(props){
+    constructor(props){
         super(props);
     }
-  handleShortcuts = (action, event) => {
-    const {handleAdvancedEdit, handleOpenNewTab, handleDelete} = this.props;
-    switch (action) {
-      case 'OPEN_SELECTED':
-        event.preventDefault();
-        if(handleOpenNewTab) {
-          handleOpenNewTab();
+    handleShortcuts = (action, event) => {
+        const {handleAdvancedEdit, handleOpenNewTab, handleDelete} = this.props;
+        
+        switch (action) {
+        case 'OPEN_SELECTED':
+            event.preventDefault();
+            if(handleOpenNewTab) {
+            handleOpenNewTab();
+            }
+            break
+        case 'REMOVE_SELECTED':
+            event.preventDefault();
+            if(handleDelete){
+            handleDelete();
+            }
+            break
+        case 'ADVANCED_EDIT':
+            event.preventDefault();
+            if(handleAdvancedEdit){
+            handleAdvancedEdit();
+            }
+            break
         }
-        break
-      case 'REMOVE_SELECTED':
-        event.preventDefault();
-        if(handleDelete){
-          handleDelete();
-        }
-        break
-      case 'ADVANCED_EDIT':
-        event.preventDefault();
-        if(handleAdvancedEdit){
-          handleAdvancedEdit();
-        }
-        break
     }
-  }
- 
-  render() {
-    return (
-      <Shortcuts
-        name={"DOCUMENT_LIST_CONTEXT"}
-        handler = { this.handleShortcuts }
-        targetNodeSelector = { "body" }
-        isolate = { true }
-        preventDefault = { true }
-        stopPropagation = { true } 
-      />
-    )
-  }
+
+    render() {
+        return (
+        <Shortcuts
+            name={"DOCUMENT_LIST_CONTEXT"}
+            handler = { this.handleShortcuts }
+            targetNodeSelector = { "body" }
+            isolate = { true }
+            preventDefault = { true }
+            stopPropagation = { true } 
+        />
+        )
+    }
 }
 
 export default DocumentListContextShortcuts;
