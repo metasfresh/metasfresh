34697eeecf363e5719d6a5e59ae3cbf5ff5308d6
Katarzyna Dunikowska <katarzyna.dunikowska@jazzy.pro>
2017-01-03 16:45:22 +0100

    remove document context shortcuts file

--- src/keymap.js
+++ src/keymap.js
@@ -16,19 +16,11 @@ export default {
         OPEN_PRINT_RAPORT: mod + '+' + 'p',
         DELETE_DOCUMENT: mod + '+' + 'd',
         NEW_DOCUMENT: mod + '+' + 'm',
-
+        FOCUS_FAST_LINE_ENTRY: mod + '+' + 'q',
     },
     DOCUMENT_LIST_CONTEXT: {
         OPEN_SELECTED: mod + '+' + 'b', //open document in new tab
         REMOVE_SELECTED: mod + '+' + 'y',
         ADVANCED_EDIT: mod + '+' + 'a',
-    },
-    DOCUMENT_CONTEXT: {
-        FOCUS_FAST_LINE_ENTRY: mod + '+' + 'q',
-        REMOVE_SELECTED: {
-            osx: 'command+backspace',
-            windows: 'delete',
-            linux: 'delete',
-        }
     },
 }
