830071f0ec2acd9dcddaa932fe32747ba36ed43b
Katarzyna Dunikowska <katarzyna.dunikowska@jazzy.pro>
2017-01-03 09:40:50 +0100

    Fix spaces

--- src/components/shortcuts/DocumentContextShortcuts.js
+++ src/components/shortcuts/DocumentContextShortcuts.js
@@ -3,30 +3,30 @@ import { Shortcuts } from 'react-shortcuts';
  
 class DocumentContextShortcuts extends Component {
   constructor(props){
-        super(props);
-        
-    }
+		super(props);
+		
+	}
   handleShortcuts = (action, event) => {
-    const {} = this.props;
-    switch (action) {
-      case 'FOCUS_FAST_LINE_ENTRY':
-        break
-      case 'REMOVE_SELECTED':
-        break
-    }
+	const {} = this.props;
+	switch (action) {
+	  case 'FOCUS_FAST_LINE_ENTRY':
+		break
+	  case 'REMOVE_SELECTED':
+		break
+	}
   }
  
   render() {
-    return (
-      <Shortcuts
-        name={"DOCUMENT_CONTEXT"}
-        handler= {this.handleShortcuts}
-        targetNodeSelector={"body"}
-        isolate = { true }
-        preventDefault = { true }
-        stopPropagation = { true } 
-      />
-    )
+	return (
+	  <Shortcuts
+		name={"DOCUMENT_CONTEXT"}
+		handler= {this.handleShortcuts}
+		targetNodeSelector={"body"}
+		isolate = { true }
+		preventDefault = { true }
+		stopPropagation = { true } 
+	  />
+	)
   }
 }
 
