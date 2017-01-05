8d59f8629c4fd1d0c7e102ebcba95b03807709d4
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-01-05 11:07:11 +0100

    FRESH-1081 #755: SQL: make it more robust
    FRESH-1081 #755 Automatic upload orders in csv file with COPY into c_olcand

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.ordercandidate/5455080_sys_gh755_fix_processor_add_datasource.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.ordercandidate/5455080_sys_gh755_fix_processor_add_datasource.sql
@@ -1,8 +1,12 @@
 
+-- choose this form for the insert in case this ad_inputdatasource was already added.
 INSERT INTO ad_inputdatasource (ad_client_id, ad_inputdatasource_id, ad_org_id, created, createdby, description, isactive, m_product_id, name, updated, updatedby, internalname, a_asset_id, isdestination, entitytype, isedienabled) 
-VALUES 
-(0, 170, 0, now(), 100, 'vgl. https://github.com/metasfresh/metasfresh/issues/755' , 'Y', NULL, 'Importiert via SQL COPY Befehl', now(), 100, 'SOURCE.SQL-COPY', NULL, 'N', 'de.metas.ordercandidate', 'N');
+SELECT
+0, 170, 0, now(), 100, 'vgl. https://github.com/metasfresh/metasfresh/issues/755' , 'Y', NULL, 'Importiert via SQL COPY Befehl', now(), 100, 'SOURCE.SQL-COPY', NULL, 'N', 'de.metas.ordercandidate', 'N'
+WHERE NOT EXISTS (select 1 from ad_inputdatasource where ad_inputdatasource_ID=170);
 
+-- to allow postgres to pick a correct C_OLCand_ID
+ALTER TABLE c_olcand ALTER COLUMN c_olcand_id SET DEFAULT nextval('c_olcand_seq');
 
 UPDATE AD_RelationType SET InternalName='C_OLCandProcessor_1000003<=>C_OLCand' WHERE InternalName='C_OLCandProcessor_1000000<=>C_OLCand';
 UPDATE AD_Reference SET Name='RelType_C_OLCandProcessor_1000003' WHERE AD_Reference_ID=540475;
