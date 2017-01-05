3d45ecfb8b5a4c07bfd0d976210720dadaab5b03
damian.przygodzki <damian.przygodzki@jazzy.pro>
2017-01-05 17:29:46 +0100

    #116 Prompt redesign

--- src/components/app/Prompt.js
+++ src/components/app/Prompt.js
@@ -10,44 +10,60 @@ class Prompt extends Component {
         super(props);
     }
 
-    componentDidMount() {
+    renderCancelButton = () => {
+        const {buttons} = this.props;
+        return(
+            <span
+                className="btn btn-meta-outline-secondary btn-distance-3 btn-sm"
+                onClick={(e) => this.props.onCancelClick(e)}
+            >
+                {buttons.cancel}
+            </span>
+        )
     }
 
-    renderCancelButton = () => {
-      return(
-        <span className="btn btn-meta-outline-secondary btn-distance-3 btn-sm" onClick={(e) => this.props.onCancelClick(e)}>
-            {this.props.buttons.cancel}
-        </span>
-      )
-    }
     renderSubmitButton = () => {
-      return(
-        <span className="btn btn-meta-primary btn-sm btn-submit" onClick={(e) => this.props.onSubmitClick(e)}>
-            {this.props.buttons.submit}
-        </span>
-      )
+        const {buttons} = this.props;
+        return(
+            <span
+                className="btn btn-meta-primary btn-sm btn-submit"
+                onClick={(e) => this.props.onSubmitClick(e)}
+            >
+                {buttons.submit}
+            </span>
+        )
     }
+
     render() {
-        const {onCancelClick, onSubmitClick} = this.props;
-        const {cancel, submit} = this.props.buttons;
+        const {onCancelClick, onSubmitClick, title, buttons} = this.props;
+        const {cancel, submit} = buttons;
         return (
-          <div className="screen-freeze screen-prompt-freeze">
-              <div className="panel panel-modal-primary panel-prompt prompt-shadow">
-                  <div className="panel-modal-header panel-prompt-header">
-                      <span className="panel-modal-header-title">{this.props.title}</span>
-                      <i className="meta-icon-close-1" onClick={(e) => this.props.onCancelClick(e)}></i>
-                  </div>
-                  <div className="panel-modal-content panel-prompt-content">
-                    <p>{this.props.text}</p>
-                  </div>
-                  <div className="panel-modal-header panel-prompt-header panel-prompt-footer">
-                    <div className="prompt-button-wrapper">
-                      {cancel ? this.renderCancelButton() : ""}
-                      {submit ? this.renderSubmitButton() : ""}
+            <div className="screen-freeze screen-prompt-freeze">
+                <div
+                    className="panel panel-modal-primary panel-prompt prompt-shadow"
+                >
+                    <div className="panel-modal-header panel-prompt-header">
+                        <span
+                            className="panel-prompt-header-title panel-modal-header-title"
+                        >
+                            {title}
+                        </span>
+                        <i
+                            className="meta-icon-close-1"
+                            onClick={(e) => onCancelClick(e)}
+                        />
                     </div>
-                  </div>
-              </div>
-          </div>
+                    <div className="panel-modal-content panel-prompt-content">
+                        <p>{this.props.text}</p>
+                    </div>
+                    <div className="panel-modal-header panel-prompt-header panel-prompt-footer">
+                        <div className="prompt-button-wrapper">
+                            {cancel ? this.renderCancelButton() : ""}
+                            {submit ? this.renderSubmitButton() : ""}
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
