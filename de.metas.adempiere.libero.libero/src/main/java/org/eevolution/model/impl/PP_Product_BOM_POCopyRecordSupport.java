14150e1c293a8aecea26c24aefe6f04e045cdc4d
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-01-09 18:45:36 +0200

    FRESH-796 #507 Copy with Details for PP_Product_BOM

---
+++ de.metas.adempiere.libero.libero/src/main/java/org/eevolution/model/impl/PP_Product_BOM_POCopyRecordSupport.java
@@ -0,0 +1,51 @@
+package org.eevolution.model.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.adempiere.model.GeneralCopyRecordSupport;
+import org.adempiere.model.TableInfoVO;
+import org.compiere.model.GridTab;
+import org.compiere.model.PO;
+import org.eevolution.model.I_PP_Product_BOMLine;
+
+/*
+ * #%L
+ * de.metas.adempiere.libero.libero
+ * %%
+ * Copyright (C) 2017 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+public class PP_Product_BOM_POCopyRecordSupport extends GeneralCopyRecordSupport
+{
+	@Override
+	public List<TableInfoVO> getSuggestedChildren(final PO po, final GridTab gridTab)
+	{
+		final List<TableInfoVO> list = super.getSuggestedChildren(po, gridTab);
+		// remove order tax from list
+		final List<TableInfoVO> finalList = new ArrayList<TableInfoVO>();
+		for (final TableInfoVO childTableInfo : list)
+		{
+			if (I_PP_Product_BOMLine.Table_Name.equals(childTableInfo.tableName))
+			{
+				finalList.add(childTableInfo);
+			}
+		}
+		return finalList;
+	}
+}
