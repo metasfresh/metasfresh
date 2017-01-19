d8a680f183505f789d0d1c5d3e0c75a4949b8cf2
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-01-19 13:31:50 +0200

    FRESH-1191 #835 do not allow rrun for all the processes

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5455380_sys_FRESH-1191_AllowProcessrerun_Script.sql
@@ -0,0 +1,26 @@
+
+-- set all preocesses to not accept rerun
+ UPDATE AD_Process
+SET AllowProcessRerun = 'N' ;
+
+
+-- set the menu processes to accept rerun
+
+UPDATE  AD_Process p
+SET AllowProcessRerun = 'Y' 
+FROM
+(
+SELECT p.AD_Process_ID from AD_Menu m
+JOIN AD_Process p on m.AD_Process_ID = p.AD_Process_ID
+WHERE m.Action = 'P'
+AND m.IsActive = 'Y' AND p.IsActive = 'Y'
+) x
+WHERE p.AD_Process_ID = x.AD_Process_ID;
+
+
+-- Activate Bestellung au Auftrag
+-- 19.01.2017 13:25
+-- URL zum Konzept
+UPDATE AD_Process SET AllowProcessReRun='Y',Updated=TO_TIMESTAMP('2017-01-19 13:25:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=193
+;
+
