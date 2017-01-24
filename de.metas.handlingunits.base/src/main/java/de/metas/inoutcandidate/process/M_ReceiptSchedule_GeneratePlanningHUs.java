244dd275a4f1d5449d5d31044693ab9de2c87e1d
Teo Sarca <teo.sarca@gmail.com>
2017-01-24 13:02:06 +0200

    FRESH-746 #461 M_ReceiptSchedule_GeneratePlanningHUs improve

--- de.metas.handlingunits.base/src/main/java/de/metas/inoutcandidate/process/M_ReceiptSchedule_GeneratePlanningHUs.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/inoutcandidate/process/M_ReceiptSchedule_GeneratePlanningHUs.java
@@ -1,18 +1,35 @@
 package de.metas.inoutcandidate.process;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import org.adempiere.ad.trx.api.ITrx;
+import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.uom.api.Quantity;
+import org.adempiere.util.Check;
+import org.adempiere.util.Services;
 import org.adempiere.util.lang.impl.TableRecordReference;
 
 import de.metas.adempiere.form.terminal.TerminalException;
+import de.metas.handlingunits.IHandlingUnitsDAO;
 import de.metas.handlingunits.allocation.ILUTUProducerAllocationDestination;
-import de.metas.handlingunits.impl.IDocumentLUTUConfigurationManager;
 import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_HU_LUTU_Configuration;
+import de.metas.handlingunits.model.I_M_HU_PI;
+import de.metas.handlingunits.model.I_M_HU_PI_Item;
+import de.metas.handlingunits.model.I_M_HU_PI_Item_Product;
+import de.metas.handlingunits.model.I_M_HU_PI_Version;
 import de.metas.handlingunits.model.I_M_ReceiptSchedule;
+import de.metas.handlingunits.model.X_M_HU_PI_Version;
+import de.metas.handlingunits.receiptschedule.IHUReceiptScheduleBL;
 import de.metas.handlingunits.receiptschedule.impl.ReceiptScheduleHUGenerator;
+import de.metas.inoutcandidate.api.IReceiptScheduleBL;
+import de.metas.process.IProcessDefaultParameter;
+import de.metas.process.IProcessDefaultParametersProvider;
+import de.metas.process.IProcessPrecondition;
 import de.metas.process.JavaProcess;
+import de.metas.process.Param;
 
 /*
  * #%L
@@ -36,28 +53,99 @@ import de.metas.process.JavaProcess;
  * #L%
  */
 
-public class M_ReceiptSchedule_GeneratePlanningHUs extends JavaProcess
+public class M_ReceiptSchedule_GeneratePlanningHUs extends JavaProcess implements IProcessPrecondition, IProcessDefaultParametersProvider
 {
+	//
+	// Parameters
+	private static final String PARAM_M_HU_PI_Item_Product_ID = "M_HU_PI_Item_Product_ID";
+	@Param(parameterName = PARAM_M_HU_PI_Item_Product_ID)
+	private int p_M_HU_PI_Item_Product_ID;
+	//
+	private static final String PARAM_M_LU_HU_PI_ID = "M_LU_HU_PI_ID";
+	@Param(parameterName = PARAM_M_LU_HU_PI_ID)
+	private int p_M_LU_HU_PI_ID;
+	//
+	private static final String PARAM_QtyCU = "QtyCU";
+	@Param(parameterName = PARAM_QtyCU)
+	private BigDecimal p_QtyCU;
+	//
+	private static final String PARAM_QtyTU = "QtyTU";
+	@Param(parameterName = PARAM_QtyTU)
+	private BigDecimal p_QtyTU;
+	//
+	private static final String PARAM_QtyLU = "QtyLU";
+	@Param(parameterName = PARAM_QtyLU)
+	private BigDecimal p_QtyLU;
+
+	//
+	// State
+	private I_M_HU_LUTU_Configuration _defaultLUTUConfiguration; // lazy
+
+	@Override
+	public boolean isPreconditionApplicable(final PreconditionsContext context)
+	{
+		final IReceiptScheduleBL receiptScheduleBL = Services.get(IReceiptScheduleBL.class);
+
+		// Receipt schedule shall not be already closed
+		final I_M_ReceiptSchedule receiptSchedule = context.getModel(I_M_ReceiptSchedule.class);
+		if (receiptScheduleBL.isClosed(receiptSchedule))
+		{
+			return false;
+		}
+
+		// Receipt schedule shall not be about packing materials
+		if (receiptSchedule.isPackagingMaterial())
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public Object getParameterDefaultValue(final IProcessDefaultParameter parameter)
+	{
+		switch (parameter.getColumnName())
+		{
+			case PARAM_M_HU_PI_Item_Product_ID:
+				return getCurrentLUTUConfiguration().getM_HU_PI_Item_Product_ID();
+			case PARAM_M_LU_HU_PI_ID:
+				return getCurrentLUTUConfiguration().getM_LU_HU_PI_ID();
+			case PARAM_QtyCU:
+				return getCurrentLUTUConfiguration().getQtyCU();
+			case PARAM_QtyTU:
+				return getCurrentLUTUConfiguration().getQtyTU();
+			case PARAM_QtyLU:
+				return getCurrentLUTUConfiguration().getQtyLU();
+			default:
+				return DEFAULT_VALUE_NOTAVAILABLE;
+		}
+	}
+
+	private I_M_HU_LUTU_Configuration getCurrentLUTUConfiguration()
+	{
+		if (_defaultLUTUConfiguration == null)
+		{
+			final I_M_ReceiptSchedule receiptSchedule = getM_ReceiptSchedule();
+			_defaultLUTUConfiguration = Services.get(IHUReceiptScheduleBL.class)
+					.createLUTUConfigurationManager(receiptSchedule)
+					.getCreateLUTUConfiguration();
+		}
+		return _defaultLUTUConfiguration;
+	}
+
 	@Override
 	protected String doIt() throws Exception
 	{
-		final ReceiptScheduleHUGenerator huGenerator = new ReceiptScheduleHUGenerator();
-		huGenerator.setContext(this);
-		final I_M_ReceiptSchedule schedule = getRecord(I_M_ReceiptSchedule.class);
-		huGenerator.addM_ReceiptSchedule(schedule);
+		final ReceiptScheduleHUGenerator huGenerator = ReceiptScheduleHUGenerator.newInstance(this)
+				.addM_ReceiptSchedule(getM_ReceiptSchedule());
 
 		//
 		// Get/Create and Edit LU/TU configuration
-		final IDocumentLUTUConfigurationManager lutuConfigurationManager = huGenerator.getLUTUConfigurationManager();
-
-		final I_M_HU_LUTU_Configuration lutuConfiguration = lutuConfigurationManager.createAndEdit(lutuConfigurationToEdit -> lutuConfigurationToEdit);
-
-		//
-		// No configuration => user cancelled => don't open editor
-		if (lutuConfiguration == null)
-		{
-			return null;
-		}
+		final I_M_HU_LUTU_Configuration lutuConfiguration = huGenerator
+				.getLUTUConfigurationManager()
+				.createAndEdit(this::updateM_HU_LUTU_Configuration);
+		Check.assumeNotNull(lutuConfiguration, "lutuConfiguration is not null");
 
 		//
 		// Calculate the target CUs that we want to allocate
@@ -75,5 +163,68 @@ public class M_ReceiptSchedule_GeneratePlanningHUs extends JavaProcess
 		getResult().setRecordsToSelectAfterExecution(TableRecordReference.ofList(hus));
 
 		return MSG_OK;
+	}
+
+	private I_M_HU_LUTU_Configuration updateM_HU_LUTU_Configuration(final I_M_HU_LUTU_Configuration lutuConfigurationToEdit)
+	{
+		if (isUseDefaultLUTUConfiguration())
+		{
+			return lutuConfigurationToEdit;
+		}
+
+		//
+		// CU
+		lutuConfigurationToEdit.setQtyCU(p_QtyCU);
+
+		//
+		// TU
+		final I_M_HU_PI_Item_Product tuPIItemProduct = InterfaceWrapperHelper.create(getCtx(), p_M_HU_PI_Item_Product_ID, I_M_HU_PI_Item_Product.class, ITrx.TRXNAME_None);
+		final I_M_HU_PI tuPI = tuPIItemProduct.getM_HU_PI_Item().getM_HU_PI_Version().getM_HU_PI();
+		lutuConfigurationToEdit.setM_HU_PI_Item_Product(tuPIItemProduct);
+		lutuConfigurationToEdit.setM_TU_HU_PI(tuPI);
+		lutuConfigurationToEdit.setQtyTU(p_QtyTU);
+
+		//
+		// LU
+		if (p_M_LU_HU_PI_ID > 0)
+		{
+			final I_M_HU_PI luPI = InterfaceWrapperHelper.create(getCtx(), p_M_LU_HU_PI_ID, I_M_HU_PI.class, ITrx.TRXNAME_None);
+
+			final IHandlingUnitsDAO handlingUnitsDAO = Services.get(IHandlingUnitsDAO.class);
+			final I_M_HU_PI_Version luPIV = handlingUnitsDAO.retrievePICurrentVersion(luPI);
+			final I_M_HU_PI_Item luPI_Item = handlingUnitsDAO.retrieveParentPIItemsForParentPI(tuPI, X_M_HU_PI_Version.HU_UNITTYPE_LoadLogistiqueUnit, lutuConfigurationToEdit.getC_BPartner())
+					.stream()
+					.filter(piItem -> piItem.getM_HU_PI_Version_ID() == luPIV.getM_HU_PI_Version_ID())
+					.findFirst()
+					.orElseThrow(() -> new AdempiereException("@NotFound@ @M_HU_PI_Version_ID@"));
+
+			lutuConfigurationToEdit.setM_LU_HU_PI(luPI);
+			lutuConfigurationToEdit.setM_LU_HU_PI_Item(luPI_Item);
+			lutuConfigurationToEdit.setQtyLU(p_QtyLU);
+		}
+		else
+		{
+			lutuConfigurationToEdit.setM_LU_HU_PI(null);
+			lutuConfigurationToEdit.setM_LU_HU_PI_Item(null);
+			lutuConfigurationToEdit.setQtyLU(null);
+		}
+
+		InterfaceWrapperHelper.save(lutuConfigurationToEdit);
+
+		return lutuConfigurationToEdit;
+	}
+
+	private boolean isUseDefaultLUTUConfiguration()
+	{
+		return p_M_HU_PI_Item_Product_ID <= 0
+				&& p_M_LU_HU_PI_ID <= 0
+				&& (p_QtyCU == null || p_QtyCU.signum() <= 0)
+				&& (p_QtyTU == null || p_QtyTU.signum() <= 0)
+				&& (p_QtyLU == null || p_QtyLU.signum() <= 0);
+	}
+
+	private I_M_ReceiptSchedule getM_ReceiptSchedule()
+	{
+		return getRecord(I_M_ReceiptSchedule.class);
 	}
 }
