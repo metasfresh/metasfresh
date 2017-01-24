metasfresh/metasfresh

commit 6efcd76118c4c7b111caff48fd13e1aafbde4912

FRESH-112 InterfaceWrapperHelper.getTableName(modelClass, expectedTableName)
* also refactor
* javadoc
* junit tests

de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/impl/QueryBuilder.java
@@ -82,26 +82,21 @@ import com.google.common.collect.ImmutableMap;
 	 */
 	public QueryBuilder(final Class<T> modelClass, final String tableName)
 	{
+		super();
 		this.modelClass = modelClass;
 		this.modelKeyColumnName = null; // lazy
 
+		this.modelTableName = InterfaceWrapperHelper.getTableName(modelClass, tableName);
+		
 		final IQueryBL factory = Services.get(IQueryBL.class);
-		if (Check.isEmpty(tableName, true))
-		{
-			filters = factory.createCompositeQueryFilter(modelClass);
-			this.modelTableName = InterfaceWrapperHelper.getTableName(modelClass);
-		}
-		else
-		{
-			filters = factory.createCompositeQueryFilter(tableName);
-			this.modelTableName = tableName; // when the table's name is required, don't use the modelClass to obtain it but go with the given table name.
-		}
+		filters = factory.createCompositeQueryFilter(tableName); // always use the tableName we just fetched because it might be that modelClass is not providing a tableName.
 	}
 
 
 
 	private QueryBuilder(final QueryBuilder<T> from)
 	{
+		super();
 		this.modelClass = from.modelClass;
 		this.modelTableName = from.modelTableName;
 		this.modelKeyColumnName = from.modelKeyColumnName;

de.metas.adempiere.adempiere/base/src/main/java-legacy/org/adempiere/ad/dao/impl/TypedSqlQuery.java
@@ -115,21 +115,11 @@ public class TypedSqlQuery<T> extends AbstractTypedQuery<T>
 	 */
 	protected TypedSqlQuery(final Properties ctx, final Class<T> modelClass, final String tableName, final String whereClause, final String trxName)
 	{
+		super();
 		Check.assumeNotNull(ctx, "ctx not null");
 
 		this.modelClass = modelClass;
-
-		if (Check.isEmpty(tableName, true))
-		{
-			this.tableName = InterfaceWrapperHelper.getTableName(modelClass);
-			Check.errorIf(Check.isEmpty(this.tableName, true),
-					"Parameter 'tableName' is empty, and tableName can not be deducted from modelClass={}; trxName={}; whereClause={}; ctx={}",
-					modelClass, trxName, whereClause, ctx);
-		}
-		else
-		{
-			this.tableName = tableName;
-		}
+		this.tableName = InterfaceWrapperHelper.getTableName(modelClass, tableName);
 
 		this.ctx = ctx;
 
@@ -141,7 +131,7 @@ public class TypedSqlQuery<T> extends AbstractTypedQuery<T>
 	{
 		this(ctx,
 				modelClass,
-				InterfaceWrapperHelper.getTableName(modelClass),
+				(String)null, // tableName
 				whereClause,
 				trxName);
 	}

de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/IQueryBL.java
@@ -38,12 +38,12 @@ public interface IQueryBL extends ISingletonService
 	IQueryBuilder<Object> createQueryBuilder(String modelTableName, Object contextProvider);
 
 	/**
-	 * Create a query builder to quirey for a class like <code>IProductAware</code>, for which the framework can't deduct the table name.
+	 * Create a query builder to query for a class like <code>IProductAware</code> (but also regular model interfaces like I_C_Order are supported), for which the framework can't deduct the table name.
 	 *
 	 * @param modelClass
-	 * @param tableName name of the table in question, which can't be deducted from the given <code>modelClass</code>.
+	 * @param tableName name of the table in question, which can't be deducted from the given <code>modelClass</code>. In case this is null, the tableName will be fetched from <code>modelClass</code>.
 	 * @param contextProvider
-	 * @return
+	 * @return query builder
 	 */
 	<T> IQueryBuilder<T> createQueryBuilder(Class<T> modelClass, String tableName, Object contextProvider);
 

de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/model/InterfaceWrapperHelper.java
@@ -30,6 +30,8 @@ import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 
+import javax.annotation.Nullable;
+
 import org.adempiere.ad.dao.cache.IModelCacheService;
 import org.adempiere.ad.model.util.IModelCopyHelper;
 import org.adempiere.ad.model.util.ModelCopyHelper;
@@ -683,13 +685,30 @@ public class InterfaceWrapperHelper
 	 * Introducing this exception to be thrown instead of ADempiereException. Reason: It's a pain if you have a breakpoint on "AdempiereException" and the debugger stops every 2 seconds because
 	 * InterfaceWrapperHelper throws it.
 	 */
-	/* package */static class MissingTableNameException extends AdempiereException
+	/* package */@SuppressWarnings("serial")
+	static class MissingTableNameException extends AdempiereException
 	{
-		private static final long serialVersionUID = 6469196469943285793L;
-
-		private MissingTableNameException(final Class<?> clazz)
+		private static final MissingTableNameException notFound(final Class<?> modelClass)
 		{
-			super("@NotFound@ @TableName@ (class=" + clazz + ")");
+			return new MissingTableNameException("@NotFound@ @TableName@ (class=" + modelClass + ")");
+		}
+		
+		private static final MissingTableNameException notFound(final Class<?> modelClass, final String fallbackTableName)
+		{
+			return new MissingTableNameException("@NotFound@ @TableName@ (class=" + modelClass + ", fallbackTableName=" + fallbackTableName + ")");
+		}
+		
+		private static final MissingTableNameException notMatching(final Class<?> modelClass, final String modelClassTableName, final String expectedTableName)
+		{
+			return new MissingTableNameException("modelClass's table name is not matching the expected table name:"
+					+ "\n modelClass=" + modelClass
+					+ "\n modelClassTableName=" + modelClassTableName
+					+ "\n expectedTableName=" + expectedTableName);
+		}
+
+		private MissingTableNameException(final String message)
+		{
+			super(message);
 		}
 	}
 
@@ -707,14 +726,14 @@ public class InterfaceWrapperHelper
 	 * @return tableName associated with given interface
 	 * @throws AdempiereException if "Table_Name" static variable is not defined or is not accessible
 	 */
-	public static String getTableName(final Class<?> clazz) throws AdempiereException
+	public static String getTableName(final Class<?> modelClass) throws AdempiereException
 	{
-		final String tableName = getTableNameOrNull(clazz);
-		if (tableName == null)
+		final String modelClassTableName = getTableNameOrNull(modelClass);
+		if (modelClassTableName == null)
 		{
-			throw new MissingTableNameException(clazz);
+			throw MissingTableNameException.notFound(modelClass);
 		}
-		return tableName;
+		return modelClassTableName;
 	}
 
 	/**
@@ -731,6 +750,52 @@ public class InterfaceWrapperHelper
 			return null;
 		}
 		return modelClassInfo.getTableName();
+	}
+
+	/**
+	 * Extracts the table name from given modelClass.
+	 * If the modelClass does not have a table name it will return <code>expectedTableName</code> if that's not null.
+	 * If the modelClass has a table name but it's not matching the expectedTableName (if not null) an exception will be thrown.
+	 * If the modelClass does not hava a table name and <code>expectedTableName</code> is null an exception will be thrown.
+	 * 
+	 * @param modelClass
+	 * @param expectedTableName
+	 * @return model table name; never returns null
+	 */
+	public static String getTableName(final Class<?> modelClass, @Nullable final String expectedTableName)
+	{
+		final String modelClassTableName = getTableNameOrNull(modelClass);
+
+		// Case: there is no expected/default table name
+		// => fail if modelClass has no table name either.
+		if(expectedTableName == null)
+		{
+			if (modelClassTableName == null)
+			{
+				throw MissingTableNameException.notFound(modelClass, expectedTableName);
+			}
+			
+			return modelClassTableName;
+		}
+		// Case: there is an expected/default table name
+		else
+		{
+			// Sub-case: no model class table name => return the default/expected one
+			if (modelClassTableName == null)
+			{
+				return expectedTableName;
+			}
+			// Sub-case: model class table name matches the expected one => perfect, return it
+			else if (modelClassTableName.equals(expectedTableName))
+			{
+				return modelClassTableName;
+			}
+			// Sub-case: model class table name DOES NOT match the expected one => fail
+			else
+			{
+				throw MissingTableNameException.notMatching(modelClass, modelClassTableName, expectedTableName);
+			}
+		}
 	}
 
 	public static final boolean isModelInterface(final Class<?> modelClass)

de.metas.adempiere.adempiere/base/src/test/java/org/adempiere/model/InterfaceWrapperHelper_Tests.java
@@ -11,6 +11,7 @@ import org.compiere.model.GridTab;
 import org.compiere.model.I_Test;
 import org.compiere.model.PO;
 import org.compiere.util.Env;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -71,6 +72,15 @@ public class InterfaceWrapperHelper_Tests
 
 	public static interface I_TestModel_Ext extends I_TestModel
 	{
+	}
+
+	public static interface ITaxAware
+	{
+		public String COLUMNNAME_C_Tax_ID = "C_Tax_ID";
+
+		public int getC_Tax_ID();
+
+		public void setC_Tax_ID(final int taxId);
 	}
 
 	private PlainContextAware contextProvider;
@@ -243,5 +253,38 @@ public class InterfaceWrapperHelper_Tests
 
 		final ITableRecordReference testModelRef = TableRecordReference.of(testModel);
 		assertThat(InterfaceWrapperHelper.getId(testModelRef), is(testModelRef.getRecord_ID()));
+	}
+
+	@Test
+	public void test_getTableName_withModelClassTableName_withExpectedTableName()
+	{
+		String tableName = InterfaceWrapperHelper.getTableName(I_TestModel.class, I_TestModel.Table_Name);
+		Assert.assertEquals(I_TestModel.Table_Name, tableName);
+	}
+
+	@Test
+	public void test_getTableName_withModelClassTableName_withNullExpectedTableName()
+	{
+		String tableName = InterfaceWrapperHelper.getTableName(I_TestModel.class, null);
+		Assert.assertEquals(I_TestModel.Table_Name, tableName);
+	}
+
+	@Test(expected = InterfaceWrapperHelper.MissingTableNameException.class)
+	public void test_getTableName_withModelClassTableName_withWrongExpectedTableName()
+	{
+		InterfaceWrapperHelper.getTableName(I_TestModel.class, "WrongTableName");
+	}
+
+	@Test
+	public void test_getTableName_withNonModelClass_withExpectedTableName()
+	{
+		String tableName = InterfaceWrapperHelper.getTableName(ITaxAware.class, "expectedTableName");
+		Assert.assertEquals("expectedTableName", tableName);
+	}
+
+	@Test(expected = InterfaceWrapperHelper.MissingTableNameException.class)
+	public void test_getTableName_withNonModelClass_withNullExpectedTableName()
+	{
+		InterfaceWrapperHelper.getTableName(ITaxAware.class, null);
 	}
 }

commit a6eeec12b7081906654d11740452396eae74fb92

FRESH-112 bugfix QueryBuilder creates the composite query filter with null tableName
the issue was initially introduced by
https://github.com/metasfresh/metasfresh/commit/6efcd76118c4c7b111caff48fd13e1aafbde4912

de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/impl/InSubQueryFilter.java
@@ -25,8 +25,6 @@ package org.adempiere.ad.dao.impl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import org.slf4j.Logger;
-import de.metas.logging.LogManager;
 
 import org.adempiere.ad.dao.IQueryFilter;
 import org.adempiere.ad.dao.IQueryFilterModifier;
@@ -38,6 +36,11 @@ import org.adempiere.model.ModelColumn;
 import org.adempiere.util.Check;
 import org.adempiere.util.Services;
 import org.compiere.model.IQuery;
+import org.slf4j.Logger;
+
+import com.google.common.base.MoreObjects;
+
+import de.metas.logging.LogManager;
 
 /**
  * Filters out only records which are present in sub-query.
@@ -91,17 +94,19 @@ public class InSubQueryFilter<T> implements IQueryFilter<T>, ISqlQueryFilter
 	@Override
 	public String toString()
 	{
-		return "InSubQueryFilter [" + (columnName != null ? "columnName=" + columnName + ", " : "")
-				+ (subQueryColumnName != null ? "subQueryColumnName=" + subQueryColumnName + ", " : "")
-				+ (modifier != null ? "modifier=" + modifier + ", " : "")
-				+ (subQuery != null ? "subQuery=" + subQuery + ", " : "")
-				+ "sqlBuilt=" + sqlBuilt + ", "
-				+ (sqlWhereClause != null ? "sqlWhereClause=" + sqlWhereClause + ", " : "")
-				+ (sqlParams != null ? "sqlParams=" + sqlParams + ", " : "")
-				+ (_subQueryValues != null ? "_subQueryValues=" + _subQueryValues : "")
-				+ "]";
+		return MoreObjects.toStringHelper(this)
+				.omitNullValues()
+				.add("tableName", tableName)
+				.add("columnName", columnName)
+				.add("subQueryColumnName", subQueryColumnName)
+				.add("modifier", modifier)
+				.add("subQuery", subQuery)
+				.add("sqlBuilt", sqlBuilt)
+				.add("sqlWhereClause", sqlWhereClause)
+				.add("sqlParams", sqlParams)
+				.add("_subQueryValues", _subQueryValues)
+				.toString();
 	}
-
 	@Override
 	public String getSql()
 	{

de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/impl/QueryBuilder.java
@@ -89,7 +89,7 @@ import com.google.common.collect.ImmutableMap;
 		this.modelTableName = InterfaceWrapperHelper.getTableName(modelClass, tableName);
 		
 		final IQueryBL factory = Services.get(IQueryBL.class);
-		filters = factory.createCompositeQueryFilter(tableName); // always use the tableName we just fetched because it might be that modelClass is not providing a tableName.
+		filters = factory.createCompositeQueryFilter(this.modelTableName); // always use the tableName we just fetched because it might be that modelClass is not providing a tableName.
 	}
 
 