5b753897959eb5e34b6899b01f98777dd0022ee9
Teo Sarca <teo.sarca@gmail.com>
2017-01-29 19:30:11 +0200

    FRESH-112 DocumentId more helpers; refactor

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/window/datatypes/DocumentId.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/window/datatypes/DocumentId.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import javax.annotation.concurrent.Immutable;
 
 import org.adempiere.util.GuavaCollectors;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
 
 import de.metas.printing.esb.base.util.Check;
@@ -42,6 +44,8 @@ public abstract class DocumentId implements Serializable
 	private static final int NEW_ID = -1;
 	public static final String NEW_ID_STRING = "NEW";
 	private static final DocumentId NEW = new IntDocumentId(NEW_ID);
+
+	private static final Splitter SPLITTER_DocumentIds = Splitter.on(",").trimResults().omitEmptyStrings();
 
 	public static final DocumentId of(String idStr)
 	{
@@ -85,6 +89,43 @@ public abstract class DocumentId implements Serializable
 	public static DocumentId ofString(final String idStr)
 	{
 		return new StringDocumentId(idStr);
+	}
+
+	public static Set<DocumentId> ofCommaSeparatedString(final String string)
+	{
+		if (string == null || string.isEmpty())
+		{
+			return ImmutableSet.of();
+		}
+		return streamFromCommaSeparatedString(string)
+				.collect(GuavaCollectors.toImmutableSet());
+	}
+
+	public static Stream<DocumentId> streamFromCommaSeparatedString(final String string)
+	{
+		// avoid NPE
+		if (string == null)
+		{
+			return Stream.empty();
+		}
+
+		return SPLITTER_DocumentIds.splitToList(string)
+				.stream()
+				.map(idStr -> DocumentId.fromNullable(idStr))
+				.filter(documentId -> documentId != null);
+
+	}
+
+	public static Set<DocumentId> ofStringSet(final Set<String> documentIds)
+	{
+		if (documentIds == null || documentIds.isEmpty())
+		{
+			return ImmutableSet.of();
+		}
+		return documentIds
+				.stream()
+				.map(idStr -> of(idStr))
+				.collect(GuavaCollectors.toImmutableSet());
 	}
 
 	public static final DocumentId fromNullable(final String idStr)
