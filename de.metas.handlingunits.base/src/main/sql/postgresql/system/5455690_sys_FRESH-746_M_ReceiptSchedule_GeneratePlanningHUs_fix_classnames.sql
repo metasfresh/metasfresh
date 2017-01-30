24594d9bf21042523907757e596cb9bb686169f4
Teo Sarca <teo.sarca@gmail.com>
2017-01-30 16:55:55 +0200

    FRESH-746 separate M_ReceiptSchedule_GeneratePlanningHUs processes

---
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5455690_sys_FRESH-746_M_ReceiptSchedule_GeneratePlanningHUs_fix_classnames.sql
@@ -0,0 +1,10 @@
+-- Jan 30, 2017 4:47 PM
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Classname='de.metas.inoutcandidate.process.M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig', Value='M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig',Updated=TO_TIMESTAMP('2017-01-30 16:47:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540753
+;
+
+-- Jan 30, 2017 4:48 PM
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Classname='de.metas.inoutcandidate.process.M_ReceiptSchedule_GeneratePlanningHUs_UsingDefaults',Updated=TO_TIMESTAMP('2017-01-30 16:48:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540749
+;
+
