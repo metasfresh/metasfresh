fc9a37bed464c984aef678d2e826291f409eaf71
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-01-31 11:10:12 +0100

    #460 add M_HU_Item.ItemType list, fields and snapshot columns
    FRESH-744 #460 Reference to HU Attributes instead of identical copies

---
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5455724_sys_gh460_M_HU_ItemType_Snapshot.sql
@@ -0,0 +1,26 @@
+
+-- 31.01.2017 08:31
+-- URL zum Konzept
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,AllowZoomTo,ColumnName,Created,CreatedBy,DDL_NoForeignKey,EntityType,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsSelectionColumn,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,556048,542144,0,17,540699,540670,'N','ItemType',TO_TIMESTAMP('2017-01-31 08:31:21','YYYY-MM-DD HH24:MI:SS'),100,'N','de.metas.handlingunits',2,'Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','Positionsart',0,TO_TIMESTAMP('2017-01-31 08:31:21','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 31.01.2017 08:31
+-- URL zum Konzept
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=556048 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+
+-- 31.01.2017 08:32
+-- URL zum Konzept
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,AllowZoomTo,ColumnName,Created,CreatedBy,DDL_NoForeignKey,EntityType,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsSelectionColumn,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,556049,542185,0,30,540670,'N','M_HU_PackingMaterial_ID',TO_TIMESTAMP('2017-01-31 08:32:02','YYYY-MM-DD HH24:MI:SS'),100,'N','de.metas.handlingunits',10,'Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','Packmittel',0,TO_TIMESTAMP('2017-01-31 08:32:02','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 31.01.2017 08:32
+-- URL zum Konzept
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=556049 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+COMMIT;
+
+SELECT db_alter_table('M_HU_Item_Snapshot', 'ALTER TABLE public.M_HU_Item_Snapshot ADD ItemType VARCHAR(2) DEFAULT NULL');
+SELECT db_alter_table('M_HU_Item_Snapshot', 'ALTER TABLE public.M_HU_Item_Snapshot ADD M_HU_PackingMaterial_ID NUMERIC(10) DEFAULT NULL');
