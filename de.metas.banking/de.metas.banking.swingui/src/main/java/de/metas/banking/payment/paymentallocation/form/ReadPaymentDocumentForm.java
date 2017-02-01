6f9a5debdc148ca9e74e0eb3839c619d345cc23f
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2017-02-01 07:11:19 +0100

    #897 minor comment
    FRESH-1113 #781 ESR scan processing returns improper bpartner

--- de.metas.banking/de.metas.banking.swingui/src/main/java/de/metas/banking/payment/paymentallocation/form/ReadPaymentDocumentForm.java
+++ de.metas.banking/de.metas.banking.swingui/src/main/java/de/metas/banking/payment/paymentallocation/form/ReadPaymentDocumentForm.java
@@ -106,7 +106,9 @@ public class ReadPaymentDocumentForm implements FormPanel, IProcessPrecondition
 	private ReadPaymentDocumentPanel createAndBindPanel(final int windowNo, final Frame frame, final int adOrgId)
 	{
 		final ReadPaymentDocumentPanel readPaymentPanel = new ReadPaymentDocumentPanel(windowNo, frame, adOrgId);
-		readPaymentPanel.setContextBPartner(invoice==null?null:invoice.getC_BPartner());
+		
+		// gh #897: provide the invoice's bPartner so the panel can filter matching accounts by relevance
+		readPaymentPanel.setContextBPartner(invoice == null ? null : invoice.getC_BPartner());
 		
 		frame.addWindowListener(new ReadPaymentDialogWindowAdapter(readPaymentPanel)
 		{
