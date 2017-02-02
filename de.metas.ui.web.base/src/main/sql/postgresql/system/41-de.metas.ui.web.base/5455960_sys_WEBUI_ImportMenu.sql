cb5132c3d0dbee8ffd3c3b6b42522a06d7f42b21
Teo Sarca <teo.sarca@gmail.com>
2017-02-02 15:21:55 +0200

    delete/import the whole webui menu+tree
    based on w101
    
    (cherry picked from commit e1f0a4e8dbc72d1954412180031840331b53af2c)

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5455960_sys_WEBUI_ImportMenu.sql
@@ -0,0 +1,79 @@
+-- https://drive.google.com/file/d/0BxR8srpWrnzvZmswWkhXZ0FHc1U/view
+
+--
+-- Backup
+create table backup.AD_TreeNodeMM_BKP_BeforeImportWebUI as select * from AD_TreeNodeMM;
+create table backup.AD_Menu_BKP_BeforeImportWebUI as select * from AD_Menu;
+create table backup.AD_Menu_Trk_BKP_BeforeImportWebUI as select * from AD_Menu_Trl;
+
+--
+-- Expected input tables:
+-- backup.WEBUI_AD_TreeNodeMM
+-- backup.WEBUI_AD_Menu
+-- backup.WEBUI_AD_Menu_Trl
+
+--
+-- Import AD_TreeNodeMM webui menu
+delete from AD_TreeNodeMM where AD_Tree_ID=1000039;
+insert into AD_TreeNodeMM
+(
+	AD_Tree_ID
+	, Node_ID
+	, Parent_ID
+	, SeqNo
+	, AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy
+)
+select
+	t.AD_Tree_ID
+	, t.Node_ID
+	, t.Parent_ID
+	, t.SeqNo
+	, 0 as AD_Client_ID, 0 as AD_Org_ID, now() as Created, 0 as CreatedBy, now() as Updated, 0 as UpdatedBy
+from backup.WEBUI_AD_TreeNodeMM t;
+
+--
+--
+-- Import AD_Menu for webui
+--
+--
+
+--
+-- Update existing entries for AD_Menu and AD_Menu_Trl
+update AD_Menu m set
+	InternalName=t.InternalName
+	, Name=t.Name
+	, Description=t.Description
+	, IsCreateNew=t.IsCreateNew
+	, WEBUI_NameBrowse=t.WEBUI_NameBrowse
+	, WEBUI_NameNew=t.WEBUI_NameNew
+	, WEBUI_NameNewBreadcrumb=t.WEBUI_NameNewBreadcrumb
+from backup.WEBUI_AD_Menu t
+where t.AD_Menu_ID=m.AD_Menu_ID;
+--
+update AD_Menu_Trl m set
+	Name=t.Name
+	, Description=t.Description
+	, WEBUI_NameBrowse=t.WEBUI_NameBrowse
+	, WEBUI_NameNew=t.WEBUI_NameNew
+	, WEBUI_NameNewBreadcrumb=t.WEBUI_NameNewBreadcrumb
+	, IsTranslated=t.IsTranslated
+from backup.WEBUI_AD_Menu_Trl t
+where t.AD_Menu_ID=m.AD_Menu_ID and t.AD_Language=m.AD_Language;
+
+
+--
+-- Insert missing entries for AD_Menu and AD_Menu_Trl
+insert into AD_Menu
+(ad_menu_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, name, updatedby, description, issummary, issotrx, isreadonly, action, ad_window_id, ad_workflow_id, ad_task_id, ad_process_id, ad_form_id, ad_workbench_id, entitytype, internalname, iscreatenew, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb)
+select
+ad_menu_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, name, updatedby, description, issummary, issotrx, isreadonly, action, ad_window_id, ad_workflow_id, ad_task_id, ad_process_id, ad_form_id, ad_workbench_id, entitytype, internalname, iscreatenew, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb
+from backup.WEBUI_AD_Menu t
+where not exists (select 1 from AD_Menu m where m.AD_Menu_ID=t.AD_Menu_ID);
+--
+insert into AD_Menu_Trl
+(ad_menu_id, ad_language, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, istranslated, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb)
+select
+ad_menu_id, ad_language, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, istranslated, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb
+from backup.WEBUI_AD_Menu_Trl t
+where not exists (select 1 from AD_Menu_Trl m where m.AD_Menu_ID=t.AD_Menu_ID and m.AD_Language=t.AD_Language);
+
