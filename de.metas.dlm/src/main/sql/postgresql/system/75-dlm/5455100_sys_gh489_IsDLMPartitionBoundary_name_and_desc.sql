493d7e77c9968f442db6d283a88ac037d27db42e
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-02-03 18:05:07 +0100

    #489 SQL encoding fix
    FRESH-771 #489 Implement DLM within single logical tables

--- de.metas.dlm/src/main/sql/postgresql/system/75-dlm/5455100_sys_gh489_IsDLMPartitionBoundary_name_and_desc.sql
+++ de.metas.dlm/src/main/sql/postgresql/system/75-dlm/5455100_sys_gh489_IsDLMPartitionBoundary_name_and_desc.sql
@@ -1,7 +1,7 @@
-
+ï»¿
 -- 06.01.2017 09:17
 -- URL zum Konzept
-UPDATE AD_Element SET Description='Falls ja, dann gehören Datensätze, die über die jeweilige Referenz verknüpft sind nicht zur selben Partition. Nach eine Änderung an eine für DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausführen.',Updated=TO_TIMESTAMP('2017-01-06 09:17:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=543253
+UPDATE AD_Element SET Description='Falls ja, dann gehÃ¶ren DatensÃ¤tze, die Ã¼ber die jeweilige Referenz verknÃ¼pft sind nicht zur selben Partition. Nach eine Ã„nderung an eine fÃ¼r DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausfÃ¼hren.',Updated=TO_TIMESTAMP('2017-01-06 09:17:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=543253
 ;
 
 -- 06.01.2017 09:17
@@ -11,20 +11,20 @@ UPDATE AD_Element_Trl SET IsTranslated='N' WHERE AD_Element_ID=543253
 
 -- 06.01.2017 09:17
 -- URL zum Konzept
-UPDATE AD_Column SET ColumnName='IsDLMPartitionBoundary', Name='Partitionsgrenze', Description='Falls ja, dann gehören Datensatze, die über die jeweilige Referenz verknüpft sind nicht zur selben Partition. Nach eine Änderung an eine für DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausführen.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers' WHERE AD_Element_ID=543253
+UPDATE AD_Column SET ColumnName='IsDLMPartitionBoundary', Name='Partitionsgrenze', Description='Falls ja, dann gehÃ¶ren Datensatze, die Ã¼ber die jeweilige Referenz verknÃ¼pft sind nicht zur selben Partition. Nach eine Ã„nderung an eine fÃ¼r DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausfÃ¼hren.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers' WHERE AD_Element_ID=543253
 ;
 
 -- 06.01.2017 09:17
 -- URL zum Konzept
-UPDATE AD_Process_Para SET ColumnName='IsDLMPartitionBoundary', Name='Partitionsgrenze', Description='Falls ja, dann gehören Datensatze, die über die jeweilige Referenz verknüpft sind nicht zur selben Partition. Nach eine Änderung an eine für DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausführen.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers', AD_Element_ID=543253 WHERE UPPER(ColumnName)='ISDLMPARTITIONBOUNDARY' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+UPDATE AD_Process_Para SET ColumnName='IsDLMPartitionBoundary', Name='Partitionsgrenze', Description='Falls ja, dann gehÃ¶ren Datensatze, die Ã¼ber die jeweilige Referenz verknÃ¼pft sind nicht zur selben Partition. Nach eine Ã„nderung an eine fÃ¼r DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausfÃ¼hren.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers', AD_Element_ID=543253 WHERE UPPER(ColumnName)='ISDLMPARTITIONBOUNDARY' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
 ;
 
 -- 06.01.2017 09:17
 -- URL zum Konzept
-UPDATE AD_Process_Para SET ColumnName='IsDLMPartitionBoundary', Name='Partitionsgrenze', Description='Falls ja, dann gehören Datensatze, die über die jeweilige Referenz verknüpft sind nicht zur selben Partition. Nach eine Änderung an eine für DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausführen.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers' WHERE AD_Element_ID=543253 AND IsCentrallyMaintained='Y'
+UPDATE AD_Process_Para SET ColumnName='IsDLMPartitionBoundary', Name='Partitionsgrenze', Description='Falls ja, dann gehÃ¶ren Datensatze, die Ã¼ber die jeweilige Referenz verknÃ¼pft sind nicht zur selben Partition. Nach eine Ã„nderung an eine fÃ¼r DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausfÃ¼hren.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers' WHERE AD_Element_ID=543253 AND IsCentrallyMaintained='Y'
 ;
 
 -- 06.01.2017 09:17
 -- URL zum Konzept
-UPDATE AD_Field SET Name='Partitionsgrenze', Description='Falls ja, dann gehören Datensätze, die über die jeweilige Referenz verknüpft sind nicht zur selben Partition. Nach eine Änderung an einer bereits für DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausführen.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=543253) AND IsCentrallyMaintained='Y'
+UPDATE AD_Field SET Name='Partitionsgrenze', Description='Falls ja, dann gehÃ¶ren DatensÃ¤tze, die Ã¼ber die jeweilige Referenz verknÃ¼pft sind nicht zur selben Partition. Nach eine Ã„nderung an einer bereits fÃ¼r DLM eingerichteten Tabelle bitte "select dlm.recreate_dlm_triggers(''tablename'')" ausfÃ¼hren.', Help='If you change this value for a DLM''ed table then please run  "select dlm.recreate_dlm_triggers(''tablename'')" to update the DLM triggers' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=543253) AND IsCentrallyMaintained='Y'
 ;
