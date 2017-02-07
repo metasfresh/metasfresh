f8ed77a1657065c74f01231a4ae5da01138bb7d8
Teo Sarca <teo.sarca@gmail.com>
2017-02-07 14:36:15 +0200

    #118 POST /rest/api/window/{windowId}/{documentId}/attachments endpoint

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/window/controller/WindowRestController.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/window/controller/WindowRestController.java
@@ -1,9 +1,12 @@
 package de.metas.ui.web.window.controller;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import org.adempiere.service.IAttachmentBL;
+import org.adempiere.util.Services;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,11 +14,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.common.collect.ImmutableList;
 
@@ -62,11 +67,11 @@ import io.swagger.annotations.ApiParam;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
@@ -501,5 +506,30 @@ public class WindowRestController
 		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
 		final ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(reportData, headers, HttpStatus.OK);
 		return response;
+	}
+
+	/**
+	 * Attaches a file to given root document.
+	 * 
+	 * @param adWindowId
+	 * @param documentId
+	 * @param file
+	 * @throws IOException
+	 */
+	@PostMapping("/{windowId}/{documentId}/attachments")
+	public void attachFile(
+			@PathVariable("windowId") final int adWindowId //
+			, @PathVariable("documentId") final String documentId //
+			, @RequestParam("file") final MultipartFile file //
+	) throws IOException
+	{
+		userSession.assertLoggedIn();
+
+		final String name = file.getOriginalFilename();
+		final byte[] data = file.getBytes();
+
+		final DocumentPath documentPath = DocumentPath.rootDocumentPath(DocumentType.Window, adWindowId, documentId);
+		final Document document = documentCollection.getDocument(documentPath);
+		Services.get(IAttachmentBL.class).createAttachment(document, name, data);
 	}
 }
