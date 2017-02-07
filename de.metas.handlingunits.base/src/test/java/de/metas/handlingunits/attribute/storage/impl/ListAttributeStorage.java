f0086bf06dc43d574b3cbb5bd140da64e16cf7f0
Teo Sarca <teo.sarca@gmail.com>
2017-02-07 16:49:13 +0200

    implement IAttributeStorage.setSaveOnChange
    task https://github.com/metasfresh/metasfresh-webui/issues/100

--- de.metas.handlingunits.base/src/test/java/de/metas/handlingunits/attribute/storage/impl/ListAttributeStorage.java
+++ de.metas.handlingunits.base/src/test/java/de/metas/handlingunits/attribute/storage/impl/ListAttributeStorage.java
@@ -13,11 +13,11 @@ package de.metas.handlingunits.attribute.storage.impl;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
@@ -124,6 +124,12 @@ public class ListAttributeStorage extends AbstractAttributeStorage
 
 	@Override
 	public void saveChangesIfNeeded()
+	{
+		throw new UnsupportedOperationException("not implemented");
+	}
+
+	@Override
+	public void setSaveOnChange(final boolean saveOnChange)
 	{
 		throw new UnsupportedOperationException("not implemented");
 	}
