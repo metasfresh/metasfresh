7c5756c012ef1b304d75f98d6e576a8041cb513b
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-02-08 16:54:10 +0100

    #781 fix NPE on missing C_BP_BankAccount
    * the actual fix is in ReadPaymentDocumentPanel
    * also fix ugly encoding error in AD_Message
    
    FRESH-1113 #781 ESR scan processing returns improper bpartner

---
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5456180_sys_gh781_fix_ad_messages.sql
@@ -0,0 +1,22 @@
+-- 08.02.2017 16:42
+-- URL zum Konzept
+UPDATE AD_Message SET MsgText='Zugehörige Rechnung wird noch von mindestens einem "Zahlung anweisen" Datensatz referenziert: 
+Datensatz-Name {0}, ZeilenNr {1}.',Updated=TO_TIMESTAMP('2017-02-08 16:42:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=543255
+;
+
+-- 08.02.2017 16:42
+-- URL zum Konzept
+UPDATE AD_Message_Trl SET IsTranslated='N' WHERE AD_Message_ID=543255
+;
+
+-- 08.02.2017 16:43
+-- URL zum Konzept
+UPDATE AD_Message SET MsgText='Zu den eingelesenen Konto-Information konnte kein bestehendes Konto ermittelt werden. 
+Bitte erstellen Sie manuell ein Geschäftspartner-Konto oder geben im Eltern-Dialog einen Geschäftspartner an. damit das Konto hier on-the-fly erstellt werden kann.',Updated=TO_TIMESTAMP('2017-02-08 16:43:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=542877
+;
+
+-- 08.02.2017 16:43
+-- URL zum Konzept
+UPDATE AD_Message_Trl SET IsTranslated='N' WHERE AD_Message_ID=542877
+;
+
