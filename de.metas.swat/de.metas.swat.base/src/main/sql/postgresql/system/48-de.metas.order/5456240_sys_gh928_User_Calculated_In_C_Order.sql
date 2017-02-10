343374852c1145a828dd43a56ad2777809bc0fab
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-02-10 12:16:50 +0200

    #928  Make AD_User and Bill_User calculated in C_Order
    FRESH-1301 #928 have a way to control which contact is used on addresses
    in order

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.order/5456240_sys_gh928_User_Calculated_In_C_Order.sql
@@ -0,0 +1,9 @@
+-- 10.02.2017 11:50
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2017-02-10 11:50:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2763
+;
+
+-- 10.02.2017 11:50
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2017-02-10 11:50:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=8763
+;
