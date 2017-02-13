99f0e1f9b0b8a480816347e7e24ad76dc03f1aef
Katarzyna Dunikowska <katarzyna.dunikowska@jazzy.pro>
2017-02-13 16:25:09 +0100

    Fix MR rows data

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -97,6 +97,10 @@ class Table extends Component {
                 }
 
             })
+        } else {
+            this.setState({
+                rows: rowData[tabid]
+            });
         }
     }
 
@@ -127,9 +131,9 @@ class Table extends Component {
     selectProduct = (id, idFocused, idFocusedDown) => {
         const {dispatch} = this.props;
 
-        this.setState(prevState => {
+        this.setState(prevState => Object.assign({}, this.state, {
             selected: prevState.selected.concat([id])
-        }, () => {
+        }), () => {
             dispatch(selectTableItems(this.state.selected))
             this.triggerFocus(idFocused, idFocusedDown);
         })
@@ -145,15 +149,9 @@ class Table extends Component {
         const {rowData, tabid, keyProperty, indentSupported} = this.props;
         const {rows} = this.state;
         const property = keyProperty ? keyProperty : "rowId";
-        
+        const toSelect = rows.map((item, index) => item[property]);
 
-        if(indentSupported){
-            const toSelect = rows.map((item, index) => item[property]);
-            this.selectRangeProduct(toSelect);
-        } else {
-            const toSelect = rowData[tabid].map((item, index) => item[property]);
-            this.selectRangeProduct(toSelect);
-        }
+        this.selectRangeProduct(toSelect);
         
     }
 
@@ -204,8 +202,7 @@ class Table extends Component {
 
     handleKeyDown = (e) => {
         const {rowData, tabid, listenOnKeys} = this.props;
-        const item = rowData[tabid];
-        const {selected} = this.state;
+        const {selected, rows} = this.state;
         const selectRange = e.shiftKey;
 
         const nodeList = Array.prototype.slice.call(document.activeElement.parentElement.children);
@@ -220,30 +217,30 @@ class Table extends Component {
             case "ArrowDown":
                 e.preventDefault();
 
-                const actualId = Object.keys(rowData[tabid]).findIndex(x => x === selected[selected.length-1])
+                const actualId = Object.keys(rows).findIndex(x => x === selected[selected.length-1])
 
-                if(actualId < Object.keys(rowData[tabid]).length-1 ){
+                if(actualId < Object.keys(rows).length-1 ){
                     let newId = actualId+1;
 
                     if(!selectRange) {
-                        this.selectOneProduct(Object.keys(rowData[tabid])[newId], false, idFocused);
+                        this.selectOneProduct(Object.keys(rows)[newId], false, idFocused);
                     } else {
-                        this.selectProduct(Object.keys(rowData[tabid])[newId], false, idFocused);
+                        this.selectProduct(Object.keys(rows)[newId], false, idFocused);
                     }
                 }
                 break;
             case "ArrowUp":
                 e.preventDefault();
 
-                const actual = Object.keys(rowData[tabid]).findIndex(x => x === selected[selected.length-1])
+                const actual = Object.keys(rows).findIndex(x => x === selected[selected.length-1])
 
                 if(actual > 0 ){
                     let newId = actual-1;
 
                     if(!selectRange) {
-                        this.selectOneProduct(Object.keys(rowData[tabid])[newId], idFocused, false);
+                        this.selectOneProduct(Object.keys(rows)[newId], idFocused, false);
                     } else {
-                        this.selectProduct(Object.keys(rowData[tabid])[newId], idFocused, false);
+                        this.selectProduct(Object.keys(rows)[newId], idFocused, false);
                     }
                 }
                 break;
@@ -305,29 +302,22 @@ class Table extends Component {
 
     handleKeyDownDocList = (e) => {
         const {selected, rows} = this.state;
+
         const {
-            rowData, tabid, listenOnKeys, onDoubleClick, closeOverlays, open,
-            indentSupported
+            rowData, tabid, listenOnKeys, onDoubleClick, closeOverlays, open
         } = this.props;
-
+        
         const selectRange = e.shiftKey;
-        let data = "";
-
-        if(indentSupported){
-            data = rows;
-        } else {
-            data = rowData[tabid];
-        }
 
         switch(e.key) {
             case "ArrowDown":
                 e.preventDefault();
 
-                const array = (data).map((item, id) => {
+                const array = rows.map((item, id) => {
                     return item.id
                 });
 
-                const actualId = array.findIndex(x => x === selected[selected.length-1])
+                const actualId = array.findIndex(x => x === selected[selected.length-1]);
 
                 if(actualId < array.length-1 ){
                     let newId = actualId+1;
@@ -342,7 +332,7 @@ class Table extends Component {
             case "ArrowUp":
                 e.preventDefault();
 
-                const arrays = (data).map((item, id) => {
+                const arrays = rows.map((item, id) => {
                     return item.id
                 });
 
