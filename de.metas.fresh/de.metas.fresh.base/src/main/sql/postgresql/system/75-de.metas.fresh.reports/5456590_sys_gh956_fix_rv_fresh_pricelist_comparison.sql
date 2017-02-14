c5e1b895760047450a5fafbf4cde7309adfbe15c
metas-lc <liana.cotuna@metasfresh.com>
2017-02-14 16:53:22 +0200

    gh956 fix rv_fresh_pricelist_comparison view

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5456590_sys_gh956_fix_rv_fresh_pricelist_comparison.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5456590_sys_gh956_fix_rv_fresh_pricelist_comparison.sql
@@ -1,6 +1,6 @@
---DROP VIEW IF EXISTS rv_fresh_pricelist 
+﻿DROP VIEW IF EXISTS rv_fresh_pricelist ;
 
---DROP VIEW IF EXISTS RV_fresh_PriceList_Comparison;
+DROP VIEW IF EXISTS RV_fresh_PriceList_Comparison;
 
 CREATE OR REPLACE VIEW RV_fresh_PriceList_Comparison AS 
 
@@ -127,3 +127,48 @@ COMMENT ON VIEW RV_fresh_PriceList_Comparison
 Refactored in Tasks 07833 and 07915
 A view for a report that displays the same data as RV_fresh_PriceList but imroved to be able to filter by two Price list versions, to be able to compare them';
 
+
+
+
+
+
+--  add rv_fresh_pricelist back
+
+DROP VIEW IF EXISTS RV_fresh_PriceList;
+
+CREATE OR REPLACE VIEW RV_fresh_PriceList AS
+SELECT
+	-- Mandatory Columns
+	plc.AD_Org_ID, plc.AD_Client_ID,
+	plc.Created, plc.CreatedBy,
+	plc.Updated, plc.UpdatedBy,
+	plc.IsActive,
+
+	-- Displayed pricelist data
+	plc.ProductCategory,
+	plc.ProductCategoryValue,
+	plc.M_Product_ID,
+	plc.Value,
+	plc.ProductName,
+	plc.IsSeasonFixedPrice,
+	plc.ItemProductName,
+	plc.PackingMaterialName,
+	plc.PriceStd,
+	plc.AltPriceStd,
+	plc.HasAltPrice,
+	plc.UOMSymbol,
+	plc.Attributes,
+
+	-- Filter Columns
+	plc.C_BPartner_ID,
+	plc.M_Pricelist_Version_ID,
+	plc.M_ProductPrice_ID
+FROM
+	RV_fresh_PriceList_Comparison plc 
+WHERE
+	plc.M_Pricelist_Version_ID = plc.Alt_PriceList_Version_ID
+;
+
+COMMENT ON VIEW RV_fresh_PriceList IS '05956 Printformat for Pricelist (109054740508) 
+Refactored in Task 07833 and 07915
+A view for a report that displays all product prices of a BPartner, including the attribute prices and CU:TU relation';
