e86bbf322befbdca6521aa6c85a9f21ebeeb15c0
Teo Sarca <teo.sarca@gmail.com>
2017-02-15 19:26:09 +0200

    #127 improve error messages

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.exceptions.FillMandatoryException;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
 
@@ -78,16 +79,15 @@ public class WEBUI_M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig extends WEB
 		}
 		return _defaultLUTUConfiguration;
 	}
-	
+
 	private static void adjustLUs(final I_M_HU_LUTU_Configuration lutuConfiguration, final I_M_ReceiptSchedule fromReceiptSchedule)
 	{
 		final BigDecimal qtyToReceiveTU = Services.get(IReceiptScheduleBL.class).getQtyToMove(fromReceiptSchedule);
-		
+
 		final ILUTUConfigurationFactory lutuConfigurationFactory = Services.get(ILUTUConfigurationFactory.class);
 		final int qtyLU = lutuConfigurationFactory.calculateQtyLUForTotalQtyTUs(lutuConfiguration, qtyToReceiveTU);
 		lutuConfiguration.setQtyLU(BigDecimal.valueOf(qtyLU));
 	}
-	
 
 	private static final String PARAM_IsSaveLUTUConfiguration = "IsSaveLUTUConfiguration";
 	@Param(parameterName = PARAM_IsSaveLUTUConfiguration)
@@ -114,7 +114,7 @@ public class WEBUI_M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig extends WEB
 	private static final String PARAM_QtyLU = "QtyLU";
 	@Param(parameterName = PARAM_QtyLU)
 	private BigDecimal p_QtyLU;
-	
+
 	@Override
 	protected boolean isUpdateReceiptScheduleDefaultConfiguration()
 	{
@@ -124,26 +124,53 @@ public class WEBUI_M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig extends WEB
 	@Override
 	protected I_M_HU_LUTU_Configuration createM_HU_LUTU_Configuration(final I_M_HU_LUTU_Configuration template)
 	{
+		// Validate parameters
+		final int M_LU_HU_PI_ID = p_M_LU_HU_PI_ID;
+		final int M_HU_PI_Item_Product_ID = p_M_HU_PI_Item_Product_ID;
+		final BigDecimal qtyCU = p_QtyCU;
+		final BigDecimal qtyTU = p_QtyTU;
+		final BigDecimal qtyLU = p_QtyLU;
+		if (M_LU_HU_PI_ID <= 0)
+		{
+			throw new FillMandatoryException(PARAM_M_LU_HU_PI_ID);
+		}
+		if (M_HU_PI_Item_Product_ID <= 0)
+		{
+			throw new FillMandatoryException(PARAM_M_HU_PI_Item_Product_ID);
+		}
+		if (qtyCU == null || qtyCU.signum() <= 0)
+		{
+			throw new FillMandatoryException(PARAM_QtyCU);
+		}
+		if (qtyTU == null || qtyTU.signum() <= 0)
+		{
+			throw new FillMandatoryException(PARAM_QtyTU);
+		}
+		if (qtyLU == null || qtyLU.signum() <= 0)
+		{
+			throw new FillMandatoryException(PARAM_QtyLU);
+		}
+
 		final I_M_HU_LUTU_Configuration lutuConfigurationNew = InterfaceWrapperHelper.copy()
 				.setFrom(template)
 				.copyToNew(I_M_HU_LUTU_Configuration.class);
 		//
 		// CU
-		lutuConfigurationNew.setQtyCU(p_QtyCU);
+		lutuConfigurationNew.setQtyCU(qtyCU);
 
 		//
 		// TU
-		final I_M_HU_PI_Item_Product tuPIItemProduct = InterfaceWrapperHelper.create(getCtx(), p_M_HU_PI_Item_Product_ID, I_M_HU_PI_Item_Product.class, ITrx.TRXNAME_None);
+		final I_M_HU_PI_Item_Product tuPIItemProduct = InterfaceWrapperHelper.create(getCtx(), M_HU_PI_Item_Product_ID, I_M_HU_PI_Item_Product.class, ITrx.TRXNAME_None);
 		final I_M_HU_PI tuPI = tuPIItemProduct.getM_HU_PI_Item().getM_HU_PI_Version().getM_HU_PI();
 		lutuConfigurationNew.setM_HU_PI_Item_Product(tuPIItemProduct);
 		lutuConfigurationNew.setM_TU_HU_PI(tuPI);
-		lutuConfigurationNew.setQtyTU(p_QtyTU);
+		lutuConfigurationNew.setQtyTU(qtyTU);
 
 		//
 		// LU
-		if (p_M_LU_HU_PI_ID > 0)
+		if (M_LU_HU_PI_ID > 0)
 		{
-			final I_M_HU_PI luPI = InterfaceWrapperHelper.create(getCtx(), p_M_LU_HU_PI_ID, I_M_HU_PI.class, ITrx.TRXNAME_None);
+			final I_M_HU_PI luPI = InterfaceWrapperHelper.create(getCtx(), M_LU_HU_PI_ID, I_M_HU_PI.class, ITrx.TRXNAME_None);
 
 			final IHandlingUnitsDAO handlingUnitsDAO = Services.get(IHandlingUnitsDAO.class);
 			final I_M_HU_PI_Version luPIV = handlingUnitsDAO.retrievePICurrentVersion(luPI);
@@ -151,11 +178,11 @@ public class WEBUI_M_ReceiptSchedule_GeneratePlanningHUs_UsingConfig extends WEB
 					.stream()
 					.filter(piItem -> piItem.getM_HU_PI_Version_ID() == luPIV.getM_HU_PI_Version_ID())
 					.findFirst()
-					.orElseThrow(() -> new AdempiereException("@NotFound@ @M_HU_PI_Version_ID@"));
+					.orElseThrow(() -> new AdempiereException(tuPI.getName() + " cannot be loaded to " + luPI.getName()));
 
 			lutuConfigurationNew.setM_LU_HU_PI(luPI);
 			lutuConfigurationNew.setM_LU_HU_PI_Item(luPI_Item);
-			lutuConfigurationNew.setQtyLU(p_QtyLU);
+			lutuConfigurationNew.setQtyLU(qtyLU);
 		}
 		else
 		{
