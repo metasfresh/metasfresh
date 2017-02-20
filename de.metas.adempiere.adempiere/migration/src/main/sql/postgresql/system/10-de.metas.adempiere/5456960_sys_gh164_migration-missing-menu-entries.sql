307672d7ae0cc4e3a9c367ef24b11b1a2dc238e6
Mark Krake <mark.krake@metasfresh.com>
2017-02-20 21:15:58 +0100

    #164 Add Tree Build Script to menu changes.
    Add rebuild script to create the needed Tree elements for updated menu
    entries.
    
    FRESH-1456 #164 Create migration script for missing Menu

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5456960_sys_gh164_migration-missing-menu-entries.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5456960_sys_gh164_migration-missing-menu-entries.sql
@@ -903,3 +903,39 @@ DELETE FROM AD_Menu WHERE AD_Menu_ID=540767
 DELETE FROM AD_TreeNodeMM n WHERE Node_ID=540767 AND EXISTS (SELECT * FROM AD_Tree t WHERE t.AD_Tree_ID=n.AD_Tree_ID AND t.AD_Table_ID=116)
 ;
 
+
+
+-- create table backup.AD_TreeNodeMM_bkp20170202 as select * from AD_TreeNodeMM;
+--
+-- Remove current tree nodes
+delete from AD_TreeNodeMM where AD_Tree_ID=1000039;
+
+--
+-- Recreate them by copying from Standard Menu tree -> Webui subtree
+insert into AD_TreeNodeMM
+(
+	AD_Tree_ID
+	, Node_ID
+	, Parent_ID
+	, SeqNo
+	, AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy
+)
+select
+	1000039 as AD_Tree_ID -- Webui AD_Tree_ID
+	, (case when Node_ID=1000007 then 0 else Node_ID end) as Node_ID
+	, (case when Parent_ID=1000007 then 0 when Parent_ID=0 then null else Parent_ID end) as Parent_ID
+	, SeqNo
+	, 0 as AD_Client_ID, 0 as AD_Org_ID, now() as Created, 0 as CreatedBy, now() as Updated, 0 as UpdatedBy
+	-- , t.Name, t.Path, t.IsCycle, t.Parent_ID as Parent_ID_Orig
+from get_AD_TreeNodeMM_Paths(10, 1000007) t
+where not IsCycle
+order by path
+;
+
+
+--
+-- Check results:
+-- select * from AD_TreeNodeMM where AD_Tree_ID=1000039 order by Node_ID
+select * from get_AD_TreeNodeMM_Paths(1000039, null) t
+-- where IsCycle
+order by path;
