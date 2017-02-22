6cb6a0906afed49feea9a9af9807c64aafbf516c
metas-lc <liana.cotuna@metasfresh.com>
2017-02-22 15:46:46 +0200

    #1018 add description fields is sales inout, shing and webui. Other fixes
    FRESH-1484 #1018 support description fields in shipment
    fix markup in description fields

--- de.metas.endcustomer.mf15/de.metas.endcustomer.mf15.base/src/main/sql/postgresql/system/75-de.metas.endcustomer.mf15.reports/5457031_sys_gh1018_update_Docs_Inout_Details_Footer.sql
+++ de.metas.endcustomer.mf15/de.metas.endcustomer.mf15.base/src/main/sql/postgresql/system/75-de.metas.endcustomer.mf15.reports/5457031_sys_gh1018_update_Docs_Inout_Details_Footer.sql
@@ -1,4 +1,4 @@
---DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Inout_Details_Footer( IN M_InOut_ID numeric, IN AD_Language Character Varying (6) );
+﻿--DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Inout_Details_Footer( IN M_InOut_ID numeric, IN AD_Language Character Varying (6) );
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Inout_Details_Footer( IN M_InOut_ID numeric, IN AD_Language Character Varying (6) )
 
 RETURNS TABLE
