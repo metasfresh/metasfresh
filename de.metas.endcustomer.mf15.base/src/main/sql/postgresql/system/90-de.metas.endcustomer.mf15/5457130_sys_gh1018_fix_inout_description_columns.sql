8a60a657ca4295eed8a2354aa3174a35d1584982
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-02-24 18:30:18 +0100

    #1018 fix desc columns of m_inout table, thx @metasnw
    FRESH-1484 #1018 support description fields in shipment

---
+++ de.metas.endcustomer.mf15.base/src/main/sql/postgresql/system/90-de.metas.endcustomer.mf15/5457130_sys_gh1018_fix_inout_description_columns.sql
@@ -0,0 +1,21 @@
+
+
+-- 24.02.2017 17:11
+-- URL zum Konzept
+UPDATE AD_Column SET AD_Reference_ID=14, FieldLength=1024,Updated=TO_TIMESTAMP('2017-02-24 17:11:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3515
+;
+
+-- 24.02.2017 17:11
+-- URL zum Konzept
+INSERT INTO t_alter_column values('m_inout','Description','VARCHAR(1024)',null,'NULL')
+;
+
+-- 24.02.2017 17:11
+-- URL zum Konzept
+UPDATE AD_Column SET AD_Reference_ID=14, FieldLength=1024,Updated=TO_TIMESTAMP('2017-02-24 17:11:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=501682
+;
+
+-- 24.02.2017 17:11
+-- URL zum Konzept
+INSERT INTO t_alter_column values('m_inout','DescriptionBottom','VARCHAR(1024)',null,'NULL')
+;
