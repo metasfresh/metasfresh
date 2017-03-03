952ac09887a8788b83fbba728c348f01ad9fc9c8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-03-03 17:32:10 +0100

    Fix: add another display logic for WEBUI_M_HU_Transform parameter
    FRESH-1504 metasfresh/metasfresh-webui#181 Transforming HU in Handling
    Unit Editor

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5457480_sys_gh181_WEBUI_M_HU_Transform_displayLogic.sql
@@ -0,0 +1,5 @@
+-- 03.03.2017 16:51
+-- URL zum Konzept
+UPDATE AD_Process_Para SET ReadOnlyLogic='@Action = ''CU_To_NewTUs'' | @Action = ''TU_To_NewTUs'' | @Action = ''TU_To_NewLU''',Updated=TO_TIMESTAMP('2017-03-03 16:51:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541161
+;
+
