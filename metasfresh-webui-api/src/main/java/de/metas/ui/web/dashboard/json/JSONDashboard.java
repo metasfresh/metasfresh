105955d8aedd3454ba8252382e94e215217d85e4
Teo Sarca <teo.sarca@gmail.com>
2017-03-03 14:49:10 +0200

    #184 don't fail the whole dashboard if one item/kpi is not valid

--- metasfresh-webui-api/src/main/java/de/metas/ui/web/dashboard/json/JSONDashboard.java
+++ metasfresh-webui-api/src/main/java/de/metas/ui/web/dashboard/json/JSONDashboard.java
@@ -5,9 +5,11 @@ import java.util.Collection;
 import java.util.List;
 
 import org.adempiere.util.GuavaCollectors;
+import org.slf4j.Logger;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import de.metas.logging.LogManager;
 import de.metas.ui.web.dashboard.UserDashboardItem;
 import de.metas.ui.web.window.datatypes.json.JSONOptions;
 
@@ -24,11 +26,11 @@ import de.metas.ui.web.window.datatypes.json.JSONOptions;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
@@ -41,6 +43,8 @@ public class JSONDashboard implements Serializable
 		return new JSONDashboard(items, jsonOpts);
 	}
 
+	private static final Logger logger = LogManager.getLogger(JSONDashboard.class);
+
 	@JsonProperty("items")
 	private final List<JSONDashboardItem> items;
 
@@ -49,7 +53,18 @@ public class JSONDashboard implements Serializable
 		super();
 
 		this.items = items.stream()
-				.map(item -> JSONDashboardItem.of(item, jsonOpts))
+				.map(item -> {
+					try
+					{
+						return JSONDashboardItem.of(item, jsonOpts);
+					}
+					catch (final Exception ex)
+					{
+						logger.warn("Failed converting {} to JSON. Skipped", item, ex);
+						return null;
+					}
+				})
+				.filter(item -> item != null)
 				.collect(GuavaCollectors.toImmutableList());
 	}
 
