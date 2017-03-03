f2836fad30a8238b79ca7ff88e79dcc6e14c509d
Mark Krake <mark.krake@metasfresh.com>
2017-03-03 13:37:29 +0100

    #1066 Add Migration script for Mandatory logic of IsHidePackingMaterialInShipmentPrint
    Changed default value from 'N' to N.
    
    FRESH-1561 https://github.com/metasfresh/metasfresh/issues/1066

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5457450_sys_gh1066-bpartner-mandatoryfield-IsHidePackingMaterialInShipmentPrint.sql
@@ -0,0 +1,5 @@
+-- 03.03.2017 13:21
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2017-03-03 13:21:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=552285
+;
+
