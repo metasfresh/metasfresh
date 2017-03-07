31af3b9fe3ef304f116cbb2fab5cf942e62f12ee
Mark Krake <mark.krake@metasfresh.com>
2017-03-07 16:32:21 +0100

    #1113 Add migration script to deactivate the name field in quickinput window for webUI
    Deactivating the name field for webUI because that cannot be resolved in
    modal overlay for now.
    
    FRESH-1663 https://github.com/metasfresh/metasfresh/issues/1113

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5457890_sys_gh1113-BPartner-quick-input-modal-overlay-layout.sql
@@ -0,0 +1,5 @@
+-- 07.03.2017 16:00
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsActive='N',Updated=TO_TIMESTAMP('2017-03-07 16:00:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=557938
+;
+
