e48121e47828f07e554934e7e79990203eaf93a1
Teo Sarca <teo.sarca@gmail.com>
2017-03-07 12:50:02 +0200

    #1106 FIX permissions shall not consider inactive orgs

--- de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_Org.java
+++ de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_Org.java
@@ -1,36 +1,20 @@
-/******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
- * This program is free software, you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
- * You should have received a copy of the GNU General Public License along    *
- * with this program, if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- * For the text or an alternative of this public license, you may reach us    *
- * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
- * or via info@compiere.org or http://www.compiere.org/license.html           *
- *****************************************************************************/
 /** Generated Model - DO NOT CHANGE */
 package org.compiere.model;
 
 import java.sql.ResultSet;
 import java.util.Properties;
-import org.compiere.util.KeyNamePair;
 
 /** Generated Model for AD_Org
  *  @author Adempiere (generated) 
- *  @version Release 3.5.4a - $Id$ */
-public class X_AD_Org extends PO implements I_AD_Org, I_Persistent 
+ */
+@SuppressWarnings("javadoc")
+public class X_AD_Org extends org.compiere.model.PO implements I_AD_Org, org.compiere.model.I_Persistent 
 {
 
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20090915L;
+	private static final long serialVersionUID = -1778171655L;
 
     /** Standard Constructor */
     public X_AD_Org (Properties ctx, int AD_Org_ID, String trxName)
@@ -50,37 +34,32 @@ public class X_AD_Org extends PO implements I_AD_Org, I_Persistent
       super (ctx, rs, trxName);
     }
 
-    /** AccessLevel
-      * @return 7 - System - Client - Org 
-      */
-    protected int get_AccessLevel()
-    {
-      return accessLevel.intValue();
-    }
 
     /** Load Meta Data */
-    protected POInfo initPO (Properties ctx)
+    @Override
+    protected org.compiere.model.POInfo initPO (Properties ctx)
     {
-      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      org.compiere.model.POInfo poi = org.compiere.model.POInfo.getPOInfo (ctx, Table_Name, get_TrxName());
       return poi;
     }
 
-    public String toString()
-    {
-      StringBuffer sb = new StringBuffer ("X_AD_Org[")
-        .append(get_ID()).append("]");
-      return sb.toString();
-    }
+	@Override
+	public org.compiere.model.I_AD_ReplicationStrategy getAD_ReplicationStrategy() throws RuntimeException
+	{
+		return get_ValueAsPO(COLUMNNAME_AD_ReplicationStrategy_ID, org.compiere.model.I_AD_ReplicationStrategy.class);
+	}
 
-	public I_AD_ReplicationStrategy getAD_ReplicationStrategy() throws RuntimeException
-    {
-		return (I_AD_ReplicationStrategy)MTable.get(getCtx(), I_AD_ReplicationStrategy.Table_Name)
-			.getPO(getAD_ReplicationStrategy_ID(), get_TrxName());	}
+	@Override
+	public void setAD_ReplicationStrategy(org.compiere.model.I_AD_ReplicationStrategy AD_ReplicationStrategy)
+	{
+		set_ValueFromPO(COLUMNNAME_AD_ReplicationStrategy_ID, org.compiere.model.I_AD_ReplicationStrategy.class, AD_ReplicationStrategy);
+	}
 
-	/** Set Replication Strategy.
+	/** Set Replizierung - Strategie.
 		@param AD_ReplicationStrategy_ID 
 		Data Replication Strategy
 	  */
+	@Override
 	public void setAD_ReplicationStrategy_ID (int AD_ReplicationStrategy_ID)
 	{
 		if (AD_ReplicationStrategy_ID < 1) 
@@ -89,9 +68,10 @@ public class X_AD_Org extends PO implements I_AD_Org, I_Persistent
 			set_Value (COLUMNNAME_AD_ReplicationStrategy_ID, Integer.valueOf(AD_ReplicationStrategy_ID));
 	}
 
-	/** Get Replication Strategy.
+	/** Get Replizierung - Strategie.
 		@return Data Replication Strategy
 	  */
+	@Override
 	public int getAD_ReplicationStrategy_ID () 
 	{
 		Integer ii = (Integer)get_Value(COLUMNNAME_AD_ReplicationStrategy_ID);
@@ -100,35 +80,36 @@ public class X_AD_Org extends PO implements I_AD_Org, I_Persistent
 		return ii.intValue();
 	}
 
-	/** Set Description.
-		@param Description 
-		Optional short description of the record
-	  */
-	public void setDescription (String Description)
+	/** Set Beschreibung.
+		@param Description Beschreibung	  */
+	@Override
+	public void setDescription (java.lang.String Description)
 	{
 		set_Value (COLUMNNAME_Description, Description);
 	}
 
-	/** Get Description.
-		@return Optional short description of the record
-	  */
-	public String getDescription () 
+	/** Get Beschreibung.
+		@return Beschreibung	  */
+	@Override
+	public java.lang.String getDescription () 
 	{
-		return (String)get_Value(COLUMNNAME_Description);
+		return (java.lang.String)get_Value(COLUMNNAME_Description);
 	}
 
-	/** Set Summary Level.
+	/** Set Zusammenfassungseintrag.
 		@param IsSummary 
 		This is a summary entity
 	  */
+	@Override
 	public void setIsSummary (boolean IsSummary)
 	{
 		set_Value (COLUMNNAME_IsSummary, Boolean.valueOf(IsSummary));
 	}
 
-	/** Get Summary Level.
+	/** Get Zusammenfassungseintrag.
 		@return This is a summary entity
 	  */
+	@Override
 	public boolean isSummary () 
 	{
 		Object oo = get_Value(COLUMNNAME_IsSummary);
@@ -145,7 +126,8 @@ public class X_AD_Org extends PO implements I_AD_Org, I_Persistent
 		@param Name 
 		Alphanumeric identifier of the entity
 	  */
-	public void setName (String Name)
+	@Override
+	public void setName (java.lang.String Name)
 	{
 		set_Value (COLUMNNAME_Name, Name);
 	}
@@ -153,33 +135,28 @@ public class X_AD_Org extends PO implements I_AD_Org, I_Persistent
 	/** Get Name.
 		@return Alphanumeric identifier of the entity
 	  */
-	public String getName () 
+	@Override
+	public java.lang.String getName () 
 	{
-		return (String)get_Value(COLUMNNAME_Name);
+		return (java.lang.String)get_Value(COLUMNNAME_Name);
 	}
 
-    /** Get Record ID/ColumnName
-        @return ID/ColumnName pair
-      */
-    public KeyNamePair getKeyNamePair() 
-    {
-        return new KeyNamePair(get_ID(), getName());
-    }
-
-	/** Set Search Key.
+	/** Set Suchschlüssel.
 		@param Value 
 		Search key for the record in the format required - must be unique
 	  */
-	public void setValue (String Value)
+	@Override
+	public void setValue (java.lang.String Value)
 	{
 		set_Value (COLUMNNAME_Value, Value);
 	}
 
-	/** Get Search Key.
+	/** Get Suchschlüssel.
 		@return Search key for the record in the format required - must be unique
 	  */
-	public String getValue () 
+	@Override
+	public java.lang.String getValue () 
 	{
-		return (String)get_Value(COLUMNNAME_Value);
+		return (java.lang.String)get_Value(COLUMNNAME_Value);
 	}
 }
