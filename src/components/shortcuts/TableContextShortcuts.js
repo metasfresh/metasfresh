a7c3effcd941c5b7c69c8741a705860e951af0fa
Katarzyna Dunikowska <katarzyna.dunikowska@jazzy.pro>
2017-03-08 08:50:19 +0100

    Change react-shortucts library

--- src/components/shortcuts/TableContextShortcuts.js
+++ src/components/shortcuts/TableContextShortcuts.js
@@ -30,7 +30,7 @@ class TableContextShortcuts extends Component {
                 isolate = { true }
                 preventDefault = { true }
                 stopPropagation = { true }
-                dataShortcuts = { true }
+                alwaysFireHandler = { true }
             />
         )
     }
