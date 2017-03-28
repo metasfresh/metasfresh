7f2184a652a8fe1f52ef11fc07f3e31226d0c168
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-03-28 16:52:59 +0200

    Fix CU-on-LU scenarios: don't try include HA in the play
    * HUJoinBL: don't join a TU to a parent's HA item, because the HA item
    is special (e.g. can represent just one type of TU), and one can't just
    attach random stuff to it.
    * TUProducerDestination: detect the CU-on-LU situation and avoid
    creating an aggregate HU. This is now also verifies in
    LUTUProducerDestinationLoadTest
    * LUTUConfigurationEditorModel: if the current lutu-config already
    contains some sort of virtualPI key, then don't add another one. It's
    confusing to the user
    * rest: minor changes
    
    FRESH-1794 #1194 HU "Herkunft" HU_Attribute is not propagated from LU to
    VHU

--- de.metas.handlingunits.client/src/main/java/de/metas/handlingunits/client/terminal/lutuconfig/model/LUTUConfigurationEditorModel.java
+++ de.metas.handlingunits.client/src/main/java/de/metas/handlingunits/client/terminal/lutuconfig/model/LUTUConfigurationEditorModel.java
@@ -49,6 +49,7 @@ import de.metas.handlingunits.IHUPIItemProductDAO;
 import de.metas.handlingunits.ILUTUConfigurationEditor;
 import de.metas.handlingunits.client.terminal.mmovement.exception.MaterialMovementException;
 import de.metas.handlingunits.client.terminal.mmovement.model.impl.AbstractLTCUModel;
+import de.metas.handlingunits.impl.HandlingUnitsDAO;
 import de.metas.handlingunits.model.I_M_HU_LUTU_Configuration;
 import de.metas.handlingunits.model.I_M_HU_PI;
 import de.metas.handlingunits.model.I_M_HU_PI_Item;
@@ -518,7 +519,7 @@ public class LUTUConfigurationEditorModel extends AbstractLTCUModel
 
 		//
 		// Create TU Keys
-		final Map<String, ILUTUCUKey> tuKeys = new LinkedHashMap<String, ILUTUCUKey>(availableHUPIItemProducts.size());
+		final Map<String, ILUTUCUKey> tuKeys = new LinkedHashMap<String, ILUTUCUKey>();
 		for (final I_M_HU_PI_Item_Product tuPIItemProduct : availableHUPIItemProducts)
 		{
 			final I_M_HU_PI tuPI = tuPIItemProduct.getM_HU_PI_Item().getM_HU_PI_Version().getM_HU_PI();
@@ -533,6 +534,8 @@ public class LUTUConfigurationEditorModel extends AbstractLTCUModel
 
 		//
 		// Add the virtual PI to TU Keys
+		// gh #1194: ... don't add them if lutuConfiguration is already about a virtual PI to start with
+		if (lutuConfiguration.getM_HU_PI_Item_Product().getM_HU_PI_Item_ID() != HandlingUnitsDAO.VIRTUAL_HU_PI_Item_ID)
 		{
 			final ILUTUCUKey tuKey = createVirtualPITUKey(lutuConfiguration);
 			final String tuKeyId = tuKey.getId();
