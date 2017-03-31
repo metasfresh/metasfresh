66e0d09ad814f9f7896edc3f5b9a5587b72be638
metas-lc <liana.cotuna@metasfresh.com>
2017-03-31 15:44:52 +0300

    #741 update packing material product category config
    FRESH-1067 #741 packing material product category config
    set configuration level to org
    
    (cherry picked from commit 36ea7ad187d445c52f004beb33a440c0ff6c6b51)

---
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5459330_sys_gh741_upate_sysconfig_PackingMaterialProductCategoryID.sql
@@ -0,0 +1,5 @@
+﻿-- 2017-03-31T14:26:31.730
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET ConfigurationLevel='O',Updated=TO_TIMESTAMP('2017-03-31 14:26:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=540579
+;
+
