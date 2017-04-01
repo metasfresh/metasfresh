cfbd65258340557eed651585092336ca609c269f
Teo Sarca <teo.sarca@gmail.com>
2017-04-01 19:12:25 +0300

    metasfresh/metasfresh#1248 fix after refactoring

--- src/main/java/de/metas/ui/web/quickinput/inout/EmptiesQuickInputProcessor.java
+++ src/main/java/de/metas/ui/web/quickinput/inout/EmptiesQuickInputProcessor.java
@@ -3,7 +3,7 @@ package de.metas.ui.web.quickinput.inout;
 import java.util.Set;
 
 import de.metas.edi.model.I_M_InOut;
-import de.metas.handlingunits.inout.impl.EmptiesInOutLinesBuilder;
+import de.metas.handlingunits.empties.EmptiesInOutLinesProducer;
 import de.metas.ui.web.quickinput.IQuickInputProcessor;
 import de.metas.ui.web.quickinput.QuickInput;
 import de.metas.ui.web.window.datatypes.DocumentId;
@@ -39,7 +39,7 @@ public class EmptiesQuickInputProcessor implements IQuickInputProcessor
 		final I_M_InOut inout = quickInput.getRootDocumentAs(I_M_InOut.class);
 		final IEmptiesQuickInput emptiesQuickInput = quickInput.getQuickInputDocumentAs(IEmptiesQuickInput.class);
 
-		final Set<Integer> affectedDocumentIds = EmptiesInOutLinesBuilder.newBuilder(inout)
+		final Set<Integer> affectedDocumentIds = EmptiesInOutLinesProducer.newInstance(inout)
 				.addSource(emptiesQuickInput.getM_HU_PackingMaterial(), emptiesQuickInput.getQty())
 				.create()
 				.getAffectedInOutLinesId();
