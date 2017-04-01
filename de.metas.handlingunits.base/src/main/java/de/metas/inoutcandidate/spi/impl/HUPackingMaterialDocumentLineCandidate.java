metasfresh/metasfresh

commit 7872fee00ea28fd9ef79c37b59f0b16e46940dd7

FRESH-744 #460 adapt the code to use the new M_HU/M_HU_Item fields
NOTE: patial, WIP
NOTE: not tested, but junit tests are working

de.metas.handlingunits.base/src/main/java/de/metas/inoutcandidate/spi/impl/HUPackingMaterialDocumentLineCandidate.java
@@ -88,7 +88,7 @@ public final class HUPackingMaterialDocumentLineCandidate
 	 *
 	 * @param product packing material product
 	 */
-	public HUPackingMaterialDocumentLineCandidate(final I_M_Product product, final I_M_Material_Tracking materialTracking, final I_M_Locator locator)
+	/* package */ HUPackingMaterialDocumentLineCandidate(final I_M_Product product, final I_M_Material_Tracking materialTracking, final I_M_Locator locator)
 	{
 		super();
 		Check.assumeNotNull(product, "product not null");
@@ -173,25 +173,14 @@ public final class HUPackingMaterialDocumentLineCandidate
 		return getQty(uomTo);
 	}
 
-	/**
-	 * Increment current packing materials amount (see {@link #getQty()}) by ONE.
-	 */
-	protected void incrementQty()
-	{
-		qty = qty.add(BigDecimal.ONE);
-	}
-
-	/**
-	 * Decrement current packing materials amount (see {@link #getQty()}) by ONE.
-	 */
-	protected void decrementQty()
-	{
-		qty = qty.subtract(BigDecimal.ONE);
-	}
-
 	protected void addQty(final int qtyToAdd)
 	{
 		qty = qty.add(BigDecimal.valueOf(qtyToAdd));
+	}
+
+	protected void subtractQty(final int qtyToSubtract)
+	{
+		qty = qty.add(BigDecimal.valueOf(qtyToSubtract));
 	}
 
 	/**

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/IHandlingUnitsDAO.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.adempiere.util.ISingletonService;
+import org.adempiere.util.lang.IPair;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_M_Locator;
 import org.compiere.model.I_M_Product;
@@ -199,6 +200,7 @@ public interface IHandlingUnitsDAO extends ISingletonService
 	 * @param hu
 	 * @return
 	 */
+	// TODO: i think we shall drop this method because is no longer valid!!!
 	I_M_HU_PackingMaterial retrievePackingMaterial(final I_M_HU hu);
 
 	I_M_HU retrieveVirtualHU(I_M_HU_Item itemMaterial);
@@ -219,14 +221,12 @@ public interface IHandlingUnitsDAO extends ISingletonService
 	 * NOTE
 	 * <ul>
 	 * <li>this method will return packing material(s) of this HU only and not for it's included HUs.</li>
-	 * <li>teoretically you can assign to an HU as many packing materials as you want, but in practice, in most of the cases we will have only one (e.g. an IFCO will have only the plastic IFCO packing
-	 * material, a Palette will have only the wonden palette etc)</li>
 	 * </ul>
 	 *
 	 * @param hu
-	 * @return packing materials
+	 * @return packing material and quantity pairs
 	 */
-	List<I_M_HU_PackingMaterial> retrievePackingMaterials(I_M_HU hu);
+	List<IPair<I_M_HU_PackingMaterial, Integer>> retrievePackingMaterialAndQtys(I_M_HU hu);
 
 	/**
 	 * The special network distribution that is defined for empties (gebinde) It contains lines that link the non-empties warehouses with the empties ones that the packing materials shall be moved to

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/allocation/join/impl/HUJoinBL.java
@@ -61,7 +61,7 @@ public class HUJoinBL implements IHUJoinBL
 				availableLUPIFound = true;
 				break;
 			}
-			if (!X_M_HU_PI_Item.ITEMTYPE_HandlingUnit.equals(luItem.getM_HU_PI_Item().getItemType()))
+			if (!X_M_HU_PI_Item.ITEMTYPE_HandlingUnit.equals(luItem.getItemType()))
 			{
 				//
 				// Item type needs to be handling unit

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/allocation/split/impl/TUProducerDestination.java
@@ -48,7 +48,7 @@ import de.metas.handlingunits.model.I_M_HU_PI;
  * @author tsa
  *
  */
-/* package */class TUProducerDestination extends AbstractProducerDestination
+/* package */final class TUProducerDestination extends AbstractProducerDestination
 {
 	private final I_M_HU_PI tuPI;
 

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/exceptions/HUPIInvalidConfigurationException.java
@@ -1,0 +1,56 @@
+package de.metas.handlingunits.exceptions;
+
+import de.metas.handlingunits.model.I_M_HU_PI;
+import de.metas.handlingunits.model.I_M_HU_PI_Item;
+import de.metas.handlingunits.model.I_M_HU_PI_Version;
+
+/*
+ * #%L
+ * de.metas.handlingunits.base
+ * %%
+ * Copyright (C) 2016 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+@SuppressWarnings("serial")
+public class HUPIInvalidConfigurationException extends HUException
+{
+	public HUPIInvalidConfigurationException(final String message, final I_M_HU_PI_Item piItem)
+	{
+		super(buildMsg(message, piItem));
+	}
+
+	private static String buildMsg(final String message, final I_M_HU_PI_Item piItem)
+	{
+		final I_M_HU_PI_Version piVersion = piItem == null ? null : piItem.getM_HU_PI_Version();
+		final I_M_HU_PI pi = piVersion == null ? null : piVersion.getM_HU_PI();
+
+		final StringBuilder sb = new StringBuilder();
+		sb.append(message);
+
+		if (pi != null)
+		{
+			sb.append("\n PI: ").append(pi);
+		}
+		if (piItem != null)
+		{
+			sb.append("\n PI item: ").append(piItem);
+		}
+
+		return sb.toString();
+	}
+}

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/impl/HUAndItemsDAO.java
@@ -1,5 +1,7 @@
 package de.metas.handlingunits.impl;
 
+import java.math.BigDecimal;
+
 /*
  * #%L
  * de.metas.handlingunits.base
@@ -22,7 +24,6 @@ package de.metas.handlingunits.impl;
  * #L%
  */
 
-
 import java.util.List;
 
 import org.adempiere.ad.trx.api.ITrxManager;
@@ -31,10 +32,14 @@ import org.adempiere.util.Check;
 import org.adempiere.util.Services;
 import org.compiere.util.TrxRunnable;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import de.metas.handlingunits.IHUAndItemsDAO;
+import de.metas.handlingunits.exceptions.HUPIInvalidConfigurationException;
 import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_HU_Item;
 import de.metas.handlingunits.model.I_M_HU_PI_Item;
+import de.metas.handlingunits.model.X_M_HU_Item;
 
 public final class HUAndItemsDAO implements IHUAndItemsDAO
 {
@@ -167,8 +172,8 @@ public final class HUAndItemsDAO implements IHUAndItemsDAO
 		return null;
 	}
 
-	@Override
-	public I_M_HU_Item createHUItem(final I_M_HU hu, final I_M_HU_PI_Item piItem)
+	@VisibleForTesting
+	public static I_M_HU_Item createHUItemNoSave(final I_M_HU hu, final I_M_HU_PI_Item piItem)
 	{
 		Check.assumeNotNull(hu, "hu not null");
 		Check.assumeNotNull(piItem, "piItem not null");
@@ -180,12 +185,38 @@ public final class HUAndItemsDAO implements IHUAndItemsDAO
 		item.setM_HU(hu);
 		item.setM_HU_PI_Item(piItem);
 
+		final String itemType = piItem.getItemType();
+		item.setItemType(itemType);
+
+		if (X_M_HU_Item.ITEMTYPE_PackingMaterial.equals(itemType))
+		{
+			final int huPackingMaterialId = piItem.getM_HU_PackingMaterial_ID();
+			if (huPackingMaterialId <= 0)
+			{
+				throw new HUPIInvalidConfigurationException("No packing material defined", piItem);
+			}
+			final BigDecimal qty = piItem.getQty();
+			if (qty.signum() <= 0)
+			{
+				throw new HUPIInvalidConfigurationException("Invalid packing material quantity defined", piItem);
+			}
+			item.setM_HU_PackingMaterial_ID(huPackingMaterialId);
+			item.setQty(qty);
+		}
+
+		return item;
+	}
+
+	@Override
+	public I_M_HU_Item createHUItem(final I_M_HU hu, final I_M_HU_PI_Item piItem)
+	{
+		final I_M_HU_Item item = createHUItemNoSave(hu, piItem);
+		InterfaceWrapperHelper.save(item);
+
 		// FIXME: this is making de.metas.customer.picking.service.impl.PackingServiceTest to fail
 		// IncludedHUsLocalCache
 		// .getCreate(item)
 		// .setEmptyNotStaled();
-
-		InterfaceWrapperHelper.save(item);
 
 		//
 		// Update HU Items cache

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/impl/HandlingUnitsBL.java
@@ -30,8 +30,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import org.slf4j.Logger;
-import de.metas.logging.LogManager;
 
 import org.adempiere.ad.trx.api.ITrxManager;
 import org.adempiere.exceptions.AdempiereException;
@@ -48,6 +46,7 @@ import org.compiere.model.I_M_Transaction;
 import org.compiere.model.I_M_Warehouse;
 import org.eevolution.drp.api.IDistributionNetworkDAO;
 import org.eevolution.model.I_DD_NetworkDistributionLine;
+import org.slf4j.Logger;
 
 import de.metas.handlingunits.HUIteratorListenerAdapter;
 import de.metas.handlingunits.IHUContext;
@@ -77,6 +76,7 @@ import de.metas.handlingunits.movement.api.impl.EmptiesMovementBuilder;
 import de.metas.handlingunits.storage.IHUStorage;
 import de.metas.handlingunits.storage.IHUStorageFactory;
 import de.metas.handlingunits.storage.impl.DefaultHUStorageFactory;
+import de.metas.logging.LogManager;
 
 public class HandlingUnitsBL implements IHandlingUnitsBL
 {
@@ -566,6 +566,13 @@ public class HandlingUnitsBL implements IHandlingUnitsBL
 	public String getItemType(final I_M_HU_Item huItem)
 	{
 		Check.assumeNotNull(huItem, "huItem not null");
+		final String itemType = huItem.getItemType();
+		if(itemType != null)
+		{
+			return itemType;
+		}
+		
+		// FIXME: remove it after we migrate all HUs
 		return huItem.getM_HU_PI_Item().getItemType();
 	}
 

de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/impl/HandlingUnitsDAO.java
@@ -48,6 +48,8 @@ import org.adempiere.util.Services;
 import org.adempiere.util.collections.FilterUtils;
 import org.adempiere.util.collections.IteratorUtils;
 import org.adempiere.util.collections.Predicate;
+import org.adempiere.util.lang.IPair;
+import org.adempiere.util.lang.ImmutablePair;
 import org.adempiere.util.proxy.Cached;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_M_Locator;
@@ -277,19 +279,28 @@ public class HandlingUnitsDAO implements IHandlingUnitsDAO
 	}
 
 	@Override
-	public List<I_M_HU_PackingMaterial> retrievePackingMaterials(final I_M_HU hu)
+	public List<IPair<I_M_HU_PackingMaterial, Integer>> retrievePackingMaterialAndQtys(final I_M_HU hu)
 	{
-		final List<I_M_HU_PackingMaterial> packingMaterials = new ArrayList<I_M_HU_PackingMaterial>();
+		final List<IPair<I_M_HU_PackingMaterial, Integer>> packingMaterials = new ArrayList<>();
 		final List<I_M_HU_Item> huItems = retrieveItems(hu);
 		for (final I_M_HU_Item huItem : huItems)
 		{
-			final I_M_HU_PI_Item huPIItem = huItem.getM_HU_PI_Item();
-			final String itemType = huPIItem.getItemType();
-			if (X_M_HU_PI_Item.ITEMTYPE_PackingMaterial.equals(itemType))
+			final String itemType = huItem.getItemType();
+			if (!X_M_HU_PI_Item.ITEMTYPE_PackingMaterial.equals(itemType))
 			{
-				final I_M_HU_PackingMaterial packingMaterial = huPIItem.getM_HU_PackingMaterial();
-				packingMaterials.add(packingMaterial);
+				continue;
 			}
+			
+			final I_M_HU_PackingMaterial packingMaterial = huItem.getM_HU_PackingMaterial();
+			
+			final int count = huItem.getQty().intValueExact();
+			if(count <= 0)
+			{
+				logger.warn("Invalid packing materials count for {}. Skip considering it.", huItem);
+				continue;
+			}
+			
+			packingMaterials.add(ImmutablePair.of(packingMaterial, count));
 		}
 		return packingMaterials;
 	}

commit 6e44f8b7051cc057fd0655c55791ad05f03f707f

#1248 MAJOR FIX empties movements were generated on opposite direction
cause: typo bug introduced in
https://github.com/metasfresh/metasfresh/commit/7872fee00ea28fd9ef79c37b59f0b16e46940dd7#diff-9bfa783d789e433f9e87c1ddebb66983

de.metas.handlingunits.base/src/main/java/de/metas/inoutcandidate/spi/impl/HUPackingMaterialDocumentLineCandidate.java
@@ -193,7 +193,7 @@ public final class HUPackingMaterialDocumentLineCandidate
 
 	protected void subtractQty(final int qtyToSubtract)
 	{
-		qty = qty.add(BigDecimal.valueOf(qtyToSubtract));
+		qty = qty.subtract(BigDecimal.valueOf(qtyToSubtract));
 	}
 
 	/**