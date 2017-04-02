f11e1f0b1bd7c4b622c264e7a0d351341004eb81
metas-lc <liana.cotuna@metasfresh.com>
2017-04-02 19:10:50 +0300

    #741 update sysconfig on hu costprice and trace reports
    FRESH-1067 #741 packing material product category config

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5459500_sys_gh741_update_hu_costprice_function.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5459500_sys_gh741_update_hu_costprice_function.sql
@@ -1,4 +1,4 @@
-
+﻿
 
 DROP FUNCTION IF EXISTS HU_CostPrice_Function (IN keydate timestamp with time zone, IN M_Product_ID numeric(10,0), IN M_Warehouse_ID numeric(10,0), showDetails character varying, IN ad_org_id numeric);
 
