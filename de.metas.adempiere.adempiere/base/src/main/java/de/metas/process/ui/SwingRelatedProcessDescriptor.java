12f5ab40ad043538bfbafd46354c85e4d6878725
Teo Sarca <teo.sarca@gmail.com>
2017-04-03 17:39:47 +0300

    allow registering related process per AD_Window_ID
    https://github.com/metasfresh/metasfresh-webui-api/issues/269

--- de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/ui/SwingRelatedProcessDescriptor.java
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/ui/SwingRelatedProcessDescriptor.java
@@ -65,7 +65,7 @@ public class SwingRelatedProcessDescriptor
 
 		Check.assumeNotNull(relatedProcessDescriptor, "Parameter relatedProcessDescriptor is not null");
 
-		adProcessId = relatedProcessDescriptor.getAD_Process_ID();
+		adProcessId = relatedProcessDescriptor.getProcessId();
 		adProcessSupplier = ExtendedMemorizingSupplier.of(() -> InterfaceWrapperHelper.create(Env.getCtx(), adProcessId, I_AD_Process.class, ITrx.TRXNAME_None));
 
 		this.preconditionsResolutionSupplier = ExtendedMemorizingSupplier.of(preconditionsResolutionSupplier);
