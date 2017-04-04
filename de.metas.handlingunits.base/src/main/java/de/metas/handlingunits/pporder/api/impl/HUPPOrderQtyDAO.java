b96a7a4631d69f6a186e0618bef103850528f412
Teo Sarca <teo.sarca@gmail.com>
2017-04-04 13:38:31 +0300

    add more methods to IHUPPOrderQtyDAO
    https://github.com/metasfresh/metasfresh-webui-api/issues/269

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/pporder/api/impl/HUPPOrderQtyDAO.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/pporder/api/impl/HUPPOrderQtyDAO.java
@@ -1,12 +1,17 @@
 package de.metas.handlingunits.pporder.api.impl;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.ad.trx.api.ITrx;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
+import org.adempiere.util.proxy.Cached;
 import org.compiere.util.Env;
 
+import de.metas.adempiere.util.CacheCtx;
+import de.metas.adempiere.util.CacheTrx;
 import de.metas.handlingunits.model.I_PP_Order_Qty;
 import de.metas.handlingunits.pporder.api.IHUPPOrderQtyDAO;
 
@@ -20,14 +25,14 @@ import de.metas.handlingunits.pporder.api.IHUPPOrderQtyDAO;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
@@ -35,15 +40,31 @@ import de.metas.handlingunits.pporder.api.IHUPPOrderQtyDAO;
 public class HUPPOrderQtyDAO implements IHUPPOrderQtyDAO
 {
 	@Override
+	public void save(final I_PP_Order_Qty ppOrderQty)
+	{
+		InterfaceWrapperHelper.save(ppOrderQty);
+	}
+
+	@Override
+	public void delete(final I_PP_Order_Qty ppOrderQty)
+	{
+		InterfaceWrapperHelper.delete(ppOrderQty);
+	}
+
+	@Override
 	public List<I_PP_Order_Qty> retrieveOrderQtys(final int ppOrderId)
 	{
+		return retrieveOrderQtys(Env.getCtx(), ppOrderId, ITrx.TRXNAME_ThreadInherited);
+	}
+
+	@Cached(cacheName = I_PP_Order_Qty.Table_Name + "#by#PP_Order_ID", expireMinutes = 10)
+	List<I_PP_Order_Qty> retrieveOrderQtys(@CacheCtx final Properties ctx, final int ppOrderId, @CacheTrx final String trxName)
+	{
 		return Services.get(IQueryBL.class)
-				.createQueryBuilder(I_PP_Order_Qty.class, Env.getCtx(), ITrx.TRXNAME_None)
+				.createQueryBuilder(I_PP_Order_Qty.class, ctx, trxName)
 				.addOnlyActiveRecordsFilter()
 				.addEqualsFilter(I_PP_Order_Qty.COLUMN_PP_Order_ID, ppOrderId)
 				.create()
-				.list(I_PP_Order_Qty.class);
-
+				.listImmutable(I_PP_Order_Qty.class);
 	}
-
 }
