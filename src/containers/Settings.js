d75a7076606cd93a0a5645d59a56641abe85ee02
damian.przygodzki <damian.przygodzki@jazzy.pro>
2017-04-05 14:11:16 +0200

    #587 Settings for language

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,15 +1,90 @@
 import React, { Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
 
+import {
+    getAvailableLang,
+    setUserLang
+} from '../actions/AppActions';
+
 import Container from '../components/Container';
+import RawList from '../components/widget/List/RawList';
 
 class Settings extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+            langs: [],
+            value: ''
+        }
+    }
+
+    componentDidMount = () => {
+        const {dispatch} = this.props;
+
+        dispatch(getAvailableLang()).then(response => {
+            const {values, defaultValue} = response.data;
+            let chosenLang = '';
+
+            values.map(item => {
+                const key = Object.keys(item)[0];
+                if(key === defaultValue){
+                    chosenLang = item
+                }
+            })
+
+            this.setState({
+                langs: values,
+                value: chosenLang
+            })
+        });
+    }
+
+    patch = (value) => {
+        const {dispatch} = this.props;
+
+        dispatch(setUserLang(value)).then(() => {
+            this.setState({
+                value: value
+            });
+        });
+    }
+
     render() {
+        const {langs, value} = this.state;
         return (
             <Container
-                siteName = "Settings"
+                siteName="Settings"
             >
-                <div>hello</div>
+                <div className="window-wrapper">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <div
+                                tabIndex={0}
+                                className="panel panel-spaced panel-distance panel-bordered panel-primary"
+                            >
+                                <div className="elements-line">
+                                    <div className="form-group row">
+                                        <div
+                                            className="form-control-label col-sm-3"
+                                        >
+                                            Language
+                                        </div>
+                                        <div className="col-sm-9">
+                                            <RawList
+                                                list={langs}
+                                                onSelect={option =>
+                                                    this.patch(option)
+                                                }
+                                                selected={value}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Container>
         );
     }
--- src/reducers/windowHandler.js
+++ src/reducers/windowHandler.js
@@ -251,7 +251,6 @@ export default function windowHandler(state = initialState, action) {
                 })
             })
 
-
         case types.UPDATE_DATA_INCLUDED_TABS_INFO:
             return Object.assign({}, state, {
                 [action.scope]: Object.assign({}, state[action.scope], {
