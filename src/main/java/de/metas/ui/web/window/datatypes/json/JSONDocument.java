c6d50343f3bd33461918dfba89997be8b57fdd3f
Teo Sarca <teo.sarca@gmail.com>
2017-04-06 14:25:08 +0300

    minor
    https://github.com/metasfresh/metasfresh-webui-api/issues/269

--- src/main/java/de/metas/ui/web/window/datatypes/json/JSONDocument.java
+++ src/main/java/de/metas/ui/web/window/datatypes/json/JSONDocument.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.collect.ImmutableList;
 
 import de.metas.ui.web.window.WindowConstants;
