af43fd92e28a23277ff53e623363fa93e46620e7
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-04-10 18:25:26 +0300

    #1064 Code cleaning
    https://metasfresh.atlassian.net/browse/FRESH-1589
    https://github.com/metasfresh/metasfresh/issues/1064

--- de.metas.handlingunits.client/src/main/java/de/metas/handlingunits/client/terminal/inventory/model/InventoryHUEditorModel.java
+++ de.metas.handlingunits.client/src/main/java/de/metas/handlingunits/client/terminal/inventory/model/InventoryHUEditorModel.java
@@ -1,15 +1,12 @@
 package de.metas.handlingunits.client.terminal.inventory.model;
 
 import org.adempiere.ad.trx.api.ITrx;
-import org.adempiere.ad.trx.api.ITrxManager;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Check;
-import org.adempiere.util.Services;
 import org.compiere.model.I_M_Warehouse;
 
 import de.metas.adempiere.form.terminal.context.ITerminalContext;
 import de.metas.handlingunits.client.terminal.editor.model.impl.HUEditorModel;
-import de.metas.handlingunits.movement.api.IHUMovementBL;
 
 /**
  *
@@ -18,21 +15,7 @@ import de.metas.handlingunits.movement.api.IHUMovementBL;
 public class InventoryHUEditorModel extends HUEditorModel
 {
 
-	/**
-	 * M_InOut SO
-	 */
-	private static final int WINDOW_CUSTOMER_RETURN = 53097; // FIXME: HARDCODED
-
-	/**
-	 * M_InOut PO
-	 */
-	private static final int WINDOW_RETURN_TO_VENDOR = 53098; // FIXME: HARDCODED
-
-	private final IHUMovementBL huMovementBL = Services.get(IHUMovementBL.class);
-
 	private final I_M_Warehouse _warehouse;
-
-	private final transient ITrxManager trxManager = Services.get(ITrxManager.class);
 
 	public InventoryHUEditorModel(final ITerminalContext terminalContext, final int warehouseId)
 	{
@@ -47,7 +30,4 @@ public class InventoryHUEditorModel extends HUEditorModel
 		return _warehouse;
 	}
 
-
-
-	
 }
