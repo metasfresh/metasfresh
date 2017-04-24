b66d43845fe8ee1ba68126822aee991ddd511e97
Teo Sarca <teo.sarca@gmail.com>
2017-04-24 11:39:07 +0300

    fix migration script
    https://github.com/metasfresh/metasfresh/issues/1370

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5460550_sys_gh1370_adding_request_trype_trl_en_us.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5460550_sys_gh1370_adding_request_trype_trl_en_us.sql
@@ -1,17 +1,17 @@
 -- 2017-04-23T13:47:44
 -- URL zum Konzept
-INSERT INTO R_RequestType_Trl (CreatedBy,UpdatedBy,Updated,Created,AD_Client_ID,AD_Language,AD_Org_ID,IsActive,IsTranslated,Name,R_RequestType_ID) VALUES ( 100,100,TO_TIMESTAMP('2017-04-23 13:47:43','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2017-04-23 13:47:43','YYYY-MM-DD HH24:MI:SS'),0,'en_US',0,'Y','Y','1',540007)
-;
+-- INSERT INTO R_RequestType_Trl (CreatedBy,UpdatedBy,Updated,Created,AD_Client_ID,AD_Language,AD_Org_ID,IsActive,IsTranslated,Name,R_RequestType_ID) VALUES ( 100,100,TO_TIMESTAMP('2017-04-23 13:47:43','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2017-04-23 13:47:43','YYYY-MM-DD HH24:MI:SS'),0,'en_US',0,'Y','Y','1',540007)
+-- ;
 
 -- 2017-04-23T13:47:55.250
 -- URL zum Konzept
-INSERT INTO R_RequestType_Trl (CreatedBy,UpdatedBy,Updated,Created,AD_Client_ID,AD_Language,AD_Org_ID,IsActive,IsTranslated,Name,R_RequestType_ID) VALUES ( 100,100,TO_TIMESTAMP('2017-04-23 13:47:55','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2017-04-23 13:47:55','YYYY-MM-DD HH24:MI:SS'),0,'en_US',0,'Y','Y','2',540005)
-;
+-- INSERT INTO R_RequestType_Trl (CreatedBy,UpdatedBy,Updated,Created,AD_Client_ID,AD_Language,AD_Org_ID,IsActive,IsTranslated,Name,R_RequestType_ID) VALUES ( 100,100,TO_TIMESTAMP('2017-04-23 13:47:55','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2017-04-23 13:47:55','YYYY-MM-DD HH24:MI:SS'),0,'en_US',0,'Y','Y','2',540005)
+-- ;
 
 -- 2017-04-23T13:51:57.406
 -- URL zum Konzept
-INSERT INTO R_RequestType_Trl (CreatedBy,UpdatedBy,Updated,Created,AD_Client_ID,AD_Language,AD_Org_ID,IsActive,IsTranslated,Name,R_RequestType_ID) VALUES ( 100,100,TO_TIMESTAMP('2017-04-23 13:51:57','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2017-04-23 13:51:57','YYYY-MM-DD HH24:MI:SS'),0,'en_US',0,'Y','Y','2',540005)
-;
+-- INSERT INTO R_RequestType_Trl (CreatedBy,UpdatedBy,Updated,Created,AD_Client_ID,AD_Language,AD_Org_ID,IsActive,IsTranslated,Name,R_RequestType_ID) VALUES ( 100,100,TO_TIMESTAMP('2017-04-23 13:51:57','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2017-04-23 13:51:57','YYYY-MM-DD HH24:MI:SS'),0,'en_US',0,'Y','Y','2',540005)
+-- ;
 
 -- 2017-04-23T14:00:07.523
 -- URL zum Konzept
