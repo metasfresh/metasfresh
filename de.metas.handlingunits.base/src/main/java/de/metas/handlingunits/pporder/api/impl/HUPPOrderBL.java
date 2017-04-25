9517ea5ba617a9e458cb277bdffbac3ef0e826a5
Teo Sarca <teo.sarca@gmail.com>
2017-04-25 22:40:51 +0300

    PP_Order.PlanningStatus
    https://github.com/metasfresh/metasfresh-webui-api/issues/321

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/pporder/api/impl/HUPPOrderBL.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/pporder/api/impl/HUPPOrderBL.java
@@ -1,12 +1,25 @@
 package de.metas.handlingunits.pporder.api.impl;
 
-import org.adempiere.model.InterfaceWrapperHelper;
+import java.util.List;
 
+import org.adempiere.ad.trx.api.ITrx;
+import org.adempiere.ad.trx.api.ITrxManager;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.util.Services;
+import org.compiere.util.Env;
+import org.eevolution.model.X_PP_Order;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableMultimap;
+
+import de.metas.handlingunits.IHandlingUnitsDAO;
 import de.metas.handlingunits.allocation.IAllocationSource;
 import de.metas.handlingunits.allocation.impl.GenericAllocationSourceDestination;
 import de.metas.handlingunits.impl.DocumentLUTUConfigurationManager;
 import de.metas.handlingunits.impl.IDocumentLUTUConfigurationManager;
 import de.metas.handlingunits.model.I_PP_Order;
+import de.metas.handlingunits.model.X_M_HU;
+import de.metas.handlingunits.pporder.api.HUPPOrderIssueReceiptCandidatesProcessor;
 import de.metas.handlingunits.pporder.api.IHUPPOrderBL;
 import de.metas.handlingunits.pporder.api.IHUPPOrderIssueProducer;
 
@@ -41,5 +54,77 @@ public class HUPPOrderBL implements IHUPPOrderBL
 	public IHUPPOrderIssueProducer createIssueProducer()
 	{
 		return new HUPPOrderIssueProducer();
+	}
+
+	@Override
+	public List<Integer> retrieveHUsAvailableToIssue(final int productId)
+	{
+		return Services.get(IHandlingUnitsDAO.class)
+				.createHUQueryBuilder()
+				.setContext(Env.getCtx(), ITrx.TRXNAME_ThreadInherited)
+				//
+				// TODO add warehouse filter
+				.addHUStatusToInclude(X_M_HU.HUSTATUS_Active)
+				.addOnlyWithProductId(productId)
+				.setOnlyTopLevelHUs()
+				.onlyNotLocked()
+				//
+				.createQuery()
+				.listIds();
+	}
+	
+	private static final ImmutableMultimap<String, String> fromPlanningStatus2toPlanningStatusAllowed = ImmutableMultimap.<String, String>builder()
+			.put(X_PP_Order.PLANNINGSTATUS_Planning, X_PP_Order.PLANNINGSTATUS_Review)
+			.put(X_PP_Order.PLANNINGSTATUS_Planning, X_PP_Order.PLANNINGSTATUS_Complete)
+			.put(X_PP_Order.PLANNINGSTATUS_Review, X_PP_Order.PLANNINGSTATUS_Planning)
+			.put(X_PP_Order.PLANNINGSTATUS_Review, X_PP_Order.PLANNINGSTATUS_Complete)
+			.put(X_PP_Order.PLANNINGSTATUS_Complete, X_PP_Order.PLANNINGSTATUS_Planning) // mainly for testing
+			.build();
+
+	@Override
+	public boolean canChangePlanningStatus(final String fromPlanningStatus, final String toPlanningStatus)
+	{
+		return fromPlanningStatus2toPlanningStatusAllowed.get(fromPlanningStatus).contains(toPlanningStatus);
+	}
+
+	@Override
+	public void processPlanning(String targetPlanningStatus, int ppOrderId)
+	{
+		Services.get(ITrxManager.class).assertThreadInheritedTrxExists();
+		
+		final I_PP_Order ppOrder = InterfaceWrapperHelper.load(ppOrderId, I_PP_Order.class);
+		final String planningStatus = ppOrder.getPlanningStatus();
+		if (Objects.equal(planningStatus, targetPlanningStatus))
+		{
+			throw new IllegalStateException("Already " + targetPlanningStatus);
+		}
+		if(!canChangePlanningStatus(planningStatus, targetPlanningStatus))
+		{
+			throw new IllegalStateException("Cannot chagen planning status from "+planningStatus+" to "+targetPlanningStatus);
+		}
+
+		if (X_PP_Order.PLANNINGSTATUS_Planning.equals(targetPlanningStatus))
+		{
+			// nothing
+		}
+		else if (X_PP_Order.PLANNINGSTATUS_Review.equals(targetPlanningStatus))
+		{
+			// nothing
+		}
+		else if (X_PP_Order.PLANNINGSTATUS_Complete.equals(targetPlanningStatus))
+		{
+			HUPPOrderIssueReceiptCandidatesProcessor.newInstance()
+					.setCandidatesToProcessByPPOrderId(ppOrderId)
+					.process();
+		}
+		else
+		{
+			throw new IllegalArgumentException("Unknown target planning status: " + targetPlanningStatus);
+		}
+
+		//
+		// Update ppOrder's planning status
+		ppOrder.setPlanningStatus(targetPlanningStatus);
+		InterfaceWrapperHelper.save(ppOrder);
 	}
 }
