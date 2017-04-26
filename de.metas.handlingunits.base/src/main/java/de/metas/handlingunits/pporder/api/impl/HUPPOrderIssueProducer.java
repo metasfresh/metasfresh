fc0964dad598510bec862e105636b0c04c74d924
Teo Sarca <teo.sarca@gmail.com>
2017-04-26 18:01:35 +0300

    when processing an Issue candidate, unlock the HU
    at that point, the HU is already destroyed anyways
    
    https://github.com/metasfresh/metasfresh/issues/1388

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/pporder/api/impl/HUPPOrderIssueProducer.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/pporder/api/impl/HUPPOrderIssueProducer.java
@@ -64,7 +64,7 @@ import de.metas.logging.LogManager;
  * @author tsa
  *
  */
-/* package */class HUPPOrderIssueProducer implements IHUPPOrderIssueProducer
+public class HUPPOrderIssueProducer implements IHUPPOrderIssueProducer
 {
 	// Services
 	private static final transient Logger logger = LogManager.getLogger(HUPPOrderIssueProducer.class);
@@ -78,12 +78,12 @@ import de.metas.logging.LogManager;
 	private final transient IHandlingUnitsBL handlingUnitsBL = Services.get(IHandlingUnitsBL.class);
 	private final transient IHUPPOrderQtyDAO huPPOrderQtyDAO = Services.get(IHUPPOrderQtyDAO.class);
 
-	private static final LockOwner lockOwner = LockOwner.forOwnerName("PP_Order_PreparedToIssue");
+	public static final LockOwner lockOwner = LockOwner.forOwnerName("PP_Order_PreparedToIssue");
 
 	private Date movementDate;
 	private List<I_PP_Order_BOMLine> targetOrderBOMLines;
 
-	public HUPPOrderIssueProducer()
+	HUPPOrderIssueProducer()
 	{
 		super();
 	}
@@ -111,7 +111,10 @@ import de.metas.logging.LogManager;
 		// the candidates are created and processed in one uber-transaction
 		// trxManager.assertThreadInheritedTrxNotExists();
 
+		// Lock the HUs first, to make sure nobody else is changing them
+		// The lock shall stay until the issue candidate is processed.
 		huLockBL.lockAll(hus, lockOwner);
+		
 		boolean success = false;
 		try
 		{
@@ -128,6 +131,7 @@ import de.metas.logging.LogManager;
 		}
 		finally
 		{
+			// In case of failure, unlock the HUs.
 			if (!success)
 			{
 				huLockBL.unlockAll(hus, lockOwner);
