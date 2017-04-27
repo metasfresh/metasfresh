9c846971f8c70c954e804b6f0bbd434e22a94864
Teo Sarca <teo.sarca@gmail.com>
2017-04-27 17:51:13 +0300

    WEBUI_PP_Order_Receipt QtyLU param shall not be mandatory
    https://github.com/metasfresh/metasfresh-webui-api/issues/328

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5460920_sys_gh328_WEBUI_PP_Order_Receipt_QtyLU_not_mandatory.sql
@@ -0,0 +1,5 @@
+-- 2017-04-27T17:36:26.587
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET IsMandatory='N',Updated=TO_TIMESTAMP('2017-04-27 17:36:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541175
+;
+
