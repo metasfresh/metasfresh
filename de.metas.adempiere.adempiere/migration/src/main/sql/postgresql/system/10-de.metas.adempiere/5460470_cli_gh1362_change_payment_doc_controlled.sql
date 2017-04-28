0a9fb0819ac40706c3f35b1f5f82f2b798a86ca1
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-04-28 10:51:40 +0200

    Update AD_Sequence.CurrentNext from possible pre existing C_Payments with overlapping DocumentNos
    Activate auto numbering for incoming and outgoing payments https://github.com/metasfresh/metasfresh/issues/1362

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5460470_cli_gh1362_change_payment_doc_controlled.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5460470_cli_gh1362_change_payment_doc_controlled.sql
@@ -33,3 +33,28 @@ UPDATE C_DocType SET DocNoSequence_ID=554390, IsDocNoControlled='Y',Updated=TO_T
 UPDATE C_DocType SET DocNoSequence_ID=554389, IsDocNoControlled='Y',Updated=TO_TIMESTAMP('2017-04-22 14:04:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_DocType_ID=1000009
 ;
 
+--
+-- update AD_Sequence.CurrentNext from possible pre existing C_Payments with overlapping DocumentNos
+--
+CREATE TEMP VIEW gh1363_update_ad_sequence AS
+SELECT 
+	s.AD_Sequence_ID, s.Name as AD_Sequence_Name, dt.Name as C_DocType_Name, s.CurrentNext, 
+	greatest(
+		(p.DocumentNo::integer + 1),
+		CASE 
+			WHEN dt.IsSOTrx='N' THEN 2000000 -- we bought, i.e. outgoing payment
+			ELSE 1000000 -- we sold, i.e. incoming payment
+		END
+	) as New_CurrentNext
+FROM C_Payment p 
+	JOIN C_Doctype dt ON dt.C_Doctype_ID=p.C_Doctype_ID
+		JOIN AD_Sequence s ON s.AD_Sequence_ID=dt.DocNoSequence_ID
+WHERE true 
+	AND p.DocumentNo SIMILAR TO '[0-9]+' -- only care for "pure number" DocumentNo strings
+;
+
+UPDATE AD_Sequence s
+SET CurrentNext=v.New_CurrentNext
+FROM gh1363_update_ad_sequence v
+WHERE s.AD_Sequence_ID=v.AD_Sequence_ID;
+
