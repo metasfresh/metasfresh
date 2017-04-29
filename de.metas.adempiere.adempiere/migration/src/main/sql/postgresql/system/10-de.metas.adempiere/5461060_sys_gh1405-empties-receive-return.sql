7d2406fda9d366cf8d6bde99c9cd739a3e08a589
Mark Krake <mark.krake@metasfresh.com>
2017-04-29 19:07:25 +0200

    #1405 Adjusting the empties return window to mandatory field primary layout
    Adjusting the empties return window to mandatory field primary layout
    
    FRESH-2098 https://github.com/metasfresh/metasfresh/issues/1405

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5461060_sys_gh1405-empties-receive-return.sql
@@ -0,0 +1,30 @@
+-- 2017-04-29T18:59:18.786
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,557578,0,540778,540112,543299,TO_TIMESTAMP('2017-04-29 18:59:18','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Belegart',20,0,0,TO_TIMESTAMP('2017-04-29 18:59:18','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-04-29T19:00:10.684
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,557563,0,540778,540113,543300,TO_TIMESTAMP('2017-04-29 19:00:10','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Aktiv',10,0,0,TO_TIMESTAMP('2017-04-29 19:00:10','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-04-29T19:00:17.107
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=541740
+;
+
+-- 2017-04-29T19:01:13.598
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,557581,0,540778,540113,543301,TO_TIMESTAMP('2017-04-29 19:01:13','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Beleg Nr.',40,0,0,TO_TIMESTAMP('2017-04-29 19:01:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-04-29T19:01:19.126
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET SeqNo=50,Updated=TO_TIMESTAMP('2017-04-29 19:01:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541753
+;
+
+-- 2017-04-29T19:01:43.461
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=541748
+;
+
