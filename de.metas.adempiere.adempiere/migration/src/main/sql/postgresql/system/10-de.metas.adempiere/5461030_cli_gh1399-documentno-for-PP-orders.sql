1a469f25210cc549862f1c493d89bad810cad55f
Mark Krake <mark.krake@metasfresh.com>
2017-04-29 15:42:59 +0200

    #1399 Adding No sequence to the manufacturing Order doctypes
    Adding No sequence to the manufacturing Order doctypes
    
    FRESH-2039 https://github.com/metasfresh/metasfresh/issues/1399

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5461030_cli_gh1399-documentno-for-PP-orders.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5461030_cli_gh1399-documentno-for-PP-orders.sql
@@ -26,3 +26,31 @@ WHERE C_DocType_ID=540938
 AND AD_Client_ID=1000000 AND 'metasfresh' = (select name from AD_Client where AD_Client_ID=1000000)
 ;
 
+-- 2017-04-29T15:19:46.069
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET DocNoSequence_ID=545473,Updated=TO_TIMESTAMP('2017-04-29 15:19:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 
+WHERE C_DocType_ID=1000038 
+AND AD_Client_ID=1000000 AND 'metasfresh' = (select name from AD_Client where AD_Client_ID=1000000)
+;
+
+-- 2017-04-29T15:19:55.746
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET DocNoSequence_ID=545473,Updated=TO_TIMESTAMP('2017-04-29 15:19:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 
+WHERE C_DocType_ID=540938 
+AND AD_Client_ID=1000000 AND 'metasfresh' = (select name from AD_Client where AD_Client_ID=1000000)
+;
+
+-- 2017-04-29T15:20:04.953
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET DocNoSequence_ID=545473,Updated=TO_TIMESTAMP('2017-04-29 15:20:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 
+WHERE C_DocType_ID=1000036 
+AND AD_Client_ID=1000000 AND 'metasfresh' = (select name from AD_Client where AD_Client_ID=1000000)
+;
+
+-- 2017-04-29T15:20:12.929
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET DocNoSequence_ID=545473,Updated=TO_TIMESTAMP('2017-04-29 15:20:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 
+WHERE C_DocType_ID=1000037 
+AND AD_Client_ID=1000000 AND 'metasfresh' = (select name from AD_Client where AD_Client_ID=1000000)
+;
+
