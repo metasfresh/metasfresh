c776ace9a583ea6d3ada62e98fc033038d9ff75a
Teo Sarca <teo.sarca@gmail.com>
2017-05-05 17:10:13 +0300

    IPPOrderDAO.retrievePPOrderIdByOrderLineId
    https://github.com/metasfresh/metasfresh-webui-api/issues/280

--- de.metas.adempiere.libero.libero/src/main/java/org/eevolution/api/impl/PPOrderDAO.java
+++ de.metas.adempiere.libero.libero/src/main/java/org/eevolution/api/impl/PPOrderDAO.java
@@ -13,15 +13,14 @@ package org.eevolution.api.impl;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-
 
 import java.util.List;
 import java.util.Properties;
@@ -105,5 +104,16 @@ public class PPOrderDAO implements IPPOrderDAO
 		return queryBuilder
 				.create()
 				.list(I_PP_Order.class);
+	}
+
+	@Override
+	public int retrievePPOrderIdByOrderLineId(int orderLineId)
+	{
+		return Services.get(IQueryBL.class)
+				.createQueryBuilder(I_PP_Order.class)
+				.addEqualsFilter(I_PP_Order.COLUMN_C_OrderLine_ID, orderLineId)
+				.addOnlyActiveRecordsFilter()
+				.create()
+				.firstIdOnly();
 	}
 }
