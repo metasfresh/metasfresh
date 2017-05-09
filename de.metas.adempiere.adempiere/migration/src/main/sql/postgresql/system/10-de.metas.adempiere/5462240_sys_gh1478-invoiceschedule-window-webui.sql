a5b8c6dc9e4c05b022192b29803e2a4a85723081
Mark Krake <mark.krake@metasfresh.com>
2017-05-09 21:06:03 +0200

    #1478 Adding the initial Layout for InvoiceSchedule Window in WebUI
    Adding the initial Layout for InvoiceSchedule Window in WebUI
    
    FRESH-2177 https://github.com/metasfresh/metasfresh/issues/1478

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5462240_sys_gh1478-invoiceschedule-window-webui.sql
@@ -0,0 +1,90 @@
+-- 2017-05-09T20:55:39.604
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Section (AD_Client_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,193,540179,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Y','main',10,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.636
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,540250,540179,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Y',10,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.661
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,540251,540179,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Y',20,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.691
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,UIStyle,Updated,UpdatedBy) VALUES (0,0,540250,540397,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Y','default',10,'primary',TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.731
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1199,0,193,540397,544015,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Mandant für diese Installation.','Ein Mandant ist eine Firma oder eine juristische Person. Sie können keine Daten über Mandanten hinweg verwenden. .','Y','N','Y','Y','N','Mandant',10,10,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.759
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,2058,0,193,540397,544016,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Organisatorische Einheit des Mandanten','Eine Organisation ist ein Bereich ihres Mandanten - z.B. Laden oder Abteilung. Sie können Daten über Organisationen hinweg gemeinsam verwenden.','Y','N','Y','Y','N','Sektion',20,20,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.785
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1236,0,193,540397,544017,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Alphanumeric identifier of the entity','The name of an entity (record) is used as an default search option in addition to the search key. The name is up to 60 characters in length.','Y','N','Y','Y','N','Name',30,30,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.810
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1237,0,193,540397,544018,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','Y','N','Beschreibung',40,40,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.836
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1200,0,193,540397,544019,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Der Eintrag ist im System aktiv','Es gibt zwei Möglichkeiten, einen Datensatz nicht mehr verfügbar zu machen: einer ist, ihn zu löschen; der andere, ihn zu deaktivieren. Ein deaktivierter Eintrag ist nicht mehr für eine Auswahl verfügbar, aber verfügbar für die Verwendung in Berichten. Es gibt zwei Gründe, Datensätze zu deaktivieren und nicht zu löschen: (1) Das System braucht den Datensatz für Revisionszwecke. (2) Der Datensatz wird von anderen Datensätzen referenziert. Z.B. können Sie keinen Geschäftspartner löschen, wenn es Rechnungen für diesen Geschäftspartner gibt. Sie deaktivieren den Geschäftspartner und verhindern, dass dieser Eintrag in zukünftigen Vorgängen verwendet wird.','Y','N','Y','Y','N','Aktiv',50,50,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.865
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,3217,0,193,540397,544020,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Default value','The Default Checkbox indicates if this record will be used as a default value.','Y','N','Y','Y','N','Standard',60,60,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.893
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1201,0,193,540397,544021,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Rechnungen nur erstellen, wenn Betrag die Betragsgrenze übersteigt','The Amount Limit checkbox indicates if invoices will be sent out if they are below the entered limit.   	','Y','N','Y','Y','N','Betragsgrenze',70,70,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.919
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1202,0,193,540397,544022,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Betrag','Betrag','Y','N','Y','Y','N','Betrag',80,80,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.945
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1203,0,193,540397,544023,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Wie oft Rechnungen erstellt werden','Die "Rechnungshäufigkeit" zeigt die Wiederholrate für die Rechnungserstellung an einen Geschäftspartner an.','Y','N','Y','Y','N','Rechnungshäufigkeit',90,90,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.971
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,2881,0,193,540397,544024,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Send invoices on even weeks','The Invoice on Even Weeks checkbox indicates if biweekly invoices should be sent on even week numbers.','Y','N','Y','Y','N','Invoice on even weeks',100,100,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:39.999
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1204,0,193,540397,544025,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100,'Tag zur Erezugung von rechnungen','der "Wochentag" gibt den Tag zur Erzeugung von Rechnungen an.','Y','N','Y','Y','N','Wochentag',110,110,0,TO_TIMESTAMP('2017-05-09 20:55:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:40.026
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,2883,0,193,540397,544026,TO_TIMESTAMP('2017-05-09 20:55:40','YYYY-MM-DD HH24:MI:SS'),100,'Letzter Wochentag für Lieferungen, die zu berücksichtigen sind','"Letzter Wochentag Lieferungen" gibt den Wochentag an, an dem eine Lieferung erfolgt sein muss, damit sie in der Rechnung berücksichtigt wird.','Y','N','Y','Y','N','Letzter Wochentag Lieferungen',120,120,0,TO_TIMESTAMP('2017-05-09 20:55:40','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:40.052
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1205,0,193,540397,544027,TO_TIMESTAMP('2017-05-09 20:55:40','YYYY-MM-DD HH24:MI:SS'),100,'Tag für die Rechnungserstellung','The Invoice Day indicates the day of invoice generation.  If twice monthly, the second time is 15 days after this day.','Y','N','Y','Y','N','Rechnungstag',130,130,0,TO_TIMESTAMP('2017-05-09 20:55:40','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-09T20:55:40.077
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,2882,0,193,540397,544028,TO_TIMESTAMP('2017-05-09 20:55:40','YYYY-MM-DD HH24:MI:SS'),100,'Letzter Tag zur Berücksichtigung von Lieferungen','The Invoice Day Cut Off indicates the last day for shipments to be included in the current invoice schedule.  For example, if the invoice schedule is defined for the first day of the month, the cut off day may be the 25th of the month.  An shipment on the 24th of May would be included in the invoices sent on June 1st but a shipment on the 26th would be included in the invoices sent on July 1st.','Y','N','Y','Y','N','Letzter Tag Lieferungen',140,140,0,TO_TIMESTAMP('2017-05-09 20:55:40','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
