c349e926a19aa9bd1d8b949b9272098b8197a30a
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-05-12 12:25:20 +0300

    #1502 Migration script fixed ( all tables added)
    https://metasfresh.atlassian.net/browse/FRESH-2203
    https://github.com/metasfresh/metasfresh/issues/1502
    (cherry picked from commit 32a06a4b8402ac0dba144abba802d76acaf049cd)

--- de.metas.handlingunits.base/src/main/sql/postgresql/system/5462430_cli_gh1502_DropM_HU_PI_Item_Product_For_NoHU_Fixed.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5462430_cli_gh1502_DropM_HU_PI_Item_Product_For_NoHU_Fixed.sql
@@ -1,21 +1,32 @@
 
-update c_invoiceLine set M_HU_PI_Item_Product_ID = 101 where M_HU_PI_Item_Product_ID = 100;
+UPDATE M_ProductPrice SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE C_Order SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE C_OrderLine SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_InOutLine SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_InventoryLine SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE C_InvoiceLine SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE DD_OrderLine SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_ShipmentSchedule SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE C_OLCand SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_HU_PI_Item_Product SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_HU SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_ProductPrice_Attribute SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_ReceiptSchedule SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_HU_LUTU_Configuration SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE C_Invoice_Detail SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE M_HU_Snapshot SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE PMM_PurchaseCandidate SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE PMM_QtyReport_Event SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE PMM_Product SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE PMM_WeekReport_Event SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE PMM_Week SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
+UPDATE PMM_Balance SET  M_HU_PI_Item_Product_ID = 101 WHERE M_HU_PI_Item_Product_ID = 100;
 
-update C_OlCand set M_HU_PI_Item_Product_ID = 101 where M_HU_PI_Item_Product_ID = 100;
 
-update c_orderline set M_HU_PI_Item_Product_ID = 101 where M_HU_PI_Item_Product_ID = 100;
-
-
-update c_order set M_HU_PI_Item_Product_ID = 101 where M_HU_PI_Item_Product_ID = 100;
-
-update m_receiptschedule set M_HU_PI_Item_Product_ID = 101 where M_HU_PI_Item_Product_ID = 100;
-
- 
-update m_shipmentschedule set M_HU_PI_Item_Product_ID = 101 where M_HU_PI_Item_Product_ID = 100;
-
-
-create table backup.M_HU_PI_Item_Product_BKP20171011 as select * from M_HU_PI_Item_Product;
 
 
 delete from M_HU_PI_Item_Product pip
 where exists (select * from M_HU_PI_Item i where i.M_HU_PI_Item_ID=pip.M_HU_PI_Item_ID and i.M_HU_PI_Version_ID=100);
+
+
+
