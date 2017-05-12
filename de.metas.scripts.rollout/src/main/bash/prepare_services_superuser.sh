360c130194af1ebc45d48006aa576242b724d383
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-05-12 07:42:37 +0200

    Fix prepare_services_superuser.sh
    Provide material-dispo and spring-boot-admin as services https://github.com/metasfresh/metasfresh/issues/1471

--- de.metas.scripts.rollout/src/main/bash/prepare_services_superuser.sh
+++ de.metas.scripts.rollout/src/main/bash/prepare_services_superuser.sh
@@ -8,19 +8,38 @@ set -o errexit  #don't allow any command to exit with an error code !=0
 # The "normal" minor_remote.sh script might ask the user to run this script as super user if it encounters a problem 
 #
 
+#Thanks to http://stackoverflow.com/questions/6643853/how-to-convert-in-path-names-to-absolute-name-in-a-bash-script for the readlink tip
+LOCAL_DIR=$(readlink -m $(dirname $0))
+#Note: ROLLOUT_DIR can be overridden from cmdline using -d
+ROLLOUT_DIR=$(readlink -m LOCAL_DIR/..)
+
+DEFAULT_LOCAL_SETTINGS_FILE=~/local_settings.properties
+
 prepare_service_superuser()
 {
 	local service_name=$1
+
+	local SYSTEM_DEPLOY_SOURCE_FOLDER=${ROLLOUT_DIR}/deploy/services
+	local SYSTEM_DEPLOY_TARGET_FOLDER=${METASFRESH_HOME}/${service_name}
 	
 	echo "Checking if /opt/${service_name} needs to be migrated"
 	if [[ -d /opt/${service_name} ]]; 
 	then
-		echo "!!! Copying /opt/${service_name} to $SYSTEM_DEPLOY_TARGET_FOLDER !!! "
-		stop_metasfresh-webui-api
-		cp -v /opt/${service_name} $SYSTEM_DEPLOY_TARGET_FOLDER
+
+		local INIT_D_FILE=/etc/init.d/${service_name}
+		if [[ -x "${INIT_D_FILE}" ]]; then
+			echo "Found executable file ${INIT_D_FILE}; Going to try and stop ${service_name}"
+			${INIT_D_FILE} stop
+		fi
+
+		echo "!!! Copying /opt/${service_name} to $SYSTEM_DEPLOY_TARGET_FOLDER (excluding /opt/${service_name}/log) !!! "
+		rsync -av --exclude='log' /opt/${service_name}/ ${SYSTEM_DEPLOY_TARGET_FOLDER}
 		
-		# needs to be replaced with a new version. details for the user will follow when this script proceeds" >> ${service_name}_copy_to_correct_folder.sh
-		rm /opt/metasfresh/metasfresh-webui-api/metasfresh-webui-api.conf
+		if [[ -f ${SYSTEM_DEPLOY_TARGET_FOLDER}/metasfresh-webui-api.conf ]];
+		then
+			# will be replaced with a new version.
+			mv -v ${SYSTEM_DEPLOY_TARGET_FOLDER}/metasfresh-webui-api.conf ${SYSTEM_DEPLOY_TARGET_FOLDER}/metasfresh-webui-api.conf_BKP
+		fi
 		mv /opt/${service_name} /opt/${service_name}_BKP
 		echo "!!!  Done !!!"
 	else
@@ -58,7 +77,24 @@ prepare_service_superuser()
 		echo "OK"
 	fi
 }
-	
+
+LOCAL_SETTINGS_FILE=$DEFAULT_LOCAL_SETTINGS_FILE
+
+#parse the command line args (getopts doesn't work from inside a procedure)
+while getopts "d:s:" OPTION; do
+	case "$OPTION" in
+		d)
+			echo "$OPTION = $OPTARG"
+			ROLLOUT_DIR="$OPTARG"
+		;;		
+		s)
+			echo "$OPTION = $OPTARG"
+			LOCAL_SETTINGS_FILE="$OPTARG"
+	esac
+done
+
+source $LOCAL_SETTINGS_FILE
+
 prepare_service_superuser metasfresh-admin
 prepare_service_superuser metasfresh-material-dispo
 prepare_service_superuser metasfresh-webui-api
