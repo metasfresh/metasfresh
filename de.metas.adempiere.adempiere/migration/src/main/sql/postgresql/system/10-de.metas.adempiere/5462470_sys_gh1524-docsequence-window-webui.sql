1b1e40cf358fc09eeb5415c2427a1dd0bbe3472c
Mark Krake <mark.krake@metasfresh.com>
2017-05-13 15:28:59 +0200

    #1524 Adding initial Layout definition for Document Sequence window in WebUI
    Adding the initial Layout definition for the Document Sequence window in
    WebUI
    
    FRESH-2241 https://github.com/metasfresh/metasfresh/issues/1524

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5462470_sys_gh1524-docsequence-window-webui.sql
@@ -0,0 +1,114 @@
+-- 2017-05-13T15:25:01.399
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Section (AD_Client_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,146,540183,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Y','main',10,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.445
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,540256,540183,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Y',10,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.472
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,540257,540183,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Y',20,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.502
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,UIStyle,Updated,UpdatedBy) VALUES (0,0,540256,540410,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Y','default',10,'primary',TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.540
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,323,0,146,540410,544111,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Mandant für diese Installation.','Ein Mandant ist eine Firma oder eine juristische Person. Sie können keine Daten über Mandanten hinweg verwenden. .','Y','N','Y','Y','N','Mandant',10,10,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.567
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,2019,0,146,540410,544112,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Organisatorische Einheit des Mandanten','Eine Organisation ist ein Bereich ihres Mandanten - z.B. Laden oder Abteilung. Sie können Daten über Organisationen hinweg gemeinsam verwenden.','Y','N','Y','Y','N','Sektion',20,20,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.593
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,324,0,146,540410,544113,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Alphanumeric identifier of the entity','The name of an entity (record) is used as an default search option in addition to the search key. The name is up to 60 characters in length.','Y','N','Y','Y','N','Name',30,30,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.629
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,325,0,146,540410,544114,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','Y','N','Beschreibung',40,40,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.661
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,326,0,146,540410,544115,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Der Eintrag ist im System aktiv','Es gibt zwei Möglichkeiten, einen Datensatz nicht mehr verfügbar zu machen: einer ist, ihn zu löschen; der andere, ihn zu deaktivieren. Ein deaktivierter Eintrag ist nicht mehr für eine Auswahl verfügbar, aber verfügbar für die Verwendung in Berichten. Es gibt zwei Gründe, Datensätze zu deaktivieren und nicht zu löschen: (1) Das System braucht den Datensatz für Revisionszwecke. (2) Der Datensatz wird von anderen Datensätzen referenziert. Z.B. können Sie keinen Geschäftspartner löschen, wenn es Rechnungen für diesen Geschäftspartner gibt. Sie deaktivieren den Geschäftspartner und verhindern, dass dieser Eintrag in zukünftigen Vorgängen verwendet wird.','Y','N','Y','Y','N','Aktiv',50,50,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.687
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,330,0,146,540410,544116,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Automatisch die nächste Nummer zuordnen','"Automatische Nummerierung" zeigt an, ob das System die nächste Nummer automatisch zuordnet.','Y','N','Y','Y','N','Automatische Nummerierung',60,60,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.713
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,331,0,146,540410,544117,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'The document number  will be used as the record key','The Used for Record ID checkbox indicates if the document id will be used as the key to the record','Y','N','Y','Y','N','Used for Record ID',70,70,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.739
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,635,0,146,540410,544118,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Format of the value; Can contain fixed format elements, Variables: "_lLoOaAcCa09"','<B>Validation elements:</B>
+ 	(Space) any character
+_	Space (fixed character)
+l	any Letter a..Z NO space
+L	any Letter a..Z NO space converted to upper case
+o	any Letter a..Z or space
+O	any Letter a..Z or space converted to upper case
+a	any Letters & Digits NO space
+A	any Letters & Digits NO space converted to upper case
+c	any Letters & Digits or space
+C	any Letters & Digits or space converted to upper case
+0	Digits 0..9 NO space
+9	Digits 0..9 or space
+
+Example of format "(000)_000-0000"','Y','N','Y','Y','N','Value Format',80,80,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.765
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1554,0,146,540410,544119,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'The number to increment the last document number by','The Increment indicates the number to increment the last document number by to arrive at the next sequence number','Y','N','Y','Y','N','Increment',90,90,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.791
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,329,0,146,540410,544120,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Die nächste zu verwendende Nummer','"Aktuell nächster Wert" bezeichnet die nächste für diesen Beleg zu verwendende Nummer','Y','N','Y','Y','N','Aktuell nächster Wert',100,100,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.816
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,54391,0,146,540410,544121,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Java Decimal Pattern','Option Decimal pattern in Java notation. Examples: 0000 will format 23 to 0023','Y','N','Y','Y','N','Decimal Pattern',110,110,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.842
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,335,0,146,540410,544122,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Restart the sequence with Start on every 1/1','The Restart Sequence Every Year checkbox indicates that the documents sequencing should return to the starting number on the first day of the year.','Y','N','Y','Y','N','Nummernfolge jedes Jahr neu beginnen',120,120,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.869
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,1555,0,146,540410,544123,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Starting number/position','The Start Number indicates the starting position in the line or field number in the line','Y','N','Y','Y','N','Start No',130,130,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.896
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,54357,0,146,540410,544124,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Fully qualified date column','The Date Column indicates the date to be used when calculating this measurement','Y','N','Y','Y','N','Date Column',140,140,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.924
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,332,0,146,540410,544125,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Prefix before the sequence number','The Prefix indicates the characters to print in front of the document number.','Y','N','Y','Y','N','Prefix',150,150,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-05-13T15:25:01.960
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,333,0,146,540410,544126,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100,'Suffix after the number','The Suffix indicates the characters to append to the document number.','Y','N','Y','Y','N','Suffix',160,160,0,TO_TIMESTAMP('2017-05-13 15:25:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
