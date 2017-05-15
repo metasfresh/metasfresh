dce834ae0892aebb80e1b61a9d8933eb2643d713
katarzynadunikowska <katarzyna.dunikowska@jazzy.pro>
2017-05-15 14:01:01 +0200

    Filter singleOverlayField support #742

--- src/components/app/OverlayField.js
+++ src/components/app/OverlayField.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MasterWidget from '../widget/MasterWidget';
+import RawWidget from '../widget/RawWidget';
 
 import {
     findRowByPropName
@@ -11,15 +12,17 @@ class OverlayField extends Component {
     }
 
     handleKeyDown = (e) => {
-        const {setFetchOnTrue, removeModal} = this.props;
+        const {handleSubmit, closeOverlay, clearData} = this.props;
         switch(e.key) {
             case 'Enter':
                 document.activeElement.blur();
-                setFetchOnTrue();
+                handleSubmit();
                 break;
             case 'Escape':
-                removeModal();
+                closeOverlay();
                 break;
+            case 'Delete':
+                clearData && clearData();
         }
     }
 
@@ -46,14 +49,50 @@ class OverlayField extends Component {
         })
     }
 
+    renderParameters = (layout) => {
+        const {
+            windowType, viewId, onShow, onHide, handlePatch, handleChange
+        } = this.props;
+        const parameters = layout.parameters;
+        return parameters.map((item, index) => {
+            return (
+                <RawWidget
+                    entity="documentView"
+                    subentity="filter"
+                    subentityId={layout.filterId}
+                    handlePatch={handlePatch}
+                    handleChange={handleChange}
+                    widgetType={item.widgetType}
+                    fields={[item]}
+                    windowType={windowType}
+                    type={item.type}
+                    widgetData={[item]}
+                    key={index}
+                    id={index}
+                    range={item.range}
+                    onShow={onShow}
+                    onHide={onHide}
+                    caption={item.caption}
+                    noLabel={false}
+                    filterWidget={true}
+                    viewId={viewId}
+                    autoFocus={index === 0}
+                />
+            )
+        })
+    }
+
     render() {
-        const {data, layout, type} = this.props;
+        const {data, layout, type, filter} = this.props;
+
         return (
             <div
                 className="overlay-field js-not-unselect"
                 onKeyDown={e => this.handleKeyDown(e)}
             >
             {
+                filter ?
+                this.renderParameters(layout) :
                 layout && layout.elements &&
                 this.renderElements(layout, data, type)
             }
