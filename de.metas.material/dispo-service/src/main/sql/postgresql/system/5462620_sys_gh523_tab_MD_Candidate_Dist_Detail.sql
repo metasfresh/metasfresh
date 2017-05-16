4c5bbac6b1caff1977482a8b9c29e825ea6df117
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-05-16 23:23:13 +0200

    SQL fix
    Switch Manufacturing and Distribution to Dispo Framework
    https://github.com/metasfresh/metasfresh/issues/523

--- de.metas.material/dispo-service/src/main/sql/postgresql/system/5462620_sys_gh523_tab_MD_Candidate_Dist_Detail.sql
+++ de.metas.material/dispo-service/src/main/sql/postgresql/system/5462620_sys_gh523_tab_MD_Candidate_Dist_Detail.sql
@@ -21,7 +21,7 @@ UPDATE AD_Tab SET SeqNo=20,Updated=TO_TIMESTAMP('2017-05-15 18:53:09','YYYY-MM-D
 
 -- 2017-05-15T18:53:15.248
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Tab SET EntityType='de.metas.manufacturing.dispo',Updated=TO_TIMESTAMP('2017-05-15 18:53:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540821
+UPDATE AD_Tab SET EntityType='de.metas.material.dispo',Updated=TO_TIMESTAMP('2017-05-15 18:53:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540821
 ;
 
 -- 2017-05-15T18:54:24.563
@@ -41,7 +41,7 @@ UPDATE AD_Tab SET AD_Column_ID=556799,Updated=TO_TIMESTAMP('2017-05-15 18:54:53'
 
 -- 2017-05-15T18:55:37.229
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556791,558421,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Mandant für diese Installation.',10,'de.metas.manufacturing.dispo','Ein Mandant ist eine Firma oder eine juristische Person. Sie können keine Daten über Mandanten hinweg verwenden. .','Y','Y','Y','N','N','N','N','N','Mandant',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556791,558421,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Mandant für diese Installation.',10,'de.metas.material.dispo','Ein Mandant ist eine Firma oder eine juristische Person. Sie können keine Daten über Mandanten hinweg verwenden. .','Y','Y','Y','N','N','N','N','N','Mandant',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.235
@@ -51,7 +51,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.323
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556792,558422,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Organisatorische Einheit des Mandanten',10,'de.metas.manufacturing.dispo','Eine Organisation ist ein Bereich ihres Mandanten - z.B. Laden oder Abteilung. Sie können Daten über Organisationen hinweg gemeinsam verwenden.','Y','Y','Y','N','N','N','N','N','Sektion',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556792,558422,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Organisatorische Einheit des Mandanten',10,'de.metas.material.dispo','Eine Organisation ist ein Bereich ihres Mandanten - z.B. Laden oder Abteilung. Sie können Daten über Organisationen hinweg gemeinsam verwenden.','Y','Y','Y','N','N','N','N','N','Sektion',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.324
@@ -61,7 +61,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.395
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556795,558423,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Der Eintrag ist im System aktiv',1,'de.metas.manufacturing.dispo','Es gibt zwei Möglichkeiten, einen Datensatz nicht mehr verfügbar zu machen: einer ist, ihn zu löschen; der andere, ihn zu deaktivieren. Ein deaktivierter Eintrag ist nicht mehr für eine Auswahl verfügbar, aber verfügbar für die Verwendung in Berichten. Es gibt zwei Gründe, Datensätze zu deaktivieren und nicht zu löschen: (1) Das System braucht den Datensatz für Revisionszwecke. (2) Der Datensatz wird von anderen Datensätzen referenziert. Z.B. können Sie keinen Geschäftspartner löschen, wenn es Rechnungen für diesen Geschäftspartner gibt. Sie deaktivieren den Geschäftspartner und verhindern, dass dieser Eintrag in zukünftigen Vorgängen verwendet wird.','Y','Y','Y','N','N','N','N','N','Aktiv',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556795,558423,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Der Eintrag ist im System aktiv',1,'de.metas.material.dispo','Es gibt zwei Möglichkeiten, einen Datensatz nicht mehr verfügbar zu machen: einer ist, ihn zu löschen; der andere, ihn zu deaktivieren. Ein deaktivierter Eintrag ist nicht mehr für eine Auswahl verfügbar, aber verfügbar für die Verwendung in Berichten. Es gibt zwei Gründe, Datensätze zu deaktivieren und nicht zu löschen: (1) Das System braucht den Datensatz für Revisionszwecke. (2) Der Datensatz wird von anderen Datensätzen referenziert. Z.B. können Sie keinen Geschäftspartner löschen, wenn es Rechnungen für diesen Geschäftspartner gibt. Sie deaktivieren den Geschäftspartner und verhindern, dass dieser Eintrag in zukünftigen Vorgängen verwendet wird.','Y','Y','Y','N','N','N','N','N','Aktiv',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.396
@@ -71,7 +71,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.472
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556798,558424,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','N','N','N','N','N','N','N','Dispo-Bereitstellungsdetail',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556798,558424,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','N','N','N','N','N','N','N','Dispo-Bereitstellungsdetail',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.473
@@ -81,7 +81,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.542
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556799,558425,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Dispositionskandidat',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556799,558425,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Dispositionskandidat',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.544
@@ -91,7 +91,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.613
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556800,558426,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,2,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Belegstatus',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556800,558426,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,2,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Belegstatus',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.614
@@ -101,7 +101,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.682
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556801,558427,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Product Planning',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556801,558427,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Product Planning',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.683
@@ -111,7 +111,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.757
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556802,558428,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Distribution Order',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556802,558428,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Distribution Order',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.758
@@ -121,7 +121,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.826
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556803,558429,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Distribution Order Line',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556803,558429,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Distribution Order Line',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.827
@@ -131,7 +131,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.903
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556804,558430,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Network Distribution Line',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556804,558430,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Network Distribution Line',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.904
@@ -141,7 +141,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:37.976
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556807,558431,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Methode oder Art der Warenlieferung',10,'de.metas.manufacturing.dispo','"Lieferweg" bezeichnet die Art der Warenlieferung.','Y','Y','Y','N','N','N','N','N','Lieferweg',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556807,558431,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,'Methode oder Art der Warenlieferung',10,'de.metas.material.dispo','"Lieferweg" bezeichnet die Art der Warenlieferung.','Y','Y','Y','N','N','N','N','N','Lieferweg',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:37.977
@@ -151,7 +151,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 2017-05-15T18:55:38.046
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556808,558432,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.manufacturing.dispo','Y','Y','Y','N','N','N','N','N','Produktionsstätte',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,556808,558432,0,540821,TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100,10,'de.metas.material.dispo','Y','Y','Y','N','N','N','N','N','Produktionsstätte',TO_TIMESTAMP('2017-05-15 18:55:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2017-05-15T18:55:38.047
