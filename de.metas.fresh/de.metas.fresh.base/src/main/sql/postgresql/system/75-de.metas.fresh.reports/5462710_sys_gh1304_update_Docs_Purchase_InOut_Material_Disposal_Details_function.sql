a8e5b84b04e951241f034ac36ce34d26c010de68
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2017-05-17 09:33:54 +0200

    Update 5462710_sys_gh1304_update_Docs_Purchase_InOut_Material_Disposal_Details_function.sql
    metasfresh/metasfresh#1304

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5462710_sys_gh1304_update_Docs_Purchase_InOut_Material_Disposal_Details_function.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5462710_sys_gh1304_update_Docs_Purchase_InOut_Material_Disposal_Details_function.sql
@@ -1,4 +1,5 @@
-﻿--DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Purchase_InOut_Material_Disposal_Details ( IN Record_ID numeric, IN AD_Language Character Varying (6) );
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Purchase_InOut_Material_Disposal_Details ( IN Record_ID numeric, IN AD_Language Character Varying (6) );
+
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Purchase_InOut_Material_Disposal_Details ( IN Record_ID numeric, IN AD_Language Character Varying (6))
 RETURNS TABLE 
 (
