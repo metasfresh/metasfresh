b7df6bd8214a4b2fdb85d0f3d5bc9923a1b487c3
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-05-18 15:38:39 +0300

    #1389 Separate ddl and dml in migration scripts
    https://metasfresh.atlassian.net/browse/FRESH-2074
    https://github.com/metasfresh/metasfresh/issues/1389

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5462840_sys_gh1389_Increase_Mail_Field_In_AD_User.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5462840_sys_gh1389_Increase_Mail_Field_In_AD_User.sql
@@ -3,14 +3,6 @@
 UPDATE AD_Column SET FieldLength=200,Updated=TO_TIMESTAMP('2017-05-17 11:53:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=5396
 ;
 
-commit;
--- 2017-05-17T11:53:08.090
--- URL zum Konzept
-INSERT INTO t_alter_column values('ad_user','EMail','VARCHAR(200)',null,null)
-;
-
-commit;
-
 -- 2017-05-17T11:54:24.850
 -- URL zum Konzept
 UPDATE AD_Field SET ColumnDisplayLength=200, DisplayLength=200,Updated=TO_TIMESTAMP('2017-05-17 11:54:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=7017
