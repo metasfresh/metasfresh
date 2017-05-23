78ed4c6239d3ef161564ad920fdeb37d42f0fc31
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-05-23 10:48:50 +0300

    #1617 Fix file

--- de.metas.payment.esr/src/main/sql/postgresql/system/5455810_sys_gh1611_sys_AddMessage.sql
+++ de.metas.payment.esr/src/main/sql/postgresql/system/5455810_sys_gh1611_sys_AddMessage.sql
@@ -1,6 +1,6 @@
 -- 13.02.2017 16:51:55
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,544264,0,TO_TIMESTAMP('2017-02-13 16:51:55','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.mo73','Y','The file must be a v11 file!','I',TO_TIMESTAMP('2017-02-13 16:51:55','YYYY-MM-DD HH24:MI:SS'),100,'ESR_Import_LoadFromFile.CheckV11File')
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,544264,0,TO_TIMESTAMP('2017-02-13 16:51:55','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment.esr','Y','The file must be a v11 file!','I',TO_TIMESTAMP('2017-02-13 16:51:55','YYYY-MM-DD HH24:MI:SS'),100,'ESR_Import_LoadFromFile.CheckV11File')
 ;
 
 -- 13.02.2017 16:51:55
