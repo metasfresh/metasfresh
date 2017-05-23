bd22d0fccb8481ef079b77c203d1ec7f4ac73e9f
Teo Sarca <teo.sarca@gmail.com>
2017-05-23 09:53:07 +0300

    minor cleanup
    https://github.com/metasfresh/metasfresh-webui-api/issues/396

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/HUInOutBL.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/HUInOutBL.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.bpartner.service.IBPartnerDAO;
@@ -269,7 +268,7 @@ public class HUInOutBL implements IHUInOutBL
 
 		// services
 		final IHUAssignmentDAO huAssignmentDAO = Services.get(IHUAssignmentDAO.class);
-		final Map<Integer, List<I_M_HU_Assignment>> partnerstoHUAssignments = new HashMap<>();
+		final Map<Integer, List<I_M_HU_Assignment>> huAssignmentsByPartnerId = new HashMap<>();
 
 		// inoutline table id
 		final int inOutLineTableId = InterfaceWrapperHelper.getTableId(I_M_InOutLine.class);
@@ -296,28 +295,15 @@ public class HUInOutBL implements IHUInOutBL
 				final I_M_InOutLine inOutLine = InterfaceWrapperHelper.create(ctxAware.getCtx(), assignment.getRecord_ID(), I_M_InOutLine.class, ITrx.TRXNAME_None);
 
 				final org.compiere.model.I_M_InOut inOut = inOutLine.getM_InOut();
-
-				final int bpartnerID = inOut.getC_BPartner_ID();
-
-				List<I_M_HU_Assignment> huAssignmentsForPartner = partnerstoHUAssignments.get(bpartnerID);
-
-				if (huAssignmentsForPartner == null)
-				{
-					huAssignmentsForPartner = new ArrayList<I_M_HU_Assignment>();
-					partnerstoHUAssignments.put(bpartnerID, huAssignmentsForPartner);
-				}
-
+				final List<I_M_HU_Assignment> huAssignmentsForPartner = huAssignmentsByPartnerId.computeIfAbsent(inOut.getC_BPartner_ID(), bpartnerId -> new ArrayList<>());
 				huAssignmentsForPartner.add(assignment);
 			}
 		}
 
 		// there will be as many return inouts as there are partners
-
-		Set<Integer> keySet = partnerstoHUAssignments.keySet();
-
-		for (final int partnerId : keySet)
+		for (final int partnerId : huAssignmentsByPartnerId.keySet())
 		{
-			final I_M_InOut returnInOut = createVendorReturnInOutForPartnerAndHUs(ctx, partnerId, partnerstoHUAssignments.get(partnerId), warehouse, movementDate);
+			final I_M_InOut returnInOut = createVendorReturnInOutForPartnerAndHUs(ctx, partnerId, huAssignmentsByPartnerId.get(partnerId), warehouse, movementDate);
 
 			de.metas.handlingunits.model.I_M_InOut huInOut = InterfaceWrapperHelper.create(returnInOut, de.metas.handlingunits.model.I_M_InOut.class);
 
