33f8afc3207ac05849d6310916ee4cb58726bb7e
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-05-23 19:13:23 +0300

    #2619 By default should be 'Y'
    #1619 Do not process reverse booking lines when import an ESR file

--- de.metas.payment.esr/src/main/sql/postgresql/system/5455820_sys_gh1619_AddSysConfig.sql
+++ de.metas.payment.esr/src/main/sql/postgresql/system/5455820_sys_gh1619_AddSysConfig.sql
@@ -2,9 +2,3 @@
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541147,'S',TO_TIMESTAMP('2017-05-23 18:41:47','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment.esr','Y','de.metas.payment.esr.MatchOrg',TO_TIMESTAMP('2017-05-23 18:41:47','YYYY-MM-DD HH24:MI:SS'),100,'Y')
 ;
-
--- 23.05.2017 18:41:53
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_SysConfig SET Value='N',Updated=TO_TIMESTAMP('2017-05-23 18:41:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541147
-;
-
