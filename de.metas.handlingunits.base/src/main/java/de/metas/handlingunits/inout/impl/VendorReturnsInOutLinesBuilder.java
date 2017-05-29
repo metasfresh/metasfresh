70f2fe869ecc8b2bd0626fb9b1f131df5bbb6db6
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-05-29 18:17:52 +0300

    #1062  make sure Shipped HUs are no londer displayed after vendor return
    https://github.com/metasfresh/metasfresh/issues/1062
    https://metasfresh.atlassian.net/browse/FRESH-1587

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/VendorReturnsInOutLinesBuilder.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/VendorReturnsInOutLinesBuilder.java
@@ -42,7 +42,6 @@ public class VendorReturnsInOutLinesBuilder extends AbstractQualityReturnsInOutL
 	// services
 	private final transient IHandlingUnitsBL handlingUnitsBL = Services.get(IHandlingUnitsBL.class);
 
-
 	public VendorReturnsInOutLinesBuilder(IReference<I_M_InOut> inoutRef)
 	{
 		super(inoutRef);
@@ -59,6 +58,8 @@ public class VendorReturnsInOutLinesBuilder extends AbstractQualityReturnsInOutL
 	protected void setHUStatus(IHUContext huContext, I_M_HU hu)
 	{
 		handlingUnitsBL.setHUStatus(huContext, hu, X_M_HU.HUSTATUS_Shipped);
+		// this handling unit must be deactivated so it is no longer considered as part of our system and no longer displayed in out POS windows.
+		hu.setIsActive(false);
 	}
 
 }
