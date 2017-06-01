657855309ef58915215426f92bc9b253f7c134d1
damian.przygodzki <damian.przygodzki@jazzy.pro>
2017-06-01 10:36:22 +0200

    #809 Avatar menu keyboard nav

--- src/components/header/UserDropdown.js
+++ src/components/header/UserDropdown.js
@@ -8,8 +8,44 @@ class UserDropdown extends Component {
     constructor(props) {
         super(props);
     }
-    
+
     handleClickOutside = () => this.props.handleUDOpen(false);
+    
+    handleKeyDown = (e) => {
+        switch(e.key){
+            case 'ArrowDown': {
+                e.preventDefault();
+                const activeElem = document.activeElement;
+                if(activeElem.nextSibling) {
+                    activeElem.nextSibling.focus();
+                }
+                break;
+            }
+            case 'ArrowUp': {
+                e.preventDefault();
+                const activeElem = document.activeElement;
+                // When focus pulled out once, do not allow to get there
+                if(
+                    activeElem.previousSibling.classList
+                        .contains('js-selection-placeholder')
+                ){
+                    return;
+                }
+                if(activeElem.previousSibling) {
+                    activeElem.previousSibling.focus();
+                }
+                break;
+            }
+            case 'Enter':
+                e.preventDefault();
+                document.activeElement.click();
+                break;
+            case 'Escape':
+                e.preventDefault();
+                this.handleClickOutside();
+                break;
+        }
+    }
 
     render() {
         const {
@@ -32,27 +68,42 @@ class UserDropdown extends Component {
                     className="header-item avatar img-fluid rounded-circle"
                 />
                 
-                {open && <div className="user-dropdown-list">
+                {open && 
                     <div
-                        className="user-dropdown-item user-dropdown-header-item meta-text-primary"
+                        className="user-dropdown-list"
+                        onKeyDown={this.handleKeyDown}
                     >
-                        Marcus Gronholm
+                        <div
+                            className="user-dropdown-item user-dropdown-header-item meta-text-primary"
+                        >
+                            Marcus Gronholm
+                        </div>
+                        <hr className="context-menu-separator" />
+                        // Placeholder, empty place, to keep focus when it is
+                        // not needed (e.g when mouse is in use)
+                        // It is always returning back there due to ref action
+                        <div
+                            ref={c => c && c.focus()}
+                            tabIndex={0}
+                            className="js-selection-placeholder"
+                        />
+                        <div
+                            className="user-dropdown-item"
+                            onClick={() => redirect('/settings')}
+                            tabIndex={0}
+                        >
+                            <i className="meta-icon-settings" /> Settings
+                        </div>
+                        <div
+                            className="user-dropdown-item"
+                            onClick={() => redirect('/logout')}
+                            tabIndex={0}
+                        >
+                            <i className="meta-icon-logout" /> Log out
+                        </div>
                     </div>
-                    <hr className="context-menu-separator" />
-                    <div
-                        className="user-dropdown-item"
-                        onClick={() => redirect('/settings')}
-                    >
-                        <i className="meta-icon-settings" /> Settings
-                    </div>
-                    <div
-                        className="user-dropdown-item"
-                        onClick={() => redirect('/logout')}
-                    >
-                        <i className="meta-icon-logout" /> Log out
-                    </div>
-                </div>}
-                {tooltipOpen === shortcut && <Tooltips
+                }
+                {tooltipOpen === shortcut && !open && <Tooltips
                     name={shortcut}
                     action={'User menu'}
                     type={''}
