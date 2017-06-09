ba41d6f1d35ad618099dddf8fa45399e198144a4
metasnw <nw@metasfresh.com>
2017-06-09 16:23:04 +0200

    #1742  Translation for en_US in WebUI - Document Outbound
    add_outbound_documents_browse_name_en_US
    https://github.com/metasfresh/metasfresh/issues/1742

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5464690_sys_gh1742_add_outbound_documents_browse_name_en_US_.sql
@@ -0,0 +1,20 @@
+-- 2017-06-09T16:19:25.394
+-- URL zum Konzept
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-09 16:19:25','YYYY-MM-DD HH24:MI:SS'),WEBUI_NameBrowse='Outbound Documents' WHERE AD_Menu_ID=540815 AND AD_Language='en_US'
+;
+
+-- 2017-06-09T16:19:39.861
+-- URL zum Konzept
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-09 16:19:39','YYYY-MM-DD HH24:MI:SS'),WEBUI_NameBrowse='Outbound Documents' WHERE AD_Menu_ID=540433 AND AD_Language='en_US'
+;
+
+-- 2017-06-09T16:20:01.745
+-- URL zum Konzept
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-09 16:20:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Outbound Documents Config',WEBUI_NameBrowse='Outbound Documents Config' WHERE AD_Menu_ID=540816 AND AD_Language='en_US'
+;
+
+-- 2017-06-09T16:20:12.805
+-- URL zum Konzept
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-09 16:20:12','YYYY-MM-DD HH24:MI:SS'),WEBUI_NameBrowse='Outbound Documents Config' WHERE AD_Menu_ID=540436 AND AD_Language='en_US'
+;
+
