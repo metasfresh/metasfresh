fcdc12039f4f32e0cd5b35ebea085e65f8662e20
damian.przygodzki <damian.przygodzki@jazzy.pro>
2017-06-13 07:23:47 +0200

    #865 Link widget fix

--- src/components/widget/Link.js
+++ src/components/widget/Link.js
@@ -3,16 +3,10 @@ import React, { Component } from 'react';
 class Link extends Component {
     constructor(props) {
         super(props);
-        
-        this.state = {
-            clickable: false
-        }
     }
     
-    handleClick = () => {
-        this.setState(prev => ({
-            clickable: !prev.clickable
-        }))
+    handleClick = (url) => {
+        window.open(url, '_blank');
     }
 
     render() {
@@ -20,7 +14,7 @@ class Link extends Component {
             getClassnames, isEdited, widgetProperties, icon, fullScreen, 
             tabIndex, widgetData
         } = this.props;
-        const {clickable} = this.state;
+
         return (
             <div className="input-inner-container">
                 <div 
@@ -29,29 +23,19 @@ class Link extends Component {
                         (isEdited ? 'input-focused ' : '')
                     }
                 >
-                    {clickable ?
-                        <a
-                            className="widget-link"
-                            tabIndex={fullScreen ? -1 : tabIndex}
-                            href={widgetData[0].value}
-                            target="_blank"
-                        >
-                            {widgetData[0].value}
-                        </a>
-                        : <input
-                            {...widgetProperties}
-                            type="text"
-                        />
-                    }
+                    <input
+                        {...widgetProperties}
+                        type="text"
+                    />
                     {icon &&
                         <i className="meta-icon-edit input-icon-right"/>
                     }
                 </div>
                 <div
-                    onClick={this.handleClick}
+                    onClick={() => this.handleClick(widgetData[0].value)}
                     className="btn btn-icon btn-meta-outline-secondary btn-inline pointer btn-distance-rev btn-sm"
                 >
-                    <i className={'meta-icon-' + (clickable ? 'edit' : 'show')} />
+                    <i className="meta-icon-show" />
                 </div>
             </div>
         );
