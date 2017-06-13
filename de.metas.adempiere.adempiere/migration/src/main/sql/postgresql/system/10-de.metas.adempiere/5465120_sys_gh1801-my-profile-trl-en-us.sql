33e60b2b9fb49add388af16116ca18673b0ed6ee
Mark Krake <mark.krake@metasfresh.com>
2017-06-13 13:35:08 +0200

    #1801 Adding missing translations to my Profile Window in en_US
    Adding missing translations to my Profile Window in en_US
    
    FERSH-2584 https://github.com/metasfresh/metasfresh/issues/1801

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5465120_sys_gh1801-my-profile-trl-en-us.sql
@@ -0,0 +1,60 @@
+-- 2017-06-13T13:24:55.196
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:24:55','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Language',Description='Language',Help='Defines the Language' WHERE AD_Field_ID=558691 AND AD_Language='en_US'
+;
+
+-- 2017-06-13T13:25:37.982
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:25:37','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Firstname',PrintName='Firstname',Description='Firstname' WHERE AD_Element_ID=540398 AND AD_Language='en_US'
+;
+
+-- 2017-06-13T13:25:38.014
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(540398,'en_US') 
+;
+
+-- 2017-06-13T13:26:03.997
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:26:03','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Firstname',PrintName='Firstname',Description='Firstname' WHERE AD_Element_ID=540398 AND AD_Language='en_GB'
+;
+
+-- 2017-06-13T13:26:04.004
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(540398,'en_GB') 
+;
+
+-- 2017-06-13T13:26:28.902
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:26:28','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Lastname',PrintName='Lastname',Description='Lastname' WHERE AD_Element_ID=540399 AND AD_Language='en_GB'
+;
+
+-- 2017-06-13T13:26:28.913
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(540399,'en_GB') 
+;
+
+-- 2017-06-13T13:26:40.894
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:26:40','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Lastname',PrintName='Lastname',Description='Lastname' WHERE AD_Element_ID=540399 AND AD_Language='en_US'
+;
+
+-- 2017-06-13T13:26:40.905
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(540399,'en_US') 
+;
+
+-- 2017-06-13T13:27:56.504
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:27:56','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Window_ID=53100 AND AD_Language='en_US'
+;
+
+-- 2017-06-13T13:28:02.994
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:28:02','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Window_ID=53100 AND AD_Language='en_GB'
+;
+
+-- 2017-06-13T13:29:09.071
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-06-13 13:29:09','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='My Profile',Description='My User Profile',WEBUI_NameBrowse='My Profile' WHERE AD_Menu_ID=540849 AND AD_Language='en_US'
+;
+
