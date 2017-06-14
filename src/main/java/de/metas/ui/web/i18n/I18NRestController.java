a984276575b85368526fc4572e0affa7a9f4afad
Teo Sarca <teo.sarca@gmail.com>
2017-06-14 15:17:50 +0300

    /rest/api/messages shall return a hierarchical map
    https://github.com/metasfresh/metasfresh-webui-api/issues/443

--- src/main/java/de/metas/ui/web/i18n/I18NRestController.java
+++ src/main/java/de/metas/ui/web/i18n/I18NRestController.java
@@ -1,5 +1,7 @@
 package de.metas.ui.web.i18n;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.adempiere.ad.dao.IQueryBL;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.base.Splitter;
 
 import de.metas.ui.web.config.WebConfig;
 import de.metas.ui.web.session.UserSession;
@@ -56,9 +60,48 @@ public class I18NRestController
 	private UserSession userSession;
 
 	@GetMapping("/messages")
-	public Map<String, String> getMessages()
+	public Map<String, Object> getMessages()
 	{
-		return Services.get(IMsgBL.class).getMsgMap(userSession.getAD_Language(), AD_MESSAGE_PREFIX, true /* removePrefix */);
+		final LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+
+		Services.get(IMsgBL.class)
+				.getMsgMap(userSession.getAD_Language(), AD_MESSAGE_PREFIX, true /* removePrefix */)
+				.forEach((key, value) -> addMessageToMap(map, key, value));
+
+		return map;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static final void addMessageToMap(final Map<String, Object> map, final String key, final String value)
+	{
+		final List<String> keyParts = Splitter.on('.').splitToList(key);
+
+		Map<String, Object> currentNode = map;
+		for (int i = 0; i < keyParts.size() - 1; i++)
+		{
+			final String keyPart = keyParts.get(i);
+			final Object currentNodeObj = currentNode.get(keyPart);
+			if (currentNodeObj == null)
+			{
+				final Map<String, Object> parentNode = currentNode;
+				currentNode = new LinkedHashMap<>();
+				parentNode.put(keyPart, currentNode);
+			}
+			else if (currentNodeObj instanceof Map)
+			{
+				currentNode = (Map<String, Object>)currentNodeObj;
+			}
+			else
+			{
+				// discarding the old value, shall not happen
+				final Map<String, Object> parentNode = currentNode;
+				currentNode = new LinkedHashMap<>();
+				parentNode.put(keyPart, currentNode);
+			}
+		}
+
+		final String keyPart = keyParts.get(keyParts.size() - 1);
+		currentNode.put(keyPart, value);
 	}
 
 	@PostMapping("/messages")
