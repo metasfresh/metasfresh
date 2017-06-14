f17342fcd6ed33db2eb6bff98dc91556ecea3cf4
Mark Krake <mark.krake@metasfresh.com>
2017-06-14 12:30:19 +0200

    #1810 Add the responsible Field To Request Window
    Add the responsible Field To Request Window
    
    FRESH-2605 https://github.com/metasfresh/metasfresh/issues/1810

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5465150_sys_gh1810-responsible-request-field-webui.sql
@@ -0,0 +1,10 @@
+-- 2017-06-14T12:24:32.576
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,5169,0,402,540031,545736,TO_TIMESTAMP('2017-06-14 12:24:32','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Verantwortlich',40,0,0,TO_TIMESTAMP('2017-06-14 12:24:32','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-06-14T12:25:24.078
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=540909
+;
+
