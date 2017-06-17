674d6ddb22963fe0243bcb7f2fa4d32d19d44932
Teo Sarca <teo.sarca@gmail.com>
2017-06-17 11:18:07 +0300

    improve /i18n/messages API
    https://github.com/metasfresh/metasfresh/issues/1840

--- src/main/java/de/metas/ui/web/i18n/I18NRestController.java
+++ src/main/java/de/metas/ui/web/i18n/I18NRestController.java
@@ -3,26 +3,32 @@ package de.metas.ui.web.i18n;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.UnaryOperator;
 
-import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.service.IADMessageDAO;
 import org.adempiere.ad.trx.api.ITrxManager;
 import org.adempiere.exceptions.AdempiereException;
-import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
 import org.adempiere.util.api.IMsgBL;
-import org.compiere.model.I_AD_Message;
 import org.compiere.model.X_AD_Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.base.Splitter;
 
+import de.metas.i18n.ILanguageBL;
+import de.metas.i18n.Language;
+import de.metas.i18n.po.POTrlRepository;
+import de.metas.printing.esb.base.util.Check;
 import de.metas.ui.web.config.WebConfig;
 import de.metas.ui.web.session.UserSession;
+import lombok.NonNull;
 
 /*
  * #%L
@@ -60,97 +66,161 @@ public class I18NRestController
 	private UserSession userSession;
 
 	@GetMapping("/messages")
-	public Map<String, Object> getMessages()
+	public Map<String, Object> getMessages(@RequestParam(name = "filter", required = false) final String filterString,
+			@RequestParam(name = "lang", required = false) final String adLanguageParam)
 	{
-		final LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+		final String adLanguage = Check.isEmpty(adLanguageParam, true) ? userSession.getAD_Language() : adLanguageParam;
 
-		Services.get(IMsgBL.class)
-				.getMsgMap(userSession.getAD_Language(), AD_MESSAGE_PREFIX, true /* removePrefix */)
-				.forEach((key, value) -> addMessageToMap(map, key, value));
-
-		return map;
-	}
-
-	@SuppressWarnings("unchecked")
-	private static final void addMessageToMap(final Map<String, Object> map, final String key, final String value)
-	{
-		final List<String> keyParts = Splitter.on('.').splitToList(key);
-
-		Map<String, Object> currentNode = map;
-		for (int i = 0; i < keyParts.size() - 1; i++)
+		final ADMessageFilter filter;
+		if (Check.isEmpty(filterString, true))
 		{
-			final String keyPart = keyParts.get(i);
-			final Object currentNodeObj = currentNode.get(keyPart);
-			if (currentNodeObj == null)
-			{
-				final Map<String, Object> parentNode = currentNode;
-				currentNode = new LinkedHashMap<>();
-				parentNode.put(keyPart, currentNode);
-			}
-			else if (currentNodeObj instanceof Map)
-			{
-				currentNode = (Map<String, Object>)currentNodeObj;
-			}
-			else
-			{
-				// discarding the old value, shall not happen
-				final Map<String, Object> parentNode = currentNode;
-				currentNode = new LinkedHashMap<>();
-				parentNode.put(keyPart, currentNode);
-			}
+			filter = null;
+		}
+		else
+		{
+			final String filterStringUC = filterString.trim().toUpperCase();
+			filter = adMessageKey -> adMessageKey.trim().toUpperCase().startsWith(filterStringUC);
 		}
 
-		final String keyPart = keyParts.get(keyParts.size() - 1);
-		currentNode.put(keyPart, value);
+		return MessagesTreeLoader.newInstance()
+				.filter(filter)
+				.load(adLanguage);
 	}
 
 	@PostMapping("/messages")
-	public void updateMessages(@RequestBody final Map<String, String> messagesToUpload)
+	public Map<String, Object> updateMessages(@RequestBody final Map<String, String> flatMessagesToUpload)
 	{
 		userSession.assertLoggedInAsSysAdmin();
 
-		if (messagesToUpload.isEmpty())
+		if (flatMessagesToUpload.isEmpty())
 		{
 			throw new AdempiereException("Nothing to upload");
 		}
 
 		Services.get(ITrxManager.class)
 				.run(() -> {
-					messagesToUpload.entrySet().forEach(this::importMessage);
+					flatMessagesToUpload.entrySet().forEach(this::importMessage);
 				});
 
 		Services.get(IMsgBL.class).cacheReset();
+
+		//
+
+		return MessagesTreeLoader.newInstance()
+				.filter(flatMessagesToUpload::containsKey)
+				.loadAllLanguages();
 	}
 
 	private final void importMessage(final Map.Entry<String, String> entry)
 	{
 		final String adMessageKey = AD_MESSAGE_PREFIX + entry.getKey();
-		try
-		{
-			final String msgText = entry.getValue();
+		final String msgText = entry.getValue();
 
-			I_AD_Message adMessage = Services.get(IQueryBL.class)
-					.createQueryBuilder(I_AD_Message.class)
-					.addEqualsFilter(I_AD_Message.COLUMNNAME_Value, adMessageKey)
-					.create()
-					.firstOnly(I_AD_Message.class);
-
-			if (adMessage == null)
-			{
-				adMessage = InterfaceWrapperHelper.newInstance(I_AD_Message.class);
-				adMessage.setValue(adMessageKey);
-				adMessage.setMsgType(X_AD_Message.MSGTYPE_Information);
-			}
-
+		Services.get(IADMessageDAO.class).createUpdateMessage(adMessageKey, adMessage -> {
+			adMessage.setMsgType(X_AD_Message.MSGTYPE_Information);
 			adMessage.setMsgText(msgText);
 			adMessage.setIsActive(true);
 			adMessage.setEntityType(ENTITY_TYPE);
-			InterfaceWrapperHelper.save(adMessage);
-		}
-		catch (final Exception ex)
+			
+			POTrlRepository.instance.setTrlUpdateModeAsUpdateIdenticalTrls(adMessage, true);
+		});
+	}
+
+	private static interface ADMessageFilter
+	{
+		/**
+		 * @param adMessageKey AD_Message before any keyMapper was applied
+		 * @return true if accepted
+		 */
+		boolean acceptMessageKey(String adMessageKey);
+	};
+
+	private static final class MessagesTreeLoader
+	{
+
+		public static final MessagesTreeLoader newInstance()
 		{
-			throw AdempiereException.wrapIfNeeded(ex)
-					.setParameter("AD_Message", adMessageKey);
+			return new MessagesTreeLoader();
+		}
+
+		private ADMessageFilter filter;
+
+		private MessagesTreeLoader()
+		{
+		}
+
+		public Map<String, Object> loadAllLanguages()
+		{
+			final Set<String> adLanguages = Services.get(ILanguageBL.class).getAvailableLanguages().getAD_Languages();
+
+			final LinkedHashMap<String, Object> tree = new LinkedHashMap<>();
+			tree.put("_baseLanguage", Language.getBaseAD_Language());
+
+			for (final String adLanguage : adLanguages)
+			{
+				loadInto(tree, adLanguage, adMessageKey -> adMessageKey + "." + adLanguage);
+			}
+
+			return tree;
+		}
+
+		public Map<String, Object> load(@NonNull final String adLanguage)
+		{
+			final LinkedHashMap<String, Object> tree = new LinkedHashMap<>();
+			tree.put("_language", adLanguage);
+			loadInto(tree, adLanguage, adMessageKey -> adMessageKey);
+			return tree;
+		}
+
+		private void loadInto(Map<String, Object> tree, @NonNull final String adLanguage, final UnaryOperator<String> keyMapper)
+		{
+			final IMsgBL msgBL = Services.get(IMsgBL.class);
+			msgBL.getMsgMap(adLanguage, AD_MESSAGE_PREFIX, true /* removePrefix */)
+					.forEach((adMessageKey, msgText) -> {
+						if (filter == null || filter.acceptMessageKey(adMessageKey))
+						{
+							addMessageToTree(tree, keyMapper.apply(adMessageKey), msgText);
+						}
+					});
+		}
+
+		@SuppressWarnings("unchecked")
+		private static final void addMessageToTree(final Map<String, Object> tree, final String key, final String value)
+		{
+			final List<String> keyParts = Splitter.on('.').splitToList(key);
+
+			Map<String, Object> currentNode = tree;
+			for (int i = 0; i < keyParts.size() - 1; i++)
+			{
+				final String keyPart = keyParts.get(i);
+				final Object currentNodeObj = currentNode.get(keyPart);
+				if (currentNodeObj == null)
+				{
+					final Map<String, Object> parentNode = currentNode;
+					currentNode = new LinkedHashMap<>();
+					parentNode.put(keyPart, currentNode);
+				}
+				else if (currentNodeObj instanceof Map)
+				{
+					currentNode = (Map<String, Object>)currentNodeObj;
+				}
+				else
+				{
+					// discarding the old value, shall not happen
+					final Map<String, Object> parentNode = currentNode;
+					currentNode = new LinkedHashMap<>();
+					parentNode.put(keyPart, currentNode);
+				}
+			}
+
+			final String keyPart = keyParts.get(keyParts.size() - 1);
+			currentNode.put(keyPart, value);
+		}
+
+		public MessagesTreeLoader filter(ADMessageFilter filter)
+		{
+			this.filter = filter;
+			return this;
 		}
 	}
 }
