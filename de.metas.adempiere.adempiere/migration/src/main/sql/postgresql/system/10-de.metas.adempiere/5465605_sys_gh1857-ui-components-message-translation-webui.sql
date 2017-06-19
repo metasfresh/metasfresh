572f8d1c33f3a10313c31f654b700a3a62dbc1c5
Mark Krake <mark.krake@metasfresh.com>
2017-06-19 21:25:59 +0200

    #1857 Adding the ad_messages for different static webui elements
    Adding the ad_messages in de_DE for different static webui elements
    
    FRESH-2666 https://github.com/metasfresh/metasfresh/issues/1857
    
    (cherry picked from commit d6c0b0d16518561447738227f01ec5dfab4285bb)

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5465605_sys_gh1857-ui-components-message-translation-webui.sql
@@ -0,0 +1,285 @@
+-- 2017-06-19T18:16:11.637
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Passwort',Updated=TO_TIMESTAMP('2017-06-19 18:16:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544381
+;
+
+-- 2017-06-19T18:16:20.484
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Rolle auswählen',Updated=TO_TIMESTAMP('2017-06-19 18:16:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544382
+;
+
+-- 2017-06-19T18:16:23.852
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Senden',Updated=TO_TIMESTAMP('2017-06-19 18:16:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544383
+;
+
+-- 2017-06-19T18:16:33.882
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Aktions Menü',Updated=TO_TIMESTAMP('2017-06-19 18:16:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544391
+;
+
+-- 2017-06-19T18:16:39.583
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Beleg Status',Updated=TO_TIMESTAMP('2017-06-19 18:16:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544393
+;
+
+-- 2017-06-19T18:16:57.035
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Anhänge',Updated=TO_TIMESTAMP('2017-06-19 18:16:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544390
+;
+
+-- 2017-06-19T18:17:04.252
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Dokumenten Liste',Updated=TO_TIMESTAMP('2017-06-19 18:17:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544388
+;
+
+-- 2017-06-19T18:17:19.076
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Referenzierte Daten',Updated=TO_TIMESTAMP('2017-06-19 18:17:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544389
+;
+
+-- 2017-06-19T18:18:02.945
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Verzeichnis',Updated=TO_TIMESTAMP('2017-06-19 18:18:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544396
+;
+
+-- 2017-06-19T18:18:10.309
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Benutzer Menü',Updated=TO_TIMESTAMP('2017-06-19 18:18:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544395
+;
+
+-- 2017-06-19T18:18:15.540
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Gehe zu Seite',Updated=TO_TIMESTAMP('2017-06-19 18:18:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544374
+;
+
+-- 2017-06-19T18:18:37.164
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Zeilen ausgewählt',Updated=TO_TIMESTAMP('2017-06-19 18:18:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544378
+;
+
+-- 2017-06-19T18:18:50.361
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Keine Zeilen ausgewählt',Updated=TO_TIMESTAMP('2017-06-19 18:18:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544379
+;
+
+-- 2017-06-19T18:18:57.008
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Alle auswählen',Updated=TO_TIMESTAMP('2017-06-19 18:18:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544376
+;
+
+-- 2017-06-19T18:19:21.502
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Wähle alle %(size) Zeilen',Updated=TO_TIMESTAMP('2017-06-19 18:19:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544399
+;
+
+-- 2017-06-19T18:19:37.743
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Alle Zeilen dieser Seite auswählen',Updated=TO_TIMESTAMP('2017-06-19 18:19:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544377
+;
+
+-- 2017-06-19T18:19:56.423
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Zeilen gesamt',Updated=TO_TIMESTAMP('2017-06-19 18:19:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544375
+;
+
+-- 2017-06-19T18:21:02.872
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Zurücksetzen',Updated=TO_TIMESTAMP('2017-06-19 18:21:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544386
+;
+
+-- 2017-06-19T18:21:34.901
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Foto von Webcam',Updated=TO_TIMESTAMP('2017-06-19 18:21:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544387
+;
+
+-- 2017-06-19T18:21:42.005
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Foto hochladen',Updated=TO_TIMESTAMP('2017-06-19 18:21:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544385
+;
+
+-- 2017-06-19T18:21:45.966
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Aktionen',Updated=TO_TIMESTAMP('2017-06-19 18:21:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544354
+;
+
+-- 2017-06-19T18:21:52.589
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Aktiver Filter',Updated=TO_TIMESTAMP('2017-06-19 18:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544362
+;
+
+-- 2017-06-19T18:22:07.729
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Neu hinzufügen',Updated=TO_TIMESTAMP('2017-06-19 18:22:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544351
+;
+
+-- 2017-06-19T18:22:21.361
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Erweiterte Erfassung',Updated=TO_TIMESTAMP('2017-06-19 18:22:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544340
+;
+
+-- 2017-06-19T18:22:26.991
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Zeige alle',Updated=TO_TIMESTAMP('2017-06-19 18:22:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544372
+;
+
+-- 2017-06-19T18:22:31.414
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Anwenden',Updated=TO_TIMESTAMP('2017-06-19 18:22:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544364
+;
+
+-- 2017-06-19T18:22:35.734
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Zurück',Updated=TO_TIMESTAMP('2017-06-19 18:22:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544368
+;
+
+-- 2017-06-19T18:22:43.718
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Massen Erfassung',Updated=TO_TIMESTAMP('2017-06-19 18:22:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544349
+;
+
+-- 2017-06-19T18:22:55.191
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Massen Erfassung schliessen',Updated=TO_TIMESTAMP('2017-06-19 18:22:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544350
+;
+
+-- 2017-06-19T18:23:10.720
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Menübaum anzeigen',Updated=TO_TIMESTAMP('2017-06-19 18:23:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544367
+;
+
+-- 2017-06-19T18:23:23.731
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Verbindungsproblem',Updated=TO_TIMESTAMP('2017-06-19 18:23:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544360
+;
+
+-- 2017-06-19T18:23:28.232
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Löschen',Updated=TO_TIMESTAMP('2017-06-19 18:23:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544348
+;
+
+-- 2017-06-19T18:23:32.175
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Löschen',Updated=TO_TIMESTAMP('2017-06-19 18:23:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544342
+;
+
+-- 2017-06-19T18:23:49.570
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Dateien hier ablegen',Updated=TO_TIMESTAMP('2017-06-19 18:23:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544339
+;
+
+-- 2017-06-19T18:24:13.601
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Element auswählen zur Anzeige der Merkmale',Updated=TO_TIMESTAMP('2017-06-19 18:24:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544355
+;
+
+-- 2017-06-19T18:24:35.572
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Verbindung unterbrochen',Updated=TO_TIMESTAMP('2017-06-19 18:24:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544358
+;
+
+-- 2017-06-19T18:25:42.754
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Es treten Verbindungsprobleme auf. Bitte kontrolliere die Verbindung und lade die Seite neu.',Updated=TO_TIMESTAMP('2017-06-19 18:25:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544359
+;
+
+-- 2017-06-19T18:25:57.203
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Fehler beim laden der Daten ...',Updated=TO_TIMESTAMP('2017-06-19 18:25:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544356
+;
+
+-- 2017-06-19T18:26:02.176
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Filter',Updated=TO_TIMESTAMP('2017-06-19 18:26:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544361
+;
+
+-- 2017-06-19T18:26:17.307
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Nachrichten',Updated=TO_TIMESTAMP('2017-06-19 18:26:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544369
+;
+
+-- 2017-06-19T18:26:31.515
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Die Nachrichtenbox ist leer',Updated=TO_TIMESTAMP('2017-06-19 18:26:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544371
+;
+
+-- 2017-06-19T18:26:52.788
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Abmelden',Updated=TO_TIMESTAMP('2017-06-19 18:26:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544345
+;
+
+-- 2017-06-19T18:27:17.837
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Nachrichtenbox',Updated=TO_TIMESTAMP('2017-06-19 18:27:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544394
+;
+
+-- 2017-06-19T18:28:01.329
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Nutzername',Updated=TO_TIMESTAMP('2017-06-19 18:28:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544380
+;
+
+-- 2017-06-19T18:28:16.781
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Alles als gelesen markieren',Updated=TO_TIMESTAMP('2017-06-19 18:28:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544370
+;
+
+-- 2017-06-19T18:28:25.798
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='In neuem Tab öffnen',Updated=TO_TIMESTAMP('2017-06-19 18:28:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544347
+;
+
+-- 2017-06-19T18:28:30.245
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Keine Daten',Updated=TO_TIMESTAMP('2017-06-19 18:28:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544357
+;
+
+-- 2017-06-19T18:28:56.361
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Pflicht Filter sind nicht ausgefüllt!',Updated=TO_TIMESTAMP('2017-06-19 18:28:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544363
+;
+
+-- 2017-06-19T18:29:03.565
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Keine Ergebnisse',Updated=TO_TIMESTAMP('2017-06-19 18:29:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544366
+;
+
+-- 2017-06-19T18:29:32.621
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Nachrict',Updated=TO_TIMESTAMP('2017-06-19 18:29:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544373
+;
+
+-- 2017-06-19T18:29:35.985
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Drucken',Updated=TO_TIMESTAMP('2017-06-19 18:29:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544341
+;
+
+-- 2017-06-19T18:29:40.637
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Einstellungen',Updated=TO_TIMESTAMP('2017-06-19 18:29:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544344
+;
+
+-- 2017-06-19T18:29:50.798
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Kein Anhang vorhanden',Updated=TO_TIMESTAMP('2017-06-19 18:29:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544352
+;
+
+-- 2017-06-19T18:29:58.991
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Keine Referenzen vorhanden',Updated=TO_TIMESTAMP('2017-06-19 18:29:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544353
+;
+
+-- 2017-06-19T18:31:09.347
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Formulierung hier eingeben',Updated=TO_TIMESTAMP('2017-06-19 18:31:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544365
+;
+
+-- 2017-06-19T18:31:41.005
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Zoom',Updated=TO_TIMESTAMP('2017-06-19 18:31:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544346
+;
+
+-- 2017-06-19T21:10:47.131
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Nachricht',Updated=TO_TIMESTAMP('2017-06-19 21:10:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544373
+;
+
