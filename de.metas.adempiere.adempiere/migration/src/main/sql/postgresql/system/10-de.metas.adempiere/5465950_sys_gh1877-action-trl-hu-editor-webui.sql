e06a83345905a6026134c20a786c0a592b41dfdb
Mark Krake <mark.krake@metasfresh.com>
2017-06-22 16:14:29 +0200

    #1877 Adding Translations for Customer and Vendor Return Quickactions
    Adding Translations de_DE for Customer and Vendor Return Quickactions
    
    FRESH-2703 https://github.com/metasfresh/metasfresh/issues/1877

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5465950_sys_gh1877-action-trl-hu-editor-webui.sql
@@ -0,0 +1,20 @@
+-- 2017-06-22T16:02:24.657
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Entsorgen',Updated=TO_TIMESTAMP('2017-06-22 16:02:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540796
+;
+
+-- 2017-06-22T16:02:55.964
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Lieferanten Rücklieferung',Updated=TO_TIMESTAMP('2017-06-22 16:02:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540795
+;
+
+-- 2017-06-22T16:03:11.248
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Kunden Rücklieferung',Updated=TO_TIMESTAMP('2017-06-22 16:03:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540797
+;
+
+-- 2017-06-22T16:03:35.667
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Nach Sperrlager verschieben',Updated=TO_TIMESTAMP('2017-06-22 16:03:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540793
+;
+
