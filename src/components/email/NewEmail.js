93736cfca196cdaeef84754a28262b41e881eefd
katarzynadunikowska <katarzyna.dunikowska@jazzy.pro>
2017-06-22 09:14:41 +0200

    add responsive #836

--- src/components/email/NewEmail.js
+++ src/components/email/NewEmail.js
@@ -125,35 +125,40 @@ class NewEmail extends Component {
                     <div className="panel-email-body">
                         <textarea></textarea>
                     </div>
-                    <div className="email-attachments-wrapper">
-                        <div className="attachment">
-                            <div>
-                                attachment.pdf <span>(230 kb)</span>
-                            </div>
-                            <div className="input-icon input-icon-lg">
-                                <i className="meta-icon-close-1"/>
-                            </div>
-                        </div>
-                        <div className="attachment">
-                            <div>
-                                new_metasfresh.pdf <span>(230 kb)</span>
-                            </div>
-                            <div className="input-icon input-icon-lg">
-                                <i className="meta-icon-close-1"/>
-                            </div>
-                        </div>
-                        <div className="attachment">
-                            <div>
-                                xxx.pdf <span>(230 kb)</span>
-                            </div>
-                            <div className="input-icon input-icon-lg">
-                                <i className="meta-icon-close-1"/>
-                            </div>
-                        </div>
-                    </div>
                     <div className="panel-email-footer">
-                        <div><i className="meta-icon-attachments"/> add attachment</div>
-                        <button className="btn btn-meta-success btn-sm btn-submit">Send</button>
+                        <div className="email-attachments-wrapper">
+                            <div className="attachment">
+                                <div className="attachnemt-text">
+                                    attachment.pdf <span>(230 kb)</span>
+                                </div>
+                                <div className="input-icon input-icon-lg">
+                                    <i className="meta-icon-close-1"/>
+                                </div>
+                            </div>
+                            <div className="attachment">
+                                <div className="attachnemt-text">
+                                    new_metasfresh.pdf <span>(230 kb)</span>
+                                </div>
+                                <div className="input-icon input-icon-lg">
+                                    <i className="meta-icon-close-1"/>
+                                </div>
+                            </div>
+                            <div className="attachment">
+                                <div className="attachnemt-text">
+                                    xxx.pdf <span>(230 kb)</span>
+                                </div>
+                                <div className="input-icon input-icon-lg">
+                                    <i className="meta-icon-close-1"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <i className="meta-icon-attachments"/>
+                             add attachment
+                        </div>
+                        <button className="btn btn-meta-success btn-sm btn-submit">
+                            Send
+                        </button>
                     </div>
 
                 </div>
