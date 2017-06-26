fdc20e2202b6edb6963d6f1c30154d288db6a4ab
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-06-26 18:27:37 +0300

    Add some new async batch types
    #1771 add legacy features

---
+++ de.metas.document.archive/de.metas.document.archive.base/src/main/java/de/metas/document/archive/Archive_Constants.java
@@ -0,0 +1,14 @@
+package de.metas.document.archive;
+
+public final class Archive_Constants
+{
+	private Archive_Constants()
+	{
+
+	}
+	
+	/**
+	 * Internal Name for ReCreatePDF async batch
+	 */
+	public static final String C_Async_Batch_InternalName_ReCreatePDF = "ReCreatePDF";
+}
