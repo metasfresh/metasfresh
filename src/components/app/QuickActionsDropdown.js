c18bfc11999d6429526e869dd3bd65f5cdf2336e
katarzynadunikowska <katarzyna.dunikowska@jazzy.pro>
2017-06-28 12:44:40 +0200

    Keyboard shortcut for Quickaction dropdown #919

--- src/components/app/QuickActionsDropdown.js
+++ src/components/app/QuickActionsDropdown.js
@@ -13,6 +13,47 @@ class QuickActionsDropdown extends Component {
         handleClickOutside();
     }
 
+    componentDidMount() {
+        document.getElementsByClassName('quick-actions-item')[0].focus();
+    }
+
+    handleKeyDown = (e, action) => {
+        const {handleClick} = this.props;
+        e.preventDefault();
+        const next = document.activeElement.nextSibling;
+        const prev = document.activeElement.previousSibling;
+        switch(e.key){
+            case 'ArrowDown':
+                if(
+                    !document.activeElement.classList
+                        .contains('quick-actions-item')
+                ){
+                    document.getElementsByClassName('quick-actions-item')[0]
+                        .focus();
+                } else {
+                    if(next && next.classList.contains('quick-actions-item')){
+                        next.focus();
+                    }
+                }
+                
+                break;
+            case 'ArrowUp':
+                if(prev && prev.classList.contains('quick-actions-item')){
+                    prev.focus();
+                }
+                break;
+            case 'Enter':
+                handleClick(action);
+                break;
+        }
+    }
+
+    handleItem = (item) => {
+        document.getElementsByClassName('quick-actions-item')[item].focus();
+    }
+
+
+
     render() {
         const {
             actions,
@@ -21,16 +62,20 @@ class QuickActionsDropdown extends Component {
 
         return (
             <div
-                className="quick-actions-dropdown"
+                className="quick-actions-dropdown" 
             >
                 {actions.map((action, index) =>
                     <div
+                        tabIndex={0}
+                        ref={c => this.item = c}
                         className={
                             'quick-actions-item ' +
                             (action.disabled ?
                                 'quick-actions-item-disabled ' : '')}
                         key={index}
                         onClick={() => handleClick(action)}
+                        onKeyDown={(e) => this.handleKeyDown(e, action)}
+                        onMouseEnter={() => this.handleItem(index)}
                     >
                         {action.caption}
                         {action.disabled &&
