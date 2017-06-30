49f8ef7c8c99e950da8a1be43bef42bbd861aa5a
Mark Krake <mark.krake@metasfresh.com>
2017-06-30 11:21:11 +0200

    #1854 Adjustment of Sequence for Fields QtyReceived/ QtyOrdered for TU/CU
    Adjustment of Sequence for Fields QtyReceived/ QtyOrdered for TU/CU
    
    FRESH-2662 https://github.com/metasfresh/metasfresh/issues/1854

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5466630_sys_gh1854-seqno-qtyreceived-ordered-materialreceipt.sql
@@ -0,0 +1,10 @@
+-- 2017-06-30T11:06:00.259
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2017-06-30 11:06:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=542028
+;
+
+-- 2017-06-30T11:06:00.270
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2017-06-30 11:06:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541847
+;
+
