7859a4c6e0a75f9be7889f7607de69807fa06a14
Mark Krake <mark.krake@metasfresh.com>
2017-07-01 17:42:33 +0200

    #1933 Adding fadditional Menu Translations for en_US Language
    Adding fadditional Menu Translations for en_US Language (Sales
    Opportunities, Purchase Candidates, Material Tracking, MRP Product Info,
    Product for Procurement Contract, Availability Trend, Request for
    Proposal + Respomses & Lines, Allotment)
    
    FRESH-2788 https://github.com/metasfresh/metasfresh/issues/1933

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5466660_sys_gh1933-menu-trl-en-us-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5466660_sys_gh1933-menu-trl-en-us-webui.sql
@@ -63,3 +63,108 @@ UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 16:52:09','
 UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 16:52:19','YYYY-MM-DD HH24:MI:SS'),Name='Purchase Candidates',WEBUI_NameBrowse='Purchase Candidates' WHERE AD_Menu_ID=540785 AND AD_Language='en_US'
 ;
 
+-- 2017-07-01T17:21:01.196
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:21:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Material Tracking',WEBUI_NameBrowse='Material Tracking' WHERE AD_Menu_ID=540786 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:21:15.650
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:21:15','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Material Tracking' WHERE AD_Window_ID=540226 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:21:35.524
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:21:35','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Material Tracking' WHERE AD_Tab_ID=540610 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:22:15.166
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:22:15','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MRP Product Info',WEBUI_NameBrowse='MRP Product Info' WHERE AD_Menu_ID=540817 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:22:28.594
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:22:28','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MRP Product Info' WHERE AD_Window_ID=540338 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:23:37.148
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:23:37','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product for Procurement Contracts' WHERE AD_Window_ID=540288 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:23:59.143
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:23:59','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product for Procurement Contracts',WEBUI_NameBrowse='Product for Procurement Contracts' WHERE AD_Menu_ID=540869 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:25:57.973
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:25:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Procurement Availability Trend',WEBUI_NameBrowse='Procurement Availability Trend' WHERE AD_Menu_ID=540870 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:30:12.581
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:30:12','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal',Description='Maintain Request for Proposals',WEBUI_NameBrowse='Request for Proposal' WHERE AD_Menu_ID=540877 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:30:17.041
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:30:17','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=540877 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:30:59.667
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:30:59','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal',Description='Manage RRequest for Proposals',Help='Request for Proposal to be sent out to vendors of a RfP Topic. After Vendor selection, optionally create Sales Order or Quote for Customer as well as Purchase Order  for Vendor(s)' WHERE AD_Window_ID=315 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:31:25.778
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:31:25','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal',Description='Request for Proposal',Help='Request for Proposal to be sent out to vendors of a RfP Topic. After Vendor selection, optionally create Sales Order or Quote for Customer as well as Purchase Order  for Vendor(s)' WHERE AD_Tab_ID=613 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:33:53.913
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:33:53','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal Response',Description='Maintain Request for Proposal Responses',WEBUI_NameBrowse='Request for Proposal Response' WHERE AD_Menu_ID=540879 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:34:17.560
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:34:17','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal Response',Description='Manage Request for Proposal Responses' WHERE AD_Window_ID=324 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:34:58.229
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:34:58','YYYY-MM-DD HH24:MI:SS'),Description='Request for Proposal Response',Help='Request for Proposal Response from a potential Vendor' WHERE AD_Tab_ID=614 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:35:35.123
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:35:35','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Request for Proposal Responseline',WEBUI_NameBrowse='Request for Proposal Responseline' WHERE AD_Menu_ID=540880 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:35:59.185
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:35:59','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Request for Proposal Responseline' WHERE AD_Window_ID=540295 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:36:18.772
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:36:18','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Request for Proposal Responseline' WHERE AD_Tab_ID=540740 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:37:17.252
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:37:17','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Allotment',WEBUI_NameBrowse='Allotment' WHERE AD_Menu_ID=540888 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:37:32.261
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:37:32','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Allotment' WHERE AD_Window_ID=540210 AND AD_Language='en_US'
+;
+
+-- 2017-07-01T17:37:46.844
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-01 17:37:46','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Allotment' WHERE AD_Tab_ID=540573 AND AD_Language='en_US'
+;
+
