3cf7bfe7994bbc220eaa3d34024fd23a79e63a30
Mark Krake <mark.krake@metasfresh.com>
2017-07-02 12:25:36 +0200

    #1933 New Translations for Language en_US in WebUI Menu and Window Names
    New Translations for Language en_US in WebUI Menu and Window Names
    (Request for Proposal Topic, Procurement Notifications, Deliveryplanning
    Record, Availability Trend Records. Price List, Product Prices, Price
    Schema, Pricing Rules, Inventory Movement)
    
    FRESH-2788 https://github.com/metasfresh/metasfresh/issues/1933

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5466670_sys_gh1933-menu-trl-en-us-webui.sql
@@ -0,0 +1,205 @@
+-- 2017-07-02T11:44:08.126
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:44:08','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal Topic',Description='Maintain Request for Proposal Topics and invite Partners',WEBUI_NameBrowse='Request for Proposal Topic' WHERE AD_Menu_ID=540878 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:44:11.068
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:44:11','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=540878 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:45:16.067
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:45:16','YYYY-MM-DD HH24:MI:SS'),Name='Request for Proposal Topic',Description='Maintain Request for Proposal Topic and Subscribers',Help='A Request for Proposal Topic allows you to maintain a subscriber list of potential Vendors to respond to RfPs' WHERE AD_Window_ID=314 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:45:46.380
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:45:46','YYYY-MM-DD HH24:MI:SS'),Description='Request for Proposal Topic',Help='A Request for Proposal Topic allows you to maintain a subscriber list of potential Vendors to respond to RfPs' WHERE AD_Tab_ID=619 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:46:36.024
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:46:36','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Procurement Notifications',WEBUI_NameBrowse='Procurement Notifications' WHERE AD_Menu_ID=540876 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:47:01.728
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:47:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Procurement Notifications' WHERE AD_Window_ID=540293 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:47:16.861
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:47:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Notifications' WHERE AD_Tab_ID=540735 AND AD_Language='de_CH'
+;
+
+-- 2017-07-02T11:47:57.244
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:47:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Deliveryplanning Record',WEBUI_NameBrowse='Deliveryplanning Record' WHERE AD_Menu_ID=540873 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:48:29.230
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:48:29','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Deliveryplanning Record' WHERE AD_Window_ID=540286 AND AD_Language='de_CH'
+;
+
+-- 2017-07-02T11:48:42.612
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:48:42','YYYY-MM-DD HH24:MI:SS'),IsTranslated='N',Name='Lieferplanungsdatensatz',Help='Deliveryplanning Record' WHERE AD_Window_ID=540286 AND AD_Language='de_CH'
+;
+
+-- 2017-07-02T11:48:45.539
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:48:45','YYYY-MM-DD HH24:MI:SS'),Help='' WHERE AD_Window_ID=540286 AND AD_Language='de_CH'
+;
+
+-- 2017-07-02T11:48:52.840
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:48:52','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Deliveryplanning Record' WHERE AD_Window_ID=540286 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:49:07.816
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:49:07','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Deliveryplanning Record' WHERE AD_Tab_ID=540727 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:49:52.064
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:49:52','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',WEBUI_NameBrowse='Availabity Trend Record' WHERE AD_Menu_ID=540874 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:50:01.946
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:50:01','YYYY-MM-DD HH24:MI:SS'),Name='Availabity Trend Record' WHERE AD_Menu_ID=540874 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:50:16.640
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:50:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Availabity Trend Record' WHERE AD_Window_ID=540290 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:50:34.674
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:50:34','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Availabity Trends' WHERE AD_Tab_ID=540733 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:52:09.187
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:52:09','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=540756 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:52:32.480
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:52:32','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Price System' WHERE AD_Window_ID=540320 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:52:49.004
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:52:49','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Preis System' WHERE AD_Tab_ID=540775 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:52:55.780
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:52:55','YYYY-MM-DD HH24:MI:SS'),Name='Price System' WHERE AD_Tab_ID=540775 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:53:22.180
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:53:22','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Price List',WEBUI_NameBrowse='Price List',WEBUI_NameNew='New Price List' WHERE AD_Menu_ID=540775 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:53:47.435
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:53:47','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='New Price List' WHERE AD_Window_ID=540321 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:54:00.243
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:54:00','YYYY-MM-DD HH24:MI:SS'),Name='Price List' WHERE AD_Window_ID=540321 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:54:16.310
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:54:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Price List' WHERE AD_Tab_ID=540776 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:54:42.270
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:54:42','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product Prices',WEBUI_NameBrowse='Product Prices' WHERE AD_Menu_ID=540776 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:54:48.832
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET EntityType='D',Updated=TO_TIMESTAMP('2017-07-02 11:54:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540776
+;
+
+-- 2017-07-02T11:55:02.844
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:55:02','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product Prices' WHERE AD_Window_ID=540325 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T11:55:16.249
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 11:55:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product Prices' WHERE AD_Tab_ID=540787 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:12:06.911
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET EntityType='D',Updated=TO_TIMESTAMP('2017-07-02 12:12:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540777
+;
+
+-- 2017-07-02T12:12:26.100
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:12:26','YYYY-MM-DD HH24:MI:SS'),Name='Price Schema',Description='Maintain Price Schemas',WEBUI_NameBrowse='Price Schema' WHERE AD_Menu_ID=540777 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:12:28.601
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:12:28','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=540777 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:13:22.499
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:13:22','YYYY-MM-DD HH24:MI:SS'),Name='Price List Schema',Description='Maintain Price List Schemas',WEBUI_NameBrowse='Price List Schema' WHERE AD_Menu_ID=540777 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:14:00.595
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:14:00','YYYY-MM-DD HH24:MI:SS'),Name='Pricing Rule',WEBUI_NameBrowse='Pricing Rule' WHERE AD_Menu_ID=540778 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:14:15.071
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='Preisberechnung',Updated=TO_TIMESTAMP('2017-07-02 12:14:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=53189
+;
+
+-- 2017-07-02T12:14:15.078
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description='Defines a list of pricing and discount rules that need to be invoked when product price is calculated.', IsActive='Y', Name='Preisberechnung',Updated=TO_TIMESTAMP('2017-07-02 12:14:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=53432
+;
+
+-- 2017-07-02T12:14:25.762
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:14:25','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Window_ID=53189 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:14:33.975
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Regel',Updated=TO_TIMESTAMP('2017-07-02 12:14:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53540
+;
+
+-- 2017-07-02T12:14:42.148
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:14:42','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Tab_ID=53540 AND AD_Language='en_US'
+;
+
+-- 2017-07-02T12:15:12.128
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET EntityType='D',Updated=TO_TIMESTAMP('2017-07-02 12:15:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540791
+;
+
+-- 2017-07-02T12:21:56.678
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-07-02 12:21:56','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Inventory Move',Description='Inventory Move',WEBUI_NameBrowse='Inventory Move' WHERE AD_Menu_ID=540791 AND AD_Language='en_US'
+;
+
