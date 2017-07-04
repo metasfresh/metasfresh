b9e990a6f6b613c4b4add7048bd8c885bfab1335
Teo Sarca <teo.sarca@gmail.com>
2017-07-04 19:42:45 +0300

    ViewActionParam: sqlLookupTableName support

--- src/main/java/de/metas/ui/web/process/view/ViewActionParamDescriptor.java
+++ src/main/java/de/metas/ui/web/process/view/ViewActionParamDescriptor.java
@@ -1,12 +1,20 @@
 package de.metas.ui.web.process.view;
 
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.util.Check;
+import org.compiere.util.DisplayType;
+import org.slf4j.Logger;
+
 import com.google.common.base.Preconditions;
 
+import de.metas.logging.LogManager;
 import de.metas.ui.web.process.view.ViewActionDescriptor.ViewActionMethodArgumentExtractor;
 import de.metas.ui.web.view.IView;
 import de.metas.ui.web.window.datatypes.DocumentIdsSelection;
 import de.metas.ui.web.window.descriptor.DocumentFieldDescriptor;
 import de.metas.ui.web.window.descriptor.DocumentFieldDescriptor.Characteristic;
+import de.metas.ui.web.window.descriptor.LookupDescriptorProvider;
+import de.metas.ui.web.window.descriptor.sql.SqlLookupDescriptor;
 import de.metas.ui.web.window.model.Document;
 import lombok.Builder;
 import lombok.NonNull;
@@ -24,19 +32,20 @@ import lombok.NonNull;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-
 @Builder
 public final class ViewActionParamDescriptor
 {
+	private static final Logger logger = LogManager.getLogger(ViewActionParamDescriptor.class);
+
 	private final @NonNull String parameterName;
 	private final @NonNull Class<?> parameterValueClass;
 	private final ViewActionParam parameterAnnotation;
@@ -52,13 +61,29 @@ public final class ViewActionParamDescriptor
 	{
 		Preconditions.checkState(isUserParameter(), "parameter is internal");
 
+		LookupDescriptorProvider lookupDescriptorProvider = LookupDescriptorProvider.NULL;
+		if (parameterAnnotation.widgetType().isLookup())
+		{
+			if (Check.isEmpty(parameterAnnotation.sqlLookupTableName(), true))
+			{
+				logger.warn("sqlLookupTableName not provided in " + parameterAnnotation + " (" + this + "). Continuing...");
+			}
+			else
+			{
+				lookupDescriptorProvider = SqlLookupDescriptor.builder()
+						.setColumnName(InterfaceWrapperHelper.getKeyColumnName(parameterAnnotation.sqlLookupTableName()))
+						.setDisplayType(DisplayType.Search)
+						.buildProvider();
+			}
+		}
+		
 		return DocumentFieldDescriptor.builder(parameterName)
 				.setCaption(parameterAnnotation.caption())
-				// .setDescription(attribute.getDescription())
+				// .setDescription(parameterAnnotation.description())
 				//
 				.setValueClass(parameterValueClass)
 				.setWidgetType(parameterAnnotation.widgetType())
-				// .setLookupDescriptorProvider(lookupDescriptor)
+				.setLookupDescriptorProvider(lookupDescriptorProvider != null ? lookupDescriptorProvider : LookupDescriptorProvider.NULL)
 				//
 				// .setDefaultValueExpression(defaultValueExpr)
 				.setReadonlyLogic(false)
