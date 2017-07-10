f1b81c2f353ad090529054b319884395fe6aa4df
Mark Krake <mark.krake@metasfresh.com>
2017-07-10 15:38:06 +0200

    #1938 Adding further adjustments/ translations/ name changes to Greeting Window in WebUI
    Adding further adjustments/ translations/ name changes to Greeting
    Window in WebUI
    
    FRESH-2793 https://github.com/metasfresh/metasfresh/issues/1938

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5467530_sys_gh1938-greeting-window-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5467530_sys_gh1938-greeting-window-webui.sql
@@ -128,3 +128,128 @@ DELETE FROM AD_UI_ElementGroup WHERE AD_UI_ElementGroup_ID=540102
 UPDATE AD_UI_Element SET UIStyle='primary',Updated=TO_TIMESTAMP('2017-07-10 11:10:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546490
 ;
 
+-- 2017-07-10T15:17:22.801
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,551741,0,282,540816,546498,TO_TIMESTAMP('2017-07-10 15:17:22','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Briefanrede',30,0,0,TO_TIMESTAMP('2017-07-10 15:17:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-07-10T15:17:29.255
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=546491
+;
+
+-- 2017-07-10T15:17:59.341
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=546492
+;
+
+-- 2017-07-10T15:18:03.260
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_ElementGroup WHERE AD_UI_ElementGroup_ID=540817
+;
+
+-- 2017-07-10T15:18:37.188
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,540074,540820,TO_TIMESTAMP('2017-07-10 15:18:37','YYYY-MM-DD HH24:MI:SS'),100,'Y','letter',20,TO_TIMESTAMP('2017-07-10 15:18:37','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-07-10T15:18:56.838
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,551741,0,282,540820,546499,TO_TIMESTAMP('2017-07-10 15:18:56','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Briefanrede',10,0,0,TO_TIMESTAMP('2017-07-10 15:18:56','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-07-10T15:19:26.802
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=546498
+;
+
+-- 2017-07-10T15:21:52.933
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=10,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546490
+;
+
+-- 2017-07-10T15:21:52.939
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546499
+;
+
+-- 2017-07-10T15:21:52.944
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546493
+;
+
+-- 2017-07-10T15:21:52.948
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546495
+;
+
+-- 2017-07-10T15:21:52.953
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546494
+;
+
+-- 2017-07-10T15:21:52.957
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546496
+;
+
+-- 2017-07-10T15:21:52.961
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2017-07-10 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546497
+;
+
+-- 2017-07-10T15:22:05.775
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayed_SideList='Y', SeqNo_SideList=10,Updated=TO_TIMESTAMP('2017-07-10 15:22:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546490
+;
+
+-- 2017-07-10T15:22:05.781
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayed_SideList='Y', SeqNo_SideList=20,Updated=TO_TIMESTAMP('2017-07-10 15:22:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546493
+;
+
+-- 2017-07-10T15:22:05.786
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayed_SideList='Y', SeqNo_SideList=30,Updated=TO_TIMESTAMP('2017-07-10 15:22:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546495
+;
+
+-- 2017-07-10T15:22:05.792
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayed_SideList='Y', SeqNo_SideList=40,Updated=TO_TIMESTAMP('2017-07-10 15:22:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546494
+;
+
+-- 2017-07-10T15:26:33.607
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nur Vorname',Updated=TO_TIMESTAMP('2017-07-10 15:26:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3161
+;
+
+-- 2017-07-10T15:26:48.701
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Nur Vorname', PrintName='Nur Vorname',Updated=TO_TIMESTAMP('2017-07-10 15:26:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=1173
+;
+
+-- 2017-07-10T15:26:48.706
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='IsFirstNameOnly', Name='Nur Vorname', Description='Nur den ersten Namen bei Anreden drucken', Help='Das Selektionsfeld "Nur erster Name" zeigt an, dass nur der erste Name dieses Kontaktes bei der Anrede gedruckt weren soll.' WHERE AD_Element_ID=1173
+;
+
+-- 2017-07-10T15:26:48.729
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IsFirstNameOnly', Name='Nur Vorname', Description='Nur den ersten Namen bei Anreden drucken', Help='Das Selektionsfeld "Nur erster Name" zeigt an, dass nur der erste Name dieses Kontaktes bei der Anrede gedruckt weren soll.', AD_Element_ID=1173 WHERE UPPER(ColumnName)='ISFIRSTNAMEONLY' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-07-10T15:26:48.730
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IsFirstNameOnly', Name='Nur Vorname', Description='Nur den ersten Namen bei Anreden drucken', Help='Das Selektionsfeld "Nur erster Name" zeigt an, dass nur der erste Name dieses Kontaktes bei der Anrede gedruckt weren soll.' WHERE AD_Element_ID=1173 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-07-10T15:26:48.731
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nur Vorname', Description='Nur den ersten Namen bei Anreden drucken', Help='Das Selektionsfeld "Nur erster Name" zeigt an, dass nur der erste Name dieses Kontaktes bei der Anrede gedruckt weren soll.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=1173) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-07-10T15:26:48.743
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Nur Vorname', Name='Nur Vorname' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=1173)
+;
+
