metasfresh/metasfresh

commit 3f005ad92d0258e073892bb7c78bddd962375683

Only create M_ShipmentSchedules for "Item" products
* the main thing is the change in the view
C_OrderLine_ID_With_Missing_ShipmentSchedule_v
* minor cleanup: remove two deprecated methods from MProduct
* minor cleanup: fix method name from
IInOutCandHandler.createDeliverReques (add a 't') 

https://github.com/metasfresh/metasfresh/issues/992 Allow subscription without shipment

de.metas.business/src/main/java-legacy/org/compiere/model/ProductCost.java
@@ -28,6 +28,7 @@ import org.slf4j.Logger;
 import de.metas.logging.LogManager;
 import org.compiere.util.Env;
 
+import de.metas.product.IProductBL;
 import de.metas.product.acct.api.IProductAcctDAO;
 
 /**
@@ -98,7 +99,9 @@ public class ProductCost
 	public boolean isService()
 	{
 		if (m_product != null)
-			return m_product.isService();
+		{
+			return Services.get(IProductBL.class).isItem(m_product);
+		}
 		return false;
 	}	// isService
 

de.metas.acct.base/src/main/java-legacy/org/compiere/acct/Doc_ProjectIssue.java
@@ -20,10 +20,9 @@ import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import org.slf4j.Logger;
-import de.metas.logging.LogManager;
 
 import org.adempiere.acct.api.ProductAcctType;
+import org.adempiere.util.Services;
 import org.compiere.model.MAcctSchema;
 import org.compiere.model.MProduct;
 import org.compiere.model.MProject;
@@ -31,6 +30,8 @@ import org.compiere.model.MProjectIssue;
 import org.compiere.model.ProductCost;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
+
+import de.metas.product.IProductBL;
 
 /**
  *	Project Issue.
@@ -155,8 +156,10 @@ public class Doc_ProjectIssue extends Doc
 		//  Inventory               CR
 		{
 			ProductAcctType acctType = ProductCost.ACCTTYPE_P_Asset;
-			if (product.isService())
+			if (Services.get(IProductBL.class).isService(product))
+			{
 				acctType = ProductCost.ACCTTYPE_P_Expense;
+			}
 			cr = fact.createLine(m_line,
 				m_line.getAccount(acctType, as),
 				as.getC_Currency_ID(), null, cost);

de.metas.business/src/main/java-legacy/org/compiere/model/MInOutLine.java
@@ -33,6 +33,7 @@ import org.compiere.util.DB;
 import org.compiere.util.Env;
 
 import de.metas.document.engine.IDocActionBL;
+import de.metas.product.IProductBL;
 
 /**
  * InOut Line
@@ -209,7 +210,7 @@ public class MInOutLine extends X_M_InOutLine
 				setM_AttributeSetInstance_ID(oLine.getM_AttributeSetInstance_ID());
 			}
 			//
-			if (product.isItem())
+			if (Services.get(IProductBL.class).isItem(product))
 			{
 				if (M_Locator_ID == 0)
 					setM_Locator_ID(Qty);	// requires warehouse, product, asi

de.metas.business/src/main/java-legacy/org/compiere/model/MProduct.java
@@ -485,30 +485,6 @@ public class MProduct extends X_M_Product
 	}	// isOneAssetPerUOM
 
 	/**
-	 * Product is Item
-	 *
-	 * @return true if item
-	 * @deprecated please use {@link IProductBL#isItem(I_M_Product)}
-	 */
-	@Deprecated
-	public boolean isItem()
-	{
-		return Services.get(IProductBL.class).isItem(this);
-	}	// isItem
-
-	/**
-	 * Is Service
-	 *
-	 * @return true if service (resource, online)
-	 * @deprecated Please use {@link IProductBL#isService(I_M_Product)}
-	 */
-	@Deprecated
-	public boolean isService()
-	{
-		return Services.get(IProductBL.class).isService(this);
-	}	// isService
-
-	/**
 	 * Get UOM Symbol
 	 *
 	 * @return UOM Symbol

de.metas.contracts/src/main/java/de/metas/contracts/subscription/inoutcandidate/spi/impl/SubscriptionInOutCandHandler.java
@@ -208,7 +208,7 @@ public class SubscriptionInOutCandHandler implements IInOutCandHandler
 	}
 
 	@Override
-	public IDeliverRequest createDeliverReques(final I_M_ShipmentSchedule sched)
+	public IDeliverRequest createDeliverRequest(final I_M_ShipmentSchedule sched)
 	{
 		final IContextAware contextAware = InterfaceWrapperHelper.getContextAware(sched);
 		final TableRecordReference ref = new TableRecordReference(sched.getAD_Table_ID(), sched.getRecord_ID());

de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/api/impl/InOutCandHandlerBL.java
@@ -235,6 +235,6 @@ public class InOutCandHandlerBL implements IInOutCandHandlerBL
 		final IInOutCandHandler inOutCandHandler = tableName2Handler.get(tableName);
 		Check.assumeNotNull(inOutCandHandler, "IInOutCandHandler for {} with table name {} is not null", sched, tableName);
 
-		return inOutCandHandler.createDeliverReques(sched);
+		return inOutCandHandler.createDeliverRequest(sched);
 	}
 }

de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/spi/IInOutCandHandler.java
@@ -110,6 +110,6 @@ public interface IInOutCandHandler
 	 * @param sched
 	 * @return
 	 */
-	IDeliverRequest createDeliverReques(I_M_ShipmentSchedule sched);
+	IDeliverRequest createDeliverRequest(I_M_ShipmentSchedule sched);
 
 }

de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/spi/impl/OrderLineInOutCandHandler.java
@@ -282,7 +282,7 @@ public class OrderLineInOutCandHandler implements IInOutCandHandler
 	 * </ul>
 	 */
 	@Override
-	public IDeliverRequest createDeliverReques(final I_M_ShipmentSchedule sched)
+	public IDeliverRequest createDeliverRequest(final I_M_ShipmentSchedule sched)
 	{
 		return new IDeliverRequest()
 		{

commit 91585cda1ee33af436c72266a8b0365fbc8d1cbb

hotfix typo
issue introduced by https://github.com/metasfresh/metasfresh/commit/3f005ad92d0258e073892bb7c78bddd962375683#diff-e20fe00454c364520fe1c8b78359655d

https://github.com/metasfresh/metasfresh/issues/1970

de.metas.business/src/main/java-legacy/org/compiere/model/ProductCost.java
@@ -100,7 +100,7 @@ public class ProductCost
 	{
 		if (m_product != null)
 		{
-			return Services.get(IProductBL.class).isItem(m_product);
+			return !Services.get(IProductBL.class).isItem(m_product);
 		}
 		return false;
 	}	// isService