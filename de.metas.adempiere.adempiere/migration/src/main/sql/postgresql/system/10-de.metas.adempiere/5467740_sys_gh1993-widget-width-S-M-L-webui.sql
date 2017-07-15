5eb223e561661018dee0f7a658ed2eb3f179fd94
Mark Krake <mark.krake@metasfresh.com>
2017-07-15 09:09:52 +0200

    #1993 Add Initialization for Widget Size and further size adjustments
    Add Initialization for Widget Size and further size adjustments
    
    FRESH-2897 https://github.com/metasfresh/metasfresh/issues/1993

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5467740_sys_gh1993-widget-width-S-M-L-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5467740_sys_gh1993-widget-width-S-M-L-webui.sql
@@ -1,5 +1,13 @@
 --
--- Widget Size S
+-- Initialize all widget sizes first
+--
+update ad_ui_element
+set widgetsize = ''
+;
+
+
+--
+-- Set Widget Size S
 --
 -- Update the widget widths of specific column Reference_ID
 update ad_ui_element
@@ -39,12 +47,13 @@ and c.columnname in (
 ,'C_Invoice_Candidate_HeaderAggregation_Effective_ID'
 ,'Line'
 ,'POReference'
-'ReferenceNo'
+,'ReferenceNo'
+,'Value'
 )
 );
 
 --
--- Widget Size M
+-- Set Widget Size M
 --
 -- Update the widget widths of specific column Reference_ID
 update ad_ui_element
@@ -78,5 +87,34 @@ and c.columnname in (
 ,'C_DocType_ID'
 ,'C_DocTypeTarget_ID'
 ,'M_Product_Category_ID'
+)
+);
+
+
+--
+-- Set Widget Size L
+--
+-- Update the widget widths of specific Column Names
+update ad_ui_element
+set widgetsize = 'L'
+where ad_ui_element_id in (
+select uie.ad_ui_element_id
+from AD_UI_Element uie
+left join ad_field f on uie.ad_field_id = f.ad_field_id
+left join ad_column c on f.ad_column_id = c.ad_column_id
+left join ad_tab t on uie.ad_tab_id = t.ad_tab_id
+left join ad_window w on t.ad_window_id = w.ad_window_id
+where true
+and c.columnname in (
+'Bill_BPartner_ID'
+,'C_BPartner_ID'
+,'C_BPartner_Override_ID'
+,'C_BPartner_Vendor_ID'
+,'DropShip_BPartner_ID'
+,'HandOver_Partner_ID'
+,'Pay_BPartner_ID'
+,'Shipper_BPartner_ID'
+,'SubProducer_BPartner_ID'
+,'M_Product_ID'
 )
 );
