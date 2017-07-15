9c6dde3c627f35bf65c5394fbdc9ca89aa972199
Mark Krake <mark.krake@metasfresh.com>
2017-07-15 08:56:05 +0200

    #1993 Update Statements for general Widget Size M
    Update Statements for general Widget Size M
    
    FRESH-2897 https://github.com/metasfresh/metasfresh/issues/1993

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5467740_sys_gh1993-widget-width-S-M-L-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5467740_sys_gh1993-widget-width-S-M-L-webui.sql
@@ -1,4 +1,6 @@
--- Size S
+--
+-- Widget Size S
+--
 -- Update the widget widths of specific column Reference_ID
 update ad_ui_element
 set widgetsize = 'S'
@@ -38,5 +40,43 @@ and c.columnname in (
 ,'Line'
 ,'POReference'
 'ReferenceNo'
+)
+);
+
+--
+-- Widget Size M
+--
+-- Update the widget widths of specific column Reference_ID
+update ad_ui_element
+set widgetsize = 'M'
+where ad_ui_element_id in (
+select uie.ad_ui_element_id
+from AD_UI_Element uie
+left join ad_field f on uie.ad_field_id = f.ad_field_id
+left join ad_column c on f.ad_column_id = c.ad_column_id
+left join ad_tab t on uie.ad_tab_id = t.ad_tab_id
+left join ad_window w on t.ad_window_id = w.ad_window_id
+where true
+-- Date+Time
+and c.ad_reference_id = 16
+);
+
+-- Update the widget widths of specific Column Names
+update ad_ui_element
+set widgetsize = 'M'
+where ad_ui_element_id in (
+select uie.ad_ui_element_id
+from AD_UI_Element uie
+left join ad_field f on uie.ad_field_id = f.ad_field_id
+left join ad_column c on f.ad_column_id = c.ad_column_id
+left join ad_tab t on uie.ad_tab_id = t.ad_tab_id
+left join ad_window w on t.ad_window_id = w.ad_window_id
+where true
+and c.columnname in (
+'AD_Org_ID'
+,'AD_Client_ID'
+,'C_DocType_ID'
+,'C_DocTypeTarget_ID'
+,'M_Product_Category_ID'
 )
 );
