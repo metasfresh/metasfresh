04a5f5421de088d5afc9ade334afb02daac728a2
Teo Sarca <teo.sarca@metasfresh.com>
2017-07-21 10:01:32 +0300

    fix migration scripts
    https://github.com/metasfresh/metasfresh-webui-api/issues/503

--- de.metas.handlingunits.base/src/main/sql/postgresql/system/5468130_sys_gh503webui_M_Picking_Candidate_replace_processed_with_status_DDL.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5468130_sys_gh503webui_M_Picking_Candidate_replace_processed_with_status_DDL.sql
@@ -4,4 +4,4 @@
 /* DDL */ SELECT public.db_alter_table('m_picking_candidate','ALTER TABLE public.M_Picking_Candidate ADD COLUMN Status VARCHAR(2) DEFAULT ''IP'' NOT NULL')
 ;
 
-SELECT public.db_alter_table('m_picking_candidate','ALTER TABLE public.M_Picking_Candidate DROP COLUMN IF EXISTS Processed');
+-- SELECT public.db_alter_table('m_picking_candidate','ALTER TABLE public.M_Picking_Candidate DROP COLUMN IF EXISTS Processed');
