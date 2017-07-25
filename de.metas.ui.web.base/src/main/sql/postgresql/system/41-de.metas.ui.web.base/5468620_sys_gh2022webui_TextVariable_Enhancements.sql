fb59adabb4e3d21220def0f847a703c05694ca4b
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-07-25 14:28:25 +0300

    #2022 Text Variable included tab enhancement
    https://github.com/metasfresh/metasfresh/issues/2022

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5468620_sys_gh2022webui_TextVariable_Enhancements.sql
@@ -0,0 +1,15 @@
+-- 2017-07-25T13:25:45.306
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET AD_UI_Column_ID=540503, SeqNo=20,Updated=TO_TIMESTAMP('2017-07-25 13:25:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540867
+;
+
+-- 2017-07-25T13:25:50.968
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET AD_UI_Column_ID=540503, SeqNo=30,Updated=TO_TIMESTAMP('2017-07-25 13:25:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540869
+;
+
+-- 2017-07-25T13:26:01.486
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Column WHERE AD_UI_Column_ID=540501
+;
+
