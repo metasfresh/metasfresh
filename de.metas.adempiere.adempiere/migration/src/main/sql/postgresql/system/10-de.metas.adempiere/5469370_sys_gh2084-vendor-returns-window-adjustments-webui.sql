11d4475234415062b785421db81403d5618fbe0b
Mark Krake <mark.krake@metasfresh.com>
2017-08-14 15:29:35 +0200

    #2084 Addinmg adjustments to Vendor Returns Window in WebUI
    Adding missing Fields, Translations. Rearranging Fields in Main and Grid
    View.
    
    FRESH-3006 https://github.com/metasfresh/metasfresh/issues/2084

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469370_sys_gh2084-vendor-returns-window-adjustments-webui.sql
@@ -0,0 +1,70 @@
+-- 2017-08-14T15:12:11.041
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nr.',Updated=TO_TIMESTAMP('2017-08-14 15:12:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=57848
+;
+
+-- 2017-08-14T15:13:27.866
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-08-14 15:13:27','YYYY-MM-DD HH24:MI:SS'),Name='No.' WHERE AD_Field_ID=57848 AND AD_Language='en_US'
+;
+
+-- 2017-08-14T15:13:41.740
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2017-08-14 15:13:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=57846
+;
+
+-- 2017-08-14T15:13:50.977
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-08-14 15:13:50','YYYY-MM-DD HH24:MI:SS'),Name='Date' WHERE AD_Field_ID=57846 AND AD_Language='en_US'
+;
+
+-- 2017-08-14T15:14:49.166
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Status',Updated=TO_TIMESTAMP('2017-08-14 15:14:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=57888
+;
+
+-- 2017-08-14T15:14:55.340
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-08-14 15:14:55','YYYY-MM-DD HH24:MI:SS'),Name='Status' WHERE AD_Field_ID=57888 AND AD_Language='en_US'
+;
+
+-- 2017-08-14T15:16:09.660
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2017-08-14 15:16:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=57852
+;
+
+-- 2017-08-14T15:16:15.903
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Auftragsdatum',Updated=TO_TIMESTAMP('2017-08-14 15:16:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=57846
+;
+
+-- 2017-08-14T15:16:22.502
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-08-14 15:16:22','YYYY-MM-DD HH24:MI:SS'),Name='Date Ordered' WHERE AD_Field_ID=57846 AND AD_Language='en_US'
+;
+
+-- 2017-08-14T15:16:30.589
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-08-14 15:16:30','YYYY-MM-DD HH24:MI:SS'),Name='Date' WHERE AD_Field_ID=57852 AND AD_Language='en_US'
+;
+
+-- 2017-08-14T15:17:35.863
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Mandant',Updated=TO_TIMESTAMP('2017-08-14 15:17:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=57843
+;
+
+-- 2017-08-14T15:18:12.102
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Pack Material ',Updated=TO_TIMESTAMP('2017-08-14 15:18:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556930
+;
+
+-- 2017-08-14T15:18:52.795
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Notiz',Updated=TO_TIMESTAMP('2017-08-14 15:18:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556623
+;
+
+-- 2017-08-14T15:19:01.233
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-08-14 15:19:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Note' WHERE AD_Field_ID=556623 AND AD_Language='en_US'
+;
+
