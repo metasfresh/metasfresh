dc8bf995804b794703c999e6b6c3f5a594de068b
Mark Krake <mark.krake@metasfresh.com>
2017-08-18 15:37:43 +0200

    #2170 Further Window adjustments to BOM Window WebUI
    Further Window adjustments to BOM Window WebUI
    
    FRESH-3123 https://github.com/metasfresh/metasfresh/issues/2170

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469610_sys_gh2170-bom-window-adjustments-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469610_sys_gh2170-bom-window-adjustments-webui.sql
@@ -148,3 +148,245 @@ UPDATE AD_UI_Element SET SeqNo=230,Updated=TO_TIMESTAMP('2017-08-18 15:14:41','Y
 UPDATE AD_UI_Element SET SeqNo=210,Updated=TO_TIMESTAMP('2017-08-18 15:14:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=544403
 ;
 
+-- 2017-08-18T15:20:48.087
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Issue Methode', PrintName='Issue Methode',Updated=TO_TIMESTAMP('2017-08-18 15:20:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=53253
+;
+
+-- 2017-08-18T15:20:48.099
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='IssueMethod', Name='Issue Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.' WHERE AD_Element_ID=53253
+;
+
+-- 2017-08-18T15:20:48.117
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IssueMethod', Name='Issue Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.', AD_Element_ID=53253 WHERE UPPER(ColumnName)='ISSUEMETHOD' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-08-18T15:20:48.119
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IssueMethod', Name='Issue Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.' WHERE AD_Element_ID=53253 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:20:48.120
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Issue Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=53253) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:20:48.137
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Issue Methode', Name='Issue Methode' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=53253)
+;
+
+-- 2017-08-18T15:21:31.753
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Zuteil Methode',Updated=TO_TIMESTAMP('2017-08-18 15:21:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53503
+;
+
+-- 2017-08-18T15:21:52.286
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Zuteil Methode', PrintName='Zuteil Methode',Updated=TO_TIMESTAMP('2017-08-18 15:21:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=53253
+;
+
+-- 2017-08-18T15:21:52.289
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='IssueMethod', Name='Zuteil Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.' WHERE AD_Element_ID=53253
+;
+
+-- 2017-08-18T15:21:52.302
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IssueMethod', Name='Zuteil Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.', AD_Element_ID=53253 WHERE UPPER(ColumnName)='ISSUEMETHOD' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-08-18T15:21:52.304
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IssueMethod', Name='Zuteil Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.' WHERE AD_Element_ID=53253 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:21:52.306
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Zuteil Methode', Description='There are two methods for issue the components to Manufacturing Order', Help='Method Issue: The component are delivered one for one and is necessary indicate the delivered quantity for each component.
+
+Method BackFlush: The component are delivered based in BOM, The  delivered quantity for each component is based in BOM or Formula and Manufacturing Order Quantity.
+
+Use the field Backflush Group for grouping the component in a Backflush Method.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=53253) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:21:52.327
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Zuteil Methode', Name='Zuteil Methode' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=53253)
+;
+
+-- 2017-08-18T15:22:58.265
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Retrograde Gruppe', PrintName='Retrograde Gruppe',Updated=TO_TIMESTAMP('2017-08-18 15:22:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=53248
+;
+
+-- 2017-08-18T15:22:58.267
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='BackflushGroup', Name='Retrograde Gruppe', Description='The Grouping Components to the Backflush', Help='When the components are deliver is possible to indicated The Backflush Group this way you only can deliver the components that are for this Backflush Group.' WHERE AD_Element_ID=53248
+;
+
+-- 2017-08-18T15:22:58.278
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='BackflushGroup', Name='Retrograde Gruppe', Description='The Grouping Components to the Backflush', Help='When the components are deliver is possible to indicated The Backflush Group this way you only can deliver the components that are for this Backflush Group.', AD_Element_ID=53248 WHERE UPPER(ColumnName)='BACKFLUSHGROUP' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-08-18T15:22:58.279
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='BackflushGroup', Name='Retrograde Gruppe', Description='The Grouping Components to the Backflush', Help='When the components are deliver is possible to indicated The Backflush Group this way you only can deliver the components that are for this Backflush Group.' WHERE AD_Element_ID=53248 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:22:58.280
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Retrograde Gruppe', Description='The Grouping Components to the Backflush', Help='When the components are deliver is possible to indicated The Backflush Group this way you only can deliver the components that are for this Backflush Group.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=53248) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:22:58.291
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Retrograde Gruppe', Name='Retrograde Gruppe' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=53248)
+;
+
+-- 2017-08-18T15:24:03.085
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Mengen Probe',Updated=TO_TIMESTAMP('2017-08-18 15:24:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53501
+;
+
+-- 2017-08-18T15:24:17.540
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Mengen Probe', PrintName='Mengen Probe',Updated=TO_TIMESTAMP('2017-08-18 15:24:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=53247
+;
+
+-- 2017-08-18T15:24:17.541
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='Assay', Name='Mengen Probe', Description='Indicated the Quantity Assay to use into Quality Order', Help='Indicated the Quantity Assay to use into Quality Order' WHERE AD_Element_ID=53247
+;
+
+-- 2017-08-18T15:24:17.550
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='Assay', Name='Mengen Probe', Description='Indicated the Quantity Assay to use into Quality Order', Help='Indicated the Quantity Assay to use into Quality Order', AD_Element_ID=53247 WHERE UPPER(ColumnName)='ASSAY' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-08-18T15:24:17.552
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='Assay', Name='Mengen Probe', Description='Indicated the Quantity Assay to use into Quality Order', Help='Indicated the Quantity Assay to use into Quality Order' WHERE AD_Element_ID=53247 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:24:17.552
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Mengen Probe', Description='Indicated the Quantity Assay to use into Quality Order', Help='Indicated the Quantity Assay to use into Quality Order' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=53247) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:24:17.568
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Mengen Probe', Name='Mengen Probe' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=53247)
+;
+
+-- 2017-08-18T15:24:49.519
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='% Ausschuss (alt)',Updated=TO_TIMESTAMP('2017-08-18 15:24:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=555024
+;
+
+-- 2017-08-18T15:25:07.056
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Erwartetes Ergebnis',Updated=TO_TIMESTAMP('2017-08-18 15:25:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=555025
+;
+
+-- 2017-08-18T15:25:16.533
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Erwartetes Ergebnis', PrintName='Erwartetes Ergebnis',Updated=TO_TIMESTAMP('2017-08-18 15:25:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=542596
+;
+
+-- 2017-08-18T15:25:16.537
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='expectedResult', Name='Erwartetes Ergebnis', Description=NULL, Help=NULL WHERE AD_Element_ID=542596
+;
+
+-- 2017-08-18T15:25:16.547
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='expectedResult', Name='Erwartetes Ergebnis', Description=NULL, Help=NULL, AD_Element_ID=542596 WHERE UPPER(ColumnName)='EXPECTEDRESULT' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-08-18T15:25:16.548
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='expectedResult', Name='Erwartetes Ergebnis', Description=NULL, Help=NULL WHERE AD_Element_ID=542596 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:25:16.549
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Erwartetes Ergebnis', Description=NULL, Help=NULL WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=542596) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:25:16.560
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Erwartetes Ergebnis', Name='Erwartetes Ergebnis' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=542596)
+;
+
+-- 2017-08-18T15:25:38.623
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Varianten Gruppe',Updated=TO_TIMESTAMP('2017-08-18 15:25:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=553984
+;
+
+-- 2017-08-18T15:25:48.540
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element SET Name='Varianten Gruppe', PrintName='Varianten Gruppe',Updated=TO_TIMESTAMP('2017-08-18 15:25:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=542377
+;
+
+-- 2017-08-18T15:25:48.545
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='VariantGroup', Name='Varianten Gruppe', Description=NULL, Help=NULL WHERE AD_Element_ID=542377
+;
+
+-- 2017-08-18T15:25:48.559
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='VariantGroup', Name='Varianten Gruppe', Description=NULL, Help=NULL, AD_Element_ID=542377 WHERE UPPER(ColumnName)='VARIANTGROUP' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2017-08-18T15:25:48.561
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='VariantGroup', Name='Varianten Gruppe', Description=NULL, Help=NULL WHERE AD_Element_ID=542377 AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:25:48.562
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Varianten Gruppe', Description=NULL, Help=NULL WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=542377) AND IsCentrallyMaintained='Y'
+;
+
+-- 2017-08-18T15:25:48.577
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Varianten Gruppe', Name='Varianten Gruppe' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=542377)
+;
+
+-- 2017-08-18T15:27:06.031
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Kritische Komponente',Updated=TO_TIMESTAMP('2017-08-18 15:27:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53497
+;
+
