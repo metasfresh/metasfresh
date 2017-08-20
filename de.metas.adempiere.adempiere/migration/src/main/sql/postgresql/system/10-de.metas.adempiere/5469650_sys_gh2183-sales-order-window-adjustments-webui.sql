520371f9dffc87daf692571f9477c0c2bfa81d28
Mark Krake <mark.krake@metasfresh.com>
2017-08-20 12:21:29 +0200

    #2183 Adjustments of Enhanced Edit translating Fields to de_DE
    Adjustments of Enhanced Edit translating Fields to de_DE
    
    FRESH-3134 https://github.com/metasfresh/metasfresh/issues/2183

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469650_sys_gh2183-sales-order-window-adjustments-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469650_sys_gh2183-sales-order-window-adjustments-webui.sql
@@ -32,4 +32,54 @@ UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2017-08-20 11:32:12','YYY
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Field SET Name='Nr.',Updated=TO_TIMESTAMP('2017-08-20 11:33:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1081
 ;
+-- 2017-08-20T12:07:20.907
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Section SET Name='',Updated=TO_TIMESTAMP('2017-08-20 12:07:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Section_ID=540216
+;
+
+-- 2017-08-20T12:09:59.037
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Volumen',Updated=TO_TIMESTAMP('2017-08-20 12:09:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=502201
+;
+
+-- 2017-08-20T12:10:07.725
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Gewicht',Updated=TO_TIMESTAMP('2017-08-20 12:10:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=502202
+;
+
+-- 2017-08-20T12:12:59.347
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Zahlung Partner',Updated=TO_TIMESTAMP('2017-08-20 12:12:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=9428
+;
+
+-- 2017-08-20T12:13:19.818
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Zahlung Anschrift',Updated=TO_TIMESTAMP('2017-08-20 12:13:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=9427
+;
+
+-- 2017-08-20T12:13:52.935
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Kassen Journal Zeile',Updated=TO_TIMESTAMP('2017-08-20 12:13:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4233
+;
+
+-- 2017-08-20T12:15:49.448
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Werbecode',Updated=TO_TIMESTAMP('2017-08-20 12:15:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=56906
+;
+
+-- 2017-08-20T12:16:24.840
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Übertragen',Updated=TO_TIMESTAMP('2017-08-20 12:16:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1092
+;
+
+-- 2017-08-20T12:17:29.020
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Andrucken',Updated=TO_TIMESTAMP('2017-08-20 12:17:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1091
+;
+
+-- 2017-08-20T12:17:50.200
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum Gedruckt',Updated=TO_TIMESTAMP('2017-08-20 12:17:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2876
+;
+
 
