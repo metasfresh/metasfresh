56af35ab953bad7cfd761b16acf0b29b3f4e556a
Mark Krake <mark.krake@metasfresh.com>
2017-08-20 12:05:56 +0200

    #2183 Adopting the Sales Order window to current design Guidelines
    Adopting the Sales Order window to current design Guidelines.
    
    FRESH-3134 https://github.com/metasfresh/metasfresh/issues/2183

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469650_sys_gh2183-sales-order-window-adjustments-webui.sql
@@ -0,0 +1,35 @@
+-- 2017-08-20T11:25:10.603
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nr.',Updated=TO_TIMESTAMP('2017-08-20 11:25:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=544811
+;
+
+-- 2017-08-20T11:25:16.912
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2017-08-20 11:25:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=544813
+;
+
+-- 2017-08-20T11:25:27.917
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Status',Updated=TO_TIMESTAMP('2017-08-20 11:25:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=544812
+;
+
+-- 2017-08-20T11:31:39.717
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET Name='Datum',Updated=TO_TIMESTAMP('2017-08-20 11:31:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000008
+;
+
+-- 2017-08-20T11:31:58.206
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET Name='Nr.',Updated=TO_TIMESTAMP('2017-08-20 11:31:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=544767
+;
+
+-- 2017-08-20T11:32:12.334
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2017-08-20 11:32:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1093
+;
+
+-- 2017-08-20T11:33:40.009
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nr.',Updated=TO_TIMESTAMP('2017-08-20 11:33:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1081
+;
+
