7e59d1e83a7252dadf82dd6722c8fe67033a3d8d
metasnw <nw@metasfresh.com>
2017-08-25 09:42:21 +0200

    #2245 replace description with memo

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5469960_sys_gh2245_b2cpartner_replace_description_by_memo.sql
@@ -0,0 +1,5 @@
+-- 2017-08-25T09:28:54.725
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_Field_ID=559016, Name='memo',Updated=TO_TIMESTAMP('2017-08-25 09:28:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546977
+;
+
