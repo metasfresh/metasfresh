b3f9430819b592984f5c4eae94f399845ed1dd54
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-08-25 15:09:49 +0300

    #2144 Action for moving HUs to other warehouse
    https://github.com/metasfresh/metasfresh/issues/2144

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5469970_sys_gh2144_Action_MoveHuToAnotherWarehouse.sql
@@ -0,0 +1,15 @@
+-- 2017-08-25T14:40:56.399
+-- URL zum Konzept
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsOneInstanceOnly,IsReport,IsServerProcess,IsUseBPartnerLanguage,LockWaitTimeout,Name,RefreshAllAfterExecution,ShowHelp,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,540820,'Y','de.metas.ui.web.handlingunits.process.WEBUI_M_HU_MoveToAnotherWarehouse','N',TO_TIMESTAMP('2017-08-25 14:40:56','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.handlingunits','Y','N','N','N','N','N','N','Y',0,'MoveToAnotherWarehouse','N','Y','Java',TO_TIMESTAMP('2017-08-25 14:40:56','YYYY-MM-DD HH24:MI:SS'),100,'WEBUI_M_HU_MoveToAnotherWarehouse')
+;
+
+-- 2017-08-25T14:40:56.419
+-- URL zum Konzept
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_ID=540820 AND NOT EXISTS (SELECT 1 FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- 2017-08-25T14:50:43.786
+-- URL zum Konzept
+UPDATE AD_Process SET Name='Lagerbewegung',Updated=TO_TIMESTAMP('2017-08-25 14:50:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540820
+;
+
