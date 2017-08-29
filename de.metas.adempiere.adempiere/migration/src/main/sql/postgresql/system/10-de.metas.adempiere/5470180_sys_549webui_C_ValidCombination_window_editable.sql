ae0392bd1f8e4a026c3bb47dfff285645e1dd2ca
teosarca <teo.sarca@metasfresh.com>
2017-08-29 15:39:31 +0300

    make Valid Combination window editable
    i.e. change WindowType from QueryOnly to Maintain
    https://github.com/metasfresh/metasfresh-webui-api/issues/549

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5470180_sys_549webui_C_ValidCombination_window_editable.sql
@@ -0,0 +1,5 @@
+-- 2017-08-29T15:35:06.044
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET WindowType='M',Updated=TO_TIMESTAMP('2017-08-29 15:35:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=153
+;
+
