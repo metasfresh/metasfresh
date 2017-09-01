9215e3a2ebcf371d9bded8ffd6f1a28710e69c82
Teo Sarca <teo.sarca@metasfresh.com>
2017-09-01 17:21:57 +0300

    cleanup/QA IHUPackingMaterialCollectorSource
    https://github.com/metasfresh/metasfresh/issues/2143
    (cherry picked from commit f5c76da2cd31e312698e71e92e84a20f4b7ae01a)
    
    resolved Conflicts:
    	de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/VendorReturnsInOutProducer.java

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/VendorReturnsInOutProducer.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inout/impl/VendorReturnsInOutProducer.java
@@ -144,6 +144,7 @@ class VendorReturnsInOutProducer extends AbstractReturnsInOutProducer
 					.collectHUPipToSource(false)
 					.build();
 			
+			final InOutLineHUPackingMaterialCollectorSource inOutLineSource = InOutLineHUPackingMaterialCollectorSource.of(inOutLine);
 			collector.addHURecursively(hu, inOutLineSource);
 
 			// Create product (non-packing material) lines
