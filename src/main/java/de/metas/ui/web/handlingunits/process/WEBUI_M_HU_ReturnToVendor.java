32c28b36cf529427fa0726c566bf1e88785fe713
Teo Sarca <teo.sarca@metasfresh.com>
2017-09-05 12:57:15 +0300

    vendor/customer return shall take only the top level HUs
    https://github.com/metasfresh/metasfresh-webui-api/issues/572

--- src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_ReturnToVendor.java
+++ src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_ReturnToVendor.java
@@ -7,6 +7,9 @@ import java.util.Set;
 import org.adempiere.util.Services;
 import org.compiere.util.Env;
 
+import com.google.common.collect.ImmutableList;
+
+import de.metas.handlingunits.IHandlingUnitsBL;
 import de.metas.handlingunits.inout.IHUInOutBL;
 import de.metas.handlingunits.model.I_M_HU;
 import de.metas.process.IProcessPrecondition;
@@ -43,7 +46,9 @@ import de.metas.process.ProcessPreconditionsResolution;
 public class WEBUI_M_HU_ReturnToVendor extends HUEditorProcessTemplate implements IProcessPrecondition
 {
 	private static final String MSG_NoSelectedHU = "NoHUSelected";
-	
+
+	private final transient IHandlingUnitsBL handlingUnitsBL = Services.get(IHandlingUnitsBL.class);
+
 	private List<I_M_HU> husToReturn = null;
 
 	@Override
@@ -61,7 +66,11 @@ public class WEBUI_M_HU_ReturnToVendor extends HUEditorProcessTemplate implement
 	@Override
 	protected String doIt() throws Exception
 	{
-		husToReturn = getSelectedHUs();
+		husToReturn = getSelectedHUs()
+				.stream()
+				.filter(handlingUnitsBL::isTopLevel) // only top level HUs
+				.collect(ImmutableList.toImmutableList());
+		
 		final Timestamp movementDate = Env.getDate(getCtx());
 		Services.get(IHUInOutBL.class).createVendorReturnInOutForHUs(husToReturn, movementDate);
 		return MSG_OK;
