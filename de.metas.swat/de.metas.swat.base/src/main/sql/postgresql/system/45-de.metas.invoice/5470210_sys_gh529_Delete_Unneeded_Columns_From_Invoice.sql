5df6a5ca61c8e1e9c392473d513398be5bc90922
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-09-06 06:18:35 +0200

    Don't drop the physical columns, they might contain data
    Possibility to easily create more than 1 referenced Doc per Invoice https://github.com/metasfresh/metasfresh/issues/529

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.invoice/5470210_sys_gh529_Delete_Unneeded_Columns_From_Invoice.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.invoice/5470210_sys_gh529_Delete_Unneeded_Columns_From_Invoice.sql
@@ -18,7 +18,6 @@ DELETE FROM  AD_Column_Trl WHERE AD_Column_ID=504606
 DELETE FROM AD_Column WHERE AD_Column_ID=504606
 ;
 
-ALTER TABLE C_Invoice DROP COLUMN Ref_CreditMemo_ID;
 
 -- 2017-08-29T17:51:37.114
 -- URL zum Konzept
@@ -30,6 +29,8 @@ DELETE FROM  AD_Column_Trl WHERE AD_Column_ID=550244
 DELETE FROM AD_Column WHERE AD_Column_ID=550244
 ;
 
-ALTER TABLE C_Invoice DROP COLUMN Ref_AdjustmentCharge_ID;
+-- do not drop the physical columns. They might already be filled
+-- ALTER TABLE C_Invoice DROP COLUMN Ref_CreditMemo_ID;
+-- ALTER TABLE C_Invoice DROP COLUMN Ref_AdjustmentCharge_ID;
 
 
