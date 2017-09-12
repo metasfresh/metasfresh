4750e4b006e592f4641dcfa435aa0d56581139b9
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-09-12 10:52:17 +0300

    #2397 Fix format
    #2397 Import and Store Contract meta-data e.g. startdate on flatrate
    data

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5471290_sys_gh2397_AddContractStartDateEndDate.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5471290_sys_gh2397_AddContractStartDateEndDate.sql
@@ -28,37 +28,6 @@ UPDATE AD_Element SET EntityType='de.metas.flatrate',Updated=TO_TIMESTAMP('2017-
 UPDATE AD_Element SET EntityType='de.metas.flatrate',Updated=TO_TIMESTAMP('2017-09-11 13:10:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=543414
 ;
 
----------------------------------------------------------
-
-
--- 2017-09-11T13:41:54.982
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataFormat,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,557134,540010,540113,0,TO_TIMESTAMP('2017-09-11 13:41:54','YYYY-MM-DD HH24:MI:SS'),100,'yyyy-MM-dd','D','.','N',0,'Y','ContractStartDate_Contract Start Date',30,3,TO_TIMESTAMP('2017-09-11 13:41:54','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2017-09-11T13:42:07.617
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataFormat,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,557135,540010,540114,0,TO_TIMESTAMP('2017-09-11 13:42:07','YYYY-MM-DD HH24:MI:SS'),100,'yyyy-MM-dd','D','.','N',0,'Y','Contract End Date',30,3,TO_TIMESTAMP('2017-09-11 13:42:07','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2017-09-11T13:42:12.026
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_ImpFormat_Row SET Name='Contract Start Date',Updated=TO_TIMESTAMP('2017-09-11 13:42:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540113
-;
-
--- 2017-09-11T13:42:44.083
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_ImpFormat_Row SET SeqNo=80, StartNo=8,Updated=TO_TIMESTAMP('2017-09-11 13:42:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540113
-;
-
--- 2017-09-11T13:42:52.679
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_ImpFormat_Row SET SeqNo=90, StartNo=9,Updated=TO_TIMESTAMP('2017-09-11 13:42:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540114
-;
-
-
----------------------------------------------------------
-
 
 -- 2017-09-11T15:21:41.356
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
@@ -238,16 +207,16 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 /* DDL */ SELECT public.db_alter_table('i_flatrate_term','ALTER TABLE public.I_Flatrate_Term ADD COLUMN MasterEndDate TIMESTAMP WITHOUT TIME ZONE')
 ;
 
-
--- 2017-09-11T17:16:37.863
+-- 2017-09-12T10:50:26.311
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_ImpFormat_Row SET Name='Master Start Date',Updated=TO_TIMESTAMP('2017-09-11 17:16:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540113
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataFormat,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,557134,540010,540118,0,TO_TIMESTAMP('2017-09-12 10:50:26','YYYY-MM-DD HH24:MI:SS'),100,'yyyy-MM-dd','D','.','N',0,'Y','Master Start Date',80,8,TO_TIMESTAMP('2017-09-12 10:50:26','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2017-09-11T17:16:40.858
+-- 2017-09-12T10:50:38.299
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_ImpFormat_Row SET Name='Master End Date',Updated=TO_TIMESTAMP('2017-09-11 17:16:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540114
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataFormat,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,557135,540010,540119,0,TO_TIMESTAMP('2017-09-12 10:50:38','YYYY-MM-DD HH24:MI:SS'),100,'yyyy-MM-dd','D','.','N',0,'Y','Master End Date',90,9,TO_TIMESTAMP('2017-09-12 10:50:38','YYYY-MM-DD HH24:MI:SS'),100)
 ;
+
 
 -- 2017-09-11T17:22:53.280
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
