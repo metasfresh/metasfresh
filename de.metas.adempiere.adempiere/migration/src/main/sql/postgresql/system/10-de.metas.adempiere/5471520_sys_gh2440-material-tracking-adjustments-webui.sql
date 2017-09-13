79108acb1ad6719771ce01577cab14a393bd5be9
Mark Krake <mark.krake@metasfresh.com>
2017-09-13 09:08:11 +0200

    #2440 Adding new Filters to Material Tracking Window in WebUI
    Adding new Filters - Business Partner, Tracking terminated/ Processed -
    to Material Tracking Window in WebUI
    
    FRESH-3395 https://github.com/metasfresh/metasfresh/issues/2440

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5471520_sys_gh2440-material-tracking-adjustments-webui.sql
@@ -0,0 +1,10 @@
+-- 2017-09-13T08:23:40.982
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2017-09-13 08:23:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=551112
+;
+
+-- 2017-09-13T08:24:30.770
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2017-09-13 08:24:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=551115
+;
+
