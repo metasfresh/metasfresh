e61c9c52e46b6f06a09597859f55575d86881ed7
Pablo Sichert <mail@pablosichert.de>
2017-09-19 16:43:14 +0200

    Add <NewLetter> component

---
+++ src/actions/LetterActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export function createLetter(windowId, documentId) {
+    return axios.post(
+        config.API_URL + '/letter', {
+            'documentPath': {
+                'documentId': documentId,
+                'windowId': windowId
+            }
+        }
+    );
+}
+
+export function completeLetter(letterId) {
+    return axios.post(config.API_URL + '/letter/' + letterId + '/complete');
+}
+
+export function getTemplates() {
+    return axios.get(config.API_URL + '/letter/templates');
+}
---
+++ src/components/letter/NewLetter.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import counterpart from 'counterpart';
+
+import {
+    addNotification
+} from '../../actions/AppActions';
+
+import {
+    patchRequest
+} from '../../actions/GenericActions';
+
+import {
+    createLetter,
+    completeLetter,
+    getTemplates
+} from '../../actions/LetterActions';
+
+import RawList from '../widget/List/RawList'
+
+class NewLetter extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            init: false,
+            cached: {},
+            templates: [],
+            template: {}
+        }
+    }
+
+    componentWillMount = () => {
+        const { windowId, docId, handleCloseLetter } = this.props;
+
+        createLetter(windowId, docId).then(res => {
+            this.setState({
+                ...res.data,
+                init: true,
+                cached: res.data
+            })
+
+            this.getTemplates();
+        }).catch(() => {
+            handleCloseLetter();
+        });
+    }
+
+    getTemplates = () => {
+        getTemplates().then(res => {
+            this.setState({
+                templates: res.data.values
+            });
+        });
+    }
+
+    handleChange = ({ target: { value: message } }) => {
+        this.setState({
+            message
+        });
+    }
+
+    handleBlur = ({ target: { value: message } }) => {
+        const { letterId } = this.state;
+
+        if (this.state.cached.message === message) {
+            return;
+        }
+
+        (patchRequest('letter', letterId, null, null, null, 'message', message)
+            .then(res => {
+                this.setState({
+                    ...res.data,
+                    cached: res.data
+                });
+            })
+        );
+    }
+
+    complete = () => {
+        const { letterId } = this.state;
+        const { handleCloseLetter, dispatch } = this.props;
+
+        completeLetter(letterId).then(() => {
+            handleCloseLetter();
+
+            dispatch(addNotification(
+                'Letter', 'Letter has been sent.', 5000, 'success'
+            ));
+        });
+    }
+
+    handleTemplate = option => {
+        const { letterId, template } = this.state;
+
+        if (template === option) {
+            return;
+        }
+
+        (patchRequest('letter', letterId, null, null, null, 'templateId', option)
+            .then(res => {
+               this.setState({
+                   ...res.data,
+                    template: option
+                });
+            })
+        );
+    }
+
+    render() {
+        const { handleCloseLetter } = this.props;
+        const { init, message, templates, template } = this.state;
+
+        if (!init) {
+            return false;
+        }
+
+        return (
+            <div className="screen-freeze">
+                <div className="panel panel-modal panel-letter panel-modal-primary">
+                    <div className="panel-letter-header-wrapper">
+                        <div className="panel-letter-header panel-letter-header-top">
+                            <span className="letter-headline">
+                                {counterpart.translate('window.letter.new')}
+                            </span>
+                            <div className="letter-templates">
+                                {templates.length>0 && (
+                                    <RawList
+                                        rank="primary"
+                                        list={templates}
+                                        onSelect={option => this.handleTemplate(option)}
+                                        selected={template}
+                                    />
+                                )}
+                            </div>
+                            <div
+                                className="input-icon input-icon-lg icon-letter"
+                                onClick={handleCloseLetter}
+                            >
+                                <i className="meta-icon-close-1"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="panel-letter-body">
+                        <textarea
+                            value={message ? message : ''}
+                            onChange={this.handleChange}
+                            onBlur={this.handleBlur}
+                        />
+                    </div>
+                    <div className="panel-letter-footer">
+                        <button
+                            onClick={this.complete}
+                            className="btn btn-meta-success btn-sm btn-submit"
+                        >
+                            {counterpart.translate('window.letter.create')}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+NewLetter = connect()(NewLetter);
+
+export default NewLetter;
