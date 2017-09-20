83790249f33a6ede69d0ec85e0100d2591b0b9a6
Mark Krake <mark.krake@metasfresh.com>
2017-09-20 18:41:11 +0200

    #2509 Adding the Translation en_US for Picking Tray Window in WebUI
    Adding the Translation en_US for Picking Tray Window in WebUI
    
    FRESH-3459 https://github.com/metasfresh/metasfresh/issues/2509

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5472240_sys_gh2509-picking-windows-trl-webui.sql
@@ -0,0 +1,10 @@
+-- 2017-09-20T18:26:52.920
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-20 18:26:52','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Picking Tray',WEBUI_NameBrowse='Picking Tray' WHERE AD_Menu_ID=540949 AND AD_Language='en_US'
+;
+
+-- 2017-09-20T18:27:08.337
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-20 18:27:08','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Picking Tray' WHERE AD_Window_ID=540206 AND AD_Language='en_US'
+;
+
