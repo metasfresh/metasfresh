824dc9b09eae0c7e7f7ac1144cd5c3b9e4876a66
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-09-20 15:07:56 +0300

    #2338 Improve the fact acct reference target relation type
    https://github.com/metasfresh/metasfresh/issues/2338

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5472180_sys_gh2338_FactAcct_ReferenceTarget_RelTypei.sql
@@ -0,0 +1,5 @@
+-- 2017-09-20T15:05:57.280
+-- URL zum Konzept
+UPDATE AD_Ref_Table SET AD_Column_ReferenceTarget_ID=5103, IsReferenceTarget='Y',Updated=TO_TIMESTAMP('2017-09-20 15:05:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=540747
+;
+
