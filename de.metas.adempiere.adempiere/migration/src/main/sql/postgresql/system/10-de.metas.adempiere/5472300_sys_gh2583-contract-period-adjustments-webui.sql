dea9aa6fd63099742824878f292a000b9e4ea946
Mark Krake <mark.krake@metasfresh.com>
2017-09-21 14:12:44 +0200

    #2533 Adding minor Layout changes
    Adding minor Layout changes
    
    FRESH-3480 https://github.com/metasfresh/metasfresh/issues/2533

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5472300_sys_gh2583-contract-period-adjustments-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5472300_sys_gh2583-contract-period-adjustments-webui.sql
@@ -63,3 +63,38 @@ UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2017-09-21 13
 UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2017-09-21 13:55:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546139
 ;
 
+-- 2017-09-21T14:02:52.918
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,540442,541162,TO_TIMESTAMP('2017-09-21 14:02:52','YYYY-MM-DD HH24:MI:SS'),100,'Y','delivery',20,TO_TIMESTAMP('2017-09-21 14:02:52','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-09-21T14:03:13.656
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541162, SeqNo=10,Updated=TO_TIMESTAMP('2017-09-21 14:03:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546138
+;
+
+-- 2017-09-21T14:03:27.496
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541162, SeqNo=20,Updated=TO_TIMESTAMP('2017-09-21 14:03:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546139
+;
+
+-- 2017-09-21T14:05:27.237
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Abointervall',Updated=TO_TIMESTAMP('2017-09-21 14:05:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=550521
+;
+
+-- 2017-09-21T14:05:32.236
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Aboeinheit',Updated=TO_TIMESTAMP('2017-09-21 14:05:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=550522
+;
+
+-- 2017-09-21T14:05:55.113
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-21 14:05:55','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Subscription Interval' WHERE AD_Field_ID=550521 AND AD_Language='en_US'
+;
+
+-- 2017-09-21T14:06:29.063
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-21 14:06:29','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Subscription Unit' WHERE AD_Field_ID=550522 AND AD_Language='en_US'
+;
+
