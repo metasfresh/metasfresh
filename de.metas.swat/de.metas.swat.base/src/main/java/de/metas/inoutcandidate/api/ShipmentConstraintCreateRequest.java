ff707ceba2b72c439db3342a40a83a9eb63cc85c
Teo Sarca <teo.sarca@metasfresh.com>
2017-09-26 13:55:27 +0300

    M_Shipment_Constraint prototype
    https://github.com/metasfresh/metasfresh/issues/2499

---
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/api/ShipmentConstraintCreateRequest.java
@@ -0,0 +1,54 @@
+package de.metas.inoutcandidate.api;
+
+import org.adempiere.util.lang.impl.TableRecordReference;
+
+import com.google.common.base.Preconditions;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+/*
+ * #%L
+ * de.metas.swat.base
+ * %%
+ * Copyright (C) 2017 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+@Value
+public class ShipmentConstraintCreateRequest
+{
+	private final int bpartnerId;
+	private final TableRecordReference sourceDocRef;
+	
+	private final boolean deliveryStop;
+
+	@Builder
+	public ShipmentConstraintCreateRequest(final int bpartnerId, @NonNull final TableRecordReference sourceDocRef, final boolean deliveryStop)
+	{
+		Preconditions.checkArgument(bpartnerId > 0, "bpartnerId > 0");
+		Preconditions.checkArgument(!deliveryStop, "at least one constraint shall be set");
+		
+		this.bpartnerId = bpartnerId;
+		this.sourceDocRef = sourceDocRef;
+		this.deliveryStop = deliveryStop;
+	}
+	
+	
+	 
+}
