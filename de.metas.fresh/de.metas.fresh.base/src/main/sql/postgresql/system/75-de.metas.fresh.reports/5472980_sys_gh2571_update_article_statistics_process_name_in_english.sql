057fc9c28be0d81b589e327959267655ecd7925a
metas-lc <liana.cotuna@metasfresh.com>
2017-09-27 16:28:10 +0300

    #2571 translate article statistics process
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5472980_sys_gh2571_update_article_statistics_process_name_in_english.sql
@@ -0,0 +1,22 @@
+﻿-- 2017-09-27T15:31:58.199
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 15:31:58','YYYY-MM-DD HH24:MI:SS'),Name='Article Statistics' WHERE AD_Process_ID=540563 AND AD_Language='en_US'
+;
+
+
+
+-- 2017-09-27T16:08:43.396
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 16:08:43','YYYY-MM-DD HH24:MI:SS'),Name='Article Statistics' WHERE AD_Menu_ID=540928 AND AD_Language='en_US'
+;
+
+-- 2017-09-27T16:09:23.739
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 16:09:23','YYYY-MM-DD HH24:MI:SS'),Name='Article Statistics' WHERE AD_Menu_ID=540618 AND AD_Language='en_US'
+;
+
+-- 2017-09-27T16:09:49.850
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 16:09:49','YYYY-MM-DD HH24:MI:SS'),WEBUI_NameBrowse='Article Statistics' WHERE AD_Menu_ID=540928 AND AD_Language='en_US'
+;
+
