582968a5cc0fc36bd55420cb8dd530b94b02509d
Mark Krake <mark.krake@metasfresh.com>
2017-09-27 15:33:07 +0200

    #2271 Adding primary Background to Sales Invoice Window
    Adding primary Background to Sales Invoice Window
    
    FRESH-3514 https://github.com/metasfresh/metasfresh/issues/2271

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5473010_sys_gh2271-sales-invoice-layout-improvements-webui.sql
@@ -0,0 +1,25 @@
+-- 2017-09-27T15:25:36.057
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2017-09-27 15:25:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540023
+;
+
+-- 2017-09-27T15:25:48.123
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2017-09-27 15:25:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540024
+;
+
+-- 2017-09-27T15:26:02.904
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2017-09-27 15:26:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540025
+;
+
+-- 2017-09-27T15:26:43.569
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2017-09-27 15:26:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540026
+;
+
+-- 2017-09-27T15:28:03.587
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Rechnungs Steuer',Updated=TO_TIMESTAMP('2017-09-27 15:28:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=271
+;
+
