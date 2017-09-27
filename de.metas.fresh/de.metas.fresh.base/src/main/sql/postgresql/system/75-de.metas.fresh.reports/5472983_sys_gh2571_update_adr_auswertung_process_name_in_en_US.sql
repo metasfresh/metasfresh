94b0e6a73adbd02916b5f20a44562c1542e5bf97
metas-lc <liana.cotuna@metasfresh.com>
2017-09-27 18:41:28 +0300

    #2571 translate umsatzreport adr bpartner
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5472983_sys_gh2571_update_adr_auswertung_process_name_in_en_US.sql
@@ -0,0 +1,15 @@
+﻿-- 2017-09-27T18:30:11.562
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 18:30:11','YYYY-MM-DD HH24:MI:SS'),Name='ADR Evaluation' WHERE AD_Process_ID=540655 AND AD_Language='en_US'
+;
+
+-- 2017-09-27T18:30:43.883
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 18:30:43','YYYY-MM-DD HH24:MI:SS'),Name='ADR Evaluation',WEBUI_NameBrowse='ADR Evaluation' WHERE AD_Menu_ID=540930 AND AD_Language='en_US'
+;
+
+-- 2017-09-27T18:30:58.514
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 18:30:58','YYYY-MM-DD HH24:MI:SS'),Name='ADR Evaluation' WHERE AD_Menu_ID=540681 AND AD_Language='en_US'
+;
+
