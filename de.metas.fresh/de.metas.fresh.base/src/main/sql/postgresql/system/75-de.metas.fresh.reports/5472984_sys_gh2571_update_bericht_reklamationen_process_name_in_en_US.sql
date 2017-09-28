b953c2d8310076c16ea1a0a4582fa5c7a44defaf
metas-lc <liana.cotuna@metasfresh.com>
2017-09-28 11:35:17 +0300

    #2571 translate request report
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5472984_sys_gh2571_update_bericht_reklamationen_process_name_in_en_US.sql
@@ -0,0 +1,15 @@
+﻿-- 2017-09-27T19:06:06.394
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 19:06:06','YYYY-MM-DD HH24:MI:SS'),Name='Complaints Report' WHERE AD_Process_ID=540723 AND AD_Language='en_US'
+;
+
+-- 2017-09-27T19:06:36.219
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 19:06:36','YYYY-MM-DD HH24:MI:SS'),Name='Complaints Report',WEBUI_NameBrowse='Complaints Report' WHERE AD_Menu_ID=540933 AND AD_Language='en_US'
+;
+
+-- 2017-09-27T19:06:50.154
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-27 19:06:50','YYYY-MM-DD HH24:MI:SS'),Name='Complaints Report' WHERE AD_Menu_ID=540725 AND AD_Language='en_US'
+;
+
--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5472985_sys_gh2571_make_request_report_function_translatable.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5472985_sys_gh2571_make_request_report_function_translatable.sql
@@ -1,4 +1,4 @@
---DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.request_report(IN ad_org_id numeric, IN R_RequestType_ID numeric, IN StartDate Date, IN EndDate Date, IN C_BPartner_ID numeric, IN QualityNote numeric, IN performanceType character varying(50), IN isMaterialReturned character(1),IN R_Resolution_ID numeric, IN R_Status_ID numeric, IN M_Product_ID numeric);
+﻿--DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.request_report(IN ad_org_id numeric, IN R_RequestType_ID numeric, IN StartDate Date, IN EndDate Date, IN C_BPartner_ID numeric, IN QualityNote numeric, IN performanceType character varying(50), IN isMaterialReturned character(1),IN R_Resolution_ID numeric, IN R_Status_ID numeric, IN M_Product_ID numeric);
 
 DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.request_report(IN ad_org_id numeric, IN R_RequestType_ID numeric, IN StartDate Date, IN EndDate Date, IN C_BPartner_ID numeric, IN QualityNote numeric, IN performanceType character varying(50), IN isMaterialReturned character(1), IN R_Status_ID numeric, IN M_Product_ID numeric);
 
