414436844c345760a0ba1dbf37ed3f9187b86aae
metas-lc <liana.cotuna@metasfresh.com>
2017-09-28 15:01:12 +0300

    #2571 translate umsatzreport week process

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5472987_sys_gh2571_translate_umsatzreport_woche_process_en_US.sql
@@ -0,0 +1,15 @@
+﻿-- 2017-09-28T14:56:14.590
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-28 14:56:14','YYYY-MM-DD HH24:MI:SS'),Name='Revenue Week Report' WHERE AD_Process_ID=540754 AND AD_Language='en_US'
+;
+
+-- 2017-09-28T14:56:40.174
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-28 14:56:40','YYYY-MM-DD HH24:MI:SS'),Name='Revenue Week Report',WEBUI_NameBrowse='Revenue Week Report' WHERE AD_Menu_ID=540927 AND AD_Language='en_US'
+;
+
+-- 2017-09-28T14:56:58.649
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-09-28 14:56:58','YYYY-MM-DD HH24:MI:SS'),Name='Revenue Week Report' WHERE AD_Menu_ID=540750 AND AD_Language='en_US'
+;
+
