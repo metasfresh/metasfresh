2d107e5f7b74b31a4a501c823318e761da7ed567
metas-lc <liana.cotuna@metasfresh.com>
2017-10-02 19:33:46 +0300

    #2571 translate all procurements conditions
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5473267_sys_gh2571_translate_procurement_conditions_processes.sql
@@ -0,0 +1,30 @@
+﻿-- 2017-10-02T19:29:23.046
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-02 19:29:23','YYYY-MM-DD HH24:MI:SS'),Name='Procurement Conditions Report' WHERE AD_Process_ID=540674 AND AD_Language='en_US'
+;
+
+-- 2017-10-02T19:29:38.525
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-02 19:29:38','YYYY-MM-DD HH24:MI:SS'),Name='Procurement Conditions Excel' WHERE AD_Process_ID=540676 AND AD_Language='en_US'
+;
+
+-- 2017-10-02T19:30:50.190
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-02 19:30:50','YYYY-MM-DD HH24:MI:SS'),Name='Procurement Conditions Report',WEBUI_NameBrowse='Procurement Conditions Report' WHERE AD_Menu_ID=540934 AND AD_Language='en_US'
+;
+
+-- 2017-10-02T19:31:06.860
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-02 19:31:06','YYYY-MM-DD HH24:MI:SS'),Name='Procurement Conditions Excel',WEBUI_NameBrowse='Procurement Conditions Excel' WHERE AD_Menu_ID=540935 AND AD_Language='en_US'
+;
+
+-- 2017-10-02T19:31:26.757
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-02 19:31:26','YYYY-MM-DD HH24:MI:SS'),Name='Procurement Conditions Excel' WHERE AD_Menu_ID=540706 AND AD_Language='en_US'
+;
+
+-- 2017-10-02T19:31:35.205
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-02 19:31:35','YYYY-MM-DD HH24:MI:SS'),Name='Procurement Conditions Report' WHERE AD_Menu_ID=540701 AND AD_Language='en_US'
+;
+
