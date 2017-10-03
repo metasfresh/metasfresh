6595adf04f023fae65dd891acdf130e51845b77c
metas-lc <liana.cotuna@metasfresh.com>
2017-10-03 16:06:10 +0300

    #2571 translate bank statement extended
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5473482_sys_gh2571_translate_bank_statement_fibu_process.sql
@@ -0,0 +1,30 @@
+﻿-- 2017-10-03T15:57:07.970
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-03 15:57:07','YYYY-MM-DD HH24:MI:SS'),Name='Bank Statement (Accounting)',Description='Bank Statement (Accounting)' WHERE AD_Process_ID=540595 AND AD_Language='en_US'
+;
+
+-- 2017-10-03T15:57:16.301
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-03 15:57:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Process_ID=540595 AND AD_Language='en_US'
+;
+
+-- 2017-10-03T16:01:09.315
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-03 16:01:09','YYYY-MM-DD HH24:MI:SS'),Name='Bank Statement (Accounting)',WEBUI_NameBrowse='Bank Statement (Accounting)' WHERE AD_Menu_ID=540945 AND AD_Language='en_US'
+;
+
+-- 2017-10-03T16:01:22.532
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-03 16:01:22','YYYY-MM-DD HH24:MI:SS'),Name='Bank Statement (Accounting)' WHERE AD_Menu_ID=540638 AND AD_Language='en_US'
+;
+
+-- 2017-10-03T16:01:57.346
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-03 16:01:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=540638 AND AD_Language='en_US'
+;
+
+-- 2017-10-03T16:02:09.428
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-03 16:02:09','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=540945 AND AD_Language='en_US'
+;
+
