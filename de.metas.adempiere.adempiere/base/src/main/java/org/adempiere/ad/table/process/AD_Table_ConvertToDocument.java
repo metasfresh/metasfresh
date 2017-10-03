a28d385ba970f87b7c978a01af86fe8e276d6ccb
Teo Sarca <teo.sarca@metasfresh.com>
2017-10-03 17:46:50 +0300

    create DB columns when convering a table to document
    https://github.com/metasfresh/metasfresh/issues/2432

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/table/process/AD_Table_ConvertToDocument.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/table/process/AD_Table_ConvertToDocument.java
@@ -2,8 +2,8 @@ package org.adempiere.ad.table.process;
 
 import org.adempiere.ad.table.api.IADTableDAO;
 import org.adempiere.ad.table.api.impl.CopyColumnsProducer;
-import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
+import org.compiere.model.I_AD_Process;
 import org.compiere.model.I_AD_Table;
 
 import de.metas.process.IADProcessBL;
@@ -19,12 +19,12 @@ import de.metas.process.JavaProcess;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -37,7 +37,7 @@ import de.metas.process.JavaProcess;
  * AD_Table_Document_Template is only defined in the Application dictionary, it is not a real table in the database (at least yet).
  * If the definition of a document table is to change (it needs extra columns), change AD_Table_Document_Template directly and run this process again for each document table that is to be extended.
  * Except from the extra columns, the document table must also have its own AD_Workflow which is specified in an AD_Process, linked with DocAction and Processing columns.
- * 
+ *
  * @author metas-dev <dev@metasfresh.com>
  *
  */
@@ -46,31 +46,25 @@ public class AD_Table_ConvertToDocument extends JavaProcess
 	private static transient IADTableDAO tableDAO = Services.get(IADTableDAO.class);
 	private static transient IADProcessBL processBL = Services.get(IADProcessBL.class);
 
-	private I_AD_Table document;
-
-	protected void prepare()
-	{
-		document = getRecord(I_AD_Table.class);
-	}
-
 	@Override
-	protected String doIt() throws Exception
+	protected String doIt()
 	{
-		// Create the document specific columns and add them to the document
-		final CopyColumnsProducer producer = new CopyColumnsProducer(this);
-		producer.setLogger(this);
-		producer.setTargetTable(document);
+		final I_AD_Table documentTable = getRecord(I_AD_Table.class);
 
 		// Get the document table template
-		final I_AD_Table sourceTable = tableDAO.retrieveDocumentTableTemplate(document);
-		producer.setSourceColumns(tableDAO.retrieveColumnsForTable(sourceTable));
-		producer.create();
+		final I_AD_Table documentTemplateTable = tableDAO.retrieveDocumentTableTemplate(documentTable);
 
-		InterfaceWrapperHelper.save(document);
+		// Create the document specific columns and add them to the document
+		CopyColumnsProducer.newInstance()
+				.setLogger(this)
+				.setTargetTable(documentTable)
+				.setSourceColumns(tableDAO.retrieveColumnsForTable(documentTemplateTable))
+				.setSyncToDatabase(true)
+				.create();
 
-		processBL.createAndLinkDocumentSpecificProcess(document);
+		final I_AD_Process adProcess = processBL.createAndLinkDocumentSpecificProcess(documentTable);
+		addLog("@Created@ @AD_Process_ID@: {}", adProcess);
 
-		return "@Created@ #" + producer.getCountCreated();
+		return MSG_OK;
 	}
-
 }
