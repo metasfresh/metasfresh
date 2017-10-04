7c7b7bfa485d02d8d717e31c3ad9a188aea0bbcb
Mark Krake <mark.krake@metasfresh.com>
2017-10-04 11:35:08 +0200

    #2632 Adding Translations for en_US in Tour Version Window
    Adding Translations for en_US Language/ Locale in Tour Version Window
    
    FRESH-3563 https://github.com/metasfresh/metasfresh/issues/2632

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5473620_sys_gh2632-tourversion-trl-webui.sql
@@ -0,0 +1,45 @@
+-- 2017-10-04T11:27:37.093
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:27:37','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Tab_ID=540800 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:28:21.150
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:28:21','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Monday',Description='Monday''s available' WHERE AD_Field_ID=558087 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:28:42.185
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:28:42','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Tuesday',Description='Tuesday''s available' WHERE AD_Field_ID=558089 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:29:01.231
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:29:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Wednesday',Description='Wednesday''s available' WHERE AD_Field_ID=558091 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:29:20.047
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:29:20','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Thursday',Description='Thursday''s available' WHERE AD_Field_ID=558093 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:29:35.137
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:29:35','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Friday',Description='Friday''s available' WHERE AD_Field_ID=558095 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:29:54.343
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:29:54','YYYY-MM-DD HH24:MI:SS'),Name='Saturday',Description='Saturday''s available' WHERE AD_Field_ID=558097 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:30:09.415
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:30:09','YYYY-MM-DD HH24:MI:SS'),Name='Sunday',Description='Sunday''s available' WHERE AD_Field_ID=558099 AND AD_Language='en_US'
+;
+
+-- 2017-10-04T11:31:17.196
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-04 11:31:17','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Valid from',Description='Valid from (incl. first Day)',Help='' WHERE AD_Field_ID=558082 AND AD_Language='en_US'
+;
+
