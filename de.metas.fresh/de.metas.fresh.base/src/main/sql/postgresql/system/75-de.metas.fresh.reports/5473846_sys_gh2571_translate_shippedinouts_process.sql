058904f4861d99d15ce2eb773b72060169684e70
metas-lc <liana.cotuna@metasfresh.com>
2017-10-09 18:07:45 +0300

    #2571 translate shipped inouts
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5473846_sys_gh2571_translate_shippedinouts_process.sql
@@ -0,0 +1,15 @@
+﻿-- 2017-10-09T17:45:39.303
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 17:45:39','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Picking List',Description='Picking List' WHERE AD_Process_ID=540530 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T17:45:58.337
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 17:45:58','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Picking List',Description='Picking List',WEBUI_NameBrowse='Picking List' WHERE AD_Menu_ID=540963 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T17:46:11.675
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 17:46:11','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Picking List',Description='Picking List' WHERE AD_Menu_ID=540582 AND AD_Language='en_US'
+;
+
