1739038153a1d27a384dd09417f9401ebf5929a0
Mark Krake <mark.krake@metasfresh.com>
2017-10-09 08:57:57 +0200

    #2636 Adding Translations to Pick List Process
    Adding Translations for en_US Language/ Locale to Pick List Process
    
    FRESH-3567 https://github.com/metasfresh/metasfresh/issues/2636

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5473970_sys_gh2636-picklist-process-trl.sql
@@ -0,0 +1,35 @@
+-- 2017-10-09T08:53:30.664
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 08:53:30','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Pick List',Description='Pick List',WEBUI_NameBrowse='Pick List' WHERE AD_Menu_ID=540963 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T08:53:52.495
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 08:53:52','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Pick List',Description='Pick List' WHERE AD_Process_ID=540530 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T08:54:16.827
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 08:54:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Business Partner',Description='',Help='' WHERE AD_Process_Para_ID=540629 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T08:54:28.758
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 08:54:28','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Location',Description='',Help='' WHERE AD_Process_Para_ID=540630 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T08:54:43.348
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 08:54:43','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Date',Description='Shipment Date',Help='' WHERE AD_Process_Para_ID=540631 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T08:55:01.459
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-09 08:55:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Group by Reference',Description='',Help='' WHERE AD_Process_Para_ID=540637 AND AD_Language='en_US'
+;
+
+-- 2017-10-09T08:55:36.174
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET Name='Nach Referenz gruppieren',Updated=TO_TIMESTAMP('2017-10-09 08:55:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=540637
+;
+
