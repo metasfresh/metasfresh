8dea23077aed62807354b1fbfe520e6ac8338114
Mark Krake <mark.krake@metasfresh.com>
2017-10-12 21:09:54 +0200

    #2603 Adding Translations for de_DE
    Adding Translations for de_DE
    
    FRESH-3531 https://github.com/metasfresh/metasfresh/issues/2603

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5474330_sys_gh2603-purchasecand-trl.sql
@@ -0,0 +1,25 @@
+-- 2017-10-12T21:06:04.233
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Bestellmenge (TU)',Updated=TO_TIMESTAMP('2017-10-12 21:06:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556859
+;
+
+-- 2017-10-12T21:06:11.857
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Bestellmenge',Updated=TO_TIMESTAMP('2017-10-12 21:06:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556659
+;
+
+-- 2017-10-12T21:06:25.464
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Letzter Beschaffungs Event',Updated=TO_TIMESTAMP('2017-10-12 21:06:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556686
+;
+
+-- 2017-10-12T21:06:41.094
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Trend (n. Woche)',Updated=TO_TIMESTAMP('2017-10-12 21:06:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556799
+;
+
+-- 2017-10-12T21:07:36.575
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='In Verarbeitung',Updated=TO_TIMESTAMP('2017-10-12 21:07:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556668
+;
+
