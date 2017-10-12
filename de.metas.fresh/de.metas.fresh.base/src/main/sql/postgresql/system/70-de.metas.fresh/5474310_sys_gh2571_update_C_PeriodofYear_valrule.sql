7aae18a7bb5262f5dc150ff116ecd7c88331ef0a
metas-lc <liana.cotuna@metasfresh.com>
2017-10-12 18:45:46 +0300

    #2571 update period of year valrule
    https://github.com/metasfresh/metasfresh/issues/2571

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5474310_sys_gh2571_update_C_PeriodofYear_valrule.sql
@@ -0,0 +1,7 @@
+﻿
+
+-- 2017-10-12T18:30:19.294
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Val_Rule SET Code='(@C_Year_ID/-1@ = -1 OR C_Period.C_Year_ID=@C_Year_ID/-1@)',Updated=TO_TIMESTAMP('2017-10-12 18:30:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=199
+;
+
