4dd19edf61892c6b8c1c51c165c331aa4ef77ed9
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2017-10-12 12:48:44 +0200

    add comment #2400

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/adempiere/service/impl/InvoiceLineBL.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/adempiere/service/impl/InvoiceLineBL.java
@@ -396,8 +396,9 @@ public class InvoiceLineBL implements IInvoiceLineBL
 		invoiceLine.setPriceEntered(pricingResult.getPriceStd());
 		invoiceLine.setPriceActual(pricingResult.getPriceStd());
 
-		//
-		// Discount
+		// Issue https://github.com/metasfresh/metasfresh/issues/2400:
+		// If the line has a discout, we assome it was manually added and stick with it
+		// When invoices are created by the system, there is no need to change an already-set discound (and this code is executed only once anyways)
 		if (invoiceLine.getDiscount().signum() == 0)   
 		{
 			invoiceLine.setDiscount(pricingResult.getDiscount());
