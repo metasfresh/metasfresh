3e324a0da8eb85b1bcb0efae876fdbd8d726e2c6
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-10-13 06:24:13 +0200

    PRevent SQL rollout problem
    i can't see why, but we got this rollout-error in one DB:
    
    duplicate key value violates unique constraint "m_iolcandhandler_unique_classname"
    DETAIL:  Key (classname, ad_client_id)=(de.metas.order.inoutcandidate.OrderLineShipmentScheduleHandler, 0) already exists.
    
    The change shall prevent it in further DBs

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5473460_sys_gh2623-app_move_and_rename_OrderLineInOutCandHandler.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5473460_sys_gh2623-app_move_and_rename_OrderLineInOutCandHandler.sql
@@ -1,4 +1,11 @@
 --
 -- OrderLineShipmentScheduleHandler
 -- 
-UPDATE M_IolCandHandler SET ClassName='de.metas.order.inoutcandidate.OrderLineShipmentScheduleHandler' WHERE ClassName='de.metas.inoutcandidate.spi.impl.OrderLineInOutCandHandler';
+-- only rename the old one if the new one doesn't already somehow exist
+UPDATE M_IolCandHandler 
+SET ClassName='de.metas.order.inoutcandidate.OrderLineShipmentScheduleHandler' 
+WHERE ClassName='de.metas.inoutcandidate.spi.impl.OrderLineInOutCandHandler'
+	AND NOT EXists (select 1 from M_IolCandHandler WHERE ClassName='de.metas.order.inoutcandidate.OrderLineShipmentScheduleHandler');
+
+-- deactivate the old one if it still exists
+UPDATE M_IolCandHandler SET IsActive='N' WHERE ClassName='de.metas.inoutcandidate.spi.impl.OrderLineInOutCandHandler';
