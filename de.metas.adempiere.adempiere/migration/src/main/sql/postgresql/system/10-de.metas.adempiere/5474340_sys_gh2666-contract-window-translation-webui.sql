6f89a3010c8ed14b12a46f68e0be776d2d700067
Mark Krake <mark.krake@metasfresh.com>
2017-10-13 18:25:42 +0200

    #2666 Adding missing Translations in Contract Window for en_US
    Adding missing Translations in Contract Window for en_US
    
    FRESH-3604 https://github.com/metasfresh/metasfresh/issues/2666

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5474340_sys_gh2666-contract-window-translation-webui.sql
@@ -0,0 +1,25 @@
+-- 2017-10-13T18:07:13.676
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-13 18:07:13','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Contract' WHERE AD_Window_ID=540359 AND AD_Language='en_US'
+;
+
+-- 2017-10-13T18:07:25.014
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-13 18:07:25','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Contract' WHERE AD_Tab_ID=540859 AND AD_Language='en_US'
+;
+
+-- 2017-10-13T18:10:26.864
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-13 18:10:26','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Shipto Partner',Description='' WHERE AD_Field_ID=559774 AND AD_Language='en_US'
+;
+
+-- 2017-10-13T18:10:46.482
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-13 18:10:46','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Price',Description='',Help='' WHERE AD_Field_ID=559765 AND AD_Language='en_US'
+;
+
+-- 2017-10-13T18:11:11.337
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-13 18:11:11','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Planned Qty per UOM',Description='' WHERE AD_Field_ID=559781 AND AD_Language='en_US'
+;
+
