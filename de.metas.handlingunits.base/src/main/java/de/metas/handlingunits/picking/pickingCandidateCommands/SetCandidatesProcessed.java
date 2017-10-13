60973f6a3ec798dddc68e4b93a1ddb918a228d3f
Teo Sarca <teo.sarca@metasfresh.com>
2017-10-13 10:59:04 +0300

    PickingCandidateService.setCandidatesProcessed: add OptionalInt shipmentScheduleId
    https://github.com/metasfresh/metasfresh/issues/472

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/pickingCandidateCommands/SetCandidatesProcessed.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/pickingCandidateCommands/SetCandidatesProcessed.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalInt;
 
 import org.adempiere.ad.dao.ICompositeQueryUpdater;
 import org.adempiere.ad.dao.IQueryBL;
@@ -72,38 +73,38 @@ public class SetCandidatesProcessed
 
 	}
 
-	public int perform(@NonNull final List<Integer> huIds)
+	public int perform(@NonNull final List<Integer> huIds, final int pickingSlotId, final OptionalInt shipmentScheduleId)
 	{
 		if (huIds.isEmpty())
 		{
 			return 0;
 		}
 
-		final Collection<I_M_HU> sourceHUs = sourceHUsRepository.retrieveActualSourceHUs(huIds);
-		destroyEmptySourceHUs(sourceHUs);
+		allocateHUsToShipmentSchedule(huIds, pickingSlotId, shipmentScheduleId);
 
-		return extracted(huIds);
+		destroyEmptySourceHUs(huIds);
+
+		return markCandidatesAsProcessed(huIds);
 	}
 
-	private int extracted(final List<Integer> huIds)
+	private void allocateHUsToShipmentSchedule(@NonNull final List<Integer> huIds, final int pickingSlotId, final OptionalInt shipmentScheduleId)
 	{
-		final IQueryBL queryBL = Services.get(IQueryBL.class);
+		if (huIds.isEmpty())
+		{
+			return;
+		}
 
-		final IQuery<I_M_Picking_Candidate> query = queryBL.createQueryBuilder(I_M_Picking_Candidate.class)
-				.addOnlyActiveRecordsFilter()
-				.addEqualsFilter(I_M_Picking_Candidate.COLUMNNAME_Status, X_M_Picking_Candidate.STATUS_IP)
-				.addInArrayFilter(I_M_Picking_Candidate.COLUMNNAME_M_HU_ID, huIds)
-				.create();
-
-		final ICompositeQueryUpdater<I_M_Picking_Candidate> updater = queryBL.createCompositeQueryUpdater(I_M_Picking_Candidate.class)
-				.addSetColumnValue(I_M_Picking_Candidate.COLUMNNAME_Status, X_M_Picking_Candidate.STATUS_PR);
-
-		return query.updateDirectly(updater);
+		Services.get(IQueryBL.class)
+				.createQueryBuilderOutOfTrx(I_M_HU.class) // HU2PackingItemsAllocator wants them to be out of trx
+				.addInArrayFilter(I_M_HU.COLUMN_M_HU_ID, huIds)
+				.create()
+				.list(I_M_HU.class)
+				.forEach(hu -> allocateHUToShipmentSchedule(hu, pickingSlotId, shipmentScheduleId));
 	}
 
-	private void allocateHuToShipmentSchedule(final int pickingSlotId, @NonNull final I_M_HU hu)
+	private void allocateHUToShipmentSchedule(@NonNull final I_M_HU hu, final int pickingSlotId, final OptionalInt shipmentScheduleId)
 	{
-		final IFreshPackingItem itemToPack = createItemToPack(hu);
+		final IFreshPackingItem itemToPack = createItemToPack(hu, shipmentScheduleId);
 
 		final PackingItemsMap packingItemsMap = new PackingItemsMap();
 		packingItemsMap.addUnpackedItem(itemToPack);
@@ -121,13 +122,18 @@ public class SetCandidatesProcessed
 				.allocate();
 	}
 
-	private IFreshPackingItem createItemToPack(@NonNull final I_M_HU hu)
+	private IFreshPackingItem createItemToPack(@NonNull final I_M_HU hu, final OptionalInt shipmentScheduleId)
 	{
 		final Map<I_M_ShipmentSchedule, BigDecimal> scheds2Qtys = new HashMap<>();
 
 		final List<I_M_Picking_Candidate> pickingCandidates = pickingCandidateRepository.retrievePickingCandidates(hu.getM_HU_ID());
 		for (final I_M_Picking_Candidate pc : pickingCandidates)
 		{
+			if(shipmentScheduleId.isPresent() && shipmentScheduleId.getAsInt() != pc.getM_ShipmentSchedule_ID())
+			{
+				continue;
+			}
+			
 			final I_M_ShipmentSchedule shipmentSchedule = pc.getM_ShipmentSchedule();
 			final BigDecimal qty = pc.getQtyPicked();
 			scheds2Qtys.put(shipmentSchedule, qty);
@@ -137,31 +143,48 @@ public class SetCandidatesProcessed
 		return itemToPack;
 	}
 
-	private void destroyEmptySourceHUs(@NonNull final Collection<I_M_HU> sourceHus)
+	private void destroyEmptySourceHUs(@NonNull final List<Integer> huIds)
 	{
+		final Collection<I_M_HU> sourceHUs = sourceHUsRepository.retrieveActualSourceHUs(huIds);
+
 		final IHUStorageFactory storageFactory = Services.get(IHandlingUnitsBL.class).getStorageFactory();
 
 		// clean up and unselect used up source HUs
-		for (final I_M_HU sourceHu : sourceHus)
+		for (final I_M_HU sourceHU : sourceHUs)
 		{
-			if (!storageFactory.getStorage(sourceHu).isEmpty())
+			if (!storageFactory.getStorage(sourceHU).isEmpty())
 			{
 				return;
 			}
-			takeSnapShotAndDestroyHu(sourceHu);
+			takeSnapShotAndDestroyHU(sourceHU);
 		}
 	}
 
-	private void takeSnapShotAndDestroyHu(@NonNull final I_M_HU sourceHu)
+	private void takeSnapShotAndDestroyHU(@NonNull final I_M_HU sourceHU)
 	{
 		final SourceHUsService sourceHuService = SourceHUsService.get();
-		sourceHuService.snapshotHuIfMarkedAsSourceHu(sourceHu);
+		sourceHuService.snapshotHuIfMarkedAsSourceHu(sourceHU);
 
 		final IHandlingUnitsBL handlingUnitsBL = Services.get(IHandlingUnitsBL.class);
-		handlingUnitsBL.destroyIfEmptyStorage(sourceHu);
+		handlingUnitsBL.destroyIfEmptyStorage(sourceHU);
 
-		Check.errorUnless(handlingUnitsBL.isDestroyed(sourceHu), "We invoked IHandlingUnitsBL.destroyIfEmptyStorage on an HU with empty storage, but its not destroyed; hu={}", sourceHu);
-		logger.info("Source M_HU with M_HU_ID={} is now destroyed", sourceHu.getM_HU_ID());
+		Check.errorUnless(handlingUnitsBL.isDestroyed(sourceHU), "We invoked IHandlingUnitsBL.destroyIfEmptyStorage on an HU with empty storage, but its not destroyed; hu={}", sourceHU);
+		logger.info("Source M_HU with M_HU_ID={} is now destroyed", sourceHU.getM_HU_ID());
 	}
 
+	private static int markCandidatesAsProcessed(final List<Integer> huIds)
+	{
+		final IQueryBL queryBL = Services.get(IQueryBL.class);
+
+		final IQuery<I_M_Picking_Candidate> query = queryBL.createQueryBuilder(I_M_Picking_Candidate.class)
+				.addOnlyActiveRecordsFilter()
+				.addEqualsFilter(I_M_Picking_Candidate.COLUMNNAME_Status, X_M_Picking_Candidate.STATUS_IP)
+				.addInArrayFilter(I_M_Picking_Candidate.COLUMNNAME_M_HU_ID, huIds)
+				.create();
+
+		final ICompositeQueryUpdater<I_M_Picking_Candidate> updater = queryBL.createCompositeQueryUpdater(I_M_Picking_Candidate.class)
+				.addSetColumnValue(I_M_Picking_Candidate.COLUMNNAME_Status, X_M_Picking_Candidate.STATUS_PR);
+
+		return query.updateDirectly(updater);
+	}
 }
