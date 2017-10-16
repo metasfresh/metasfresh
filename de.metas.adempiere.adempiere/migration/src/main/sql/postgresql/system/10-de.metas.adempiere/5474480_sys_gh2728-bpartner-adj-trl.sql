d81833ea03cedbd508df11a6c0608a3755c4ea22
Mark Krake <mark.krake@metasfresh.com>
2017-10-16 10:38:29 +0200

    #2728 Adjusting BPartner Window. Adding Tarnslations for en_US
    Adjusting BPartner Window. Adding Tarnslations for en_US
    
    FRESH-3665 https://github.com/metasfresh/metasfresh/issues/2728

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5474480_sys_gh2728-bpartner-adj-trl.sql
@@ -0,0 +1,45 @@
+-- 2017-10-16T10:22:16.791
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:22:16','YYYY-MM-DD HH24:MI:SS'),Name='Customer Group' WHERE AD_Field_ID=556295 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:27:11.343
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:27:11','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create Businesspartner Relation',Description='' WHERE AD_Process_ID=540162 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:27:50.093
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:27:50','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create Pricelist' WHERE AD_Process_ID=540399 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:28:18.230
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:28:18','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Pricelist Comparison' WHERE AD_Process_ID=540407 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:30:16.536
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:30:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create Contract for filtered Businesspartner',Description='',Help='' WHERE AD_Process_ID=540460 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:30:51.361
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:30:51','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create Empties Balance',Description='' WHERE AD_Process_ID=540467 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:31:12.018
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:31:12','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create Request' WHERE AD_Process_ID=540780 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:33:54.294
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:33:54','YYYY-MM-DD HH24:MI:SS'),Name='Create Business Partner Relation' WHERE AD_Process_ID=540162 AND AD_Language='en_US'
+;
+
+-- 2017-10-16T10:34:19.619
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-16 10:34:19','YYYY-MM-DD HH24:MI:SS'),Name='Create Contract for filtered Business Partner' WHERE AD_Process_ID=540460 AND AD_Language='en_US'
+;
+
