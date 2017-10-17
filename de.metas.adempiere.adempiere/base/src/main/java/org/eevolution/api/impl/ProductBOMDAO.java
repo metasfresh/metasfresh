d2174fec5539377390e337898684a678257bf89f
Teo Sarca <teo.sarca@metasfresh.com>
2017-10-17 15:40:35 +0300

    code format
    https://github.com/metasfresh/metasfresh/issues/480

--- de.metas.adempiere.adempiere/base/src/main/java/org/eevolution/api/impl/ProductBOMDAO.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/eevolution/api/impl/ProductBOMDAO.java
@@ -10,18 +10,17 @@ package org.eevolution.api.impl;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -82,8 +81,8 @@ public class ProductBOMDAO implements IProductBOMDAO
 	public List<I_PP_Product_BOMLine> retrieveLines(final I_PP_Product_BOM productBOM, final Date date)
 	{
 		final List<I_PP_Product_BOMLine> linesAll = retrieveLines(productBOM);
-		final List<I_PP_Product_BOMLine> linesValid = new ArrayList<I_PP_Product_BOMLine>(linesAll.size()); // Selected BOM Lines Only
-		for (I_PP_Product_BOMLine bomLine : linesAll)
+		final List<I_PP_Product_BOMLine> linesValid = new ArrayList<>(linesAll.size()); // Selected BOM Lines Only
+		for (final I_PP_Product_BOMLine bomLine : linesAll)
 		{
 			if (!Services.get(IProductBOMBL.class).isValidFromTo(bomLine, date))
 			{
