bd7e1c064fa11e7567ceb65a66f66586011080c9
Mark Krake <mark.krake@metasfresh.com>
2017-10-23 15:38:52 +0200

    #2783 Adjusting a Field Translation in Invoice Candidates Window
    Adjusting a Field Translation in Invoice Candidates Window
    
    FRESH-3726 https://github.com/metasfresh/metasfresh/issues/2783

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5475190_sys_gh2783-invoice-cand-trl-adj.sql
@@ -0,0 +1,10 @@
+-- 2017-10-23T15:33:57.024
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-23 15:33:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=548191 AND AD_Language='en_US'
+;
+
+-- 2017-10-23T15:34:33.699
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2017-10-23 15:34:33','YYYY-MM-DD HH24:MI:SS'),Name='Qty to Invoice override fulfilled',Description='',Help='' WHERE AD_Field_ID=548191 AND AD_Language='en_US'
+;
+
