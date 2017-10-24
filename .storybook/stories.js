d6b8b651be659c11fa1d6876e60194d9380d5a8b
Pablo Sichert <mail@pablosichert.com>
2017-10-24 18:42:35 +0200

    Add example story

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+const example = require('./Example/Example.story.js');
 const stories = require('./foundStories').default;
 
-for (const [storyName, storyExport] of stories) {
+for (const [storyName, storyExport] of [['Example', example], ...stories]) {
     const { name, default: story } = storyExport;
 
     story(storiesOf(name || storyName, module));
