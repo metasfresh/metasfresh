af18f2950ef5f52749f574c9cc7831de8fc01886
metas-lc <liana.cotuna@metasfresh.com>
2017-10-25 17:47:33 +0300

    #76 add printname in flatrate conditions
    https://github.com/metasfresh/metasfresh-dist-orgs/issues/76

---
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5475501_sys_gh76_Add_PrintName_c_flatrate_conditions_db.sql
@@ -0,0 +1,6 @@
+﻿-- 2017-10-25T17:00:41.111
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ SELECT public.db_alter_table('C_Flatrate_Conditions','ALTER TABLE public.C_Flatrate_Conditions ADD COLUMN PrintName VARCHAR(120)')
+;
+
+
