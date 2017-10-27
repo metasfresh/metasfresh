b49bb3fedf58a00c36f46ec2c8fcab7eeed80e41
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2017-10-27 23:41:59 +0200

    Update 547578_sys_gh_2676_AddSysConfigForDaysInPast.sql
    change configuration level to O(rganisation) because imho it make sense that this setting might be overwrittern for individual orgs

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/547578_sys_gh_2676_AddSysConfigForDaysInPast.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/547578_sys_gh_2676_AddSysConfigForDaysInPast.sql
@@ -1,4 +1,4 @@
 -- 2017-10-27T19:28:51.839
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541166,'S',TO_TIMESTAMP('2017-10-27 19:28:51','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.contracts','Y','C_Flatrate_Term.Create_SubscriptionProgressInPastDays',TO_TIMESTAMP('2017-10-27 19:28:51','YYYY-MM-DD HH24:MI:SS'),100,'0')
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541166,'O',TO_TIMESTAMP('2017-10-27 19:28:51','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.contracts','Y','C_Flatrate_Term.Create_SubscriptionProgressInPastDays',TO_TIMESTAMP('2017-10-27 19:28:51','YYYY-MM-DD HH24:MI:SS'),100,'0')
 ;
