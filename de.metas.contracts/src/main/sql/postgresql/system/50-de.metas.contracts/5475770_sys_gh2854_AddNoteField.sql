434a15307f393bc4e56e1b5915bae679fa05798d
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-10-27 20:36:34 +0300

    #2854 Add proper sequence
    #2854 Save Termination Reason when terminating subscription

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5475770_sys_gh2854_AddNoteField.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5475770_sys_gh2854_AddNoteField.sql
@@ -8,3 +8,8 @@ INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,
 INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=541229 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
 ;
 
+-- 2017-10-27T20:13:31.013
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET SeqNo=8,Updated=TO_TIMESTAMP('2017-10-27 20:13:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541229
+;
+
