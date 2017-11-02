6de397fbfb19aa07469087e6b684cbc3a3373d55
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-02 14:31:18 +0100

    fix IsCalculated flag for AD_SysConfig and M_AttributeInstance
    #2546 Mark all Value and DocumentNo columns as IsCalculated=Y
    
    (cherry picked from commit 382f91e33d1f9f9178c1bb136cf89e64f640db68)

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5476150_sys_gh2546-app_fix_value_iscalculated_on_some_tables.sql
@@ -0,0 +1,16 @@
+--
+-- AD_SysConfig.Value
+--
+-- 2017-11-02T14:23:59.375
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='N',Updated=TO_TIMESTAMP('2017-11-02 14:23:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50196
+;
+
+--
+-- M_AttributeInstance.Value
+--
+-- 2017-11-02T14:26:27.744
+-- URL zum Konzept
+UPDATE AD_Column SET IsCalculated='N',Updated=TO_TIMESTAMP('2017-11-02 14:26:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=8501
+;
+
