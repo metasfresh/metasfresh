e68cc7024f260a1d7b2792bdd45dfafa55d8c692
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-06 07:23:24 +0100

    Relax consistency checking for swingUI
    also in HUStatusBL.java: minor
    
    Issued HUs are still selectable in HU editor while production is not
    completed metasfresh/metasfresh-webui-api#590

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/impl/HUStatusBL.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/impl/HUStatusBL.java
@@ -41,7 +41,7 @@ import lombok.NonNull;
 
 public class HUStatusBL implements IHUStatusBL
 {
-	private final static List<String> HUSTATUSES_QtyOnHand = ImmutableList.of(
+	private final static List<String> HU_STATUSES_THAT_COUNT_FOR_QTY_ON_HAND = ImmutableList.of(
 			X_M_HU.HUSTATUS_Active,
 			X_M_HU.HUSTATUS_Picked,
 			X_M_HU.HUSTATUS_Issued);
@@ -86,13 +86,13 @@ public class HUStatusBL implements IHUStatusBL
 	@Override
 	public boolean isQtyOnHand(final String huStatus)
 	{
-		return HUSTATUSES_QtyOnHand.contains(huStatus);
+		return HU_STATUSES_THAT_COUNT_FOR_QTY_ON_HAND.contains(huStatus);
 	}
 
 	@Override
 	public List<String> getQtyOnHandStatuses()
 	{
-		return HUSTATUSES_QtyOnHand;
+		return HU_STATUSES_THAT_COUNT_FOR_QTY_ON_HAND;
 	}
 
 	@Override
