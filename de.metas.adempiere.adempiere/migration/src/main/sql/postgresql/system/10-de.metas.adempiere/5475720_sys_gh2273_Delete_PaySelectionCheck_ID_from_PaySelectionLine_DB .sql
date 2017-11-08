3e386aa20bbea87578813c6e0a5bb420f4c7a51a
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-11-08 15:51:25 +0200

    #2273  Repair the alter table SQL for dropping C_PaySelectionCheck_ID from C_PaySelectionLine
    https://github.com/metasfresh/metasfresh/issues/2273

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5475720_sys_gh2273_Delete_PaySelectionCheck_ID_from_PaySelectionLine_DB .sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5475720_sys_gh2273_Delete_PaySelectionCheck_ID_from_PaySelectionLine_DB .sql
@@ -1,2 +1,5 @@
 
-ALTER TABLE C_PaySelectionLine DROP COLUMN IF EXISTS C_PaySelectionCheck_ID;
+
+
+
+SELECT public.db_alter_table('C_PaySelectionLine', 'ALTER TABLE C_PaySelectionLine DROP COLUMN IF EXISTS C_PaySelectionCheck_ID');
