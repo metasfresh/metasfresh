e841f9c77a9fe5fec9b87cfcd0d3f1e3c1981927
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-11-09 13:25:20 +0200

    #2924  Add a test for canceling an extended contract
    #2924  Allow Termination of all selected contract periods

--- de.metas.contracts/src/test/java/de/metas/contracts/impl/ContractChangeBLTest.java
+++ de.metas.contracts/src/test/java/de/metas/contracts/impl/ContractChangeBLTest.java
@@ -1,6 +1,7 @@
 package de.metas.contracts.impl;
 
 import static org.adempiere.model.InterfaceWrapperHelper.save;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /*
  * #%L
@@ -15,15 +16,14 @@ import static org.adempiere.model.InterfaceWrapperHelper.save;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-
 
 import java.sql.Timestamp;
 
@@ -41,54 +41,71 @@ import de.metas.contracts.interceptor.C_Flatrate_Term;
 import de.metas.contracts.model.I_C_Flatrate_Conditions;
 import de.metas.contracts.model.I_C_Flatrate_Term;
 import de.metas.contracts.model.X_C_Flatrate_Term;
+import lombok.NonNull;
 
 public class ContractChangeBLTest extends AbstractFlatrateTermTest
 {
-	final private static Timestamp startDate = TimeUtil.parseTimestamp("2017-09-10");
 	final private IContractChangeBL contractChangeBL = Services.get(IContractChangeBL.class);
+	
+	final private static Timestamp startDate = TimeUtil.parseTimestamp("2017-09-10");
+	final private static Timestamp cancelDate = TimeUtil.parseTimestamp("2017-12-10");
+	final private static String terminationMemo = "note: cancelContract_test";
+	final private static ContractChangeParameters contractChangeParameters = ContractChangeParameters.builder()
+			.changeDate(cancelDate)
+			.isCloseInvoiceCandidate(true)
+			.terminationReason(X_C_Flatrate_Term.TERMINATIONREASON_General)
+			.terminationMemo(terminationMemo)
+			.build();
 
 	@Before
 	public void before()
 	{
 		Services.get(IModelInterceptorRegistry.class).addModelInterceptor(C_Flatrate_Term.INSTANCE);
 	}
-	
+
 	@Test
 	public void cancelContract_test()
 	{
 		final I_C_Flatrate_Term contract = prepareContractForTest(true);
-
-		final Timestamp changeTime = TimeUtil.getDay(2013,7,27);
-
-		final ContractChangeParameters contractChangeParameters = ContractChangeParameters.builder()
-				.changeDate(changeTime)
-				.isCloseInvoiceCandidate(true)
-				.terminationReason(X_C_Flatrate_Term.TERMINATIONREASON_General)
-				.terminationMemo("note: cancelContract_test")
-				.build();
-		
 		contractChangeBL.cancelContract(contract, contractChangeParameters);
-
+		assertFlatrateTerm(contract);
 	}
-	
+
 	@Test
-	public void extendContractWithAutoRenewOnNo_test()
+	public void cancel_a_Contract_which_was_extended()
 	{
 		final I_C_Flatrate_Term contract = prepareContractForTest(true);
-			
 		Services.get(IFlatrateBL.class).extendContract(contract, true, true, null, null);
 		save(contract);
-
+		
+		final I_C_Flatrate_Term extendedContract = contract.getC_FlatrateTerm_Next();
+		assertThat(extendedContract).isNotNull();
+		
+		contractChangeBL.cancelContract(contract, contractChangeParameters);
+		assertFlatrateTerm(contract);
+		assertThat(contract.getMasterEndDate()).isEqualTo(cancelDate);
+		assertFlatrateTerm(extendedContract);
 	}
-	
+
 	private I_C_Flatrate_Term prepareContractForTest(final boolean isAutoRenew)
 	{
 		prepareBPartner();
 		final ProductAndPricingSystem productAndPricingSystem = createProductAndPricingSystem(startDate);
 		createProductAcct(productAndPricingSystem);
 		final I_C_Flatrate_Conditions conditions = createFlatrateConditions(productAndPricingSystem, isAutoRenew);
-		createContractChange(conditions); 
+		createContractChange(conditions);
 		final I_C_Flatrate_Term contract = createFlatrateTerm(conditions, productAndPricingSystem.getProduct(), startDate);
 		return contract;
+	}
+
+	private void assertFlatrateTerm(@NonNull final I_C_Flatrate_Term currentflatrateTerm)
+	{
+		assertThat(currentflatrateTerm.getContractStatus()).isEqualTo(X_C_Flatrate_Term.CONTRACTSTATUS_Quit);
+		assertThat(currentflatrateTerm.isAutoRenew()).isFalse();
+		assertThat(currentflatrateTerm.getMasterStartDate()).isNotNull();
+		assertThat(currentflatrateTerm.getMasterEndDate()).isEqualTo(cancelDate);
+		assertThat(currentflatrateTerm.getTerminationMemo()).isEqualTo(terminationMemo);
+		assertThat(currentflatrateTerm.getTerminationReason()).isEqualTo(X_C_Flatrate_Term.TERMINATIONREASON_General);
+		
 	}
 }
