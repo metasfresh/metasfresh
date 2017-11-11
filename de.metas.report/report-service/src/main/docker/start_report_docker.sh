05d06cd933f9e22ac83aaf04b145c4e31113a32f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-11 11:17:47 +0100

    solve problem off missing local_settings.properties
    also attempt to make the image more transparent and flexibe
    
    standalone report service https://github.com/metasfresh/metasfresh/issues/2932

--- de.metas.report/report-service/src/main/docker/start_report_docker.sh
+++ de.metas.report/report-service/src/main/docker/start_report_docker.sh
@@ -2,25 +2,46 @@
 
 set -e
 
-#The variable DB_HOST shall be set from outside, e.g. via -e "DB_HOST=mydbms" or from the docker-compose.yml file
-#DB_HOST=db
 
-APP_HOST=app
+# The variables have defaults that can be set from outside, e.g. via -e "DB_HOST=mydbms" or from the docker-compose.yml file.
+# Also see https://docs.docker.com/engine/reference/builder/#environment-replacement
+db_host=${DB_HOST:-db}
+db_port=${DB_PORT:-5432}
+db_name=${DB_NAME:-metasfresh}
+db_user=${DB_USER:-metasfresh}
+db_password=${DB_PASSWORD:-metasfresh}
+app_host=${APP_HOST:-app}
+
+echo_variable_values()
+{
+ echo "Note: all these variables can be set using the -e parameter."
+ echo ""
+ echo "DB_HOST=${db_host}"
+ echo "DB_PORT=${db_port}"
+ echo "DB_NAME={db_name}"
+ echo "DB_USER=${db_user}"
+ echo "DB_PASSWORD=dude, that's a secret"
+ echo "APP_HOST=${app_host}"
+}
 
 set_properties()
 {
  echo "set_properties BEGIN"
  local prop_file="$1"
  if [[ $(cat $prop_file | grep FOO | wc -l) -ge "1" ]]; then
-	sed -Ei "s/FOO_DBMS/$DB_HOST/g" $prop_file
-	sed -Ei "s/FOO_APP/$APP_HOST/g" $prop_file
+	sed -Ei "s/FOO_DBMS_HOST/${db_host}/g" $prop_file
+	sed -Ei "s/FOO_DBMS_PORT/${db_port}/g" $prop_file
+	sed -Ei "s/FOO_DB_NAME/${db_name}/g" $prop_file
+	sed -Ei "s/FOO_DB_USER/${db_user}/g" $prop_file
+	sed -Ei "s/FOO_DB_PASSWORD/${db_password}/g" $prop_file
+	sed -Ei "s/FOO_APP/${app_host}/g" $prop_file
  fi
  echo "set_properties END"
 }
  
 wait_dbms()
 {
- until nc -z $DB_HOST 5432
+ until nc -z $db_host $db_port
  do
    sleep 1
  done
@@ -29,7 +50,23 @@ wait_dbms()
 run_db_update()
 {
  sleep 10
- cd /opt/metasfresh/dist/install/ && java -jar ./lib/de.metas.migration.cli.jar $@
+ 
+ settings_file="/opt/metasfresh/run_db_update_settings.properties"
+
+ echo "" > $settings_file
+ echo "IMPORTANT" >> $settings_file
+ echo "This file is rewritten each time the container starts" >> $settings_file
+ echo "" >> $settings_file
+ echo "METASFRESH_DB_SERVER=${db_host}" >> $settings_file
+ echo "METASFRESH_DB_PORT=${db_port}" >> $settings_file
+ echo "METASFRESH_DB_NAME=${db_name}" >> $settings_file
+ echo "METASFRESH_DB_USER=${db_user}" >> $settings_file
+ echo "METASFRESH_DB_PASSWORD=${db_password}" >> $settings_file
+ echo "" > $settings_file
+ 
+ # -s sets the "Name of the (s)ettings file (e.g. settings_<hostname>.properties) *within the Rollout-Directory*" (which is /opt/metasfresh in this case)
+ # run with -h for a description of all params
+ cd /opt/metasfresh/dist/install/ && java -jar ./lib/de.metas.migration.cli.jar -s run_db_update_settings.properties
 }
 
 # Note: the Djava.security.egd param is supposed to let tomcat start quicker, see https://spring.io/guides/gs/spring-boot-docker/
@@ -43,10 +80,17 @@ run_metasfresh()
  -jar metasfresh-report.jar
 }
 
-set_properties /opt/metasfresh/metasfresh-report/metasfresh.properties
+echo "*************************************************************"
+echo "Display the variable values we run with"
+echo "*************************************************************"
+echo_variable_values
+echo ""
+
+set_properties /opt/metasfresh/metasfresh-report/
+
 
 echo "*************************************************************"
-echo "Wait for the database server to start on DB_HOST = '${DB_HOST}'"
+echo "Wait for the database server to start on DB_HOST = '${db_host}'"
 echo "*************************************************************"
 wait_dbms
 echo ">>>>>>>>>>>> Database Server has started"
