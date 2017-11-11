879756a82bb04d46529e5585ce817059df46a5ea
Mark Krake <mark.krake@metasfresh.com>
2017-11-11 09:41:22 +0100

    #2947 Adding Translations in de_DE for main view
    Adding Translations in de_DE for main view
    
    FRESH-3988 https://github.com/metasfresh/metasfresh/issues/2947

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5476910_sys_gh2947_adapt-role-window-webui.sql
@@ -0,0 +1,80 @@
+-- 2017-11-11T09:33:03.329
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Rolenauswahl überspringen',Updated=TO_TIMESTAMP('2017-11-11 09:33:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=58863
+;
+
+-- 2017-11-11T09:33:48.428
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nutzer Rabatt',Updated=TO_TIMESTAMP('2017-11-11 09:33:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=52018
+;
+
+-- 2017-11-11T09:34:08.736
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Sektion Login Pflicht',Updated=TO_TIMESTAMP('2017-11-11 09:34:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=551446
+;
+
+-- 2017-11-11T09:34:26.689
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Menü anzeigen',Updated=TO_TIMESTAMP('2017-11-11 09:34:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=64774
+;
+
+-- 2017-11-11T09:34:47.621
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Login Datum änderbar',Updated=TO_TIMESTAMP('2017-11-11 09:34:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=555206
+;
+
+-- 2017-11-11T09:34:56.424
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Alle Entity Typen anzeigen',Updated=TO_TIMESTAMP('2017-11-11 09:34:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556365
+;
+
+-- 2017-11-11T09:35:06.692
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Migrationsscripte erlauben',Updated=TO_TIMESTAMP('2017-11-11 09:35:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556218
+;
+
+-- 2017-11-11T09:35:19.953
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Abrechnung Prio erlaubt',Updated=TO_TIMESTAMP('2017-11-11 09:35:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=556522
+;
+
+-- 2017-11-11T09:35:34.351
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Wurtelknoten Menü',Updated=TO_TIMESTAMP('2017-11-11 09:35:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=558496
+;
+
+-- 2017-11-11T09:35:45.104
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Rabatt auf Summe erlaubt',Updated=TO_TIMESTAMP('2017-11-11 09:35:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=64772
+;
+
+-- 2017-11-11T09:36:00.051
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Rabatt bit Limit Preis erlaubt',Updated=TO_TIMESTAMP('2017-11-11 09:36:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=64773
+;
+
+-- 2017-11-11T09:36:31.787
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Login Sektion',Updated=TO_TIMESTAMP('2017-11-11 09:36:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=551780
+;
+
+-- 2017-11-11T09:37:03.703
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Keine Rollenauswahl',Updated=TO_TIMESTAMP('2017-11-11 09:37:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=58863
+;
+
+-- 2017-11-11T09:37:45.162
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Sektion Anmeldung Pflicht',Updated=TO_TIMESTAMP('2017-11-11 09:37:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=551446
+;
+
+-- 2017-11-11T09:38:18.746
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Sonder Fenster nach Anmeldung',Updated=TO_TIMESTAMP('2017-11-11 09:38:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=72252
+;
+
+-- 2017-11-11T09:38:33.509
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Anmeldung Datum änderbar',Updated=TO_TIMESTAMP('2017-11-11 09:38:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=555206
+;
+
