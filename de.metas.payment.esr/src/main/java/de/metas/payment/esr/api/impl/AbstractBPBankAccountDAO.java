45bdff7478d74671da8c864cc3b6a275f6555770
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-11-16 14:39:21 +0200

    #2944 dao modified to support ers number from bank account or from the esrPostFinance table
    https://github.com/metasfresh/metasfresh/issues/2944

--- de.metas.payment.esr/src/main/java/de/metas/payment/esr/api/impl/AbstractBPBankAccountDAO.java
+++ de.metas.payment.esr/src/main/java/de/metas/payment/esr/api/impl/AbstractBPBankAccountDAO.java
@@ -2,36 +2,11 @@ package de.metas.payment.esr.api.impl;
 
 import java.util.List;
 
-/*
- * #%L
- * de.metas.payment.esr
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
-import java.util.Properties;
-
 import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.ad.dao.IQueryBuilder;
-import org.adempiere.ad.trx.api.ITrx;
+import org.adempiere.ad.dao.IQueryFilter;
 import org.adempiere.util.Check;
 import org.adempiere.util.Services;
-import org.compiere.util.Env;
 
 import de.metas.payment.esr.api.IESRBPBankAccountDAO;
 import de.metas.payment.esr.model.I_C_BP_BankAccount;
@@ -45,26 +20,37 @@ public abstract class AbstractBPBankAccountDAO implements IESRBPBankAccountDAO
 		Check.assumeNotEmpty(postAccountNo, "postAccountNo not null");
 		Check.assumeNotEmpty(innerAccountNo, "innerAccountNo not null");
 
-		final Properties ctx = Env.getCtx();
-		final String trxName = ITrx.TRXNAME_None;
-
 		final IQueryBL queryBL = Services.get(IQueryBL.class);
 
-		final IQueryBuilder<I_C_BP_BankAccount> qb = queryBL.createQueryBuilder(I_ESR_PostFinanceUserNumber.class, ctx, trxName)
+		final IQueryBuilder<I_C_BP_BankAccount> bpBankAccountESRRenderedAccountNo = queryBL.createQueryBuilder(I_C_BP_BankAccount.class)
+				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_ESR_RenderedAccountNo, postAccountNo);
+
+		final IQueryBuilder<I_C_BP_BankAccount> esrPostalFinanceUserESRRenderedAccountNo = queryBL.createQueryBuilder(I_C_BP_BankAccount.class)
+				.andCollectChildren(I_ESR_PostFinanceUserNumber.COLUMN_C_BP_BankAccount_ID, I_ESR_PostFinanceUserNumber.class)
 				.addEqualsFilter(I_ESR_PostFinanceUserNumber.COLUMNNAME_ESR_RenderedAccountNo, postAccountNo)
-				.andCollect(I_ESR_PostFinanceUserNumber.COLUMN_C_BP_BankAccount_ID, I_C_BP_BankAccount.class)
-				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_IsEsrAccount, true);
+				.andCollect(I_ESR_PostFinanceUserNumber.COLUMN_C_BP_BankAccount_ID, I_C_BP_BankAccount.class);
+
+		final IQueryFilter<I_C_BP_BankAccount> esrAccountNoFromBankAcctOrESRPostFinanceUser = queryBL.createCompositeQueryFilter(I_C_BP_BankAccount.class)
+				.setJoinOr()
+				.addInSubQueryFilter(I_C_BP_BankAccount.COLUMNNAME_C_BP_BankAccount_ID, I_C_BP_BankAccount.COLUMNNAME_C_BP_BankAccount_ID, bpBankAccountESRRenderedAccountNo.create())
+				.addInSubQueryFilter(I_C_BP_BankAccount.COLUMNNAME_C_BP_BankAccount_ID, I_C_BP_BankAccount.COLUMNNAME_C_BP_BankAccount_ID, esrPostalFinanceUserESRRenderedAccountNo.create());
+
+		final IQueryBuilder<I_C_BP_BankAccount> bankAccountQuery = queryBL.createQueryBuilder(I_C_BP_BankAccount.class)
+				.addOnlyActiveRecordsFilter()
+				.addOnlyContextClient()
+				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_IsEsrAccount, true)
+				.filter(esrAccountNoFromBankAcctOrESRPostFinanceUser);
 
 		if (!innerAccountNo.equals("0000000")) // 7 x 0
 		{
-			qb.addEqualsFilter(org.compiere.model.I_C_BP_BankAccount.COLUMNNAME_AccountNo, innerAccountNo);
+			bankAccountQuery.addEqualsFilter(org.compiere.model.I_C_BP_BankAccount.COLUMNNAME_AccountNo, innerAccountNo);
 		}
 
-		qb
+		bankAccountQuery
 				.addOnlyActiveRecordsFilter()
 				.addOnlyContextClient();
 
-		return qb
+		return bankAccountQuery
 				.create()
 				.list();
 	}
