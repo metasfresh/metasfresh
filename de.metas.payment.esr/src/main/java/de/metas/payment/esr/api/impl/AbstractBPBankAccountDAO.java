958e6760a336b58f42d1e8af3fa73d4cb42b18e7
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-11-16 18:25:42 +0200

    #2944 ESR_PostPostFinanceUserNumbers retrieval for bank account
    https://github.com/metasfresh/metasfresh/issues/2944

--- de.metas.payment.esr/src/main/java/de/metas/payment/esr/api/impl/AbstractBPBankAccountDAO.java
+++ de.metas.payment.esr/src/main/java/de/metas/payment/esr/api/impl/AbstractBPBankAccountDAO.java
@@ -11,11 +11,12 @@ import org.adempiere.util.Services;
 import de.metas.payment.esr.api.IESRBPBankAccountDAO;
 import de.metas.payment.esr.model.I_C_BP_BankAccount;
 import de.metas.payment.esr.model.I_ESR_PostFinanceUserNumber;
+import lombok.NonNull;
 
 public abstract class AbstractBPBankAccountDAO implements IESRBPBankAccountDAO
 {
 	@Override
-	public List<I_C_BP_BankAccount> retrieveESRBPBankAccounts(final String postAccountNo, final String innerAccountNo)
+	public List<I_C_BP_BankAccount> retrieveESRBPBankAccounts(@NonNull final String postAccountNo, @NonNull final String innerAccountNo)
 	{
 		Check.assumeNotEmpty(postAccountNo, "postAccountNo not null");
 		Check.assumeNotEmpty(innerAccountNo, "innerAccountNo not null");
@@ -55,4 +56,16 @@ public abstract class AbstractBPBankAccountDAO implements IESRBPBankAccountDAO
 				.list();
 	}
 
+	@Override
+	public List<I_ESR_PostFinanceUserNumber> retrieveESRPostFinanceUserNumbers(@NonNull final I_C_BP_BankAccount bankAcct)
+	{
+		final IQueryBL queryBL = Services.get(IQueryBL.class);
+
+		return queryBL.createQueryBuilder(I_ESR_PostFinanceUserNumber.class)
+				.addOnlyContextClient()
+				.addOnlyActiveRecordsFilter()
+				.addEqualsFilter(I_ESR_PostFinanceUserNumber.COLUMNNAME_C_BP_BankAccount_ID, bankAcct.getC_BP_BankAccount_ID())
+				.create()
+				.list(I_ESR_PostFinanceUserNumber.class);
+	}
 }
