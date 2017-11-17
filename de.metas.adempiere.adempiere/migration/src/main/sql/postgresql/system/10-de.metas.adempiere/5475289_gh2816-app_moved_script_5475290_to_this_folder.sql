8ef0a85eba2784f86767a7730b6515971dd47bdc
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-17 11:03:04 +0100

    move migration file,
    because we also need the column name change on DBs where there isn't
    even material dispo
    
    Material Dispo - include storage-relevant attributes #2816

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5475289_gh2816-app_moved_script_5475290_to_this_folder.sql
@@ -0,0 +1,7 @@
+﻿
+UPDATE AD_MigrationScript
+SET
+	Description='"moved" from 75-material-dispo to 10-de.metas.adempiere by script 5475289;Applied by de.metas.migration.impl.SQLDatabaseScriptsRegistry',
+	ProjectName='10-de.metas.adempiere',
+	Name='10-de.metas.adempiere->5475290_gh2816-app_rename_M_Attribute_column_to_IsStorageRelevant.sql'
+WHERE Name='75-material-dispo->5475290_gh2816-app_rename_M_Attribute_column_to_IsStorageRelevant.sql';
