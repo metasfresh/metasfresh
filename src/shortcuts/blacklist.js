dd5dea72f76266b05942ca32cf9a592ce7664dc6
Pablo Sichert <mail@pablosichert.com>
2017-11-20 16:44:33 +0100

    Add more browser defaults to hotkey blacklist
    Issue #1283

--- src/shortcuts/blacklist.js
+++ src/shortcuts/blacklist.js
@@ -4,8 +4,13 @@ const mods = [
 ];
 
 export default Object.entries(Object.assign(...mods.map(mod => ({
+    [`${mod}+c`]: 'Copy',
+    [`${mod}+f`]: 'Search',
+    [`${mod}+h`]: 'History',
     [`${mod}+l`]: 'Focus address bar',
+    [`${mod}+n`]: 'New window',
     [`${mod}+t`]: 'New tab',
+    [`${mod}+v`]: 'Paste',
     [`${mod}+w`]: 'Close tab'
 })))).reduce((blacklist, pair) => {
     const [key, value] = pair;
