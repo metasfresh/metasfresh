a3a5feeaa5724a6a84a769ae2e7b1d7e1dbf3a47
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-20 14:47:45 +0100

    solve typo

--- vagrant/provision/provision_docker_metasfresh-services.sh
+++ vagrant/provision/provision_docker_metasfresh-services.sh
@@ -11,7 +11,7 @@
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Providing & starting metasfresh-admin on port 9090"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-docker run --name metasfresh-admin -d -rm -p 9090:9090 -t --name metasfresh-admin metasfresh/metasfresh-admin:master-latest
+docker run --name metasfresh-admin -d -p 9090:9090 -t metasfresh/metasfresh-admin:master-latest
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Done providing & starting metasfresh-admin on port 9091"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -19,7 +19,7 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Providing & starting metasfresh-db on port 5432"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-docker run --name metasfresh-db -d -p 5432:5432 -t --name metasfresh-db metasfresh/metasfresh-db:latest
+docker run --name metasfresh-db -d -p 5432:5432 -t metasfresh/metasfresh-db:latest
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Done providing & starting metasfresh-db on port 5432"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
