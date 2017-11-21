688dd7b647019e246cda0819174e7610546894ba
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-11-21 14:19:53 +0200

    #3022 Add sequence for client!=0
    #3022 Contracts: ContractDocumentNo is 0 all the time

---
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5477770_cli_gh3022_AddSequence_DocumentNo_C_Flatrate_Term.sql
@@ -0,0 +1,5 @@
+-- 2017-11-21T14:08:53.809
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Sequence (AD_Client_ID,AD_Org_ID,AD_Sequence_ID,Created,CreatedBy,CurrentNext,CurrentNextSys,Description,IncrementNo,IsActive,IsAudited,IsAutoSequence,IsTableID,Name,StartNewYear,StartNo,Updated,UpdatedBy) VALUES (1000000,0,554443,TO_TIMESTAMP('2017-11-21 14:08:53','YYYY-MM-DD HH24:MI:SS'),100,10000000,10000006,'DocumentNo/Value for Table C_Flatrate_Term',1,'Y','N','Y','N','DocumentNo_C_Flatrate_Term','N',10000000,TO_TIMESTAMP('2017-11-21 14:08:53','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
