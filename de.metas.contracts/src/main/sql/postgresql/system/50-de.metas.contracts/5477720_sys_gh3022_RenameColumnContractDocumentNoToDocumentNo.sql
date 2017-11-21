1faf198011e0f7445104b12f9590c42b1695164e
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-11-21 13:28:57 +0200

    #3022 Add sequence for DocumentNo_C_Flatrate_Term
    #3022 Contracts: ContractDocumentNo is 0 all the time

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5477720_sys_gh3022_RenameColumnContractDocumentNoToDocumentNo.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5477720_sys_gh3022_RenameColumnContractDocumentNoToDocumentNo.sql
@@ -14,3 +14,9 @@ If the document type of your document has no automatic document sequence defined
 
 ALTER TABLE C_Flatrate_Term RENAME COLUMN ContractDocumentNo TO DocumentNo;
 
+
+-- 2017-11-21T13:26:22.730
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Sequence (AD_Client_ID,AD_Org_ID,AD_Sequence_ID,Created,CreatedBy,CurrentNext,CurrentNextSys,Description,IncrementNo,IsActive,IsAudited,IsAutoSequence,IsTableID,Name,StartNewYear,StartNo,Updated,UpdatedBy) VALUES (0,0,554442,TO_TIMESTAMP('2017-11-21 13:26:22','YYYY-MM-DD HH24:MI:SS'),100,10000000,10000006,'DocumentNo/Value for Table C_Flatrate_Term',1,'Y','N','Y','N','DocumentNo_C_Flatrate_Term','N',10000000,TO_TIMESTAMP('2017-11-21 13:26:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
