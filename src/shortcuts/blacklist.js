2219644242e07729004796a856e8b2fedb1182c2
Pablo Sichert <mail@pablosichert.com>
2017-11-21 18:50:44 +0100

    Add "Alt+Spacebar" to blacklist since it's not interceptible on Windows/Linux
    Issue #1365

--- src/shortcuts/blacklist.js
+++ src/shortcuts/blacklist.js
@@ -3,16 +3,19 @@ const mods = [
     'Meta' // macOS
 ];
 
-export default Object.entries(Object.assign(...mods.map(mod => ({
-    [`${mod}+C`]: 'Copy',
-    [`${mod}+F`]: 'Search',
-    [`${mod}+H`]: 'History',
-    [`${mod}+L`]: 'Focus address bar',
-    [`${mod}+N`]: 'New window',
-    [`${mod}+T`]: 'New tab',
-    [`${mod}+V`]: 'Paste',
-    [`${mod}+W`]: 'Close tab'
-})))).reduce((blacklist, pair) => {
+export default Object.entries({
+    'Alt+Spacebar': 'Windows window menu',
+    ...Object.assign(...mods.map(mod => ({
+        [`${mod}+C`]: 'Copy',
+        [`${mod}+F`]: 'Search',
+        [`${mod}+H`]: 'History',
+        [`${mod}+L`]: 'Focus address bar',
+        [`${mod}+N`]: 'New window',
+        [`${mod}+T`]: 'New tab',
+        [`${mod}+V`]: 'Paste',
+        [`${mod}+W`]: 'Close tab'
+    })))
+}).reduce((blacklist, pair) => {
     const [key, value] = pair;
 
     blacklist[key.toUpperCase()] = value;
