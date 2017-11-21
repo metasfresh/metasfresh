e778400580cacb0d8cb72cad8fa826a3bb81d756
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-21 16:18:13 +0100

    solve bug of IQuery filter not containing the parentID...
    ...even if the CandidatesQuery had one
    Aggregate Storage for Product and Warehouse
    https://github.com/metasfresh/metasfresh-webui-api/issues/662

--- de.metas.material/dispo-commons/src/main/java/de/metas/material/dispo/commons/repository/RepositoryCommons.java
+++ de.metas.material/dispo-commons/src/main/java/de/metas/material/dispo/commons/repository/RepositoryCommons.java
@@ -79,6 +79,11 @@ public class RepositoryCommons
 			builder.addEqualsFilter(I_MD_Candidate.COLUMN_MD_Candidate_ID, query.getId());
 		}
 
+		if (query.getParentId() > 0)
+		{
+			builder.addEqualsFilter(I_MD_Candidate.COLUMN_MD_Candidate_Parent_ID, query.getParentId());
+		}
+
 		addMaterialDescriptorToQueryBuilderIfNotNull(
 				query.getMaterialDescriptor(),
 				query.isMatchExactStorageAttributesKey(),
