8cd58888fee4fe47af6d43f1cb33c7d1e84bb695
Pablo Sichert <mail@pablosichert.com>
2017-11-22 16:50:43 +0100

    Fix document actions being filtered with old API format
    Issue #1374

--- src/components/widget/ActionButton.js
+++ src/components/widget/ActionButton.js
@@ -178,8 +178,8 @@ class ActionButton extends Component {
                 <DocumentStatusContextShortcuts
                     handleDocumentCompleteStatus={() => {
                         this.handleChangeStatus(
-                            list.filter(elem => !!elem.CO)[0]
-                        )
+                            list.find(elem => elem.key === 'CO')
+                        );
                     }}
                 />
             </div>
