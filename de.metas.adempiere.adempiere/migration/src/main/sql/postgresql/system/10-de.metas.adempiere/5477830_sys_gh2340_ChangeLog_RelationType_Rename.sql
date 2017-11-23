c2605b8fb33072c14fc72f4cb12e6b38e8ec4443
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-11-23 18:51:14 +0200

    #2340 Rename the relation type for changelog to fit TableRecordIdTarget
    https://github.com/metasfresh/metasfresh/issues/2340

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5477830_sys_gh2340_ChangeLog_RelationType_Rename.sql
+++
@@ -1,5 +0,0 @@
--- 2017-11-20T16:01:32.259
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_RelationType SET Name='AD_ChangeLog_ReferenceTarget',Updated=TO_TIMESTAMP('2017-11-20 16:01:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540188
-;
-
