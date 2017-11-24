b92c72b7dbcf814fadca241867192ed551d12f62
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-11-24 19:45:36 +0200

    #3042 Minor
    #3042 C_Bpartner: new Additional fields

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5478360_sys_gh3042_AddShortDescription.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5478360_sys_gh3042_AddShortDescription.sql
@@ -499,3 +499,4 @@ UPDATE AD_Field SET IsDisplayed='Y', SeqNo=670,Updated=TO_TIMESTAMP('2017-11-24 
 UPDATE AD_Field SET IsDisplayed='Y', SeqNo=680,Updated=TO_TIMESTAMP('2017-11-24 18:23:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5924
 ;
 
+
