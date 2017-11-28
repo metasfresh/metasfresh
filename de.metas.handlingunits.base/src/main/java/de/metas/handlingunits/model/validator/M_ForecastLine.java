218fc3ae1211bbde3fc594d249967821c3ecd11a
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-11-28 20:07:56 +0200

    #699 Add Forecast line validator
    #699 Implement Batch entry in Forecast Window
    https://github.com/metasfresh/metasfresh-webui-api/issues/699

---
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/model/validator/M_ForecastLine.java
@@ -0,0 +1,65 @@
+package de.metas.handlingunits.model.validator;
+
+/*
+ * #%L
+ * de.metas.handlingunits.base
+ * %%
+ * Copyright (C) 2015 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+import org.adempiere.ad.modelvalidator.annotations.ModelChange;
+import org.adempiere.ad.modelvalidator.annotations.Validator;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.util.Services;
+import org.compiere.model.ModelValidator;
+
+import de.metas.adempiere.gui.search.IHUPackingAware;
+import de.metas.adempiere.gui.search.IHUPackingAwareBL;
+import de.metas.adempiere.gui.search.impl.ForecastLineHUPackingAware;
+import de.metas.handlingunits.IHUDocumentHandler;
+import de.metas.handlingunits.IHUDocumentHandlerFactory;
+import de.metas.handlingunits.model.I_C_OrderLine;
+import de.metas.handlingunits.model.I_M_ForecastLine;
+
+@Validator(I_M_ForecastLine.class)
+public class M_ForecastLine
+{
+	@ModelChange(timings = {
+			ModelValidator.TYPE_BEFORE_NEW,
+			ModelValidator.TYPE_BEFORE_CHANGE
+	}, ifColumnsChanged = {
+			I_M_ForecastLine.COLUMNNAME_C_BPartner_ID, I_M_ForecastLine.COLUMNNAME_M_Product_ID, I_M_ForecastLine.COLUMNNAME_Qty, de.metas.handlingunits.model.I_M_ForecastLine.COLUMNNAME_M_HU_PI_Item_Product_ID
+	})
+	public void add_M_HU_PI_Item_Product(final I_C_OrderLine olPO)
+	{
+			final IHUDocumentHandlerFactory huDocumentHandlerFactory = Services.get(IHUDocumentHandlerFactory.class);
+		final IHUDocumentHandler handler = huDocumentHandlerFactory.createHandler(I_M_ForecastLine.Table_Name);
+		if (null != handler)
+		{
+			final de.metas.handlingunits.model.I_M_ForecastLine olEx = InterfaceWrapperHelper.create(olPO, de.metas.handlingunits.model.I_M_ForecastLine.class);
+			handler.applyChangesFor(olEx);
+			updateQtyPacks(olEx);
+		}
+	}
+
+	private void updateQtyPacks(final de.metas.handlingunits.model.I_M_ForecastLine forecastLine)
+	{
+		final IHUPackingAware packingAware = new ForecastLineHUPackingAware(forecastLine);
+		Services.get(IHUPackingAwareBL.class).setQtyPacks(packingAware);
+	}
+}
