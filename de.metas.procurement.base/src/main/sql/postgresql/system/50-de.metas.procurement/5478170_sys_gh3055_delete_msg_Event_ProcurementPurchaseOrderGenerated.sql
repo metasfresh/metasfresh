f0df8abe1fc8e2d6b5a38d128c8b52929b14143f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-11-30 10:44:56 +0100

    update AD_Message_ID in existing AD_Notes before deleting the old Msg
    (MsgText is the same)
    https://github.com/metasfresh/metasfresh/issues/3055
    
    (cherry picked from commit 142c6af125663dff2572324ffd9a49e4fecb02e2)

--- de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5478170_sys_gh3055_delete_msg_Event_ProcurementPurchaseOrderGenerated.sql
+++ de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5478170_sys_gh3055_delete_msg_Event_ProcurementPurchaseOrderGenerated.sql
@@ -1,3 +1,5 @@
+DELETE FROM AD_Note WHERE AD_Message_ID=543816;
+
 -- 2017-11-23T18:45:57.818
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 DELETE FROM  AD_Message_Trl WHERE AD_Message_ID=543816
