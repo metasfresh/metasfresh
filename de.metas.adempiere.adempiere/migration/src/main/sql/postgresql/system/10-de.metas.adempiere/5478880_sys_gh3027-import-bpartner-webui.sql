231b9d1a8803d36f16f8eaabd1df77efecf91334
Mark Krake <mark.krake@metasfresh.com>
2017-12-01 12:33:02 +0100

    #3027 Moved Element Group to Advanced edit
    Moved Element Group to Advanced edit
    
    https://github.com/metasfresh/metasfresh/issues/3027

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5478880_sys_gh3027-import-bpartner-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5478880_sys_gh3027-import-bpartner-webui.sql
@@ -955,3 +955,8 @@ INSERT INTO AD_UI_Section_Trl (AD_Language,AD_UI_Section_ID, Description,Name, I
 INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,540730,540547,TO_TIMESTAMP('2017-11-30 14:29:58','YYYY-MM-DD HH24:MI:SS'),100,'Y',10,TO_TIMESTAMP('2017-11-30 14:29:58','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+-- 2017-12-01T12:23:01.944
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET AD_UI_Column_ID=540730, SeqNo=10,Updated=TO_TIMESTAMP('2017-12-01 12:23:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=541262
+;
+
