576b6aac72f9d939eb0eb4ea138d79e84267aaf6
Teo Sarca <teo.sarca@metasfresh.com>
2017-12-07 16:47:53 +0200

    set the first flatrate conditions to whole group when the compensation group is created; refactoring
    https://github.com/metasfresh/metasfresh/issues/3150

--- de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_OrderLine.java
+++ de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_OrderLine.java
@@ -32,6 +32,7 @@ import org.adempiere.exceptions.AdempiereException;
 import org.compiere.Adempiere;
 import org.compiere.model.I_C_Order;
 import org.compiere.model.ModelValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import de.metas.contracts.subscription.model.I_C_OrderLine;
 import de.metas.order.compensationGroup.GroupId;
@@ -48,6 +49,31 @@ public class C_OrderLine
 {
 	private static final ModelDynAttributeAccessor<I_C_OrderLine, Boolean> DYNATTR_SkipUpdatingGroupFlatrateConditions = new ModelDynAttributeAccessor<>("SkipUpdatingGroupFlatrateConditions", Boolean.class);
 
+	@Autowired
+	private OrderGroupRepository orderGroupsRepo;
+
+	public C_OrderLine()
+	{
+		Adempiere.autowire(this);
+	}
+
+	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_NEW })
+	public void setSameFlatrateConditionsForWholeCompensationGroupWhenGroupIsCreated(final I_C_OrderLine orderLine)
+	{
+		if (!orderLine.isGroupCompensationLine())
+		{
+			return;
+		}
+
+		final GroupId groupId = OrderGroupRepository.extractGroupId(orderLine);
+		final int flatrateConditionsId = retrieveFirstFlatrateConditionsIdForCompensationGroup(groupId);
+
+		orderLine.setC_Flatrate_Conditions_ID(flatrateConditionsId);
+
+		int excludeOrderLineId = orderLine.getC_OrderLine_ID();
+		setFlatrateConditionsIdToCompensationGroup(flatrateConditionsId, groupId, excludeOrderLineId);
+	}
+
 	/**
 	 * In case the flatrate conditions for an order line is updated and that line is part of an compensation group,
 	 * then set the same flatrate conditions to all other lines from the same compensation group.
@@ -55,7 +81,7 @@ public class C_OrderLine
 	 * @task https://github.com/metasfresh/metasfresh/issues/3150
 	 */
 	@ModelChange(timings = { ModelValidator.TYPE_AFTER_CHANGE }, ifColumnsChanged = I_C_OrderLine.COLUMNNAME_C_Flatrate_Conditions_ID)
-	public void setSameFlatrateConditionsForWholeCompensationGroup(final I_C_OrderLine orderLine)
+	public void setSameFlatrateConditionsForWholeCompensationGroupWhenOneGroupLineChanged(final I_C_OrderLine orderLine)
 	{
 		if (DYNATTR_SkipUpdatingGroupFlatrateConditions.getValue(orderLine, Boolean.FALSE))
 		{
@@ -69,10 +95,25 @@ public class C_OrderLine
 		}
 
 		final int flatrateConditionsId = orderLine.getC_Flatrate_Conditions_ID();
+		int excludeOrderLineId = orderLine.getC_OrderLine_ID();
+		setFlatrateConditionsIdToCompensationGroup(flatrateConditionsId, groupId, excludeOrderLineId);
+	}
 
-		final OrderGroupRepository orderGroupsRepo = Adempiere.getBean(OrderGroupRepository.class);
+	private int retrieveFirstFlatrateConditionsIdForCompensationGroup(final GroupId groupId)
+	{
+		return orderGroupsRepo.retrieveGroupOrderLinesQuery(groupId)
+				.addNotNull(I_C_OrderLine.COLUMNNAME_C_Flatrate_Conditions_ID)
+				.orderBy(I_C_OrderLine.COLUMNNAME_Line)
+				.orderBy(I_C_OrderLine.COLUMNNAME_C_OrderLine_ID)
+				.create()
+				.first(I_C_OrderLine.COLUMNNAME_C_Flatrate_Conditions_ID, Integer.class);
+	}
+
+	private void setFlatrateConditionsIdToCompensationGroup(final int flatrateConditionsId, final GroupId groupId, final int excludeOrderLineId)
+	{
 		orderGroupsRepo.retrieveGroupOrderLinesQuery(groupId)
-				.addNotEqualsFilter(I_C_OrderLine.COLUMN_C_OrderLine_ID, orderLine.getC_OrderLine_ID()) // skip this line
+				.addNotEqualsFilter(I_C_OrderLine.COLUMN_C_OrderLine_ID, excludeOrderLineId)
+				.addNotEqualsFilter(I_C_OrderLine.COLUMNNAME_C_Flatrate_Conditions_ID, flatrateConditionsId > 0 ? flatrateConditionsId : null)
 				.create()
 				.list(I_C_OrderLine.class)
 				.forEach(otherOrderLine -> {
