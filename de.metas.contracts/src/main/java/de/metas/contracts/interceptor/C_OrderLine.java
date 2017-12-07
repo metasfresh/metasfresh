469f34005b73c67b06ae0352f16da0b479fc658a
Teo Sarca <teo.sarca@metasfresh.com>
2017-12-07 16:48:42 +0200

    drop de.metas.contracts.interceptor.C_OrderLine.updateDataEntry
    as discussed and agreed with @metas-ts
    
    https://github.com/metasfresh/metasfresh/issues/3150

--- de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_OrderLine.java
+++ de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_OrderLine.java
@@ -5,32 +5,7 @@ import static org.adempiere.model.InterfaceWrapperHelper.save;
 import org.adempiere.ad.modelvalidator.annotations.Interceptor;
 import org.adempiere.ad.modelvalidator.annotations.ModelChange;
 import org.adempiere.ad.persistence.ModelDynAttributeAccessor;
-
-/*
- * #%L
- * de.metas.contracts
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
-import org.adempiere.exceptions.AdempiereException;
 import org.compiere.Adempiere;
-import org.compiere.model.I_C_Order;
 import org.compiere.model.ModelValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -121,22 +96,5 @@ public class C_OrderLine
 					DYNATTR_SkipUpdatingGroupFlatrateConditions.setValue(otherOrderLine, Boolean.TRUE);
 					save(otherOrderLine);
 				});
-	}
-
-	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_DELETE })
-	public void updateDataEntry(final I_C_OrderLine ol)
-	{
-		if (ol.isProcessed())
-		{
-			final I_C_Order order = ol.getC_Order();
-			final int subscriptionId = ol.getC_Flatrate_Conditions_ID();
-			if (subscriptionId <= 0)
-			{
-				// TODO figure out wtf this check and this error message mean
-				throw new AdempiereException("OrderLine " + ol.getLine()
-						+ " of order " + order.getDocumentNo()
-						+ " is processed, but I don't know why");
-			}
-		}
 	}
 }
