4ba15aea0c88824470ff7b9bac2605bdaaa78e52
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-12-07 17:24:51 +0200

    #1752 assign the AD_Element_WithColumnName valrule to AD_Column
    https://github.com/metasfresh/metasfresh/issues/1752

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5479850_sys_gh1752_AD_Element_WithColumnName_ValRule_In_AD_Column.sql
@@ -0,0 +1,5 @@
+-- 2017-12-07T17:05:14.199
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Val_Rule_ID=540382,Updated=TO_TIMESTAMP('2017-12-07 17:05:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2608
+;
+
