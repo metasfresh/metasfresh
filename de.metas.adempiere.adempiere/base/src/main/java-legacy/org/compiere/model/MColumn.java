468f508d1b7d521c713e90e1c7c0fb8caf56a066
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-12-08 16:12:32 +0200

    #1752 delete harcoded condition for centrallyMainatained
    https://github.com/metasfresh/metasfresh/issues/1752

--- de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/MColumn.java
+++ de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/MColumn.java
@@ -335,8 +335,7 @@ public class MColumn extends X_AD_Column
 						.append(DB.TO_STRING(getName()))
 						.append(", Description=").append(DB.TO_STRING(getDescription()))
 						.append(", Help=").append(DB.TO_STRING(getHelp()))
-						.append(" WHERE AD_Column_ID=").append(get_ID())
-						.append(" AND IsCentrallyMaintained='Y'");
+						.append(" WHERE AD_Column_ID=").append(get_ID());
 				int no = DB.executeUpdate(sql.toString(), get_TrxName());
 				log.debug("afterSave - Fields updated #" + no);
 			}
