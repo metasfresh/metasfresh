3223d4317eb6bdc06fa330096bbeb63330a81f40
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-12-13 19:30:17 +0200

    #3155 Update field names
    #3155 Create special pharma import product table

---
+++ de.metas.vertical.pharma/src/main/sql/postgresql/system/5480331_sys_gh3155_UpdatFieldNames.sql
@@ -0,0 +1,87 @@
+UPDATE AD_Field SET Name = 'A00SSATZ - Statusindikator Artikelsatz'  WHERE Name= 'A00SSATZ';
+UPDATE AD_Field SET Name = 'A00PZN - PZN Artikel'  WHERE Name= 'A00PZN';
+UPDATE AD_Field SET Name = 'A00GDAT - Gültigkeit Artikelgrunddaten'  WHERE Name= 'A00GDAT';
+UPDATE AD_Field SET Name = 'A00PBEZ - Produktbezeichnung'  WHERE Name= 'A00PBEZ';
+UPDATE AD_Field SET Name = 'A00PGMENG - Packungsgröße Menge'  WHERE Name= 'A00PGMENG';
+UPDATE AD_Field SET Name = 'A00PGEINH - Packungsgröße Einheit'  WHERE Name= 'A00PGEINH';
+UPDATE AD_Field SET Name = 'A00DARFO - Darreichungsform'  WHERE Name= 'A00DARFO';
+UPDATE AD_Field SET Name = 'A00ANBNR4 - Anbieter-Nr.'  WHERE Name= 'A00ANBNR4';
+UPDATE AD_Field SET Name = 'A00ATYP - Kennzeichen Artikeltyp'  WHERE Name= 'A00ATYP';
+UPDATE AD_Field SET Name = 'A00LKZ - Löschkennzeichen Artikel'  WHERE Name= 'A00LKZ';
+UPDATE AD_Field SET Name = 'A00GTIN - GTIN/UPC'  WHERE Name= 'A00GTIN';
+UPDATE AD_Field SET Name = 'A00ARTNR - Artikel-Nr.'  WHERE Name= 'A00ARTNR';
+UPDATE AD_Field SET Name = 'A00HMNR - Hilfsmittelpositions-nummer'  WHERE Name= 'A00HMNR';
+UPDATE AD_Field SET Name = 'A00PNAM - Produktname'  WHERE Name= 'A00PNAM';
+UPDATE AD_Field SET Name = 'A00WGA - Kennzeichen Waren-gruppe A'  WHERE Name= 'A00WGA';
+UPDATE AD_Field SET Name = 'A00PPN - Pharmacy Product Number'  WHERE Name= 'A00PPN';
+UPDATE AD_Field SET Name = 'A05GDAT - Gültigkeit Lagerungsinfos'  WHERE Name= 'A05GDAT';
+UPDATE AD_Field SET Name = 'A05VFDAT - Kennzeichen Verfalldatum'  WHERE Name= 'A05VFDAT';
+UPDATE AD_Field SET Name = 'A05LZEIT - Laufzeit'  WHERE Name= 'A05LZEIT';
+UPDATE AD_Field SET Name = 'A05KKETTE - Kennzeichen Kühlkette'  WHERE Name= 'A05KKETTE';
+UPDATE AD_Field SET Name = 'A05TMAX - Lagertemperatur max.'  WHERE Name= 'A05TMAX';
+UPDATE AD_Field SET Name = 'A05TMIN - Lagertemperatur min.'  WHERE Name= 'A05TMIN';
+UPDATE AD_Field SET Name = 'A05LICHT - Kennzeichen lichtempfindlich'  WHERE Name= 'A05LICHT';
+UPDATE AD_Field SET Name = 'A05FEUCHT - Kennzeichen feuchteempfindlich'  WHERE Name= 'A05FEUCHT';
+UPDATE AD_Field SET Name = 'A05LAGE - Kennzeichen lageempfindlich'  WHERE Name= 'A05LAGE';
+UPDATE AD_Field SET Name = 'A05ZERBR - Kennzeichen zerbrechlich'  WHERE Name= 'A05ZERBR';
+UPDATE AD_Field SET Name = 'A05EICH - Kennzeichen Eichung'  WHERE Name= 'A05EICH';
+UPDATE AD_Field SET Name = 'A05LZEICH - Laufzeit Eichung'  WHERE Name= 'A05LZEICH';
+UPDATE AD_Field SET Name = 'A04GDAT - Gültigkeit Packungsinfos'  WHERE Name= 'A04GDAT';
+UPDATE AD_Field SET Name = 'A04LAENGE - Länge'  WHERE Name= 'A04LAENGE';
+UPDATE AD_Field SET Name = 'A04HOEHE - Höhe'  WHERE Name= 'A04HOEHE';
+UPDATE AD_Field SET Name = 'A04BREITE - Breite'  WHERE Name= 'A04BREITE';
+UPDATE AD_Field SET Name = 'A04GWCHT - Gewicht'  WHERE Name= 'A04GWCHT';
+UPDATE AD_Field SET Name = 'A04VPART - Kennzeichen Verpackungsart'  WHERE Name= 'A04VPART';
+UPDATE AD_Field SET Name = 'A04MBEST - Mindestbestellmenge'  WHERE Name= 'A04MBEST';
+UPDATE AD_Field SET Name = 'A01GDAT - Gültigkeit Preisinfos'  WHERE Name= 'A01GDAT';
+UPDATE AD_Field SET Name = 'A01SKAEP - Statusindikator KAEP '  WHERE Name= 'A01SKAEP';
+UPDATE AD_Field SET Name = 'A01SAPU - Statusindikator APU '  WHERE Name= 'A01SAPU';
+UPDATE AD_Field SET Name = 'A01SAEP - Statusindikator AEP '  WHERE Name= 'A01SAEP';
+UPDATE AD_Field SET Name = 'A01SAVP - Statusindikator AVP'  WHERE Name= 'A01SAVP';
+UPDATE AD_Field SET Name = 'A01SUVP - Statusindikator UVP'  WHERE Name= 'A01SUVP';
+UPDATE AD_Field SET Name = 'A01SZBV - Statusindikator ZBV'  WHERE Name= 'A01SZBV';
+
+UPDATE AD_Field SET Name = 'A01SAB130A - Statusindikator Abschlag nach § 130a'  WHERE Name= 'A01SAB130A';
+UPDATE AD_Field SET Name = 'A01KAEP - KAEP'  WHERE Name= 'A01KAEP';
+UPDATE AD_Field SET Name = 'A01APU - APU'  WHERE Name= 'A01APU';
+UPDATE AD_Field SET Name = 'A01AMPVSGB - Kennzeichen AMPreisV SGB V'  WHERE Name= 'A01AMPVSGB';
+UPDATE AD_Field SET Name = 'A01AMPVAMG - Kennzeichen AMPreisV AMG'  WHERE Name= 'A01AMPVAMG';
+UPDATE AD_Field SET Name = 'A01AEP - AEP'  WHERE Name= 'A01AEP';
+UPDATE AD_Field SET Name = 'A01AVP - AVP'  WHERE Name= 'A01AVP';
+UPDATE AD_Field SET Name = 'A01UVP - UVP'  WHERE Name= 'A01UVP';
+UPDATE AD_Field SET Name = 'A01ZBV - ZBV'  WHERE Name= 'A01ZBV';
+UPDATE AD_Field SET Name = 'A01MWST - Kennzeichen MwST-Satz'  WHERE Name= 'A01MWST';
+UPDATE AD_Field SET Name = 'A01PPU - PPU'  WHERE Name= 'A01PPU';
+UPDATE AD_Field SET Name = 'A01AB130A2 - Abschlag nach § 130a'  WHERE Name= 'A01AB130A2';
+UPDATE AD_Field SET Name = 'A01APU783A - APU § 78 (3a) Satz 1'  WHERE Name= 'A01APU783A';
+UPDATE AD_Field SET Name = 'A01AEPPPU - AEP PPU'  WHERE Name= 'A01AEPPPU';
+UPDATE AD_Field SET Name = 'A01AVPPPU - AVP PPU'  WHERE Name= 'A01AVPPPU';
+
+
+
+
+UPDATE AD_Field SET Name = 'A01DPPUAPU - Differenz zwischen dem PPU und APU'  WHERE Name= 'A01DPPUAPU';
+UPDATE AD_Field SET Name = 'A01RESERV1 - Reservefeld 1'  WHERE Name= 'A01RESERV1';
+UPDATE AD_Field SET Name = 'A01RESERV2 - Reservefeld 2'  WHERE Name= 'A01RESERV2';
+UPDATE AD_Field SET Name = 'A01RESERV3 - Reservefeld 3'  WHERE Name= 'A01RESERV3';
+UPDATE AD_Field SET Name = 'A02GDAT - Gültigkeit Rechtsinfos'  WHERE Name= 'A02GDAT';
+UPDATE AD_Field SET Name = 'A02AM - Kennzeichen Arzneimittel'  WHERE Name= 'A02AM';
+UPDATE AD_Field SET Name = 'A02MP - Kennzeichen Medizinprodukt'  WHERE Name= 'A02MP';
+UPDATE AD_Field SET Name = 'A02APPFL - Kennzeichen apothekenpflichtig'  WHERE Name= 'A02APPFL';
+UPDATE AD_Field SET Name = 'A02VSPFL - Kennzeichen verschreibungspflichtig'  WHERE Name= 'A02VSPFL';
+UPDATE AD_Field SET Name = 'A02BTM - Kennzeichen BTM'  WHERE Name= 'A02BTM';
+UPDATE AD_Field SET Name = 'A02BOPST - BOPST-Nr.'  WHERE Name= 'A02BOPST';
+UPDATE AD_Field SET Name = 'A02TFG - Kennzeichen TFG'  WHERE Name= 'A02TFG';
+UPDATE AD_Field SET Name = 'A02DROCH - Kennzeichen Droge / Chemikalie'  WHERE Name= 'A02DROCH';
+UPDATE AD_Field SET Name = 'A02TIERAM - Kennzeichen Tierarzneimittel'  WHERE Name= 'A02TIERAM';
+UPDATE AD_Field SET Name = 'A02NEGLIS - Kennzeichen Negativliste'  WHERE Name= 'A02NEGLIS';
+UPDATE AD_Field SET Name = 'A02HMZV - Kennzeichen Hilfsmittel zum Verbrauch'  WHERE Name= 'A02HMZV';
+UPDATE AD_Field SET Name = 'A02LIFSTYL - Kennzeichen Life-Style'  WHERE Name= 'A02LIFSTYL';
+UPDATE AD_Field SET Name = 'A02HOMOEO - Kennzeichen Homöopathikum'  WHERE Name= 'A02HOMOEO';
+UPDATE AD_Field SET Name = 'A02ANTHRO - Kennzeichen Anthroposophikum'  WHERE Name= 'A02ANTHRO';
+
+
+
+
+
+
