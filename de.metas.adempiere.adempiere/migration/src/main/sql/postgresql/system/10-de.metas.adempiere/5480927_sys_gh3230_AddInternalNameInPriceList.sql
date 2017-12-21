1957e990619d8ddc3af83f12e0ae12290d968ec4
Cristina Ghita <cristina.ghita@metasfresh.com>
2017-12-21 17:06:13 +0200

    #3230 Show internal name in window
    #3230 Pharma: import prices

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5480927_sys_gh3230_AddInternalNameInPriceList.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5480927_sys_gh3230_AddInternalNameInPriceList.sql
@@ -19,3 +19,26 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 UPDATE AD_Column SET EntityType='D',Updated=TO_TIMESTAMP('2017-12-21 13:50:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558398
 ;
 
+
+
+-- 2017-12-21T15:39:07.422
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,558398,561229,0,540776,0,TO_TIMESTAMP('2017-12-21 15:39:07','YYYY-MM-DD HH24:MI:SS'),100,0,'D',0,'Y','Y','Y','N','N','N','N','N','d',10,10,0,1,1,TO_TIMESTAMP('2017-12-21 15:39:07','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-12-21T15:39:07.433
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=561229 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2017-12-21T16:30:07.590
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,561229,0,540776,549850,540088,'F',TO_TIMESTAMP('2017-12-21 16:30:07','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N','Interner Name',30,0,0,TO_TIMESTAMP('2017-12-21 16:30:07','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-12-21T16:30:12.780
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Interner Name',Updated=TO_TIMESTAMP('2017-12-21 16:30:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=561229
+;
+
+
