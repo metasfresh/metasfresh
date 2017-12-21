00a4d51bc66f3f96e6b58cb54d90e610d82876e6
Mark Krake <mark.krake@metasfresh.com>
2017-12-21 15:10:32 +0100

    #3239 Adjusting the windo Layout for material receipt candidates
    Adjusting the windo Layout for material receipt candidates
    
    https://github.com/metasfresh/metasfresh/issues/3239

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5481150_sys_gh3239-material-receipt-candidates-readonly-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5481150_sys_gh3239-material-receipt-candidates-readonly-webui.sql
@@ -113,3 +113,53 @@ UPDATE AD_Field SET IsSameLine='N',Updated=TO_TIMESTAMP('2017-12-21 14:58:53','Y
 UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2017-12-21 15:00:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=554193
 ;
 
+-- 2017-12-21T15:01:54.812
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Section (AD_Client_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy,Value) VALUES (0,0,540526,540571,TO_TIMESTAMP('2017-12-21 15:01:54','YYYY-MM-DD HH24:MI:SS'),100,'Y',20,TO_TIMESTAMP('2017-12-21 15:01:54','YYYY-MM-DD HH24:MI:SS'),100,'advanced edit')
+;
+
+-- 2017-12-21T15:01:54.813
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Section_Trl (AD_Language,AD_UI_Section_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_UI_Section_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_UI_Section t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_UI_Section_ID=540571 AND NOT EXISTS (SELECT 1 FROM AD_UI_Section_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_UI_Section_ID=t.AD_UI_Section_ID)
+;
+
+-- 2017-12-21T15:01:57.844
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,540764,540571,TO_TIMESTAMP('2017-12-21 15:01:57','YYYY-MM-DD HH24:MI:SS'),100,'Y',10,TO_TIMESTAMP('2017-12-21 15:01:57','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-12-21T15:02:19.581
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET AD_UI_Column_ID=540764, SeqNo=10,Updated=TO_TIMESTAMP('2017-12-21 15:02:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540129
+;
+
+-- 2017-12-21T15:05:05.420
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2017-12-21 15:05:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540129
+;
+
+-- 2017-12-21T15:06:08.436
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,540091,541315,TO_TIMESTAMP('2017-12-21 15:06:08','YYYY-MM-DD HH24:MI:SS'),100,'Y','target',20,TO_TIMESTAMP('2017-12-21 15:06:08','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2017-12-21T15:06:53.420
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541315, SeqNo=10,Updated=TO_TIMESTAMP('2017-12-21 15:06:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541821
+;
+
+-- 2017-12-21T15:07:09.196
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541315, SeqNo=20,Updated=TO_TIMESTAMP('2017-12-21 15:07:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541844
+;
+
+-- 2017-12-21T15:07:43.886
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541315, SeqNo=30,Updated=TO_TIMESTAMP('2017-12-21 15:07:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541847
+;
+
+-- 2017-12-21T15:07:56.633
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541315, SeqNo=40,Updated=TO_TIMESTAMP('2017-12-21 15:07:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=542028
+;
+
