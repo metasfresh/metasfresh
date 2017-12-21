2e661e2145bf065441cfd47ef88cb3f5fdd8d2a8
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-12-21 18:04:00 +0200

    #3232 Delete the relation types between invoice candidate and inventory lines
    https://github.com/metasfresh/metasfresh/issues/3232

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5481230_sys_gh3232_Delete_IRelations_Between_InvoiceCandidate_And_Inventory.sql
@@ -0,0 +1,10 @@
+-- 2017-12-21T18:01:29.058
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_RelationType WHERE AD_RelationType_ID=540182
+;
+
+-- 2017-12-21T18:01:32.134
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_RelationType WHERE AD_RelationType_ID=540183
+;
+
