2b1648507d9192faedcb6858e68c3d1c2cf13ce8
metas-rc <ruxandra.craciunescu@metasfresh.com>
2017-12-21 18:03:24 +0200

    #3232 Delete the invoice candidate handlers for inventory
    https://github.com/metasfresh/metasfresh/issues/3232

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5481200_sys_gh3232_Delete_Invoice_CandidateHandlers_Inventory.sql
@@ -0,0 +1,10 @@
+-- 2017-12-21T17:43:56.010
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM C_ILCandHandler WHERE C_ILCandHandler_ID=540016
+;
+
+-- 2017-12-21T17:44:01.841
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM C_ILCandHandler WHERE C_ILCandHandler_ID=540017
+;
+
