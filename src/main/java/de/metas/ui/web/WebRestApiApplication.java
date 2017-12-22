a47654a20e5753d33c45cbdd752f60215892d9b5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2017-12-22 11:21:42 +0100

    clear temporary hack from master
    Show onhand quantity in new WebUI MRP Product Info Window https://github.com/metasfresh/metasfresh-webui-api/issues/762

--- src/main/java/de/metas/ui/web/WebRestApiApplication.java
+++ src/main/java/de/metas/ui/web/WebRestApiApplication.java
@@ -25,7 +25,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 import de.metas.Profiles;
 import de.metas.ui.web.base.model.I_T_WEBUI_ViewSelection;
-import de.metas.ui.web.material.cockpit.event.MaterialCockpitEventListener;
 import de.metas.ui.web.session.WebRestApiContextProvider;
 import de.metas.ui.web.window.model.DocumentInterfaceWrapperHelper;
 
@@ -96,9 +95,6 @@ public class WebRestApiApplication
 		adempiere.startup(RunMode.WEBUI);
 
 		Services.get(IMigrationLogger.class).addTableToIgnoreList(I_T_WEBUI_ViewSelection.Table_Name);
-
-		// Qnd hack to get this lazy service initialized
-		final MaterialCockpitEventListener materialCockpitEventListener = Adempiere.getBean(MaterialCockpitEventListener.class);
 
 		return adempiere;
 	}
