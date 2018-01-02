7bea26e60149d444d6f043be30df157dbdf77860
Mark Krake <mark.krake@metasfresh.com>
2018-01-02 11:35:06 +0100

    #3267 Adding the Period create action to Calendar Window in Subtab Year
    Adding the Period create action to Calendar Window in Subtab Year
    
    https://github.com/metasfresh/metasfresh/issues/3267

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5481420_sys_gh3267-period-action-calendar-webui.sql
@@ -0,0 +1,30 @@
+-- 2018-01-02T11:28:00.610
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,100,177,TO_TIMESTAMP('2018-01-02 11:28:00','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2018-01-02 11:28:00','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
+;
+
+-- 2018-01-02T11:30:11.352
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-02 11:30:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=545913
+;
+
+-- 2018-01-02T11:30:11.356
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2018-01-02 11:30:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=545914
+;
+
+-- 2018-01-02T11:30:11.358
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2018-01-02 11:30:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=545915
+;
+
+-- 2018-01-02T11:30:23.626
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=545913
+;
+
+-- 2018-01-02T11:31:17.244
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET WidgetSize='S',Updated=TO_TIMESTAMP('2018-01-02 11:31:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=545911
+;
+
