ff43ffe4e3969c8ff1d54f5a434653c732adf5f3
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-01-05 09:47:57 +0100

    fix file name to match class name
    https://github.com/metasfresh/metasfresh-webui-api/issues/768

--- de.metas.shipper.gateway.dpd-legacy/src/main/java/de/metas/dpd/service/IDPDRoutingService.java
+++ de.metas.shipper.gateway.dpd-legacy/src/main/java/de/metas/dpd/service/IDPDRoutingService.java
@@ -12,12 +12,12 @@ import java.math.BigDecimal;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -32,7 +32,7 @@ import org.compiere.model.I_M_Package;
 import de.metas.inout.model.I_M_InOut;
 
 /**
- * 
+ *
  * @author ts
  * @see "<a href='http://dewiki908/mediawiki/index.php/Transportverpackung_%282009_0022_G61%29'>(2009_0022_G61)</a>"
  */
