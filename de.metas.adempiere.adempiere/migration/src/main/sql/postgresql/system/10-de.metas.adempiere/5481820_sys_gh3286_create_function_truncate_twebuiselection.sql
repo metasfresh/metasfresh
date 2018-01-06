7c49b266a2e5163bdcf52000e7b0de1ca88dd5f7
metasnw <nw@metasfresh.com>
2018-01-06 17:02:20 +0100

    #3286 create function truncate twebuiselection

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5481820_sys_gh3286_create_function_truncate_twebuiselection.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION trunc_twebuiselection()  returns integer
+LANGUAGE plpgsql
+AS $$
+DECLARE
+BEGIN
+        truncate t_webui_viewselection;
+        truncate t_webui_viewselectionline;
+	RETURN 0;
+END;
+$$;
