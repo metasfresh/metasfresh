741885f2d71be4ff966f4c8a83c04a6c48327f74
Mark Krake <mark.krake@metasfresh.com>
2018-01-09 14:01:27 +0100

    #3293 Adding Messaged for Apply, Cancel and Custom
    Adding Messaged for Apply, Cancel and Custom
    
    https://github.com/metasfresh/metasfresh/issues/3293

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5481950_sys_gh3293-webui-translation-messages.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5481950_sys_gh3293-webui-translation-messages.sql
@@ -123,4 +123,49 @@ UPDATE AD_Message SET Value='webui.window.daterange.thismonth',Updated=TO_TIMEST
 UPDATE AD_Message SET Value='webui.window.daterange.yesterday',Updated=TO_TIMESTAMP('2018-01-09 13:39:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544620
 ;
 
+-- 2018-01-09T13:53:05.537
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgTip,MsgType,Updated,UpdatedBy,Value) VALUES (0,544625,0,TO_TIMESTAMP('2018-01-09 13:53:05','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web','Y','Individuell','','I',TO_TIMESTAMP('2018-01-09 13:53:05','YYYY-MM-DD HH24:MI:SS'),100,'webui.window.daterange.custom')
+;
+
+-- 2018-01-09T13:53:05.541
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=544625 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2018-01-09T13:53:17.056
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-09 13:53:17','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',MsgText='Custom' WHERE AD_Message_ID=544625 AND AD_Language='en_US'
+;
+
+-- 2018-01-09T13:58:41.671
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgTip,MsgType,Updated,UpdatedBy,Value) VALUES (0,544626,0,TO_TIMESTAMP('2018-01-09 13:58:41','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web','Y','Anwenden','','I',TO_TIMESTAMP('2018-01-09 13:58:41','YYYY-MM-DD HH24:MI:SS'),100,'webui.window.daterange.apply')
+;
+
+-- 2018-01-09T13:58:41.676
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=544626 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2018-01-09T13:58:49.325
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-09 13:58:49','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',MsgText='Apply' WHERE AD_Message_ID=544626 AND AD_Language='en_US'
+;
+
+-- 2018-01-09T13:59:23.046
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgTip,MsgType,Updated,UpdatedBy,Value) VALUES (0,544627,0,TO_TIMESTAMP('2018-01-09 13:59:22','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web','Y','Abbrechen','','I',TO_TIMESTAMP('2018-01-09 13:59:22','YYYY-MM-DD HH24:MI:SS'),100,'webui.window.daterange.cancel')
+;
+
+-- 2018-01-09T13:59:23.049
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=544627 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2018-01-09T13:59:30.848
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-09 13:59:30','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',MsgText='Cancel' WHERE AD_Message_ID=544627 AND AD_Language='en_US'
+;
+
 
