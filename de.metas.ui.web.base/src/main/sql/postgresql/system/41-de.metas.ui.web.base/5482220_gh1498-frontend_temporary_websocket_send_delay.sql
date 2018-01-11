ef193cc409d504f95e32726455456780b3a5770d
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-01-11 18:07:43 +0100

    #1498 add AD_Sysconfig that allows us to set Websocket send-delay
    frontend subscribscriptions to api's view topic get out of hand https://github.com/metasfresh/metasfresh-webui-frontend/issues/1498

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5482220_gh1498-frontend_temporary_websocket_send_delay.sql
@@ -0,0 +1,7 @@
+
+-- 2018-01-11T17:45:40.490
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541188,'S',TO_TIMESTAMP('2018-01-11 17:45:40','YYYY-MM-DD HH24:MI:SS'),100,'Temprary attempt to work around
+issue frontend subscribscriptions to api''s view topic get out of hand https://github.com/metasfresh/metasfresh-webui-frontend/issues/1498','de.metas.ui.web','Y','de.metas.ui.web.websocket.WebsocketSender.sendEventDelayMillis',TO_TIMESTAMP('2018-01-11 17:45:40','YYYY-MM-DD HH24:MI:SS'),100,'500')
+;
+
