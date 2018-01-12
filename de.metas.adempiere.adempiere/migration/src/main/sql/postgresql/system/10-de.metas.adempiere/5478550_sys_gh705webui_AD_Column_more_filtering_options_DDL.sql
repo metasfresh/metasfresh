9074f39b57bd8c47b2b9fe2024470ced392ef0fe
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2018-01-12 12:26:19 +0100

    solve a secondary rollout issue

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5478550_sys_gh705webui_AD_Column_more_filtering_options_DDL.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5478550_sys_gh705webui_AD_Column_more_filtering_options_DDL.sql
@@ -1,15 +1,40 @@
+--thanks to/further reading: https://stackoverflow.com/a/12608570/1012103
+DO $$ 
+    BEGIN
+        BEGIN
 -- 2017-11-27T16:40:14.774
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-/* DDL */ SELECT public.db_alter_table('AD_Column','ALTER TABLE public.AD_Column ADD COLUMN IsRangeFilter CHAR(1) DEFAULT ''N'' CHECK (IsRangeFilter IN (''Y'',''N'')) NOT NULL')
-;
+/* DDL */ ALTER TABLE public.AD_Column ADD COLUMN IsRangeFilter CHAR(1) DEFAULT 'N' CHECK (IsRangeFilter IN ('Y','N')) NOT NULL;
 
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column IsRangeFilter already exists in AD_Column.';
+        END;
+    END;
+$$;
+
+DO $$ 
+    BEGIN
+        BEGIN
 -- 2017-11-27T16:41:03.523
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-/* DDL */ SELECT public.db_alter_table('AD_Column','ALTER TABLE public.AD_Column ADD COLUMN IsShowFilterIncrementButtons CHAR(1) DEFAULT ''N'' CHECK (IsShowFilterIncrementButtons IN (''Y'',''N'')) NOT NULL')
-;
+ALTER TABLE public.AD_Column ADD COLUMN IsShowFilterIncrementButtons CHAR(1) DEFAULT 'N' CHECK (IsShowFilterIncrementButtons IN ('Y','N')) NOT NULL;
 
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column IsShowFilterIncrementButtons already exists in AD_Column.';
+        END;
+    END;
+$$;
+
+
+DO $$ 
+    BEGIN
+        BEGIN
 -- 2017-11-27T16:41:34.856
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-/* DDL */ SELECT public.db_alter_table('AD_Column','ALTER TABLE public.AD_Column ADD COLUMN FilterDefaultValue VARCHAR(255)')
-;
+/* DDL */ ALTER TABLE public.AD_Column ADD COLUMN FilterDefaultValue VARCHAR(255);
 
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column FilterDefaultValue already exists in AD_Column.';
+        END;
+    END;
+$$;
