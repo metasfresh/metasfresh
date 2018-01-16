57fefdb2b52d4ef00ac34ac2a66c4b5e8f1e3317
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-01-16 17:01:34 +0200

    #3302 Add new client aggregation rule
    #3302 Payment Discount on Orderline, Invoice Candidate

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5482512_sys_3302_AddNewLineInInvoicingAggregationRule.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5482512_sys_3302_AddNewLineInInvoicingAggregationRule.sql
@@ -2,3 +2,11 @@
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO C_AggregationItem (AD_Client_ID,AD_Column_ID,AD_Org_ID,C_Aggregation_ID,C_AggregationItem_ID,Created,CreatedBy,EntityType,IsActive,Type,Updated,UpdatedBy) VALUES (0,558578,0,540003,540027,TO_TIMESTAMP('2018-01-12 17:44:37','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.invoicecandidate','Y','COL',TO_TIMESTAMP('2018-01-12 17:44:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
+
+
+
+-- 2018-01-16T13:33:42.120
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM C_AggregationItem WHERE C_AggregationItem_ID=540027
+;
+
