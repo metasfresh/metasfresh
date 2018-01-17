02088e941dd688cea6e7666776c463b4758bf85b
Teo Sarca <teo.sarca@metasfresh.com>
2018-01-17 20:16:44 +0200

    avoid WS disconnect/connect if topic didn't actually changed
    https://github.com/metasfresh/metasfresh-webui-frontend/issues/1498

--- src/actions/WindowActions.js
+++ src/actions/WindowActions.js
@@ -1227,12 +1227,30 @@ export function collapsedMap(node, isCollapsed, initialMap) {
   return collapsedMap;
 }
 
-export function connectWS(topic, cb) {
+export function connectWS(topic, onMessageCallback) {
+  // Avoid disconnecting and reconnecting to same topic.
+  // IMPORTANT: we assume the "onMessageCallback" is same
+  if (this.sockTopic === topic) {
+    // console.log("WS: Skip subscribing because already subscrinbed to %s", this.sockTopic);
+    return;
+  }
+
   const subscribe = ({ tries = 3 } = {}) => {
     if (this.sockClient.connected || tries <= 0) {
       this.sockSubscription = this.sockClient.subscribe(topic, msg => {
-        cb(JSON.parse(msg.body));
+        // console.log("WS: Got event on %s: %s", topic, msg.body);
+        if (topic === this.sockTopic) {
+          onMessageCallback(JSON.parse(msg.body));
+        } else {
+          // console.warn(
+          //   "Discard event because the WS topic changed. Current WS topic is %s",
+          //   this.sockTopic
+          // );
+        }
       });
+
+      this.sockTopic = topic;
+      // console.log("WS: Subscribed to %s (tries=%s)", this.sockTopic, tries);
     } else {
       // not ready yet
       setTimeout(() => {
@@ -1240,26 +1258,29 @@ export function connectWS(topic, cb) {
       }, 200);
     }
   };
+
   const connect = () => {
     this.sock = new SockJs(config.WS_URL);
     this.sockClient = Stomp.Stomp.over(this.sock);
     this.sockClient.debug = null;
     this.sockClient.connect({}, subscribe);
   };
-  const connected = disconnectWS.call(this, connect);
 
-  if (!connected) {
+  const wasConnected = disconnectWS.call(this, connect);
+  if (!wasConnected) {
     connect();
   }
 }
 
-export function disconnectWS(callback) {
+export function disconnectWS(onDisconnectCallback) {
   const connected =
     this.sockClient && this.sockClient.connected && this.sockSubscription;
 
   if (connected) {
+    // console.log("WS: Unsubscribing from %s", this.sockTopic);
     this.sockSubscription.unsubscribe();
-    this.sockClient.disconnect(callback);
+    this.sockClient.disconnect(onDisconnectCallback);
+    this.sockTopic = null;
   }
 
   return connected;
