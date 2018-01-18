e2756a769ffb3ec7f06319b34abdacec7b080985
Teo Sarca <teo.sarca@metasfresh.com>
2018-01-18 13:56:47 +0200

    generate costing related models
    https://github.com/metasfresh/metasfresh/issues/478

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5483190_sys_gh478_fix_trls.sql
@@ -0,0 +1,21 @@
+-- Generated with class de.metas.adempiere.tools.AD_Ref_List_ValueName_UpdateFromClass by tsa
+
+
+-- COSTELEMENTTYPE_
+UPDATE AD_Ref_List SET ValueName='Material' WHERE AD_Reference_ID=338 AND Value='M';
+UPDATE AD_Ref_List SET ValueName='Overhead' WHERE AD_Reference_ID=338 AND Value='O';
+UPDATE AD_Ref_List SET ValueName='BurdenMOverhead' WHERE AD_Reference_ID=338 AND Value='B';
+UPDATE AD_Ref_List SET ValueName='OutsideProcessing' WHERE AD_Reference_ID=338 AND Value='X';
+UPDATE AD_Ref_List SET ValueName='Resource' WHERE AD_Reference_ID=338 AND Value='R';
+
+
+-- COSTINGMETHOD_
+UPDATE AD_Ref_List SET ValueName='StandardCosting' WHERE AD_Reference_ID=122 AND Value='S';
+UPDATE AD_Ref_List SET ValueName='AveragePO' WHERE AD_Reference_ID=122 AND Value='A';
+UPDATE AD_Ref_List SET ValueName='Lifo' WHERE AD_Reference_ID=122 AND Value='L';
+UPDATE AD_Ref_List SET ValueName='Fifo' WHERE AD_Reference_ID=122 AND Value='F';
+UPDATE AD_Ref_List SET ValueName='LastPOPrice' WHERE AD_Reference_ID=122 AND Value='p';
+UPDATE AD_Ref_List SET ValueName='AverageInvoice' WHERE AD_Reference_ID=122 AND Value='I';
+UPDATE AD_Ref_List SET ValueName='LastInvoice' WHERE AD_Reference_ID=122 AND Value='i';
+UPDATE AD_Ref_List SET ValueName='UserDefined' WHERE AD_Reference_ID=122 AND Value='U';
+UPDATE AD_Ref_List SET ValueName='_' WHERE AD_Reference_ID=122 AND Value='x';
