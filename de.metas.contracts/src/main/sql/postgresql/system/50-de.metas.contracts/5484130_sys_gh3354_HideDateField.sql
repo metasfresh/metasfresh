7dea279f05d9407befa0c486f229b57a0fc73fba
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-01-30 12:22:47 +0200

    #3354 Hide date field if IsPermanentRecipient is ticked
    #3354 Different subscription Receiver for ever

---
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5484130_sys_gh3354_HideDateField.sql
@@ -0,0 +1,5 @@
+-- 2018-01-30T12:03:49.215
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET DisplayLogic='@IsPermanentRecipient@=N',Updated=TO_TIMESTAMP('2018-01-30 12:03:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541211
+;
+
