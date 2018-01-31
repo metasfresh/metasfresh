6c2e18d24c5baceefbfe22bda30b224ae52f540b
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-01-31 16:15:09 +0200

    #3413  Add default value for ApprovedBy
    #3413  Credit Limit data structure as subtab in Businesspartner

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5484320_sys_gh3413_CreateTableAndWindo_C_BPartner_CreditLimit.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5484320_sys_gh3413_CreateTableAndWindo_C_BPartner_CreditLimit.sql
@@ -969,3 +969,17 @@ UPDATE AD_Column SET IsParent='Y', IsUpdateable='N',Updated=TO_TIMESTAMP('2018-0
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2018-01-31 16:05:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=561832
 ;
+
+
+-- 2018-01-31T16:13:19.271
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET DefaultValue='@CreatedBy@',Updated=TO_TIMESTAMP('2018-01-31 16:13:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558980
+;
+
+-- 2018-01-31T16:13:35.739
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-01-31 16:13:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558980
+;
+
+
+
