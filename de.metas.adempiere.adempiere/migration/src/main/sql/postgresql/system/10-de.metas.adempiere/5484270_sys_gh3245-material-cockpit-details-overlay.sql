8dc48ed7e88efef172763d3685e146ed38c28e1b
Mark Krake <mark.krake@metasfresh.com>
2018-01-31 12:34:55 +0100

    .#3245 Adds Translations for en_US Language
    https://github.com/metasfresh/metasfresh/issues/3245

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5484270_sys_gh3245-material-cockpit-details-overlay.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5484270_sys_gh3245-material-cockpit-details-overlay.sql
@@ -23,3 +23,48 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP(
 UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2018-01-31 12:16:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550051
 ;
 
+-- 2018-01-31T12:30:37.761
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:30:37','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561321 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:30:42.529
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:30:42','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561322 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:30:46.284
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:30:46','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561323 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:30:51.637
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:30:51','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561315 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:30:56.293
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:30:56','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561317 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:31:07.651
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:31:07','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Subscription Progress' WHERE AD_Field_ID=561318 AND AD_Language='nl_NL'
+;
+
+-- 2018-01-31T12:31:48.557
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:31:48','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561314 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:32:02.145
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:32:02','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Material Cockpit' WHERE AD_Field_ID=561316 AND AD_Language='en_US'
+;
+
+-- 2018-01-31T12:32:06.785
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-01-31 12:32:06','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=561313 AND AD_Language='en_US'
+;
+
