dfe0187e416828911215e7538a123e89cfa5e9a1
Mark Krake <mark.krake@metasfresh.com>
2018-02-06 14:27:21 +0100

    .#3407 Adds menu Translation en_US for new Window
    https://github.com/metasfresh/metasfresh/issues/3407

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5484720_sys_gh3407-bpartner-pharma-window-webui.sql
@@ -0,0 +1,10 @@
+-- 2018-02-06T14:23:45.518
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-02-06 14:23:45','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Business Partner Pharma',Description='',Help='' WHERE AD_Window_ID=540409 AND AD_Language='en_US'
+;
+
+-- 2018-02-06T14:24:58.582
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-02-06 14:24:58','YYYY-MM-DD HH24:MI:SS'),Name='Business Partner Pharma',WEBUI_NameBrowse='Business Partner Pharma' WHERE AD_Menu_ID=541033 AND AD_Language='en_US'
+;
+
