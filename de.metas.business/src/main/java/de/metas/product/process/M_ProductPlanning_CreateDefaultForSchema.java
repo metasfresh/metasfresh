fbebecd8a9ac78f9abc20a1d20f16a16eabd090d
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-02-06 13:06:26 +0100

    Small changes
    * IProductPlanningSchemaDAO wasn't used from outside, so remove it
    * IProductDAO.retrieveProductsWithNoProductPlanning() also wasn't used from outside..add it to ProductPlanningSchemaDAO
      * also make it return an Iterator because we don't know how many products there will be
    * Add some nonNull and >0 checks here and there
    
    Set Selector in M_Product after Product Data Import https://github.com/metasfresh/metasfresh/issues/3406

--- de.metas.business/src/main/java/de/metas/product/process/M_ProductPlanning_CreateDefaultForSchema.java
+++ de.metas.business/src/main/java/de/metas/product/process/M_ProductPlanning_CreateDefaultForSchema.java
@@ -5,7 +5,6 @@ import org.adempiere.util.Services;
 
 import de.metas.process.JavaProcess;
 import de.metas.product.IProductPlanningSchemaBL;
-import de.metas.product.IProductPlanningSchemaDAO;
 import de.metas.product.model.I_M_Product_PlanningSchema;
 
 /*
@@ -18,12 +17,12 @@ import de.metas.product.model.I_M_Product_PlanningSchema;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -32,15 +31,14 @@ import de.metas.product.model.I_M_Product_PlanningSchema;
 
 public class M_ProductPlanning_CreateDefaultForSchema extends JavaProcess
 {
-	final IProductPlanningSchemaDAO productPlanningSchemaDAO = Services.get(IProductPlanningSchemaDAO.class);
-	final IProductPlanningSchemaBL productPlanningSchemaBL = Services.get(IProductPlanningSchemaBL.class);
+	private final IProductPlanningSchemaBL productPlanningSchemaBL = Services.get(IProductPlanningSchemaBL.class);
 
 	@Override
 	protected String doIt() throws Exception
 	{
 		final I_M_Product_PlanningSchema productPlanningSchema = getRecord(I_M_Product_PlanningSchema.class);
 		Check.assumeNotNull(productPlanningSchema, "@NoSelection@");
-		
+
 		productPlanningSchemaBL.createUpdateDefaultProductPlanningsForSchema(productPlanningSchema);
 
 
