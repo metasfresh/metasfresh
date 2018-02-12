5fba1d42fa980c7992d67ff9d3bd6155f15654c5
siemiatj <kuba@saskla.cz>
2018-02-12 09:46:23 +0100

    Add Enzyme for our UI test setup

--- src/__tests__/components/Shortcuts/generateHotkeys.test.js
+++ src/__tests__/components/Shortcuts/generateHotkeys.test.js
@@ -1,33 +1,32 @@
-import "babel-polyfill";
-import generateHotkeys from "../../../components/Shortcuts/generateHotkeys";
+import generateHotkeys from '../../../components/Shortcuts/generateHotkeys';
 
-describe("generateHotkeys", () => {
-  it("should return an object", () => {
+describe('generateHotkeys', () => {
+  it('should return an object', () => {
     const hotkeys = generateHotkeys({});
 
-    expect(typeof hotkeys).toBe("object");
+    expect(typeof hotkeys).toBe('object');
   });
 
-  it("should transform a key map to a map of hotkeys", () => {
+  it('should transform a key map to a map of hotkeys', () => {
     const keymap = {
-      COMBO_A: "CONTROL+A",
-      COMBO_B: "CONTROL+B"
+      COMBO_A: 'CONTROL+A',
+      COMBO_B: 'CONTROL+B'
     };
 
     const hotkeys = generateHotkeys({ keymap });
 
     expect(hotkeys).toEqual({
-      "CONTROL+A": [],
-      "CONTROL+B": []
+      'CONTROL+A': [],
+      'CONTROL+B': []
     });
   });
 
-  it("should warn about duplicate keys", () => {
-    jest.spyOn(global.console, "warn");
+  it('should warn about duplicate keys', () => {
+    jest.spyOn(global.console, 'warn');
 
     const keymap = {
-      COMBO_1: "CONTROL+A",
-      COMBO_2: "CONTROL+A"
+      COMBO_1: 'CONTROL+A',
+      COMBO_2: 'CONTROL+A'
     };
 
     generateHotkeys({ keymap });
@@ -35,15 +34,15 @@ describe("generateHotkeys", () => {
     expect(console.warn).toBeCalled();
   });
 
-  it("should warn about blacklisted hotkeys", () => {
-    jest.spyOn(global.console, "warn");
+  it('should warn about blacklisted hotkeys', () => {
+    jest.spyOn(global.console, 'warn');
 
     const blacklist = {
-      "CONTROL+W": "Close tab"
+      'CONTROL+W': 'Close tab'
     };
 
     const keymap = {
-      COMBO_1: "CONTROL+W"
+      COMBO_1: 'CONTROL+W'
     };
 
     generateHotkeys({ keymap, blacklist });
