35c409387ebdb32c6fdb398d392882c7c1b28a33
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-02-16 16:59:32 +0200

    #3490 Set approved
    #3490 Paperless Credit Limit approval

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/bpartner/process/BPartnerCreditLimit_Approve.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/bpartner/process/BPartnerCreditLimit_Approve.java
@@ -16,6 +16,7 @@ public class BPartnerCreditLimit_Approve extends JavaProcess implements IProcess
 	{
 		final I_C_BPartner_CreditLimit bpCreditLimit = getRecord(I_C_BPartner_CreditLimit.class);
 		bpCreditLimit.setApprovedBy_ID(getAD_User_ID());
+		bpCreditLimit.setIsApproved(true);
 		InterfaceWrapperHelper.save(bpCreditLimit);
 		return "@Success@";
 	}
