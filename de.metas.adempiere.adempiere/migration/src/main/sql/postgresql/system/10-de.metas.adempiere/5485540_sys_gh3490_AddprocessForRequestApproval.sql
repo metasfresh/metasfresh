a0528f4c23cecf57934489fc51f1e72be2fa728e
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-02-16 11:56:01 +0200

    #3490 Add message for request approval
    #3490 Paperless Credit Limit approval

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5485540_sys_gh3490_AddprocessForRequestApproval.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5485540_sys_gh3490_AddprocessForRequestApproval.sql
@@ -28,3 +28,13 @@ INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Hel
 INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,540922,540929,TO_TIMESTAMP('2018-02-15 17:08:57','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2018-02-15 17:08:57','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
 ;
 
+
+-- 2018-02-16T11:40:03.855
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,544642,0,TO_TIMESTAMP('2018-02-16 11:40:03','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Approve credit limit.','I',TO_TIMESTAMP('2018-02-16 11:40:03','YYYY-MM-DD HH24:MI:SS'),100,'org.adempiere.bpartner.process.BPartnerCreditLimit_RequestApproval')
+;
+
+-- 2018-02-16T11:40:03.858
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=544642 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
