2ac7cb750b543f25a2cc66dc23d79023b26c2235
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-02-16 14:33:50 +0200

    #3490 Tak out data fill
    #3490 Paperless Credit Limit approval

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5485541_sys_gh3490_AddWindowCreditLimit.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5485541_sys_gh3490_AddWindowCreditLimit.sql
@@ -1923,13 +1923,4 @@ UPDATE AD_TreeNodeMM SET Parent_ID=1000010, SeqNo=6, Updated=now(), UpdatedBy=10
 UPDATE AD_TreeNodeMM SET Parent_ID=1000010, SeqNo=7, Updated=now(), UpdatedBy=100 WHERE  Node_ID=1000050 AND AD_Tree_ID=10
 ;
 
--- 2018-02-16T11:50:37.995
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO C_CreditLimit_Type (AD_Client_ID,AD_Org_ID,C_CreditLimit_Type_ID,Created,CreatedBy,IsActive,IsAutoApproval,Name,SeqNo,Updated,UpdatedBy) VALUES (1000000,1000000,540000,TO_TIMESTAMP('2018-02-16 11:50:37','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Insurance',1,TO_TIMESTAMP('2018-02-16 11:50:37','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2018-02-16T11:50:55.810
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO C_CreditLimit_Type (AD_Client_ID,AD_Org_ID,C_CreditLimit_Type_ID,Created,CreatedBy,IsActive,IsAutoApproval,Name,SeqNo,Updated,UpdatedBy) VALUES (1000000,1000000,540001,TO_TIMESTAMP('2018-02-16 11:50:55','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Management',2.000000000000,TO_TIMESTAMP('2018-02-16 11:50:55','YYYY-MM-DD HH24:MI:SS'),100)
-;
 
