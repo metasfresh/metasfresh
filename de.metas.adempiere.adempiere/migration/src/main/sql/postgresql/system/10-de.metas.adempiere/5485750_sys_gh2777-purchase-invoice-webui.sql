1baec3903bbd0210ccef59ad403dba56049a79ef
Mark Krake <mark.krake@metasfresh.com>
2018-02-17 11:52:37 +0100

    .#2777 Adds Translations to Purchase Invoice
    Adds Translations for de_De, eb_US to Purchase Invoice Window.
    https://github.com/metasfresh/metasfresh/issues/2777

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5485750_sys_gh2777-purchase-invoice-webui.sql
@@ -0,0 +1,45 @@
+-- 2018-02-17T10:28:38.777
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-02-17 10:28:38','YYYY-MM-DD HH24:MI:SS'),Name='Document Type' WHERE AD_Field_ID=3344 AND AD_Language='en_US'
+;
+
+-- 2018-02-17T10:29:13.788
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2018-02-17 10:29:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3348
+;
+
+-- 2018-02-17T10:29:19.602
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-02-17 10:29:19','YYYY-MM-DD HH24:MI:SS'),Name='Date' WHERE AD_Field_ID=3348 AND AD_Language='en_US'
+;
+
+-- 2018-02-17T10:29:39.933
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-02-17 10:29:39','YYYY-MM-DD HH24:MI:SS'),Name='Status' WHERE AD_Field_ID=3335 AND AD_Language='en_US'
+;
+
+-- 2018-02-17T10:30:05.860
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-17 10:30:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540219
+;
+
+-- 2018-02-17T10:30:42.586
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-17 10:30:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540220
+;
+
+-- 2018-02-17T10:30:55.500
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-17 10:30:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540221
+;
+
+-- 2018-02-17T10:31:04.265
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-17 10:31:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540222
+;
+
+-- 2018-02-17T10:31:15.611
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-17 10:31:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540223
+;
+
