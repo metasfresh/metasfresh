e19eea34ff2d9cc735c6944f5556b0377aa4adb8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-02-19 17:37:33 +0100

    #3520 solve migration script issues
    Workpackage with error for Update Invalid Shipment Schedules https://github.com/metasfresh/metasfresh/issues/3520

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5486000_sys_gh3520-app_cleanup_M_ShipmentSchedules.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5486000_sys_gh3520-app_cleanup_M_ShipmentSchedules.sql
@@ -1,12 +1,15 @@
 
 --
--- delete m_shipmentschedule records whose referenced C_SubscriptionProgress records are already gone
+-- Update m_shipmentschedule records whose referenced C_SubscriptionProgress records are already gone
+-- Note: we can't delete them, because they are already referenced from e.g. ShipmentScheduleQtyPicked, M_HU_Trace etc
 --
-DELETE m_shipmentschedule 
+UPDATE m_shipmentschedule 
+SET 
+	IsActive='N', Updated=now(), updatedby=99,
+	Status='the referenced C_SubscriptionProgress is gone; see https://github.com/metasfresh/metasfresh/issues/3520'
 WHERE m_shipmentschedule_id IN 
 (
-	select M_ShipmentSchedule_ID 
-	from M_ShipmentSchedule sched
+	select sched.M_ShipmentSchedule_ID 
 	from M_ShipmentSchedule sched
 		left join C_SubscriptionProgress s ON 
 			sched.AD_table_ID=540029 /*C_SubscriptionProgress*/
