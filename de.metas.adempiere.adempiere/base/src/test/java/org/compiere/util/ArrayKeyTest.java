34c694fda531ecf15fe890c60e140aa902d730ec
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-02-20 15:24:31 +0100

    solve bug in CtxNames where defaultValue="NULL" could not be handeled
    ..by getValueAsDate()
    
    Also
    * add new method CtxNames.ofNameAndDefaultValue to avoid parsing where it is not needed
    
    fix available stock (ATP) display in sales order https://github.com/metasfresh/metasfresh-webui-api/issues/863

--- de.metas.adempiere.adempiere/base/src/test/java/org/compiere/util/ArrayKeyTest.java
+++ de.metas.adempiere.adempiere/base/src/test/java/org/compiere/util/ArrayKeyTest.java
@@ -1,5 +1,7 @@
 package org.compiere.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /*
  * #%L
  * de.metas.adempiere.adempiere.base
@@ -10,12 +12,12 @@ package org.compiere.util;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -24,6 +26,7 @@ package org.compiere.util;
 
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 import org.compiere.util.Util.ArrayKey;
 import org.junit.Assert;
@@ -36,8 +39,8 @@ public class ArrayKeyTest
 	{
 		final Timestamp date = TimeUtil.getDay(2013, 02, 05);
 		final ArrayKey key = new ArrayKey(
-				(Integer)5,
-				(String)"s1",
+				5,
+				"s1",
 				null,
 				date
 				);
@@ -45,6 +48,26 @@ public class ArrayKeyTest
 		final String keyStrExpected = "5#s1#NULL#2013-02-05 00:00:00.0";
 		final String keyStrActual = key.toString();
 		Assert.assertEquals(keyStrExpected, keyStrActual);
+	}
+
+	@Test
+	public void test_with_optionals()
+	{
+		final ArrayKey key1 = ArrayKey.of("s1", 5, Optional.of(true));
+		final ArrayKey key2 = ArrayKey.of("s1", 5, Optional.of(true));
+
+		assertThat(key1).isEqualTo(key2);
+
+		// Optional<true> is not equal to Optional<false> or an empty optional
+		final ArrayKey key3 = ArrayKey.of("s1", 5, Optional.of(false));
+		assertThat(key1).isNotEqualTo(key3);
+
+		final ArrayKey key4 = ArrayKey.of("s1", 5, Optional.empty());
+		assertThat(key1).isNotEqualTo(key4);
+
+		// two arrayKeys with empty optionals can be equal to each other
+		final ArrayKey key5 = ArrayKey.of("s1", 5, Optional.empty());
+		assertThat(key4).isEqualTo(key5);
 	}
 
 	@Test
