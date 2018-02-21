45e84a12a000949aa2f733a8b5f650716835f8c6
Teo Sarca <teo.sarca@metasfresh.com>
2018-02-21 14:56:20 +0200

    fix M_Product's column entity type
    https://github.com/metasfresh/metasfresh/issues/3430

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5486220_sys_gh3430_M_Product_fix_EntityTypes.sql
@@ -0,0 +1,15 @@
+-- 2018-02-21T14:48:28.403
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET EntityType='D',Updated=TO_TIMESTAMP('2018-02-21 14:48:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558983
+;
+
+-- 2018-02-21T14:51:08.137
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List SET EntityType='D',Updated=TO_TIMESTAMP('2018-02-21 14:51:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=541584
+;
+
+-- 2018-02-21T14:51:13.497
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Reference SET EntityType='D',Updated=TO_TIMESTAMP('2018-02-21 14:51:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=540829
+;
+
