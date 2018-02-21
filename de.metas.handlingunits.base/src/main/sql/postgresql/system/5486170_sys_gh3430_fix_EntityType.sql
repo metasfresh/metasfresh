1965687b3b47b465e16aced4cf6977d6dce54f75
Teo Sarca <teo.sarca@metasfresh.com>
2018-02-21 14:25:58 +0200

    fix EntityType
    https://github.com/metasfresh/metasfresh/issues/3430

---
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5486170_sys_gh3430_fix_EntityType.sql
@@ -0,0 +1,5 @@
+-- 2018-02-21T12:12:36.417
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET EntityType='de.metas.handlingunits',Updated=TO_TIMESTAMP('2018-02-21 12:12:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=557018
+;
+
