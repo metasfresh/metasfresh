99c8c92962f92af56fd777ea8176f4bddab6d71f
Mark Krake <mark.krake@metasfresh.com>
2018-02-26 12:33:17 +0100

    .#3562 Adds primary layout to subtabs
    Adds the primary layout to subtabs in Product planning window.
    https://github.com/metasfresh/metasfresh/issues/3562

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5486830_sys_gh3562_product-planning-subtab-primary.sql
@@ -0,0 +1,20 @@
+-- 2018-02-26T12:21:28.425
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-26 12:21:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540382
+;
+
+-- 2018-02-26T12:22:26.880
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-26 12:22:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540383
+;
+
+-- 2018-02-26T12:22:39.492
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-26 12:22:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540384
+;
+
+-- 2018-02-26T12:22:52.463
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_ElementGroup SET UIStyle='primary',Updated=TO_TIMESTAMP('2018-02-26 12:22:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=540385
+;
+
