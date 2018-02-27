335925d7a2bc3dcd66ed74ea111b459bc9b547b8
Teo Sarca <teo.sarca@metasfresh.com>
2018-02-27 17:26:55 +0200

    OLCandDAO: use query API
    https://github.com/metasfresh/metasfresh-webui-api/issues/664

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/ordercandidate/api/impl/OLCandDAO.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/ordercandidate/api/impl/OLCandDAO.java
@@ -10,27 +10,25 @@ package de.metas.ordercandidate.api.impl;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-
 import java.util.List;
 import java.util.Properties;
 
+import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.ad.table.api.IADTableDAO;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Check;
 import org.adempiere.util.Services;
-import org.compiere.model.Query;
-import org.compiere.util.DB;
 
 import de.metas.interfaces.I_C_OrderLine;
 import de.metas.ordercandidate.api.IOLCandDAO;
@@ -45,40 +43,36 @@ public class OLCandDAO implements IOLCandDAO
 		Check.assume(!Check.isEmpty(tableName), "Param 'tableName' is not empty");
 		Check.assume(recordId > 0, "Param 'recordId' is > 0");
 
-		final String whereClause = I_C_OLCand.COLUMNNAME_AD_Table_ID + "=? AND " + I_C_OLCand.COLUMNNAME_Record_ID + "=?";
-		final int tableID = Services.get(IADTableDAO.class).retrieveTableId(tableName);
+		final int tableId = Services.get(IADTableDAO.class).retrieveTableId(tableName);
 
-		return new Query(ctx, I_C_OLCand.Table_Name, whereClause, trxName)
-				.setParameters(tableID, recordId)
-				.setOnlyActiveRecords(true)
-				.setClient_ID()
-				.setOrderBy(I_C_OLCand.COLUMNNAME_C_OLCand_ID)
+		return Services.get(IQueryBL.class)
+				.createQueryBuilder(I_C_OLCand.class, ctx, trxName)
+				.addEqualsFilter(I_C_OLCand.COLUMN_AD_Table_ID, tableId)
+				.addEqualsFilter(I_C_OLCand.COLUMN_Record_ID, recordId)
+				.addOnlyActiveRecordsFilter()
+				.addOnlyContextClient()
+				.orderBy(I_C_OLCand.COLUMN_C_OLCand_ID)
+				.create()
 				.list(I_C_OLCand.class);
 	}
 
 	@Override
-	public <T extends I_C_OLCand> List<T> retrieveOLCands(
-			final I_C_OrderLine ol,
-			final Class<T> clazz)
+	public <T extends I_C_OLCand> List<T> retrieveOLCands(final I_C_OrderLine ol, final Class<T> clazz)
 	{
 		final Properties ctx = InterfaceWrapperHelper.getCtx(ol);
 		final String trxName = InterfaceWrapperHelper.getTrxName(ol);
+		final int orderLineId = ol.getC_OrderLine_ID();
 
-		final String wc = I_C_OLCand.COLUMNNAME_C_OLCand_ID + " IN (" +
-				"  select " + I_C_Order_Line_Alloc.COLUMNNAME_C_OLCand_ID +
-				"  from " + I_C_Order_Line_Alloc.Table_Name +
-				"  where " + I_C_Order_Line_Alloc.COLUMNNAME_C_OrderLine_ID + "=? AND " +
-				"     " + I_C_Order_Line_Alloc.COLUMNNAME_IsActive + "=" + DB.TO_STRING("Y") +
-				")";
-
-		final List<T> result = new Query(ctx, I_C_OLCand.Table_Name, wc, trxName)
-				.setParameters(ol.getC_OrderLine_ID())
-				.setOnlyActiveRecords(true)
-				.setClient_ID()
-				.setOrderBy(I_C_OLCand.COLUMNNAME_C_OLCand_ID)
+		return Services.get(IQueryBL.class)
+				.createQueryBuilder(I_C_Order_Line_Alloc.class, ctx, trxName)
+				.addEqualsFilter(I_C_Order_Line_Alloc.COLUMN_C_OrderLine_ID, orderLineId)
+				.addOnlyActiveRecordsFilter()
+				.andCollect(I_C_Order_Line_Alloc.COLUMN_C_OLCand_ID)
+				.addOnlyActiveRecordsFilter()
+				.addOnlyContextClient()
+				.orderBy(I_C_OLCand.COLUMN_C_OLCand_ID)
+				.create()
 				.list(clazz);
-
-		return result;
 	}
 
 	@Override
@@ -86,14 +80,14 @@ public class OLCandDAO implements IOLCandDAO
 	{
 		final Properties ctx = InterfaceWrapperHelper.getCtx(ol);
 		final String trxName = InterfaceWrapperHelper.getTrxName(ol);
+		final int orderLineId = ol.getC_OrderLine_ID();
 
-		final String wc = I_C_Order_Line_Alloc.COLUMNNAME_C_OrderLine_ID + "=?";
-
-		return new Query(ctx, I_C_Order_Line_Alloc.Table_Name, wc, trxName)
-				.setParameters(ol.getC_OrderLine_ID())
-				.setOnlyActiveRecords(false) // note that we also load records that are inactive or belong to
-											 // different AD_Clients
-				.setOrderBy(I_C_Order_Line_Alloc.COLUMNNAME_C_Order_Line_Alloc_ID)
+		return Services.get(IQueryBL.class)
+				.createQueryBuilder(I_C_Order_Line_Alloc.class, ctx, trxName)
+				.addEqualsFilter(I_C_Order_Line_Alloc.COLUMN_C_OrderLine_ID, orderLineId)
+				// .addOnlyActiveRecordsFilter() // note that we also load records that are inactive or belong to different AD_Clients
+				.orderBy(I_C_Order_Line_Alloc.COLUMN_C_Order_Line_Alloc_ID)
+				.create()
 				.list(I_C_Order_Line_Alloc.class);
 	}
 
@@ -102,14 +96,14 @@ public class OLCandDAO implements IOLCandDAO
 	{
 		final Properties ctx = InterfaceWrapperHelper.getCtx(olCand);
 		final String trxName = InterfaceWrapperHelper.getTrxName(olCand);
+		final int olCandId = olCand.getC_OLCand_ID();
 
-		final String wc = I_C_Order_Line_Alloc.COLUMNNAME_C_OLCand_ID + "=?";
-
-		return new Query(ctx, I_C_Order_Line_Alloc.Table_Name, wc, trxName)
-				.setParameters(olCand.getC_OLCand_ID())
-				.setOnlyActiveRecords(false) // note that we also load records that are inactive or belong to
-											 // different AD_Clients
-				.setOrderBy(I_C_Order_Line_Alloc.COLUMNNAME_C_Order_Line_Alloc_ID)
+		return Services.get(IQueryBL.class)
+				.createQueryBuilder(I_C_Order_Line_Alloc.class, ctx, trxName)
+				.addEqualsFilter(I_C_Order_Line_Alloc.COLUMN_C_OLCand_ID, olCandId)
+				// .addOnlyActiveRecordsFilter() // note that we also load records that are inactive or belong to different AD_Clients
+				.orderBy(I_C_Order_Line_Alloc.COLUMN_C_Order_Line_Alloc_ID)
+				.create()
 				.list(I_C_Order_Line_Alloc.class);
 	}
 }
