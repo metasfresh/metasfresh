a449a6f3b9fbe8d4ca707a98f08105f793a10aef
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-02-28 11:45:20 +0100

    Add api methods to enable one schedule invokaction at the end
    SubscriptionBL: minor
    
    Avoid mass-creation of "MissingShipmentSchedulesWorkpackages" https://github.com/metasfresh/metasfresh/issues/3582

--- de.metas.util/src/main/java/org/adempiere/util/lang/IAutoCloseable.java
+++ de.metas.util/src/main/java/org/adempiere/util/lang/IAutoCloseable.java
@@ -10,12 +10,12 @@ package org.adempiere.util.lang;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -25,13 +25,14 @@ package org.adempiere.util.lang;
 
 /**
  * Exactly the same as {@link AutoCloseable} but {@link #close()} method is not declared to throw exceptions.
- * 
+ *
  * The main benefit of this interface is that allows you to quickly create annonymous inner {@link AutoCloseable} instances,
  * which you can use in your try-with-resources block without having to declare an "catch" block.
- * 
+ *
  * @author tsa
  *
  */
+@FunctionalInterface
 public interface IAutoCloseable extends AutoCloseable
 {
 	@Override
