545191fa31ae71bfb2310f3602d3f9af20a99ea5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-03-01 08:37:04 +0100

    SQL fix; also split the sql file in two
    make shipment HU aggregation configurable https://github.com/metasfresh/metasfresh/issues/3575

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5487040_sys_gh3575-app_M_ShipmentSchedule_AttributeConfig.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5487040_sys_gh3575-app_M_ShipmentSchedule_AttributeConfig.sql
@@ -1,8 +1,4 @@
 
--- cleanup old handler records..the classes were renamed some time ago
-DELETE FROM M_IolCandHandler WHERE ClassName = 'de.metas.contracts.subscription.inoutcandidate.spi.impl.SubscriptionInOutCandHandler';
-DELETE FROM M_IolCandHandler WHERE ClassName = 'de.metas.inoutcandidate.spi.impl.OrderLineInOutCandHandler';
-
 -- 2018-02-27T15:28:20.700
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Table (AccessLevel,ACTriggerLength,AD_Client_ID,AD_Org_ID,AD_Table_ID,CopyColumnsFromTable,Created,CreatedBy,EntityType,ImportTable,IsActive,IsAutocomplete,IsChangeLog,IsDeleteable,IsDLM,IsHighVolume,IsSecurityEnabled,IsView,LoadSeq,Name,ReplicationType,TableName,Updated,UpdatedBy) VALUES ('7',0,0,0,540951,'N',TO_TIMESTAMP('2018-02-27 15:28:20','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.inoutcandidate','N','Y','N','Y','Y','N','N','N','N',0,'M_ShipmentSchedule_HUAttributeConfig','L','M_ShipmentSchedule_HUAttributeConfig',TO_TIMESTAMP('2018-02-27 15:28:20','YYYY-MM-DD HH24:MI:SS'),100)
@@ -383,43 +379,3 @@ UPDATE AD_Tab SET TabLevel=1,Updated=TO_TIMESTAMP('2018-02-28 14:50:54','YYYY-MM
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Tab SET Name='HU-Aggregation',Updated=TO_TIMESTAMP('2018-02-28 16:24:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541052
 ;
-
-
-
---
--- insert records according to the "template" packing instruction
---
-INSERT INTO m_shipmentschedule_attributeconfig (
-  ad_client_id, -- numeric(10,0) NOT NULL,
-  ad_org_id, -- numeric(10,0) NOT NULL,
-  created, -- timestamp with time zone NOT NULL,
-  createdby, -- numeric(10,0) NOT NULL,
-  isactive, -- character(1) NOT NULL,
-  m_attribute_id, -- numeric(10,0),
-  m_iolcandhandler_id, -- numeric(10,0) NOT NULL,
-  m_shipmentschedule_attributeconfig_id, -- numeric(10,0) NOT NULL,
-  onlyifinreferencedasi, -- character(1) NOT NULL,
-  updated, -- timestamp with time zone NOT NULL,
-  updatedby -- numeric(10,0) NOT NULL,
-)
-select --ha.M_Attribute_ID, a.Name
-  1000000 as ad_client_id, -- numeric(10,0) NOT NULL,
-  0 as ad_org_id, -- numeric(10,0) NOT NULL,
-  now() as created, -- timestamp with time zone NOT NULL,
-  99 as createdby, -- numeric(10,0) NOT NULL,
-  'Y' as isactive, -- character(1) NOT NULL,
-  ha.m_attribute_id, -- numeric(10,0),
-  iolch.m_iolcandhandler_id, -- numeric(10,0) NOT NULL,
-  nextval('m_shipmentschedule_attributeconfig_seq') as m_shipmentschedule_attributeconfig_id, -- numeric(10,0) NOT NULL,
-  'N' as onlyifinreferencedasi, -- character(1) NOT NULL,
-  now() as updated, -- timestamp with time zone NOT NULL,
-  99 as updatedby -- numeric(10,0) NOT NULL,
-from M_HU_PI_Attribute ha
-	join M_Attribute a ON a.M_Attribute_ID=ha.M_Attribute_ID and a.IsActive='Y'
-	join m_iolcandhandler iolch ON true
-where ha.M_HU_PI_Version_ID = 100 
-	AND ha.IsActive='Y' 
-	AND ha.UseInASI='Y'
-	AND NOT EXISTS (select 1 from m_shipmentschedule_attributeconfig e where e.m_attribute_id=a.m_attribute_id and e.m_iolcandhandler_id=iolch.m_iolcandhandler_id)
-;
-
