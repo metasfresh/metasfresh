de502d63dc7dde1c9a15ea8294e253b009630888
Teo Sarca <teo.sarca@metasfresh.com>
2018-03-01 20:41:15 +0200

    refactor M_HU_Process functionality
    https://github.com/metasfresh/metasfresh-webui-api/issues/883

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/process/api/impl/MHUProcessDAO.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/process/api/impl/MHUProcessDAO.java
@@ -1,24 +1,94 @@
 package de.metas.handlingunits.process.api.impl;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.util.Services;
-import org.adempiere.util.proxy.Cached;
+import org.compiere.util.CCache;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 
 import de.metas.handlingunits.model.I_M_HU_Process;
+import de.metas.handlingunits.model.X_M_HU_PI_Version;
+import de.metas.handlingunits.process.api.HUProcessDescriptor;
+import de.metas.handlingunits.process.api.HUProcessDescriptor.HUProcessDescriptorBuilder;
 import de.metas.handlingunits.process.api.IMHUProcessDAO;
 
 public class MHUProcessDAO implements IMHUProcessDAO
 {
+	private final CCache<Integer, IndexedHUProcessDescriptors> huProcessDescriptors = CCache.newCache(I_M_HU_Process.Table_Name, 1, CCache.EXPIREMINUTES_Never);
 
 	@Override
-	@Cached(cacheName = I_M_HU_Process.Table_Name + "#by#" + I_M_HU_Process.COLUMNNAME_AD_Process_ID)
-	public I_M_HU_Process retrieveHUProcess(final int adProcessId)
+	public Collection<HUProcessDescriptor> getAllHUProcessDescriptors()
 	{
-		return Services.get(IQueryBL.class).createQueryBuilderOutOfTrx(I_M_HU_Process.class)
-				.addOnlyActiveRecordsFilter()
-				.addEqualsFilter(I_M_HU_Process.COLUMNNAME_AD_Process_ID, adProcessId)
-				.create()
-				.firstOnly(I_M_HU_Process.class); // There should be only one M_HU_Process active entry for an AD_Process_ID
+		return getIndexedHUProcessDescriptors().getAsCollection();
 	}
 
+	@Override
+	public HUProcessDescriptor getByProcessIdOrNull(final int processId)
+	{
+		return getIndexedHUProcessDescriptors().getByProcessIdOrNull(processId);
+	}
+
+	private IndexedHUProcessDescriptors getIndexedHUProcessDescriptors()
+	{
+		return huProcessDescriptors.getOrLoad(0, this::retrieveIndexedHUProcessDescriptors);
+	}
+
+	private IndexedHUProcessDescriptors retrieveIndexedHUProcessDescriptors()
+	{
+		final ImmutableList<HUProcessDescriptor> huProcessDescriptors = Services.get(IQueryBL.class)
+				.createQueryBuilderOutOfTrx(I_M_HU_Process.class)
+				.addOnlyActiveRecordsFilter()
+				.create()
+				.stream(I_M_HU_Process.class)
+				.map(huProcessRecord -> toHUProcessDescriptor(huProcessRecord))
+				.collect(ImmutableList.toImmutableList());
+
+		return new IndexedHUProcessDescriptors(huProcessDescriptors);
+	}
+
+	private static HUProcessDescriptor toHUProcessDescriptor(final I_M_HU_Process huProcessRecord)
+	{
+		final HUProcessDescriptorBuilder builder = HUProcessDescriptor.builder()
+				.processId(huProcessRecord.getAD_Process_ID());
+
+		if (huProcessRecord.isApplyLU())
+		{
+			builder.acceptHUUnitType(X_M_HU_PI_Version.HU_UNITTYPE_LoadLogistiqueUnit);
+		}
+		if (huProcessRecord.isApplyTU())
+		{
+			builder.acceptHUUnitType(X_M_HU_PI_Version.HU_UNITTYPE_TransportUnit);
+		}
+		if (huProcessRecord.isApplyVirtualPI())
+		{
+			builder.acceptHUUnitType(X_M_HU_PI_Version.HU_UNITTYPE_VirtualPI);
+		}
+
+		return builder.build();
+	}
+
+	private static final class IndexedHUProcessDescriptors
+	{
+		private final ImmutableMap<Integer, HUProcessDescriptor> huProcessDescriptorsByProcessId;
+
+		private IndexedHUProcessDescriptors(final List<HUProcessDescriptor> huProcessDescriptors)
+		{
+			huProcessDescriptorsByProcessId = Maps.uniqueIndex(huProcessDescriptors, HUProcessDescriptor::getProcessId);
+		}
+
+		public Collection<HUProcessDescriptor> getAsCollection()
+		{
+			return huProcessDescriptorsByProcessId.values();
+		}
+
+		public HUProcessDescriptor getByProcessIdOrNull(final int processId)
+		{
+			return huProcessDescriptorsByProcessId.get(processId);
+		}
+	}
 }
