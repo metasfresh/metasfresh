c6467dee6d35c875278dd20a67ff52a95eb7dcf6
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-03-02 20:39:07 +0100

    Solve problem that WPs were still scheduled on new contract
    * also refactor/rename methods for more clarity
    
    Avoid mass-creation of "MissingShipmentSchedulesWorkpackages" #3582

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/modelvalidator/C_Order_ShipmentSchedule.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/modelvalidator/C_Order_ShipmentSchedule.java
@@ -4,11 +4,9 @@ import static org.adempiere.model.InterfaceWrapperHelper.getContextAware;
 
 import org.adempiere.ad.modelvalidator.annotations.DocValidate;
 import org.adempiere.ad.modelvalidator.annotations.Validator;
-import org.adempiere.util.Services;
 import org.compiere.model.I_C_Order;
 import org.compiere.model.ModelValidator;
 
-import de.metas.inoutcandidate.api.IShipmentScheduleBL;
 import de.metas.inoutcandidate.async.CreateMissingShipmentSchedulesWorkpackageProcessor;
 
 @Validator(I_C_Order.class)
@@ -17,12 +15,6 @@ public class C_Order_ShipmentSchedule
 	@DocValidate(timings = ModelValidator.TIMING_AFTER_COMPLETE)
 	public void createMissingShipmentSchedules(final I_C_Order order)
 	{
-		if(Services.get(IShipmentScheduleBL.class).isMissingSchedsCreatedLater())
-		{
-			return; // nothing to do now
-		}
-
-		CreateMissingShipmentSchedulesWorkpackageProcessor.schedule(getContextAware(order));
+		CreateMissingShipmentSchedulesWorkpackageProcessor.scheduleIfNotPostponed(getContextAware(order));
 	}
-
 }
