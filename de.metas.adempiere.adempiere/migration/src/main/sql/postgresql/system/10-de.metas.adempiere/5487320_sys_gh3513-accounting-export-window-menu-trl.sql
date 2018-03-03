7494794b7f0757d576c27658095a17270f0fb713
Mark Krake <mark.krake@metasfresh.com>
2018-03-03 12:53:53 +0100

    .#3513 Adds Translations to menu entries & windows
    Adds Translations for en_US to Menu Entries and Window names for
    accounting export format and accounting export. Add minor layout change.
    https://github.com/metasfresh/metasfresh/issues/3513

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5487320_sys_gh3513-accounting-export-window-menu-trl.sql
@@ -0,0 +1,25 @@
+-- 2018-03-03T12:30:46.726
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-03 12:30:46','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Accountings Export Format',WEBUI_NameBrowse='Accountings Export Format' WHERE AD_Menu_ID=541041 AND AD_Language='en_US'
+;
+
+-- 2018-03-03T12:31:18.220
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-03 12:31:18','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Accounting Export',WEBUI_NameBrowse='Accounting Export' WHERE AD_Menu_ID=541042 AND AD_Language='en_US'
+;
+
+-- 2018-03-03T12:31:30.330
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-03 12:31:30','YYYY-MM-DD HH24:MI:SS'),Name='Accounting Export Format',WEBUI_NameBrowse='Accounting Export Format' WHERE AD_Menu_ID=541041 AND AD_Language='en_US'
+;
+
+-- 2018-03-03T12:31:56.457
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-03 12:31:56','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Accounting Export Format' WHERE AD_Window_ID=540414 AND AD_Language='en_US'
+;
+
+-- 2018-03-03T12:32:52.067
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541473, SeqNo=40,Updated=TO_TIMESTAMP('2018-03-03 12:32:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=551021
+;
+
