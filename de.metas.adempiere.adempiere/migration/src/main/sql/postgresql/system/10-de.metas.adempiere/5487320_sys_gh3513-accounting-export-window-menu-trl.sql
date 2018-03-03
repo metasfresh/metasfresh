eb2071fb765dca6ac79c7b791a6f2afcd0c5dd8a
Mark Krake <mark.krake@metasfresh.com>
2018-03-03 12:59:42 +0100

    .#3513 Adds Translation for window actions
    Adds Translations to en_US for window actions.
    https://github.com/metasfresh/metasfresh/issues/3513

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5487320_sys_gh3513-accounting-export-window-menu-trl.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5487320_sys_gh3513-accounting-export-window-menu-trl.sql
@@ -23,3 +23,13 @@ UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-03 12:31:56
 UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541473, SeqNo=40,Updated=TO_TIMESTAMP('2018-03-03 12:32:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=551021
 ;
 
+-- 2018-03-03T12:57:59.860
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Erstelle Zeilen zum Export',Updated=TO_TIMESTAMP('2018-03-03 12:57:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540923
+;
+
+-- 2018-03-03T12:58:25.123
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='In Datei speichern',Updated=TO_TIMESTAMP('2018-03-03 12:58:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540924
+;
+
