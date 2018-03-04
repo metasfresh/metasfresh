75ddaca042d3c521fbfe412cc834b4278e17551a
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-03-04 22:44:23 +0100

    align expected env variables
    create docker image automatically via jenkins https://github.com/metasfresh/metasfresh-dist/issues/18

--- docker/prod/start_webui.sh
+++ docker/prod/start_webui.sh
@@ -2,8 +2,8 @@
 
 # The variables have defaults that can be set from outside, e.g. via -e "DB_HOST=mydbms" or from the docker-compose.yml file.
 # Also see https://docs.docker.com/engine/reference/builder/#environment-replacement
-webapi_host=${WEBAPI_HOST:-webapi}
-webapi_port=${WEBAPI_PORT:-8080}
+webapi_host=${WEBUI_API_HOST:-webapi}
+webapi_port=${WEBUI_API_PORT:-8080}
 
 config_js_file='/opt/metasfresh-webui-frontend/dist/config.js'
 sed -Ei "s/WEBUI_API_HOST/${webapi_host}/g" $config_js_file
