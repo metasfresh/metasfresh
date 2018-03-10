9332bdecfc1863fd4ddf3e1c2767f402e37dd29c
Pablo Sichert <mail@pablosichert.com>
2018-03-10 14:37:29 +0100

    Refactor Object.assign -> spread operator
    Issue #1640

--- src/reducers/appHandler.js
+++ src/reducers/appHandler.js
@@ -17,32 +17,44 @@ const initialState = {
 export default function appHandler(state = initialState, action) {
   switch (action.type) {
     case types.USER_SESSION_INIT:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         me: action.me,
-      });
+      };
+
     case types.USER_SESSION_UPDATE:
-      return Object.assign({}, state, {
-        me: Object.assign({}, state.me, action.me),
-      });
+      return {
+        ...state,
+        me: {
+          ...state.me,
+          ...action.me,
+        },
+      };
+
     case types.LOGIN_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isLogged: true,
-      });
+      };
 
     case types.LOGOUT_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isLogged: false,
-      });
+      };
 
     case types.ENABLE_TUTORIAL:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         enableTutorial: action.flag,
-      });
+      };
 
     // NOTIFICATION ACTIONS
     case types.ADD_NOTIFICATION:
-      return Object.assign({}, state, {
-        notifications: Object.assign({}, state.notifications, {
+      return {
+        ...state,
+        notifications: {
+          ...state.notifications,
           [action.title]: {
             title: action.title,
             msg: action.msg,
@@ -53,51 +65,64 @@ export default function appHandler(state = initialState, action) {
               ? state.notifications[action.title].count + 1
               : 0,
           },
-        }),
-      });
+        },
+      };
 
     case types.SET_NOTIFICATION_PROGRESS: {
-      const notifications = Object.assign({}, state.notifications, {
-        [action.key]: Object.assign({}, state.notifications[action.key], {
+      const notifications = {
+        ...state.notifications,
+        [action.key]: {
+          ...state.notifications[action.key],
           progress: action.progress,
-        }),
-      });
+        },
+      };
 
-      return Object.assign({}, state, {
+      return {
+        ...state,
         notifications: notifications,
-      });
+      };
     }
 
     case types.DELETE_NOTIFICATION:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         notifications: Object.keys(state.notifications).reduce((res, key) => {
           if (key !== action.key) {
             res[key] = state.notifications[key];
           }
           return res;
         }, {}),
-      });
+      };
 
     case types.CLEAR_NOTIFICATIONS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         notifications: {},
-      });
+      };
 
     // END OF NOTIFICATION ACTIONS
     case types.GET_NOTIFICATIONS_SUCCESS:
-      return Object.assign({}, state, {
-        inbox: Object.assign({}, state.inbox, {
+      return {
+        ...state,
+        inbox: {
+          ...state.inbox,
           notifications: action.notifications,
           unreadCount: action.unreadCount,
-        }),
-      });
+        },
+      };
+
     case types.NEW_NOTIFICATION:
       return update(state, {
         inbox: {
-          notifications: { $unshift: [action.notification] },
-          unreadCount: { $set: action.unreadCount },
+          notifications: {
+            $unshift: [action.notification],
+          },
+          unreadCount: {
+            $set: action.unreadCount,
+          },
         },
       });
+
     case types.REMOVE_NOTIFICATION:
       return update(state, {
         inbox: {
@@ -106,9 +131,12 @@ export default function appHandler(state = initialState, action) {
               item => item.id !== action.notification
             ),
           },
-          unreadCount: { $set: action.unreadCount },
+          unreadCount: {
+            $set: action.unreadCount,
+          },
         },
       });
+
     case types.UPDATE_NOTIFICATION:
       return update(state, {
         inbox: {
@@ -116,21 +144,27 @@ export default function appHandler(state = initialState, action) {
             $set: state.inbox.notifications.map(
               item =>
                 item.id === action.notification.id
-                  ? Object.assign({}, item, action.notification)
+                  ? { ...item, ...action.notification }
                   : item
             ),
           },
-          unreadCount: { $set: action.unreadCount },
+          unreadCount: {
+            $set: action.unreadCount,
+          },
         },
       });
+
     case types.SET_PROCESS_STATE_PENDING:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         processStatus: 'pending',
-      });
+      };
+
     case types.SET_PROCESS_STATE_SAVED:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         processStatus: 'saved',
-      });
+      };
 
     default:
       return state;
