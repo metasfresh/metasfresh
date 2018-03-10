b693294400952a57985bbb8d0abbf769569ed3c6
Mark Krake <mark.krake@metasfresh.com>
2018-03-10 12:43:26 +0100

    .#3591 Adds trl for de_DE to menu, window & fields
    https://github.com/metasfresh/metasfresh/issues/3591

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5487880_sys_gh3591-shipment-stats-trl.sql
@@ -0,0 +1,80 @@
+-- 2018-03-10T12:34:49.262
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-10 12:34:49','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Menu_ID=541044 AND AD_Language='en_US'
+;
+
+-- 2018-03-10T12:35:20.519
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Name='Liefer Statistik', WEBUI_NameBrowse='Liefer Statistik',Updated=TO_TIMESTAMP('2018-03-10 12:35:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541044
+;
+
+-- 2018-03-10T12:35:29.321
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='Liefer Statistik',Updated=TO_TIMESTAMP('2018-03-10 12:35:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540417
+;
+
+-- 2018-03-10T12:35:35.361
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Liefer Statistik',Updated=TO_TIMESTAMP('2018-03-10 12:35:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541047
+;
+
+-- 2018-03-10T12:35:44.648
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-10 12:35:44','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Tab_ID=541047 AND AD_Language='en_US'
+;
+
+-- 2018-03-10T12:35:54.614
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-10 12:35:54','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Window_ID=540417 AND AD_Language='en_US'
+;
+
+-- 2018-03-10T12:38:49.616
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Nr.',Updated=TO_TIMESTAMP('2018-03-10 12:38:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562993
+;
+
+-- 2018-03-10T12:38:53.204
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Datum',Updated=TO_TIMESTAMP('2018-03-10 12:38:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562994
+;
+
+-- 2018-03-10T12:39:04.832
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Partner Nr.',Updated=TO_TIMESTAMP('2018-03-10 12:39:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562995
+;
+
+-- 2018-03-10T12:39:08.739
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='PLZ',Updated=TO_TIMESTAMP('2018-03-10 12:39:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562997
+;
+
+-- 2018-03-10T12:39:32.081
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Lieferant',Updated=TO_TIMESTAMP('2018-03-10 12:39:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563000
+;
+
+-- 2018-03-10T12:40:40.636
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Packungsgröße',Updated=TO_TIMESTAMP('2018-03-10 12:40:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563009
+;
+
+-- 2018-03-10T12:41:08.840
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Packung Einheit',Updated=TO_TIMESTAMP('2018-03-10 12:41:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563001
+;
+
+-- 2018-03-10T12:41:15.902
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Mengeneinheit',Updated=TO_TIMESTAMP('2018-03-10 12:41:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563002
+;
+
+-- 2018-03-10T12:41:30.055
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Darreichungsform',Updated=TO_TIMESTAMP('2018-03-10 12:41:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563008
+;
+
+-- 2018-03-10T12:41:34.266
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Hersteller ID',Updated=TO_TIMESTAMP('2018-03-10 12:41:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563004
+;
+
