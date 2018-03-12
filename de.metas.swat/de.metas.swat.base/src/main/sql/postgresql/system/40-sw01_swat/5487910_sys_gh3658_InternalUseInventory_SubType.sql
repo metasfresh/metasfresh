85359326da9b828728caece2f28cd94c3863797a
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-03-12 13:20:51 +0200

    #3658 #3659 Internal Use Inventory doc subtype
    https://github.com/metasfresh/metasfresh/issues/3658
    https://github.com/metasfresh/metasfresh/issues/3659

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5487910_sys_gh3658_InternalUseInventory_SubType.sql
@@ -0,0 +1,9 @@
+-- 2018-03-12T11:51:06.404
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List SET Name='Internal Use Inventory', Value='IUI',Updated=TO_TIMESTAMP('2018-03-12 11:51:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=541260
+;
+
+-- 2018-03-12T11:51:18.293
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET FieldLength=3,Updated=TO_TIMESTAMP('2018-03-12 11:51:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3392
+;
