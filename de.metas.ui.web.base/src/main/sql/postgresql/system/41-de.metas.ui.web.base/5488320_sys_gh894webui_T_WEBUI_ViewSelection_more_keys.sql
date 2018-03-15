48ccd938b771f9f635dc952bbbeec4cec7510cc9
Teo Sarca <teo.sarca@metasfresh.com>
2018-03-15 11:28:19 +0200

    add a comment with T_WEBUI_ViewSelection tables structure before this change
    https://github.com/metasfresh/metasfresh-webui-api/issues/894

--- de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5488320_sys_gh894webui_T_WEBUI_ViewSelection_more_keys.sql
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5488320_sys_gh894webui_T_WEBUI_ViewSelection_more_keys.sql
@@ -11,3 +11,30 @@ alter table T_WEBUI_ViewSelectionLine add column StringKey1 varchar(60);
 
 
 
+/* Backup:
+
+DROP TABLE t_webui_viewselection;
+CREATE TABLE t_webui_viewselection
+(
+  uuid character varying(60) NOT NULL,
+  line numeric(10,0) NOT NULL,
+  record_id numeric(10,0) NOT NULL,
+  created timestamp with time zone NOT NULL DEFAULT now(),
+  CONSTRAINT t_webui_viewselection_pkey PRIMARY KEY (uuid, line, record_id)
+);
+CREATE INDEX t_webui_viewselection_search ON t_webui_viewselection (uuid, record_id);
+CREATE INDEX t_webui_viewselection_uuid ON t_webui_viewselection (uuid);
+
+
+DROP TABLE public.t_webui_viewselectionline;
+CREATE TABLE public.t_webui_viewselectionline
+(
+  uuid character varying(60) NOT NULL,
+  record_id numeric(10,0) NOT NULL,
+  line_id numeric NOT NULL,
+  created timestamp with time zone NOT NULL DEFAULT now(),
+  CONSTRAINT t_webui_viewselectionline_pkey PRIMARY KEY (uuid, record_id, line_id)
+);
+CREATE INDEX t_webui_viewselectionline_uuid ON public.t_webui_viewselectionline (uuid);
+
+*/
