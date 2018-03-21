6aa73bcdb13f681bd90104bc66974e9744305afe
Mark Krake <mark.krake@metasfresh.com>
2018-03-21 14:46:14 +0100

    .#3585 Adds Translations/ Renaming Fields
    https://github.com/metasfresh/metasfresh/issues/3585

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5489070_sys_gh3585-bpartner-creditlimit-improvements.sql
@@ -0,0 +1,95 @@
+-- 2018-03-21T14:22:42.881
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Kredit Limit',Updated=TO_TIMESTAMP('2018-03-21 14:22:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540994
+;
+
+-- 2018-03-21T14:23:02.164
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-21 14:23:02','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Credit Limit' WHERE AD_Tab_ID=540994 AND AD_Language='en_US'
+;
+
+-- 2018-03-21T14:24:42.002
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Help='', Name='Kredit Limit freigeben',Updated=TO_TIMESTAMP('2018-03-21 14:24:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540921
+;
+
+-- 2018-03-21T14:24:55.834
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-21 14:24:55','YYYY-MM-DD HH24:MI:SS'),Name='Approve Credit Limit' WHERE AD_Process_ID=540921 AND AD_Language='de_CH'
+;
+
+-- 2018-03-21T14:25:13.844
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-21 14:25:13','YYYY-MM-DD HH24:MI:SS'),Name='Credit Limit Approve' WHERE AD_Process_ID=540921 AND AD_Language='en_US'
+;
+
+-- 2018-03-21T14:25:16.262
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-21 14:25:16','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Process_ID=540921 AND AD_Language='en_US'
+;
+
+-- 2018-03-21T14:26:11.210
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Kredit Limit Freigabe anfordern',Updated=TO_TIMESTAMP('2018-03-21 14:26:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540922
+;
+
+-- 2018-03-21T14:26:41.385
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-21 14:26:41','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Credit Limit Request Approval' WHERE AD_Process_ID=540922 AND AD_Language='en_US'
+;
+
+-- 2018-03-21T14:27:00.912
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Kredit Limit',Updated=TO_TIMESTAMP('2018-03-21 14:27:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541032
+;
+
+-- 2018-03-21T14:27:04.392
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-03-21 14:27:04','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Tab_ID=541032 AND AD_Language='en_US'
+;
+
+-- 2018-03-21T14:27:28.028
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Kredit Limit Art',Updated=TO_TIMESTAMP('2018-03-21 14:27:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562691
+;
+
+-- 2018-03-21T14:27:44.275
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Freigegeben durch',Updated=TO_TIMESTAMP('2018-03-21 14:27:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562693
+;
+
+-- 2018-03-21T14:30:11.954
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Kredit Limit Art',Updated=TO_TIMESTAMP('2018-03-21 14:30:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=561830
+;
+
+-- 2018-03-21T14:30:27.390
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Freigegeben von',Updated=TO_TIMESTAMP('2018-03-21 14:30:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=561832
+;
+
+-- 2018-03-21T14:30:56.906
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Freigegeben von',Updated=TO_TIMESTAMP('2018-03-21 14:30:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562693
+;
+
+-- 2018-03-21T14:34:45.762
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Kredit Limit Indikator %',Updated=TO_TIMESTAMP('2018-03-21 14:34:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=561919
+;
+
+-- 2018-03-21T14:35:18.559
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Kredit Limit Indikator %',Updated=TO_TIMESTAMP('2018-03-21 14:35:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562685
+;
+
+-- 2018-03-21T14:38:17.082
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Partner Merkmale',Updated=TO_TIMESTAMP('2018-03-21 14:38:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540877
+;
+
+-- 2018-03-21T14:40:36.329
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=541441, SeqNo=70,Updated=TO_TIMESTAMP('2018-03-21 14:40:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550944
+;
+
