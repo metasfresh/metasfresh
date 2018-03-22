3481411f82e502a470d73447c0d12b817b32d227
Teo Sarca <teo.sarca@metasfresh.com>
2018-03-22 18:04:32 +0200

    GeneralCopyRecord: order child POs by Line/SeqNo/PK
    https://github.com/metasfresh/metasfresh/issues/3745
    (cherry picked from commit f74f153e5951741dbca68a146d784dc0dc086b38)

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/model/TableInfoVO.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/model/TableInfoVO.java
@@ -3,24 +3,42 @@
  */
 package org.adempiere.model;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import de.metas.i18n.ITranslatableString;
 import lombok.Builder;
+import lombok.NonNull;
 import lombok.Value;
 
 @Value
 public class TableInfoVO
 {
 	/** Translated name */
-	private final String name;
+	private final ITranslatableString name;
 	private final String tableName;
 	private final String linkColumnName;
 	private final String parentTableName;
+	private final List<String> orderByColumnNames;
 
 	@Builder
-	private TableInfoVO(final String name, final String tableName, final String linkColumnName, final String parentTableName)
+	private TableInfoVO(
+			@NonNull final ITranslatableString name,
+			final String tableName,
+			final String linkColumnName,
+			final String parentTableName,
+			final List<String> orderByColumnNames)
 	{
 		this.name = name;
 		this.tableName = tableName;
 		this.linkColumnName = linkColumnName;
 		this.parentTableName = parentTableName;
+		this.orderByColumnNames = orderByColumnNames != null ? ImmutableList.copyOf(orderByColumnNames) : ImmutableList.of();
+	}
+
+	public String getName(final String adLanguage)
+	{
+		return name.translate(adLanguage);
 	}
 }
