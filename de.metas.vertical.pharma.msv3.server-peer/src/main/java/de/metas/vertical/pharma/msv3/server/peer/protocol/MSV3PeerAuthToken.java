ed79c4c722e6af70fcaea0402b926968748b2d17
Teo Sarca <teo.sarca@metasfresh.com>
2018-03-23 15:21:57 +0200

    use MSV3PeerAuthToken
    https://github.com/metasfresh/metasfresh/issues/3716

--- de.metas.vertical.pharma.msv3.server-peer/src/main/java/de/metas/vertical/pharma/msv3/server/peer/protocol/MSV3PeerAuthToken.java
+++ de.metas.vertical.pharma.msv3.server-peer/src/main/java/de/metas/vertical/pharma/msv3/server/peer/protocol/MSV3PeerAuthToken.java
@@ -2,9 +2,12 @@ package de.metas.vertical.pharma.msv3.server.peer.protocol;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.common.base.MoreObjects;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.NonNull;
+import lombok.Value;
 
 /*
  * #%L
@@ -16,12 +19,12 @@ import lombok.Data;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -29,18 +32,40 @@ import lombok.Data;
  */
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
-@Data
-@Builder
-public class MSV3ServerRequest
+@Value
+public class MSV3PeerAuthToken
 {
-	public static MSV3ServerRequest requestAll()
+	public static final String NAME = "AuthToken";
+
+	@JsonCreator
+	public static MSV3PeerAuthToken of(final String value)
 	{
-		return MSV3ServerRequest.builder()
-				.requestAllUsers(true)
-				.requestAllStockAvailabilities(true)
-				.build();
+		return new MSV3PeerAuthToken(value);
 	}
 
-	boolean requestAllUsers;
-	boolean requestAllStockAvailabilities;
+	private final String valueAsString;
+
+	private MSV3PeerAuthToken(@NonNull final String valueAsString)
+	{
+		if (valueAsString.isEmpty())
+		{
+			throw new IllegalArgumentException("Empty string is not a valid " + MSV3PeerAuthToken.class);
+		}
+
+		this.valueAsString = valueAsString;
+	}
+
+	@Override
+	public String toString()
+	{
+		return MoreObjects.toStringHelper(this)
+				.addValue("********")
+				.toString();
+	}
+
+	@JsonValue
+	public String toJson()
+	{
+		return valueAsString;
+	}
 }
--- de.metas.vertical.pharma.msv3.server-peer/src/main/java/de/metas/vertical/pharma/msv3/server/peer/service/MSV3ServerPeerService.java
+++ de.metas.vertical.pharma.msv3.server-peer/src/main/java/de/metas/vertical/pharma/msv3/server/peer/service/MSV3ServerPeerService.java
@@ -1,15 +1,17 @@
 package de.metas.vertical.pharma.msv3.server.peer.service;
 
-import java.util.UUID;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import de.metas.vertical.pharma.msv3.protocol.order.OrderCreateRequest;
 import de.metas.vertical.pharma.msv3.protocol.order.OrderCreateResponse;
 import de.metas.vertical.pharma.msv3.server.peer.RabbitMQConfig;
+import de.metas.vertical.pharma.msv3.server.peer.protocol.MSV3PeerAuthToken;
 import de.metas.vertical.pharma.msv3.server.peer.protocol.MSV3ServerRequest;
 import de.metas.vertical.pharma.msv3.server.peer.protocol.MSV3StockAvailabilityUpdatedEvent;
 import de.metas.vertical.pharma.msv3.server.peer.protocol.MSV3UserChangedEvent;
@@ -25,12 +27,12 @@ import lombok.NonNull;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -43,33 +45,48 @@ public class MSV3ServerPeerService
 	private static final Logger logger = LoggerFactory.getLogger(MSV3ServerPeerService.class);
 
 	private final RabbitTemplate rabbitTemplate;
+	private final MSV3PeerAuthToken msv3PeerAuthToken;
 
-	private final String nodeId = UUID.randomUUID().toString();
-
-	public MSV3ServerPeerService(@NonNull final RabbitTemplate rabbitTemplate)
+	public MSV3ServerPeerService(
+			@NonNull final RabbitTemplate rabbitTemplate,
+			final Optional<MSV3PeerAuthToken> msv3PeerAuthToken)
 	{
 		this.rabbitTemplate = rabbitTemplate;
+		this.msv3PeerAuthToken = msv3PeerAuthToken.orElse(null);
+	}
+
+	private Message messagePostProcess(final Message message)
+	{
+		if (msv3PeerAuthToken != null)
+		{
+			message.getMessageProperties().getHeaders().put(MSV3PeerAuthToken.NAME, msv3PeerAuthToken.toJson());
+		}
+
+		return message;
 	}
 
 	public void requestAllUpdates()
 	{
-		rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUENAME_MSV3ServerRequests, MSV3ServerRequest.requestAll(nodeId));
+		rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUENAME_MSV3ServerRequests, MSV3ServerRequest.requestAll(), this::messagePostProcess);
 		logger.info("Requested all data from MSV3 server peer");
 	}
 
 	public void publishUserChangedEvent(@NonNull final MSV3UserChangedEvent event)
 	{
-		rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUENAME_UserChangedEvents, event);
+		rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUENAME_UserChangedEvents, event, this::messagePostProcess);
 	}
 
 	public void publishStockAvailabilityUpdatedEvent(@NonNull final MSV3StockAvailabilityUpdatedEvent event)
 	{
-		rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUENAME_StockAvailabilityUpdatedEvent, event);
+		rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUENAME_StockAvailabilityUpdatedEvent, event, this::messagePostProcess);
 	}
 
 	public OrderCreateResponse createOrderRequest(final OrderCreateRequest request)
 	{
-		final Object responseObj = rabbitTemplate.convertSendAndReceive(RabbitMQConfig.QUEUENAME_CreateOrderRequestEvents, request);
+		final Object responseObj = rabbitTemplate.convertSendAndReceive(
+				RabbitMQConfig.QUEUENAME_CreateOrderRequestEvents,
+				request,
+				this::messagePostProcess);
 		return (OrderCreateResponse)responseObj;
 	}
 }
