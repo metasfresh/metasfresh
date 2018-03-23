88d3cfe5c6da4731623a7028c517b193d5fafdb8
Teo Sarca <teo.sarca@metasfresh.com>
2018-03-23 13:16:24 +0200

    IInputDataSourceDAO: more methods
    https://github.com/metasfresh/metasfresh/issues/3716

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/impex/api/InputDataSourceCreateRequest.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/impex/api/InputDataSourceCreateRequest.java
@@ -1,38 +1,50 @@
 package de.metas.impex.api;
 
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
 /*
  * #%L
  * de.metas.swat.base
  * %%
- * Copyright (C) 2015 metas GmbH
+ * Copyright (C) 2018 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-import java.util.Properties;
-
-import org.adempiere.util.ISingletonService;
-
-import de.metas.impex.model.I_AD_InputDataSource;
-
-public interface IInputDataSourceDAO extends ISingletonService
+@Value
+public class InputDataSourceCreateRequest
 {
-	I_AD_InputDataSource retrieveInputDataSource(Properties ctx, String internalName, boolean throwEx, String trxName);
-	
-	int retrieveInputDataSourceId(String internalName);
+	String internalName;
+	String name;
+	String entityType;
+	boolean destination;
 
-	void createIfMissing(InputDataSourceCreateRequest request);
+	@Builder
+	private InputDataSourceCreateRequest(
+			@NonNull final String internalName,
+			final String name,
+			@NonNull final String entityType,
+			@NonNull final Boolean destination)
+	{
+		this.internalName = internalName;
+		this.name = name != null ? name : internalName;
+		this.entityType = entityType;
+		this.destination = destination;
+	}
+
 }
