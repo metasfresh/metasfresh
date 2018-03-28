f31840320c307e1ef822a44b627afacdaf79538c
Teo Sarca <teo.sarca@metasfresh.com>
2018-03-29 00:13:22 +0300

    Upgrade to spring 5.0.4.RELEASE
    https://github.com/metasfresh/metasfresh/issues/3787

--- de.metas.elasticsearch.server/src/main/java/de/metas/elasticsearch/ESServer.java
+++ de.metas.elasticsearch.server/src/main/java/de/metas/elasticsearch/ESServer.java
@@ -1,8 +1,6 @@
 package de.metas.elasticsearch;
 
 import org.adempiere.util.Services;
-import org.elasticsearch.common.logging.ESLoggerFactory;
-import org.elasticsearch.common.logging.slf4j.Slf4jESLoggerFactory;
 
 import de.metas.elasticsearch.config.ESModelIndexerConfigBuilder;
 import de.metas.elasticsearch.impl.IESServer;
@@ -36,7 +34,7 @@ public class ESServer implements IESServer
 	{
 		// Make sure slf4j is used.
 		// (by default, log4j is used)
-		ESLoggerFactory.setDefaultFactory(new Slf4jESLoggerFactory());
+		// ESLoggerFactory.setDefaultFactory(new Slf4jESLoggerFactory()); // FIXME: shall we use log4j-over-slf4j?!
 	}
 	@Override
 	public void installConfig(final ESModelIndexerConfigBuilder config)
