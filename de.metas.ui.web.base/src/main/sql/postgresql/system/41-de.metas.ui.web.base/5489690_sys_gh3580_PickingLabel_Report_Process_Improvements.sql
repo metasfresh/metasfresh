4ca043a5d3ba86e13f4f51f00d9c73f707782058
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-03-29 13:45:22 +0300

    #3580 Improvements to the process in db
    https://github.com/metasfresh/metasfresh/issues/3580

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5489690_sys_gh3580_PickingLabel_Report_Process_Improvements.sql
@@ -0,0 +1,10 @@
+-- 2018-03-29T13:33:15.519
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsBetaFunctionality='N', Value='WEBUI_Picking_TU_Label_For_HU',Updated=TO_TIMESTAMP('2018-03-29 13:33:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540948
+;
+
+-- 2018-03-29T13:43:20.256
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET IsReport='N',Updated=TO_TIMESTAMP('2018-03-29 13:43:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=540948
+;
+
