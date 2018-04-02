ba44d5f53209d0a1aa714136b6da9427d18b2a42
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-04-02 13:50:38 +0300

    #3777 getSingleQty ATP
    https://github.com/metasfresh/metasfresh/issues/3777

--- de.metas.material/dispo-commons/src/main/java/de/metas/material/dispo/commons/repository/AvailableToPromiseResult.java
+++ de.metas.material/dispo-commons/src/main/java/de/metas/material/dispo/commons/repository/AvailableToPromiseResult.java
@@ -314,4 +314,12 @@ public class AvailableToPromiseResult
 
 		resultGroups.add(group);
 	}
+	
+	public BigDecimal getSingleQty()
+	{
+		// TODO Check if this is ok
+		Check.assume(resultGroups.size() == 1, " There should be only one group the the result group {}." , resultGroups);
+		
+		return resultGroups.get(0).getQty();
+	}
 }
