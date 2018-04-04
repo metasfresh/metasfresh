e8510434b129adc96d600257a25b58fcc8aeec58
Teo Sarca <teo.sarca@metasfresh.com>
2018-04-04 16:14:06 +0300

    fix failing tests
    https://github.com/metasfresh/metasfresh/issues/3708

--- de.metas.swat/de.metas.swat.base/src/test/java/de/metas/invoicecandidate/api/impl/InvoiceGeneratedNotificationChecker.java
+++ de.metas.swat/de.metas.swat.base/src/test/java/de/metas/invoicecandidate/api/impl/InvoiceGeneratedNotificationChecker.java
@@ -13,28 +13,29 @@ package de.metas.invoicecandidate.api.impl;
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.adempiere.invoice.event.InvoiceGeneratedEventBus;
+import org.adempiere.invoice.event.InvoiceUserNotificationsProducer;
+import org.adempiere.util.Services;
 import org.adempiere.util.lang.ITableRecordReference;
 import org.compiere.model.I_C_Invoice;
 import org.junit.Assert;
 
 import de.metas.event.Event;
 import de.metas.event.IEventBus;
+import de.metas.event.IEventBusFactory;
 import de.metas.event.IEventListener;
 
 /**
@@ -48,7 +49,9 @@ public class InvoiceGeneratedNotificationChecker implements IEventListener
 	public static final InvoiceGeneratedNotificationChecker createAnSubscribe()
 	{
 		final InvoiceGeneratedNotificationChecker notificationsChecker = new InvoiceGeneratedNotificationChecker();
-		InvoiceGeneratedEventBus.newInstance().subscribe(notificationsChecker);
+		Services.get(IEventBusFactory.class)
+				.getEventBus(InvoiceUserNotificationsProducer.EVENTBUS_TOPIC)
+				.subscribe(notificationsChecker);
 
 		return notificationsChecker;
 	}
@@ -57,7 +60,6 @@ public class InvoiceGeneratedNotificationChecker implements IEventListener
 
 	private InvoiceGeneratedNotificationChecker()
 	{
-		super();
 	}
 
 	@Override
