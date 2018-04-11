d9e6d36cc547335dbc6b91bd6b37f963d8ba947a
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-04-11 17:09:19 +0300

    #3833 Delete obsolete, unused method + formatting
    https://github.com/metasfresh/metasfresh/issues/3833

--- de.metas.business/src/main/java-legacy/org/compiere/model/MDiscountSchemaBreak.java
+++ de.metas.business/src/main/java-legacy/org/compiere/model/MDiscountSchemaBreak.java
@@ -1,28 +1,23 @@
 /******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved.                *
- * This program is free software; you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * by the Free Software Foundation. This program is distributed in the hope   *
+ * Product: Adempiere ERP & CRM Smart Business Solution *
+ * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved. *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms version 2 of the GNU General Public License as published *
+ * by the Free Software Foundation. This program is distributed in the hope *
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
- * You should have received a copy of the GNU General Public License along    *
- * with this program; if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- * For the text or an alternative of this public license, you may reach us    *
- * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
- * or via info@compiere.org or http://www.compiere.org/license.html           *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. *
+ * See the GNU General Public License for more details. *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA. *
+ * For the text or an alternative of this public license, you may reach us *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA *
+ * or via info@compiere.org or http://www.compiere.org/license.html *
  *****************************************************************************/
 package org.compiere.model;
 
-import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.Properties;
-
-import org.adempiere.model.InterfaceWrapperHelper;
-import org.adempiere.pricing.api.IMDiscountSchemaBL;
-import org.adempiere.util.Services;
 
 /**
  * Discount Break Schema (Model)
@@ -30,7 +25,8 @@ import org.adempiere.util.Services;
  * @author Jorg Janke
  * @version $Id: MDiscountSchemaBreak.java,v 1.3 2006/07/30 00:51:05 jjanke Exp $
  * 
- * @author Teo Sarca, SC ARHIPAC SERVICE SRL <li>BF [ 2012439 ] DiscountSchemaBreak: setting product & category is allowed
+ * @author Teo Sarca, SC ARHIPAC SERVICE SRL
+ *         <li>BF [ 2012439 ] DiscountSchemaBreak: setting product & category is allowed
  */
 public class MDiscountSchemaBreak extends X_M_DiscountSchemaBreak
 {
@@ -63,26 +59,6 @@ public class MDiscountSchemaBreak extends X_M_DiscountSchemaBreak
 	{
 		super(ctx, rs, trxName);
 	}	// MDiscountSchemaBreak
-
-	/**
-	 * Criteria apply
-	 *
-	 * @param Value amt or qty
-	 * @param M_Product_ID product
-	 * @param M_Product_Category_ID category
-	 * @return true if criteria met
-	 */
-	@Deprecated
-	public boolean applies(BigDecimal Value, int M_Product_ID, int M_Product_Category_ID)
-	{
-		final I_M_DiscountSchemaBreak schemaBreak = InterfaceWrapperHelper.create(this, I_M_DiscountSchemaBreak.class);
-		return Services.get(IMDiscountSchemaBL.class).breakApplies(
-				schemaBreak,
-				Value,
-				M_Product_ID,
-				M_Product_Category_ID
-				);
-	}	// applies
 
 	@Override
 	protected boolean beforeSave(boolean newRecord)
