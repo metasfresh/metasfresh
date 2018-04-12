52f5151b2946a32693621e038d254920edc3ccfe
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-04-12 17:40:28 +0300

    #3835 colour sys config
    https://github.com/metasfresh/metasfresh/issues/3835

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.pricing/5490520_gh3835_NoPriceConditionsColor_ID_SysConfig.sql
@@ -0,0 +1,6 @@
+-- 2018-04-12T16:28:25.005
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541204,'S',TO_TIMESTAMP('2018-04-12 16:28:24','YYYY-MM-DD HH24:MI:SS'),100,'The value will contain the  AD_Color_ID that will be set in C_OrderLine.NoPriceConditionsColor_ID if the C_OrderLine.M_DiscountSchemaBreak_ID is null.
+Leave empty or set the value to "-" if the functionality is not needed','D','Y','de.metas.order.NoPriceConditionsColorName',TO_TIMESTAMP('2018-04-12 16:28:24','YYYY-MM-DD HH24:MI:SS'),100,'-')
+;
+
