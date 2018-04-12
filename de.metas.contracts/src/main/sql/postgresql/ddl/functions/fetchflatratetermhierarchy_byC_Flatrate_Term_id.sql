be378e009a9ba26078a8004f49f05d8cf12585d2
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-04-12 14:55:12 +0300

    #3763 Add comment on function fetchflatratetermhierarchy_byC_Flatrate_Term_id
    #3763 Contracts: Create a function that provides a hierarchy of
    contracts

--- de.metas.contracts/src/main/sql/postgresql/ddl/functions/fetchflatratetermhierarchy_byC_Flatrate_Term_id.sql
+++ de.metas.contracts/src/main/sql/postgresql/ddl/functions/fetchflatratetermhierarchy_byC_Flatrate_Term_id.sql
@@ -27,4 +27,5 @@ $BODY$
   LANGUAGE sql STABLE
   COST 100
   ROWS 1000;
-comment on function de_metas_contracts.fetchflatratetermhierarchy_byC_Flatrate_Term_id(numeric) is 'This function returns the hierachy chain of a contract';     
+comment on function de_metas_contracts.fetchflatratetermhierarchy_byC_Flatrate_Term_id(numeric) is 'This function returns the hierachy chain of a contract. It start from the initial contract (which is provided by function fetchInitialC_Flatrate_term_ID) and goes recursivelly building the inheritence tree and put it in an array. The function only look foraward, based on the link current.c_flatrateterm_next_id = next.c_flatrate_term_id
+E.g. We have next inheritence tree: 1000281->1000282->1000283->1000284->1000285. The result if we run for 1000281 is  1000281;{1000281,1000282,1000283,1000284,1000285,NULL}. And the result if we run for 1000283 or 1000285  1000281;{1000281,1000282,1000283,1000284,1000285,NULL}';
