1ebf2680458f8d73c0228296cf34a24a94692532
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-04-13 18:57:06 +0300

    #3800 Refactor
    #3800 Improve inventory import

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/interceptor/M_Inventory.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/interceptor/M_Inventory.java
@@ -51,12 +51,12 @@ import de.metas.quantity.Quantity;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -214,7 +214,7 @@ public class M_Inventory
 					.stream()
 					.map(I_M_InventoryLine::getM_HU_ID)
 					.collect(ImmutableList.toImmutableList());
-			
+
 			Services.get(IHUSnapshotDAO.class).restoreHUs()
 					.setContext(PlainContextAware.newWithThreadInheritedTrx())
 					.setSnapshotId(snapshotId)
