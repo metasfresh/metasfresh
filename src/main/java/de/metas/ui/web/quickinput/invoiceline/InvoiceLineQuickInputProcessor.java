5363a3d20dbcdfa22badc1b633fdea54775ebef0
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-04-16 17:09:34 +0300

    #3834 Refactoring Use a uniquemethod when checking for banned products
    https://github.com/metasfresh/metasfresh/issues/3834

--- src/main/java/de/metas/ui/web/quickinput/invoiceline/InvoiceLineQuickInputProcessor.java
+++ src/main/java/de/metas/ui/web/quickinput/invoiceline/InvoiceLineQuickInputProcessor.java
@@ -1,17 +1,13 @@
 package de.metas.ui.web.quickinput.invoiceline;
 
-import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.invoice.service.IInvoiceBL;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
 import org.compiere.model.I_C_Invoice;
-import org.compiere.util.Env;
 
 import de.metas.adempiere.model.I_C_InvoiceLine;
 import de.metas.adempiere.service.IInvoiceLineBL;
-import de.metas.i18n.IMsgBL;
-import de.metas.interfaces.I_C_BPartner_Product;
-import de.metas.purchasing.api.IBPartnerProductDAO;
+import de.metas.purchasing.api.IBPartnerProductBL;
 import de.metas.ui.web.quickinput.IQuickInputProcessor;
 import de.metas.ui.web.quickinput.QuickInput;
 import de.metas.ui.web.window.datatypes.DocumentId;
@@ -40,7 +36,6 @@ import de.metas.ui.web.window.datatypes.DocumentId;
 
 public class InvoiceLineQuickInputProcessor implements IQuickInputProcessor
 {
-	private static final IBPartnerProductDAO bpProductDAO = Services.get(IBPartnerProductDAO.class);
 
 	@Override
 	public DocumentId process(final QuickInput quickInput)
@@ -51,19 +46,10 @@ public class InvoiceLineQuickInputProcessor implements IQuickInputProcessor
 		final I_C_Invoice invoice = quickInput.getRootDocumentAs(I_C_Invoice.class);
 		final IInvoiceLineQuickInput invoiceLineQuickInput = quickInput.getQuickInputDocumentAs(IInvoiceLineQuickInput.class);
 
-		final I_C_BPartner_Product bannedProductForPartner = bpProductDAO.getBannedProductForPartner(
+		// 3834
+		Services.get(IBPartnerProductBL.class).assertProductNotBanned(
 				invoiceLineQuickInput.getM_Product_ID(),
 				invoice.getC_BPartner_ID());
-
-		if (bannedProductForPartner != null)
-		{
-			final String msg = Services.get(IMsgBL.class).getMsg(
-					Env.getCtx(),
-					IBPartnerProductDAO.MSG_ProductSalesBanError,
-					new Object[] { invoice.getC_BPartner().getName(), bannedProductForPartner.getSalesBanReason() });
-
-			throw new AdempiereException(msg);
-		}
 
 		final I_C_InvoiceLine invoiceLine = InterfaceWrapperHelper.newInstance(I_C_InvoiceLine.class, invoice);
 		invoiceLine.setC_Invoice(invoice);
