6f7deb77d868aa806d4a8b11738240c25951af89
Mark Krake <mark.krake@metasfresh.com>
2018-04-17 13:14:05 +0200

    .#gh3866 Adds Translations for de_DE, en_US
    https://github.com/metasfresh/metasfresh/issues/3866

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5491060_sys_gh3866-lot-control-window-webui-trl.sql
@@ -0,0 +1,75 @@
+-- 2018-04-17T13:01:15.608
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='Chargensperre',Updated=TO_TIMESTAMP('2018-04-17 13:01:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540422
+;
+
+-- 2018-04-17T13:01:15.618
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Chargensperre',Updated=TO_TIMESTAMP('2018-04-17 13:01:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541062
+;
+
+-- 2018-04-17T13:04:01.383
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:04:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Lot control' WHERE AD_Window_ID=540422 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:04:59.104
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='Chargen Kontrolle',Updated=TO_TIMESTAMP('2018-04-17 13:04:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540422
+;
+
+-- 2018-04-17T13:04:59.108
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Chargen Kontrolle',Updated=TO_TIMESTAMP('2018-04-17 13:04:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541062
+;
+
+-- 2018-04-17T13:06:51.317
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Chargen Kontrolle',Updated=TO_TIMESTAMP('2018-04-17 13:06:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541056
+;
+
+-- 2018-04-17T13:07:18.863
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Chargen Nr.',Updated=TO_TIMESTAMP('2018-04-17 13:07:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563116
+;
+
+-- 2018-04-17T13:07:32.797
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:07:32','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Client',Description='',Help='' WHERE AD_Field_ID=563111 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:07:45.505
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:07:45','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Organisation',Description='',Help='' WHERE AD_Field_ID=563112 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:07:56.764
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:07:56','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product',Description='',Help='' WHERE AD_Field_ID=563115 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:08:08.733
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:08:08','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Lot No.',Description='',Help='' WHERE AD_Field_ID=563116 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:08:25.371
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:08:25','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Active',Description='',Help='' WHERE AD_Field_ID=563113 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:08:34.148
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:08:34','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Description' WHERE AD_Field_ID=563125 AND AD_Language='en_US'
+;
+
+-- 2018-04-17T13:09:28.302
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET WEBUI_NameBrowse='Chargen Kontrolle',Updated=TO_TIMESTAMP('2018-04-17 13:09:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541062
+;
+
+-- 2018-04-17T13:10:05.069
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-17 13:10:05','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Lot control',WEBUI_NameBrowse='Lot control' WHERE AD_Menu_ID=541062 AND AD_Language='en_US'
+;
+
