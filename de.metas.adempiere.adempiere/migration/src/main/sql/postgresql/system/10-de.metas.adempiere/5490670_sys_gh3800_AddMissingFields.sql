67795990e3e9d2d9da2c7c89adabd7ab886db4ab
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-04-18 18:24:54 +0300

    #3800 Fix scripts
    #3800 Improve inventory import

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5490670_sys_gh3800_AddMissingFields.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5490670_sys_gh3800_AddMissingFields.sql
@@ -613,4 +613,8 @@ UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2018-04-13 19:25:49','Y
 ;
 
 
+-- 2018-04-16T20:37:51.676
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET EntityType='D',Updated=TO_TIMESTAMP('2018-04-16 20:37:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=559686
+;
 
