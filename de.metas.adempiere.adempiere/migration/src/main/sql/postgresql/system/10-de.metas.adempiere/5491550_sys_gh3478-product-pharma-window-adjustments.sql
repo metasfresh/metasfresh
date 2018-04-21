4a8696afa2a88956543ac6a8108dd58f4da96d92
Mark Krake <mark.krake@metasfresh.com>
2018-04-21 11:08:12 +0200

    .#3478 Adds Field Translations for en_US
    https://github.com/metasfresh/metasfresh/issues/3478

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5491550_sys_gh3478-product-pharma-window-adjustments.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5491550_sys_gh3478-product-pharma-window-adjustments.sql
@@ -8,3 +8,58 @@ UPDATE AD_Field SET Name='Pharma Produkt Kategorie',Updated=TO_TIMESTAMP('2018-0
 UPDATE AD_Field SET Name='Quell Lager',Updated=TO_TIMESTAMP('2018-04-21 10:52:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562595
 ;
 
+-- 2018-04-21T10:57:59.923
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 10:57:59','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Statistic Group' WHERE AD_Field_ID=562520 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T10:58:42.877
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 10:58:42','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Is Manufactured' WHERE AD_Field_ID=562526 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T10:59:13.773
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 10:59:13','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='BOM created' WHERE AD_Field_ID=562527 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T10:59:40.705
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 10:59:40','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Is Diverse' WHERE AD_Field_ID=562536 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:00:34.920
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:00:34','YYYY-MM-DD HH24:MI:SS'),Name='Diverse' WHERE AD_Field_ID=562536 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:01:15.323
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:01:15','YYYY-MM-DD HH24:MI:SS'),Name='Manufactured' WHERE AD_Field_ID=562526 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:04:35.217
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:04:35','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Used for Vendor' WHERE AD_Field_ID=562622 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:04:54.283
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:04:54','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Used for Customer' WHERE AD_Field_ID=562620 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:05:13.539
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:05:13','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product No' WHERE AD_Field_ID=562616 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:05:43.623
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:05:43','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product Category' WHERE AD_Field_ID=562618 AND AD_Language='en_US'
+;
+
+-- 2018-04-21T11:07:03.844
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-21 11:07:03','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Product Mapping' WHERE AD_Field_ID=562647 AND AD_Language='en_US'
+;
+
