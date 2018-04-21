370e9656553c083baf9bf75c88e8536bb433e50a
Mark Krake <mark.krake@metasfresh.com>
2018-04-21 10:55:26 +0200

    .#3478 Adds Translations de_DE
    https://github.com/metasfresh/metasfresh/issues/3478

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5491550_sys_gh3478-product-pharma-window-adjustments.sql
@@ -0,0 +1,10 @@
+-- 2018-04-21T10:51:18.988
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Pharma Produkt Kategorie',Updated=TO_TIMESTAMP('2018-04-21 10:51:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=563107
+;
+
+-- 2018-04-21T10:52:38.975
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Quell Lager',Updated=TO_TIMESTAMP('2018-04-21 10:52:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=562595
+;
+
