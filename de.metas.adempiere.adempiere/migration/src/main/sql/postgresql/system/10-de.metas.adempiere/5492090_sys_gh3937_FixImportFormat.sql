49dc817b013ed75ea9e8492c0d970b8d40e86419
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-04-27 13:11:06 +0300

    #3937 Fix import format
    #3937 Create process for importing discount schema

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492090_sys_gh3937_FixImportFormat.sql
@@ -0,0 +1,35 @@
+-- 2018-04-27T13:07:17.133
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ImpFormat SET Name='Import Discount Schema',Updated=TO_TIMESTAMP('2018-04-27 13:07:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_ID=540019
+;
+
+-- 2018-04-27T13:07:50.976
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ImpFormat_Row SET SeqNo=80, StartNo=8,Updated=TO_TIMESTAMP('2018-04-27 13:07:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540544
+;
+
+-- 2018-04-27T13:08:03.355
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ImpFormat_Row SET SeqNo=100, StartNo=10,Updated=TO_TIMESTAMP('2018-04-27 13:08:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540545
+;
+
+-- 2018-04-27T13:08:36.737
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,559795,540019,540546,0,TO_TIMESTAMP('2018-04-27 13:08:36','YYYY-MM-DD HH24:MI:SS'),100,'N','.','N',0,'Y','Break Discount %',90,9,TO_TIMESTAMP('2018-04-27 13:08:36','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2018-04-27T13:09:02.740
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,559798,540019,540547,0,TO_TIMESTAMP('2018-04-27 13:09:02','YYYY-MM-DD HH24:MI:SS'),100,'S','.','N',0,'Y','IsPriceOverride',50,5,TO_TIMESTAMP('2018-04-27 13:09:02','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2018-04-27T13:09:22.850
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,559797,540019,540548,0,TO_TIMESTAMP('2018-04-27 13:09:22','YYYY-MM-DD HH24:MI:SS'),100,'S','.','N',0,'Y','PriceBase',60,6,TO_TIMESTAMP('2018-04-27 13:09:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2018-04-27T13:09:44.697
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,559786,540019,540549,0,TO_TIMESTAMP('2018-04-27 13:09:44','YYYY-MM-DD HH24:MI:SS'),100,'N','.','N',0,'Y','Standardpreis',70,7,TO_TIMESTAMP('2018-04-27 13:09:44','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
