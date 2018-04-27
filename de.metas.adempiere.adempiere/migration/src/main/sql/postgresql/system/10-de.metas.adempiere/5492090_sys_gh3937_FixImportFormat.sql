75e2e411bba21ba487560d9e7ddab72f9cca57ca
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-04-27 15:36:45 +0300

    #3937 Minor
    #3937 Create process for importing discount schema

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492090_sys_gh3937_FixImportFormat.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492090_sys_gh3937_FixImportFormat.sql
@@ -33,3 +33,23 @@ INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFo
 INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,559786,540019,540549,0,TO_TIMESTAMP('2018-04-27 13:09:44','YYYY-MM-DD HH24:MI:SS'),100,'N','.','N',0,'Y','Standardpreis',70,7,TO_TIMESTAMP('2018-04-27 13:09:44','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+
+
+
+-- 2018-04-27T15:29:44.337
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ImpFormat_Row SET DecimalPoint=',',Updated=TO_TIMESTAMP('2018-04-27 15:29:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540549
+;
+
+-- 2018-04-27T15:29:48.647
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ImpFormat_Row SET DecimalPoint=',',Updated=TO_TIMESTAMP('2018-04-27 15:29:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540544
+;
+
+-- 2018-04-27T15:29:52.339
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ImpFormat_Row SET DecimalPoint=',',Updated=TO_TIMESTAMP('2018-04-27 15:29:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ImpFormat_Row_ID=540546
+;
+
+
+
--- de.metas.business/src/main/java/de/metas/pricing/impexp/DiscountSchemaImportProcess.java
+++ de.metas.business/src/main/java/de/metas/pricing/impexp/DiscountSchemaImportProcess.java
@@ -18,6 +18,7 @@ import org.compiere.model.I_M_DiscountSchema;
 import org.compiere.model.I_M_DiscountSchemaBreak;
 import org.compiere.model.ModelValidationEngine;
 import org.compiere.model.X_I_DiscountSchema;
+import org.compiere.model.X_I_Request;
 import org.compiere.model.X_M_DiscountSchema;
 
 import lombok.NonNull;
@@ -208,4 +209,11 @@ public class DiscountSchemaImportProcess extends AbstractImportProcess<I_I_Disco
 		schemaBreak.setStd_AddAmt(importRecord.getStd_AddAmt());
 	}
 
+	@Override
+	protected void markImported(@NonNull final I_I_DiscountSchema importRecord)
+	{
+		importRecord.setI_IsImported(X_I_Request.I_ISIMPORTED_Imported);
+		importRecord.setProcessed(true);
+		InterfaceWrapperHelper.save(importRecord);
+	}
 }
