8e43e1faf6b7a6ca07083f393efe104aa14f14be
Mark Krake <mark.krake@metasfresh.com>
2018-04-28 09:26:56 +0200

    Adds Action "Create from"
    https://github.com/metasfresh/metasfresh/issues/3137

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492150_sys_gh3137-payment-selection-trl.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492150_sys_gh3137-payment-selection-trl.sql
@@ -18,3 +18,8 @@ UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:20:3
 UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:20:52','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Process_ID=540461 AND AD_Language='en_US'
 ;
 
+-- 2018-04-28T09:25:22.711
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,156,426,TO_TIMESTAMP('2018-04-28 09:25:22','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.banking','Y',TO_TIMESTAMP('2018-04-28 09:25:22','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
+;
+
