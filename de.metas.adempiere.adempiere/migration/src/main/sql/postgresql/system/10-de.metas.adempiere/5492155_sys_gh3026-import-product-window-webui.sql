8ad9fa25f939a13ef60a09bea321b2a6cb9353d6
Mark Krake <mark.krake@metasfresh.com>
2018-04-28 12:44:07 +0200

    Minor bump

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492155_sys_gh3026-import-product-window-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492155_sys_gh3026-import-product-window-webui.sql
@@ -1,4 +1,4 @@
--- 2018-04-28T09:35:51.570
+-- 2018-04-28T09:35:51.570 
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Menu (Action,AD_Client_ID,AD_Menu_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,Description,EntityType,InternalName,IsActive,IsCreateNew,IsReadOnly,IsSOTrx,IsSummary,Name,Updated,UpdatedBy,WEBUI_NameBrowse) VALUES ('W',0,541080,0,247,TO_TIMESTAMP('2018-04-28 09:35:51','YYYY-MM-DD HH24:MI:SS'),100,'Produkte importieren','EE05','_Import_Produkt','Y','N','N','N','N','Import Produkt',TO_TIMESTAMP('2018-04-28 09:35:51','YYYY-MM-DD HH24:MI:SS'),100,'Import Produkt')
 ;
