599a7df663719f0632bcaee0e376f6d35ba8b67d
Mark Krake <mark.krake@metasfresh.com>
2018-04-28 09:23:08 +0200

    Adds Action Translations for en_US #3137
    https://github.com/metasfresh/metasfresh/issues/3137

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492150_sys_gh3137-payment-selection-trl.sql
@@ -0,0 +1,20 @@
+-- 2018-04-28T09:19:34.784
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:19:34','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Print Advice of Settlement',Description='' WHERE AD_Process_ID=540531 AND AD_Language='en_US'
+;
+
+-- 2018-04-28T09:20:12.781
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:20:12','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Print Payment Journal',Description='' WHERE AD_Process_ID=540534 AND AD_Language='en_US'
+;
+
+-- 2018-04-28T09:20:38.072
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:20:38','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create Payments' WHERE AD_Process_ID=540593 AND AD_Language='en_US'
+;
+
+-- 2018-04-28T09:20:52.250
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:20:52','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Process_ID=540461 AND AD_Language='en_US'
+;
+
