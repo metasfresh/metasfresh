261941f22feebc061d27e074db92314e5cb0738d
Teo Sarca <teo.sarca@metasfresh.com>
2018-04-28 13:04:34 +0300

    bugfix failing migration script
    https://github.com/metasfresh/metasfresh/issues/3026
    
    ```
    11:53:33
    psql:/home/metasfresh/metasfresh-dist-dist-5.55.1-9911+master-sql-only-master/dist/sql/10-de.metas.adempiere/5492155_sys_gh3026-import-product-window-webui.sql:24:
    ERROR:  duplicate key value violates unique constraint
    "ad_table_process_key"
    11:53:33     DETAIL:  Key (ad_process_id, ad_table_id)=(156, 426)
    already exists.
    ```

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492155_sys_gh3026-import-product-window-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492155_sys_gh3026-import-product-window-webui.sql
@@ -20,8 +20,8 @@ UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-28 09:20:5
 
 -- 2018-04-28T09:25:22.711
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,156,426,TO_TIMESTAMP('2018-04-28 09:25:22','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.banking','Y',TO_TIMESTAMP('2018-04-28 09:25:22','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
-;
+-- INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,156,426,TO_TIMESTAMP('2018-04-28 09:25:22','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.banking','Y',TO_TIMESTAMP('2018-04-28 09:25:22','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
+-- ;
 
 -- 2018-04-28T09:35:51.570
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
