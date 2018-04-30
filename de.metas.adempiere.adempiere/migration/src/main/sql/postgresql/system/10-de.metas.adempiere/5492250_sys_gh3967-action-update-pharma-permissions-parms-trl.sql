fb3c407a6da58531a9cea440b0cebde18cf14c6e
Mark Krake <mark.krake@metasfresh.com>
2018-04-30 08:01:45 +0200

    Adds Translations for Vendor en_US #3967
    https://github.com/metasfresh/metasfresh/issues/3967

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492250_sys_gh3967-action-update-pharma-permissions-parms-trl.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492250_sys_gh3967-action-update-pharma-permissions-parms-trl.sql
@@ -38,3 +38,18 @@ UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-30 07
 UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-30 07:56:37','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Veterinary Pharmacy Pemission' WHERE AD_Process_Para_ID=541286 AND AD_Language='en_US'
 ;
 
+-- 2018-04-30T08:00:18.754
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-30 08:00:18','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Pharma Agent Permission' WHERE AD_Process_Para_ID=541287 AND AD_Language='en_US'
+;
+
+-- 2018-04-30T08:00:37.767
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-30 08:00:37','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Pharma Manufacturer Permission' WHERE AD_Process_Para_ID=541288 AND AD_Language='en_US'
+;
+
+-- 2018-04-30T08:00:57.676
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-04-30 08:00:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Pharma Wholesale Permission' WHERE AD_Process_Para_ID=541289 AND AD_Language='en_US'
+;
+
