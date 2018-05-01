50473186c87bcb7fd3bba0970e1b7c1986920739
Mark Krake <mark.krake@metasfresh.com>
2018-05-01 11:14:11 +0200

    Adds Action Validate workflow & removes Button
    https://github.com/metasfresh/metasfresh/issues/3973

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5492310_sys_gh3873-pp-workflow-adjustments-webui.sql
@@ -0,0 +1,10 @@
+-- 2018-05-01T11:01:18.474
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,AD_Window_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,304,117,53005,TO_TIMESTAMP('2018-05-01 11:01:18','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2018-05-01 11:01:18','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
+;
+
+-- 2018-05-01T11:08:28.457
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=543854
+;
+
