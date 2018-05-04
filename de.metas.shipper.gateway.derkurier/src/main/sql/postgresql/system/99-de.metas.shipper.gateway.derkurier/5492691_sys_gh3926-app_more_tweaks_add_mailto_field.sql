f317d9d314a5913c91013814a9fee615cf81d17b
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-05-04 18:23:24 +0200

    solve SQL problem
    Implement support for `der Kurier` shipper https://github.com/metasfresh/metasfresh/issues/3926

--- de.metas.shipper.gateway.derkurier/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.derkurier/5492691_sys_gh3926-app_more_tweaks_add_mailto_field.sql
+++ de.metas.shipper.gateway.derkurier/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.derkurier/5492691_sys_gh3926-app_more_tweaks_add_mailto_field.sql
@@ -1,12 +1,3 @@
--- 2018-05-04T16:12:37.488
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-/* DDL */ SELECT public.db_alter_table('DerKurier_Shipper_Config','ALTER TABLE public.DerKurier_Shipper_Config ADD COLUMN AD_Sequence_ID NUMERIC(10) NOT NULL')
-;
-
--- 2018-05-04T16:12:37.500
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-ALTER TABLE DerKurier_Shipper_Config ADD CONSTRAINT ADSequence_DerKurierShipperConfig FOREIGN KEY (AD_Sequence_ID) REFERENCES public.AD_Sequence DEFERRABLE INITIALLY DEFERRED
-;
 
 -- 2018-05-04T16:38:25.949
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
