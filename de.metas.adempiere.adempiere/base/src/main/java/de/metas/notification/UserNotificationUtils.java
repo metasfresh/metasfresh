8be23677ccc1e2ea58386e988c5edb9675cde767
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-05-04 16:16:48 +0300

    #3980 Add message to the event
    #3980 Notifications in Swing Client show no info

--- de.metas.adempiere.adempiere/base/src/main/java/de/metas/notification/UserNotificationUtils.java
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/notification/UserNotificationUtils.java
@@ -19,12 +19,12 @@ import lombok.experimental.UtilityClass;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -58,6 +58,8 @@ public class UserNotificationUtils
 
 		return Event.builder()
 				.addRecipient_User_ID(notification.getRecipientUserId())
+				.setDetailADMessage(notification.getDetailADMessage())
+				.setDetailPlain(notification.getDetailPlain())
 				.putProperty(EVENT_PARAM_Notification, notificationAsJson)
 				.build();
 	}
