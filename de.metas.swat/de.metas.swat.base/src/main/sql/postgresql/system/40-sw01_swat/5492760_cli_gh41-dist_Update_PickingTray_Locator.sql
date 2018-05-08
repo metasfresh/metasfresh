4f7015367e3538aac3ba57f5734422484261f3c8
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-05-08 11:41:59 +0300

    #gh41-dist hardcode the corect locator id
    https://github.com/metasfresh/metasfresh-dist/issues/41

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5492760_cli_gh41-dist_Update_PickingTray_Locator.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5492760_cli_gh41-dist_Update_PickingTray_Locator.sql
@@ -1,8 +1,4 @@
 
 UPDATE M_PickingSlot ps
-SET M_Locator_ID = ( select l.M_Locator_ID from M_Locator l 
-		where l.IsAfterPickingLocator = 'Y'
-		and l.IsActive = 'Y' and l.M_Warehouse_ID = ps.M_Warehouse_ID
-		limit 1
-	) ;
+SET M_Locator_ID = 540010;
 	
