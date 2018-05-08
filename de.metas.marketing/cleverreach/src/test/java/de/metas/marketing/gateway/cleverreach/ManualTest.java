6048865ecbf3e9fd8978cd6627bf5aebc6d6097e
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-05-08 15:16:00 +0200

    move basic and cleverreach stuff into separate projects
    CleverReach Interface https://github.com/metasfresh/metasfresh/issues/4015

---
+++ de.metas.marketing/cleverreach/src/test/java/de/metas/marketing/gateway/cleverreach/ManualTest.java
@@ -0,0 +1,41 @@
+package de.metas.marketing.gateway.cleverreach;
+
+import de.metas.marketing.gateway.cleverreach.CleverReachClient;
+import de.metas.marketing.gateway.cleverreach.CleverReachConfig;
+
+/*
+ * #%L
+ * de.metas.marketing
+ * %%
+ * Copyright (C) 2018 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+public class ManualTest
+{
+
+	public static void main(String args[])
+	{
+		final CleverReachConfig cleverReachConfig = CleverReachConfig.builder()
+				.client_id("")
+				.login("")
+				.password("").build();
+
+		final CleverReachClient cleverReachClient = CleverReachClient.createNewClientAndLogin(cleverReachConfig);
+		cleverReachClient.retrieveGroups();
+	}
+}
