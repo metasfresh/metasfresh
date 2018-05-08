642ec566134d01f530d5f1d0dd74f3641f3c5f2c
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-05-08 11:12:07 +0300

    #gh41-dist change update query for picking locator in picking slot
    https://github.com/metasfresh/metasfresh-dist/issues/41

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5492760_cli_gh41-dist_Update_PickingTray_Locator.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5492760_cli_gh41-dist_Update_PickingTray_Locator.sql
@@ -1,10 +1,8 @@
+
 UPDATE M_PickingSlot ps
-SET M_Locator_ID = x.M_Locator_ID
-FROM
-	(
-		select l.M_Locator_ID, l.M_Warehouse_ID
-		from M_Locator l
+SET M_Locator_ID = ( select l.M_Locator_ID from M_Locator l 
 		where l.IsAfterPickingLocator = 'Y'
-		and l.IsActive = 'Y' 
-	) x
-WHERE x.M_Warehouse_ID = ps.M_Warehouse_ID and x.M_Warehouse_ID <> ps.M_Warehouse_ID;
+		and l.IsActive = 'Y' and l.M_Warehouse_ID = ps.M_Warehouse_ID
+		limit 1
+	) ;
+	
