cc26b2c45de73d9bd3651616db8a00b6da3ad9ab
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-05-11 13:56:21 +0300

    #4034 make customerCode and collectorCode mandatory
    https://github.com/metasfresh/metasfresh/issues/4034

---
+++ de.metas.shipper.gateway.derkurier/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.derkurier/5493290_sys_gh4034_DerKurierShipperConfig_CustomerCode_And_CollectorCodeMandatory.sql
@@ -0,0 +1,9 @@
+-- 2018-05-11T11:44:23.696
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-05-11 11:44:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560052
+;
+
+-- 2018-05-11T13:53:34.274
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-05-11 13:53:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560053
+;
---
+++ de.metas.shipper.gateway.derkurier/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.derkurier/5493320_sys_gh4034_DerKurierShipperConfig_CustomerCode_And_CollectorCodeMandatory_DDL.sql
@@ -0,0 +1,22 @@
+
+-- 2018-05-11T11:44:34.689
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('derkurier_deliveryorderline','CustomerCode','VARCHAR(250)',null,null)
+;
+
+-- 2018-05-11T11:44:34.698
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('derkurier_deliveryorderline','CustomerCode',null,'NOT NULL',null)
+;
+
+
+-- 2018-05-11T13:53:37.843
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('derkurier_deliveryorderline','CollectorCode','VARCHAR(250)',null,null)
+;
+
+-- 2018-05-11T13:53:37.849
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('derkurier_deliveryorderline','CollectorCode',null,'NOT NULL',null)
+;
+
