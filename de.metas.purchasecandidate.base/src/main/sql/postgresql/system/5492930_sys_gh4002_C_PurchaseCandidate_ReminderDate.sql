b084e23d5b2c5a42b098120a0d46a6f86d9c6cb4
Teo Sarca <teo.sarca@metasfresh.com>
2018-05-12 10:36:43 +0300

    C_PurchaseCandidate.ReminderDate DDL
    https://github.com/metasfresh/metasfresh/issues/4002

--- de.metas.purchasecandidate.base/src/main/sql/postgresql/system/5492930_sys_gh4002_C_PurchaseCandidate_ReminderDate.sql
+++ de.metas.purchasecandidate.base/src/main/sql/postgresql/system/5492930_sys_gh4002_C_PurchaseCandidate_ReminderDate.sql
@@ -13,3 +13,8 @@ VALUES (16,'N','N','N','N',0,'Y',100,543388,'Y','N','N','N','N','N','N','N','Y',
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=560047 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
 
+-- 2018-05-11T18:05:37.305
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ SELECT public.db_alter_table('C_PurchaseCandidate','ALTER TABLE public.C_PurchaseCandidate ADD COLUMN ReminderDate TIMESTAMP WITH TIME ZONE')
+;
+
