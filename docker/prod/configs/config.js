d8f7617632caf7b21647a212ea835595a483fa05
siemiatj <kuba@saskla.cz>
2018-05-14 11:58:31 +0200

    - add missing plugins entry to config - change path to config

--- docker/prod/configs/config.js
+++ docker/prod/configs/config.js
@@ -1,6 +1,9 @@
 /*eslint-disable */
 const config = {
         API_URL: 'http://WEBUI_API_HOST:WEBUI_API_PORT/rest/api',
-        WS_URL: 'http://WEBUI_API_HOST:WEBUI_API_PORT/stomp'
+        WS_URL: 'http://WEBUI_API_HOST:WEBUI_API_PORT/stomp',
+        plugins: []
 }
+
+module.exports = config;
 /*eslint-enable */
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var config = require('./config');
+var config = require('./dist/config');
 
 module.exports = {
   mode: 'production',
