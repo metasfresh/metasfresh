ce8a97b647d2907bc0156b22dd7fc1e420129cbf
siemiatj <kuba@saskla.cz>
2018-05-17 01:56:41 +0200

    - experimenting with injecting redux state from plugins

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { routerMiddleware } from 'react-router-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux-dynamic-reducer';
 import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
 
 import rootReducer from '../reducers';
 
 export default function configureStore(history) {
-  const middleware = [thunk, routerMiddleware(history)];
+  const middleware = [thunk, promiseMiddleware, routerMiddleware(history)];
   const store = createStore(
     rootReducer,
     compose(
