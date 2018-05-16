217cce7620258b517e62d1be67a101e7954c2236
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-05-16 17:01:14 +0300

    #3753 Update existent lines
    #3753 Inventory: Prepare Inventorylines shall update existing lines

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/process/M_Inventory_CreateCountLinesFromHU.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/process/M_Inventory_CreateCountLinesFromHU.java
@@ -1,11 +1,15 @@
 package de.metas.handlingunits.inventory.process;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.stream.Stream;
 
+import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.Services;
 import org.compiere.model.I_M_Inventory;
+
+import com.google.common.collect.ImmutableList;
 
 import de.metas.adempiere.model.I_M_Product;
 import de.metas.handlingunits.IHUQueryBuilder;
@@ -16,11 +20,13 @@ import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_InventoryLine;
 import de.metas.handlingunits.model.I_M_Locator;
 import de.metas.handlingunits.storage.IHUProductStorage;
+import de.metas.inventory.IInventoryDAO;
 import de.metas.process.IProcessPrecondition;
 import de.metas.process.IProcessPreconditionsContext;
 import de.metas.process.JavaProcess;
 import de.metas.process.Param;
 import de.metas.process.ProcessPreconditionsResolution;
+import lombok.NonNull;
 
 /*
  * #%L
@@ -77,11 +83,13 @@ public class M_Inventory_CreateCountLinesFromHU extends JavaProcess implements I
 	@Override
 	protected String doIt()
 	{
+		final List<Integer> exitentHUIdsInInventoryLines = fetchExistentHUIds();
+
 		final long countInventoryLines = streamHUs()
-				.flatMap(hu -> createInventoryLines(hu))
+				.flatMap(hu -> createUpdateInventoryLines(hu, exitentHUIdsInInventoryLines.contains(hu.getM_HU_ID())))
 				.count();
 
-		return "@Created@ #" + countInventoryLines;
+		return "@Created@/@Updated@ #" + countInventoryLines;
 	}
 
 	private I_M_Inventory getInventory()
@@ -91,6 +99,16 @@ public class M_Inventory_CreateCountLinesFromHU extends JavaProcess implements I
 			_inventory = getRecord(I_M_Inventory.class);
 		}
 		return _inventory;
+	}
+
+	private List<Integer> fetchExistentHUIds()
+	{
+		final I_M_Inventory inventory = getInventory();
+		return Services.get(IInventoryDAO.class).retrieveLinesForInventoryId(inventory.getM_Inventory_ID(), I_M_InventoryLine.class)
+				.stream()
+				.map(I_M_InventoryLine::getM_HU_ID)
+				.collect(ImmutableList.toImmutableList());
+
 	}
 
 	private Stream<I_M_HU> streamHUs()
@@ -117,24 +135,33 @@ public class M_Inventory_CreateCountLinesFromHU extends JavaProcess implements I
 				.iterateAndStream();
 	}
 
-	private Stream<I_M_InventoryLine> createInventoryLines(final I_M_HU hu)
+	private Stream<I_M_InventoryLine> createUpdateInventoryLines(@NonNull final I_M_HU hu, final boolean updateLine)
 	{
 		return Services.get(IHandlingUnitsBL.class)
 				.getStorageFactory()
 				.streamHUProductStorages(hu)
-				.map(huProductStorage -> createInventoryLine(huProductStorage));
+				.map(huProductStorage -> createUpdateInventoryLine(huProductStorage, updateLine));
 	}
 
-	private I_M_InventoryLine createInventoryLine(final IHUProductStorage huProductStorage)
+	private I_M_InventoryLine createUpdateInventoryLine(@NonNull final IHUProductStorage huProductStorage, final boolean updateLine)
 	{
 		final I_M_Inventory inventory = getInventory();
-		final I_M_InventoryLine inventoryLine = InterfaceWrapperHelper.newInstance(I_M_InventoryLine.class);
+		final I_M_InventoryLine inventoryLine;
+		final I_M_HU hu = huProductStorage.getM_HU();
+		if (updateLine)
+		{
+			inventoryLine = fetchInventoryLineForHU(hu);
+		}
+		else
+		{
+			inventoryLine = InterfaceWrapperHelper.newInstance(I_M_InventoryLine.class);
+		}
+
 		inventoryLine.setM_Inventory(inventory);
 		inventoryLine.setAD_Org_ID(inventory.getAD_Org_ID());
 		inventoryLine.setM_Product_ID(huProductStorage.getM_Product_ID());
 		inventoryLine.setM_AttributeSetInstance_ID(0);
 
-		final I_M_HU hu = huProductStorage.getM_HU();
 		inventoryLine.setM_Locator_ID(hu.getM_Locator_ID());
 		inventoryLine.setM_HU_ID(hu.getM_HU_ID());
 		inventoryLine.setM_HU_PI_Item_Product(null); // TODO
@@ -147,5 +174,15 @@ public class M_Inventory_CreateCountLinesFromHU extends JavaProcess implements I
 		InterfaceWrapperHelper.save(inventoryLine);
 
 		return inventoryLine;
+	}
+
+	private I_M_InventoryLine fetchInventoryLineForHU(final I_M_HU hu)
+	{
+		return Services.get(IQueryBL.class).createQueryBuilder(I_M_InventoryLine.class)
+				.addOnlyActiveRecordsFilter()
+				.addEqualsFilter(I_M_InventoryLine.COLUMNNAME_M_Inventory_ID, getInventory().getM_Inventory_ID())
+				.addEqualsFilter(I_M_InventoryLine.COLUMNNAME_M_HU_ID, hu.getM_HU_ID())
+				.create()
+				.firstOnly(I_M_InventoryLine.class);
 	}
 }
