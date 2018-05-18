81b1bf02d419857832516836c0d4a9c1d34fa092
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-05-18 13:24:31 +0200

    minor
    New contract Type: Yearly Bonus from vendors https://github.com/metasfresh/metasfresh/issues/3999

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5493880_sys_gh3999-app_invoice_candidate_assignement_tab.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5493880_sys_gh3999-app_invoice_candidate_assignement_tab.sql
@@ -103,3 +103,8 @@ UPDATE AD_Tab SET Name='Zuordnung Rückvergütung',Updated=TO_TIMESTAMP('2018-05
 UPDATE AD_Tab SET IsInsertRecord='N', IsReadOnly='Y',Updated=TO_TIMESTAMP('2018-05-18 10:17:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541110
 ;
 
+-- 2018-05-18T13:22:52.345
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET AD_Window_ID=540092,Updated=TO_TIMESTAMP('2018-05-18 13:22:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=540981
+;
+
