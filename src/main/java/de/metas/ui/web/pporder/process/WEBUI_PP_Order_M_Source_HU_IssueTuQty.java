bfa8050140c277e6e8eabcb6740812a0dd1a3a74
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-05-18 13:59:36 +0300

    #3463 Refactoring
    https://github.com/metasfresh/metasfresh/issues/3463

--- src/main/java/de/metas/ui/web/pporder/process/WEBUI_PP_Order_M_Source_HU_IssueTuQty.java
+++ src/main/java/de/metas/ui/web/pporder/process/WEBUI_PP_Order_M_Source_HU_IssueTuQty.java
@@ -1,7 +1,5 @@
 package de.metas.ui.web.pporder.process;
 
-import static org.adempiere.model.InterfaceWrapperHelper.load;
-
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -9,8 +7,6 @@ import java.util.Map;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.util.Services;
-import org.adempiere.util.StringUtils;
-import org.eevolution.model.I_PP_Order;
 
 import com.google.common.collect.ImmutableList;
 
@@ -20,14 +16,13 @@ import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_Source_HU;
 import de.metas.handlingunits.pporder.api.IHUPPOrderBL;
 import de.metas.handlingunits.sourcehu.SourceHUsService;
-import de.metas.handlingunits.sourcehu.SourceHUsService.MatchingSourceHusQuery;
 import de.metas.handlingunits.storage.EmptyHUListener;
 import de.metas.process.IProcessPrecondition;
 import de.metas.process.Param;
 import de.metas.process.ProcessPreconditionsResolution;
 import de.metas.ui.web.pporder.PPOrderLineRow;
 import de.metas.ui.web.pporder.PPOrderLinesView;
-import lombok.NonNull;
+import de.metas.ui.web.pporder.util.WEBUI_PP_Order_ProcessHelper;
 
 /*
  * #%L
@@ -65,25 +60,10 @@ public class WEBUI_PP_Order_M_Source_HU_IssueTuQty
 		{
 			return ProcessPreconditionsResolution.rejectBecauseNotSingleSelection();
 		}
-		final PPOrderLineRow singleSelectedRow = getSingleSelectedRow();
-		if (!singleSelectedRow.isIssue())
-		{
-			final String internalReason = StringUtils.formatMessage("The selected row is not an issue-row; row={}", singleSelectedRow);
-			return ProcessPreconditionsResolution.rejectWithInternalReason(internalReason);
-		}
-		if (singleSelectedRow.isProcessed())
-		{
-			final String internalReason = StringUtils.formatMessage("The selected row is already processed; row={}", singleSelectedRow);
-			return ProcessPreconditionsResolution.rejectWithInternalReason(internalReason);
-		}
 
-		final List<I_M_Source_HU> sourceHus = retrieveActiveSourceHus(singleSelectedRow);
-		if (sourceHus.isEmpty())
-		{
-			final String internalReason = StringUtils.formatMessage("There are no sourceHU records for the selected row; row={}", singleSelectedRow);
-			return ProcessPreconditionsResolution.rejectWithInternalReason(internalReason);
-		}
-		return ProcessPreconditionsResolution.accept();
+		final PPOrderLineRow singleSelectedRow = getSingleSelectedRow();
+
+		return WEBUI_PP_Order_ProcessHelper.checkPreconditionsApplicable(singleSelectedRow);
 	}
 
 	@Override
@@ -91,7 +71,7 @@ public class WEBUI_PP_Order_M_Source_HU_IssueTuQty
 	{
 		final PPOrderLineRow row = getSingleSelectedRow();
 
-		final List<I_M_Source_HU> sourceHus = retrieveActiveSourceHus(row);
+		final List<I_M_Source_HU> sourceHus = WEBUI_PP_Order_ProcessHelper.retrieveActiveSourceHus(row);
 		if (sourceHus.isEmpty())
 		{
 			throw new AdempiereException("@NoSelection@");
@@ -103,7 +83,7 @@ public class WEBUI_PP_Order_M_Source_HU_IssueTuQty
 				.peek(sourceHu -> huId2SourceHu.put(sourceHu.getM_HU_ID(), sourceHu))
 				.map(I_M_Source_HU::getM_HU)
 				.collect(ImmutableList.toImmutableList());
-
+	
 		final HUsToNewTUsRequest request = HUsToNewTUsRequest.builder()
 				.sourceHUs(husThatAreFlaggedAsSource)
 				.qtyTU(qtyTU.intValue())
@@ -136,13 +116,4 @@ public class WEBUI_PP_Order_M_Source_HU_IssueTuQty
 		return MSG_OK;
 	}
 
-	private static List<I_M_Source_HU> retrieveActiveSourceHus(@NonNull final PPOrderLineRow row)
-	{
-		final I_PP_Order ppOrder = load(row.getPP_Order_ID(), I_PP_Order.class);
-
-		final MatchingSourceHusQuery query = MatchingSourceHusQuery.builder()
-				.productId(row.getM_Product_ID())
-				.warehouseId(ppOrder.getM_Warehouse_ID()).build();
-		return SourceHUsService.get().retrieveMatchingSourceHuMarkers(query);
-	}
 }
