a5b0a9129adbccd32a0a612b00440e5c975b3379
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-05-21 15:43:15 +0300

    #4079 Don;t show fields
    #4079 Contact is not set when order is cloned

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5493940_sys_gh4079_CopyAD_User_ID_Bill_User_ID.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5493940_sys_gh4079_CopyAD_User_ID_Bill_User_ID.sql
@@ -8,23 +8,3 @@ UPDATE AD_Column SET IsCalculated='N',Updated=TO_TIMESTAMP('2018-05-21 15:06:04'
 UPDATE AD_Column SET IsCalculated='N',Updated=TO_TIMESTAMP('2018-05-21 15:06:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=8763
 ;
 
--- 2018-05-21T15:24:11.231
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,557546,0,496,552065,540578,'F',TO_TIMESTAMP('2018-05-21 15:24:11','YYYY-MM-DD HH24:MI:SS'),100,'Y','Y','N','Y','N','N','IsSalesContact_Default',240,0,0,TO_TIMESTAMP('2018-05-21 15:24:11','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2018-05-21T15:24:41.719
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,557547,0,496,552066,540578,'F',TO_TIMESTAMP('2018-05-21 15:24:41','YYYY-MM-DD HH24:MI:SS'),100,'Y','Y','N','Y','N','N','IsPurchaseContact',240,0,0,TO_TIMESTAMP('2018-05-21 15:24:41','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2018-05-21T15:27:04.971
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_UI_Element SET SeqNo=102,Updated=TO_TIMESTAMP('2018-05-21 15:27:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552065
-;
-
--- 2018-05-21T15:27:08.508
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_UI_Element SET SeqNo=103,Updated=TO_TIMESTAMP('2018-05-21 15:27:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552066
-;
-
