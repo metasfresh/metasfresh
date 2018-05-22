4ae3df805a138ea684684c7a26913cdb1585f479
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-05-22 16:16:27 +0300

    #4058 Improve TimeUtil.asTimestamp: add LocalTIme argument
    https://github.com/metasfresh/metasfresh/issues/4058

--- de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/util/TimeUtil.java
+++ de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/util/TimeUtil.java
@@ -32,6 +32,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 import java.time.ZoneId;
 import java.util.BitSet;
 import java.util.Calendar;
@@ -56,6 +57,8 @@ import lombok.NonNull;
  */
 public class TimeUtil
 {
+	private static final LocalDate DATE_1970_01_01 = LocalDate.of(1970, Month.JANUARY, 1);
+
 	/**
 	 * Get earliest time of a day (truncate)
 	 *
@@ -1225,6 +1228,12 @@ public class TimeUtil
 		{
 			final LocalDate localDate = (LocalDate)obj;
 			final Instant instant = localDate.atStartOfDay(ZoneId.systemDefault()).toInstant();
+			return Timestamp.from(instant);
+		}
+		else if(obj instanceof LocalTime)
+		{
+			final LocalTime localTime = (LocalTime)obj;
+			final Instant instant = localTime.atDate(DATE_1970_01_01).atZone(ZoneId.systemDefault()).toInstant();
 			return Timestamp.from(instant);
 		}
 		else if(obj instanceof Instant)
