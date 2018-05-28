06ed440a6995b3857579ad86c0ab6c6dcc8929f1
Mark Krake <mark.krake@metasfresh.com>
2018-05-28 15:11:13 +0200

    Adds missing Translations EN_US #4117
    https://github.com/metasfresh/metasfresh/issues/4117

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5494590_sys_gh4117-product-window-field-trl.sql
@@ -0,0 +1,15 @@
+-- 2018-05-28T15:07:24.635
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-28 15:07:24','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='BOM' WHERE AD_Field_ID=3743 AND AD_Language='en_US'
+;
+
+-- 2018-05-28T15:07:34.659
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-28 15:07:34','YYYY-MM-DD HH24:MI:SS'),Name='Bill of Material' WHERE AD_Field_ID=3743 AND AD_Language='en_US'
+;
+
+-- 2018-05-28T15:07:57.815
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-28 15:07:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Manufactured' WHERE AD_Field_ID=556296 AND AD_Language='en_US'
+;
+
