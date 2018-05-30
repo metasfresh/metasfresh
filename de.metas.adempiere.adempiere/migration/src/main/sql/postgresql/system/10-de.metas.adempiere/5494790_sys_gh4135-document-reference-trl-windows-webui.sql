13585f5fc38a83a3c1fb03e7bbb4afbb71bc32e2
Mark Krake <mark.krake@metasfresh.com>
2018-05-30 12:12:18 +0200

    Adds docref for BOM trl window webui #4135
    https://github.com/metasfresh/metasfresh/issues/4135

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5494790_sys_gh4135-document-reference-trl-windows-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5494790_sys_gh4135-document-reference-trl-windows-webui.sql
@@ -2,3 +2,34 @@
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Table SET AD_Window_ID=540441,Updated=TO_TIMESTAMP('2018-05-30 11:57:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=339
 ;
+
+-- 2018-05-30T12:05:53.697
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET AD_Window_ID=540438,Updated=TO_TIMESTAMP('2018-05-30 12:05:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53191
+;
+
+-- 2018-05-30T12:06:51.456
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-30 12:06:51','YYYY-MM-DD HH24:MI:SS'),Name='Bill of Material Translation' WHERE AD_Tab_ID=541115 AND AD_Language='en_US'
+;
+
+-- 2018-05-30T12:07:04.107
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-30 12:07:04','YYYY-MM-DD HH24:MI:SS'),Name='Bill of Material Translation' WHERE AD_Window_ID=540438 AND AD_Language='en_US'
+;
+
+-- 2018-05-30T12:07:44.874
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-30 12:07:44','YYYY-MM-DD HH24:MI:SS'),Name='Bill of Material Translation',WEBUI_NameBrowse='Bill of Material Translation' WHERE AD_Menu_ID=541096 AND AD_Language='en_US'
+;
+
+-- 2018-05-30T12:08:13.788
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-30 12:08:13','YYYY-MM-DD HH24:MI:SS'),Name='Bill of Material',WEBUI_NameBrowse='Bill of Material' WHERE AD_Menu_ID=1000092 AND AD_Language='en_US'
+;
+
+-- 2018-05-30T12:08:19.432
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-05-30 12:08:19','YYYY-MM-DD HH24:MI:SS'),Description='Maintain Product Bill of Material & Formula ' WHERE AD_Menu_ID=1000092 AND AD_Language='en_US'
+;
+
