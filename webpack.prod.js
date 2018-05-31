12afae0be760af0f5d82ee7d3157e974f151790f
Pablo Sichert <mail@pablosichert.com>
2018-05-31 13:32:21 +0200

    Fix constant defined at wrong place
    Issue #1311

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,8 +11,8 @@ const plugins = [
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify('production'),
-      COMMIT_HASH: JSON.stringify(commitHash),
     },
+    COMMIT_HASH: JSON.stringify(commitHash),
   }),
   new HtmlWebpackPlugin({
     template: './index.html',
