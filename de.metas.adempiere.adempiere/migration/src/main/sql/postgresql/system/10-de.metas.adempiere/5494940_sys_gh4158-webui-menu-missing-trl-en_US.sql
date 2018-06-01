6e297c1879d16d7446454a8d2a26d38c7b2fe98d
Mark Krake <mark.krake@metasfresh.com>
2018-06-01 12:14:24 +0200

    Adds try for MSV3 avail. windows, menus #4158
    https://github.com/metasfresh/metasfresh/issues/4158

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5494940_sys_gh4158-webui-menu-missing-trl-en_US.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5494940_sys_gh4158-webui-menu-missing-trl-en_US.sql
@@ -113,3 +113,58 @@ UPDATE AD_Window SET Name='MSV3 Verfuegbarkeit Transaktion',Updated=TO_TIMESTAMP
 UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='MSV3 Verfuegbarkeit Transaktion',Updated=TO_TIMESTAMP('2018-06-01 12:05:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541026
 ;
 
+-- 2018-06-01T12:09:52.043
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Name='MSV3 Verfuegbarkeitsanfrage Einzelne', WEBUI_NameBrowse='MSV3 Verfuegbarkeitsanfrage Einzelne',Updated=TO_TIMESTAMP('2018-06-01 12:09:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541027
+;
+
+-- 2018-06-01T12:10:18.466
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 12:10:18','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MSV3 Single Availability Check',WEBUI_NameBrowse='MSV3 Single Availability Check' WHERE AD_Menu_ID=541027 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T12:10:30.286
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Name='MSV3 Verfuegbarkeitsanfrage Einzelne',Updated=TO_TIMESTAMP('2018-06-01 12:10:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540407
+;
+
+-- 2018-06-01T12:10:38.992
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 12:10:38','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MSV3 Single Availability Check' WHERE AD_Window_ID=540407 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T12:10:50.066
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Verfuegbarkeitsanfrage Einzelne',Updated=TO_TIMESTAMP('2018-06-01 12:10:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541007
+;
+
+-- 2018-06-01T12:10:57.888
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 12:10:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MSV3 Single Availability Check' WHERE AD_Tab_ID=541007 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T12:11:52.803
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Name='MSV3 Verfuegbarkeitsanfrage Einzelne Antwort', WEBUI_NameBrowse='MSV3 Verfuegbarkeitsanfrage Einzelne Antwort',Updated=TO_TIMESTAMP('2018-06-01 12:11:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541028
+;
+
+-- 2018-06-01T12:12:22.259
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 12:12:22','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MSV3 Single Availability Check Response',WEBUI_NameBrowse='MSV3 Single Availability Check Response' WHERE AD_Menu_ID=541028 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T12:12:36.995
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 12:12:36','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='MSV3 Single Availability Check Response' WHERE AD_Window_ID=540408 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T12:12:49.160
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Verfuegbarkeitsanfrage Einzelne Antwort',Updated=TO_TIMESTAMP('2018-06-01 12:12:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541009
+;
+
+-- 2018-06-01T12:13:01.705
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 12:13:01','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Single Availability Check Response' WHERE AD_Tab_ID=541009 AND AD_Language='en_US'
+;
+
