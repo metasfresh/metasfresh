ad73cfb6ca740b76e17bf4b06e473d7edc23ee32
Mark Krake <mark.krake@metasfresh.com>
2018-06-01 11:32:07 +0200

    Adds missing trl marketing person #4158
    Adds en_US Translation for Menu entry, window, browse name and tab.
    https://github.com/metasfresh/metasfresh/issues/4158

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5494940_sys_gh4158-webui-menu-missing-trl-en_US.sql
@@ -0,0 +1,25 @@
+-- 2018-06-01T11:23:22.544
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET WEBUI_NameBrowse='Marketing Kontaktperson',Updated=TO_TIMESTAMP('2018-06-01 11:23:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541090
+;
+
+-- 2018-06-01T11:23:47.722
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 11:23:47','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Marketing Contact' WHERE AD_Window_ID=540435 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T11:24:26.354
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 11:24:26','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Contact Person' WHERE AD_Tab_ID=541097 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T11:26:05.785
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 11:26:05','YYYY-MM-DD HH24:MI:SS'),Name='Marketing Contact Person' WHERE AD_Window_ID=540435 AND AD_Language='en_US'
+;
+
+-- 2018-06-01T11:26:30.274
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-01 11:26:30','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Marketing Contact Person',WEBUI_NameBrowse='Marketing Contact Person' WHERE AD_Menu_ID=541090 AND AD_Language='en_US'
+;
+
