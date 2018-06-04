29dc47f7a2c47d2fd32a939bd13597716f0a1153
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-04 12:20:52 +0300

    #4151 Fix method for updating print format
    #4151 Import: extend process of importing vendors

--- de.metas.business/src/main/java/org/adempiere/impexp/bpartner/BPartnerImportTableSqlUpdater.java
+++ de.metas.business/src/main/java/org/adempiere/impexp/bpartner/BPartnerImportTableSqlUpdater.java
@@ -340,7 +340,7 @@ public class BPartnerImportTableSqlUpdater
 		int no;
 		sql = new StringBuilder("UPDATE I_BPartner i "
 				+ "SET C_Aggregation_ID=(SELECT C_Aggregation_ID FROM C_Aggregation a"
-				+ " WHERE i.AggregationName=a.Name AND a.AD_Client_ID IN (0, i.AD_Client_ID) AND a.AD_Org_ID IN (0, i.AD_Org_ID ) "
+				+ " WHERE i.AggregationName=a.Name AND a.AD_Client_ID IN (0, i.AD_Client_ID) AND a.AD_Org_ID IN (0, i.AD_Org_ID ) )"
 				+ "WHERE C_Aggregation_ID IS NULL AND AggregationName IS NOT NULL"
 				+ " AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
 		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
@@ -388,7 +388,7 @@ public class BPartnerImportTableSqlUpdater
 		int no;
 		sql = new StringBuilder("UPDATE I_BPartner i "
 				+ "SET AD_PrintFormat_ID=(SELECT AD_PrintFormat_ID FROM AD_PrintFormat pf"
-				+ " WHERE i.PrintFormat_Name=pf.Name AND pf.AD_Client_ID IN (pf, i.AD_Client_ID) AND pf.AD_Org_ID IN (pf, i.AD_Org_ID ) "
+				+ " WHERE i.PrintFormat_Name=pf.Name AND pf.AD_Client_ID IN (0, i.AD_Client_ID) AND pf.AD_Org_ID IN (0, i.AD_Org_ID ) ) "
 				+ "WHERE AD_PrintFormat_ID IS NULL AND PrintFormat_Name IS NOT NULL"
 				+ " AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
 		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
