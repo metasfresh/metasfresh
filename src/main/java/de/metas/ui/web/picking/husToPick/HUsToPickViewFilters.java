6b2af6cbbfe915281516e58f1ba535758ba76e7f
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-06-04 10:17:18 +0300

    #4146 option for picking with no matching attributes
    (not finished)
    https://github.com/metasfresh/metasfresh/issues/4146

--- src/main/java/de/metas/ui/web/picking/husToPick/HUsToPickViewFilters.java
+++ src/main/java/de/metas/ui/web/picking/husToPick/HUsToPickViewFilters.java
@@ -1,15 +1,19 @@
 package de.metas.ui.web.picking.husToPick;
 
+import java.util.List;
 import java.util.Map;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.util.Services;
 import org.adempiere.warehouse.api.IWarehouseDAO;
+import org.compiere.util.DB;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 import de.metas.handlingunits.model.I_M_HU;
+import de.metas.handlingunits.picking.IHUPickingSlotBL;
+import de.metas.handlingunits.picking.IHUPickingSlotBL.PickingHUsQuery;
 import de.metas.i18n.IMsgBL;
 import de.metas.ui.web.document.filter.DocumentFilter;
 import de.metas.ui.web.document.filter.DocumentFilterDescriptor;
@@ -50,15 +54,21 @@ import lombok.experimental.UtilityClass;
 	private static final String LocatorBarcode_FilterId = "LocatorBarcodeFilter";
 	private static final String PARAM_Barcode = "Barcode";
 
+	private static final String HU_IDS_FilterId = "HU_IDS_Filter";
+	private static final String PARAM_ConsiderAttributes = "ConsiderAttributes";
+
 	public static ImmutableList<DocumentFilterDescriptor> createFilterDescriptors()
 	{
-		return ImmutableList.of(createLocatorBarcodeFilters());
+		return ImmutableList.of(
+				createLocatorBarcodeFilters(),
+				createHUIdsFilter());
 	}
 
 	public static Map<String, SqlDocumentFilterConverter> createFilterConvertersIndexedByFilterId()
 	{
 		return ImmutableMap.<String, SqlDocumentFilterConverter> builder()
 				.put(LocatorBarcode_FilterId, HUsToPickViewFilters::getLocatorBarcodeFilterSql)
+				.put(HU_IDS_FilterId, HUsToPickViewFilters::getHUIdsFilterSql)
 				.build();
 	}
 
@@ -95,4 +105,49 @@ import lombok.experimental.UtilityClass;
 		return sql;
 	}
 
+	private static final DocumentFilterDescriptor createHUIdsFilter()
+	{
+		return DocumentFilterDescriptor.builder()
+				.setFilterId(HU_IDS_FilterId)
+				.setFrequentUsed(true)
+				.addParameter(DocumentFilterParamDescriptor.builder()
+						.setFieldName(PARAM_ConsiderAttributes)
+						.setDisplayName(Services.get(IMsgBL.class).getTranslatableMsgText(PARAM_ConsiderAttributes))
+						.setMandatory(true)
+						.setWidgetType(DocumentFieldWidgetType.YesNo))
+				.build();
+	}
+
+	public static String getHUIdsFilterSql(SqlParamsCollector sqlParamsOut, DocumentFilter filter, final SqlOptions sqlOpts)
+	{
+		if (!HU_IDS_FilterId.equals(filter.getFilterId()))
+		{
+			throw new AdempiereException("Invalid filterId " + filter.getFilterId() + ". Expected: " + HU_IDS_FilterId);
+		}
+
+		final Boolean considerAttributes = filter.getParameterValueAsBoolean(PARAM_ConsiderAttributes, true);
+		final List<Integer> huIds = retrieveAvailableHuIdsForCurrentShipmentScheduleId(considerAttributes);
+		if (huIds.isEmpty())
+		{
+			return "/* no M_HU_IDs */ 1=0";
+		}
+
+		final String sql = sqlOpts.getTableNameOrAlias() + "." + I_M_HU.COLUMNNAME_M_HU_ID + " IN " + DB.buildSqlList(huIds);
+		return sql;
+	}
+
+	private static List<Integer> retrieveAvailableHuIdsForCurrentShipmentScheduleId(final boolean considerAttributes)
+	{
+		final IHUPickingSlotBL huPickingSlotBL = Services.get(IHUPickingSlotBL.class);
+
+		// final int shipmentScheduleId = getView().getCurrentShipmentScheduleId();
+
+		final PickingHUsQuery query = PickingHUsQuery.builder()
+				// .shipmentSchedule(loadOutOfTrx(shipmentScheduleId, I_M_ShipmentSchedule.class)) // TODO: provide the IView as parameter; take the shipment schedule from the view
+				.onlyTopLevelHUs(false)
+				.onlyIfAttributesMatchWithShipmentSchedules(considerAttributes)
+				.build();
+		final List<Integer> availableHUIdsToPick = huPickingSlotBL.retrieveAvailableHUIdsToPick(query);
+		return availableHUIdsToPick;
+	}
 }
