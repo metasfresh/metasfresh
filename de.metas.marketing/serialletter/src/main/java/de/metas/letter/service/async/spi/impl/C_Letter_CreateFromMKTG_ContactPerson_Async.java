f4fdea9bb65538c52e3f02c2d977fd89fb333a98
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-06 12:27:41 +0300

    #4153 Remove unnecessary code
    #4153 Serial Letter Feature

--- de.metas.marketing/serialletter/src/main/java/de/metas/letter/service/async/spi/impl/C_Letter_CreateFromMKTG_ContactPerson_Async.java
+++ de.metas.marketing/serialletter/src/main/java/de/metas/letter/service/async/spi/impl/C_Letter_CreateFromMKTG_ContactPerson_Async.java
@@ -1,22 +1,15 @@
 package de.metas.letter.service.async.spi.impl;
 
-import static org.adempiere.model.InterfaceWrapperHelper.load;
-
 import java.util.List;
-import java.util.Properties;
 
-import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.location.Location;
 import org.adempiere.location.LocationRepository;
 import org.adempiere.util.Services;
-import org.adempiere.util.lang.impl.TableRecordReference;
 import org.compiere.Adempiere;
-import org.compiere.util.Env;
 
 import de.metas.async.api.IQueueDAO;
 import de.metas.async.model.I_C_Queue_WorkPackage;
 import de.metas.async.spi.WorkpackageProcessorAdapter;
-import de.metas.async.spi.WorkpackagesOnCommitSchedulerTemplate;
 import de.metas.letter.model.Letter;
 import de.metas.letter.model.LetterRepository;
 import de.metas.marketing.base.model.ContactPerson;
@@ -47,58 +40,6 @@ import de.metas.marketing.base.model.I_MKTG_Campaign_ContactPerson;
 
 public class C_Letter_CreateFromMKTG_ContactPerson_Async extends WorkpackageProcessorAdapter
 {
-	/**
-	 *
-	 * Schedule the letter creation based on the given MKTG_Campaign_ContactPerson ids and the MKTG_Campaign
-	 *
-	 */
-	public static void createWorkpackage(final List<Integer> campaignContactPersonIds)
-	{
-		if (campaignContactPersonIds == null || campaignContactPersonIds.isEmpty())
-		{
-			// no lines to process
-			return;
-		}
-
-		for (final Integer campaignContactPersonId : campaignContactPersonIds)
-		{
-			if (campaignContactPersonId == null || campaignContactPersonId <= 0)
-			{
-				// should not happen
-				continue;
-			}
-			final I_MKTG_Campaign_ContactPerson campaignContactPerson = load(campaignContactPersonId, I_MKTG_Campaign_ContactPerson.class);
-
-			SCHEDULER.schedule(campaignContactPerson);
-		}
-	}
-
-	private static final WorkpackagesOnCommitSchedulerTemplate<I_MKTG_Campaign_ContactPerson> SCHEDULER = new WorkpackagesOnCommitSchedulerTemplate<I_MKTG_Campaign_ContactPerson>(C_Letter_CreateFromMKTG_ContactPerson_Async.class)
-	{
-		@Override
-		protected boolean isEligibleForScheduling(final I_MKTG_Campaign_ContactPerson model)
-		{
-			return model != null && model.getMKTG_Campaign_ContactPerson_ID() > 0;
-		};
-
-		@Override
-		protected Properties extractCtxFromItem(final I_MKTG_Campaign_ContactPerson item)
-		{
-			return Env.getCtx();
-		}
-
-		@Override
-		protected String extractTrxNameFromItem(final I_MKTG_Campaign_ContactPerson item)
-		{
-			return ITrx.TRXNAME_ThreadInherited;
-		}
-
-		@Override
-		protected Object extractModelToEnqueueFromItem(final Collector collector, final I_MKTG_Campaign_ContactPerson item)
-		{
-			return new TableRecordReference(I_MKTG_Campaign_ContactPerson.Table_Name, item.getMKTG_Campaign_ContactPerson_ID());
-		}
-	};
 
 
 	@Override
