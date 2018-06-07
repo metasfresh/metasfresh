172f0fc62254e7f85aecad2365ee913b70c9a237
siemiatj <kuba@saskla.cz>
2018-06-07 12:52:19 +0200

    - rebuild loginByForm command #1686

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,12 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import { List } from 'immutable';
+import { goBack, push } from 'react-router-redux';
+
+import { loginSuccess } from '../../src/actions/AppActions';
+import { localLoginRequest } from '../../src/api';
+import Auth from '../../src/services/Auth';
 
 context('Reusable "login" custom command', function() {
   Cypress.Commands.add('loginByForm', (username, password) => {
@@ -31,16 +37,71 @@ context('Reusable "login" custom command', function() {
       message: username + ' | ' + password,
     });
 
+    const handleSuccess = function(){
+      console.log('CYPRESS handleSuccess')
+
+      // getUserLang().then(response => {
+        //GET language shall always return a result
+        // Moment.locale(response.data['key']);
+
+        Cypress.reduxStore.dispatch(push('/'));
+      // });
+    };
+
+    const checkIfAlreadyLogged = function() {
+      console.log('chickifUserLogggedIn');
+      const error = new Error('bla')
+      // // return Promise.reject(error)
+      // return;
+      return localLoginRequest().then(response => {
+        if (!response.body.error) {
+          return Cypress.reduxStore.dispatch(push('/'));
+        }
+
+        return Promise.reject(error);
+      });
+    };
+
+    const auth = new Auth();
+
+    cy.on("emit:reduxStore", store => {
+      Cypress.reduxStore = store;
+    });
+
+    cy.visit('/login');
+
     return cy.request({
       method: 'POST',
       url: 'http://w101.metasfresh.com:8081/rest/api/login/authenticate',
       // form: true,
+      failOnStatusCode: false,
       body: {
-        username: username,
-        password: password,
+        username,
+        password,
       },
-    });
+    }).then(response => {
+      console.log('response: ', response)
+      if (!response.isOkStatusCode) {
+        return checkIfAlreadyLogged();
+      }
+
+      if (response.body.loginComplete) {
+        return handleSuccess();
+      }
+      const roles = List(response.body.roles);
+
+      return cy.request({
+        method: 'POST',
+        url: 'http://w101.metasfresh.com:8081/rest/api/login/loginComplete',
+        body: { ...roles[0] },
+      }).then(() => {
+        Cypress.reduxStore.dispatch(loginSuccess(auth));
+        
+        handleSuccess();
+      })
+    })
   });
+
 
   // beforeEach(function(){
   //   // login before each test
