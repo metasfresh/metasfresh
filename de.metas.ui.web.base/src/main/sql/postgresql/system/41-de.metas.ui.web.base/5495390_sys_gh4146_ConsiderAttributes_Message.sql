b5d3cde4c6e08a9d2a988de6fd9c3de487e39717
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-06-07 15:38:46 +0300

    #4146 Consider Attributes Message
    https://github.com/metasfresh/metasfresh/issues/4146

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5495390_sys_gh4146_ConsiderAttributes_Message.sql
@@ -0,0 +1,45 @@
+-- 2018-06-07T15:24:21.564
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET EntityType='de.metas.picking', Value='de.metas.ui.web.picking.HUsToPickViewFilters.ConsiderAttributes',Updated=TO_TIMESTAMP('2018-06-07 15:24:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544744
+;
+
+-- 2018-06-07T15:24:29.838
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Attribute berücksichtigen',Updated=TO_TIMESTAMP('2018-06-07 15:24:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544744
+;
+
+-- 2018-06-07T15:28:45.592
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:28:45','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Message_ID=544744 AND AD_Language='en_US'
+;
+
+-- 2018-06-07T15:28:55.583
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:28:55','YYYY-MM-DD HH24:MI:SS'),MsgText='Attribute berücksichtigen' WHERE AD_Message_ID=544744 AND AD_Language='de_CH'
+;
+
+-- 2018-06-07T15:28:57.615
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:28:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Message_ID=544744 AND AD_Language='de_CH'
+;
+
+-- 2018-06-07T15:29:00.699
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:29:00','YYYY-MM-DD HH24:MI:SS'),IsTranslated='N' WHERE AD_Message_ID=544744 AND AD_Language='de_CH'
+;
+
+-- 2018-06-07T15:29:08.440
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:29:08','YYYY-MM-DD HH24:MI:SS'),MsgText='Attribute berücksichtigen' WHERE AD_Message_ID=544744 AND AD_Language='nl_NL'
+;
+
+-- 2018-06-07T15:29:19.503
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:29:19','YYYY-MM-DD HH24:MI:SS'),MsgText='Consider Attributes' WHERE AD_Message_ID=544744 AND AD_Language='en_US'
+;
+
+-- 2018-06-07T15:35:49.813
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET EntityType='de.metas.ui.web',Updated=TO_TIMESTAMP('2018-06-07 15:35:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544744
+;
+
