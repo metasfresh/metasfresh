24bc2fb27bb80cf7ae4030499b3b78f9f56d6ca5
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-06-07 16:14:37 +0300

    #4146 Repair script
    https://github.com/metasfresh/metasfresh/issues/4146

--- de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5495390_sys_gh4146_ConsiderAttributes_Message.sql
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5495390_sys_gh4146_ConsiderAttributes_Message.sql
@@ -1,45 +1,17 @@
--- 2018-06-07T15:24:21.564
+
+-- 2018-06-07T16:13:09.587
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message SET EntityType='de.metas.picking', Value='de.metas.ui.web.picking.HUsToPickViewFilters.ConsiderAttributes',Updated=TO_TIMESTAMP('2018-06-07 15:24:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544744
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,544746,0,TO_TIMESTAMP('2018-06-07 16:13:09','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web','Y','Attribute berücksichtigen','I',TO_TIMESTAMP('2018-06-07 16:13:09','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web.picking.HUsToPickViewFilters.ConsiderAttributes')
 ;
 
--- 2018-06-07T15:24:29.838
+-- 2018-06-07T16:13:09.599
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message SET MsgText='Attribute berücksichtigen',Updated=TO_TIMESTAMP('2018-06-07 15:24:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544744
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=544746 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
 ;
 
--- 2018-06-07T15:28:45.592
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:28:45','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Message_ID=544744 AND AD_Language='en_US'
-;
 
--- 2018-06-07T15:28:55.583
+-- 2018-06-07T16:13:49.237
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:28:55','YYYY-MM-DD HH24:MI:SS'),MsgText='Attribute berücksichtigen' WHERE AD_Message_ID=544744 AND AD_Language='de_CH'
-;
-
--- 2018-06-07T15:28:57.615
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:28:57','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Message_ID=544744 AND AD_Language='de_CH'
-;
-
--- 2018-06-07T15:29:00.699
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:29:00','YYYY-MM-DD HH24:MI:SS'),IsTranslated='N' WHERE AD_Message_ID=544744 AND AD_Language='de_CH'
-;
-
--- 2018-06-07T15:29:08.440
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:29:08','YYYY-MM-DD HH24:MI:SS'),MsgText='Attribute berücksichtigen' WHERE AD_Message_ID=544744 AND AD_Language='nl_NL'
-;
-
--- 2018-06-07T15:29:19.503
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 15:29:19','YYYY-MM-DD HH24:MI:SS'),MsgText='Consider Attributes' WHERE AD_Message_ID=544744 AND AD_Language='en_US'
-;
-
--- 2018-06-07T15:35:49.813
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message SET EntityType='de.metas.ui.web',Updated=TO_TIMESTAMP('2018-06-07 15:35:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544744
+UPDATE AD_Message_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-07 16:13:49','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',MsgText='Consider Attributes' WHERE AD_Message_ID=544746 AND AD_Language='en_US'
 ;
 
