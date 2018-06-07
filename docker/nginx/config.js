c02de4ac371db564f5607251f643e7c2bc2cf244
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-06-07 17:04:25 +0200

    allow to specify a non-https API URL for local development

--- docker/nginx/config.js
+++ docker/nginx/config.js
@@ -1,6 +1,6 @@
 /*eslint-disable */
 const config = {
-        API_URL: 'https://WEBUI_API_HOST:WEBUI_API_PORT/rest/api',
-        WS_URL: 'https://WEBUI_API_HOST:WEBUI_API_PORT/stomp'
+        API_URL: 'WEBUI_API_PROTOCOL://WEBUI_API_HOST:WEBUI_API_PORT/rest/api',
+        WS_URL: 'WEBUI_API_PROTOCOL://WEBUI_API_HOST:WEBUI_API_PORT/stomp'
 }
 /*eslint-enable */
