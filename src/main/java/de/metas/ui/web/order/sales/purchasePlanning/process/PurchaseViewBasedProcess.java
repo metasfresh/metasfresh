f251c26d34d5b4346d276520e534875687bddc21
Teo Sarca <teo.sarca@metasfresh.com>
2018-06-08 17:59:04 +0300

    improve PurchaseViewBasedProcess
    https://github.com/metasfresh/metasfresh/issues/4033

--- src/main/java/de/metas/ui/web/order/sales/purchasePlanning/process/PurchaseViewBasedProcess.java
+++ src/main/java/de/metas/ui/web/order/sales/purchasePlanning/process/PurchaseViewBasedProcess.java
@@ -1,6 +1,8 @@
 package de.metas.ui.web.order.sales.purchasePlanning.process;
 
 import de.metas.process.IProcessPrecondition;
+import de.metas.ui.web.order.sales.purchasePlanning.view.PurchaseRowChangeRequest;
+import de.metas.ui.web.order.sales.purchasePlanning.view.PurchaseRowId;
 import de.metas.ui.web.order.sales.purchasePlanning.view.PurchaseView;
 import de.metas.ui.web.process.adprocess.ViewBasedProcessTemplate;
 
@@ -14,12 +16,12 @@ import de.metas.ui.web.process.adprocess.ViewBasedProcessTemplate;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -34,6 +36,11 @@ public abstract class PurchaseViewBasedProcess
 	protected final PurchaseView getView()
 	{
 		return PurchaseView.cast(super.getView());
+	}
+
+	protected final void patchViewRow(final PurchaseRowId rowId, final PurchaseRowChangeRequest rowChangeRequest)
+	{
+		getView().patchViewRow(rowId, rowChangeRequest);
 	}
 
 }
