ce48cdc042744fa5c130023c910412c2b739bdea
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-11 11:50:54 +0300

    #4153 minor
    #4153 Serial Letter Feature

--- de.metas.marketing/base/src/main/java/de/metas/marketing/base/model/Platform.java
+++ de.metas.marketing/base/src/main/java/de/metas/marketing/base/model/Platform.java
@@ -36,8 +36,8 @@ public class Platform
 	String name;
 	String platformGatewayId;
 	PlatformId platformId;
-	boolean isRequiredLocation;
-	boolean isRequiredMailAddress;
+	boolean requiredLocation;
+	boolean requiredMailAddress;
 
 	/** @return existing repository ID or throws exception */
 	public int getRepoId()
--- de.metas.marketing/base/src/main/java/de/metas/marketing/base/model/PlatformRepository.java
+++ de.metas.marketing/base/src/main/java/de/metas/marketing/base/model/PlatformRepository.java
@@ -35,8 +35,8 @@ public class PlatformRepository
 		return Platform.builder()
 				.name(platformRecord.getName())
 				.platformGatewayId(platformRecord.getMarketingPlatformGatewayId())
-				.isRequiredLocation(platformRecord.isRequiredLocation())
-				.isRequiredMailAddress(platformRecord.isRequiredMailAddres())
+				.requiredLocation(platformRecord.isRequiredLocation())
+				.requiredMailAddress(platformRecord.isRequiredMailAddres())
 				.platformId(PlatformId.ofRepoId(platformRecord.getMKTG_Platform_ID()))
 				.build();
 	}
