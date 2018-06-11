b38fa1cf758651bd48ca87fd63b96ccbe1a6fdc5
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-11 13:50:29 +0300

    #4153 Show locations
    #4153 Serial Letter Feature

---
+++ de.metas.marketing/base/src/main/sql/postgresql/system/90-de.metas.marketing.base/5495650_sys_gh4153_ShowC_Location.sql
@@ -0,0 +1,5 @@
+-- 2018-06-11T13:45:35.618
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,564475,0,541102,552191,541591,'F',TO_TIMESTAMP('2018-06-11 13:45:34','YYYY-MM-DD HH24:MI:SS'),100,'User within the system - Internal or Business Partner Contact','The User identifies a unique user in the system. This could be an internal user or a business partner contact','Y','N','N','Y','N','Y','Anschrift',85,0,80,TO_TIMESTAMP('2018-06-11 13:45:34','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
