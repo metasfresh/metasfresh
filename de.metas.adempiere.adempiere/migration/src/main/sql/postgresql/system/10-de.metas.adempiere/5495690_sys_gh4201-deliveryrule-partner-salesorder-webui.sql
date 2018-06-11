648e325bb40ab14dc4a4ab41acbc40bc71c773da
Mark Krake <mark.krake@metasfresh.com>
2018-06-11 17:07:07 +0200

    Adds DeliveryRule to Sales Order Header #4201
    https://github.com/metasfresh/metasfresh/issues/4201

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5495690_sys_gh4201-deliveryrule-partner-salesorder-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5495690_sys_gh4201-deliveryrule-partner-salesorder-webui.sql
@@ -43,3 +43,18 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 UPDATE AD_UI_Element SET SeqNo=55,Updated=TO_TIMESTAMP('2018-06-11 17:00:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552193
 ;
 
+-- 2018-06-11T17:04:48.650
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,2877,0,186,540499,552194,'F',TO_TIMESTAMP('2018-06-11 17:04:48','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','Lieferart',400,0,0,TO_TIMESTAMP('2018-06-11 17:04:48','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2018-06-11T17:05:04.496
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET SeqNo=55,Updated=TO_TIMESTAMP('2018-06-11 17:05:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552194
+;
+
+-- 2018-06-11T17:05:20.246
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='Y',Updated=TO_TIMESTAMP('2018-06-11 17:05:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552194
+;
+
