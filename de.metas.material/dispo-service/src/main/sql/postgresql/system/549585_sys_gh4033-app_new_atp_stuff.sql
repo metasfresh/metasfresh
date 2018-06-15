87595610595c6dd35f0ffd72217c8ef226798dd9
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-06-15 06:26:40 +0200

    solve sql issue
    https://github.com/metasfresh/metasfresh/issues/4033 Pharma: MSV3 Purchase Functionality in purchase candidates

--- de.metas.material/dispo-service/src/main/sql/postgresql/system/549585_sys_gh4033-app_new_atp_stuff.sql
+++ de.metas.material/dispo-service/src/main/sql/postgresql/system/549585_sys_gh4033-app_new_atp_stuff.sql
@@ -1,65 +1,4 @@
 
-CREATE SCHEMA IF NOT EXISTS de_metas_material;
-
-DROP FUNCTION IF EXISTS de_metas_material.retrieve_atp_at_date(timestamp with time zone);
-CREATE FUNCTION de_metas_material.retrieve_atp_at_date(IN p_date timestamp with time zone)
-RETURNS TABLE(
-	M_Product_ID numeric, 
-	M_Warehouse_ID numeric, 
-	C_BPartner_Customer_ID numeric, 
-	StorageAttributesKey character varying, 
-	DateProjected timestamp with time zone,
-	SeqNo numeric,
-	Qty numeric) AS
-$BODY$
-	SELECT DISTINCT ON (M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey)
-		M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, DateProjected, SeqNo, Qty
-	FROM MD_Candidate 
-	WHERE IsActive='Y' AND MD_Candidate_Type='STOCK' and DateProjected <= p_date
-	ORDER BY M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, Dateprojected DESC, SeqNo DESC
-$BODY$
-  LANGUAGE sql STABLE;
-COMMENT ON FUNCTION de_metas_material.retrieve_atp_at_date(timestamp with time zone)
-  IS 'Note that the Qtys can be from MD_Candidates whose DateProjected is before the given p_date, if they were not yet superseeded by more recent values.
-  Please keep this function in sync with the AD_Table named MD_Candidate_ATP_QueryResult and also with the function retrieve_atp_at_date_debug';
-  
-DROP INDEX IF EXISTS md_candidate_stock_v_perf;
-CREATE INDEX md_candidate_stock_v_perf
-  ON public.md_candidate
-  USING btree
-  (M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, Dateprojected DESC, SeqNo DESC, Qty)
-  WHERE isactive = 'Y' AND md_candidate_type = 'STOCK';
-COMMENT ON INDEX public.md_candidate_stock_v_perf
-  IS 'This index has the purpose of supporting the function de_metas_material.retrieve_atp_at_date 
-in finding the latest DateProjected for a given product-id, warehouse-id, partner-id and StorageAttributesKey-(like-)expression.
-
-Note: the Qty column is in so that hopefully all this can be done by the DBMS using index-only-scans.';  
-
-DROP FUNCTION IF EXISTS de_metas_material.retrieve_atp_at_date_debug(timestamp with time zone);
-CREATE FUNCTION de_metas_material.retrieve_atp_at_date_debug(IN p_date timestamp with time zone)
-RETURNS TABLE(
-	M_Product_ID numeric, 
-	M_Warehouse_ID numeric, 
-	C_BPartner_Customer_ID numeric, 
-	StorageAttributesKey character varying, 
-	SeqNo numeric,
-	Qty numeric, 
-	MD_Candidate_ID numeric,
-	Dateprojected timestamp with time zone) AS
-$BODY$
-	SELECT DISTINCT ON (M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey)
-		M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, SeqNo, Qty, MD_Candidate_ID, Dateprojected
-	FROM MD_Candidate 
-	WHERE IsActive='Y' AND MD_Candidate_Type='STOCK' and DateProjected <= p_date
-	ORDER BY M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, Dateprojected DESC, SeqNo DESC
-$BODY$
-  LANGUAGE sql STABLE;
-COMMENT ON FUNCTION de_metas_material.retrieve_atp_at_date_debug(timestamp with time zone)
-  IS 'Like de_metas_material.retrieve_atp_at_date, but returns additional columns that are not part of the index.';
-
-DROP VIEW IF EXISTS md_candidate_stock_v;
-
-
 -- 2018-06-13T19:57:25.177
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO t_alter_column values('c_purchasecandidate','M_AttributeInstance_ID','NUMERIC(10)',null,null)
@@ -198,3 +137,63 @@ UPDATE AD_Table SET Help='Please keep in sync with the DB function de_metas_mate
 UPDATE AD_Table SET Help='Please keep in sync with the DB function de_metas_material.retrieve_atp_at_date(timestamp with time zone)!',Updated=TO_TIMESTAMP('2018-06-14 16:39:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=540859
 ;
 
+
+CREATE SCHEMA IF NOT EXISTS de_metas_material;
+
+DROP FUNCTION IF EXISTS de_metas_material.retrieve_atp_at_date(timestamp with time zone);
+CREATE FUNCTION de_metas_material.retrieve_atp_at_date(IN p_date timestamp with time zone)
+RETURNS TABLE(
+	M_Product_ID numeric, 
+	M_Warehouse_ID numeric, 
+	C_BPartner_Customer_ID numeric, 
+	StorageAttributesKey character varying, 
+	DateProjected timestamp with time zone,
+	SeqNo numeric,
+	Qty numeric) AS
+$BODY$
+	SELECT DISTINCT ON (M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey)
+		M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, DateProjected, SeqNo, Qty
+	FROM MD_Candidate 
+	WHERE IsActive='Y' AND MD_Candidate_Type='STOCK' and DateProjected <= p_date
+	ORDER BY M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, Dateprojected DESC, SeqNo DESC
+$BODY$
+  LANGUAGE sql STABLE;
+COMMENT ON FUNCTION de_metas_material.retrieve_atp_at_date(timestamp with time zone)
+  IS 'Note that the Qtys can be from MD_Candidates whose DateProjected is before the given p_date, if they were not yet superseeded by more recent values.
+  Please keep this function in sync with the AD_Table named MD_Candidate_ATP_QueryResult and also with the function retrieve_atp_at_date_debug';
+  
+DROP INDEX IF EXISTS md_candidate_stock_v_perf;
+CREATE INDEX md_candidate_stock_v_perf
+  ON public.md_candidate
+  USING btree
+  (M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, Dateprojected DESC, SeqNo DESC, Qty)
+  WHERE isactive = 'Y' AND md_candidate_type = 'STOCK';
+COMMENT ON INDEX public.md_candidate_stock_v_perf
+  IS 'This index has the purpose of supporting the function de_metas_material.retrieve_atp_at_date 
+in finding the latest DateProjected for a given product-id, warehouse-id, partner-id and StorageAttributesKey-(like-)expression.
+
+Note: the Qty column is in so that hopefully all this can be done by the DBMS using index-only-scans.';  
+
+DROP FUNCTION IF EXISTS de_metas_material.retrieve_atp_at_date_debug(timestamp with time zone);
+CREATE FUNCTION de_metas_material.retrieve_atp_at_date_debug(IN p_date timestamp with time zone)
+RETURNS TABLE(
+	M_Product_ID numeric, 
+	M_Warehouse_ID numeric, 
+	C_BPartner_Customer_ID numeric, 
+	StorageAttributesKey character varying, 
+	SeqNo numeric,
+	Qty numeric, 
+	MD_Candidate_ID numeric,
+	Dateprojected timestamp with time zone) AS
+$BODY$
+	SELECT DISTINCT ON (M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey)
+		M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, SeqNo, Qty, MD_Candidate_ID, Dateprojected
+	FROM MD_Candidate 
+	WHERE IsActive='Y' AND MD_Candidate_Type='STOCK' and DateProjected <= p_date
+	ORDER BY M_Product_ID, M_Warehouse_ID, C_BPartner_Customer_ID, StorageAttributesKey, Dateprojected DESC, SeqNo DESC
+$BODY$
+  LANGUAGE sql STABLE;
+COMMENT ON FUNCTION de_metas_material.retrieve_atp_at_date_debug(timestamp with time zone)
+  IS 'Like de_metas_material.retrieve_atp_at_date, but returns additional columns that are not part of the index.';
+
+DROP VIEW IF EXISTS md_candidate_stock_v;
