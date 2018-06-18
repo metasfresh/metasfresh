919d2dd843d189273e9e22e9fb8a7933cae21dd2
Mark Krake <mark.krake@metasfresh.com>
2018-06-18 16:54:58 +0200

    Adds Translations for de_DE, en_US #4244
    https://github.com/metasfresh/metasfresh/issues/4244

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5496040_sys_gh4244-purchase-cand-adjustments-webui.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5496040_sys_gh4244-purchase-cand-adjustments-webui.sql
@@ -13,3 +13,48 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,564736,0,540894,541249,552268,'F',TO_TIMESTAMP('2018-06-18 16:38:06','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','Disponiert',50,0,0,TO_TIMESTAMP('2018-06-18 16:38:06','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+-- 2018-06-18T16:49:41.236
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Disponiert',Updated=TO_TIMESTAMP('2018-06-18 16:49:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=564736
+;
+
+-- 2018-06-18T16:49:46.696
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Bestellt',Updated=TO_TIMESTAMP('2018-06-18 16:49:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=560591
+;
+
+-- 2018-06-18T16:49:59.573
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-18 16:49:59','YYYY-MM-DD HH24:MI:SS'),Name='Purchased',Help='The Processed checkbox indicates that a document has been processed.	' WHERE AD_Field_ID=560591 AND AD_Language='en_US'
+;
+
+-- 2018-06-18T16:50:08.307
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2018-06-18 16:50:08','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y' WHERE AD_Field_ID=564736 AND AD_Language='en_US'
+;
+
+-- 2018-06-18T16:50:35.629
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2018-06-18 16:50:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=549343
+;
+
+-- 2018-06-18T16:50:35.635
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=90,Updated=TO_TIMESTAMP('2018-06-18 16:50:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=549345
+;
+
+-- 2018-06-18T16:50:35.641
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2018-06-18 16:50:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=549347
+;
+
+-- 2018-06-18T16:50:35.646
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=110,Updated=TO_TIMESTAMP('2018-06-18 16:50:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552268
+;
+
+-- 2018-06-18T16:50:35.652
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=120,Updated=TO_TIMESTAMP('2018-06-18 16:50:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=549349
+;
+
