0f04158c804cba941085dc857adbf5975796aba2
Mark Krake <mark.krake@metasfresh.com>
2018-06-19 07:03:55 +0200

    Add Translations for Attributes #4240
    Adds Translations for different Attributes in de_DE.
    https://github.com/metasfresh/metasfresh/issues/4240

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5496100_sys_gh4240-repack-nr-trl.sql
@@ -0,0 +1,30 @@
+-- 2018-06-19T06:55:59.415
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Name='Repack Nummer',Updated=TO_TIMESTAMP('2018-06-19 06:55:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540038
+;
+
+-- 2018-06-19T06:56:26.090
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Name='Repack Nummer erforderlich',Updated=TO_TIMESTAMP('2018-06-19 06:56:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540037
+;
+
+-- 2018-06-19T06:56:30.326
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Name='Repack Nr.',Updated=TO_TIMESTAMP('2018-06-19 06:56:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540038
+;
+
+-- 2018-06-19T06:56:41.127
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Name='Repack Nr. erforderlich',Updated=TO_TIMESTAMP('2018-06-19 06:56:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540037
+;
+
+-- 2018-06-19T06:59:23.883
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Description='', Name='Einkaufspreis',Updated=TO_TIMESTAMP('2018-06-19 06:59:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540019
+;
+
+-- 2018-06-19T06:59:41.134
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Name='Eingangsdatum',Updated=TO_TIMESTAMP('2018-06-19 06:59:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540036
+;
+
