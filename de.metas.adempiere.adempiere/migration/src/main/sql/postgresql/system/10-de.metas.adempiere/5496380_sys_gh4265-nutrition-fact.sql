8d2bb8a184046edd29779bd673514d61ecad49a5
Mark Krake <mark.krake@metasfresh.com>
2018-06-22 10:21:29 +0200

    SQL Fix, missing ad_element #4265
    https://github.com/metasfresh/metasfresh/issues/4265

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5496380_sys_gh4265-nutrition-fact.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5496380_sys_gh4265-nutrition-fact.sql
@@ -3,6 +3,16 @@
 /* DDL */ CREATE TABLE public.M_Nutrition_Fact (AD_Client_ID NUMERIC(10) NOT NULL, AD_Org_ID NUMERIC(10) NOT NULL, Created TIMESTAMP WITH TIME ZONE NOT NULL, CreatedBy NUMERIC(10) NOT NULL, Description VARCHAR(255), IsActive CHAR(1) CHECK (IsActive IN ('Y','N')) NOT NULL, M_Nutrition_Fact_ID NUMERIC(10) NOT NULL, Name VARCHAR(60) NOT NULL, Updated TIMESTAMP WITH TIME ZONE NOT NULL, UpdatedBy NUMERIC(10) NOT NULL, CONSTRAINT M_Nutrition_Fact_Key PRIMARY KEY (M_Nutrition_Fact_ID))
 ;
 
+-- 2018-06-21T14:30:44.187
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,544131,0,'M_Nutrition_Fact_ID',TO_TIMESTAMP('2018-06-21 14:30:44','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Nutrition Fact','Nutrition Fact',TO_TIMESTAMP('2018-06-21 14:30:44','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2018-06-21T14:30:44.188
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=544131 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
 -- 2018-06-21T16:07:06.953
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Table (AccessLevel,ACTriggerLength,AD_Client_ID,AD_Org_ID,AD_Table_ID,CopyColumnsFromTable,Created,CreatedBy,EntityType,ImportTable,IsActive,IsAutocomplete,IsChangeLog,IsDeleteable,IsDLM,IsHighVolume,IsSecurityEnabled,IsView,LoadSeq,Name,PersonalDataCategory,ReplicationType,TableName,Updated,UpdatedBy) VALUES ('3',0,0,0,540996,'N',TO_TIMESTAMP('2018-06-21 16:07:06','YYYY-MM-DD HH24:MI:SS'),100,'D','N','Y','N','N','Y','N','N','N','N',0,'Nutrition Fact','NP','L','M_Nutrition_Fact',TO_TIMESTAMP('2018-06-21 16:07:06','YYYY-MM-DD HH24:MI:SS'),100)
