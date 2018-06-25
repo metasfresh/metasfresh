eac3e290aaacf0f09c410865e080235336d7400e
Teo Sarca <teo.sarca@metasfresh.com>
2018-06-25 18:16:14 +0300

    autoindex ES models
    https://github.com/metasfresh/metasfresh/issues/4275
    (cherry picked from commit df043010143ee5affebcf965312a45464a0a21f2)

---
+++ de.metas.elasticsearch/src/main/sql/postgresql/system/40-de.metas.elasticsearch/5496580_sys_gh4275_sysconfig_AutoIndexModels_orders.sql
@@ -0,0 +1,5 @@
+-- 2018-06-25T18:12:40.292
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (Value,EntityType,AD_Client_ID,CreatedBy,UpdatedBy,IsActive,ConfigurationLevel,AD_SysConfig_ID,Description,AD_Org_ID,Name,Created,Updated) VALUES ('N','de.metas.elasticsearch',0,100,100,'Y','S',541219,'Set it to Y if you want to automatically index ALL models when server is starting and the index was missing.',0,'de.metas.elasticsearch.indexer.AutoIndexModels.orders',TO_TIMESTAMP('2018-06-25 18:12:40','YYYY-MM-DD HH24:MI:SS'),TO_TIMESTAMP('2018-06-25 18:12:40','YYYY-MM-DD HH24:MI:SS'))
+;
+
