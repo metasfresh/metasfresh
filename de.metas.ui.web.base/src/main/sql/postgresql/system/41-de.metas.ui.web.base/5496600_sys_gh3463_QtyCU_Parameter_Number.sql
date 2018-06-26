5e7878cdd9e2d26dede6c079a4b5e0cb891b68cc
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-06-26 13:43:47 +0300

    #3463 #3210 Make the QtyCU parameter a number instead of an integer
    https://github.com/metasfresh/metasfresh/issues/3210
    https://github.com/metasfresh/metasfresh/issues/3463

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5496600_sys_gh3463_QtyCU_Parameter_Number.sql
@@ -0,0 +1,5 @@
+-- 2018-06-26T13:26:40.357
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Reference_ID=22,Updated=TO_TIMESTAMP('2018-06-26 13:26:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541302
+;
+
