3e2c85389e999aa8a9d1f6fb014b53b2470ca097
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-28 18:10:50 +0300

    #4219 Add import process
    #4219 Import Datev Primanota for Payments, Allocations

---
+++ de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/impexp/DatevPaymentImportProcess.java
@@ -0,0 +1,115 @@
+package de.metas.banking.payment.impexp;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.adempiere.ad.trx.api.ITrx;
+import org.adempiere.impexp.AbstractImportProcess;
+import org.adempiere.impexp.IImportInterceptor;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.util.lang.IMutable;
+import org.compiere.model.I_C_Payment;
+import org.compiere.model.ModelValidationEngine;
+
+import de.metas.banking.model.I_I_Datev_Payment;
+import de.metas.banking.model.X_I_Datev_Payment;
+import de.metas.pricing.impexp.MDiscountSchemaImportTableSqlUpdater;
+import lombok.NonNull;
+
+/**
+ * Import {@link I_I_Datev_Payment} to {@link I_C_Payment}.
+ *
+ */
+public class DatevPaymentImportProcess extends AbstractImportProcess<I_I_Datev_Payment>
+{
+	@Override
+	public Class<I_I_Datev_Payment> getImportModelClass()
+	{
+		return I_I_Datev_Payment.class;
+	}
+
+	@Override
+	public String getImportTableName()
+	{
+		return I_I_Datev_Payment.Table_Name;
+	}
+
+	@Override
+	protected String getTargetTableName()
+	{
+		return I_C_Payment.Table_Name;
+	}
+
+	@Override
+	protected void updateAndValidateImportRecords()
+	{
+		MDiscountSchemaImportTableSqlUpdater.updateDiscountSchemaImportTable(getWhereClause());
+	}
+
+	@Override
+	protected String getImportOrderBySql()
+	{
+		return I_I_Datev_Payment.COLUMNNAME_C_BPartner_ID;
+	}
+
+	@Override
+	protected I_I_Datev_Payment retrieveImportRecord(final Properties ctx, final ResultSet rs) throws SQLException
+	{
+		return new X_I_Datev_Payment(ctx, rs, ITrx.TRXNAME_ThreadInherited);
+	}
+
+	@Override
+	protected ImportRecordResult importRecord(@NonNull final IMutable<Object> state, @NonNull final I_I_Datev_Payment importRecord) throws Exception
+	{
+		return importDatevPayment(importRecord);
+	}
+
+	private ImportRecordResult importDatevPayment(@NonNull final I_I_Datev_Payment importRecord)
+	{
+		final ImportRecordResult schemaImportResult;
+
+		final I_C_Payment payment;
+		if (importRecord.getC_Payment_ID() <= 0)
+		{
+			payment = createNewPayment(importRecord);
+			schemaImportResult = ImportRecordResult.Inserted;
+		}
+		else
+		{
+			payment = importRecord.getC_Payment();
+			schemaImportResult = ImportRecordResult.Updated;
+		}
+
+		ModelValidationEngine.get().fireImportValidate(this, importRecord, payment, IImportInterceptor.TIMING_AFTER_IMPORT);
+		InterfaceWrapperHelper.save(payment);
+
+		importRecord.setC_Payment_ID(payment.getC_Payment_ID());
+		InterfaceWrapperHelper.save(importRecord);
+
+		return schemaImportResult;
+	}
+
+	private I_C_Payment createNewPayment(I_I_Datev_Payment importRecord)
+	{
+		final I_C_Payment payment;
+		payment = InterfaceWrapperHelper.create(getCtx(), I_C_Payment.class, ITrx.TRXNAME_ThreadInherited);
+		payment.setAD_Org_ID(importRecord.getAD_Org_ID());
+		payment.setDescription("Import for debitorId/creditorId" + importRecord.getBPartnerValue());
+		payment.setPayAmt(importRecord.getPayAmt());
+		payment.setDiscountAmt(importRecord.getDiscountAmt());
+		payment.setC_BPartner_ID(importRecord.getC_BPartner_ID());
+		payment.setIsReceipt(importRecord.isReceipt());
+		InterfaceWrapperHelper.save(payment);
+		return payment;
+	}
+
+
+	@Override
+	protected void markImported(@NonNull final I_I_Datev_Payment importRecord)
+	{
+		importRecord.setI_IsImported(X_I_Datev_Payment.I_ISIMPORTED_Imported);
+		importRecord.setProcessed(true);
+		InterfaceWrapperHelper.save(importRecord);
+	}
+}
