4fd81750fe0fb735fff09967d02c031044894dde
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-28 14:15:54 +0300

    #4219 Create I_Datev_Payment table and window
    #4219 Import Datev Primanota for Payments, Allocations

---
+++ de.metas.banking/de.metas.banking.base/src/main/java-gen/de/metas/banking/model/X_I_Datev_Payment.java
@@ -0,0 +1,549 @@
+/** Generated Model - DO NOT CHANGE */
+package de.metas.banking.model;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+/** Generated Model for I_Datev_Payment
+ *  @author Adempiere (generated) 
+ */
+@SuppressWarnings("javadoc")
+public class X_I_Datev_Payment extends org.compiere.model.PO implements I_I_Datev_Payment, org.compiere.model.I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1590859627L;
+
+    /** Standard Constructor */
+    public X_I_Datev_Payment (Properties ctx, int I_Datev_Payment_ID, String trxName)
+    {
+      super (ctx, I_Datev_Payment_ID, trxName);
+      /** if (I_Datev_Payment_ID == 0)
+        {
+			setI_Datev_Payment_ID (0);
+			setI_IsImported (false);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_I_Datev_Payment (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+
+    /** Load Meta Data */
+    @Override
+    protected org.compiere.model.POInfo initPO (Properties ctx)
+    {
+      org.compiere.model.POInfo poi = org.compiere.model.POInfo.getPOInfo (ctx, Table_Name, get_TrxName());
+      return poi;
+    }
+
+	/** Set Konto-Nr..
+		@param AccountNo 
+		Kontonummer
+	  */
+	@Override
+	public void setAccountNo (java.lang.String AccountNo)
+	{
+		set_Value (COLUMNNAME_AccountNo, AccountNo);
+	}
+
+	/** Get Konto-Nr..
+		@return Kontonummer
+	  */
+	@Override
+	public java.lang.String getAccountNo () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_AccountNo);
+	}
+
+	/** Set Bank Account No.
+		@param BankAccountNo 
+		Bank Account Number
+	  */
+	@Override
+	public void setBankAccountNo (java.lang.String BankAccountNo)
+	{
+		set_Value (COLUMNNAME_BankAccountNo, BankAccountNo);
+	}
+
+	/** Get Bank Account No.
+		@return Bank Account Number
+	  */
+	@Override
+	public java.lang.String getBankAccountNo () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_BankAccountNo);
+	}
+
+	/** Set Geschäftspartner-Schlüssel.
+		@param BPartnerValue 
+		Key of the Business Partner
+	  */
+	@Override
+	public void setBPartnerValue (java.lang.String BPartnerValue)
+	{
+		set_Value (COLUMNNAME_BPartnerValue, BPartnerValue);
+	}
+
+	/** Get Geschäftspartner-Schlüssel.
+		@return Key of the Business Partner
+	  */
+	@Override
+	public java.lang.String getBPartnerValue () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_BPartnerValue);
+	}
+
+	@Override
+	public org.compiere.model.I_C_BP_BankAccount getC_BP_BankAccount() throws RuntimeException
+	{
+		return get_ValueAsPO(COLUMNNAME_C_BP_BankAccount_ID, org.compiere.model.I_C_BP_BankAccount.class);
+	}
+
+	@Override
+	public void setC_BP_BankAccount(org.compiere.model.I_C_BP_BankAccount C_BP_BankAccount)
+	{
+		set_ValueFromPO(COLUMNNAME_C_BP_BankAccount_ID, org.compiere.model.I_C_BP_BankAccount.class, C_BP_BankAccount);
+	}
+
+	/** Set Bankverbindung.
+		@param C_BP_BankAccount_ID 
+		Bankverbindung des Geschäftspartners
+	  */
+	@Override
+	public void setC_BP_BankAccount_ID (int C_BP_BankAccount_ID)
+	{
+		if (C_BP_BankAccount_ID < 1) 
+			set_Value (COLUMNNAME_C_BP_BankAccount_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_BP_BankAccount_ID, Integer.valueOf(C_BP_BankAccount_ID));
+	}
+
+	/** Get Bankverbindung.
+		@return Bankverbindung des Geschäftspartners
+	  */
+	@Override
+	public int getC_BP_BankAccount_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_BP_BankAccount_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	@Override
+	public org.compiere.model.I_C_BPartner getC_BPartner() throws RuntimeException
+	{
+		return get_ValueAsPO(COLUMNNAME_C_BPartner_ID, org.compiere.model.I_C_BPartner.class);
+	}
+
+	@Override
+	public void setC_BPartner(org.compiere.model.I_C_BPartner C_BPartner)
+	{
+		set_ValueFromPO(COLUMNNAME_C_BPartner_ID, org.compiere.model.I_C_BPartner.class, C_BPartner);
+	}
+
+	/** Set Geschäftspartner.
+		@param C_BPartner_ID 
+		Bezeichnet einen Geschäftspartner
+	  */
+	@Override
+	public void setC_BPartner_ID (int C_BPartner_ID)
+	{
+		if (C_BPartner_ID < 1) 
+			set_Value (COLUMNNAME_C_BPartner_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_BPartner_ID, Integer.valueOf(C_BPartner_ID));
+	}
+
+	/** Get Geschäftspartner.
+		@return Bezeichnet einen Geschäftspartner
+	  */
+	@Override
+	public int getC_BPartner_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_BPartner_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	@Override
+	public org.compiere.model.I_C_DocType getC_DocType() throws RuntimeException
+	{
+		return get_ValueAsPO(COLUMNNAME_C_DocType_ID, org.compiere.model.I_C_DocType.class);
+	}
+
+	@Override
+	public void setC_DocType(org.compiere.model.I_C_DocType C_DocType)
+	{
+		set_ValueFromPO(COLUMNNAME_C_DocType_ID, org.compiere.model.I_C_DocType.class, C_DocType);
+	}
+
+	/** Set Belegart.
+		@param C_DocType_ID 
+		Belegart oder Verarbeitungsvorgaben
+	  */
+	@Override
+	public void setC_DocType_ID (int C_DocType_ID)
+	{
+		if (C_DocType_ID < 0) 
+			set_Value (COLUMNNAME_C_DocType_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_DocType_ID, Integer.valueOf(C_DocType_ID));
+	}
+
+	/** Get Belegart.
+		@return Belegart oder Verarbeitungsvorgaben
+	  */
+	@Override
+	public int getC_DocType_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_DocType_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	@Override
+	public org.compiere.model.I_C_Invoice getC_Invoice() throws RuntimeException
+	{
+		return get_ValueAsPO(COLUMNNAME_C_Invoice_ID, org.compiere.model.I_C_Invoice.class);
+	}
+
+	@Override
+	public void setC_Invoice(org.compiere.model.I_C_Invoice C_Invoice)
+	{
+		set_ValueFromPO(COLUMNNAME_C_Invoice_ID, org.compiere.model.I_C_Invoice.class, C_Invoice);
+	}
+
+	/** Set Rechnung.
+		@param C_Invoice_ID 
+		Invoice Identifier
+	  */
+	@Override
+	public void setC_Invoice_ID (int C_Invoice_ID)
+	{
+		if (C_Invoice_ID < 1) 
+			set_Value (COLUMNNAME_C_Invoice_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_Invoice_ID, Integer.valueOf(C_Invoice_ID));
+	}
+
+	/** Get Rechnung.
+		@return Invoice Identifier
+	  */
+	@Override
+	public int getC_Invoice_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_Invoice_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	@Override
+	public org.compiere.model.I_C_Payment getC_Payment() throws RuntimeException
+	{
+		return get_ValueAsPO(COLUMNNAME_C_Payment_ID, org.compiere.model.I_C_Payment.class);
+	}
+
+	@Override
+	public void setC_Payment(org.compiere.model.I_C_Payment C_Payment)
+	{
+		set_ValueFromPO(COLUMNNAME_C_Payment_ID, org.compiere.model.I_C_Payment.class, C_Payment);
+	}
+
+	/** Set Zahlung.
+		@param C_Payment_ID 
+		Zahlung
+	  */
+	@Override
+	public void setC_Payment_ID (int C_Payment_ID)
+	{
+		if (C_Payment_ID < 1) 
+			set_Value (COLUMNNAME_C_Payment_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_Payment_ID, Integer.valueOf(C_Payment_ID));
+	}
+
+	/** Get Zahlung.
+		@return Zahlung
+	  */
+	@Override
+	public int getC_Payment_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_Payment_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Vorgangsdatum.
+		@param DateTrx 
+		Vorgangsdatum
+	  */
+	@Override
+	public void setDateTrx (java.sql.Timestamp DateTrx)
+	{
+		set_Value (COLUMNNAME_DateTrx, DateTrx);
+	}
+
+	/** Get Vorgangsdatum.
+		@return Vorgangsdatum
+	  */
+	@Override
+	public java.sql.Timestamp getDateTrx () 
+	{
+		return (java.sql.Timestamp)get_Value(COLUMNNAME_DateTrx);
+	}
+
+	/** Set Skonto.
+		@param DiscountAmt 
+		Calculated amount of discount
+	  */
+	@Override
+	public void setDiscountAmt (java.math.BigDecimal DiscountAmt)
+	{
+		set_Value (COLUMNNAME_DiscountAmt, DiscountAmt);
+	}
+
+	/** Get Skonto.
+		@return Calculated amount of discount
+	  */
+	@Override
+	public java.math.BigDecimal getDiscountAmt () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_DiscountAmt);
+		if (bd == null)
+			 return BigDecimal.ZERO;
+		return bd;
+	}
+
+	/** Set Belegart-Bezeichnung.
+		@param DocTypeName 
+		Name der Belegart
+	  */
+	@Override
+	public void setDocTypeName (java.lang.String DocTypeName)
+	{
+		set_Value (COLUMNNAME_DocTypeName, DocTypeName);
+	}
+
+	/** Get Belegart-Bezeichnung.
+		@return Name der Belegart
+	  */
+	@Override
+	public java.lang.String getDocTypeName () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_DocTypeName);
+	}
+
+	/** Set Nr..
+		@param DocumentNo 
+		Document sequence number of the document
+	  */
+	@Override
+	public void setDocumentNo (java.lang.String DocumentNo)
+	{
+		set_Value (COLUMNNAME_DocumentNo, DocumentNo);
+	}
+
+	/** Get Nr..
+		@return Document sequence number of the document
+	  */
+	@Override
+	public java.lang.String getDocumentNo () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_DocumentNo);
+	}
+
+	/** Set Datev Payment.
+		@param I_Datev_Payment_ID Datev Payment	  */
+	@Override
+	public void setI_Datev_Payment_ID (int I_Datev_Payment_ID)
+	{
+		if (I_Datev_Payment_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_I_Datev_Payment_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_I_Datev_Payment_ID, Integer.valueOf(I_Datev_Payment_ID));
+	}
+
+	/** Get Datev Payment.
+		@return Datev Payment	  */
+	@Override
+	public int getI_Datev_Payment_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_I_Datev_Payment_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Import-Fehlermeldung.
+		@param I_ErrorMsg 
+		Meldungen, die durch den Importprozess generiert wurden
+	  */
+	@Override
+	public void setI_ErrorMsg (java.lang.String I_ErrorMsg)
+	{
+		set_Value (COLUMNNAME_I_ErrorMsg, I_ErrorMsg);
+	}
+
+	/** Get Import-Fehlermeldung.
+		@return Meldungen, die durch den Importprozess generiert wurden
+	  */
+	@Override
+	public java.lang.String getI_ErrorMsg () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_I_ErrorMsg);
+	}
+
+	/** Set Importiert.
+		@param I_IsImported 
+		Ist dieser Import verarbeitet worden?
+	  */
+	@Override
+	public void setI_IsImported (boolean I_IsImported)
+	{
+		set_Value (COLUMNNAME_I_IsImported, Boolean.valueOf(I_IsImported));
+	}
+
+	/** Get Importiert.
+		@return Ist dieser Import verarbeitet worden?
+	  */
+	@Override
+	public boolean isI_IsImported () 
+	{
+		Object oo = get_Value(COLUMNNAME_I_IsImported);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Invoice Document No.
+		@param InvoiceDocumentNo 
+		Document Number of the Invoice
+	  */
+	@Override
+	public void setInvoiceDocumentNo (java.lang.String InvoiceDocumentNo)
+	{
+		set_Value (COLUMNNAME_InvoiceDocumentNo, InvoiceDocumentNo);
+	}
+
+	/** Get Invoice Document No.
+		@return Document Number of the Invoice
+	  */
+	@Override
+	public java.lang.String getInvoiceDocumentNo () 
+	{
+		return (java.lang.String)get_Value(COLUMNNAME_InvoiceDocumentNo);
+	}
+
+	/** Set Zahlungseingang.
+		@param IsReceipt 
+		Dies ist eine Verkaufs-Transaktion (Zahlungseingang)
+	  */
+	@Override
+	public void setIsReceipt (boolean IsReceipt)
+	{
+		set_Value (COLUMNNAME_IsReceipt, Boolean.valueOf(IsReceipt));
+	}
+
+	/** Get Zahlungseingang.
+		@return Dies ist eine Verkaufs-Transaktion (Zahlungseingang)
+	  */
+	@Override
+	public boolean isReceipt () 
+	{
+		Object oo = get_Value(COLUMNNAME_IsReceipt);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Zahlungsbetrag.
+		@param PayAmt 
+		Gezahlter Betrag
+	  */
+	@Override
+	public void setPayAmt (java.math.BigDecimal PayAmt)
+	{
+		set_Value (COLUMNNAME_PayAmt, PayAmt);
+	}
+
+	/** Get Zahlungsbetrag.
+		@return Gezahlter Betrag
+	  */
+	@Override
+	public java.math.BigDecimal getPayAmt () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_PayAmt);
+		if (bd == null)
+			 return BigDecimal.ZERO;
+		return bd;
+	}
+
+	/** Set Verarbeitet.
+		@param Processed 
+		Checkbox sagt aus, ob der Beleg verarbeitet wurde. 
+	  */
+	@Override
+	public void setProcessed (boolean Processed)
+	{
+		set_Value (COLUMNNAME_Processed, Boolean.valueOf(Processed));
+	}
+
+	/** Get Verarbeitet.
+		@return Checkbox sagt aus, ob der Beleg verarbeitet wurde. 
+	  */
+	@Override
+	public boolean isProcessed () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processed);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Process Now.
+		@param Processing Process Now	  */
+	@Override
+	public void setProcessing (boolean Processing)
+	{
+		set_Value (COLUMNNAME_Processing, Boolean.valueOf(Processing));
+	}
+
+	/** Get Process Now.
+		@return Process Now	  */
+	@Override
+	public boolean isProcessing () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processing);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+}
