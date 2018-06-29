3b6430c158827a6c47bf613971f4c1927358073e
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-06-29 18:29:38 +0300

    #4219 Fix constraint
    #4219 Import Datev Primanota for Payments, Allocations

--- de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5496910_sys_gh4219_FixColumnType.sql
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5496910_sys_gh4219_FixColumnType.sql
@@ -12,3 +12,6 @@ UPDATE AD_Column SET AD_Reference_ID=11,Updated=TO_TIMESTAMP('2018-06-29 18:14:1
 /* DDL */ SELECT public.db_alter_table('I_Datev_Payment','ALTER TABLE public.I_Datev_Payment ADD COLUMN BPartnerValue NUMERIC(10)')
 ;
 
+ALTER TABLE I_Datev_Payment DROP CONSTRAINT i_datev_payment_i_isimported_check;
+ALTER TABLE I_Datev_Payment ADD CONSTRAINT i_datev_payment_i_isimported_check CHECK (i_isimported = ANY (ARRAY['Y'::bpchar, 'N'::bpchar, 'E'::bpchar]));
+
