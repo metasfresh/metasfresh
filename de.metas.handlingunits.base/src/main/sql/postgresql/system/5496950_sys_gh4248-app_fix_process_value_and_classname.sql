4ebfa89ae856dfca848d9004c2626b88d9e62be3
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-07-02 11:38:57 +0200

    #4248-app make another AD_Process class name fix
    Details about hu attributes during sales order creation WIP https://github.com/metasfresh/metasfresh/issues/4248

--- de.metas.handlingunits.base/src/main/sql/postgresql/system/5496950_sys_gh4248-app_fix_process_value_and_classname.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5496950_sys_gh4248-app_fix_process_value_and_classname.sql
@@ -9,7 +9,3 @@ UPDATE AD_Process SET
 	ClassName=replace(ClassName, 'WEBUI_C_OrderLineSO_Make_HuReservation', 'WEBUI_C_OrderLineSO_Make_HUReservation')
 WHERE Value='WEBUI_C_OrderLineSO_Make_HuReservation';
 
-UPDATE AD_Process SET 
-	Value='WEBUI_C_OrderLineSO_Launch_HUEditor',
-	ClassName=replace(ClassName, 'WEBUI_C_OrderLineSO_Launch_HuEditor', 'WEBUI_C_OrderLineSO_Launch_HUEditor')
-WHERE Value='WEBUI_C_OrderLineSO_Launch_HuEditor';
