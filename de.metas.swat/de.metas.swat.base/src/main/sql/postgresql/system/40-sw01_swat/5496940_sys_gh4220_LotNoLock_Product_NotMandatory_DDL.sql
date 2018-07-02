55c2ce0123286fb01dea88d181a9b9ccf6dff33c
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-07-02 11:47:40 +0300

    #4220 make the Product not mandatory for lotnolock
    https://github.com/metasfresh/metasfresh/issues/4220

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5496940_sys_gh4220_LotNoLock_Product_NotMandatory_DDL.sql
@@ -0,0 +1,10 @@
+-- 2018-07-02T11:45:41.234
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_product_lotnumber_lock','M_Product_ID','NUMERIC(10)',null,null)
+;
+
+-- 2018-07-02T11:45:41.241
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_product_lotnumber_lock','M_Product_ID',null,'NULL',null)
+;
+
