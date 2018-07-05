1caa2ad7abaeb27f264c98664adccbe3dcff51ce
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-07-05 11:50:19 +0200

    #4285 cleanup AbstractSendDocumentsForSelection
    * remove a number of reasons to not enqueue a record. they are now obsolete
    * also iterate over the docOutBound records rather then loading them all
    Invoice Document Outbound eMail enqueing https://github.com/metasfresh/metasfresh/issues/4285

--- de.metas.document.archive/de.metas.document.archive.base/src/main/java/de/metas/document/archive/process/SendPDFMailsForSelection.java
+++ de.metas.document.archive/de.metas.document.archive.base/src/main/java/de/metas/document/archive/process/SendPDFMailsForSelection.java
@@ -1,10 +1,26 @@
 package de.metas.document.archive.process;
 
+import de.metas.process.IProcessPrecondition;
+import de.metas.process.IProcessPreconditionsContext;
+import de.metas.process.ProcessPreconditionsResolution;
+
 /**
  * Send PDF mails for selected entries.
  *
  * @author al
  */
-public class SendPDFMailsForSelection extends AbstractSendDocumentsForSelection
+public class SendPDFMailsForSelection
+		extends AbstractSendDocumentsForSelection
+		implements IProcessPrecondition
 {
+
+	@Override
+	public ProcessPreconditionsResolution checkPreconditionsApplicable(IProcessPreconditionsContext context)
+	{
+		if(context.isNoSelection())
+		{
+			return ProcessPreconditionsResolution.rejectWithInternalReason("No records selected");
+		}
+		return ProcessPreconditionsResolution.accept();
+	}
 }
