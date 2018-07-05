ea0f63471e26620095b7e92239bc9c3e750d62bb
Mark Krake <mark.krake@metasfresh.com>
2018-07-05 17:07:02 +0200

    Adding further adjustments #4323

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5497100_sys_gh4323_payment_allocations-editable.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5497100_sys_gh4323_payment_allocations-editable.sql
@@ -98,3 +98,8 @@ UPDATE AD_Tab SET IsInsertRecord='Y', IsReadOnly='N',Updated=TO_TIMESTAMP('2018-
 UPDATE AD_Field SET IsReadOnly='N',Updated=TO_TIMESTAMP('2018-07-04 11:38:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=10438
 ;
 
+-- 2018-07-04T13:05:06.256
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsReadOnly='N',Updated=TO_TIMESTAMP('2018-07-04 13:05:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4370
+;
+
