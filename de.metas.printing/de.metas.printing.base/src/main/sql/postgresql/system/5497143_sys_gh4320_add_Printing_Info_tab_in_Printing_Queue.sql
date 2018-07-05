6a019acf80cb7d81fe6fd736c74ff7e5e4d6131f
metas-lc <liana.cotuna@metasfresh.com>
2018-07-05 11:36:09 +0300

    #4320 solve encoding
    #4320

--- de.metas.printing/de.metas.printing.base/src/main/sql/postgresql/system/5497143_sys_gh4320_add_Printing_Info_tab_in_Printing_Queue.sql
+++ de.metas.printing/de.metas.printing.base/src/main/sql/postgresql/system/5497143_sys_gh4320_add_Printing_Info_tab_in_Printing_Queue.sql
@@ -177,4 +177,3 @@ INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=565088 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
 ;
-
