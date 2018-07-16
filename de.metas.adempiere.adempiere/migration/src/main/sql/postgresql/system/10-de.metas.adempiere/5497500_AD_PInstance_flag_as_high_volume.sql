82031e21cef8a2123d98756325d90e82b246d4bd
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-07-16 12:35:55 +0200

    #4380-app - unrelated: flag AD_PInstance as high volume table
    consolidate profit related columns in order line and purchase candidate https://github.com/metasfresh/metasfresh/issues/4380

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5497500_AD_PInstance_flag_as_high_volume.sql
@@ -0,0 +1,5 @@
+-- 2018-07-13T08:43:20.060
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET IsHighVolume='Y',Updated=TO_TIMESTAMP('2018-07-13 08:43:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=282
+;
+
