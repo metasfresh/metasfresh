c92905336972ba8fd4084abf14f5038e6dabf9fb
siemiatj <kuba@saskla.cz>
2018-07-18 13:13:09 +0200

    - extend sales order tests

--- cypress/integration/sales_order_spec.js
+++ cypress/integration/sales_order_spec.js
@@ -30,11 +30,22 @@ describe('New sales order test', function() {
         .type('G0001');
 
       cy.get('.input-dropdown-list').should('exist');
-      cy.contains('.input-dropdown-list-option', 'G0001345a0_Test Kunde 1').click();
+      cy.contains('.input-dropdown-list-option', 'G0001345a0_Test Kunde 1')
+        .click();
       cy.get('.input-dropdown-list .input-dropdown-list-header')
         .should('not.exist');
 
       cy.get('.header-breadcrumb-sitename').should('not.contain', '<');
+    });
+
+    it('Fill order reference to differentiate cypress tests', function() {
+      cy.get('.form-field-POReference')
+        .find('input')
+        .type('Cyress Test{enter}');
+
+      cy.get('.indicator-pending').should('exist');
+      cy.wait(100);
+      cy.get('.indicator-pending').should('not.exist');
     });
 
     it('Add new product', function() {
@@ -43,17 +54,44 @@ describe('New sales order test', function() {
         .should('exist')
         .click();
 
-      cy.get('.panel-modal')
-        .should('exist');
+      cy.get('.panel-modal').should('exist');
 
       cy.get('.form-field-M_Product_ID')
         .find('input')
         .type('C');
 
       cy.get('.input-dropdown-list').should('exist');
-      cy.contains('.input-dropdown-list-option', 'P002737_Convenience Salat 250g').click();
+      cy.contains('.input-dropdown-list-option', 'P002737_Convenience Salat 250g')
+        .click();
       cy.get('.input-dropdown-list .input-dropdown-list-header')
         .should('not.exist');
+
+      cy.get('.form-field-QtyEntered', { timeout: 12000 })
+        .find('input')
+        .should('not.have.value', '0');
+
+      cy.get('.panel-modal-header')
+        .find('.btn')
+        .click();
+    });
+
+    it('Change document status', function() {
+      cy.get('.form-field-DocAction')
+        .find('.meta-dropdown-toggle')
+        .click();
+
+      cy.get('.form-field-DocAction')
+        .find('.dropdown-status-toggler')
+        .should('have.class', 'dropdown-status-open');
+
+      cy.get('.form-field-DocAction .dropdown-status-list')
+        .find('.dropdown-status-item')
+        .contains('Complete')
+        .click();
+
+      cy.get('.indicator-pending', { timeout: 10000 }).should('not.exist');
+
+      cy.get('.meta-dropdown-toggle .tag-success').contains('Completed');
     });
   });
 });
