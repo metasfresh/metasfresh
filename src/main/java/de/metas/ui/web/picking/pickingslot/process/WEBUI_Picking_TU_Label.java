8287ca365a33f2a136c930b6f707a465d741eb26
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-07-20 10:32:02 +0200

    minor (add single "@NonNull")
    Notification if automatic TU label print failed https://github.com/metasfresh/metasfresh/issues/4395

--- src/main/java/de/metas/ui/web/picking/pickingslot/process/WEBUI_Picking_TU_Label.java
+++ src/main/java/de/metas/ui/web/picking/pickingslot/process/WEBUI_Picking_TU_Label.java
@@ -9,6 +9,7 @@ import de.metas.handlingunits.report.HUReportService;
 import de.metas.handlingunits.report.HUToReportWrapper;
 import de.metas.process.ProcessPreconditionsResolution;
 import de.metas.ui.web.picking.pickingslot.PickingSlotRow;
+import lombok.NonNull;
 
 /*
  * #%L
@@ -20,12 +21,12 @@ import de.metas.ui.web.picking.pickingslot.PickingSlotRow;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -75,7 +76,7 @@ public class WEBUI_Picking_TU_Label extends PickingSlotViewBasedProcess
 
 	}
 
-	private void printPickingLabel(final HUToReportWrapper huToReport)
+	private void printPickingLabel(@NonNull final HUToReportWrapper huToReport)
 	{
 		final HUReportService huReportService = HUReportService.get();
 		huReportService.printPickingLabel(huToReport, false);
