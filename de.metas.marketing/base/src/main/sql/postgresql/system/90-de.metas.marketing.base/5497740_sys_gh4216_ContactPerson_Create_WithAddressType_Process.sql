5ff455d709a16273423600e99c96d6367948494c
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-07-23 16:34:11 +0300

    #4216 repair script
    * it looks like I lost the part where the reference was created
    https://github.com/metasfresh/metasfresh/issues/4216

--- de.metas.marketing/base/src/main/sql/postgresql/system/90-de.metas.marketing.base/5497740_sys_gh4216_ContactPerson_Create_WithAddressType_Process.sql
+++ de.metas.marketing/base/src/main/sql/postgresql/system/90-de.metas.marketing.base/5497740_sys_gh4216_ContactPerson_Create_WithAddressType_Process.sql
@@ -5,6 +5,46 @@
 INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,544180,0,'DefaultAddressType',TO_TIMESTAMP('2018-07-19 18:01:28','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','DefaultAddressType','DefaultAddressType',TO_TIMESTAMP('2018-07-19 18:01:28','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+
+
+
+-- 2018-07-23T16:20:24.110
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,540874,TO_TIMESTAMP('2018-07-23 16:20:23','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.marketing.base','Y','N','DefaultAddressTypes',TO_TIMESTAMP('2018-07-23 16:20:23','YYYY-MM-DD HH24:MI:SS'),100,'L')
+;
+
+-- 2018-07-23T16:20:24.120
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Reference_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Reference t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Reference_ID=540874 AND NOT EXISTS (SELECT 1 FROM AD_Reference_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Reference_ID=t.AD_Reference_ID)
+;
+
+-- 2018-07-23T16:20:56.753
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Ref_List_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value,ValueName) VALUES (0,0,541661,540874,TO_TIMESTAMP('2018-07-23 16:20:56','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.marketing.base','Y','BillToDefault',TO_TIMESTAMP('2018-07-23 16:20:56','YYYY-MM-DD HH24:MI:SS'),100,'B','BillToDefault')
+;
+
+-- 2018-07-23T16:20:56.763
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Ref_List_ID=541661 AND NOT EXISTS (SELECT 1 FROM AD_Ref_List_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Ref_List_ID=t.AD_Ref_List_ID)
+;
+
+-- 2018-07-23T16:21:17.578
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Ref_List_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value,ValueName) VALUES (0,0,541662,540874,TO_TIMESTAMP('2018-07-23 16:21:17','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.marketing.base','Y','ShipToDefault',TO_TIMESTAMP('2018-07-23 16:21:17','YYYY-MM-DD HH24:MI:SS'),100,'S','ShipToDefault')
+;
+
+-- 2018-07-23T16:21:17.578
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Ref_List_ID=541662 AND NOT EXISTS (SELECT 1 FROM AD_Ref_List_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Ref_List_ID=t.AD_Ref_List_ID)
+;
+
+
+
+
+
+
+
+
 -- 2018-07-19T18:01:28.486
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=544180 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
@@ -46,7 +86,7 @@ UPDATE AD_Process_Para SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-07-20 14:4
 
 -- 2018-07-20T14:42:06.945
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Reference_Value_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,544180,0,540990,541325,17,540873,'DefaultAddressType',TO_TIMESTAMP('2018-07-20 14:42:06','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.marketing.base',0,'Y','N','Y','N','N','N','DefaultAddressType',20,TO_TIMESTAMP('2018-07-20 14:42:06','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Reference_Value_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,544180,0,540990,541325,17,540874,'DefaultAddressType',TO_TIMESTAMP('2018-07-20 14:42:06','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.marketing.base',0,'Y','N','Y','N','N','N','DefaultAddressType',20,TO_TIMESTAMP('2018-07-20 14:42:06','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2018-07-20T14:42:06.947
