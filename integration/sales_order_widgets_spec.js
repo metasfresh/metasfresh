0d74222d425dd4990a99aa91f29846465d9d1f6a
siemiatj <kuba@saskla.cz>
2018-07-30 19:30:21 +0200

    - tests for sales order attributes widget #1895
    (cherry picked from commit dae337e6fa05a17af2fc0bda97a8447643a064c1)

--- integration/sales_order_widgets_spec.js
+++ integration/sales_order_widgets_spec.js
@@ -2,27 +2,54 @@ describe('Sales order window widgets test', function() {
   before(function() {
     // login before each test
     cy.loginByForm();
+    cy.visit('/window/143/1000489');
+    cy.get('.header-breadcrumb-sitename').should('contain', '0359');
   });
 
+  // This is tested in sales_order_spec.js
   context('Toggle widgets', function() {
-    beforeEach(function() {
-      cy.visit('/window/143/1000489');
-      cy.get('.header-breadcrumb-sitename').should('contain', '0359');
-    });
+    // beforeEach(function() {
+    //   cy.visit('/window/143/1000489');
+    //   cy.get('.header-breadcrumb-sitename').should('contain', '0359');
+    // });
 
-    it('Select lookup option', function() {
-      cy
-        .get('.input-dropdown-container .raw-lookup-wrapper')
-        .first()
-        .find('input')
-        .clear()
-        .type('test');
+    // it('Select lookup option', function() {
+    //   cy
+    //     .get('.input-dropdown-container .raw-lookup-wrapper')
+    //     .first()
+    //     .find('input')
+    //     .clear()
+    //     .type('test');
 
-      cy.get('.input-dropdown-list').should('exist');
-      cy.contains('.input-dropdown-list-option', '1000004_kaystest').click();
-      cy
-        .get('.input-dropdown-list .input-dropdown-list-header')
-        .should('not.exist');
+    //   cy.get('.input-dropdown-list').should('exist');
+    //   cy.contains('.input-dropdown-list-option', '1000004_kaystest').click();
+    //   cy
+    //     .get('.input-dropdown-list .input-dropdown-list-header')
+    //     .should('not.exist');
+    // });
+  });
+
+  context('Edit product in list', function() {
+    it('Change product attributes', function() {
+      cy.get('.ProductAttributes').find('.productattributes-cell').last().dblclick();
+
+      cy.get('.form-field-M_AttributeSetInstance_ID').should('exist')
+        .find('button').click();
+
+      cy.get('.attributes-dropdown').should('exist');
+
+      cy.get('.form-field-IsRepackNumberRequired')
+        .find('.input-dropdown-container')
+        .click();
+
+      cy.get('.input-dropdown-list').should('exist')
+        .find('.input-dropdown-list-option')
+        .contains('No')
+        .click()
+
+      // this should hide the attributes panel
+      cy.get('.row-selected .Integer').click();
+      cy.get('.input-dropdown-list').should('not.exist')
     });
   });
 });
