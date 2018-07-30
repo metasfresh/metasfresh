13cf7da27dc599a08862f2f7fde84e34f4f5f9e1
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-07-30 17:47:28 +0200

    #4303-app show the fields in webui
    Extend webui for PaymentDiscount https://github.com/metasfresh/metasfresh/issues/4303

--- de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5498350_sys_gh4303-app_add_payment_discount_fields_to_webui.sql
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5498350_sys_gh4303-app_add_payment_discount_fields_to_webui.sql
@@ -101,3 +101,37 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,565026,0,406,540609,552489,'F',TO_TIMESTAMP('2018-07-30 17:01:36','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','Skonto %',135,0,0,TO_TIMESTAMP('2018-07-30 17:01:36','YYYY-MM-DD HH24:MI:SS'),100)
 ;
+
+
+--
+-- fixes
+--
+-- 2018-07-30T17:21:29.164
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2018-07-30 17:21:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550143
+;
+
+-- 2018-07-30T17:23:21.211
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2018-07-30 17:23:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552486
+;
+
+-- 2018-07-30T17:26:50.852
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=160,Updated=TO_TIMESTAMP('2018-07-30 17:26:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550167
+;
+
+-- 2018-07-30T17:26:50.858
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=170,Updated=TO_TIMESTAMP('2018-07-30 17:26:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552489
+;
+
+-- 2018-07-30T17:26:50.861
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=180,Updated=TO_TIMESTAMP('2018-07-30 17:26:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=545437
+;
+
+-- 2018-07-30T17:26:50.867
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=190,Updated=TO_TIMESTAMP('2018-07-30 17:26:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=551573
+;
