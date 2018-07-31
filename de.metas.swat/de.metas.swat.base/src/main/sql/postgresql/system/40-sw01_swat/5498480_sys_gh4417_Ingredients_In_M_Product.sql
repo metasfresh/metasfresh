10cbccde67adbd388b15f8391c73f45cddd0fda4
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-07-31 17:03:02 +0300

    #4417swingui product window improvements
    https://github.com/metasfresh/metasfresh/issues/4417

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5498480_sys_gh4417_Ingredients_In_M_Product.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5498480_sys_gh4417_Ingredients_In_M_Product.sql
@@ -32,3 +32,10 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=560725 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
+
+
+
+-- 2018-07-31T15:48:52.772
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_ID=14,Updated=TO_TIMESTAMP('2018-07-31 15:48:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560725
+;
--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5498490_sys_gh4417_Ingredients_In_M_Product_DDL.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5498490_sys_gh4417_Ingredients_In_M_Product_DDL.sql
@@ -1,5 +1,6 @@
 
--- 2018-07-31T15:38:05.521
+
+-- 2018-07-31T15:54:58.109
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 /* DDL */ SELECT public.db_alter_table('M_Product','ALTER TABLE public.M_Product ADD COLUMN Ingredients VARCHAR(2000)')
 ;
