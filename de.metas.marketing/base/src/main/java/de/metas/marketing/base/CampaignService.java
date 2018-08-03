520bb60bafae3159031bc5cb590c35d1e52b50a1
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-03 15:35:04 +0300

    #4216 make sure the contact person is not created if the location is required by the process
    https://github.com/metasfresh/metasfresh/issues/4216

--- de.metas.marketing/base/src/main/java/de/metas/marketing/base/CampaignService.java
+++ de.metas.marketing/base/src/main/java/de/metas/marketing/base/CampaignService.java
@@ -94,13 +94,13 @@ public class CampaignService
 			return;
 		}
 
-		final boolean isRequiredLocation = platform.isRequiredLocation();
+		final boolean isRequiredLocation = platform.isRequiredLocation() || defaultAddressType != null;
 
 		final BPartnerLocationId addressToUse = computeAddressToUse(user, defaultAddressType);
 
-		if (isRequiredLocation && addressToUse == null)
+		if (isRequiredLocation && addressToUse == null )
 		{
-			Loggables.get().addLog("Skip user because it has no bill to default location or campaign does not require location; user={}", user);
+			Loggables.get().addLog("Skip user because it has no "+ defaultAddressType != null ? defaultAddressType.toString() : DefaultAddressType.BillToDefault + "location and the campaign requires location; user={}", user);
 			return;
 		}
 
@@ -133,7 +133,7 @@ public class CampaignService
 			// Keep as before, and consider Bill address as default
 			addressToUse = bpartnerDAO.getBilltoDefaultLocationIdByBpartnerId(bpartnerId);
 		}
-		
+
 		return addressToUse;
 
 	}
