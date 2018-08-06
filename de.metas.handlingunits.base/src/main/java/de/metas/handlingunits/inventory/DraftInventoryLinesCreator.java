1ede75a967e7a08d56f4709bc882de0cd08c0332
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-08-06 20:15:04 +0200

    Extract existing code from M_Inventory_CreateCountLinesFromHU
    Also create HUsForInventoryStrategy that can be implemented for the two "modes" that need to be supported as of this issue
    Advanced BLs to create inventory lines #4448

---
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/DraftInventoryLinesCreator.java
@@ -0,0 +1,136 @@
+package de.metas.handlingunits.inventory;
+
+import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.util.Check;
+import org.adempiere.util.GuavaCollectors;
+import org.adempiere.util.Services;
+import org.compiere.model.I_M_Inventory;
+
+import de.metas.document.engine.IDocumentBL;
+import de.metas.handlingunits.HuId;
+import de.metas.handlingunits.IHandlingUnitsBL;
+import de.metas.handlingunits.model.I_M_HU;
+import de.metas.handlingunits.model.I_M_InventoryLine;
+import de.metas.handlingunits.storage.IHUProductStorage;
+import de.metas.inventory.IInventoryDAO;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+/*
+ * #%L
+ * de.metas.handlingunits.base
+ * %%
+ * Copyright (C) 2018 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+/**
+ * Creates or updates inventory lines for
+ *
+ * @author metas-dev <dev@metasfresh.com>
+ *
+ */
+@Value
+public class DraftInventoryLinesCreator
+{
+	IDocumentBL documentBL = Services.get(IDocumentBL.class);
+	IInventoryDAO inventoryDAO = Services.get(IInventoryDAO.class);
+	IHandlingUnitsBL handlingUnitsBL = Services.get(IHandlingUnitsBL.class);
+
+	I_M_Inventory inventoryRecord;
+
+	Map<HuId, I_M_InventoryLine> inventoryLinesByHU;
+
+	long countInventoryLines;
+
+	@Builder
+	private DraftInventoryLinesCreator(
+			@NonNull final I_M_Inventory inventoryRecord,
+			@NonNull final HUsForInventoryStrategy strategy)
+	{
+		Check.errorUnless(
+				documentBL.issDocumentDraftedOrInProgress(inventoryRecord),
+				"the given inventory record needs to be in status 'DR' or 'IP', but is in status={}; inventoryRecord={}",
+				inventoryRecord.getDocStatus(), inventoryRecord);
+		this.inventoryRecord = inventoryRecord;
+
+		// get existing lines' HuIds
+		this.inventoryLinesByHU = inventoryDAO
+				.retrieveLinesForInventoryId(inventoryRecord.getM_Inventory_ID(), I_M_InventoryLine.class)
+				.stream()
+				.filter(line -> line.getM_HU_ID() > 0)
+				.collect(GuavaCollectors.toImmutableMapByKey(line -> HuId.ofRepoId(line.getM_HU_ID())));
+
+		// create/update new lines
+		countInventoryLines = strategy
+				.streamHus()
+				.flatMap(this::createUpdateInventoryLines)
+				.count();
+	}
+
+	private Stream<I_M_InventoryLine> createUpdateInventoryLines(@NonNull final I_M_HU hu)
+	{
+		return handlingUnitsBL
+				.getStorageFactory()
+				.streamHUProductStorages(hu)
+				.map(this::createOrUpdateInventoryLine);
+	}
+
+	private I_M_InventoryLine createOrUpdateInventoryLine(@NonNull final IHUProductStorage huProductStorage)
+	{
+		final I_M_InventoryLine inventoryLine;
+		final I_M_HU hu = huProductStorage.getM_HU();
+
+		final HuId huId = HuId.ofRepoId(hu.getM_HU_ID());
+
+		if (inventoryLinesByHU.containsKey(huId))
+		{
+			// update line
+			inventoryLine = inventoryLinesByHU.get(huId);
+		}
+		else
+		{
+			// create line
+			inventoryLine = InterfaceWrapperHelper.newInstance(I_M_InventoryLine.class);
+			inventoryLine.setM_Inventory(inventoryRecord);
+			inventoryLine.setAD_Org_ID(inventoryRecord.getAD_Org_ID());
+		}
+
+		inventoryLine.setM_AttributeSetInstance_ID(0);
+		inventoryLine.setM_HU_ID(hu.getM_HU_ID());
+		inventoryLine.setM_HU_PI_Item_Product(null); // TODO
+		inventoryLine.setQtyTU(BigDecimal.ZERO); // TODO
+
+		inventoryLine.setM_Locator_ID(hu.getM_Locator_ID());
+		inventoryLine.setM_Product_ID(huProductStorage.getM_Product_ID());
+		inventoryLine.setC_UOM(huProductStorage.getC_UOM());
+
+		inventoryLine.setQtyBook(huProductStorage.getQty());
+		inventoryLine.setQtyCount(huProductStorage.getQty());
+
+		saveRecord(inventoryLine);
+
+		return inventoryLine;
+	}
+}
---
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/process/DraftInventoryBase.java
@@ -0,0 +1,69 @@
+package de.metas.handlingunits.inventory.process;
+
+import org.compiere.model.I_M_Inventory;
+
+import de.metas.handlingunits.inventory.DraftInventoryLinesCreator;
+import de.metas.handlingunits.inventory.LocatorAndProductHUsStrategy;
+import de.metas.process.IProcessPrecondition;
+import de.metas.process.IProcessPreconditionsContext;
+import de.metas.process.JavaProcess;
+import de.metas.process.ProcessPreconditionsResolution;
+
+/*
+ * #%L
+ * de.metas.handlingunits.base
+ * %%
+ * Copyright (C) 2018 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+public abstract class DraftInventoryBase extends JavaProcess implements IProcessPrecondition
+{
+	@Override
+	public ProcessPreconditionsResolution checkPreconditionsApplicable(final IProcessPreconditionsContext context)
+	{
+		if (!context.isSingleSelection())
+		{
+			return ProcessPreconditionsResolution.rejectBecauseNotSingleSelection();
+		}
+
+		final I_M_Inventory inventory = context.getSelectedModel(I_M_Inventory.class);
+		if (inventory.isProcessed())
+		{
+			return ProcessPreconditionsResolution.rejectWithInternalReason("inventory is processed");
+		}
+
+		return ProcessPreconditionsResolution.accept();
+	}
+
+	@Override
+	protected String doIt()
+	{
+		final I_M_Inventory inventory = getRecord(I_M_Inventory.class);
+
+		final LocatorAndProductHUsStrategy strategy = createStrategy(inventory);
+
+		final DraftInventoryLinesCreator draftLinesCreator = DraftInventoryLinesCreator.builder()
+				.inventoryRecord(inventory)
+				.strategy(strategy)
+				.build();
+
+		return "@Created@/@Updated@ #" + draftLinesCreator.getCountInventoryLines();
+	}
+
+	protected abstract LocatorAndProductHUsStrategy createStrategy(I_M_Inventory inventory);
+}
