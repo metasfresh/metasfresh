0fd0027c6cd721976f71d8a63e60e4c4e5897e49
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-09 13:47:38 +0300

    #4417 repair script
    https://github.com/metasfresh/metasfresh/issues/4417

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5498930_sys_gh4417_Ingredients_In_ProductTranslation_And_BPProductTranslation.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5498930_sys_gh4417_Ingredients_In_ProductTranslation_And_BPProductTranslation.sql
@@ -1,12 +1,4 @@
--- 2018-08-07T12:02:52.900
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,560718,565517,0,540895,0,TO_TIMESTAMP('2018-08-07 12:02:52','YYYY-MM-DD HH24:MI:SS'),100,0,'U',0,'Y','Y','Y','N','N','N','N','N','Zutaten',10,10,0,1,1,TO_TIMESTAMP('2018-08-07 12:02:52','YYYY-MM-DD HH24:MI:SS'),100)
-;
 
--- 2018-08-07T12:02:52.903
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=565517 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
-;
 
 -- 2018-08-07T12:03:06.193
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
@@ -38,10 +30,7 @@ UPDATE AD_Field SET SeqNo=50,Updated=TO_TIMESTAMP('2018-08-07 12:03:32','YYYY-MM
 UPDATE AD_Field SET SeqNo=60,Updated=TO_TIMESTAMP('2018-08-07 12:03:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=560608
 ;
 
--- 2018-08-07T12:03:39.482
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Field SET SeqNo=70,Updated=TO_TIMESTAMP('2018-08-07 12:03:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=565517
-;
+
 
 -- 2018-08-07T15:52:49.043
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
