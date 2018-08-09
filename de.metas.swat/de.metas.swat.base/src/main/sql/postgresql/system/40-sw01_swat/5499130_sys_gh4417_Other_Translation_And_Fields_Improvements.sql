d48626754d082333cc8971abe09956c53fa68356
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-09 16:50:16 +0300

    #4417 repair script
    https://github.com/metasfresh/metasfresh/issues/4417

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5499130_sys_gh4417_Other_Translation_And_Fields_Improvements.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5499130_sys_gh4417_Other_Translation_And_Fields_Improvements.sql
@@ -3,21 +3,11 @@
 UPDATE AD_Column SET IsTranslated='Y',Updated=TO_TIMESTAMP('2018-08-08 11:30:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560725
 ;
 
--- 2018-08-08T11:37:35.454
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,DDL_NoForeignKey,EntityType,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRangeFilter,IsSelectionColumn,IsShowFilterIncrementButtons,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,560758,544186,0,10,312,'Ingredients',TO_TIMESTAMP('2018-08-08 11:37:35','YYYY-MM-DD HH24:MI:SS'),100,'N','D',2000,'Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','Zutaten',0,0,TO_TIMESTAMP('2018-08-08 11:37:35','YYYY-MM-DD HH24:MI:SS'),100,0)
-;
-
--- 2018-08-08T11:37:35.463
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=560758 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
-;
-
 
 
 -- 2018-08-08T12:05:49.326
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,560758,565581,0,541054,0,TO_TIMESTAMP('2018-08-08 12:05:49','YYYY-MM-DD HH24:MI:SS'),100,0,'U',0,'Y','Y','Y','N','N','N','N','N','Zutaten',110,110,0,1,1,TO_TIMESTAMP('2018-08-08 12:05:49','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,560751,565581,0,541054,0,TO_TIMESTAMP('2018-08-08 12:05:49','YYYY-MM-DD HH24:MI:SS'),100,0,'U',0,'Y','Y','Y','N','N','N','N','N','Zutaten',110,110,0,1,1,TO_TIMESTAMP('2018-08-08 12:05:49','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2018-08-08T12:05:49.336
@@ -50,24 +40,16 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 UPDATE AD_Field_Trl SET IsTranslated='Y',Name='Ingredients' WHERE AD_Field_ID=565581 AND AD_Language='en_US'
 ;
 
--- 2018-08-08T12:08:36.746
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,560758,565582,0,235,0,TO_TIMESTAMP('2018-08-08 12:08:36','YYYY-MM-DD HH24:MI:SS'),100,0,'D',0,'Y','Y','Y','N','N','N','N','N','Zutaten',110,110,0,1,1,TO_TIMESTAMP('2018-08-08 12:08:36','YYYY-MM-DD HH24:MI:SS'),100)
-;
 
--- 2018-08-08T12:08:36.751
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=565582 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
-;
 
 -- 2018-08-08T12:08:41.077
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Field SET SpanX=999,Updated=TO_TIMESTAMP('2018-08-08 12:08:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=565582
+UPDATE AD_Field SET SpanX=999,Updated=TO_TIMESTAMP('2018-08-08 12:08:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=565555
 ;
 
 -- 2018-08-08T12:09:04.106
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Field_Trl SET IsTranslated='Y',Name='Ingredients' WHERE AD_Field_ID=565582 AND AD_Language='en_US'
+UPDATE AD_Field_Trl SET IsTranslated='Y',Name='Ingredients' WHERE AD_Field_ID=565555 AND AD_Language='en_US'
 ;
 
 -- 2018-08-08T12:20:24.315
@@ -142,12 +124,12 @@ UPDATE AD_Field_Trl SET IsTranslated='Y',Name='Customer Label Name' WHERE AD_Fie
 
 -- 2018-08-08T13:12:07.626
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,565582,0,235,552505,1000023,'F',TO_TIMESTAMP('2018-08-08 13:12:07','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','Zutaten',100,0,0,TO_TIMESTAMP('2018-08-08 13:12:07','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,565555,0,235,552505,1000023,'F',TO_TIMESTAMP('2018-08-08 13:12:07','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','Zutaten',100,0,0,TO_TIMESTAMP('2018-08-08 13:12:07','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2018-08-08T14:42:28.815
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET AD_Reference_ID=14,Updated=TO_TIMESTAMP('2018-08-08 14:42:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560758
+UPDATE AD_Column SET AD_Reference_ID=14,Updated=TO_TIMESTAMP('2018-08-08 14:42:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560751
 ;
 
 -- 2018-08-08T15:50:18.671
