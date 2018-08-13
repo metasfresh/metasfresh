15e17e48ba49a14155e7c04974f700983a55f6fb
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-13 18:02:24 +0300

    #4450 display IsCounted in Invent ur window
    https://github.com/metasfresh/metasfresh/issues/4450

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5499090_sys_gh4450_M_InventoryLine_IsCounted.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5499090_sys_gh4450_M_InventoryLine_IsCounted.sql
@@ -7,3 +7,36 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=560761 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
+
+
+
+-- 2018-08-13T14:22:42.148
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET Name='Gezählt',Updated=TO_TIMESTAMP('2018-08-13 14:22:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560761
+;
+
+-- 2018-08-13T14:22:42.148
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Gezählt', Description='Count number of not empty elements', Help='Calculate the total number (?) of not empty (NULL) elements (maximum is the number of lines).' WHERE AD_Column_ID=560761
+;
+
+-- 2018-08-13T14:22:54.115
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column_Trl SET Name='Gezählt' WHERE AD_Column_ID=560761 AND AD_Language='de_CH'
+;
+
+-- 2018-08-13T14:22:55.864
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column_Trl SET IsTranslated='Y' WHERE AD_Column_ID=560761 AND AD_Language='de_CH'
+;
+
+-- 2018-08-13T14:23:13.714
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column_Trl SET Name='Gezählt' WHERE AD_Column_ID=560761 AND AD_Language='nl_NL'
+;
+
+-- 2018-08-13T14:23:21.011
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column_Trl SET Name='Counted',IsTranslated='Y' WHERE AD_Column_ID=560761 AND AD_Language='en_US'
+;
+
