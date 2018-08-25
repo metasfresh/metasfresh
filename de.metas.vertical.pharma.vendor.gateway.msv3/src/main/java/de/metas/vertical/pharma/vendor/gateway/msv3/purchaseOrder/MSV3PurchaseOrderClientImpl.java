6a7e1a8ce0be67ab6e9c73a21ba86de24e173c5e
Teo Sarca <teo.sarca@metasfresh.com>
2018-08-25 16:10:16 +0300

    refactor
    https://github.com/metasfresh/metasfresh/issues/4511

--- de.metas.vertical.pharma.vendor.gateway.msv3/src/main/java/de/metas/vertical/pharma/vendor/gateway/msv3/purchaseOrder/MSV3PurchaseOrderClientImpl.java
+++ de.metas.vertical.pharma.vendor.gateway.msv3/src/main/java/de/metas/vertical/pharma/vendor/gateway/msv3/purchaseOrder/MSV3PurchaseOrderClientImpl.java
@@ -1,5 +1,9 @@
 package de.metas.vertical.pharma.vendor.gateway.msv3.purchaseOrder;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 import javax.xml.bind.JAXBElement;
 
 import org.adempiere.service.OrgId;
@@ -7,21 +11,43 @@ import org.adempiere.util.Check;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 
+import de.metas.vendor.gateway.api.ProductAndQuantity;
+import de.metas.vendor.gateway.api.order.MSV3OrderResponsePackageItemPartRepoId;
 import de.metas.vendor.gateway.api.order.PurchaseOrderRequest;
 import de.metas.vendor.gateway.api.order.PurchaseOrderRequestItem;
+import de.metas.vendor.gateway.api.order.PurchaseOrderRequestItemId;
 import de.metas.vendor.gateway.api.order.RemotePurchaseOrderCreated;
+import de.metas.vendor.gateway.api.order.RemotePurchaseOrderCreated.RemotePurchaseOrderCreatedBuilder;
+import de.metas.vendor.gateway.api.order.RemotePurchaseOrderCreatedItem;
+import de.metas.vendor.gateway.api.order.RemotePurchaseOrderCreatedItem.RemotePurchaseOrderCreatedItemBuilder;
+import de.metas.vertical.pharma.msv3.protocol.order.DeliverySpecifications;
+import de.metas.vertical.pharma.msv3.protocol.order.MSV3PurchaseCandidateId;
 import de.metas.vertical.pharma.msv3.protocol.order.OrderCreateRequest;
+import de.metas.vertical.pharma.msv3.protocol.order.OrderCreateRequestPackage;
+import de.metas.vertical.pharma.msv3.protocol.order.OrderCreateRequestPackageItem;
 import de.metas.vertical.pharma.msv3.protocol.order.OrderCreateRequestPackageItemId;
 import de.metas.vertical.pharma.msv3.protocol.order.OrderJAXBConverters;
 import de.metas.vertical.pharma.msv3.protocol.order.OrderResponse;
+import de.metas.vertical.pharma.msv3.protocol.order.OrderResponsePackage;
+import de.metas.vertical.pharma.msv3.protocol.order.OrderResponsePackageItem;
+import de.metas.vertical.pharma.msv3.protocol.order.OrderResponsePackageItemPart;
+import de.metas.vertical.pharma.msv3.protocol.order.OrderType;
 import de.metas.vertical.pharma.msv3.protocol.order.SupportIDType;
+import de.metas.vertical.pharma.msv3.protocol.types.BPartnerId;
 import de.metas.vertical.pharma.msv3.protocol.types.ClientSoftwareId;
+import de.metas.vertical.pharma.msv3.protocol.types.FaultInfo;
+import de.metas.vertical.pharma.msv3.protocol.types.Id;
+import de.metas.vertical.pharma.msv3.protocol.types.PZN;
+import de.metas.vertical.pharma.msv3.protocol.types.Quantity;
 import de.metas.vertical.pharma.vendor.gateway.msv3.MSV3Client;
 import de.metas.vertical.pharma.vendor.gateway.msv3.MSV3ConnectionFactory;
+import de.metas.vertical.pharma.vendor.gateway.msv3.common.Msv3ClientException;
 import de.metas.vertical.pharma.vendor.gateway.msv3.config.MSV3ClientConfig;
+import de.metas.vertical.pharma.vendor.gateway.msv3.model.I_MSV3_Bestellung_Transaction;
 import lombok.Builder;
 import lombok.NonNull;
 
@@ -86,10 +112,10 @@ public class MSV3PurchaseOrderClientImpl implements MSV3PurchaseOrderClient
 
 		purchaseOrderRequestItems = Maps.uniqueIndex(
 				request.getItems(),
-				item -> MSV3PurchaseOrderClientUtils.toOrderCreateRequestPackageItemId(item.getId()));
+				item -> toOrderCreateRequestPackageItemId(item.getId()));
 
 		final SupportIDType supportId = supportIdProvider.getNextSupportId();
-		this.request = MSV3PurchaseOrderClientUtils.createBestellungWithOneAuftrag(request, supportId);
+		this.request = createBestellungWithOneAuftrag(request, supportId);
 
 		return this;
 	}
@@ -107,9 +133,9 @@ public class MSV3PurchaseOrderClientImpl implements MSV3PurchaseOrderClient
 					.build();
 
 			// invoke the deeper layer
-			MSV3PurchaseOrderClientUtils.performOrdering(purchaseTransaction, this::callWebService);
+			performOrdering(purchaseTransaction, this::callWebService);
 
-			return MSV3PurchaseOrderClientUtils.createRemotePurchaseOrderCreated(purchaseTransaction, purchaseOrderRequestItems);
+			return createRemotePurchaseOrderCreated(purchaseTransaction, purchaseOrderRequestItems);
 		}
 		finally
 		{
@@ -157,5 +183,140 @@ public class MSV3PurchaseOrderClientImpl implements MSV3PurchaseOrderClient
 	OrderCreateRequest getRequest()
 	{
 		return request;
+	}
+
+	private static OrderCreateRequest createBestellungWithOneAuftrag(@NonNull final PurchaseOrderRequest request, @NonNull final SupportIDType supportId)
+	{
+		return OrderCreateRequest.builder()
+				.orderId(Id.random())
+				.supportId(supportId)
+				.bpartnerId(BPartnerId.of(request.getVendorId()))
+				.orderPackage(OrderCreateRequestPackage.builder()
+						.id(Id.random())
+						.orderType(OrderType.NORMAL)
+						.orderIdentification(supportId.getValueAsString())
+						.supportId(supportId)
+						.items(request.getItems().stream()
+								.map(MSV3PurchaseOrderClientImpl::createOrderCreateRequestPackageItem)
+								.collect(ImmutableList.toImmutableList()))
+						.build())
+				.build();
+	}
+
+	private static void performOrdering(
+			@NonNull final MSV3PurchaseOrderTransaction purchaseTransaction,
+			final Function<OrderCreateRequest, OrderResponse> webService)
+	{
+		try
+		{
+			// invoke webservice
+			final OrderResponse response = webService.apply(purchaseTransaction.getRequest());
+
+			purchaseTransaction.setResponse(response);
+
+			final FaultInfo faultInfo = response.getSingleOrderFaultInfo();
+			if (faultInfo != null)
+			{
+				purchaseTransaction.setFaultInfo(faultInfo);
+			}
+		}
+		catch (final Msv3ClientException msv3ClientException)
+		{
+			purchaseTransaction.setFaultInfo(msv3ClientException.getMsv3FaultInfo());
+		}
+		catch (final Exception e)
+		{
+			purchaseTransaction.setOtherException(e);
+		}
+	}
+
+	private static OrderCreateRequestPackageItem createOrderCreateRequestPackageItem(@NonNull final PurchaseOrderRequestItem purchaseOrderRequestItem)
+	{
+		final ProductAndQuantity productAndQuantity = purchaseOrderRequestItem.getProductAndQuantity();
+		return OrderCreateRequestPackageItem.builder()
+				.id(toOrderCreateRequestPackageItemId(purchaseOrderRequestItem.getId()))
+				.deliverySpecifications(DeliverySpecifications.NORMAL)
+				.pzn(PZN.of(productAndQuantity.getProductIdentifier()))
+				.qty(Quantity.of(productAndQuantity.getQuantity()))
+				.purchaseCandidateId(MSV3PurchaseCandidateId.ofRepoIdOrNull(purchaseOrderRequestItem.getPurchaseCandidateId()))
+				.build();
+	}
+
+	private static OrderCreateRequestPackageItemId toOrderCreateRequestPackageItemId(final PurchaseOrderRequestItemId purchaseOrderRequestItemId)
+	{
+		return OrderCreateRequestPackageItemId.of(purchaseOrderRequestItemId.getValueAsString());
+	}
+
+	private static RemotePurchaseOrderCreated createRemotePurchaseOrderCreated(
+			final MSV3PurchaseOrderTransaction purchaseTransaction,
+			final Map<OrderCreateRequestPackageItemId, PurchaseOrderRequestItem> purchaseOrderRequestItems)
+	{
+		final I_MSV3_Bestellung_Transaction purchaseTransactionRecord = purchaseTransaction.store();
+
+		final RemotePurchaseOrderCreatedBuilder responseBuilder = RemotePurchaseOrderCreated.builder()
+				.transactionRecordId(purchaseTransactionRecord.getMSV3_Bestellung_Transaction_ID())
+				.transactionTableName(I_MSV3_Bestellung_Transaction.Table_Name)
+				.exception(purchaseTransaction.getExceptionOrNull());
+
+		final List<RemotePurchaseOrderCreatedItem> purchaseOrderResponseItems = createResponseItems(purchaseTransaction, purchaseOrderRequestItems);
+
+		responseBuilder.purchaseOrderResponseItems(purchaseOrderResponseItems);
+
+		return responseBuilder.build();
+	}
+
+	private static List<RemotePurchaseOrderCreatedItem> createResponseItems(
+			@NonNull final MSV3PurchaseOrderTransaction purchaseTransaction,
+			@NonNull final Map<OrderCreateRequestPackageItemId, PurchaseOrderRequestItem> purchaseOrderRequestItems)
+	{
+		if (purchaseTransaction.getResponse() == null)
+		{
+			return ImmutableList.of();
+		}
+
+		final OrderCreateRequest request = purchaseTransaction.getRequest();
+
+		final ImmutableList.Builder<RemotePurchaseOrderCreatedItem> purchaseOrderResponseItems = ImmutableList.builder();
+
+		final List<OrderResponsePackage> responseOrders = purchaseTransaction
+				.getResponse()
+				.getOrderPackages();
+		for (int orderIdx = 0; orderIdx < responseOrders.size(); orderIdx++)
+		{
+			final OrderCreateRequestPackage requestOrder = request.getOrderPackages().get(orderIdx);
+			final OrderResponsePackage responseOrder = responseOrders.get(orderIdx);
+
+			final List<OrderCreateRequestPackageItem> requestItems = requestOrder.getItems();
+			final List<OrderResponsePackageItem> responseItems = responseOrder.getItems();
+
+			for (int itemIdx = 0; itemIdx < responseItems.size(); itemIdx++)
+			{
+				final OrderCreateRequestPackageItem requestItem = requestItems.get(itemIdx);
+				final OrderResponsePackageItem responseItem = responseItems.get(itemIdx);
+
+				final RemotePurchaseOrderCreatedItemBuilder builder = RemotePurchaseOrderCreatedItem.builder()
+						.remotePurchaseOrderId(responseOrder.getId().getValueAsString())
+						.correspondingRequestItem(purchaseOrderRequestItems.get(requestItem.getId()));
+
+				for (final OrderResponsePackageItemPart responseItemPart : responseItem.getParts())
+				{
+					if (responseItemPart.getDeliveryDate() == null)
+					{
+						continue;
+					}
+
+					final MSV3OrderResponsePackageItemPartRepoId internalItemId = purchaseTransaction.getResponseItemPartRepoId(responseItemPart.getId());
+
+					builder
+							.confirmedDeliveryDate(responseItemPart.getDeliveryDate())
+							.confirmedOrderQuantity(responseItemPart.getQty().getValueAsBigDecimal())
+							.internalItemId(internalItemId);
+
+					purchaseOrderResponseItems.add(builder.build());
+				}
+			}
+		}
+
+		return purchaseOrderResponseItems.build();
 	}
 }
