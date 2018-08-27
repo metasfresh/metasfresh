dde40a7702551e314f7fcf8a47b1fe78820db618
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-27 16:18:33 +0300

    #4519 AllergenName in I_Product
    https://github.com/metasfresh/metasfresh/issues/4519

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5499850_sys_gh4519_I_Product_AllergenName_DDL.sql
@@ -0,0 +1,6 @@
+
+
+-- 2018-08-27T14:38:45.603
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ SELECT public.db_alter_table('I_Product','ALTER TABLE public.I_Product ADD COLUMN AllergenName VARCHAR(2000)')
+;
