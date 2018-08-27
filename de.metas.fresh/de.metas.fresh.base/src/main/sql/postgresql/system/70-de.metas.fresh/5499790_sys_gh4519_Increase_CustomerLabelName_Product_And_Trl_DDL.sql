f8a8d7aa1e30687fb053255e8fc7cd3dd8e9dd7a
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-27 15:59:31 +0300

    #4519 Increase CustomerLabelName column
    https://github.com/metasfresh/metasfresh/issues/4519

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5499790_sys_gh4519_Increase_CustomerLabelName_Product_And_Trl_DDL.sql
@@ -0,0 +1,9 @@
+ALTER TABLE M_Product 
+ALTER COLUMN customerlabelname
+SET DATA TYPE  character varying(2000);
+
+
+
+ALTER TABLE M_Product_Trl
+ALTER COLUMN customerlabelname
+SET DATA TYPE  character varying(2000);
