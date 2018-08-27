f44eeb8281d2648e5aca1f1f38380d2f0d55230f
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-27 15:58:52 +0300

    #4519 make sure the Name columns are mandatory
    https://github.com/metasfresh/metasfresh/issues/4519

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5499710_sys_gh4519_Make_Columns_Name_Mandatory.sql
@@ -0,0 +1,9 @@
+-- 2018-08-24T12:42:32.524
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-08-24 12:42:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560710
+;
+
+-- 2018-08-24T12:42:51.084
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-08-24 12:42:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560689
+;
---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5499720_sys_gh4519_Make_Columns_Name_Mandatory_DDL.sql
@@ -0,0 +1,21 @@
+
+-- 2018-08-24T12:43:18.060
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_allergen','Name','VARCHAR(2000)',null,null)
+;
+
+-- 2018-08-24T12:43:18.065
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_allergen','Name',null,'NOT NULL',null)
+;
+
+-- 2018-08-24T12:43:29.248
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_allergen_trl','Name','VARCHAR(2000)',null,null)
+;
+
+-- 2018-08-24T12:43:29.254
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_allergen_trl','Name',null,'NOT NULL',null)
+;
+
