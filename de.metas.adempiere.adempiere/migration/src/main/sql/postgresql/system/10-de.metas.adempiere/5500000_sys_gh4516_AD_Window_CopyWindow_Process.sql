79f26e28df0733c12c706f197ad121fe20353626
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-08-28 18:56:54 +0300

    #4516 assign process to table window
    https://github.com/metasfresh/metasfresh/issues/4516

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5500000_sys_gh4516_AD_Window_CopyWindow_Process.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5500000_sys_gh4516_AD_Window_CopyWindow_Process.sql
@@ -33,3 +33,20 @@ INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Hel
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Process_Para SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-08-28 18:20:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541335
 ;
+
+
+-- 2018-08-28T18:55:36.139
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Window (AD_Client_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,EntityType,IsActive,IsBetaFunctionality,IsDefault,IsEnableRemoteCacheInvalidation,IsOneInstanceOnly,IsSOTrx,Name,Processing,Updated,UpdatedBy,WindowType,WinHeight,WinWidth) VALUES (0,0,540461,TO_TIMESTAMP('2018-08-28 18:55:35','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','N','N','N','N','Y','TestProduct1','N',TO_TIMESTAMP('2018-08-28 18:55:35','YYYY-MM-DD HH24:MI:SS'),100,'M',0,0)
+;
+
+-- 2018-08-28T18:55:36.151
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Window_Trl (AD_Language,AD_Window_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Window_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Window t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Window_ID=540461 AND NOT EXISTS (SELECT 1 FROM AD_Window_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Window_ID=t.AD_Window_ID)
+;
+
+-- 2018-08-28T18:56:18.887
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,540998,105,TO_TIMESTAMP('2018-08-28 18:56:18','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2018-08-28 18:56:18','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
+;
+
