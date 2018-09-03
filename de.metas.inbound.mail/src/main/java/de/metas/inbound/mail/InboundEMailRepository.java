013cb94a38f44bc181c8f95efc1079e011f546a8
Teo Sarca <teo.sarca@metasfresh.com>
2018-09-03 13:06:33 +0300

    InboundEMailRepository: set C_Mail.From_User_ID
    https://github.com/metasfresh/metasfresh/issues/4524

--- de.metas.inbound.mail/src/main/java/de/metas/inbound/mail/InboundEMailRepository.java
+++ de.metas.inbound.mail/src/main/java/de/metas/inbound/mail/InboundEMailRepository.java
@@ -8,13 +8,20 @@ import java.util.List;
 import java.util.Map;
 
 import javax.activation.DataSource;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.service.ClientId;
+import org.adempiere.user.UserId;
+import org.adempiere.user.api.IUserDAO;
+import org.adempiere.util.Check;
 import org.adempiere.util.Services;
 import org.compiere.model.I_C_Mail;
 import org.compiere.util.TimeUtil;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -23,6 +30,7 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 
 import de.metas.attachments.IAttachmentBL;
+import de.metas.logging.LogManager;
 import de.metas.request.RequestId;
 import lombok.NonNull;
 
@@ -51,6 +59,8 @@ import lombok.NonNull;
 @Repository
 public class InboundEMailRepository
 {
+	private static final Logger logger = LogManager.getLogger(InboundEMailRepository.class);
+
 	private final ObjectMapper jsonObjectMapper;
 
 	public InboundEMailRepository(@NonNull final ObjectMapper jsonObjectMapper)
@@ -94,6 +104,14 @@ public class InboundEMailRepository
 		mailRecord.setEMailHeadersJSON(toJson(email.getHeaders()));
 
 		mailRecord.setR_Request_ID(RequestId.toRepoId(email.getRequestId()));
+
+		final ClientId adClientId = ClientId.ofRepoId(mailRecord.getAD_Client_ID());
+		final UserId fromUserId = getUserIdByEMailOrNull(email.getFrom(), adClientId);
+		if (fromUserId != null)
+		{
+			mailRecord.setFrom_User_ID(fromUserId.getRepoId());
+		}
+
 		return mailRecord;
 	}
 
@@ -107,6 +125,35 @@ public class InboundEMailRepository
 		{
 			throw AdempiereException.wrapIfNeeded(ex)
 					.setParameter("map", map);
+		}
+	}
+
+	private UserId getUserIdByEMailOrNull(final String email, final ClientId adClientId)
+	{
+		if (Check.isEmpty(email, true))
+		{
+			return null;
+		}
+
+		final String emailNorm = extractEMailAddressOrNull(email);
+		if (Check.isEmpty(emailNorm, true))
+		{
+			return null;
+		}
+
+		return Services.get(IUserDAO.class).retrieveUserIdByEMail(emailNorm, adClientId);
+	}
+
+	private static final String extractEMailAddressOrNull(final String email)
+	{
+		try
+		{
+			return new InternetAddress(email).getAddress();
+		}
+		catch (AddressException e)
+		{
+			logger.warn("Invalid email address `{}`. Returning null.", email, e);
+			return null;
 		}
 	}
 
