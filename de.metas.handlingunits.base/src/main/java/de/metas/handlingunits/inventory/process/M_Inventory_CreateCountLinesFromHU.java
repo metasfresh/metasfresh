630609a85f85e3b1e9439b013162419f96fa3784
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-09-04 09:30:03 +0300

    #4448 Use WarehouseId.ofRepoIdOrNull
    #4448 Advanced BLs to create inventory lines

--- de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/process/M_Inventory_CreateCountLinesFromHU.java
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/process/M_Inventory_CreateCountLinesFromHU.java
@@ -44,7 +44,7 @@ public class M_Inventory_CreateCountLinesFromHU extends DraftInventoryBase
 	@Override
 	protected LocatorAndProductHUsStrategy createStrategy(@NonNull final I_M_Inventory inventoryRecord)
 	{
-		final WarehouseId warehouseId = inventoryRecord.getM_Warehouse_ID() > 0 ? WarehouseId.ofRepoId(inventoryRecord.getM_Warehouse_ID()) : null;
+		final WarehouseId warehouseId = WarehouseId.ofRepoIdOrNull(inventoryRecord.getM_Warehouse_ID()); 
 
 		final LocatorAndProductHUsStrategy strategy = LocatorAndProductHUsStrategy
 				.builder()
