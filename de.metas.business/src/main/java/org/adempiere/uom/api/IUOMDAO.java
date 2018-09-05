d02c260e8f526900ed60db95f8d34b701f18208a
Teo Sarca <teo.sarca@metasfresh.com>
2018-09-05 13:14:49 +0300

    IUOMDAO more getters
    https://github.com/metasfresh/metasfresh/issues/4549

--- de.metas.business/src/main/java/org/adempiere/uom/api/IUOMDAO.java
+++ de.metas.business/src/main/java/org/adempiere/uom/api/IUOMDAO.java
@@ -13,43 +13,41 @@ package org.adempiere.uom.api;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-
 import java.util.Properties;
 
+import org.adempiere.uom.UomId;
 import org.adempiere.util.ISingletonService;
 import org.compiere.model.I_C_UOM;
 
 public interface IUOMDAO extends ISingletonService
 {
-	I_C_UOM getById(int uomId);
-	
 	String X12DE355_Each = "PCE";
 	int C_UOM_ID_Each = 100;
 	String X12DE355_Kilogram = "KGM";
 
+	I_C_UOM getById(int uomId);
+
+	I_C_UOM getById(UomId uomId);
+
+	UomId getUomIdByX12DE355(String x12de355);
+
 	/**
-	 *
-	 * @param ctx
-	 * @param x12de355
 	 * @return uom; never return null
 	 */
 	I_C_UOM retrieveByX12DE355(Properties ctx, String x12de355);
 
 	/**
-	 *
-	 * @param ctx
-	 * @param x12de355
 	 * @param throwExIfNull if <code>false</code> and there is no UOM with the given <code>x12de355</code>, then we return <code>null</code>.
-	 * @return
+	 * @return uom; never return null
 	 */
 	I_C_UOM retrieveByX12DE355(Properties ctx, String x12de355, boolean throwExIfNull);
 
