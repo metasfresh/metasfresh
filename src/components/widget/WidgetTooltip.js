13ef6a859224b2f9cf4c6107919aa665d59d6747
siemiatj <kuba@saskla.cz>
2018-09-14 12:23:18 +0200

    - change layout for tooltip widget #1952

--- src/components/widget/WidgetTooltip.js
+++ src/components/widget/WidgetTooltip.js
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 // import { List } from 'immutable';
@@ -8,7 +8,9 @@ import React, { Component } from 'react';
 import onClickOutside from 'react-onclickoutside';
 import TetherComponent from 'react-tether';
 // import PropTypes from 'prop-types';
-import classnames from 'classnames';
+// import classnames from 'classnames';
+import iconHelp from '../../assets/images/tooltip-help.png';
+import iconText from '../../assets/images/tooltip-text.png';
 // import SelectionDropdown from '../SelectionDropdown';
 
 // import {
@@ -19,40 +21,40 @@ import classnames from 'classnames';
 // import RawList from './RawList';
 
 class WidgetTooltip extends Component {
-  previousValue = '';
+  // previousValue = '';
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-    };
-  }
+  //   this.state = {
+  //   };
+  // }
 
-  componentDidMount() {
-  }
+  // componentDidMount() {
+  // }
 
-  componentWillReceiveProps(nextProps) {
-  }
+  // componentWillReceiveProps(nextProps) {
+  // }
 
-  componentDidUpdate(prevProps) {
-  }
+  // componentDidUpdate(prevProps) {
+  // }
 
-  closeDropdownList = () => {
-    // this.setState({
-    //   listToggled: false,
-    // });
-  };
+  // closeDropdownList = () => {
+  //   // this.setState({
+  //   //   listToggled: false,
+  //   // });
+  // };
 
-  activate = () => {
-    // const { list, listToggled } = this.state;
-    // const { lookupList } = this.props;
+  // activate = () => {
+  //   // const { list, listToggled } = this.state;
+  //   // const { lookupList } = this.props;
 
-    // if (list && !listToggled && !(lookupList && list.size < 1)) {
-    //   this.setState({
-    //     listToggled: true,
-    //   });
-    // }
-  };
+  //   // if (list && !listToggled && !(lookupList && list.size < 1)) {
+  //   //   this.setState({
+  //   //     listToggled: true,
+  //   //   });
+  //   // }
+  // };
 
   /*
    * Alternative method to open dropdown, in case of disabled opening
@@ -60,6 +62,9 @@ class WidgetTooltip extends Component {
    */
   handleClick = () => {
     // const { onOpenDropdown, isToggled, onCloseDropdown } = this.props;
+    const { onToggle } = this.props;
+
+    onToggle();
 
     // if (!isToggled) {
     //   this.dropdown.focus();
@@ -93,53 +98,79 @@ class WidgetTooltip extends Component {
     //     }
     //   );
     // }
+    const { onToggle } = this.props;
+
+    onToggle(false);
   }
 
   render() {
-    const { isToggled } = this.props;
+    const { isToggled, item } = this.props;
 
     console.log('WidgetTooltip props: ', this.props);
 
+    // return (
+    //   <TetherComponent
+    //     attachment="top left"
+    //     targetAttachment="bottom left"
+    //     constraints={[
+    //       {
+    //         to: 'scrollParent',
+    //       },
+    //       {
+    //         to: 'window',
+    //         pin: ['bottom'],
+    //       },
+    //     ]}
+    //   >
+    //     <div
+    //       className={classnames('widget-tooltip', {
+    //         opened: isToggled,
+    //       })}
+    //       tabIndex={-1}
+    //       onClick={this.handleClick}
+    //     >
+    //     </div>
+    //     {isToggled && (
+    //       <div></div>
+    //     )}
+    //   </TetherComponent>
+    // );
     return (
-      <TetherComponent
-        attachment="top left"
-        targetAttachment="bottom left"
-        constraints={[
-          {
-            to: 'scrollParent',
-          },
-          {
-            to: 'window',
-            pin: ['bottom'],
-          },
-        ]}
-      >
-        <div
-          ref={ref => (this.dropdown = ref)}
-          className={classnames('widget-tooltip', {
-            opened: isToggled,
-          })}
-          tabIndex={-1}
+      <div className="widget-tooltip" tabIndex={-1}>
+        <img
+          src={item.tooltipIconName === 'text' ? iconText : iconHelp}
+          className={'rounded-circle'}
           onClick={this.handleClick}
-        >
-        </div>
+        />
         {isToggled && (
-          <div></div>
+          <TetherComponent
+            attachment="top left"
+            targetAttachment="bottom left"
+            constraints={[
+              {
+                to: 'scrollParent',
+              },
+              {
+                to: 'window',
+                pin: ['bottom'],
+              },
+            ]}
+          >
+            <div>Text</div>
+          </TetherComponent>
         )}
-      </TetherComponent>
+      </div>
     );
   }
 }
 
-
-WidgetTooltip.defaultProps = {
-};
-
 WidgetTooltip.propTypes = {
+  item: PropTypes.any.isRequired,
+  isToggled: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = state => ({
-});
+// const mapStateToProps = state => ({
+// });
 
 export default onClickOutside(WidgetTooltip);
 
