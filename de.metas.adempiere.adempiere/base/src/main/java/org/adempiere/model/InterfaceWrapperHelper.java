ed2e0eafc5984404a48c440ed0684b4ca237d19d
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-09-14 13:23:21 +0300

    #4572 Implement method InterfaceWrapperHelper.isCopying
    #4572 Texts missing in cloned sales quotation
    
    (cherry picked from commit e3f4cfb9f012f728d4bd9c9c11a40fbfa39cc567)

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/model/InterfaceWrapperHelper.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/model/InterfaceWrapperHelper.java
@@ -1712,4 +1712,9 @@ public class InterfaceWrapperHelper
 	{
 		return helpers.isCopy(model);
 	}
+	
+	public static boolean isCopying(final Object model)
+	{
+		return helpers.isCopying(model);
+	}
 }
