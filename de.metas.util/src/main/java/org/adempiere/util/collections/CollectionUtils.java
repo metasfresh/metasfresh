fc060a61a0fd5f10cc031ed2a9ad2a99687dc5cb
Teo Sarca <teo.sarca@metasfresh.com>
2018-09-20 16:49:42 +0300

    CollectionUtils.getAllOrLoad
    https://github.com/metasfresh/metasfresh/issues/4577

--- de.metas.util/src/main/java/org/adempiere/util/collections/CollectionUtils.java
+++ de.metas.util/src/main/java/org/adempiere/util/collections/CollectionUtils.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -14,6 +15,7 @@ import javax.annotation.Nullable;
 import org.adempiere.util.Check;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import lombok.NonNull;
 
@@ -306,4 +308,43 @@ public final class CollectionUtils
 		return element;
 	}
 
+	public static <K, V> Collection<V> getAllOrLoad(
+			@NonNull final Map<K, V> map,
+			@NonNull final Collection<K> keys,
+			@NonNull final Function<Collection<K>, Map<K, V>> valuesLoader)
+	{
+		if (keys.isEmpty())
+		{
+			return ImmutableList.of();
+		}
+
+		//
+		// Fetch from cache what's available
+		final List<V> values = new ArrayList<>(keys.size());
+		final Set<K> keysToLoad = new HashSet<>();
+		for (final K key : ImmutableSet.copyOf(keys))
+		{
+			final V value = map.get(key);
+			if (value == null)
+			{
+				keysToLoad.add(key);
+			}
+			else
+			{
+				values.add(value);
+			}
+		}
+
+		//
+		// Load the missing keys if any
+		if (!keysToLoad.isEmpty())
+		{
+			final Map<K, V> valuesLoaded = valuesLoader.apply(keysToLoad);
+			map.putAll(valuesLoaded); // add loaded values to cache
+			values.addAll(valuesLoaded.values()); // add loaded values to the list we will return
+		}
+
+		//
+		return values;
+	}
 }
