87c4891965f48c1b6ba4a75581f7634a85de55d7
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-09-28 13:19:55 +0300

    #4609 HU sys configs
    https://github.com/metasfresh/metasfresh/issues/4609

--- de.metas.handlingunits.base/src/main/sql/postgresql/system/5502270_sys_gh4609_HUSysConfigs.sql
+++ de.metas.handlingunits.base/src/main/sql/postgresql/system/5502270_sys_gh4609_HUSysConfigs.sql
@@ -28,3 +28,23 @@ INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLe
 INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541241,'S',TO_TIMESTAMP('2018-09-27 11:21:04','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.handlingunits','Y','de.metas.ui.web.handlingunits.HUEditorViewFactory.AlwaysUseSameLayout',TO_TIMESTAMP('2018-09-27 11:21:04','YYYY-MM-DD HH24:MI:SS'),100,'N')
 ;
 
+-- 2018-09-28T12:27:23.495
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Value='',Updated=TO_TIMESTAMP('2018-09-28 12:27:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541241
+;
+
+-- 2018-09-28T13:10:20.560
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Value='N',Updated=TO_TIMESTAMP('2018-09-28 13:10:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541241
+;
+
+-- 2018-09-28T13:10:49.912
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Value='N',Updated=TO_TIMESTAMP('2018-09-28 13:10:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541239
+;
+
+-- 2018-09-28T13:13:56.388
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Value='Y',Updated=TO_TIMESTAMP('2018-09-28 13:13:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541238
+;
+
