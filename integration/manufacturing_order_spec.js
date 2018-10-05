171db6c374328ffbe84dc538463accbe5def2946
Mark Krake <mark.krake@metasfresh.com>
2018-10-05 16:50:45 +0200

    Adjusts the quickaction call for barcode #1995
    https://github.com/metasfresh/metasfresh-webui-frontend/issues/1995

--- integration/manufacturing_order_spec.js
+++ integration/manufacturing_order_spec.js
@@ -6,7 +6,9 @@ describe('Manufacturing order Test', function() {
   it('Test if Barcode widget shows up', function() {
     cy.visit('/window/53009');
 
-    cy.executeQuickAction('ADP_540788');
+    // Start Quickaction 'Issue/ Receipt (barcode)' and check if the barcode field is available
+    cy.executeQuickAction('WEBUI_PP_Order_IssueReceipt_BarcodeLauncher');
+    cy.get('.form-field-Barcode').should('exist');
 
     cy.get('.form-field-Barcode').should('exist');
   });
