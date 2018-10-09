2c64a4898ab2c22c0f9beccb7bfaebd6a19bb4e4
siemiatj <kuba@saskla.cz>
2018-10-09 23:28:28 +0200

    - move zxing implementation to a separate component #1990

--- src/components/widget/BarcodeScanner/BarcodeScanner.js
+++ src/components/widget/BarcodeScanner/BarcodeScanner.js
@@ -1,103 +1,184 @@
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import Quagga from 'quagga';
+
+// export default class BarcodeScanner extends Component {
+//   componentDidMount() {
+//     Quagga.init(
+//       {
+//         inputStream: {
+//           type: 'LiveStream',
+//           constraints: {
+//             width: 1280,
+//             height: 720,
+//             facingMode: 'environment',
+//           },
+//         },
+//         locator: {
+//           patchSize: 'medium',
+//           halfSample: true,
+//         },
+//         numOfWorkers: 4,
+//         frequency: 5,
+//         decoder: {
+//           readers: ['ean_reader'],
+//         },
+//         locate: true,
+//       },
+//       err => {
+//         if (err) {
+//           // eslint-disable-next-line no-console
+//           return console.log(err);
+//         }
+//         Quagga.start();
+//       }
+//     );
+//     Quagga.onDetected(this._onDetected);
+
+//     if (this.props.debug) {
+//       Quagga.onProcessed(this._onProcessed);
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     Quagga.offDetected(this._onDetected);
+//   }
+
+//   _handleStop = () => {
+//     Quagga.offDetected(this._onDetected);
+//     Quagga.stop();
+
+//     this.props.onClose();
+//   };
+
+//   _onDetected = result => {
+//     this._handleStop(true);
+//     this.props.onDetected(result);
+//   };
+
+//   _onProcessed(result) {
+//     const drawingCtx = Quagga.canvas.ctx.overlay;
+//     const drawingCanvas = Quagga.canvas.dom.overlay;
+
+//     if (result) {
+//       if (result.boxes) {
+//         drawingCtx.clearRect(
+//           0,
+//           0,
+//           parseInt(drawingCanvas.getAttribute('width')),
+//           parseInt(drawingCanvas.getAttribute('height'))
+//         );
+//         result.boxes.filter(box => box !== result.box).forEach(box => {
+//           Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+//             color: 'green',
+//             lineWidth: 2,
+//           });
+//         });
+//       }
+
+//       if (result.box) {
+//         Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+//           color: '#00F',
+//           lineWidth: 2,
+//         });
+//       }
+
+//       if (result.codeResult && result.codeResult.code) {
+//         Quagga.ImageDebug.drawPath(
+//           result.line,
+//           { x: 'x', y: 'y' },
+//           drawingCtx,
+//           { color: 'red', lineWidth: 3 }
+//         );
+//       }
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="row scanner-wrapper">
+//         <div id="interactive" className="col-sm-12 viewport scanner-window" />
+//         {/*<video className="col-sm-12 viewport scanner-window" id="video" width="1280" height="720"></video>*/}
+//         <i className="btn-close meta-icon-close-1" onClick={this._handleStop} />
+//       </div>
+//     );
+//   }
+// }
+
+// BarcodeScanner.propTypes = {
+//   onDetected: PropTypes.func.isRequired,
+//   onClose: PropTypes.func.isRequired,
+//   onReset: PropTypes.func.isRequired,
+// };
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Quagga from 'quagga';
+import { BrowserQRCodeReader, BrowserBarcodeReader } from '@zxing/library';
 
 export default class BarcodeScanner extends Component {
-  componentDidMount() {
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          constraints: {
-            width: 1280,
-            height: 720,
-            facingMode: 'environment',
-          },
-        },
-        locator: {
-          patchSize: 'medium',
-          halfSample: true,
-        },
-        numOfWorkers: 4,
-        frequency: 5,
-        decoder: {
-          readers: ['ean_reader'],
-        },
-        locate: true,
-      },
-      err => {
-        if (err) {
-          // eslint-disable-next-line no-console
-          return console.log(err);
-        }
-        Quagga.start();
-      }
-    );
-    Quagga.onDetected(this._onDetected);
+  constructor(props) {
+    super(props);
 
-    if (this.props.debug) {
-      Quagga.onProcessed(this._onProcessed);
-    }
+    this.state = {
+      mode: 'Barcode',
+    };
+
+    this.QrReader = new BrowserQRCodeReader();
+    this.BarcodeReader = new BrowserBarcodeReader();
+  }
+
+  componentDidMount() {
+    this._process();
   }
 
   componentWillUnmount() {
-    Quagga.offDetected(this._onDetected);
+    this._handleStop();
   }
 
+  _process = () => {
+    const reader = this[`${this.state.mode}Reader`];
+
+    reader
+      .decodeFromInputVideoDevice(undefined, 'video')
+      .then(result => this._onDetected(result))
+      .catch(err => console.error(err));
+  };
+
+  _onDetected = result => {
+    this._handleStop();
+    this.props.onDetected(result.text);
+  };
+
   _handleStop = () => {
-    Quagga.offDetected(this._onDetected);
-    Quagga.stop();
+    this.QrReader.stop();
+    this.BarcodeReader.stop();
 
     this.props.onClose();
   };
 
-  _onDetected = result => {
-    this._handleStop(true);
-    this.props.onDetected(result);
+  _changeReader = type => {
+    this.QrReader.reset();
+    this.BarcodeReader.reset();
+
+    this.props.onClose();
+
+    this.setState(
+      {
+        mode: type,
+      },
+      () => this._process
+    );
   };
-
-  _onProcessed(result) {
-    const drawingCtx = Quagga.canvas.ctx.overlay;
-    const drawingCanvas = Quagga.canvas.dom.overlay;
-
-    if (result) {
-      if (result.boxes) {
-        drawingCtx.clearRect(
-          0,
-          0,
-          parseInt(drawingCanvas.getAttribute('width')),
-          parseInt(drawingCanvas.getAttribute('height'))
-        );
-        result.boxes.filter(box => box !== result.box).forEach(box => {
-          Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
-            color: 'green',
-            lineWidth: 2,
-          });
-        });
-      }
-
-      if (result.box) {
-        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
-          color: '#00F',
-          lineWidth: 2,
-        });
-      }
-
-      if (result.codeResult && result.codeResult.code) {
-        Quagga.ImageDebug.drawPath(
-          result.line,
-          { x: 'x', y: 'y' },
-          drawingCtx,
-          { color: 'red', lineWidth: 3 }
-        );
-      }
-    }
-  }
 
   render() {
     return (
       <div className="row scanner-wrapper">
-        <div id="interactive" className="col-sm-12 viewport scanner-window" />
+        <video
+          className="col-sm-12 viewport scanner-window"
+          id="video"
+          width="1280"
+          height="720"
+        />
         <i className="btn-close meta-icon-close-1" onClick={this._handleStop} />
       </div>
     );
@@ -107,5 +188,4 @@ export default class BarcodeScanner extends Component {
 BarcodeScanner.propTypes = {
   onDetected: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
-  onReset: PropTypes.func.isRequired,
 };
--- src/components/widget/BarcodeScanner/BarcodeScannerWidget.js
+++ src/components/widget/BarcodeScanner/BarcodeScannerWidget.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import BarcodeScanner from './BarcodeScanner';
-import { BrowserQRCodeReader, VideoInputDevice } from '@zxing/library';
-import ZXing from '@zxing/library';
 
 function addBarcodeScanner(WrappedComponent) {
   return class BarcodeScannerWidget extends Component {
@@ -10,57 +8,34 @@ function addBarcodeScanner(WrappedComponent) {
 
       this.state = {
         scanning: false,
-        barcodeSelected: null,
+        codeSelected: null,
       };
-
-      this.codeReader = new BrowserQRCodeReader();
     }
 
     scanBarcode = val => {
       this.setState({
         scanning: typeof val !== 'undefined' ? val : !this.state.scanning,
-        barcodeSelected: null,
+        codeSelected: null,
       });
-
-      let firstDeviceId = null;
-
-      this.codeReader.getVideoInputDevices()
-        .then(videoInputDevices => {
-          videoInputDevices.forEach(
-              device => console.log(`${device.label}, ${device.deviceId}`)
-          );
-
-          firstDeviceId = videoInputDevices[0].deviceId;
-
-          this.codeReader.decodeFromInputVideoDevice(firstDeviceId, 'video')
-            .then(result => console.log(result.text))
-            .catch(err => console.error(err));
-        })
-        .catch(err => console.error(err));
     };
 
     onBarcodeDetected = result => {
       this.setState({
-        barcodeSelected: result ? result.codeResult.code : null,
+        codeSelected: result || null,
         scanning: false,
       });
+
+      console.log('DETECTED: ', result);
     };
 
     renderScanner = () => {
       return (
         <div className="row barcode-scanner-widget">
           <div className="col-sm-12">
-{/*            <BarcodeScanner
+            <BarcodeScanner
               onDetected={this.onBarcodeDetected}
               onClose={() => this.scanBarcode(false)}
-              onReset={() => this.scanBarcode(true)}
-            />*/}
-            
-            <div className="row scanner-wrapper">
-              <div id="interactive"  />
-              <video className="col-sm-12 viewport scanner-window" id="video" width="1280" height="720"></video>
-              <i className="btn-close meta-icon-close-1" onClick={this._handleStop} />
-            </div>
+            />
           </div>
         </div>
       );
