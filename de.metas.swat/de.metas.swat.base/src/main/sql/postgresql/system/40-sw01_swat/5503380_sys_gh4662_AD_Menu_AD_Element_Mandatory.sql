f97a8ab28fc3d9549ab0625858e8cdc6a8df0b96
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-10-12 19:18:02 +0300

    #4662 Element in tab, window and menu
    https://github.com/metasfresh/metasfresh/issues/4662

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5503380_sys_gh4662_AD_Menu_AD_Element_Mandatory.sql
@@ -0,0 +1,4 @@
+-- 2018-10-12T19:13:34.328
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-10-12 19:13:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=563264
+;
