2dfb4363e56f5f271b67d27797cd01189f4370f9
teosarca <teo.sarca@metasfresh.com>
2018-10-17 15:39:23 +0300

    hotfix: picking actions (pick HU / pick CU) and selecting source HU not available in original picking terminal anymore
    https://github.com/metasfresh/metasfresh/issues/4609#issuecomment-430610626

--- src/main/java/de/metas/ui/web/handlingunits/HUEditorViewFactoryTemplate.java
+++ src/main/java/de/metas/ui/web/handlingunits/HUEditorViewFactoryTemplate.java
@@ -292,10 +292,7 @@ public abstract class HUEditorViewFactoryTemplate implements IViewFactory
 					.sqlViewBinding(sqlViewBinding)
 					.huReservationService(huReservationService);
 
-			if (!alwaysUseSameLayout())
-			{
-				customizeHUEditorViewRepository(huEditorViewRepositoryBuilder);
-			}
+			customizeHUEditorViewRepository(huEditorViewRepositoryBuilder);
 
 			huEditorViewRepository = huEditorViewRepositoryBuilder.build();
 		}
@@ -325,13 +322,10 @@ public abstract class HUEditorViewFactoryTemplate implements IViewFactory
 					.setHUEditorViewRepository(huEditorViewRepository)
 					.setParameters(request.getParameters());
 
-			if (!alwaysUseSameLayout())
-			{
-				//
-				// Call view customizers
-				getViewCustomizers(referencingTableName).forEach(viewCustomizer -> viewCustomizer.beforeCreate(huViewBuilder));
-				customizeHUEditorView(huViewBuilder);
-			}
+			//
+			// Call view customizers
+			getViewCustomizers(referencingTableName).forEach(viewCustomizer -> viewCustomizer.beforeCreate(huViewBuilder));
+			customizeHUEditorView(huViewBuilder);
 
 			return huViewBuilder.build();
 		}
