5285787b200b93a6ff57c2f92bbbeaa13539b7d8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-10-17 19:56:05 +0200

    #4621-app - Support forum-datenaustausch.ch XML invoice
    * solve SQL problem
    
    https://github.com/metasfresh/metasfresh/issues/4621

--- de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5503400_sys_gh4621-app_minor_cleanups.sql
+++ de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5503400_sys_gh4621-app_minor_cleanups.sql
@@ -6,5 +6,7 @@
 UPDATE AD_Column SET EntityType='D', TechnicalNote='I think the column has IsCalculculated because generally each location has its own GLN, so there is no point in coyping it.',Updated=TO_TIMESTAMP('2018-10-14 17:34:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=548323
 ;
 
+
 -- cleanup
-DELETE FROM AD_Process WHERE AD_Process_ID=53070; -- 'PrepareMigrationScripts'
+DELETE FROM AD_Menu WHERE AD_Process_ID=53070; -- 'PrepareMigrationScripts'
+DELETE FROM AD_Process WHERE AD_Process_ID=53070; 
