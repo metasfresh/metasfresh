ef6fc1cc52dd22d63eb81858537c044def2a880c
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-10-19 07:48:01 +0200

    #4678-app - Deactivate legacy "Order overview (internal)" window

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5500960_sys_gh4678-app_deactivate_order_overview_table_and_window.sql
@@ -0,0 +1,15 @@
+-- 2018-09-07T14:26:04.845
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Window SET Help='this window can cause huge performance problems', IsActive='N',Updated=TO_TIMESTAMP('2018-09-07 14:26:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540089
+;
+
+-- 2018-09-07T14:26:40.187
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET Help='this view can cause a performance meltdown',Updated=TO_TIMESTAMP('2018-09-07 14:26:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=540261
+;
+
+-- 2018-09-07T14:26:47.548
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET IsActive='N',Updated=TO_TIMESTAMP('2018-09-07 14:26:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=540261
+;
+
