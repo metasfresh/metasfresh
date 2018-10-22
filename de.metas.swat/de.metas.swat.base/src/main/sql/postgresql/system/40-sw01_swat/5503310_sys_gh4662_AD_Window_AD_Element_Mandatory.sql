4c4faa81855801960ae160faff11ac93a9c75e73
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-10-22 18:11:14 +0300

    #4662 delete scripts that could cause problems
    * These scripts could cause problems if we work on a different database (e.g. a customer db with other IDs)
    * These will be replaced with a ore general approach that will use the IDs from the application server
    
    https://github.com/metasfresh/metasfresh/issues/4662

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5503310_sys_gh4662_AD_Window_AD_Element_Mandatory.sql
+++
@@ -1,4 +0,0 @@
--- 2018-10-12T18:52:56.588
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2018-10-12 18:52:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=563263
-;
