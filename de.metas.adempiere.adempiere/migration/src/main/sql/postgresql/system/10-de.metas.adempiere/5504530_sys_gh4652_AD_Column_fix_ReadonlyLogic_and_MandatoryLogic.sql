5968ba9ab6bb8050c802b82882e88a7cc254025f
teosarca <teo.sarca@metasfresh.com>
2018-10-23 16:23:34 +0300

    AD_Column: fix MandatoryLogic and ReadonlyLogic
    https://github.com/metasfresh/metasfresh/issues/4652

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5504530_sys_gh4652_AD_Column_fix_ReadonlyLogic_and_MandatoryLogic.sql
@@ -0,0 +1,10 @@
+-- 2018-10-23T16:03:24.058
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET MandatoryLogic='@AD_Element_ID/0@>0',Updated=TO_TIMESTAMP('2018-10-23 16:03:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=116
+;
+
+-- 2018-10-23T16:03:44.579
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET MandatoryLogic='@AD_Element_ID/0@>0',Updated=TO_TIMESTAMP('2018-10-23 16:03:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=111
+;
+
