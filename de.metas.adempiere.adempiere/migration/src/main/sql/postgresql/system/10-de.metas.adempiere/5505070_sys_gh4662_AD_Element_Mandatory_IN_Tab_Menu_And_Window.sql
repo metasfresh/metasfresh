9e1f06900b485f9a44fc1da67c0aecbd34d3f110
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-10-31 13:18:10 +0200

    #4662 split ddl changes
    https://github.com/metasfresh/metasfresh/issues/4662

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5505070_sys_gh4662_AD_Element_Mandatory_IN_Tab_Menu_And_Window.sql
@@ -0,0 +1,34 @@
+
+-- 2018-10-30T16:48:07.419
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_tab','AD_Element_ID','NUMERIC(10)',null,null)
+;
+
+-- 2018-10-30T16:48:07.419
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_tab','AD_Element_ID',null,'NOT NULL',null)
+;
+
+
+-- 2018-10-30T16:48:07.455
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_window','AD_Element_ID','NUMERIC(10)',null,null)
+;
+
+-- 2018-10-30T16:48:07.457
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_window','AD_Element_ID',null,'NOT NULL',null)
+;
+
+
+
+-- 2018-10-30T16:48:07.484
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_menu','AD_Element_ID','NUMERIC(10)',null,null)
+;
+
+-- 2018-10-30T16:48:07.487
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('ad_menu','AD_Element_ID',null,'NOT NULL',null)
+;
+
