584b99a51da45ba25c56407d5e49ff35f9ad083b
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-11-08 17:24:00 +0200

    #4664 don't mark the tab as trl tab
    https://github.com/metasfresh/metasfresh/issues/4664

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5506040_sys_gh4664_AD_Element_Trl_Not_TRLTAB.sql
@@ -0,0 +1,5 @@
+
+-- 2018-11-08T16:22:34.523
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET IsTranslationTab='N',Updated=TO_TIMESTAMP('2018-11-08 16:22:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541365
+;
