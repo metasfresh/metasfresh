95b1029333a76a0fedd8f3ba8dc28218a677f312
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-11-19 16:05:52 +0200

    #4762 only display this field in advanced edit
    https://github.com/metasfresh/metasfresh/issues/4762

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5506700_sys_gh4762_Display_PaymentTerm_PurchaseOrder_AdvancedOnly.sql
@@ -0,0 +1,5 @@
+-- 2018-11-19T16:01:14.629
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='Y',Updated=TO_TIMESTAMP('2018-11-19 16:01:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=554239
+;
+
