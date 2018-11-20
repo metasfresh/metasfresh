d9077d6f2f373e04deec343ab93359b9a31929bc
teosarca <teo.sarca@metasfresh.com>
2018-11-20 16:12:42 +0200

    fail if no IAttributeValueGenerator classes found
    https://github.com/metasfresh/metasfresh/issues/4773

--- de.metas.handlingunits.base/src/test/java/de/metas/handlingunits/attribute/storage/impl/HUAttributeStorage_generateInitialAttributes_Integration_Test.java
+++ de.metas.handlingunits.base/src/test/java/de/metas/handlingunits/attribute/storage/impl/HUAttributeStorage_generateInitialAttributes_Integration_Test.java
@@ -76,6 +76,11 @@ public class HUAttributeStorage_generateInitialAttributes_Integration_Test exten
 				.setScanners(new SubTypesScanner()));
 
 		final Set<Class<? extends IAttributeValueGenerator>> attributeValueGeneratorClassnames = reflections.getSubTypesOf(IAttributeValueGenerator.class);
+		if (attributeValueGeneratorClassnames.isEmpty())
+		{
+			throw new RuntimeException("No classes found. Might be because for some reason Reflections does not work correctly with maven surefire plugin."
+					+ "\n See https://github.com/metasfresh/metasfresh/issues/4773.");
+		}
 
 		System.out.println("Found " + attributeValueGeneratorClassnames.size() + " classes in " + stopwatch.toString());
 
