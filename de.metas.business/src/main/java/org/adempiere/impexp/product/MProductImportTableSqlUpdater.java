8fc81b604526a2e7b5609318a8934abdac4d9339
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-11-23 11:14:37 +0200

    #4782 Do not set to error when we can not find the manufacturer
    #4782 Various fixes to import processes

--- de.metas.business/src/main/java/org/adempiere/impexp/product/MProductImportTableSqlUpdater.java
+++ de.metas.business/src/main/java/org/adempiere/impexp/product/MProductImportTableSqlUpdater.java
@@ -145,22 +145,13 @@ public class MProductImportTableSqlUpdater
 	private void dbUpdateManufacturers(@NonNull final String whereClause)
 	{
 		StringBuilder sql;
-		int no;
 		sql = new StringBuilder("UPDATE ")
 				.append(targetTableName + " i ")
 				.append(" SET Manufacturer_ID=(SELECT C_BPartner_ID FROM C_BPartner p")
-				.append(" WHERE i.ProductManufacturer ilike '%'||p.companyname||'%' AND i.AD_Client_ID=p.AD_Client_ID) ")
+				.append(" WHERE i.ProductManufacturer ilike '%'||p.companyname||'%' AND i.AD_Client_ID=p.AD_Client_ID LIMIT 1) ")
 				.append("WHERE Manufacturer_ID IS NULL")
 				.append(" AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
-		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
-
-		sql = new StringBuilder("UPDATE ")
-				.append(targetTableName)
-				.append(" SET " + COLUMNNAME_I_IsImported + "='E', " + COLUMNNAME_I_ErrorMsg + "=" + COLUMNNAME_I_ErrorMsg + "||'ERR=Invalid ProductManufacturer,' ")
-				.append("WHERE Manufacturer_ID IS NULL AND ProductManufacturer IS NOT NULL")
-				.append(" AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
-		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
-		logger.warn("Invalid BPartner={}", no);
+		DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
 	}
 
 	private void dbUpdateProducts(@NonNull final String whereClause)
