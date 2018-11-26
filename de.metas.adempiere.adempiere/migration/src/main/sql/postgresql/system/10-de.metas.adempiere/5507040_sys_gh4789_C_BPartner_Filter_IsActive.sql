188699c10c0ff9c036672611c31315b5859664a1
metas-rc <ruxandra.craciunescu@metasfresh.com>
2018-11-26 12:52:58 +0200

    #4789 IsActive filter in C_BPartner
    https://github.com/metasfresh/metasfresh/issues/4789

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5507040_sys_gh4789_C_BPartner_Filter_IsActive.sql
@@ -0,0 +1,7 @@
+
+
+-- 2018-11-26T12:45:10.428
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2018-11-26 12:45:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2896
+;
+
