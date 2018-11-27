c684df8be7455feff163c80a67921339dd812e14
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-11-27 15:12:15 +0100

    Refund contract - add further options
    fix problems in subscription conditions test
    
    https://github.com/metasfresh/metasfresh/issues/4475

--- integration/contracts/flatrate_conditions_subscription_spec.js
+++ integration/contracts/flatrate_conditions_subscription_spec.js
@@ -3,36 +3,30 @@
 import { createAndCompleteTransition } from '../../support/utils/contract';
 
 describe('Create subscription flatrate conditions for three 1-year-periods', function() {
-    // before(function() {
-    //     // login before each test and open the flatrate conditions window
-    //     cy.loginByForm();
-    // });
-    
-    // it('Create subscription flatrate conditions for three 1-year-periods', function () {
+    before(function() {
+        // login before each test and open the flatrate conditions window
+        cy.loginByForm();
+    });
+
+    it('Create subscription flatrate conditions for three 1-year-periods', function () 
+    {
         const timestamp = new Date().getTime(); // used in the document names, for ordering
 
-    const year3TransitionName = `${timestamp} Year 3-dontExtend (Cypress Test)`;
-    const year3ConditionsName = `${timestamp} Year 3 (Cypress Test)`;
-    it('Create transition and conditions for 3rd year', function () {
-        cy.loginByForm();
+        const year3TransitionName = `${timestamp} Year 3-dontExtend (Cypress Test)`;
+        const year3ConditionsName = `${timestamp} Year 3 (Cypress Test)`;
         createAndCompleteTransition(year3TransitionName, null, null);
         createAndCompleteConditions(year3ConditionsName, year3TransitionName);
-    });
-      
-    const year2To3TransitionName = `${timestamp} Year 2-3 (Cypress Test)`;
-    const year2ConditionsName = `${timestamp} Year 2 (Cypress Test)`;
-    it('Create transition and conditions for 2nd year', function () {
+        
+        const year2To3TransitionName = `${timestamp} Year 2-3 (Cypress Test)`;
+        const year2ConditionsName = `${timestamp} Year 2 (Cypress Test)`;
         createAndCompleteTransition(year2To3TransitionName, 'Extend contract for all periods', year3ConditionsName);
         createAndCompleteConditions(year2ConditionsName, year2To3TransitionName);
-    });
 
-    const year1To2TransitionName = `${timestamp} Year 1-2 (Cypress Test)`;
-    const year1ConditionsName = `${timestamp} Year 1 (Cypress Test)`;
-    it('Create transition and conditions for 1st year', function () {
+        const year1To2TransitionName = `${timestamp} Year 1-2 (Cypress Test)`;
+        const year1ConditionsName = `${timestamp} Year 1 (Cypress Test)`;
         createAndCompleteTransition(year1To2TransitionName, 'Extend contract for all periods', year2ConditionsName);
         createAndCompleteConditions(year1ConditionsName, year1To2TransitionName);
     });
-    // });
 });
 
 
