6be131056802679d9e514f28d3c708a17df313a4
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-11-27 14:32:25 +0100

    Refund contract - add further options
    tweak the test names etc
    
    https://github.com/metasfresh/metasfresh/issues/4475

--- integration/contracts/flatrate_conditions_refund_tiered_percent_spec.js
+++ integration/contracts/flatrate_conditions_refund_tiered_percent_spec.js
@@ -2,7 +2,7 @@
 
 import { createAndCompleteTransition, createAndCompleteRefundPercentConditions } from '../../support/utils/contract';
 
-describe('Create refund flatrate conditions', function() {
+describe('Create tiered percent-based refund conditions', function() {
     before(function() {
         // login before each test and open the flatrate conditions window
         cy.loginByForm();
@@ -11,7 +11,7 @@ describe('Create refund flatrate conditions', function() {
     it('Create tiered percent-based refund conditions', function () {
         const timestamp = new Date().getTime(); // used in the document names, for ordering
 
-        const transitionName = `${timestamp} Refund contract transisiton (Cypress Test)`;
+        const transitionName = `${timestamp} tiered percent-based refund conditions (Cypress Test)`;
         createAndCompleteTransition(transitionName, null, null);
 
         const conditionsName = `${timestamp} tiered percent-based refund conditions (Cypress Test)`;
--- integration/contracts/flatrate_conditions_subscription_spec.js
+++ integration/contracts/flatrate_conditions_subscription_spec.js
@@ -1,37 +1,43 @@
 /// <reference types="Cypress" />
 
-import createAndCompleteTransition from '../../support/utils/contract';
+import { createAndCompleteTransition } from '../../support/utils/contract';
 
 describe('Create subscription flatrate conditions for three 1-year-periods', function() {
-    before(function() {
-        // login before each test and open the flatrate conditions window
-        cy.loginByForm();
-    });
+    // before(function() {
+    //     // login before each test and open the flatrate conditions window
+    //     cy.loginByForm();
+    // });
     
-    const timestamp = new Date().getTime(); // used in the document names, for ordering
+    // it('Create subscription flatrate conditions for three 1-year-periods', function () {
+        const timestamp = new Date().getTime(); // used in the document names, for ordering
 
     const year3TransitionName = `${timestamp} Year 3-dontExtend (Cypress Test)`;
-    createAndCompleteTransition(year3TransitionName, null, null);
-
     const year3ConditionsName = `${timestamp} Year 3 (Cypress Test)`;
-    createAndCompleteConditions(year3ConditionsName, year3TransitionName);
-    
+    it('Create transition and conditions for 3rd year', function () {
+        cy.loginByForm();
+        createAndCompleteTransition(year3TransitionName, null, null);
+        createAndCompleteConditions(year3ConditionsName, year3TransitionName);
+    });
+      
     const year2To3TransitionName = `${timestamp} Year 2-3 (Cypress Test)`;
-    createAndCompleteTransition(year2To3TransitionName, 'Extend contract for all periods', year3ConditionsName);
-
     const year2ConditionsName = `${timestamp} Year 2 (Cypress Test)`;
-    createAndCompleteConditions(year2ConditionsName, year2To3TransitionName);
+    it('Create transition and conditions for 2nd year', function () {
+        createAndCompleteTransition(year2To3TransitionName, 'Extend contract for all periods', year3ConditionsName);
+        createAndCompleteConditions(year2ConditionsName, year2To3TransitionName);
+    });
 
     const year1To2TransitionName = `${timestamp} Year 1-2 (Cypress Test)`;
-    createAndCompleteTransition(year1To2TransitionName, 'Extend contract for all periods', year2ConditionsName);
-
     const year1ConditionsName = `${timestamp} Year 1 (Cypress Test)`;
-    createAndCompleteConditions(year1ConditionsName, year1To2TransitionName);
+    it('Create transition and conditions for 1st year', function () {
+        createAndCompleteTransition(year1To2TransitionName, 'Extend contract for all periods', year2ConditionsName);
+        createAndCompleteConditions(year1ConditionsName, year1To2TransitionName);
+    });
+    // });
 });
 
 
-function createAndCompleteConditions(conditionsName, transitionName) {
-    it(`Create and complete conditions record ${conditionsName}`, function () {
+function createAndCompleteConditions(conditionsName, transitionName) 
+{
         cy.visit('/window/540113/NEW');
         cy.writeIntoStringField('Name', conditionsName);
         cy.writeIntoLookupListField('Type_Conditions', 'A', 'Abonnement');
@@ -39,5 +45,4 @@ function createAndCompleteConditions(conditionsName, transitionName) {
         cy.writeIntoLookupListField('C_Flatrate_Transition_ID', transitionName, transitionName);
 
         cy.processDocument('Complete', 'Completed');
-    });
 }
