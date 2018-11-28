7a172e9f31a46d948fcb73822af6833add68891a
teosarca <teo.sarca@metasfresh.com>
2018-11-28 06:11:39 +0200

    rename
    https://github.com/metasfresh/metasfresh/issues/478

--- de.metas.vertical.pharma.vendor.gateway.msv3/src/main/java/de/metas/vertical/pharma/vendor/gateway/msv3/common/Msv3ClientMultiException.java
+++ de.metas.vertical.pharma.vendor.gateway.msv3/src/main/java/de/metas/vertical/pharma/vendor/gateway/msv3/common/Msv3ClientMultiException.java
@@ -25,17 +25,18 @@ import lombok.NonNull;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
+
 public class Msv3ClientMultiException extends AdempiereException implements AvailabilityRequestException
 {
 	public static Msv3ClientMultiException createAllItemsSameThrowable(
@@ -43,6 +44,7 @@ public class Msv3ClientMultiException extends AdempiereException implements Avai
 			@NonNull final Throwable throwable)
 	{
 		final Map<AvailabilityRequestItem, Throwable> allItemsWithSameThrowable = Maps.toMap(items, requestItem -> throwable);
+
 		return new Msv3ClientMultiException(allItemsWithSameThrowable, throwable);
 	}
 
@@ -53,11 +55,14 @@ public class Msv3ClientMultiException extends AdempiereException implements Avai
 		final Msv3ClientException msv3ClientException = Msv3ClientException.builder()
 				.msv3FaultInfo(msv3FaultInfo)
 				.build();
+
 		final Map<AvailabilityRequestItem, Throwable> allItemsWithSameThrowable = Maps.toMap(items, requestItem -> msv3ClientException);
+
 		return new Msv3ClientMultiException(allItemsWithSameThrowable, msv3ClientException);
 	}
 
 	private static final long serialVersionUID = -8058915938494697758L;
+
 	@Getter
 	private final Map<AvailabilityRequestItem, Throwable> requestItem2Exception;
 
