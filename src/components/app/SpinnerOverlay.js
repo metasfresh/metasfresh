595ca8200e5885bc6488368737a1a85fd6025a28
siemiatj <kuba@saskla.cz>
2018-11-30 15:24:17 +0100

    - update import after switching to `uuid` #2080
    # Conflicts:
    #	src/components/app/SpinnerOverlay.js

--- src/components/app/SpinnerOverlay.js
+++ src/components/app/SpinnerOverlay.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { v4 as uuid } from 'uuid';
+import uuid from 'uuid/v4';
 
 import { showSpinner, hideSpinner } from '../../actions/WindowActions';
 
