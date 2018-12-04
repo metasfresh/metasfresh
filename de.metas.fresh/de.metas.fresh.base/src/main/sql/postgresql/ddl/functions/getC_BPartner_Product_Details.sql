77ca5e3de48c9be0d4f0d9788399882d3b3b33ae
Cristina Ghita <cristina.ghita@metasfresh.com>
2018-12-04 16:35:23 +0200

    #4795 Retrieve pruct partner details
    #4795 Product No. of customer also with ASI

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/getC_BPartner_Product_Details.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/getC_BPartner_Product_Details.sql
@@ -1,22 +1,55 @@
--- Function: de_metas_endcustomer_fresh_reports.getC_BPartner_Product_Details(numeric, numeric, character varying)
-
--- DROP FUNCTION de_metas_endcustomer_fresh_reports.getC_BPartner_Product_Details(numeric, numeric, character varying);
-
-CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.getC_BPartner_Product_Details(IN M_Product_id numeric, IN C_BPartner_ID numeric, IN attributes text)
-  RETURNS TABLE(ProductNo character varying, ProductName character varying, C_BPartner_ID numeric) AS
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.getC_BPartner_Product_Details(IN M_Product_id numeric, IN C_BPartner_ID numeric, IN M_AttributeSetInstance_ID numeric)
+  RETURNS TABLE(M_Product_ID numeric, ProductNo character varying, ProductName character varying, attributes text[], C_BPartner_ID numeric) AS
 $BODY$
-	SELECT ProductNo,ProductName, C_BPartner_ID
-	FROM
-		(
-		   SELECT bpp.ProductNo, bpp.ProductName, bpp.M_Product_ID, bpp.C_BPartner_ID, bpp.M_AttributeSetInstance_ID,seqno,
-				String_agg ( ratt.ai_value, ', ' ORDER BY length(ratt.ai_value)) as bppAttributes
-		   FROM C_BPartner_Product bpp 
-		   LEFT OUTER JOIN Report.fresh_Attributes ratt ON bpp.M_AttributeSetInstance_ID = ratt.M_AttributeSetInstance_ID
-		   WHERE bpp.isActive = 'Y'
-		   GROUP BY bpp.ProductNo, bpp.ProductName, bpp.M_Product_ID, bpp.C_BPartner_ID, bpp.M_AttributeSetInstance_ID, seqno
-		   ORDER by bpp.M_AttributeSetInstance_ID DESC, seqno
-		) bpp
-WHERE bpp.M_Product_ID = $1 AND bpp.C_BPartner_ID = $2 AND (bpp.bppAttributes = $3 OR bpp.bppAttributes is null)
-Limit 1
+	SELECT M_Product_ID, ProductNo, ProductName, Attributes, C_BPartner_ID
+FROM
+  (
+    SELECT
+      array_agg(bpp.AttValue) as paramAttributes,
+      array_agg(bpp.Value)    as Attributes,
+      ProductNo,
+      ProductName,
+      M_Product_ID,
+      bpp.C_BPartner_ID
+    FROM
+      (
+        SELECT
+          ai1.M_attribute_ID,
+          bpp.ProductNo,
+          bpp.ProductName,
+          bpp.M_Product_ID,
+          bpp.C_BPartner_ID,
+          bpp.M_AttributeSetInstance_ID,
+          seqno,
+          ai1.M_attribute_ID || '_' ||
+          coalesce(ai1.m_attributevalue_id :: VARCHAR, ai1.value, ai1.valuenumber :: VARCHAR, ai1.valuedate :: VARCHAR,
+                   '') as value,
+          att.M_attribute_ID || '_' ||
+          coalesce(att.m_attributevalue_id :: VARCHAR, att.value, att.valuenumber :: VARCHAR, att.valuedate :: VARCHAR,
+                   '') as attValue
+        FROM C_BPartner_Product bpp
+          LEFT JOIN M_AttributeSetInstance asi1 on asi1.M_AttributeSetInstance_ID = bpp.M_AttributeSetInstance_ID
+          LEFT JOIN M_AttributeInstance ai1 ON ai1.M_AttributeSetInstance_ID = asi1.M_AttributeSetInstance_ID
+          LEFT JOIN
+          (
+            SELECT
+              ai2.M_attribute_ID,
+              ai2.value,
+              ai2.m_attributevalue_id,
+              ai2.valuenumber,
+              ai2.valuedate
+            FROM M_AttributeSetInstance asi2
+              INNER JOIN M_AttributeInstance ai2 ON ai2.M_AttributeSetInstance_ID = asi2.M_AttributeSetInstance_ID
+            WHERE asi2.M_AttributeSetInstance_ID = $3
+          ) as att on att.m_attribute_id = ai1.m_attribute_id OR ai1.m_attribute_id is null
+        WHERE bpp.isActive = 'Y'
+        ORDER by bpp.M_AttributeSetInstance_ID desc, seqno
+      ) bpp
+    WHERE bpp.M_Product_ID = $1 AND bpp.C_BPartner_ID = $2
+    GROUP by ProductNo, ProductName, M_Product_ID, bpp.C_BPartner_ID
+  ) sub
+where sub.paramAttributes @> sub.Attributes or sub.Attributes = ARRAY[NULL]
+order by Attributes
+limit 1
 $BODY$
 LANGUAGE sql STABLE;
