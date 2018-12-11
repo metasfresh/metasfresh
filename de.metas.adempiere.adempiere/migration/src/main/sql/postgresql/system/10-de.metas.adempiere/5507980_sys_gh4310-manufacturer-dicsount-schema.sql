b7da02d490d1a03974e34835bba07253304288ab
Mark Krake <mark.krake@metasfresh.com>
2018-12-11 13:18:26 +0100

    Adjustment to Table Window Reference #4310
    https://github.com/metasfresh/metasfresh/issues/4310

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5507980_sys_gh4310-manufacturer-dicsount-schema.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5507980_sys_gh4310-manufacturer-dicsount-schema.sql
@@ -1,7 +1,7 @@
 -- 2018-12-11T13:08:08.481
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Table SET AD_Window_ID=540433,Updated=TO_TIMESTAMP('2018-12-11 13:08:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=475
-;
+--UPDATE AD_Table SET AD_Window_ID=540433,Updated=TO_TIMESTAMP('2018-12-11 13:08:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=475
+--;
 
 -- 2018-12-11T13:08:54.794
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
