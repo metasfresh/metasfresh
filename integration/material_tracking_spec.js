867772e11574259d7eac2c16eef71887defc8614
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-12-17 11:25:53 +0100

    Fix specs in cypress/integration/contracts

--- integration/material_tracking_spec.js
+++ integration/material_tracking_spec.js
@@ -18,24 +18,24 @@ describe('material tracking test', function() {
         cy.writeIntoStringField('ValidFrom','01/11/2018{enter}');
         cy.writeIntoStringField('ValidTo','01/11/2019{enter}');
 
-        cy.writeIntoLookupListField('M_Product_Scrap_ID','P002749','P002749_Erdbesatz');
-        cy.writeIntoLookupListField('C_UOM_Scrap_ID', 'Kilo', 'Kilogramm');
+        cy.writeIntoLookupField('M_Product_Scrap_ID','P002749','P002749_Erdbesatz');
+        cy.writeIntoLookupField('C_UOM_Scrap_ID', 'Kilo', 'Kilogramm');
 
-        cy.writeIntoLookupListField('M_Product_ProcessingFee_ID', 'P002750', 'P002750_Futterkarotten');
+        cy.writeIntoLookupField('M_Product_ProcessingFee_ID', 'P002750', 'P002750_Futterkarotten');
 
-        cy.writeIntoLookupListField('M_Product_Witholding_ID', 'P002751', 'P002751_Akonto (Einbehalt)');
+        cy.writeIntoLookupField('M_Product_Witholding_ID', 'P002751', 'P002751_Akonto (Einbehalt)');
 
-        cy.writeIntoLookupListField('M_Product_RegularPPOrder_ID', 'P002752', 'P002752_Produkt für Auslagerung');
+        cy.writeIntoLookupField('M_Product_RegularPPOrder_ID', 'P002752', 'P002752_Produkt für Auslagerung');
         cy.pressDoneButton();
         
         cy.visit('/window/540226/NEW');
 
         cy.writeIntoStringField('Lot', lot);
 
-        cy.writeIntoLookupListField('M_Product_ID', 'Convenience', 'Convenience Salat 250g_P002737');
-        cy.writeIntoLookupListField('C_BPartner_ID', 'Test Liefer', 'Test Lieferant 1_G0002');
+        cy.writeIntoLookupField('M_Product_ID', 'Convenience', 'Convenience Salat 250g_P002737');
+        cy.writeIntoLookupField('C_BPartner_ID', 'Test Liefer', 'Test Lieferant 1_G0002');
 
-        cy.writeIntoLookupListField('M_QualityInsp_LagerKonf_Version_ID', lagerKonfName, lagerKonfName);
+        cy.writeIntoLookupField('M_QualityInsp_LagerKonf_Version_ID', lagerKonfName, lagerKonfName);
     });
 
 });
