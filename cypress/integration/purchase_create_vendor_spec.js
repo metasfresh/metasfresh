5cf696e32703d87a456a5bfc84584118e4a61dd2
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2018-12-19 07:43:17 +0100

    Add builder stuff to cypress-test business partners in metasfresh
    Many thanks to https://medium.com/@axelhadfeg/builder-pattern-using-javascript-and-es6-ec1539182e24 by @axelerate

--- cypress/integration/purchase_create_vendor_spec.js
+++ cypress/integration/purchase_create_vendor_spec.js
@@ -1,3 +1,7 @@
+/// <reference types="Cypress" />
+
+import { BPartner, BPartnerLocation, BPartnerContact } from '../support/utils/bPartner';
+
 describe('purchase order Test', function() {
   before(function() {
     // login before each test
@@ -5,50 +9,29 @@ describe('purchase order Test', function() {
   });
 
   const timestamp = new Date().getTime(); // used in the document names, for ordering
-  const vendorValue = `${timestamp} (Cypress Test)`;
+  const vendorName = `${timestamp} (Cypress Test)`;
 
   it('Create a vendor with two contacts', function() {
-    cy.visit('/window/123/NEW');
-    // cy.writeIntoStringField('Value', `{selectall}{backspace}${vendorValue}`);
-    cy.writeIntoStringField('CompanyName', vendorValue);
-    cy.writeIntoStringField('Name2', 'CompanyName');
-
-    cy.selectTab('Vendor');
-    cy.selectSingleTabRow();
-
-    cy.openAdvancedEdit();
-    cy.clickOnCheckBox('IsVendor');
-    cy.pressDoneButton();
-
-    // note that the partner needs an address to be eligible in the purchase order without causing an error
-    // TODO: Use proper tab name when rolled out - Kuba
-    cy.selectTab('C_BPartner_Location');
-    cy.pressAddNewButton();
-    cy.writeIntoStringField('Name', 'Address1');
-    
-    cy.editAddress('C_Location_ID', function a() {
-        cy.writeIntoStringField('City', 'Cologne')
-        cy.writeIntoLookupListField('C_Country_ID', 'Deu', 'Deutschland');
-        // cy.get('.panel-modal-header').click();
-    });
-    cy.get('.form-field-Address')
-        .should('contain', 'Cologne');
-    cy.pressDoneButton();  
-
-      cy.selectTab('AD_User');
-      cy.pressAddNewButton();
-      cy.writeIntoStringField('Firstname', 'Default');
-      cy.writeIntoStringField('Lastname', 'Contact');
-      cy.clickOnCheckBox('IsDefaultContact');
-      cy.pressDoneButton();
-
-      cy.selectTab('AD_User');
-      cy.pressAddNewButton();
-      cy.writeIntoStringField('Firstname', 'Secondary');
-      cy.writeIntoStringField('Lastname', 'Contact');
-      cy.pressDoneButton();
-
-      cy.get('table tbody tr')
-        .should('have.length', 2);
+    new BPartner
+      .builder(vendorName)
+      .vendor(true)
+      .location(new BPartnerLocation
+        .builder('Address1')
+        .city('Cologne')
+        .country('Deutschland')
+        .build())
+      .contact(new BPartnerContact
+        .builder()
+        .firstName('Default')
+        .lastName('Contact')
+        .defaultContact(true)
+        .build())
+        .contact(new BPartnerContact
+          .builder()
+          .firstName('Secondary')
+          .lastName('Contact')
+          .build())
+      .build()
+      .apply();
   });
 });
