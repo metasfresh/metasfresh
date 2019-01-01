db8d0916f5641ad0962cadc9f567e19e3ca1f52f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-01-01 20:19:11 +0100

    add support for debug and no-exit

--- run_cypress.sh
+++ run_cypress.sh
@@ -7,6 +7,8 @@ ws_url=${WS_URL:-http://localhost:8080/stomp}
 username=${USERNAME:-metasfresh}
 password=${PASSWORD:-metasfresh}
 cypress_record_key=${RECORD_KEY:-NOT_SET}
+no_exit=${NO_EXIT:-NOT_SET}
+debug=${DEBUG:-NOT_SET}
 
 echo "*************************************************************"
 echo "Display the variable values we run with"
@@ -18,13 +20,32 @@ echo "PLUGIN_API_URL=$plugin_api_url"
 echo "WS_URL=$ws_url"
 echo "USERNAME=$username"
 echo "PASSWORD=***"
-
 if [[ $cypress_record_key = "NOT_SET" ]]
 then
     echo "RECORD_KEY is not set"
+    record_param=""
 else
     echo "RECORD_KEY=***"
+    record_param="--record --key $cypress_record_key"
 fi
+if [[ no_exit = "NOT_SET" ]]
+then
+    echo "NO_EXIT is not set"
+    noexit_param=""
+else
+    echo "NO_EXIT is set (=$no_exit)"
+    noexit_param="--no-exit"
+fi
+if [[ debug = "NOT_SET" ]]
+then
+    echo "DEBUG is not set"
+    debug_param=""
+else
+    echo "DEBUG is set (=$debug)"
+    debug_param="DEBUG=cypress:*"
+fi
+
+
 cd /e2e
 
 # write the config file used by cypress to acces the rest-API etc
@@ -36,11 +57,4 @@ echo "  username: '${username}'," >> cypress/config.js
 echo "  password: '${password}'," >> cypress/config.js
 echo "};" >> cypress/config.js
 
-if [[ $cypress_record_key = "NOT_SET" ]]
-then
-    record_param=""
-else
-    record_param="--record --key $cypress_record_key"
-fi
-
-CYPRESS_baseUrl=$frontend_url node_modules/.bin/cypress run $record_param #|| sleep 1h #if the cypress command fails, keep running so that we can inspect the container
+$debug_param CYPRESS_baseUrl=$frontend_url node_modules/.bin/cypress run $record_param $noexit_param
