c352b1bf2cfc95dacbc9c5527025b7ee00b5741c
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-01-02 17:14:49 +0100

    sync all contract specs - add contracted vendor and discount schema
    also
    * update to cypress 3.1.4
    * add discountschema "DSL"
    * experiment with taking screenshots
    * maybe improve commands (executeDocAction)

--- cypress/integration/contracts/flatrate_conditions_refund_accumulated_amount_spec.js
+++ cypress/integration/contracts/flatrate_conditions_refund_accumulated_amount_spec.js
@@ -1,11 +1,13 @@
 /// <reference types="Cypress" />
 
 import { createAndCompleteTransition, createAndCompleteRefundAmountConditions } from '../../support/utils/contract';
+import { BPartner, BPartnerLocation } from '../../support/utils/bpartner';
+import { DiscountSchema, DiscountBreak } from '../../support/utils/discountschema';
 
 describe('Create accumulated amount-based refund conditions', function() {
     before(function() {
         // login before each test and open the flatrate conditions window
-        cy.loginByForm();
+        cy.loginByForm()
     });
     
     it('Create accumulated amount-based refund conditions', function () {
@@ -13,9 +15,42 @@ describe('Create accumulated amount-based refund conditions', function() {
 
         const transitionName = `${timestamp} accumulated amount-based refund conditions (Cypress Test)`;
         createAndCompleteTransition(transitionName, null, null);
+        cy.screenshot();
 
         const conditionsName = `${timestamp} accumulated amount-based refund conditions (Cypress Test)`;
         createAndCompleteRefundAmountConditions(conditionsName, transitionName, 'A'/*Accumulated / Gesamtrückvergütung*/);
+        cy.screenshot();
+
+        const discountSchemaName = `${timestamp} accumulated amount-based refund conditions (Cypress Test)`;
+        new DiscountSchema
+            .builder(discountSchemaName)
+            .addDiscountBreak(new DiscountBreak
+                .builder()
+                .setBreakValue(0)
+                .setBreakDiscount(0)
+                .build())
+            .build()
+            .apply()
+        cy.screenshot();
+
+        const bPartnerName = `accumulated percent-based refund conditions ${timestamp}`
+        new BPartner
+            .builder(bPartnerName)
+            .setVendor(true)
+            .setVendorDiscountSchema(discountSchemaName)
+            .addLocation(new BPartnerLocation
+                .builder('Address1')
+                .setCity('Cologne')
+                .setCountry('Deutschland')
+                .build())
+            .build()
+            .apply()
+
+        cy.executeHeaderAction('C_Flatrate_Term_Create_For_BPartners')
+            .selectInListField('C_Flatrate_Conditions_ID', conditionsName, conditionsName)
+            .writeIntoStringField('StartDate', '01/01/2019{enter}')
+            .pressStartButton();
+        cy.screenshot();
     });
 });
 
--- cypress/integration/contracts/flatrate_conditions_refund_tiered_percent_spec.js
+++ cypress/integration/contracts/flatrate_conditions_refund_tiered_percent_spec.js
@@ -1,6 +1,8 @@
 /// <reference types="Cypress" />
 
 import { createAndCompleteTransition, createAndCompleteRefundPercentConditions } from '../../support/utils/contract';
+import { BPartner, BPartnerLocation } from '../../support/utils/bpartner';
+import { DiscountSchema, DiscountBreak } from '../../support/utils/discountschema';
 
 describe('Create tiered percent-based refund conditions', function() {
     before(function() {
@@ -8,13 +10,46 @@ describe('Create tiered percent-based refund conditions', function() {
         cy.loginByForm();
     });
     
-    it('Create tiered percent-based refund conditions', function () {
+    it('Create tiered percent-based refund conditions and a vendor with a respective contract', function () {
         const timestamp = new Date().getTime(); // used in the document names, for ordering
 
         const transitionName = `${timestamp} tiered percent-based refund conditions (Cypress Test)`;
         createAndCompleteTransition(transitionName, null, null);
+        cy.screenshot()
 
         const conditionsName = `${timestamp} tiered percent-based refund conditions (Cypress Test)`;
         createAndCompleteRefundPercentConditions(conditionsName, transitionName, 'T'/*Tiered / Gestaffelte Rückvergütung*/);
+        cy.screenshot()
+
+        const discountSchemaName = `${timestamp} accumulated amount-based refund conditions (Cypress Test)`;
+        new DiscountSchema
+            .builder(discountSchemaName)
+            .addDiscountBreak(new DiscountBreak
+                .builder()
+                .setBreakValue(0)
+                .setBreakDiscount(0)
+                .build())
+            .build()
+            .apply()
+        cy.screenshot()
+
+        const bPartnerName = `t5 tiered percent-based refund conditions ${timestamp}`;
+        new BPartner
+            .builder(bPartnerName)
+            .setVendor(true)
+            .setVendorDiscountSchema(discountSchemaName)
+            .addLocation(new BPartnerLocation
+                .builder('Address1')
+                .setCity('Cologne')
+                .setCountry('Deutschland')
+                .build())
+            .build()
+            .apply()
+
+        cy.executeHeaderAction('C_Flatrate_Term_Create_For_BPartners')
+            .selectInListField('C_Flatrate_Conditions_ID', conditionsName, conditionsName)
+            .writeIntoStringField('StartDate', '01/01/2019{enter}')
+            .pressStartButton();
+        cy.screenshot()
     });
 });
