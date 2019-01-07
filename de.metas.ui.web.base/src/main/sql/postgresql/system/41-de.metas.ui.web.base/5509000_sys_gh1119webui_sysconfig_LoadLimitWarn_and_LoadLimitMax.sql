39828bacf52199ca0d7491f49a46b087462bdda5
Teo Sarca <teo.sarca@metasfresh.com>
2019-01-07 15:52:22 +0200

    Create 5509000_sys_gh1119webui_sysconfig_LoadLimitWarn_and_LoadLimitMax.sql
    https://github.com/metasfresh/metasfresh-webui-api/issues/1119
    (cherry picked from commit cbc47b7125cf3180009a383ae74df2adba60cabb)

---
+++ de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5509000_sys_gh1119webui_sysconfig_LoadLimitWarn_and_LoadLimitMax.sql
@@ -0,0 +1,10 @@
+-- 2019-01-07T15:32:44.999
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541255,'S',TO_TIMESTAMP('2019-01-07 15:32:44','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web','Y','webui.documents.LoadLimitWarn',TO_TIMESTAMP('2019-01-07 15:32:44','YYYY-MM-DD HH24:MI:SS'),100,'100')
+;
+
+-- 2019-01-07T15:33:11.169
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541256,'S',TO_TIMESTAMP('2019-01-07 15:33:11','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web','Y','webui.documents.LoadLimitMax',TO_TIMESTAMP('2019-01-07 15:33:11','YYYY-MM-DD HH24:MI:SS'),100,'300')
+;
+
