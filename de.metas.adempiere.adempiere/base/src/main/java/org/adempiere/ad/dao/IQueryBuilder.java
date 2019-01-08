81f1e4ec7b8b6e1054cca25112db9275a75f19d2
Teo Sarca <teo.sarca@metasfresh.com>
2019-01-08 18:51:16 +0200

    IQueryBuilder.clearOrderBys
    https://github.com/metasfresh/metasfresh/issues/478

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/IQueryBuilder.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/IQueryBuilder.java
@@ -117,6 +117,7 @@ public interface IQueryBuilder<T>
 	IQueryBuilderOrderByClause<T> orderBy();
 
 	//@formatter:off
+	default IQueryBuilder<T> clearOrderBys() { orderBy().clear(); return this; }
 	default IQueryBuilder<T> orderBy(final String columnName) { orderBy().addColumn(columnName); return this; }
 	default IQueryBuilder<T> orderBy(final ModelColumn<T, ?> column) { orderBy().addColumn(column); return this; }
 	default IQueryBuilder<T> orderByDescending(final String columnName) { orderBy().addColumnDescending(columnName); return this; }
