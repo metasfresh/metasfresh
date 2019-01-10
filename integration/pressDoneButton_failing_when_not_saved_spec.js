099641b7ac09469c6912ea2779645ad2b48f8e32
siemiatj <kuba@saskla.cz>
2019-01-10 18:57:12 +0100

    - small cleanup in cypress tests

--- integration/pressDoneButton_failing_when_not_saved_spec.js
+++ integration/pressDoneButton_failing_when_not_saved_spec.js
@@ -1,12 +1,10 @@
 describe('cypress pressDoneButton shall fail if the respective record was not saved #2083 - https://github.com/metasfresh/metasfresh-webui-frontend/issues/2083',  function() {
-  
-  before
-before(function() {
-  // login before each test
-  cy.loginByForm();
-});
+  before(function() {
+    // login before each test
+    cy.loginByForm();
+  });
 
-  it ('Open bpartner window', function() {
+  it('Open bpartner window', function() {
     cy.visit('/window/123');
     cy.get('.cell-text-wrapper.text-cell')
       .contains('Test Kunde 1')
