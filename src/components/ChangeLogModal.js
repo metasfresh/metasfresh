b1207a40287eec9757236c1b7b7ad9c187900163
siemiatj <kuba@saskla.cz>
2019-01-16 19:34:11 +0100

    - add modified data to static modal #1945

--- src/components/ChangeLogModal.js
+++ src/components/ChangeLogModal.js
@@ -47,6 +47,46 @@ export default class ChangeLogModal extends Component {
             </div>
           </div>
         </div>
+        <div className="panel panel-spaced panel-distance panel-bordered panel-secondary">
+          <div className="elements-line">
+            <div className="form-group row  form-field-Value">
+              <div className="form-control-label col-sm-3">Updated By:</div>
+              <div className="col-sm-9 ">
+                <div className="input-body-container">
+                  <span />
+                  <div className="input-block input-icon-container input-disabled input-secondary pulse-off">
+                    <input
+                      className="input-field js-input-field"
+                      disabled
+                      tabIndex="-1"
+                      type="text"
+                      value={data.lastChangedByUsername}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="elements-line">
+            <div className="form-group row  form-field-Value">
+              <div className="form-control-label col-sm-3">Updated At:</div>
+              <div className="col-sm-9 ">
+                <div className="input-body-container">
+                  <span />
+                  <div className="input-block input-icon-container input-disabled input-secondary pulse-off">
+                    <input
+                      className="input-field js-input-field"
+                      disabled
+                      tabIndex="-1"
+                      type="text"
+                      value={data.lastChangedTimestamp}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
