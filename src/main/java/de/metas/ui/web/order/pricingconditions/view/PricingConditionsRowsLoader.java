15059a374ac0dc32fac744a07b468831e1cc833e
Teo Sarca <teo.sarca@metasfresh.com>
2019-01-17 11:31:06 +0200

    SourceDocumentLine: annotate what's not null
    https://github.com/metasfresh/metasfresh/issues/4282

--- src/main/java/de/metas/ui/web/order/pricingconditions/view/PricingConditionsRowsLoader.java
+++ src/main/java/de/metas/ui/web/order/pricingconditions/view/PricingConditionsRowsLoader.java
@@ -338,21 +338,30 @@ class PricingConditionsRowsLoader
 	@lombok.Builder
 	public static final class SourceDocumentLine
 	{
+		@Nullable
 		OrderLineId orderLineId;
+		@NonNull
 		SOTrx soTrx;
 
+		@NonNull
 		BPartnerId bpartnerId;
 
+		@NonNull
 		ProductId productId;
+		@NonNull
 		ProductCategoryId productCategoryId;
 
+		@NonNull
 		Money priceEntered;
 
 		@lombok.Builder.Default
+		@NonNull
 		Percent discount = Percent.ZERO;
 
+		@Nullable
 		PaymentTermId paymentTermId;
 
+		@Nullable
 		PricingConditionsBreakId pricingConditionsBreakId;
 	}
 
