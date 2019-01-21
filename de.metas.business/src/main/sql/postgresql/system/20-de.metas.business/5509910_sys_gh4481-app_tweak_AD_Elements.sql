a10acd83a625677273de264bfc9d745115ef3af5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-01-21 16:57:15 +0100

    4881-app - MSV3 user sync: identify/update users by some external ID
    * solve SQL problem
    
    https://github.com/metasfresh/metasfresh/issues/4881

--- de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5509910_sys_gh4481-app_tweak_AD_Elements.sql
+++ de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5509910_sys_gh4481-app_tweak_AD_Elements.sql
@@ -316,11 +316,6 @@ UPDATE AD_Menu SET Name='Kommissionier-Lagergruppe ', Description=NULL, WEBUI_Na
 DELETE FROM AD_Element_Link WHERE AD_Element_Link_ID=622181
 ;
 
--- 2019-01-21T14:02:47.064
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Element_Link (AD_Client_ID,AD_Element_ID,AD_Element_Link_ID,AD_Field_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,543497,626231,563151,0,540424,TO_TIMESTAMP('2019-01-21 14:02:46','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2019-01-21 14:02:46','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
 -- 2019-01-21T14:06:02.447
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 DELETE FROM AD_Element_Link WHERE AD_Element_Link_ID=626233
