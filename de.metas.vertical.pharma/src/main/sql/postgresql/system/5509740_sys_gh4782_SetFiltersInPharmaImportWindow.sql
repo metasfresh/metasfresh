233fba6d7ca6c573b5e57cce5c88cd98994d6733
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-01-21 13:23:25 +0200

    #4782 Fix NPE exception and add filter and IFA import product window
    #4782 Various fixes to import processes

---
+++ de.metas.vertical.pharma/src/main/sql/postgresql/system/5509740_sys_gh4782_SetFiltersInPharmaImportWindow.sql
@@ -0,0 +1,30 @@
+-- 2019-01-21T12:57:58.132
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2019-01-21 12:57:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558279
+;
+
+-- 2019-01-21T12:58:07.393
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2019-01-21 12:58:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558280
+;
+
+-- 2019-01-21T12:58:24.409
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsRangeFilter='Y', IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2019-01-21 12:58:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558273
+;
+
+-- 2019-01-21T12:59:21.992
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2019-01-21 12:59:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558139
+;
+
+-- 2019-01-21T12:59:45.314
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2019-01-21 12:59:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558141
+;
+
+-- 2019-01-21T13:02:10.496
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET FilterDefaultValue='E',Updated=TO_TIMESTAMP('2019-01-21 13:02:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558279
+;
+
