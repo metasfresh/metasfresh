dd89f1d382ddd73e0db8c7436f56322d0e7c9a07
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-01-21 18:53:38 +0100

    4881-app - MSV3 user sync: identify/update users by some external ID
    * solve SQL problem
    
    https://github.com/metasfresh/metasfresh/issues/4881

--- de.metas.vertical.pharma.msv3.server-peer-metasfresh/src/main/sql/postgresql/system/5509910_sys_gh4481-app_trl_msv3_warehouse_picking_group.sql
+++ de.metas.vertical.pharma.msv3.server-peer-metasfresh/src/main/sql/postgresql/system/5509910_sys_gh4481-app_trl_msv3_warehouse_picking_group.sql
@@ -31,8 +31,8 @@ DELETE FROM AD_Element_Link WHERE AD_Element_Link_ID=626232
 
 -- 2019-01-21T14:05:59.831
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Element_Link (AD_Client_ID,AD_Element_ID,AD_Element_Link_ID,AD_Field_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,543497,626233,563151,0,540424,TO_TIMESTAMP('2019-01-21 14:05:59','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2019-01-21 14:05:59','YYYY-MM-DD HH24:MI:SS'),100)
-;
+--INSERT INTO AD_Element_Link (AD_Client_ID,AD_Element_ID,AD_Element_Link_ID,AD_Field_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,543497,626233,563151,0,540424,TO_TIMESTAMP('2019-01-21 14:05:59','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2019-01-21 14:05:59','YYYY-MM-DD HH24:MI:SS'),100)
+--;
 
 -- 2019-01-21T14:06:02.403
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
