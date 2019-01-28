bcd1fcb176cbe8beacb22901c3f90e6d994780ec
siemiatj <kuba@saskla.cz>
2019-01-28 17:54:16 +0100

    - more linter cleanup

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -11,29 +11,23 @@ class Auth {
   initNotificationClient = (topic, cb) => {
     this.notificationClient = Stomp.Stomp.over(new SockJs(config.WS_URL));
     this.notificationClient.debug = null;
-    this.notificationClient.connect(
-      {},
-      () => {
-        this.notificationClient.connected &&
-          this.notificationClient.subscribe(topic.data, msg => {
-            cb && cb(msg);
-          });
-      }
-    );
+    this.notificationClient.connect({}, () => {
+      this.notificationClient.connected &&
+        this.notificationClient.subscribe(topic.data, msg => {
+          cb && cb(msg);
+        });
+    });
   };
 
   initSessionClient = (topic, cb) => {
     this.sessionClient = Stomp.Stomp.over(new SockJs(config.WS_URL));
     this.sessionClient.debug = null;
-    this.sessionClient.connect(
-      {},
-      () => {
-        this.sessionClient.connected &&
-          this.sessionClient.subscribe(topic, msg => {
-            cb && cb(msg);
-          });
-      }
-    );
+    this.sessionClient.connect({}, () => {
+      this.sessionClient.connected &&
+        this.sessionClient.subscribe(topic, msg => {
+          cb && cb(msg);
+        });
+    });
   };
 
   close = () => {
