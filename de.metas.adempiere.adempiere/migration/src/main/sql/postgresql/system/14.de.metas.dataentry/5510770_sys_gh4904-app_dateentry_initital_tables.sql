50176a6f0548b4cc098bc38c2a23140eb1110956
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-01-29 09:13:34 +0100

    4904-app - data entry prototype
    * sql: set AD_Table.AD_Widnow_ID
    https://github.com/metasfresh/metasfresh/issues/4904

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/14.de.metas.dataentry/5510770_sys_gh4904-app_dateentry_initital_tables.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/14.de.metas.dataentry/5510770_sys_gh4904-app_dateentry_initital_tables.sql
@@ -3149,3 +3149,25 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Column_ID=564007 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
+
+
+-- 2019-01-29T08:58:18.193
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET AD_Window_ID=540572,Updated=TO_TIMESTAMP('2019-01-29 08:58:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=541167
+;
+
+-- 2019-01-29T08:58:30.305
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET AD_Window_ID=540571,Updated=TO_TIMESTAMP('2019-01-29 08:58:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=541165
+;
+
+-- 2019-01-29T08:58:35.929
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET AD_Window_ID=540572,Updated=TO_TIMESTAMP('2019-01-29 08:58:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=541168
+;
+
+-- 2019-01-29T08:58:48.225
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET AD_Window_ID=540571,Updated=TO_TIMESTAMP('2019-01-29 08:58:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=541166
+;
+
