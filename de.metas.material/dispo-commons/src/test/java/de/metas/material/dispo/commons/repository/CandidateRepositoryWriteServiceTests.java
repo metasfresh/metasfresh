6ba2991223a4101740d47ae43de4ce206551039f
Teo Sarca <teo.sarca@metasfresh.com>
2019-01-31 09:57:54 +0200

    Update CandidateRepositoryWriteServiceTests.java
    minor changes for debugging
    
    https://github.com/metasfresh/metasfresh/issues/4894

--- de.metas.material/dispo-commons/src/test/java/de/metas/material/dispo/commons/repository/CandidateRepositoryWriteServiceTests.java
+++ de.metas.material/dispo-commons/src/test/java/de/metas/material/dispo/commons/repository/CandidateRepositoryWriteServiceTests.java
@@ -23,6 +23,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.wrapper.POJOLookupMap;
 import org.adempiere.test.AdempiereTestHelper;
 import org.adempiere.test.AdempiereTestWatcher;
 import org.junit.Before;
@@ -172,14 +173,17 @@ public class CandidateRepositoryWriteServiceTests
 	@Test
 	public void addOrUpdateOverwriteStoredSeqNo_returns_equal_candidate()
 	{
+		POJOLookupMap.get().dumpStatus("before actual test starts", I_MD_Candidate.Table_Name);
+
 		final Candidate originalCandidate = repositoryTestHelper.stockCandidate;
+		System.out.println("originalCandidate: " + originalCandidate);
+
 		final Candidate candidateReturnedfromRepo = candidateRepositoryWriteService
 				.addOrUpdateOverwriteStoredSeqNo(originalCandidate)
 				.getCandidate();
 
 		final Candidate originalCandidateWithZeroDelta = originalCandidate.withQuantity(ZERO);
-		assertThat(candidateReturnedfromRepo)
-				.isEqualTo(originalCandidateWithZeroDelta);
+		assertThat(candidateReturnedfromRepo).isEqualTo(originalCandidateWithZeroDelta);
 	}
 
 	/**
