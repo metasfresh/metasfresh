13bc6e6730214e22e657de15b43a8e46e75a9845
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-02-01 17:39:27 +0100

    4919-app - Process to create missing ESR payment requests
    * minor: add en trl for the process name
    https://github.com/metasfresh/metasfresh/issues/4919

--- de.metas.payment.esr/src/main/sql/postgresql/system/5511470_sys_gh4919-app_add_process_C_Payment_Request_CreateMissingFor_C_Invoices.sql
+++ de.metas.payment.esr/src/main/sql/postgresql/system/5511470_sys_gh4919-app_add_process_C_Payment_Request_CreateMissingFor_C_Invoices.sql
@@ -10,11 +10,26 @@ INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, Is
 
 -- 2019-01-31T11:59:01.126
 -- #298 changing anz. stellen
-INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_QuickAction,WEBUI_QuickAction_Default) VALUES (0,0,541041,318,TO_TIMESTAMP('2019-01-31 11:59:00','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment.esr','Y',TO_TIMESTAMP('2019-01-31 11:59:00','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_ViewQuickAction,WEBUI_ViewQuickAction_Default) VALUES (0,0,541041,318,TO_TIMESTAMP('2019-01-31 11:59:00','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment.esr','Y',TO_TIMESTAMP('2019-01-31 11:59:00','YYYY-MM-DD HH24:MI:SS'),100,'N','N')
 ;
 
 -- 2019-01-31T13:16:38.246
 -- #298 changing anz. stellen
 UPDATE AD_Process SET Description='Erzeugt ESR Zahldaten und PDF-Druckstücke für die ausgewählten Rechnungen, sofern diese fertig gestellt sind, aber noch keine ESR-Referenznummer haben.', Name='ESR-Zahldaten erstellen',Updated=TO_TIMESTAMP('2019-01-31 13:16:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=541041
+;
+
+-- 2019-02-01T17:37:01.630
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2019-02-01 17:37:01','YYYY-MM-DD HH24:MI:SS'),Name='ESR-Zahldaten erstellen' WHERE AD_Process_ID=541041 AND AD_Language='de_CH'
+;
+
+-- 2019-02-01T17:37:07.413
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2019-02-01 17:37:07','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Description='Erzeugt ESR Zahldaten und PDF-Druckstücke für die ausgewählten Rechnungen, sofern diese fertig gestellt sind, aber noch keine ESR-Referenznummer haben.' WHERE AD_Process_ID=541041 AND AD_Language='de_CH'
+;
+
+-- 2019-02-01T17:37:33.069
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2019-02-01 17:37:33','YYYY-MM-DD HH24:MI:SS'),IsTranslated='Y',Name='Create ESR payment data' WHERE AD_Process_ID=541041 AND AD_Language='en_US'
 ;
 
