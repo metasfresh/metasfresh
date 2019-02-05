209d1cb14da7db39e76e9096c6d2b99317c93783
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2019-02-05 06:17:02 +0100

    cleanup; avoid "pending triggere events" problem
    #4929

--- de.metas.salescandidate.base/src/main/sql/postgresql/system/5511810_sys_gh4929-app_C_OLCand_AD_DataDestination_ID_mandatory.sql
+++ de.metas.salescandidate.base/src/main/sql/postgresql/system/5511810_sys_gh4929-app_C_OLCand_AD_DataDestination_ID_mandatory.sql
@@ -1,25 +1,13 @@
--- 2019-02-04T17:26:38.143
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET DefaultValue='540003', IsMandatory='Y', TechnicalNote='Out default AD_DataDestination_ID=540003 is "DEST.de.metas.ordercandidate"',Updated=TO_TIMESTAMP('2019-02-04 17:26:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=547734
-;
 
--- 2019-02-04T17:26:42.416
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO t_alter_column values('c_olcand','AD_DataDestination_ID','NUMERIC(10)',null,'540003')
-;
-
+-- AD_DataDestination_ID=540003 is "DEST.de.metas.ordercandidate"
 -- 2019-02-04T17:26:42.422
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE C_OLCand SET AD_DataDestination_ID=540003 WHERE AD_DataDestination_ID IS NULL
 ;
 
+COMMIT;
+
 -- 2019-02-04T17:26:42.424
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO t_alter_column values('c_olcand','AD_DataDestination_ID',null,'NOT NULL',null)
 ;
-
--- 2019-02-04T17:27:13.205
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET DefaultValue='', TechnicalNote='',Updated=TO_TIMESTAMP('2019-02-04 17:27:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=547734
-;
-
