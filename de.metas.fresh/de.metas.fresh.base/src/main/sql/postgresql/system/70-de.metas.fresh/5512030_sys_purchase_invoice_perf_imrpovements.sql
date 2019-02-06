5a51c9d25fb010d9ed69907934eb2c4145ae2ef1
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-02-06 13:52:15 +0100

    minor improvements found during postgresql performance troubleshooting

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5512030_sys_purchase_invoice_perf_imrpovements.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5512030_sys_purchase_invoice_perf_imrpovements.sql
@@ -1,3 +1,11 @@
+DROP INDEX IF EXISTS m_hu_assignment_document_idx;
+CREATE INDEX m_hu_assignment_document_idx
+    ON public.m_hu_assignment USING btree
+    (record_id, ad_table_id)
+    TABLESPACE pg_default;
+COMMENT ON INDEX m_hu_assignment_document_idx
+IS 'have record_id first, because it''s much, much more distinctive than ad_table_id';
+
 DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Purchase_Invoice_Details ( IN C_Invoice_ID numeric, IN AD_Language Character Varying (6) );
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Purchase_Invoice_Details ( IN C_Invoice_ID numeric, IN AD_Language Character Varying (6) )
 RETURNS TABLE 
