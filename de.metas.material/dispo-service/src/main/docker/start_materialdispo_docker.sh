a49263cbd5ffb952308a9486d26b4397eb687632
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-02-07 17:04:51 +0100

    4939-app - Configure c3p0's max db connection pool size
    * not-100%-related - update material dispo startup file and metasfresh.properties
    https://github.com/metasfresh/metasfresh/issues/4939

--- de.metas.material/dispo-service/src/main/docker/start_materialdispo_docker.sh
+++ de.metas.material/dispo-service/src/main/docker/start_materialdispo_docker.sh
@@ -35,8 +35,12 @@ set_properties()
  echo "set_properties BEGIN"
  local prop_file="$1"
  if [[ $(cat $prop_file | grep FOO | wc -l) -ge "1" ]]; then
-	sed -Ei "s/FOO_DBMS/$DB_HOST/g" $prop_file
-	sed -Ei "s/FOO_APP/$APP_HOST/g" $prop_file
+	sed -Ei "s/FOO_DBMS/${db_host}/g" $prop_file
+	sed -Ei "s/FOO_DBMS_PORT/${db_port}/g" $prop_file
+	sed -Ei "s/FOO_DB_NAME/${db_name}/g" $prop_file
+	sed -Ei "s/FOO_DB_USER/${db_user}/g" $prop_file
+	sed -Ei "s/FOO_DB_PASSWORD/${db_password}/g" $prop_file
+	sed -Ei "s/FOO_APP/${app_host}/g" $prop_file
  fi
  echo "set_properties END"
 }
