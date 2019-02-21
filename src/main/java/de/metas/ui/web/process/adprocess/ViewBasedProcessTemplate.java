1c7fec19e3573da846f0defc5841275a624188b6
Teo Sarca <teo.sarca@metasfresh.com>
2019-02-21 18:49:56 +0200

    ViewBaseProcessTemplate.getViewProfileId
    https://github.com/metasfresh/metasfresh/issues/4967

--- src/main/java/de/metas/ui/web/process/adprocess/ViewBasedProcessTemplate.java
+++ src/main/java/de/metas/ui/web/process/adprocess/ViewBasedProcessTemplate.java
@@ -19,6 +19,7 @@ import de.metas.ui.web.view.IView;
 import de.metas.ui.web.view.IViewRow;
 import de.metas.ui.web.view.IViewsRepository;
 import de.metas.ui.web.view.ViewId;
+import de.metas.ui.web.view.ViewProfileId;
 import de.metas.ui.web.view.ViewRowIdsSelection;
 import de.metas.ui.web.window.datatypes.DocumentId;
 import de.metas.ui.web.window.datatypes.DocumentIdsSelection;
@@ -88,6 +89,7 @@ public abstract class ViewBasedProcessTemplate extends JavaProcess
 	private String p_WebuiChildViewSelectedIdsStr;
 
 	private IView _view;
+	private ViewProfileId _viewProfileId;
 	private ViewRowIdsSelection _viewRowIdsSelection;
 	private ViewRowIdsSelection _parentViewRowIdsSelection;
 	private ViewRowIdsSelection _childViewRowIdsSelection;
@@ -151,9 +153,15 @@ public abstract class ViewBasedProcessTemplate extends JavaProcess
 		return getView().getViewId().getWindowId();
 	}
 
+	protected final ViewProfileId getViewProfileId()
+	{
+		return _viewProfileId;
+	}
+
 	private final void setViewInfos(@NonNull final ViewAsPreconditionsContext viewContext)
 	{
 		_view = viewContext.getView();
+		_viewProfileId = viewContext.getViewProfileId();
 		_viewRowIdsSelection = viewContext.getViewRowIdsSelection();
 		_parentViewRowIdsSelection = viewContext.getParentViewRowIdsSelection();
 		_childViewRowIdsSelection = viewContext.getChildViewRowIdsSelection();
