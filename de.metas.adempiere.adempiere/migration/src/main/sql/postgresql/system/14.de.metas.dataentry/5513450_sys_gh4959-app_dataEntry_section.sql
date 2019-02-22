50379a031240439b3dc502cd299fe2286a90b992
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-02-22 07:44:05 +0100

    4959-app - data entry implementation
    * solve SQL problem
    https://github.com/metasfresh/metasfresh/issues/4959

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/14.de.metas.dataentry/5513450_sys_gh4959-app_dataEntry_section.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/14.de.metas.dataentry/5513450_sys_gh4959-app_dataEntry_section.sql
@@ -1056,14 +1056,3 @@ UPDATE AD_Element_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2019-02-21 10:37:2
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Column SET DefaultValue='N',Updated=TO_TIMESTAMP('2019-02-21 10:39:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=564206
 ;
-
--- 2019-02-21T10:39:59.519
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO t_alter_column values('dataentry_section','IsInitiallyClosed','CHAR(1)',null,'N')
-;
-
--- 2019-02-21T10:39:59.537
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE DataEntry_Section SET IsInitiallyClosed='N' WHERE IsInitiallyClosed IS NULL
-;
-
