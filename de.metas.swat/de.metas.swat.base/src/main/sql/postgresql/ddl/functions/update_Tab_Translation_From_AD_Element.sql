c3639f01bb0ed20f3ccc8115a1ecc9597bac7f6a
Teo Sarca <teo.sarca@metasfresh.com>
2019-02-22 21:09:01 +0200

    AD_Element_Trl: customization fields for Name, Description, Help
    also changed all update trl functions
    
    https://github.com/metasfresh/metasfresh/issues/4982

--- de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_Tab_Translation_From_AD_Element.sql
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_Tab_Translation_From_AD_Element.sql
@@ -1,40 +1,55 @@
--- Function: public.update_tab_translation_from_ad_element(numeric)
+DROP FUNCTION IF EXISTS update_Tab_Translation_From_AD_Element(numeric, character varying);
 
--- DROP FUNCTION IF EXISTS public.update_tab_translation_from_ad_element(numeric);
-
-CREATE OR REPLACE FUNCTION public.update_tab_translation_from_ad_element(ad_element_id numeric)
-  RETURNS void AS
+CREATE OR REPLACE FUNCTION update_Tab_Translation_From_AD_Element
+(
+	p_AD_Element_ID numeric,
+	p_AD_Language character varying = null
+)
+RETURNS void
+AS
 $BODY$
-
+DECLARE
+	update_count integer;
 BEGIN
 
-
--- AD_Tab_TRL via AD_Element
-
- UPDATE  AD_Tab_TRL tabtrl
-	SET name = x.name, isTranslated = x.isTranslated, description = x.description, help = x.help, commitwarning = x.commitwarning
+	UPDATE AD_Tab_Trl t
+	SET
+		IsTranslated = x.IsTranslated,
+		Name = x.Name,
+		Description = x.Description,
+		Help = x.Help,
+		CommitWarning = x.CommitWarning
 	FROM
 	(
-		select t.AD_Element_ID, t.AD_Tab_ID,   tt.ad_language, etrl.Name, etrl.IsTranslated, etrl.description, etrl.help, etrl.commitwarning
-	
-		from AD_Element_Trl etrl 
-		join AD_Tab t on etrl.AD_Element_ID = t.AD_Element_ID
-		join AD_Tab_Trl tt on t.AD_Tab_ID = tt.AD_Tab_ID
+		select
+			tab.AD_Tab_ID,
+			etrl.AD_Element_ID,
+			etrl.AD_Language,
+			etrl.IsTranslated,
+			etrl.Name,
+			etrl.Description,
+			etrl.Help,
+			etrl.CommitWarning
+		from AD_Element_Trl_Effective_v etrl
+			join AD_Tab tab on tab.AD_Element_ID = etrl.AD_Element_ID
 		where 
-			t.AD_Element_ID = update_tab_translation_From_AD_Element.AD_Element_ID  
-			and tt.ad_language = etrl.ad_language 
-			
+			etrl.AD_Element_ID = p_AD_Element_ID  
+			and (p_AD_Language is null OR etrl.AD_Language = p_AD_Language)
 	) x
-WHERE tabtrl.AD_Tab_ID = x.AD_Tab_ID and tabtrl.ad_language = x.ad_language;
+	WHERE
+		t.AD_Tab_ID = x.AD_Tab_ID
+		and t.AD_Language = x.AD_Language
+	;
 
-
-
-
+	GET DIAGNOSTICS update_count = ROW_COUNT;
+	raise notice 'Update % AD_Tab_Trl rows using AD_Element_ID=%, AD_Language=%', update_count, p_AD_Element_ID, p_AD_Language;
 
 END;
 $BODY$
-  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
-  COST 100;
-ALTER FUNCTION public.update_tab_translation_from_ad_element(numeric)
-  OWNER TO metasfresh;
-COMMENT ON FUNCTION public.update_tab_translation_from_ad_element(numeric) IS 'When the AD_Tab.AD_Element_ID is changed, update all the AD_Tab_Trl entries of the AD_Tab, based on the AD_Element.';
+LANGUAGE plpgsql
+VOLATILE 
+SECURITY DEFINER
+COST 100;
+
+COMMENT ON FUNCTION update_Tab_Translation_From_AD_Element(numeric, character varying) IS
+'When the AD_Tab.AD_Element_ID is changed, update all the AD_Tab_Trl entries of the AD_Tab, based on the AD_Element.';
