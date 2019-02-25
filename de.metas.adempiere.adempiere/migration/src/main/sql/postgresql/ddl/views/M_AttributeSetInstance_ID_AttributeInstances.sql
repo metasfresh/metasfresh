db9c8a9a92dac2ba585136db361aa4f3b4447611
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-02-25 13:08:39 +0200

    #4987 use the valuenumber or the value date and not the value field
    #4987 M_Atribute_Value is not saved correctly

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/views/M_AttributeSetInstance_ID_AttributeInstances.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/views/M_AttributeSetInstance_ID_AttributeInstances.sql
@@ -1,16 +1,21 @@
-
 DROP VIEW IF EXISTS M_AttributeSetInstance_ID_AttributeInstances;
 
 CREATE OR REPLACE VIEW M_AttributeSetInstance_ID_AttributeInstances AS
 SELECT
     ai.M_AttributeSetInstance_ID,
+	CASE
+      WHEN a.AttributeValueType = 'N' THEN array_agg(ai.M_Attribute_ID || '_'
+		||  coalesce(ai.m_attributevalue_id::character varying, ai.valuenumber::character varying, ''))
+      WHEN a.AttributeValueType = 'D' THEN array_agg(ai.M_Attribute_ID || '_'
+        ||  coalesce(ai.m_attributevalue_id::character varying, ai.valuedate::character varying, ''))
+	  ELSE
     array_agg(
-        ai.M_Attribute_ID || '_' || 
-        coalesce(
-            ai.m_attributevalue_id::character varying, ai.value, ai.valuenumber::character varying, ai.valuedate::character varying, '')
-    ) as AttributeInstances
+        ai.M_Attribute_ID || '_' ||
+        coalesce(ai.m_attributevalue_id::character varying, ai.value, ''))
+    END AS AttributeInstances
 FROM M_AttributeInstance ai
-GROUP BY ai.M_AttributeSetInstance_ID;
-COMMENT ON VIEW M_AttributeSetInstance_ID_AttributeInstances 
+JOIN M_Attribute a ON a.M_Attribute_ID = ai.M_Attribute_ID
+GROUP BY ai.M_AttributeSetInstance_ID, a.AttributeValueType;
+COMMENT ON VIEW M_AttributeSetInstance_ID_AttributeInstances
 IS 'Returns M_AttributeSetInstance_IDs with arrays that represent all M_AttributeInstances of the respective ASI.
 Each array element is a string containing of <M_Attribute_ID>_<Attribute-Instance-Value>';
--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5513740_sys_gh4987_RepairAttributeValueWhenValueTypeIsNumber.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5513740_sys_gh4987_RepairAttributeValueWhenValueTypeIsNumber.sql
@@ -1,6 +1,23 @@
--- View: Report.fresh_Attributes
+CREATE OR REPLACE VIEW M_AttributeSetInstance_ID_AttributeInstances AS
+SELECT
+    ai.M_AttributeSetInstance_ID,
+	CASE
+      WHEN a.AttributeValueType = 'N' THEN array_agg(ai.M_Attribute_ID || '_'
+		||  coalesce(ai.m_attributevalue_id::character varying, ai.valuenumber::character varying, ''))
+      WHEN a.AttributeValueType = 'D' THEN array_agg(ai.M_Attribute_ID || '_'
+        ||  coalesce(ai.m_attributevalue_id::character varying, ai.valuedate::character varying, ''))
+	  ELSE
+    array_agg(
+        ai.M_Attribute_ID || '_' ||
+        coalesce(ai.m_attributevalue_id::character varying, ai.value, ''))
+    END AS AttributeInstances
+FROM M_AttributeInstance ai
+JOIN M_Attribute a ON a.M_Attribute_ID = ai.M_Attribute_ID
+GROUP BY ai.M_AttributeSetInstance_ID, a.AttributeValueType;
+COMMENT ON VIEW M_AttributeSetInstance_ID_AttributeInstances
+IS 'Returns M_AttributeSetInstance_IDs with arrays that represent all M_AttributeInstances of the respective ASI.
+Each array element is a string containing of <M_Attribute_ID>_<Attribute-Instance-Value>';
 
--- DROP VIEW IF EXISTS Report.fresh_Attributes;
 
 CREATE OR REPLACE VIEW Report.fresh_Attributes AS 
 SELECT * FROM
@@ -38,5 +55,4 @@ SELECT * FROM
 ) att
 WHERE COALESCE( ai_value, '') != ''
 ;
-
 COMMENT ON VIEW Report.fresh_Attributes IS 'retrieves Attributes in the way they are needed for the jasper reports';
