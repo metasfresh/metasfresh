9cf4c1e53b58a9dba38340f172285d4ca0afffbf
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-02-26 13:15:57 +0200

    #4991 Add Global and url3 in C_BPartne
    #4991 URL csv Importer for BPartner

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5513820_sys_gh4991_UpdateEntityType.sql
@@ -0,0 +1,10 @@
+-- 2019-02-26T13:13:19.402
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET EntityType='D',Updated=TO_TIMESTAMP('2019-02-26 13:13:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558551
+;
+
+-- 2019-02-26T13:13:31.885
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET EntityType='D',Updated=TO_TIMESTAMP('2019-02-26 13:13:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=560188
+;
+
