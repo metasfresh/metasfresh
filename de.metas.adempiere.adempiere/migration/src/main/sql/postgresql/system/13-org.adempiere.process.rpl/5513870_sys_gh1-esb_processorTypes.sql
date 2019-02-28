33fef3c6d05ea0b139de708226114075c40cecd4
haneloreianoseck <hanelore.ianoseck@metasfresh.com>
2019-02-28 15:12:39 +0200

    metasfresh/metasfresh-esb-camel#1 : Updated processor types migration script

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/13-org.adempiere.process.rpl/5513870_sys_gh1-esb_processorTypes.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/13-org.adempiere.process.rpl/5513870_sys_gh1-esb_processorTypes.sql
@@ -1,3 +1,8 @@
-INSERT INTO public.imp_processor_type (imp_processor_type_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, help, javaclass) VALUES (1000001, 1000000, 1000000, 'Y', '2019-02-26 11:21:18+02', 100, '2019-02-26 11:21:18+02', 100, 'RabbitMQ', 'RabbitMQ', NULL, NULL, 'org.adempiere.server.rpl.imp.RabbitMqImportProcessor');
-INSERT INTO public.exp_processor_type (exp_processor_type_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, value, name, description, help, javaclass) VALUES (540007, 0, 0, 'Y', '2019-02-26 17:04:08+02', 100, '2019-02-26 17:04:08+02', 100, 'RabbitMQ', 'RabbitMQ', NULL, NULL, 'org.adempiere.server.rpl.exp.RabbitMqExportProcessor');
-
+-- 2019-02-26T17:04:08.561
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO EXP_Processor_Type (AD_Client_ID,AD_Org_ID,Created,CreatedBy,EXP_Processor_Type_ID,IsActive,JavaClass,Name,Updated,UpdatedBy,Value) VALUES (0,0,TO_TIMESTAMP('2019-02-26 17:04:08','YYYY-MM-DD HH24:MI:SS'),100,540007,'Y','org.adempiere.server.rpl.exp.RabbitMqExportProcessor','RabbitMQ',TO_TIMESTAMP('2019-02-26 17:04:08','YYYY-MM-DD HH24:MI:SS'),100,'RabbitMQ')
+;
+-- 2019-02-28T13:26:04.425
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO IMP_Processor_Type (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IMP_Processor_Type_ID,IsActive,JavaClass,Name,Updated,UpdatedBy,Value) VALUES (0,0,TO_TIMESTAMP('2019-02-28 13:26:04','YYYY-MM-DD HH24:MI:SS'),100,540008,'Y','org.adempiere.server.rpl.imp.RabbitMqImportProcessor','RabbitMQ',TO_TIMESTAMP('2019-02-28 13:26:04','YYYY-MM-DD HH24:MI:SS'),100,'RabbitMQ')
+;
