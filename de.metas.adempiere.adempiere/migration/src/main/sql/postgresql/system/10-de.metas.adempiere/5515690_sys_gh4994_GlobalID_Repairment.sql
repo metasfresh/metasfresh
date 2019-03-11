f6a95b6572a89c18d6153bbc0ee88f8ea175f606
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-03-11 17:58:10 +0200

    #4994 repair BPGlobalID in I_User
    https://github.com/metasfresh/metasfresh/issues/4994

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5515690_sys_gh4994_GlobalID_Repairment.sql
+++
@@ -1,8 +0,0 @@
--- ALTER TABLE I_BPartner
--- DROP COLUMN IF EXISTS 
--- GlobalID;
-
-
-ALTER TABLE I_User
-DROP COLUMN IF EXISTS 
-GlobalID;
--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5515730_sys_gh4994_GlobalID_I_User_DDL.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5515730_sys_gh4994_GlobalID_I_User_DDL.sql
@@ -1,7 +1,14 @@
 
 
--- 2019-03-11T15:25:07.175
+ALTER TABLE I_User
+DROP COLUMN IF EXISTS 
+GlobalID;
+
+
+
+
+-- 2019-03-11T17:56:52.703
 -- #298 changing anz. stellen
-/* DDL */ SELECT public.db_alter_table('I_User','ALTER TABLE public.I_User ADD COLUMN GlobalID VARCHAR(250)')
+/* DDL */ SELECT public.db_alter_table('I_User','ALTER TABLE public.I_User ADD COLUMN BP_GlobalID VARCHAR(10)')
 ;
 
