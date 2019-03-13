d805b3f769095b4e475006de8015c8540f42ba77
siemiatj <kuba@saskla.cz>
2019-03-13 16:12:04 +0100

    - move files to /cypress folder

---
+++ cypress/integration/contracts/flatrate_conditions_refund_tiered_amount_spec.js
@@ -0,0 +1,71 @@
+/// <reference types="Cypress" />
+
+import { createAndCompleteTransition, createAndCompleteRefundAmountConditions } from '../../support/utils/contract';
+import { BPartner, BPartnerLocation } from '../../support/utils/bpartner';
+import { DiscountSchema, DiscountBreak } from '../../support/utils/discountschema';
+//import { PurchaseOrder, PurchaseOrderLine } from '../../support/utils/purchase_order';
+
+describe('Create tiered amount-based (TA) refund conditions', function() {
+    before(function() {
+        // login before each test and open the flatrate conditions window
+        cy.loginByForm();
+    });
+    
+    it('Create tiered amount-based refund conditions and a vendor with a respective contract', function () {
+        const timestamp = new Date().getTime(); // used in the document names, for ordering
+
+        const transitionName = `Transition (TA) ${timestamp}`;
+        createAndCompleteTransition(transitionName, null, null);
+        cy.screenshot()
+
+        const conditionsName = `Conditions (TA) ${timestamp}`;
+        createAndCompleteRefundAmountConditions(conditionsName, transitionName, 'T'/*Tiered / Gestaffelte Rückvergütung*/);
+        cy.screenshot()
+
+        const discountSchemaName = `DiscountSchema (TA) ${timestamp}`;
+        new DiscountSchema
+            .builder(discountSchemaName)
+            .addDiscountBreak(new DiscountBreak
+                .builder()
+                .setBreakValue(0)
+                .setBreakDiscount(0)
+                .build())
+            .build()
+            .apply()
+        cy.screenshot()
+
+        const bPartnerName = `Vendor (TA) ${timestamp}`;
+        new BPartner
+            .builder(bPartnerName)
+            .setVendor(true)
+            .setVendorDiscountSchema(discountSchemaName)
+            .addLocation(new BPartnerLocation
+                .builder('Address1')
+                .setCity('Cologne')
+                .setCountry('Deutschland')
+                .build())
+            .build()
+            .apply();
+        cy.screenshot()
+
+        cy.executeHeaderAction('C_Flatrate_Term_Create_For_BPartners')
+            .selectInListField('C_Flatrate_Conditions_ID', conditionsName, conditionsName)
+            .writeIntoStringField('StartDate', '01/01/2019{enter}')
+            .pressStartButton();
+        cy.screenshot();
+        // TODO: verify that we have a contract (maybe via selectReference)
+
+        // new PurchaseOrder
+        //     .builder()
+        //         .bPartner(bPartnerName)
+        //         .line(new PurchaseOrderLine
+        //             .builder()
+        //             .product('Convenience')
+        //             .tuQuantity('2')
+        //             .build())
+        //         .docAction('Complete')
+        //         .docStatus('Completed')
+        //     .build()
+        //     .apply();
+    });
+});
