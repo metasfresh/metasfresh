b340491985b4c5398102bd80013cca6aada4b651
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-03-13 13:20:25 +0200

    #4994 capital letter isManualInsert
    https://github.com/metasfresh/metasfresh/issues/4994

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5515990_sys_gh4994_AD_ImpFormat_IsManualImport_Uppercase.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5515990_sys_gh4994_AD_ImpFormat_IsManualImport_Uppercase.sql
@@ -89,4 +89,14 @@ UPDATE AD_ImpFormat SET IsManualImport='N' WHERE IsManualImport IS NULL
 
 
 
+-- 2019-03-13T13:19:35.354
+-- #298 changing anz. stellen
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='IsManualImport', PrintName='IsManualImport',Updated=TO_TIMESTAMP('2019-03-13 13:19:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576158 AND AD_Language='en_US'
+;
+
+-- 2019-03-13T13:19:35.633
+-- #298 changing anz. stellen
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576158,'en_US') 
+;
+
 
