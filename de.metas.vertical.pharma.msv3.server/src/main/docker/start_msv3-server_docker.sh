ac629872bda6a654c3dfc38414424f768dfd9306
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2019-03-15 12:18:03 +0100

    Update start_msv3-server_docker.sh
    #4939

--- de.metas.vertical.pharma.msv3.server/src/main/docker/start_msv3-server_docker.sh
+++ de.metas.vertical.pharma.msv3.server/src/main/docker/start_msv3-server_docker.sh
@@ -85,7 +85,9 @@ run_metasfresh()
 
 # thx to 
 # https://blog.csanchez.org/2017/05/31/running-a-jvm-in-a-container-without-getting-killed/
-MEMORY_PARAMS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
+# MaxRAMFraction=1 doesn't leave any memory for anything else and might cause the OS to kill the java process
+# local MEMORY_PARAMS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
+MEMORY_PARAMS="-Xmx512M"
 
  cd /opt/metasfresh-msv3-server/\
  && java\
