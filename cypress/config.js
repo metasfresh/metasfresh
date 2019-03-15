a87d35669d13b52df2bf3029a728877ead130cab
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-03-15 17:30:18 +0100

    4-e2e - Reorganize and further integrate e2e into CI process
    * delete cypress/config.js; user needs to create her own version based on config.js_template
    #4

--- cypress/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  API_URL: 'http://localhost:8080/rest/api',
-  PLUGIN_API_URL: 'http://localhost:9192/',
-  WS_URL: 'http://localhost:8080/stomp',
-  username: 'metasfresh',
-  password: 'metasfresh'
-};
