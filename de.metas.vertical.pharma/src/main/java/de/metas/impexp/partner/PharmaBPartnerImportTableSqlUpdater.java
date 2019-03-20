064d17e3363cf82960a8535e8a115eb5830112fb
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-03-20 16:38:31 +0200

    #5058 Add utility class for updating data using sql
    #5058 Manufacturer Import IFA

---
+++ de.metas.vertical.pharma/src/main/java/de/metas/impexp/partner/PharmaBPartnerImportTableSqlUpdater.java
@@ -0,0 +1,74 @@
+package de.metas.impexp.partner;
+
+import static org.adempiere.impexp.AbstractImportProcess.COLUMNNAME_I_ErrorMsg;
+import static org.adempiere.impexp.AbstractImportProcess.COLUMNNAME_I_IsImported;
+
+import org.adempiere.ad.trx.api.ITrx;
+import org.compiere.util.DB;
+import org.slf4j.Logger;
+
+import de.metas.logging.LogManager;
+import de.metas.vertical.pharma.model.I_I_Pharma_BPartner;
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2017 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+/**
+ * A helper class for {@link PharmaPartnerImportProcess} that performs the "dirty" but efficient SQL updates on the {@link I_I_Pharma_BPartner} table.
+ * Those updates complements the data from existing metasfresh records and flag those import records that can't yet be imported.
+ *
+ * @author metas-dev <dev@metasfresh.com>
+ *
+ */
+@UtilityClass
+public class PharmaBPartnerImportTableSqlUpdater
+{
+	private static final transient Logger logger = LogManager.getLogger(PharmaBPartnerImportTableSqlUpdater.class);
+
+	public void updateBPartnerImportTable(@NonNull final String whereClause)
+	{
+		dbUpdateBPartners(whereClause);
+	}
+
+	private void dbUpdateBPartners(@NonNull final String whereClause)
+	{
+		StringBuilder sql;
+		int no;
+		sql = new StringBuilder("UPDATE ")
+				.append(I_I_Pharma_BPartner.Table_Name + " i ")
+				.append(" SET C_BPartner_ID=(SELECT C_BPartner_ID FROM C_BPartner p")
+				.append(" WHERE i.B00ADRNR=p.IFA_Manufacturer AND i.AD_Client_ID=p.AD_Client_ID) ")
+				.append("WHERE C_BPartner_ID IS NULL")
+				.append(" AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
+		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
+
+		sql = new StringBuilder("UPDATE ")
+				.append(I_I_Pharma_BPartner.Table_Name)
+				.append(" SET " + COLUMNNAME_I_IsImported + "='E', " + COLUMNNAME_I_ErrorMsg + "=" + COLUMNNAME_I_ErrorMsg + "||'ERR=Invalid BPartner,' ")
+				.append("WHERE C_BPartner_ID IS NULL AND BPartner_Value IS NOT NULL")
+				.append(" AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
+		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
+		logger.warn("Invalid BPartner={}", no);
+	}
+}
