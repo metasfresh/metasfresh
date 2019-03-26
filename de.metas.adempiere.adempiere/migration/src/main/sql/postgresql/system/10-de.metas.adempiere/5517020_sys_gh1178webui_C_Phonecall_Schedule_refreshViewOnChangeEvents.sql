adb7381ec0dce5937b4f9a84198a3618df810f88
Teo Sarca <teo.sarca@metasfresh.com>
2019-03-26 08:44:54 +0200

    Create 5517020_sys_gh1178webui_C_Phonecall_Schedule_refreshViewOnChangeEvents.sql
    https://github.com/metasfresh/metasfresh-webui-api/issues/1178

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5517020_sys_gh1178webui_C_Phonecall_Schedule_refreshViewOnChangeEvents.sql
@@ -0,0 +1,5 @@
+-- 2019-03-26T08:32:21.267
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Table SET IsEnableRemoteCacheInvalidation='Y',Updated=TO_TIMESTAMP('2019-03-26 08:32:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=541176
+;
+
