651f4b1fc5ad8858b7bf36a1335a387c8744f6a0
TheBestPessimist <cristian@tbp.land>
2019-03-27 10:19:13 +0200

    Fixes after the review: add `final` modifiers to everything!
    https://github.com/metasfresh/metasfresh/issues/5071

--- de.metas.vertical.pharma/src/test/java/de/metas/vertical/pharma/PharmaOrderLineInputValidatorTest.java
+++ de.metas.vertical.pharma/src/test/java/de/metas/vertical/pharma/PharmaOrderLineInputValidatorTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Nullable;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -59,118 +60,118 @@ public class PharmaOrderLineInputValidatorTest
 	@Test
 	public void assertTypeBbPartnerCanReceiveNonRxProduct()
 	{
-		I_M_Product mProduct = createMProduct("NonPrescription(Non-RX)Product", false, false, "NonPrescriptionProduct");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("NonPrescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeB, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("NonPrescription(Non-RX)Product", false, false, "NonPrescriptionProduct");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("NonPrescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeB, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	@Test
 	public void assertTypeBbPartnerCanNotReceiveRxProduct()
 	{
-		I_M_Product mProduct = createMProduct("Prescription(RX)Product", true, false, "PrescriptionProduct");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("NonPrescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeB, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("Prescription(RX)Product", true, false, "PrescriptionProduct");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("NonPrescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeB, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
 		thrown.expect(AdempiereException.class);
 		// i' not sure if it's all right to check the expected message (below)
 		// because the string is an ITranslatableString, so the message may be changed depending on language settings.
 		thrown.expectMessage("NoPrescriptionPermission");
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	@Test
 	public void assertTypeAbPartnerCanReceiveNonRxProduct()
 	{
-		I_M_Product mProduct = createMProduct("NonPrescription(Non-RX)Product", false, false, "NonPrescriptionProduct");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("Prescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeA, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("NonPrescription(Non-RX)Product", false, false, "NonPrescriptionProduct");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("Prescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeA, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	@Test
 	public void assertTypeAbPartnerCanReceiveRxProduct()
 	{
-		I_M_Product mProduct = createMProduct("Prescription(RX)Product", true, false, "PrescriptionProduct");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("Prescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeA, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("Prescription(RX)Product", true, false, "PrescriptionProduct");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("Prescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeA, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	@Test
 	public void assertTypeAbPartnerCanNotReceiveRxNarcoticProduct()
 	{
-		I_M_Product mProduct = createMProduct("Narcotic Product", true, true, "Narcotic Product");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("Prescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeA, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("Narcotic Product", true, true, "Narcotic Product");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("Prescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeA, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
 		thrown.expect(AdempiereException.class);
 		// i' not sure if it's all right to check the expected message (below)
 		// because the string is an ITranslatableString, so the message may be changed depending on language settings.
 		thrown.expectMessage("NoNarcoticPermissions");
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	@Test
 	public void assertTypeBbPartnerCanNotReceiveRxNarcoticProduct()
 	{
-		I_M_Product mProduct = createMProduct("Narcotic Product", true, true, "Narcotic Product");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("NonPrescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeB, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("Narcotic Product", true, true, "Narcotic Product");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("NonPrescription Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeB, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
 		thrown.expect(AdempiereException.class);
 		// i' not sure if it's all right to check the expected message (below)
 		// because the string is an ITranslatableString, so the message may be changed depending on language settings.
 		thrown.expectMessage("NoNarcoticPermissions");
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	@Test
 	public void assertTypeCbPartnerCanReceiveRxNarcoticProduct()
 	{
-		I_M_Product mProduct = createMProduct("Narcotic Product", true, true, "Narcotic Product");
-		I_M_Warehouse mWarehouse = createMWarehouse();
-		I_C_BPartner cbPartner = createCBpartner("Narcotic Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeC, true);
-		I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
-		I_C_Order cOrder = createCOrder(true);
-		I_C_Currency cCurrency = createCCurrency();
-		I_C_UOM cUom = createCUom();
-		I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
+		final I_M_Product mProduct = createMProduct("Narcotic Product", true, true, "Narcotic Product");
+		final I_M_Warehouse mWarehouse = createMWarehouse();
+		final I_C_BPartner cbPartner = createCBpartner("Narcotic Product Customer", I_C_BPartner.ShipmentPermissionPharma_TypeC, true);
+		final I_C_PaymentTerm cPaymentTerm = createCPaymentTerm();
+		final I_C_Order cOrder = createCOrder(true);
+		final I_C_Currency cCurrency = createCCurrency();
+		final I_C_UOM cUom = createCUom();
+		final I_C_OrderLine cOrderLine = createCOrderLine(mProduct, mWarehouse, cbPartner, cPaymentTerm, cOrder, cCurrency, cUom, BigDecimal.ONE, BigDecimal.ONE);
 
-		orderLineInterceptor.validatePrescriptionProduct(cOrderLine);
+		orderLineInterceptor.validateTheProducts(cOrderLine);
 	}
 
 	/**
@@ -178,9 +179,9 @@ public class PharmaOrderLineInputValidatorTest
 	 * It's really bad to keep creating these for all tests in all the different projects.
 	 */
 
-	private I_C_OrderLine createCOrderLine(I_M_Product mProduct, I_M_Warehouse mWarehouse, I_C_BPartner cbPartner, I_C_PaymentTerm cPaymentTerm, I_C_Order cOrder, I_C_Currency cCurrency, I_C_UOM cUom, BigDecimal priceActual, BigDecimal qtyEntered)
+	private I_C_OrderLine createCOrderLine(final I_M_Product mProduct, final I_M_Warehouse mWarehouse, final I_C_BPartner cbPartner, final I_C_PaymentTerm cPaymentTerm, final I_C_Order cOrder, final I_C_Currency cCurrency, final I_C_UOM cUom, final BigDecimal priceActual, final BigDecimal qtyEntered)
 	{
-		I_C_OrderLine cOrderLine = newInstance(I_C_OrderLine.class);
+		final I_C_OrderLine cOrderLine = newInstance(I_C_OrderLine.class);
 		cOrderLine.setM_Product(mProduct);
 		cOrderLine.setM_Warehouse(mWarehouse);
 		cOrderLine.setC_BPartner(cbPartner);
@@ -197,23 +198,23 @@ public class PharmaOrderLineInputValidatorTest
 
 	private I_C_UOM createCUom()
 	{
-		I_C_UOM cUom = newInstance(I_C_UOM.class);
+		final I_C_UOM cUom = newInstance(I_C_UOM.class);
 		save(cUom);
 		return cUom;
 	}
 
 	private I_C_Currency createCCurrency()
 	{
-		I_C_Currency cCurrency = newInstance(I_C_Currency.class);
+		final I_C_Currency cCurrency = newInstance(I_C_Currency.class);
 		cCurrency.setISO_Code("EUR");
 		cCurrency.setStdPrecision(2);
 		save(cCurrency);
 		return cCurrency;
 	}
 
-	private I_C_Order createCOrder(boolean isSOTrx)
+	private I_C_Order createCOrder(final boolean isSOTrx)
 	{
-		I_C_Order cOrder = newInstance(I_C_Order.class);
+		final I_C_Order cOrder = newInstance(I_C_Order.class);
 		cOrder.setIsSOTrx(isSOTrx);
 		save(cOrder);
 		return cOrder;
@@ -221,15 +222,15 @@ public class PharmaOrderLineInputValidatorTest
 
 	private I_C_PaymentTerm createCPaymentTerm()
 	{
-		I_C_PaymentTerm cPaymentTerm = newInstance(I_C_PaymentTerm.class);
+		final I_C_PaymentTerm cPaymentTerm = newInstance(I_C_PaymentTerm.class);
 		save(cPaymentTerm);
 		return cPaymentTerm;
 	}
 
-	private I_C_BPartner createCBpartner(String name, String shipmentPermissionPharma, boolean isCustomer)
+	private I_C_BPartner createCBpartner(final String name, final String shipmentPermissionPharma, final boolean isCustomer)
 	{
 		// since this is a sales order, this should be the seller, right?
-		I_C_BPartner cbPartner = newInstance(I_C_BPartner.class);
+		final I_C_BPartner cbPartner = newInstance(I_C_BPartner.class);
 		cbPartner.setName(name);
 		cbPartner.setShipmentPermissionPharma(shipmentPermissionPharma);
 		if (shipmentPermissionPharma.equals(I_C_BPartner.ShipmentPermissionPharma_TypeA) || shipmentPermissionPharma.equals(I_C_BPartner.ShipmentPermissionPharma_TypeC))
@@ -247,14 +248,14 @@ public class PharmaOrderLineInputValidatorTest
 
 	private I_M_Warehouse createMWarehouse()
 	{
-		I_M_Warehouse mWarehouse = newInstance(I_M_Warehouse.class);
+		final I_M_Warehouse mWarehouse = newInstance(I_M_Warehouse.class);
 		save(mWarehouse);
 		return mWarehouse;
 	}
 
-	private I_M_Product createMProduct(String name, boolean isPrescription, boolean isNarcotic, String vitaminC)
+	private I_M_Product createMProduct(final String name, final boolean isPrescription, final boolean isNarcotic, @Nullable final String vitaminC)
 	{
-		I_M_Product mProduct = newInstance(I_M_Product.class);
+		final I_M_Product mProduct = newInstance(I_M_Product.class);
 		mProduct.setName(name);
 		mProduct.setIsPrescription(isPrescription);
 		mProduct.setIsNarcotic(isNarcotic);
