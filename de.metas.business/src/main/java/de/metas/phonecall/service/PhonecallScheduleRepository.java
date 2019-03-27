7394f27b13532d92954a8e963de33e9140216c00
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-03-27 13:02:03 +0200

    #4951 Set the salesrep in phonecall schedule from the partner
    https://github.com/metasfresh/metasfresh/issues/4951

--- de.metas.business/src/main/java/de/metas/phonecall/service/PhonecallScheduleRepository.java
+++ de.metas.business/src/main/java/de/metas/phonecall/service/PhonecallScheduleRepository.java
@@ -5,15 +5,18 @@ import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
 import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
 
 import org.adempiere.user.UserId;
+import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_C_Phonecall_Schedule;
 import org.compiere.util.TimeUtil;
 import org.springframework.stereotype.Repository;
 
 import de.metas.bpartner.BPartnerLocationId;
+import de.metas.bpartner.service.IBPartnerDAO;
 import de.metas.phonecall.PhonecallSchedule;
 import de.metas.phonecall.PhonecallScheduleId;
 import de.metas.phonecall.PhonecallSchemaVersionId;
 import de.metas.phonecall.PhonecallSchemaVersionLineId;
+import de.metas.util.Services;
 import lombok.NonNull;
 
 /*
@@ -69,7 +72,8 @@ public class PhonecallScheduleRepository
 		scheduleRecord.setIsCalled(schedule.isCalled());
 		scheduleRecord.setIsOrdered(schedule.isOrdered());
 
-		scheduleRecord.setSalesRep_ID(UserId.toRepoId(schedule.getSalesRepId()));
+		final I_C_BPartner bpartner = Services.get(IBPartnerDAO.class).getById(schedule.getBpartnerAndLocationId().getBpartnerId());
+		scheduleRecord.setSalesRep_ID(bpartner.getSalesRep_ID());
 
 		saveRecord(scheduleRecord);
 	}
