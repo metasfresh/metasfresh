9c6805ccd7abca94e90e701adfc5e9bbac7aa718
metas-dh <daniela.hoffmann@metasfresh.com>
2019-04-04 16:37:14 +0200

    13 Create Attribute Masterdata
    Create Attribute Masterdata

---
+++ cypress/integration/dataEntry/attributeset_attributes_setup_spec.js
@@ -0,0 +1,45 @@
+describe('Create Attribute Masterdata for Automatic End2End Tests with cypress https://github.com/metasfresh/me03/issues/1362', function() {
+  before(function() {
+    // login before each test
+    cy.loginByForm();
+  });
+
+  it('Create a new Attribute Set and Attributes', function() {
+
+    //create Attribute1
+    cy.visitWindow('260', 'NEW');
+    cy.writeIntoStringField('Name', 'TestAttribute1');
+    cy.writeIntoStringField('Description', 'TestAttribute1');
+    cy.clearField('Value');
+    cy.writeIntoStringField('Value', 'TestAttribute1');
+    
+    cy.selectInListField('AttributeValueType', 'List')
+    cy.clickOnCheckBox('IsInstanceAttribute')    
+    cy.clickOnCheckBox('IsPricingRelevant')  
+    cy.clickOnCheckBox('IsStorageRelevant')  
+    cy.clickOnCheckBox('IsAttrDocumentRelevant')  
+
+
+   //create AttributeValue1
+    cy.get('#tab_M_AttributeValue').click(); 
+    cy.pressAddNewButton();
+    cy.writeIntoStringField('Name', 'TestAttributeName1',true);
+    cy.writeIntoStringField('Value', 'TestAttributeValue1',true);
+    cy.pressDoneButton();
+
+
+   //create AttributeSet1
+   cy.visitWindow('256', 'NEW');
+   cy.writeIntoStringField('Name', 'TestAttributeSet1');
+   cy.selectInListField('MandatoryType', 'Not Mandatary')
+   
+
+  //set AttributeValue
+   cy.get('#tab_M_AttributeUse').click(); 
+   cy.pressAddNewButton();
+   cy.selectInListField('M_Attribute_ID', 'TestAttribute1_TestAttribute1');
+   cy.pressDoneButton();
+
+ });
+});
+
