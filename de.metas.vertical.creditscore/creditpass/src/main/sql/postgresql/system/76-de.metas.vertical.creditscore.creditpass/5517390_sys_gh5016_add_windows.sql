c21bc3cde403c23b2d0c72b974a0c008dec989b1
Teo Sarca <teo.sarca@metasfresh.com>
2019-04-08 12:53:17 +0300

    Update 5517390_sys_gh5016_add_windows.sql
    https://github.com/metasfresh/metasfresh/issues/5016

--- de.metas.vertical.creditscore/creditpass/src/main/sql/postgresql/system/76-de.metas.vertical.creditscore.creditpass/5517390_sys_gh5016_add_windows.sql
+++ de.metas.vertical.creditscore/creditpass/src/main/sql/postgresql/system/76-de.metas.vertical.creditscore.creditpass/5517390_sys_gh5016_add_windows.sql
@@ -1497,8 +1497,8 @@ INSERT INTO AD_Element_Link (AD_Client_ID,AD_Element_ID,AD_Element_Link_ID,AD_Fi
 
 -- 2019-03-20T14:38:15.736
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Element_Link (AD_Client_ID,AD_Element_ID,AD_Element_Link_ID,AD_Field_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,575995,627466,572919,0,140,TO_TIMESTAMP('2019-03-20 14:38:15','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2019-03-20 14:38:15','YYYY-MM-DD HH24:MI:SS'),100)
-;
+-- INSERT INTO AD_Element_Link (AD_Client_ID,AD_Element_ID,AD_Element_Link_ID,AD_Field_ID,AD_Org_ID,AD_Window_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,575995,627466,572919,0,140,TO_TIMESTAMP('2019-03-20 14:38:15','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2019-03-20 14:38:15','YYYY-MM-DD HH24:MI:SS'),100)
+-- ;
 
 -- 2019-03-20T14:38:15.801
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
