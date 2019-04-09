18c1367662df0f05b2156c037beabb6ec19bb913
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-04-09 13:19:04 +0300

    #5125 Repair names
    #5125 Add several functions for getting manufacturing order details

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Manufacturing_Order_Description.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Manufacturing_Order_Description.sql
@@ -1,7 +1,7 @@
 DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Manufacturing_Order_Description( IN record_id numeric, IN ad_language Character Varying(6) );
 
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Manufacturing_Order_Description
-(IN record_id   numeric,                                              
+(IN record_id   numeric,
  IN ad_language character Varying(6))
 
   RETURNS TABLE
@@ -19,12 +19,16 @@ SELECT
   pp.DocumentNo,
   pp.DateOrdered,
   pbom.value,
-  pbom.name,
+  coalesce(pt.name, pbom.name) as name,
   o.DocumentNo                          as orderno,
   COALESCE(dtt.PrintName, dt.PrintName) AS PrintName
 FROM PP_Order pp
   JOIN PP_Product_BOM bom on pp.PP_Product_BOM_ID = bom.PP_Product_BOM_ID
-  JOIN m_product pbom on bom.m_product_id = pbom.m_product_id
+
+   -- Product and its translation
+  JOIN M_product pbom on bom.m_product_id = pbom.m_product_id
+  LEFT OUTER JOIN M_Product_Trl pt ON bom.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = $2 AND pt.isActive ='Y'
+
   LEFT JOIN C_OrderLine ol on pp.c_orderline_id = ol.c_orderline_id
   LEFT JOIN c_order o on ol.C_Order_ID = o.c_order_id
   LEFT OUTER JOIN C_DocType dt ON pp.C_DocTypeTarget_ID = dt.C_DocType_ID AND dt.isActive = 'Y'
--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5518792_sys_gh5125_AddFunction_Docs_Manufacturing_Order_Details.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5518792_sys_gh5125_AddFunction_Docs_Manufacturing_Order_Details.sql
@@ -1,36 +1,41 @@
-DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Manufacturing_Order_Details( IN record_id numeric, IN ad_language Character Varying(6) );
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Manufacturing_Order_Description( IN record_id numeric, IN ad_language Character Varying );
 
-CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Manufacturing_Order_Details
-(IN record_id   numeric,                                              
- IN ad_language character Varying(6))
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Manufacturing_Order_Description
+  (IN record_id   numeric,
+   IN ad_language character Varying)
 
   RETURNS TABLE
   (
-    documentno  character varying(30),
-    dateordered timestamp with time zone,
-    value       character varying(60),
-    name        character varying(255),
-    orderno     character varying(30),
-    PrintName   character varying(60)
+    line            numeric,
+    qtyrequiered    numeric,
+    uomsymbol       character varying,
+    value           character varying,
+    vendorProductNo character varying,
+    description     character varying
   )
 AS
 $$
+
 SELECT
-  pp.DocumentNo,
-  pp.DateOrdered,
-  pbom.value,
-  pbom.name,
-  o.DocumentNo                          as orderno,
-  COALESCE(dtt.PrintName, dt.PrintName) AS PrintName
-FROM PP_Order pp
-  JOIN PP_Product_BOM bom on pp.PP_Product_BOM_ID = bom.PP_Product_BOM_ID
-  JOIN m_product pbom on bom.m_product_id = pbom.m_product_id
-  LEFT JOIN C_OrderLine ol on pp.c_orderline_id = ol.c_orderline_id
-  LEFT JOIN c_order o on ol.C_Order_ID = o.c_order_id
-  LEFT OUTER JOIN C_DocType dt ON pp.C_DocTypeTarget_ID = dt.C_DocType_ID AND dt.isActive = 'Y'
-  LEFT OUTER JOIN C_DocType_Trl dtt
-    ON pp.C_DocTypeTarget_ID = dtt.C_DocType_ID AND dtt.AD_Language = $2 AND dtt.isActive = 'Y'
-WHERE pp.PP_Order_ID = $1 AND o.isActive = 'Y'
+  line,
+  qtyrequiered,
+  COALESCE(uom.UOMSymbol, uomt.UOMSymbol) AS UOMSymbol,
+  p.value,
+  coalesce(bpp.productno, p.value)        as vendorProductNo,
+  coalesce(pt.description, p.description) as description
+FROM PP_Order_BOMLine bomLine
+
+  -- Product and its translation
+  JOIN M_Product p ON bomLine.m_product_id = p.m_product_id AND p.isActive = 'Y'
+  LEFT OUTER JOIN M_Product_Trl pt ON bomLine.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = $2 AND pt.isActive = 'Y'
+  -- Unit of measurement and its translation
+  JOIN c_uom uom on bomLine.c_uom_id = uom.c_uom_id AND uom.isActive = 'Y'
+  LEFT OUTER JOIN C_UOM_Trl uomt ON bomLine.C_UOM_ID = uomt.C_UOM_ID AND uomt.AD_Language = $2
+  LEFT JOIN getc_bpartner_product_vendor(p.m_product_id) bpp on 1 = 1
+WHERE
+  bomLine.PP_Order_ID = $1 AND bomLine.isActive = 'Y'
+ORDER BY
+  line
 $$
 LANGUAGE sql
 STABLE;
