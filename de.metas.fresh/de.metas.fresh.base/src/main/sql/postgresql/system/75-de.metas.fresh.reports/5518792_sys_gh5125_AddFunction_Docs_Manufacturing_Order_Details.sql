9c64e5ebd9db6264d58a58b3e5c90956a9224b64
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-04-11 09:37:19 +0300

    #5125 Do not filter by active product and active uom
    #5125 Add several functions for getting manufacturing order details

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5518792_sys_gh5125_AddFunction_Docs_Manufacturing_Order_Details.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5518792_sys_gh5125_AddFunction_Docs_Manufacturing_Order_Details.sql
@@ -29,10 +29,10 @@ SELECT
 FROM PP_Order_BOMLine bomLine
 
   -- Product and its translation
-  JOIN M_Product p ON bomLine.m_product_id = p.m_product_id AND p.isActive = 'Y'
+  JOIN M_Product p ON bomLine.m_product_id = p.m_product_id 
   LEFT OUTER JOIN M_Product_Trl pt ON bomLine.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = p_ad_language AND pt.isActive = 'Y'
   -- Unit of measurement and its translation
-  JOIN c_uom uom on bomLine.c_uom_id = uom.c_uom_id AND uom.isActive = 'Y'
+  JOIN c_uom uom on bomLine.c_uom_id = uom.c_uom_id 
   LEFT OUTER JOIN C_UOM_Trl uomt ON bomLine.C_UOM_ID = uomt.C_UOM_ID AND uomt.AD_Language = p_ad_language
   LEFT JOIN getc_bpartner_product_vendor(p.m_product_id) bpp on 1 = 1
   LEFT JOIN de_metas_endcustomer_fresh_reports.get_hu_attribute_value_for_pp_order_and_pp_order_bomline(p_m_attribute_id, bomLine.pp_order_id, bomLine.pp_order_bomline_id)  as Attributes on 1=1
