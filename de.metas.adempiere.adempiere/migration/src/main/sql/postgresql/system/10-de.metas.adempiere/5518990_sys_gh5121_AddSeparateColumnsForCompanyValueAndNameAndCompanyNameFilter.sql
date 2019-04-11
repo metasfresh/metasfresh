15f82d622ad2fcb749b3fe46d283bd86b3dc0b06
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-04-11 18:58:21 +0300

    #5121 Use C_Bpartner as value
    #5121  Create Company Phone Book window

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5518990_sys_gh5121_AddSeparateColumnsForCompanyValueAndNameAndCompanyNameFilter.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5518990_sys_gh5121_AddSeparateColumnsForCompanyValueAndNameAndCompanyNameFilter.sql
@@ -1,5 +1,3 @@
-
-
 -- 2019-04-10T17:32:15.453
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,DDL_NoForeignKey,EntityType,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRangeFilter,IsSelectionColumn,IsShowFilterIncrementButtons,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,567650,540648,0,10,114,'Companyname','(select Companyname from C_BPartner bp where bp.C_BPartner_ID = AD_User.C_BPartner_ID)',TO_TIMESTAMP('2019-04-10 17:32:15','YYYY-MM-DD HH24:MI:SS'),100,'N','D',255,'Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','Firmenname',0,0,TO_TIMESTAMP('2019-04-10 17:32:15','YYYY-MM-DD HH24:MI:SS'),100,0)
@@ -188,4 +186,95 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 -- add company name in filter
 update ad_userquery set code='AND<^>Companyname<^> ILIKE /*icase*/ <^><^><~>AND<^>Firstname<^> ILIKE /*icase*/ <^><^><~>AND<^>Lastname<^> ILIKE /*icase*/ <^><^><~>AND<^>EMail<^> ILIKE /*icase*/ <^><^><~>AND<^>Phone<^> ILIKE /*icase*/ <^><^><~>AND<^>MobilePhone<^> ILIKE /*icase*/ <^><^><~>AND<^>IsDefaultContact<^>=<^><^>'
 where ad_userquery_id = 540051;
+
+
+
+
+
+
+-- 2019-04-11T18:19:37.205
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=558358
+;
+
+-- 2019-04-11T18:19:47.294
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=578824
+;
+
+-- 2019-04-11T18:19:47.340
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Field_Trl WHERE AD_Field_ID=578824
+;
+
+-- 2019-04-11T18:19:47.346
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Field WHERE AD_Field_ID=578824
+;
+
+-- 2019-04-11T18:19:52.451
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Column_Trl WHERE AD_Column_ID=567651
+;
+
+-- 2019-04-11T18:19:52.455
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=567651
+;
+
+-- 2019-04-11T18:20:22.280
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,578456,0,541699,558361,542436,'F',TO_TIMESTAMP('2019-04-11 18:20:22','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Business Partner_C_BPartner_ID_Business Partner',5,0,0,TO_TIMESTAMP('2019-04-11 18:20:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-04-11T18:20:42.027
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsDisplayed='Y', Help='A search key allows you a fast method of finding a particular record.
+If you leave the search key empty, the system automatically creates a numeric number.  The document sequence used for this fallback number is defined in the "Maintain Sequence" window with the name "DocumentNo_<TableName>", where TableName is the actual name of the table (e.g. C_Order).', IsDisplayedGrid='Y', AD_Name_ID=620, Description='Suchschlüssel für den Eintrag im erforderlichen Format - muss eindeutig sein', Name='Suchschlüssel',Updated=TO_TIMESTAMP('2019-04-11 18:20:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=578456
+;
+
+-- 2019-04-11T18:20:42.037
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(620) 
+;
+
+-- 2019-04-11T18:20:42.220
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=578456
+;
+
+-- 2019-04-11T18:20:42.221
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(578456)
+;
+
+-- 2019-04-11T18:21:53.915
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,Description,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,540975,TO_TIMESTAMP('2019-04-11 18:21:53','YYYY-MM-DD HH24:MI:SS'),100,'Business Partner','D','Y','N','C_BPartner_value',TO_TIMESTAMP('2019-04-11 18:21:53','YYYY-MM-DD HH24:MI:SS'),100,'T')
+;
+
+-- 2019-04-11T18:21:53.926
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Reference_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Reference t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Reference_ID=540975 AND NOT EXISTS (SELECT 1 FROM AD_Reference_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Reference_ID=t.AD_Reference_ID)
+;
+
+-- 2019-04-11T18:22:43.658
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,Updated,UpdatedBy) VALUES (0,2893,0,540975,291,TO_TIMESTAMP('2019-04-11 18:22:43','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Y',TO_TIMESTAMP('2019-04-11 18:22:43','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-04-11T18:22:59.602
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET AD_Reference_ID=30, AD_Reference_Value_ID=540975,Updated=TO_TIMESTAMP('2019-04-11 18:22:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=578456
+;
+
+-- 2019-04-11T18:27:06.637
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_Table SET AD_Display=2901, IsValueDisplayed='N',Updated=TO_TIMESTAMP('2019-04-11 18:27:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=540975
+;
+
+-- 2019-04-11T18:37:06.931
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET IsSearchActive='N', AllowQuickInput='N',Updated=TO_TIMESTAMP('2019-04-11 18:37:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541699
+;
 
