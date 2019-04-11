efb9a6042dc088c9a1ada7ddc63f11f03f446571
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2019-04-11 08:24:27 +0200

    order by SeqNo (before name)
    #5125

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5518790_sys_gh5125_AddFunction_getc_bpartner_product_vendor.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5518790_sys_gh5125_AddFunction_getc_bpartner_product_vendor.sql
@@ -14,11 +14,11 @@ FROM C_BPartner_Product bpp
 WHERE true
       AND bpp.M_Product_id = p_M_Product_id
       AND bpp.usedforvendor = 'Y'
-ORDER BY bpp.iscurrentvendor desc, ProductNo, ProductName, seqno
+ORDER BY bpp.iscurrentvendor desc, SeqNo, ProductNo, ProductName
 LIMIT 1
 $$;
 
 comment on function getc_bpartner_product_vendor(p_m_product_id numeric)
-is 'This function returns a VENDOR C_BPartner_Product record with a matching matching given product-Id
+is 'This function returns a VENDOR C_BPartner_Product record with a matching  given product-Id
 If multiple C_BPartner_Product records match, the one with iscurrentvendor on Y and the lowest SeqNo is returned.
 ';
