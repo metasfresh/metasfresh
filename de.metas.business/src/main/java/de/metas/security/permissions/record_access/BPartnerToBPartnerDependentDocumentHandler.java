1ff5609a20000694349375e5e76bb64bc14db7f7
Teo Sarca <teo.sarca@metasfresh.com>
2019-04-12 15:59:40 +0300

    Create BPartnerToBPartnerDependentDocumentHandler.java
    https://github.com/metasfresh/metasfresh/issues/5027

---
+++ de.metas.business/src/main/java/de/metas/security/permissions/record_access/BPartnerToBPartnerDependentDocumentHandler.java
@@ -0,0 +1,55 @@
+package de.metas.security.permissions.record_access;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.adempiere.util.lang.impl.TableRecordReference;
+import org.compiere.model.I_C_BPartner;
+
+import de.metas.bpartner.BPartnerId;
+
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+public class BPartnerToBPartnerDependentDocumentHandler implements BPartnerDependentDocumentHandler
+{
+
+	@Override
+	public String getDocumentTableName()
+	{
+		return I_C_BPartner.Table_Name;
+	}
+
+	@Override
+	public Optional<BPartnerId> extractBPartnerIdFromDependentDocument(final TableRecordReference documentRef)
+	{
+		documentRef.assertTableName(I_C_BPartner.Table_Name);
+		return BPartnerId.optionalOfRepoId(documentRef.getRecord_ID());
+	}
+
+	@Override
+	public Stream<TableRecordReference> streamRelatedDocumentsByBPartnerId(final BPartnerId bpartnerId)
+	{
+		// TODO fetch child BPartners from parent `bpartnerId`
+		return Stream.empty();
+	}
+}
