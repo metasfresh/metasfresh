5be80e587fc9f6093cf23b08257e6d579b22a5d8
Mark Krake <mark.krake@metasfresh.com>
2019-04-13 11:22:21 +0200

    Adds setup spec. Modifies product Object. #40
    https://github.com/metasfresh/metasfresh-e2e/issues/40

---
+++ cypress/integration/product/productcategory_product_setup_spec.1.js
@@ -0,0 +1,37 @@
+import { Product } from '../../support/utils/product';
+
+describe('Create Attribute Masterdata for Automatic End2End Tests with cypress https://github.com/metasfresh/metasfresh-e2e/issues/13', function() {
+  before(function() {
+    // login before each test
+    cy.loginByForm();
+  });
+
+  const timestamp = new Date().getTime(); // used in the document names, for ordering
+  const productName = `ProductName ${timestamp}`;
+  const productValue = `ProductNameValue ${timestamp}`;
+
+  it('Create a new ProductCategory', function() {
+    cy.fixture('misc/simple_product.json').then(productJson => {
+      Object.assign(new Product(), productJson)
+        .setName(productName)
+        .setValue(productValue)
+        .apply();
+    });
+  });
+});
+
+describe('Create a Product Category and Products with Product Price', function() {
+  before(function() {
+    // login before each test
+    cy.loginByForm();
+  });
+
+  it('Create a new Product Category and Products', function() {
+    //create ProductCategory
+    cy.visitWindow('144', 'NEW');
+    cy.writeIntoStringField('Name', 'TestProductCategory1');
+    cy.clearField('Value');
+    cy.writeIntoStringField('Value', 'TestProductCategory1');
+    cy.selectInListField('M_Product_Category_ID', 'Convenience Früchte');
+  });
+});
--- cypress/support/utils/product.js
+++ cypress/support/utils/product.js
@@ -11,9 +11,52 @@ export class Product {
     return this;
   }
 
-  setValue(name) {
-    cy.log(`Product - set value = ${name}`);
-    this.name = name;
+  setValue(value) {
+    cy.log(`Product - set value = ${value}`);
+    this.value = value;
+    return this;
+  }
+
+  setDescription(description) {
+    cy.log(`Product - set description = ${description}`);
+    this.description = description;
+    return this;
+  }
+
+  setStocked(isPurchased) {
+    cy.log(`Product - set isPurchased = ${isPurchased}`);
+    this.isPurchased = isPurchased;
+    return this;
+  }
+
+  setSold(isSold) {
+    cy.log(`Product - set isSold = ${isSold}`);
+    this.isSold = isSold;
+    return this;
+  }
+
+  setDiverse(isDiverse) {
+    cy.log(`Product - set isDiverse = ${isDiverse}`);
+    this.isDiverse = isDiverse;
+    return this;
+  }
+
+  setProductType(c_uom_id) {
+    cy.log(`Product - set UOM = ${c_uom_id}`);
+    this.c_uom_id = c_uom_id;
+    return this;
+  }
+
+  setProductCategory(m_product_category_id) {
+    cy.log(`Product - set Product Category = ${m_product_category_id}`);
+    this.m_product_category_id = m_product_category_id;
+    return this;
+  }
+
+  apply() {
+    cy.log(`Product - apply - START (name=${this.name})`);
+    applyProduct(this);
+    cy.log(`Product - apply - END (name=${this.name})`);
     return this;
   }
 }
@@ -29,4 +72,31 @@ export class ProductCategory {
     this.name = name;
     return this;
   }
+}
+
+function applyProduct(product) {
+  describe(`Create new Product ${product.name}`, function() {
+    cy.visitWindow('140', 'NEW');
+    cy.writeIntoStringField('Name', product.name);
+
+    cy.clearField('Value');
+    cy.writeIntoStringField('Value', product.value);
+
+    cy.selectInListField('M_Product_Category_ID', product.m_product_category_id);
+
+    cy.writeIntoStringField('Description', product.description);
+
+    if (product.isStocked) {
+      cy.clickOnCheckBox('IsStocked');
+    }
+    if (product.isPurchased) {
+      cy.clickOnCheckBox('IsPurchased');
+    }
+    if (product.isSold) {
+      cy.clickOnCheckBox('IsSold');
+    }
+    if (product.isDiverse) {
+      cy.clickOnCheckBox('IsDiverse');
+    }
+  });
 }
