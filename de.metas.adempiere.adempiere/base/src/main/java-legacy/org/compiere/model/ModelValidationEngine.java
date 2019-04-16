ca456bafe4d6f6dd29ad6df204468d519f711d89
Teo Sarca <teo.sarca@metasfresh.com>
2019-04-16 16:37:59 +0300

    Update ModelValidationEngine.java
    improve error message
    
    https://github.com/metasfresh/metasfresh/issues/5027

--- de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/ModelValidationEngine.java
+++ de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/ModelValidationEngine.java
@@ -277,9 +277,12 @@ public class ModelValidationEngine implements IModelValidationEngine
 	private static Collection<Object> getSpringInterceptors()
 	{
 		final ApplicationContext context = Adempiere.getSpringApplicationContext();
-		// NOTE: atm it returns null only when started from our tools (like the "model generator")
-		// but it's not preventing the tool execution because this is the last thing we do here and also because usually it's configured to not fail on init error.
-		// so we can leave with the NPE here
+		if (context == null)
+		{
+			// NOTE: atm it returns null only when started from our tools (like the "model generator")
+			// but it's not preventing the tool execution because this is the last thing we do here and also because usually it's configured to not fail on init error.
+			throw new AdempiereException("Cannot fetch Spring interceptors because spring context is not available");
+		}
 
 		final LinkedHashMap<String, Object> interceptorsByName = new LinkedHashMap<>();
 		interceptorsByName.putAll(context.getBeansWithAnnotation(org.adempiere.ad.modelvalidator.annotations.Interceptor.class));
