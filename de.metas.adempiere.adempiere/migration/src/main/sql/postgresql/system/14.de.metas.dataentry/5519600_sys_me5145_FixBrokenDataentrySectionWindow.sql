aea0ca8748a67cf9ef9813f3cd3d3c108ac3a7c9
TheBestPessimist <cristian@tbp.land>
2019-04-18 11:09:41 +0300

    Fix custom data entry section window -- line tab
    https://github.com/metasfresh/metasfresh/issues/5145

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/14.de.metas.dataentry/5519600_sys_me5145_FixBrokenDataentrySectionWindow.sql
@@ -0,0 +1,110 @@
+-- 2019-04-17T12:06:42.380
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Parent_Column_ID=564201,Updated=TO_TIMESTAMP('2019-04-17 12:06:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541591
+;
+
+-- 2019-04-17T12:58:29.446
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET AD_Table_ID=541182,Updated=TO_TIMESTAMP('2019-04-17 12:58:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=541591
+;
+
+-- 2019-04-17T12:58:29.531
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET AD_Column_ID=564280,Updated=TO_TIMESTAMP('2019-04-17 12:58:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=575731
+;
+
+-- 2019-04-17T12:58:29.631
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(102) 
+;
+
+-- 2019-04-17T12:58:29.997
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=575731
+;
+
+-- 2019-04-17T12:58:30.002
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(575731)
+;
+
+-- 2019-04-17T12:58:30.034
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET AD_Column_ID=564281,Updated=TO_TIMESTAMP('2019-04-17 12:58:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=575732
+;
+
+-- 2019-04-17T12:58:30.047
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(113) 
+;
+
+-- 2019-04-17T12:58:30.217
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=575732
+;
+
+-- 2019-04-17T12:58:30.219
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(575732)
+;
+
+-- 2019-04-17T12:58:30.237
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET AD_Column_ID=564284,Updated=TO_TIMESTAMP('2019-04-17 12:58:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=575733
+;
+
+-- 2019-04-17T12:58:30.250
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(348) 
+;
+
+-- 2019-04-17T12:58:30.523
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=575733
+;
+
+-- 2019-04-17T12:58:30.525
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(575733)
+;
+
+-- 2019-04-17T12:58:30.539
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET AD_Column_ID=564288,Updated=TO_TIMESTAMP('2019-04-17 12:58:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=575735
+;
+
+-- 2019-04-17T12:58:30.551
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(576133) 
+;
+
+-- 2019-04-17T12:58:30.556
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=575735
+;
+
+-- 2019-04-17T12:58:30.558
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(575735)
+;
+
+-- 2019-04-17T12:58:30.572
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET AD_Column_ID=564289,Updated=TO_TIMESTAMP('2019-04-17 12:58:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=575736
+;
+
+-- 2019-04-17T12:58:30.581
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(566) 
+;
+
+-- 2019-04-17T12:58:30.600
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=575736
+;
+
+-- 2019-04-17T12:58:30.602
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(575736)
+;
+
