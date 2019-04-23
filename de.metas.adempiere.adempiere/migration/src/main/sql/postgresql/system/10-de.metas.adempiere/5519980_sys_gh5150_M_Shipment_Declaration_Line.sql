751097707c9573ea9cad03fcc1d2fd0a8c3e4313
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-04-23 14:50:51 +0300

    #5150 Make packageSize String
    https://github.com/metasfresh/metasfresh/issues/5150

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5519980_sys_gh5150_M_Shipment_Declaration_Line.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5519980_sys_gh5150_M_Shipment_Declaration_Line.sql
@@ -243,3 +243,13 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 /* DDL */  select update_Column_Translation_From_AD_Element(543503) 
 ;
 
+
+
+-- 2019-04-23T13:27:27.305
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_ID=10,Updated=TO_TIMESTAMP('2019-04-23 13:27:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=567852
+;
+
+
+
+
