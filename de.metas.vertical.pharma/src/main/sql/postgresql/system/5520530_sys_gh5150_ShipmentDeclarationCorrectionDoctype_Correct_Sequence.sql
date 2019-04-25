317174fda642768db28c1d9e8abea22ee9a4f5ed
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-04-25 13:14:22 +0300

    #5150 repair shipment declaration correction doctype sequence
    https://github.com/metasfresh/metasfresh/issues/5150

---
+++ de.metas.vertical.pharma/src/main/sql/postgresql/system/5520530_sys_gh5150_ShipmentDeclarationCorrectionDoctype_Correct_Sequence.sql
@@ -0,0 +1,5 @@
+-- 2019-04-25T13:12:07.286
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET DocNoSequence_ID=555008,Updated=TO_TIMESTAMP('2019-04-25 13:12:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_DocType_ID=540972
+;
+
