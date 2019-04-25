91df759eda286cdaf709705c2af17cba6aec7094
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-04-25 14:34:17 +0300

    #5150 translations
    https://github.com/metasfresh/metasfresh/issues/5150

---
+++ de.metas.vertical.pharma/src/main/sql/postgresql/system/5520550_sys_gh5150_ShipmentDeclaration_Translations.sql
@@ -0,0 +1,160 @@
+-- 2019-04-25T14:23:56.175
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply', PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:23:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576686 AND AD_Language='en_US'
+;
+
+-- 2019-04-25T14:23:56.237
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576686,'en_US') 
+;
+
+-- 2019-04-25T14:24:01.916
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply', PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:24:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576686 AND AD_Language='nl_NL'
+;
+
+-- 2019-04-25T14:24:01.920
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576686,'nl_NL') 
+;
+
+-- 2019-04-25T14:24:25.710
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply', PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:24:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576670 AND AD_Language='nl_NL'
+;
+
+-- 2019-04-25T14:24:25.714
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576670,'nl_NL') 
+;
+
+-- 2019-04-25T14:24:29.752
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply', PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:24:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576670 AND AD_Language='en_US'
+;
+
+-- 2019-04-25T14:24:29.756
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576670,'en_US') 
+;
+
+-- 2019-04-25T14:27:52.867
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply (duplicate) ', PrintName='Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:27:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576687 AND AD_Language='en_US'
+;
+
+-- 2019-04-25T14:27:52.868
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576687,'en_US') 
+;
+
+-- 2019-04-25T14:27:56.597
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply (duplicate) ', PrintName='Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:27:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576687 AND AD_Language='nl_NL'
+;
+
+-- 2019-04-25T14:27:56.599
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576687,'nl_NL') 
+;
+
+-- 2019-04-25T14:29:02.456
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply Config', PrintName='Notification of supply Config',Updated=TO_TIMESTAMP('2019-04-25 14:29:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576676 AND AD_Language='en_US'
+;
+
+-- 2019-04-25T14:29:02.464
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576676,'en_US') 
+;
+
+-- 2019-04-25T14:29:08.320
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Notification of supply Config', PrintName='Notification of supply Config',Updated=TO_TIMESTAMP('2019-04-25 14:29:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576676 AND AD_Language='nl_NL'
+;
+
+-- 2019-04-25T14:29:08.321
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576676,'nl_NL') 
+;
+
+-- 2019-04-25T14:30:04.438
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='The shipment  cannot de reactivated because it has a completed notification of supply .',Updated=TO_TIMESTAMP('2019-04-25 14:30:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544914
+;
+
+-- 2019-04-25T14:30:16.906
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='Notification of supply Config',Updated=TO_TIMESTAMP('2019-04-25 14:30:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=544914
+;
+
+-- 2019-04-25T14:30:23.951
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='The shipment  cannot de reactivated because it has a completed notification of supply.',Updated=TO_TIMESTAMP('2019-04-25 14:30:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544914
+;
+
+-- 2019-04-25T14:30:27.733
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='The shipment  cannot de reactivated because it has a completed notification of supply.',Updated=TO_TIMESTAMP('2019-04-25 14:30:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=544914
+;
+
+-- 2019-04-25T14:30:32.030
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2019-04-25 14:30:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=544914
+;
+
+-- 2019-04-25T14:30:43.708
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='The shipment  cannot de reactivated because it has a completed notification of supply.',Updated=TO_TIMESTAMP('2019-04-25 14:30:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Message_ID=544914
+;
+
+-- 2019-04-25T14:31:51.624
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name=' Notification of supply (duplicate) ', PrintName=' Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:31:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=540972
+;
+
+-- 2019-04-25T14:32:01.637
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name=' Notification of supply (duplicate) ', PrintName=' Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:32:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND C_DocType_ID=540972
+;
+
+-- 2019-04-25T14:32:19.557
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name='Narcotics Notification of supply (duplicate) ', PrintName='Narcotics Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:32:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND C_DocType_ID=540972
+;
+
+-- 2019-04-25T14:32:26.605
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name='Narcotics Notification of supply (duplicate) ', PrintName='Narcotics Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:32:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=540972
+;
+
+-- 2019-04-25T14:32:36.427
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name='Notification of supply (duplicate) ', PrintName='Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:32:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND C_DocType_ID=540972
+;
+
+-- 2019-04-25T14:32:41.564
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name='Notification of supply (duplicate) ', PrintName='Notification of supply (duplicate) ',Updated=TO_TIMESTAMP('2019-04-25 14:32:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=540972
+;
+
+-- 2019-04-25T14:33:17.546
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET Name='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:33:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_DocType_ID=540970
+;
+
+-- 2019-04-25T14:33:20.259
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType SET PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:33:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_DocType_ID=540970
+;
+
+-- 2019-04-25T14:33:26.060
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name='Notification of supply', PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:33:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=540970
+;
+
+-- 2019-04-25T14:33:36.667
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_DocType_Trl SET Name='Notification of supply', PrintName='Notification of supply',Updated=TO_TIMESTAMP('2019-04-25 14:33:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND C_DocType_ID=540970
+;
+
