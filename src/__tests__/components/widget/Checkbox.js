773c7a95e4ae5612eeff6a375b0dc9d83fffa9e1
siemiatj <kuba@saskla.cz>
2019-04-30 11:10:02 +0200

    - more tests #2256

--- src/__tests__/components/widget/Checkbox.js
+++ src/__tests__/components/widget/Checkbox.js
@@ -52,4 +52,31 @@ describe('Checkbox component', () => {
       expect(wrapper.find('.input-checkbox-tick').html()).toContain('checked'); 
     });
   });
+
+  describe('functional tests', () => {
+    it('renders without errors with props set', () => {
+      const handlePatchSpy = jest.fn();
+      const props = createDummyProps({
+        ...fixtures.data1.widgetProps,
+        handlePatch: handlePatchSpy
+      });
+
+      const updatedWidgetData = [
+        {
+          ...fixtures.data1.widgetProps.widgetData,
+          value: false,
+        }
+      ];
+      const wrapper = mount(<Checkbox {...props} />);
+
+      expect(wrapper.find('.input-checkbox-tick').html()).toContain('checked');
+      wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: false } })
+
+      expect(handlePatchSpy).toHaveBeenCalled();
+
+      wrapper.setProps({ widgetData: updatedWidgetData });
+
+      expect(wrapper.find('.input-checkbox-tick').html()).not.toContain('checked');
+    });
+  });
 });
