eedc1c209fa2d84bdc547b9497bd99e7bfaed019
TheBestPessimist <cristian@tbp.land>
2019-05-03 14:51:40 +0300

    Add test for the rest controller
    https://github.com/metasfresh/metasfresh/issues/5162

---
+++ de.metas.business.rest-api/src/test/java/de/metas/dataentry/rest_api/DataEntryRestControllerTest.java
@@ -0,0 +1,309 @@
+package de.metas.dataentry.rest_api;
+
+import de.metas.dataentry.data.DataEntryRecordRepository;
+import de.metas.dataentry.data.json.JSONDataEntryRecordMapper;
+import de.metas.dataentry.layout.DataEntryLayoutRepository;
+import de.metas.dataentry.model.I_DataEntry_Field;
+import de.metas.dataentry.model.I_DataEntry_Group;
+import de.metas.dataentry.model.I_DataEntry_Line;
+import de.metas.dataentry.model.I_DataEntry_ListValue;
+import de.metas.dataentry.model.I_DataEntry_Section;
+import de.metas.dataentry.model.I_DataEntry_SubGroup;
+import de.metas.dataentry.model.X_DataEntry_Field;
+import de.metas.dataentry.rest_api.dto.JsonDataEntry;
+import lombok.NonNull;
+import org.adempiere.ad.element.api.AdWindowId;
+import org.adempiere.test.AdempiereTestHelper;
+import org.compiere.model.I_AD_Tab;
+import org.compiere.model.I_AD_Table;
+import org.compiere.model.I_C_BPartner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.github.jsonSnapshot.SnapshotMatcher.expect;
+import static io.github.jsonSnapshot.SnapshotMatcher.start;
+import static io.github.jsonSnapshot.SnapshotMatcher.validateSnapshots;
+import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
+import static org.adempiere.model.InterfaceWrapperHelper.save;
+import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
+
+public class DataEntryRestControllerTest
+{
+
+	private static final int BPARTNER_REPO_ID = 123;
+	private DataEntryRestController dataEntryRestController;
+
+	@BeforeAll
+	static void initStatic()
+	{
+		AdempiereTestHelper.get().init();
+		start(AdempiereTestHelper.SNAPSHOT_CONFIG);
+	}
+
+	@AfterAll
+	static void afterAll()
+	{
+		validateSnapshots();
+	}
+
+	@BeforeEach void init()
+	{
+		final DataEntryLayoutRepository dataEntryLayoutRepository = new DataEntryLayoutRepository();
+		final JSONDataEntryRecordMapper jsonDataEntryRecordMapper = new JSONDataEntryRecordMapper();
+		final DataEntryRecordRepository dataEntryRecordRepository = new DataEntryRecordRepository(jsonDataEntryRecordMapper);
+		dataEntryRestController = new DataEntryRestController(dataEntryLayoutRepository, dataEntryRecordRepository);
+	}
+
+	@Test
+	void testFieldNamesAreNotChanged()
+	{
+		final I_DataEntry_Group groupRecord1 = createRecords();
+		final AdWindowId windowId_1 = AdWindowId.ofRepoId(groupRecord1.getDataEntry_TargetWindow_ID());
+
+		final I_C_BPartner bPartner = createBPartner("G0002");
+
+		final JsonDataEntry g0002 = dataEntryRestController.getByBPartnerValue(bPartner.getValue());
+		System.out.println(g0002);
+		expect(g0002).toMatchSnapshot();
+	}
+
+	private I_DataEntry_Group createRecords()
+	{
+		final AdWindowId windowId_1 = AdWindowId.ofRepoId(BPARTNER_REPO_ID);
+
+		final I_AD_Table tableRecord_1 = newInstance(I_AD_Table.class);
+		tableRecord_1.setTableName("tableRecord_1_tableName");
+		saveRecord(tableRecord_1);
+
+		final I_AD_Tab tabRecord_1 = newInstance(I_AD_Tab.class);
+		tabRecord_1.setAD_Window_ID(windowId_1.getRepoId());
+		tabRecord_1.setAD_Table(tableRecord_1);
+		saveRecord(tabRecord_1);
+
+		// group
+		final I_DataEntry_Group groupRecord1 = newInstance(I_DataEntry_Group.class);
+		groupRecord1.setDataEntry_TargetWindow_ID(windowId_1.getRepoId());
+		groupRecord1.setName("groupRecord1_name");
+		groupRecord1.setDescription("groupRecord1_description");
+		groupRecord1.setTabName("groupRecord1_tabName");
+		saveRecord(groupRecord1);
+
+		createSubgroup1Records(groupRecord1);
+
+		createSubgroup2Records(groupRecord1);
+
+		return groupRecord1;
+	}
+
+	private void createSubgroup1Records(@NonNull final I_DataEntry_Group groupRecord1)
+	{
+		final I_DataEntry_SubGroup subgroupRecord1_1 = newInstance(I_DataEntry_SubGroup.class);
+		subgroupRecord1_1.setDataEntry_Group(groupRecord1);
+		subgroupRecord1_1.setName("subgroupRecord1_1_name");
+		subgroupRecord1_1.setDescription("subgroupRecord1_1_description - seqNo20");
+		subgroupRecord1_1.setTabName("subgroupRecord1_1_tabName");
+		subgroupRecord1_1.setSeqNo(20);
+		saveRecord(subgroupRecord1_1);
+
+		final I_DataEntry_Section sectionRecord1_1_2 = newInstance(I_DataEntry_Section.class);
+		sectionRecord1_1_2.setDataEntry_SubGroup(subgroupRecord1_1);
+		sectionRecord1_1_2.setSeqNo(10);
+		sectionRecord1_1_2.setIsInitiallyClosed(false);
+		sectionRecord1_1_2.setName("sectionRecord1_1_2_name");
+		sectionRecord1_1_2.setSectionName("sectionRecord1_1_2_sectionName");
+		sectionRecord1_1_2.setDescription("sectionRecord1_1_2_description - seqNo10");
+		saveRecord(sectionRecord1_1_2);
+
+		final I_DataEntry_Line lineRecord1_1_2_1 = newInstance(I_DataEntry_Line.class);
+		lineRecord1_1_2_1.setDataEntry_Section(sectionRecord1_1_2);
+		lineRecord1_1_2_1.setSeqNo(10);
+		saveRecord(lineRecord1_1_2_1);
+
+		// note: the fields of group 1 are created in reverse order (SeqNo)
+		final I_DataEntry_Field fieldRecord1_1_2_1_1 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_1_2_1_1.setDataEntry_Line(lineRecord1_1_2_1);
+		fieldRecord1_1_2_1_1.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_Text);
+		fieldRecord1_1_2_1_1.setName("fieldRecord1_1_2_1_1_name");
+		fieldRecord1_1_2_1_1.setDescription("fieldRecord1_1_2_1_1_description - seqNo20");
+		fieldRecord1_1_2_1_1.setIsMandatory(true);
+		fieldRecord1_1_2_1_1.setSeqNo(20);
+		fieldRecord1_1_2_1_1.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_Personal);
+		saveRecord(fieldRecord1_1_2_1_1);
+
+		final I_DataEntry_Field fieldRecord1_1_2_1_2 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_1_2_1_2.setDataEntry_Line(lineRecord1_1_2_1);
+		fieldRecord1_1_2_1_2.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_Number);
+		fieldRecord1_1_2_1_2.setName("fieldRecord1_1_2_1_2_name");
+		fieldRecord1_1_2_1_2.setDescription("fieldRecord1_1_2_1_2_description - seqNo10");
+		fieldRecord1_1_2_1_2.setIsMandatory(false);
+		fieldRecord1_1_2_1_2.setSeqNo(10);
+		fieldRecord1_1_2_1_2.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_NotPersonal);
+		saveRecord(fieldRecord1_1_2_1_2);
+	}
+
+	private void createSubgroup2Records(@NonNull final I_DataEntry_Group groupRecord1)
+	{
+		final I_DataEntry_SubGroup subgroupRecord1_2 = newInstance(I_DataEntry_SubGroup.class);
+		subgroupRecord1_2.setDataEntry_Group(groupRecord1);
+		subgroupRecord1_2.setName("subgroupRecord1_2_name");
+		subgroupRecord1_2.setDescription("subgroupRecord1_2_description - seqNo10");
+		subgroupRecord1_2.setTabName("subgroupRecord1_2_tabName");
+		subgroupRecord1_2.setSeqNo(10);
+		saveRecord(subgroupRecord1_2);
+
+		// note: the section records are created in reverse order (SeqNo)
+		final I_DataEntry_Section sectionRecord1_2_1 = newInstance(I_DataEntry_Section.class);
+		sectionRecord1_2_1.setDataEntry_SubGroup(subgroupRecord1_2);
+		sectionRecord1_2_1.setSeqNo(20);
+		sectionRecord1_2_1.setIsInitiallyClosed(true);
+		sectionRecord1_2_1.setName("sectionRecord1_2_1_name");
+		sectionRecord1_2_1.setSectionName("sectionRecord1_2_1_sectionName");
+		sectionRecord1_2_1.setDescription("sectionRecord1_2_1_description - seqNo20");
+		saveRecord(sectionRecord1_2_1);
+
+		final I_DataEntry_Line lineRecord1_2_1_1 = newInstance(I_DataEntry_Line.class);
+		lineRecord1_2_1_1.setDataEntry_Section(sectionRecord1_2_1);
+		lineRecord1_2_1_1.setSeqNo(10);
+		saveRecord(lineRecord1_2_1_1);
+
+		final I_DataEntry_Field fieldRecord1_2_1_1_1 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_2_1_1_1.setDataEntry_Line(lineRecord1_2_1_1);
+		fieldRecord1_2_1_1_1.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_Date);
+		fieldRecord1_2_1_1_1.setName("fieldRecord1_2_1_1_1_name");
+		fieldRecord1_2_1_1_1.setDescription("fieldRecord1_2_1_1_1_description - seqNo10");
+		fieldRecord1_2_1_1_1.setIsMandatory(false);
+		fieldRecord1_2_1_1_1.setSeqNo(10);
+		fieldRecord1_2_1_1_1.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_Personal);
+		saveRecord(fieldRecord1_2_1_1_1);
+
+		final I_DataEntry_Field fieldRecord1_2_1_1_2 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_2_1_1_2.setDataEntry_Line(lineRecord1_2_1_1);
+		fieldRecord1_2_1_1_2.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_List);
+		fieldRecord1_2_1_1_2.setName("fieldRecord1_2_1_1_2_name");
+		fieldRecord1_2_1_1_2.setDescription("fieldRecord1_2_1_1_2_description");
+		fieldRecord1_2_1_1_2.setIsMandatory(false);
+		fieldRecord1_2_1_1_2.setSeqNo(20);
+		fieldRecord1_2_1_1_2.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_NotPersonal);
+		saveRecord(fieldRecord1_2_1_1_2);
+
+		final I_DataEntry_ListValue listValueRecord1_2_1_1_2_1 = newInstance(I_DataEntry_ListValue.class);
+		listValueRecord1_2_1_1_2_1.setDataEntry_Field(fieldRecord1_2_1_1_2);
+		listValueRecord1_2_1_1_2_1.setName("listValueRecord1_2_1_1_2_1_name");
+		listValueRecord1_2_1_1_2_1.setDescription("listValueRecord1_2_1_1_2_1_description - seqNo20");
+		listValueRecord1_2_1_1_2_1.setSeqNo(20);
+		saveRecord(listValueRecord1_2_1_1_2_1);
+
+		final I_DataEntry_ListValue listValueRecord1_2_1_1_2_2 = newInstance(I_DataEntry_ListValue.class);
+		listValueRecord1_2_1_1_2_2.setDataEntry_Field(fieldRecord1_2_1_1_2);
+		listValueRecord1_2_1_1_2_2.setName("listValueRecord1_2_1_1_2_2_name");
+		listValueRecord1_2_1_1_2_2.setDescription("listValueRecord1_2_1_1_2_2_description - seqNo10");
+		listValueRecord1_2_1_1_2_2.setSeqNo(10);
+		saveRecord(listValueRecord1_2_1_1_2_2);
+
+		final I_DataEntry_Field fieldRecord1_2_1_1_3 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_2_1_1_3.setDataEntry_Line(lineRecord1_2_1_1);
+		fieldRecord1_2_1_1_3.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_YesNo);
+		fieldRecord1_2_1_1_3.setName("fieldRecord1_2_1_1_3_name");
+		fieldRecord1_2_1_1_3.setDescription("fieldRecord1_2_1_1_3_description");
+		fieldRecord1_2_1_1_3.setIsMandatory(false);
+		fieldRecord1_2_1_1_3.setSeqNo(30);
+		fieldRecord1_2_1_1_3.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_NotPersonal);
+		saveRecord(fieldRecord1_2_1_1_3);
+
+		final I_DataEntry_Field fieldRecord1_2_1_1_4 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_2_1_1_4.setDataEntry_Line(lineRecord1_2_1_1);
+		fieldRecord1_2_1_1_4.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_Text);
+		fieldRecord1_2_1_1_4.setName("fieldRecord1_2_1_1_4_name");
+		fieldRecord1_2_1_1_4.setDescription("fieldRecord1_2_1_1_4_description");
+		fieldRecord1_2_1_1_4.setIsMandatory(true);
+		fieldRecord1_2_1_1_4.setSeqNo(40);
+		fieldRecord1_2_1_1_4.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_Personal);
+		saveRecord(fieldRecord1_2_1_1_4);
+
+		// the last field has no section so it shall be added to the "default" section
+		final I_DataEntry_Field fieldRecord1_2_1_1_5 = newInstance(I_DataEntry_Field.class);
+		fieldRecord1_2_1_1_5.setDataEntry_Line(lineRecord1_2_1_1);
+		fieldRecord1_2_1_1_5.setDataEntry_RecordType(X_DataEntry_Field.DATAENTRY_RECORDTYPE_LongText);
+		fieldRecord1_2_1_1_5.setName("fieldRecord1_2_1_1_5_name");
+		fieldRecord1_2_1_1_5.setDescription("fieldRecord1_2_1_1_5_description");
+		fieldRecord1_2_1_1_5.setIsMandatory(true);
+		fieldRecord1_2_1_1_5.setSeqNo(50);
+		fieldRecord1_2_1_1_5.setPersonalDataCategory(X_DataEntry_Field.PERSONALDATACATEGORY_SensitivePersonal);
+		saveRecord(fieldRecord1_2_1_1_5);
+	}
+
+	public static I_C_BPartner createBPartner(final String nameAndValue)
+	{
+		final I_C_BPartner bpartner = newInstance(I_C_BPartner.class);
+		bpartner.setValue(nameAndValue);
+		bpartner.setName(nameAndValue);
+		save(bpartner);
+
+		return bpartner;
+	}
+
+	//	private DataEntryGroup createGroup()
+	//	{
+	//		final DataEntrySubGroup subGroup = createSubGroup();
+	//		DataEntryGroup group = DataEntryGroup.builder()
+	//				.id(DataEntryGroupId.ofRepoId(100000))
+	//				.caption(ITranslatableString.constant("Group1-Tab1"))
+	//				.description(ITranslatableString.constant("Description of Example-Group1"))
+	//				.dataEntrySubGroup(subGroup)
+	//				.documentLinkColumnName()
+	//				.build();
+	//		return group;
+	//	}
+	//
+	//	private DataEntrySubGroup createSubGroup()
+	//	{
+	//		final DataEntrySubGroup subGroup = DataEntrySubGroup.builder()
+	//				.id(DataEntrySubGroupId.ofRepoId(100000))
+	//				.caption(ITranslatableString.constant("Group1-Tab1-SubTab1"))
+	//				.description(ITranslatableString.constant("Description for SubGroup1-1"))
+	//				.dataEntrySections()
+	//				.dataEntrySection()
+	//				.build();
+	//
+	//		return subGroup;
+	//	}
+	//
+	//	private DataEntrySection createSection()
+	//	{
+	//		final DataEntrySection section = DataEntrySection.builder()
+	//				.id(DataEntrySectionId.ofRepoId(100000))
+	//				.caption(ITranslatableString.constant("Section1-1 1551945835494"))
+	//				.description(ITranslatableString.constant("Section with 3 lines; in the 1st, just one col is used; in the 2nd, one field is long-text, yet the two fields of the 3rd line shall be alligned!"))
+	//				.initallyClosed(true)
+	//				.dataEntryLines()
+	//				.build();
+	//
+	//		return section;
+	//	}
+	//
+	//	private DataEntryLine createLine()
+	//	{
+	//		final DataEntryLine line = DataEntryLine.builder()
+	//				.dataEntryFields()
+	//				.build();
+	//
+	//		return line;
+	//	}
+	//
+	//	private DataEntryField createField()
+	//	{
+	//		final DataEntryField field = DataEntryField.builder()
+	//				.id(DataEntryFieldId.ofRepoId(100000))
+	//				.caption(ITranslatableString.constant("Tab1-Section1-Line1-Field1"))
+	//				.description(ITranslatableString.constant("Yes-No, single field in its line"))
+	//				.type(FieldType.YESNO)
+	//				.mandatory(true)
+	//				.listValues()
+	//				.build();
+	//
+	//		return field;
+	//	}
+
+}
