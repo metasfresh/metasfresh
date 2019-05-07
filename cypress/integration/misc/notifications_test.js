e95e2e47668b99a8a4a4310c17b1e710624bc85a
siemiatj <kuba@saskla.cz>
2019-05-08 01:17:04 +0200

    - use commands in test #78

--- cypress/integration/misc/notifications_test.js
+++ cypress/integration/misc/notifications_test.js
@@ -12,46 +12,25 @@ describe('Test notifications', function() {
       .its('store')
       .invoke('getState')
       .then(state => {
-        assert.isAbove(state.appHandler.inbox.unreadCount, 0);
+        assert.notEqual(state.appHandler.inbox.unreadCount, 0);
       });
   });
 
   it('Check if new notifications are added correctly', function() {
-    const timestamp = new Date().getTime();
-    const message = `Test notification ${timestamp}`;
+    cy.newNotification(null, 1).then(notifObj => {
+      cy.get('.header-item-badge')
+        .find('.notification-number')
+        .contains('1');
 
-    cy.window()
-      .its('store')
-      .invoke('dispatch', {
-        type: 'NEW_NOTIFICATION',
-        notification: {
-          id: '1003176',
-          message,
-          timestamp: '2019-05-07T10:58:48.832+02:00',
-          important: false,
-          read: false,
-          target: {
-            targetType: 'view',
-            windowId: '540375',
-            viewId: '540375-Jn',
-            documentType: '540375',
-          },
-        },
-        unreadCount: 1,
-      });
+      cy.window()
+        .its('store')
+        .invoke('getState')
+        .then(state => {
+          assert.equal(state.appHandler.inbox.unreadCount, 1);
+          const notificationsInboxString = JSON.stringify(state.appHandler.inbox.notifications);
 
-    cy.get('.header-item-badge')
-      .find('.notification-number')
-      .contains('1');
-
-    cy.window()
-      .its('store')
-      .invoke('getState')
-      .then(state => {
-        assert.equal(state.appHandler.inbox.unreadCount, 1);
-        const notificationsInboxString = JSON.stringify(state.appHandler.inbox.notifications);
-
-        expect(notificationsInboxString).to.include(message);
-      });
+          expect(notificationsInboxString).to.include(notifObj.message);
+        });
+    });
   });
 });
