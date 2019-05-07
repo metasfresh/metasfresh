34112b9728940836c7688eb92777ffd986123c64
siemiatj <kuba@saskla.cz>
2019-05-08 01:36:19 +0200

    - rewrite test to use commands #78

--- cypress/integration/misc/notifications_test.js
+++ cypress/integration/misc/notifications_test.js
@@ -4,9 +4,7 @@ describe('Test notifications', function() {
   });
 
   it('Test if older notifications are visible', function() {
-    cy.get('.header-item-badge')
-      .find('.notification-number')
-      .should('not.contain', 0);
+    cy.getDOMNotificationsNumber().should('not.equal', 0);
 
     cy.window()
       .its('store')
@@ -18,19 +16,14 @@ describe('Test notifications', function() {
 
   it('Check if new notifications are added correctly', function() {
     cy.newNotification(null, 1).then(notifObj => {
-      cy.get('.header-item-badge')
-        .find('.notification-number')
-        .contains('1');
+      cy.getDOMNotificationsNumber().should('equal', 1);
 
-      cy.window()
-        .its('store')
-        .invoke('getState')
-        .then(state => {
-          assert.equal(state.appHandler.inbox.unreadCount, 1);
-          const notificationsInboxString = JSON.stringify(state.appHandler.inbox.notifications);
+      cy.getNotificationsInbox(state => {
+        assert.equal(state.unreadCount, 1);
+        const notificationsInboxString = JSON.stringify(state.notifications);
 
-          expect(notificationsInboxString).to.include(notifObj.message);
-        });
+        expect(notificationsInboxString).to.include(notifObj.message);
+      });
     });
   });
 });
