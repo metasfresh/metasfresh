3fd6913dd8aaa5d9e7c617af0c234fe8cf49dd5c
Mark Krake <mark.krake@metasfresh.com>
2019-05-08 17:52:04 +0200

    Adding test spec #74
    https://github.com/metasfresh/metasfresh-e2e/issues/74

---
+++ cypress/integration/tax/taxrate_setup_spec.js
@@ -0,0 +1,12 @@
+import { Taxrate } from '../../support/utils/taxrate';
+
+describe('Create Taxrate for Automatic End2End Tests with cypress https://github.com/metasfresh/metasfresh-e2e/issues/74', function() {
+  it('Create new Taxrate', function() {
+    cy.fixture('tax/taxrate.json').then(taxrateJson => {
+      Object.assign(
+        new Taxrate('Normaler Steuersatz 10%', '2019-05-01', 'Normaler Steuersatz 19% (Deutschland)'),
+        taxrateJson
+      ).apply();
+    });
+  });
+});
