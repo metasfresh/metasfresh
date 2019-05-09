6052e04a8abb10b2048192c9cb3a9729abcd1835
Teo Sarca <teo.sarca@metasfresh.com>
2019-05-09 23:08:04 +0300

    JsonDataEntryFactory
    https://github.com/metasfresh/metasfresh/issues/5162

--- de.metas.business.rest-api/src/main/java/de/metas/dataentry/rest_api/DataEntryRestController.java
+++ de.metas.business.rest-api/src/main/java/de/metas/dataentry/rest_api/DataEntryRestController.java
@@ -1,8 +1,5 @@
 package de.metas.dataentry.rest_api;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.adempiere.ad.element.api.AdWindowId;
 import org.compiere.util.Env;
 import org.slf4j.Logger;
@@ -19,25 +16,12 @@ import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
 
 import de.metas.Profiles;
-import de.metas.dataentry.data.DataEntryRecord;
 import de.metas.dataentry.data.DataEntryRecordRepository;
-import de.metas.dataentry.layout.DataEntryField;
 import de.metas.dataentry.layout.DataEntryLayoutRepository;
-import de.metas.dataentry.layout.DataEntryLine;
-import de.metas.dataentry.layout.DataEntrySection;
-import de.metas.dataentry.layout.DataEntrySubTab;
 import de.metas.dataentry.layout.DataEntryTab;
 import de.metas.dataentry.rest_api.dto.JsonDataEntry;
-import de.metas.dataentry.rest_api.dto.JsonDataEntryField;
-import de.metas.dataentry.rest_api.dto.JsonDataEntryLine;
-import de.metas.dataentry.rest_api.dto.JsonDataEntryListValue;
 import de.metas.dataentry.rest_api.dto.JsonDataEntryResponse;
-import de.metas.dataentry.rest_api.dto.JsonDataEntrySection;
-import de.metas.dataentry.rest_api.dto.JsonDataEntrySubTab;
-import de.metas.dataentry.rest_api.dto.JsonDataEntryTab;
-import de.metas.dataentry.rest_api.dto.JsonFieldType;
 import de.metas.logging.LogManager;
-import de.metas.util.GuavaCollectors;
 import de.metas.util.web.MetasfreshRestAPIConstants;
 import lombok.NonNull;
 
@@ -126,137 +110,11 @@ public class DataEntryRestController
 		final ImmutableList<DataEntryTab> layout = layoutRepo.getByWindowId(windowId);
 		final DataEntryRecordsMap records = dataRecords.get(recordId, DataEntryTab.getSubTabIds(layout));
 
-		final ImmutableList<JsonDataEntryTab> tabs = getJsonDataEntryTabs(adLanguage, layout, records);
-
-		return JsonDataEntry.builder()
-				.tabs(tabs)
+		return JsonDataEntryFactory.builder()
+				.layout(layout)
+				.records(records)
+				.adLanguage(adLanguage)
 				.build();
 	}
 
-	@NonNull
-	private static ImmutableList<JsonDataEntryTab> getJsonDataEntryTabs(
-			final String adLanguage,
-			@NonNull final List<DataEntryTab> layout,
-			@NonNull final DataEntryRecordsMap records)
-	{
-		final ImmutableList.Builder<JsonDataEntryTab> tabs = ImmutableList.builder();
-		for (final DataEntryTab layoutTab : layout)
-		{
-			final ImmutableList<JsonDataEntrySubTab> subTabs = getJsonDataEntrySubTabs(adLanguage, layoutTab, records);
-
-			final JsonDataEntryTab tab = JsonDataEntryTab.builder()
-					.id(layoutTab.getId())
-					.caption(layoutTab.getCaption().translate(adLanguage))
-					.description(layoutTab.getDescription().translate(adLanguage))
-					.subTabs(subTabs)
-					.build();
-			tabs.add(tab);
-		}
-		return tabs.build();
-	}
-
-	@NonNull
-	private static ImmutableList<JsonDataEntrySubTab> getJsonDataEntrySubTabs(
-			final String adLanguage,
-			@NonNull final DataEntryTab layoutTab,
-			@NonNull final DataEntryRecordsMap records)
-	{
-		final ImmutableList.Builder<JsonDataEntrySubTab> subTabs = ImmutableList.builder();
-		for (final DataEntrySubTab layoutSubTab : layoutTab.getDataEntrySubTabs())
-		{
-			final Optional<DataEntryRecord> dataEntryRecord = records.getBySubTabId(layoutSubTab.getId());
-			final ImmutableList<JsonDataEntrySection> sections = getJsonDataEntrySections(adLanguage, layoutSubTab, dataEntryRecord);
-
-			final JsonDataEntrySubTab subTab = JsonDataEntrySubTab.builder()
-					.id(layoutSubTab.getId())
-					.caption(layoutSubTab.getCaption().translate(adLanguage))
-					.description(layoutSubTab.getDescription().translate(adLanguage))
-					.sections(sections)
-					.build();
-			subTabs.add(subTab);
-		}
-		return subTabs.build();
-	}
-
-	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-	@NonNull
-	private static ImmutableList<JsonDataEntrySection> getJsonDataEntrySections(final String adLanguage, @NonNull final DataEntrySubTab layoutSubTab, final Optional<DataEntryRecord> dataEntryRecord)
-	{
-		final ImmutableList.Builder<JsonDataEntrySection> sections = ImmutableList.builder();
-		for (final DataEntrySection layoutSection : layoutSubTab.getDataEntrySections())
-		{
-			final ImmutableList<JsonDataEntryLine> lines = getJsonDataEntryLines(adLanguage, layoutSection, dataEntryRecord);
-
-			final JsonDataEntrySection section = JsonDataEntrySection.builder()
-					.id(layoutSection.getId())
-					.caption(layoutSection.getCaption().translate(adLanguage))
-					.description(layoutSection.getDescription().translate(adLanguage))
-					.initiallyClosed(layoutSection.isInitiallyClosed())
-					.lines(lines)
-					.build();
-			sections.add(section);
-		}
-		return sections.build();
-	}
-
-	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-	private static ImmutableList<JsonDataEntryLine> getJsonDataEntryLines(final String adLanguage, @NonNull final DataEntrySection layoutSection, final Optional<DataEntryRecord> dataEntryRecord)
-	{
-		final ImmutableList.Builder<JsonDataEntryLine> lines = ImmutableList.builder();
-		for (final DataEntryLine layoutLine : layoutSection.getDataEntryLines())
-		{
-			final ImmutableList<JsonDataEntryField> fields = getJsonDataEntryFields(adLanguage, layoutLine, dataEntryRecord);
-
-			final JsonDataEntryLine line = JsonDataEntryLine.builder()
-					.fields(fields)
-					.build();
-			lines.add(line);
-		}
-		return lines.build();
-	}
-
-	@NonNull
-	@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-	private static ImmutableList<JsonDataEntryField> getJsonDataEntryFields(final String adLanguage, @NonNull final DataEntryLine layoutLine, final Optional<DataEntryRecord> dataEntryRecord)
-	{
-		final ImmutableList.Builder<JsonDataEntryField> fields = ImmutableList.builder();
-		for (final DataEntryField layoutField : layoutLine.getDataEntryFields())
-		{
-			// some fields can be excluded from the response, according to this flag
-			if (!layoutField.isAvailableInApi())
-			{
-				continue;
-			}
-
-			Object dataEntryRecordValue = null;
-			if (dataEntryRecord.isPresent())
-			{
-				dataEntryRecordValue = dataEntryRecord.get().getFieldValue(layoutField.getId()).orElse(null);
-			}
-
-			final JsonDataEntryField field = JsonDataEntryField.builder()
-					.id(layoutField.getId())
-					.caption(layoutField.getCaption().translate(adLanguage))
-					.description(layoutField.getDescription().translate(adLanguage))
-					.type(JsonFieldType.getBy(layoutField.getType()))
-					.mandatory(layoutField.isMandatory())
-					.listValues(getDataEntryFieldListValues(layoutField, adLanguage))
-					.value(dataEntryRecordValue)
-					.build();
-			fields.add(field);
-		}
-		return fields.build();
-	}
-
-	@NonNull
-	private static ImmutableList<JsonDataEntryListValue> getDataEntryFieldListValues(@NonNull final DataEntryField layoutField, final String adLanguage)
-	{
-		return layoutField.getListValues().stream()
-				.map(it -> JsonDataEntryListValue.builder()
-						.id(it.getId())
-						.name(it.getName().translate(adLanguage))
-						.description(it.getDescription().translate(adLanguage))
-						.build())
-				.collect(GuavaCollectors.toImmutableList());
-	}
 }
---
+++ de.metas.business.rest-api/src/main/java/de/metas/dataentry/rest_api/JsonDataEntryFactory.java
@@ -0,0 +1,211 @@
+package de.metas.dataentry.rest_api;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import de.metas.dataentry.data.DataEntryRecord;
+import de.metas.dataentry.layout.DataEntryField;
+import de.metas.dataentry.layout.DataEntryLine;
+import de.metas.dataentry.layout.DataEntryListValue;
+import de.metas.dataentry.layout.DataEntrySection;
+import de.metas.dataentry.layout.DataEntrySubTab;
+import de.metas.dataentry.layout.DataEntryTab;
+import de.metas.dataentry.rest_api.dto.JsonDataEntry;
+import de.metas.dataentry.rest_api.dto.JsonDataEntryField;
+import de.metas.dataentry.rest_api.dto.JsonDataEntryLine;
+import de.metas.dataentry.rest_api.dto.JsonDataEntryListValue;
+import de.metas.dataentry.rest_api.dto.JsonDataEntrySection;
+import de.metas.dataentry.rest_api.dto.JsonDataEntrySubTab;
+import de.metas.dataentry.rest_api.dto.JsonDataEntryTab;
+import de.metas.dataentry.rest_api.dto.JsonFieldType;
+import lombok.Builder;
+import lombok.NonNull;
+
+/*
+ * #%L
+ * de.metas.business.rest-api
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+final class JsonDataEntryFactory
+{
+	private final ImmutableList<DataEntryTab> layout;
+	private final DataEntryRecordsMap records;
+	private final String adLanguage;
+
+	@Builder(buildMethodName = "buildFactory")
+	private JsonDataEntryFactory(
+			@NonNull final List<DataEntryTab> layout,
+			@NonNull final DataEntryRecordsMap records,
+			@NonNull final String adLanguage)
+	{
+		this.layout = ImmutableList.copyOf(layout);
+		this.records = records;
+		this.adLanguage = adLanguage;
+	}
+
+	public static class JsonDataEntryFactoryBuilder
+	{
+		public JsonDataEntry build()
+		{
+			return buildFactory().create();
+		}
+	}
+
+	private JsonDataEntry create()
+	{
+		return JsonDataEntry.builder()
+				.tabs(toJsonDataEntryTabs())
+				.build();
+	}
+
+	@NonNull
+	private ImmutableList<JsonDataEntryTab> toJsonDataEntryTabs()
+	{
+		final ImmutableList.Builder<JsonDataEntryTab> tabs = ImmutableList.builder();
+		for (final DataEntryTab layoutTab : layout)
+		{
+			final ImmutableList<JsonDataEntrySubTab> subTabs = toJsonDataEntrySubTabs(layoutTab);
+
+			final JsonDataEntryTab tab = JsonDataEntryTab.builder()
+					.id(layoutTab.getId())
+					.caption(layoutTab.getCaption().translate(adLanguage))
+					.description(layoutTab.getDescription().translate(adLanguage))
+					.subTabs(subTabs)
+					.build();
+			tabs.add(tab);
+		}
+
+		return tabs.build();
+	}
+
+	@NonNull
+	private ImmutableList<JsonDataEntrySubTab> toJsonDataEntrySubTabs(@NonNull final DataEntryTab layoutTab)
+	{
+		final ImmutableList.Builder<JsonDataEntrySubTab> subTabs = ImmutableList.builder();
+		for (final DataEntrySubTab layoutSubTab : layoutTab.getDataEntrySubTabs())
+		{
+			final DataEntryRecord dataEntryRecord = records.getBySubTabId(layoutSubTab.getId()).orElse(null);
+			if (dataEntryRecord == null)
+			{
+				continue;
+			}
+
+			final ImmutableList<JsonDataEntrySection> sections = toJsonDataEntrySections(layoutSubTab, dataEntryRecord);
+
+			final JsonDataEntrySubTab subTab = JsonDataEntrySubTab.builder()
+					.id(layoutSubTab.getId())
+					.caption(layoutSubTab.getCaption().translate(adLanguage))
+					.description(layoutSubTab.getDescription().translate(adLanguage))
+					.sections(sections)
+					.build();
+			subTabs.add(subTab);
+		}
+		return subTabs.build();
+	}
+
+	@NonNull
+	private ImmutableList<JsonDataEntrySection> toJsonDataEntrySections(
+			@NonNull final DataEntrySubTab layoutSubTab,
+			@NonNull final DataEntryRecord dataEntryRecord)
+	{
+		final ImmutableList.Builder<JsonDataEntrySection> sections = ImmutableList.builder();
+		for (final DataEntrySection layoutSection : layoutSubTab.getDataEntrySections())
+		{
+			final ImmutableList<JsonDataEntryLine> lines = toJsonDataEntryLines(layoutSection, dataEntryRecord);
+
+			final JsonDataEntrySection section = JsonDataEntrySection.builder()
+					.id(layoutSection.getId())
+					.caption(layoutSection.getCaption().translate(adLanguage))
+					.description(layoutSection.getDescription().translate(adLanguage))
+					.initiallyClosed(layoutSection.isInitiallyClosed())
+					.lines(lines)
+					.build();
+			sections.add(section);
+		}
+		return sections.build();
+	}
+
+	private ImmutableList<JsonDataEntryLine> toJsonDataEntryLines(
+			@NonNull final DataEntrySection layoutSection,
+			@NonNull final DataEntryRecord dataEntryRecord)
+	{
+		final ImmutableList.Builder<JsonDataEntryLine> lines = ImmutableList.builder();
+		for (final DataEntryLine layoutLine : layoutSection.getDataEntryLines())
+		{
+			final ImmutableList<JsonDataEntryField> fields = toJsonDataEntryFields(layoutLine, dataEntryRecord);
+
+			final JsonDataEntryLine line = JsonDataEntryLine.builder()
+					.fields(fields)
+					.build();
+			lines.add(line);
+		}
+		return lines.build();
+	}
+
+	@NonNull
+	private ImmutableList<JsonDataEntryField> toJsonDataEntryFields(
+			@NonNull final DataEntryLine layoutLine,
+			@NonNull final DataEntryRecord dataEntryRecord)
+	{
+		final ImmutableList.Builder<JsonDataEntryField> fields = ImmutableList.builder();
+		for (final DataEntryField layoutField : layoutLine.getDataEntryFields())
+		{
+			// some fields can be excluded from the response, according to this flag
+			if (!layoutField.isAvailableInApi())
+			{
+				continue;
+			}
+
+			final Object fieldValue = dataEntryRecord.getFieldValue(layoutField.getId()).orElse(null);
+
+			final JsonDataEntryField field = JsonDataEntryField.builder()
+					.id(layoutField.getId())
+					.caption(layoutField.getCaption().translate(adLanguage))
+					.description(layoutField.getDescription().translate(adLanguage))
+					.type(JsonFieldType.getBy(layoutField.getType()))
+					.mandatory(layoutField.isMandatory())
+					.listValues(toJsonDataEntryListValues(layoutField))
+					.value(fieldValue)
+					.build();
+			fields.add(field);
+		}
+		return fields.build();
+	}
+
+	@NonNull
+	private ImmutableList<JsonDataEntryListValue> toJsonDataEntryListValues(@NonNull final DataEntryField layoutField)
+	{
+		return layoutField.getListValues()
+				.stream()
+				.map(this::toJsonDataEntryListValue)
+				.collect(ImmutableList.toImmutableList());
+	}
+
+	private JsonDataEntryListValue toJsonDataEntryListValue(@NonNull final DataEntryListValue listValue)
+	{
+		return JsonDataEntryListValue.builder()
+				.id(listValue.getId())
+				.name(listValue.getName().translate(adLanguage))
+				.description(listValue.getDescription().translate(adLanguage))
+				.build();
+	}
+
+}
