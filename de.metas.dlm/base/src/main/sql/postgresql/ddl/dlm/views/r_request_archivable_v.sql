31e474ffcb7063543bdb79a273e572a0f5a459e8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-05-14 15:19:05 +0200

    5189-app - DB performance problems with large instances and swingclient
    * 5521570_sys_gh5189-app_perf_indices.sql - add missing indexes I stumbled over
    *  5521375_sys_gh5189-app_fix_r_request_function_and_view.sql - fix view and DB-function to archive old order related R_Requests
    https://github.com/metasfresh/metasfresh/issues/5189

--- de.metas.dlm/base/src/main/sql/postgresql/ddl/dlm/views/r_request_archivable_v.sql
+++ de.metas.dlm/base/src/main/sql/postgresql/ddl/dlm/views/r_request_archivable_v.sql
@@ -1,18 +1,25 @@
-CREATE OR REPLACE VIEW dlm.R_Request_Archivable_v
+
+DROP VIEW IF EXISTS dlm.R_Request_Archivable_v;
+CREATE VIEW dlm.R_Request_Archivable_v
 AS
 select 
-	r.R_Request_ID, 
-	r.Updated AS ArchivableSince,
+	GREATEST(r.Updated, COALESCE(r_related.Updated, r.Updated)) AS ArchivableSince,
+	r.R_Request_ID,
+	r.Updated AS r_Updated, 
 	rt.name, 
 	rt.isuseforpartnerrequestwindow, 
 	rs.IsClosed,  
-	r.created, 
-	r.updated,
+	r.created AS r_created, 
+
+	r_related.R_Request_ID AS r_related_R_Request_ID,
+	r_related.created AS r_related_created, 
+	r_related.Updated AS r_related_Updated,
 	o.C_Order_ID
 from r_request r
 	join r_requesttype rt on rt.r_requesttype_id=r.r_requesttype_id
 	join r_status rs on rs.r_status_id = r.r_status_id 
 	join C_Order o on o.C_Order_ID=r.C_Order_ID
+	LEFT join r_request r_related ON r_related.r_requestrelated_id = r.r_request_id
 where rt.name='eMail'
 	AND o.QtyOrdered = o.QtyInvoiced AND o.QtyOrdered = o.QtyInvoiced
 ;
