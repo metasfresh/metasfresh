848a186860c600bc1fbe98144e1e1d38d7e888ea
TheBestPessimist <cristian@tbp.land>
2019-05-21 08:41:03 +0300

    Age is updated in Candidate Receipt window when receiving CU, by editing `Production Date`.
    https://github.com/metasfresh/metasfresh/issues/5208

---
+++ de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/age/AgeAttributeStorageListener.java
@@ -0,0 +1,98 @@
+package de.metas.handlingunits.age;
+
+import de.metas.handlingunits.attribute.HUAttributeConstants;
+import de.metas.handlingunits.attribute.IAttributeValue;
+import de.metas.handlingunits.attribute.storage.IAttributeStorage;
+import de.metas.handlingunits.attribute.storage.IAttributeStorageFactoryService;
+import de.metas.handlingunits.attribute.storage.IAttributeStorageListener;
+import de.metas.handlingunits.attribute.storage.impl.AbstractHUAttributeStorage;
+import de.metas.util.Services;
+import lombok.NonNull;
+import org.adempiere.mm.attributes.spi.IAttributeValueContext;
+import org.compiere.util.TimeUtil;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+/*
+ * #%L
+ * metasfresh-pharma
+ * %%
+ * Copyright (C) 2018 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+/**
+ * This one is used to set the age whenever production date is changed.
+ */
+@Component
+public class AgeAttributeStorageListener implements IAttributeStorageListener
+{
+	public AgeAttributeStorageListener()
+	{
+		Services.get(IAttributeStorageFactoryService.class).addAttributeStorageListener(this);
+	}
+
+	@Override
+	public void onAttributeValueChanged(
+			@NonNull final IAttributeValueContext attributeValueContext,
+			@NonNull final IAttributeStorage storage,
+			final IAttributeValue attributeValue,
+			final Object valueOld)
+	{
+		// checks and so on
+		final AbstractHUAttributeStorage huAttributeStorage = AbstractHUAttributeStorage.castOrNull(storage);
+		storage.setSaveOnChange(true); // TODO this may or may not be done!
+
+		final boolean storageIsAboutHUs = huAttributeStorage != null;
+		if (!storageIsAboutHUs)
+		{
+			return;
+		}
+
+		final boolean relevantAttributesArePresent = storage.hasAttribute(HUAttributeConstants.ATTR_Age)
+				&& storage.hasAttribute(HUAttributeConstants.ATTR_ProductionDate);
+		if (!relevantAttributesArePresent)
+		{
+			return;
+		}
+
+		final String attributeIdentifier = attributeValue.getM_Attribute().getValue();
+		final boolean relevantAttributeHasChanged = HUAttributeConstants.ATTR_ProductionDate.equals(attributeIdentifier);
+		if (!relevantAttributeHasChanged)
+		{
+			return;
+		}
+
+		// actual logic starts here
+		final Date productionDate = storage.getValueAsDate(HUAttributeConstants.ATTR_ProductionDate);
+
+		final long age = getAge(TimeUtil.asLocalDateTime(productionDate));
+
+
+		storage.setValue(HUAttributeConstants.ATTR_Age, String.valueOf(age));
+	}
+
+	private long getAge(final LocalDateTime start)
+	{
+		final LocalDateTime end = LocalDateTime.now();
+
+		return ChronoUnit.MONTHS.between(start, end);
+	}
+}
