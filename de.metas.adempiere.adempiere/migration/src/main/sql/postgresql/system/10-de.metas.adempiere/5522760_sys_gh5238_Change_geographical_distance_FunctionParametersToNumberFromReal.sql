4b5f6d9f4a27a16696c6aa880701a9891d1437eb
TheBestPessimist <cristian@tbp.land>
2019-05-28 11:36:17 +0300

    Change geographical_distance function parameters to number from real
    https://github.com/metasfresh/metasfresh/issues/5238

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5522760_sys_gh5238_Change_geographical_distance_FunctionParametersToNumberFromReal.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5522760_sys_gh5238_Change_geographical_distance_FunctionParametersToNumberFromReal.sql
@@ -1,7 +1,9 @@
+DROP FUNCTION IF EXISTS geographical_distance(real, real, real, real);
 DROP FUNCTION IF EXISTS geographical_distance(numeric, numeric, numeric, numeric);
 
 CREATE FUNCTION geographical_distance(lat1 numeric, lon1 numeric, lat2 numeric, lon2 numeric) RETURNS numeric
-    LANGUAGE plpgsql IMMUTABLE
+    LANGUAGE plpgsql
+    IMMUTABLE
 AS
 $$
 DECLARE
@@ -11,7 +13,7 @@ BEGIN
                     cos(radians(lat1)) *
                     cos(radians(lat2)) *
                     cos(
-                                radians(lon1) - radians(lon2)
+                            radians(lon1) - radians(lon2)
                         ) +
                     sin(radians(lat1)) *
                     sin(radians(lat2))
