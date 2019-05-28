90137e36f438fb7feeb5532379f76e0784a6ffa2
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-05-28 13:27:11 +0300

    #5239 WIP - customs invoice report
    #5239 Jasper for Customs Invoice

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Sales_Custom_Invoice_Details.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Sales_Custom_Invoice_Details.sql
@@ -1,6 +1,6 @@
-DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Sales_Custom_Invoice_Details ( IN C_Invoice_ID numeric, IN AD_Language Character Varying(6) );
-CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Sales_Custom_Invoice_Details(IN C_Invoice_ID numeric,
-                                                                                                IN AD_Language  Character Varying(6))
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Sales_Custom_Invoice_Details ( IN p_C_Customs_Invoice_ID numeric, IN p_AD_Language Character Varying(6) );
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Sales_Custom_Invoice_Details(IN p_C_Customs_Invoice_ID numeric,
+                                                                                                IN p_AD_Language  Character Varying(6))
   RETURNS TABLE
   (docType            character varying,
    documentno            character varying,
@@ -42,11 +42,11 @@ FROM
 
   -- Get Product and its translation
   LEFT OUTER JOIN M_Product p ON il.M_Product_ID = p.M_Product_ID
-  LEFT OUTER JOIN M_Product_Trl pt ON il.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = $2 AND pt.isActive = 'Y'
+  LEFT OUTER JOIN M_Product_Trl pt ON il.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = p_AD_Language AND pt.isActive = 'Y'
 
   -- Get Unit of measurement and its translation
   LEFT OUTER JOIN C_UOM uom ON il.C_UOM_ID = uom.C_UOM_ID
-  LEFT OUTER JOIN C_UOM_Trl uomt ON il.C_UOM_ID = uomt.C_UOM_ID AND uomt.AD_Language = $2 AND uomt.isActive = 'Y'
+  LEFT OUTER JOIN C_UOM_Trl uomt ON il.C_UOM_ID = uomt.C_UOM_ID AND uomt.AD_Language = p_AD_Language AND uomt.isActive = 'Y'
 
   LEFT OUTER JOIN
       de_metas_endcustomer_fresh_reports.getC_BPartner_Product_Details(p.M_Product_ID, bp.C_BPartner_ID, null) as bpp
@@ -62,7 +62,7 @@ FROM
     ON io.C_DocType_ID = dtt.C_DocType_ID AND dtt.AD_Language = $2 AND dtt.isActive = 'Y'
 
 WHERE
-  il.C_Customs_Invoice_ID = $1
+  il.C_Customs_Invoice_ID = p_C_Customs_Invoice_ID
 
 ORDER BY
   lineno
