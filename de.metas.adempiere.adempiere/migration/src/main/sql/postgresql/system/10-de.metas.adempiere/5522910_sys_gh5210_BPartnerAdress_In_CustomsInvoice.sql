c0b7c31e70958636c52a3faf0cb73c99857059e8
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-05-29 14:57:37 +0300

    #5210 Bpartner Address in Customs Invoice
    https://github.com/metasfresh/metasfresh/issues/5210

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5522910_sys_gh5210_BPartnerAdress_In_CustomsInvoice.sql
@@ -0,0 +1,14 @@
+-- 2019-05-29T11:17:32.822
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,DDL_NoForeignKey,EntityType,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRangeFilter,IsSelectionColumn,IsShowFilterIncrementButtons,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,568124,505103,0,14,541360,'BPartnerAddress',TO_TIMESTAMP('2019-05-29 11:17:32','YYYY-MM-DD HH24:MI:SS'),100,'N','D',1000,'Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','Anschrift-Text',0,0,TO_TIMESTAMP('2019-05-29 11:17:32','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 2019-05-29T11:17:32.828
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Column_ID=568124 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 2019-05-29T11:17:32.871
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_Column_Translation_From_AD_Element(505103) 
+;
---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5522920_sys_gh5210_BPartnerAdress_In_CustomsInvoice_DDL.sql
@@ -0,0 +1,6 @@
+
+-- 2019-05-29T11:17:34.707
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ SELECT public.db_alter_table('C_Customs_Invoice','ALTER TABLE public.C_Customs_Invoice ADD COLUMN BPartnerAddress VARCHAR(1000)')
+;
+
