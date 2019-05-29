3424ecbb49327b7e3c299b4f5800466069974f5c
Olly <pisollie94@gmail.com>
2019-05-29 16:25:37 +0300

    #2149 the value is now a float as server returns it

--- src/components/widget/RawWidget.js
+++ src/components/widget/RawWidget.js
@@ -255,7 +255,7 @@ export class RawWidget extends Component {
       initialFocus,
     } = this.props;
 
-    let widgetValue = data != null ? data : widgetData[0].value;
+    let widgetValue = data != null ? widgetData[0].value : null;
     const { isEdited } = this.state;
 
     // TODO: API SHOULD RETURN THE SAME PROPERTIES FOR FILTERS
