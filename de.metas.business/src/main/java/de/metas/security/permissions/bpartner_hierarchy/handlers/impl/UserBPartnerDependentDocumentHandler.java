8afe77a13ad2aefdb4a8ac13f398507291c9fb52
Teo Sarca <teo.sarca@metasfresh.com>
2019-06-04 16:18:58 +0300

    Update UserBPartnerDependentDocumentHandler.java
    minor
    
    https://github.com/metasfresh/metasfresh/issues/5027

--- de.metas.business/src/main/java/de/metas/security/permissions/bpartner_hierarchy/handlers/impl/UserBPartnerDependentDocumentHandler.java
+++ de.metas.business/src/main/java/de/metas/security/permissions/bpartner_hierarchy/handlers/impl/UserBPartnerDependentDocumentHandler.java
@@ -43,7 +43,6 @@ public class UserBPartnerDependentDocumentHandler implements BPartnerDependentDo
 	@Override
 	public String getDocumentTableName()
 	{
-		// TODO Auto-generated method stub
 		return I_AD_User.Table_Name;
 	}
 
