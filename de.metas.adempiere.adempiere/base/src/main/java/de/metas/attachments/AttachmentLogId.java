11e5a4fe604e56cf0723b8eebd8f874ef243e2a7
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-06-06 13:04:55 +0300

    5261 model and repository created
    # 5261 Prevent Users from Accidentally deleting attachment files

---
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/attachments/AttachmentLogId.java
@@ -0,0 +1,51 @@
+package de.metas.attachments;
+
+import de.metas.util.Check;
+import lombok.Value;
+
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+@Value
+public class AttachmentLogId
+{
+	public static AttachmentLogId ofRepoId(final int repoId)
+	{
+		return new AttachmentLogId(repoId);
+	}
+
+	public static AttachmentLogId ofRepoIdOrNull(final int repoId)
+	{
+		return repoId > 0 ? ofRepoId(repoId) : null;
+	}
+
+	public static int getRepoId(final AttachmentLogId attachmentLogId)
+	{
+		return attachmentLogId != null ? attachmentLogId.getRepoId() : -1;
+	}
+
+	int repoId;
+
+	private AttachmentLogId(final int repoId)
+	{
+		this.repoId = Check.assumeGreaterThanZero(repoId, "repoId");
+	}
+}
