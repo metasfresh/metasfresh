b159330a160cb90643e5de4a75317c7f58107b29
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-06-10 12:04:06 +0300

    #5205 Use uomsymbol for matching uom
    #5205 Improve IFA import

--- de.metas.business/src/main/java/org/adempiere/impexp/product/MProductImportTableSqlUpdater.java
+++ de.metas.business/src/main/java/org/adempiere/impexp/product/MProductImportTableSqlUpdater.java
@@ -311,7 +311,7 @@ public class MProductImportTableSqlUpdater
 
 		sql = new StringBuilder("UPDATE ")
 				.append(targetTableName + " i ")
-				.append(" SET Package_UOM_ID = (SELECT C_UOM_ID FROM C_UOM u WHERE lower(u.X12DE355)=lower(i.A00PGEINH) AND u.AD_Client_ID IN (0,i.AD_Client_ID) AND u.IsActive='Y' ORDER BY u.AD_Client_ID DESC, u.C_UOM_ID ASC LIMIT 1) ")
+				.append(" SET Package_UOM_ID = (SELECT C_UOM_ID FROM C_UOM u WHERE lower(u.uomsymbol)=lower(i.A00PGEINH) AND u.AD_Client_ID IN (0,i.AD_Client_ID) AND u.IsActive='Y' ORDER BY u.AD_Client_ID DESC, u.C_UOM_ID ASC LIMIT 1) ")
 				.append("WHERE Package_UOM_ID IS NULL")
 				.append(" AND " + COLUMNNAME_I_IsImported + "<>'Y'").append(whereClause);
 		no = DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
