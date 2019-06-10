ed0d9a187ea4fdc133a27c9a9ff95e6fbe023250
Teo Sarca <teo.sarca@metasfresh.com>
2019-06-10 13:17:19 +0300

    Create 5524190_sys_gh5139_fix_Attributes_Values_tab.sql
    https://github.com/metasfresh/metasfresh/issues/5139

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5524190_sys_gh5139_fix_Attributes_Values_tab.sql
@@ -0,0 +1,20 @@
+-- 2019-06-10T11:41:39.761
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET DefaultValue='@AD_Client_ID@', IsReadOnly='N',Updated=TO_TIMESTAMP('2019-06-10 11:41:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=6370
+;
+
+-- 2019-06-10T11:41:47.969
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET DefaultValue='0', IsReadOnly='N',Updated=TO_TIMESTAMP('2019-06-10 11:41:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=6367
+;
+
+-- 2019-06-10T11:43:01.360
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Val_Rule_ID=NULL,Updated=TO_TIMESTAMP('2019-06-10 11:43:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=8464
+;
+
+-- 2019-06-10T11:43:10.787
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Val_Rule_ID=NULL,Updated=TO_TIMESTAMP('2019-06-10 11:43:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=8458
+;
+
