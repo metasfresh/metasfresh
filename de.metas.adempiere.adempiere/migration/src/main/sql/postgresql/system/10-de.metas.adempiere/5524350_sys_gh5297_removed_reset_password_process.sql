28c6ac0196c896738936ff7322d0074ff138c8f1
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-06-12 10:12:35 +0300

    5297 removed reset password process sql
    #5297 Drop AD_User_ResetPassword_EnqueueForSelection process

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5524350_sys_gh5297_removed_reset_password_process.sql
@@ -0,0 +1,15 @@
+-- 2019-06-12T09:50:04.397
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Table_Process WHERE AD_Table_Process_ID=540420
+;
+
+-- 2019-06-12T09:50:10.155
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Process_Trl WHERE AD_Process_ID=540348
+;
+
+-- 2019-06-12T09:50:10.156
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Process WHERE AD_Process_ID=540348
+;
+
