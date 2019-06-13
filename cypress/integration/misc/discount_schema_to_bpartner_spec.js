191c0ef4415d5542f4c1cfefa7e959cd5773c7e8
siemiatj <kuba@saskla.cz>
2019-06-13 15:47:59 +0200

    - fix discountschema fixtures #113

--- cypress/integration/misc/discount_schema_to_bpartner_spec.js
+++ cypress/integration/misc/discount_schema_to_bpartner_spec.js
@@ -4,8 +4,8 @@ import { BPartner } from '../../support/utils/bpartner';
 describe('Create test: discount schema set to bpartner, https://github.com/metasfresh/metasfresh-e2e/issues/113', function() {
   //create bpartner
   it('Create bpartner', function() {
-    cy.fixture('discount/discountschema.json').then(customerJson => {
-      Object.assign(new DiscountSchema(), customerJson).apply();
+    cy.fixture('discount/discountschema.json').then(discountSchemaJson => {
+      Object.assign(new DiscountSchema(), discountSchemaJson).apply();
     });
     cy.fixture('sales/simple_customer.json').then(customerJson => {
       Object.assign(new BPartner(), customerJson)
