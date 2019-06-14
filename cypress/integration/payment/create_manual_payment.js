33c19a4a22ec9d7824b7e7d9ad7a3d9c90081902
siemiatj <kuba@saskla.cz>
2019-06-14 19:10:42 +0200

    - cleanup test #111

--- cypress/integration/payment/create_manual_payment.js
+++ cypress/integration/payment/create_manual_payment.js
@@ -1,16 +1,14 @@
 /// <reference types="Cypress" />
 
-import {Product, ProductCategory} from "../../support/utils/product";
-import {object} from "prop-types";
-import {BPartner} from "../../support/utils/bpartner";
-import {salesInvoices} from "../../page_objects/sales_invoices";
-import {SalesInvoice, SalesInvoiceLine} from "../../support/utils/sales_invoice";
+// import {Product, ProductCategory} from '../../support/utils/product';
+// import {object} from 'prop-types';
+// import {BPartner} from '../../support/utils/bpartner';
+// import {salesInvoices} from '../../page_objects/sales_invoices';
+import { SalesInvoice, SalesInvoiceLine } from '../../support/utils/sales_invoice';
 
-describe('Create a manual Payment for a Sales Invoice', function () {
-
-  const timestamp = new Date().getTime();
-
-  const productName = `Sales Order-to-Invoice Test ${timestamp}`;
+describe('Create a manual Payment for a Sales Invoice', function() {
+  // const timestamp = new Date().getTime();
+  // const productName = `Sales Order-to-Invoice Test ${timestamp}`;
   // const customerName = `Sales Order-to-Invoice Test ${timestamp}`;
 
   const customerName = 'Test Kunde 1_G0001';
@@ -19,13 +17,13 @@ describe('Create a manual Payment for a Sales Invoice', function () {
   let salesInvoiceNumber;
   let salesInvoiceTotalAmount = 0;
 
-
   const paymentDocumentType = 'Zahlungseingang';
 
-  before(function () {
-    const productValue = `sales_order_to_invoice_test ${timestamp}`;
-    const productCategoryName = `ProductCategoryName ${timestamp}`;
-    const productCategoryValue = `ProductNameValue ${timestamp}`;
+  /*
+  before(function() {
+    // const productValue = `sales_order_to_invoice_test ${timestamp}`;
+    // const productCategoryName = `ProductCategoryName ${timestamp}`;
+    // const productCategoryValue = `ProductNameValue ${timestamp}`;
 
     // cy.fixture('product/simple_productCategory.json').then(productCategoryJson => {
     //   Object.assign(new ProductCategory(), productCategoryJson)
@@ -49,46 +47,52 @@ describe('Create a manual Payment for a Sales Invoice', function () {
 
     cy.readAllNotifications();
   });
-
+  */
 
   it('Creates a Sales Order and Invoice', () => {
     new SalesInvoice(customerName, salesInvoiceTargetDocumentType)
-      .addLine(new SalesInvoiceLine()
-        .setProduct('Convenience Salat 250g_P002737')
-        .setQuantity(6)
-        .setPackingItem('IFCO 6410 x 10 Stk')
-        .setTuQuantity(1)
+      .addLine(
+        new SalesInvoiceLine()
+          .setProduct('Convenience Salat 250g_P002737')
+          .setQuantity(6)
+          .setPackingItem('IFCO 6410 x 10 Stk')
+          .setTuQuantity(1)
       )
       .apply();
+
+    cy.getFieldValue('DocumentNo').then(documentNumber => {
+      salesInvoiceNumber = documentNumber;
+    });
   });
-
-
-
-
-
 
   it('Creates a manual Payment', () => {
     cy.visitWindow('195', 'NEW');
 
-    cy.writeIntoLookupListField("C_BPartner_ID", customerName, customerName);
+    cy.writeIntoLookupListField('C_BPartner_ID', customerName, customerName);
 
     cy.getFieldValue('DocumentNo').should('be.empty');
     cy.selectInListField('C_DocType_ID', paymentDocumentType);
     cy.getFieldValue('DocumentNo').should('not.be.empty');
 
+    // cy.getFieldValue('DocumentNo').then(documentNumber => {
+      // @kuba this is not working
+      // cy.getFieldValue('PayAmt').should('equal', salesInvoiceTotalAmount); // todo how do i even equal?!!?!!
+      cy.getFieldValue('PayAmt').then(val => {
+        const cast = parseInt(val, 10);
+        assert.equal(cast, salesInvoiceTotalAmount);
+      });
 
-    // @kuba this is not working
-    cy.getFieldValue('PayAmt').should('equal', salesInvoiceTotalAmount); // todo how do i even equal?!!?!!
+      // salesInvoiceNumber = 145824; // todo this has to be read somehow from the previously created sales invoice;
+      cy.writeIntoLookupListField('C_Invoice_ID', salesInvoiceNumber, salesInvoiceNumber);
 
-    salesInvoiceNumber = 145824;  // todo this has to be read somehow from the previously created sales invoice;
-    cy.writeIntoLookupListField('C_Invoice_ID', salesInvoiceNumber, salesInvoiceNumber);
+      // @kuba this is not working either
+      // salesInvoiceTotalAmount = 2.2; // todo this has to be read somehow from the previously created sales invoice;
+      // cy.getFieldValue('PayAmt').should('equal', salesInvoiceTotalAmount); // todo how do i even equal?!!?!!
 
-    // @kuba this is not working either
-    salesInvoiceTotalAmount = 2.2; // todo this has to be read somehow from the previously created sales invoice;
-    cy.getFieldValue('PayAmt').should('equal', salesInvoiceTotalAmount); // todo how do i even equal?!!?!!
-
-
-
-  })
-
+      // cy.getFieldValue('PayAmt').then(val => {
+      //   const cast = parseInt(val, 10);
+      //   cast.should('equal', salesInvoiceTotalAmount);
+      // });
+    // });
+  });
 });
--- cypress/support/utils/sales_invoice.js
+++ cypress/support/utils/sales_invoice.js
@@ -27,7 +27,7 @@ export class SalesInvoice {
         .should('contain', new Date().getDate())
         .should('contain', new Date().getFullYear());
 
-      cy.writeIntoLookupListField("C_BPartner_ID", salesInvoice.businessPartner, salesInvoice.businessPartner);
+      cy.writeIntoLookupListField('C_BPartner_ID', salesInvoice.businessPartner, salesInvoice.businessPartner);
       cy.getFieldValue('M_PriceList_ID').should('not.be.empty');
       cy.getFieldValue('C_Currency_ID').should('not.be.empty');
 
@@ -35,40 +35,37 @@ export class SalesInvoice {
       cy.selectInListField('C_DocTypeTarget_ID', salesInvoice.targetDocumentType);
       cy.getFieldValue('DocumentNo').should('not.be.empty');
 
-      salesInvoice.lines.forEach((line) => {
+      salesInvoice.lines.forEach(line => {
         SalesInvoice.applyLine(line);
       });
-
     });
   }
 
   static applyLine(salesInvoiceLine) {
     cy.pressAddNewButton();
 
-    cy.writeIntoLookupListField("M_Product_ID", salesInvoiceLine.product, salesInvoiceLine.product);
-
+    cy.writeIntoLookupListField('M_Product_ID', salesInvoiceLine.product, salesInvoiceLine.product);
 
     // cannot set the packingItem
-    cy.writeIntoLookupListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,true, true);
-    cy.writeIntoStringField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem);
-    cy.writeIntoTextField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem, true);
+    cy.writeIntoLookupListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,true, true);
+    // cy.writeIntoStringField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem);
+    // cy.writeIntoTextField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, true);
 
-    cy.writeIntoLookupListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,true, true);
-    cy.writeIntoLookupListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,true, false);
-    cy.writeIntoLookupListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,false, false);
-    cy.writeIntoLookupListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,false, true);
+    // cy.writeIntoLookupListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,true, true);
+    // cy.writeIntoLookupListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,true, false);
+    // cy.writeIntoLookupListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,false, false);
+    // cy.writeIntoLookupListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, salesInvoiceLine.packingItem,false, true);
 
-    cy.selectInListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem,  true);
-    cy.selectInListField("M_HU_PI_Item_Product_ID", salesInvoiceLine.packingItem,  false);
+    // cy.selectInListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, true);
+    // cy.selectInListField('M_HU_PI_Item_Product_ID', salesInvoiceLine.packingItem, false);
 
     cy.pressDoneButton();
   }
 }
 
-
 export class SalesInvoiceLine {
-  constructor() {
-  }
+  // constructor() {
+  // }
 
   setProduct(product) {
     this.product = product;
