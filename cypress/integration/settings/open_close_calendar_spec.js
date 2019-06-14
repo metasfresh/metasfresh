eb1ef4c070e0112b140a7f57922549f30859fb3c
Olly <pisollie94@gmail.com>
2019-06-14 12:04:36 +0300

    test works as described

---
+++ cypress/integration/settings/open_close_calendar_spec.js
@@ -0,0 +1,57 @@
+describe('Filter calendar periods', function() {
+  it('open page', function() {
+    cy.visit('/window/540349');
+  });
+
+  it('press filter button', function() {
+    cy.get('div')
+      .should('have.class', 'filter-wrapper')
+      .get('button')
+      .contains('Filter')
+      .click({ force: true })
+      .get('div.filters-overlay')
+      .get('ul.filter-menu')
+      .get('li.filter-option-default')
+      .click({ force: true });
+  });
+
+  it('change year', function() {
+    cy.get('.form-field-C_Year_ID > :nth-child(2) > .input-body-container > .input-dropdown-container').click();
+
+    cy.get('div.input-dropdown-list')
+      .get('div.input-dropdown-list-option.ignore-react-onclickoutside')
+      .contains('2018')
+      .click();
+
+    cy.get('.input-body-container > .input-block > .input-field')
+      .click()
+      .type('Dez-18');
+
+    cy.get('.filter-menu > .filter-btn-wrapper > button').click();
+  });
+
+  it('enter in item', function() {
+    cy.wait(1000);
+    cy.get('.Text > :nth-child(1) > .cell-text-wrapper')
+      .contains('Dez-18')
+      .dblclick();
+  });
+});
+
+describe('Actions on selected item', function() {
+  it('open/close action - close periods', function() {
+    cy.get('.header-left-side > .btn-square').click();
+    cy.get('#headerAction_C_Period_Process').click();
+    cy.get(
+      '.window-wrapper > :nth-child(1) > .form-group > .col-sm-9 > .input-body-container > .input-dropdown-container > .input-dropdown > .input-editable > .input-field'
+    ).click();
+    cy.get('[data-test-id="CClose Period"]').click();
+    cy.get('.items-row-2 > :nth-child(2)').click();
+  });
+
+  it('open/close action - open periods', function() {
+    cy.get('.header-left-side > .btn-square').click();
+    cy.get('#headerAction_C_Period_Process').click();
+    cy.get('.items-row-2 > :nth-child(2)').click();
+  });
+});
