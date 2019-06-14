477d0a5bd291d6e57e8c32d63ec57a61cd19e225
Olly <pisollie94@gmail.com>
2019-06-14 16:00:00 +0300

    improvment on code

--- cypress/integration/settings/create_new_parzelle_spec.js
+++ cypress/integration/settings/create_new_parzelle_spec.js
@@ -1,21 +1,12 @@
 describe('Create new Parzelle/Allotment', function() {
   it('open new parzelle', function() {
     cy.visit('/window/540210');
-    cy.get('.btn-square').click();
-    cy.get('#subheaderNav_new').click();
+    cy.clickHeaderNav('new');
   });
 
   it('create it', function() {
-    cy.get('.elements-line > .form-group').children(
-      'div[title="Search key for the record in the format required - must be unique"]'
-    );
-    cy.get(':nth-child(1) > .form-group > .col-sm-9 > .input-body-container > .input-block > .input-field')
-      .click()
-      .type('Industriestrasse');
-    cy.get('.elements-line > .form-group').children('div[title="Name"]');
-    cy.get(':nth-child(2) > .form-group > .col-sm-9 > .input-body-container > .input-block > .input-field')
-      .click()
-      .type('Industriestrasse')
-      .trigger('esc');
+    cy.writeIntoStringField('Value', 'Industriestrasse');
+    cy.writeIntoStringField('Name', 'Industriestrasse');
+    cy.get(':nth-child(4) > .header-btn > .header-item-container > .header-item').click();
   });
 });
