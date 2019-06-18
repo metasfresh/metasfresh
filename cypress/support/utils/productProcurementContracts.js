6d2937ce9abe9bdaf47d5f9e46187a85003597cd
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-06-18 11:56:29 +0300

    82 added a new product in product for procurement contracts
    #82 Add a new product to "Lieferantenvertrag"

---
+++ cypress/support/utils/productProcurementContracts.js
@@ -0,0 +1,27 @@
+export class ProductProcurementContracts {
+  constructor(name) {
+    cy.log(`ProductProcurementContracts - set name = ${name}`);
+    this.name = name;
+    this.attributeValues = [];
+  }
+
+  setName(name) {
+    cy.log(`Product - set name = ${name}`);
+    this.name = name;
+    return this;
+  }
+  apply() {
+    cy.log(`ProductProcurementContracts - apply - START (name=${this.name})`);
+    applyProduct(this);
+    cy.log(`ProductProcurementContracts - apply - END (name=${this.name})`);
+    return this;
+  }
+}
+function applyProduct(product) {
+  describe(`Create new ProductProcurementContracts ${product.name}`, function() {
+    cy.visitWindow('540288', 'NEW');
+    cy.get('#lookup_M_Product_ID input')
+      .type(product.name)
+      .type('\n');
+  });
+}
