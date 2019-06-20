33f352cb0f47a4cc4003ed758b5877cf943ef412
TheBestPessimist <cristian@tbp.land>
2019-06-20 10:04:56 +0300

    Fix Price List casing, fields and json
    https://github.com/metasfresh/metasfresh-e2e/issues/111

--- cypress/support/utils/pricelist.js
+++ cypress/support/utils/pricelist.js
@@ -1,54 +1,65 @@
 export class PriceList {
   constructor(name) {
-    this.Name = name;
+    this.name = name;
   }
 
   setName(name) {
-    cy.log(`Pricelist - set name = ${name}`);
-    this.Name = name;
+    cy.log(`PriceList - set name = ${name}`);
+    this.name = name;
     return this;
   }
 
   setDescription(description) {
-    cy.log(`Pricelist - set description = ${description}`);
-    this.Description = description;
+    cy.log(`PriceList - set description = ${description}`);
+    this.description = description;
     return this;
   }
 
-  setPricingSystem(pricingsystem) {
-    cy.log(`Pricelist - set pricingsystem = ${pricingsystem}`);
-    this.PricingSystem = pricingsystem;
+  setCountry(country) {
+    cy.log(`PriceList - set country = ${country}`);
+    this.country = country;
     return this;
   }
 
-  setActive(isActive) {
-    cy.log(`Pricelist - set isActive = ${isActive}`);
-    this.IsActive = isActive;
+  setCurrency(currency) {
+    cy.log(`PriceList - set currency = ${currency}`);
+    this.currency = currency;
+    return this;
+  }
+
+  setPricePrecision(pricePrecision) {
+    cy.log(`PriceList - set pricePrecision = ${pricePrecision}`);
+    this.pricePrecision = pricePrecision;
+    return this;
+  }
+
+
+  setPriceSystem(priceSystem) {
+    cy.log(`PriceList - set priceSystem = ${priceSystem}`);
+    this.priceSystem = priceSystem;
     return this;
   }
 
   apply() {
-    cy.log(`Pricelist - apply - START (name=${this.name})`);
-    applyPricelist(this);
-    cy.log(`Pricelist - apply - END (name=${this.name})`);
+    cy.log(`PriceList - apply - START (name=${this.name})`);
+    applyPriceList(this);
+    cy.log(`PriceList - apply - END (name=${this.name})`);
     return this;
   }
 }
 
-function applyPricelist(pricelist) {
-  const timestamp = new Date().getTime();
-
-  describe(`Create new Pricelist ${pricelist.Name}`, function() {
+function applyPriceList(priceList) {
+  describe(`Create new PriceList ${priceList.name}`, function () {
     cy.visitWindow('540321', 'NEW', 'priceListObj');
-    cy.writeIntoStringField('Name', `${pricelist.Name} ${timestamp}`);
-    cy.selectInListField('C_Country_ID', `none`);
-    cy.selectInListField('C_Country_ID', `${pricelist.Country}`);
-    cy.selectInListField('C_Currency_ID', `${pricelist.Currency}`);
+    cy.writeIntoStringField('Name', priceList.name);
+    // cy.selectInListField('C_Country_ID', `none`);
+    cy.selectInListField('C_Country_ID', priceList.country);
+    cy.selectInListField('C_Currency_ID', priceList.currency);
     cy.get('.form-field-PricePrecision')
       .find('input')
       .clear()
-      .type(`${pricelist.PricePrecision}`);
-    cy.selectInListField('M_PricingSystem_ID', `${pricelist.PricingSystem}`);
-    cy.writeIntoStringField('Description', `${pricelist.Description}`);
+      .type(priceList.pricePrecision);
+    cy.selectInListField('M_PricingSystem_ID', priceList.priceSystem);
+    cy.writeIntoStringField('Description', priceList.description);
   });
 }
