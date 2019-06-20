a020ce366b82a40a13236b7dbb0708b0b678921e
TheBestPessimist <cristian@tbp.land>
2019-06-20 13:10:19 +0300

    Prepare before section for test part 1
    https://github.com/metasfresh/metasfresh-e2e/issues/111

--- cypress/integration/payment/create_manual_payment.js
+++ cypress/integration/payment/create_manual_payment.js
@@ -1,15 +1,17 @@
 /// <reference types="Cypress" />
 
-// import {Product, ProductCategory} from '../../support/utils/product';
-// import {BPartner} from '../../support/utils/bpartner';
+import {Product, ProductCategory, ProductPrice} from '../../support/utils/product';
+import {BPartner} from '../../support/utils/bpartner';
 import {SalesInvoice, SalesInvoiceLine} from '../../support/utils/sales_invoice';
+import {DiscountSchema} from "../../support/utils/discountschema";
+import {Bank} from "../../support/utils/bank";
+import {PriceList, PriceListVersion} from "../../support/utils/pricelist";
+import {Pricesystem} from "../../support/utils/pricesystem";
 
 describe('Create a manual Payment for a Sales Invoice', function () {
-  // const timestamp = new Date().getTime();
-  // const productName = `Sales Order-to-Invoice Test ${timestamp}`;
-  // const bPartnerName = `Sales Order-to-Invoice Test ${timestamp}`;
+  const timestamp = new Date().getTime();
 
-  const bPartnerName = 'Test Kunde 1_G0001';
+  // const bPartnerName = 'Test Kunde 1_G0001';
 
   let salesInvoiceNumber;
   const salesInvoiceTargetDocumentType = 'Sales Invoice';
@@ -20,157 +22,246 @@ describe('Create a manual Payment for a Sales Invoice', function () {
   let paymentTotalAmount = 0; // may be different from salesInvoiceTotalAmount because there could be some discounts
 
 
+  // data for "before" section
+  const priceSystemName = `PriceSystem ${timestamp}`;
+  const priceListName = `PriceList ${timestamp}`;
+  const priceListVersionName = `PriceVersionList ${timestamp}`;
 
-  // const productValue = `sales_order_to_invoice_test ${timestamp}`;
-  // const productCategoryName = `ProductCategoryName ${timestamp}`;
-  // const productCategoryValue = `ProductNameValue ${timestamp}`;
+  const productName = `Product ${timestamp}`;
+  const productValue = `manual_payment_test ${timestamp}`;
+  const productCategoryName = `ProductCategoryName ${timestamp}`;
+  const productCategoryValue = `ProductNameValue ${timestamp}`;
+  const productPriceAmount = 1.34;
+  const productPriceTaxCategory = 'Normaler Steuersatz 19% (Deutschland)';
 
-  // before(function () {
-  //   cy.fixture('product/simple_productCategory.json').then(productCategoryJson => {
-  //     Object.assign(new ProductCategory(), productCategoryJson)
-  //       .setName(productCategoryName)
-  //       .setValue(productCategoryValue)
-  //       .apply();
+  const discountSchemaName = `DiscountSchemaTest ${timestamp}`;
+
+  const bPartnerName = `bPartner ${timestamp}`;
+
+
+  before(function () {
+
+
+    //   cy.fixture('discount/discountschema.json').then(discountSchemaJson => {
+    //     Object.assign(new DiscountSchema(), discountSchemaJson)
+    //       .setName(discountSchemaName)
+    //       .apply();
+    //   });
+    //   cy.fixture('finance/bank.json').then(productJson => {
+    //     Object.assign(new Bank(), productJson)
+    //       .apply();
+    //   });
+    //   cy.fixture('sales/simple_customer.json').then(customerJson => {
+    //     Object.assign(new BPartner(), customerJson)
+    //       .setName(bPartnerName)
+    //       .apply();
+    //   });
+    //
+    //   cy.readAllNotifications();
+
+
+  });
+
+
+  it('creates PS PL and PLV', () => {
+
+    cy.fixture('price/pricesystem.json').then(priceSystemJson => {
+      Object.assign(new Pricesystem(/* useless to set anything here since it's replaced by the fixture */), priceSystemJson)
+        .setName(priceSystemName)
+        .apply();
+    });
+
+
+    let priceListVersion;
+    cy.fixture('price/pricelistversion.json').then(priceListVersionJson => {
+      priceListVersion = Object.assign(new PriceListVersion(/* useless to set anything here since it's replaced by the fixture */), priceListVersionJson)
+        .setName(priceListVersionName)
+    });
+
+    cy.fixture('price/pricelist.json').then(pricelistJson => {
+      Object.assign(new PriceList(/* useless to set anything here since it's replaced by the fixture */), pricelistJson)
+        .setName(priceListName)
+        .setPriceSystem(priceSystemName)
+        .addPriceListVersion(priceListVersion)
+        .apply();
+    });
+
+    cy.fixture('product/simple_productCategory.json').then(productCategoryJson => {
+      Object.assign(new ProductCategory(), productCategoryJson)
+        .setName(productCategoryName)
+        .setValue(productCategoryValue)
+        .apply();
+    });
+
+
+    let productPrice;
+    cy.fixture('product/product_price.json').then(productPriceJson => {
+      productPrice = Object.assign(new ProductPrice(), productPriceJson)
+        .setPriceList(priceListName)
+    });
+
+    cy.fixture('product/simple_product.json').then(productJson => {
+      Object.assign(new Product(), productJson)
+        .setName(productName)
+        .setValue(productValue)
+        .setProductType('Service')
+        .setProductCategory(productCategoryValue + '_' + productCategoryName)
+        .addProductPrice(productPrice)
+        .apply();
+    });
+
+  });
+
+
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+//////////////////////
+
+
+  // it('Creates a Sales Invoice', function () {
+  //   new SalesInvoice(bPartnerName, salesInvoiceTargetDocumentType)
+  //     .addLine(
+  //       new SalesInvoiceLine()
+  //         .setProduct(productName)
+  //         .setQuantity(20)
+  //       // .setPackingItem('IFCO 6410 x 10 Stk')
+  //       // .setTuQuantity(2)
+  //     )
+  //     // .addLine(
+  //     //   new SalesInvoiceLine()
+  //     //     .setProduct('IFCO 6410_P001512')
+  //     //     .setQuantity(2)
+  //     // )
+  //     .setDocumentAction('Complete')
+  //     .setDocumentStatus('Completed')
+  //     .apply();
+  // });
+  //
+  //
+  // it('The Sales Invoice should not be paid', function () {
+  //   cy.isChecked('IsPaid').then(checkBoxValue => {
+  //     assert.equal(checkBoxValue, false);
   //   });
-  //   cy.fixture('product/simple_product.json').then(productJson => {
-  //     Object.assign(new Product(), productJson)
-  //       .setName(productName)
-  //       .setValue(productValue)
-  //       .setProductType('Service')
-  //       .setProductCategory(productCategoryValue + '_' + productCategoryName)
-  //       .apply();
-  //   });
-  //   cy.fixture('sales/simple_customer.json').then(customerJson => {
-  //     Object.assign(new BPartner(), customerJson)
-  //       .setName(bPartnerName)
-  //       .apply();
+  // });
+  //
+  //
+  // it('Save values needed for the next step', function () {
+  //   cy.getFieldValue('DocumentNo').then(documentNumber => {
+  //     salesInvoiceNumber = documentNumber;
   //   });
   //
-  //   cy.readAllNotifications();
+  //   cy.get('.header-breadcrumb-sitename').then(si => {
+  //     salesInvoiceTotalAmount = parseFloat(si.html().split(' ')[2], 10); // the format is "DOC_NO MM/DD/YYYY total"
+  //   });
+  //
+  //   cy.url().then(ulrr => {
+  //     salesInvoiceID = ulrr.split('/').pop();
+  //     cy.log(`salesInvoiceID is ${salesInvoiceID}`);
+  //   });
   // });
-
-  it('Creates a Sales Invoice', function () {
-    new SalesInvoice(bPartnerName, salesInvoiceTargetDocumentType)
-      .addLine(
-        new SalesInvoiceLine()
-          .setProduct('Convenience Salat 250g_P002737')
-          .setQuantity(20)
-          .setPackingItem('IFCO 6410 x 10 Stk')
-          .setTuQuantity(2)
-      )
-      .addLine(
-        new SalesInvoiceLine()
-          .setProduct('IFCO 6410_P001512')
-          .setQuantity(2)
-      )
-      .setDocumentAction('Complete')
-      .setDocumentStatus('Completed')
-      .apply();
-  });
-
-
-  it('Save values needed for the next step', function () {
-    cy.getFieldValue('DocumentNo').then(documentNumber => {
-      salesInvoiceNumber = documentNumber;
-    });
-
-    cy.get('.header-breadcrumb-sitename').then(si => {
-      salesInvoiceTotalAmount = parseFloat(si.html().split(' ')[2], 10); // the format is "DOC_NO MM/DD/YYYY total"
-    });
-
-    cy.url().then(ulrr => {
-      salesInvoiceID = ulrr.split('/').pop();
-      cy.log(`salesInvoiceID is ${salesInvoiceID}`);
-    });
-  });
-
-
-  it('The Sales Invoice should not be paid', function () {
-    cy.isChecked('IsPaid').then(checkBoxValue => {
-      assert.equal(checkBoxValue, false);
-    });
-  });
-
-
-  it('Creates a manual Payment', function () {
-    cy.visitWindow('195', 'NEW');
-
-    cy.writeIntoLookupListField('C_BPartner_ID', bPartnerName, bPartnerName);
-
-    cy.getFieldValue('DocumentNo').should('be.empty');
-    cy.selectInListField('C_DocType_ID', paymentDocumentType);
-    cy.getFieldValue('DocumentNo').should('not.be.empty');
-
-    cy.getFieldValue('PayAmt').then(val => {
-      const cast = parseFloat(val);
-      assert.equal(cast, 0);
-    });
-
-    cy.writeIntoLookupListField('C_Invoice_ID', salesInvoiceNumber, salesInvoiceNumber);
-
-    cy.processDocument('Complete', 'Completed');
-  });
-
-
-  it('Checks the paid + discount amount', function () {
-    // discount amount is taken from the advanced edit dialog of the line
-    cy.selectTab('C_AllocationLine');
-    cy.selectSingleTabRow();
-    cy.openAdvancedEdit();
-
-    let discountAmount;
-    cy.getFieldValue('DiscountAmt', true).then(val => {
-      discountAmount = parseFloat(val);
-    });
-
-    cy.getFieldValue('Amount').then(val => {
-      const value = parseFloat(val).toFixed(2);
-      paymentTotalAmount = parseFloat(salesInvoiceTotalAmount - discountAmount).toFixed(2);
-      assert.equal(value, paymentTotalAmount);
-    });
-
-    cy.pressDoneButton();
-  });
-
-
-  it('Accounting transactions are created', function () {
-    cy.wait(5000);  // give the system some breathing space
-    cy.get('body').type('{alt}6'); // open referenced-records-sidelist
-    cy.selectReference('AD_RelationType_ID-540201');
-  });
-
-
-  it('The paid Sales Invoice is linked to this Payment', function () {
-    cy.selectTab('C_AllocationLine');
-
-    cy.get('.table-flex-wrapper')
-      .find('tbody td')
-      .contains(salesInvoiceNumber)
-      .click()
-      .trigger('contextmenu');
-
-    // - invoice of the bpartner can be selected/zoomed into
-    cy.get('.context-menu-item')
-      .contains('Zoom Into');
-
-    // note: we must not click the zoom into button, since that will open a new browser tab, and cypress
-    // cannot handle that.
-    // instead we have to save the ID of the sales invoice we want to return to, and use that one to visit the window.
-
-    cy.visitWindow('167', salesInvoiceID);
-  });
-
-
-  it('Sales Invoice is now marked as paid', function () {
-    cy.isChecked('IsPaid').then(checkBoxValue => {
-      cy.log(`IsPaid = ${checkBoxValue}`);
-      assert.equal(checkBoxValue, true);
-    });
-  });
-
-  it('After selecting the Sales Invoice, the currency and the amt are updated accordingly', function () {
-
-    // todo @dh: what do you mean by this
-  });
+  //
+  // it('Creates a manual Payment', function () {
+  //   cy.visitWindow('195', 'NEW');
+  //
+  //   cy.writeIntoLookupListField('C_BPartner_ID', bPartnerName, bPartnerName);
+  //
+  //   cy.getFieldValue('DocumentNo').should('be.empty');
+  //   cy.selectInListField('C_DocType_ID', paymentDocumentType);
+  //   cy.getFieldValue('DocumentNo').should('not.be.empty');
+  //
+  //   cy.getFieldValue('PayAmt').then(val => {
+  //     const cast = parseFloat(val);
+  //     assert.equal(cast, 0);
+  //   });
+  //
+  //   cy.writeIntoLookupListField('C_Invoice_ID', salesInvoiceNumber, salesInvoiceNumber);
+  //
+  //   cy.processDocument('Complete', 'Completed');
+  // });
+  //
+  //
+  // it('Checks the paid + discount amount', function () {
+  //   // discount amount is taken from the advanced edit dialog of the line
+  //   cy.selectTab('C_AllocationLine');
+  //   cy.selectSingleTabRow();
+  //   cy.openAdvancedEdit();
+  //
+  //   let discountAmount;
+  //   cy.getFieldValue('DiscountAmt', true).then(val => {
+  //     discountAmount = parseFloat(val);
+  //   });
+  //
+  //   cy.getFieldValue('Amount').then(val => {
+  //     const value = parseFloat(val).toFixed(2);
+  //     paymentTotalAmount = parseFloat(salesInvoiceTotalAmount - discountAmount).toFixed(2);
+  //     assert.equal(value, paymentTotalAmount);
+  //   });
+  //
+  //   cy.pressDoneButton();
+  // });
+  //
+  //
+  // it('Accounting transactions are created', function () {
+  //   cy.wait(5000);  // give the system some breathing space
+  //   cy.get('body').type('{alt}6'); // open referenced-records-sidelist
+  //   cy.selectReference('AD_RelationType_ID-540201');
+  // });
+  //
+  //
+  // it('The paid Sales Invoice is linked to this Payment', function () {
+  //   cy.selectTab('C_AllocationLine');
+  //
+  //   cy.get('.table-flex-wrapper')
+  //     .find('tbody td')
+  //     .contains(salesInvoiceNumber)
+  //     .click()
+  //     .trigger('contextmenu');
+  //
+  //   // - invoice of the bpartner can be selected/zoomed into
+  //   cy.get('.context-menu-item')
+  //     .contains('Zoom Into');
+  //
+  //   // note: we must not click the zoom into button, since that will open a new browser tab, and cypress
+  //   // cannot handle that.
+  //   // instead we have to save the ID of the sales invoice we want to return to, and use that one to visit the window.
+  //
+  //   cy.visitWindow('167', salesInvoiceID);
+  // });
+  //
+  //
+  // it('Sales Invoice is now marked as paid', function () {
+  //   cy.isChecked('IsPaid').then(checkBoxValue => {
+  //     cy.log(`IsPaid = ${checkBoxValue}`);
+  //     assert.equal(checkBoxValue, true);
+  //   });
+  // });
+  //
+  // it('After selecting the Sales Invoice, the currency and the amt are updated accordingly', function () {
+  //
+  //   // todo @dh: what do you mean by this
+  // });
+  //
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
+  //////////////////////
 
 });
