24a3758d9ee8e4ab1eb60416fc279b10132a0ffc
TheBestPessimist <cristian@tbp.land>
2019-06-20 13:52:40 +0300

    Manual payment test is complete
    https://github.com/metasfresh/metasfresh-e2e/issues/111

--- cypress/integration/payment/create_manual_payment.js
+++ cypress/integration/payment/create_manual_payment.js
@@ -11,10 +11,8 @@ import {Pricesystem} from "../../support/utils/pricesystem";
 describe('Create a manual Payment for a Sales Invoice', function () {
   const timestamp = new Date().getTime();
 
-  // const bPartnerName = 'Test Kunde 1_G0001';
-
-  let salesInvoiceNumber;
   const salesInvoiceTargetDocumentType = 'Sales Invoice';
+  let salesInvoiceNumber;
   let salesInvoiceTotalAmount = 0;
   let salesInvoiceID = 0;
 
@@ -31,8 +29,6 @@ describe('Create a manual Payment for a Sales Invoice', function () {
   const productValue = `manual_payment_test ${timestamp}`;
   const productCategoryName = `ProductCategoryName ${timestamp}`;
   const productCategoryValue = `ProductNameValue ${timestamp}`;
-  const productPriceAmount = 1.34;
-  const productPriceTaxCategory = 'Normaler Steuersatz 19% (Deutschland)';
 
   const discountSchemaName = `DiscountSchemaTest ${timestamp}`;
 
@@ -40,30 +36,6 @@ describe('Create a manual Payment for a Sales Invoice', function () {
 
 
   before(function () {
-
-
-    //   cy.fixture('discount/discountschema.json').then(discountSchemaJson => {
-    //     Object.assign(new DiscountSchema(), discountSchemaJson)
-    //       .setName(discountSchemaName)
-    //       .apply();
-    //   });
-    //   cy.fixture('finance/bank.json').then(productJson => {
-    //     Object.assign(new Bank(), productJson)
-    //       .apply();
-    //   });
-    //   cy.fixture('sales/simple_customer.json').then(customerJson => {
-    //     Object.assign(new BPartner(), customerJson)
-    //       .setName(bPartnerName)
-    //       .apply();
-    //   });
-    //
-    //   cy.readAllNotifications();
-
-
-  });
-
-
-  it('creates PS PL and PLV', () => {
 
     cy.fixture('price/pricesystem.json').then(priceSystemJson => {
       Object.assign(new Pricesystem(/* useless to set anything here since it's replaced by the fixture */), priceSystemJson)
@@ -93,7 +65,6 @@ describe('Create a manual Payment for a Sales Invoice', function () {
         .apply();
     });
 
-
     let productPrice;
     cy.fixture('product/product_price.json').then(productPriceJson => {
       productPrice = Object.assign(new ProductPrice(), productPriceJson)
@@ -110,158 +81,148 @@ describe('Create a manual Payment for a Sales Invoice', function () {
         .apply();
     });
 
+    cy.fixture('discount/discountschema.json').then(discountSchemaJson => {
+      Object.assign(new DiscountSchema(), discountSchemaJson)
+        .setName(discountSchemaName)
+        .apply();
+    });
+    cy.fixture('finance/bank.json').then(productJson => {
+      Object.assign(new Bank(), productJson)
+        .apply();
+    });
+    cy.fixture('sales/simple_customer.json').then(customerJson => {
+      Object.assign(new BPartner(), customerJson)
+        .setName(bPartnerName)
+        .apply();
+    });
+
+    cy.readAllNotifications();
   });
 
 
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
-//////////////////////
+  it('Creates a Sales Invoice', function () {
+    new SalesInvoice(bPartnerName, salesInvoiceTargetDocumentType)
+      .addLine(
+        new SalesInvoiceLine()
+          .setProduct(productName)
+          .setQuantity(20)
+        // todo @dh: how to add packing item
+        // .setPackingItem('IFCO 6410 x 10 Stk')
+        // .setTuQuantity(2)
+      )
+      // .addLine(
+      // todo @dh: how to add this line?
+      //   new SalesInvoiceLine()
+      //     .setProduct('IFCO 6410_P001512')
+      //     .setQuantity(2)
+      // )
+      .setPriceList(priceListName)
+      .setDocumentAction('Complete')
+      .setDocumentStatus('Completed')
+      .apply();
+  });
 
 
-  // it('Creates a Sales Invoice', function () {
-  //   new SalesInvoice(bPartnerName, salesInvoiceTargetDocumentType)
-  //     .addLine(
-  //       new SalesInvoiceLine()
-  //         .setProduct(productName)
-  //         .setQuantity(20)
-  //       // .setPackingItem('IFCO 6410 x 10 Stk')
-  //       // .setTuQuantity(2)
-  //     )
-  //     // .addLine(
-  //     //   new SalesInvoiceLine()
-  //     //     .setProduct('IFCO 6410_P001512')
-  //     //     .setQuantity(2)
-  //     // )
-  //     .setDocumentAction('Complete')
-  //     .setDocumentStatus('Completed')
-  //     .apply();
-  // });
-  //
-  //
-  // it('The Sales Invoice should not be paid', function () {
-  //   cy.isChecked('IsPaid').then(checkBoxValue => {
-  //     assert.equal(checkBoxValue, false);
-  //   });
-  // });
-  //
-  //
-  // it('Save values needed for the next step', function () {
-  //   cy.getFieldValue('DocumentNo').then(documentNumber => {
-  //     salesInvoiceNumber = documentNumber;
-  //   });
-  //
-  //   cy.get('.header-breadcrumb-sitename').then(si => {
-  //     salesInvoiceTotalAmount = parseFloat(si.html().split(' ')[2], 10); // the format is "DOC_NO MM/DD/YYYY total"
-  //   });
-  //
-  //   cy.url().then(ulrr => {
-  //     salesInvoiceID = ulrr.split('/').pop();
-  //     cy.log(`salesInvoiceID is ${salesInvoiceID}`);
-  //   });
-  // });
-  //
-  // it('Creates a manual Payment', function () {
-  //   cy.visitWindow('195', 'NEW');
-  //
-  //   cy.writeIntoLookupListField('C_BPartner_ID', bPartnerName, bPartnerName);
-  //
-  //   cy.getFieldValue('DocumentNo').should('be.empty');
-  //   cy.selectInListField('C_DocType_ID', paymentDocumentType);
-  //   cy.getFieldValue('DocumentNo').should('not.be.empty');
-  //
-  //   cy.getFieldValue('PayAmt').then(val => {
-  //     const cast = parseFloat(val);
-  //     assert.equal(cast, 0);
-  //   });
-  //
-  //   cy.writeIntoLookupListField('C_Invoice_ID', salesInvoiceNumber, salesInvoiceNumber);
-  //
-  //   cy.processDocument('Complete', 'Completed');
-  // });
-  //
-  //
-  // it('Checks the paid + discount amount', function () {
-  //   // discount amount is taken from the advanced edit dialog of the line
-  //   cy.selectTab('C_AllocationLine');
-  //   cy.selectSingleTabRow();
-  //   cy.openAdvancedEdit();
-  //
-  //   let discountAmount;
-  //   cy.getFieldValue('DiscountAmt', true).then(val => {
-  //     discountAmount = parseFloat(val);
-  //   });
-  //
-  //   cy.getFieldValue('Amount').then(val => {
-  //     const value = parseFloat(val).toFixed(2);
-  //     paymentTotalAmount = parseFloat(salesInvoiceTotalAmount - discountAmount).toFixed(2);
-  //     assert.equal(value, paymentTotalAmount);
-  //   });
-  //
-  //   cy.pressDoneButton();
-  // });
-  //
-  //
-  // it('Accounting transactions are created', function () {
-  //   cy.wait(5000);  // give the system some breathing space
-  //   cy.get('body').type('{alt}6'); // open referenced-records-sidelist
-  //   cy.selectReference('AD_RelationType_ID-540201');
-  // });
-  //
-  //
-  // it('The paid Sales Invoice is linked to this Payment', function () {
-  //   cy.selectTab('C_AllocationLine');
-  //
-  //   cy.get('.table-flex-wrapper')
-  //     .find('tbody td')
-  //     .contains(salesInvoiceNumber)
-  //     .click()
-  //     .trigger('contextmenu');
-  //
-  //   // - invoice of the bpartner can be selected/zoomed into
-  //   cy.get('.context-menu-item')
-  //     .contains('Zoom Into');
-  //
-  //   // note: we must not click the zoom into button, since that will open a new browser tab, and cypress
-  //   // cannot handle that.
-  //   // instead we have to save the ID of the sales invoice we want to return to, and use that one to visit the window.
-  //
-  //   cy.visitWindow('167', salesInvoiceID);
-  // });
-  //
-  //
-  // it('Sales Invoice is now marked as paid', function () {
-  //   cy.isChecked('IsPaid').then(checkBoxValue => {
-  //     cy.log(`IsPaid = ${checkBoxValue}`);
-  //     assert.equal(checkBoxValue, true);
-  //   });
-  // });
-  //
-  // it('After selecting the Sales Invoice, the currency and the amt are updated accordingly', function () {
-  //
-  //   // todo @dh: what do you mean by this
-  // });
-  //
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
-  //////////////////////
+  it('The Sales Invoice should not be paid', function () {
+    cy.isChecked('IsPaid').then(checkBoxValue => {
+      assert.equal(checkBoxValue, false);
+    });
+  });
 
+
+  it('Save values needed for the next step', function () {
+    cy.getFieldValue('DocumentNo').then(documentNumber => {
+      salesInvoiceNumber = documentNumber;
+    });
+
+    cy.get('.header-breadcrumb-sitename').then(si => {
+      salesInvoiceTotalAmount = parseFloat(si.html().split(' ')[2], 10); // the format is "DOC_NO MM/DD/YYYY total"
+    });
+
+    cy.url().then(ulrr => {
+      salesInvoiceID = ulrr.split('/').pop();
+      cy.log(`salesInvoiceID is ${salesInvoiceID}`);
+    });
+  });
+
+  it('Creates a manual Payment', function () {
+    cy.visitWindow('195', 'NEW');
+
+    cy.writeIntoLookupListField('C_BPartner_ID', bPartnerName, bPartnerName);
+
+    cy.getFieldValue('DocumentNo').should('be.empty');
+    cy.selectInListField('C_DocType_ID', paymentDocumentType);
+    cy.getFieldValue('DocumentNo').should('not.be.empty');
+
+    cy.getFieldValue('PayAmt').then(val => {
+      const cast = parseFloat(val);
+      assert.equal(cast, 0);
+    });
+
+    cy.writeIntoLookupListField('C_Invoice_ID', salesInvoiceNumber, salesInvoiceNumber);
+
+    cy.processDocument('Complete', 'Completed');
+  });
+
+
+  it('Checks the paid + discount amount', function () {
+    // discount amount is taken from the advanced edit dialog of the line
+    cy.selectTab('C_AllocationLine');
+    cy.selectSingleTabRow();
+    cy.openAdvancedEdit();
+
+    let discountAmount;
+    cy.getFieldValue('DiscountAmt', true).then(val => {
+      discountAmount = parseFloat(val);
+    });
+
+    cy.getFieldValue('Amount').then(val => {
+      const value = parseFloat(val).toFixed(2);
+      paymentTotalAmount = parseFloat(salesInvoiceTotalAmount - discountAmount).toFixed(2);
+      assert.equal(value, paymentTotalAmount);
+    });
+
+    cy.pressDoneButton();
+  });
+
+
+  it('Accounting transactions are created', function () {
+    cy.wait(5000);  // give the system some breathing space
+    cy.get('body').type('{alt}6'); // open referenced-records-sidelist
+    cy.selectReference('AD_RelationType_ID-540201');
+  });
+
+
+  it('The paid Sales Invoice is linked to this Payment', function () {
+    cy.selectTab('C_AllocationLine');
+
+    cy.get('.table-flex-wrapper')
+      .find('tbody td')
+      .contains(salesInvoiceNumber)
+      .click()
+      .trigger('contextmenu');
+
+    // - invoice of the bpartner can be selected/zoomed into
+    cy.get('.context-menu-item')
+      .contains('Zoom Into');
+
+    // note: we must not click the zoom into button, since that will open a new browser tab, and cypress
+    // cannot handle that.
+    // instead we have to save the ID of the sales invoice we want to return to, and use that one to visit the window.
+
+    cy.visitWindow('167', salesInvoiceID);
+  });
+
+
+  it('Sales Invoice is now marked as paid', function () {
+    cy.isChecked('IsPaid').then(checkBoxValue => {
+      cy.log(`IsPaid = ${checkBoxValue}`);
+      assert.equal(checkBoxValue, true);
+    });
+  });
+
+  it('After selecting the Sales Invoice, the currency and the amt are updated accordingly', function () {
+
+    // todo @dh: what do you mean by this
+  });
 });
