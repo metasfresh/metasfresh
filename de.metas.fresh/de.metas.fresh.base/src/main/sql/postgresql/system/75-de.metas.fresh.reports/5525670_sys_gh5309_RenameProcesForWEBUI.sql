c94e1a4fc911e843991ccb7e8fd9402d4203f912
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-06-21 11:18:51 +0300

    #5309 Rename process for webui and fix text sizze
    #5309 Adjustment of Tax Code Report

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5525670_sys_gh5309_RenameProcesForWEBUI.sql
@@ -0,0 +1,6 @@
+-- 2019-06-20T18:28:04.605
+-- 2019-06-21T11:16:47.310
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET InternalName='Mehrwertsteuer-Verprobung', WEBUI_NameBrowse='Mehrwertsteuer-Verprobung',Updated=TO_TIMESTAMP('2019-06-21 11:16:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540946
+;
+
