e9d27fb406d4e68188b530cfce5c155e647b79f4
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-06-25 09:01:34 +0300

    2160 changed update statement
    #2160 Fix the en_US trl for tax category

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/552588_sys_gh2160_update_trl_for_tax_category.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/552588_sys_gh2160_update_trl_for_tax_category.sql
@@ -1,1 +1,6 @@
-update c_taxcategory_trl set name = 'Regular Tax Rate 19% (Germany)',istranslated='Y' where ad_language='en_US' and name='Normaler Steuersatz 19% (Deutschland)';
+update c_taxcategory_trl 
+set name = 'Regular Tax Rate 19% (Germany)', istranslated='Y', 
+	updatedby=99, /*user-id used to indicate "manual migration"*/
+	updated='2019-06-25 08:58:32.935051+03' /*select now();*/
+where c_taxcategory_id=1000009
+	and ad_language='en_US';
