4f272a00ed7a31f7f21d393f9ce6f619cb0b480d
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-06-26 11:57:23 +0300

    #5348 customs tariff in M_Product
    https://github.com/metasfresh/metasfresh/issues/5348

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5526080_sys_gh5348_CustmosTariff_In_M_Product_DDL.sql
@@ -0,0 +1,6 @@
+
+-- 2019-06-26T11:51:11.170
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ SELECT public.db_alter_table('M_Product','ALTER TABLE public.M_Product ADD COLUMN CustomsTariff NUMERIC')
+;
+
