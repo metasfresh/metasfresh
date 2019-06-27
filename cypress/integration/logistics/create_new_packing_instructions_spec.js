41676b817425001e98c822e97d497e972cd1d369
Oliviu <oliviu.pis@sensidev.com>
2019-06-27 12:22:01 +0300

    test improved and changed as requested

--- cypress/integration/logistics/create_new_packing_instructions_spec.js
+++ cypress/integration/logistics/create_new_packing_instructions_spec.js
@@ -1,35 +1,58 @@
+import { BPartner } from '../../support/utils/bpartner';
+import { Product } from '../../support/utils/product';
+
 describe('create new packing instructions ', function() {
+  const timestamp = new Date().getTime();
+  const customerName = `TestBPartnerPackingMaterial ${timestamp}`;
+  const packingMaterialInstruction = `TestPackingMaterialInstruction ${timestamp}`;
+  const productName = `TestProductPackingMaterial ${timestamp}`;
+  const packingMaterial = `TestPackingMaterial ${timestamp}`;
+
   before(function() {
-    cy.visit('/window/540343');
+    cy.fixture('product/simple_product.json').then(productJson => {
+      Object.assign(new Product(), productJson)
+        .setName(productName)
+        .setProductCategory('24_Gebinde')
+        .apply();
+    });
+
+    cy.visitWindow('540192');
+    cy.clickHeaderNav(Cypress.messages.window.new.caption);
+    cy.writeIntoStringField('Name', packingMaterial);
+    cy.selectInListField('M_Product_ID', productName);
+
+    cy.fixture('sales/simple_customer.json').then(customerJson => {
+      Object.assign(new BPartner(), customerJson)
+        .setName(customerName)
+        .apply();
+    });
   });
 
-  const name = 'TestPackingMaterial';
-
   it('creating packing instructions', function() {
+    cy.visitWindow('540343');
     cy.clickHeaderNav(Cypress.messages.window.new.caption);
-    cy.writeIntoStringField('Name', name);
-    cy.wait(1000);
+    cy.writeIntoStringField('Name', packingMaterialInstruction);
   });
 
   it('creating packing instructions version', function() {
-    cy.visit('/window/540344');
+    cy.visitWindow('540344');
     cy.clickHeaderNav(Cypress.messages.window.new.caption);
-    cy.selectInListField('M_HU_PI_ID', name);
-    cy.writeIntoStringField('Name', name);
+    cy.selectInListField('M_HU_PI_ID', packingMaterialInstruction);
+    cy.writeIntoStringField('Name', packingMaterialInstruction);
     cy.selectInListField('HU_UnitType', 'Load/Logistique Unit');
   });
 
   it('add new in modal', function() {
     cy.pressAddNewButton();
     cy.selectInListField('ItemType', 'Packmittel');
-    cy.selectInListField('M_HU_PackingMaterial_ID', name);
+    cy.selectInListField('M_HU_PackingMaterial_ID', packingMaterial);
     cy.pressDoneButton();
 
     cy.pressAddNewButton();
     cy.selectInListField('ItemType', 'Unter-Packvorschrift');
     cy.selectInListField('Included_HU_PI_ID', 'IFCO 6416');
     cy.writeIntoStringField('Qty', '10');
-    cy.writeIntoLookupListField('C_BPartner_ID', 'c', '1000001_Customer1 1560426344773');
+    cy.writeIntoLookupListField('C_BPartner_ID', customerName, customerName);
     cy.pressDoneButton();
   });
 });
