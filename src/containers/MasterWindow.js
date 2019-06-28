1d1cc76393b643fea52b1c47cb07d7f406cfb389
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-06-29 00:48:37 +0200

    pretier-fix

--- src/containers/MasterWindow.js
+++ src/containers/MasterWindow.js
@@ -221,10 +221,10 @@ class MasterWindow extends Component {
   };
 
   initEventListeners = () => {
-	if (!navigator.userAgent.includes('Cypress')) {
-	  // try workaround https://github.com/cypress-io/cypress/issues/1235#issuecomment-411839157 for our "hanging" problem
+    if (!navigator.userAgent.includes('Cypress')) {
+      // try workaround https://github.com/cypress-io/cypress/issues/1235#issuecomment-411839157 for our "hanging" problem
       window.addEventListener('beforeunload', this.confirm);
-	}
+    }
   };
 
   removeEventListeners = () => {
