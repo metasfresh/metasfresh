b1b43143c3787554d9b4531d1715e82d62f6d00f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-07-01 16:59:35 +0200

    fix bpartner_group_spec by having the simplest possible partner
    now even *with* the fixed spec file

---
+++ cypress/integration/masterdata/bpartner_group_spec.js
@@ -0,0 +1,33 @@
+import { BPartner } from '../../support/utils/bpartner';
+
+describe('Create new bpartner group, https://github.com/metasfresh/metasfresh-e2e/issues/70', function() {
+  const timestamp = new Date().getTime(); // used in the document names, for ordering
+  const GroupTestPartnerName = `GroupTestPartnerName ${timestamp}`;
+  const GroupName = `TestGroup ${timestamp}`;
+  let groupDocumentId;
+
+  //create bpartnergroup
+  it('Create bpartnergroup', function() {
+    cy.visitWindow('192', 'NEW', 'newGroupId');
+    cy.writeIntoStringField('Name', `TestGroup ${timestamp}`, false);
+    cy.clearField('Value');
+    cy.writeIntoStringField('Value', `TestGroup ${timestamp}`, false);
+    cy.get('@newGroupId').then(groupId => {
+      groupDocumentId = groupId.documentId;
+    });
+  });
+
+  //create bpartner
+  it('Create Testpartner', function() {
+    new BPartner().setName(GroupTestPartnerName).apply();
+    cy.selectInListField('C_BP_Group_ID', GroupName, false /*modal*/, '/rest/api/window/.*' /*rewriteUrl*/);
+  });
+
+  //check bpartnergroup
+  it('Check bpartnergroup', function() {
+    cy.visitWindow('192', groupDocumentId);
+
+    cy.selectTab('C_BPartner');
+    cy.get('.cell-text-wrapper.text-cell').contains(GroupTestPartnerName);
+  });
+});
