d3b30fca30f623149797173d2ea15fa5381fbd89
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-07-02 22:48:47 +0200

    smaller changes; also apply our prettier rules

--- cypress/integration/inventory/inventory_aggregatedHUs_spec.js
+++ cypress/integration/inventory/inventory_aggregatedHUs_spec.js
@@ -1,13 +1,10 @@
-import { inventory } from '../../page_objects/inventory';
-
 import { Product } from '../../support/utils/product';
 
-import { NewInventory, NewInventoryLine } from '../../support/utils/create_inventory';
+import { Inventory, InventoryLine } from '../../support/utils/inventory';
 
 import { getLanguageSpecific } from '../../support/utils/utils';
-//import { toggleNotFrequentFilters, selectNotFrequentFilterWidget, applyFilters } from '../../support/functions';
 
-describe('Aggregated inventory test', function() {
+describe('Aggregated-HUs inventory test', function() {
   const timestamp = new Date().getTime();
   const productName = `AggregatedHUsInventory ${timestamp}`;
   const productValue = `${timestamp}`;
@@ -21,20 +18,21 @@ describe('Aggregated inventory test', function() {
         .apply();
     });
   });
-  
+
   it('Create a new single-HU inventory doc', function() {
     cy.fixture('product/simple_product.json').then(productJson => {
       const uomName = getLanguageSpecific(productJson, 'c_uom');
       cy.fixture('inventory/inventory.json').then(inventoryJson => {
         const docTypeName = getLanguageSpecific(inventoryJson, `aggregatedHUsInventoryDocTypeName`);
-        var inventoryLine = new NewInventoryLine()
-            .setProductName(productName)
-            .setQuantity(`20`)
-            .setC_UOM_ID(uomName)
-            .setM_Locator_ID('0_0_0')
-            .setIsCounted(true);
 
-        var newInventory = new NewInventory()
+        const inventoryLine = new InventoryLine()
+          .setProductName(productName)
+          .setQuantity(`20`)
+          .setC_UOM_ID(uomName)
+          .setM_Locator_ID('0_0_0')
+          .setIsCounted(true);
+
+        new Inventory()
           .setWarehouse(inventoryJson.warehouseName)
           .setDocType(docTypeName)
           .addInventoryLine(inventoryLine)
@@ -42,9 +40,8 @@ describe('Aggregated inventory test', function() {
       });
     });
   });
-  
-    
-    //check if snapshots match
+
+  //check if snapshots match
   /*
   it('Check snapshots', function() {
     cy.get('@newInventoryRecord').then(newInventoryRecord => {
@@ -53,6 +50,3 @@ describe('Aggregated inventory test', function() {
   });
   */
 });
-
-
-
