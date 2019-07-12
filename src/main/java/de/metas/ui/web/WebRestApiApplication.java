eace2b0ed2bba73d5c04438036484d751df03bcb
Teo Sarca <teo.sarca@metasfresh.com>
2019-07-12 15:25:09 +0300

    use JsonObjectMapperHolder
    https://github.com/metasfresh/metasfresh/issues/5380

--- src/main/java/de/metas/ui/web/WebRestApiApplication.java
+++ src/main/java/de/metas/ui/web/WebRestApiApplication.java
@@ -9,7 +9,6 @@ import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.service.ISysConfigBL;
 import org.adempiere.util.lang.IAutoCloseable;
-import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.compiere.Adempiere;
 import org.compiere.Adempiere.RunMode;
@@ -21,18 +20,20 @@ import org.elasticsearch.common.logging.slf4j.Slf4jESLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.metas.JsonObjectMapperHolder;
 import de.metas.Profiles;
 import de.metas.ui.web.base.model.I_T_WEBUI_ViewSelection;
 import de.metas.ui.web.session.WebRestApiContextProvider;
@@ -123,6 +124,13 @@ public class WebRestApiApplication
 		return activeProfiles;
 	}
 
+	@Bean
+	@Primary
+	public ObjectMapper jsonObjectMapper()
+	{
+		return JsonObjectMapperHolder.sharedJsonObjectMapper();
+	}
+
 	@Bean(Adempiere.BEAN_NAME)
 	public Adempiere adempiere(final WebRestApiContextProvider webuiContextProvider)
 	{
@@ -141,26 +149,16 @@ public class WebRestApiApplication
 	@Bean
 	public EmbeddedServletContainerCustomizer servletContainerCustomizer()
 	{
-		return new EmbeddedServletContainerCustomizer()
-		{
-			@Override
-			public void customize(final ConfigurableEmbeddedServletContainer servletContainer)
-			{
-				final TomcatEmbeddedServletContainerFactory tomcatContainerFactory = (TomcatEmbeddedServletContainerFactory)servletContainer;
-				tomcatContainerFactory.addConnectorCustomizers(new TomcatConnectorCustomizer()
-				{
-					@Override
-					public void customize(final Connector connector)
-					{
-						final AbstractHttp11Protocol<?> httpProtocol = (AbstractHttp11Protocol<?>)connector.getProtocolHandler();
-						httpProtocol.setCompression("on");
-						httpProtocol.setCompressionMinSize(256);
-						final String mimeTypes = httpProtocol.getCompressibleMimeType();
-						final String mimeTypesWithJson = mimeTypes + "," + MediaType.APPLICATION_JSON_VALUE + ",application/javascript";
-						httpProtocol.setCompressibleMimeType(mimeTypesWithJson);
-					}
-				});
-			}
+		return servletContainer -> {
+			final TomcatEmbeddedServletContainerFactory tomcatContainerFactory = (TomcatEmbeddedServletContainerFactory)servletContainer;
+			tomcatContainerFactory.addConnectorCustomizers(connector -> {
+				final AbstractHttp11Protocol<?> httpProtocol = (AbstractHttp11Protocol<?>)connector.getProtocolHandler();
+				httpProtocol.setCompression("on");
+				httpProtocol.setCompressionMinSize(256);
+				final String mimeTypes = httpProtocol.getCompressibleMimeType();
+				final String mimeTypesWithJson = mimeTypes + "," + MediaType.APPLICATION_JSON_VALUE + ",application/javascript";
+				httpProtocol.setCompressibleMimeType(mimeTypesWithJson);
+			});
 		};
 	}
 
