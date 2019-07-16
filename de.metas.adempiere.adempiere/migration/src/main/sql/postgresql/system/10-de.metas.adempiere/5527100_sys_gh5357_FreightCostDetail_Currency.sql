62935c5f2dd11820dd0cf82420511f160816d835
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-07-16 10:52:34 +0300

    #5357 reorganize scripts
    https://github.com/metasfresh/metasfresh/issues/5357

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5527100_sys_gh5357_FreightCostDetail_Currency.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5527100_sys_gh5357_FreightCostDetail_Currency.sql
@@ -27,30 +27,3 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 ALTER TABLE M_FreightCostDetail ADD CONSTRAINT CCurrency_MFreightCostDetail FOREIGN KEY (C_Currency_ID) REFERENCES public.C_Currency DEFERRABLE INITIALLY DEFERRED
 ;
 
-
-
--- 2019-07-15T17:29:30.342
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2019-07-15 17:29:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=568420
-;
-
-
-
-
-
-UPDATE M_FreightCostDetail d
-SET C_Currency_ID = coalesce((select c.C_Currency_ID from C_Country c where c.C_Country_ID = d.C_Country_Id), 102);
-
-
-
-
--- 2019-07-15T17:56:13.973
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO t_alter_column values('m_freightcostdetail','C_Currency_ID','NUMERIC(10)',null,null)
-;
-
--- 2019-07-15T17:56:13.981
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO t_alter_column values('m_freightcostdetail','C_Currency_ID',null,'NOT NULL',null)
-;
-
