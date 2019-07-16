d83858075146c022cf4a3141bebf13bc24098232
TheBestPessimist <cristian@tbp.land>
2019-07-16 09:41:07 +0300

    Add SalesOrderLine class
    https://github.com/metasfresh/metasfresh-e2e/issues/132

--- cypress/support/utils/sales_order.js
+++ cypress/support/utils/sales_order.js
@@ -3,11 +3,17 @@ export class SalesOrder {
     this.reference = reference;
     this.bPartner = undefined;
     this.bPartnerLocation = undefined;
+    this.lines = [];
   }
 
   setBPartner(bPartner) {
     cy.log(`SalesOrder - setBPartner = ${bPartner}`);
     this.bPartner = bPartner;
+    return this;
+  }
+
+  addLine(salesOrderLine) {
+    this.lines.push(salesOrderLine);
     return this;
   }
 
@@ -23,30 +29,89 @@ export class SalesOrder {
     return this;
   }
 
+  setDocumentAction(documentAction) {
+    this.documentAction = documentAction;
+    return this;
+  }
+
+  setDocumentStatus(documentStatus) {
+    this.documentStatus = documentStatus;
+    return this;
+  }
+
   apply() {
     cy.log(`SalesOrder - apply - START (${this.reference})`);
-    applySalesOrder(this);
+    SalesOrder.applySalesOrder(this);
     cy.log(`SalesOrder - apply - END (${this.reference})`);
     return this;
   }
+
+  static applySalesOrder(salesOrder) {
+    describe(`Create new salesOrder`, function() {
+      cy.visitWindow('143', 'NEW');
+      cy.get('.header-breadcrumb-sitename').should('contain', '<');
+
+      cy.writeIntoLookupListField('C_BPartner_ID', salesOrder.bPartner, salesOrder.bPartner);
+      if (salesOrder.bPartnerLocation) {
+        cy.writeIntoLookupListField(
+          'C_BPartner_Location_ID',
+          salesOrder.bPartnerLocation,
+          salesOrder.bPartnerLocation,
+          true /*typeList*/
+        );
+      }
+      cy.get('.header-breadcrumb-sitename').should('not.contain', '<');
+
+      if (salesOrder.reference) {
+        cy.writeIntoStringField('POReference', salesOrder.reference);
+      }
+
+      salesOrder.lines.forEach(line => {
+        SalesOrder.applyLine(line);
+      });
+
+      if (salesOrder.documentAction) {
+        if (salesOrder.documentStatus) {
+          cy.processDocument(salesOrder.documentAction, salesOrder.documentStatus);
+        } else {
+          cy.processDocument(salesOrder.documentAction);
+        }
+      }
+    });
+  }
+
+  static applyLine(salesOrderLine) {
+    cy.selectTab('C_OrderLine');
+    cy.pressAddNewButton();
+
+    cy.writeIntoLookupListField(
+      'M_Product_ID',
+      salesOrderLine.product,
+      salesOrderLine.product,
+      false /*typeList*/,
+      true /*modal*/
+    );
+
+    cy.writeIntoStringField(
+      'QtyEntered',
+      salesOrderLine.quantity,
+      true /*modal*/,
+      null /*rewriteUrl*/,
+      true /*noRequest, bc the patch response is e.g. 20 and we would be waiting for e.g. '20' */
+    );
+
+    cy.pressDoneButton();
+  }
 }
 
-function applySalesOrder(salesOrder) {
-  describe(`Create new salesOrder`, function() {
-    cy.visitWindow('143', 'NEW');
-    cy.get('.header-breadcrumb-sitename').should('contain', '<');
+export class SalesOrderLine {
+  setProduct(product) {
+    this.product = product;
+    return this;
+  }
 
-    cy.writeIntoLookupListField('C_BPartner_ID', salesOrder.bPartner, salesOrder.bPartner);
-    if (salesOrder.bPartnerLocation) {
-      cy.writeIntoLookupListField(
-        'C_BPartner_Location_ID',
-        salesOrder.bPartnerLocation,
-        salesOrder.bPartnerLocation,
-        true /*typeList*/
-      );
-    }
-    cy.get('.header-breadcrumb-sitename').should('not.contain', '<');
-
-    cy.writeIntoStringField('POReference', salesOrder.reference);
-  });
+  setQuantity(quantity) {
+    this.quantity = quantity;
+    return this;
+  }
 }
