007ee4783603c8172942b56bd93c95e909e91cc6
TheBestPessimist <cristian@tbp.land>
2019-07-17 10:23:18 +0300

    More checks for `executeHeaderAction` so that actions aren't triggered before the content loads
    https://github.com/metasfresh/metasfresh-e2e/issues/132

--- cypress/support/commands/action.js
+++ cypress/support/commands/action.js
@@ -3,12 +3,24 @@ function executeHeaderAction(actionName) {
    * Only specific windows can have actions. They match one of the following urls:
    *
    * https://dev586.metasfresh.com/window/123?viewId=123-o&page=1
+   *    - in this case also '.table-flex-wrapper' should exist
    * https://dev586.metasfresh.com/window/123/2156425
+   *    - in this case also '.panel' should exist
    *
    * This match is needed because cypress is so fast that it may press the action button before any viewId is available, and the system will error out.
    */
   cy.url().should('matches', new RegExp(`window/[0-9]+(/[0-9]+|.*viewId=)`));
-  cy.get('.indicator-pending').should('not.exist');
+
+  cy.url().then(url => {
+    const listRegexp = new RegExp(`window/[0-9]+.*viewId=`);
+    // const gridRegexp = new RegExp(`window/[0-9]+/[0-9]+`);
+
+    if (url.match(listRegexp)) {
+      cy.get('.table-flex-wrapper').should('exist');
+    } else {
+      cy.get('.panel').should('exist');
+    }
+  });
 
   cy.get('.header-container .btn-square .meta-icon-more').click();
   cy.get('.subheader-container').should('exist');
@@ -18,7 +30,7 @@ function executeHeaderAction(actionName) {
 Cypress.Commands.add('clickHeaderNav', navName => {
   const name = navName.toLowerCase().replace(/\s/g, '');
 
-  describe('Fire header action with a certain name', function () {
+  describe('Fire header action with a certain name', function() {
     cy.get('.header-container .btn-header').click();
     cy.get('.subheader-container').should('exist');
 
@@ -27,13 +39,13 @@ Cypress.Commands.add('clickHeaderNav', navName => {
 });
 
 Cypress.Commands.add('executeHeaderAction', actionName => {
-  describe('Fire header action with a certain name', function () {
+  describe('Fire header action with a certain name', function() {
     executeHeaderAction(actionName);
   });
 });
 
 Cypress.Commands.add('executeHeaderActionWithDialog', actionName => {
-  describe('Fire header action with a certain name and expect a modal dialog to pop up within 10 secs', function () {
+  describe('Fire header action with a certain name and expect a modal dialog to pop up within 10 secs', function() {
     cy.server();
     cy.route('GET', 'rest/api/process/*/layout').as('dialogLayout');
 
@@ -42,13 +54,13 @@ Cypress.Commands.add('executeHeaderActionWithDialog', actionName => {
     cy.wait('@dialogLayout');
 
     return cy
-      .get('.panel-modal', {timeout: 10000}) // wait up to 10 secs for the modal to appear
+      .get('.panel-modal', { timeout: 10000 }) // wait up to 10 secs for the modal to appear
       .should('exist');
   });
 });
 
 Cypress.Commands.add('executeQuickAction', (actionName, active) => {
-  describe('Fire a quick action with a certain name', function () {
+  describe('Fire a quick action with a certain name', function() {
     let path = `.quick-actions-wrapper`; // default action
 
     if (!active) {
@@ -63,7 +75,7 @@ Cypress.Commands.add('executeQuickAction', (actionName, active) => {
     return cy
       .get(path)
       .click()
-      .get('.panel-modal', {timeout: 10000}) // wait up to 10 secs for the modal to appear
+      .get('.panel-modal', { timeout: 10000 }) // wait up to 10 secs for the modal to appear
       .should('exist');
   });
 });
