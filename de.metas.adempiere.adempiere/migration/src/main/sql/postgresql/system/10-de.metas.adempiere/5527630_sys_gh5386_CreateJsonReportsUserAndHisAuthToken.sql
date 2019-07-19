3fd677e76bf49bf708825d71afd658a86e41b81c
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-07-19 15:31:27 +0300

    #5386 Fix script
    #5386 Create a fallback for auth token when using Jasper Json reports

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5527630_sys_gh5386_CreateJsonReportsUserAndHisAuthToken.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5527630_sys_gh5386_CreateJsonReportsUserAndHisAuthToken.sql
@@ -2,7 +2,7 @@
 
 -- 2019-07-19T09:29:11.356Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_User (AD_Client_ID,AD_Language,AD_Org_ID,AD_User_ID,Comments,Created,CreatedBy,CustomerProtection,Description,Fresh_Gift,IsAccountLocked,IsActive,IsBillToContact_Default,IsDecider,IsDefaultContact,IsFullBPAccess,IsInPayroll,IsLoginAsHostKey,IsManagement,IsMFProcurementUser,IsMultiplier,IsNews,IsNewsletter,IsPrivate,IsPurchaseContact,IsPurchaseContact_Default,IsSalesContact,IsSalesContact_Default,IsShipToContact_Default,IsSubjectMatterContact,IsSystemUser,Login,LoginFailureCount,Name,NotificationType,Processing,PromotionalActivities,ProtectionArea,UnlockAccount,Updated,UpdatedBy,Value) VALUES (1000000,'en_US',1000000,540057,'',TO_TIMESTAMP('2019-07-19 12:29:11','YYYY-MM-DD HH24:MI:SS'),100,'N','User used for authentication for Json Reports','N','N','Y','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','Json Reports',0,'Json Reports','N','N','N','N','N',TO_TIMESTAMP('2019-07-19 12:29:11','YYYY-MM-DD HH24:MI:SS'),100,'jreports')
+INSERT INTO AD_User (AD_Client_ID,AD_Language,AD_Org_ID,AD_User_ID,Comments,Created,CreatedBy,Description,Fresh_Gift,IsAccountLocked,IsActive,IsBillToContact_Default,IsDecider,IsDefaultContact,IsFullBPAccess,IsInPayroll,IsLoginAsHostKey,IsManagement,IsMFProcurementUser,IsMultiplier,IsNews,IsNewsletter,IsPrivate,IsPurchaseContact,IsPurchaseContact_Default,IsSalesContact,IsSalesContact_Default,IsShipToContact_Default,IsSubjectMatterContact,IsSystemUser,Login,LoginFailureCount,Name,NotificationType,Processing,UnlockAccount,Updated,UpdatedBy,Value) VALUES (1000000,'en_US',1000000,540057,'',TO_TIMESTAMP('2019-07-19 12:29:11','YYYY-MM-DD HH24:MI:SS'),100,'User used for authentication for Json Reports','N','N','Y','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','Json Reports',0,'Json Reports','N','N','N',TO_TIMESTAMP('2019-07-19 12:29:11','YYYY-MM-DD HH24:MI:SS'),100,'jreports')
 ;
 
 -- 2019-07-19T09:29:12.118Z
@@ -19,3 +19,5 @@ INSERT INTO AD_User_Record_Access (Access,AD_Client_ID,AD_Org_ID,AD_Table_ID,AD_
 -- Create a default auth token for user Json Reports
 
 INSERT INTO public.ad_user_authtoken (ad_client_id, ad_org_id, ad_role_id, ad_user_authtoken_id, ad_user_id, authtoken, created, createdby, description, isactive, updated, updatedby) VALUES (1000000, 1000000, 1000000, 540001, 540057, '0d46ecc8fc8148049ddc7eb19433d3b6', '2019-07-19 09:32:54.000000', 100, null, 'Y', '2019-07-19 09:32:59.000000', 100);
+
+
