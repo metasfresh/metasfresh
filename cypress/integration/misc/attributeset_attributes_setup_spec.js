47842b7c67396a030c3d7ef33d67b3b89d705c23
TheBestPessimist <cristian@tbp.land>
2019-07-26 10:25:09 +0300

    Update Attribute spec
    https://github.com/metasfresh/metasfresh-e2e/issues/233

--- cypress/integration/misc/attributeset_attributes_setup_spec.js
+++ cypress/integration/misc/attributeset_attributes_setup_spec.js
@@ -1,17 +1,26 @@
-import { Attribute } from '../../support/utils/attribute';
+import { Attribute, AttributeSet } from '../../support/utils/attribute';
+import { humanReadableNow } from '../../support/utils/utils';
 
 describe('Create Attribute Masterdata for Automatic End2End Tests with cypress https://github.com/metasfresh/metasfresh-e2e/issues/13', function() {
-  const timestamp = new Date().getTime(); // used in the document names, for ordering
-  const attributeName = `ListAttributeName ${timestamp}`;
-  const attributeValue = `ListAttributeValue ${timestamp}`;
-  //const attributeSetName = `TestAttributeSet ${timestamp}`;
+  const date = humanReadableNow();
+  const attributeName = `ListAttributeName ${date}`;
+  const attributeValue = `ListAttributeValue ${date}`;
+  const attributeSetName = `AttributeSet ${date}`;
+  let attribute;
 
-  it('Create a new Attribute Set and Attributes', function() {
+  it('Create a new Attribute', function() {
     cy.fixture('misc/simple_attribute.json').then(attributeJson => {
-      Object.assign(new Attribute(), attributeJson)
+      attribute = Object.assign(new Attribute(), attributeJson)
         .setName(attributeName)
         .setValue(attributeValue)
         .apply();
     });
+  });
+
+  it('Create AttributeSet', function() {
+    // eslint-disable-next-line
+    new AttributeSet(attributeSetName)
+      .addAttribute(attribute)
+      .apply();
   });
 });
