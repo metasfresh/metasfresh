f0ad8f45a7d8fff035b3fad6375169fdb7323dbc
Teo Sarca <teo.sarca@metasfresh.com>
2019-07-27 00:16:18 +0300

    Create 5528260_sys_gh5139_fix_processes.sql
    https://github.com/metasfresh/metasfresh/issues/5139

---
+++ de.metas.vertical.pharma.securpharm/src/main/sql/postgresql/system/86-de.metas.vertical.pharma.securpharm/5528260_sys_gh5139_fix_processes.sql
@@ -0,0 +1,10 @@
+-- 2019-07-26T21:12:29.028Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Element_ID=543328, ColumnName='Barcode', Description=NULL, Name='Barcode',Updated=TO_TIMESTAMP('2019-07-27 00:12:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541409
+;
+
+-- 2019-07-26T21:13:10.559Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Element_ID=543328, ColumnName='Barcode', Description=NULL, EntityType='de.metas.ui.web', Name='Barcode',Updated=TO_TIMESTAMP('2019-07-27 00:13:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541439
+;
+
