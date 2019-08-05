20120a9c950ed3e4db1df19efaf359d106b5b8a0
TheBestPessimist <cristian@tbp.land>
2019-08-05 10:15:34 +0300

    Update `inventory_singleHU_spec.js`
    https://github.com/metasfresh/metasfresh-e2e/issues/248

--- cypress/integration/inventory/inventory_singleHU_spec.js
+++ cypress/integration/inventory/inventory_singleHU_spec.js
@@ -2,51 +2,46 @@ import { Product } from '../../support/utils/product';
 
 import { Inventory, InventoryLine } from '../../support/utils/inventory';
 
-import { getLanguageSpecific } from '../../support/utils/utils';
+import { getLanguageSpecific, humanReadableNow } from '../../support/utils/utils';
 
-describe('Single-HUs inventory test', function() {
-  const timestamp = new Date().getTime();
-  const productName = `SingleHUInventory ${timestamp}`;
-  const productValue = `${timestamp}`;
+const date = humanReadableNow();
+const productName = `SingleHUInventory_${date}`;
+const productQty = 20;
+const locatorId = 'Hauptlager_StdWarehouse_Hauptlager_0_0_0';
 
-  before(function() {
+describe('Create a single HU', function() {
+  it('Create Product', function() {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000); // see comment/doc of getLanguageSpecific
     cy.fixture('product/simple_product.json').then(productJson => {
       Object.assign(new Product(), productJson)
         .setName(productName)
-        .setValue(productValue)
+        .setValue(productName)
         .apply();
     });
   });
 
   it('Create a new single-HU inventory doc', function() {
+    let uomName;
     cy.fixture('product/simple_product.json').then(productJson => {
-      const uomName = getLanguageSpecific(productJson, 'c_uom');
-      cy.fixture('inventory/inventory.json').then(inventoryJson => {
-        const docTypeName = getLanguageSpecific(inventoryJson, 'singleHUInventoryDocTypeName');
+      uomName = getLanguageSpecific(productJson, 'c_uom');
+    });
 
-        const inventoryLine = new InventoryLine()
-          .setProductName(productName)
-          .setQuantity(`20`)
-          .setC_UOM_ID(uomName)
-          .setM_Locator_ID('0_0_0')
-          .setIsCounted(true);
+    cy.fixture('inventory/inventory.json').then(inventoryJson => {
+      const docTypeName = getLanguageSpecific(inventoryJson, 'singleHUInventoryDocTypeName');
 
-        new Inventory()
-          .setWarehouse(inventoryJson.warehouseName)
-          .setDocType(docTypeName)
-          .addInventoryLine(inventoryLine)
-          .apply();
-      });
+      const inventoryLine = new InventoryLine()
+        .setProductName(productName)
+        .setQuantity(productQty)
+        .setC_UOM_ID(uomName)
+        .setM_Locator_ID(locatorId)
+        .setIsCounted(true);
+
+      new Inventory()
+        .setWarehouse(inventoryJson.warehouseName)
+        .setDocType(docTypeName)
+        .addInventoryLine(inventoryLine)
+        .apply();
     });
   });
-
-  //check if snapshots match
-  /*
-  it('Check snapshots', function() {
-    cy.get('@newInventoryRecord').then(newInventoryRecord => {
-      inventory.toMatchSnapshots(newInventoryRecord, 'inventory_singleHU');
-    });
-  });
-  */
 });
