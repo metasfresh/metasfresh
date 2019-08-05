24d81a60ddb2c89526c5b66c9cdccd1370394155
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-08-05 14:09:42 +0300

    #116 create transportation order - tour
    #116 Create new tour and transportation order

--- cypress/integration/logistics/create_new_tour_and_transportation_order_spec.js
+++ cypress/integration/logistics/create_new_tour_and_transportation_order_spec.js
@@ -1,20 +1,32 @@
+import { BPartner } from '../../support/utils/bpartner';
+import { humanReadableNow } from '../../support/utils/utils';
 import { Tour } from '../../support/utils/tour';
 import { TransportationOrder } from '../../support/utils/transportationOrder';
 
 describe('create new tour and transportation order', function() {
-  const timestamp = new Date().getTime();
-  const testTour = `TestTour ${timestamp}`;
+  const date = humanReadableNow();
+  const shipperBPartner = `metasfresh AG`;
+  const shipperLocation = `Am Nossbacher Weg 2`;
+  const tourName = `TestTour_${date}`;
+  const shipper = `Eigentransport`;
+  const documentNo = `X`;
 
-  it('create tour and transportation order', function() {
-    cy.fixture('logistics/tour.json').then(tour => {
-      Object.assign(new Tour(), tour)
-        .setName(testTour)
+  it('Create a tour', function() {
+    cy.fixture('logistics/tour.json').then(tourJson => {
+      Object.assign(new Tour(), tourJson)
+        .setName(tourName)
         .apply();
     });
+  });
 
+  it('create tour and transportation order', function() {
     cy.fixture('logistics/transportation_order.json').then(tour => {
       Object.assign(new TransportationOrder(), tour)
-        .setTour(testTour)
+        .setShipperBPartnerID(shipperBPartner)
+        .setShipperLocationID(shipperLocation)
+        .setShipper(shipper)
+        .setTour(tourName)
+        .setDocumentNo(documentNo)
         .apply();
     });
   });
--- cypress/support/utils/transportationOrder.js
+++ cypress/support/utils/transportationOrder.js
@@ -1,11 +1,35 @@
 export class TransportationOrder {
-  constructor() {
-    this.tour = undefined;
+  constructor(tour) {
+    cy.log(`TransportationOrderBuilder - set tour = ${tour}`);
+    this.tour = tour;
   }
 
   setTour(tour) {
     cy.log(`TransportationOrder - set tour = ${tour}`);
     this.tour = tour;
+    return this;
+  }
+  setShipperBPartnerID(shipperBPartnerID) {
+    cy.log(`TransportationOrder - set shipper bpartner = ${shipperBPartnerID}`);
+    this.shipperBPartnerID = shipperBPartnerID;
+    return this;
+  }
+
+  setShipperLocationID(shipperLocationID) {
+    cy.log(`TransportationOrder - set shipper location = ${shipperLocationID}`);
+    this.shipperLocationID = shipperLocationID;
+    return this;
+  }
+
+  setShipper(shipper) {
+    cy.log(`TransportationOrder - set shipper = ${shipper}`);
+    this.shipper = shipper;
+    return this;
+  }
+
+  setDocumentNo(documentNo) {
+    cy.log(`TransportationOrder - set documentNo = ${documentNo}`);
+    this.documentNo = documentNo;
     return this;
   }
 
@@ -18,11 +42,12 @@ export class TransportationOrder {
 }
 
 function applyTransportationOrder(tOrder) {
-  cy.visitWindow('540020');
-
-  cy.clickHeaderNav(Cypress.messages.window.new.caption);
-  cy.selectInListField('Shipper_BPartner_ID', tOrder.shipperBPartnerID);
-  cy.selectInListField('Shipper_Location_ID', tOrder.shipperLocationID);
-  cy.selectInListField('M_Tour_ID', tOrder.tour);
-  cy.writeIntoStringField('DocumentNo', tOrder.documentNo);
+  describe(`Create new TransportationOrder ${tOrder.tour}`, function() {
+    cy.visitWindow(540020, 'NEW');
+    cy.selectInListField('Shipper_BPartner_ID', tOrder.shipperBPartnerID);
+    cy.selectInListField('Shipper_Location_ID', tOrder.shipperLocationID);
+    cy.selectInListField('M_Shipper_ID', tOrder.shipper);
+    cy.selectInListField('M_Tour_ID', tOrder.tour);
+    cy.writeIntoStringField('DocumentNo', tOrder.documentNo);
+  });
 }
