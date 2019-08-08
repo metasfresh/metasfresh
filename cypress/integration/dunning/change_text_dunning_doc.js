b4e57edceaf2b519b2c0699b99f9e0ea0c1d090b
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-08-08 16:34:07 +0300

    #131
    #131 Change text dunning document

---
+++ cypress/integration/dunning/change_text_dunning_doc.js
@@ -0,0 +1,135 @@
+/// <reference types="Cypress" />
+
+import { SalesInvoice, SalesInvoiceLine } from '../../support/utils/sales_invoice';
+import { getLanguageSpecific, humanReadableNow } from '../../support/utils/utils';
+import { DocumentActionKey, DocumentStatusKey } from '../../support/utils/constants';
+import { BPartner } from '../../support/utils/bpartner';
+import { DunningCandidates } from '../../page_objects/dunning_candidates';
+import { applyFilters, selectNotFrequentFilterWidget, toggleNotFrequentFilters } from '../../support/functions';
+import { DunningType } from '../../support/utils/dunning_type';
+import { DunningDocuments } from '../../page_objects/dunning_documents';
+import { salesInvoices } from '../../page_objects/sales_invoices';
+
+describe('Create Dunning Documents', function() {
+  const date = humanReadableNow();
+
+  const dunningTypeName = `Dunning ${date}`;
+
+  const businessPartnerName = `Customer Dunning ${date}`;
+  const paymentTerm = 'immediately';
+
+  const salesInvoiceTargetDocumentType = 'Sales Invoice';
+  const productName = 'Convenience Salat 250g';
+  const originalQuantity = 200;
+
+  let siDocumentNumber;
+  let dunningTypeRecordId;
+  let siRecordId;
+  let dunningDocId;
+
+  it('Create dunning type', function() {
+    cy.fixture('settings/dunning_type.json').then(dunningType => {
+      Object.assign(new DunningType(), dunningType)
+        .setName(dunningTypeName)
+        .apply();
+    });
+    cy.getCurrentWindowRecordId().then(recordId => {
+      dunningTypeRecordId = recordId;
+    });
+  });
+
+  it('Create bpartner', function() {
+    cy.fixture('sales/simple_customer.json').then(customerJson => {
+      const bpartner = new BPartner({ ...customerJson, name: businessPartnerName })
+        .setCustomer(true)
+        .setDunning(dunningTypeName)
+        .setPaymentTerm(paymentTerm)
+        .setBank(undefined);
+
+      bpartner.apply();
+    });
+  });
+
+  it('Create sales invoice', function() {
+    cy.fixture('sales/sales_invoice.json').then(salesInvoiceJson => {
+      new SalesInvoice(businessPartnerName, salesInvoiceTargetDocumentType)
+        .addLine(new SalesInvoiceLine().setProduct(productName).setQuantity(originalQuantity))
+        .setDocumentAction(getLanguageSpecific(salesInvoiceJson, DocumentActionKey.Complete))
+        .setDocumentStatus(getLanguageSpecific(salesInvoiceJson, DocumentStatusKey.Completed))
+        .apply();
+    });
+  });
+  it('Sales Invoice is not paid', function() {
+    cy.getCheckboxValue('IsPaid').then(checkBoxValue => {
+      cy.log(`IsPaid = ${checkBoxValue}`);
+      assert.equal(checkBoxValue, false);
+    });
+  });
+
+  it('Save document no from invoice', function() {
+    cy.getStringFieldValue('DocumentNo').then(documentNumber => {
+      siDocumentNumber = documentNumber;
+    });
+  });
+  it('Save current record id of the invoice', function() {
+    cy.getCurrentWindowRecordId().then(function(record) {
+      siRecordId = record;
+    });
+  });
+
+  it('Create Dunning Candidates', function() {
+    DunningCandidates.visit();
+    cy.executeHeaderActionWithDialog('C_Dunning_Candidate_Create');
+    cy.setCheckBoxValue('IsFullUpdate', true, true);
+    cy.pressStartButton();
+    // cy.wait(8000);
+  });
+
+  it('Ensure there are exactly 2 Dunning Candidates - one for each dunning type level', function() {
+    DunningCandidates.visit();
+    filterBySalesInvoiceDocNumber(siDocumentNumber);
+    DunningCandidates.getRows().should('have.length', 2);
+  });
+
+  function filterBySalesInvoiceDocNumber(siDocNumber) {
+    toggleNotFrequentFilters();
+    selectNotFrequentFilterWidget('default');
+    cy.writeIntoStringField('DocumentNo', siDocNumber, false, null, true);
+    applyFilters();
+  }
+
+  it('Create Dunning Documents', function() {
+    DunningCandidates.selectAllVisibleRows();
+    cy.executeHeaderActionWithDialog('C_Dunning_Candidate_Process');
+    cy.setCheckBoxValue('IsComplete', true, true);
+    cy.pressStartButton();
+  });
+  it('Open and check dunning documents from sales invoice - open pdf report to check initial text', function() {
+    cy.visitWindow(salesInvoices.windowId, siRecordId);
+    cy.openReferencedDocuments('C_Invoice-C_DunningDoc');
+    DunningDocuments.getRows().should('have.length', 2);
+    cy.selectNthRow(0).dblclick();
+    cy.getCurrentWindowRecordId().then(function(record) {
+      dunningDocId = record;
+    });
+    /**open header action 'Print' to check pdf report initial text 
+    cy.clickHeaderNav('print');
+    *The test is partialy implemented as for the moment(8/8/2019)
+    * we cannot verify text inside pdf reports using cypress)
+    */
+  });
+
+  it('Change text in dunning type - level 2', function() {
+    cy.visitWindow('159', dunningTypeRecordId);
+    cy.selectTab('C_DunningLevel');
+    cy.selectNthRow(1);
+    cy.openAdvancedEdit();
+    cy.writeIntoStringField('PrintName', 'Verrechnung Mahnkosten CHF 15');
+    cy.pressDoneButton();
+  });
+  it('Compare text in pdf report from dunning doc with the newly changed text in dunning type - level 2', function() {
+    cy.visitWindow('540155', dunningDocId);
+    /**open header action 'Print' to check pdf report initial text 
+    cy.clickHeaderNav('print');*/
+  });
+});
