9880fc97ef0b94efc8fd2ebc27ad10fd797b9ab2
Mark Krake <mark.krake@metasfresh.com>
2019-08-21 16:52:10 +0200

    Adds Translation for Discount Rows System Element #5070
    https://github.com/metasfresh/metasfresh/issues/5070

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5529150_sys_gh5070_discount-schema-lines-trl.sql
@@ -0,0 +1,20 @@
+-- 2019-08-21T14:46:23.553Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Discount Rows', PrintName='Discount Rows', WEBUI_NameBrowse='Discount Rows',Updated=TO_TIMESTAMP('2019-08-21 16:46:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576294 AND AD_Language='en_US'
+;
+
+-- 2019-08-21T14:46:23.586Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576294,'en_US') 
+;
+
+-- 2019-08-21T14:46:31.210Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET WEBUI_NameNew='New Discount Row',Updated=TO_TIMESTAMP('2019-08-21 16:46:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=576294 AND AD_Language='en_US'
+;
+
+-- 2019-08-21T14:46:31.212Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576294,'en_US') 
+;
+
