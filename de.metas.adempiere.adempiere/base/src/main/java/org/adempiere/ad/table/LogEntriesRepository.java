106c098e311ccfcc61a0020cef4f806a4229eab5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-08-22 14:39:42 +0200

    5447-app - Optionally include C_Location changes..
    implement most of it, it think; more QA pending
    * introduce LogEntriesQuery to specifiy whether the feature shall be used or not
      * use the feature when change log is loaded by BPartnerCompositeRepository
    * Fix typo in classname - rename RecordChangeLogEntryEntryLoader to RecordChangeLogEntryLoader
    * implement the new feature, mostly in RecordChangeLogEntryLoader
    * BPartnerCompositeRepository - don't update C_Location anymore; create new one; it's immutable
    https://github.com/metasfresh/metasfresh/issues/5447
    
    (cherry picked from commit 6ba305a40e5d70d9db4feb68d86f06fd894150a4)
    
    sovled Conflicts:
    	de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/table/RecordChangeLogEntryLoader.java

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/table/LogEntriesRepository.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/table/LogEntriesRepository.java
@@ -3,8 +3,15 @@ package org.adempiere.ad.table;
 import java.util.List;
 
 import org.adempiere.util.lang.impl.TableRecordReference;
+import org.compiere.util.KeyNamePair;
 
 import com.google.common.collect.ImmutableListMultimap;
+
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.NonNull;
+import lombok.Singular;
+import lombok.Value;
 
 /*
  * #%L
@@ -30,5 +37,43 @@ import com.google.common.collect.ImmutableListMultimap;
 
 public interface LogEntriesRepository
 {
-	ImmutableListMultimap<TableRecordReference, RecordChangeLogEntry> getLogEntriesForRecordReferences(List<TableRecordReference> tableRecordReference);
+	ImmutableListMultimap<TableRecordReference, RecordChangeLogEntry> getLogEntriesForRecordReferences(LogEntriesQuery logEntriesQuery);
+
+	@Value
+	@Builder
+	public static class LogEntriesQuery
+	{
+		public static LogEntriesQuery of(@NonNull final TableRecordReference tableRecordReference)
+		{
+			return LogEntriesQuery.builder()
+					.tableRecordReference(tableRecordReference)
+					.followLocationIdChanges(false)
+					.build();
+		}
+
+		@Singular
+		List<TableRecordReference> tableRecordReferences;
+
+		/**
+		 * If {@code true} and the respective record's change logs contain changes to {@code C_Location} records,
+		 * then those records are loaded (all of them in one DB-access).
+		 * Then, instead of adding {@link RecordChangeLogEntry}s for the different {@code C_Location_ID} values,
+		 * the {@code C_Location} records are compared with each other and {@link RecordChangeLogEntry} are derived from their differences.
+		 * Then those @{@code C_Location}-derived {@link RecordChangeLogEntry} added to the respective record's change log.
+		 *
+		 * If you don't want this to happen e.g. for {@code C_BPartner_Location}, you can set {@code IsAllowLogging='N'} for {@code C_BPartner_Location.C_Location_ID}.
+		 *
+		 * Also note that {@link RecordChangeLogEntry} are <b>not</b> derived for the {@code C_Location}-columns:
+		 * <li>{@code Created}, but note that the {@code C_Location} record's {@code Created} value is used in the {@link RecordChangeLogEntry}
+		 * <li>{@code CreatedBy} same are {@code Created}
+ 		 * <li>{@code Updated} and {@code UpdatedBy}: since {@code C_Location} is immutable, they don't matter anyways
+		 * <li>{@code C_Location_ID}
+  		 * <li>Every {@code C_Location}-column with {@code IsAllowLogging='N'}
+  		 *
+  		 * Note: if you have e.g. {@code IsAllowLogging='N'} for {@code C_BPartner_Location.C_Location_ID}, and {@link followLocationIdChanges=false}, then the changelog contains the different locations, i.e.
+  		 * {@link KeyNamePair}s with the {@code C_Location_ID} and the locations rendered address string..
+		 */
+		@Default
+		boolean followLocationIdChanges = false;
+	}
 }
