37c48369afbc588096ea3f8eaff105aaa20375c9
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-08-23 15:59:52 +0300

    #2591 tests added
    #2591 Cypress: Update tests to use new fixture approach

--- cypress/integration/finance/create_GL_journal_line_entry_spec.js
+++ cypress/integration/finance/create_GL_journal_line_entry_spec.js
@@ -1,4 +1,24 @@
-import { getLanguageSpecific } from '../../support/utils/utils';
+let description;
+let category;
+let postingType;
+let currency;
+let docType;
+let account;
+let amountSource;
+let activity;
+
+it('Read fixture and prepare the names', function() {
+  cy.fixture('finance/create_GL_journal_line_entry_spec.json').then(f => {
+    description = f['description'];
+    category = f['category'];
+    postingType = f['postingType'];
+    currency = f['currency'];
+    docType = f['docType'];
+    account = f['account'];
+    amountSource = f['amountSource'];
+    activity = f['activity'];
+  });
+});
 
 describe('Creating GL journal entry and change posting type', function() {
   before(function() {
@@ -8,30 +28,27 @@ describe('Creating GL journal entry and change posting type', function() {
   it('create journal', function() {
     cy.clickHeaderNav(Cypress.messages.window.new.caption);
 
-    cy.writeIntoStringField('Description', 'Spende');
-    cy.selectInListField('GL_Category_ID', 'Standard');
-    cy.selectInListField('PostingType', 'Actual');
-    cy.selectInListField('C_Currency_ID', 'CHF');
-    cy.selectInListField('C_DocType_ID', 'Journal');
+    cy.writeIntoStringField('Description', description);
+    cy.selectInListField('GL_Category_ID', category);
+    cy.selectInListField('PostingType', postingType);
+    cy.selectInListField('C_Currency_ID', currency);
+    cy.selectInListField('C_DocType_ID', docType);
     cy.selectDateViaPicker('DateDoc');
   });
 
   it('add new entry line', function() {
     cy.pressAddNewButton();
-    cy.writeIntoStringField('Description', 'Spende', true /*modal*/);
-    cy.writeIntoLookupListField('Account_DR_ID', '10', '1020_UBS Kontokorrent', false /*typeList*/, true /*modal*/);
-    cy.writeIntoStringField('AmtSourceDr', '100.00', true /*modal*/);
-    cy.writeIntoLookupListField('Account_CR_ID', '10', '1020_UBS Kontokorrent', false /*typeList*/, true /*modal*/);
-    cy.selectInListField('C_Activity_ID', '250_Verwaltung', true /*modal*/);
+    cy.writeIntoStringField('Description', description, true /*modal*/);
+    cy.writeIntoLookupListField('Account_DR_ID', account, account, false /*typeList*/, true /*modal*/);
+    cy.writeIntoStringField('AmtSourceDr', amountSource, true /*modal*/);
+    cy.writeIntoLookupListField('Account_CR_ID', account, account, false /*typeList*/, true /*modal*/);
+    cy.selectInListField('C_Activity_ID', activity, true /*modal*/);
     cy.pressDoneButton();
   });
 
   it('complete', function() {
     cy.fixture('misc/misc_dictionary.json').then(miscDictionary => {
-      cy.processDocument(
-        getLanguageSpecific(miscDictionary, 'docActionComplete'),
-        getLanguageSpecific(miscDictionary, 'docStatusCompleted')
-      );
+      cy.completeDocument();
     });
   });
 });
--- cypress/integration/finance/create_GL_journal_type_spec.js
+++ cypress/integration/finance/create_GL_journal_type_spec.js
@@ -1,3 +1,25 @@
+let description="test";
+let category="Standard";
+let postingType1="Statistical";
+let currency="CHF";
+let docType="Journal";
+let postingType2="Budget";
+let postingType3="Ist (Jahr Ende)";
+let postingType4="Statistical";
+
+it('Read fixture and prepare the names', function() {
+  cy.fixture('finance/create_GL_journal_type_spec.json').then(f => {
+    description = f['description'];
+    category = f['category'];
+    postingType1 = f['postingType1'];
+    currency = f['currency'];
+    docType = f['docType'];
+    postingType2 = f['postingType2'];
+    postingType3 = f['postingType3'];
+    postingType4 = f['postingType4'];
+  });
+});
+
 describe('Creating GL journal entry and change posting type', function() {
   before(function() {
     cy.visit('/window/540356');
@@ -6,14 +28,14 @@ describe('Creating GL journal entry and change posting type', function() {
   it('create tour', function() {
     cy.clickHeaderNav(Cypress.messages.window.new.caption);
 
-    cy.writeIntoStringField('Description', 'test');
-    cy.selectInListField('GL_Category_ID', 'Standard');
-    cy.selectInListField('PostingType', 'Statistical');
-    cy.selectInListField('C_Currency_ID', 'CHF');
-    cy.selectInListField('C_DocType_ID', 'Journal');
+    cy.writeIntoStringField('Description', description);
+    cy.selectInListField('GL_Category_ID', category);
+    cy.selectInListField('PostingType', postingType1);
+    cy.selectInListField('C_Currency_ID', currency);
+    cy.selectInListField('C_DocType_ID', docType);
     cy.selectDateViaPicker('DateDoc');
-    cy.selectInListField('PostingType', 'Budget');
-    cy.selectInListField('PostingType', 'Ist (Jahr Ende)');
-    cy.selectInListField('PostingType', 'Statistical');
+    cy.selectInListField('PostingType', postingType2);
+    cy.selectInListField('PostingType', postingType3);
+    cy.selectInListField('PostingType', postingType4);
   });
 });
