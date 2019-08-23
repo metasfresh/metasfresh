49cc1640735850331b1599ea938bfe4f7e00259a
Teo Sarca <teo.sarca@metasfresh.com>
2019-08-23 23:09:58 +0300

    HOTFIX: don't throw exception when loading readonly attribute instances
    https://github.com/metasfresh/metasfresh/issues/5440

--- src/main/java/de/metas/ui/web/pattribute/ASIRepository.java
+++ src/main/java/de/metas/ui/web/pattribute/ASIRepository.java
@@ -1,5 +1,7 @@
 package de.metas.ui.web.pattribute;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -12,16 +14,19 @@ import org.compiere.model.I_M_AttributeInstance;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Repository;
 
+import com.google.common.collect.ImmutableMap;
+
 import de.metas.cache.CCache;
 import de.metas.logging.LogManager;
 import de.metas.ui.web.exceptions.EntityNotFoundException;
 import de.metas.ui.web.pattribute.ASIDescriptorFactory.ASIAttributeFieldBinding;
 import de.metas.ui.web.window.datatypes.DocumentId;
+import de.metas.ui.web.window.descriptor.DocumentEntityDescriptor;
+import de.metas.ui.web.window.descriptor.DocumentFieldDescriptor;
 import de.metas.ui.web.window.model.Document;
 import de.metas.ui.web.window.model.Document.CopyMode;
 import de.metas.ui.web.window.model.DocumentCollection;
 import de.metas.ui.web.window.model.IDocumentChangesCollector;
-import de.metas.ui.web.window.model.IDocumentFieldView;
 import de.metas.ui.web.window.model.NullDocumentChangesCollector;
 import de.metas.util.Services;
 import lombok.NonNull;
@@ -60,8 +65,6 @@ public class ASIRepository
 	private final Supplier<DocumentId> nextASIDocId = DocumentId.supplier("N", 1);
 	private final CCache<DocumentId, ASIDocument> id2asiDoc = CCache.newLRUCache("ASIDocuments", 500, 0);
 
-	private static final String VERSION_DEFAULT = "0";
-
 	public ASIRepository(
 			@NonNull final ASIDescriptorFactory descriptorsFactory)
 	{
@@ -76,20 +79,9 @@ public class ASIRepository
 
 		//
 		// Create the new ASI document
-		final Document asiDocData = Document.builder(asiDescriptor.getEntityDescriptor())
-				.initializeAsNewDocument(nextASIDocId, VERSION_DEFAULT)
-				.build();
-
-		//
-		// If we have a template ASI, populate the ASI document from it
 		final AttributeSetInstanceId templateAsiId = info.getAttributeSetInstanceId();
-		if (templateAsiId.isRegular())
-		{
-			for (final I_M_AttributeInstance fromAI : attributesRepo.retrieveAttributeInstances(templateAsiId))
-			{
-				loadASIDocumentField(asiDocData, fromAI);
-			}
-		}
+		final Document asiDocData = Document.builder(asiDescriptor.getEntityDescriptor())
+				.initializeAsNewDocument(createASIDocumentValuesSupplier(templateAsiId, asiDescriptor.getEntityDescriptor()));
 
 		//
 		// Validate, log and add the new ASI document to our index
@@ -117,33 +109,67 @@ public class ASIRepository
 			throw new EntityNotFoundException("ASI " + attributeSetInstanceId);
 		}
 
-		final WebuiASIEditingInfo info = WebuiASIEditingInfo.readonlyASI(attributeSetInstanceId);
-
 		//
 		// Get the ASI descriptor
+		final WebuiASIEditingInfo info = WebuiASIEditingInfo.readonlyASI(attributeSetInstanceId);
 		final ASIDescriptor asiDescriptor = descriptorsFactory.getASIDescriptor(info);
 
 		//
 		// Create the new ASI document
 		final Document asiDocData = Document.builder(asiDescriptor.getEntityDescriptor())
-				.initializeAsNewDocument(() -> DocumentId.of(attributeSetInstanceId), VERSION_DEFAULT)
-				.build();
-
-		//
-		// If we have a template ASI, populate the ASI document from it
-		final AttributeSetInstanceId templateAsiId = info.getAttributeSetInstanceId();
-		for (final I_M_AttributeInstance fromAI : attributesRepo.retrieveAttributeInstances(templateAsiId))
-		{
-			loadASIDocumentField(asiDocData, fromAI);
-		}
+				.initializeAsNewDocument(createASIDocumentValuesSupplier(attributeSetInstanceId, asiDescriptor.getEntityDescriptor()));
 
 		//
 		// Validate, log and add the new ASI document to our index
 		asiDocData.checkAndGetValidStatus();
-		logger.trace("Created from ASI={}: {}", templateAsiId, asiDocData);
+		logger.trace("Created from ASI={}: {}", attributeSetInstanceId, asiDocData);
 
 		final ASIDocument asiDoc = new ASIDocument(asiDescriptor, asiDocData);
 		return asiDoc.copy(CopyMode.CheckInReadonly, NullDocumentChangesCollector.instance);
+	}
+
+	private ASIDocumentValuesSupplier createASIDocumentValuesSupplier(
+			@NonNull final AttributeSetInstanceId asiId,
+			@NonNull final DocumentEntityDescriptor descriptor)
+	{
+		return ASIDocumentValuesSupplier.builder()
+				.documentIdSupplier(nextASIDocId)
+				.values(retrieveASIValuesMap(asiId, descriptor))
+				.build();
+	}
+
+	private Map<String, Object> retrieveASIValuesMap(
+			@NonNull final AttributeSetInstanceId asiId,
+			@NonNull final DocumentEntityDescriptor descriptor)
+	{
+		if (asiId.isNone())
+		{
+			return ImmutableMap.of();
+		}
+
+		final HashMap<String, Object> valuesMap = new HashMap<>();
+		for (final I_M_AttributeInstance fromAI : attributesRepo.retrieveAttributeInstances(asiId))
+		{
+			final I_M_Attribute attribute = attributesRepo.getAttributeById(fromAI.getM_Attribute_ID());
+			final String fieldName = attribute.getValue();
+			final DocumentFieldDescriptor fieldDescriptor = descriptor.getFieldOrNull(fieldName);
+
+			// Skip loading the attribute instance if it's no longer exist.
+			// This can happen if we are trying to load an old ASI but in meantime the AttributeSet was changed and the attribute was removed or deactivated.
+			if (fieldDescriptor == null)
+			{
+				logger.warn("Attribute {} no longer exist in {}", fieldName, descriptor);
+				continue;
+			}
+
+			final Object value = fieldDescriptor
+					.getDataBindingNotNull(ASIAttributeFieldBinding.class)
+					.readValue(fromAI);
+
+			valuesMap.put(fieldName, value);
+		}
+
+		return valuesMap;
 	}
 
 	private ASIDocument getASIDocumentNoLock(final DocumentId asiDocId)
@@ -204,27 +230,5 @@ public class ASIRepository
 
 			return result;
 		}
-	}
-
-	private void loadASIDocumentField(final Document asiDoc, final I_M_AttributeInstance fromAI)
-	{
-		final I_M_Attribute attribute = attributesRepo.getAttributeById(fromAI.getM_Attribute_ID());
-		final String fieldName = attribute.getValue();
-		final IDocumentFieldView field = asiDoc.getFieldViewOrNull(fieldName);
-
-		// Skip loading the attribute instance if it's no longer exist.
-		// This can happen if we are trying to load an old ASI but in meantime the AttributeSet was changed and the attribute was removed or deactivated.
-		if (field == null)
-		{
-			logger.warn("Attribute {} no longer exist in {}", fieldName, asiDoc.getEntityDescriptor());
-			return;
-		}
-
-		final Object value = field
-				.getDescriptor()
-				.getDataBindingNotNull(ASIAttributeFieldBinding.class)
-				.readValue(fromAI);
-
-		asiDoc.processValueChange(fieldName, value, () -> "update from " + fromAI);
 	}
 }
