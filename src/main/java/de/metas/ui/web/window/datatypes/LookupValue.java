083015ab4f58d780ec4efb3148463f9fd92e8bd5
Teo Sarca <teo.sarca@metasfresh.com>
2019-08-23 22:14:08 +0300

    minor
    https://github.com/metasfresh/metasfresh/issues/5440

--- src/main/java/de/metas/ui/web/window/datatypes/LookupValue.java
+++ src/main/java/de/metas/ui/web/window/datatypes/LookupValue.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 
 import javax.annotation.Nullable;
 
@@ -304,7 +305,7 @@ public abstract class LookupValue
 		return id.toString();
 	}
 
-	public <T extends RepoIdAware> T getIdAs(@NonNull final Function<Integer, T> idMapper)
+	public <T extends RepoIdAware> T getIdAs(@NonNull final IntFunction<T> idMapper)
 	{
 		return idMapper.apply(getIdAsInt());
 	}
