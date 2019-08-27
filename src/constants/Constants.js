82730bd2294ede185ebde1b5de7be5adda250c65
siemiatj <kuba@saskla.cz>
2019-08-27 11:57:25 +0200

    #2317 small cleanup and fix DatetimeRange date format sent to the backend
    (cherry picked from commit 1f4ae349e084fea08bf5b9b93b6441f277b99baa)

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,9 +1,7 @@
-export const DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSZ';
 export const LOCAL_LANG = 'metasfreshLanguage';
 export const VIEW_EDITOR_RENDER_MODES_NEVER = 'never';
 export const VIEW_EDITOR_RENDER_MODES_ON_DEMAND = 'on-demand';
 export const VIEW_EDITOR_RENDER_MODES_ALWAYS = 'always';
-export const DATE_FIELDS = ['Date', 'DateTime', 'Time'];
 export const INITIALLY_CLOSED = 'INITIALLY_CLOSED';
 export const INITIALLY_OPEN = 'INITIALLY_OPEN';
 
@@ -17,6 +15,10 @@ export const AMOUNT_FIELD_FORMATS_BY_PRECISION = [
   '0,0.00000',
 ];
 export const SPECIAL_FIELD_TYPES = ['Color'];
+
+export const DATE_FORMAT = 'YYYY-MM-DD';
+export const DATE_TIMEZONE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSZ';
+export const DATE_FIELDS = ['Date', 'DateTime'];
 export const DATE_FIELD_TYPES = [
   'Date',
   'DateTime',
