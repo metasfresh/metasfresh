12a905a70b2a111c4fe361b6da3aa14e3f6c92db
TheBestPessimist <cristian@tbp.land>
2019-08-30 13:41:09 +0300

    Update adapt_tour.js to use fixture
    https://github.com/metasfresh/metasfresh-e2e/issues/350

--- cypress/integration/logistics/adapt_tour.js
+++ cypress/integration/logistics/adapt_tour.js
@@ -1,13 +1,20 @@
 import { BPartner } from '../../support/utils/bpartner';
-import { humanReadableNow } from '../../support/utils/utils';
+import { appendHumanReadableNow } from '../../support/utils/utils';
 import { Tour } from '../../support/utils/tour';
 import { TourVersion, TourVersionLine } from '../../support/utils/tour_version';
 
 describe('Adapt tour version', function() {
-  const date = humanReadableNow();
-  const customerName = `Customer ${date}`;
-  const tourName = `TestTour ${date}`;
-  const tourVersionName = `TestTourVersion ${date}`;
+  let customerName;
+  let tourName;
+  let tourVersionName;
+
+  it('Read the fixture', function() {
+    cy.fixture('logistics/adapt_tour.json').then(f => {
+      customerName = appendHumanReadableNow(f['customerName']);
+      tourName = appendHumanReadableNow(f['tourName']);
+      tourVersionName = appendHumanReadableNow(f['tourVersionName']);
+    });
+  });
 
   it('Create customer for which we will create a tour', function() {
     cy.fixture('sales/simple_customer.json').then(customerJson => {
@@ -33,7 +40,7 @@ describe('Adapt tour version', function() {
     /**Add a new version line */
     cy.selectTab('M_TourVersionLine');
     cy.pressAddNewButton();
-    cy.writeIntoLookupListField('C_BPartner_ID', customerName, customerName, false, true, null, true);
+    cy.writeIntoLookupListField('C_BPartner_ID', customerName, customerName, false, true);
     cy.pressDoneButton();
   });
 });
