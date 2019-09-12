e243248f272d4211568900ba8d1ce811c024c83c
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-09-12 17:10:11 +0300

    #2719 refinements and fixes for purchase order from sales order test
    #2719 Fix failing tests on jenkins

--- cypress/integration/purchase/purchase_order_from_sales_order.js
+++ cypress/integration/purchase/purchase_order_from_sales_order.js
@@ -1,46 +1,65 @@
 import { BPartner } from '../../support/utils/bpartner';
-import { BPartnerLocation } from '../../support/utils/bpartner_ui';
 import { ProductCategory, ProductPrice, Product } from '../../support/utils/product';
-import { PackingMaterial } from '../../support/utils/packing_material';
-import { PackingInstructions } from '../../support/utils/packing_instructions';
-import { PackingInstructionsVersion } from '../../support/utils/packing_instructions_version';
 import { Builder } from '../../support/utils/builder';
-import { getLanguageSpecific, humanReadableNow } from '../../support/utils/utils';
-import { DocumentActionKey, DocumentStatusKey } from '../../support/utils/constants';
+import { appendHumanReadableNow } from '../../support/utils/utils';
+import { DocumentStatusKey } from '../../support/utils/constants';
 import { SalesOrder, SalesOrderLine } from '../../support/utils/sales_order';
 
-describe('Create Purchase order from sales order', function() {
-  const date = humanReadableNow();
-  const productForPackingMaterial = `ProductPackingMaterial ${date}`;
-  const packingMaterialName = `ProductPackingMaterial ${date}`;
-  const packingInstructionsName = `ProductPackingInstrutions ${date}`;
-  const productName1 = `ProductTest ${date}`;
-  const productCategoryName = `ProductCategoryName ${date}`;
-  const discountSchemaName = `DiscountSchemaTest ${date}`;
-  const purchasePriceSystem = `PurchasePriceSystem ${date}`;
-  const purchasePriceList = `PurchasePriceList ${date}`;
-  const purchasePriceListVersion = `PurchasePriceListVersion ${date}`;
-  const salesPriceSystem = `SalesPriceSystem ${date}`;
-  const salesPriceList = `SalesPriceList ${date}`;
-  const salesPriceListVersion = `SalesPriceListVersion ${date}`;
-  const productType = 'Item';
-  const vendorName = `Vendor ${date}`;
-  const customerName = `Customer ${date}`;
+let productForPackingMaterial;
+let packingInstructionsName;
+let productName;
+let productCategoryName;
+let discountSchemaName;
+let purchasePriceSystem;
+let purchasePriceList;
+let purchasePriceListVersion;
+let salesPriceSystem;
+let salesPriceList;
+let salesPriceListVersion;
+let productType;
+let vendorName;
+let customerName;
+let soQty;
 
-  it('Create price and product entities to be used in purchase order', function() {
+let productPricePM1;
+let productPricePM2;
+let productPrice1;
+let productPrice2;
+describe('Create Purchase order from sales order', function() {
+  it('Read the fixture', function() {
+    cy.fixture('purchase/purchase_order_from_sales_order.json').then(f => {
+      productForPackingMaterial = appendHumanReadableNow(f['productForPackingMaterial']);
+      packingInstructionsName = appendHumanReadableNow(f['packingInstructionsName']);
+      productName = appendHumanReadableNow(f['productName']);
+      productCategoryName = appendHumanReadableNow(f['productCategoryName']);
+      discountSchemaName = appendHumanReadableNow(f['discountSchemaName']);
+      purchasePriceSystem = appendHumanReadableNow(f['purchasePriceSystem']);
+      purchasePriceList = appendHumanReadableNow(f['purchasePriceList']);
+      purchasePriceListVersion = appendHumanReadableNow(f['purchasePriceListVersion']);
+      productType = f['productType'];
+      vendorName = appendHumanReadableNow(f['vendorName']);
+      customerName = appendHumanReadableNow(f['customerName']);
+      salesPriceSystem = appendHumanReadableNow(f['salesPriceSystem']);
+      salesPriceList = appendHumanReadableNow(f['salesPriceList']);
+      salesPriceListVersion = appendHumanReadableNow(f['salesPriceListVersion']);
+      soQty = f['soQty'];
+    });
+  });
+
+  it('Create price entities', function() {
     /**purchase price list */
     Builder.createBasicPriceEntities(purchasePriceSystem, purchasePriceListVersion, purchasePriceList, false);
     /**sales price list */
     Builder.createBasicPriceEntities(salesPriceSystem, salesPriceListVersion, salesPriceList, true);
     /**Create product for packing material which has both a purchase price list and a sales price list */
-    let productPricePM1;
-    let productPricePM2;
     cy.fixture('product/product_price.json').then(productPriceJson => {
       productPricePM1 = Object.assign(new ProductPrice(), productPriceJson).setPriceList(purchasePriceList);
     });
     cy.fixture('product/product_price.json').then(productPriceJson => {
       productPricePM2 = Object.assign(new ProductPrice(), productPriceJson).setPriceList(salesPriceList);
     });
+  });
+  it('Create product entities to be used in purchase order', function() {
     cy.fixture('product/simple_product.json').then(productJson => {
       Object.assign(new Product(), productJson)
         .setName(productForPackingMaterial)
@@ -52,58 +71,43 @@ describe('Create Purchase order from sales order', function() {
     });
   });
   it('Create packing related entities to be used in purchase order', function() {
-    cy.fixture('product/packing_material.json').then(packingMaterialJson => {
-      Object.assign(new PackingMaterial(), packingMaterialJson)
-        .setName(packingMaterialName)
-        .setProduct(productForPackingMaterial)
-        .apply();
-    });
-    cy.fixture('product/packing_instructions.json').then(packingInstructionsJson => {
-      Object.assign(new PackingInstructions(), packingInstructionsJson)
-        .setName(packingInstructionsName)
-        .apply();
-    });
-    cy.fixture('product/packing_instructions_version.json').then(pivJson => {
-      Object.assign(new PackingInstructionsVersion(), pivJson)
-        .setName(packingInstructionsName)
-        .setPackingInstructions(packingInstructionsName)
-        .setPackingMaterial(packingMaterialName)
-        .apply();
-    });
+    Builder.createPackingMaterial(productForPackingMaterial, packingInstructionsName);
   });
-  it('Create product category, product, vendor and customer entities to be used in purchase order', function() {
+  it('Create product category', function() {
     cy.fixture('product/simple_productCategory.json').then(productCategoryJson => {
       Object.assign(new ProductCategory(), productCategoryJson)
         .setName(productCategoryName)
         .apply();
     });
-    /**Create vendor to use in product - Business partner tab - current vendor */
+  });
+  it('Create vendor to use in product - Business partner tab - current vendor', function() {
     cy.fixture('sales/simple_vendor.json').then(vendorJson => {
       new BPartner({ ...vendorJson, name: vendorName })
         .setVendorPricingSystem(purchasePriceSystem)
         .setVendorDiscountSchema(discountSchemaName)
         .apply();
     });
-    /**Create customer for sales order */
+  });
+  it('Create customer for sales order', function() {
     cy.fixture('sales/simple_customer.json').then(customerJson => {
       new BPartner({ ...customerJson, name: customerName })
         .setCustomerPricingSystem(salesPriceSystem)
         .setCustomerDiscountSchema(discountSchemaName)
         .apply();
     });
-
-    let productPrice1;
-    let productPrice2;
+  });
+  it('Create product prices', function() {
     cy.fixture('product/product_price.json').then(productPriceJson => {
       productPrice1 = Object.assign(new ProductPrice(), productPriceJson).setPriceList(purchasePriceList);
     });
     cy.fixture('product/product_price.json').then(productPriceJson => {
       productPrice2 = Object.assign(new ProductPrice(), productPriceJson).setPriceList(salesPriceList);
     });
-
+  });
+  it('Create product', function() {
     cy.fixture('product/simple_product.json').then(productJson => {
       Object.assign(new Product(), productJson)
-        .setName(productName1)
+        .setName(productName)
         .setProductType(productType)
         .setProductCategory(productCategoryName + '_' + productCategoryName)
         .addProductPrice(productPrice1)
@@ -117,40 +121,33 @@ describe('Create Purchase order from sales order', function() {
     cy.fixture('misc/misc_dictionary.json').then(miscDictionary => {
       new SalesOrder()
         .setBPartner(customerName)
-        .setPriceSystem(salesPriceSystem)
-        .addLine(new SalesOrderLine().setProduct(productName1).setQuantity(1))
-        .setDocumentAction(getLanguageSpecific(miscDictionary, DocumentActionKey.Complete))
-        .setDocumentStatus(getLanguageSpecific(miscDictionary, DocumentStatusKey.Completed))
+        .addLine(new SalesOrderLine().setProduct(productName).setQuantity(1))
         .apply();
     });
-    /**Create purchase order from sales order */
+    cy.selectInListField('M_PricingSystem_ID', salesPriceSystem, false, null, true);
+    cy.completeDocument();
+  });
+
+  it('Create purchase order from sales order', function() {
     cy.executeHeaderActionWithDialog('C_Order_CreatePOFromSOs');
     cy.pressStartButton();
     cy.waitUntilProcessIsFinished();
-    /**Go to purchase order */
+  });
+  it('Go to purchase order and check product name and price, vendor, quantity and status', function() {
     cy.openReferencedDocuments('AD_RelationType_ID-540164');
+    cy.expectNumberOfRows(1);
     cy.selectNthRow(0).dblclick();
-    /**check product name */
-    cy.selectNthRow(0)
-      .find('.Lookup')
-      .find('.lookup-cell')
-      .contains(productName1);
-    /**check price of product */
-    cy.selectNthRow(0)
-      .find('.CostPrice')
-      .find('.costprice-cell')
-      .eq(0)
-      .contains('1.23');
-    /**check if vendor in purchase order is the current vendor set in product  */
-    cy.selectNthRow(0)
-      .find('.list-cell')
-      .contains(vendorName);
-    cy.selectNthRow(0)
-      .find('.quantity-cell')
-      .contains('1');
-    /**purchase order should be drafted */
-    cy.fixture('misc/misc_dictionary.json').then(miscDictionary => {
-      cy.get('.tag.tag-primary').contains(getLanguageSpecific(miscDictionary, DocumentStatusKey.Drafted));
-    });
+    cy.selectTab('C_OrderLine');
+    cy.expectNumberOfRows(1);
+    cy.selectNthRow(0);
+    cy.openAdvancedEdit();
+
+    cy.getStringFieldValue('M_Product_ID', true).should('contain', productName);
+    cy.getStringFieldValue('PriceEntered', true).should('contain', '1.23');
+    cy.getStringFieldValue('QtyEnteredTU', true).should('equals', soQty.toString());
+    cy.getStringFieldValue('C_BPartner_ID', true).should('contains', vendorName);
+    cy.pressDoneButton();
+
+    cy.expectDocumentStatus(DocumentStatusKey.Drafted);
   });
 });
