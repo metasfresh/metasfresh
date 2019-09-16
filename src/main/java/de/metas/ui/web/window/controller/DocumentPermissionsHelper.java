f08d1453476fb8d829cf88e90e72b9d0e59ae543
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-09-16 10:48:42 +0200

    Organisation as Search Widget
    minor comment addition
    https://github.com/metasfresh/metasfresh-webui-api/pull/1257
    
    (cherry picked from commit 4691cbb74aa5e1c4d192987caf586651968b6b56)

--- src/main/java/de/metas/ui/web/window/controller/DocumentPermissionsHelper.java
+++ src/main/java/de/metas/ui/web/window/controller/DocumentPermissionsHelper.java
@@ -75,7 +75,7 @@ public class DocumentPermissionsHelper
 
 	/**
 	 * Asserts view access
-	 * 
+	 *
 	 * @param windowId
 	 * @param viewId optional viewId, used only for error reporting
 	 * @param permissions
@@ -146,10 +146,10 @@ public class DocumentPermissionsHelper
 		}
 
 		final int recordId = getRecordId(document);
-		OrgId orgId = document.getOrgId();
+		final OrgId orgId = document.getOrgId();
 		if (orgId == null)
 		{
-			return;
+			return; // the user cleared the field; field is flagged as mandatory; until the user set the field, don't make a fuss.
 		}
 		final String errmsg = permissions.checkCanView(document.getClientId(), orgId, adTableId, recordId);
 		if (errmsg != null)
@@ -207,11 +207,11 @@ public class DocumentPermissionsHelper
 		}
 		final int recordId = getRecordId(document);
 
-		ClientId adClientId = document.getClientId();
-		OrgId adOrgId = document.getOrgId();
+		final ClientId adClientId = document.getClientId();
+		final OrgId adOrgId = document.getOrgId();
 		if (adOrgId == null)
 		{
-			return null;
+			return null; // the user cleared the field; field is flagged as mandatory; until user set the field, don't make a fuss.
 		}
 		return permissions.checkCanUpdate(adClientId, adOrgId, adTableId, recordId);
 	}
