8bcb131978636625dc04d696501ab3382a4744ca
Teo Sarca <teo.sarca@metasfresh.com>
2019-09-18 13:23:08 +0300

    order line quick input: ShipmentAllocation_BestBefore_Policy (prototyping)
    https://github.com/metasfresh/metasfresh/issues/5544

--- src/main/java/de/metas/ui/web/quickinput/orderline/OrderLineQuickInputDescriptorFactory.java
+++ src/main/java/de/metas/ui/web/quickinput/orderline/OrderLineQuickInputDescriptorFactory.java
@@ -7,12 +7,12 @@ import org.adempiere.ad.callout.api.ICalloutField;
 import org.adempiere.ad.expression.api.ConstantLogicExpression;
 import org.compiere.model.I_C_OrderLine;
 import org.compiere.util.DisplayType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.ImmutableSet;
 
 import de.metas.adempiere.model.I_C_Order;
+import de.metas.bpartner.ShipmentAllocationBestBeforePolicy;
 import de.metas.handlingunits.order.api.IHUOrderBL;
 import de.metas.i18n.IMsgBL;
 import de.metas.i18n.ITranslatableString;
@@ -28,6 +28,7 @@ import de.metas.ui.web.window.datatypes.DocumentId;
 import de.metas.ui.web.window.datatypes.DocumentType;
 import de.metas.ui.web.window.datatypes.LookupValue;
 import de.metas.ui.web.window.datatypes.LookupValue.IntegerLookupValue;
+import de.metas.ui.web.window.datatypes.LookupValue.StringLookupValue;
 import de.metas.ui.web.window.descriptor.DetailId;
 import de.metas.ui.web.window.descriptor.DocumentEntityDescriptor;
 import de.metas.ui.web.window.descriptor.DocumentFieldDescriptor;
@@ -65,8 +66,15 @@ import lombok.NonNull;
 @Component
 /* package */ final class OrderLineQuickInputDescriptorFactory implements IQuickInputDescriptorFactory
 {
-	@Autowired
-	private AvailableToPromiseAdapter availableToPromiseAdapter;
+	private final IMsgBL msgBL = Services.get(IMsgBL.class);
+	private final IHUOrderBL huOrderBL = Services.get(IHUOrderBL.class);
+	private final AvailableToPromiseAdapter availableToPromiseAdapter;
+
+	public OrderLineQuickInputDescriptorFactory(
+			@NonNull final AvailableToPromiseAdapter availableToPromiseAdapter)
+	{
+		this.availableToPromiseAdapter = availableToPromiseAdapter;
+	}
 
 	@Override
 	public Set<MatchingKey> getMatchingKeys()
@@ -101,31 +109,23 @@ import lombok.NonNull;
 			final DetailId detailId,
 			@NonNull final Optional<SOTrx> soTrx)
 	{
-		return createDescriptorBuilder(documentTypeId, detailId, soTrx)
-				.addField(createProductFieldBuilder(soTrx))
-				.addFieldIf(QuickInputConstants.isEnablePackingInstructionsField(), () -> createPackingInstructionFieldBuilder())
-				.addField(createQuantityFieldBuilder())
-				.build();
-	}
-
-	private static DocumentEntityDescriptor.Builder createDescriptorBuilder(
-			final DocumentId documentTypeId,
-			final DetailId detailId,
-			@NonNull final Optional<SOTrx> soTrx)
-	{
 		return DocumentEntityDescriptor.builder()
 				.setDocumentType(DocumentType.QuickInput, documentTypeId)
 				.setIsSOTrx(soTrx)
 				.disableDefaultTableCallouts()
-				// Defaults:
 				.setDetailId(detailId)
-				.setTableName(I_C_OrderLine.Table_Name); // TODO: figure out if it's needed
+				.setTableName(I_C_OrderLine.Table_Name) // TODO: figure out if it's needed
+				//
+				.addField(createProductFieldBuilder(soTrx))
+				.addFieldIf(QuickInputConstants.isEnablePackingInstructionsField(), () -> createPackingInstructionFieldBuilder())
+				.addField(createQuantityFieldBuilder())
+				.addFieldIf(QuickInputConstants.isEnableBestBeforePolicy(), () -> createBestBeforePolicyFieldBuilder())
+				//
+				.build();
 	}
 
 	private DocumentFieldDescriptor.Builder createProductFieldBuilder(@NonNull final Optional<SOTrx> soTrx)
 	{
-		final IMsgBL msgBL = Services.get(IMsgBL.class);
-
 		final ProductLookupDescriptor productLookupDescriptor = createProductLookupDescriptor(soTrx);
 		final ITranslatableString caption = msgBL.translatable(IOrderLineQuickInput.COLUMNNAME_M_Product_ID);
 
@@ -137,7 +137,7 @@ import lombok.NonNull;
 				.setAlwaysUpdateable(true)
 				.setMandatoryLogic(ConstantLogicExpression.TRUE)
 				.setDisplayLogic(ConstantLogicExpression.TRUE)
-				.addCallout(OrderLineQuickInputDescriptorFactory::onProductChangedCallout)
+				.addCallout(this::onProductChangedCallout)
 				.addCharacteristic(Characteristic.PublicField);
 	}
 
@@ -165,7 +165,7 @@ import lombok.NonNull;
 		}
 	}
 
-	private static void onProductChangedCallout(final ICalloutField calloutField)
+	private void onProductChangedCallout(final ICalloutField calloutField)
 	{
 		final QuickInput quickInput = QuickInput.getQuickInputOrNull(calloutField);
 		if (quickInput == null)
@@ -190,13 +190,11 @@ import lombok.NonNull;
 		final ProductId quickInputProductId = productAndAttributes.getProductId();
 
 		final I_C_Order order = quickInput.getRootDocumentAs(I_C_Order.class);
-		Services.get(IHUOrderBL.class).findM_HU_PI_Item_Product(order, quickInputProductId, quickInputModel::setM_HU_PI_Item_Product);
+		huOrderBL.findM_HU_PI_Item_Product(order, quickInputProductId, quickInputModel::setM_HU_PI_Item_Product);
 	}
 
-	private static DocumentFieldDescriptor.Builder createPackingInstructionFieldBuilder()
+	private DocumentFieldDescriptor.Builder createPackingInstructionFieldBuilder()
 	{
-		final IMsgBL msgBL = Services.get(IMsgBL.class);
-
 		return DocumentFieldDescriptor.builder(IOrderLineQuickInput.COLUMNNAME_M_HU_PI_Item_Product_ID)
 				.setCaption(msgBL.translatable(IOrderLineQuickInput.COLUMNNAME_M_HU_PI_Item_Product_ID))
 				//
@@ -215,10 +213,10 @@ import lombok.NonNull;
 				.addCharacteristic(Characteristic.PublicField);
 	}
 
-	private static DocumentFieldDescriptor.Builder createQuantityFieldBuilder()
+	private DocumentFieldDescriptor.Builder createQuantityFieldBuilder()
 	{
 		return DocumentFieldDescriptor.builder(IOrderLineQuickInput.COLUMNNAME_Qty)
-				.setCaption(Services.get(IMsgBL.class).translatable(IOrderLineQuickInput.COLUMNNAME_Qty))
+				.setCaption(msgBL.translatable(IOrderLineQuickInput.COLUMNNAME_Qty))
 				.setWidgetType(DocumentFieldWidgetType.Quantity)
 				.setReadonlyLogic(ConstantLogicExpression.FALSE)
 				.setAlwaysUpdateable(true)
@@ -227,11 +225,27 @@ import lombok.NonNull;
 				.addCharacteristic(Characteristic.PublicField);
 	}
 
+	private DocumentFieldDescriptor.Builder createBestBeforePolicyFieldBuilder()
+	{
+		return DocumentFieldDescriptor.builder(IOrderLineQuickInput.COLUMNNAME_ShipmentAllocation_BestBefore_Policy)
+				.setCaption(msgBL.translatable(IOrderLineQuickInput.COLUMNNAME_ShipmentAllocation_BestBefore_Policy))
+				//
+				.setWidgetType(DocumentFieldWidgetType.List)
+				.setLookupDescriptorProvider(SqlLookupDescriptor.listByAD_Reference_Value_ID(ShipmentAllocationBestBeforePolicy.AD_REFERENCE_ID))
+				.setValueClass(StringLookupValue.class)
+				.setReadonlyLogic(ConstantLogicExpression.FALSE)
+				.setAlwaysUpdateable(true)
+				.setMandatoryLogic(ConstantLogicExpression.FALSE)
+				.setDisplayLogic(ConstantLogicExpression.TRUE)
+				.addCharacteristic(Characteristic.PublicField);
+	}
+
 	private static QuickInputLayoutDescriptor createLayout(final DocumentEntityDescriptor entityDescriptor)
 	{
 		return QuickInputLayoutDescriptor.build(entityDescriptor, new String[][] {
 				{ "M_Product_ID", "M_HU_PI_Item_Product_ID" },
-				{ "Qty" }
+				{ "Qty" },
+				{ "ShipmentAllocation_BestBefore_Policy" }
 		});
 	}
 }
