29f449e86eb872b5750488a3d2069020c672f168
Teo Sarca <teo.sarca@metasfresh.com>
2019-09-19 00:58:33 +0300

    avoid starting main spring boot classes if they are not started standalone

--- de.metas.report/report-service/src/main/java/de/metas/report/ReportServiceMain.java
+++ de.metas.report/report-service/src/main/java/de/metas/report/ReportServiceMain.java
@@ -48,17 +48,19 @@ import de.metas.util.StringUtils;
 
 @SpringBootApplication(scanBasePackages = { "de.metas" })
 @ServletComponentScan(value = { "de.metas.adempiere.report.jasper.servlet" })
-@Profile(Profiles.PROFILE_JasperService)
+@Profile(ReportServiceMain.PROFILE_JasperService_Standalone)
 public class ReportServiceMain
 {
 	@Autowired
 	private ApplicationContext applicationContext;
 
+	static final String PROFILE_JasperService_Standalone = Profiles.PROFILE_JasperService + "-standalone";
+
 	/**
 	 * By default, we run in headless mode. But using this system property, we can also run with headless=false.
 	 * The only known use of that is that metasfresh can open the initial license & connection dialog to store the initial properties file.
 	 */
-	public static final String SYSTEM_PROPERTY_HEADLESS = "app-server-run-headless";
+	static final String SYSTEM_PROPERTY_HEADLESS = "app-server-run-headless";
 
 	public static void main(final String[] args)
 	{
@@ -71,13 +73,17 @@ public class ReportServiceMain
 			new SpringApplicationBuilder(ReportServiceMain.class)
 					.headless(StringUtils.toBoolean(headless)) // we need headless=false for initial connection setup popup (if any), usually this only applies on dev workstations.
 					.web(true)
-					.profiles(Profiles.PROFILE_JasperService)
+					.profiles(Profiles.PROFILE_JasperService, PROFILE_JasperService_Standalone)
 					.beanNameGenerator(new MetasfreshBeanNameGenerator())
 					.run(args);
 		}
 
 		// now init the model validation engine
 		ModelValidationEngine.get();
+	}
+
+	public ReportServiceMain()
+	{
 	}
 
 	@Bean
