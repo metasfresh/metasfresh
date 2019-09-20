30ed8ed51d15cf5559d870966107b87ed69c2d82
metasnw <nw@metasfresh.com>
2019-09-20 10:33:32 +0200

    #5484 adding status categories for roles

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5531390_cli_gh5484_seminar_project_adding_status_categories.sql
@@ -0,0 +1,15 @@
+-- 2019-09-20T07:33:48.012Z
+-- URL zum Konzept
+INSERT INTO R_StatusCategory (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,IsDefault,Name,R_StatusCategory_ID,Updated,UpdatedBy) VALUES (1000000,0,TO_TIMESTAMP('2019-09-20 09:33:47','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Seminar Teilnehmer Status',540011,TO_TIMESTAMP('2019-09-20 09:33:47','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-09-20T07:33:57.252Z
+-- URL zum Konzept
+INSERT INTO R_StatusCategory (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,IsDefault,Name,R_StatusCategory_ID,Updated,UpdatedBy) VALUES (1000000,0,TO_TIMESTAMP('2019-09-20 09:33:57','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Seminar Dozent Status',540012,TO_TIMESTAMP('2019-09-20 09:33:57','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-09-20T07:34:11.320Z
+-- URL zum Konzept
+INSERT INTO R_StatusCategory (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,IsDefault,Name,R_StatusCategory_ID,Updated,UpdatedBy) VALUES (1000000,0,TO_TIMESTAMP('2019-09-20 09:34:11','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Seminar Location Status',540013,TO_TIMESTAMP('2019-09-20 09:34:11','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
