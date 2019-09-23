6c0ba05c2191ea7637f35312f8e34352dfc8f100
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-09-23 10:24:32 +0300

    #5536 Remove location as param from and a separate report for locations
    #5536 Allow possibility to run TU Label from BPartner
    
    (cherry picked from commit d6165a8d3a93ebc1e4f25d1c380c1a135ba66404)
    
    solved Conflicts:
    	de.metas.fresh/de.metas.fresh.base/src/main/jasperreports/de/metas/docs/label/picking_tu_label/label.jrxml

--- de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/housekeeping/spi/impl/ClearTemporaryTables.java
+++ de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/dao/housekeeping/spi/impl/ClearTemporaryTables.java
@@ -13,12 +13,12 @@ import org.adempiere.ad.dao.model.I_T_Query_Selection_ToDelete;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -35,9 +35,6 @@ import de.metas.logging.LogManager;
 
 /**
  * Clears several temporary tables.
- * 
- * @author ts
- * 
  */
 public class ClearTemporaryTables implements IStartupHouseKeepingTask
 {
