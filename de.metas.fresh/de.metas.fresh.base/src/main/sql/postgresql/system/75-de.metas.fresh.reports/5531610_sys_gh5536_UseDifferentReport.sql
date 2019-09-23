2f28a66427b50de5ce507c51c9259393d780f5b1
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-09-23 11:43:59 +0300

    #5536 New report
    #5536 Allow possibility to run TU Label from BPartner

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5531610_sys_gh5536_UseDifferentReport.sql
@@ -0,0 +1,5 @@
+-- 2019-09-23T08:39:25.427Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET JasperReport='@PREFIX@de/metas/docs/label/c_bpartner_location/label.jasper', Name='Picking Label', Value='Picking Label-Partner (Jasper)',Updated=TO_TIMESTAMP('2019-09-23 11:39:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=541194
+;
+
