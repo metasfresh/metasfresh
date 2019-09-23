318b4154376e5ddef2735956d0aac346c345dcc8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-09-23 17:25:05 +0200

    5536-app - Allow possibility to run TU Label from BPartner
    add required cleanup-script for this branch
    https://github.com/metasfresh/metasfresh/issues/5536

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5531530_sys_gh5536_DeleteReporFromHUTable.sql
@@ -0,0 +1,12 @@
+-- 2019-09-20T16:57:05.020
+-- URL zum Konzept
+DELETE FROM AD_Table_Process WHERE AD_Process_ID=540933 AND AD_Table_ID=291
+;
+
+;
+
+-- 2019-09-20T17:15:35.781
+-- URL zum Konzept
+UPDATE AD_Process SET Classname='org.compiere.report.ReportStarter',Updated=TO_TIMESTAMP('2019-09-20 17:15:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=541194
+;
+
