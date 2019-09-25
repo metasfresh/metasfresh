88c48f60ad1faee684fd293540ae13baef35aaae
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-09-25 16:26:55 +0300

    #5569 coalesce function used for retrieving warehouse
    #5569 Mandatory Field in Purchase Order: Warehouse ID

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5531815_sys_gh2754update_warehouse_where_null.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5531815_sys_gh2754update_warehouse_where_null.sql
@@ -1,8 +1,4 @@
-update c_order o set m_warehouse_id=(select oi.m_warehousepo_id from ad_orginfo oi where oi.ad_org_id=o.ad_org_id)
-where issotrx='N'
-and m_warehouse_id is null;
-
-update c_order o set m_warehouse_id=(select oi.m_warehouse_id from ad_orginfo oi where oi.ad_org_id=o.ad_org_id)
+update c_order o set m_warehouse_id=(select coalesce(oi.m_warehousepo_id, oi.m_warehouse_id, oi.dropship_warehouse_id) from ad_orginfo oi where oi.ad_org_id=o.ad_org_id)
 where issotrx='N'
 and m_warehouse_id is null;
 
