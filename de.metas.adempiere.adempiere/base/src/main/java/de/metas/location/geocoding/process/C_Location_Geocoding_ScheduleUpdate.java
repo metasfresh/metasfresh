2c3c4c5d292b13d88bdb228c727ff88f22c484bf
TheBestPessimist <cristian@tbp.land>
2019-10-04 14:39:49 +0300

    Geocoding update process includes GeocodingStatus=Error, and throw exception if no provider is selected
    https://github.com/metasfresh/metasfresh/issues/5566

--- de.metas.adempiere.adempiere/base/src/main/java/de/metas/location/geocoding/process/C_Location_Geocoding_ScheduleUpdate.java
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/location/geocoding/process/C_Location_Geocoding_ScheduleUpdate.java
@@ -1,5 +1,6 @@
 package de.metas.location.geocoding.process;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import org.adempiere.ad.dao.IQueryBL;
@@ -30,12 +31,12 @@ import lombok.NonNull;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -78,7 +79,7 @@ public class C_Location_Geocoding_ScheduleUpdate extends JavaProcess
 				.createQueryBuilderOutOfTrx(I_C_Location.class)
 				//
 				.addOnlyActiveRecordsFilter()
-				.addEqualsFilter(I_C_Location.COLUMN_GeocodingStatus, X_C_Location.GEOCODINGSTATUS_NotChecked)
+				.addInArrayOrAllFilter(I_C_Location.COLUMN_GeocodingStatus, Arrays.asList(X_C_Location.GEOCODINGSTATUS_Error, X_C_Location.GEOCODINGSTATUS_NotChecked))
 				//
 				.addCompareFilter(I_C_Location.COLUMN_C_Location_ID, Operator.GREATER, LocationId.toRepoIdOr(maxLocationIdScheduled, 0))
 				.orderByDescending(I_C_Location.COLUMN_C_Location_ID)
