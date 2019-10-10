5e8f8cf2dd6e7738dc53202184d14426f171d698
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2019-10-10 12:05:20 +0300

    #5649 gtin in advanced edit
    #5649 GTIN in Product window

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5533720_sys_gh5649_gtin_in_advanced_edit.sql
@@ -0,0 +1,5 @@
+-- 2019-10-10T08:59:09.773Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='Y',Updated=TO_TIMESTAMP('2019-10-10 11:59:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=563218
+;
+
