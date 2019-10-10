04ab014a1d5acf3e7b9ed722221875e76797e370
TheBestPessimist <cristian@tbp.land>
2019-10-10 14:42:03 +0300

    First draft of DhlDraftDeliveryOrderCreator
    https://github.com/metasfresh/metasfresh/issues/5611

--- de.metas.shipper.gateway.dhl/src/main/java/de/metas/shipper/gateway/dhl/DhlDraftDeliveryOrderCreator.java
+++ de.metas.shipper.gateway.dhl/src/main/java/de/metas/shipper/gateway/dhl/DhlDraftDeliveryOrderCreator.java
@@ -25,11 +25,14 @@ package de.metas.shipper.gateway.dhl;
 import de.metas.bpartner.service.IBPartnerOrgBL;
 import de.metas.organization.OrgId;
 import de.metas.shipper.gateway.commons.DeliveryOrderUtil;
+import de.metas.shipper.gateway.dhl.model.DhlServiceType;
 import de.metas.shipper.gateway.spi.DraftDeliveryOrderCreator;
+import de.metas.shipper.gateway.spi.model.ContactPerson;
 import de.metas.shipper.gateway.spi.model.DeliveryOrder;
 import de.metas.shipper.gateway.spi.model.DeliveryPosition;
 import de.metas.shipper.gateway.spi.model.PickupDate;
 import de.metas.util.Services;
+import de.metas.util.lang.CoalesceUtil;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_C_BPartner_Location;
 import org.compiere.model.I_C_Location;
@@ -68,6 +71,7 @@ public class DhlDraftDeliveryOrderCreator implements DraftDeliveryOrderCreator
 		final int deliverToBPartnerLocationId = deliveryOrderKey.getDeliverToBPartnerLocationId();
 		final I_C_BPartner_Location deliverToBPLocation = load(deliverToBPartnerLocationId, I_C_BPartner_Location.class);
 		final I_C_Location deliverToLocation = deliverToBPLocation.getC_Location();
+		final String deliverToPhoneNumber = CoalesceUtil.firstNotEmptyTrimmed(deliverToBPLocation.getPhone(), deliverToBPLocation.getPhone2(), deliverToBPartner.getPhone2());
 
 		// todo: implement DHL custom delivery order data, the rest of the code is similar to the 2 other shippers
 		//		final GoDeliveryOrderData goDeliveryOrderData = GoDeliveryOrderData.builder()
@@ -77,14 +81,16 @@ public class DhlDraftDeliveryOrderCreator implements DraftDeliveryOrderCreator
 		//				.selfDelivery(GOSelfDelivery.Pickup)
 		//				.build();
 
+		// todo when editing this don't forget to update de.metas.shipper.gateway.dhl.DhlDeliveryOrderRepository.toDeliveryOrderPO
 		return DeliveryOrder.builder()
-				.shipperId(deliveryOrderKey.getShipperId())
-				.shipperTransportationId(deliveryOrderKey.getShipperTransportationId())
+				//	todo what's this? what is it used for?			.shipperId(deliveryOrderKey.getShipperId())
+				//	todo what's this? what is it used for?			.shipperTransportationId(deliveryOrderKey.getShipperTransportationId())
 				//
-				//				todo .serviceType(GOServiceType.Overnight)
+				.serviceType(DhlServiceType.V01PAK) // todo how to change the service type?
+				//				.customerReference() // todo this is not set in any place with any user-relevant value afaics!
 				//				todo .customDeliveryData(goDeliveryOrderData)
 				//
-				// Pickup
+				// Pickup aka Shipper
 				.pickupAddress(DeliveryOrderUtil.prepareAddressFromLocation(pickupFromLocation)
 						.companyName1(pickupFromBPartner.getName())
 						.companyName2(pickupFromBPartner.getName2())
@@ -93,13 +99,16 @@ public class DhlDraftDeliveryOrderCreator implements DraftDeliveryOrderCreator
 						.date(pickupDate)
 						.build())
 				//
-				// Delivery
+				// Delivery aka Receiver
 				.deliveryAddress(DeliveryOrderUtil.prepareAddressFromLocation(deliverToLocation)
 						.companyName1(deliverToBPartner.getName())
 						.companyName2(deliverToBPartner.getName2())
-						.companyDepartment("-") // N/A
-						.bpartnerId(deliverToBPartnerId)
-						.bpartnerLocationId(deliverToBPartnerLocationId)
+						.bpartnerId(deliverToBPartnerId) // afaics used only for logging
+						.bpartnerLocationId(deliverToBPartnerLocationId) // afaics used only for logging
+						.build())
+				.deliveryContact(ContactPerson.builder()
+						.emailAddress(deliverToBPartner.getEMail())
+						.simplePhoneNumber(deliverToPhoneNumber)
 						.build())
 				//
 				// Delivery content
@@ -107,7 +116,7 @@ public class DhlDraftDeliveryOrderCreator implements DraftDeliveryOrderCreator
 						.numberOfPackages(mpackageIds.size())
 						.packageIds(mpackageIds)
 						.grossWeightKg(Math.max(request.getGrossWeightInKg(), 1))
-						.content(request.getPackageContentDescription())
+//						.packageDimensions() // todo how to get these?
 						.build())
 				// todo if needed .customerReference(null)
 				//
