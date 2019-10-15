4c3b7c97832fc5248e427a559af10cecbb42163d
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-10-15 13:28:02 +0200

    5656-app - EDI-Tweaks
    - minor documentation
    https://github.com/metasfresh/metasfresh/issues/5656

--- de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/ordercandidates/impl/ProductPriceCreateRequest.java
+++ de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/ordercandidates/impl/ProductPriceCreateRequest.java
@@ -23,12 +23,12 @@ import lombok.Value;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -42,6 +42,7 @@ public class ProductPriceCreateRequest
 	@NonNull
 	BPartnerLocationId bpartnerAndLocationId;
 
+	/** If null, the system will attempt to get the pricing system id from the bartner's mater data. */
 	@Nullable
 	final PricingSystemId pricingSystemId;
 
