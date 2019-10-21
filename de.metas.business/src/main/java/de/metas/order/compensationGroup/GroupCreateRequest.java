94af133f558a5b0dbd046841784c90788a9d1729
Teo Sarca <teo.sarca@metasfresh.com>
2019-10-21 10:43:24 +0300

    refactor OrderGroupRepository; introduce GroupCreateRequest
    https://github.com/metasfresh/metasfresh/issues/5679

--- de.metas.business/src/main/java/de/metas/order/compensationGroup/GroupCreateRequest.java
+++ de.metas.business/src/main/java/de/metas/order/compensationGroup/GroupCreateRequest.java
@@ -1,8 +1,9 @@
 package de.metas.order.compensationGroup;
 
-import java.util.Set;
+import javax.annotation.Nullable;
 
-import de.metas.order.OrderLineId;
+import de.metas.order.OrderId;
+import de.metas.product.ProductCategoryId;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
@@ -11,39 +12,37 @@ import lombok.Value;
  * #%L
  * de.metas.business
  * %%
- * Copyright (C) 2017 metas GmbH
+ * Copyright (C) 2019 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-public interface GroupRepository
+@Value
+@Builder
+public class GroupCreateRequest
 {
-	Group retrieveGroup(GroupId groupId);
+	@NonNull
+	OrderId orderId;
 
-	void saveGroup(Group group);
-
-	GroupCreator prepareNewGroup();
-
-	Group retrieveOrCreateGroup(RetrieveOrCreateGroupRequest request);
+	@NonNull
+	String name;
 	
-	@Value
-	@Builder
-	class RetrieveOrCreateGroupRequest
-	{
-		@NonNull Set<OrderLineId> orderLineIds;
-		@NonNull GroupTemplate newGroupTemplate;
-	}
+	@Nullable
+	ProductCategoryId productCategoryId;
+	
+	@Nullable
+	GroupTemplateId groupTemplateId;
 }
