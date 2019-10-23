52c3faba16482ec87cb54593cc1f47bc270af031
metas-rc <ruxandra.craciunescu@metasfresh.com>
2019-10-23 14:21:32 +0300

    #5702 Display Description and BottomDescription in orders
    https://github.com/metasfresh/metasfresh/issues/5702

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5535020_sys_gh5702_Display_Description_And_BottomDescription_In_Orders.sql
@@ -0,0 +1,30 @@
+-- 2019-10-23T11:04:09.917Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,1086,0,186,563599,540499,'F',TO_TIMESTAMP('2019-10-23 14:04:09','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Beschreibung',5,0,0,TO_TIMESTAMP('2019-10-23 14:04:09','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-10-23T11:04:11.459Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='Y',Updated=TO_TIMESTAMP('2019-10-23 14:04:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=563599
+;
+
+-- 2019-10-23T11:04:34.342Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,565200,0,186,563600,540499,'F',TO_TIMESTAMP('2019-10-23 14:04:34','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Schlusstext',6,0,0,TO_TIMESTAMP('2019-10-23 14:04:34','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-10-23T11:04:38.351Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='Y',Updated=TO_TIMESTAMP('2019-10-23 14:04:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=563600
+;
+
+-- 2019-10-23T11:06:03.239Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET SeqNo=5,Updated=TO_TIMESTAMP('2019-10-23 14:06:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552493
+;
+
+-- 2019-10-23T11:06:07.507Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET SeqNo=6,Updated=TO_TIMESTAMP('2019-10-23 14:06:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=552492
+;
+
