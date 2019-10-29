1d295ef6076e29dd3254acbf92ec1a3408689d7f
TheBestPessimist <cristian@tbp.land>
2019-10-29 16:51:39 +0200

    Refactor stuff around; fix warnings
    https://github.com/metasfresh/metasfresh/issues/5637

---
+++ de.metas.shipper.gateway.dpd/src/main/java/de/metas/shipper/gateway/dpd/util/DpdSoapHeaderWithAuth.java
@@ -0,0 +1,64 @@
+/*
+ * #%L
+ * de.metas.shipper.gateway.dpd
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.shipper.gateway.dpd.util;
+
+import com.dpd.common.ws.authentication.v2_0.types.Authentication;
+import org.adempiere.exceptions.AdempiereException;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
+import org.springframework.ws.soap.SoapHeader;
+import org.springframework.ws.soap.SoapMessage;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
+public class DpdSoapHeaderWithAuth implements WebServiceMessageCallback
+{
+	private final Authentication authentication;
+
+	public DpdSoapHeaderWithAuth(final Authentication authentication)
+	{
+		this.authentication = authentication;
+	}
+
+	// thx to https://www.devglan.com/spring-mvc/custom-header-in-spring-soap-request
+	// 		for the SoapHeader reference
+	@Override
+	public void doWithMessage(final WebServiceMessage message)
+	{
+		try
+		{
+			// add the authentication to header
+			final SoapMessage soapMessage = (SoapMessage)message;
+			final SoapHeader header = soapMessage.getSoapHeader();
+
+			final JAXBContext context = JAXBContext.newInstance(authentication.getClass());
+			final javax.xml.bind.Marshaller marshaller = context.createMarshaller();
+			marshaller.marshal(authentication, header.getResult());
+		}
+		catch (final JAXBException e)
+		{
+			throw new AdempiereException("Error while setting soap header");
+		}
+	}
+}
--- de.metas.shipper.gateway.dpd/src/test/java/de/metas/shipper/gateway/dpd/Test1.java
+++ de.metas.shipper.gateway.dpd/src/test/java/de/metas/shipper/gateway/dpd/Test1.java
@@ -16,25 +16,17 @@ import com.dpd.common.ws.authentication.v2_0.types.ObjectFactory;
 import com.dpd.common.ws.loginservice.v2_0.types.GetAuth;
 import com.dpd.common.ws.loginservice.v2_0.types.GetAuthResponse;
 import com.dpd.common.ws.loginservice.v2_0.types.Login;
-import lombok.NonNull;
-import org.adempiere.exceptions.AdempiereException;
+import de.metas.shipper.gateway.dpd.util.DpdClientUtil;
+import de.metas.shipper.gateway.dpd.util.DpdSoapHeaderWithAuth;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.oxm.Marshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.WebServiceMessage;
-import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.soap.SoapHeader;
-import org.springframework.ws.soap.SoapMessage;
-import org.springframework.xml.transform.StringResult;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import java.io.IOException;
 
+import static de.metas.shipper.gateway.dpd.util.DpdClientUtil.LOGIN_SERVICE_API_URL;
+import static de.metas.shipper.gateway.dpd.util.DpdClientUtil.SHIPMENT_SERVICE_API_URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -44,9 +36,6 @@ class Test1
 	private static final String DELIS_PASSWORD = "a";
 	private static final String MESSAGE_LANGUAGE = "en_EN";
 
-	public static final String LOGIN_SERVICE_API_URL = "https://public-ws-stage.dpd.com/services/LoginService/V2_0/";
-	public static final String SHIPMENT_SERVICE_API_URL = "https://public-ws-stage.dpd.com/services/ShipmentService/V3_2/";
-
 	private WebServiceTemplate webServiceTemplate;
 	private com.dpd.common.ws.loginservice.v2_0.types.ObjectFactory loginServiceOF;
 	private com.dpd.common.service.types.shipmentservice._3.ObjectFactory shipmentServiceOF;
@@ -54,13 +43,13 @@ class Test1
 	@BeforeEach
 	private void beforeEach()
 	{
-		webServiceTemplate = createWebServiceTemplate();
+		webServiceTemplate = DpdClientUtil.createWebServiceTemplate();
 		loginServiceOF = new com.dpd.common.ws.loginservice.v2_0.types.ObjectFactory();
 		shipmentServiceOF = new com.dpd.common.service.types.shipmentservice._3.ObjectFactory();
 	}
 
 	@Test
-	void _1_testLogin() throws IOException
+	void _1_testLogin()
 	{
 
 		final GetAuth getAuthValue = loginServiceOF.createGetAuth();
@@ -70,23 +59,21 @@ class Test1
 
 		final JAXBElement<GetAuth> getAuth = loginServiceOF.createGetAuth(getAuthValue);
 
-		System.out.println(elementToString(getAuth, webServiceTemplate.getMarshaller()));
 		//noinspection unchecked
 		final JAXBElement<GetAuthResponse> authenticationElement = (JAXBElement<GetAuthResponse>)webServiceTemplate.marshalSendAndReceive(LOGIN_SERVICE_API_URL, getAuth);
-		System.out.println(elementToString(authenticationElement, webServiceTemplate.getMarshaller()));
 
-		final Login authentication = authenticationElement.getValue().getReturn();
+		final Login login = authenticationElement.getValue().getReturn();
 
-		assertTrue(StringUtils.isNotBlank(authentication.getAuthToken()));
-		assertTrue(StringUtils.isNotBlank(authentication.getDepot()));
-		assertEquals(authentication.getDelisId(), DELIS_ID);
-		assertEquals(authentication.getCustomerUid(), DELIS_ID);
+		assertTrue(StringUtils.isNotBlank(login.getAuthToken()));
+		assertTrue(StringUtils.isNotBlank(login.getDepot()));
+		assertEquals(login.getDelisId(), DELIS_ID);
+		assertEquals(login.getCustomerUid(), DELIS_ID);
 	}
 
 	@Test
 	void _2_createOrder()
 	{
-		final Login login;
+		final Authentication authentication;
 		{
 			// Login
 			final GetAuth getAuthValue = loginServiceOF.createGetAuth();
@@ -95,8 +82,14 @@ class Test1
 			getAuthValue.setMessageLanguage(MESSAGE_LANGUAGE);
 
 			final JAXBElement<GetAuth> getAuth = loginServiceOF.createGetAuth(getAuthValue);
+			//noinspection unchecked
 			final JAXBElement<GetAuthResponse> authenticationElement = (JAXBElement<GetAuthResponse>)webServiceTemplate.marshalSendAndReceive(LOGIN_SERVICE_API_URL, getAuth);
-			login = authenticationElement.getValue().getReturn();
+			final Login login = authenticationElement.getValue().getReturn();
+
+			authentication = new ObjectFactory().createAuthentication();
+			authentication.setDelisId(login.getDelisId());
+			authentication.setAuthToken(login.getAuthToken());
+			authentication.setMessageLanguage(MESSAGE_LANGUAGE);
 		}
 
 		final StoreOrders storeOrders;
@@ -124,7 +117,7 @@ class Test1
 					generalShipmentData.setIdentificationNumber("Article 456"); // what is this? optional?
 					generalShipmentData.setSendingDepot("0112"); // mandatory? (taken from login)
 					generalShipmentData.setProduct("CL"); // this is the DPD product
-					generalShipmentData.setMpsWeight(Long.valueOf(500)); // what UOM? optional?
+					generalShipmentData.setMpsWeight(500L); // what UOM? optional?
 
 					{
 						// Sender
@@ -136,6 +129,7 @@ class Test1
 						sender.setName2("TheBestPessimist");
 						sender.setStreet("Schröderstr.");
 						sender.setHouseNo("66");
+						//noinspection ConstantConditions
 						sender.setState(null);
 						sender.setCountry("DE");
 						sender.setZipCode("10115");
@@ -146,12 +140,13 @@ class Test1
 					}
 					{
 						// Recipient
-						Address recipient = shipmentServiceOF.createAddress();
+						final Address recipient = shipmentServiceOF.createAddress();
 						generalShipmentData.setRecipient(recipient);
 						recipient.setName1("DPD Deutschland GmbH");
 						recipient.setName2("");
 						recipient.setStreet("Wailandtstr.");
 						recipient.setHouseNo("1");
+						//noinspection ConstantConditions
 						recipient.setState(null);
 						recipient.setCountry("DE");
 						recipient.setZipCode("63741");
@@ -199,6 +194,7 @@ class Test1
 						pickupAddress.setName2("TheBestPessimist");
 						pickupAddress.setStreet("Schröderstr.");
 						pickupAddress.setHouseNo("66");
+						//noinspection ConstantConditions
 						pickupAddress.setState(null);
 						pickupAddress.setCountry("DE");
 						pickupAddress.setZipCode("10115");
@@ -213,73 +209,16 @@ class Test1
 		{
 			// Call the Shipment API
 			final JAXBElement<StoreOrders> storeOrdersElement = shipmentServiceOF.createStoreOrders(storeOrders);
+			//noinspection unchecked
 			final JAXBElement<StoreOrdersResponse> storeOrdersResponseElement =
-					(JAXBElement<StoreOrdersResponse>)webServiceTemplate.marshalSendAndReceive(SHIPMENT_SERVICE_API_URL, storeOrdersElement, new SoapHeaderWithAuth(login));
+					(JAXBElement<StoreOrdersResponse>)webServiceTemplate.marshalSendAndReceive(SHIPMENT_SERVICE_API_URL, storeOrdersElement, new DpdSoapHeaderWithAuth(authentication));
 
 			final StoreOrdersResponseType orderResult = storeOrdersResponseElement.getValue().getOrderResult();
 			assertTrue(orderResult.getParcellabelsPDF().length > 2);
-			assertTrue(orderResult.getShipmentResponses().size() == 1);
+			assertEquals(1, orderResult.getShipmentResponses().size());
 			final ShipmentResponse shipmentResponse = orderResult.getShipmentResponses().get(0);
 			assertEquals("Article 456", shipmentResponse.getIdentificationNumber());
 			assertTrue(shipmentResponse.getMpsId().startsWith("MPS"));
-		}
-	}
-
-	@NonNull
-	private WebServiceTemplate createWebServiceTemplate()
-	{
-		final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		marshaller.setPackagesToScan(
-				"com.dpd.common"
-		);
-
-		final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
-		//		webServiceTemplate.setDefaultUri(apiUrl);
-		webServiceTemplate.setMarshaller(marshaller);
-		webServiceTemplate.setUnmarshaller(marshaller);
-		return webServiceTemplate;
-	}
-
-	private static StringResult elementToString(final Object element, final Marshaller marshaller) throws IOException
-	{
-		final StringResult result = new StringResult();
-		marshaller.marshal(element, result);
-		return result;
-	}
-
-	private static class SoapHeaderWithAuth implements WebServiceMessageCallback
-	{
-		private final Login login;
-
-		private SoapHeaderWithAuth(final Login login)
-		{
-			this.login = login;
-		}
-
-		// thx to https://www.devglan.com/spring-mvc/custom-header-in-spring-soap-request
-		// 		for the SoapHeader reference
-		@Override
-		public void doWithMessage(final WebServiceMessage message)
-		{
-			try
-			{
-				final Authentication authentication = new ObjectFactory().createAuthentication();
-				authentication.setDelisId(login.getDelisId());
-				authentication.setAuthToken(login.getAuthToken());
-				authentication.setMessageLanguage(MESSAGE_LANGUAGE);
-
-				// add the authentication to header
-				final SoapMessage soapMessage = (SoapMessage)message;
-				final SoapHeader header = soapMessage.getSoapHeader();
-
-				final JAXBContext context = JAXBContext.newInstance(authentication.getClass());
-				final javax.xml.bind.Marshaller marshaller = context.createMarshaller();
-				marshaller.marshal(authentication, header.getResult());
-			}
-			catch (final JAXBException e)
-			{
-				throw new AdempiereException("Error while setting soap header");
-			}
 		}
 	}
 }
