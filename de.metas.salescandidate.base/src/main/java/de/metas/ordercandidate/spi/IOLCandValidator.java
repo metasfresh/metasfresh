5217ba2f037f159980f70256227a2be1c2a70be2
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2019-11-14 12:08:20 +0100

    5656-app - EDI-Tweaks
    * add a sequence number to IOLCandValidator so that we can control their other of execution
    https://github.com/metasfresh/metasfresh/issues/5656

--- de.metas.salescandidate.base/src/main/java/de/metas/ordercandidate/spi/IOLCandValidator.java
+++ de.metas.salescandidate.base/src/main/java/de/metas/ordercandidate/spi/IOLCandValidator.java
@@ -13,11 +13,11 @@ package de.metas.ordercandidate.spi;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
@@ -31,6 +31,9 @@ import de.metas.ordercandidate.model.I_C_OLCand;
  */
 public interface IOLCandValidator
 {
+	/** Validators with lower sequence number are executed first. They might set fields that later validators might need. */
+	int getSeqNo();
+
 	/**
 	 * Validate the given <code>olCand</code>.
 	 * <p>
