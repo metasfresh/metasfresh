704890d3f06e8403420919f70a02c33df90c4509
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-11-21 11:00:20 +0200

    #5804 Make BPName identifier
    #5804 BPartner Name overwrite in BPartner Location

---
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5536520_sys_gh5804_MakeBPName_Identifier_in_BPLocation.sql
@@ -0,0 +1,10 @@
+-- 2019-11-21T08:58:52.923Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET SeqNo=2,Updated=TO_TIMESTAMP('2019-11-21 10:58:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2960
+;
+
+-- 2019-11-21T08:59:06.791Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsIdentifier='Y', SeqNo=1,Updated=TO_TIMESTAMP('2019-11-21 10:59:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=569638
+;
+
