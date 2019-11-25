ce69db5d4d1cccd3c4d39fbebdcc5370ebd68835
pvpurcarcosmin <pv.purcar.cosmin@gmail.com>
2019-11-25 17:41:05 +0200

    added default packing instructions into Picking terminal v2

---
+++ de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5536690_sys_gh3334_add_default_packing_intruct_in_pickingT_v2.sql
@@ -0,0 +1,6 @@
+-- 2019-11-25T12:07:47.446Z
+-- URL zum Konzept
+UPDATE AD_Process_Para SET DefaultValue='1000000',Updated=TO_TIMESTAMP('2019-11-25 14:07:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541346
+;
+
+      
