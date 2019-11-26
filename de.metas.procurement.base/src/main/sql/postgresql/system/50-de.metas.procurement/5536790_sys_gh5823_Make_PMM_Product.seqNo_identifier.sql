3cefa39c0b4d1ab6b7de52a8e8293252569a2437
TheBestPessimist <cristian@tbp.land>
2019-11-26 12:33:27 +0200

    Mark PMM_Product.seqNo as identifier
    https://github.com/metasfresh/metasfresh/issues/5823

---
+++ de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5536790_sys_gh5823_Make_PMM_Product.seqNo_identifier.sql
@@ -0,0 +1,5 @@
+-- 2019-11-26T07:49:29.402Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2019-11-26 09:49:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=556027
+;
+
