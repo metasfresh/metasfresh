51116ebb6867cce29a874a4c1f10438829f035a2
Teo Sarca <teo.sarca@metasfresh.com>
2019-11-28 22:08:32 +0200

    PaymentAllocationRepository.retrievePaymentsToAllocate
    https://github.com/metasfresh/metasfresh-webui-api/issues/1320

--- de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/paymentallocation/PaymentAllocationRepository.java
+++ de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/paymentallocation/PaymentAllocationRepository.java
@@ -29,6 +29,7 @@ import de.metas.money.CurrencyId;
 import de.metas.order.OrderId;
 import de.metas.organization.ClientAndOrgId;
 import de.metas.organization.OrgId;
+import de.metas.payment.PaymentId;
 import de.metas.util.Check;
 import lombok.NonNull;
 
@@ -42,12 +43,12 @@ import lombok.NonNull;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -201,8 +202,6 @@ public class PaymentAllocationRepository
 			@NonNull final ResultSet rs,
 			@NonNull final ZonedDateTime evaluationDate) throws SQLException
 	{
-		final boolean isPaid = false;  // assumed in above query where clause
-
 		final InvoiceId invoiceId;
 		final OrderId prepayOrderId;
 		final boolean isPrePayOrder = DisplayType.toBoolean(rs.getBoolean("isprepayorder")); // is Source document an order?
@@ -237,7 +236,7 @@ public class PaymentAllocationRepository
 		final BigDecimal multiplierCreditMemo = rs.getBigDecimal("multiplier"); // CreditMemo=-1, Regular Invoice=+1
 		final boolean isCreditMemo = multiplierCreditMemo.signum() < 0 || grandTotalConv.signum() < 0; // task 09429: also if grandTotal<0
 
-		if (!isPrePayOrder && grandTotal.signum() == 0 && !isPaid)
+		if (!isPrePayOrder && grandTotal.signum() == 0)
 		{
 			// nothing - i.e. allow zero amount invoices to be visible for allocation
 			// see: http://dewiki908/mediawiki/index.php/01955:_Fenster_Zahlung-Zuordnung_zeigt_Gutschrift_nicht_an_%282011080910000037%29
@@ -278,6 +277,95 @@ public class PaymentAllocationRepository
 		return amountBD != null
 				? Amount.of(amountBD, currencyCode)
 				: Amount.zero(currencyCode);
+	}
+
+	public List<PaymentToAllocate> retrievePaymentsToAllocate(final PaymentToAllocateQuery query)
+	{
+		final List<Object> sqlParams = new ArrayList<>();
+		final String sql = buildSelectPaymentsToAllocateSql(query, sqlParams);
+
+		return DB.retrieveRows(sql, sqlParams, this::retrievePaymentToAllocateOrNull);
+	}
+
+	private String buildSelectPaymentsToAllocateSql(final PaymentToAllocateQuery query, final List<Object> sqlParams)
+	{
+		final CurrencyId currencyId = query.getCurrencyId();
+		final boolean isMultiCurrency = currencyId == null;
+		final ClientAndOrgId clientAndOrgId = query.getClientAndOrgId();
+		final OrgId orgId = clientAndOrgId != null ? clientAndOrgId.getOrgId() : null;
+
+		final StringBuilder sql = new StringBuilder();
+
+		if (query.getBpartnerId() != null)
+		{
+			sql.append("SELECT * FROM GetOpenPayments(?, ?, ?, ?, ?, ?)");
+			sqlParams.addAll(Arrays.asList(
+					query.getBpartnerId(),
+					currencyId,
+					isMultiCurrency,
+					orgId,
+					query.getEvaluationDate(),
+					null // c_payment_id
+			));
+		}
+
+		//
+		// Add particular documents mentioned to be included
+		for (final PaymentId paymentId : query.getAdditionalPaymentIdsToInclude())
+		{
+			if (sql.length() > 0)
+			{
+				sql.append("\n UNION \n");
+			}
+			sql.append("SELECT * FROM GetOpenPayments(?, ?, ?, ?, ?, ?)");
+			sqlParams.addAll(Arrays.asList(
+					null, // don't filter by BPartner context.getC_BPartner_ID()
+					null, // currency
+					true, // multiCurrency
+					null, // org
+					query.getEvaluationDate(),
+					paymentId // c_payment_id
+			));
+		}
+
+		//
+		// Builder the final outer SQL:
+		sql.insert(0, "SELECT * FROM ( ").append(") p WHERE true");
+
+		if (clientAndOrgId != null)
+		{
+			sql.append(" AND p.AD_Client_ID = ?");
+			sqlParams.add(clientAndOrgId.getClientId());
+		}
+		sql.append(" ORDER BY p.paymentDate, p.DocNo");
+
+		return sql.toString();
+	}
+
+	private final PaymentToAllocate retrievePaymentToAllocateOrNull(final ResultSet rs) throws SQLException
+	{
+		// final CurrencyCode documentCurrencyCode = CurrencyCode.ofThreeLetterCode(rs.getString("iso_code"));
+		final CurrencyCode convertedToCurrencyCode = CurrencyCode.ofThreeLetterCode(rs.getString("ConvertTo_Currency_ISO_Code"));
+
+		final BigDecimal multiplierAP = rs.getBigDecimal("multiplierap"); // +1=Inbound Payment, -1=Outbound Payment
+		final boolean inboundPayment = multiplierAP.signum() > 0;
+
+		// NOTE: we assume the amounts were already AP adjusted, so we are converting them back to relative values (i.e. not AP adjusted)
+		// final Amount payAmt = retrieveAmount(rs, "orig_total", documentCurrencyCode).negateIf(!inboundPayment);
+		final Amount payAmtConv = retrieveAmount(rs, "conv_total", convertedToCurrencyCode).negateIf(!inboundPayment);
+		final Amount openAmtConv = retrieveAmount(rs, "conv_open", convertedToCurrencyCode).negateIf(!inboundPayment);
+
+		return PaymentToAllocate.builder()
+				.paymentId(PaymentId.ofRepoId(rs.getInt("c_payment_id")))
+				.clientAndOrgId(ClientAndOrgId.ofClientAndOrg(rs.getInt("AD_Client_ID"), rs.getInt("AD_Org_ID")))
+				.documentNo(rs.getString("DocNo"))
+				.bpartnerId(BPartnerId.ofRepoId(rs.getInt("c_bpartner_id")))
+				.dateTrx(TimeUtil.asLocalDate(rs.getTimestamp("paymentdate")))
+				.dateAcct(TimeUtil.asLocalDate(rs.getTimestamp("dateacct"))) // task 09643
+				.payAmt(payAmtConv)
+				.openAmt(openAmtConv)
+				.inboundPayment(inboundPayment)
+				.build();
 	}
 
 }
