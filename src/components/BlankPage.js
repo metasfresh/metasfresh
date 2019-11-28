05d285c01a504d3f75c483ca178919de26a548cb
siemiatj <kuba@saskla.cz>
2019-11-28 11:05:31 +0100

    - rewrite BlankPage to functional component

--- src/components/BlankPage.js
+++ src/components/BlankPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 /**
@@ -6,27 +6,12 @@ import PropTypes from 'prop-types';
  * @module BlankPage
  * @extends Component
  */
-class BlankPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  /**
-   * @method render
-   * @summary ToDo: Describe the method.
-   * @todo Write the documentation
-   */
-  render() {
-    const { what } = this.props;
-
-    return (
-      <div className="blank-page">
-        <h1>404</h1>
-        <h3>{what ? what + ' not found.' : 'Not found'}</h3>
-      </div>
-    );
-  }
-}
+const BlankPage = ({ what }) => (
+  <div className="blank-page">
+    <h1>404</h1>
+    <h3>{what ? what + ' not found.' : 'Not found'}</h3>
+  </div>
+);
 
 /**
  * @typedef {object} Props Component props
