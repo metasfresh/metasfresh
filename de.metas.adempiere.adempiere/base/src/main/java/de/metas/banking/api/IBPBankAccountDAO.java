174a3e9941f4c1e675ba40f245e6d0a96d97f20c
TheBestPessimist <cristian@tbp.land>
2019-12-03 14:22:42 +0200

    Add query for BankAccount by bpartner, currency, iban
    Add BankAccountId
    
    https://github.com/metasfresh/metasfresh/issues/5856

--- de.metas.adempiere.adempiere/base/src/main/java/de/metas/banking/api/IBPBankAccountDAO.java
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/banking/api/IBPBankAccountDAO.java
@@ -10,12 +10,12 @@ package de.metas.banking.api;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -23,18 +23,18 @@ package de.metas.banking.api;
  */
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
+import de.metas.bpartner.BPartnerId;
+import de.metas.money.CurrencyId;
+import lombok.NonNull;
 import org.compiere.model.I_C_BP_BankAccount;
 
 import de.metas.util.ISingletonService;
 
 /**
- * 
  * This interface must be moved in de.metas.banking after we get rid of org.compiere.model.MPaySelectionCheck !
- * 
- * @author metas-dev <dev@metasfresh.com>
- *
  */
 public interface IBPBankAccountDAO extends ISingletonService
 {
@@ -44,11 +44,8 @@ public interface IBPBankAccountDAO extends ISingletonService
 	 * Retrieve all the bank accounts of the currency <code>currencyID</code> for the partner <code> partnerID</code>
 	 * In case the currencyID is not set (<=0) just retrieve all accounts of the bpartner
 	 * The bank accounts will be ordered by their IsDefault values, with true first.
-	 * 
-	 * @param ctx
-	 * @param partner
-	 * @param currency
-	 * @return
 	 */
 	List<I_C_BP_BankAccount> retrieveBankAccountsForPartnerAndCurrency(Properties ctx, int partnerID, int currencyID);
+
+	Optional<BankAccountId> retrieveBankAccountByBPartnerAndCurrencyAndIBAN(@NonNull BPartnerId bPartnerId, @NonNull CurrencyId currencyId, @NonNull String iban);
 }
