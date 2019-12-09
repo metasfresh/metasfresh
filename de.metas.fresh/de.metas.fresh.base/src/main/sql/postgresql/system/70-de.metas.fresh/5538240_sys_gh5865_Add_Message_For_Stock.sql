6b2b1102b59bcd992e2268060097664977385ff8
Cristina Ghita <cristina.ghita@metasfresh.com>
2019-12-09 10:37:40 +0200

    #5865 Add message for unssuppoorted row type
    https://github.com/metasfresh/metasfresh/issues/5865
    #5865 Picklist not working: "Type not supported: UNALLOCABLE"

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5538240_sys_gh5865_Add_Message_For_Stock.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5538240_sys_gh5865_Add_Message_For_Stock.sql
@@ -59,3 +59,37 @@ UPDATE AD_Message SET MsgText='Es kann kein Bestand für diese Auftragszeile gef
 UPDATE AD_Message_Trl SET MsgText='No onhand qty can be found for this order line.',Updated=TO_TIMESTAMP('2019-12-06 19:10:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=544947
 ;
 
+
+
+
+-- 2019-12-09T08:21:10.226Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET Value='de.metas.ui.web.pickingV2.productsToPick.rows.ProductsToPickRowsService.UnAllocated_Type_Error',Updated=TO_TIMESTAMP('2019-12-09 10:21:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544947
+;
+
+
+
+
+
+-- 2019-12-09T08:33:30.330Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,544948,0,TO_TIMESTAMP('2019-12-09 10:33:30','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Typ row {0}  wird nicht unterstützt.','E',TO_TIMESTAMP('2019-12-09 10:33:30','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.ui.web.pickingV2.productsToPick.rows.ProductsToPickRowsService.TypeRow_NotSupported')
+;
+
+-- 2019-12-09T08:33:30.336Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Message_ID=544948 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2019-12-09T08:33:56.275Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Type row  {0}  is not supported.',Updated=TO_TIMESTAMP('2019-12-09 10:33:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=544948
+;
+
+-- 2019-12-09T08:33:59.934Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2019-12-09 10:33:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=544948
+;
+
+
+
