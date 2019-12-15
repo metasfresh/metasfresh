2f6b3dc9d526c5a2c539fcdac0e98d1c971a6e38
Teo Sarca <teo.sarca@metasfresh.com>
2019-12-15 10:38:21 +0200

    Create 5539030_sys_gh5924_RV_UnPosted_DateAcct_filter.sql
    https://github.com/metasfresh/metasfresh/issues/5924
    (cherry picked from commit e81f2ed26f57e18660b047819c6035b1194e2a8f)

---
+++ de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5539030_sys_gh5924_RV_UnPosted_DateAcct_filter.sql
@@ -0,0 +1,10 @@
+-- 2019-12-15T08:30:58.462Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=40,Updated=TO_TIMESTAMP('2019-12-15 10:30:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=12332
+;
+
+-- 2019-12-15T08:31:37.552Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsRangeFilter='Y',Updated=TO_TIMESTAMP('2019-12-15 10:31:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=12332
+;
+
