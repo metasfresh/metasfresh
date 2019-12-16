496dd78ac0a370790ca2cc0200fc78ca387e9ce2
TheBestPessimist <cristian@tbp.land>
2019-12-16 11:47:58 +0200

    Update process and trl
    https://github.com/metasfresh/metasfresh/issues/5885

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5538890_sys_gh5885_UpdateProcessClass.sql
@@ -0,0 +1,5 @@
+-- 2019-12-13T08:56:38.077Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Classname='de.metas.handlingunits.inout.process.M_Shipment_AddToTransportationOrderProcess',Updated=TO_TIMESTAMP('2019-12-13 10:56:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=541225
+;
+
