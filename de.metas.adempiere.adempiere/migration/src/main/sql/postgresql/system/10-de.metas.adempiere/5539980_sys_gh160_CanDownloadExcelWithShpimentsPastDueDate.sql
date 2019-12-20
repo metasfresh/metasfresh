f97ce646677540f3cabbd19bf282f50e81f39467
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2019-12-20 15:30:54 +0200

    #160 Can now download in excel shipment dispositions past their due date.

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5539980_sys_gh160_CanDownloadExcelWithShpimentsPastDueDate.sql
@@ -0,0 +1,86 @@
+DROP FUNCTION IF EXISTS shipmentDispositionExcelDownload(TIMESTAMP With Time Zone, numeric);
+
+CREATE OR REPLACE FUNCTION shipmentDispositionExcelDownload(
+    IN M_ShipmentSchedule_Deliverydate TIMESTAMP With Time Zone,
+	IN M_BPartner_ID numeric )
+
+  RETURNS TABLE
+  (
+    Deliverydate TIMESTAMP With Time Zone,
+    BPartnerid numeric,
+    BPdetails character varying,
+    Orderid numeric,
+    Documentno character varying,
+    Productname character varying ,
+    Productvalue character varying,
+    Qtyordered numeric,
+    Qtydelivered numeric
+  )
+   AS
+
+$BODY$
+
+SELECT
+    sps.deliverydate,
+    sps.c_bpartner_id,
+    CONCAT(bp.value, ' ', bp.name) as bpdetails,
+    o.c_order_id,
+    o.documentno,
+    pt.name,
+    pt.value,
+    sps.qtyordered_tu,
+    sps.qtydelivered
+
+FROM M_ShipmentSchedule sps
+    LEFT OUTER JOIN c_bpartner bp on bp.c_bpartner_id = sps.c_bpartner_id
+    LEFT OUTER JOIN c_order o on o.c_order_id = sps.c_order_id
+    LEFT OUTER JOIN m_product pt on pt.m_product_id = sps.m_product_id
+	WHERE M_ShipmentSchedule_Deliverydate >= sps.deliverydate
+	AND CASE WHEN M_BPartner_ID > 0 THEN bp.c_bpartner_id = M_BPartner_ID ELSE 1=1 END
+
+$BODY$
+  LANGUAGE sql STABLE
+  COST 100
+  ROWS 1000;
+
+
+-- 2019-12-20T12:29:14.962Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsOneInstanceOnly,IsReport,IsServerProcess,IsTranslateExcelHeaders,IsUseBPartnerLanguage,LockWaitTimeout,Name,RefreshAllAfterExecution,ShowHelp,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,541238,'Y','de.metas.impexp.excel.process.ExportToExcelProcess','N',TO_TIMESTAMP('2019-12-20 14:29:14','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N','N','N','N','N','Y','Y',0,'ShipmentDispositionExcelDownload','Y','N','Excel',TO_TIMESTAMP('2019-12-20 14:29:14','YYYY-MM-DD HH24:MI:SS'),100,'ShipmentDispositionExcelDownload')
+;
+
+-- 2019-12-20T12:29:14.966Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Process_ID=541238 AND NOT EXISTS (SELECT 1 FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- 2019-12-20T12:30:50.458Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,DefaultValue,EntityType,FieldLength,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,541376,0,541238,541658,16,'DeliveryDate',TO_TIMESTAMP('2019-12-20 14:30:50','YYYY-MM-DD HH24:MI:SS'),100,'@#Date@','D',0,'Y','N','Y','N','N','N','Lieferdatum',10,TO_TIMESTAMP('2019-12-20 14:30:50','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-12-20T12:30:50.459Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Process_Para_ID=541658 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2019-12-20T12:33:14.344Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Reference_Value_ID,ColumnName,Created,CreatedBy,DefaultValue,Description,EntityType,FieldLength,Help,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,187,0,541238,541659,30,138,'C_BPartner_ID',TO_TIMESTAMP('2019-12-20 14:33:14','YYYY-MM-DD HH24:MI:SS'),100,'@C_BPartner_ID/-1@','Bezeichnet einen GeschÃ¤ftspartner','U',0,'Ein GeschÃ¤ftspartner ist jemand, mit dem Sie interagieren. Dies kann Lieferanten, Kunden, Mitarbeiter oder Handelsvertreter umfassen.','Y','Y','Y','N','N','N','GeschÃ¤ftspartner',20,TO_TIMESTAMP('2019-12-20 14:33:14','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2019-12-20T12:33:14.345Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Process_Para_ID=541659 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2019-12-20T12:33:59.779Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,AD_Table_Process_ID,AD_Window_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_DocumentAction,WEBUI_IncludedTabTopAction,WEBUI_ViewAction,WEBUI_ViewQuickAction,WEBUI_ViewQuickAction_Default) VALUES (0,0,541238,500221,540772,500221,TO_TIMESTAMP('2019-12-20 14:33:59','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2019-12-20 14:33:59','YYYY-MM-DD HH24:MI:SS'),100,'Y','Y','Y','N','N')
+;
+
+-- 2019-12-20T12:34:42.306Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='select * from shipmentdispositionexceldownload(''@DeliveryDate@'', @C_BPartner_ID/-1@)',Updated=TO_TIMESTAMP('2019-12-20 14:34:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=541238
+;
+
