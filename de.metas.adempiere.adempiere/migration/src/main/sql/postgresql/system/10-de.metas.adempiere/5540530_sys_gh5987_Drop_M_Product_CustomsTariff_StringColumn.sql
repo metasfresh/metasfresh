97157d8faedcb3ef5ad3b81f2487226bffed8033
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-01-07 15:12:42 +0200

    #5987 Delete CustomsTariff string column from M_Product
    -  it was replaced by C_CustomsTarrif_ID
    https://github.com/metasfresh/metasfresh/issues/5987

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5540530_sys_gh5987_Drop_M_Product_CustomsTariff_StringColumn.sql
@@ -0,0 +1,48 @@
+-- 2020-01-07T09:31:00.107Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=582016
+;
+
+-- 2020-01-07T09:31:00.115Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Field_Trl WHERE AD_Field_ID=582016
+;
+
+-- 2020-01-07T09:31:00.121Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Field WHERE AD_Field_ID=582016
+;
+
+-- 2020-01-07T09:31:00.107Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID in ( select ad_field_ID from ad_field where AD_Column_ID=568324)
+;
+
+-- 2020-01-07T09:31:00.115Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Field_Trl WHERE AD_Field_ID in ( select ad_field_ID from ad_field where AD_Column_ID=568324)
+;
+
+-- 2020-01-07T09:31:00.121Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Field WHERE AD_Field_ID in ( select ad_field_ID from ad_field where AD_Column_ID=568324)
+;
+
+-- 2020-01-07T09:33:19.691Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Column_Trl WHERE AD_Column_ID=568324
+;
+
+-- 2020-01-07T09:33:19.707Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=568324
+;
+
+ALTER TABLE M_Product DROP COLUMN CustomsTariff;-- 2020-01-07T09:40:32.828Z
+
+
+
+
+
+
+
