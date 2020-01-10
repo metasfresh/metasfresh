5ab22e066ceb4ae135a9f51818f6f0919f7b0d29
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-01-10 10:59:03 +0200

    #5895 Added ifcolumnschanged conditions to the @ModelChange annotation.
    (cherry picked from commit 49a24fee829d1ff0b849e5f2359d3f939ea191d9)

--- de.metas.business/src/main/java/de/metas/pricing/interceptor/M_ProductPrice.java
+++ de.metas.business/src/main/java/de/metas/pricing/interceptor/M_ProductPrice.java
@@ -57,7 +57,7 @@ public class M_ProductPrice
 		ProductPrices.assertMainProductPriceIsNotDuplicate(productPrice);
 	}
 
-	@ModelChange(timings = { ModelValidator.TYPE_AFTER_NEW, ModelValidator.TYPE_AFTER_CHANGE }, ifColumnsChanged = { I_M_ProductPrice.COLUMNNAME_C_UOM_ID })
+	@ModelChange(timings = { ModelValidator.TYPE_AFTER_NEW, ModelValidator.TYPE_AFTER_CHANGE }, ifColumnsChanged = { I_M_ProductPrice.COLUMNNAME_C_UOM_ID, I_M_ProductPrice.COLUMNNAME_IsInvalidPrice, I_M_ProductPrice.COLUMNNAME_IsActive })
 	public void assertUomConversionExists(@NonNull final I_M_ProductPrice productPrice)
 	{
 		ProductPrices.assertUomConversionExists(productPrice);
