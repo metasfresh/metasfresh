1b47731e5bc06bee7a2372e932e5a4d1e8653e46
Teo Sarca <teo.sarca@metasfresh.com>
2020-01-15 17:56:18 +0200

    measure and log how much each model interceptor took (#6050)
    https://github.com/metasfresh/metasfresh/issues/6001

--- de.metas.adempiere.adempiere/base/src/test/java/org/adempiere/ad/modelvalidator/ModelChangeTypeTest.java
+++ de.metas.adempiere.adempiere/base/src/test/java/org/adempiere/ad/modelvalidator/ModelChangeTypeTest.java
@@ -8,49 +8,41 @@ import org.junit.jupiter.api.Test;
  * #%L
  * de.metas.adempiere.adempiere.base
  * %%
- * Copyright (C) 2016 metas GmbH
+ * Copyright (C) 2020 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-public class DocTimingTypeTest
+public class ModelChangeTypeTest
 {
 	@Test
-	public void test_valueOfTimingInt()
+	public void test_valueOf()
 	{
-		for (final DocTimingType timing : DocTimingType.values())
+		for (final ModelChangeType changeType : ModelChangeType.values())
 		{
-			assertThat(DocTimingType.valueOf(timing.toInt())).isSameAs(timing);
+			assertThat(ModelChangeType.valueOf(changeType.toInt())).isSameAs(changeType);
 		}
 	}
 
 	@Test
-	public void test_forAction()
+	public void test_isBeforeSaveTrx()
 	{
-		for (final DocTimingType timing : DocTimingType.values())
-		{
-			assertThat(DocTimingType.forAction(timing.getDocAction(), timing.getBeforeAfter())).isSameAs(timing);
-		}
-	}
+		assertThat(ModelChangeType.isBeforeSaveTrx(ModelChangeType.AFTER_NEW)).isFalse();
+		assertThat(ModelChangeType.isBeforeSaveTrx(ModelChangeType.BEFORE_SAVE_TRX)).isTrue();
 
-	public void test_isDocAction()
-	{
-		for (final DocTimingType timing : DocTimingType.values())
-		{
-			assertThat(timing.isDocAction(timing.getDocAction())).isTrue();
-		}
+		assertThat(ModelChangeType.isBeforeSaveTrx(DocTimingType.BEFORE_COMPLETE)).isFalse();
 	}
 }
