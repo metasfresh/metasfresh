78e4cb9f5463cf6340f64c16ff412b11dd42775c
TheBestPessimist <cristian@tbp.land>
2020-01-15 11:38:23 +0200

    Report Average Product Prices: Negate in case of Credit Memo (#6042)
    * Indentation and casing
    
    https://github.com/metasfresh/metasfresh/issues/6030
    
    * Replace the CASE clause with a single currencyConvert, as the calculation there was duplicated
    
    https://github.com/metasfresh/metasfresh/issues/6030
    
    * Negate `il.PriceActual` when the invoice has doctype Credit Memo
    
    https://github.com/metasfresh/metasfresh/issues/6030
    (cherry picked from commit 4f97460d706575dc6082686c3de58822d26d26f1)
    
    solved Conflicts:
    	de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Average_Product_Prices.sql

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5541520_sys_gh6030_report_Average_Product_Prices_negate_il.PriceActual_WhenTheInvoiceHasDoctype_CreditMemo.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5541520_sys_gh6030_report_Average_Product_Prices_negate_il.PriceActual_WhenTheInvoiceHasDoctype_CreditMemo.sql
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.fresh.base
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 DROP FUNCTION de_metas_endcustomer_fresh_reports.average_product_prices(date, date, character);
 
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.average_product_prices(IN p_datefrom date, IN p_dateto date, IN p_issotrx character)
@@ -141,7 +163,7 @@ FROM C_Invoice_Candidate ic
 
          LEFT OUTER JOIN C_UOM_Conversion uconv
                          ON
-                                 uconv.C_UOM_ID = iol.C_UOM_ID
+                                     uconv.C_UOM_ID = iol.C_UOM_ID
                                  AND uconv.C_UOM_To_ID = price_uom.C_UOM_ID
                                  AND p.M_Product_ID = uconv.M_Product_ID
 
