07e08864085e9c0136838ed2dc88fab554d11fb2
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2020-01-27 13:02:30 +0100

    Shipment cannot be created with qtytodeliveroverride = 0/ NULL (#6088)
    * minor improvements: rename private field; add log-msg; add javadoc
    
    * 6087-app Shipment cannot be created with qtytodeliveroverride = 0/ NULL
    
    be lenient and don't try to on-the-fly-pick but log it if qtyToDelivere is zero
    #6087

--- de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/ModelValidationEngine.java
+++ de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/ModelValidationEngine.java
@@ -101,8 +101,6 @@ public class ModelValidationEngine implements IModelValidationEngine
 
 	/**
 	 * Get Singleton
-	 *
-	 * @return engine
 	 */
 	public synchronized static ModelValidationEngine get()
 	{
@@ -114,7 +112,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 		}
 		if (State.TO_BE_INITALIZED.equals(state))
 		{
-			log.info("Start initializing ModelValidationEngine");
+			logger.info("Start initializing ModelValidationEngine");
 
 			state = State.INITIALIZING;
 
@@ -124,7 +122,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 
 			state = State.INITIALIZED;
 
-			log.info("Done initializing ModelValidationEngine; it took {}; m_globalValidators.size={}; m_validators.size={}",
+			logger.info("Done initializing ModelValidationEngine; it took {}; m_globalValidators.size={}; m_validators.size={}",
 					stopwatch, s_engine.m_globalValidators.size(), s_engine.m_validators.size());
 		}
 		return s_engine;
@@ -151,11 +149,11 @@ public class ModelValidationEngine implements IModelValidationEngine
 		if (s_engine != null)
 		{
 			final AdempiereException ex = new AdempiereException("Setting InitEntityTypes after the model validation engine was initialized has no effect.");
-			log.warn(ex.getLocalizedMessage(), ex);
+			logger.warn(ex.getLocalizedMessage(), ex);
 		}
 
 		_initEntityTypes = ImmutableList.copyOf(initEntityTypes);
-		log.info("IMPORTANT: Only following ADempiere modules will be activated: " + _initEntityTypes);
+		logger.info("IMPORTANT: Only following ADempiere modules will be activated: " + _initEntityTypes);
 	}
 
 	/** @return entity types which shall be initialized or <code>null</code> if ALL of them shall be initialized */
@@ -206,7 +204,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			{
 				if (!moduleActivatorDescriptor.isActive())
 				{
-					log.info("Skip {} (not active)", moduleActivatorDescriptor);
+					logger.info("Skip {} (not active)", moduleActivatorDescriptor);
 					continue;
 				}
 
@@ -215,7 +213,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				final String entityType = moduleActivatorDescriptor.getEntityType();
 				if (!EntityTypesCache.instance.isActive(entityType))
 				{
-					log.info("Skip {} (entityType is not active)", moduleActivatorDescriptor);
+					logger.info("Skip {} (entityType is not active)", moduleActivatorDescriptor);
 					continue;
 				}
 
@@ -223,7 +221,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				// Skip model validator if entity type is not in list of allowed entity types (task 03023)
 				if (initEntityTypes != null && !initEntityTypes.contains(entityType))
 				{
-					log.info("Skip {} (entityType not in initEntityTypes=" + initEntityTypes + ")", moduleActivatorDescriptor);
+					logger.info("Skip {} (entityType not in initEntityTypes=" + initEntityTypes + ")", moduleActivatorDescriptor);
 					continue;
 				}
 
@@ -242,7 +240,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				}
 				else
 				{
-					log.warn("More than one spring interceptors found for module activator class '{}'. Ignoring then and will try to create a new instance. -- {}", moduleActivatorClassname, existingSpringInstances);
+					logger.warn("More than one spring interceptors found for module activator class '{}'. Ignoring then and will try to create a new instance. -- {}", moduleActivatorClassname, existingSpringInstances);
 					existingSpringInstance = null;
 				}
 
@@ -251,7 +249,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			currentClassName = null;
 
 			stopwatch.stop();
-			log.debug("Done initializing database registered interceptors; it took {}", stopwatch);
+			logger.debug("Done initializing database registered interceptors; it took {}", stopwatch);
 
 			//
 			// Register from Spring context
@@ -262,7 +260,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				currentClassName = springInterceptor.getClass().getName();
 				if (moduleActivatorDescriptors.containsClassname(currentClassName))
 				{
-					log.debug("Skip {} because was already considered", springInterceptor);
+					logger.debug("Skip {} because was already considered", springInterceptor);
 					continue;
 				}
 
@@ -270,7 +268,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			}
 			currentClassName = null;
 
-			log.debug("Done initializing spring based interceptors; it took {}", stopwatch);
+			logger.debug("Done initializing spring based interceptors; it took {}", stopwatch);
 		}
 		catch (final Exception ex)
 		{
@@ -428,7 +426,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 	}
 
 	/** Logger */
-	private static final Logger log = LogManager.getLogger(ModelValidationEngine.class);
+	private static final Logger logger = LogManager.getLogger(ModelValidationEngine.class);
 	// /** Change Support */
 	// private VetoableChangeSupport m_changeSupport = new VetoableChangeSupport(this);
 
@@ -512,7 +510,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				}
 				catch (final Exception e)
 				{
-					log.warn("Failed executing login script for {}", loginRule, e);
+					logger.warn("Failed executing login script for {}", loginRule, e);
 					final String error = AdempiereException.extractMessage(e);
 					return error;
 				}
@@ -578,7 +576,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			}
 			catch (Exception e)
 			{
-				log.error(e.getLocalizedMessage(), e);
+				logger.error(e.getLocalizedMessage(), e);
 			}
 		}
 	}
@@ -595,7 +593,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			}
 			catch (Exception e)
 			{
-				log.error(e.getLocalizedMessage(), e);
+				logger.error(e.getLocalizedMessage(), e);
 			}
 		}
 	}
@@ -634,7 +632,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			}
 			else
 			{
-				log.debug("Listener " + listener + " already added for " + propertyName);
+				logger.debug("Listener " + listener + " already added for " + propertyName);
 			}
 		}
 	}	// addModelValidator
@@ -760,7 +758,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 		final String trxName = po.get_TrxName();
 		executeInTrx(trxName, changeType, () -> fireModelChange0(po, changeType, interceptorsSystem, interceptorsClient, scriptValidators));
 
-		log.trace("Executed in {}: ALL {} {} interceptors for {}", stopwatch, countInterceptors, changeType, po);
+		logger.trace("Executed in {}: ALL {} {} interceptors for {}", stopwatch, countInterceptors, changeType, po);
 	}	// fireModelChange
 
 	private final void executeInTrx(final String trxName, final TimingType changeTypeOrDocTiming, @NonNull final Runnable runnable)
@@ -905,7 +903,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 						+ "\n PO: " + model
 						+ "\n Event: " + timingType
 						+ "\n trxName: " + trxName);
-				log.warn(ex.getLocalizedMessage(), ex);
+				logger.warn(ex.getLocalizedMessage(), ex);
 			}
 		}
 	}
@@ -929,7 +927,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 	{
 		if (!appliesFor(validator, po.getAD_Client_ID()))
 		{
-			log.trace("Skip {} ({}) for {}", validator, changeType, po);
+			logger.trace("Skip {} ({}) for {}", validator, changeType, po);
 			return;
 		}
 
@@ -950,11 +948,11 @@ public class ModelValidationEngine implements IModelValidationEngine
 					throw new AdempiereException(error);
 				}
 
-				log.trace("Executed in {}: {} ({}) for {}", stopwatch, validator, changeType, po);
+				logger.trace("Executed in {}: {} ({}) for {}", stopwatch, validator, changeType, po);
 			}
 			catch (final Exception ex)
 			{
-				log.trace("Failed executing in {}: {} ({}) for {}", stopwatch, validator, changeType, po, ex);
+				logger.trace("Failed executing in {}: {} ({}) for {}", stopwatch, validator, changeType, po, ex);
 				throw AdempiereException.wrapIfNeeded(ex);
 			}
 		}
@@ -1065,11 +1063,11 @@ public class ModelValidationEngine implements IModelValidationEngine
 		final DocTimingType docTiming = DocTimingType.valueOf(docTimingInt);
 		return fireDocValidate(model, docTiming);
 	}
-	
+
 	public String fireDocValidate(final Object model, final DocTimingType docTiming)
 	{
 		final Stopwatch stopwatch = Stopwatch.createStarted();
-		
+
 		if (model == null)
 		{
 			return null; // avoid InterfaceWrapperHelper from throwing an exception under any circumstances
@@ -1133,7 +1131,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 		final String trxName = po.get_TrxName();
 		executeInTrx(trxName, docTiming, () -> fireDocValidate0(po, docTiming, interceptorsSystem, interceptorsClient, scriptValidators));
 
-		log.trace("Executed in {}: ALL {} {} interceptors for {}", stopwatch, countInterceptors, docTiming, po);
+		logger.trace("Executed in {}: ALL {} {} interceptors for {}", stopwatch, countInterceptors, docTiming, po);
 
 		return null;
 	}	// fireDocValidate
@@ -1185,7 +1183,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 	{
 		if (!appliesFor(interceptor, po.getAD_Client_ID()))
 		{
-			log.trace("Skip {} ({}) for {}", interceptor, docTiming, po);
+			logger.trace("Skip {} ({}) for {}", interceptor, docTiming, po);
 			return;
 		}
 
@@ -1198,11 +1196,11 @@ public class ModelValidationEngine implements IModelValidationEngine
 				throw new AdempiereException(error);
 			}
 
-			log.trace("Executed in {}: {} ({}) for {}", stopwatch, interceptor, docTiming, po);
+			logger.trace("Executed in {}: {} ({}) for {}", stopwatch, interceptor, docTiming, po);
 		}
 		catch (final Exception ex)
 		{
-			log.trace("Failed executing in {}: {} ({}) for {}", stopwatch, interceptor, docTiming, po, ex);
+			logger.trace("Failed executing in {}: {} ({}) for {}", stopwatch, interceptor, docTiming, po, ex);
 			throw AdempiereException.wrapIfNeeded(ex);
 		}
 	}
@@ -1352,7 +1350,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				}
 				catch (Exception e)
 				{
-					log.warn("" + validator + ": " + e.getLocalizedMessage());
+					logger.warn("" + validator + ": " + e.getLocalizedMessage());
 				}
 			}
 		}
@@ -1388,7 +1386,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 				}
 				catch (Exception e)
 				{
-					log.warn("" + validator + ": " + e.getLocalizedMessage());
+					logger.warn("" + validator + ": " + e.getLocalizedMessage());
 				}
 			}
 		}
@@ -1448,7 +1446,7 @@ public class ModelValidationEngine implements IModelValidationEngine
 			final IModelInterceptor annotatedInterceptor = AnnotatedModelInterceptorFactory.get().createModelInterceptor(validator);
 			if (annotatedInterceptor == null)
 			{
-				log.warn("No pointcuts found for model validator: " + validator + " [SKIP]");
+				logger.warn("No pointcuts found for model validator: " + validator + " [SKIP]");
 			}
 			else
 			{
