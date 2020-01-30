eab6f22078dc9a030137ff0e1bc882a9bac9ae8d
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-01-30 15:47:53 +0100

    Support: Product cannot be invoiced, why?
    * The bug happend when invoicableQtyBaseOn=CatchWeight and invoiceRule=Immediate and QtyToInvoiceOverride<QtyOrdered
    * Now it's fixed in InvoiceCandidate
    * Also add logging to the places that this time i had to remote-debug
    * Also change "=>" to "->" in various log messages; proposing a common practice there
    https://github.com/metasfresh/metasfresh/issues/6104

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/InvoiceCandInvalidUpdater.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/InvoiceCandInvalidUpdater.java
@@ -38,6 +38,7 @@ import de.metas.invoicecandidate.model.I_M_InOutLine;
 import de.metas.invoicecandidate.spi.IInvoiceCandidateHandler.PriceAndTax;
 import de.metas.lock.api.ILock;
 import de.metas.logging.LogManager;
+import de.metas.logging.TableRecordMDC;
 import de.metas.util.Check;
 import de.metas.util.Loggables;
 import de.metas.util.Services;
@@ -55,6 +56,7 @@ import org.adempiere.util.lang.IAutoCloseable;
 import org.adempiere.util.lang.IContextAware;
 import org.compiere.SpringContextHolder;
 import org.slf4j.Logger;
+import org.slf4j.MDC.MDCCloseable;
 
 import javax.annotation.Nullable;
 import java.math.BigDecimal;
@@ -163,7 +165,7 @@ import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
 		final ICUpdateResult result = new ICUpdateResult();
 		try (final IAutoCloseable updateInProgressCloseable = invoiceCandBL.setUpdateProcessInProgress())
 		{
-			trxItemProcessorExecutorService.<I_C_Invoice_Candidate, ICUpdateResult>createExecutor()
+			trxItemProcessorExecutorService.<I_C_Invoice_Candidate, ICUpdateResult> createExecutor()
 					.setContext(getCtx(), getTrxName()) // if called from process or wp-processor then getTrxName() is null because *we* want to manage the trx => commit after each chunk
 					.setItemsPerBatch(itemsPerBatch)
 
@@ -184,18 +186,21 @@ import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
 						@Override
 						public void process(final I_C_Invoice_Candidate ic)
 						{
-							chunkInvoiceCandidateIds.add(ic.getC_Invoice_Candidate_ID());
+							try (final MDCCloseable icRecordMDC = TableRecordMDC.putTableRecordReference(ic))
+							{
+								chunkInvoiceCandidateIds.add(ic.getC_Invoice_Candidate_ID());
 
-							updateInvalid(ic);
-							if (!ic.isError())
-							{
-								result.addInvoiceCandidate(ic);
-							}
-							else
-							{
-								Loggables.withLogger(logger, Level.DEBUG)
-										.addLog("Error processing invoice; ic.errorMessage={}; ic={}", ic.getErrorMsg(), ic);
-								result.incrementErrorsCount();
+								updateInvalid(ic);
+								if (!ic.isError())
+								{
+									result.addInvoiceCandidate(ic);
+								}
+								else
+								{
+									Loggables.withLogger(logger, Level.DEBUG)
+											.addLog("Error processing invoice; ic.errorMessage={}; ic={}", ic.getErrorMsg(), ic);
+									result.incrementErrorsCount();
+								}
 							}
 						}
 
@@ -239,7 +244,7 @@ import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
 		Loggables.addLog("Update invalid result: {}", result.getSummary());
 	}
 
-	private void updateInvalid(final I_C_Invoice_Candidate icRecord)
+	private void updateInvalid(@NonNull final I_C_Invoice_Candidate icRecord)
 	{
 		final Properties ctx = InterfaceWrapperHelper.getCtx(icRecord);
 
