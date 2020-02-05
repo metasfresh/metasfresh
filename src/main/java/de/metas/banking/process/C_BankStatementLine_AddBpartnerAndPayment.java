4be8f09e8ff5e235025a5c6b3c63283c99b1e361
TheBestPessimist <cristian@tbp.land>
2020-02-05 14:18:57 +0200

    Bank Statement Import directly in Bank Statement window (#1348)
    * Ping teo for more work
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Add first draft of C_BankStatement_AllocateInvoice process and sql
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Refactoring
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Extract defaultInvoiceLookupProvider to own service
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Make `LookupDataSource.findByIdsOrdered` stable
    
    It respects the order of the items in the input collection
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Create the payment with dates = statementLineDate
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Exclude payment if it is reconciled from allocating to a BankStatementLine
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Refactoring: Extract the logic regarding Payment selection and allocation from process to separate classes
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Refactoring: dont use list if we only expect a single param
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Add first test draft; refactor methods for easier testing
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Test refactoring
    Add test for Outbound BankStatementLine
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Handle the case where no Invoice is selected; disable old case for multiple invoices until clarification with mark
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Refactor test
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Add test for BankStatement completion
    
    Refactor classes around
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Add process `C_BankStatement_AddBpartnerAndPayment`
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Simplify `paymentLookupProvider`
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Seems like I cant `Just DO IT!`. It was too good to be true anyway :(
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Refactor: Use Money instead of BigDecimal/Currency pair
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * nullcheck
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Add process C_BankStatementLine_AllocatePayment
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Process refresh bugfix
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Update trl
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Rename process
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Move method to `IBankStatmentPaymentBL` from `BankStatementBL`
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Bugfix: forgot to save bpartner
    
    https://github.com/metasfresh/metasfresh/issues/6061
    
    * Throw exception if no b Bank Account exists
    
    https://github.com/metasfresh/metasfresh/issues/6061

---
+++ src/main/java/de/metas/banking/process/C_BankStatementLine_AddBpartnerAndPayment.java
@@ -0,0 +1,159 @@
+/*
+ * #%L
+ * metasfresh-webui-api
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.banking.process;
+
+import com.google.common.collect.ImmutableSet;
+import de.metas.banking.api.BankAccountId;
+import de.metas.banking.api.IBPBankAccountDAO;
+import de.metas.banking.model.I_C_BankStatement;
+import de.metas.banking.model.I_C_BankStatementLine;
+import de.metas.banking.payment.IBankStatmentPaymentBL;
+import de.metas.banking.service.IBankStatementDAO;
+import de.metas.bpartner.BPartnerId;
+import de.metas.document.engine.DocStatus;
+import de.metas.i18n.IMsgBL;
+import de.metas.money.CurrencyId;
+import de.metas.money.Money;
+import de.metas.payment.PaymentId;
+import de.metas.payment.api.IPaymentDAO;
+import de.metas.process.IProcessPrecondition;
+import de.metas.process.IProcessPreconditionsContext;
+import de.metas.process.JavaProcess;
+import de.metas.process.Param;
+import de.metas.process.ProcessPreconditionsResolution;
+import de.metas.ui.web.process.descriptor.ProcessParamLookupValuesProvider;
+import de.metas.ui.web.window.datatypes.LookupValuesList;
+import de.metas.ui.web.window.descriptor.DocumentLayoutElementFieldDescriptor;
+import de.metas.ui.web.window.model.lookup.LookupDataSourceFactory;
+import de.metas.util.Services;
+import lombok.NonNull;
+import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.util.lang.impl.TableRecordReference;
+import org.compiere.model.I_C_Payment;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+
+public class C_BankStatementLine_AddBpartnerAndPayment extends JavaProcess implements IProcessPrecondition
+{
+	public static final String BANK_STATEMENT_MUST_BE_COMPLETED_OR_IN_PROGRESS_MSG = "de.metas.banking.process.C_BankStatement_AddBpartnerAndPayment.BankStatement_must_be_Completed_or_In_Progress";
+	public static final String A_SINGLE_LINE_SHOULD_BE_SELECTED_MSG = "de.metas.banking.process.C_BankStatement_AddBpartnerAndPayment.A_single_line_should_be_selected";
+	public static final String LINE_SHOULD_NOT_HAVE_A_PAYMENT_MSG = "de.metas.banking.process.C_BankStatement_AddBpartnerAndPayment.Line_should_not_have_a_Payment";
+
+	private static final String C_BPartner_ID_PARAM_NAME = "C_BPartner_ID";
+	@Param(parameterName = C_BPartner_ID_PARAM_NAME)
+	private BPartnerId bPartnerId;
+
+	private static final String C_Payment_ID_PARAM_NAME = "C_Payment_ID";
+	@Param(parameterName = C_Payment_ID_PARAM_NAME)
+	private PaymentId paymentId;
+
+	private final IMsgBL iMsgBL = Services.get(IMsgBL.class);
+	private final IBankStatementDAO bankStatementDAO = Services.get(IBankStatementDAO.class);
+
+	@Override
+	public ProcessPreconditionsResolution checkPreconditionsApplicable(@NonNull final IProcessPreconditionsContext context)
+	{
+		if (context.isNoSelection())
+		{
+			return ProcessPreconditionsResolution.rejectBecauseNoSelection();
+		}
+
+		if (!context.isSingleSelection())
+		{
+			return ProcessPreconditionsResolution.rejectBecauseNotSingleSelection();
+		}
+
+		final I_C_BankStatement selectedBankStatement = context.getSelectedModel(I_C_BankStatement.class);
+		final DocStatus docStatus = DocStatus.ofCode(selectedBankStatement.getDocStatus());
+		if (!docStatus.isCompleted() && !docStatus.isDraftedOrInProgress())
+		{
+			return ProcessPreconditionsResolution.reject(iMsgBL.getTranslatableMsgText(BANK_STATEMENT_MUST_BE_COMPLETED_OR_IN_PROGRESS_MSG));
+		}
+
+		// there should be a single line selected
+		final Set<TableRecordReference> selectedLineReferences = context.getSelectedIncludedRecords();
+		if (selectedLineReferences.size() != 1)
+		{
+			return ProcessPreconditionsResolution.reject(iMsgBL.getTranslatableMsgText(A_SINGLE_LINE_SHOULD_BE_SELECTED_MSG));
+		}
+
+		final TableRecordReference reference = selectedLineReferences.iterator().next();
+		final I_C_BankStatementLine line = reference.getModel(I_C_BankStatementLine.class);
+		if (line == null)
+		{
+			return ProcessPreconditionsResolution.reject(iMsgBL.getTranslatableMsgText(A_SINGLE_LINE_SHOULD_BE_SELECTED_MSG));
+		}
+
+		if (line.getC_Payment_ID() > 0)
+		{
+			return ProcessPreconditionsResolution.reject(iMsgBL.getTranslatableMsgText(LINE_SHOULD_NOT_HAVE_A_PAYMENT_MSG));
+		}
+
+		return ProcessPreconditionsResolution.accept();
+	}
+
+	@ProcessParamLookupValuesProvider(parameterName = C_Payment_ID_PARAM_NAME, numericKey = true, lookupSource = DocumentLayoutElementFieldDescriptor.LookupSource.lookup, lookupTableName = I_C_Payment.Table_Name)
+	private LookupValuesList paymentLookupProvider()
+	{
+		final I_C_BankStatementLine line = getSelectedBankStatementLine();
+		final CurrencyId currencyId = CurrencyId.ofRepoId(line.getC_Currency_ID());
+		final boolean isReceipt = line.getStmtAmt().signum() >= 0;
+		final BigDecimal paymentAmount = isReceipt ? line.getStmtAmt() : line.getStmtAmt().negate();
+		final Money money = Money.of(paymentAmount, currencyId);
+
+		final ImmutableSet<PaymentId> paymentIds = Services.get(IPaymentDAO.class).retrieveAllMatchingPayments(isReceipt, bPartnerId, money);
+
+		return LookupDataSourceFactory.instance.searchInTableLookup(I_C_Payment.Table_Name).findByIdsOrdered(paymentIds);
+	}
+
+	@Override
+	protected String doIt() throws Exception
+	{
+		doIt(getSelectedBankStatementLine(), paymentId, bPartnerId);
+
+		return MSG_OK;
+	}
+
+	private void doIt(final I_C_BankStatementLine bankStatementLine, final PaymentId paymentId, final BPartnerId bPartnerId)
+	{
+		bankStatementLine.setC_BPartner_ID(bPartnerId.getRepoId());
+		final IBankStatmentPaymentBL bankStatementPaymentBL = Services.get(IBankStatmentPaymentBL.class);
+
+		final CurrencyId currencyId = CurrencyId.ofRepoId(bankStatementLine.getC_Currency_ID());
+		final Optional<BankAccountId> bankAccountIdOptional = Services.get(IBPBankAccountDAO.class).retrieveFirstIdByBPartnerAndCurrency(bPartnerId, currencyId);
+		if (!bankAccountIdOptional.isPresent())
+		{
+			throw new AdempiereException("@NotFound@ @C_BP_BankAccount_ID@");
+		}
+
+		bankStatementPaymentBL.setOrCreateAndLinkPaymentToBankStatementLine(bankStatementLine, paymentId);
+	}
+
+	private I_C_BankStatementLine getSelectedBankStatementLine()
+	{
+		final Integer lineId = getSelectedIncludedRecordIds(I_C_BankStatementLine.class).iterator().next();
+		return bankStatementDAO.getLineById(lineId);
+	}
+}
