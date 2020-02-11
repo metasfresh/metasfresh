5c8a2eb83aa47cfa24c2b99607dfd7537c1a7405
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-02-11 12:45:58 +0100

    5816-app - Effort based invoicing for service companies
    * attempts to fix SQL error
    #5816

--- de.metas.serviceprovider/src/main/sql/postgresql/system/45-de.metas.serviceprovider/5551510_sys_gh5816-app_additional_tables.sql
+++ de.metas.serviceprovider/src/main/sql/postgresql/system/45-de.metas.serviceprovider/5551510_sys_gh5816-app_additional_tables.sql
@@ -8,11 +8,6 @@ UPDATE AD_Table SET Name='S_InternalEffort_Issue', TableName='S_InternalEffort_I
 UPDATE AD_Sequence SET Name='S_InternalEffort_Issue',Updated=TO_TIMESTAMP('2020-02-10 07:13:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Sequence_ID=555084
 ;
 
--- 2020-02-10T06:13:27.658Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-ALTER SEQUENCE S_Issue_SEQ RENAME TO S_InternalEffort_Issue_SEQ
-;
-
 -- 2020-02-10T06:13:41.853Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Element SET ColumnName='S_InternalEffort_Issue_ID',Updated=TO_TIMESTAMP('2020-02-10 07:13:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=577376
