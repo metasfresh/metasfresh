d08fac02f39ada432f0f82e216c69c4607e0086f
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-02-12 15:40:44 +0200

    #6174 old tests refactoring and cleanup
    https://github.com/metasfresh/metasfresh/issues/6174

--- de.metas.swat/de.metas.swat.base/src/test/java/de/metas/tourplanning/api/impl/DeliveryDayBLTest.java
+++ de.metas.swat/de.metas.swat.base/src/test/java/de/metas/tourplanning/api/impl/DeliveryDayBLTest.java
@@ -1,18 +1,19 @@
 package de.metas.tourplanning.api.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
-import de.metas.tourplanning.model.TourId;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.lang.IContextAware;
 import org.adempiere.util.lang.ImmutablePair;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import de.metas.bpartner.BPartnerLocationId;
 import de.metas.lang.SOTrx;
 import de.metas.tourplanning.TourPlanningTestBase;
+import de.metas.tourplanning.model.TourId;
 
 /*
  * #%L
@@ -24,12 +25,12 @@ import de.metas.tourplanning.TourPlanningTestBase;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -55,21 +56,21 @@ public class DeliveryDayBLTest extends TourPlanningTestBase
 		final IContextAware ctx = InterfaceWrapperHelper.getContextAware(tour);
 		final BPartnerLocationId bpartnerLocationId = BPartnerLocationId.ofRepoId(bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
 
-		createDeliveryDay("27.03.2017 11:30:00.000", 4);
-		createDeliveryDay("28.03.2017 11:30:00.000", 4);
-		createDeliveryDay("29.03.2017 11:30:00.000", 4);
-		createDeliveryDay("30.03.2017 11:30:00.000", 4);
-		createDeliveryDay("31.03.2017 11:30:00.000", 4);
-		createDeliveryDay("01.04.2017 11:30:00.000", 4);
-		createDeliveryDay("02.04.2017 11:30:00.000", 4);
+		createDeliveryDay("27.03.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("28.03.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("29.03.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("30.03.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("31.03.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("01.04.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("02.04.2017 11:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
 
-		createDeliveryDay("27.03.2017 18:30:00.000", 4);
-		createDeliveryDay("28.03.2017 18:30:00.000", 4);
-		createDeliveryDay("29.03.2017 18:30:00.000", 4);
-		createDeliveryDay("30.03.2017 18:30:00.000", 4);
-		createDeliveryDay("31.03.2017 18:30:00.000", 4);
-		createDeliveryDay("01.04.2017 18:30:00.000", 4);
-		createDeliveryDay("02.04.2017 18:30:00.000", 4);
+		createDeliveryDay("27.03.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("28.03.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("29.03.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("30.03.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("31.03.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("01.04.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
+		createDeliveryDay("02.04.2017 18:30:00.000", 4, bpLocation.getC_BPartner_ID(), bpLocation.getC_BPartner_Location_ID());
 
 		// 27.03.2017 23:59h, dateOrdered 27.03.2017 11:29h
 		final ZonedDateTime dateOrdered1 = toZonedDateTime("27.03.2017 11:29:00.000");
@@ -78,8 +79,8 @@ public class DeliveryDayBLTest extends TourPlanningTestBase
 		final ZonedDateTime preparationDate1 = tourAndDate1.getRight();
 		final ZonedDateTime expected1 = toZonedDateTime("27.03.2017 11:30:00.000");
 
-		Assert.assertEquals(preparationDate1, expected1);
-		Assert.assertNotNull(tourAndDate1.getLeft());
+		assertThat(preparationDate1).isEqualTo(expected1);
+		assertThat(tourAndDate1.getLeft()).isNotNull();
 
 		// 27.03.2017 23:59h, dateOrdered 27.03.2017 11:31h
 		final ZonedDateTime dateOrdered2 = toZonedDateTime("27.03.2017 11:31:00.000");
@@ -88,8 +89,8 @@ public class DeliveryDayBLTest extends TourPlanningTestBase
 		final ZonedDateTime preparationDate2 = tourAndDate2.getRight();
 		final ZonedDateTime expected2 = toZonedDateTime("27.03.2017 18:30:00.000");
 
-		Assert.assertEquals(preparationDate2, expected2);
-		Assert.assertNotNull(tourAndDate2.getLeft());
+		assertThat(preparationDate2).isEqualTo(expected2);
+		assertThat(tourAndDate2.getLeft()).isNotNull();
 
 		// 02.04.2017 23:59h, dateOrdered 27.03.2017 11:29h
 		final ZonedDateTime dateOrdered3 = toZonedDateTime("27.03.2017 11:29:00.000");
@@ -98,8 +99,9 @@ public class DeliveryDayBLTest extends TourPlanningTestBase
 		final ZonedDateTime preparationDate3 = tourAndDate3.getRight();
 		final ZonedDateTime expected3 = toZonedDateTime("02.04.2017 11:30:00.000");
 
-		Assert.assertEquals(preparationDate3, expected3);
-		Assert.assertNotNull(tourAndDate3.getLeft());
+		assertThat(preparationDate3).isEqualTo(expected3);
+		assertThat(tourAndDate3.getLeft()).isNotNull();
+
 	}
 
 }
