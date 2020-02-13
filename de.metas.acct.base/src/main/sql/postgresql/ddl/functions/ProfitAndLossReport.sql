961ecb148c9da079679799f4c369e8ed5e0f1b88
TheBestPessimist <cristian@tbp.land>
2020-02-13 13:13:30 +0200

    Change trls
    https://github.com/metasfresh/metasfresh/issues/6148

--- de.metas.acct.base/src/main/sql/postgresql/ddl/functions/ProfitAndLossReport.sql
+++ de.metas.acct.base/src/main/sql/postgresql/ddl/functions/ProfitAndLossReport.sql
@@ -1,30 +1,30 @@
 DROP FUNCTION IF EXISTS ProfitAndLossReport(IN p_startDate timestamp, IN p_endDate timestamp, IN p_compareWithPrevious1Year boolean, IN p_compareWithPrevious3Years boolean);
 DROP FUNCTION IF EXISTS ProfitAndLossReport(IN p_startDate timestamp, IN p_endDate timestamp);
 
-CREATE OR REPLACE FUNCTION ProfitAndLossReport(IN p_startDate timestamp, IN p_endDate timestamp)
+CREATE OR REPLACE FUNCTION ProfitAndLossReport(IN p_startDate timestamp,
+                                               IN p_endDate timestamp)
     RETURNS TABLE
             (
-                name            text,
-                value           text,
-                isSummary       text,
-                three_years_ago numeric,
-                two_years_ago   numeric,
-                one_year_ago    numeric,
-                current_period  numeric
+                AccountName             text,
+                AccountValue            text,
+                balance_three_years_ago numeric,
+                balance_two_years_ago   numeric,
+                balance_one_year_ago    numeric,
+                balance                 numeric
             )
 AS
 $BODY$
 
-SELECT ev.name,
-       ev.value,
-       ev.issummary::text,
-       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '3 Year'::interval, p_endDate - '3 Year'::interval) three_years_ago_balance,
-       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '2 Year'::interval, p_endDate - '2 Year'::interval) two_years_ago_balance,
-       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '1 Year'::interval, p_endDate - '1 Year'::interval) one_year_ago_balance,
+SELECT ev.name                                                                                                                        AccountName,
+       ev.value                                                                                                                       AccountValue,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '3 Year'::interval, p_endDate - '3 Year'::interval) balance_three_years_ago,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '2 Year'::interval, p_endDate - '2 Year'::interval) balance_two_years_ago,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '1 Year'::interval, p_endDate - '1 Year'::interval) balance_one_year_ago,
        ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate, p_endDate)                                           current_balance
 FROM c_elementvalue ev
 WHERE TRUE
   AND ev.accounttype IN ('E', 'R')
+  AND ev.IsSummary = 'N'
 ORDER BY ev.name
     ;
 
