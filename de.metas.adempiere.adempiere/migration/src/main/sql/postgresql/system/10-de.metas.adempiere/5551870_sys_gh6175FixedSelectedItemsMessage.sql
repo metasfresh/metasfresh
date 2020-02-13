5ea5bfa983cdaa4a6cf1cd9ad56bb931a9e0d246
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-02-13 13:18:06 +0200

    #6175 Fixed selected items message. (#6177)

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5551870_sys_gh6175FixedSelectedItemsMessage.sql
@@ -0,0 +1,25 @@
+-- 2020-02-12T13:33:13.019Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='%(size)s Zeile(n) ausgewählt',Updated=TO_TIMESTAMP('2020-02-12 15:33:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=544378
+;
+
+-- 2020-02-12T13:33:20.142Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='%(size)s item(s) selected',Updated=TO_TIMESTAMP('2020-02-12 15:33:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=544378
+;
+
+-- 2020-02-12T13:33:26.587Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='%(size)s Zeile(n) ausgewählt',Updated=TO_TIMESTAMP('2020-02-12 15:33:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Message_ID=544378
+;
+
+-- 2020-02-12T13:33:31.444Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='%(size)s Zeile(n) ausgewählt',Updated=TO_TIMESTAMP('2020-02-12 15:33:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=544378
+;
+
+-- 2020-02-12T13:34:47.684Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='%(size)s Zeile(n) ausgewählt',Updated=TO_TIMESTAMP('2020-02-12 15:34:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544378
+;
+
