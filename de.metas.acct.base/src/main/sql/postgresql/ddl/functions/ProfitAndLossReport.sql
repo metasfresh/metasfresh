cd120ced9fa903dbca2459e0e1f1c7d13a86c5a7
TheBestPessimist <cristian@tbp.land>
2020-02-14 13:44:46 +0200

    Jasper Report: Profit & Loss Report (#6160)
    * Add ProfitAndLossBalance related sql functions
    
    * Element Values window: add AccountType to filters
    
    * Mark sql for deletion
    
    * Update function and columns name
    
    * Move sql file
    
    * Add Profit And Loss Report excel process
    
    * Mark ProfitAndLossBalanceForAccountInPeriod as stable
    
    * Add comment for function deprecation warning
    
    * Add IsSummary flag to ProfitAndLossReport
    
    * Change trls
    
    * Mark function as stable, not volatile
    
    * Update trl + add new ProfitAndLoss functions migration scripts
    
    https://github.com/metasfresh/metasfresh/issues/6148

---
+++ de.metas.acct.base/src/main/sql/postgresql/ddl/functions/ProfitAndLossReport.sql
@@ -0,0 +1,39 @@
+DROP FUNCTION IF EXISTS ProfitAndLossReport(IN p_startDate timestamp, IN p_endDate timestamp, IN p_compareWithPrevious1Year boolean, IN p_compareWithPrevious3Years boolean);
+DROP FUNCTION IF EXISTS ProfitAndLossReport(IN p_startDate timestamp, IN p_endDate timestamp);
+
+CREATE OR REPLACE FUNCTION ProfitAndLossReport(IN p_startDate timestamp,
+                                               IN p_endDate timestamp)
+    RETURNS TABLE
+            (
+                AccountName             text,
+                AccountValue            text,
+                balance_three_years_ago numeric,
+                balance_two_years_ago   numeric,
+                balance_one_year_ago    numeric,
+                balance                 numeric
+            )
+AS
+$BODY$
+
+SELECT ev.name                                                                                                                        AccountName,
+       ev.value                                                                                                                       AccountValue,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '3 Year'::interval, p_endDate - '3 Year'::interval) balance_three_years_ago,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '2 Year'::interval, p_endDate - '2 Year'::interval) balance_two_years_ago,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate - '1 Year'::interval, p_endDate - '1 Year'::interval) balance_one_year_ago,
+       ProfitAndLossBalanceForAccountInPeriod(ev.c_elementvalue_id, p_startDate, p_endDate)                                           current_balance
+FROM c_elementvalue ev
+WHERE TRUE
+  AND ev.accounttype IN ('E', 'R')
+  AND ev.IsSummary = 'N'
+ORDER BY ev.name
+    ;
+
+$BODY$
+    LANGUAGE sql
+    STABLE;
+
+ALTER FUNCTION ProfitAndLossReport(timestamp, timestamp)
+    OWNER TO metasfresh;
+
+-- test: SELECT * FROM ProfitAndLossReport('1993-01-01'::Timestamp, '2992-01-01'::Timestamp);
+-- test SELECT * FROM ProfitAndLossReport('2018-01-01'::Timestamp, '2018-07-01'::Timestamp);
---
+++ de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5551730_sys_gh6148_AnnounceFunctionDeprecation.sql
@@ -0,0 +1,5 @@
+COMMENT ON FUNCTION de_metas_acct.acctbalanceuntildate(numeric, numeric, date, numeric, character , character) IS 'DEPRECATION WARNING'
+' this will be deleted in https://github.com/metasfresh/metasfresh/issues/6149 '
+' DO NOT USE THIS ANYMORE '
+' PLEASE USE acctBalanceToDate '
+' (for reference, this one is called `UntilDate` instead of `ToDate`)' ;
