b70be8a037574d5856b7858715e448c57a26015b
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-02-14 11:21:28 +0200

    #6125 updated 'updated' field.

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5552160_sys_gh6125UpdatedBankStatementDocType.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5552160_sys_gh6125UpdatedBankStatementDocType.sql
@@ -1,10 +1,10 @@
 update c_bankstatement
-set c_doctype_id = (select dt.c_docType_id 
-					from c_doctype dt 
-					where (dt.ad_org_id=c_bankstatement.ad_org_id or dt.ad_org_id = 0)
-					and dt.DocbaseType = 'CMB' 
-					and dt.DocSubType= 'BS' 
-					limit 1),
-	updatedBy = 99,
-	Updated=(select now())
+set c_doctype_id = (select dt.c_docType_id
+                    from c_doctype dt
+                    where (dt.ad_org_id = c_bankstatement.ad_org_id or dt.ad_org_id = 0)
+                      and dt.DocbaseType = 'CMB'
+                      and dt.DocSubType = 'BS'
+                    limit 1),
+    updatedBy    = 99,
+    Updated      = '2020-02-14 10:20:45.55786+01'
 where c_doctype_id = null;
