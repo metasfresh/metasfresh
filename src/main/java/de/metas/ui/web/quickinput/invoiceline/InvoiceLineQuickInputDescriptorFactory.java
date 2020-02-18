393f18bd37b58ec486b1287e8529f70ddd9e2aab
pvpurcarcosmin <50879041+pvpurcarcosmin@users.noreply.github.com>
2020-02-18 12:57:36 +0200

    Support packing instructions for invoice line 'Batch entry' (#1368)
    https://github.com/metasfresh/metasfresh/issues/6201

--- src/main/java/de/metas/ui/web/quickinput/invoiceline/InvoiceLineQuickInputDescriptorFactory.java
+++ src/main/java/de/metas/ui/web/quickinput/invoiceline/InvoiceLineQuickInputDescriptorFactory.java
@@ -3,8 +3,12 @@ package de.metas.ui.web.quickinput.invoiceline;
 import java.util.Optional;
 import java.util.Set;
 
+import de.metas.ui.web.quickinput.QuickInputConstants;
+import de.metas.ui.web.window.datatypes.LookupValue;
+import de.metas.ui.web.window.descriptor.sql.SqlLookupDescriptor;
 import org.adempiere.ad.expression.api.ConstantLogicExpression;
 import org.compiere.model.I_C_InvoiceLine;
+import org.compiere.util.DisplayType;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.ImmutableSet;
@@ -51,6 +55,15 @@ import lombok.NonNull;
 @Component
 public class InvoiceLineQuickInputDescriptorFactory implements IQuickInputDescriptorFactory
 {
+	private final IMsgBL msgBL = Services.get(IMsgBL.class);
+
+	private final InvoiceLineQuickInputCallout invoiceLineQuickInputCallout;
+
+	public InvoiceLineQuickInputDescriptorFactory(final InvoiceLineQuickInputCallout invoiceLineQuickInputCallout)
+	{
+		this.invoiceLineQuickInputCallout = invoiceLineQuickInputCallout;
+	}
+
 	@Override
 	public Set<MatchingKey> getMatchingKeys()
 	{
@@ -64,52 +77,79 @@ public class InvoiceLineQuickInputDescriptorFactory implements IQuickInputDescri
 			final DetailId detailId,
 			@NonNull final Optional<SOTrx> soTrx)
 	{
-		final DocumentEntityDescriptor entityDescriptor = createEntityDescriptor(documentType, documentTypeId, detailId, soTrx);
+		final DocumentEntityDescriptor entityDescriptor = createEntityDescriptor(documentTypeId, detailId, soTrx);
 		final QuickInputLayoutDescriptor layout = createLayout(entityDescriptor);
 
 		return QuickInputDescriptor.of(entityDescriptor, layout, InvoiceLineQuickInputProcessor.class);
 	}
 
 	private DocumentEntityDescriptor createEntityDescriptor(
-			final DocumentType documentType,
 			final DocumentId documentTypeId,
 			final DetailId detailId,
 			@NonNull final Optional<SOTrx> soTrx)
 	{
-		final IMsgBL msgBL = Services.get(IMsgBL.class);
 
-		final DocumentEntityDescriptor.Builder entityDescriptor = DocumentEntityDescriptor.builder()
+		return DocumentEntityDescriptor.builder()
 				.setDocumentType(DocumentType.QuickInput, documentTypeId)
 				.setIsSOTrx(soTrx)
 				.disableDefaultTableCallouts()
-				// Defaults:
-				.setDetailId(detailId);
+				.setDetailId(detailId)
+				.addField( createProductFieldDescriptor() )
+				.addFieldIf( QuickInputConstants.isEnablePackingInstructionsField(), this::createPackingItemFieldDescriptor )
+				.addField( createQtyFieldDescriptor() )
+				.build();
+	}
 
-		entityDescriptor.addField(DocumentFieldDescriptor.builder(IInvoiceLineQuickInput.COLUMNNAME_M_Product_ID)
+	private DocumentFieldDescriptor.Builder createProductFieldDescriptor()
+	{
+		return DocumentFieldDescriptor
+				.builder(IInvoiceLineQuickInput.COLUMNNAME_M_Product_ID)
 				.setCaption(msgBL.translatable(IInvoiceLineQuickInput.COLUMNNAME_M_Product_ID))
-				//
 				.setWidgetType(DocumentFieldWidgetType.Lookup)
-				.setLookupDescriptorProvider(ProductLookupDescriptor.builderWithoutStockInfo()
+				.setLookupDescriptorProvider(
+						ProductLookupDescriptor
+						.builderWithoutStockInfo()
 						.bpartnerParamName(I_C_Invoice.COLUMNNAME_C_BPartner_ID)
 						.pricingDateParamName(I_C_Invoice.COLUMNNAME_DateInvoiced)
-						.build())
+						.build() )
 				.setMandatoryLogic(true)
 				.setDisplayLogic(ConstantLogicExpression.TRUE)
-				.addCharacteristic(Characteristic.PublicField));
+				.addCallout(invoiceLineQuickInputCallout::onProductChange)
+				.addCharacteristic(Characteristic.PublicField);
+	}
 
-		entityDescriptor.addField(DocumentFieldDescriptor.builder(IInvoiceLineQuickInput.COLUMNNAME_Qty)
+	private DocumentFieldDescriptor.Builder createQtyFieldDescriptor()
+	{
+		return DocumentFieldDescriptor.builder(IInvoiceLineQuickInput.COLUMNNAME_Qty)
 				.setCaption(msgBL.translatable(IInvoiceLineQuickInput.COLUMNNAME_Qty))
 				.setWidgetType(DocumentFieldWidgetType.Quantity)
 				.setMandatoryLogic(true)
-				.addCharacteristic(Characteristic.PublicField));
+				.addCharacteristic(Characteristic.PublicField);
+	}
 
-		return entityDescriptor.build();
+	private DocumentFieldDescriptor.Builder createPackingItemFieldDescriptor()
+	{
+		return DocumentFieldDescriptor.builder(IInvoiceLineQuickInput.COLUMNNAME_M_HU_PI_Item_Product_ID)
+				.setCaption(msgBL.translatable(IInvoiceLineQuickInput.COLUMNNAME_M_HU_PI_Item_Product_ID))
+				.setWidgetType(DocumentFieldWidgetType.Lookup)
+				.setLookupDescriptorProvider(SqlLookupDescriptor.builder()
+						.setCtxTableName(null) // ctxTableName
+						.setCtxColumnName(IInvoiceLineQuickInput.COLUMNNAME_M_HU_PI_Item_Product_ID)
+						.setDisplayType(DisplayType.TableDir)
+						.setAD_Val_Rule_ID(540480) // FIXME: hardcoded "M_HU_PI_Item_Product_For_Org_Product_And_DateInvoiced"
+						.buildProvider())
+				.setValueClass(LookupValue.IntegerLookupValue.class)
+				.setReadonlyLogic(ConstantLogicExpression.FALSE)
+				.setAlwaysUpdateable(true)
+				.setMandatoryLogic(ConstantLogicExpression.FALSE)
+				.setDisplayLogic(ConstantLogicExpression.TRUE)
+				.addCharacteristic(Characteristic.PublicField);
 	}
 
 	private QuickInputLayoutDescriptor createLayout(final DocumentEntityDescriptor entityDescriptor)
 	{
 		return QuickInputLayoutDescriptor.build(entityDescriptor, new String[][] {
-				{ IInvoiceLineQuickInput.COLUMNNAME_M_Product_ID }, //
+				{ IInvoiceLineQuickInput.COLUMNNAME_M_Product_ID, IInvoiceLineQuickInput.COLUMNNAME_M_HU_PI_Item_Product_ID },
 				{ IInvoiceLineQuickInput.COLUMNNAME_Qty }
 		});
 	}
