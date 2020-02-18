f5e116ef99f709cdf4da8c66c0374191ec8e4bc7
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-02-18 15:00:46 +0200

    #6150 Top Products : adjustments and indentation
    https://github.com/metasfresh/metasfresh/issues/6150

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/functions/ProductsTopRevenue.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/functions/ProductsTopRevenue.sql
@@ -1,49 +1,57 @@
-DROP FUNCTION IF EXISTS ProductsTopRevenue( numeric, numeric, date,  date, numeric);
+DROP FUNCTION IF EXISTS ProductsTopRevenue(numeric, numeric, date, date, numeric);
 
-CREATE OR REPLACE FUNCTION ProductsTopRevenue 
-( 
-     p_AD_Client_ID numeric, 
-     p_AD_Org_ID numeric ,
-	 p_DateFrom date, 
-	 p_DateTo date, 
-	 p_Limit numeric
-)
+CREATE OR REPLACE FUNCTION ProductsTopRevenue
+(p_AD_Client_ID numeric,
+ p_AD_Org_ID numeric,
+ p_DateFrom date,
+ p_DateTo date,
+ p_Limit numeric)
 
-RETURNS TABLE 
-(
-	Rang integer,
-	ProductValue character varying,
-	Name character varying,
-	Revenue numeric,
-	QtyOnHandStock numeric,
-	SalesPercentProduct numeric
-)
+    RETURNS TABLE
+            (
+                Rang                integer,
+                ProductValue        character varying,
+                Name                character varying,
+                Revenue             numeric,
+                ISO_Code            character(3),
+                QtyOnHandStock      numeric,
+                SalesPercentOfTotal numeric
+            )
 AS
 $$
 
-SELECT 
-	ROW_NUMBER () OVER (ORDER BY  t.Revenue DESC NULLS LAST, t.ProductValue) :: integer as Rang,
-	t.ProductValue,
-	t.Name,
-	t.Revenue,
-	t.QtyOnHandStock,
-	round((case when totals.totalRevenueAmt != 0 then (t.Revenue * 100) / totals.totalRevenueAmt else 0 end),2) AS SalesPercentProduct
-	
-	
+SELECT ROW_NUMBER() OVER (ORDER BY t.Revenue DESC NULLS LAST, t.ProductValue) :: integer AS Rang,
+       t.ProductValue,
+       t.Name,
+       t.Revenue,
+       accounting.Iso_Code,
+       t.QtyOnHandStock,
+       round((CASE
+                  WHEN totals.totalRevenueAmt != 0 THEN (t.Revenue * 100) / totals.totalRevenueAmt
+                  ELSE 0 END), 2)                                                        AS SalesPercentOfTotal
+
+
 FROM (
-		SELECT 
-			 p.value as ProductValue,
-			 p.Name as Name,
-			 getProductRevenue(p.M_Product_ID, p_AD_Client_ID, p_AD_Org_ID, p_DateFrom, p_DateTo) as Revenue,
-			 getProductCurrentStock(p.M_Product_ID, p_AD_Client_ID, p_AD_Org_ID) as QtyOnHandStock
-		FROM M_Product p
-		
-	) t,
-	(
-		SELECT COALESCE(getTotalRevenue(p_AD_Client_ID, p_AD_Org_ID, p_DateFrom, p_DateTo),0) as totalRevenueAmt
-	) totals
-ORDER BY Rang 
-LIMIT (CASE WHEN p_Limit > 0 then p_Limit ELSE 999999999999999 END)
+         SELECT p.value                                                                              AS ProductValue,
+                p.Name                                                                               AS Name,
+                getProductRevenue(p.M_Product_ID, p_AD_Client_ID, p_AD_Org_ID, p_DateFrom, p_DateTo) AS Revenue,
+                getProductCurrentStock(p.M_Product_ID, p_AD_Client_ID, p_AD_Org_ID)                  AS QtyOnHandStock
+         FROM M_Product p
+     ) t,
+     (
+         SELECT COALESCE(getTotalRevenue(p_AD_Client_ID, p_AD_Org_ID, p_DateFrom, p_DateTo), 0) AS totalRevenueAmt
+     ) totals,
+     (
+         SELECT ISO_Code
+         FROM C_Currency c
+                  JOIN C_AcctSchema acc ON c.c_currency_id = acc.c_currency_id
+                  JOIN AD_ClientInfo ci ON ci.C_AcctSchema1_ID = acc.C_AcctSchema_ID
+         WHERE ci.AD_Client_ID = p_AD_Client_ID
+     ) accounting
+ORDER BY Rang
+LIMIT (CASE
+           WHEN p_Limit > 0 THEN p_Limit
+           ELSE 999999999999999 END)
 
 $$
-LANGUAGE sql STABLE;
+    LANGUAGE SQL STABLE;
