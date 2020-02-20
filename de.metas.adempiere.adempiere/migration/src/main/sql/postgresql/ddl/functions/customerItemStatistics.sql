ba55d8d67f03449a4fa42c20cfeb205aebcfa72b
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-02-20 17:22:52 +0200

    #6133 function in ddl
    https://github.com/metasfresh/metasfresh/issues/6133

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/functions/customerItemStatistics.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/functions/customerItemStatistics.sql
@@ -1,11 +1,13 @@
 DROP FUNCTION IF EXISTS customerItemStatistics(numeric, numeric, date, date, numeric, numeric);
+DROP FUNCTION IF EXISTS customerItemStatistics(numeric, numeric, date, date, numeric, character varying);
 
 CREATE OR REPLACE FUNCTION customerItemStatistics(p_AD_Client_ID numeric,
                                                   p_AD_Org_ID numeric,
                                                   p_dateFrom date,
                                                   p_dateTo date,
                                                   p_C_BPartner_ID numeric,
-                                                  p_M_Product_ID numeric)
+                                                  p_M_Product_ID numeric,
+                                                  p_AD_Language character varying)
     RETURNS TABLE
             (
 
@@ -16,10 +18,11 @@ CREATE OR REPLACE FUNCTION customerItemStatistics(p_AD_Client_ID numeric,
                 UOM                 character varying,
                 Revenue             numeric,
                 ProductCosts        numeric,
-                ISO_Code            character (3),
+                ISO_Code            character(3),
                 ProductCostsPercent numeric
 
-            ) AS
+            )
+AS
 
 
 $$
@@ -30,19 +33,25 @@ SELECT t.BPValue,
        t.Name,
        t.qtyInvoiced,
        t.UOM,
-       t.Revenue AS Revenue,
+       t.Revenue                   AS Revenue,
        t.costPrice * t.qtyInvoiced AS ProductCosts,
        accounting.iso_Code,
-       round((CASE WHEN t.Revenue > 0 THEN (t.costPrice * t.qtyInvoiced * 100) / t.Revenue ELSE 0 END), 2) AS ProductCostsPercent
+       round((CASE
+                  WHEN t.Revenue > 0 THEN (t.costPrice * t.qtyInvoiced * 100) / t.Revenue
+                  ELSE 0 END), 2)  AS ProductCostsPercent
 
 FROM (
-         SELECT bp.value AS BPValue,
-                p.value AS productValue,
-                p.name AS Name,
+         SELECT bp.value                                                     AS BPValue,
+                p.value                                                      AS productValue,
+                COALESCE(trl.name, p.name)                                   AS Name,
                 sum(
-                    uomconvert(p.M_Product_ID, il.c_uom_id, p.C_UOM_ID, il.qtyInvoiced)
-                ) AS qtyInvoiced,
-                (SELECT UOMSymbol FROM C_UOM WHERE C_UOM_ID = p.C_UOM_ID) AS UOM,
+                        uomconvert(p.M_Product_ID, il.c_uom_id, p.C_UOM_ID, il.qtyInvoiced)
+                    )                                                        AS qtyInvoiced,
+                (SELECT COALESCE(uomtrl.UOMSymbol, uom.UOMSymbol)
+                 FROM C_UOM uom
+                          LEFT JOIN C_UOM_TRL uomtrl ON uom.C_UOM_ID = uomtrl.C_UOM_ID AND uomtrl.AD_Language = p_AD_Language
+                 WHERE uom.C_UOM_ID = p.C_UOM_ID)                            AS UOM,
+
                 sum
                     (
                         currencyBase
@@ -59,12 +68,13 @@ FROM (
                                 p_AD_Org_ID
                             )
                     )
-                    AS Revenue,
+                                                                             AS Revenue,
                 -- note: We don't have to convert this to the accounting currency because these costs are already in the accounting currency
                 -- see de.metas.costing.impl.CurrentCostsRepository.create(CostSegmentAndElement)
                 getCostPrice(p.M_Product_ID, p_AD_Client_ID, p_AD_Client_ID) AS costPrice
 
          FROM M_Product p
+                  LEFT JOIN M_Product_TRL trl ON p.M_Product_ID = trl.M_Product_ID AND trl.AD_language = p_AD_Language
                   JOIN C_InvoiceLine il ON p.M_Product_ID = il.M_Product_ID
                   JOIN C_Invoice i ON il.C_Invoice_ID = i.C_Invoice_ID
                   JOIN C_BPartner bp ON i.C_BPartner_ID = bp.C_BPartner_ID
@@ -79,13 +89,15 @@ FROM (
            AND (p_DateFrom IS NULL OR i.DateInvoiced >= p_dateFrom)
            AND (p_DateTo IS NULL OR i.DateInvoiced <= p_dateTo)
 
-         GROUP BY bp.value, p.M_Product_ID, p.C_UOM_ID
+         GROUP BY bp.value, p.M_Product_ID, p.C_UOM_ID, trl.name
      ) t,
-	(
-		SELECT ISO_Code from C_Currency c
-		    JOIN C_AcctSchema  acc on c.c_currency_id = acc.c_currency_id
-		    JOIN  AD_ClientInfo ci  on ci.C_AcctSchema1_ID  =  acc.C_AcctSchema_ID where ci.AD_Client_ID = p_AD_Client_ID
-	) accounting
+     (
+         SELECT ISO_Code
+         FROM C_Currency c
+                  JOIN C_AcctSchema acc ON c.c_currency_id = acc.c_currency_id
+                  JOIN AD_ClientInfo ci ON ci.C_AcctSchema1_ID = acc.C_AcctSchema_ID
+         WHERE ci.AD_Client_ID = p_AD_Client_ID
+     ) accounting
 ORDER BY t.BPValue, t.productValue
 $$
     LANGUAGE SQL
