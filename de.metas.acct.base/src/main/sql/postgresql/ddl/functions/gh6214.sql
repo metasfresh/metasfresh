6d2f162e3bdab6aceaaa484858d1b22ff0a2ac9b
TheBestPessimist <cristian@tbp.land>
2020-02-21 11:25:39 +0200

    Midwork
    https://github.com/metasfresh/metasfresh/issues/6214

--- de.metas.acct.base/src/main/sql/postgresql/ddl/functions/gh6214.sql
+++ de.metas.acct.base/src/main/sql/postgresql/ddl/functions/gh6214.sql
@@ -1,26 +1,29 @@
 -- todo name this Partner Account Sheet
+DROP FUNCTION IF EXISTS gh6214(p_c_bpartner_id numeric, p_dateFrom date, p_dateTo date, p_ad_client_id numeric, p_ad_org_id numeric, p_isSoTrx TEXT);
+
 CREATE OR REPLACE FUNCTION gh6214(p_c_bpartner_id numeric,
-                                  p_dateFrom date,
-                                  p_dateTo date,
-                                  p_ad_client_id numeric,
-                                  p_ad_org_id numeric = NULL,
-                                  p_isSoTrx TEXT = 'Y')
+                                  p_dateFrom      date,
+                                  p_dateTo        date,
+                                  p_ad_client_id  numeric,
+                                  p_ad_org_id     numeric = NULL,
+                                  p_isSoTrx       TEXT = 'Y')
     RETURNS table
             (
-                beginningBalance numeric,
-                endingBalance    numeric,
-                dateAcct         date,
-                doctype          TEXT,
-                documentno       text,
-                description      text,
+                beginningBalance NUMERIC,
                 amount           NUMERIC,
-                currency         text
+                endingBalance    NUMERIC,
+                dateAcct         DATE,
+                doctype          TEXT,
+                documentno       TEXT,
+                description      TEXT,
+                currency         TEXT
             )
 AS
 $BODY$
 
-DROP TABLE IF EXISTS temp_gh6241;
-    CREATE TEMPORARY TABLE temp_gh6241
+DROP TABLE IF EXISTS temp_gh6214;
+
+    CREATE TEMPORARY TABLE temp_gh6214
     (
         beginningBalance       numeric,
         amount                 numeric,
@@ -79,7 +82,7 @@ DROP TABLE IF EXISTS temp_gh6241;
                    AND (coalesce(p_ad_org_id, 0) <= 0 OR p.ad_org_id = p_ad_org_id)
              )
     INSERT
-    INTO temp_gh6241(beginningBalance,
+    INTO temp_gh6214(beginningBalance,
                      amount,
                      endingBalance,
                      dateacct,
@@ -105,43 +108,42 @@ DROP TABLE IF EXISTS temp_gh6241;
     FROM invoicesAndPaymentsInPeriod
     ;
 
---
--- Update the amount to be in the base currency
-    UPDATE temp_gh6241
-    SET amount = (SELECT currencybase(amount, c_currency_id_original, dateacct, p_ad_client_id, p_ad_org_id));
--- todo @ teo: is it correct to set conversion date to dateacct?
 
---
--- Update the amount according to the document type
--- todo (this one  uses some case statements)
+    --
+    -- Update the amount to be in the base currency -- todo maybe this can be merged into the insert step
+    UPDATE temp_gh6214
+    SET amount = (SELECT currencybase(amount, c_currency_id_original, dateacct, p_ad_client_id, ad_org_id));
 
---
--- Update the beginning and end balances with the initial "Open Invoice Amount to Date"
-    UPDATE temp_gh6241
+
+    --
+    -- Update the amount according to the document type
+    -- todo (this one  uses some case statements)
+
+
+    --
+    -- Update the beginning and end balances with the initial "Open Invoice Amount to Date"
+    UPDATE temp_gh6214
     SET beginningBalance = t.OpenInvoiceAmountToDate,
         endingBalance    = t.OpenInvoiceAmountToDate
     FROM (
              SELECT --
                     sum((openItems).openamt) OpenInvoiceAmountToDate
-             FROM de_metas_endcustomer_fresh_reports.OpenItems_Report(('3111-07-07'::date - INTERVAL '1 days')::date) openItems -- dateFrom
+             FROM de_metas_endcustomer_fresh_reports.OpenItems_Report((p_dateFrom - INTERVAL '1 days')::date) openItems -- dateFrom
          ) t
     ;
 
 
---
--- Compute rolling sum
+    --
+    -- Compute rolling sum
     WITH endingBalanceSum AS
              (
                  SELECT --
                         t.rowid,
---t.endingBalance
--- +
-                        sum(t.amount) OVER
-                            (
-                            ORDER BY t.dateacct, t.created, t.documentno ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-                            )    endingBalance,
-                        t.amount currentAmount
-                 FROM temp_gh6241 t
+                        -- t.endingBalance
+                        -- +
+                        sum(t.amount) OVER ( ORDER BY t.dateacct, t.created, t.documentno ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW ) endingBalance,
+                        t.amount                                                                                                             currentAmount
+                 FROM temp_gh6214 t
              ),
          finalData AS
              (
@@ -151,15 +153,25 @@ DROP TABLE IF EXISTS temp_gh6241;
                         endingBalance - currentAmount beginningBalance
                  FROM endingBalanceSum
              )
-    UPDATE temp_gh6241 t
+    UPDATE temp_gh6214 t
     SET endingBalance    = d.endingBalance,
         beginningBalance = d.beginningBalance
     FROM finalData d
     WHERE t.rowid = d.rowid
     ;
 
-
-    SELECT NULL;
+    --
+    -- return the data
+    SELECT --
+           beginningBalance,
+           amount,
+           endingBalance,
+           dateAcct,
+           NULL doctype,     -- todo
+           documentno,
+           description,
+           NULL org_currency -- todo
+    FROM temp_gh6214;
 
     $BODY$
     LANGUAGE SQL STABLE;
@@ -169,18 +181,16 @@ SELECT beginningBalance,
        amount,
        endingBalance,
        beginningBalance + amount AS checkk,
-       rowid,
        dateacct,
-       description,
-       c_doctype_id,
+       doctype,
        documentno,
-       created,
-       c_currency_id_original
-FROM gh6214(p_c_bpartner_id,
-            dateFrom,
-            dateTo,
-            'Y')
-ORDER BY dateacct, created, documentno
+       description,
+       currency
+FROM gh6214(NULL,
+            '1111-1-1'::date,
+            '3333-1-1'::date,
+            1000000)
+ORDER BY dateacct, documentno
 ;
 
 
