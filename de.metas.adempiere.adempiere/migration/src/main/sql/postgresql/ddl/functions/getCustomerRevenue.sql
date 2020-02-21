0e347d6eb8a3feb073b287facd7418e927e70196
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-02-21 17:37:59 +0200

    #6191 Customer Top Revenue: subtract credit memo, refactoring
    https://github.com/metasfresh/metasfresh/issues/6191

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/functions/getCustomerRevenue.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/functions/getCustomerRevenue.sql
@@ -1,33 +1,40 @@
 DROP FUNCTION IF EXISTS getCustomerRevenue(numeric, numeric, numeric, date, date);
 
-CREATE OR REPLACE FUNCTION getCustomerRevenue
-(p_C_BPartner_ID numeric,
- p_AD_Client_ID numeric,
- p_AD_Org_ID numeric,
- p_DateFrom date,
- p_DateTo date)
+CREATE OR REPLACE FUNCTION getCustomerRevenue(p_C_BPartner_ID numeric,
+                                              p_AD_Client_ID numeric,
+                                              p_AD_Org_ID numeric,
+                                              p_DateFrom date,
+                                              p_DateTo date)
     RETURNS numeric
 AS
 $$
 SELECT sum
            (
-               currencyBase
-                   (
+                   CASE
+                       WHEN dt.DocBaseType IN ('ARC', 'APC') -- subtract credit memos
+                           THEN -1
+                       ELSE
+                           1
+                       END
+                   *
+                   currencyBase
                        (
-                           SELECT CASE
-                                      WHEN i.IsTaxIncluded = 'Y' THEN il.LineNetAmt - il.TaxAmtInfo
-                                      ELSE il.LineNetAmt
-                                      END
-                       ), -- amt
-                       i.C_Currency_ID, -- currencyFrom
-                       i.DateInvoiced, -- date
-                       p_AD_Client_ID,
-                       p_AD_Org_ID
-                   )
+                           (
+                               SELECT CASE
+                                          WHEN i.IsTaxIncluded = 'Y' THEN il.LineNetAmt - il.TaxAmtInfo
+                                          ELSE il.LineNetAmt
+                                          END
+                           ), -- amt
+                           i.C_Currency_ID, -- currencyFrom
+                           i.DateInvoiced, -- date
+                           p_AD_Client_ID,
+                           p_AD_Org_ID
+                       )
            )
 
 FROM C_InvoiceLine il
          JOIN C_Invoice i ON il.C_Invoice_ID = i.C_Invoice_ID
+         JOIN C_DocTYpe dt ON i.C_DocType_ID = dt.C_DocType_ID
 
 WHERE p_C_BPartner_ID = i.C_Bpartner_ID
   AND il.IsActive = 'Y'
