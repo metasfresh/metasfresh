4deea75d87ed75f2a5f9c10f7b33fbbeaa560729
TheBestPessimist <cristian@tbp.land>
2020-02-24 15:31:59 +0200

    Add migration for sql functions
    https://github.com/metasfresh/metasfresh/issues/6214

--- de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5553020_sys_gh6214_AddFunctionMigrationScript.sql
+++ de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5553020_sys_gh6214_AddFunctionMigrationScript.sql
@@ -1,3 +1,60 @@
+DROP FUNCTION IF EXISTS getBPOpenAmtToDate(numeric, numeric, date, numeric, numeric, text, text);
+
+CREATE OR REPLACE FUNCTION getBPOpenAmtToDate(p_AD_Client_ID  numeric,
+                                              p_AD_Org_ID     numeric,
+                                              p_Date          date,
+                                              p_C_BPartner_id numeric,
+                                              p_C_Currency_ID numeric,
+                                              p_UseDateAcct   text = 'Y',
+                                              p_IsSOTrx       text = 'Y')
+    RETURNS numeric
+AS
+$$
+DECLARE
+    v_sum numeric;
+BEGIN
+
+    SELECT INTO v_sum sum(
+                              (SELECT openamt
+                               FROM invoiceOpenToDate(
+                                       p_C_Invoice_ID := i.C_Invoice_ID,
+                                       p_c_invoicepayschedule_id := COALESCE(ips.C_InvoicePaySchedule_ID, 0::numeric),
+                                       p_DateType := (
+                                           CASE
+                                               WHEN p_UseDateAcct = 'Y' THEN 'A'
+                                                                        ELSE 'T'
+                                           END
+                                           ),
+                                       p_Date := p_Date,
+                                       p_Result_Currency_ID := p_C_Currency_ID
+                                   )
+                              )
+                          )
+    FROM C_Invoice i
+             LEFT OUTER JOIN C_InvoicePaySchedule ips
+                             ON i.C_Invoice_ID = ips.C_Invoice_ID
+                                 AND ips.isvalid = 'Y'
+                                 AND ips.isActive = 'Y'
+    WHERE TRUE
+      AND i.IsSOTrx = p_IsSOTrx
+      AND (CASE
+               WHEN p_UseDateAcct = 'Y' THEN i.dateacct
+                                        ELSE i.dateinvoiced
+           END) <= p_Date
+      AND i.DocStatus IN ('CO', 'CL')
+      AND i.c_bpartner_id = p_c_bpartner_id
+      AND i.AD_CLient_ID = p_AD_Client_ID
+      AND (COALESCE(p_AD_Org_ID, 0) <= 0 OR i.AD_Org_ID = p_AD_Org_ID);
+
+    RETURN coalesce(v_sum, 0);
+END ;
+$$
+    LANGUAGE plpgsql VOLATILE;
+
+
+
+
+
 DROP FUNCTION IF EXISTS BusinessPartnerAccountSheetReport(p_c_bpartner_id numeric, p_dateFrom date, p_dateTo date, p_ad_client_id numeric, p_ad_org_id numeric, p_isSoTrx TEXT, p_ad_language text);
 
 CREATE OR REPLACE FUNCTION BusinessPartnerAccountSheetReport(p_c_bpartner_id numeric,
@@ -23,7 +80,6 @@ $BODY$
 DECLARE
     v_time timestamp;
     v_temp numeric;
-
 BEGIN
     v_time := logDebug('start');
 
@@ -234,16 +290,17 @@ $BODY$
     LANGUAGE plpgsql
     VOLATILE;
 
-COMMENT ON FUNCTION BusinessPartnerAccountSheetReport(p_c_bpartner_id numeric, p_dateFrom date, p_dateTo date, p_ad_client_id numeric, p_ad_org_id numeric, p_isSoTrx TEXT, p_ad_language text) IS
-'How to run:
 
-SELECT*
-FROM BusinessPartnerAccountSheetReport(2000252,
-                                       ''1111-1-1''::date,
-                                       ''3333-1-1''::date,
-                                       1000000)
-;
-';
+COMMENT ON FUNCTION BusinessPartnerAccountSheetReport(p_c_bpartner_id numeric, p_dateFrom date, p_dateTo date, p_ad_client_id numeric, p_ad_org_id numeric, p_isSoTrx TEXT, p_ad_language text) IS
+    'How to run:
+
+    SELECT*
+    FROM BusinessPartnerAccountSheetReport(2000252,
+                                           ''1111-1-1''::date,
+                                           ''3333-1-1''::date,
+                                           1000000)
+    ;
+    ';
 
 /*
 How to run:
@@ -256,5 +313,3 @@ FROM BusinessPartnerAccountSheetReport(2000252,
 ;
 
 */
-
-
