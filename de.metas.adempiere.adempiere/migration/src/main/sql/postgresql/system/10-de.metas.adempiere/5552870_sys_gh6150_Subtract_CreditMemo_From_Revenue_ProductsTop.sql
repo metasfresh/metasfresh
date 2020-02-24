67ee03adb7cddab4f37b7b1f93550a7f3353c6c0
Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
2020-02-24 12:14:10 +0200

    #6150 Subtract Credit Memo amounts from revenue (#6226)
    https://github.com/metasfresh/metasfresh/issues/6150

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5552870_sys_gh6150_Subtract_CreditMemo_From_Revenue_ProductsTop.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5552870_sys_gh6150_Subtract_CreditMemo_From_Revenue_ProductsTop.sql
@@ -50,3 +50,62 @@ WHERE p_M_Product_ID = il.M_Product_ID
 
 $$
     LANGUAGE SQL STABLE;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+DROP FUNCTION IF EXISTS getTotalRevenue(numeric, numeric, date, date);
+
+CREATE OR REPLACE FUNCTION getTotalRevenue(p_AD_Client_ID numeric,
+                                           p_AD_Org_ID numeric,
+                                           p_DateFrom date,
+                                           p_DateTo date)
+    RETURNS numeric
+AS
+
+$$
+
+SELECT sum
+           (
+                   CASE
+                       WHEN dt.DocBaseType IN ('ARC', 'APC') -- subtract credit memos
+                           THEN -1
+                       ELSE
+                           1
+                       END
+                   *
+                   currencyBase
+                       (
+                           (
+                               SELECT CASE
+                                          WHEN i.IsTaxIncluded = 'Y' THEN il.LineNetAmt - il.TaxAmtInfo
+                                          ELSE il.LineNetAmt
+                                          END
+                           ), -- amt
+                           i.C_Currency_ID, -- currencyFrom
+                           i.DateInvoiced, -- date
+                           p_AD_Client_ID,
+                           p_AD_Org_ID
+                       )
+           )
+
+FROM C_InvoiceLine il
+         JOIN C_Invoice i ON il.C_Invoice_ID = i.C_Invoice_ID
+         JOIN C_DocType dt ON i.C_DocType_ID = dt.C_DocType_ID
+
+WHERE i.isSOTrx = 'Y'
+  AND il.IsActive = 'Y'
+  AND i.IsActive = 'Y'
+  AND i.DocStatus IN ('CO', 'CL')
+  AND i.AD_Client_ID = p_AD_Client_ID
+  AND i.AD_Org_ID = p_AD_Org_ID
+  AND (p_DateFrom IS NULL OR i.DateInvoiced >= p_dateFrom)
+  AND (p_DateTo IS NULL OR i.DateInvoiced <= p_dateTo)
+
+$$
+    LANGUAGE SQL STABLE;
