f5e02647e920e1051ad8d7e693541dbaf5332127
Julian Bischof <metas-jb@users.noreply.github.com>
2020-02-26 07:14:24 +0100

    #5 use env variables for elastic logstash index size and rollover… (#6)
    * #5 add parameters and fix launch scripts
    
    * #5 add example docker-compose.yml file

--- elk/elastic-post-hooks.sh
+++ elk/elastic-post-hooks.sh
@@ -4,21 +4,36 @@
 # Invoked from start.sh right after elastic started
 #
 
-echo "put customized logstash ilm policy"
-curl -s -XPUT "http://localhost:9200/_ilm/policy/logstash-policy" -H 'Content-Type: application/json' -d'{
+
+elastic_hostname=${ELK_ELASTIC_HOSTNAME:-localhost}
+elastic_json_port=${ELK_ELASTIC_JSON_PORT:-9200}
+elastic_protocol=${ELK_ELASTIC_PROTOCOL:-http}
+
+policy_max_size=$(echo ${ELK_INDEX_ROLLOVER_SIZE:-1GB} | tr -d \")
+policy_max_age=$(echo ${ELK_INDEX_ROLLOVER_AGE:-30d} | tr -d \")
+policy_del_min_age=$(echo ${ELK_INDEX_DELETE_AGE:-7d} | tr -d \")
+
+echo "[INFO] put customized logstash ilm policy"
+echo "       - ${elastic_protocol}//${elastic_hostname}:${elastic_json_port}"
+echo "       - ELK_INDEX_ROLLOVER_SIZE: ${policy_max_size}"
+echo "       - ELK_INDEX_ROLLOVER_AGE: ${policy_max_age}"
+echo "       - ELK_INDEX_DELETE_AGE: ${policy_del_min_age}"
+
+
+curl -XPUT "${elastic_protocol}://${elastic_hostname}:${elastic_json_port}/_ilm/policy/logstash-policy" -H 'Content-Type: application/json' -d'{
   "policy": {
     "phases": {
       "hot": {
         "min_age": "0ms",
         "actions": {
           "rollover": {
-            "max_age": "30d",
-            "max_size": "1GB"
+            "max_age": "'"${policy_max_age}"'",
+            "max_size": "'"${policy_max_size}"'"
           }
         }
       },
       "delete": {
-        "min_age": "7d",
+        "min_age": "'"${policy_del_min_age}"'",
         "actions": {
           "delete": {}
         }
