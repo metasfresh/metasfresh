1fa96e5cbaa4b5cc52d3ce8c3bb5d32bb9d88c1b
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-02-26 12:01:56 +0200

    #3426 Added Title Field to validation Message.

--- src/main/java/de/metas/ui/web/window/datatypes/json/JSONLookupValueValidationInformation.java
+++ src/main/java/de/metas/ui/web/window/datatypes/json/JSONLookupValueValidationInformation.java
@@ -1,18 +1,16 @@
 package de.metas.ui.web.window.datatypes.json;
 
-import javax.annotation.Nullable;
-
-import org.compiere.util.ValueNamePairValidationInformation;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import de.metas.i18n.IMsgBL;
 import de.metas.util.Services;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
+import org.compiere.util.ValueNamePairValidationInformation;
+
+import javax.annotation.Nullable;
 
 /*
  * #%L
@@ -24,12 +22,12 @@ import lombok.Value;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -40,6 +38,9 @@ import lombok.Value;
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public final class JSONLookupValueValidationInformation
 {
+	@JsonProperty("title")
+	private final String title;
+
 	@JsonProperty("question")
 	private final String question;
 
@@ -51,10 +52,12 @@ public final class JSONLookupValueValidationInformation
 
 	@Builder
 	private JSONLookupValueValidationInformation(
+			@JsonProperty("title") final String title,
 			@JsonProperty("question") final String question,
 			@JsonProperty("answerYes") final String answerYes,
 			@JsonProperty("answerNo") final String answerNo)
 	{
+		this.title = title;
 		this.question = question;
 		this.answerYes = answerYes;
 		this.answerNo = answerNo;
@@ -72,6 +75,7 @@ public final class JSONLookupValueValidationInformation
 		final IMsgBL msgBL = Services.get(IMsgBL.class);
 
 		return JSONLookupValueValidationInformation.builder()
+				.title(msgBL.translate(adLanguage, validationInformation.getTitle().toAD_Message()))
 				.question(msgBL.translate(adLanguage, validationInformation.getQuestion().toAD_Message()))
 				.answerYes(msgBL.translate(adLanguage, validationInformation.getAnswerYes().toAD_Message()))
 				.answerNo(msgBL.translate(adLanguage, validationInformation.getAnswerNo().toAD_Message()))
