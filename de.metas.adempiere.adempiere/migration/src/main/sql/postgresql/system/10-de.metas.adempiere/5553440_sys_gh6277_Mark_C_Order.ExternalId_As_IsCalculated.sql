b900a271602e747add9d5a3ad8c152ebdd7a3cfe
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2020-02-27 16:51:59 +0100

    6277-app - Issues with payment endpoint (#6279)
    * Update Prepay Order doctype trl
    
    https://github.com/metasfresh/metasfresh/issues/6277
    
    * Fix Payment Void action not working
    
    Use `ofRepoIdOrNull` instead of `ofRepoId`, since that was throwing NPE
    Revert small portion of ee576fee
    
    https://github.com/metasfresh/metasfresh/issues/6277
    
    * Fix an error when deserializing an JsonInboundPaymentInfo
    
    https://github.com/metasfresh/metasfresh/issues/6277
    
    * Mark `C_Order.ExternalID` as `IsCalculated`
    
    Doing this makes the clone operation to not copy the field
    
    https://github.com/metasfresh/metasfresh/issues/6277
    
    * In romanian `pom` means `tree`.
    
    https://github.com/metasfresh/metasfresh/issues/6277
    
    Co-authored-by: TheBestPessimist <cristian@tbp.land>

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5553440_sys_gh6277_Mark_C_Order.ExternalId_As_IsCalculated.sql
@@ -0,0 +1,5 @@
+-- 2020-02-27T12:12:34.830Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2020-02-27 14:12:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=569692
+;
+
