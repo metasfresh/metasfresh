8b858f9afa603e1e3ab142c74d28dc8a5eb14230
TheBestPessimist <cristian@tbp.land>
2020-03-02 14:36:51 +0200

    Add 1 test for preserving number of empty lines
    https://github.com/metasfresh/metasfresh/issues/6289

--- de.metas.adempiere.adempiere/base/src/test/java/de/metas/impexp/parser/FileImportReaderTest.java
+++ de.metas.adempiere.adempiere/base/src/test/java/de/metas/impexp/parser/FileImportReaderTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package de.metas.impexp.parser;
 
@@ -8,11 +8,18 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /*
  * #%L
@@ -24,22 +31,18 @@ import org.junit.Test;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-/**
- * @author metas-dev <dev@metasfresh.com>
- *
- */
 public class FileImportReaderTest
 {
 	private static final String packagePath = "/de/metas/impexp/parser";
@@ -65,22 +68,61 @@ public class FileImportReaderTest
 	}
 
 	@Test
+	public void multilineFile_NumberOfEmptyLinesIsPreserved() throws IOException
+	{
+		final URL url = getClass().getResource(packagePath + "/NumberOfEmptyLinesIsPreserved.csv");
+		assertNotNull("url null", url);
+		final File file = FileUtils.toFile(url);
+		assertNotNull("file null", file);
+
+		//
+		final Charset charset = StandardCharsets.UTF_8;
+		final List<String> lines = FileImportReader.readMultiLines(file, charset);
+
+		assertNotNull("lines null", lines);
+		assertFalse(lines.isEmpty());
+		assertEquals(14, lines.size());
+
+		Arrays.asList(1, 3, 4, 5, 6, 7, 8, 9, 11, 12).forEach(i -> assertEquals(0, lines.get(i).length()));
+
+		assertEquals("Buchungsdatum;Valuta;Buchungstext;Details;Detail;Belastung;Gutschrift;Saldo CHF", lines.get(0));
+		assertEquals("tttttttt;;;;;4444;2222;\n"
+				+ "11.01.1111;11.01.1111;aaaaaaaaaaaaaa;\"aaaaaaaaa\n"
+				+ "aaaaaaaaaa\n"
+				+ "aaaaaaaaaaaaaaa\n"
+				+ "aaaaaaaaaaaaaaaaaaaa\";;1111;;", lines.get(2));
+
+		assertEquals("22.02.2222;22.02.2222;bbbbbbbbbbbbb;\"bbbbbbbbbbbbbb\n"
+				+ "bbbbbbbbbbbbbb\n"
+				+ "bbbbbbb\n"
+				+ "bbbbbbbbb\n"
+				+ "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+				+ "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+				+ "bbbbbbbbbbbbbbb\n"
+				+ "bbbbbbbbbbbb\";;;2222;", lines.get(10));
+
+		assertEquals("33.03.3333;33.03.3333;ccccccccccccc;\"ccccccccccc\n"
+				+ "ccccccccccc\";;3333;;", lines.get(13));
+
+	}
+
+	@Test
 	public void testRegularLinesFieldFile() throws IOException
 	{
 		final URL url = getClass().getResource(packagePath + "/regularlines.csv");
-		Assert.assertNotNull("url null", url);
+		assertNotNull("url null", url);
 		final File file = FileUtils.toFile(url);
-		Assert.assertNotNull("file null", file);
+		assertNotNull("file null", file);
 
 		final Charset charset = StandardCharsets.UTF_8;
 		final List<String> lines = FileImportReader.readRegularLines(file, charset);
 
-		Assert.assertNotNull("lines null", lines);
-		Assert.assertFalse(lines.isEmpty());
-		Assert.assertEquals(3, lines.size());
-		lines.forEach(l -> Assert.assertTrue(l.startsWith("G00")));
-		Assert.assertTrue(lines.get(0).endsWith("80"));
-		Assert.assertTrue(lines.get(1).endsWith("90"));
-		Assert.assertTrue(lines.get(2).endsWith("100"));
+		assertNotNull("lines null", lines);
+		assertFalse(lines.isEmpty());
+		assertEquals(3, lines.size());
+		lines.forEach(l -> assertTrue(l.startsWith("G00")));
+		assertTrue(lines.get(0).endsWith("80"));
+		assertTrue(lines.get(1).endsWith("90"));
+		assertTrue(lines.get(2).endsWith("100"));
 	}
 }
