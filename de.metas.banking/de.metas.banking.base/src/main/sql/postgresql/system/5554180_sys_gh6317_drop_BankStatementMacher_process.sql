402975101f1dabbca1a1fc26c83493f0d29b549c
Teo Sarca <teo.sarca@metasfresh.com>
2020-03-09 17:15:04 +0200

    drop BankStatementMatcher process

---
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5554180_sys_gh6317_drop_BankStatementMacher_process.sql
@@ -0,0 +1,10 @@
+-- 2020-03-09T15:10:19.592Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Process_Trl WHERE AD_Process_ID=256
+;
+
+-- 2020-03-09T15:10:19.607Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Process WHERE AD_Process_ID=256
+;
+
