0441874e2ad1b83b556db1a5d6d133dae134d63b
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-03-10 18:48:07 +0200

    #6329 set standard columns to iscalculated = 'Y' and updated time and by details;

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554310_sys_gh6329SetDefaultColumnsToIsCalculated.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554310_sys_gh6329SetDefaultColumnsToIsCalculated.sql
@@ -1,9 +1,11 @@
 update ad_column c
-set iscalculated = 'Y'
+set iscalculated = 'Y',
+    updated      = '2020-03-10 17:01:56.767453+01',
+    updatedBy    = 99
 from ad_table t
 where true
   and t.ad_table_id = c.ad_table_id
-  and t.isview='N'
+  and t.isview = 'N'
   and c.columnname in ('DocStatus', 'DocAction', 'Processed', 'Processing')
   and c.columnsql is null
-  and c.iscalculated='N';
+  and c.iscalculated = 'N';
