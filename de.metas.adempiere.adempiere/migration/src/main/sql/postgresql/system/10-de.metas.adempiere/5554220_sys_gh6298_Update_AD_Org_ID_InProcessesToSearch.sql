241520b9d3bc94d6b1adac0a6e2204d5aedaafb9
TheBestPessimist <cristian@tbp.land>
2020-03-10 09:23:49 +0200

    Unconditionally migrate `AD_Org_ID` process parameter to `Search` from whatever it is now
    https://github.com/metasfresh/metasfresh/issues/6298

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554220_sys_gh6298_Update_AD_Org_ID_InProcessesToSearch.sql
@@ -0,0 +1,18 @@
+-- Backup the table AD_Process_Para, Even though only the rows containing 'AD_Org_ID' will be modified
+CREATE TABLE backup.ad_process_para_gh6298_ad_org_id_as_search AS
+    (
+        SELECT *
+        FROM ad_process_para
+    )
+;
+
+
+-- Unconditionally update the `ad_reference_id` to `Search`
+UPDATE ad_process_para
+SET --
+    ad_reference_id = (SELECT ad_reference_id FROM ad_reference WHERE name = 'Search'),
+    UpdatedBy=0,
+    Updated=now()
+WHERE TRUE
+  AND ad_element_id = (SELECT ad_element_id FROM ad_element WHERE columnname = 'AD_Org_ID')
+;
