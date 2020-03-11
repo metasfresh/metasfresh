b9d7fbd21c332fcad2f95c8db74b1dc8f0890a0f
TheBestPessimist <cristian@tbp.land>
2020-03-11 08:02:37 +0200

    Fixes after review
    - update only the records that actually need updating
    - use a hardcoded date string that will be the same in the SQL and the DB
    - use AD_User_ID 99 which is generally used for "manual" migrations
    
    https://github.com/metasfresh/metasfresh/issues/6298

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554220_sys_gh6298_Update_AD_Org_ID_InProcessesToSearch.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554220_sys_gh6298_Update_AD_Org_ID_InProcessesToSearch.sql
@@ -11,8 +11,9 @@ CREATE TABLE backup.ad_process_para_gh6298_ad_org_id_as_search AS
 UPDATE ad_process_para
 SET --
     ad_reference_id = (SELECT ad_reference_id FROM ad_reference WHERE name = 'Search'),
-    UpdatedBy=0,
-    Updated=now()
+    UpdatedBy=99,
+    Updated='2020-03-11 05:54:10.748455'
 WHERE TRUE
   AND ad_element_id = (SELECT ad_element_id FROM ad_element WHERE columnname = 'AD_Org_ID')
+  AND ad_reference_id = (SELECT ad_reference_id FROM ad_reference WHERE name = 'Search')
 ;
