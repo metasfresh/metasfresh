70ffbf45ea553e3d81c94dc13cf0adb805ee0915
TheBestPessimist <cristian@tbp.land>
2020-03-11 08:15:06 +0200

    Bugfix: Update only those process params which are not of type `Search`
    https://github.com/metasfresh/metasfresh/issues/6298

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554220_sys_gh6298_Update_AD_Org_ID_InProcessesToSearch.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5554220_sys_gh6298_Update_AD_Org_ID_InProcessesToSearch.sql
@@ -15,5 +15,5 @@ SET --
     Updated='2020-03-11 05:54:10.748455'
 WHERE TRUE
   AND ad_element_id = (SELECT ad_element_id FROM ad_element WHERE columnname = 'AD_Org_ID')
-  AND ad_reference_id = (SELECT ad_reference_id FROM ad_reference WHERE name = 'Search')
+  AND ad_reference_id != (SELECT ad_reference_id FROM ad_reference WHERE name = 'Search')
 ;
