4416d78b9cd32c34c70816e0a5d0f06240010dec
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-03-11 14:16:05 +0200

    #6325 Modernized SQL.
    (cherry picked from commit b1540bddc7ceb9339491e5bc6ef2feeaf2afb9f1)

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/fresh_PriceList_Details_Template_Report.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/fresh_PriceList_Details_Template_Report.sql
@@ -34,13 +34,13 @@ SELECT plc.value                                                                
        plc.uomsymbol                                                             as uomsymbol,
        plc.pricestd                                                              as pricestd,
        plc.M_ProductPrice_ID                                                     as m_productprice_id,
-       $1                                                                        as c_bpartner_id,
+       p_c_bpartner_id                                                           as c_bpartner_id,
        plc.M_HU_PI_Item_Product_ID                                               as m_hu_pi_item_product_id,
        plc.UOM_X12DE355                                                          as uom_x12de355,
-       $5                                                                        as c_bpartner_location_id,
+       p_c_bpartner_location_id                                                  as c_bpartner_location_id,
        plc.qtycuspertu                                                           as qtycuspertu,
        plc.M_Product_ID                                                          as m_product_id
-FROM report.fresh_PriceList_Details_Report($1, $2, $3, $4) plc;
+FROM report.fresh_PriceList_Details_Report(p_c_bpartner_id, p_m_pricelist_version_id, p_alt_pricelist_version_id, p_ad_language) plc;
 --
 $BODY$
     LANGUAGE sql STABLE
