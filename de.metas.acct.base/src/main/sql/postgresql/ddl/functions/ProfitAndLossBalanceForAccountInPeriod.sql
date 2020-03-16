1c9bde75e3f9ac760b3922b9388c949d4c5bfabf
TheBestPessimist <cristian@tbp.land>
2020-03-16 15:27:11 +0200

    Update Profit & Loss Report Calculation Formula
    https://github.com/metasfresh/metasfresh/issues/6148

--- de.metas.acct.base/src/main/sql/postgresql/ddl/functions/ProfitAndLossBalanceForAccountInPeriod.sql
+++ de.metas.acct.base/src/main/sql/postgresql/ddl/functions/ProfitAndLossBalanceForAccountInPeriod.sql
@@ -1,50 +1,82 @@
-DROP FUNCTION IF EXISTS ProfitAndLossBalanceForAccountInPeriod(IN p_C_ElementValue_ID numeric, IN p_startDate timestamp, IN p_endDate timestamp);
+/*
+ * #%L
+ * de.metas.acct.base
+ * %%
+ * Copyright (C) 2020 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 
-CREATE OR REPLACE FUNCTION ProfitAndLossBalanceForAccountInPeriod(IN p_C_ElementValue_ID numeric, IN p_startDate timestamp, IN p_endDate timestamp)
+DROP FUNCTION IF EXISTS ProfitAndLossBalanceForAccountInPeriod(IN p_C_ElementValue_ID numeric, IN p_startDate timestamp, IN p_endDate timestamp)
+;
+
+DROP FUNCTION IF EXISTS ProfitAndLossBalanceForAccountInPeriod(IN p_C_ElementValue_ID numeric, IN p_startDate timestamp, IN p_endDate timestamp, IN p_AD_Org_Id numeric)
+;
+
+/**
+  fact_acct_summary's amtacctdr and amtacctcr are always increasing, and exactly 1 per day per organisation per posting type.
+  Therefore calculating the balance for the winter sales period: 2019-12-20 -> 2020-02-01 is done by subtracting endDate balance - startDate balance as follows:
+  (2020-02-01.debit - 2020-02-01.credit) - (2019-12-20.debit - 2019-12-20.credit)
+ */
+CREATE OR REPLACE FUNCTION ProfitAndLossBalanceForAccountInPeriod(IN p_C_ElementValue_ID numeric,
+                                                                  IN p_startDate         timestamp,
+                                                                  IN p_endDate           timestamp,
+                                                                  IN p_AD_Org_Id         numeric)
     RETURNS NUMERIC
 AS
 $BODY$
 
-WITH periods_between_dates AS
+
+WITH factAcctSummaryInPeriod AS
          (
-             SELECT --
-                    p.c_period_id,
-                    p.startdate,
-                    p.enddate
-             FROM c_period p
-             WHERE TRUE
-               AND p.startdate >= p_startDate
-               AND p.enddate <= p_endDate
-         ),
-     fact_acct_summary_for_periods AS
-         (
-             -- fact_acct_summary filtered for selected period
              SELECT fas.*
-             FROM periods_between_dates p
-                      INNER JOIN fact_acct_summary fas ON fas.c_period_id = p.c_period_id
-             WHERE fas.account_id = p_C_ElementValue_ID
-         ),
-     result_table AS
-         (
-             SELECT coalesce(sum(acctbalance(fas.account_id, fas.amtacctdr, fas.amtacctcr)), 0) balance
              FROM c_elementvalue ev
-                      INNER JOIN fact_acct_summary_for_periods fas ON fas.account_id = ev.c_elementvalue_id
+                      INNER JOIN fact_acct_summary fas ON fas.account_id = ev.c_elementvalue_id
              WHERE TRUE
+               AND ev.c_elementvalue_id = p_C_ElementValue_ID
                AND ev.accounttype IN ('E', 'R')
+               AND fas.ad_org_id = p_AD_Org_ID
                -- only 'Actual' posting type is used
                AND fas.postingtype = 'A'
-             GROUP BY ev.value, ev.name
-             ORDER BY ev.name
+               AND fas.dateacct >= p_startDate
+               AND fas.dateacct <= p_endDate
+         ),
+     minFactAcctSummary AS
+         (
+             SELECT fas.*
+             FROM factAcctSummaryInPeriod fas
+             WHERE fas.dateacct = (SELECT min(fas2.dateacct) FROM factAcctSummaryInPeriod fas2)
+         ),
+     maxFactAcctSummary AS
+         (
+             SELECT fas.*
+             FROM factAcctSummaryInPeriod fas
+             WHERE fas.dateacct = (SELECT max(fas2.dateacct) FROM factAcctSummaryInPeriod fas2)
          )
-SELECT balance
-FROM result_table
 
-/**
-  Why is this union needed?
-  In case no `fact_acct_summary` for an account_id exists, `result_table` returns no rows. It doesn't return null (so that i can use coalesce), but returns NO rows.
-     example: SELECT ProfitAndLossBalanceForAccountInPeriod(1, '1993-01-01'::Timestamp, '2992-01-01'::Timestamp); -- this returns NO rows
-  I have simply added this union in case the select returns nothing. If you have a better solution, i'm more than happy to hear it, as this feels like a hack.
- */
+SELECT (maxfas.amtacctdr - maxfas.amtacctcr) - (minfas.amtacctdr - minfas.amtacctcr)
+FROM minFactAcctSummary minfas,
+     maxFactAcctSummary maxfas
+
+    /**
+      Why is this union needed?
+      In case no `fact_acct_summary` for an account_id exists, `result_table` returns no rows. It doesn't return null (so that i can use coalesce), but returns NO rows.
+         example: SELECT ProfitAndLossBalanceForAccountInPeriod(1, '1993-01-01'::Timestamp, '2992-01-01'::Timestamp, 1000000); -- this returns NO rows
+      I have simply added this union in case the select returns nothing. If you have a better solution, i'm more than happy to hear it, as this feels like a hack.
+     */
 UNION ALL
 (
     SELECT 0
@@ -53,7 +85,9 @@ LIMIT 1
     ;
 $BODY$
     LANGUAGE SQL
-    STABLE;
+    STABLE
+;
 
-ALTER FUNCTION ProfitAndLossBalanceForAccountInPeriod(numeric, timestamp, timestamp)
-    OWNER TO metasfresh;
+ALTER FUNCTION ProfitAndLossBalanceForAccountInPeriod(numeric, timestamp, timestamp, numeric)
+    OWNER TO metasfresh
+;
