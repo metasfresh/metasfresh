b0e62348f2a4a47921307846b1e18c54eb6eece3
Cristina <cristinamghita@gmail.com>
2020-03-18 15:32:40 +0200

    #6354 Add trl for the process
    #6354 Create a process that allows to set a parent folder for an account

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5554610_sys_gh6354_AddSetParentAccountProcess.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5554610_sys_gh6354_AddSetParentAccountProcess.sql
@@ -507,3 +507,20 @@ UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2020-03-18 13:58:58','Y
 UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2020-03-18 13:59:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=598472
 ;
 
+
+
+-- 2020-03-18T13:31:46.942Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Übergeordnetes Konto festlege',Updated=TO_TIMESTAMP('2020-03-18 15:31:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=584668
+;
+
+-- 2020-03-18T13:31:49.609Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-03-18 15:31:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584668
+;
+
+-- 2020-03-18T13:31:57.918Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Übergeordnetes Konto festlege',Updated=TO_TIMESTAMP('2020-03-18 15:31:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584668
+;
+
