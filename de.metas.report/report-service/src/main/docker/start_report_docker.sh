e5ed653ee3432e1b5386e3942d5e87318814b0ce
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-03-19 14:25:19 +0100

    attempt to fix deployment issue that i created earlier

--- de.metas.report/report-service/src/main/docker/start_report_docker.sh
+++ de.metas.report/report-service/src/main/docker/start_report_docker.sh
@@ -1,8 +1,21 @@
-﻿#!/bin/bash
+#!/bin/bash
+
+#title           :start_app.sh
+#
+#description     :initial start script for Docker container 
+#                 also used to update the database with custom SQLs (see 'run_db_update()')
+#
+#author          :tobias.schoeneberg@metasfresh.com
+#                :leaning on work done by julian.bischof@metasfresh.com
+#date            :2020-03-15
+#==============================================================================
 set -e
+set -u
 
 # The variables have defaults that can be set from outside, e.g. via -e "DB_HOST=mydbms" or from the docker-compose.yml file.
 # Also see https://docs.docker.com/engine/reference/builder/#environment-replacement
+
+# Container hostnames
 db_host=${DB_HOST:-db}
 db_port=${DB_PORT:-5432}
 db_name=${DB_NAME:-metasfresh}
@@ -12,37 +25,89 @@ db_wait_for_dbms=${DB_WAIT_FOR_DBMS:-y}
 db_connection_pool_max_size=${DB_CONNECTION_POOL_MAX_SIZE:-UNSET}
 app_host=${APP_HOST:-app}
 skip_run_db_update=${SKIP_DB_UPDATE:-false}
-debug_port=${DEBUG_PORT:-8791}
-debug_suspend=${DEBUG_SUSPEND:-n}
-debug_print_bash_cmds=${DEBUG_PRINT_BASH_CMDS:-n}
 admin_url=${METASFRESH_ADMIN_URL:-NONE}
 server_port=${SERVER_PORT:-8183}
-java_max_heap=${JAVA_MAX_HEAP:-128M}
 
-echo_variable_values()
-{
- echo "*************************************************************"
- echo "Display the variable values we run with"
- echo "*************************************************************"
- echo "Note: all these variables can be set using the -e parameter."
- echo ""
- echo "DB_HOST=${db_host}"
- echo "DB_PORT=${db_port}"
- echo "DB_NAME=${db_name}"
- echo "DB_USER=${db_user}"
- echo "DB_PASSWORD=*******"
- echo "DB_WAIT_FOR_DBMS=${db_wait_for_dbms}"
- echo "DB_CONNECTION_POOL_MAX_SIZE=${db_connection_pool_max_size}"
- echo "SKIP_DB_UPDATE=${skip_run_db_update}"
- echo "APP_HOST=${app_host}"
- echo "DEBUG_PORT=${debug_port}"
- echo "DEBUG_SUSPEND=${debug_suspend}"
- echo "DEBUG_PRINT_BASH_CMDS=${debug_print_bash_cmds}"
- echo "METASFRESH_ADMIN_URL=${admin_url}"
- echo "SERVER_PORT=${server_port}"
- echo "JAVA_MAX_HEAP=${java_max_heap}"
+# Java parameters
+java_disable_jar_checking=${JAVA_DISABLE_JAR_CHECKING:-true}
+java_max_heap=${JAVA_MAX_HEAP:-128M}
+java_property_file=${JAVA_PROPERTY_FILE:-/opt/metasfresh/metasfresh-report/metasfresh.properties}
+java_heap_dump_path=${JAVA_HEAP_DUMP_PATH:-/opt/metasfresh/heapdump}
+debug_port=${DEBUG_PORT:-8788}
+debug_suspend=${DEBUG_SUSPEND:-n}
+java_additional_jvm_params=${JAVA_JVM_PARAMS:-""}
+
+# Spring profiles
+spring_active_profiles=${SPRING_ACTIVE_PROFILES:-""}
+
+
+# logstash settings
+apm_server_url=${APM_SERVER_URL:-http://172.17.0.1:8200}
+
+# general settings
+metasfresh_instance_name=${METASFRESH_INSTANCE_NAME:-metasfresh}
+debug_print_bash_cmds=${DEBUG_PRINT_BASH_CMDS:-n}
+
+
+# output variables for easy debugging
+#
+echo_variable_values(){
+
+echo "
+*************************************************************
+Display the variable values we run with
+*************************************************************
+Note: all these variables can be set using the -e parameter.
+
+DB_HOST=${db_host}
+DB_PORT=${db_port}
+DB_NAME=${db_name}
+DB_USER=${db_user}
+DB_PASSWORD=*****
+DB_WAIT_FOR_DBMS=${db_wait_for_dbms}
+DB_CONNECTION_POOL_MAX_SIZE=${db_connection_pool_max_size}
+
+SKIP_DB_UPDATE=${skip_run_db_update}
+
+APP_HOST=${app_host}
+SERVER_PORT=${server_port}
+
+METASFRESH_INSTANCE_NAME=${metasfresh_instance_name}
+SPRING_ACTIVE_PROFILES=${spring_active_profiles}
+
+
+**JAVA SETTINGS**********************************************
+
+JAVA_MAX_HEAP=${java_max_heap}
+JAVA_DISABLE_JAR_CHECKING=${java_disable_jar_checking}
+JAVA_PROPERTY_FILE=${java_property_file}
+JAVA_HEAP_DUMP_PATH=${java_heap_dump_path}
+JAVA_JVM_PARAMS=${java_additional_jvm_params}
+"
+
+if [[ "${spring_active_profiles}" = *"logstash"* ]]; then
+echo "
+**LOGSTASH SETTINGS******************************************
+
+APM_SERVER_URL=${apm_server_url}
+
+"
+fi
+
+echo "
+**DEBUG SETTINGS*********************************************
+
+DEBUG_PRINT_BASH_CMDS=${debug_print_bash_cmds}
+DEBUG_PORT=${debug_port}
+DEBUG_SUSPEND=${debug_suspend}
+
+*************************************************************"
 }
 
+
+# replace "FOO_" with Docker internal hostname of "app" and "db" container
+# required for setting correct properties
+# 
 set_properties()
 {
  echo "set_properties BEGIN"
@@ -57,7 +122,8 @@ set_properties()
  fi
  echo "set_properties END"
 }
- 
+
+
 wait_dbms()
 {
 	echo "**************************************"
@@ -103,51 +169,66 @@ run_db_update()
  echo "Local migration scripts were run"
 }
 
+
+
+# resolve all required java parameters used for executing the application
+#
+resolve_java_params(){
+
 # Note: the Djava.security.egd param is supposed to let tomcat start quicker, see https://spring.io/guides/gs/spring-boot-docker/
-run_metasfresh()
-{
- if [ "$db_connection_pool_max_size" != "UNSET" ];
- then
- 	metasfresh_db_connectionpool_params="-Dc3p0.maxPoolSize=${db_connection_pool_max_size}"
- else 
-	metasfresh_db_connectionpool_params=""
- fi
 
- if [ "$admin_url" != "NONE" ]; 
- then
-	# see https://codecentric.github.io/spring-boot-admin/1.5.0/#spring-boot-admin-client
-	# spring.boot.admin.client.prefer-ip=true because within docker, the hostname is no help
-	metasfresh_admin_params="-Dspring.boot.admin.url=${admin_url} -Dmanagement.security.enabled=false -Dspring.boot.admin.client.prefer-ip=true"
- else
-	metasfresh_admin_params=""
- fi
-
- # add the external font jars we might have in the external lib folder
- # this assumes that the metasfresh-report.jar uses PropertiesLauncher (can be verified by opening the jar e.g. with 7-zip and checking META-INF/MANIFEST.MF)
- # Also see https://docs.spring.io/spring-boot/docs/current/reference/html/executable-jar.html#executable-jar-property-launcher-features
- ext_lib_param="-Dloader.path=/opt/metasfresh/metasfresh-report/external-lib"
-
- # thx to 
- # https://blog.csanchez.org/2017/05/31/running-a-jvm-in-a-container-without-getting-killed/
-# MaxRAMFraction=1 doesn't any emory for anything else and might cause the OS to kill the java process
-# local MEMORY_PARAMS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
-local MEMORY_PARAMS="-Xmx${java_max_heap}"
-
- cd /opt/metasfresh/metasfresh-report/ && java -Dsun.misc.URLClassPath.disableJarChecking=true \
- ${ext_lib_param}\
- ${MEMORY_PARAMS}\
- -XX:+HeapDumpOnOutOfMemoryError ${metasfresh_admin_params}\
- ${metasfresh_db_connectionpool_params}\
- -DPropertyFile=/opt/metasfresh/metasfresh-report/metasfresh.properties\
+java_params=" \
+ -Dsun.misc.URLClassPath.disableJarChecking=${java_disable_jar_checking} \
+ -Xmx${java_max_heap}\
+ -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${java_heap_dump_path} \
+ -DPropertyFile=${java_property_file} \
  -Djava.security.egd=file:/dev/./urandom\
  -Dserver.port=${server_port}\
- -agentlib:jdwp=transport=dt_socket,server=y,suspend=${debug_suspend},address=${debug_port}\
- org.springframework.boot.loader.PropertiesLauncher
+ -agentlib:jdwp=transport=dt_socket,server=y,suspend=${debug_suspend},address=${debug_port} \
+ -Dspring.profiles.active=${spring_active_profiles} \
+"
+
+if [ "$db_connection_pool_max_size" != "UNSET" ]; then
+	java_params="${java_params} -Dc3p0.maxPoolSize=${db_connection_pool_max_size}"
+fi
+
+if [ "$admin_url" != "NONE" ]; then
+	# see https://codecentric.github.io/spring-boot-admin/1.5.0/#spring-boot-admin-client
+	# spring.boot.admin.client.prefer-ip=true because within docker, the hostname is no help
+	java_params="${java_params} -Dspring.boot.admin.url=${admin_url} -Dmanagement.security.enabled=false -Dspring.boot.admin.client.prefer-ip=true"
+fi
+
+# add the external font jars we might have in the external lib folder
+# this assumes that the metasfresh-report.jar uses PropertiesLauncher (can be verified by opening the jar e.g. with 7-zip and checking META-INF/MANIFEST.MF)
+# Also see https://docs.spring.io/spring-boot/docs/current/reference/html/executable-jar.html#executable-jar-property-launcher-features
+java_params="${java_params} -Dloader.path=/opt/metasfresh/metasfresh-report/external-lib"
+
+if [[ -n "$spring_active_profiles" ]]; then
+    case "${spring_active_profiles}" in
+        *"logstash"*) logstash_java_params="-javaagent:/opt/metasfresh/metasfresh-report/elastic-apm-agent.jar \
+                                        -Delastic.apm.application_packages=de.metas.*,org.compiere.*,org.adempiere.* \
+                                        -Delastic.apm.server_urls=${apm_server_url} \
+                                        "
+                      java_params="${java_params} ${logstash_java_params}"
+                      ;;
+    esac
+fi
+
+java_params="${java_params} ${java_additional_jvm_params}"
+
 }
 
-echo_variable_values
+# start the application with java parameters generated in resolve_java_params()
+#
+run_metasfresh(){
 
-# start printing all bash commands from here onwards, if activated
+ resolve_java_params
+ cd /opt/metasfresh/metasfresh-report/ && java ${java_params} org.springframework.boot.loader.PropertiesLauncher
+ 
+}
+
+# start printing all bash commands from here onwards, if "DEBUG_PRINT_BASH_CMDS" is set
+#
 if [ "$debug_print_bash_cmds" != "n" ];
 then
 	echo "DEBUG_PRINT_BASH_CMDS=${debug_print_bash_cmds}, so from here we will output all bash commands; set to n (just the lowercase letter) to skip this."
@@ -175,5 +256,6 @@ echo "*************************************************************"
 echo "Start metasfresh-report";
 echo "*************************************************************"
 run_metasfresh
+
 
 exit 0 
