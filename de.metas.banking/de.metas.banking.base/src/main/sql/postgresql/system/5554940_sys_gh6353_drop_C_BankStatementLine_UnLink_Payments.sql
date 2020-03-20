7165f369773dca6c53bafcd4ea601f88451417a8
Teo Sarca <teo.sarca@metasfresh.com>
2020-03-20 08:48:42 +0200

    drop C_BankStatementLine_UnLink_Payments

---
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5554940_sys_gh6353_drop_C_BankStatementLine_UnLink_Payments.sql
@@ -0,0 +1,15 @@
+-- 2020-03-20T06:47:47.258Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Table_Process WHERE AD_Table_Process_ID=540493
+;
+
+-- 2020-03-20T06:47:57.492Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Process_Trl WHERE AD_Process_ID=540629
+;
+
+-- 2020-03-20T06:47:57.503Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Process WHERE AD_Process_ID=540629
+;
+
