87ade74b712656d90ea3c32bdc4c11e2c8d8d421
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-03-23 06:58:01 +0100

    support manual invoice as commission trigger
    * fix sql file name and content
    metasfresh/metasfresh#6320

--- de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5555025_sys_gh6320-app_CommissionDate_migrate_and_make_mandatory.sql
+++ de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5555025_sys_gh6320-app_CommissionDate_migrate_and_make_mandatory.sql
@@ -15,6 +15,14 @@ SET
 WHERE ci.CommissionDate IS NULL AND ci.C_Invoice_ID IS NOT NULL
 ;
 
+UPDATE C_Commission_Instance ci 
+SET 
+    CommissionDate=ci.Created,
+    UpdatedBy=99,
+    Updated='2020-03-22T10:55:47.844Z'
+WHERE ci.CommissionDate IS NULL
+;
+
 -- 2020-03-22T11:02:13.667Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2020-03-22 12:02:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=570122
