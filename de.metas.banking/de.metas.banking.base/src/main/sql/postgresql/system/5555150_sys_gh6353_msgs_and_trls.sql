275078be1302b7b3a76cf20302e46c470176e203
Teo Sarca <teo.sarca@metasfresh.com>
2020-03-24 11:32:19 +0200

    Create 5555150_sys_gh6353_msgs_and_trls.sql

---
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5555150_sys_gh6353_msgs_and_trls.sql
@@ -0,0 +1,100 @@
+-- 2020-03-24T09:02:18.287Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Geschäftspartner und Zahlung zuordnen',Updated=TO_TIMESTAMP('2020-03-24 11:02:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584643
+;
+
+-- 2020-03-24T09:02:28.329Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Geschäftspartner und Zahlung zuordnen',Updated=TO_TIMESTAMP('2020-03-24 11:02:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584643
+;
+
+-- 2020-03-24T09:02:48.566Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Zahlung-Zuordnung',Updated=TO_TIMESTAMP('2020-03-24 11:02:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584646
+;
+
+-- 2020-03-24T09:02:58.962Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Zahlung-Zuordnung',Updated=TO_TIMESTAMP('2020-03-24 11:02:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584646
+;
+
+-- 2020-03-24T09:03:27.331Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Mit ESR Zahlungsimport abgleichen',Updated=TO_TIMESTAMP('2020-03-24 11:03:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584670
+;
+
+-- 2020-03-24T09:03:31.178Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Mit ESR Zahlungsimport abgleichen',Updated=TO_TIMESTAMP('2020-03-24 11:03:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=584670
+;
+
+-- 2020-03-24T09:03:33.530Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Mit ESR Zahlungsimport abgleichen',Updated=TO_TIMESTAMP('2020-03-24 11:03:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584670
+;
+
+-- 2020-03-24T09:03:35.182Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-03-24 11:03:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584670
+;
+
+-- 2020-03-24T09:04:36.391Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Mit angewiesenen Zahlungen abgleichen',Updated=TO_TIMESTAMP('2020-03-24 11:04:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584667
+;
+
+-- 2020-03-24T09:04:41.445Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Mit angewiesenen Zahlungen abgleichen',Updated=TO_TIMESTAMP('2020-03-24 11:04:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=584667
+;
+
+-- 2020-03-24T09:04:47.199Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Mit angewiesenen Zahlungen abgleichen',Updated=TO_TIMESTAMP('2020-03-24 11:04:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584667
+;
+
+-- 2020-03-24T09:04:49.973Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-03-24 11:04:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584667
+;
+
+-- 2020-03-24T09:05:23.003Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Reverse payment reconciliation',Updated=TO_TIMESTAMP('2020-03-24 11:05:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584671
+;
+
+-- 2020-03-24T09:05:31.551Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Reverse payment reconciliation',Updated=TO_TIMESTAMP('2020-03-24 11:05:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584671
+;
+
+-- 2020-03-24T09:05:46.044Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Zahlungsabgleich rückgängig machen',Updated=TO_TIMESTAMP('2020-03-24 11:05:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584671
+;
+
+-- 2020-03-24T09:05:52.689Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Zahlungsabgleich rückgängig machen',Updated=TO_TIMESTAMP('2020-03-24 11:05:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=584671
+;
+
+-- 2020-03-24T09:05:57.954Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Zahlungsabgleich rückgängig machen',Updated=TO_TIMESTAMP('2020-03-24 11:05:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584671
+;
+
+-- 2020-03-24T09:06:25.829Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET Name='Revert payment reconciliation',Updated=TO_TIMESTAMP('2020-03-24 11:06:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584671
+;
+
+-- 2020-03-24T09:31:39.880Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,544970,0,TO_TIMESTAMP('2020-03-24 11:31:39','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.banking','Y','Amt To Reconcile: {0}','I',TO_TIMESTAMP('2020-03-24 11:31:39','YYYY-MM-DD HH24:MI:SS'),100,'StatementLineAmtToReconcileIs')
+;
+
+-- 2020-03-24T09:31:39.884Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Message_ID=544970 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
