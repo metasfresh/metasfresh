66830753bd781411d1406a032ba965dd3844a32b
TheBestPessimist <cristian@tbp.land>
2020-03-24 09:34:48 +0200

    Check/ Fix Barcode Filtering and `Materialentnahme`=`Material W… (#1389)
    * Show error notification when `Material Withdrawal` cannot be performed
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * List default filter was null
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Fix warnings, mark nullability
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Refactoring
    
    Replace `!Check.isBlank` with `Check.isNotBlank`
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Enable searching for non Top Level HUs in HUEditor->BarcodeSearch
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Fix warnings, mark nullability
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Implement autoFiltering for HU Editor Window
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * prototyping: handing lookup values for auto filters
    
    * Small refactoring
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Apply default filters only for default HU Editor window
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Add UseAutoFilters to `HUEditorViewBuilder`
    
    https://github.com/metasfresh/metasfresh/issues/6365
    
    * Update src/main/java/de/metas/ui/web/view/SqlViewFactory.java
    
    metasfresh/metasfresh#6365
    
    Co-authored-by: Teo Sarca <teo.sarca@metasfresh.com>
    Co-authored-by: Tobias Schöneberg <metas-ts@users.noreply.github.com>

--- src/main/java/de/metas/ui/web/handlingunits/HUEditorViewBuilder.java
+++ src/main/java/de/metas/ui/web/handlingunits/HUEditorViewBuilder.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * metasfresh-webui-api
+ * %%
+ * Copyright (C) 2020 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.ui.web.handlingunits;
 
 import java.util.ArrayList;
@@ -24,28 +46,6 @@ import de.metas.ui.web.window.model.DocumentQueryOrderBy;
 import de.metas.ui.web.window.model.DocumentQueryOrderByList;
 import lombok.NonNull;
 
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2017 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 public final class HUEditorViewBuilder
 {
 	private ViewId parentViewId;
@@ -67,6 +67,7 @@ public final class HUEditorViewBuilder
 
 	private LinkedHashMap<String, Object> parameters;
 	private HUEditorViewRepository huEditorViewRepository;
+	private boolean useAutoFilters;
 
 	HUEditorViewBuilder()
 	{
@@ -310,5 +311,16 @@ public final class HUEditorViewBuilder
 		{
 			return new HUEditorViewBuffer_FullyCached(viewId, huEditorViewRepository, stickyFilters, filters, getOrderBys(), context);
 		}
+	}
+
+	public HUEditorViewBuilder setUseAutoFilters(final boolean useAutoFilters)
+	{
+		this.useAutoFilters = useAutoFilters;
+		return this;
+	}
+
+	public boolean isUseAutoFilters()
+	{
+		return useAutoFilters;
 	}
 }
--- src/main/java/de/metas/ui/web/view/descriptor/SqlViewSelectionQueryBuilder.java
+++ src/main/java/de/metas/ui/web/view/descriptor/SqlViewSelectionQueryBuilder.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * metasfresh-webui-api
+ * %%
+ * Copyright (C) 2020 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.ui.web.view.descriptor;
 
 import java.util.ArrayList;
@@ -47,28 +69,6 @@ import de.metas.util.Check;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
-
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2017 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 /**
  * Helper class used to build the SQL queries needed for creating and manipulating the view selections.
@@ -153,7 +153,7 @@ public final class SqlViewSelectionQueryBuilder
 
 		final SqlParamsCollector sqlParamsOut = SqlParamsCollector.newInstance();
 		final String sql = getSqlDocumentFilterConverter().getSql(sqlParamsOut, filters, sqlOpts, context);
-		return !Check.isBlank(sql)
+		return Check.isNotBlank(sql)
 				? SqlAndParams.of(sql, sqlParamsOut.toList())
 				: null;
 	}
