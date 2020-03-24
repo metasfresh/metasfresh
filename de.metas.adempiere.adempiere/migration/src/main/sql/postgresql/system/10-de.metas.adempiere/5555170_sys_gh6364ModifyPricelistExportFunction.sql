fbfd73b51401460170b6f80b916fe478fae8caec
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-03-24 12:07:14 +0200

    #6364 implemented review notes.

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5555170_sys_gh6364ModifyPricelistExportFunction.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5555170_sys_gh6364ModifyPricelistExportFunction.sql
@@ -20,7 +20,6 @@ CREATE OR REPLACE FUNCTION report.fresh_pricelist_details_template_report(IN p_c
                 qtycuspertu             numeric,
                 bp_value                text,
                 bp_name                 text,
-                validfrom               timestamp without time zone,
                 reportfilename          text
             )
 
@@ -28,30 +27,32 @@ AS
 $BODY$
 --
 
-SELECT plc.value                                                                                                              AS prodvalue,
-       plc.customerproductnumber                                                                                              as customerproductnumber,
-       plc.productcategory                                                                                                    as productcategory,
-       plc.productname                                                                                                        as productname,
-       plc.attributes                                                                                                         as attributes,
-       coalesce(hupiv.description, hupip.name, plc.uomsymbol)                                                                 as itemproductname,
-       NULL::numeric                                                                                                          as qty,
-       plc.uomsymbol                                                                                                          as uomsymbol,
-       cast(plc.pricestd as decimal(19, 2))::text                                                                             as pricestd,
-       plc.M_ProductPrice_ID                                                                                                  as m_productprice_id,
-       p_c_bpartner_id                                                                                                        as c_bpartner_id,
-       plc.M_HU_PI_Item_Product_ID                                                                                            as m_hu_pi_item_product_id,
-       case when plc.m_hu_pi_item_product_id is not null then 'COLI' else plc.uom_x12de355 end                                as uom_x12de355,
-       p_c_bpartner_location_id                                                                                               as c_bpartner_location_id,
-       plc.qtycuspertu                                                                                                        as qtycuspertu,
-       plc.BP_Value                                                                                                           as bp_value,
-       plc.BP_Name                                                                                                            as bp_name,
-       (SELECT validfrom FROM M_Pricelist_Version WHERE M_Pricelist_Version_ID = p_m_pricelist_version_id AND isActive = 'Y') AS validfrom,
-       CONCAT(bp_value, '_', bp_name, '_', validfrom, '.xls')                                                                 as reportfilename
+SELECT plc.value                                                                                                    AS prodvalue,
+       plc.customerproductnumber                                                                                    as customerproductnumber,
+       plc.productcategory                                                                                          as productcategory,
+       plc.productname                                                                                              as productname,
+       plc.attributes                                                                                               as attributes,
+       coalesce(hupiv.description, hupip.name, plc.uomsymbol)                                                       as itemproductname,
+       NULL::numeric                                                                                                as qty,
+       plc.uomsymbol                                                                                                as uomsymbol,
+       round(plc.pricestd, cur.stdprecision)::text                                                                  as pricestd,
+       plc.M_ProductPrice_ID                                                                                        as m_productprice_id,
+       p_c_bpartner_id                                                                                              as c_bpartner_id,
+       plc.M_HU_PI_Item_Product_ID                                                                                  as m_hu_pi_item_product_id,
+       case when plc.m_hu_pi_item_product_id is not null then 'COLI' else plc.uom_x12de355 end                      as uom_x12de355,
+       p_c_bpartner_location_id                                                                                     as c_bpartner_location_id,
+       plc.qtycuspertu                                                                                              as qtycuspertu,
+       plc.BP_Value                                                                                                 as bp_value,
+       plc.BP_Name                                                                                                  as bp_name,
+       CONCAT(bp_value, '_', bp_name, '_', case when prlv.isactive = 'Y' then prlv.validfrom else null end, '.xls') as reportfilename
 
 FROM report.fresh_PriceList_Details_Report(p_c_bpartner_id, p_m_pricelist_version_id, NULL, p_ad_language) plc
          LEFT OUTER JOIN M_HU_PI_Item_Product hupip on hupip.M_HU_PI_Item_Product_ID = plc.M_HU_PI_Item_Product_ID
          LEFT OUTER JOIN M_HU_PI_Item hupii on hupii.M_HU_PI_Item_ID = hupip.M_HU_PI_Item_ID
          LEFT OUTER JOIN M_HU_PI_Version hupiv on hupiv.M_HU_PI_Version_ID = hupii.M_HU_PI_Version_ID
+         LEFT OUTER JOIN M_Pricelist_Version prlv on prlv.m_pricelist_version_id = p_m_pricelist_version_id
+         LEFT OUTER JOIN M_Pricelist prl on prlv.m_pricelist_id = prl.m_pricelist_id
+         LEFT OUTER JOIN C_Currency cur on prl.c_currency_id = cur.c_currency_id
 --
 
 
