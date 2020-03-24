f6915945fffbcf16aa6624d92ac52f3e918e2707
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-03-24 22:41:08 +0100

    SQL syntax fix

--- de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5555180_sys_AD_Process_IsNotifyUserAfterExecution_DDL.sql
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5555180_sys_AD_Process_IsNotifyUserAfterExecution_DDL.sql
@@ -3,7 +3,7 @@ DO $$
 BEGIN
 -- 2020-02-11T22:27:45.947Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-/* DDL */ SELECT public.db_alter_table('AD_Process','ALTER TABLE public.AD_Process ADD COLUMN IsNotifyUserAfterExecution CHAR(1) DEFAULT ''N'' CHECK (IsNotifyUserAfterExecution IN (''Y'',''N'')) NOT NULL')
+/* DDL */ PERFORM public.db_alter_table('AD_Process','ALTER TABLE public.AD_Process ADD COLUMN IsNotifyUserAfterExecution CHAR(1) DEFAULT ''N'' CHECK (IsNotifyUserAfterExecution IN (''Y'',''N'')) NOT NULL')
 ;
 EXCEPTION WHEN SQLSTATE '42701' THEN
     RAISE NOTICE 'At least one column already existed; Ignoring; Error-Code %, Message=%', SQLSTATE, SQLERRM;
