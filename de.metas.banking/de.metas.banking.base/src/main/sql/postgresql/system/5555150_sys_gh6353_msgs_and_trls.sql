fb528d40a3d14adac533d9a5010a16f91609f7de
Teo Sarca <teo.sarca@metasfresh.com>
2020-03-24 11:47:49 +0200

    Update 5555150_sys_gh6353_msgs_and_trls.sql

--- de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5555150_sys_gh6353_msgs_and_trls.sql
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5555150_sys_gh6353_msgs_and_trls.sql
@@ -98,3 +98,13 @@ INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,E
 INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Message_ID=544970 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
 ;
 
+-- 2020-03-24T09:47:15.668Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Abzugleichender Betrag: {0}',Updated=TO_TIMESTAMP('2020-03-24 11:47:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=544970
+;
+
+-- 2020-03-24T09:47:21.697Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Abzugleichender Betrag: {0}',Updated=TO_TIMESTAMP('2020-03-24 11:47:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=544970
+;
+
