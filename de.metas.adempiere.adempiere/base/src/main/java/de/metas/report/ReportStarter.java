944fcb7d933c6ddeb5583a3b9e00063ca581763c
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-03-25 15:33:01 +0200

    #6364 Implemented review notes

--- de.metas.adempiere.adempiere/base/src/main/java/de/metas/report/ReportStarter.java
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/report/ReportStarter.java
@@ -180,7 +180,17 @@ public abstract class ReportStarter extends JavaProcess
 		//
 		// Set report data to process execution result
 		final ProcessExecutionResult processExecutionResult = processInfo.getResult();
-		final String reportFilename = result.getFilename() != null ? result.getFilename() : extractReportFilename(processInfo, outputType);
+		final String reportFilename;
+		if (Check.isNotBlank(result.getFilename()))
+		{
+			reportFilename = result.getFilename();
+			logger.debug("executeReport's result has a non-blank filename={}; -> use it for the exported file", reportFilename);
+		}
+		else
+		{
+			reportFilename = extractReportFilename(processInfo, outputType);
+			logger.debug("executeReport's result has a blank filename; -> use generic filename={} for the exported file", result.getFilename());
+		}
 		final String reportContentType = outputType.getContentType();
 		processExecutionResult.setReportData(result.getReportData(), reportFilename, reportContentType);
 
