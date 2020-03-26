0f2c699bd68786b740fead783b32e39ee78ec372
Teo Sarca <teo.sarca@metasfresh.com>
2020-03-26 09:31:49 +0200

    Update 5555241_cli_gh6353_C_PaySelection_IsReconciled_migrate.sql

--- de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5555241_cli_gh6353_C_PaySelection_IsReconciled_migrate.sql
+++ de.metas.banking/de.metas.banking.base/src/main/sql/postgresql/system/5555241_cli_gh6353_C_PaySelection_IsReconciled_migrate.sql
@@ -1,5 +1,5 @@
 UPDATE c_payselection ps
-SET isactive=(
+SET isreconciled=(
     CASE
         WHEN (SELECT count(1) FROM c_payselectionline psl WHERE psl.c_payselection_id = ps.c_payselection_id AND psl.isactive = 'Y' AND c_bankstatement_id IS NULL) = 0
             THEN 'Y'
