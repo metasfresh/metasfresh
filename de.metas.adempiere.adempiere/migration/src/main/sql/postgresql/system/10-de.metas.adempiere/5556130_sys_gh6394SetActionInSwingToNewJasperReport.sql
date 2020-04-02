467002e19759bd6f19b8dd52ca252c66b30ae60c
TheBestPessimist <cristian@tbp.land>
2020-04-02 20:44:57 +0300

    Fine tuning after review (#6431)
    * #6394 Showing logo on each page and the right total pages count!
    
    * Add `AD_Org_ID` to report
    
    https://github.com/metasfresh/metasfresh/issues/6394
    
    * #6394 fix organisation and logo subreport.
    
    * Add `AD_Org_ID` to report
    
    https://github.com/metasfresh/metasfresh/issues/6394
    
    * #6394 logo subreport.
    
    * Return bpartner location id instead of name
    
    https://github.com/metasfresh/metasfresh/issues/6394
    
    * Update default param value
    
    https://github.com/metasfresh/metasfresh/issues/6394
    
    * #6394 updated report according to customer needs.
    
    * #6394 Updated Report!
    
    * #6394 Renamed&moved new, shorter height bussinesspartner info general report.
    
    Co-authored-by: dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5556130_sys_gh6394SetActionInSwingToNewJasperReport.sql
@@ -0,0 +1,5 @@
+-- 2020-04-02T14:09:46.382Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET JasperReport='@PREFIX@de/metas/reports/pricelist/report_CurrentVsLastPricelistComparison.jasper', JasperReport_Tabular='@PREFIX@de/metas/reports/pricelist/report_CurrentVsLastPricelistComparison.jasper',Updated=TO_TIMESTAMP('2020-04-02 17:09:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584680
+;
+
