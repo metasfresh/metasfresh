651edce15e4e50a53c24dd9bfe562c161f089c13
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-04-11 14:24:02 +0200

    gh6205-app - fix the next stupid mistake
    #6205

--- misc/services/procurement-webui/buildfile.groovy
+++ misc/services/procurement-webui/buildfile.groovy
@@ -8,52 +8,51 @@ import de.metas.jenkins.MvnConf
 import de.metas.jenkins.Misc
 
 
-
-final String VERSIONS_PLUGIN = 'org.codehaus.mojo:versions-maven-plugin:2.5'
-
 def build(final MvnConf mvnConf, final Map scmVars)
 {
-        stage('Build procurement-webui')
-        {
+	final String VERSIONS_PLUGIN = 'org.codehaus.mojo:versions-maven-plugin:2.5'
+	
+    stage('Build procurement-webui')
+    {
+		currentBuild.description= """${currentBuild.description}<p/>
+			<h2>procurement-webui</h2>
+		"""
+		def status = sh(returnStatus: true, script: "git diff --name-only ${scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT} ${scmVars.GIT_COMMIT} . | grep .") // see if anything at all changed in this folder
+		echo "status of git dif command=${status}"
+		if(scmVars.GIT_COMMIT && scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT && status != 0)
+		{
 			currentBuild.description= """${currentBuild.description}<p/>
-				<h2>procurement-webui</h2>
+			No changes happened in procurement-webu.
 			"""
-			def status = sh(returnStatus: true, script: "git diff --name-only ${scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT} ${scmVars.GIT_COMMIT} . | grep .") // see if anything at all changed in this folder
-			echo "status of git dif command=${status}"
-			if(scmVars.GIT_COMMIT && scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT && status != 0)
-			{
-				currentBuild.description= """${currentBuild.description}<p/>
-				No changes happened in procurement-webu.
-				"""
-				echo "no changes happened in procurement-webu; skip building procurement-webu";
-				return;
-			}
-			
-			// update the parent pom version
-			mvnUpdateParentPomVersion mvnConf
+			echo "no changes happened in procurement-webu; skip building procurement-webu";
+			return;
+		}
+		
+		// update the parent pom version
+		mvnUpdateParentPomVersion mvnConf
 
-			final String mavenUpdatePropertyParam'-Dproperty=metasfresh.version';
-			
-			// update the metasfresh.version property. either to the latest version or to the given params.MF_UPSTREAM_VERSION.
-			sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode ${mvnConf.resolveParams} ${mavenUpdatePropertyParam} ${VERSIONS_PLUGIN}:update-property"
+		final String mavenUpdatePropertyParam'-Dproperty=metasfresh.version';
+		
+		// update the metasfresh.version property. either to the latest version or to the given params.MF_UPSTREAM_VERSION.
+		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode ${mvnConf.resolveParams} ${mavenUpdatePropertyParam} ${VERSIONS_PLUGIN}:update-property"
 
-			// set the artifact version of everything below the webui's pom.xml
-			sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DnewVersion=${MF_VERSION} -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:set"
+		// set the artifact version of everything below the webui's pom.xml
+		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DnewVersion=${MF_VERSION} -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:set"
 
-			// do the actual building and deployment
-			// maven.test.failure.ignore=true: continue if tests fail, because we want a full report.
-			sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dmaven.test.failure.ignore=true ${mvnConf.resolveParams} ${mvnConf.deployParam} clean deploy"
+		// do the actual building and deployment
+		// maven.test.failure.ignore=true: continue if tests fail, because we want a full report.
+		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dmaven.test.failure.ignore=true ${mvnConf.resolveParams} ${mvnConf.deployParam} clean deploy"
 
-			currentBuild.description="""${currentBuild.description}<p/>
-			artifacts (if not yet cleaned up)
-				<ul>
+		currentBuild.description="""${currentBuild.description}<p/>
+		artifacts (if not yet cleaned up)
+			<ul>
 <li><a href=\"https://repo.metasfresh.com/content/repositories/${mvnConf.mvnRepoName}/de/metas/procurement/de.metas.procurement.webui/${MF_VERSION}/de.metas.procurement.webui-${MF_VERSION}.jar\">de.metas.procurement.webui-${MF_VERSION}.jar</a></li>
 </ul>""";
 
-				junit '**/target/surefire-reports/*.xml'
+		junit '**/target/surefire-reports/*.xml'
 
-				jacoco exclusionPattern: '**/src/main/java-gen'
-      } // stage
+		jacoco exclusionPattern: '**/src/main/java-gen'
+    } // stage
 } 
 
 return this
