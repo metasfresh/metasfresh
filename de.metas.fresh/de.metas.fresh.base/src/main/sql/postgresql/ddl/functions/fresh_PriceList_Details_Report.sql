e1f6f43131ccbf65fefb7cb97f009490925ef956
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-04-13 18:22:32 +0300

    #6475 Intermediary commit.

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/fresh_PriceList_Details_Report.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/fresh_PriceList_Details_Report.sql
@@ -66,6 +66,7 @@ WHERE TRUE
   AND plc.C_BPartner_ID = p_c_bpartner_id
   AND plc.M_Pricelist_Version_ID = p_m_pricelist_version_id
   AND plc.Alt_Pricelist_Version_ID = coalesce(p_alt_pricelist_version_id, plc.m_pricelist_version_id)
+  AND ppr.m_product_id is not null
   AND CASE
           WHEN p_alt_pricelist_version_id IS NOT NULL
               THEN plc.PriceStd != 0
