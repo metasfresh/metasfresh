58a5b5da43a0ea91f1b9048a774321c677974e3a
TheBestPessimist <cristian@tbp.land>
2020-04-21 08:53:59 +0300

    Hide PriceListVersion.Name from webui; Update PLV identifier
    https://github.com/metasfresh/metasfresh/issues/6485

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5557260_sys_gh6485_hide_PLVName_FromUI.sql
@@ -0,0 +1,15 @@
+-- 2020-04-15T07:00:01.213Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET SeqNo=10, IsUpdateable='N',Updated=TO_TIMESTAMP('2020-04-15 10:00:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2997
+;
+
+-- 2020-04-15T07:00:16.773Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET SeqNo=20,Updated=TO_TIMESTAMP('2020-04-15 10:00:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2998
+;
+
+-- 2020-04-15T10:49:25.520Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsActive='N',Updated=TO_TIMESTAMP('2020-04-15 13:49:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541383
+;
+
