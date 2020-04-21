25089114546b0da18830a2f2b14f008c70ad43b3
TheBestPessimist <cristian@tbp.land>
2020-04-21 15:22:17 +0300

    Update trl
    https://github.com/metasfresh/metasfresh/issues/6485

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5557610_sys_gh6485_UpdateTrl.sql
@@ -0,0 +1,35 @@
+-- 2020-04-21T11:44:29.596Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-04-21 14:44:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Element_ID=573824
+;
+
+-- 2020-04-21T11:44:29.639Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573824,'de_CH') 
+;
+
+-- 2020-04-21T11:44:39.551Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET PrintName='PriceList Version', IsTranslated='Y', Name='PriceList Version',Updated=TO_TIMESTAMP('2020-04-21 14:44:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Element_ID=573824
+;
+
+-- 2020-04-21T11:44:39.552Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573824,'en_US') 
+;
+
+-- 2020-04-21T11:44:43.117Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-04-21 14:44:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Element_ID=573824
+;
+
+-- 2020-04-21T11:44:43.118Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573824,'de_DE') 
+;
+
+-- 2020-04-21T11:44:43.140Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_ad_element_on_ad_element_trl_update(573824,'de_DE') 
+;
+
