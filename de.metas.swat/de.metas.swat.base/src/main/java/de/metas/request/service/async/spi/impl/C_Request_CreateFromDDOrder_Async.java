66faf4423f2aa11c1ef3e4fdd9f675f4499eda31
TheBestPessimist <cristian@tbp.land>
2020-04-22 14:59:40 +0300

    Update references to TableRecordReference; fix warnings (2)
    https://github.com/metasfresh/metasfresh/issues/6504

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/request/service/async/spi/impl/C_Request_CreateFromDDOrder_Async.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/request/service/async/spi/impl/C_Request_CreateFromDDOrder_Async.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.swat.base
+ * %%
+ * Copyright (C) 2020 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.request.service.async.spi.impl;
 
 import static org.adempiere.model.InterfaceWrapperHelper.load;
@@ -17,28 +39,6 @@ import de.metas.async.spi.WorkpackagesOnCommitSchedulerTemplate;
 import de.metas.request.api.IRequestBL;
 import de.metas.util.Check;
 import de.metas.util.Services;
-
-/*
- * #%L
- * de.metas.swat.base
- * %%
- * Copyright (C) 2018 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 public class C_Request_CreateFromDDOrder_Async extends WorkpackageProcessorAdapter
 {
@@ -108,7 +108,7 @@ public class C_Request_CreateFromDDOrder_Async extends WorkpackageProcessorAdapt
 		@Override
 		protected Object extractModelToEnqueueFromItem(final Collector collector, final I_DD_OrderLine item)
 		{
-			return new TableRecordReference(I_DD_OrderLine.Table_Name, item.getDD_OrderLine_ID());
+			return TableRecordReference.of(I_DD_OrderLine.Table_Name, item.getDD_OrderLine_ID());
 		}
 	};
 
