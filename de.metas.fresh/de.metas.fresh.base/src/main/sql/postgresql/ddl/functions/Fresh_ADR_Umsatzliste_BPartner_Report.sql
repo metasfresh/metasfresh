0f85203ab1c80c702943967808cb4788935466ab
Cristina Ghita <cristina.ghita@metasfresh.com>
2020-04-28 20:35:20 +0300

    #6527 Do not filter out inactive products (#6528)

--- de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Fresh_ADR_Umsatzliste_BPartner_Report.sql
+++ de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Fresh_ADR_Umsatzliste_BPartner_Report.sql
@@ -77,7 +77,7 @@ SELECT
 			$5, --$P{M_AttributeSetInstance_ID}
 			$7 -- AD_Org_ID
 			) um 
-	join m_product p on p.name = um.p_name AND p.isActive = 'Y' AND p.ad_org_id = $7
+	join m_product p on p.name = um.p_name AND p.ad_org_id = $7
 	LEFT OUTER JOIN M_Product_Trl pt ON p.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = $6 AND pt.isActive = 'Y'
 	left join C_BPartner_Product bpp  ON p.M_Product_ID = bpp.M_Product_ID and bpp.c_bpartner_id = $4 AND bpp.isActive = 'Y'
 	group by  COALESCE(pt.name, p.name), bpp.productcategory,um.ad_org_id 
