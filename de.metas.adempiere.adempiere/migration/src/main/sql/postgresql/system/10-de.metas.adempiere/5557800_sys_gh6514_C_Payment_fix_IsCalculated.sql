7bc1c8f42a60e52644a5e77a91175a1886d0435a
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2020-05-06 10:21:10 +0200

    Gh6514 (#6585)

---
+++ de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5557800_sys_gh6514_C_Payment_fix_IsCalculated.sql
@@ -0,0 +1,25 @@
+-- 2020-04-23T09:52:14.545Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2020-04-23 12:52:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3879
+;
+
+-- 2020-04-23T09:52:30.219Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2020-04-23 12:52:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=570629
+;
+
+-- 2020-04-23T09:52:36.808Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2020-04-23 12:52:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=570630
+;
+
+-- 2020-04-23T09:52:42.843Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2020-04-23 12:52:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=570631
+;
+
+-- 2020-04-23T09:53:09.630Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2020-04-23 12:53:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=55309
+;
+
