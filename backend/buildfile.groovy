e836424c571babd68be35f6288b92544e1f69fc8
metas-ts <metas-ts@users.noreply.github.com>
2020-05-07 20:53:35 +0200

    apply build-system-fix written in blue_moon

--- backend/buildfile.groovy
+++ backend/buildfile.groovy
@@ -27,7 +27,7 @@ Map build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild=fal
 			}
 			final String VERSIONS_PLUGIN='org.codehaus.mojo:versions-maven-plugin:2.7' // make sure we know which plugin version we run
 
-			// set the root-pom's parent pom. Although the parent pom is avaialbe via relativePath, we need it to be this build's version then the root pom is deployed to our maven-repo
+			// set the root-pom's parent pom. Although the parent pom is available via relativePath, we need it to be this build's version then the root pom is deployed to our maven-repo
 			sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DparentVersion=${env.MF_VERSION} ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:update-parent"
 
 			// set the artifact version of everything below ${mvnConf.pomFile}
