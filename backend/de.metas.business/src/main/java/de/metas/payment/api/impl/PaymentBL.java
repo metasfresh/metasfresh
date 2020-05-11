d6aef766054e1fa1b5df7e0ff32f5c0b7c88d863
metas-ts <metas-ts@users.noreply.github.com>
2020-05-11 15:35:34 +0200

    solve compile error by synchronizing changes from master manually

--- backend/de.metas.business/src/main/java/de/metas/payment/api/impl/PaymentBL.java
+++ backend/de.metas.business/src/main/java/de/metas/payment/api/impl/PaymentBL.java
@@ -20,51 +20,16 @@
  * #L%
  */
 
-/**
- *
- */
 package de.metas.payment.api.impl;
-
-import static java.math.BigDecimal.ZERO;
-
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
-import java.util.Set;
-
-import org.adempiere.ad.trx.api.ITrx;
-import org.adempiere.ad.trx.api.ITrxManager;
-import org.adempiere.bank.BankId;
-import org.adempiere.bank.BankRepository;
-import org.adempiere.exceptions.AdempiereException;
-import org.adempiere.service.ClientId;
-import org.adempiere.service.ISysConfigBL;
-import org.adempiere.util.lang.Mutable;
-import org.compiere.SpringContextHolder;
-import org.compiere.model.I_C_AllocationHdr;
-import org.compiere.model.I_C_AllocationLine;
-import org.compiere.model.I_C_DocType;
-import org.compiere.model.I_C_Invoice;
-import org.compiere.model.I_C_Payment;
-import org.compiere.util.TimeUtil;
-import org.compiere.util.TrxRunnableAdapter;
-import org.slf4j.Logger;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-
 import de.metas.allocation.api.IAllocationBL;
 import de.metas.banking.BankStatementId;
 import de.metas.banking.BankStatementLineId;
 import de.metas.banking.BankStatementLineRefId;
+import de.metas.bpartner.BPartnerBankAccountId;
 import de.metas.currency.CurrencyConversionContext;
 import de.metas.currency.CurrencyPrecision;
 import de.metas.currency.ICurrencyBL;
@@ -90,6 +55,36 @@ import de.metas.payment.api.PaymentReconcileRequest;
 import de.metas.util.Check;
 import de.metas.util.Services;
 import lombok.NonNull;
+import org.adempiere.ad.trx.api.ITrx;
+import org.adempiere.ad.trx.api.ITrxManager;
+import org.adempiere.bank.BankId;
+import org.adempiere.bank.BankRepository;
+import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.service.ClientId;
+import org.adempiere.service.ISysConfigBL;
+import org.adempiere.util.lang.Mutable;
+import org.compiere.SpringContextHolder;
+import org.compiere.model.I_C_AllocationHdr;
+import org.compiere.model.I_C_AllocationLine;
+import org.compiere.model.I_C_DocType;
+import org.compiere.model.I_C_Invoice;
+import org.compiere.model.I_C_Payment;
+import org.compiere.util.TimeUtil;
+import org.compiere.util.TrxRunnableAdapter;
+import org.slf4j.Logger;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import static java.math.BigDecimal.ZERO;
 
 public class PaymentBL implements IPaymentBL
 {
@@ -674,5 +669,23 @@ public class PaymentBL implements IPaymentBL
 		{
 			throw new AdempiereException("Failed extracting payment reconcile reference from " + payment, ex);
 		}
+	}
+
+	@Override
+	public @NonNull TenderType getTenderType(final @NonNull BPartnerBankAccountId bPartnerBankAccountId)
+	{
+		final BankRepository bankRepository = SpringContextHolder.instance.getBean(BankRepository.class);
+		final BankId bankId = bankRepository.getBankId(bPartnerBankAccountId);
+		if (bankId == null)
+		{
+			return TenderType.Check;
+		}
+
+		if (bankRepository.isCashBank(bankId))
+		{
+			return TenderType.Cash;
+		}
+
+		return TenderType.Check;
 	}
 }
