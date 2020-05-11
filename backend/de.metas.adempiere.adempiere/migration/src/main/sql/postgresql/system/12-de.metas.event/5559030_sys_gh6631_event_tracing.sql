b677120eca7fc86185d7942d7c2543525378e6f8
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-05-11 19:31:01 +0200

    add a bit more description to the sysconfigs

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/12-de.metas.event/5559030_sys_gh6631_event_tracing.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/12-de.metas.event/5559030_sys_gh6631_event_tracing.sql
@@ -20,3 +20,24 @@ UPDATE AD_SysConfig SET Value='N',Updated=TO_TIMESTAMP('2020-05-11 17:49:28','YY
 UPDATE AD_SysConfig SET Value='Y',Updated=TO_TIMESTAMP('2020-05-11 17:56:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541322
 ;
 
+-- 2020-05-11T17:16:26.993Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Description='If a thread posts an event to the local event bus and this is Y, then the respective event listeners are invoked in a dedicated worked-thread. 
+This means the the posting thread doesn''t have to wait for the listerners to finish, but it can also make things more complex and harder to trace.
+A change requires a restart.',Updated=TO_TIMESTAMP('2020-05-11 19:16:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541321
+;
+
+-- 2020-05-11T17:27:43.362Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Description='When an event is posted via RabbitMQ then we invoke the performance monitor such that tracing infos are added to the event.
+That way we can do distributred tracing.
+',Updated=TO_TIMESTAMP('2020-05-11 19:27:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541322
+;
+
+-- 2020-05-11T17:28:36.145Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Description='When an event is posted via RabbitMQ then we invoke the performance monitor such that tracing infos are added to the event.
+That way we can do distributred tracing.
+Note that currently, EventBusPostEventsAsync needs to be N for this to work. If it''s Y them AFAIU the required context is lost right before the interesting things happen.',Updated=TO_TIMESTAMP('2020-05-11 19:28:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541322
+;
+
