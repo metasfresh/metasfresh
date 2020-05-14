07f5d29729e3d47d4549c898fcdfb4e94f9b84e9
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-05-14 14:18:00 +0200

    be lenient towards Processing IS NULL
    https://github.com/metasfresh/metasfresh/issues/6645

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5559255_sys_fact_acct_unpost_tolerate_processing_IS_NULL.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5559255_sys_fact_acct_unpost_tolerate_processing_IS_NULL.sql
@@ -1,3 +1,4 @@
+
 DROP FUNCTION IF EXISTS "de_metas_acct".fact_acct_unpost(character varying, numeric, char(1));
 
 CREATE OR REPLACE FUNCTION "de_metas_acct".fact_acct_unpost(p_tablename character varying,
@@ -32,7 +33,7 @@ BEGIN
     -- Make sure it was not already locked.
     v_lockSQL := 'UPDATE ' || p_TableName || ' SET Processing=''Y'' WHERE ' || p_TableName || '_ID=' || p_Record_ID;
      IF (p_Force <> 'Y') THEN
-             v_lockSQL := v_lockSQL || ' AND COALESCE(Processing,''N'')=''N''/*be lenient towards Processing IS NULL*/';
+             v_lockSQL := v_lockSQL || ' AND COALESCE(Processing,''N'')!=''Y''/*be lenient towards Processing IS NULL*/';
      END IF;
     EXECUTE v_lockSQL;
     GET DIAGNOSTICS rowcount = ROW_COUNT;
