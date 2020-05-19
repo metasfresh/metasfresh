5fedf8771669e6dcff60c2f203fb6168c7d6a4f6
TheBestPessimist <cristian@tbp.land>
2020-05-19 16:06:31 +0300

    Add IElementValueDAO.getElementValueIdsBetween
    https://github.com/metasfresh/metasfresh/issues/6682

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/acct/api/impl/ElementValueDAO.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/acct/api/impl/ElementValueDAO.java
@@ -1,37 +1,40 @@
-package de.metas.acct.api.impl;
-
 /*
  * #%L
  * de.metas.adempiere.adempiere.base
  * %%
- * Copyright (C) 2015 metas GmbH
+ * Copyright (C) 2020 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
+package de.metas.acct.api.impl;
 
-import java.util.Properties;
-
+import com.google.common.collect.ImmutableSet;
+import de.metas.acct.api.IElementValueDAO;
+import de.metas.cache.annotation.CacheCtx;
+import de.metas.util.Services;
+import lombok.NonNull;
+import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.dao.impl.RPadQueryFilterModifier;
 import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.proxy.Cached;
 import org.compiere.model.I_C_ElementValue;
 
-import de.metas.acct.api.IElementValueDAO;
-import de.metas.cache.annotation.CacheCtx;
+import java.util.Properties;
 
 public class ElementValueDAO implements IElementValueDAO
 {
@@ -44,5 +47,20 @@ public class ElementValueDAO implements IElementValueDAO
 			return null;
 		}
 		return InterfaceWrapperHelper.create(ctx, elementValueId, I_C_ElementValue.class, ITrx.TRXNAME_None);
+	}
+
+	@Override
+	public @NonNull ImmutableSet<ElementValueId> getElementValueIdsBetween(@NonNull final String valueFrom, @NonNull final String valueTo)
+	{
+		// TODO @tbp @teo: this query needs to be refined and its test updated after a discussion with mark
+
+		final RPadQueryFilterModifier lpadModifier = new RPadQueryFilterModifier(20, " ");
+
+		final IQueryBL queryBL = Services.get(IQueryBL.class);
+		return queryBL.createQueryBuilder(I_C_ElementValue.class)
+				.addBetweenFilter(I_C_ElementValue.COLUMNNAME_Value, valueFrom, valueTo, lpadModifier)
+				.addOnlyActiveRecordsFilter()
+				.create()
+				.listIds(ElementValueId::ofRepoId);
 	}
 }
