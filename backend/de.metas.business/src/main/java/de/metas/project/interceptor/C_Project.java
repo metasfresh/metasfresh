8293b2243ead50e5463d524ff8417a180206630b
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-05-20 17:06:41 +0300

    #6691 Imported CopyRecordFactory.

--- backend/de.metas.business/src/main/java/de/metas/project/interceptor/C_Project.java
+++ backend/de.metas.business/src/main/java/de/metas/project/interceptor/C_Project.java
@@ -3,6 +3,7 @@ package de.metas.project.interceptor;
 import org.adempiere.ad.callout.annotations.Callout;
 import org.adempiere.ad.callout.annotations.CalloutMethod;
 import org.adempiere.ad.callout.spi.IProgramaticCalloutProvider;
+import org.adempiere.model.CopyRecordFactory;
 import org.compiere.model.I_C_Project;
 import org.springframework.stereotype.Component;
 
