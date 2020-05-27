b23319f258ed5b171d9679172d4dbc0acf30b820
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-05-27 16:57:04 +0300

    Skel for TableContextMenu #6723

---
+++ frontend/src/__tests__/components/table/TableContextMenu.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+// import { ShortcutProvider } from '../../../components/keyshortcuts/ShortcutProvider';
+import { initialState as appHandlerState } from '../../../reducers/appHandler';
+import { initialState as windowHandlerState } from '../../../reducers/windowHandler';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import merge from 'merge';
+import tableCMenuProps from '../../../../test_setup/fixtures/table/table_context_menu.json';
+import TableContextMenu from '../../../components/table/TableContextMenu';
+
+const mockStore = configureStore([]);
+const createStore = function(state = {}) {
+  const res = merge.recursive(
+    true,
+    {
+      appHandler: {
+        ...appHandlerState,
+        me: { timeZone: 'America/Los_Angeles' },
+      },
+      windowHandler: { ...windowHandlerState },
+    },
+    state
+  );
+
+  return res;
+};
+const initialState = createStore({
+  windowHandler: {
+    allowShortcut: true,
+    modal: {
+      visible: false,
+    },
+  },
+});
+const store = mockStore(initialState);
+
+tableCMenuProps.selected = []; // nothing selected by default
+describe('TableContextMenu', () => {
+  it('renders without errors with the given props', () => {
+    const wrapperTableCMenu = mount(
+      <Provider store={store}>
+        <TableContextMenu {...tableCMenuProps} />
+      </Provider>
+    );
+    const html = wrapperTableCMenu.html();
+  });
+});
