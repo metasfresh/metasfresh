075af468bb3031bc795742518f1b3ee554772a5c
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2020-05-29 13:30:55 +0200

    show helpful error message on missing price (#6746)

--- de.metas.adempiere.adempiere/base/src/main/java/de/metas/attachments/AttachmentEntryService.java
+++ de.metas.adempiere.adempiere/base/src/main/java/de/metas/attachments/AttachmentEntryService.java
@@ -193,7 +193,10 @@ public class AttachmentEntryService
 		return expandAndSave(referencedRecords, unsavedAttachmentsWithLinks);
 	}
 
-	/** Note: the given objects may be "record" models or {@link TableRecordReference}s. */
+	/**
+	 * Link all given {@code referencedRecordsDest} with the attachments currently linked to the given {@code referencedRecordsSource}
+	 * Note: the given objects may be "record" models {@code I_C_Order} or {@link TableRecordReference}s.
+	 */
 	public Collection<AttachmentEntry> shareAttachmentLinks(
 			@NonNull final Collection<? extends Object> referencedRecordsSource,
 			@NonNull final Collection<? extends Object> referencedRecordsDest)
