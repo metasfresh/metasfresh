09191f23d49ae5401141ee9ffc22f130f494a9e2
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-06-04 15:49:51 +0200

    Fix accidental inactivation of not-IBAN bank accounts
    * basically the fix is to not deactivate C_BP_BankAccounts that have no IBAN, because those
     * are not loaded into the bpartnercomposite and the API can't access them anyways
     * would otherwise always be deactiaved
    * application.properties: unrelated cleanup

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/banking/api/impl/BPBankAccountDAO.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/banking/api/impl/BPBankAccountDAO.java
@@ -66,7 +66,7 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 	}
 
 	@Override
-	public ImmutableListMultimap<BPartnerId, I_C_BP_BankAccount> getByBPartnerIds(@NonNull final Collection<BPartnerId> bpartnerIds)
+	public ImmutableListMultimap<BPartnerId, I_C_BP_BankAccount> getAllByBPartnerIds(@NonNull final Collection<BPartnerId> bpartnerIds)
 	{
 		if (bpartnerIds.isEmpty())
 		{
@@ -103,20 +103,6 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 				.list();
 
 		return bpBankAccounts;
-		// return LegacyAdapters.convertToPOArray(bpBankAccounts, MBPBankAccount.class);
-	}    // getOfBPartner
-
-	@Override
-	public Optional<BankAccountId> retrieveFirstIdByBPartnerAndCurrency(@NonNull final BPartnerId bPartnerId, @NonNull final CurrencyId currencyId)
-	{
-		final BankAccountId bankAccountId = queryBL.createQueryBuilder(I_C_BP_BankAccount.class)
-				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_C_BPartner_ID, bPartnerId)
-				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_C_Currency_ID, currencyId)
-				.addOnlyActiveRecordsFilter()
-				.create()
-				.firstId(BankAccountId::ofRepoIdOrNull);
-
-		return Optional.ofNullable(bankAccountId);
 	}
 
 	@Override
@@ -134,7 +120,7 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 	}
 
 	@Override
-	public void deactivateByBPartnerExcept(
+	public void deactivateIBANAccountsByBPartnerExcept(
 			@NonNull final BPartnerId bpartnerId,
 			@NonNull final Collection<BPartnerBankAccountId> exceptIds)
 	{
@@ -144,6 +130,7 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 
 		queryBL.createQueryBuilder(I_C_BP_BankAccount.class)
 				.addOnlyActiveRecordsFilter()
+				.addNotNull(I_C_BP_BankAccount.COLUMNNAME_IBAN)
 				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_C_BPartner_ID, bpartnerId)
 				.addNotInArrayFilter(I_C_BP_BankAccount.COLUMN_C_BP_BankAccount_ID, exceptIds)
 				.create()
