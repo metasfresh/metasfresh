0d7c7984662bf1b3f9c8f6933749a00d55c575ad
TheBestPessimist <cristian@tbp.land>
2020-06-11 13:46:37 +0300

    Fix trl in Transportation Order window
    https://github.com/metasfresh/metasfresh/issues/6822

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5561070_sys_gh6822_FixTrlInTransportationOrderWindow.sql
@@ -0,0 +1,20 @@
+-- 2020-06-11T10:35:49.680Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-06-11 13:35:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=540011
+;
+
+-- 2020-06-11T10:35:53.451Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Ladeliste erstellen',Updated=TO_TIMESTAMP('2020-06-11 13:35:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=540011
+;
+
+-- 2020-06-11T10:35:58.234Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-06-11 13:35:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=540011
+;
+
+-- 2020-06-11T10:42:18.973Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Alle Lieferpapiere drucken',Updated=TO_TIMESTAMP('2020-06-11 13:42:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=541228
+;
+
