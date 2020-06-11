2262f6bbc65cc0b21f882f977393768ca7f04409
TheBestPessimist <cristian@tbp.land>
2020-06-11 09:55:05 +0300

    Redo scale calculation: don't store more than required decimals
    https://github.com/metasfresh/metasfresh/issues/6808

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/allocation/impl/HUListAllocationSourceDestination.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/allocation/impl/HUListAllocationSourceDestination.java
@@ -259,7 +259,7 @@ public class HUListAllocationSourceDestination implements IAllocationSource, IAl
 
 				final int scale = storageUOM.getStdPrecision();
 				cuQty = storageQty.divide(aggregateHUQty,
-						scale * 3,
+						scale,
 						RoundingMode.HALF_UP);
 			}
 			request.getHUContext().setProperty(AggregateHUTrxListener.mkItemCuQtyPropertyKey(haItem), cuQty);
