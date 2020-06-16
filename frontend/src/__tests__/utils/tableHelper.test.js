051331551d16d6fc213af79514d20e365031727e
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-06-16 09:06:11 +0300

    Completed the unit tests

--- frontend/src/__tests__/utils/tableHelper.test.js
+++ frontend/src/__tests__/utils/tableHelper.test.js
@@ -30,7 +30,6 @@ describe('Table helpers', () => {
 
       const fieldToCheckTwo = createSpecialField('Color', '#EEEEEE');
       expect(fieldToCheckTwo.props.style.backgroundColor).toBe('#EEEEEE'); // For the special `Color` type case you get a react el piece (used in table cell)
-
     });
 
     it('fieldValueToString works correctly when null value given', () => {
@@ -67,5 +66,248 @@ describe('Table helpers', () => {
       let test = fieldToCheck.includes('-');
       expect(test).toBe(true); // check formatting with a dash when array given
     });
+
+    it('fieldValueToString works correctly when boolean true value given', () => {
+      let fieldValue = true;
+      let fieldType = 'Text';
+      let precision = null;
+      let isGerman = true;
+      let activeLocale = { key: 'de_DE' };
+      const fieldToCheck = fieldValueToString({
+        fieldValue,
+        fieldType,
+        precision,
+        isGerman,
+        activeLocale,
+      });
+
+      expect(fieldToCheck.props.className).toBe('meta-icon-checkbox-1');
+    });
+
+    it('fieldValueToString works correctly when boolean false value given', () => {
+      let fieldValue = false;
+      let fieldType = 'Text';
+      let precision = null;
+      let isGerman = true;
+      let activeLocale = { key: 'de_DE' };
+      const fieldToCheck = fieldValueToString({
+        fieldValue,
+        fieldType,
+        precision,
+        isGerman,
+        activeLocale,
+      });
+
+      expect(fieldToCheck.props.className).toBe('meta-icon-checkbox');
+    });
+
+    it('fieldValueToString works correctly when Date type given', () => {
+      let fieldValue = '2020-01-02';
+      let fieldType = 'Date';
+      let precision = null;
+      let isGerman = true;
+      let activeLocale = { key: 'de_DE' };
+      let fieldToCheck = fieldValueToString({
+        fieldValue,
+        fieldType,
+        precision,
+        isGerman,
+        activeLocale,
+      });
+
+      expect(fieldToCheck).toBe('02.01.2020');
+
+      // we check also en locale
+      fieldValue = '2020-01-02';
+      fieldType = 'Date';
+      precision = null;
+      isGerman = false;
+      activeLocale = { key: 'en_EN' };
+      fieldToCheck = fieldValueToString({
+        fieldValue,
+        fieldType,
+        precision,
+        isGerman,
+        activeLocale,
+      });
+
+      expect(fieldToCheck).toBe('01/02/2020');
+    });
+  });
+
+  it('fieldValueToString works correctly when Date type given', () => {
+    let fieldValue = '2020-01-02';
+    let fieldType = 'Date';
+    let precision = null;
+    let isGerman = true;
+    let activeLocale = { key: 'de_DE' };
+    let fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('02.01.2020');
+
+    // we check also en locale
+    fieldValue = '2020-01-02';
+    fieldType = 'Date';
+    precision = null;
+    isGerman = false;
+    activeLocale = { key: 'en_EN' };
+    fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('01/02/2020');
+  });
+
+  it('fieldValueToString works correctly when DateTime type given', () => {
+    let fieldValue = '2020-01-02';
+    let fieldType = 'DateTime';
+    let precision = null;
+    let isGerman = true;
+    let activeLocale = { key: 'de_DE' };
+    let fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('02.01.2020 00:00:00');
+
+    // we check also en locale
+    fieldValue = '2020-01-02';
+    fieldType = 'DateTime';
+    precision = null;
+    isGerman = false;
+    activeLocale = { key: 'en_EN' };
+    fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('01/02/2020 12:00:00 AM');
+  });
+
+  it('fieldValueToString works correctly when ZoneDateTime type given', () => {
+    let fieldValue = '2020-01-02';
+    let fieldType = 'ZoneDateTime';
+    let precision = null;
+    let isGerman = true;
+    let activeLocale = { key: 'de_DE' };
+    let fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('2020-01-02');
+
+    // we check also en locale
+    fieldValue = '2020-01-02';
+    fieldType = 'ZoneDateTime';
+    precision = null;
+    isGerman = false;
+    activeLocale = { key: 'en_EN' };
+    fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('2020-01-02');
+  });
+
+  it('fieldValueToString works correctly when Timestamp type given', () => {
+    let fieldValue = '2020-01-02';
+    let fieldType = 'Timestamp';
+    let precision = null;
+    let isGerman = true;
+    let activeLocale = { key: 'de_DE' };
+    let fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('02.01.2020 00:00:00');
+
+    // we check also en locale
+    fieldValue = '2020-01-02';
+    fieldType = 'Timestamp';
+    precision = null;
+    isGerman = false;
+    activeLocale = { key: 'en_EN' };
+    fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('01/02/2020 00:00:00');
+  });
+
+  it('fieldValueToString works correctly when Amount fieldType type given', () => {
+    let fieldValue = '22,38342';
+    let fieldType = 'Amount';
+    let precision = null;
+    let isGerman = true;
+    let activeLocale = { key: 'de_DE' };
+    let fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('22');
+
+    precision = 2; // changing precision works
+    fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+
+    expect(fieldToCheck).toBe('22,00');
+  });
+
+  it('fieldValueToString works correctly when special type Color fieldType type given', () => {
+    let fieldValue = 'DDDDDD';
+    let fieldType = 'Color';
+    let precision = null;
+    let isGerman = true;
+    let activeLocale = { key: 'de_DE' };
+    let fieldToCheck = fieldValueToString({
+      fieldValue,
+      fieldType,
+      precision,
+      isGerman,
+      activeLocale,
+    });
+    expect(fieldToCheck.props.style.backgroundColor).toBe('DDDDDD');
   });
 });
