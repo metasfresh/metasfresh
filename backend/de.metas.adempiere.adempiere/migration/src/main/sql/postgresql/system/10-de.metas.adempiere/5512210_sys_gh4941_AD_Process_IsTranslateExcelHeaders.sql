f997a95f2f889c50f4e355005893535d92110531
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-06-22 12:09:50 +0200

    solve SQL rollout problem on "old" metasfresh-instance

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5512210_sys_gh4941_AD_Process_IsTranslateExcelHeaders.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5512210_sys_gh4941_AD_Process_IsTranslateExcelHeaders.sql
@@ -18,11 +18,6 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Column_ID=564052 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
 
--- 2019-02-07T17:32:44.291
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-/* DDL */ SELECT public.db_alter_table('AD_Process','ALTER TABLE public.AD_Process ADD COLUMN IsTranslateExcelHeaders CHAR(1) DEFAULT ''Y'' CHECK (IsTranslateExcelHeaders IN (''Y'',''N'')) NOT NULL')
-;
-
 -- 2019-02-07T17:33:16.198
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,564052,574792,0,245,TO_TIMESTAMP('2019-02-07 17:33:16','YYYY-MM-DD HH24:MI:SS'),100,1,'D','Y','N','N','N','N','N','N','N','Translate Excel Headers',TO_TIMESTAMP('2019-02-07 17:33:16','YYYY-MM-DD HH24:MI:SS'),100)
@@ -228,3 +223,4 @@ UPDATE AD_Element_Trl SET UpdatedBy=100,Updated=TO_TIMESTAMP('2019-02-07 17:42:5
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576095,'en_US') 
 ;
 
+--select migrationscript_ignore('10-de.metas.adempiere/5512210_sys_gh4941_AD_Process_IsTranslateExcelHeaders.sql');
