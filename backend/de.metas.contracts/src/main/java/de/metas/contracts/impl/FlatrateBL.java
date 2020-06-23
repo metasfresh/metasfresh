31a32adb4c31969d6a0e7aa2771606e8319d0fc0
TheBestPessimist <cristian@tbp.land>
2020-06-23 08:47:52 +0300

    Add note about exception swallowing
    https://github.com/metasfresh/metasfresh/issues/6887

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/impl/FlatrateBL.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/impl/FlatrateBL.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.contracts
+ * %%
+ * Copyright (C) 2020 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.contracts.impl;
 
 import ch.qos.logback.classic.Level;
@@ -97,28 +119,6 @@ import java.util.Map;
 import java.util.Properties;
 
 import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
-
-/*
- * #%L
- * de.metas.contracts
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 public class FlatrateBL implements IFlatrateBL
 {
@@ -1627,7 +1627,7 @@ public class FlatrateBL implements IFlatrateBL
 
 		if (dontCreateTerm)
 		{
-			throw new AdempiereException("@NotCreated@ @C_Flatrate_Term_ID@ (@C_BPartner_ID@: " + bPartner.getValue() + "): " + notCreatedReason);
+			throw new AdempiereException("@NotCreated@ @C_Flatrate_Term_ID@ (@C_BPartner_ID@: " + bPartner.getValue() + "): " + notCreatedReason).markAsUserValidationError();
 		}
 
 		final I_C_Flatrate_Term newTerm = InterfaceWrapperHelper.newInstance(I_C_Flatrate_Term.class, bPartner);
--- backend/de.metas.contracts/src/main/java/de/metas/contracts/process/FlatrateTermCreator.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/process/FlatrateTermCreator.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.contracts
+ * %%
+ * Copyright (C) 2020 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.contracts.process;
 
 import de.metas.contracts.CreateFlatrateTermRequest;
@@ -27,28 +49,6 @@ import javax.annotation.Nullable;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Properties;
-
-/*
- * #%L
- * de.metas.contracts
- * %%
- * Copyright (C) 2018 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 @Builder
 @Value
@@ -95,6 +95,9 @@ public class FlatrateTermCreator
 						logger.debug("Created contract(s) for {}", partner);
 					}
 
+					// note for future developer: this swallows the user exception so it's no longer shown in webui, but as a "bell notification".
+					// Please consult with mark or torby if we want to swallow or throw.
+					// Please remember that this can be run for 10000 Partners when proposing this idea.
 					@Override
 					public boolean doCatch(Throwable ex)
 					{
