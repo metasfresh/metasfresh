a9363bcd7df815a97e057138f190dac50f26aa6b
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-06-30 14:05:59 +0300

    Remove redundant code

--- frontend/src/actions/TableActions.js
+++ frontend/src/actions/TableActions.js
@@ -241,7 +241,7 @@ export function updateGridTable(tableId, tableResponse) {
         const { keyProperty } = tableData;
 
         // Parse `rows` to add `indent` property
-        if (tableData.rows.length && tableData.rows.length) {
+        if (tableData.rows.length) {
           tableData.rows = flattenRows(tableData.rows);
         }
 
