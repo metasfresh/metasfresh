a1efdb35b5a6ced6024f02425157df36397fce2c
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-06-30 12:06:51 +0200

    #6342 Update pay selection total ammount when deleting line
    * keep PaySelectionUpdater out of the game; making a private method public, getting IPayselectionUpdated and casting it, and all this only to invalidate te cache is a smell
    * make sure the cache is invalidated after commit; otherwise, event handlers might  refresh but still get the old value
    * ITrxManager and ITrxListenerManager cleanup javadocs (boyscout rule)

--- backend/de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/impl/PaySelectionUpdater.java
+++ backend/de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/impl/PaySelectionUpdater.java
@@ -58,7 +58,7 @@ public class PaySelectionUpdater implements IPaySelectionUpdater
 	private static final transient Logger logger = LogManager.getLogger(PaySelectionUpdater.class);
 	private final transient ITrxManager trxManager = Services.get(ITrxManager.class);
 	private final transient IPaySelectionDAO paySelectionsRepo = Services.get(IPaySelectionDAO.class);
-	final IModelCacheInvalidationService modelCacheInvalidationService = Services.get(IModelCacheInvalidationService.class);
+	private final transient IModelCacheInvalidationService modelCacheInvalidationService = Services.get(IModelCacheInvalidationService.class);
 
 	private boolean _configurable = true;
 
@@ -608,10 +608,7 @@ public class PaySelectionUpdater implements IPaySelectionUpdater
 				.collect(ImmutableSet.toImmutableSet());
 	}
 
-	/**
-	 * Invalidates Cache for current PaySelection.
-	 */
-	public void cacheInvalidationForCurrentPaySelection()
+	private void cacheInvalidationForCurrentPaySelection()
 	{
 		modelCacheInvalidationService.invalidate(
 				CacheInvalidateMultiRequest.fromTableNameAndRecordId(I_C_PaySelection.Table_Name, getPaySelectionId().getRepoId()),
--- backend/de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/modelvalidator/C_PaySelectionLine.java
+++ backend/de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/payment/modelvalidator/C_PaySelectionLine.java
@@ -3,9 +3,10 @@ package de.metas.banking.payment.modelvalidator;
 import de.metas.banking.PaySelectionId;
 import de.metas.banking.payment.IPaySelectionBL;
 import de.metas.banking.payment.IPaySelectionDAO;
-import de.metas.banking.payment.IPaySelectionUpdater;
 import de.metas.banking.payment.IPaymentRequestBL;
-import de.metas.banking.payment.impl.PaySelectionUpdater;
+import de.metas.cache.model.CacheInvalidateMultiRequest;
+import de.metas.cache.model.IModelCacheInvalidationService;
+import de.metas.cache.model.ModelCacheInvalidationTiming;
 import de.metas.currency.CurrencyCode;
 import de.metas.currency.ICurrencyDAO;
 import de.metas.i18n.AdMessageKey;
@@ -14,6 +15,7 @@ import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.modelvalidator.annotations.Interceptor;
 import org.adempiere.ad.modelvalidator.annotations.ModelChange;
+import org.adempiere.ad.trx.api.ITrxManager;
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.model.I_C_BP_BankAccount;
@@ -29,6 +31,8 @@ public class C_PaySelectionLine
 
 	private final IPaySelectionDAO paySelectionDAO = Services.get(IPaySelectionDAO.class);
 	private final IPaySelectionBL paySelectionBL = Services.get(IPaySelectionBL.class);
+	private final IModelCacheInvalidationService modelCacheInvalidationService = Services.get(IModelCacheInvalidationService.class);
+	private final ITrxManager trxManager = Services.get(ITrxManager.class);
 
 	private static final AdMessageKey MSG_PaySelectionLine_Invoice_InvalidCurrency = AdMessageKey.of("PaySelectionLine.Invoice.InvalidCurrency");
 
@@ -91,30 +95,36 @@ public class C_PaySelectionLine
 		final CurrencyCode invoiceCurrencyCode = currenciesRepo.getCurrencyCodeById(CurrencyId.ofRepoId(invoice.getC_Currency_ID()));
 		final CurrencyCode bankAccountCurrencyCode = currenciesRepo.getCurrencyCodeById(CurrencyId.ofRepoId(bankAccount.getC_Currency_ID()));
 
-		throw new AdempiereException(MSG_PaySelectionLine_Invoice_InvalidCurrency, new Object[] {
-				invoice.getDocumentNo(),     // invoice
-				invoiceCurrencyCode.toThreeLetterCode(),      // Actual
-				bankAccountCurrencyCode.toThreeLetterCode() }); // Expected
+		throw new AdempiereException(MSG_PaySelectionLine_Invoice_InvalidCurrency,
+				new Object[] {
+						invoice.getDocumentNo(),     // invoice
+						invoiceCurrencyCode.toThreeLetterCode(),      // Actual
+						bankAccountCurrencyCode.toThreeLetterCode() }) // Expected
+				.markAsUserValidationError();
 	}
 
 	@ModelChange(timings = { ModelValidator.TYPE_AFTER_NEW, ModelValidator.TYPE_AFTER_CHANGE })
 	public void afterNewOrChange(final I_C_PaySelectionLine paySelectionLine)
 	{
-		final PaySelectionId paySelectionId = PaySelectionId.ofRepoId(paySelectionLine.getC_PaySelection_ID());
-		paySelectionDAO.updatePaySelectionTotalAmt(paySelectionId);
+		updatePaySelectionAmount(paySelectionLine);
 	}
 
 	@ModelChange(timings = { ModelValidator.TYPE_AFTER_DELETE })
 	public void afterDelete(final I_C_PaySelectionLine paySelectionLine)
 	{
-		final IPaySelectionUpdater paySelectionUpdater = paySelectionBL.newPaySelectionUpdater();
-
-		final PaySelectionId paySelectionId = PaySelectionId.ofRepoId(paySelectionLine.getC_PaySelection_ID());
-		final I_C_PaySelection paySelection = paySelectionDAO.getById(paySelectionId).get();
-
-		paySelectionDAO.updatePaySelectionTotalAmt(paySelectionId);
-		paySelectionUpdater.setC_PaySelection(paySelection);
-		((PaySelectionUpdater)paySelectionUpdater).cacheInvalidationForCurrentPaySelection();
+		updatePaySelectionAmount(paySelectionLine);
 	}
 
+	private void updatePaySelectionAmount(@NonNull final I_C_PaySelectionLine paySelectionLine)
+	{
+		final PaySelectionId paySelectionId = PaySelectionId.ofRepoId(paySelectionLine.getC_PaySelection_ID());
+		paySelectionDAO.updatePaySelectionTotalAmt(paySelectionId);
+
+		// make sure the is invalidated *after* the change is visible for everyone
+		trxManager
+				.getCurrentTrxListenerManagerOrAutoCommit()
+				.runAfterCommit(() -> modelCacheInvalidationService.invalidate(
+						CacheInvalidateMultiRequest.fromTableNameAndRecordId(I_C_PaySelection.Table_Name, paySelectionId.getRepoId()),
+						ModelCacheInvalidationTiming.CHANGE));
+	}
 }
