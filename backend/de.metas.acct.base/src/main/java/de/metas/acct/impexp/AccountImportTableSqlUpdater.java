bc6873bde2fd12567fa57e34f9de0fe6ac6bcafc
Cristina <cristinamghita@gmail.com>
2020-07-01 16:17:41 +0300

    #6921 Fix typo

--- backend/de.metas.acct.base/src/main/java/de/metas/acct/impexp/AccountImportTableSqlUpdater.java
+++ backend/de.metas.acct.base/src/main/java/de/metas/acct/impexp/AccountImportTableSqlUpdater.java
@@ -120,7 +120,7 @@ public class AccountImportTableSqlUpdater
 	{
 		final StringBuilder sql = new StringBuilder("update c_elementvalue set Parent_id = tn.parent_id, seqno = tn.seqno ")
 				.append(" from AD_treenode tn where tn.node_id = c_elementvalue.c_elementvalue_id and tn.ad_org_id = c_elementvalue.ad_org_id")
-				.append(" and exists ( select 1 from c_element e where e.c_element_id = c_elementvalue.c_element_id and e.ad_tree_id = tn.ad_tree_id); " )
+				.append(" and exists ( select 1 from c_element e where e.c_element_id = c_elementvalue.c_element_id and e.ad_tree_id = tn.ad_tree_id) " )
 				.append(" and c_element_id = ? " )
 				.append("  " );
 		int no = DB.executeUpdateEx(sql.toString(), new Object[] {c_element_id}, ITrx.TRXNAME_None);
