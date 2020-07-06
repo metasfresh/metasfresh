47d9f2bcf4f460e67d63dbd729e4d55bb86253f5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-07-06 17:41:54 +0200

    WEBUI_Fact_Acct_Repost_ViewRows - DocumentToRepost seems to be ignored
    hope to fix it by threating FACT_ACCT_TRANSACTIONS_VIEW item like fact_acct
    metasfresh/metasfresh#6965

--- backend/metasfresh-webui-api/src/main/java/de/metas/ui/web/accounting/process/WEBUI_Fact_Acct_Repost_ViewRows.java
+++ backend/metasfresh-webui-api/src/main/java/de/metas/ui/web/accounting/process/WEBUI_Fact_Acct_Repost_ViewRows.java
@@ -2,6 +2,7 @@ package de.metas.ui.web.accounting.process;
 
 import java.util.Set;
 
+import de.metas.ui.web.accounting.filters.FactAcctFilterDescriptorsProviderFactory;
 import org.adempiere.ad.table.api.IADTableDAO;
 import org.adempiere.service.ClientId;
 import org.compiere.model.I_Fact_Acct;
@@ -89,6 +90,7 @@ public class WEBUI_Fact_Acct_Repost_ViewRows extends ViewBasedProcessTemplate im
 	private DocumentToRepost extractDocumentToRepost(final IViewRow row)
 	{
 		if (I_Fact_Acct.Table_Name.equals(getTableName())
+				|| FactAcctFilterDescriptorsProviderFactory.FACT_ACCT_TRANSACTIONS_VIEW.equals(getTableName())
 				|| TABLENAME_RV_UnPosted.equals(getTableName()))
 		{
 			return extractDocumentToRepostFromTableAndRecordIdRow(row);
