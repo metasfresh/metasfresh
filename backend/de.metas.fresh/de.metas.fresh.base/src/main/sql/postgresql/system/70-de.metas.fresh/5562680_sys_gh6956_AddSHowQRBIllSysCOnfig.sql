cce53313092adaaef0d4676c687a714d768205fe
Cristina <cristinamghita@gmail.com>
2020-07-06 16:40:24 +0300

    #6956 Simplify things

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5562680_sys_gh6956_AddSHowQRBIllSysCOnfig.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5562680_sys_gh6956_AddSHowQRBIllSysCOnfig.sql
@@ -8,3 +8,8 @@ INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLe
 UPDATE AD_SysConfig SET ConfigurationLevel='O',Updated=TO_TIMESTAMP('2020-07-02 15:09:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541328
 ;
 
+-- 2020-07-06T13:19:46.063Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Description='This is used to swtich between QR Bill and ESR. By default, we set on Y, and we show the QR Bill.',Updated=TO_TIMESTAMP('2020-07-06 16:19:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541328
+;
+
--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5562840_sys_gh6956_isShowQRBill.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5562840_sys_gh6956_isShowQRBill.sql
@@ -4,38 +4,12 @@ create or replace function report.isShowQRBill(IN p_AD_Client_ID numeric, IN p_A
 $$
 DECLARE
     isShowQRBill CHAR DEFAULT 'Y';
-    foundValue   BOOLEAN DEFAULT FALSE;
-    rec_config   RECORD;
-    sysConfigs CURSOR FOR SELECT *
-                          from AD_SysConfig
-                          where Name = 'ShowQRBill';
 BEGIN
-    OPEN sysConfigs;
-
-    LOOP
-        -- fetch row into the film
-        FETCH sysConfigs INTO rec_config;
-        -- exit when no more row to fetch
-        EXIT WHEN NOT FOUND;
-
-        -- build the output
-        IF rec_config.AD_Client_ID = p_AD_Client_ID AND rec_config.AD_Org_ID = p_AD_Org_ID THEN
-            isShowQRBill := rec_config.value;
-            foundValue := TRUE;
-        END IF;
-
-    END LOOP;
-
-    -- Close the cursor
-    CLOSE sysConfigs;
-
-
-    IF foundValue = FALSE THEN
+ 
         SELECT VALUE
         INTO isShowQRBill
         from AD_SysConfig
-        where Name = 'ShowQRBill' AND AD_Client_ID = 0 AND AD_Org_ID = 0;
-    END IF;
+        where Name = 'ShowQRBill' AND AD_Client_ID IN (0,1000000) AND AD_Org_ID IN (0, p_AD_Org_ID) ORDER BY AD_Org_ID DESC, AD_Client_ID DESC LIMIT 1;
 
     return isShowQRBill;
 END
