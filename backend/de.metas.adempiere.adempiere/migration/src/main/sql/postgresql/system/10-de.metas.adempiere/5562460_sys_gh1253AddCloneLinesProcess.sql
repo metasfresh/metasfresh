b64daf5920d86def97d3aa96e7420181ea5888ad
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-07-07 18:45:25 +0300

    #1253 Translated process to german.

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5562460_sys_gh1253AddCloneLinesProcess.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5562460_sys_gh1253AddCloneLinesProcess.sql
@@ -18,3 +18,33 @@ UPDATE AD_Process SET Classname='de.metas.ui.web.process.WEBUI_CloneLine.java',U
 UPDATE AD_Process SET Classname='de.metas.ui.web.process.WEBUI_CloneLine',Updated=TO_TIMESTAMP('2020-06-25 17:34:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584720
 ;
 
+-- 2020-07-07T15:44:17.690Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET Name='Zeile Klonen',Updated=TO_TIMESTAMP('2020-07-07 18:44:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=584720
+;
+
+-- 2020-07-07T15:44:22.101Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET Name='Zeile Klonen',Updated=TO_TIMESTAMP('2020-07-07 18:44:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584720
+;
+
+-- 2020-07-07T15:44:28.603Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET Name='Zeile Klonen',Updated=TO_TIMESTAMP('2020-07-07 18:44:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=584720
+;
+
+-- 2020-07-07T15:44:31.626Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2020-07-07 18:44:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584720
+;
+
+-- 2020-07-07T15:44:33.717Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET Name='Clone Line',Updated=TO_TIMESTAMP('2020-07-07 18:44:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=584720
+;
+
+-- 2020-07-07T15:44:41.578Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Zeile Klonen',Updated=TO_TIMESTAMP('2020-07-07 18:44:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584720
+;
+
