d818a37628df7d7061ca9c08eb4f3ac782129392
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-07-11 10:14:54 +0200

    explore whether we can use latest camel-3
    created camel gettings-started problect with
    mvn archetype:generate -DarchetypeGroupId=org.apache.camel.archetypes -DarchetypeArtifactId=camel-archetype-java -DarchetypeVersion=3.0.0 -DgroupId=de.metas.esb.inout.shipment -DartifactId=de-metas-esb-shipmentschedule-camel -Dversion=10.0.0
    
    also, Jenkinsfile: extract some code into method buildAll

--- misc/services/buildfile.groovy
+++ misc/services/buildfile.groovy
@@ -15,22 +15,33 @@ def build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild=fal
 	currentBuild.description= """${currentBuild.description}<p/>
 			<h2>misc services</h2>
 		"""
+		withMaven(jdk: 'java-8', maven: 'maven-3.6.3', mavenLocalRepo: '.repository', mavenOpts: '-Xmx1536M', options: [artifactsPublisher(disabled: true)])
+				{
+					dir('edi') // todo: modernize and move to camel
+							{
+								def ediBuildFile = load('buildfile.groovy')
+								ediBuildFile.build(mvnConf, scmVars, forceBuild)
+							}
 
-		dir('edi')
-		{
-			def ediBuildFile = load('buildfile.groovy')
-			ediBuildFile.build(mvnConf, scmVars, forceBuild)
-		}
-		dir('procurement-webui')
-		{
-			def procurementWebuiBuildFile = load('buildfile.groovy')
-			procurementWebuiBuildFile.build(mvnConf, scmVars, forceBuild)
-		}
-		dir('admin')
-		{
-			def procurementWebuiBuildFile = load('buildfile.groovy')
-			procurementWebuiBuildFile.build(mvnConf, scmVars, forceBuild)
-		}
+					dir('procurement-webui')
+							{
+								def procurementWebuiBuildFile = load('buildfile.groovy')
+								procurementWebuiBuildFile.build(mvnConf, scmVars, forceBuild)
+							}
+					dir('admin')
+							{
+								def procurementWebuiBuildFile = load('buildfile.groovy')
+								procurementWebuiBuildFile.build(mvnConf, scmVars, forceBuild)
+							}
+				}
+		withMaven(jdk: 'java-14', maven: 'maven-3.6.3', mavenLocalRepo: '.repository', mavenOpts: '-Xmx1536M', options: [artifactsPublisher(disabled: true)])
+				{
+					dir('camel')
+							{
+								def camelBuildFile = load('buildfile.groovy')
+								camelBuildFile.build(mvnConf, scmVars, forceBuild)
+							}
+				}
     } // stage
 } 
 
