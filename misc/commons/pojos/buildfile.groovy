0a2708134a5c6fb9440acff43c6a37d43457f520
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-07-12 21:22:18 +0200

    attempt to fix another build problem

--- misc/commons/pojos/buildfile.groovy
+++ misc/commons/pojos/buildfile.groovy
@@ -7,51 +7,50 @@
 import de.metas.jenkins.MvnConf
 
 
-def build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild=false)
-{
-	final String VERSIONS_PLUGIN = 'org.codehaus.mojo:versions-maven-plugin:2.7'
+def build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild = false) {
+    final String VERSIONS_PLUGIN = 'org.codehaus.mojo:versions-maven-plugin:2.7'
 
-	// stage('Build edi')  // too many stages clutter the build info
+    // stage('Build camel components')  // too many stages clutter the build info
     //{
-		currentBuild.description= """${currentBuild.description}<p/>
+    currentBuild.description = """${currentBuild.description}<p/>
 			<h3>shipment-schedule-adapter</h3>
 		"""
 
-	def anyFileChanged
-	try {
-		def vgitout = sh(returnStdout: true, script: "git diff --name-only ${scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT} ${scmVars.GIT_COMMIT} .").trim()
-		echo "git diff output (modified files):\n>>>>>\n${vgitout}\n<<<<<"
-		anyFileChanged = !vgitout.isEmpty()
-		// see if anything at all changed in this folder
-		echo "Any file changed compared to last build: ${anyFileChanged}"
-	} catch (ignored) {
-		echo "git diff error => assume something must have changed"
-		anyFileChanged = true
-	}
+    def anyFileChanged
+    try {
+        def vgitout = sh(returnStdout: true, script: "git diff --name-only ${scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT} ${scmVars.GIT_COMMIT} .").trim()
+        echo "git diff output (modified files):\n>>>>>\n${vgitout}\n<<<<<"
+        anyFileChanged = !vgitout.isEmpty()
+        // see if anything at all changed in this folder
+        echo "Any file changed compared to last build: ${anyFileChanged}"
+    } catch (ignored) {
+        echo "git diff error => assume something must have changed"
+        anyFileChanged = true
+    }
 
-	if(scmVars.GIT_COMMIT && scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT && !anyFileChanged && !forceBuild)
-	{
-		currentBuild.description= """${currentBuild.description}<p/>
+    if (scmVars.GIT_COMMIT && scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT && !anyFileChanged && !forceBuild) {
+        currentBuild.description = """${currentBuild.description}<p/>
 					No changes happened in EDI.
 					"""
-		echo "no changes happened in EDI; skip building EDI";
-		return;
-	}
+        echo "no changes happened in EDI; skip building EDI";
+        return;
+    }
 
 
-		// set the root-pom's parent pom. Although the parent pom is avaialbe via relativePath, we need it to be this build's version then the root pom is deployed to our maven-repo
-		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DparentVersion=${env.MF_VERSION} ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:update-parent"
-		
-		// set the artifact version of everything below de.metas.esb/pom.xml
-		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} -DnewVersion=${env.MF_VERSION} ${VERSIONS_PLUGIN}:set"
+    // set the root-pom's parent pom. Although the parent pom is avaialbe via relativePath, we need it to be this build's version then the root pom is deployed to our maven-repo
+    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DparentVersion=${env.MF_VERSION} ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:update-parent"
 
-		// update the versions of metas dependencies that are external to the de.metas.esb reactor modules
-		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:use-latest-versions"
+    // set the artifact version of everything below de.metas.esb/pom.xml
+    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} -DnewVersion=${env.MF_VERSION} ${VERSIONS_PLUGIN}:set"
 
-		// build and install
-		// about -Dmetasfresh.assembly.descriptor.version: the versions plugin can't update the version of our shared assembly descriptor de.metas.assemblies. Therefore we need to provide the version from outside via this property
-		// maven.test.failure.ignore=true: see metasfresh stage
-		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dmaven.test.failure.ignore=true -Dmetasfresh.assembly.descriptor.version=${env.MF_VERSION} ${mvnConf.resolveParams} ${mvnConf.deployParam} clean install"
+    // Set the metasfresh.version property from 10.0.0 to our current build version
+    // From the documentation: "Set a property to a given version without any sanity checks"; that's what we want here..sanity is too much in the eye of the beholder
+    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dproperty=metasfresh.version -DnewVersion=${env.MF_VERSION} ${VERSIONS_PLUGIN}:set-property"
+
+    // build and install
+    // about -Dmetasfresh.assembly.descriptor.version: the versions plugin can't update the version of our shared assembly descriptor de.metas.assemblies. Therefore we need to provide the version from outside via this property
+    // maven.test.failure.ignore=true: see metasfresh stage
+    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dmaven.test.failure.ignore=true -Dmetasfresh.assembly.descriptor.version=${env.MF_VERSION} ${mvnConf.resolveParams} ${mvnConf.deployParam} clean install"
     //} // stage
 }
 
