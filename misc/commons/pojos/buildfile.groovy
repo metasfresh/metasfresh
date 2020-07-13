e85a62fb71efcb03b843ac10b0ebfbf51988b956
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-07-13 07:58:05 +0200

    attempts to fix build by deploying commons artifact

--- misc/commons/pojos/buildfile.groovy
+++ misc/commons/pojos/buildfile.groovy
@@ -47,10 +47,10 @@ def build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild = f
     // From the documentation: "Set a property to a given version without any sanity checks"; that's what we want here..sanity is too much in the eye of the beholder
     sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dproperty=metasfresh.version -DnewVersion=${env.MF_VERSION} ${VERSIONS_PLUGIN}:set-property"
 
-    // build and install
+    // build and deploy - components which need this might be in other builds
     // about -Dmetasfresh.assembly.descriptor.version: the versions plugin can't update the version of our shared assembly descriptor de.metas.assemblies. Therefore we need to provide the version from outside via this property
     // maven.test.failure.ignore=true: see metasfresh stage
-    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dmaven.test.failure.ignore=true -Dmetasfresh.assembly.descriptor.version=${env.MF_VERSION} ${mvnConf.resolveParams} ${mvnConf.deployParam} clean install"
+    sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -Dmaven.test.failure.ignore=true -Dmetasfresh.assembly.descriptor.version=${env.MF_VERSION} ${mvnConf.resolveParams} ${mvnConf.deployParam} clean deploy"
     //} // stage
 }
 
