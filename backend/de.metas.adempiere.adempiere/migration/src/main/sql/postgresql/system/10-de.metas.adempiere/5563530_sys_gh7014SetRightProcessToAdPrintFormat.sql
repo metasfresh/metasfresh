ae6a101ab24277704d9e789d8b877b4109184efb
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-07-15 15:30:44 +0300

    #7014 Set a working 'Bestellkontrolle' process to the corresponding ad_printformat entry.

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5563530_sys_gh7014SetRightProcessToAdPrintFormat.sql
@@ -0,0 +1,5 @@
+-- 2020-07-15T11:16:53.470Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormat SET JasperProcess_ID=540609,Updated=TO_TIMESTAMP('2020-07-15 14:16:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormat_ID=540097
+;
+
