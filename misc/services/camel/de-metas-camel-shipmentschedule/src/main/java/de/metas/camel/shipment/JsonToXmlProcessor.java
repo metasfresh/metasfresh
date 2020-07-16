f34de60e0533465b15e1feca4dff82e397b0e190
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-07-16 22:03:03 +0200

    create filemaker-pojos so we can read&write XML
    TODO
    * MIs, ErrorMessages etc to prevent fiddling with exported shipment schedules
    * dev-test;
    * also see & coordinate what's missing
    
    #7012

--- misc/services/camel/de-metas-camel-shipmentschedule/src/main/java/de/metas/camel/shipment/JsonToXmlProcessor.java
+++ misc/services/camel/de-metas-camel-shipmentschedule/src/main/java/de/metas/camel/shipment/JsonToXmlProcessor.java
@@ -22,6 +22,13 @@
 
 package de.metas.camel.shipment;
 
+import de.metas.common.filemaker.DATABASE;
+import de.metas.common.filemaker.FIELD;
+import de.metas.common.filemaker.FMPXMLRESULT;
+import de.metas.common.filemaker.FMPXMLRESULT.FMPXMLRESULTBuilder;
+import de.metas.common.filemaker.METADATA;
+import de.metas.common.filemaker.PRODUCT;
+import de.metas.common.shipmentschedule.JsonResponseShipmentCandidate;
 import de.metas.common.shipmentschedule.JsonResponseShipmentCandidates;
 import lombok.NonNull;
 import org.apache.camel.Exchange;
@@ -31,12 +38,50 @@ import org.apache.commons.logging.LogFactory;
 
 public class JsonToXmlProcessor implements Processor
 {
+	public static final METADATA METADATA = de.metas.common.filemaker.METADATA.builder()
+			.field(FIELD.builder().name("_bestellung_nummer").build())
+			.field(FIELD.builder().name("_bestellung_datum").build())
+			.field(FIELD.builder().name("_bestellung_zeitstempel").build())
+			.field(FIELD.builder().name("_artikel_nummer").build())
+			.field(FIELD.builder().name("_artikel_bezeichnung").build())
+			.field(FIELD.builder().name("_artikel_menge").build())
+			.field(FIELD.builder().name("_artikel_gewicht_1_stueck").build())
+			.field(FIELD.builder().name("_artikel_geschmacksrichtung").build())
+			.field(FIELD.builder().name("_artikel_verpackungsgroesse").build())
+			.field(FIELD.builder().name("_empfaenger_firma").build())
+			.field(FIELD.builder().name("_empfaenger_ansprechpartner").build())
+			.field(FIELD.builder().name("_empfaenger_strasse").build())
+			.field(FIELD.builder().name("_empfaenger_hausnummer").build())
+			.field(FIELD.builder().name("_empfaenger_zustellinfo").build())
+			.field(FIELD.builder().name("_empfaenger_plz").build())
+			.field(FIELD.builder().name("_empfaenger_ort").build())
+			.field(FIELD.builder().name("_empfaenger_land").build())
+			.field(FIELD.builder().name("_empfaenger_email").build())
+			.field(FIELD.builder().name("_empfaenger_telefon_muss_bei_express").build())
+			.build();
+
 	private final Log log = LogFactory.getLog(JsonToXmlProcessor.class);
 
 	@Override
-	public void process(@NonNull final Exchange exchange) throws Exception
+	public void process(@NonNull final Exchange exchange)
 	{
 		final JsonResponseShipmentCandidates scheduleList = exchange.getIn().getBody(JsonResponseShipmentCandidates.class);
 		log.info("process method called; scheduleList=" + scheduleList);
+
+		final FMPXMLRESULTBuilder builder = FMPXMLRESULT.builder()
+				.errorCode("0")
+				.product(new PRODUCT())
+				.database(DATABASE.builder()
+						.name("databaseName")// TODO set from application.properties
+						.records(Integer.toString(scheduleList.getItems().size()))
+						.build())
+				.metadata(METADATA);
+
+		for (final JsonResponseShipmentCandidate item : scheduleList.getItems())
+		{
+			// TODO
+		}
+		exchange.getIn().setBody(builder.build());
+		//	exchange.getIn().setHeader(Exchange.FILE_NAME, fileName);
 	}
 }
