8783266f29a2322ddf995ebd7036fc43692f017e
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-07-23 17:18:22 +0300

    #7012 moved migration scripts to right folder. update wrong script number.

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5564170_sys_gh7012NewAdMessagesProcessAndReadOnlyLogicInMReceiptSchedule.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5564170_sys_gh7012NewAdMessagesProcessAndReadOnlyLogicInMReceiptSchedule.sql
@@ -67,4 +67,3 @@ UPDATE AD_Column
 SET ReadOnlyLogic='@ExportStatus/''X''@ = ''EXPORTED'' | @ExportStatus/''X''@ = ''EXPORTED_AND_FORWARDED'' | @ExportStatus/''X''@ = ''EXPORTED_FORWARD_ERROR''',
     Updated=now(),UpdatedBy=100
 WHERE AD_Table_ID=540524
-;
