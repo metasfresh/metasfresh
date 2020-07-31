9327ff211c694879f318a5b054fc1c3db9796f16
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-07-31 10:23:31 +0200

    fix startup script
    #7085

--- misc/services/camel/de-metas-camel-edi/start_esb-camel_docker.sh
+++ misc/services/camel/de-metas-camel-edi/start_esb-camel_docker.sh
@@ -122,7 +122,7 @@ java_params="${java_params} ${java_additional_jvm_params}"
 run_metasfresh(){
 
  resolve_java_params
- cd /opt/metasfresh-esb-camel/ && java ${java_params} -jar de-metas-edi-esb-camel-exec.jar
+ cd /opt/metasfresh-esb-camel/ && java ${java_params} -jar de-metas-camel-edi-exec.jar
  
 }
 
