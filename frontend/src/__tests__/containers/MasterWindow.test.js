2101525cf9f86ad21a6db20a598459afbf2c8955
siemiatj <kuba@saskla.cz>
2020-08-04 21:09:44 +0200

    #7036 this should make the unit tests more stable

--- frontend/src/__tests__/containers/MasterWindow.test.js
+++ frontend/src/__tests__/containers/MasterWindow.test.js
@@ -90,113 +90,25 @@ const createInitialState = function(state = {}) {
 };
 
 describe('MasterWindowContainer', () => {
-  describe("'integration' tests:", () => {
-    it('renders without errors', async (done) => {
-      const initialState = createInitialState();
-      const store = createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(...middleware)
-      );
-      const windowType = FIXTURES_PROPS.props1.params.windowType;
-      const docId = FIXTURES_PROPS.props1.params.docId;
-      const tabId = layoutFixtures.layout1.tabs[0].tabId;
-      const auth = {
-        initNotificationClient: jest.fn(),
-        initSessionClient: jest.fn(),
-      };
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/${docId}/`)
-        .reply(200, dataFixtures.data1);
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/layout`)
-        .reply(200, layoutFixtures.layout1);
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get('/userSession')
-        .reply(200, userSessionData);
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/notifications/websocketEndpoint`)
-        .reply(200, `/notifications/${userSessionData.userProfileId}`);
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get('/notifications/all?limit=20')
-        .reply(200, notificationsData.data1);
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/${docId}/${tabId}/`)
-        .reply(200, { result: rowFixtures.row_data1 });
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/${docId}/${tabId}/?orderBy=%2BLine`)
-        .reply(200, { result: rowFixtures.row_data1 });
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/${docId}/${tabId}/topActions`)
-        .reply(200, topActionsFixtures.top_actions1);
-
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/${docId}/field/DocAction/dropdown`)
-        .reply(200, docActionFixtures.data1);
-
-      // This request doesn't happen here but in the `websockets` test. But component
-      // created here still exists and thus tries to handle this XHR response.
-      nock(config.API_URL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`/window/${windowType}/${docId}/?noTabs=true`)
-        .reply(200, dataFixtures.data1);
-
-      const wrapper = mount(
-        <Provider store={store}>
-          <ShortcutProvider hotkeys={{}} keymap={{}}>
-            <CustomRouter history={history} auth={auth} />
-          </ShortcutProvider>
-        </Provider>
-      );
-
-      await waitForExpect(() => {
-        wrapper.update();
-
-        const html = wrapper.html();
-        expect(html).toContain('<table');
-      }, 6000);
-
-      done();
-    }, 10000);
-  });
-
   describe('websocket tests', () => {
     let mockServer;
     let server;
 
-    beforeEach(() => {
+    beforeAll(() => {
       server = http.createServer();
 
       mockServer = new StompServer({
         server: server,
         path: '/ws',
-        heartbeat: [1000, 1000],
       });
 
       server.listen(8080);
     });
 
     // afterEach stop server
-    afterEach(() => {
+    afterAll(() => {
       server.close();
-    });
+    });    
 
     it('reacts to websocket events and updates the UI correctly', async (done) => {
       const initialState = createInitialState();
@@ -434,5 +346,95 @@ describe('MasterWindowContainer', () => {
 
       done();
     }, 20000);
+  });
+
+  describe("'integration' tests:", () => {
+    it('renders without errors', async (done) => {
+      const initialState = createInitialState();
+      const store = createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(...middleware)
+      );
+      const windowType = FIXTURES_PROPS.props1.params.windowType;
+      const docId = FIXTURES_PROPS.props1.params.docId;
+      const tabId = layoutFixtures.layout1.tabs[0].tabId;
+      const auth = {
+        initNotificationClient: jest.fn(),
+        initSessionClient: jest.fn(),
+      };
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/${docId}/`)
+        .reply(200, dataFixtures.data1);
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/layout`)
+        .reply(200, layoutFixtures.layout1);
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get('/userSession')
+        .reply(200, userSessionData);
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/notifications/websocketEndpoint`)
+        .reply(200, `/notifications/${userSessionData.userProfileId}`);
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get('/notifications/all?limit=20')
+        .reply(200, notificationsData.data1);
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/${docId}/${tabId}/`)
+        .reply(200, { result: rowFixtures.row_data1 });
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/${docId}/${tabId}/?orderBy=%2BLine`)
+        .reply(200, { result: rowFixtures.row_data1 });
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/${docId}/${tabId}/topActions`)
+        .reply(200, topActionsFixtures.top_actions1);
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/${docId}/field/DocAction/dropdown`)
+        .reply(200, docActionFixtures.data1);
+
+      let wrapper;
+      try {
+        wrapper = mount(
+        <Provider store={store}>
+          <ShortcutProvider hotkeys={{}} keymap={{}}>
+            <CustomRouter history={history} auth={auth} />
+          </ShortcutProvider>
+        </Provider>
+      );
+      } catch(e) {
+        console.log('e: ', e);
+      }
+
+      await waitForExpect(() => {
+        try {
+          wrapper.update();
+        } catch(e) {
+          console.log(e)
+        }
+        
+
+        const html = wrapper.html();
+        expect(html).toContain('<table');
+      }, 6000);
+
+      done();
+    }, 10000);
   });
 });
