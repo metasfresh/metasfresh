7a008e7a81bdfb53d81a138e1dfdb9108986fa23
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-08-11 11:59:09 +0300

    Jenkins rebuild test 3

--- frontend/src/__tests__/containers/MasterWindow.test.js
+++ frontend/src/__tests__/containers/MasterWindow.test.js
@@ -342,7 +342,7 @@ describe('MasterWindowContainer', () => {
         expect(wrapper.find('tbody tr').length).toBe(4);
         expect(wrapper.html()).toContain('288.86');
       });
-      // test 2
+      // test 3
 
       // wait for the DOM to be updated
       await waitFor(
