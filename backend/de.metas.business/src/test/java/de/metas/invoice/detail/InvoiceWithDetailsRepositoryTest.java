8a420da0e13f3b9a005c06fb54b21cdf2dac7ee4
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-08-27 07:05:50 +0200

    Fix the unit test.
    now if shows the bug in the code under test
    https://github.com/metasfresh/metasfresh/issues/9850

--- backend/de.metas.business/src/test/java/de/metas/invoice/detail/InvoiceWithDetailsRepositoryTest.java
+++ backend/de.metas.business/src/test/java/de/metas/invoice/detail/InvoiceWithDetailsRepositoryTest.java
@@ -22,20 +22,15 @@
 
 package de.metas.invoice.detail;
 
-
 import de.metas.business.BusinessTestHelper;
 import de.metas.invoice.InvoiceId;
 import de.metas.invoice.InvoiceLineId;
 import de.metas.invoice.service.IInvoiceDAO;
 import de.metas.organization.OrgId;
 import de.metas.util.Services;
-import org.adempiere.ad.dao.IQueryBL;
 import org.adempiere.ad.wrapper.POJOLookupMap;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.test.AdempiereTestHelper;
-
-import static org.assertj.core.api.Assertions.*;
-
 import org.compiere.model.I_C_Invoice;
 import org.compiere.model.I_C_InvoiceLine;
 import org.compiere.model.I_C_Invoice_Detail;
@@ -46,6 +41,11 @@ import org.junit.jupiter.api.Test;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
+import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 public class InvoiceWithDetailsRepositoryTest
 {
@@ -108,76 +108,66 @@ public class InvoiceWithDetailsRepositoryTest
 	{
 		final IInvoiceDAO invoiceDAO = Services.get(IInvoiceDAO.class);
 
-		I_C_Invoice invoice = InterfaceWrapperHelper.newInstance(I_C_Invoice.class);
-		InterfaceWrapperHelper.create(invoice, I_C_Invoice.class);
+		I_C_Invoice invoice = newInstance(I_C_Invoice.class);
 		invoice.setC_Invoice_ID(24);
 		invoice.setReversal_ID(42);
 		invoice.setC_BPartner_Location_ID(10);
-		InterfaceWrapperHelper.save(invoice);
+		saveRecord(invoice);
 
-		I_C_InvoiceLine invoiceLine = InterfaceWrapperHelper.newInstance(I_C_InvoiceLine.class);
+		I_C_InvoiceLine invoiceLine = newInstance(I_C_InvoiceLine.class);
 		invoiceLine.setC_Invoice_ID(24);
 		invoiceLine.setC_InvoiceLine_ID(241);
 		invoiceLine.setLine(1);
-		InterfaceWrapperHelper.save(invoiceLine);
+		saveRecord(invoiceLine);
 
-		I_C_Invoice_Detail invoice_detail = InterfaceWrapperHelper.newInstance(I_C_Invoice_Detail.class);
+		I_C_Invoice_Detail invoice_detail = newInstance(I_C_Invoice_Detail.class);
 		invoice_detail.setC_Invoice_Detail_ID(1001);
 		invoice_detail.setC_Invoice_ID(24);
 		invoice_detail.setSeqNo(25);
 		invoice_detail.setLabel("TestLabel");
 		invoice_detail.setDescription("Test Description");
-		InterfaceWrapperHelper.save(invoice_detail);
+		saveRecord(invoice_detail);
 
-		I_C_Invoice_Detail invoice_detailLine = InterfaceWrapperHelper.newInstance(I_C_Invoice_Detail.class);
+		I_C_Invoice_Detail invoice_detailLine = newInstance(I_C_Invoice_Detail.class);
 		invoice_detailLine.setC_Invoice_Detail_ID(1002);
 		invoice_detailLine.setC_Invoice_ID(24);
 		invoice_detailLine.setC_InvoiceLine_ID(241);
 		invoice_detailLine.setSeqNo(26);
 		invoice_detailLine.setLabel("TestLabel for Line");
 		invoice_detailLine.setDescription("Test Description for Line");
-		InterfaceWrapperHelper.save(invoice_detailLine);
+		saveRecord(invoice_detailLine);
 
-
-		I_C_Invoice reversalInvoice = InterfaceWrapperHelper.newInstance(I_C_Invoice.class);
+		I_C_Invoice reversalInvoice = newInstance(I_C_Invoice.class);
 		InterfaceWrapperHelper.create(invoice, I_C_Invoice.class);
 		reversalInvoice.setC_Invoice_ID(42);
 		reversalInvoice.setReversal_ID(24);
 		reversalInvoice.setC_BPartner_Location_ID(10);
-		InterfaceWrapperHelper.save(reversalInvoice);
+		saveRecord(reversalInvoice);
 
-		I_C_InvoiceLine reversalInvoiceLine = InterfaceWrapperHelper.newInstance(I_C_InvoiceLine.class);
+		I_C_InvoiceLine reversalInvoiceLine = newInstance(I_C_InvoiceLine.class);
 		reversalInvoiceLine.setC_InvoiceLine_ID(421);
 		reversalInvoiceLine.setC_Invoice_ID(42);
 		reversalInvoiceLine.setLine(1);
-		InterfaceWrapperHelper.save(reversalInvoiceLine);
+		saveRecord(reversalInvoiceLine);
 
-		final List<I_C_Invoice_Detail> detailRecords = Services.get(IQueryBL.class)
-				.createQueryBuilder(I_C_Invoice_Detail.class)
-				.addOnlyActiveRecordsFilter()
-				.addEqualsFilter(I_C_Invoice_Detail.COLUMNNAME_C_Invoice_ID, 24)
-				.create()
-				.list();
+		// guard
+		final List<I_C_Invoice_Detail> invoiceDetailRecordsBefore = POJOLookupMap.get().getRecords(I_C_Invoice_Detail.class);
+		assertThat(invoiceDetailRecordsBefore).extracting("C_Invoice_ID", "C_InvoiceLine_ID")
+				.containsExactlyInAnyOrder(
+						tuple(24, 0),
+						tuple(24, 241));
 
-		assertThat(detailRecords.size() == 2);
-		assertThat(detailRecords.get(0).getC_Invoice_ID() == 24);
-		assertThat(detailRecords.get(1).getC_Invoice_ID() == 24);
-		assertThat(detailRecords.get(1).getC_InvoiceLine_ID() == 241);
-
-
+		// when
 		InvoiceWithDetailsRepository invoiceWithDetailsRepository = new InvoiceWithDetailsRepository();
 		invoiceWithDetailsRepository.saveReversalDetails(InvoiceId.ofRepoId(24), InvoiceId.ofRepoId(42));
 
-		final List<I_C_Invoice_Detail> reversedDetailRecords = Services.get(IQueryBL.class)
-				.createQueryBuilder(I_C_Invoice_Detail.class)
-				.addOnlyActiveRecordsFilter()
-				.addEqualsFilter(I_C_Invoice_Detail.COLUMNNAME_C_Invoice_ID, 42)
-				.create()
-				.list();
-
-		assertThat(reversedDetailRecords.size() == 2);
-		assertThat(reversedDetailRecords.get(0).getC_Invoice_ID() == 42);
-		assertThat(reversedDetailRecords.get(1).getC_Invoice_ID() == 42);
-		assertThat(reversedDetailRecords.get(1).getC_InvoiceLine_ID() == 421);
+		// then
+		final List<I_C_Invoice_Detail> invoiceDetailRecords = POJOLookupMap.get().getRecords(I_C_Invoice_Detail.class);
+		assertThat(invoiceDetailRecords).extracting("C_Invoice_ID", "C_InvoiceLine_ID").hasSize(4)
+				.containsExactlyInAnyOrder(
+						tuple(24, 0),
+						tuple(24, 241),
+						tuple(42, 0),
+						tuple(42, 421));
 	}
 }
