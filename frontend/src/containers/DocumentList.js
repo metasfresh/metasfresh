8f18a2933bf79b8c3b28c5b3edb6e3dd4bc778e9
siemiatj <kuba@saskla.cz>
2020-08-31 17:58:17 +0200

    #9865 update included views props everywhere

--- frontend/src/containers/DocumentList.js
+++ frontend/src/containers/DocumentList.js
@@ -24,6 +24,8 @@ import {
   resetView,
   deleteView,
   showIncludedView,
+  setIncludedView,
+  unsetIncludedView,
 } from '../actions/ViewActions';
 import {
   deleteTable,
@@ -33,8 +35,6 @@ import {
 } from '../actions/TableActions';
 import { clearAllFilters } from '../actions/FiltersActions';
 import {
-  closeListIncludedView,
-  setListIncludedView,
   setListId,
   setPagination as setListPagination,
   setSorting as setListSorting,
@@ -110,7 +110,7 @@ class DocumentListContainer extends Component {
       windowId,
       refDocumentId,
       referenceId,
-      closeListIncludedView,
+      unsetIncludedView,
       viewId,
       resetView,
       deleteView,
@@ -124,7 +124,7 @@ class DocumentListContainer extends Component {
     const { staticFilterCleared } = this.state;
 
     const included =
-      includedView && includedView.windowType && includedView.viewId;
+      includedView && includedView.windowId && includedView.viewId;
     const location = document.location;
 
     if (nextProps.filters.clearAll) {
@@ -191,7 +191,7 @@ class DocumentListContainer extends Component {
           // TODO: Check if we can just call `showIncludedView` to hide
           // it in the resetView Action Creator
           if (included) {
-            closeListIncludedView(includedView);
+            unsetIncludedView(includedView);
           }
           this.fetchLayoutAndData();
         }
@@ -435,7 +435,7 @@ class DocumentListContainer extends Component {
       isIncluded,
       sort,
       viewId,
-      setListIncludedView,
+      setIncludedView,
       setModalDescription,
       filterView,
       isModal,
@@ -463,7 +463,7 @@ class DocumentListContainer extends Component {
         }
 
         if (isIncluded) {
-          setListIncludedView({ windowType: windowId, viewId: newViewId });
+          setIncludedView({ windowType: windowId, viewId: newViewId });
         }
 
         if (isModal) {
@@ -752,7 +752,7 @@ class DocumentListContainer extends Component {
             id: identifier,
             showIncludedView: item.supportIncludedViews,
             windowId: item.supportIncludedViews
-              ? item.includedView.windowType || item.includedView.windowId
+              ? item.includedView.windowId || item.includedView.windowId
               : null,
             viewId: item.supportIncludedViews ? item.includedView.viewId : '',
             isModal,
@@ -774,7 +774,7 @@ class DocumentListContainer extends Component {
       layout &&
       layout.includedView &&
       includedView &&
-      includedView.windowType &&
+      includedView.windowId &&
       includedView.viewId;
     const triggerSpinner = layout.supportAttributes
       ? layoutPending
@@ -822,8 +822,8 @@ export default connect(
     filterView,
     deleteTable,
     indicatorState,
-    closeListIncludedView,
-    setListIncludedView,
+    unsetIncludedView,
+    setIncludedView,
     setListPagination,
     setListSorting,
     setListId,
