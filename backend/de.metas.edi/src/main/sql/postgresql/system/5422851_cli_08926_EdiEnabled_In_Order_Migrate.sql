fbcbe3a223b2a6dd4173fe6be16f9777b879ca5f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-09-14 13:37:55 +0200

    move migration scripts to the right place

--- backend/de.metas.edi/src/main/sql/postgresql/system/5422851_cli_08926_EdiEnabled_In_Order_Migrate.sql
+++
@@ -1,4 +0,0 @@
-UPDATE C_Order
-SET IsEdiEnabled = 'Y'
-WHERE AD_InputDataSource_ID = 150 -- "Importiert aus EDI-ORDERS-Datei"
-;
--- backend/de.metas.edi/src/main/sql/postgresql/system/5431010_sys_Fix_EntityType.sql
+++
@@ -1,6 +0,0 @@
-update AD_Field set EntityType='de.metas.esb.edi'
-where AD_Field_ID in (
-	-- M_InOut.IsEdiEnabled (shipment, receipt windows)
-	553213, 553216
-);
-
