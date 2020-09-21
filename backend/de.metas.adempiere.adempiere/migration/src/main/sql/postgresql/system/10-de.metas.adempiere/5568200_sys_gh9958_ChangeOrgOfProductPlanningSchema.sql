c3c265f1539aa43f455ea156b38b49577d0a703d
TheBestPessimist <cristian@tbp.land>
2020-09-21 13:58:15 +0300

    This sql looks better
    https://github.com/metasfresh/metasfresh/issues/9958

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5568200_sys_gh9958_ChangeOrgOfProductPlanningSchema.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5568200_sys_gh9958_ChangeOrgOfProductPlanningSchema.sql
@@ -1,9 +1,9 @@
 UPDATE m_product_planningschema
-SET ad_org_id = (SELECT ad_org_id
-                 FROM ad_org
-                 WHERE ad_org.ad_org_id != 0
-                   AND ad_org.isactive = 'Y'
-                 ORDER BY ad_org_id
+SET ad_org_id = (SELECT o.ad_org_id
+                 FROM ad_org o
+                 WHERE o.ad_org_id != 0
+                   AND o.isactive = 'Y'
+                 ORDER BY o.ad_org_id
                  LIMIT 1
 )
   , Updated=TO_TIMESTAMP('2020-09-21 12:16:00', 'YYYY-MM-DD HH24:MI:SS')
