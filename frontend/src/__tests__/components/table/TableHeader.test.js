092fcd9e26909ae8a8855ba6714c05f9f50d5b99
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-09-22 14:33:05 +0300

    Restore the initial TableHeader test

--- frontend/src/__tests__/components/table/TableHeader.test.js
+++ frontend/src/__tests__/components/table/TableHeader.test.js
@@ -1,49 +1,23 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-import { ShortcutProvider } from '../../../components/keyshortcuts/ShortcutProvider';
+import { shallow, mount } from 'enzyme';
+
 import tableHeaderProps from '../../../../test_setup/fixtures/table/table_header.json';
-import { initialState as appHandlerState } from '../../../reducers/appHandler';
-import { initialState as windowHandlerState } from '../../../reducers/windowHandler';
-import TableHeader from '../../../components/table/TableHeader';
-import hotkeys from '../../../../test_setup/fixtures/hotkeys.json';
-import keymap from '../../../../test_setup/fixtures/keymap.json';
-import merge from 'merge';
-import thunk from 'redux-thunk';
-const mockStore = configureStore([thunk]);
+
+import { TableHeader } from '../../../components/table/TableHeader';
 
 const props = {
   ...tableHeaderProps,
   selected: [],
 };
 
-const initialState = function(state = {}) {
-  const res = merge.recursive(
-    true,
-    {
-      appHandler: { ...appHandlerState },
-      windowHandler: { ...windowHandlerState },
-    },
-    state
-  );
-
-  return res;
-};
-const store = mockStore(initialState);
-
 describe('TableHeader', () => {
   it('renders without errors with the given props', () => {
-    const wrapperTableCMenu = render(
-      <Provider store={store}>
-        <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
-          <table>
-            <thead>
-              <TableHeader {...props} />
-            </thead>
-          </table>
-        </ShortcutProvider>
-      </Provider>
+    const wrapperTableCMenu = shallow(
+      <table>
+        <thead>
+          <TableHeader {...props} />
+        </thead>
+      </table>
     );
     const html = wrapperTableCMenu.html();
 
@@ -62,16 +36,12 @@ describe('TableHeader', () => {
       indentSupported: true,
     };
 
-    const wrapperTableCMenu = render(
-      <Provider store={store}>
-        <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
-          <table>
-            <thead>
-              <TableHeader {...localProps} />
-            </thead>
-          </table>
-        </ShortcutProvider>
-      </Provider>
+    const wrapperTableCMenu = shallow(
+      <table>
+        <thead>
+          <TableHeader {...localProps} />
+        </thead>
+      </table>
     );
     const html = wrapperTableCMenu.html();
 
@@ -85,30 +55,21 @@ describe('TableHeader', () => {
       onSortTable: onSortTableSpy,
       setActiveSort: jest.fn(),
       deselect: jest.fn(),
+      headersFields: { DateOrdered: false },
     };
 
     const wrapper = mount(
-      <Provider store={store}>
-        <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
-          <table>
-            <thead>
-              <TableHeader {...localProps} />
-            </thead>
-          </table>
-        </ShortcutProvider>
-      </Provider>
+      <table>
+        <thead>
+          <TableHeader {...localProps} />
+        </thead>
+      </table>
     );
     const html = wrapper.html();
 
     expect(html).toContain('sort-menu');
-    expect(
-      wrapper.find(
-        `.th-caption[title="${tableHeaderProps.cols[0].description}"]`
-      ).length
-    ).toBe(1);
-    wrapper
-      .find(`.th-caption[title="${tableHeaderProps.cols[0].description}"]`)
-      .simulate('click');
+    expect(wrapper.find(`.th-caption[title="${tableHeaderProps.cols[0].description}"]`).length).toBe(1);
+    wrapper.find(`.th-caption[title="${tableHeaderProps.cols[0].description}"]`).simulate('click')
     expect(onSortTableSpy).toHaveBeenCalled();
   });
 });
