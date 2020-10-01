1ac333a6ec5df5e4a5f45debbc1f62bd84cb4627
Cristina <cristinamghita@gmail.com>
2020-10-01 12:24:18 +0300

    #10045 Add sys config

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5569170_sys_gh10045_AddSysConfig.sql
@@ -0,0 +1,4 @@
+-- 2020-10-01T08:04:59.066Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541342,'C',TO_TIMESTAMP('2020-10-01 11:04:58','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.handlingunits.impl.HUPIItemProductDAO.Enforce_ M_HU_PI_Item_Product_ID',TO_TIMESTAMP('2020-10-01 11:04:58','YYYY-MM-DD HH24:MI:SS'),100,'N')
+;
