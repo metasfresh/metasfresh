c3c82c44a9857e72141287d6fba43f389eba0687
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-10-05 12:42:07 +0300

    Put an active filter

--- frontend/src/__tests__/components/filters/Filters.test.js
+++ frontend/src/__tests__/components/filters/Filters.test.js
@@ -118,175 +118,154 @@ describe('Filters tests', () => {
     expect(wrapper.find('.filter-widget .filter-default').length).toBe(1);
   });
 
-  // //@TODO: I expect this to be replaced by a combination of small unit and e2e tests, but
-  // // for now it doesn't make sense to write targeted unit tests for Filter descendant components
-  // // as the widgets need an architecture overhaul, and filters should be moved to redux state
-  // describe('Temporary bloated filter tests', () => {
-  //   // https://github.com/metasfresh/me03/issues/3649
-  //   it('clears list filters and applies without error', () => {
-  //     const dummyProps = createInitialProps(undefined, {
-  //       filtersActive: filtersFixtures.filtersActive2,
-  //     });
-  //     const initialState = createStore({
-  //       windowHandler: {
-  //         allowShortcut: true,
-  //         modal: {
-  //           visible: false,
-  //         },
-  //       },
-  //     });
-  //     const store = mockStore(initialState);
-  //     const wrapper = mount(
-  //       <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
-  //         <Provider store={store}>
-  //           <div className="document-lists-wrapper">
-  //             <Filters {...dummyProps} />
-  //           </div>
-  //         </Provider>
-  //       </ShortcutProvider>
-  //     );
+  //@TODO: I expect this to be replaced by a combination of small unit and e2e tests, but
+  // for now it doesn't make sense to write targeted unit tests for Filter descendant components
+  // as the widgets need an architecture overhaul, and filters should be moved to redux state
+  describe('Temporary bloated filter tests', () => {
+    // https://github.com/metasfresh/me03/issues/3649
+    it('clears list filters and applies without error', () => {
+      const dummyProps = createInitialProps(undefined, {
+        filtersActive: filtersFixtures.filtersActive2,
+      });
 
-  //     wrapper.find('.filters-not-frequent .btn-filter').simulate('click');
-  //     expect(wrapper.find('.filters-overlay').length).toBe(1);
-  //     expect(wrapper.find('.filter-option-default').length).toBe(0);
+      const store = mockStore(initialState);
+      const wrapper = mount(
+        <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
+          <Provider store={store}>
+            <div className="document-lists-wrapper">
+              <Filters {...dummyProps} />
+            </div>
+          </Provider>
+        </ShortcutProvider>
+      );
 
-  //     expect(wrapper.find('FiltersItem').state().activeFilter).toBeTruthy();
+      wrapper.find('.filters-not-frequent .btn-filter').simulate('click');
+      expect(wrapper.find('.filters-overlay').length).toBe(1);
+      expect(wrapper.find('.filter-option-default').length).toBe(0);
 
-  //     wrapper
-  //       .find('.form-field-C_DocType_ID .meta-icon-close-alt')
-  //       .simulate('click');
-  //     wrapper.update();
-  //     wrapper
-  //       .find('.form-field-DocStatus .meta-icon-close-alt')
-  //       .simulate('click');
-  //     wrapper.update();
+      expect(wrapper.find('FiltersItem').state().activeFilter).toBeTruthy();
 
-  //     expect(wrapper.find('FiltersItem').state().activeFilter).toBeFalsy();
-  //     wrapper
-  //       .find('.filter-widget .filter-btn-wrapper .applyBtn')
-  //       .simulate('click');
+      wrapper
+        .find('.form-field-C_DocType_ID .meta-icon-close-alt')
+        .simulate('click');
+      wrapper.update();
+      wrapper
+        .find('.form-field-DocStatus .meta-icon-close-alt')
+        .simulate('click');
+      wrapper.update();
 
-  //     wrapper.update();
-  //     expect(wrapper.find('.filters-overlay').length).toBe(0);
-  //   });
+      expect(wrapper.find('FiltersItem').state().activeFilter).toBeFalsy();
+      wrapper
+        .find('.filter-widget .filter-btn-wrapper .applyBtn')
+        .simulate('click');
 
-  //   it('supports `false` values for checkbox widgets', () => {
-  //     const updateDocListListener = jest.fn();
-  //     const dummyProps = createInitialProps(filtersFixtures.data2, {
-  //       filtersActive: filtersFixtures.filtersActive3,
-  //       updateDocList: updateDocListListener,
-  //     });
-  //     const initialState = createStore({
-  //       windowHandler: {
-  //         allowShortcut: true,
-  //         modal: {
-  //           visible: false,
-  //         },
-  //       },
-  //     });
-  //     const store = mockStore(initialState);
-  //     const wrapper = mount(
-  //       <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
-  //         <Provider store={store}>
-  //           <div className="document-lists-wrapper">
-  //             <Filters {...dummyProps} />
-  //           </div>
-  //         </Provider>
-  //       </ShortcutProvider>
-  //     );
+      wrapper.update();
+      expect(wrapper.find('.filters-overlay').length).toBe(0);
+    });
 
-  //     wrapper.find('.filters-not-frequent .btn-filter').simulate('click');
-  //     expect(wrapper.find('.filters-overlay').length).toBe(1);
-  //     expect(wrapper.find('.filter-option-default').length).toBe(0);
-  //     expect(wrapper.find('FiltersItem').state().activeFilter).toBeTruthy();
-  //     expect(
-  //       wrapper.find('.form-field-Processed .input-checkbox-tick.checked')
-  //         .length
-  //     ).toBe(1);
+    // it('supports `false` values for checkbox widgets', () => {
+    //   const updateDocListListener = jest.fn();
+    //   const dummyProps = createInitialProps(filtersFixtures.data2, {
+    //     filtersActive: filtersFixtures.filtersActive3,
+    //     updateDocList: updateDocListListener,
+    //   });
 
-  //     wrapper
-  //       .find('.form-field-Processed input[type="checkbox"]')
-  //       .simulate('change', { target: { checked: false } });
-  //     wrapper.update();
+    //   const store = mockStore(initialState);
+    //   const wrapper = mount(
+    //     <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
+    //       <Provider store={store}>
+    //         <div className="document-lists-wrapper">
+    //           <Filters {...dummyProps} />
+    //         </div>
+    //       </Provider>
+    //     </ShortcutProvider>
+    //   );
 
-  //     expect(
-  //       wrapper.find(
-  //         '.form-field-Processed .input-checkbox-tick.input-state-false'
-  //       ).length
-  //     ).toBe(1);
-  //     expect(wrapper.find('FiltersItem').state().activeFilter).toBeTruthy();
-  //     wrapper
-  //       .find('.filter-widget .filter-btn-wrapper .applyBtn')
-  //       .simulate('click');
-  //     wrapper.update();
+    //   wrapper.find('.filters-not-frequent .btn-filter').simulate('click');
+    //   expect(wrapper.find('.filters-overlay').length).toBe(1);
+    //   expect(wrapper.find('.filter-option-default').length).toBe(0);
+    //   expect(wrapper.find('FiltersItem').state().activeFilter).toBeTruthy();
+    //   expect(
+    //     wrapper.find('.form-field-Processed .input-checkbox-tick.checked')
+    //       .length
+    //   ).toBe(1);
 
-  //     const filterResult = Immutable.Map({
-  //       default: {
-  //         defaultVal: false,
-  //         filterId: 'default',
-  //         parameters: [
-  //           {
-  //             parameterName: 'Processed',
-  //             value: false,
-  //             valueTo: '',
-  //             defaultValue: null,
-  //             defaultValueTo: null,
-  //           },
-  //         ],
-  //       },
-  //     });
+    //   wrapper
+    //     .find('.form-field-Processed input[type="checkbox"]')
+    //     .simulate('change', { target: { checked: false } });
+    //   wrapper.update();
 
-  //     expect(updateDocListListener).toBeCalledWith(filterResult);
-  //   });
+    //   expect(
+    //     wrapper.find(
+    //       '.form-field-Processed .input-checkbox-tick.input-state-false'
+    //     ).length
+    //   ).toBe(1);
+    //   expect(wrapper.find('FiltersItem').state().activeFilter).toBeTruthy();
+    //   wrapper
+    //     .find('.filter-widget .filter-btn-wrapper .applyBtn')
+    //     .simulate('click');
+    //   wrapper.update();
 
-  //   it('supports filters without parameters', () => {
-  //     const updateDocListListener = jest.fn();
-  //     const dummyProps = createInitialProps(filtersFixtures.data3, {
-  //       updateDocList: updateDocListListener,
-  //     });
-  //     const initialState = createStore({
-  //       windowHandler: {
-  //         allowShortcut: true,
-  //         modal: {
-  //           visible: false,
-  //         },
-  //       },
-  //     });
-  //     const store = mockStore(initialState);
-  //     const wrapper = mount(
-  //       <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
-  //         <Provider store={store}>
-  //           <div className="document-lists-wrapper">
-  //             <Filters {...dummyProps} />
-  //           </div>
-  //         </Provider>
-  //       </ShortcutProvider>
-  //     );
-  //     wrapper.find('.filters-not-frequent .btn-filter').simulate('click');
-  //     expect(wrapper.find('.filters-overlay').length).toBe(1);
+    //   const filterResult = Immutable.Map({
+    //     default: {
+    //       defaultVal: false,
+    //       filterId: 'default',
+    //       parameters: [
+    //         {
+    //           parameterName: 'Processed',
+    //           value: false,
+    //           valueTo: '',
+    //           defaultValue: null,
+    //           defaultValueTo: null,
+    //         },
+    //       ],
+    //     },
+    //   });
 
-  //     wrapper
-  //       .find('.filters-overlay .filter-option-userquery-540024')
-  //       .simulate('click');
-  //     expect(wrapper.find('FiltersItem').state().activeFilter).toBeFalsy();
+    //   expect(updateDocListListener).toBeCalledWith(filterResult);
+    // });
 
-  //     wrapper
-  //       .find('.filter-widget .filter-btn-wrapper .applyBtn')
-  //       .simulate('click');
-  //     wrapper.update();
+    // it('supports filters without parameters', () => {
+    //   const updateDocListListener = jest.fn();
+    //   const dummyProps = createInitialProps(filtersFixtures.data3, {
+    //     updateDocList: updateDocListListener,
+    //   });
 
-  //     const filterResult = Immutable.Map({
-  //       'userquery-540024': {
-  //         filterId: 'userquery-540024',
-  //         caption: 'Abrechnung_offen_normal',
-  //         frequent: false,
-  //         inlineRenderMode: 'button',
-  //         parametersLayoutType: 'panel',
-  //         debugProperties: {},
-  //       },
-  //     });
+    //   const store = mockStore(initialState);
+    //   const wrapper = mount(
+    //     <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
+    //       <Provider store={store}>
+    //         <div className="document-lists-wrapper">
+    //           <Filters {...dummyProps} />
+    //         </div>
+    //       </Provider>
+    //     </ShortcutProvider>
+    //   );
+    //   wrapper.find('.filters-not-frequent .btn-filter').simulate('click');
+    //   expect(wrapper.find('.filters-overlay').length).toBe(1);
 
-  //     expect(updateDocListListener).toBeCalledWith(filterResult);
-  //   });
-  // });
+    //   wrapper
+    //     .find('.filters-overlay .filter-option-userquery-540024')
+    //     .simulate('click');
+    //   expect(wrapper.find('FiltersItem').state().activeFilter).toBeFalsy();
+
+    //   wrapper
+    //     .find('.filter-widget .filter-btn-wrapper .applyBtn')
+    //     .simulate('click');
+    //   wrapper.update();
+
+    //   const filterResult = Immutable.Map({
+    //     'userquery-540024': {
+    //       filterId: 'userquery-540024',
+    //       caption: 'Abrechnung_offen_normal',
+    //       frequent: false,
+    //       inlineRenderMode: 'button',
+    //       parametersLayoutType: 'panel',
+    //       debugProperties: {},
+    //     },
+    //   });
+
+    //   expect(updateDocListListener).toBeCalledWith(filterResult);
+    // });
+  });
 });
