630747f64836b10c7f1219562a297829d4ad3ae3
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-10-06 11:35:12 +0300

    test dispatching CREATE_FILTER action

---
+++ frontend/src/__tests__/actions/FiltersActions.test.js
@@ -0,0 +1,46 @@
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import produce from 'immer';
+import merge from 'merge';
+
+import tablesHandler, { tableState, getTableId } from '../../reducers/tables';
+import viewHandler from '../../reducers/viewHandler';
+
+import {
+  clearAllFilters,
+  createFilter,
+  deleteFilter,
+  updateNotValidFields,
+  updateActiveFilter,
+  updateInlineFilter,
+  updateFilterWidgetShown,
+  clearStaticFilters,
+} from '../../actions/FiltersActions';
+import * as ACTION_TYPES from '../../constants/FilterTypes';
+import filtersData from '../../../test_setup/fixtures/filters/filtersActionsMock.json';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('FiltersActions general', () => {
+  it('should call DELETE_FILTER action with correct payload', () => {
+    const payload = { id: '143_143-Q' };
+    const action = deleteFilter(payload.id);
+
+    expect(action.type).toEqual(ACTION_TYPES.DELETE_FILTER);
+    expect(action.payload).toHaveProperty('id', payload.id);
+  });
+
+  it(`dispatches 'CREATE_FILTER' action when creating the filters`, () => {
+    const expectedActions = [
+      {
+        type: ACTION_TYPES.CREATE_FILTER,
+        payload: { id: '540092_540092-FF', data: filtersData },
+      },
+    ];
+
+    const store = mockStore();
+    store.dispatch(createFilter({ id: '540092_540092-FF', data: filtersData }));
+    expect(store.getActions()).toEqual(expect.arrayContaining(expectedActions));
+  });
+});
