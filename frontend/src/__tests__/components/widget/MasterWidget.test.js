35462e2f8ffcd3efdb16c73f5045d13643dc2ae7
siemiatj <kuba@saskla.cz>
2020-10-12 16:00:10 +0200

    #7126 more tests

--- frontend/src/__tests__/components/widget/MasterWidget.test.js
+++ frontend/src/__tests__/components/widget/MasterWidget.test.js
@@ -84,4 +84,65 @@ describe('MasterWidget component', () => {
     expect(wrapper.state('updated')).toBe(false);
     expect(wrapper.state('value')).toBe(newValue);
   });
+
+  it('does not call `updatePropertyValue` on patch', () => {
+    const localFixtures = rawWidgetFixtures.text;
+    const props = createDummyProps(
+      {
+        ...localFixtures.data1,
+        ...localFixtures.props1,
+        onChange: jest.fn(),
+      },
+    );
+    const wrapper = shallow(<MasterWidget {...props} />);
+    const property = localFixtures.props1.fieldName;
+
+    wrapper.instance().handlePatch(property, 'foo');
+
+    expect(props.patch).toBeCalled();
+    expect(props.onChange).toBeCalled();
+    expect(props.updatePropertyValue).not.toBeCalled();
+  });
+
+  it('does not call `updatePropertyValue` on patch #2', () => {
+    const localFixtures = rawWidgetFixtures.text;
+    const widgetData = [{ ...fixtures.text.data1, widgetType: 'Quantity' }];
+    const props = createDummyProps(
+      {
+        ...localFixtures.data1,
+        ...localFixtures.props1,
+        widgetType: 'Quantity',
+        widgetData,
+        dataId: '123',
+        updatePropertyValue: jest.fn(),
+      },
+    );
+    const wrapper = shallow(<MasterWidget {...props} />);
+    const property = localFixtures.props1.fieldName;
+
+    wrapper.instance().handlePatch(property, 42);
+
+    expect(props.updatePropertyValue).not.toBeCalled();
+  });
+
+  it('calls `updatePropertyValue` on patch', () => {
+    const localFixtures = rawWidgetFixtures.text;
+    const widgetData = [{ ...fixtures.text.data1, widgetType: 'Quantity' }];
+    const props = createDummyProps(
+      {
+        ...localFixtures.data1,
+        ...localFixtures.props1,
+        widgetType: 'Quantity',
+        widgetData,
+        dataId: null,
+        updatePropertyValue: jest.fn(),
+      },
+    );
+    const wrapper = shallow(<MasterWidget {...props} />);
+    const property = localFixtures.props1.fieldName;
+
+    wrapper.instance().handlePatch(property, 42);
+
+    expect(props.updatePropertyValue).toBeCalled();
+  });
 });
