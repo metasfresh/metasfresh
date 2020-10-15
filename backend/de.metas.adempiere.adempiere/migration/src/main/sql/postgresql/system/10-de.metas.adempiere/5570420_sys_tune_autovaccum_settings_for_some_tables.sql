24ffb2c385cee4b184b08261957ccc5c3ac9d95e
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-10-15 06:38:20 +0200

    ALTER System doesn't work like this; only do the other changes for now

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5570420_sys_tune_autovaccum_settings_for_some_tables.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5570420_sys_tune_autovaccum_settings_for_some_tables.sql
@@ -87,3 +87,14 @@ ALTER TABLE c_flatrate_term SET (autovacuum_vacuum_threshold = 10000);
 
 ALTER TABLE c_invoice_candidate SET (autovacuum_vacuum_scale_factor = 0);
 ALTER TABLE c_invoice_candidate SET (autovacuum_vacuum_threshold = 10000); 
+
+
+--
+-- we can't run this via migration script: we get "ERROR:  ALTER SYSTEM cannot run inside a transaction block"
+--
+-- Thx to https://medium.com/contactually-engineering/postgres-at-scale-query-performance-and-autovacuuming-for-large-tables-d7e8ad40b16b
+-- this ends up in postgresql.auto.conf. 
+-- note "postgresql.auto.conf override those in postgresql.conf." (from https://www.postgresql.org/docs/9.5/config-setting.html)
+-- but whenever we manually edited and postgresql.conf, we set these to settings to 0.05 there as well
+--ALTER SYSTEM SET autovacuum_vacuum_scale_factor = 0.05;
+--ALTER SYSTEM SET autovacuum_analyze_scale_factor = 0.05;
