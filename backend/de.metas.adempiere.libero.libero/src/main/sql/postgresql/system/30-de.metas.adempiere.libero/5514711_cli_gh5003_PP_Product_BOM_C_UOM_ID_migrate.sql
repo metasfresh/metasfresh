5ed8b017e447ab09a224df2de8d28e613d11469c
Teo Sarca <teo.sarca@metasfresh.com>
2020-10-21 20:41:38 +0300

    sync with master: rename project to de.metas.manufacturing

--- backend/de.metas.adempiere.libero.libero/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5514711_cli_gh5003_PP_Product_BOM_C_UOM_ID_migrate.sql
+++
@@ -1,7 +0,0 @@
-create table backup.PP_Product_BOM_BKP20190304_gh5003 as select * from PP_Product_BOM;
-
-update PP_Product_BOM bom set C_UOM_ID=p.C_UOM_ID
-FROM M_Product p
-where bom.M_Product_ID=p.M_Product_ID
-and bom.C_UOM_ID is null;
-
