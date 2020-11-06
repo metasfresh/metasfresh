773768ea303e9e3b3e994ef88e870d5249cb37cd
Kuba Siemiątkowski <kuba@saskla.cz>
2020-11-06 13:31:54 +0100

    Fix failing e2e tests and some refactoring (#10189)
    * Fix failing create disposal from handling unit editor tests
    
    * correct issue pointer
    
    * Update cypress to 5.5.0
    
    * Adapt to have the row selected in picking
    
    * Fix picking tests - adapt selection
    
    * Add comment
    
    * - cleanup and refactor filters
    
    * - remove filtlers when fetching document
    
    * - code comments
    
    * #10177 don't refetch document list data after filtering
    - refactor DocumentList to get rid of the local active filters
    - use selector for filters
    
    * #10177 we don't need to click the rows
    
    * #10177 fix failing unit tests
    
    * Add wait for spinner
    
    * Execute quickaction after spinner is gone
    
    * #10177 run all unit tests
    
    * Pick second hu wait for spinner
    
    * Revert last change
    
    Co-authored-by: Petrica Nanca <petrica.nanca@metasfresh.com>

--- frontend/src/utils/documentListHelper.js
+++ frontend/src/utils/documentListHelper.js
@@ -8,7 +8,7 @@ import { toInteger } from 'lodash';
 import { getItemsByProperty, nullToEmptyStrings } from './index';
 import { viewState, getView } from '../reducers/viewHandler';
 import { getTable, getTableId, getSelection } from '../reducers/tables';
-import { getEntityRelatedId } from '../reducers/filters';
+import { getEntityRelatedId, getCachedFilter } from '../reducers/filters';
 import { TIME_REGEX_TEST } from '../constants/Constants';
 import { getCurrentActiveLocale } from './locale';
 
@@ -124,6 +124,8 @@ const DLmapStateToProps = (state, props) => {
     });
   }
   const filterId = getEntityRelatedId({ windowId, viewId });
+  const filters = getCachedFilter(state, filterId);
+
   return {
     page,
     sort,
@@ -141,7 +143,7 @@ const DLmapStateToProps = (state, props) => {
     parentSelected: parentSelector(state, parentTableId),
     modal: state.windowHandler.modal,
     rawModalVisible: state.windowHandler.rawModal.visible,
-    filters: windowId && viewId && state.filters ? state.filters[filterId] : {},
+    filters: filters ? filters : {},
     filterId,
   };
 };
