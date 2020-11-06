9f894c84062bcd8d91229396a2370b398897ae33
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-11-06 14:09:30 +0200

    Revert last change

--- e2e/cypress/integration/picking/pick_HUs_and_create_shipment.js
+++ e2e/cypress/integration/picking/pick_HUs_and_create_shipment.js
@@ -104,11 +104,8 @@ describe('Pick the SO', function() {
     cy.selectRightTable().within(() => {
       cy.selectItemUsingBarcodeFilter({ column: huSelectionHuCodeColumn, value: huValue2 }, false, true);
     });
-    cy.get('.spinner', { timeout: 10000 })
-      .should('not.exist')
-      .then(() => {
-        cy.executeQuickAction('WEBUI_Picking_HUEditor_PickHU', true, false);
-      });
+
+    cy.executeQuickAction('WEBUI_Picking_HUEditor_PickHU', true, false);
   });
 
   it('Confirm Picks', function() {
