107101b150986c1adf631a79df609d8882c579e1
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-11-18 10:18:59 +0200

    Basic options listing

--- frontend/src/components/app/PrintingOptions.js
+++ frontend/src/components/app/PrintingOptions.js
@@ -1,31 +1,30 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 class PrintingOptions extends PureComponent {
   render() {
+    const { options } = this.props.printingOptions;
+    console.log(options);
     return (
       <div className="panel-modal-content js-panel-modal-content container-fluid">
         <div className="window-wrapper">
           <div className="document-file-dropzone">
             <div className="sections-wrapper">
               <div className="panel panel-spaced panel-distance panel-bordered panel-primary">
-                <div>&nbsp;</div>
+                {options.map((printOptItem) => (
+                  <div key={printOptItem.caption}>
+                    <div>&nbsp;</div>
 
-                <div className="row">
-                  <div className="col-lg-6 col-md-6">
-                    <label className="checkbox-inline">
-                      <input type="checkbox" value="option1" />
-                      Print logo
-                    </label>
+                    <div className="row">
+                      <div className="col-lg-6 col-md-6">
+                        <label className="checkbox-inline">
+                          <input type="checkbox" value="option1" />
+                          {printOptItem.caption}
+                        </label>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div>&nbsp;</div>
-                <div className="row">
-                  <div className="col-lg-6 col-md-6">
-                    <label className="checkbox-inline">
-                      <input type="checkbox" value="option2" />
-                      Print totals
-                    </label>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
@@ -35,4 +34,14 @@ class PrintingOptions extends PureComponent {
   }
 }
 
-export default PrintingOptions;
+PrintingOptions.propTypes = {
+  printingOptions: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    printingOptions: state.windowHandler.printingOptions,
+  };
+};
+
+export default connect(mapStateToProps)(PrintingOptions);
