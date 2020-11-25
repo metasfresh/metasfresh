9b864ecff8de9888cf85125a2bf86de4559b602f
TheBestPessimist <cristian@tbp.land>
2020-11-25 15:06:56 +0200

    Add PP_ComponentGenerator and its Param
    https://github.com/metasfresh/metasfresh/issues/10305
    Signed-off-by: TheBestPessimist <cristian@tbp.land>

---
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_PP_ComponentGenerator.java
@@ -0,0 +1,65 @@
+// Generated Model - DO NOT CHANGE
+package org.compiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+import javax.annotation.Nullable;
+
+/** Generated Model for PP_ComponentGenerator
+ *  @author metasfresh (generated) 
+ */
+public class X_PP_ComponentGenerator extends org.compiere.model.PO implements I_PP_ComponentGenerator, org.compiere.model.I_Persistent 
+{
+
+	private static final long serialVersionUID = -578508515L;
+
+    /** Standard Constructor */
+    public X_PP_ComponentGenerator (final Properties ctx, final int PP_ComponentGenerator_ID, @Nullable final String trxName)
+    {
+      super (ctx, PP_ComponentGenerator_ID, trxName);
+    }
+
+    /** Load Constructor */
+    public X_PP_ComponentGenerator (final Properties ctx, final ResultSet rs, @Nullable final String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+
+	/** Load Meta Data */
+	@Override
+	protected org.compiere.model.POInfo initPO(final Properties ctx)
+	{
+		return org.compiere.model.POInfo.getPOInfo(Table_Name);
+	}
+
+	@Override
+	public void setM_Product_ID (final int M_Product_ID)
+	{
+		if (M_Product_ID < 1) 
+			set_Value (COLUMNNAME_M_Product_ID, null);
+		else 
+			set_Value (COLUMNNAME_M_Product_ID, M_Product_ID);
+	}
+
+	@Override
+	public int getM_Product_ID() 
+	{
+		return get_ValueAsInt(COLUMNNAME_M_Product_ID);
+	}
+
+	@Override
+	public void setPP_ComponentGenerator_ID (final int PP_ComponentGenerator_ID)
+	{
+		if (PP_ComponentGenerator_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_PP_ComponentGenerator_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_PP_ComponentGenerator_ID, PP_ComponentGenerator_ID);
+	}
+
+	@Override
+	public int getPP_ComponentGenerator_ID() 
+	{
+		return get_ValueAsInt(COLUMNNAME_PP_ComponentGenerator_ID);
+	}
+}
