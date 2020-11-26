f02e81529e8e5d15b6e82db7de52120589be9cde
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-11-26 08:33:51 +0200

    fetchTab in the wrapper

--- frontend/src/components/widget/InlineTabWrapper.js
+++ frontend/src/components/widget/InlineTabWrapper.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import InlineTab from './InlineTab';
 import PropTypes from 'prop-types';
+import { fetchTab } from '../../actions/WindowActions';
+import { connect } from 'react-redux';
 
 class InlineTabWrapper extends Component {
   constructor(props) {
     super(props);
+    const query = '';
+    const {
+      inlineTab: { windowId, tabId },
+      dataId: docId,
+      fetchTab,
+    } = props;
+    fetchTab({ tabId, windowId, docId, query });
   }
 
   render() {
@@ -20,6 +29,14 @@ class InlineTabWrapper extends Component {
 
 InlineTabWrapper.propTypes = {
   caption: PropTypes.string.isRequired,
+  inlineTab: PropTypes.object.isRequired,
+  dataId: PropTypes.string.isRequired,
+  fetchTab: PropTypes.func.isRequired,
 };
 
-export default InlineTabWrapper;
+export default connect(
+  null,
+  {
+    fetchTab,
+  }
+)(InlineTabWrapper);
