4350c9dee7b603d8c49b714ad2d057a86bdaef15
dragospodariu96 <58212697+dragospodariu96@users.noreply.github.com>
2020-12-02 12:36:47 +0200

    #10323  - Creating marketing contact only if sys config is activated when setting user's isnewsletter flag.

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5573720_sys_gh10323AddSysConfigForNewsletterMarketingContactSync.sql
@@ -0,0 +1,4 @@
+-- 2020-12-02T09:53:51.937Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541347,'O',TO_TIMESTAMP('2020-12-02 11:53:51','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.marketing.base','Y','de.metas.marketing.SyncUserNewsletterFlagToMarketingContact',TO_TIMESTAMP('2020-12-02 11:53:51','YYYY-MM-DD HH24:MI:SS'),100,'Y')
+;
