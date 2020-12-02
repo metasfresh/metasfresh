de787a425d414051e16c4b658bb6c52633cccf92
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2020-12-02 12:55:08 +0100

    Update backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5573720_sys_gh10323AddSysConfigForNewsletterMarketingContactSync.sql

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5573720_sys_gh10323AddSysConfigForNewsletterMarketingContactSync.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5573720_sys_gh10323AddSysConfigForNewsletterMarketingContactSync.sql
@@ -5,5 +5,5 @@ INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLe
 
 -- 2020-12-02T11:19:33.803Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_SysConfig SET Description='This sys config specifies whether a use should be added or removed from a marketing campaign when setting the ''isNewsletter'' flag to Y or N.',Updated=TO_TIMESTAMP('2020-12-02 13:19:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541347
+UPDATE AD_SysConfig SET Description='This sys config specifies whether a user should be added to or removed from the default "newsletter" marketing campaign when setting the user''s ''IsNewsletter'' flag to Y or N.',Updated=TO_TIMESTAMP('2020-12-02 13:19:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541347
 ;
