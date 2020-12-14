eff5c36aee24e97ca922b3b520285a8ae22ff3da
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-12-14 08:33:57 +0200

    Prereq InlineTabWrapper tests

---
+++ frontend/src/__tests__/components/widget/InlineTabWrapper.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import merge from 'merge';
+import viewHandler from '../../../reducers/viewHandler';
+import InlineTabWrapper from '../../../components/widget/InlineTabWrapper';
+import hotkeys from '../../../../test_setup/fixtures/hotkeys.json';
+import keymap from '../../../../test_setup/fixtures/keymap.json';
+import { ShortcutProvider } from '../../../components/keyshortcuts/ShortcutProvider';
+import { initialState as appHandlerState } from '../../../reducers/appHandler';
+import { initialState as windowHandlerState } from '../../../reducers/windowHandler';
+import tablesHandler from '../../../reducers/tables';
+import { Provider } from 'react-redux';
+import props from '../../../../test_setup/fixtures/widget/inline_tab_wrapper.json';
+
+const mockStore = configureStore([]);
+
+const createStore = function(state = {}) {
+  const res = merge.recursive(
+    true,
+    {
+      appHandler: {
+        ...appHandlerState,
+        me: { timeZone: 'America/Los_Angeles' },
+      },
+      windowHandler: { ...windowHandlerState },
+      ...viewHandler,
+      tables: { ...tablesHandler(undefined, {}) },
+    },
+    state
+  );
+
+  return res;
+};
+
+const initialState = createStore({
+  windowHandler: {
+    allowShortcut: true,
+    modal: {
+      visible: false,
+    },
+  },
+});
+const store = mockStore(initialState);
+
+describe('InlineTabWrapper component', () => {
+  describe('rendering tests:', () => {
+    it('renders without errors', () => {
+      shallow(<InlineTabWrapper {...props} />);
+    });
+    // it('renders without errors', () => {
+    //   mount(
+    //     <ShortcutProvider hotkeys={hotkeys} keymap={keymap}>
+    //       <Provider store={store}>
+    //         <InlineTabWrapper {...props} />
+    //       </Provider>
+    //     </ShortcutProvider>
+    //   );
+    // });
+  });
+});
