7d2b6ec7bcdc9371079fca54c809b7da494e6274
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2020-12-15 12:16:50 +0200

    (#10307) Packings Instructions available in sales order batch entry although not in pricelist Increase Ad_Val_RULE.code field size Change Validation rule for Packing Instruction

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5574860_sys_gh6429_ChangeAD_VAL_RULE_codeFieldSize.sql
@@ -0,0 +1,10 @@
+
+-- 2020-12-15T10:15:18.818Z
+-- URL zum Konzept
+UPDATE AD_Column SET FieldLength=6000,Updated=TO_TIMESTAMP('2020-12-15 12:15:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=193
+;
+
+-- 2020-12-15T10:16:00.663Z
+-- URL zum Konzept
+INSERT INTO t_alter_column values('ad_val_rule','Code','TEXT',null,null)
+;
