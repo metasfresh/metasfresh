08c4f40ab9c48cbf100d71460933dbb9490aa4b0
TheBestPessimist <cristian@tbp.land>
2020-12-15 12:43:19 +0200

    Update LineNetAmt whenever price is changed

--- backend/de.metas.business/src/main/java/de/metas/order/OrderLinePriceUpdateRequest.java
+++ backend/de.metas.business/src/main/java/de/metas/order/OrderLinePriceUpdateRequest.java
@@ -46,6 +46,8 @@ public class OrderLinePriceUpdateRequest
 	public static OrderLinePriceUpdateRequestBuilder prepare(final org.compiere.model.I_C_OrderLine orderLine)
 	{
 		return builder()
+				.updatePriceEnteredAndDiscountOnlyIfNotAlreadySet(true)
+				.updateLineNetAmt(true)
 				.orderLine(InterfaceWrapperHelper.create(orderLine, I_C_OrderLine.class))
 				.resultUOM(ResultUOM.PRICE_UOM_IF_ORDERLINE_IS_NEW);
 	}
