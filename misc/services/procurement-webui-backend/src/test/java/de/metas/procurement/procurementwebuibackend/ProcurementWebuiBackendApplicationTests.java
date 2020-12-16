8f12c1ae055e5020cce5f8289ec82ece76f4e65d
TheBestPessimist <cristian@tbp.land>
2020-12-16 16:02:15 +0200

    Add rest of spring project

---
+++ misc/services/procurement-webui-backend/src/test/java/de/metas/procurement/procurementwebuibackend/ProcurementWebuiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.metas.procurement.procurementwebuibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementWebuiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
