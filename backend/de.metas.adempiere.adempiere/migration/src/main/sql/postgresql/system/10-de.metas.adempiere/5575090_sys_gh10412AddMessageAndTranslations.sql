199ebb4fbace1e1a298a0181f5e72e2a0d1f5f6e
arthurlmf <arthurlucio@gmail.com>
2020-12-16 15:20:46 -0300

    Adding messagens, translations, and sysconfig

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5575090_sys_gh10412AddMessageAndTranslations.sql
@@ -0,0 +1,35 @@
+-- 2020-12-16T13:17:59.005Z
+-- URL zum Konzept
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545019,0,TO_TIMESTAMP('2020-12-16 10:17:56','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','Cannot add product due haddex check','E',TO_TIMESTAMP('2020-12-16 10:17:56','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.order.producthadexerror1')
+;
+
+-- 2020-12-16T13:17:59.234Z
+-- URL zum Konzept
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545019 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2020-12-16T13:19:25.696Z
+-- URL zum Konzept
+UPDATE AD_Message SET Value='de.metas.order.producthadexerror',Updated=TO_TIMESTAMP('2020-12-16 10:19:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545019
+;
+
+-- 2020-12-16T13:21:30.016Z
+-- URL zum Konzept
+UPDATE AD_Message_Trl SET MsgText='Die Haddex-Prüfung für das Produkt muss erneuert werden.',Updated=TO_TIMESTAMP('2020-12-16 10:21:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545019
+;
+
+-- 2020-12-16T13:21:51.881Z
+-- URL zum Konzept
+UPDATE AD_Message_Trl SET MsgText='Die Haddex-Prüfung für das Produkt muss erneuert werden.',Updated=TO_TIMESTAMP('2020-12-16 10:21:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545019
+;
+
+-- 2020-12-16T13:22:30.034Z
+-- URL zum Konzept
+UPDATE AD_Message SET MsgText='The product''s haddex check has to be renewed',Updated=TO_TIMESTAMP('2020-12-16 10:22:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545019
+;
+
+-- 2020-12-16T13:27:36.733Z
+-- URL zum Konzept
+UPDATE AD_Message SET Value='de.metas.order.model.interceptor.producthadexerror',Updated=TO_TIMESTAMP('2020-12-16 10:27:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545019
+;
+
---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5575110_sys_gh10412_AddSysconfig.sql
@@ -0,0 +1,5 @@
+-- 2020-12-16T13:50:13.970Z
+-- URL zum Konzept
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541351,'O',TO_TIMESTAMP('2020-12-16 10:50:11','YYYY-MM-DD HH24:MI:SS'),100,'If the current date - the value from the haddex date column of a product is bigger than the value from this sys config so that product can''t be added to a sales orderline. This value must be in months.','D','Y','MAX_HADDEX_AGE_IN_MONTHS',TO_TIMESTAMP('2020-12-16 10:50:11','YYYY-MM-DD HH24:MI:SS'),100,'24')
+;
+
