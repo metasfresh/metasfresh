ff7714206ca2cd6a8e360cfdbfca618314705d0a
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-12-17 10:56:37 +0200

    More post review fixes

--- frontend/src/reducers/windowHandler.js
+++ frontend/src/reducers/windowHandler.js
@@ -848,7 +848,7 @@ export default function windowHandler(state = initialState, action) {
 
       const targetTabData = { ...state.inlineTab[`${inlineTabId}`].data };
 
-      Object.keys(fieldsByName).map((fieldItem) => {
+      Object.keys(fieldsByName).forEach((fieldItem) => {
         targetTabData.fieldsByName[fieldItem] = {
           ...targetTabData.fieldsByName[fieldItem],
           ...fieldsByName[fieldItem],
