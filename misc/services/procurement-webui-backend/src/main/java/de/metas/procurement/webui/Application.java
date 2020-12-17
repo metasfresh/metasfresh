73ea7de849e1df06056d3543a8891115b032cc77
TheBestPessimist <cristian@tbp.land>
2020-12-17 11:40:36 +0200

    Midwork

--- misc/services/procurement-webui-backend/src/main/java/de/metas/procurement/webui/Application.java
+++ misc/services/procurement-webui-backend/src/main/java/de/metas/procurement/webui/Application.java
@@ -1,28 +1,15 @@
 package de.metas.procurement.webui;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.vaadin.spring.i18n.MessageProvider;
-import org.vaadin.spring.i18n.ResourceBundleMessageProvider;
-
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.spring.boot.internal.VaadinServletConfiguration;
 
 /*
  * #%L
@@ -49,8 +36,6 @@ import com.vaadin.spring.boot.internal.VaadinServletConfiguration;
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableJpaRepositories
-// Make config here (vaadinServlet) override stuff in VaadinServletConfiguration
-@Import(VaadinServletConfiguration.class)
 public class Application
 {
 	public static final void main(final String[] args)
@@ -80,55 +65,9 @@ public class Application
 		getContext().getAutowireCapableBeanFactory().autowireBean(bean);
 	}
 
-	@Bean
-	public VaadinServlet vaadinServlet()
-	{
-		return new SpringAwareTouchKitServlet();
-	}
-
 	/**
-	 * Configure embedded tomcat so that is use gzip for various resources.
+	 * @return default task executor used by {@link Async} calls
 	 */
-	@Bean
-	public EmbeddedServletContainerCustomizer servletContainerCustomizer()
-	{
-		return new EmbeddedServletContainerCustomizer()
-		{
-			@Override
-			public void customize(final ConfigurableEmbeddedServletContainer servletContainer)
-			{
-				final TomcatEmbeddedServletContainerFactory tomcatContainerFactory = (TomcatEmbeddedServletContainerFactory)servletContainer;
-				tomcatContainerFactory.addConnectorCustomizers(new TomcatConnectorCustomizer()
-				{
-					@Override
-					public void customize(final Connector connector)
-					{
-						final AbstractHttp11Protocol<?> httpProtocol = (AbstractHttp11Protocol<?>)connector.getProtocolHandler();
-						httpProtocol.setCompression("on");
-						httpProtocol.setCompressionMinSize(256);
-						final StringBuffer mimeTypes = new StringBuffer(httpProtocol.getCompressableMimeType());
-						if(!mimeTypes.toString().contains("MediaType.APPLICATION_JSON_VALUE"))
-						{
-							mimeTypes.append(","+MediaType.APPLICATION_JSON_VALUE);
-						}
-						if(!mimeTypes.toString().contains("application/javascript"))
-						{
-							mimeTypes.append(",application/javascript");
-						}
-						httpProtocol.setCompressableMimeType(mimeTypes.toString());
-					}
-				});
-			}
-		};
-	}
-
-	@Bean
-	MessageProvider messageProvider()
-	{
-		return new ResourceBundleMessageProvider("messages");
-	}
-
-	/** @return default task executor used by {@link Async} calls */
 	@Bean
 	public TaskExecutor taskExecutor()
 	{
