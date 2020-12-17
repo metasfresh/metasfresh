6538b33e697784f88b8fba21c9d1a705ee3c9d68
arthurlmf <arthurlucio@gmail.com>
2020-12-17 11:26:36 -0300

    Solving duplicated sysconfig and message

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5574550_sys_gh10412HaddexColumnsToProductTableAndSysConfigsAndErrMsgAndProductWindowAdjustments.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5574550_sys_gh10412HaddexColumnsToProductTableAndSysConfigsAndErrMsgAndProductWindowAdjustments.sql
@@ -144,11 +144,6 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 /* DDL */ SELECT public.db_alter_table('M_Product','ALTER TABLE public.M_Product ADD COLUMN HaddexControlNr VARCHAR(100)')
 ;
 
--- 2020-12-16T13:50:13.970Z
--- URL zum Konzept
-INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541351,'O',TO_TIMESTAMP('2020-12-16 10:50:11','YYYY-MM-DD HH24:MI:SS'),100,'If the current date - the value from the haddex date column of a product is bigger than the value from this sys config so that product can''t be added to a sales orderline. This value must be in months.','D','Y','MAX_HADDEX_AGE_IN_MONTHS',TO_TIMESTAMP('2020-12-16 10:50:11','YYYY-MM-DD HH24:MI:SS'),100,'24')
-;
-
 -- 2020-12-10T12:50:10.299Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,572310,627737,0,180,TO_TIMESTAMP('2020-12-10 14:50:09','YYYY-MM-DD HH24:MI:SS'),100,1,'D','Y','N','N','N','N','N','N','N','Haddex Prüfung erforderlich',TO_TIMESTAMP('2020-12-10 14:50:09','YYYY-MM-DD HH24:MI:SS'),100)
@@ -257,25 +252,5 @@ UPDATE AD_Field SET DisplayLogic='@HaddexCheck@=''Y''',Updated=TO_TIMESTAMP('202
 -- 2020-12-10T13:57:38.967Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Field SET DisplayLogic='@HaddexCheck@=''Y''',Updated=TO_TIMESTAMP('2020-12-10 15:57:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=627739
-;
-
--- 2020-12-10T14:44:09.142Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545017,0,TO_TIMESTAMP('2020-12-10 16:44:08','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Die Haddex-Prüfung für das Produkt muss erneuert werden.','I',TO_TIMESTAMP('2020-12-10 16:44:08','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.base.producthadexerror')
-;
-
--- 2020-12-10T14:44:09.145Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545017 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
-;
-
--- 2020-12-10T14:44:21.455Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='The product''s haddex check has to be renewed',Updated=TO_TIMESTAMP('2020-12-10 16:44:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545017
-;
-
--- 2020-12-10T14:44:27.835Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Message_Trl SET MsgText='The product''s haddex check has to be renewed.',Updated=TO_TIMESTAMP('2020-12-10 16:44:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545017
 ;
 
