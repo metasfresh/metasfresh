d213c0ef7b9a7b7239e5976ca15a1959852e8d29
Teo Sarca <teo.sarca@metasfresh.com>
2021-01-04 16:21:37 +0200

    LoginRestController prototype

---
+++ misc/services/procurement-webui-backend/src/main/java/de/metas/procurement/webui/rest/LoginRestController.java
@@ -0,0 +1,47 @@
+/*
+ * #%L
+ * procurement-webui-backend
+ * %%
+ * Copyright (C) 2021 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.procurement.webui.rest;
+
+import de.metas.procurement.webui.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = LoginRestController.ENDPOINT)
+public class LoginRestController
+{
+	public static final String ENDPOINT = Constants.ENDPOINT_ROOT + "/login";
+
+	private final static Logger logger = LoggerFactory.getLogger(LoginRestController.class);
+
+	@PostMapping("/authenticate")
+	public JsonLoginResponse authenticate(@RequestBody final JsonLoginRequest request)
+	{
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+}
