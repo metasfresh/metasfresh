acfab53645b064796d7942467b1a5f8121e749ac
Teo Sarca <teo.sarca@metasfresh.com>
2021-01-06 13:45:29 +0200

    prototyping

--- misc/services/procurement-webui/procurement-webui-backend/src/main/java/de/metas/procurement/webui/service/impl/ProductSuppliesService.java
+++ misc/services/procurement-webui/procurement-webui-backend/src/main/java/de/metas/procurement/webui/service/impl/ProductSuppliesService.java
@@ -28,6 +28,7 @@ import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
