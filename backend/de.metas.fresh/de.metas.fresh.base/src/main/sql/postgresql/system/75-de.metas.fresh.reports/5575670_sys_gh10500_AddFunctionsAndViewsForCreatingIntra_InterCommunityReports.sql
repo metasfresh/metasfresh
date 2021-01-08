f3e60ce73fdc04c5a07ab59f5b65f1d945112e53
Cristina <cristinamghita@gmail.com>
2021-01-08 17:07:58 +0200

    #10500 Add function isEUCountry

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5575670_sys_gh10500_AddFunctionsAndViewsForCreatingIntra_InterCommunityReports.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5575670_sys_gh10500_AddFunctionsAndViewsForCreatingIntra_InterCommunityReports.sql
@@ -1,3 +1,25 @@
+CREATE OR REPLACE FUNCTION isEUCountry(IN p_C_Country_ID numeric)
+    RETURNS char(1) AS
+$BODY$
+DECLARE
+
+    isEUCountry char(1);
+
+BEGIN
+
+    isEUCountry := (select case when (count(1) > 0) then 'Y' else 'N' end
+                    from C_CountryArea_Assign ca
+                    where ca.C_CountryArea_ID = 540000 -- European Union
+                      and ca.C_Country_ID = p_C_Country_ID);
+
+    IF isEUCountry IS NOT NUll THEN
+        RETURN isEUCountry;
+    ELSE
+        RETURN 'N';
+    END IF;
+END;
+$BODY$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.isEUShippingFromInvoice(IN p_C_Invoice_ID numeric)
     RETURNS char(1) AS
 $BODY$
