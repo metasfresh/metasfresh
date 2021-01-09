80c3da530eb8d218c36f6d8a1dfdb1c643b8dd5d
Teo Sarca <teo.sarca@metasfresh.com>
2021-01-09 11:54:36 +0200

    RepairCustomerReturnsService: create VHU when adding spare parts

--- backend/metasfresh-webui-api/src/main/java/de/metas/servicerepair/customerreturns/RepairCustomerReturnsService.java
+++ backend/metasfresh-webui-api/src/main/java/de/metas/servicerepair/customerreturns/RepairCustomerReturnsService.java
@@ -22,21 +22,30 @@
 
 package de.metas.servicerepair.customerreturns;
 
+import de.metas.common.util.time.SystemTime;
 import de.metas.document.DocBaseAndSubType;
 import de.metas.document.DocTypeId;
 import de.metas.document.IDocTypeDAO;
 import de.metas.handlingunits.HuId;
 import de.metas.handlingunits.IHandlingUnitsBL;
+import de.metas.handlingunits.allocation.IHUProducerAllocationDestination;
+import de.metas.handlingunits.allocation.impl.AllocationUtils;
+import de.metas.handlingunits.allocation.impl.GenericAllocationSourceDestination;
+import de.metas.handlingunits.allocation.impl.HULoader;
+import de.metas.handlingunits.allocation.impl.HUProducerDestination;
 import de.metas.handlingunits.inout.returns.ReturnsServiceFacade;
 import de.metas.handlingunits.inout.returns.customer.CreateCustomerReturnLineReq;
 import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_InOutLine;
+import de.metas.handlingunits.model.X_M_HU;
+import de.metas.handlingunits.storage.impl.PlainProductStorage;
 import de.metas.inout.InOutId;
 import de.metas.product.ProductId;
 import de.metas.quantity.Quantity;
 import de.metas.util.Services;
 import lombok.Builder;
 import lombok.NonNull;
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.I_M_InOut;
 import org.compiere.model.X_C_DocType;
 import org.springframework.stereotype.Service;
@@ -71,12 +80,46 @@ public class RepairCustomerReturnsService
 			@NonNull final ProductId productId,
 			@NonNull final Quantity qtyReturned)
 	{
-		returnsServiceFacade.createCustomerReturnLine(
+		if (qtyReturned.signum() <= 0)
+		{
+			throw new AdempiereException("@QtyReturned@ <= 0");
+		}
+
+		final I_M_InOutLine customerReturnLine = returnsServiceFacade.createCustomerReturnLine(
 				CreateCustomerReturnLineReq.builder()
 						.headerId(customerReturnId)
 						.productId(productId)
 						.qtyReturned(qtyReturned)
 						.build());
+
+		createAndAssignPlanningVHU(customerReturnLine, productId, qtyReturned);
+	}
+
+	private void createAndAssignPlanningVHU(
+			@NonNull final I_M_InOutLine customerReturnLine,
+			@NonNull final ProductId productId,
+			@NonNull final Quantity qtyReturned)
+	{
+		final IHUProducerAllocationDestination destination;
+
+		HULoader.builder()
+				.source(new GenericAllocationSourceDestination(
+						new PlainProductStorage(productId, qtyReturned),
+						customerReturnLine))
+				.destination(destination = HUProducerDestination.ofVirtualPI()
+						.setHUStatus(X_M_HU.HUSTATUS_Planning))
+				.load(AllocationUtils.createAllocationRequestBuilder()
+						.setHUContext(handlingUnitsBL.createMutableHUContextForProcessing())
+						.setProduct(productId)
+						.setQuantity(qtyReturned)
+						.setDate(SystemTime.asZonedDateTime())
+						.setFromReferencedModel(customerReturnLine)
+						.setForceQtyAllocation(true)
+						.create());
+
+		final I_M_HU vhu = destination.getSingleCreatedHU().orElseThrow(() -> new AdempiereException("No VHU created"));
+
+		returnsServiceFacade.assignHandlingUnitToHeaderAndLine(customerReturnLine, vhu);
 	}
 
 	@Builder(builderMethodName = "prepareCloneHUAndCreateCustomerReturnLine", builderClassName = "$CloneHUAndCreateCustomerReturnLineBuilder")
