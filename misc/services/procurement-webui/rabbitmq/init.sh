44ac2775a543cc8c1907254b5c94825284765958
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-01-11 15:03:01 +0100

    rabbitmq: copy default seddings to mounted volume

--- misc/services/procurement-webui/rabbitmq/init.sh
+++ misc/services/procurement-webui/rabbitmq/init.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-echo "init.sh - before rabbitmq start - RABBITMQ_PROCUREMENT_WEBUI_PASSWORD=$RABBITMQ_PROCUREMENT_WEBUI_PASSWORD"
-echo "init.sh - before rabbitmq start - RABBITMQ_METASFRESH_PASSWORD=$RABBITMQ_METASFRESH_PASSWORD"
+/etc/rabbitmq_default_configs
+
+echo "init.sh - before rabbitmq start - copy not-yet-existing config files"
+cp --no-clobber --recursive --verbose /etc/rabbitmq_default_configs/* /etc/rabbitmq/
+
+#echo "init.sh - before rabbitmq start - RABBITMQ_PROCUREMENT_WEBUI_PASSWORD=$RABBITMQ_PROCUREMENT_WEBUI_PASSWORD"
+#echo "init.sh - before rabbitmq start - RABBITMQ_METASFRESH_PASSWORD=$RABBITMQ_METASFRESH_PASSWORD"
 
 # thx to https://stackoverflow.com/a/30773882/1012103
 # Create Rabbitmq user when the server is up
 # IDK how to wait for rabbitmq to be up
 # rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbitmq --timeout 30 &&
 ( rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit@$(hostname).pid --timeout 30 && \
-echo "init.sh - RABBITMQ_PROCUREMENT_WEBUI_PASSWORD=$RABBITMQ_PROCUREMENT_WEBUI_PASSWORD" && \
-echo "init.sh - RABBITMQ_METASFRESH_PASSWORD=$RABBITMQ_METASFRESH_PASSWORD" && \
 echo "init.sh - Assuming that rabbitmq is up; setting passwords" && \
 rabbitmqctl change_password procurement_webui $RABBITMQ_PROCUREMENT_WEBUI_PASSWORD 2>/dev/null && \
 echo "init.sh - New password for user procurement_webui set from environment variable RABBITMQ_PROCUREMENT_WEBUI_PASSWORD" && \
