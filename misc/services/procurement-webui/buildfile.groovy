01f4bd2082e2d71d1e607660b584f6ea17da9a14
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-01-11 10:40:35 +0100

    add URL of the latest docker-compose-yml

--- misc/services/procurement-webui/buildfile.groovy
+++ misc/services/procurement-webui/buildfile.groovy
@@ -62,14 +62,18 @@ def build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild = f
     sh "sed -i 's|\${dockerImage.procurement_backend}|${backendDockerImage}|g' docker-compose/docker-compose.yml"
     sh "sed -i 's|\${dockerImage.procurement_frontend}|${frontendDockerImage}|g' docker-compose/docker-compose.yml"
 
+    String dockerComposeGroupId='de.metas.procurement'
+    String dockerComposeArtifactId='procurement-webui'
+    String dockerComposeClassifier='docker-compose'
     withMaven(jdk: 'java-14', maven: 'maven-3.6.3', mavenLocalRepo: '.repository', options: [artifactsPublisher(disabled: true)]) {
-        sh "mvn --settings ${mvnConf.settingsFile} ${mvnConf.resolveParams} -Dfile=docker-compose/docker-compose.yml -Durl=${mvnConf.deployRepoURL} -DrepositoryId=${mvnConf.MF_MAVEN_REPO_ID} -DgroupId=de.metas.procurement -DartifactId=procurement-webui -Dversion=${env.MF_VERSION} -Dclassifier=docker-compose -Dpackaging=yml -DgeneratePom=true org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file"
+        sh "mvn --settings ${mvnConf.settingsFile} ${mvnConf.resolveParams} -Dfile=docker-compose/docker-compose.yml -Durl=${mvnConf.deployRepoURL} -DrepositoryId=${mvnConf.MF_MAVEN_REPO_ID} -DgroupId=${dockerComposeGroupId} -DartifactId=${dockerComposeArtifactId} -Dversion=${env.MF_VERSION} -Dclassifier=${dockerComposeClassifier} -Dpackaging=yml -DgeneratePom=true org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file"
     }
 
     final Misc misc = new Misc()
     currentBuild.description = """${currentBuild.description}
 <p>
-  <a href="${mvnConf.deployRepoURL}/de/metas/procurement/procurement-webui/${misc.urlEncode(env.MF_VERSION)}/procurement-webui-${misc.urlEncode(env.MF_VERSION)}.yml">docker-compose.yml</a>
+  this build's <a href="${mvnConf.deployRepoURL}/de/metas/procurement/procurement-webui/${misc.urlEncode(env.MF_VERSION)}/procurement-webui-${misc.urlEncode(env.MF_VERSION)}.yml">docker-compose.yml</a>.<br>
+You can always get the latest docker-compose.yml at <code>${mvnResolveRepoBaseURL}/service/rest/v1/search/assets/download?sort=version&repository=${mvnRepoName}&maven.groupId=${dockerComposeGroupId}&maven.artifactId=${dockerComposeArtifactId}&maven.classifier=${dockerComposeClassifier}&maven.extension=yml</code>
 
 ${frontendBuildDescription}<p>
 ${backendBuildDescription}<p>
