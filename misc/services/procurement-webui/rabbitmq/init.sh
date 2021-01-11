183763f098caa18a439d366126177ef3390e2f9f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-01-11 15:50:42 +0100

    fix in nginx Dockerfile; minor improvements in the rest

--- misc/services/procurement-webui/rabbitmq/init.sh
+++ misc/services/procurement-webui/rabbitmq/init.sh
@@ -22,4 +22,4 @@ echo "init.sh - New password for user metasfresh set from environment variable R
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
 # it will be as you run in the container rabbitmq-server arg1 arg2
-rabbitmq-server $@
+rabbitmq-server "$@"
