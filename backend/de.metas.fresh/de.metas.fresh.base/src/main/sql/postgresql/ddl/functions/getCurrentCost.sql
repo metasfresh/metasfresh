bd802a7f7a4d474f0ace51e0a7ad71220ac8ee73
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-01-11 11:16:38 +0200

    getCurrentCost function: Formatting and indentation

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/getCurrentCost.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/getCurrentCost.sql
@@ -1,53 +1,62 @@
-DROP FUNCTION if exists getCurrentCost(numeric, date, numeric, numeric, numeric);
-CREATE OR REPLACE FUNCTION getCurrentCost(p_M_Product_ID numeric, p_Date date, p_AcctSchema_ID numeric,
+DROP FUNCTION IF EXISTS getCurrentCost(numeric,
+                                       date,
+                                       numeric,
+                                       numeric,
+                                       numeric)
+;
+
+CREATE OR REPLACE FUNCTION getCurrentCost(p_M_Product_ID     numeric,
+                                          p_Date             date,
+                                          p_AcctSchema_ID    numeric,
                                           p_M_CostElement_Id numeric,
-                                          p_AD_Client_ID numeric, p_AD_Org_ID numeric)
-    returns numeric
+                                          p_AD_Client_ID     numeric,
+                                          p_AD_Org_ID        numeric)
+    RETURNS numeric
 AS
 $BODY$
-WITH x as
+WITH x AS
          (
              SELECT m_costdetail_ID,
                     cd.prev_currentcostprice,
                     cd.M_Product_ID,
                     COALESCE(mi.dateAcct, mpo.dateAcct, pp.dateAcct, inv.movementDate, m.MovementDate, io.dateAcct,
-                             NULL) as dateAcct
+                             NULL) AS dateAcct
 
-             from m_costdetail cd
+             FROM m_costdetail cd
 
-                      JOIN AD_Org org on cd.ad_org_ID = org.ad_org_id
+                      JOIN AD_Org org ON cd.ad_org_ID = org.ad_org_id
 
 
-                      left join M_MatchInv mi ON mi.m_matchinv_id = cd.m_matchinv_id
-                      left JOIN M_MatchPO mpo on mpo.M_MatchPO_ID = cd.m_MatchPO_ID
-                      left JOIN PP_Cost_Collector pp ON pp.PP_Cost_Collector_ID = cd.pp_cost_collector_id
-                      left JOIN m_inventoryline il on il.m_inventoryline_id = cd.m_inventoryline_id
-                      LEFT JOIN M_Inventory inv on inv.m_inventory_id = il.m_inventory_id
-                      LEFT JOIN M_MovementLine ml on ml.m_movementline_id = cd.m_movementline_id
-                      LEFT JOIN M_Movement m on m.m_movement_id = ml.m_movement_ID
-                      LEFT JOIN m_inoutline iol on iol.m_inoutline_id = cd.m_inoutline_id
-                      LEFT JOIN M_InOut io on iol.M_InOut_ID = io.M_InOut_ID
+                      LEFT JOIN M_MatchInv mi ON mi.m_matchinv_id = cd.m_matchinv_id
+                      LEFT JOIN M_MatchPO mpo ON mpo.M_MatchPO_ID = cd.m_MatchPO_ID
+                      LEFT JOIN PP_Cost_Collector pp ON pp.PP_Cost_Collector_ID = cd.pp_cost_collector_id
+                      LEFT JOIN m_inventoryline il ON il.m_inventoryline_id = cd.m_inventoryline_id
+                      LEFT JOIN M_Inventory inv ON inv.m_inventory_id = il.m_inventory_id
+                      LEFT JOIN M_MovementLine ml ON ml.m_movementline_id = cd.m_movementline_id
+                      LEFT JOIN M_Movement m ON m.m_movement_id = ml.m_movement_ID
+                      LEFT JOIN m_inoutline iol ON iol.m_inoutline_id = cd.m_inoutline_id
+                      LEFT JOIN M_InOut io ON iol.M_InOut_ID = io.M_InOut_ID
 
              WHERE cd.ischangingcosts = 'Y'
-               and cd.c_acctschema_id = p_acctSchema_ID
-               and cd.m_costelement_id = p_M_CostElement_Id
-               and cd.M_Product_ID = p_M_Product_ID
-               and cd.ad_client_id = p_AD_Client_ID
-               and cd.ad_org_ID = p_AD_Org_ID
+               AND cd.c_acctschema_id = p_acctSchema_ID
+               AND cd.m_costelement_id = p_M_CostElement_Id
+               AND cd.M_Product_ID = p_M_Product_ID
+               AND cd.ad_client_id = p_AD_Client_ID
+               AND cd.ad_org_ID = p_AD_Org_ID
          )
 
 
-Select COALESCE((SELECT prev_currentcostprice from x where dateAcct > p_Date limit 1), cost.currentCostPrice)
-           as currentCostPrice
+SELECT COALESCE((SELECT prev_currentcostprice FROM x WHERE dateAcct > p_Date LIMIT 1), cost.currentCostPrice)
+           AS currentCostPrice
 
-from M_Cost cost
+FROM M_Cost cost
 
 
 WHERE cost.m_product_id = p_M_Product_ID
-  and cost.c_acctschema_id = p_acctSchema_ID
-  and cost.m_costelement_id = p_M_CostElement_Id
-  and cost.ad_client_id = p_AD_Client_ID
-  and cost.AD_Org_Id = p_AD_Org_ID;
+  AND cost.c_acctschema_id = p_acctSchema_ID
+  AND cost.m_costelement_id = p_M_CostElement_Id
+  AND cost.ad_client_id = p_AD_Client_ID
+  AND cost.AD_Org_Id = p_AD_Org_ID;
 
 
 $BODY$
