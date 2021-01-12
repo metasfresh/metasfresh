4de6a02de4ec25bf802746c67f41830637a442f2
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-12 15:57:41 +0200

    Add Product  component skel

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Product.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Product.tsx
@@ -1,9 +1,30 @@
 import React, { FunctionComponent, ReactElement } from 'react';
 
-const Product: FunctionComponent = (): ReactElement => {
+interface Props {
+  id: string;
+  name: string;
+  packType: string;
+  itemsNo: number;
+  isEdited: boolean;
+  editedItemsNo: number;
+}
+
+const Product: FunctionComponent<Props> = ({ name, itemsNo, packType }: Props): ReactElement => {
   return (
     <div className="product">
-      <div className="box">Product box</div>
+      <div className="box">
+        <div className="columns is-mobile">
+          <div className="column is-9">
+            <div className="columns">
+              <div className="column is-size-4-mobile no-p">{name}</div>
+              <div className="column is-size-7 no-p">{packType}</div>
+            </div>
+          </div>
+          <div className="column is-3 no-p">
+            <div className="is-size-2-mobile no-p">{itemsNo}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductItems.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductItems.tsx
@@ -4,9 +4,16 @@ import Product from './Product';
 const ProductItems: FunctionComponent = (): ReactElement => {
   return (
     <div className="custom-top-offset">
-      <Product />
-      <Product />
-      <Product />
+      <Product id={`prodId1`} name={`Batavia`} packType={`G2 x 6Stk`} itemsNo={0} isEdited={false} editedItemsNo={0} />
+      <Product id={`prodId2`} name={`Broccoli`} packType={`G2 x 6 Kg`} itemsNo={0} isEdited={false} editedItemsNo={0} />
+      <Product
+        id={`prodId3`}
+        name={`Lauch grun`}
+        packType={`G2 x 7 Kg`}
+        itemsNo={0}
+        isEdited={false}
+        editedItemsNo={0}
+      />
     </div>
   );
 };
