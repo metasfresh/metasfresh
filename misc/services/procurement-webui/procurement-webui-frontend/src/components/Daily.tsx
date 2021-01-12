ad52124ffc48f0279f24d54df6a2050b4e9f6575
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-12 12:02:01 +0200

    Fixed the Daily.tsx

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Daily.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Daily.tsx
@@ -1,18 +1,36 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { Component, ReactElement } from 'react';
+import { observer, inject } from 'mobx-react';
+
+import { fetchDailyReport } from '../api';
+
+import { RootInstance } from '../models/Store';
 import DailyNav from './DailyNav';
 import View from './View';
 
-const DailyView: FunctionComponent = (): ReactElement => {
-  return (
-    <View>
-      <div>
-        <h1 className="title p-4">Daily view</h1>
-        <DailyNav />
-        <br />
-        <p className="subtitle p-4">Some content</p>
-      </div>
-    </View>
-  );
-};
+interface Props {
+  store?: RootInstance;
+}
+@inject('store')
+@observer
+export default class DailyView extends Component<Props> {
+  componentDidMount(): void {
+    const { store } = this.props;
 
-export default DailyView;
+    fetchDailyReport('12-01-2020').then((resp) => {
+      store.navigation.setViewName(resp.data.dayCaption);
+    });
+  }
+
+  render(): ReactElement {
+    return (
+      <View>
+        <div>
+          <h1 className="title p-4">Daily view</h1>
+          <DailyNav />
+          <br />
+          <p className="subtitle p-4">Some content</p>
+        </div>
+      </View>
+    );
+  }
+}
