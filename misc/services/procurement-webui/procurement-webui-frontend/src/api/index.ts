06e8add7aabd8c82e82d185ec66bd04a62996974
siemiatj <kuba@saskla.cz>
2021-01-12 13:49:29 +0100

    - fix miragejs fake api

--- misc/services/procurement-webui/procurement-webui-frontend/src/api/index.ts
+++ misc/services/procurement-webui/procurement-webui-frontend/src/api/index.ts
@@ -1,80 +1,80 @@
 import axios from 'axios';
 
-const loadMirage = () => import('miragejs');
+import { createServer } from 'miragejs';
 
-export function loadMirageInDev() {
+export function loadMirageInDev(): void {
   if (process.env.NODE_ENV === 'development') {
-    loadMirage().then(({ Server }) => {
-      console.log('MIRAGE listening');
-      return new Server({
-        routes() {
-          this.namespace = '/rest/';
+    createServer({
+      routes() {
+        console.log('MIRAGE listening');
 
-          this.get('dailyReport/:date', (schema, request) => {
-            const date = request.params.date;
-            const products = [
-              {
-                confirmedByUser: true,
-                packingInfo: 'packing info 1',
-                productId: '1234',
-                productName: 'Broccoli',
-                qty: 2,
-              },
-              {
-                confirmedByUser: false,
-                packingInfo: 'packing info 2',
-                productId: '5678',
-                productName: 'Lauch grun',
-                qty: 4,
-              },
-              {
-                confirmedByUser: true,
-                packingInfo: 'packing info 3',
-                productId: '9101',
-                productName: 'Apfel',
-                qty: 33,
-              },
-              {
-                confirmedByUser: true,
-                packingInfo: 'packing info 4',
-                productId: '1213',
-                productName: 'Kartoffel',
-                qty: 99,
-              },
-              {
-                confirmedByUser: true,
-                packingInfo: 'packing info 5',
-                productId: '1415',
-                productName: 'Zweibel',
-                qty: 105,
-              },
-            ];
+        this.namespace = '/rest';
+        this.urlPrefix = 'http://localhost:3000';
 
-            const elements = Math.floor(Math.random() * Math.floor(products.length));
-            const pickedProducts = products.sort(() => 0.5 - Math.random()).slice(0, elements);
+        this.get('/dailyReport/:date', (schema, request) => {
+          const date = request.params.date;
+          const products = [
+            {
+              confirmedByUser: true,
+              packingInfo: 'packing info 1',
+              productId: '1234',
+              productName: 'Broccoli',
+              qty: 2,
+            },
+            {
+              confirmedByUser: false,
+              packingInfo: 'packing info 2',
+              productId: '5678',
+              productName: 'Lauch grun',
+              qty: 4,
+            },
+            {
+              confirmedByUser: true,
+              packingInfo: 'packing info 3',
+              productId: '9101',
+              productName: 'Apfel',
+              qty: 33,
+            },
+            {
+              confirmedByUser: true,
+              packingInfo: 'packing info 4',
+              productId: '1213',
+              productName: 'Kartoffel',
+              qty: 99,
+            },
+            {
+              confirmedByUser: true,
+              packingInfo: 'packing info 5',
+              productId: '1415',
+              productName: 'Zweibel',
+              qty: 105,
+            },
+          ];
 
-            return {
-              date,
-              dayCaption: 'Daycaption',
-              products: pickedProducts,
-            };
-          });
+          const elements = Math.floor(Math.random() * Math.floor(products.length));
+          const pickedProducts = products.sort(() => 0.5 - Math.random()).slice(0, elements);
 
-          this.get('session', () => {
-            return {
-              countUnconfirmed: 2,
-              date: '01-12-2021',
-              dayCaption: 'string',
-              email: 'string',
-              language: 'de_DE',
-              loggedIn: true,
-              loginError: 'string',
-              week: 'string',
-              weekCaption: 'string',
-            };
-          });
-        },
-      });
+          return {
+            date,
+            dayCaption: 'Daycaption',
+            products: pickedProducts,
+          };
+        });
+
+        this.get('/session', () => {
+          return {
+            countUnconfirmed: 2,
+            date: '01-12-2021',
+            dayCaption: 'string',
+            email: 'string',
+            language: 'de_DE',
+            loggedIn: true,
+            loginError: 'string',
+            week: 'string',
+            weekCaption: 'string',
+          };
+        });
+      },
     });
   }
 }
