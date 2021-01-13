dcd1b499fdcef2d5ee9ac9f1f2881914dd29e51b
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-13 14:33:26 +0200

    Rem Weekly h tag

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Weekly.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Weekly.tsx
@@ -6,7 +6,6 @@ const Weekly: FunctionComponent = (): ReactElement => {
   return (
     <View>
       <div>
-        <h1 className="title p-4">Weekly view</h1>
         <WeeklyNav />
         <div className="mt-1 p-4">
           <p className="subtitle">Some content</p>
