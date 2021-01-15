cea95ded58e1db9f278dc42d4d361068f9cea9d5
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-15 15:15:04 +0200

    Upgrade mobx-state-tree

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductList.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductList.tsx
@@ -1,21 +1,43 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import Product from './Product';
-
-const ProductList: FunctionComponent = (): ReactElement => {
-  return (
-    <div className="mt-4">
-      <Product id={`prodId1`} name={`Batavia`} packType={`G2 x 6Stk`} itemsNo={0} isEdited={false} editedItemsNo={0} />
-      <Product id={`prodId2`} name={`Broccoli`} packType={`G2 x 6 Kg`} itemsNo={0} isEdited={false} editedItemsNo={0} />
-      <Product
-        id={`prodId3`}
-        name={`Lauch grun`}
-        packType={`G2 x 7 Kg`}
-        itemsNo={0}
-        isEdited={false}
-        editedItemsNo={0}
-      />
-    </div>
-  );
-};
+import { observer, inject } from 'mobx-react';
+import { RootInstance } from '../models/Store';
+interface Props {
+  store?: RootInstance;
+}
+@inject('store')
+@observer
+class ProductList extends React.Component<Props> {
+  render(): ReactElement {
+    return (
+      <div className="mt-4">
+        <Product
+          id={`prodId1`}
+          name={`Batavia`}
+          packType={`G2 x 6Stk`}
+          itemsNo={0}
+          isEdited={false}
+          editedItemsNo={0}
+        />
+        <Product
+          id={`prodId2`}
+          name={`Broccoli`}
+          packType={`G2 x 6 Kg`}
+          itemsNo={0}
+          isEdited={false}
+          editedItemsNo={0}
+        />
+        <Product
+          id={`prodId3`}
+          name={`Lauch grun`}
+          packType={`G2 x 7 Kg`}
+          itemsNo={0}
+          isEdited={false}
+          editedItemsNo={0}
+        />
+      </div>
+    );
+  }
+}
 
 export default ProductList;
