cf61381f4f64701891e3a0f2a8d522347e073707
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-18 12:53:14 +0200

    Correct header caption

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Info.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Info.tsx
@@ -2,18 +2,19 @@ import React, { useEffect, useContext } from 'react';
 import { observer } from 'mobx-react';
 import View from './View';
 import { RootStoreContext } from '../models/Store';
+import { translate } from '../utils/translate';
 
 const Info: React.FC = observer(() => {
   const store = useContext(RootStoreContext);
 
   useEffect(() => {
     store.info.fetchInfo();
+    store.navigation.setViewName(translate('InfoMessageView.caption'));
   }, [store]);
 
   return (
     <View>
       <div>
-        <h1 className="title p-4">Info/News</h1>
         <div className="mt-1 p-4">
           <p className="subtitle preserve-new-lines" dangerouslySetInnerHTML={{ __html: store.info.getContent }}></p>
         </div>
