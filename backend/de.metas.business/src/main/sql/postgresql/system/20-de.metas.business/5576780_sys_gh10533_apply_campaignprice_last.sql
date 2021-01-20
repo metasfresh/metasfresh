0d8874332cbea93e1a413f4fd76319a3a988b33d
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-01-20 05:45:17 +0100

    rearange campain price; add some more docu
    https://github.com/metasfresh/metasfresh/issues/10533
    (cherry picked from commit 9785d7edeb9724286b652b91fa978ec7c017c859)

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5576780_sys_gh10533_apply_campaignprice_last.sql
+++
@@ -1,8 +0,0 @@
-
--- CampaignPricingRule
-UPDATE c_pricingrule
-SET seqno=3000,
-    description='Needs to be applied last, so it can verify whether it actually improves on the normal rules'' price. Also see https://github.com/metasfresh/metasfresh/issues/4952',
-    updatedby=99,
-    updated='2021-01-19 20:49:49.572807 +01:00'
-WHERE c_pricingrule_id = 540014;
