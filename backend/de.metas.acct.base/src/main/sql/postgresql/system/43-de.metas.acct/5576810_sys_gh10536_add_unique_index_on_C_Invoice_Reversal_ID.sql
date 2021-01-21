407ebb77acfc054b9dc3d61d20296cd4e3caa09e
Adrian <adrian.stefan@metasfresh.com>
2021-01-21 12:21:51 +0200

    #10536 added cleanup commented scripts to migration script

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5576810_sys_gh10536_add_unique_index_on_C_Invoice_Reversal_ID.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5576810_sys_gh10536_add_unique_index_on_C_Invoice_Reversal_ID.sql
@@ -18,3 +18,56 @@ INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Ind
 CREATE UNIQUE INDEX C_Invoice_Reversal_ID ON C_Invoice (Reversal_ID)
 ;
 
+-- When this migration script is failing, it's because there are multiple reversals for the same invoice ID.
+-- In this case, manually run the below script.
+/*
+BEGIN;
+
+CREATE TEMP TABLE Original_C_Invoice_IDs_With_Duplicate_Reversals
+AS
+SELECT c_invoice_id FROM c_invoice WHERE c_invoice_id in (SELECT reversal_id as c_invoice_id FROM (SELECT sum(1) AS total, reversal_id FROM c_invoice WHERE reversal_id IS NOT NULL GROUP BY reversal_id) a WHERE a.total > 1);
+
+CREATE TEMP TABLE Duplicate_C_Invoice_IDs
+AS
+SELECT c_invoice_id FROM c_invoice WHERE c_invoice_id NOT IN (SELECT reversal_id FROM c_invoice WHERE c_invoice_id in (select c_invoice_id from Original_C_Invoice_IDs_With_Duplicate_Reversals)) AND reversal_id IN (SELECT c_invoice_id FROM Original_C_Invoice_IDs_With_Duplicate_Reversals);
+
+CREATE TEMP TABLE Duplicate_C_AllocationHrd_Ids
+AS
+SELECT c_allocationhdr_id FROM c_allocationline WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+
+
+CREATE TABLE backup.BKP_Duplicate_C_Invoice_20210120
+AS
+SELECT * FROM c_invoice WHERE c_invoice_id in (SELECT reversal_id as c_invoice_id FROM (SELECT sum(1) AS total, reversal_id FROM c_invoice WHERE reversal_id IS NOT NULL GROUP BY reversal_id) a WHERE a.total > 1);
+
+
+CREATE TABLE backup.BKP_C_AllocationLine_20210120
+AS
+SELECT * FROM C_AllocationLine WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+
+CREATE TABLE backup.BKP_M_MatchInv_20210120
+AS
+SELECT * FROM M_MatchInv WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+
+CREATE TABLE backup.BKP_C_InvoiceLine_20210120
+AS
+SELECT * FROM C_InvoiceLine WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+
+CREATE TABLE backup.BKP_C_InvoiceLine_Alloc_20210120
+AS
+SELECT * FROM c_invoice_line_alloc WHERE c_invoiceline_id  IN (SELECT c_invoiceline_id FROM c_invoiceline WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs));
+
+CREATE TABLE backup.BKP_C_AllocationHdr_20210120
+AS
+SELECT * FROM C_AllocationHdr WHERE c_allocationhdr_id  IN (SELECT c_allocationhdr_id FROM c_allocationline WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs));
+
+--fact_acct data can be repopulated, no need to backup
+DELETE FROM fact_acct WHERE  ad_table_id=get_table_id('C_Invoice') AND record_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+
+DELETE FROM c_invoice_line_alloc where c_invoiceline_id IN (SELECT c_invoiceline_id FROM c_invoiceline WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs));
+DELETE FROM m_matchinv WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+DELETE FROM c_allocationline WHERE c_allocationhdr_id  IN (SELECT c_allocationhdr_id from Duplicate_C_AllocationHrd_Ids);
+DELETE FROM C_AllocationHdr WHERE c_allocationhdr_id  IN (SELECT c_allocationhdr_id from Duplicate_C_AllocationHrd_Ids);
+DELETE FROM c_invoiceline WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+DELETE FROM c_invoice WHERE c_invoice_id in (select c_invoice_id from Duplicate_C_Invoice_IDs);
+COMMIT;*/
