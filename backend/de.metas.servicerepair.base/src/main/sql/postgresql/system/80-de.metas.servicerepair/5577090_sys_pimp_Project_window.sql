a202b54264aeb5d31e52e9ef24b969b36a5502fe
Teo Sarca <teo.sarca@metasfresh.com>
2021-01-21 22:29:20 +0200

    pimp project window

--- backend/de.metas.servicerepair.base/src/main/sql/postgresql/system/80-de.metas.servicerepair/5577090_sys_pimp_Project_window.sql
+++ backend/de.metas.servicerepair.base/src/main/sql/postgresql/system/80-de.metas.servicerepair/5577090_sys_pimp_Project_window.sql
@@ -162,3 +162,38 @@ UPDATE AD_UI_Element SET WidgetSize='S',Updated=TO_TIMESTAMP('2021-01-21 20:52:2
 UPDATE AD_UI_Element SET WidgetSize='S',Updated=TO_TIMESTAMP('2021-01-21 20:52:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=576319
 ;
 
+
+
+
+
+
+
+-- 2021-01-21T20:23:32.903Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,543164,544762,TO_TIMESTAMP('2021-01-21 22:23:32','YYYY-MM-DD HH24:MI:SS'),100,'Y','status',20,TO_TIMESTAMP('2021-01-21 22:23:32','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-01-21T20:24:03.233Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,627751,0,543279,576320,544762,'F',TO_TIMESTAMP('2021-01-21 22:24:03','YYYY-MM-DD HH24:MI:SS'),100,'Checkbox sagt aus, ob der Datensatz verarbeitet wurde. ','Verarbeitete Datensatz dürfen in der Regel nich mehr geändert werden.','Y','N','Y','N','N','Verarbeitet',10,0,0,TO_TIMESTAMP('2021-01-21 22:24:03','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-01-21T20:24:12.902Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2021-01-21 22:24:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=627751
+;
+
+-- 2021-01-21T20:24:59.649Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_TIMESTAMP('2021-01-21 22:24:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=543279
+;
+
+
+
+
+
+
+
+
+
+
