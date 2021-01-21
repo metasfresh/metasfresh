c700b49395db280dbb9f89b34af8a348d554857e
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-21 16:04:56 +0200

    Arrange the items in the screen

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductWeeklyScreen.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductWeeklyScreen.tsx
@@ -44,34 +44,36 @@ const ProductWeeklyScreen: React.FunctionComponent = observer(() => {
   return (
     <View>
       <div>
-        {dailyQuantities.length &&
-          dailyQuantities.map((dItem) => {
-            return (
-              <div key={dItem.dayCaption} className="product">
-                <div className="box">
-                  <div className="columns is-mobile">
-                    <div className="column is-8">
-                      <div className="columns">
-                        <div className="column is-size-4-mobile no-p">{dItem.dayCaption}</div>
-                        <div className="column is-size-7 no-p">{dItem.date}</div>
-                      </div>
-                    </div>
-                    <div className="column is-4 no-p">
-                      <div className="columns is-mobile">
-                        <div className="column mt-2 is-size-2-mobile no-p has-text-right">{dItem.qty}</div>
-                        <div className="column green-check is-hidden-mobile">
-                          <i className="fas fa-check"></i>
+        <section className="section pt-0">
+          {dailyQuantities.length &&
+            dailyQuantities.map((dItem) => {
+              return (
+                <div key={dItem.dayCaption} className="product">
+                  <div className="box">
+                    <div className="columns is-mobile">
+                      <div className="column is-8">
+                        <div className="columns">
+                          <div className="column is-size-4-mobile no-p">{dItem.dayCaption}</div>
+                          <div className="column is-size-7 no-p">{dItem.date}</div>
                         </div>
-                        <div className="column mt-4 green-check is-hidden-desktop is-hidden-tablet is-4">
-                          {/* Trend listing in here */}
+                      </div>
+                      <div className="column is-4 no-p">
+                        <div className="columns is-mobile">
+                          <div className="column mt-2 is-size-2-mobile no-p has-text-right">{dItem.qty}</div>
+                          <div className="column green-check is-hidden-mobile">
+                            <i className="fas fa-check"></i>
+                          </div>
+                          <div className="column mt-4 green-check is-hidden-desktop is-hidden-tablet is-4">
+                            {/* Trend listing in here */}
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </section>
       </div>
     </View>
   );
