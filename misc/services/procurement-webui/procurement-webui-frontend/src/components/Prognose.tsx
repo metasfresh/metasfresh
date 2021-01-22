dfd4c65cb9f204b0834d8a4ba0f05e5cbddb714e
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-22 10:27:04 +0200

    Prognose widget skel

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Prognose.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Prognose.tsx
@@ -9,9 +9,23 @@ const Prognose: FunctionComponent<Props> = ({ nextWeek }: Props): ReactElement =
   prognoseHeader = prognoseHeader.replace(`{0}`, nextWeek);
   return (
     <div>
-      <div className="container prognose-box">
+      <div className="container prognose-box pl-3 pr-3">
         <div className="columns is-mobile">
-          <div className="column is-12 has-text-centered">{prognoseHeader}</div>
+          <div className="column is-12 has-text-centered pb-0">{prognoseHeader}</div>
+        </div>
+        <div className="columns is-mobile has-text-centered pb-3">
+          <div className="column is-mobile-4 up">
+            <i className="fas fa-arrow-up fa-lg"></i>
+          </div>
+          <div className="column is-mobile-4 down">
+            <i className="fas fa-arrow-down fa-lg"></i>
+          </div>
+          <div className="column is-mobile-4 right">
+            <i className="fas fa-arrow-right fa-lg"></i>
+          </div>
+          <div className="column is-mobile-4 disabled">
+            <i className="fas fa-times fa-lg"></i>
+          </div>
         </div>
       </div>
     </div>
