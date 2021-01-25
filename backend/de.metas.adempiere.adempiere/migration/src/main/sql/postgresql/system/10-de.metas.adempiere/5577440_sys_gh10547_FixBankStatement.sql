97fcbe7f553e1d49fa70aff8986ef30b0274a916
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-01-25 16:49:10 +0200

    #6545 Creating rows in subtab of Bank Statement via import not working https://github.com/metasfresh/metasfresh/issues/10547

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5577440_sys_gh10547_FixBankStatement.sql
@@ -0,0 +1,9 @@
+UPDATE ad_impformat_row
+SET  seqno = 80,  ad_column_id = 9322, startno = 7
+WHERE ad_impformat_row_id = 541333
+;
+
+UPDATE ad_impformat_row
+SET seqno = 90,  startno = 8
+WHERE ad_impformat_row_id = 541332
+;
