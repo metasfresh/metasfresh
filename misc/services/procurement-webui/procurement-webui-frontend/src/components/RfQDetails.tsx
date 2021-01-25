c2bd94252ee8a09fb69f137a7a16153efdc19ea7
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-25 12:44:27 +0200

    Added translations

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/RfQDetails.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/RfQDetails.tsx
@@ -24,31 +24,31 @@ const RfQDetails: React.FunctionComponent = observer(() => {
     <View>
       <section className="container pl-4 pr-4">
         <div className="columns is-mobile bt-1">
-          <div className="column pt-1 pb-1">Product</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.ProductName')}</div>
           <div className="column pt-1 pb-1">{quotation.productName}</div>
         </div>
         <div className="columns is-mobile bt-1">
-          <div className="column pt-1 pb-1">Packing</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.ProductPackingInfo')}</div>
           <div className="column pt-1 pb-1">{quotation.packingInfo}</div>
         </div>
         <div className="columns is-mobile bt-1">
-          <div className="column pt-1 pb-1">From</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.DateStart')}</div>
           <div className="column pt-1 pb-1">{quotation.dateStart}</div>
         </div>
         <div className="columns is-mobile bt-1">
-          <div className="column pt-1 pb-1">To</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.DateEnd')}</div>
           <div className="column pt-1 pb-1">{quotation.dateEnd}</div>
         </div>
         <div className="columns is-mobile bt-1">
-          <div className="column pt-1 pb-1">Close</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.DateClose')}</div>
           <div className="column pt-1 pb-1">{quotation.dateClose}</div>
         </div>
         <div className="columns is-mobile bt-1">
-          <div className="column pt-1 pb-1">Qty requested</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.QtyRequested')}</div>
           <div className="column pt-1 pb-1">{quotation.qtyRequested}</div>
         </div>
         <div className="columns is-mobile bt-1 bb-1">
-          <div className="column pt-1 pb-1">Qty promissed</div>
+          <div className="column pt-1 pb-1">{translate('RfQView.QtyPromised')}</div>
           <div className="column pt-1 pb-1">{quotation.qtyPromised}</div>
         </div>
       </section>
