e0c9e867420b32903ab2863746f8792a65df2510
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-26 15:50:39 +0200

    Make trend fnct component

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Trend.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Trend.tsx
@@ -3,22 +3,19 @@ import classnames from 'classnames';
 interface Props {
   trend: string;
 }
-class Trend extends React.Component<Props> {
-  render(): ReactElement {
-    const { trend } = this.props;
-    return (
-      <div>
-        <i
-          className={classnames('', {
-            'fas fa-lg fa-arrow-up up': trend === 'trend-up',
-            'fas fa-lg fa-arrow-down down': trend === 'trend-down',
-            'fas fa-lg fa-arrow-right right': trend === 'trend-even',
-            'fas fa-lg fa-times disabled': trend === 'trend-zero',
-          })}
-        ></i>
-      </div>
-    );
-  }
-}
+const Trend: React.FunctionComponent<Props> = ({ trend }: Props): ReactElement => {
+  return (
+    <div>
+      <i
+        className={classnames('', {
+          'fas fa-lg fa-arrow-up up': trend === 'trend-up',
+          'fas fa-lg fa-arrow-down down': trend === 'trend-down',
+          'fas fa-lg fa-arrow-right right': trend === 'trend-even',
+          'fas fa-lg fa-times disabled': trend === 'trend-zero',
+        })}
+      ></i>
+    </div>
+  );
+};
 
 export default Trend;
