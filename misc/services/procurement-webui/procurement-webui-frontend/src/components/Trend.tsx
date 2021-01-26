1984081cd9791b4ba695feae91b98037a5ebe511
siemiatj <kuba@saskla.cz>
2021-01-26 15:39:51 +0100

    - cleanup

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Trend.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Trend.tsx
@@ -1,8 +1,10 @@
 import React, { ReactElement } from 'react';
 import classnames from 'classnames';
+
 interface Props {
   trend: string;
 }
+
 const Trend: React.FunctionComponent<Props> = ({ trend }: Props): ReactElement => {
   return (
     <div>
@@ -13,7 +15,7 @@ const Trend: React.FunctionComponent<Props> = ({ trend }: Props): ReactElement =
           'fas fa-lg fa-arrow-right right': trend === 'trend-even',
           'fas fa-lg fa-times disabled': trend === 'trend-zero',
         })}
-      ></i>
+      />
     </div>
   );
 };
