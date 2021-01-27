b35813310047f30882c8e48b72133e1bdc3fcca5
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-01-27 10:18:16 +0200

    Remove debug

--- misc/services/procurement-webui/procurement-webui-frontend/src/models/Store.ts
+++ misc/services/procurement-webui/procurement-webui-frontend/src/models/Store.ts
@@ -128,7 +128,7 @@ export const store = initialState;
  * Create a localStorage entry from the snapshot
  */
 onSnapshot(store, (snapshot) => {
-  console.log('Snapshot: ', snapshot); // this is for debugging purposes - can be removed in prod
+  // console.log('Snapshot: ', snapshot); // this is for debugging purposes - can be removed in prod
   localStorage.setItem('initialState', JSON.stringify(snapshot));
 });
 
