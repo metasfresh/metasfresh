856c550f7d0ef60bdbcf80740a67da4c36de3094
siemiatj <kuba@saskla.cz>
2021-02-01 21:00:39 +0100

    - add eslint ignore for two type warnings

--- misc/services/procurement-webui/procurement-webui-frontend/src/models/Store.ts
+++ misc/services/procurement-webui/procurement-webui-frontend/src/models/Store.ts
@@ -14,6 +14,7 @@ import { RFQList } from './RFQList';
 export default function actionCatch(store: IAnyStateTreeNode) {
   return (
     call: IMiddlewareEvent,
+    /* eslint-disable @typescript-eslint/no-explicit-any */
     next: (actionCall: IMiddlewareEvent, callback?: (value: any) => any) => void
   ): void => {
     try {
