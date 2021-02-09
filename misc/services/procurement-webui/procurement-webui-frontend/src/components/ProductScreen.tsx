9e2612e5dd248d3eaad530485ccd576e19fec16c
siemiatj <kuba@saskla.cz>
2021-02-09 18:47:35 +0100

    - fixkeyboard not showing on iOS

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductScreen.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/ProductScreen.tsx
@@ -1,41 +1,44 @@
-import React, { useContext, useEffect } from 'react';
-import { observer } from 'mobx-react';
+import React from 'react';
+import { observer, inject } from 'mobx-react';
+import { withRouter, RouteComponentProps } from 'react-router';
 import { getSnapshot } from 'mobx-state-tree';
-import { useParams, useHistory } from 'react-router-dom';
+
 import { formDate, prettyDate } from '../utils/date';
 import DailyNav from './DailyNav';
 import View from './View';
-import { RootStoreContext } from '../models/Store';
+import { RootInstance } from '../models/Store';
 
-interface RouteParams {
-  productId?: string;
-  targetDay?: string;
-  targetDayCaption?: string;
+interface MatchParams {
+  name: string;
+  productId: string;
+  targetDay: string;
+  targetDayCaption: string;
 }
 
-const ProductScreen: React.FunctionComponent = observer(() => {
-  const { productId, targetDay, targetDayCaption } = useParams<RouteParams>();
-  const store = useContext(RootStoreContext);
-  const products = getSnapshot(store.dailyProducts.products);
-  const product = products.find((prod) => prod.productId === productId);
-  const { lang } = store.i18n;
-  const currentDay = targetDay ? targetDay : store.app.currentDay;
-  const currentCaption = targetDayCaption ? targetDayCaption : store.app.dayCaption;
-  const qtyInput = React.createRef<HTMLInputElement>();
+interface Props extends RouteComponentProps<MatchParams> {
+  store?: RootInstance;
+}
 
-  const selectAndFocus = () => {
-    if (qtyInput.current) {
-      qtyInput.current.focus();
-      qtyInput.current.select();
-    }
-  };
+@inject('store')
+@observer
+class ProductScreen extends React.Component<Props> {
+  private qtyInput = React.createRef<HTMLInputElement>();
 
-  useEffect(() => {
-    store.navigation.setViewNames(product.productName);
-    selectAndFocus();
-  }, [store]);
+  componentDidMount() {
+    document.addEventListener('focusout', this.handleFocusOut);
+  }
 
-  const saveQty = (newQty: number) => {
+  componentWillUnmount() {
+    document.removeEventListener('focusout', this.handleFocusOut);
+  }
+
+  saveQty = (newQty: number) => {
+    const { store, match } = this.props;
+    const { productId, targetDay } = match.params;
+    const products = getSnapshot(store.dailyProducts.products);
+    const product = products.find((prod) => prod.productId === productId);
+    const currentDay = targetDay ? targetDay : store.app.currentDay;
+
     store
       .postDailyReport({
         items: [
@@ -51,69 +54,86 @@ const ProductScreen: React.FunctionComponent = observer(() => {
         store.app.getUserSession();
       });
   };
-  const history = useHistory();
-  const { navigation } = store;
-  const productQty = product.qty.toString();
 
-  return (
-    <View>
-      <div>
-        <DailyNav
-          isStatic={true}
-          staticDay={prettyDate({ lang, date: formDate({ currentDay: new Date(currentDay) }) })}
-          staticCaption={currentCaption}
-        />
-        <div className="mt-5 p-4">
-          <div className="columns is-mobile">
-            <div className="column is-11">
-              <input
-                className="product-input"
-                type="number"
-                onKeyUp={(e) => {
-                  if (e.key === 'Enter') {
-                    qtyInput.current.blur();
-                    navigation.removeViewFromHistory();
-                    history.goBack();
-                  }
-                }}
-                ref={qtyInput}
-                step="1"
-                value={productQty.length > 1 ? productQty.replace(/^0+/, '') : productQty}
-                onChange={(e) => {
-                  let updatedQty = parseInt(e.target.value, 10);
-                  updatedQty = isNaN(updatedQty) ? 0 : updatedQty;
-                  store.dailyProducts.updateProductQty(product.productId, updatedQty);
-                }}
-                onBlur={(e) => saveQty(parseInt(e.target.value, 10))}
-              />
-            </div>
-            {/* The arrows */}
-            <div className="columns pt-4 green-color">
-              <div
-                className="column is-6"
-                onClick={() => {
-                  saveQty(parseInt(qtyInput.current.value, 10) + 1);
-                }}
-              >
-                <i className="fas fa-2x fa-arrow-up"></i>
+  handleFocusOut = () => {
+    const { store, history } = this.props;
+    const { navigation } = store;
+
+    this.qtyInput.current.blur();
+    navigation.removeViewFromHistory();
+    history.goBack();
+  };
+
+  render() {
+    const { store, match } = this.props;
+    const { productId, targetDay, targetDayCaption } = match.params;
+
+    const products = getSnapshot(store.dailyProducts.products);
+    const product = products.find((prod) => prod.productId === productId);
+    const { lang } = store.i18n;
+    const currentDay = targetDay ? targetDay : store.app.currentDay;
+    const currentCaption = targetDayCaption ? targetDayCaption : store.app.dayCaption;
+    const productQty = product.qty.toString();
+
+    return (
+      <View>
+        <div>
+          <DailyNav
+            isStatic={true}
+            staticDay={prettyDate({ lang, date: formDate({ currentDay: new Date(currentDay) }) })}
+            staticCaption={currentCaption}
+          />
+          <div className="mt-5 p-4">
+            <div className="columns is-mobile">
+              <div className="column is-11">
+                <input
+                  className="product-input"
+                  type="number"
+                  onBlur={(e) => {
+                    this.saveQty(parseInt(e.target.value, 10));
+                  }}
+                  onTouchStart={() => {
+                    this.qtyInput.current.focus();
+                    this.qtyInput.current.select();
+                  }}
+                  ref={this.qtyInput}
+                  step="1"
+                  value={productQty.length > 1 ? productQty.replace(/^0+/, '') : productQty}
+                  onChange={(e) => {
+                    let updatedQty = parseInt(e.target.value, 10);
+                    updatedQty = isNaN(updatedQty) ? 0 : updatedQty;
+                    store.dailyProducts.updateProductQty(product.productId, updatedQty);
+                  }}
+                />
               </div>
-              <div
-                className="column is-6"
-                onClick={() => {
-                  const currentQty = parseInt(qtyInput.current.value, 10);
-                  currentQty > 0 && saveQty(currentQty - 1);
-                }}
-              >
-                <span>
-                  <i className="fas fa-2x fa-arrow-down"></i>
-                </span>
+              {/* The arrows */}
+              <div className="columns pt-4 green-color">
+                <div
+                  className="column is-6"
+                  onClick={() => {
+                    this.saveQty(parseInt(this.qtyInput.current.value, 10) + 1);
+                  }}
+                >
+                  <i className="fas fa-2x fa-arrow-up"></i>
+                </div>
+                <div
+                  className="column is-6"
+                  onClick={() => {
+                    const currentQty = parseInt(this.qtyInput.current.value, 10);
+                    currentQty > 0 && this.saveQty(currentQty - 1);
+                  }}
+                >
+                  <span>
+                    <i className="fas fa-2x fa-arrow-down"></i>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </View>
-  );
-});
+      </View>
+    );
+  }
+}
 
-export default ProductScreen;
+export default withRouter(ProductScreen);
