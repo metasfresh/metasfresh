4aaea4b11bf4d7fa188d9c2a3240636231a4596b
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2021-02-12 14:23:11 +0100

    attempt to quick-fix the build

--- misc/services/camel/de-metas-camel-edi/buildfile.groovy
+++ misc/services/camel/de-metas-camel-edi/buildfile.groovy
@@ -43,9 +43,14 @@ def build(final MvnConf mvnConf, final Map scmVars, final boolean forceBuild=fal
 		
 		// set the artifact version of everything below de.metas.esb/pom.xml
 		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} -DnewVersion=${env.MF_VERSION} ${VERSIONS_PLUGIN}:set"
-
+		
+	        // resolve the version property ${metasfresh-common.version} in the pom.xml
+	        final String commonPropertyParam="-Dproperty=metasfresh-common.version -DnewVersion=LATEST"
+                sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode ${mvnConf.resolveParams} -DallowDowngrade=true ${commonPropertyParam} ${VERSIONS_PLUGIN}:update-property"
+	
+	        // TODO i believe we can drop this; commenting it out for now
 		// update the versions of metas dependencies that are external to the de.metas.esb reactor modules
-		sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:use-latest-versions"
+		//sh "mvn --settings ${mvnConf.settingsFile} --file ${mvnConf.pomFile} --batch-mode -DallowSnapshots=false -DgenerateBackupPoms=true -DprocessDependencies=true -DprocessParent=true -DexcludeReactor=true -Dincludes=\"de.metas*:*\" ${mvnConf.resolveParams} ${VERSIONS_PLUGIN}:use-latest-versions"
 
 		// build and install
 		// about -Dmetasfresh.assembly.descriptor.version: the versions plugin can't update the version of our shared assembly descriptor de.metas.assemblies. Therefore we need to provide the version from outside via this property
