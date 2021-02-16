0c8ee762a26f0268999335728750a90886f61539
cp-ps <cp-ps@pevesoft.ro>
2021-02-16 10:39:28 +0200

    refs: https://github.com/metasfresh/metasfresh/issues/10595 changed the ad_reference for the BP_BankAccount columns

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5579200_sys_gh10595_RemittanceAdvice_BP_BankAccount_Reference.sql
@@ -0,0 +1,25 @@
+-- 2021-02-16T08:33:09.543Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,541257,TO_TIMESTAMP('2021-02-16 10:33:09','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','C_BP_BankAccount(isIdentifier)',TO_TIMESTAMP('2021-02-16 10:33:09','YYYY-MM-DD HH24:MI:SS'),100,'T')
+;
+
+-- 2021-02-16T08:33:09.558Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Reference_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Reference t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Reference_ID=541257 AND NOT EXISTS (SELECT 1 FROM AD_Reference_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Reference_ID=t.AD_Reference_ID)
+;
+
+-- 2021-02-16T08:33:39.110Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,ShowInactiveValues,Updated,UpdatedBy) VALUES (0,3094,0,541257,298,TO_TIMESTAMP('2021-02-16 10:33:39','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N',TO_TIMESTAMP('2021-02-16 10:33:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-02-16T08:35:00.576Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_Value_ID=541257,Updated=TO_TIMESTAMP('2021-02-16 10:35:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=572572
+;
+
+-- 2021-02-16T08:35:11.465Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_Value_ID=541257,Updated=TO_TIMESTAMP('2021-02-16 10:35:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=572573
+;
+
