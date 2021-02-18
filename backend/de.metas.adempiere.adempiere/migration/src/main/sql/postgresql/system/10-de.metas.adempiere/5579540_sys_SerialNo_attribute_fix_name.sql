addcd4f477b378bacf8d193da5a6fc16a2692036
Teo Sarca <teo.sarca@metasfresh.com>
2021-02-18 16:53:21 +0200

    Show SerialNo attribute + trl (#10664)
    * consider extracting SerialNo attribute
    
    * fix naming and trls

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5579540_sys_SerialNo_attribute_fix_name.sql
@@ -0,0 +1,5 @@
+-- 2021-02-18T08:38:09.652Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE M_Attribute SET Name='S/N',Updated=TO_TIMESTAMP('2021-02-18 10:38:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Attribute_ID=540034
+;
+
