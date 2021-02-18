ed24eb59cacb4f4b79489bffccf239cc64d0c1ac
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-02-18 02:16:47 +0100

    minor

--- backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/util/Env.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/util/Env.java
@@ -2495,8 +2495,6 @@ public final class Env
 
 	/**
 	 * Helper method to bind <code>@Autowire</code> annotated properties of given bean using current Spring Application Context.
-	 *
-	 * @param bean
 	 */
 	public static void autowireBean(final Object bean)
 	{
@@ -2506,7 +2504,6 @@ public final class Env
 	/**
 	 * Gets Login/System date
 	 *
-	 * @param ctx
 	 * @return login/system date; never return null
 	 */
 	public static Timestamp getDate(final Properties ctx)
