26b47e41d1fb3c563ce16e6278dfee452bf620b4
adi-stefan <60300682+adi-stefan@users.noreply.github.com>
2021-02-22 07:10:42 -0800

    #gh10670 added accounting date field to windows that support it (#10690)

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5579640_sys_gh10670_add_accounting_date_to_UI.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5579640_sys_gh10670_add_accounting_date_to_UI.sql
@@ -353,3 +353,33 @@ INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Ele
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,879,0,159,544957,577984,'F',TO_TIMESTAMP('2021-02-18 17:34:58','YYYY-MM-DD HH24:MI:SS'),100,'Accounting Date','The Accounting Date indicates the date to be used on the General Ledger account entries generated from this document. It is also used for any currency conversion.','Y','N','N','Y','N','N','N',0,'Buchungsdatum',10,0,0,TO_TIMESTAMP('2021-02-18 17:34:58','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+-- 2021-02-22T14:48:39.103Z
+-- URL zum Konzept
+UPDATE AD_Field SET AD_Name_ID=NULL, Description='Accounting Date', Help='The Accounting Date indicates the date to be used on the General Ledger account entries generated from this document. It is also used for any currency conversion.', Name='Buchungsdatum',Updated=TO_TIMESTAMP('2021-02-22 16:48:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=559512
+;
+
+-- 2021-02-22T14:48:39.567Z
+-- URL zum Konzept
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(263) 
+;
+
+-- 2021-02-22T14:48:39.636Z
+-- URL zum Konzept
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=559512
+;
+
+-- 2021-02-22T14:48:39.685Z
+-- URL zum Konzept
+/* DDL */ select AD_Element_Link_Create_Missing_Field(559512)
+;
+
+-- 2021-02-22T14:51:22.781Z
+-- URL zum Konzept
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=547475
+;
+
+-- 2021-02-22T14:57:38.162Z
+-- URL zum Konzept
+UPDATE AD_Field SET DisplayLogic='@Processed@=''Y''',Updated=TO_TIMESTAMP('2021-02-22 16:57:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=557702
+;
+ 
