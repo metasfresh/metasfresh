e4bf1052ae560219a4def93d5fba142557672c77
Teo Sarca <teo.sarca@metasfresh.com>
2021-02-24 01:23:24 +0200

    5580300_sys_PMM_Purchase_Candidate_M_HU_PI_Item_Product_set_ViewEditMode.sql

---
+++ backend/de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5580300_sys_PMM_Purchase_Candidate_M_HU_PI_Item_Product_set_ViewEditMode.sql
@@ -0,0 +1,27 @@
+/*
+ * #%L
+ * de.metas.procurement.base
+ * %%
+ * Copyright (C) 2021 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+-- 2021-02-23T23:05:58.017Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET ViewEditMode='D',Updated=TO_TIMESTAMP('2021-02-24 01:05:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550982
+;
+
