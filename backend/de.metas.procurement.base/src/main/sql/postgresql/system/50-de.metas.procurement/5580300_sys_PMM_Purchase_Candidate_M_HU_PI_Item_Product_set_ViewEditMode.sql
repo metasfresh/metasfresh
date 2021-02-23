5b40c2079c7a9ce3b102bdeab97e802c2cc9afde
Teo Sarca <teo.sarca@metasfresh.com>
2021-02-24 01:30:40 +0200

    5580300_sys_PMM_Purchase_Candidate_M_HU_PI_Item_Product_set_ViewEditMode.sql

--- backend/de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5580300_sys_PMM_Purchase_Candidate_M_HU_PI_Item_Product_set_ViewEditMode.sql
+++ backend/de.metas.procurement.base/src/main/sql/postgresql/system/50-de.metas.procurement/5580300_sys_PMM_Purchase_Candidate_M_HU_PI_Item_Product_set_ViewEditMode.sql
@@ -1,27 +1,14 @@
-/*
- * #%L
- * de.metas.procurement.base
- * %%
- * Copyright (C) 2021 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- 2021-02-23T23:05:58.017Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_UI_Element SET ViewEditMode='D',Updated=TO_TIMESTAMP('2021-02-24 01:05:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550982
+UPDATE AD_UI_Element SET ViewEditMode='D',Updated=TO_TIMESTAMP('2021-02-24 01:05:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=541894
 ;
 
+/*
+SELECT t.tablename, c.columnname, f.ad_field_id, aue.ad_ui_element_id
+FROM ad_column c
+         INNER JOIN ad_table t ON c.ad_table_id = t.ad_table_id
+         INNER JOIN ad_field f ON c.ad_column_id = f.ad_column_id
+         INNER JOIN ad_ui_element aue ON f.ad_field_id = aue.ad_field_id
+WHERE t.tablename = 'PMM_PurchaseCandidate'
+  AND c.columnname = 'M_HU_PI_Item_Product_Override_ID'
+*/
