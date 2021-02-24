aad1a1d361e50dfcb9d5907e6ab7bc9a31b0ee03
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-02-24 06:28:22 +0100

    minor code and api-doc improvements

--- backend/de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/process/impl/ProcessRestController.java
+++ backend/de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/process/impl/ProcessRestController.java
@@ -46,6 +46,7 @@ import de.metas.util.Check;
 import de.metas.util.Services;
 import de.metas.util.web.MetasfreshRestAPIConstants;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.NonNull;
@@ -70,6 +71,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * This is the rest controller used when processes are invoked via REST-API on the app-server (ServerRoot).
+ */
 @RestController
 @RequestMapping(ProcessRestController.ENDPOINT)
 @Profile(Profiles.PROFILE_App)
@@ -89,12 +93,12 @@ public class ProcessRestController
 		this.processService = processService;
 	}
 
+	@ApiOperation("Invoke a process from the list returned by the `available` endpoint")
 	@PostMapping("{value}/invoke")
 	public ResponseEntity<?> invokeProcess(
-			@NonNull @PathVariable("value") final String processValue,
+			@NonNull @PathVariable("value") @ApiParam("Translates to `AD_Process.Value`") final String processValue,
 			@Nullable @RequestBody(required = false) final RunProcessRequest request)
 	{
-
 		final Optional<AdProcessId> processId = getProcessIdIfRunnable(processValue);
 
 		if (!processId.isPresent())
@@ -148,15 +152,14 @@ public class ProcessRestController
 				.body(response);
 	}
 
-	@ApiOperation("Create an AD_Issue")
+	@ApiOperation("Create an AD_Issue. Note: it's not necessary that the process in question was started by the `invoke` endpoint.")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "Successfully created issue"),
 			@ApiResponse(code = 401, message = "You are not authorized to create new issue"),
 			@ApiResponse(code = 403, message = "Accessing a related resource is forbidden"),
 			@ApiResponse(code = 422, message = "The request body could not be processed")
 	})
-
-	@PostMapping(path = "{AD_PInstance_ID}/externalstatus/error",consumes = "application/json", produces = "application/json")
+	@PostMapping(path = "{AD_PInstance_ID}/externalstatus/error", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<JsonCreateIssueResponse> handleError(@RequestBody @NonNull final JsonError request, @PathVariable final Integer AD_PInstance_ID)
 	{
 		final JsonCreateIssueResponse issueResponse = processService.createIssue(request, PInstanceId.ofRepoId(AD_PInstance_ID));
@@ -184,7 +187,7 @@ public class ProcessRestController
 
 			return ResponseEntity.ok()
 					.contentType(MediaType.parseMediaType(contentType))
-					.header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + processExecutionResult.getReportFilename() + "\"")
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + processExecutionResult.getReportFilename() + "\"")
 					.body(processExecutionResult.getReportDataAsByteArray());
 		}
 		else
