a478f2a75cb38cd68a842ad7d52e319d0553ca79
Teo Sarca <teo.sarca@metasfresh.com>
2021-02-25 05:44:36 +0200

    webui: Zoom Into shall open the right window (#10565)

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5578670_sys_delete_TabCopy_legacy_process.sql
@@ -0,0 +1,25 @@
+-- 2021-02-09T07:54:07.608Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2021-02-09 09:54:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3203
+;
+
+-- 2021-02-09T07:56:26.011Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Table_Process WHERE AD_Table_Process_ID=540343
+;
+
+-- 2021-02-09T07:56:43.662Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Process_Trl WHERE AD_Process_ID=114
+;
+
+-- 2021-02-09T07:56:43.664Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Process WHERE AD_Process_ID=114
+;
+
+-- 2021-02-09T07:57:17.385Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsDisplayed='N', IsDisplayedGrid='N',Updated=TO_TIMESTAMP('2021-02-09 09:57:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3205
+;
+
