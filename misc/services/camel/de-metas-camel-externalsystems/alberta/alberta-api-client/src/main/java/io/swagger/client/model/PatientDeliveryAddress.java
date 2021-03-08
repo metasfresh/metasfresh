4a349c04dbaf320ddd69543dbae5277bb6e35f07
cp-ps <77785468+cp-ps@users.noreply.github.com>
2021-03-08 15:34:46 +0200

    sync pharmacy (#10777)
    * sync pharmacy
    * Update misc/services/camel/de-metas-camel-externalsystems/alberta/de-metas-camel-alberta-camelroutes/src/main/java/de/metas/camel/alberta/BPartnerUpsertRequestProducer.java
    * Update misc/de-metas-common/de-metas-common-bpartner/src/main/java/de/metas/common/bprelation/JsonBPRelationRole.java
    - refs: https://github.com/metasfresh/metasfresh/issues/10775

--- misc/services/camel/de-metas-camel-externalsystems/alberta/alberta-api-client/src/main/java/io/swagger/client/model/PatientDeliveryAddress.java
+++ misc/services/camel/de-metas-camel-externalsystems/alberta/alberta-api-client/src/main/java/io/swagger/client/model/PatientDeliveryAddress.java
@@ -26,7 +26,7 @@ import java.math.BigDecimal;
  * generell ABWEICHENDE Lieferadresse
  */
 @Schema(description = "generell ABWEICHENDE Lieferadresse")
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-02-11T10:30:25.848Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-03-05T06:49:14.310Z[GMT]")
 public class PatientDeliveryAddress {
   @SerializedName("gender")
   private BigDecimal gender = null;
@@ -160,7 +160,7 @@ public class PatientDeliveryAddress {
 
 
   @Override
-  public boolean equals(java.lang.Object o) {
+  public boolean equals(Object o) {
     if (this == o) {
       return true;
     }
@@ -201,7 +201,7 @@ public class PatientDeliveryAddress {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(java.lang.Object o) {
+  private String toIndentedString(Object o) {
     if (o == null) {
       return "null";
     }
