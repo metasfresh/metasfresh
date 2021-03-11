3d8c799a70f99c35fef6702989e390dacf207f0f
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-03-11 14:56:07 +0200

    some more prototyping

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/bpartner/service/OrgChangeRepository.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/bpartner/service/OrgChangeRepository.java
@@ -25,18 +25,29 @@ package de.metas.contracts.bpartner.service;
 import de.metas.bpartner.BPartnerId;
 import de.metas.bpartner.service.IBPartnerDAO;
 import de.metas.contracts.model.I_C_Flatrate_Term;
+import de.metas.contracts.model.X_C_Flatrate_Term;
+import de.metas.contracts.process.FlatrateTermCreator;
 import de.metas.organization.OrgId;
+import de.metas.product.IProductDAO;
+import de.metas.product.ProductId;
+import de.metas.product.model.I_M_Product_Mapping;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.dao.IQueryBL;
 import org.compiere.model.IQuery;
+import org.compiere.model.I_AD_OrgChange_History;
 import org.compiere.model.I_AD_Org_Mapping;
+import org.compiere.model.I_AD_User;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_C_BPartner_Location;
 import org.compiere.model.I_M_Product;
 import org.compiere.model.I_M_Product_Category;
+import org.compiere.util.TimeUtil;
+import org.jolokia.util.DateUtil;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.adempiere.model.InterfaceWrapperHelper.copy;
@@ -49,18 +60,11 @@ public class OrgChangeRepository
 {
 	final IQueryBL queryBL = Services.get(IQueryBL.class);
 	final IBPartnerDAO bpartnerDAO = Services.get(IBPartnerDAO.class);
+	final IProductDAO productDAO = Services.get(IProductDAO.class);
 
 	public boolean hasMembershipSubscriptions(final BPartnerId bPartnerId)
 	{
-		final IQuery<I_M_Product_Category> membershipCategoryQuery = getMembershipCategoryQuery();
-
-		return queryBL.createQueryBuilder(I_C_Flatrate_Term.class)
-				.addEqualsFilter(I_C_Flatrate_Term.COLUMNNAME_Bill_BPartner_ID, bPartnerId)
-				.addInSubQueryFilter(I_C_Flatrate_Term.COLUMNNAME_M_Product_ID,
-									 I_M_Product_Category.COLUMNNAME_M_Product_Category_ID,
-									 membershipCategoryQuery)
-				.create()
-				.anyMatch();
+		return createMembershipRunningSubscriptionQuery(bPartnerId).anyMatch();
 	}
 
 	public boolean hasAnyMembershipProduct(final OrgId orgId)
@@ -76,6 +80,16 @@ public class OrgChangeRepository
 
 	}
 
+	public I_M_Product getNewOrgProductForMapping(final ProductId productId, final OrgId orgId)
+	{
+		final de.metas.product.model.I_M_Product productRecord = productDAO.getById(productId, de.metas.product.model.I_M_Product.class);
+		return queryBL.createQueryBuilder(de.metas.product.model.I_M_Product.class)
+				.addEqualsFilter(de.metas.product.model.I_M_Product.COLUMNNAME_AD_Org_ID, orgId)
+				.addEqualsFilter(de.metas.product.model.I_M_Product.COLUMNNAME_M_Product_Mapping_ID, productRecord.getM_Product_Mapping_ID())
+				.create()
+				.first();
+	}
+
 	private IQuery<I_M_Product_Category> getMembershipCategoryQuery()
 	{
 		final IQuery<I_M_Product_Category> membershipCategoryQuery = queryBL.createQueryBuilder(I_M_Product_Category.class)
@@ -83,6 +97,19 @@ public class OrgChangeRepository
 				.addEqualsFilter(I_M_Product_Category.COLUMNNAME_AD_Org_ID, 0)
 				.create();
 		return membershipCategoryQuery;
+	}
+
+	private IQuery<I_C_Flatrate_Term> createMembershipRunningSubscriptionQuery(final BPartnerId bPartnerId)
+	{
+		final IQuery<I_M_Product_Category> membershipCategoryQuery = getMembershipCategoryQuery();
+
+		return queryBL.createQueryBuilder(I_C_Flatrate_Term.class)
+				.addEqualsFilter(I_C_Flatrate_Term.COLUMNNAME_Bill_BPartner_ID, bPartnerId)
+				.addInSubQueryFilter(I_C_Flatrate_Term.COLUMNNAME_M_Product_ID,
+									 I_M_Product_Category.COLUMNNAME_M_Product_Category_ID,
+									 membershipCategoryQuery)
+				.addEqualsFilter(I_C_Flatrate_Term.COLUMNNAME_ContractStatus, X_C_Flatrate_Term.CONTRACTSTATUS_Running)
+				.create();
 	}
 
 	public BPartnerId retrieveOrCloneBPartner(final @NonNull OrgChangeParameters orgChangeParameters)
@@ -111,7 +138,21 @@ public class OrgChangeRepository
 			}
 		}
 
+		createOrgChangehistory(orgMappingId, bPartnerRecord.getAD_Org_ID(), orgChangeParameters.getOrgToId());
+
 		return cloneBPartner(orgChangeParameters, orgMappingId);
+	}
+
+	private void createOrgChangehistory(final int orgMappingId,
+			final int orgFromId,
+			final OrgId orgToId)
+	{
+		final I_AD_OrgChange_History orgChangeHistory = newInstance(I_AD_OrgChange_History.class);
+		orgChangeHistory.setAD_Org_From_ID(orgFromId);
+		orgChangeHistory.setAD_OrgTo_ID(orgToId.getRepoId());
+		orgChangeHistory.setAD_Org_Mapping_ID(orgMappingId);
+
+		save(orgChangeHistory);
 	}
 
 	private int createOrgMappingForBPartner(final I_C_BPartner bPartnerRecord)
@@ -163,11 +204,29 @@ public class OrgChangeRepository
 	public void retrieveOrCloneLocations(final @NonNull OrgChangeParameters orgChangeParameters,
 			final @NonNull BPartnerId newBPartnerId)
 	{
-		final List<I_C_BPartner_Location> bpartnerLocationRecords = bpartnerDAO.retrieveBPartnerLocations(orgChangeParameters.getBpartnerId());
+		final List<I_C_BPartner_Location> bpartnerActiveLocationRecords = bpartnerDAO.retrieveBPartnerLocations(orgChangeParameters.getBpartnerId());
 
-		reactivateLocationsForPartner(orgChangeParameters, bpartnerLocationRecords);
+		for (I_C_BPartner_Location location : bpartnerActiveLocationRecords)
+		{
+			final int orgMappingId = location.getAD_Org_Mapping_ID();
 
+			// if(orgMappingId > 0 )
+			// {
+			// 	I_C_BPartner_Location counterpartBpartnerLocation = retrieveCounterpartBPartnerLocation(orgMappingId, orgChangeParameters.getOrgToId());
+			// }
+			// if(counterpartBpartnerLocation == null)
+			// {
+			// 	counterpartBpartnerLocation = cloneBPartnerLocation(location, orgMappingId )
+			// }
+		}
+		reactivateLocationsForPartner(orgChangeParameters, bpartnerActiveLocationRecords);
 
+	}
+
+	private I_C_BPartner_Location retrieveCounterpartBPartnerLocation(final int orgMappingId, final OrgId orgToId)
+	{
+		// TODO
+		return null;
 	}
 
 	private void reactivateLocationsForPartner(final OrgChangeParameters orgChangeParameters, final List<I_C_BPartner_Location> bpartnerLocationRecords)
@@ -184,5 +243,61 @@ public class OrgChangeRepository
 	//
 	// }
 
-	// TODO
+	public void createSubscription(final @NonNull OrgChangeParameters orgChangeParams)
+	{
+		// now the source partner should have a counterpart in the new org.
+		// Also, the active users and locations have counterparts as well
+		// => we can now create counterparts for the active subscriptions
+
+		// 1. create the membership subscription
+
+		final ProductId membershipProductId = orgChangeParams.getMembershipProductId();
+		final I_M_Product newOrgMembershipProduct = getNewOrgProductForMapping(membershipProductId, orgChangeParams.getOrgToId());
+		final I_C_Flatrate_Term flatrateTerm = retrieveMembershipSubscription(orgChangeParams.getBpartnerId());
+
+		final I_C_BPartner partner = retrieveCounterpartBPartner(flatrateTerm.getBill_BPartner_ID()); // todo
+		final I_C_BPartner_Location location = retrieveCounterpartBPartnerLocation(flatrateTerm.getBill_Location_ID());
+		final I_AD_User user = retrieveCounterpartUser(flatrateTerm.getAD_User_InCharge_ID());
+
+		final Iterator<I_C_BPartner> it = queryBL.createQueryBuilder(I_C_BPartner.class)
+				.addEqualsFilter(I_C_BPartner.COLUMNNAME_C_BPartner_ID, partner.getC_BPartner_ID())
+				.create()
+				.iterate(I_C_BPartner.class);
+
+		final FlatrateTermCreator creator=  FlatrateTermCreator.builder().conditions(flatrateTerm.getC_Flatrate_Conditions())
+				.bPartners(() -> it)
+				.startDate(TimeUtil.asTimestamp(orgChangeParams.getStartDate()))
+				.isSimulation(false)
+				.conditions(flatrateTerm.getC_Flatrate_Conditions())
+				.product(newOrgMembershipProduct)
+				.userInCharge(user)
+				.build();
+
+		creator.createTermsForBPartners();
+
+		// 2. clone the other subscriptions if their products have counterparts in this org
+
+	}
+
+	private I_AD_User retrieveCounterpartUser(final int ad_user_inCharge_id)
+	{
+		return null;
+	}
+
+	private I_C_BPartner_Location retrieveCounterpartBPartnerLocation(final int bill_location_id)
+	{
+		return null;
+	}
+
+	private I_C_BPartner retrieveCounterpartBPartner(final int bill_bPartner_id)
+	{
+		return null;
+	}
+
+	private I_C_Flatrate_Term retrieveMembershipSubscription(final @NonNull BPartnerId bpartnerId)
+	{
+		return createMembershipRunningSubscriptionQuery(bpartnerId)
+				.first();
+	}
+
 }
