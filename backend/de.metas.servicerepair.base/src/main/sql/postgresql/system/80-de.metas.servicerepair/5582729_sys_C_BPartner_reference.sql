69c832673bcfdc0b4f41a2bef941037a24a58d73
Teo Sarca <teo.sarca@metasfresh.com>
2021-03-18 12:31:51 +0200

    5582729_sys_C_BPartner_reference.sql - fix for the case when the reference already exists

--- backend/de.metas.servicerepair.base/src/main/sql/postgresql/system/80-de.metas.servicerepair/5582729_sys_C_BPartner_reference.sql
+++ backend/de.metas.servicerepair.base/src/main/sql/postgresql/system/80-de.metas.servicerepair/5582729_sys_C_BPartner_reference.sql
@@ -1,5 +1,16 @@
 -- NOTE: copied from 5577730_sys_gh10595_Add_RemittanceAdvice_tabels.sql line 573
 
+DO
+$$
+    BEGIN
+        IF exists(SELECT 1 from ad_reference where ad_reference_id=541252)
+        THEN
+            RAISE NOTICE 'AD_Reference_ID=541252 already exists. Skip creating it!';
+        ELSE
+
+
+
+
 -- 2021-01-28T12:01:52.823Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,541252,TO_TIMESTAMP('2021-01-28 14:01:52','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','C_BPartner',TO_TIMESTAMP('2021-01-28 14:01:52','YYYY-MM-DD HH24:MI:SS'),100,'T')
@@ -20,3 +31,8 @@ INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table
 UPDATE AD_Ref_Table SET WhereClause='C_BPartner.isActive=''Y''',Updated=TO_TIMESTAMP('2021-01-28 14:05:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=541252
 ;
 
+
+        END IF;
+    END
+$$
+;
