233c656e921111d07452adadf2cc9c76f9607766
Cristina <cristinamghita@gmail.com>
2021-03-18 17:22:52 +0200

    #10843 Add  message for multiple transactions

--- backend/de.metas.payment.esr/src/main/sql/postgresql/system/5582920_sys_gh10843_AddTypeColumnIntoESR_ImportLine.sql
+++ backend/de.metas.payment.esr/src/main/sql/postgresql/system/5582920_sys_gh10843_AddTypeColumnIntoESR_ImportLine.sql
@@ -58,3 +58,24 @@ UPDATE AD_Column SET DefaultValue='''ISR Reference''', IsMandatory='Y',Updated=T
 /* DDL */ SELECT public.db_alter_table('ESR_ImportLine','ALTER TABLE public.ESR_ImportLine ADD COLUMN Type VARCHAR(15) DEFAULT ''ISR Reference'' NOT NULL')
 ;
 
+
+-- 2021-03-18T15:21:06.272Z
+-- URL zum Konzept
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545027,0,TO_TIMESTAMP('2021-03-18 16:21:01','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment.esr','Y','Unable to process file because it contains both QRR and ESR transactions. Please create a separate file for each transaction type.','I',TO_TIMESTAMP('2021-03-18 16:21:01','YYYY-MM-DD HH24:MI:SS'),100,'ESR_CAMT54_MultipleTransactionsTypes')
+;
+
+-- 2021-03-18T15:21:06.276Z
+-- URL zum Konzept
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545027 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2021-03-18T15:21:20.260Z
+-- URL zum Konzept
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Die Datei kann nicht verarbeitet werden, da sie sowohl QRR- als auch ESR-Transaktionen enthĂ¤lt. Bitte pro Transaktionstyp eine eigene Datei erstellen.',Updated=TO_TIMESTAMP('2021-03-18 16:21:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545027
+;
+
+-- 2021-03-18T15:21:24.071Z
+-- URL zum Konzept
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-03-18 16:21:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545027
+;
+
