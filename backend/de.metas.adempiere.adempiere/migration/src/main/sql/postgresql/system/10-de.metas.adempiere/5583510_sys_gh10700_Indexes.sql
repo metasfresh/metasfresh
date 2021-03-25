bbecec936452335da6301ae9c4d585d9f72d4a86
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-03-25 21:42:28 +0200

    Indexes, request summary message

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5583510_sys_gh10700_Indexes.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5583510_sys_gh10700_Indexes.sql
@@ -1,61 +1,80 @@
-
--- 2021-03-25T09:01:49.944057900Z
+-- 2021-03-25T19:38:19.312759800Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540578,0,541601,TO_TIMESTAMP('2021-03-25 11:01:49','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','AD_OrgChange_History_C_BPartner_From','N',TO_TIMESTAMP('2021-03-25 11:01:49','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540581,0,114,TO_TIMESTAMP('2021-03-25 21:38:19','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','AD_User_AD_Org_Mapping_ID','N',TO_TIMESTAMP('2021-03-25 21:38:19','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2021-03-25T09:01:49.963098Z
+-- 2021-03-25T19:38:19.338662800Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540578 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
+INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540581 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
 ;
 
--- 2021-03-25T09:02:04.236571500Z
+-- 2021-03-25T19:38:36.131676Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573232,541075,540578,0,TO_TIMESTAMP('2021-03-25 11:02:04','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 11:02:04','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573131,541078,540581,0,TO_TIMESTAMP('2021-03-25 21:38:35','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 21:38:35','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2021-03-25T09:02:05.735049600Z
+-- 2021-03-25T19:38:36.789672Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-CREATE INDEX AD_OrgChange_History_C_BPartner_From ON AD_OrgChange_History (C_BPartner_From_ID)
+CREATE INDEX AD_User_AD_Org_Mapping_ID ON AD_User (AD_Org_Mapping_ID)
 ;
 
--- 2021-03-25T09:02:22.207294400Z
+-- 2021-03-25T19:39:03.369436900Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540579,0,541601,TO_TIMESTAMP('2021-03-25 11:02:22','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','AD_OrgChange_History_C_BPartner_To','N',TO_TIMESTAMP('2021-03-25 11:02:22','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540582,0,291,TO_TIMESTAMP('2021-03-25 21:39:03','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','C_Bpartner_AD_Org_Mapping_ID','N',TO_TIMESTAMP('2021-03-25 21:39:03','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2021-03-25T09:02:22.211848400Z
+-- 2021-03-25T19:39:03.372693300Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540579 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
+INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540582 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
 ;
 
--- 2021-03-25T09:02:31.233765700Z
+-- 2021-03-25T19:39:11.340872800Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573234,541076,540579,0,TO_TIMESTAMP('2021-03-25 11:02:31','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 11:02:31','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573097,541079,540582,0,TO_TIMESTAMP('2021-03-25 21:39:11','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 21:39:11','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2021-03-25T09:02:32.092339600Z
+-- 2021-03-25T19:39:11.661707700Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-CREATE INDEX AD_OrgChange_History_C_BPartner_To ON AD_OrgChange_History (C_BPartner_To_ID)
+CREATE INDEX C_Bpartner_AD_Org_Mapping_ID ON C_BPartner (AD_Org_Mapping_ID)
 ;
 
--- 2021-03-25T09:09:41.635188700Z
+-- 2021-03-25T19:39:38.432840Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540580,0,541601,TO_TIMESTAMP('2021-03-25 11:09:41','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','AD_OrgChange_History_OrgChangeDate','N',TO_TIMESTAMP('2021-03-25 11:09:41','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540583,0,293,TO_TIMESTAMP('2021-03-25 21:39:38','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','C_BPartner_Location_AD_Org_Mapping_ID','N',TO_TIMESTAMP('2021-03-25 21:39:38','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2021-03-25T09:09:41.640366800Z
+-- 2021-03-25T19:39:38.434445200Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540580 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
+INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540583 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
 ;
 
--- 2021-03-25T09:09:57.833750100Z
+-- 2021-03-25T19:39:46.443451400Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573233,541077,540580,0,TO_TIMESTAMP('2021-03-25 11:09:57','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 11:09:57','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573118,541080,540583,0,TO_TIMESTAMP('2021-03-25 21:39:46','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 21:39:46','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2021-03-25T09:09:58.160945700Z
+-- 2021-03-25T19:39:46.770218200Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-CREATE INDEX AD_OrgChange_History_OrgChangeDate ON AD_OrgChange_History (Date_OrgChange)
+CREATE INDEX C_BPartner_Location_AD_Org_Mapping_ID ON C_BPartner_Location (AD_Org_Mapping_ID)
+;
+
+-- 2021-03-25T19:40:14.176130600Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy) VALUES (0,540584,0,298,TO_TIMESTAMP('2021-03-25 21:40:14','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','C_BP_BankAccount_AD_Org_Mapping_ID','N',TO_TIMESTAMP('2021-03-25 21:40:14','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-03-25T19:40:14.180002400Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Index_Table_ID=540584 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
+;
+
+-- 2021-03-25T19:40:22.768288600Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,573227,541081,540584,0,TO_TIMESTAMP('2021-03-25 21:40:22','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',10,TO_TIMESTAMP('2021-03-25 21:40:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-03-25T19:40:23.126028800Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+CREATE INDEX C_BP_BankAccount_AD_Org_Mapping_ID ON C_BP_BankAccount (AD_Org_Mapping_ID)
 ;
 
--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5583850_sys_gh10700_OrgChange_RequestType_And_TerminationReason.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5583850_sys_gh10700_OrgChange_RequestType_And_TerminationReason.sql
@@ -52,3 +52,20 @@ SET isactive = 'Y', internalname='S_OrgSwitch',
 	
 	
 	
+	-- 2021-03-25T17:30:32.223193900Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545028,0,TO_TIMESTAMP('2021-03-25 19:30:32','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Org switch from organization {0} to organization {1}  performed on date {2}','I',TO_TIMESTAMP('2021-03-25 19:30:32','YYYY-MM-DD HH24:MI:SS'),100,'R_Request_OrgChange_Summaty')
+;
+
+-- 2021-03-25T17:30:32.227194200Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545028 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2021-03-25T17:31:32.268797100Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Org switch from organization {0} to organization {1}  performed on date {2}.', Value='R_Request_OrgChange_Summary',Updated=TO_TIMESTAMP('2021-03-25 19:31:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545028
+;
+
+
+	
