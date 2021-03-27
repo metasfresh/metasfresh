c3c179707cdd36204d3506f16bd74d2fea5fe5c9
Teo Sarca <teo.sarca@metasfresh.com>
2021-03-27 10:12:44 +0200

    Fail if the AD_Column's or AD_Field's lookup is not correctly defined (#10898)

--- backend/de.metas.business/src/main/java/org/adempiere/model/validator/AdempiereBaseValidator.java
+++ backend/de.metas.business/src/main/java/org/adempiere/model/validator/AdempiereBaseValidator.java
@@ -190,7 +190,7 @@ public final class AdempiereBaseValidator extends AbstractModuleInterceptor
 		// #2895
 		engine.addModelValidator(AD_Ref_Table.instance);
 
-		engine.addModelValidator(org.adempiere.ad.column.model.interceptor.AD_Column.instance); // #2913
+		//engine.addModelValidator(org.adempiere.ad.column.model.interceptor.AD_Column.instance); // #2913
 		engine.addModelValidator(new org.adempiere.ad.column.model.interceptor.AD_SQLColumn_SourceTableColumn());
 
 		engine.addModelValidator(new AD_Element());
