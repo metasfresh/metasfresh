metasfresh/metasfresh

commit 1fa8de857ae6beae7cf9d1a263682870af442ed4

#6650 Prevent Reverting Inventories if HU was already transformed (#10562)
* #6650 Prevent Reverting Inventories if HU was already transformed
https://github.com/metasfresh/metasfresh/issues/10560

backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/hutransaction/impl/HUTransactionBL.java
@@ -1,5 +1,6 @@
 package de.metas.handlingunits.hutransaction.impl;
 
+import de.metas.handlingunits.HuId;
 import de.metas.handlingunits.IHandlingUnitsDAO;
 import de.metas.handlingunits.allocation.IAllocationRequest;
 import de.metas.handlingunits.allocation.impl.AllocationUtils;
@@ -8,7 +9,11 @@ import de.metas.handlingunits.hutransaction.IHUTransactionCandidate;
 import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_HU_Item;
 import de.metas.handlingunits.model.I_M_HU_PI_Item;
+import de.metas.handlingunits.model.I_M_HU_Trx_Line;
 import de.metas.util.Services;
+import lombok.NonNull;
+import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.util.lang.impl.TableRecordReference;
 
 public class HUTransactionBL implements IHUTransactionBL
 {
@@ -29,4 +34,18 @@ public class HUTransactionBL implements IHUTransactionBL
 		return luTrx;
 	}
 
+	@Override
+	public boolean isLatestHUTrx(@NonNull final HuId huId, @NonNull final TableRecordReference tableRecordReference)
+	{
+		boolean result = Services.get(IQueryBL.class)
+				.createQueryBuilder(I_M_HU_Trx_Line.class)
+				.addOnlyActiveRecordsFilter()
+				.addEqualsFilter(I_M_HU_Trx_Line.COLUMN_M_HU_ID, huId)
+				.addNotEqualsFilter(I_M_HU_Trx_Line.COLUMNNAME_AD_Table_ID, tableRecordReference.getAD_Table_ID())
+				.orderByDescending(I_M_HU_Trx_Line.COLUMN_Created)
+				.orderBy(I_M_HU_Trx_Line.COLUMN_M_HU_Trx_Line_ID)
+				.create().anyMatch();
+		return !result;
+
+	}
 }

backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5577620_sys_gh10560_InventoryRevertErrorMessage.sql
@@ -1,0 +1,20 @@
+-- 2021-01-27T15:16:50.676Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545022,0,TO_TIMESTAMP('2021-01-27 17:16:50','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Einige Handling Units des Inventurbelegs wurden bereits weiter verarbeitet','E',TO_TIMESTAMP('2021-01-27 17:16:50','YYYY-MM-DD HH24:MI:SS'),100,'InventoryReverseError')
+;
+
+-- 2021-01-27T15:16:50.686Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545022 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2021-01-27T15:17:03.410Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-01-27 17:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545022
+;
+
+-- 2021-01-27T15:17:16.571Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Some handling units of the inventory document were already transformed',Updated=TO_TIMESTAMP('2021-01-27 17:17:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545022
+;
+

backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/hutransaction/IHUTransactionBL.java
@@ -1,9 +1,12 @@
 package de.metas.handlingunits.hutransaction;
 
+import de.metas.handlingunits.HuId;
 import de.metas.handlingunits.allocation.IAllocationRequest;
 import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.model.I_M_HU_PI_Item;
 import de.metas.util.ISingletonService;
+import lombok.NonNull;
+import org.adempiere.util.lang.impl.TableRecordReference;
 
 /**
  * Declare business logic to create different {@link IHUTransactionCandidate}s for different purposes.
@@ -23,5 +26,13 @@ public interface IHUTransactionBL extends ISingletonService
 	 * @return
 	 */
 	IHUTransactionCandidate createLUTransactionForAttributeTransfer(I_M_HU luHU, I_M_HU_PI_Item luItemPI, IAllocationRequest request);
+
+	/**
+	 * This method is used to find out for a given document, an HU was changed another time after having been affected by a particular document.
+	 * In the current use-case, we want to know if we can revert an inventory-document. The can only do that if the corresponding HUs were not changed further.
+	 *
+	 * @return {@code true} if for the given huId there is no HU-transaction after the one which references the given referencedRecord.
+	 */
+	boolean isLatestHUTrx(@NonNull final HuId huId, @NonNull final TableRecordReference tableRecordReference);
 
 }

backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/interceptor/M_Inventory.java
@@ -1,21 +1,11 @@
 package de.metas.handlingunits.inventory.interceptor;
 
-import java.util.List;
-
-import org.adempiere.ad.modelvalidator.annotations.DocValidate;
-import org.adempiere.ad.modelvalidator.annotations.Interceptor;
-import org.adempiere.ad.modelvalidator.annotations.ModelChange;
-import org.adempiere.ad.ui.api.ITabCalloutFactory;
-import org.adempiere.mmovement.api.IMovementDAO;
-import org.adempiere.model.PlainContextAware;
-import org.compiere.model.ModelValidator;
-import org.compiere.model.X_M_Inventory;
-import org.springframework.stereotype.Component;
-
 import com.google.common.collect.ImmutableList;
-
 import de.metas.document.engine.IDocumentBL;
 import de.metas.handlingunits.exceptions.HUException;
+import de.metas.handlingunits.hutransaction.IHUTransactionBL;
+import de.metas.handlingunits.inventory.Inventory;
+import de.metas.handlingunits.inventory.InventoryRepository;
 import de.metas.handlingunits.inventory.InventoryService;
 import de.metas.handlingunits.inventory.tabcallout.M_InventoryLineTabCallout;
 import de.metas.handlingunits.model.I_M_Inventory;
@@ -26,6 +16,19 @@ import de.metas.inventory.InventoryId;
 import de.metas.util.Check;
 import de.metas.util.Services;
 import lombok.NonNull;
+import org.adempiere.ad.modelvalidator.annotations.DocValidate;
+import org.adempiere.ad.modelvalidator.annotations.Interceptor;
+import org.adempiere.ad.modelvalidator.annotations.ModelChange;
+import org.adempiere.ad.ui.api.ITabCalloutFactory;
+import org.adempiere.mmovement.api.IMovementDAO;
+import org.adempiere.model.PlainContextAware;
+import org.adempiere.util.lang.impl.TableRecordReference;
+import org.compiere.SpringContextHolder;
+import org.compiere.model.ModelValidator;
+import org.compiere.model.X_M_Inventory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /*
  * #%L
@@ -53,6 +56,8 @@ import lombok.NonNull;
 public class M_Inventory
 {
 	private final InventoryService inventoryLineRecordService;
+	private final IHUTransactionBL huTransactionBL = Services.get(IHUTransactionBL.class);
+	private final IInventoryDAO inventoryDAO = Services.get(IInventoryDAO.class);
 
 	public M_Inventory(@NonNull final InventoryService inventoryRecordHUService)
 	{
@@ -83,11 +88,25 @@ public class M_Inventory
 		inventoryLineRecordService.syncToHUs(inventoryRecord);
 	}
 
+	@DocValidate(timings = ModelValidator.TIMING_BEFORE_REVERSECORRECT)
+	public void checkHUTransformationBeforeReverseCorrect(final I_M_Inventory inventory)
+	{
+		final InventoryRepository inventoryRepository = SpringContextHolder.instance.getBean(InventoryRepository.class);
+		final Inventory invObj = inventoryRepository.toInventory(inventory);
+		invObj.getLines().forEach( line ->
+	    {
+			line.getInventoryLineHUs().forEach(hu -> {
+				if (!huTransactionBL.isLatestHUTrx(hu.getHuId(), TableRecordReference.of(I_M_InventoryLine.Table_Name, line.getId())))
+					{
+						throw new HUException("@InventoryReverseError@");
+					}
+			});
+		});
+	}
+
 	@DocValidate(timings = ModelValidator.TIMING_AFTER_REVERSECORRECT)
 	public void reverseDisposal(final I_M_Inventory inventory)
 	{
-		final IInventoryDAO inventoryDAO = Services.get(IInventoryDAO.class);
-
 		if (!inventoryLineRecordService.isMaterialDisposal(inventory))
 		{
 			return; // nothing to do

commit 9e84f6ac61b7b2a1a9b6d2f073e8d6bec2cf01dd

fix HUTransactionBL.isLatestHUTrx
issue introduced by PR https://github.com/metasfresh/metasfresh/pull/10562

backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/hutransaction/impl/HUTransactionBL.java
@@ -13,10 +13,14 @@ import de.metas.handlingunits.model.I_M_HU_Trx_Line;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.adempiere.util.lang.impl.TableRecordReference;
+
+import java.util.Optional;
 
 public class HUTransactionBL implements IHUTransactionBL
 {
+	private final IQueryBL queryBL = Services.get(IQueryBL.class);
 
 	@Override
 	public IHUTransactionCandidate createLUTransactionForAttributeTransfer(final I_M_HU luHU, final I_M_HU_PI_Item luItemPI, final IAllocationRequest request)
@@ -24,12 +28,12 @@ public class HUTransactionBL implements IHUTransactionBL
 		final I_M_HU_Item luItem = Services.get(IHandlingUnitsDAO.class).retrieveItem(luHU, luItemPI);
 
 		final HUTransactionCandidate luTrx = new HUTransactionCandidate(AllocationUtils.getReferencedModel(request), // receipt schedule
-				luItem, // huItem,
-				null, // vhuItem (note: at this level we're not talking about VHUs but actual LU-HUs)
-				request.getProductId(),
-				request.getQuantity().toZero(),
-				request.getDate()
-				);
+																		luItem, // huItem,
+																		null, // vhuItem (note: at this level we're not talking about VHUs but actual LU-HUs)
+																		request.getProductId(),
+																		request.getQuantity().toZero(),
+																		request.getDate()
+		);
 
 		return luTrx;
 	}
@@ -37,15 +41,35 @@ public class HUTransactionBL implements IHUTransactionBL
 	@Override
 	public boolean isLatestHUTrx(@NonNull final HuId huId, @NonNull final TableRecordReference tableRecordReference)
 	{
-		boolean result = Services.get(IQueryBL.class)
+		final TableRecordReference recordRefOfLastTrx = getRecordRefOfLastTrx(huId).orElse(null);
+		return TableRecordReference.equals(tableRecordReference, recordRefOfLastTrx);
+	}
+
+	private Optional<TableRecordReference> getRecordRefOfLastTrx(final @NonNull HuId huId)
+	{
+		final I_M_HU_Trx_Line lastQtyChangeHUTrx = queryBL
 				.createQueryBuilder(I_M_HU_Trx_Line.class)
 				.addOnlyActiveRecordsFilter()
-				.addEqualsFilter(I_M_HU_Trx_Line.COLUMN_M_HU_ID, huId)
-				.addNotEqualsFilter(I_M_HU_Trx_Line.COLUMNNAME_AD_Table_ID, tableRecordReference.getAD_Table_ID())
-				.orderByDescending(I_M_HU_Trx_Line.COLUMN_Created)
-				.orderBy(I_M_HU_Trx_Line.COLUMN_M_HU_Trx_Line_ID)
-				.create().anyMatch();
-		return !result;
+				.addEqualsFilter(I_M_HU_Trx_Line.COLUMNNAME_M_HU_ID, huId)
+				.addNotEqualsFilter(I_M_HU_Trx_Line.COLUMNNAME_Qty, 0)
+				.orderByDescending(I_M_HU_Trx_Line.COLUMN_M_HU_Trx_Line_ID)
+				.create()
+				.first();
 
+		if (lastQtyChangeHUTrx == null)
+		{
+			return Optional.empty();
+		}
+
+		final int counterpartTrxLineId = lastQtyChangeHUTrx.getParent_HU_Trx_Line_ID();
+		if (counterpartTrxLineId <= 0)
+		{
+			// those are some very old trx
+			return Optional.empty();
+		}
+
+		final I_M_HU_Trx_Line counterpartTrxLine = InterfaceWrapperHelper.load(counterpartTrxLineId, I_M_HU_Trx_Line.class);
+		final TableRecordReference recordRef = TableRecordReference.ofOrNull(counterpartTrxLine.getAD_Table_ID(), counterpartTrxLine.getRecord_ID());
+		return Optional.ofNullable(recordRef);
 	}
 }