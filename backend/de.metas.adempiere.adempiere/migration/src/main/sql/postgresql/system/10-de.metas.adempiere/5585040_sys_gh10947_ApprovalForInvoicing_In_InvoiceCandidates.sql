d99dfd897981932f446ea363af0ce0b362cb8fcc
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-04-09 20:12:40 +0300

    Approve for invoicing always read-only

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5585040_sys_gh10947_ApprovalForInvoicing_In_InvoiceCandidates.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5585040_sys_gh10947_ApprovalForInvoicing_In_InvoiceCandidates.sql
@@ -8,3 +8,16 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,622380,0,543052,544365,583339,'F',TO_TIMESTAMP('2021-04-07 19:30:32','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Freigabe zur Fakturierung',35,0,0,TO_TIMESTAMP('2021-04-07 19:30:32','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+
+-- 2021-04-09T17:09:23.500Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ReadOnlyLogic='',Updated=TO_TIMESTAMP('2021-04-09 20:09:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=551293
+;
+
+-- 2021-04-09T17:10:22.203Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2021-04-09 20:10:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 
+
+WHERE AD_Column_ID=551293
+;
+
