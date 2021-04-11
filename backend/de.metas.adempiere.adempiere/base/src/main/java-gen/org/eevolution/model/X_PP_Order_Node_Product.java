1de1ebfb96c5e43b8f2eae38da8a8a1254374e85
Adrian <adrian.stefan@metasfresh.com>
2021-04-11 14:32:32 +0300

    #gh10962 create pp_wf_node_product.specification column

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/eevolution/model/X_PP_Order_Node_Product.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/eevolution/model/X_PP_Order_Node_Product.java
@@ -1,262 +1,217 @@
-/******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
- * This program is free software, you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
- * You should have received a copy of the GNU General Public License along    *
- * with this program, if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- * For the text or an alternative of this public license, you may reach us    *
- * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
- * or via info@compiere.org or http://www.compiere.org/license.html           *
- *****************************************************************************/
-/** Generated Model - DO NOT CHANGE */
+// Generated Model - DO NOT CHANGE
 package org.eevolution.model;
 
+import javax.annotation.Nullable;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.Properties;
-import org.compiere.model.*;
-import org.compiere.util.Env;
 
-/** Generated Model for PP_Order_Node_Product
- *  @author Adempiere (generated) 
- *  @version Release 3.5.4a - $Id$ */
-public class X_PP_Order_Node_Product extends PO implements I_PP_Order_Node_Product, I_Persistent 
+/**
+ * Generated Model for PP_Order_Node_Product
+ *
+ * @author metasfresh (generated)
+ */
+@SuppressWarnings("unused")
+public class X_PP_Order_Node_Product extends org.compiere.model.PO implements I_PP_Order_Node_Product, org.compiere.model.I_Persistent
 {
 
+	private static final long serialVersionUID = -560375951L;
+
 	/**
-	 *
+	 * Standard Constructor
 	 */
-	private static final long serialVersionUID = 20090915L;
-
-    /** Standard Constructor */
-    public X_PP_Order_Node_Product (Properties ctx, int PP_Order_Node_Product_ID, String trxName)
-    {
-      super (ctx, PP_Order_Node_Product_ID, trxName);
-      /** if (PP_Order_Node_Product_ID == 0)
-        {
-			setM_Product_ID (0);
-			setPP_Order_ID (0);
-			setPP_Order_Node_ID (0);
-			setPP_Order_Node_Product_ID (0);
-			setPP_Order_Workflow_ID (0);
-        } */
-    }
-
-    /** Load Constructor */
-    public X_PP_Order_Node_Product (Properties ctx, ResultSet rs, String trxName)
-    {
-      super (ctx, rs, trxName);
-    }
-
-    /** AccessLevel
-      * @return 3 - Client - Org 
-      */
-    protected int get_AccessLevel()
-    {
-      return accessLevel.intValue();
-    }
-
-    /** Load Meta Data */
-    protected POInfo initPO (Properties ctx)
-    {
-      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
-      return poi;
-    }
-
-    public String toString()
-    {
-      StringBuffer sb = new StringBuffer ("X_PP_Order_Node_Product[")
-        .append(get_ID()).append("]");
-      return sb.toString();
-    }
-
-	/** Set Is Subcontracting.
-		@param IsSubcontracting Is Subcontracting	  */
-	public void setIsSubcontracting (boolean IsSubcontracting)
+	public X_PP_Order_Node_Product(final Properties ctx, final int PP_Order_Node_Product_ID, @Nullable final String trxName)
 	{
-		set_Value (COLUMNNAME_IsSubcontracting, Boolean.valueOf(IsSubcontracting));
+		super(ctx, PP_Order_Node_Product_ID, trxName);
 	}
 
-	/** Get Is Subcontracting.
-		@return Is Subcontracting	  */
-	public boolean isSubcontracting () 
+	/**
+	 * Load Constructor
+	 */
+	public X_PP_Order_Node_Product(final Properties ctx, final ResultSet rs, @Nullable final String trxName)
 	{
-		Object oo = get_Value(COLUMNNAME_IsSubcontracting);
-		if (oo != null) 
-		{
-			 if (oo instanceof Boolean) 
-				 return ((Boolean)oo).booleanValue(); 
-			return "Y".equals(oo);
-		}
-		return false;
+		super(ctx, rs, trxName);
 	}
 
-	public I_M_Product getM_Product() throws RuntimeException
-    {
-		return (I_M_Product)MTable.get(getCtx(), I_M_Product.Table_Name)
-			.getPO(getM_Product_ID(), get_TrxName());	}
-
-	/** Set Product.
-		@param M_Product_ID 
-		Product, Service, Item
-	  */
-	public void setM_Product_ID (int M_Product_ID)
+	/**
+	 * Load Meta Data
+	 */
+	@Override
+	protected org.compiere.model.POInfo initPO(final Properties ctx)
 	{
-		if (M_Product_ID < 1) 
-			set_Value (COLUMNNAME_M_Product_ID, null);
-		else 
-			set_Value (COLUMNNAME_M_Product_ID, Integer.valueOf(M_Product_ID));
+		return org.compiere.model.POInfo.getPOInfo(Table_Name);
 	}
 
-	/** Get Product.
-		@return Product, Service, Item
-	  */
-	public int getM_Product_ID () 
+	@Override
+	public void setIsSubcontracting(final boolean IsSubcontracting)
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_M_Product_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		set_Value(COLUMNNAME_IsSubcontracting, IsSubcontracting);
 	}
 
-	public org.eevolution.model.I_PP_Order getPP_Order() throws RuntimeException
-    {
-		return (org.eevolution.model.I_PP_Order)MTable.get(getCtx(), org.eevolution.model.I_PP_Order.Table_Name)
-			.getPO(getPP_Order_ID(), get_TrxName());	}
-
-	/** Set Manufacturing Order.
-		@param PP_Order_ID Manufacturing Order	  */
-	public void setPP_Order_ID (int PP_Order_ID)
+	@Override
+	public boolean isSubcontracting()
 	{
-		if (PP_Order_ID < 1) 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_ID, null);
-		else 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_ID, Integer.valueOf(PP_Order_ID));
+		return get_ValueAsBoolean(COLUMNNAME_IsSubcontracting);
 	}
 
-	/** Get Manufacturing Order.
-		@return Manufacturing Order	  */
-	public int getPP_Order_ID () 
+	@Override
+	public int getM_Product_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_PP_Order_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_M_Product_ID);
 	}
 
-	public org.eevolution.model.I_PP_Order_Node getPP_Order_Node() throws RuntimeException
-    {
-		return (org.eevolution.model.I_PP_Order_Node)MTable.get(getCtx(), org.eevolution.model.I_PP_Order_Node.Table_Name)
-			.getPO(getPP_Order_Node_ID(), get_TrxName());	}
-
-	/** Set Manufacturing Order Activity.
-		@param PP_Order_Node_ID Manufacturing Order Activity	  */
-	public void setPP_Order_Node_ID (int PP_Order_Node_ID)
+	@Override
+	public void setM_Product_ID(final int M_Product_ID)
 	{
-		if (PP_Order_Node_ID < 1) 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_Node_ID, null);
-		else 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_Node_ID, Integer.valueOf(PP_Order_Node_ID));
+		if (M_Product_ID < 1)
+			set_Value(COLUMNNAME_M_Product_ID, null);
+		else
+			set_Value(COLUMNNAME_M_Product_ID, M_Product_ID);
 	}
 
-	/** Get Manufacturing Order Activity.
-		@return Manufacturing Order Activity	  */
-	public int getPP_Order_Node_ID () 
+	@Override
+	public org.eevolution.model.I_PP_Order getPP_Order()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_PP_Order_Node_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsPO(COLUMNNAME_PP_Order_ID, org.eevolution.model.I_PP_Order.class);
 	}
 
-	/** Set Manufacturing Order Activity Product.
-		@param PP_Order_Node_Product_ID Manufacturing Order Activity Product	  */
-	public void setPP_Order_Node_Product_ID (int PP_Order_Node_Product_ID)
+	@Override
+	public void setPP_Order(final org.eevolution.model.I_PP_Order PP_Order)
 	{
-		if (PP_Order_Node_Product_ID < 1) 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_Node_Product_ID, null);
-		else 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_Node_Product_ID, Integer.valueOf(PP_Order_Node_Product_ID));
+		set_ValueFromPO(COLUMNNAME_PP_Order_ID, org.eevolution.model.I_PP_Order.class, PP_Order);
 	}
 
-	/** Get Manufacturing Order Activity Product.
-		@return Manufacturing Order Activity Product	  */
-	public int getPP_Order_Node_Product_ID () 
+	@Override
+	public int getPP_Order_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_PP_Order_Node_Product_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_PP_Order_ID);
 	}
 
-	public org.eevolution.model.I_PP_Order_Workflow getPP_Order_Workflow() throws RuntimeException
-    {
-		return (org.eevolution.model.I_PP_Order_Workflow)MTable.get(getCtx(), org.eevolution.model.I_PP_Order_Workflow.Table_Name)
-			.getPO(getPP_Order_Workflow_ID(), get_TrxName());	}
-
-	/** Set Manufacturing Order Workflow.
-		@param PP_Order_Workflow_ID Manufacturing Order Workflow	  */
-	public void setPP_Order_Workflow_ID (int PP_Order_Workflow_ID)
+	@Override
+	public void setPP_Order_ID(final int PP_Order_ID)
 	{
-		if (PP_Order_Workflow_ID < 1) 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_Workflow_ID, null);
-		else 
-			set_ValueNoCheck (COLUMNNAME_PP_Order_Workflow_ID, Integer.valueOf(PP_Order_Workflow_ID));
+		if (PP_Order_ID < 1)
+			set_ValueNoCheck(COLUMNNAME_PP_Order_ID, null);
+		else
+			set_ValueNoCheck(COLUMNNAME_PP_Order_ID, PP_Order_ID);
 	}
 
-	/** Get Manufacturing Order Workflow.
-		@return Manufacturing Order Workflow	  */
-	public int getPP_Order_Workflow_ID () 
+	@Override
+	public org.eevolution.model.I_PP_Order_Node getPP_Order_Node()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_PP_Order_Workflow_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsPO(COLUMNNAME_PP_Order_Node_ID, org.eevolution.model.I_PP_Order_Node.class);
 	}
 
-	/** Set Quantity.
-		@param Qty 
-		Quantity
-	  */
-	public void setQty (BigDecimal Qty)
+	@Override
+	public void setPP_Order_Node(final org.eevolution.model.I_PP_Order_Node PP_Order_Node)
 	{
-		set_Value (COLUMNNAME_Qty, Qty);
+		set_ValueFromPO(COLUMNNAME_PP_Order_Node_ID, org.eevolution.model.I_PP_Order_Node.class, PP_Order_Node);
 	}
 
-	/** Get Quantity.
-		@return Quantity
-	  */
-	public BigDecimal getQty () 
+	@Override
+	public int getPP_Order_Node_ID()
 	{
-		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_Qty);
-		if (bd == null)
-			 return Env.ZERO;
-		return bd;
+		return get_ValueAsInt(COLUMNNAME_PP_Order_Node_ID);
 	}
 
-	/** Set Sequence.
-		@param SeqNo 
-		Method of ordering records; lowest number comes first
-	  */
-	public void setSeqNo (int SeqNo)
+	@Override
+	public void setPP_Order_Node_ID(final int PP_Order_Node_ID)
 	{
-		set_Value (COLUMNNAME_SeqNo, Integer.valueOf(SeqNo));
+		if (PP_Order_Node_ID < 1)
+			set_ValueNoCheck(COLUMNNAME_PP_Order_Node_ID, null);
+		else
+			set_ValueNoCheck(COLUMNNAME_PP_Order_Node_ID, PP_Order_Node_ID);
 	}
 
-	/** Get Sequence.
-		@return Method of ordering records; lowest number comes first
-	  */
-	public int getSeqNo () 
+	@Override
+	public int getPP_Order_Node_Product_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_SeqNo);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_PP_Order_Node_Product_ID);
+	}
+
+	@Override
+	public void setPP_Order_Node_Product_ID(final int PP_Order_Node_Product_ID)
+	{
+		if (PP_Order_Node_Product_ID < 1)
+			set_ValueNoCheck(COLUMNNAME_PP_Order_Node_Product_ID, null);
+		else
+			set_ValueNoCheck(COLUMNNAME_PP_Order_Node_Product_ID, PP_Order_Node_Product_ID);
+	}
+
+	@Override
+	public org.eevolution.model.I_PP_Order_Workflow getPP_Order_Workflow()
+	{
+		return get_ValueAsPO(COLUMNNAME_PP_Order_Workflow_ID, org.eevolution.model.I_PP_Order_Workflow.class);
+	}
+
+	@Override
+	public void setPP_Order_Workflow(final org.eevolution.model.I_PP_Order_Workflow PP_Order_Workflow)
+	{
+		set_ValueFromPO(COLUMNNAME_PP_Order_Workflow_ID, org.eevolution.model.I_PP_Order_Workflow.class, PP_Order_Workflow);
+	}
+
+	@Override
+	public int getPP_Order_Workflow_ID()
+	{
+		return get_ValueAsInt(COLUMNNAME_PP_Order_Workflow_ID);
+	}
+
+	@Override
+	public void setPP_Order_Workflow_ID(final int PP_Order_Workflow_ID)
+	{
+		if (PP_Order_Workflow_ID < 1)
+			set_ValueNoCheck(COLUMNNAME_PP_Order_Workflow_ID, null);
+		else
+			set_ValueNoCheck(COLUMNNAME_PP_Order_Workflow_ID, PP_Order_Workflow_ID);
+	}
+
+	@Override
+	public BigDecimal getQty()
+	{
+		final BigDecimal bd = get_ValueAsBigDecimal(COLUMNNAME_Qty);
+		return bd != null ? bd : BigDecimal.ZERO;
+	}
+
+	@Override
+	public void setQty(final @Nullable BigDecimal Qty)
+	{
+		set_Value(COLUMNNAME_Qty, Qty);
+	}
+
+	@Override
+	public int getSeqNo()
+	{
+		return get_ValueAsInt(COLUMNNAME_SeqNo);
+	}
+
+	@Override
+	public void setSeqNo(final int SeqNo)
+	{
+		set_Value(COLUMNNAME_SeqNo, SeqNo);
+	}
+
+	@Override
+	public java.lang.String getSpecification()
+	{
+		return get_ValueAsString(COLUMNNAME_Specification);
+	}
+
+	@Override
+	public void setSpecification(final @Nullable java.lang.String Specification)
+	{
+		set_Value(COLUMNNAME_Specification, Specification);
+	}
+
+	@Override
+	public BigDecimal getYield()
+	{
+		final BigDecimal bd = get_ValueAsBigDecimal(COLUMNNAME_Yield);
+		return bd != null ? bd : BigDecimal.ZERO;
+	}
+
+	@Override
+	public void setYield(final @Nullable BigDecimal Yield)
+	{
+		set_Value(COLUMNNAME_Yield, Yield);
 	}
 }
