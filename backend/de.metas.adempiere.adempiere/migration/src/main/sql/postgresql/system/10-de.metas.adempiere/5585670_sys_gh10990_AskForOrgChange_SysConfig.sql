e845c0ddbb2cb0b19051263b5d3de68c49d8b57d
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-04-21 10:55:55 +0300

    sys config description

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5585670_sys_gh10990_AskForOrgChange_SysConfig.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5585670_sys_gh10990_AskForOrgChange_SysConfig.sql
@@ -5,3 +5,13 @@ VALUES (0,0,541364,'S',TO_TIMESTAMP('2021-04-15 15:56:01','YYYY-MM-DD HH24:MI:SS
 TO_TIMESTAMP('2021-04-15 15:56:01','YYYY-MM-DD HH24:MI:SS'),100,'N')
 ;
 
+-- 2021-04-20T20:37:03.551Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Description='When set on ''Y'', this config will trigger the process "Organisation Change", which allows moving a partner, together with the locations, users and bankaccounts, to be moved to another organization. This process will be triggered when a user changes the Region of a location.',Updated=TO_TIMESTAMP('2021-04-20 23:37:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541364
+;
+
+-- 2021-04-21T07:38:37.543Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_SysConfig SET Description='When set on ''Y'', this configurtion will trigger the process "Organisation Change", which allows moving a partner, together with the locations, users and bankaccounts to be moved to another organization. This process will be triggered when a user changes the Region of a location.',Updated=TO_TIMESTAMP('2021-04-21 10:38:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541364
+;
+
