67791d76ad263d606d699808a6271226bc0ac1c2
Teo Sarca <teo.sarca@metasfresh.com>
2021-04-27 12:50:56 +0300

    5586610_sys_InvoiceDefaultAgg_add_Bill_Location_Value_ID.sql

---
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5586610_sys_InvoiceDefaultAgg_add_Bill_Location_Value_ID.sql
@@ -0,0 +1,10 @@
+-- 2021-04-27T09:49:39.913Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO C_AggregationItem (AD_Client_ID,AD_Column_ID,AD_Org_ID,C_Aggregation_ID,C_AggregationItem_ID,Created,CreatedBy,EntityType,IsActive,Type,Updated,UpdatedBy) VALUES (1000000,573528,1000000,1000000,540035,TO_TIMESTAMP('2021-04-27 12:49:39','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.invoicecandidate','Y','COL',TO_TIMESTAMP('2021-04-27 12:49:39','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-04-27T09:49:40.588Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO C_Queue_Block (AD_Client_ID,AD_Org_ID,C_Queue_Block_ID,C_Queue_PackageProcessor_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (1000000,1000000,561956,540028,TO_TIMESTAMP('2021-04-27 12:49:40','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2021-04-27 12:49:40','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
