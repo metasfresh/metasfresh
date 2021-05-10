d8ab1b30a681ee2c984c1948106521bb80a56957
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-05-10 11:30:53 +0300

    Fix prettier issues

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Login.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Login.tsx
@@ -14,112 +14,110 @@ interface Props {
 }
 
 const Login: FunctionComponent<Props> = inject('store')(
-  observer(
-    ({ store }): ReactElement => {
-      const history = useHistory();
-      const [state, setState] = useState({
-        email: '',
-        password: '',
+  observer(({ store }): ReactElement => {
+    const history = useHistory();
+    const [state, setState] = useState({
+      email: '',
+      password: '',
+    });
+    const { app } = store;
+
+    useEffect(() => {
+      if (!(state.email || state.password)) {
+        store.navigation.setViewNames(translate('LoginView.fields.loginButton'));
+      }
+    });
+
+    const handleChange = (e) => {
+      const { id, value } = e.target;
+      setState((prevState) => ({
+        ...prevState,
+        [id]: value,
+      }));
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      const { email, password } = state;
+
+      store.logIn(email, password).then((response) => {
+        !response.loginError && history.push('/');
       });
-      const { app } = store;
+    };
 
-      useEffect(() => {
-        if (!(state.email || state.password)) {
-          store.navigation.setViewNames(translate('LoginView.fields.loginButton'));
-        }
-      });
+    const handleForgotPassword = (event) => {
+      event.preventDefault();
 
-      const handleChange = (e) => {
-        const { id, value } = e.target;
-        setState((prevState) => ({
-          ...prevState,
-          [id]: value,
-        }));
-      };
+      history.push('/forgottenPassword');
+    };
 
-      const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const { email, password } = state;
-
-        store.logIn(email, password).then((response) => {
-          !response.loginError && history.push('/');
-        });
-      };
-
-      const handleForgotPassword = (event) => {
-        event.preventDefault();
-
-        history.push('/forgottenPassword');
-      };
-
-      return (
-        <View>
-          <div className="container p-4 pb-6 login-view">
-            <div className="login-logo pb-6">
-              <img src="../../static/media/logo.png" className="logo" />
+    return (
+      <View>
+        <div className="container p-4 pb-6 login-view">
+          <div className="login-logo pb-6">
+            <img src="../../static/media/logo.png" className="logo" />
+          </div>
+          <form>
+            <div className="field">
+              <p className="control has-icons-left">
+                <input
+                  className="input is-medium"
+                  id="email"
+                  type="email"
+                  value={state.email}
+                  placeholder={translate('LoginView.fields.email')}
+                  onChange={handleChange}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-envelope"></i>
+                </span>
+              </p>
             </div>
-            <form>
-              <div className="field">
-                <p className="control has-icons-left">
-                  <input
-                    className="input is-medium"
-                    id="email"
-                    type="email"
-                    value={state.email}
-                    placeholder={translate('LoginView.fields.email')}
-                    onChange={handleChange}
-                  />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-envelope"></i>
-                  </span>
-                </p>
-              </div>
-              <div className="field">
-                <p className="control has-icons-left">
-                  <input
-                    className="input is-medium"
-                    id="password"
-                    type="password"
-                    value={state.password}
-                    placeholder={translate('LoginView.fields.password')}
-                    onChange={handleChange}
-                  />
-                  <span className="icon is-small is-left">
-                    <i className="fas fa-lock"></i>
-                  </span>
-                </p>
-              </div>
+            <div className="field">
+              <p className="control has-icons-left">
+                <input
+                  className="input is-medium"
+                  id="password"
+                  type="password"
+                  value={state.password}
+                  placeholder={translate('LoginView.fields.password')}
+                  onChange={handleChange}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
 
-              <div className="field my-4">
-                <p className="control">
-                  <a onClick={handleForgotPassword} className="green-color">
-                    {translate('LoginView.fields.forgotPasswordButton')}
-                  </a>
-                </p>
-              </div>
+            <div className="field my-4">
+              <p className="control">
+                <a onClick={handleForgotPassword} className="green-color">
+                  {translate('LoginView.fields.forgotPasswordButton')}
+                </a>
+              </p>
+            </div>
 
-              <div className="field">
-                <div className="control">
-                  <button
-                    type="submit"
-                    className="button is-medium is-fullwidth is-success is-green-bg"
-                    onClick={handleSubmit}
-                  >
-                    {translate('LoginView.fields.loginButton')}
-                  </button>
-                </div>
-                {app.loginError ? <p className="help is-danger">{app.loginError}</p> : null}
+            <div className="field">
+              <div className="control">
+                <button
+                  type="submit"
+                  className="button is-medium is-fullwidth is-success is-green-bg"
+                  onClick={handleSubmit}
+                >
+                  {translate('LoginView.fields.loginButton')}
+                </button>
               </div>
-            </form>
-          </div>
-          <div className="login-poweredby-logo">
-            <img src={poweredByLogo} className="poweredby" />
-          </div>
-        </View>
-      );
-    }
-  )
+              {app.loginError ? <p className="help is-danger">{app.loginError}</p> : null}
+            </div>
+          </form>
+        </div>
+        <div className="login-poweredby-logo">
+          <img src={poweredByLogo} className="poweredby" />
+        </div>
+      </View>
+    );
+  })
 );
 
 export default Login;
