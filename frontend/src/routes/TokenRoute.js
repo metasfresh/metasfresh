ee1916cb943fec07c372ce29db3487998e366aac
siemiatj <kuba@saskla.cz>
2021-05-11 14:46:28 +0200

    #11015 create route for handling token logging

---
+++ frontend/src/routes/TokenRoute.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { useAuth } from '../hooks/useAuth';
+import history from '../services/History';
+import { loginWithToken } from '../api';
+
+/**
+ * @file Function component.
+ * @module TokenRoute
+ * @summary - handles the case when we authenticate directly by using a `token` without the need to supply a `username` and a `password`
+ *
+ * @param {object} - tokenId prop given as match.param to the /token path i.e  /token/xxxxxxx   (`xxxxxxx` will be the value of the tokenId )
+ */
+const TokenRoute = ({ match }) => {
+  const { tokenId } = match.params;
+  const auth = useAuth();
+
+  loginWithToken(tokenId)
+    .then(() => {
+      auth.loginSuccess();
+      history.push('/');
+    })
+    .catch(() => {
+      history.push('/login?redirect=true');
+    });
+};
+
+export default React.memo(TokenRoute);
