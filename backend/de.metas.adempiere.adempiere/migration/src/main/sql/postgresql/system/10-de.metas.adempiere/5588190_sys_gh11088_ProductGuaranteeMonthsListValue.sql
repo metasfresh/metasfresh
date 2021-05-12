87a0556e472b77ee3a315387329abee2d00db235
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-05-12 09:57:03 +0300

    #11088 Garantiezeit 60 Monate hinzufügen https://github.com/metasfresh/metasfresh/issues/11088

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5588190_sys_gh11088_ProductGuaranteeMonthsListValue.sql
@@ -0,0 +1,9 @@
+-- 2021-05-11T13:33:14.996Z
+-- URL zum Konzept
+INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value,ValueName) VALUES (0,0,541258,542498,TO_TIMESTAMP('2021-05-11 15:33:14','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','60',TO_TIMESTAMP('2021-05-11 15:33:14','YYYY-MM-DD HH24:MI:SS'),100,'60','60')
+;
+
+-- 2021-05-11T13:33:14.999Z
+-- URL zum Konzept
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Ref_List_ID=542498 AND NOT EXISTS (SELECT 1 FROM AD_Ref_List_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Ref_List_ID=t.AD_Ref_List_ID)
+;
