8a6c5a864e47133f5c2c71a441397387b66f5238
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-05-13 19:19:53 +0300

    improvements

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5588320_sys_gh11094_Products_Winthout_Initial_Cost_Function.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5588320_sys_gh11094_Products_Winthout_Initial_Cost_Function.sql
@@ -1,12 +1,18 @@
 DROP FUNCTION IF EXISTS ProductsWithoutInitialCost
-    (p_ad_org_id NUMERIC);
+(p_D_Client_ID NUMERIC,
+ p_ad_org_id NUMERIC);
 
-CREATE OR REPLACE FUNCTION ProductsWithoutInitialCost(p_ad_org_id NUMERIC)
+CREATE OR REPLACE FUNCTION ProductsWithoutInitialCost(
+    p_AD_Client_Id NUMERIC,
+    p_AD_Org_Id NUMERIC)
     RETURNS table
             (
+                AD_Org_ID    numeric,
+                OrgValue     text,
+                OrgName      text,
                 M_Product_ID numeric,
                 ProductValue text,
-                ProductName text
+                ProductName  text
             )
 AS
 $$
@@ -15,40 +21,49 @@ WITH element AS
              SELECT el.m_costelement_id
 
              FROM c_acctschema s
+                      JOIN ad_clientinfo ci on ci.ad_client_id = p_AD_Client_Id
+                 AND s.c_acctschema_id = ci.c_acctschema1_id
                       JOIN m_costelement el on s.costingmethod = el.costingmethod
-             WHERE s.ad_org_id = 1000000
          ),
      firstCostDetails AS
          (
              SELECT cd.M_Product_ID,
+                    cd.ad_org_id,
                     min(M_CostDetail_ID) as firstCostDetail
 
              from M_costDetail cd
                       JOIN element on cd.m_costelement_id = element.m_costelement_id
 
-             group by cd.M_Product_ID
+             where (case when p_AD_Org_Id < 0 or p_ad_org_ID IS NULL then 1 = 1 else cd.ad_org_id = p_AD_Org_Id end)
+             group by cd.M_Product_ID, cd.AD_Org_ID
          )
         ,
      firstPrices as
          (
              SELECT p.m_product_id,
+                    coalesce(fcd.ad_org_id, p.ad_org_id) as ad_org_ID,
                     cd.isSOTrx,
                     (CASE
                          WHEN (cd.Qty = 0 or cd.QTY IS NULL) THEN COALESCE(cd.Amt, 0)
-                         ELSE cd.Amt / cd.Qty END) as price
+                         ELSE cd.Amt / cd.Qty END)       as price
              FROM M_Product p
                       LEFT JOIN firstCostDetails fcd on p.m_product_id = fcd.m_product_id
                       LEFT JOIN m_costdetail cd on fcd.firstCostDetail = cd.m_costdetail_id
              WHERE cd.m_costdetail_id IS NULL
                 OR cd.issotrx = 'N'
          )
-SELECT p.M_Product_ID,
+SELECT fp.AD_Org_ID,
+       o.value as OrgValue,
+       o.name  as OrgName,
+       p.M_Product_ID,
        p.value,
        p.name
 from m_product p
          JOIN firstPrices fp on p.m_product_id = fp.m_product_id
+         JOIN AD_Org o on fp.ad_org_ID = o.ad_org_ID
 where fp.price = 0
-order by p.value
+  AND ((p_AD_Org_Id < 0 or p_ad_org_ID IS NULL) OR o.ad_org_ID = p_AD_Org_ID)
+order by o.value, p.value
     ;
 
 
@@ -60,6 +75,6 @@ $$
 How to run:
 
 SELECT *
-FROM ProductsWithoutInitialCost(1000000)
+FROM ProductsWithoutInitialCost(1000000, 1000000)
 ;
  */
--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5588330_sys_gh11094_Products_Winthout_Initial_Cost_Process.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5588330_sys_gh11094_Products_Winthout_Initial_Cost_Process.sql
@@ -668,5 +668,129 @@ UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Produkte ohne initialen Kosten
 
 
 
+-- 2021-05-13T11:45:55.222Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Reference_Value_ID,ColumnName,Created,CreatedBy,DefaultValue,Description,EntityType,FieldLength,Help,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,113,0,584828,541975,30,322,'AD_Org_ID',TO_TIMESTAMP('2021-05-13 14:45:55','YYYY-MM-DD HH24:MI:SS'),100,'@AD_Org_ID/-1@','Organisatorische Einheit des Mandanten','D',0,'Eine Organisation ist ein Bereich ihres Mandanten - z.B. Laden oder Abteilung. Sie können Daten über Organisationen hinweg gemeinsam verwenden.','Y','N','Y','N','Y','N','Sektion',10,TO_TIMESTAMP('2021-05-13 14:45:55','YYYY-MM-DD HH24:MI:SS'),100)
+;
 
+-- 2021-05-13T11:45:55.226Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_Para_ID=541975 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2021-05-13T11:46:25.126Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,DefaultValue,Description,DisplayLogic,EntityType,FieldLength,Help,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,102,0,584828,541976,18,'AD_Client_ID',TO_TIMESTAMP('2021-05-13 14:46:24','YYYY-MM-DD HH24:MI:SS'),100,'@AD_Client_ID/-1@','Mandant für diese Installation.','1=0','D',0,'Ein Mandant ist eine Firma oder eine juristische Person. Sie können keine Daten über Mandanten hinweg verwenden. .','Y','N','Y','N','Y','N','Mandant',20,TO_TIMESTAMP('2021-05-13 14:46:24','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-05-13T11:46:25.132Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_Para_ID=541976 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2021-05-13T11:46:46.501Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='SELECT * FROM ProductsWithoutInitialCost(@AD_Client_ID/-1@, @AD_Org_ID/-1@)',Updated=TO_TIMESTAMP('2021-05-13 14:46:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584828
+;
+
+-- 2021-05-13T12:38:43.341Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET IsMandatory='N',Updated=TO_TIMESTAMP('2021-05-13 15:38:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541975
+;
+
+
+
+-- 2021-05-13T13:12:05.916Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='SELECT * FROM ProductsWithoutInitialCost(@#AD_Client_ID/-1@, @#AD_Org_ID/-1@)',Updated=TO_TIMESTAMP('2021-05-13 16:12:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584828
+;
+
+-- 2021-05-13T13:12:19.368Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET DefaultValue='@#AD_Client_ID/-1@',Updated=TO_TIMESTAMP('2021-05-13 16:12:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541976
+;
+
+-- 2021-05-13T13:12:22.857Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET DefaultValue='@#AD_Org_ID/-1@',Updated=TO_TIMESTAMP('2021-05-13 16:12:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541975
+;
+
+-- 2021-05-13T13:15:34.210Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='SELECT * FROM ProductsWithoutInitialCost(@#AD_Client_ID/-1@, @AD_Org_ID/-1@)',Updated=TO_TIMESTAMP('2021-05-13 16:15:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584828
+;
+
+-- 2021-05-13T13:15:43.772Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM  AD_Process_Para_Trl WHERE AD_Process_Para_ID=541976
+;
+
+-- 2021-05-13T13:15:43.784Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Process_Para WHERE AD_Process_Para_ID=541976
+;
+
+
+
+
+
+-- 2021-05-13T14:12:44.370Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='SELECT * FROM ProductsWithoutInitialCost(@#AD_Client_ID/-1@, @AD_Org_ID@)',Updated=TO_TIMESTAMP('2021-05-13 17:12:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584828
+;
+
+-- 2021-05-13T15:59:36.521Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Reference_Value_ID=276,Updated=TO_TIMESTAMP('2021-05-13 18:59:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541975
+;
+
+-- 2021-05-13T16:05:50.076Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET DefaultValue='@#AD_Org_ID@',Updated=TO_TIMESTAMP('2021-05-13 19:05:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541975
+;
+
+
+
+
+
+-- 2021-05-13T16:09:28.196Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,579153,0,'PARAM_AD_Org_ID',TO_TIMESTAMP('2021-05-13 19:09:26','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Sektion','Sektion',TO_TIMESTAMP('2021-05-13 19:09:26','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-05-13T16:09:28.209Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=579153 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- 2021-05-13T16:09:47.915Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Organisation', PrintName='Organisation',Updated=TO_TIMESTAMP('2021-05-13 19:09:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=579153 AND AD_Language='en_US'
+;
+
+-- 2021-05-13T16:09:47.918Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579153,'en_US') 
+;
+
+-- 2021-05-13T16:10:12.299Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET AD_Element_ID=579153, ColumnName='PARAM_AD_Org_ID', Description=NULL, Help=NULL, IsAutocomplete='Y',Updated=TO_TIMESTAMP('2021-05-13 19:10:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541975
+;
+
+-- 2021-05-13T16:10:25.022Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='SELECT * FROM ProductsWithoutInitialCost(@#AD_Client_ID/-1@, @PARAM_AD_Org_ID@)',Updated=TO_TIMESTAMP('2021-05-13 19:10:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584828
+;
+
+
+
+
+
+
+
+-- 2021-05-13T16:11:43.147Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET SQLStatement='SELECT * FROM ProductsWithoutInitialCost(@#AD_Client_ID/-1@, @PARAM_AD_Org_ID/-1@)',Updated=TO_TIMESTAMP('2021-05-13 19:11:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584828
+;
 
