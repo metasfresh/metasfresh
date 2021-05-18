0550e419b8e33c810838485abb56948aef9db71b
siemiatj <kuba@saskla.cz>
2021-05-18 14:32:40 +0200

    #11015 optimize token login route

--- frontend/src/routes/TokenRoute.js
+++ frontend/src/routes/TokenRoute.js
@@ -1,38 +1,42 @@
 import React from 'react';
+import { isEqual } from 'lodash';
+import PropTypes from 'prop-types';
 
 import { useAuth } from '../hooks/useAuth';
 import history from '../services/History';
-// import { loginWithToken } from '../api';
 
 /**
  * @file Function component.
  * @module TokenRoute
  * @summary - handles the case when we authenticate directly by using a `token` without the need to supply a `username` and a `password`
  *
- * @param {object} - tokenId prop given as match.param to the /token path i.e  /token/xxxxxxx   (`xxxxxxx` will be the value of the tokenId )
+ * @param {string} - tokenId prop given as match.param to the /token path i.e  /token/xxxxxxx   (`xxxxxxx` will be the value of the tokenId )
  */
-const TokenRoute = ({ match }) => {
-  const { tokenId } = match.params;
+const TokenRoute = (props) => {
+  const { tokenId } = props.match.params;
   const auth = useAuth();
-  // const { loginRequest } = auth;
-
-  // if (loginRequest) {
-  //   return null;
-  //   }
 
   auth
     .tokenLogin(tokenId)
     .then(() => history.push('/'))
     .catch(() => history.push('/login?redirect=true'));
 
-  // loginWithToken(tokenId)
-  //   .then(() => {
-  //     auth.loginSuccess();
-  //     history.push('/');
-  //   })
-  //   .catch(() => {
-  //     history.push('/login?redirect=true');
-  //   });
+  return null;
 };
 
-export default React.memo(TokenRoute);
+function propsAreEqual(prevProps, nextProps) {
+  const { match } = prevProps;
+  const { match: nextMatch } = nextProps;
+
+  if (isEqual(match, nextMatch)) {
+    return true;
+  }
+
+  return false;
+}
+
+TokenRoute.propTypes = {
+  tokenId: PropTypes.string,
+};
+
+export default React.memo(TokenRoute, propsAreEqual);
