447fd03eb7599f7fe91a3b1a5a581969003c7281
Adrian <adrian.stefan@metasfresh.com>
2021-05-20 11:06:17 +0300

    #gh11062 changed from JOIN to UNION to prevent cartesian issues

--- backend/de.metas.material/cockpit/src/main/sql/postgresql/ddl/functions/de_metas_material/Retrieve_available_for_Sales.sql
+++ backend/de.metas.material/cockpit/src/main/sql/postgresql/ddl/functions/de_metas_material/Retrieve_available_for_Sales.sql
@@ -1,4 +1,3 @@
-
 DROP FUNCTION IF EXISTS de_metas_material.retrieve_available_for_sales(integer, numeric, character varying, timestamptz, integer, integer);
 CREATE FUNCTION de_metas_material.retrieve_available_for_sales(
     IN p_QueryNo integer,
@@ -8,55 +7,62 @@ CREATE FUNCTION de_metas_material.retrieve_available_for_sales(
     IN p_shipmentDateLookAheadHours integer,
     IN p_salesOrderLookBehindHours integer)
     RETURNS TABLE
-            ( QueryNo integer,
-              M_Product_ID numeric,
-              StorageAttributesKey character varying,
-              QtyToBeShipped numeric,
-              QtyOnHandStock numeric,
-              C_UOM_ID numeric
+            (
+                QueryNo              integer,
+                M_Product_ID         numeric,
+                StorageAttributesKey character varying,
+                QtyToBeShipped       numeric,
+                QtyOnHandStock       numeric,
+                C_UOM_ID             numeric
             )
 AS
 $BODY$
 SELECT p_QueryNo,
        p_M_Product_ID,
-       COALESCE(sales.AttributesKey, stock.AttributesKey),
+       AttributesKey,
        QtyToBeShipped,
        QtyOnHandStock,
-       stock.C_UOM_ID
-FROM (
-         SELECT SUM(QtyToBeShipped) AS QtyToBeShipped,
-                sq.AttributesKey    AS AttributesKey,
-                C_UOM_ID
-         FROM de_metas_material.MD_ShipmentQty_V sq
-         WHERE sq.M_Product_ID = p_M_Product_ID
-           -- if not ALL, then we want an exact match. i.e. when looking for goods without any attribute, we do not want to find e.g. goods with an "expired" attribute
-           AND ('-1000' LIKE p_StorageAttributesKey /*ALL*/ OR sq.AttributesKey LIKE p_StorageAttributesKey)
-           AND ( /*max. future PreparationDates we still care about; avoiding coalesce in an attempt to make it easier for the planner */
-                 sq.s_PreparationDate_Override <= (p_PreparationDate + (p_shipmentDateLookAheadHours || ' hours')::INTERVAL)
-                 OR sq.s_PreparationDate_Override IS NULL AND sq.s_PreparationDate <= (p_PreparationDate + (p_shipmentDateLookAheadHours || ' hours')::INTERVAL)
-                 OR sq.s_PreparationDate_Override IS NULL AND sq.s_PreparationDate IS NULL AND sq.o_PreparationDate <= (p_PreparationDate + (p_shipmentDateLookAheadHours || ' hours')::INTERVAL)
-             )
-           AND (sq.SalesOrderLastUpdated IS NULL /*could have used COALESCE(sq.SalesOrderLastUpdated, now()), but i hope this makes it easier for the planner*/
-             OR sq.SalesOrderLastUpdated >= (NOW() - (p_salesOrderLookBehindHours || ' hours')::INTERVAL) /*min updated value that is not yet too old for us to care */
-             )
-         GROUP BY C_UOM_ID, sq.AttributesKey
-     ) sales
-         FULL OUTER JOIN (
-    SELECT SUM(QtyOnHand)  AS QtyOnHandStock,
-           s.AttributesKey AS AttributesKey,
-           p.C_UOM_ID
-    FROM MD_Stock s
-             JOIN M_Product p ON p.M_Product_ID = s.M_Product_ID
-    WHERE s.M_Product_ID = p_M_Product_ID
-      AND s.IsActive = 'Y'
-      -- if not ALL, then we want an exact match. i.e. when looking for goods without any attribute, we do not want to find e.g. goods with an "expired" attribute
-      AND ('-1000' LIKE p_StorageAttributesKey /*ALL*/ OR s.AttributesKey LIKE p_StorageAttributesKey)
-    GROUP BY s.AttributesKey,
+       C_UOM_ID
+FROM (SELECT p_QueryNo,
+             p_M_Product_ID,
+             sq.AttributesKey    AS AttributesKey,
+             SUM(QtyToBeShipped) AS QtyToBeShipped,
+             0                   AS QtyOnHandStock,
              C_UOM_ID
-) stock ON TRUE
+      FROM de_metas_material.MD_ShipmentQty_V sq
+      WHERE sq.M_Product_ID = p_M_Product_ID
+        -- if not ALL, then we want an exact match. i.e. when looking for goods without any attribute, we do not want to find e.g. goods with an "expired" attribute
+        AND ('-1000' LIKE p_StorageAttributesKey /*ALL*/ OR sq.AttributesKey LIKE p_StorageAttributesKey)
+        AND ( /*max. future PreparationDates we still care about; avoiding coalesce in an attempt to make it easier for the planner */
+              sq.s_PreparationDate_Override <= (p_PreparationDate + (p_shipmentDateLookAheadHours || ' hours')::INTERVAL)
+              OR sq.s_PreparationDate_Override IS NULL AND sq.s_PreparationDate <= (p_PreparationDate + (p_shipmentDateLookAheadHours || ' hours')::INTERVAL)
+              OR sq.s_PreparationDate_Override IS NULL AND sq.s_PreparationDate IS NULL AND sq.o_PreparationDate <= (p_PreparationDate + (p_shipmentDateLookAheadHours || ' hours')::INTERVAL)
+          )
+        AND (sq.SalesOrderLastUpdated IS NULL /*could have used COALESCE(sq.SalesOrderLastUpdated, now()), but i hope this makes it easier for the planner*/
+          OR sq.SalesOrderLastUpdated >= (NOW() - (p_salesOrderLookBehindHours || ' hours')::INTERVAL) /*min updated value that is not yet too old for us to care */
+          )
+      GROUP BY C_UOM_ID, sq.AttributesKey) sales
+
+UNION
+(SELECT p_QueryNo,
+        p_M_Product_ID,
+        s.AttributesKey AS AttributesKey,
+        0               AS QtyToBeShipped,
+        SUM(QtyOnHand)  AS QtyOnHandStock,
+        p.C_UOM_ID      AS C_UOM_ID
+ FROM MD_Stock s
+          JOIN M_Product p ON p.M_Product_ID = s.M_Product_ID
+ WHERE s.M_Product_ID = p_M_Product_ID
+   AND s.IsActive = 'Y'
+   -- if not ALL, then we want an exact match. i.e. when looking for goods without any attribute, we do not want to find e.g. goods with an "expired" attribute
+   AND ('-1000' LIKE p_StorageAttributesKey /*ALL*/ OR s.AttributesKey LIKE p_StorageAttributesKey)
+ GROUP BY s.AttributesKey,
+          C_UOM_ID)
 $BODY$
 
-LANGUAGE sql STABLE;
+    LANGUAGE sql STABLE
+;
+
 COMMENT ON FUNCTION de_metas_material.retrieve_available_for_sales(integer, numeric, character varying, timestamptz, integer, integer) IS
     'Returns the current stock and the shipments to be expected in the nearest future.
     Parameters:
