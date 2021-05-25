3500a702239ec0a02fc283ad0031bc7da03e7e5e
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-05-25 19:26:03 +0300

    Weight attributes readonly flags https://github.com/metasfresh/metasfresh/issues/11159

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5589930_sys_gh11159_Weight_Attributes_ReadOnly_Flags.sql
@@ -0,0 +1,60 @@
+
+CREATE table backup.BKP_M_HU_PI_Attribute_gh1125_25052021
+AS
+    SELECT * FROM M_HU_PI_Attribute;
+	
+	
+
+
+-------------- Packing instructions version for CUs (a.k.a. virtual HUs)
+
+
+
+
+-- 2021-05-25T11:49:28.076Z
+-- URL zum Konzept
+UPDATE M_HU_PI_Attribute
+SET IsReadOnly='Y', Updated=TO_TIMESTAMP('2021-05-25 13:49:28', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
+WHERE M_HU_PI_Attribute_ID=1000020 -- Gewicht Brutto kg
+;
+
+-- 2021-05-25T11:49:28.076Z
+-- URL zum Konzept
+UPDATE M_HU_PI_Attribute
+SET IsReadOnly='Y', Updated=TO_TIMESTAMP('2021-05-25 13:49:28', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
+WHERE M_HU_PI_Attribute_ID=1000021 -- Gewicht Tara
+;
+
+
+-- 2021-05-25T11:49:28.076Z
+-- URL zum Konzept
+UPDATE M_HU_PI_Attribute
+SET IsReadOnly='Y', Updated=TO_TIMESTAMP('2021-05-25 13:49:28', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
+WHERE M_HU_PI_Attribute_ID=540069 -- Gewicht Netto
+;
+
+
+
+----Packing Item Template
+
+
+-- 2021-05-25T11:49:28.076Z
+-- URL zum Konzept
+UPDATE M_HU_PI_Attribute
+SET IsReadOnly='N', Updated=TO_TIMESTAMP('2021-05-25 13:49:28', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
+WHERE M_HU_PI_Attribute_ID=1000023 -- Gewicht Brutto kg
+;
+-- 2021-05-25T11:49:28.076Z
+-- URL zum Konzept
+UPDATE M_HU_PI_Attribute
+SET IsReadOnly='N', Updated=TO_TIMESTAMP('2021-05-25 13:49:28', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
+WHERE M_HU_PI_Attribute_ID=1000025 -- Gewicht Tara
+;
+-- 2021-05-25T11:49:28.076Z
+-- URL zum Konzept
+UPDATE M_HU_PI_Attribute
+SET IsReadOnly='Y', Updated=TO_TIMESTAMP('2021-05-25 13:49:28', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
+WHERE M_HU_PI_Attribute_ID=1000024 -- Gewicht Netto
+;
+
+
