e5372a2108456444e13193709419e82b20e63f6f
Adrian <adrian.stefan@metasfresh.com>
2021-05-26 16:02:00 +0300

    #gh11171 add bpartner_location_warehouse foreign key constraint

---
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5590040_sys_gh11171_sync_c_bpartner_id.sql
@@ -0,0 +1,11 @@
+UPDATE m_warehouse
+SET c_bpartner_id = loc.c_bpartner_id
+FROM c_bpartner_location loc
+WHERE m_warehouse.c_bpartner_location_id = loc.c_bpartner_location_id
+;
+
+ALTER TABLE m_warehouse
+    ADD CONSTRAINT bpartner_location_warehouse
+        FOREIGN KEY (c_bpartner_id, c_bpartner_location_id) REFERENCES c_bpartner_location (c_bpartner_id, c_bpartner_location_id)
+            DEFERRABLE INITIALLY DEFERRED
+;
