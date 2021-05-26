0242ad390fcd9cb640d8ef6b4b11be559e2f2743
Adrian <adrian.stefan@metasfresh.com>
2021-05-26 18:39:03 +0300

    #gh11171 prevent group with no value from incorrectly matching groups with an actual value

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5590040_sys_gh11171_sync_c_bpartner_id.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5590040_sys_gh11171_sync_c_bpartner_id.sql
@@ -4,6 +4,10 @@ FROM c_bpartner_location loc
 WHERE m_warehouse.c_bpartner_location_id = loc.c_bpartner_location_id
 ;
 
+ALTER TABLE c_bpartner_location
+    ADD UNIQUE (c_bpartner_id, c_bpartner_location_id)
+;
+
 ALTER TABLE m_warehouse
     ADD CONSTRAINT bpartner_location_warehouse
         FOREIGN KEY (c_bpartner_id, c_bpartner_location_id) REFERENCES c_bpartner_location (c_bpartner_id, c_bpartner_location_id)
