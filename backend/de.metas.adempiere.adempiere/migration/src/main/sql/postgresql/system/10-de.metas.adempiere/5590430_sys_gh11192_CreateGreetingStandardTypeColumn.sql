fe0795a3dd93e64f15be19c9e8c397e53bc6c8b1
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-05-28 16:04:37 +0300

    #11192 Introduce C_Greeting.GreetingStandardType https://github.com/metasfresh/metasfresh/issues/11192

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5590430_sys_gh11192_CreateGreetingStandardTypeColumn.sql
@@ -0,0 +1,4 @@
+-- 2021-05-28T12:38:39.335Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ SELECT public.db_alter_table('C_Greeting','ALTER TABLE public.C_Greeting ADD COLUMN GreetingStandardType VARCHAR(20)')
+;
