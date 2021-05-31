c7781550120185487c3f3386f5347ecd4cc314b7
BodorAnnamaria <73820536+BodorAnnamaria@users.noreply.github.com>
2021-05-31 18:45:37 +0300

    #11166 Display field IsAutoAllocateAvailableAmt in Payment window (#11175)
    https://github.com/metasfresh/metasfresh/issues/11166

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5590120_sys_gh11107_AddTrlIncotermListValue.sql
@@ -0,0 +1,95 @@
+-- 2021-05-26T14:41:12.502Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET Name='EXW - Ex Works',Updated=TO_TIMESTAMP('2021-05-26 17:41:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501600
+;
+
+-- 2021-05-26T14:41:32.603Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-05-26 17:41:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501600
+;
+
+-- 2021-05-26T14:41:39.465Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='EXW - Ex Works',Updated=TO_TIMESTAMP('2021-05-26 17:41:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501600
+;
+
+-- 2021-05-26T14:42:02.424Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='FCA - Free Carrier',Updated=TO_TIMESTAMP('2021-05-26 17:42:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501601
+;
+
+-- 2021-05-26T14:42:12.288Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='FCA - Free Carrier',Updated=TO_TIMESTAMP('2021-05-26 17:42:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501601
+;
+
+-- 2021-05-26T14:42:46.541Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CPT - Carriage Paid',Updated=TO_TIMESTAMP('2021-05-26 17:42:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501606
+;
+
+-- 2021-05-26T14:42:56.915Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CPT - Carriage Paid',Updated=TO_TIMESTAMP('2021-05-26 17:42:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501606
+;
+
+-- 2021-05-26T14:43:32.792Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET Name='CIP - Carriage and Insurance Paid',Updated=TO_TIMESTAMP('2021-05-26 17:43:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501607
+;
+
+-- 2021-05-26T14:43:43.065Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CIP - Carriage and Insurance Paid',Updated=TO_TIMESTAMP('2021-05-26 17:43:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501607
+;
+
+-- 2021-05-26T14:45:01.972Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='DDP - Delivered Duty Paid',Updated=TO_TIMESTAMP('2021-05-26 17:45:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501612
+;
+
+-- 2021-05-26T14:45:08.591Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='DDP - Delivered Duty Paid',Updated=TO_TIMESTAMP('2021-05-26 17:45:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501612
+;
+
+-- 2021-05-26T14:45:56.880Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='FAS - Free Alongside Ship',Updated=TO_TIMESTAMP('2021-05-26 17:45:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501602
+;
+
+-- 2021-05-26T14:46:06.125Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='FAS - Free Alongside Ship',Updated=TO_TIMESTAMP('2021-05-26 17:46:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501602
+;
+
+-- 2021-05-26T14:46:30.739Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='FOB - Free on Board',Updated=TO_TIMESTAMP('2021-05-26 17:46:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501603
+;
+
+-- 2021-05-26T14:46:41.369Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='FOB - Free on Board',Updated=TO_TIMESTAMP('2021-05-26 17:46:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501603
+;
+
+-- 2021-05-26T14:47:29.250Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CFR - Cost and Freight',Updated=TO_TIMESTAMP('2021-05-26 17:47:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501604
+;
+
+-- 2021-05-26T14:47:37.772Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CFR - Cost and Freight',Updated=TO_TIMESTAMP('2021-05-26 17:47:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501604
+;
+
+-- 2021-05-26T14:48:04.550Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CIF - Cost Insurance and Freight',Updated=TO_TIMESTAMP('2021-05-26 17:48:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=501605
+;
+
+-- 2021-05-26T14:48:13.792Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='CIF - Cost Insurance and Freight',Updated=TO_TIMESTAMP('2021-05-26 17:48:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND AD_Ref_List_ID=501605
+;
+
