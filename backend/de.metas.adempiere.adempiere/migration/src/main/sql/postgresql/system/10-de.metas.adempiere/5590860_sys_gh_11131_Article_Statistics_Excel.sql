ba45a410c180b5d8fcb0ae9fc375098c8e56e050
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-06-02 18:08:03 +0300

    Product category parameter https://github.com/metasfresh/metasfresh/issues/11131

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5590860_sys_gh_11131_Article_Statistics_Excel.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5590860_sys_gh_11131_Article_Statistics_Excel.sql
@@ -626,3 +626,39 @@ DELETE FROM AD_Process_Para WHERE AD_Process_Para_ID=542001
 UPDATE AD_Process_Para SET DefaultValue='@#AD_Org_ID@', ReadOnlyLogic='',Updated=TO_TIMESTAMP('2021-05-21 19:41:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=541106
 ;
 
+
+
+
+
+
+
+
+
+
+-- 2021-06-02T14:56:47.010Z
+-- URL zum Konzept
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,453,0,584831,542020,30,'M_Product_Category_ID',TO_TIMESTAMP('2021-06-02 16:56:47','YYYY-MM-DD HH24:MI:SS'),100,'Kategorie eines Produktes','U',0,'Identifiziert die Kategorie zu der ein Produkt gehört. Produktkategorien werden für Preisfindung und Auswahl verwendet.','Y','N','Y','N','N','N','Produkt Kategorie',80,TO_TIMESTAMP('2021-06-02 16:56:47','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-06-02T14:56:47.011Z
+-- URL zum Konzept
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_Para_ID=542020 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2021-06-02T14:56:53.211Z
+-- URL zum Konzept
+UPDATE AD_Process_Para SET SeqNo=60,Updated=TO_TIMESTAMP('2021-06-02 16:56:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=542020
+;
+
+-- 2021-06-02T14:57:04.084Z
+-- URL zum Konzept
+UPDATE AD_Process_Para SET EntityType='D',Updated=TO_TIMESTAMP('2021-06-02 16:57:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=542020
+;
+
+
+
+
+
+
+
+
