db2e009a56e8b7f780a5ad11efb7b103fd7d2111
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-06-03 17:24:17 +0300

    #11227 Bank Account Invoice Auto Allocation Rules https://github.com/metasfresh/metasfresh/issues/11227

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_C_BP_BankAccount_InvoiceAutoAllocateRule.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_C_BP_BankAccount_InvoiceAutoAllocateRule.java
@@ -1,39 +1,189 @@
-/*
- * #%L
- * de.metas.adempiere.adempiere.base
- * %%
- * Copyright (C) 2021 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 package org.compiere.model;
 
-// TODO: create a real model. this is just prototyping
-public interface I_C_BP_BankAccount_InvoiceAutoAllocateRule
+import org.adempiere.model.ModelColumn;
+
+/** Generated Interface for C_BP_BankAccount_InvoiceAutoAllocateRule
+ *  @author metasfresh (generated) 
+ */
+@SuppressWarnings("unused")
+public interface I_C_BP_BankAccount_InvoiceAutoAllocateRule 
 {
+
 	String Table_Name = "C_BP_BankAccount_InvoiceAutoAllocateRule";
+
+//	/** AD_Table_ID=541673 */
+//	int Table_ID = org.compiere.model.MTable.getTable_ID(Table_Name);
+
+
+	/**
+	 * Get Client.
+	 * Client/Tenant for this installation.
+	 *
+	 * <br>Type: TableDir
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getAD_Client_ID();
+
+	String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/**
+	 * Set Organisation.
+	 * Organisational entity within client
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setAD_Org_ID (int AD_Org_ID);
+
+	/**
+	 * Get Organisation.
+	 * Organisational entity within client
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getAD_Org_ID();
+
+	String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/**
+	 * Set Partner Bank Account.
+	 * Bank Account of the Business Partner
+	 *
+	 * <br>Type: TableDir
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setC_BP_BankAccount_ID (int C_BP_BankAccount_ID);
+
+	/**
+	 * Get Partner Bank Account.
+	 * Bank Account of the Business Partner
+	 *
+	 * <br>Type: TableDir
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getC_BP_BankAccount_ID();
 
 	String COLUMNNAME_C_BP_BankAccount_ID = "C_BP_BankAccount_ID";
 
-	int getC_BP_BankAccount_ID();
+	/**
+	 * Set Invoice Auto allocation Restrictions.
+	 *
+	 * <br>Type: ID
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setC_BP_BankAccount_InvoiceAutoAllocateRule_ID (int C_BP_BankAccount_InvoiceAutoAllocateRule_ID);
 
-	void setC_BP_BankAccount_ID(int id);
+	/**
+	 * Get Invoice Auto allocation Restrictions.
+	 *
+	 * <br>Type: ID
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getC_BP_BankAccount_InvoiceAutoAllocateRule_ID();
 
+	ModelColumn<I_C_BP_BankAccount_InvoiceAutoAllocateRule, Object> COLUMN_C_BP_BankAccount_InvoiceAutoAllocateRule_ID = new ModelColumn<>(I_C_BP_BankAccount_InvoiceAutoAllocateRule.class, "C_BP_BankAccount_InvoiceAutoAllocateRule_ID", null);
+	String COLUMNNAME_C_BP_BankAccount_InvoiceAutoAllocateRule_ID = "C_BP_BankAccount_InvoiceAutoAllocateRule_ID";
+
+	/**
+	 * Set DocType Invoice.
+	 * Document type used for invoices generated from this sales document
+	 *
+	 * <br>Type: Table
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setC_DocTypeInvoice_ID (int C_DocTypeInvoice_ID);
+
+	/**
+	 * Get DocType Invoice.
+	 * Document type used for invoices generated from this sales document
+	 *
+	 * <br>Type: Table
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
 	int getC_DocTypeInvoice_ID();
 
-	void setC_DocTypeInvoice_ID(int id);
+	String COLUMNNAME_C_DocTypeInvoice_ID = "C_DocTypeInvoice_ID";
+
+	/**
+	 * Get Created.
+	 * Date this record was created
+	 *
+	 * <br>Type: DateTime
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	java.sql.Timestamp getCreated();
+
+	ModelColumn<I_C_BP_BankAccount_InvoiceAutoAllocateRule, Object> COLUMN_Created = new ModelColumn<>(I_C_BP_BankAccount_InvoiceAutoAllocateRule.class, "Created", null);
+	String COLUMNNAME_Created = "Created";
+
+	/**
+	 * Get Created By.
+	 * User who created this records
+	 *
+	 * <br>Type: Table
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getCreatedBy();
+
+	String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/**
+	 * Set Active.
+	 * The record is active in the system
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setIsActive (boolean IsActive);
+
+	/**
+	 * Get Active.
+	 * The record is active in the system
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	boolean isActive();
+
+	ModelColumn<I_C_BP_BankAccount_InvoiceAutoAllocateRule, Object> COLUMN_IsActive = new ModelColumn<>(I_C_BP_BankAccount_InvoiceAutoAllocateRule.class, "IsActive", null);
+	String COLUMNNAME_IsActive = "IsActive";
+
+	/**
+	 * Get Updated.
+	 * Date this record was updated
+	 *
+	 * <br>Type: DateTime
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	java.sql.Timestamp getUpdated();
+
+	ModelColumn<I_C_BP_BankAccount_InvoiceAutoAllocateRule, Object> COLUMN_Updated = new ModelColumn<>(I_C_BP_BankAccount_InvoiceAutoAllocateRule.class, "Updated", null);
+	String COLUMNNAME_Updated = "Updated";
+
+	/**
+	 * Get Updated By.
+	 * User who updated this records
+	 *
+	 * <br>Type: Table
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getUpdatedBy();
+
+	String COLUMNNAME_UpdatedBy = "UpdatedBy";
 }
