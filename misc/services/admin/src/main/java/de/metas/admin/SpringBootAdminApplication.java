36b0597275f9a94c4b6cef7a2329e5cecd15bc91
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-06-09 01:21:24 +0200

    cleanup

--- misc/services/admin/src/main/java/de/metas/admin/SpringBootAdminApplication.java
+++ misc/services/admin/src/main/java/de/metas/admin/SpringBootAdminApplication.java
@@ -1,12 +1,9 @@
 package de.metas.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import io.micrometer.core.lang.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
@@ -16,16 +13,5 @@ public class SpringBootAdminApplication
 	public static void main(final String[] args)
 	{
 		SpringApplication.run(SpringBootAdminApplication.class, args);
-	}
-
-	@Configuration
-	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter
-	{
-		@Override
-		protected void configure(@NonNull final HttpSecurity http) throws Exception
-		{
-			http.authorizeRequests().anyRequest().permitAll()
-					.and().csrf().disable();
-		}
 	}
 }
