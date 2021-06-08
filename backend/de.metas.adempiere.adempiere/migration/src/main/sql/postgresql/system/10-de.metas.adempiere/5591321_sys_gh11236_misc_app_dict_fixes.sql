7ad1a7f9269c5091b61ee199443a67c92c73c2e0
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-06-08 09:58:52 +0200

    move FK-constraint-drop to a dedicated file
    https://github.com/metasfresh/metasfresh/issues/11236
    (cherry picked from commit c693698fbda5e590538e6dc51cae47c68b8ce082)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5591321_sys_gh11236_misc_app_dict_fixes.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5591321_sys_gh11236_misc_app_dict_fixes.sql
@@ -1,7 +1,5 @@
 
 update AD_table set isview='Y' where tablename='C_BPartner_Adv_Search_v';
-update ad_column set ddl_noforeignkey='Y' where AD_Table_ID=get_table_id('api_request_audit_log');
-alter table api_request_audit_log DROP CONSTRAINT adissue_apirequestauditlog;
 
 delete from ad_ui_element where ad_field_id=(select ad_field_id from ad_field where ad_column_id=(select ad_column.ad_column_id from ad_column WHERE columnname = 'TrackingNo'  AND ad_table_id = get_table_id('DD_Order')));
 delete from ad_field where ad_column_id=(select ad_column_id from ad_column WHERE columnname = 'TrackingNo'  AND ad_table_id = get_table_id('DD_Order'));
