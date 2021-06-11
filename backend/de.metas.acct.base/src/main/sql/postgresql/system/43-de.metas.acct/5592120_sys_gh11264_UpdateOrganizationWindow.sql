a7acfb01d20e6f90a62d8baa3d1a4b1e7285c52c
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-06-11 10:46:39 +0300

    #11264 Tax changes https://github.com/metasfresh/metasfresh/issues/11264 - Hide Client_ID from Fiscal Representation Tab - Add EN Trl for Tax Rate Category

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5592120_sys_gh11264_UpdateOrganizationWindow.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5592120_sys_gh11264_UpdateOrganizationWindow.sql
@@ -158,6 +158,10 @@ UPDATE AD_UI_Element SET IsDisplayed='N',Updated=TO_TIMESTAMP('2021-06-10 15:29:
 UPDATE AD_UI_Element SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2021-06-10 15:30:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=584649
 ;
 
-
+-- Un display Client_ID from Fiscal Representation Tab Single view
+-- 2021-06-11T07:37:14.823Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayed='N',Updated=TO_TIMESTAMP('2021-06-11 10:37:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=584648
+;
 
 
--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5592130_sys_gh11264_UpdateTaxRateCategoryName.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5592130_sys_gh11264_UpdateTaxRateCategoryName.sql
@@ -14,6 +14,21 @@ UPDATE C_TaxCategory_Trl trl SET Description=NULL, Name='Normale MWSt'  WHERE C_
 UPDATE C_TaxCategory_Trl SET Name='Normale MWSt',Updated=TO_TIMESTAMP('2021-06-10 16:18:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_TaxCategory_ID=1000009
 ;
 
+-- Add EN Trl for Normal Tax Rate Category
+-- 2021-06-11T07:43:10.614Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET Name='Standard VAT',Updated=TO_TIMESTAMP('2021-06-11 10:43:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_TaxCategory_ID=1000009
+;
+
+-- 2021-06-11T07:43:16.455Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET Name='Standard VAT',Updated=TO_TIMESTAMP('2021-06-11 10:43:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND C_TaxCategory_ID=1000009
+;
+
+-- 2021-06-11T07:43:18.485Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-06-11 10:43:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND C_TaxCategory_ID=1000009
+;
 
 -- Update reduced tax rate name
 -- 2021-06-10T12:44:06.830Z
@@ -24,4 +39,25 @@ UPDATE C_TaxCategory SET Name='Reduzierte MWSt',Updated=TO_TIMESTAMP('2021-06-10
 -- 2021-06-10T12:44:06.833Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE C_TaxCategory_Trl trl SET Description=NULL, Name='Reduzierte MWSt'  WHERE C_TaxCategory_ID=1000010 AND ( trl.isTranslated = 'N'  OR  exists(select 1 from ad_language lang where lang.ad_language = trl.ad_language and lang.isBaseLanguage = 'Y') )
+;
+
+-- Add EN TRL for reduced tax rate category
+-- 2021-06-11T07:40:59.378Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET Name='Reduced VAT',Updated=TO_TIMESTAMP('2021-06-11 10:40:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND C_TaxCategory_ID=1000010
+;
+
+-- 2021-06-11T07:41:03.800Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-06-11 10:41:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_GB' AND C_TaxCategory_ID=1000010
+;
+
+-- 2021-06-11T07:41:26.067Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET Name='Reduced VAT',Updated=TO_TIMESTAMP('2021-06-11 10:41:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_TaxCategory_ID=1000010
+;
+
+-- 2021-06-11T07:41:26.252Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_TaxCategory_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-06-11 10:41:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_TaxCategory_ID=1000010
 ;
