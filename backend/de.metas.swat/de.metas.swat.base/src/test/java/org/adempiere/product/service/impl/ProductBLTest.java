4f4428b071c0901ca4aecf13c58838bdb0b64358
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-06-11 12:39:25 +0300

    Unit Tests Corrections
    https://github.com/metasfresh/metasfresh/issues/11267

--- backend/de.metas.swat/de.metas.swat.base/src/test/java/org/adempiere/product/service/impl/ProductBLTest.java
+++ backend/de.metas.swat/de.metas.swat.base/src/test/java/org/adempiere/product/service/impl/ProductBLTest.java
@@ -1,35 +1,10 @@
 package org.adempiere.product.service.impl;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-/*
- * #%L
- * de.metas.swat.base
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
-import java.util.Properties;
-
+import de.metas.product.IProductBL;
+import de.metas.util.Services;
 import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.ad.wrapper.POJOWrapper;
-import org.adempiere.mm.attributes.AttributeSetId;
-import org.adempiere.model.InterfaceWrapperHelper;
+import org.adempiere.mm.attributes.api.AttributeConstants;
 import org.adempiere.test.AdempiereTestHelper;
 import org.compiere.model.I_M_AttributeSet;
 import org.compiere.model.I_M_Product;
@@ -38,8 +13,11 @@ import org.compiere.util.Env;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import de.metas.product.IProductBL;
-import de.metas.util.Services;
+import java.util.Properties;
+
+import static org.adempiere.model.InterfaceWrapperHelper.create;
+import static org.adempiere.model.InterfaceWrapperHelper.save;
+import static org.assertj.core.api.Assertions.*;
 
 public class ProductBLTest
 {
@@ -69,31 +47,31 @@ public class ProductBLTest
 	public void testGetAttributeSet_Product()
 	{
 
-		as1 = InterfaceWrapperHelper.create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
-		InterfaceWrapperHelper.save(as1);
+		as1 = create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
+		save(as1);
 
-		product1 = InterfaceWrapperHelper.create(ctx, I_M_Product.class, ITrx.TRXNAME_None);
+		product1 = create(ctx, I_M_Product.class, ITrx.TRXNAME_None);
 		product1.setM_AttributeSet_ID(as1.getM_AttributeSet_ID());
-		InterfaceWrapperHelper.save(product1);
+		save(product1);
 
 		final int productAS_ID = Services.get(IProductBL.class).getAttributeSetId(product1).getRepoId();
 
-		assertThat(productAS_ID).isEqualTo(AttributeSetId.NONE);
+		assertThat(productAS_ID).isEqualTo(AttributeConstants.M_AttributeSet_ID_None);
 	}
 
 	@Test
 	public void testGetAttributeSet_ProductCategory()
 	{
-		as1 = InterfaceWrapperHelper.create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
-		InterfaceWrapperHelper.save(as1);
+		as1 = create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
+		save(as1);
 
-		category1 = InterfaceWrapperHelper.create(ctx, I_M_Product_Category.class, ITrx.TRXNAME_None);
+		category1 = create(ctx, I_M_Product_Category.class, ITrx.TRXNAME_None);
 		category1.setM_AttributeSet(as1);
-		InterfaceWrapperHelper.save(category1);
+		save(category1);
 
-		product1 = InterfaceWrapperHelper.create(ctx, I_M_Product.class, ITrx.TRXNAME_None);
+		product1 = create(ctx, I_M_Product.class, ITrx.TRXNAME_None);
 		product1.setM_Product_Category_ID(category1.getM_Product_Category_ID());
-		InterfaceWrapperHelper.save(product1);
+		save(product1);
 
 		final int productAS_ID = Services.get(IProductBL.class).getAttributeSetId(product1).getRepoId();
 
@@ -107,20 +85,20 @@ public class ProductBLTest
 	@Test
 	public void testGetAttributeSet_ProductCategory_And_Product()
 	{
-		as1 = InterfaceWrapperHelper.create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
-		InterfaceWrapperHelper.save(as1);
+		as1 = create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
+		save(as1);
 
-		final I_M_AttributeSet as2 = InterfaceWrapperHelper.create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
-		InterfaceWrapperHelper.save(as2);
+		final I_M_AttributeSet as2 = create(ctx, I_M_AttributeSet.class, ITrx.TRXNAME_None);
+		save(as2);
 
-		category1 = InterfaceWrapperHelper.create(ctx, I_M_Product_Category.class, ITrx.TRXNAME_None);
+		category1 = create(ctx, I_M_Product_Category.class, ITrx.TRXNAME_None);
 		category1.setM_AttributeSet(as1);
-		InterfaceWrapperHelper.save(category1);
+		save(category1);
 
-		product1 = InterfaceWrapperHelper.create(ctx, I_M_Product.class, ITrx.TRXNAME_None);
+		product1 = create(ctx, I_M_Product.class, ITrx.TRXNAME_None);
 		product1.setM_AttributeSet_ID(as2.getM_AttributeSet_ID());
 		product1.setM_Product_Category_ID(category1.getM_Product_Category_ID());
-		InterfaceWrapperHelper.save(product1);
+		save(product1);
 
 		final int productAS_ID = Services.get(IProductBL.class).getAttributeSetId(product1).getRepoId();
 
