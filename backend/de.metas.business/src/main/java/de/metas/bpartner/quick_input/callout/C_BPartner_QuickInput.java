e13f144d8e15ae06dcc474ea1b9a3a1f8b6586f1
Teo Sarca <teo.sarca@metasfresh.com>
2021-06-12 13:15:04 +0300

    C_BPartner_QuickInput callout: set AD_Org_ID from Postal

--- backend/de.metas.business/src/main/java/de/metas/bpartner/quick_input/callout/C_BPartner_QuickInput.java
+++ backend/de.metas.business/src/main/java/de/metas/bpartner/quick_input/callout/C_BPartner_QuickInput.java
@@ -23,20 +23,28 @@
 package de.metas.bpartner.quick_input.callout;
 
 import de.metas.bpartner.quick_input.service.BPartnerQuickInputService;
+import de.metas.location.ILocationDAO;
+import de.metas.location.LocationId;
+import de.metas.location.PostalId;
+import de.metas.organization.OrgId;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.callout.annotations.Callout;
 import org.adempiere.ad.callout.annotations.CalloutMethod;
 import org.adempiere.ad.callout.spi.IProgramaticCalloutProvider;
 import org.compiere.model.I_C_BPartner_QuickInput;
+import org.compiere.model.I_C_Location;
+import org.compiere.model.I_C_Postal;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Nullable;
 import javax.annotation.PostConstruct;
 
 @Component
 @Callout(I_C_BPartner_QuickInput.class)
 public class C_BPartner_QuickInput
 {
+	private final ILocationDAO locationDAO = Services.get(ILocationDAO.class);
 	private final BPartnerQuickInputService bpartnerQuickInputService;
 
 	public C_BPartner_QuickInput(
@@ -56,5 +64,38 @@ public class C_BPartner_QuickInput
 	public void onIsCompanyFlagChanged(@NonNull final I_C_BPartner_QuickInput record)
 	{
 		bpartnerQuickInputService.updateNameAndGreetingNoSave(record);
+	}
+
+	@CalloutMethod(columnNames = I_C_BPartner_QuickInput.COLUMNNAME_C_Location_ID)
+	public void onLocationChanged(@NonNull final I_C_BPartner_QuickInput record)
+	{
+		final OrgId orgInChangeId = getOrgInChangeViaLocationPostalCode(record);
+		if (orgInChangeId == null)
+		{
+			return;
+		}
+
+		record.setAD_Org_ID(orgInChangeId.getRepoId());
+	}
+
+	@Nullable
+	private OrgId getOrgInChangeViaLocationPostalCode(final @NonNull I_C_BPartner_QuickInput record)
+	{
+		final LocationId locationId = LocationId.ofRepoIdOrNull(record.getC_Location_ID());
+		if (locationId == null)
+		{
+			return null;
+		}
+
+		final I_C_Location locationRecord = locationDAO.getById(locationId);
+		final PostalId postalId = PostalId.ofRepoIdOrNull(locationRecord.getC_Postal_ID());
+		if (postalId == null)
+		{
+			return null;
+		}
+
+		final I_C_Postal postalRecord = locationDAO.getPostalById(postalId);
+		final OrgId orgInChargeId = OrgId.ofRepoId(postalRecord.getAD_Org_InCharge_ID());
+		return orgInChargeId.isRegular() ? orgInChargeId : null;
 	}
 }
