91f44a2fa1d00a1bc0f1b6c5f4cd2fe88c782dbe
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2021-06-14 06:27:59 +0200

    Update backend/de.metas.vertical.healthcare.alberta/src/main/sql/postgresql/system/80-de.metas.vertical.healthcare.alberta/5591390_sys_gh11262_C_BPartner_Sync_Alberta_Institutions.sql

--- backend/de.metas.vertical.healthcare.alberta/src/main/sql/postgresql/system/80-de.metas.vertical.healthcare.alberta/5591390_sys_gh11262_C_BPartner_Sync_Alberta_Institutions.sql
+++ backend/de.metas.vertical.healthcare.alberta/src/main/sql/postgresql/system/80-de.metas.vertical.healthcare.alberta/5591390_sys_gh11262_C_BPartner_Sync_Alberta_Institutions.sql
@@ -48,28 +48,6 @@ INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,
 INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_Para_ID=542022 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
 ;
 
-/*
- * #%L
- * de.metas.vertical.healthcare.alberta
- * %%
- * Copyright (C) 2021 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- 2021-06-08T15:19:30.512Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,578730,0,584838,542023,19,'ExternalSystem_Config_Alberta_ID',TO_TIMESTAMP('2021-06-08 18:19:30','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.externalsystem',7,'Y','N','Y','N','Y','N','ExternalSystem_Config_Alberta',20,TO_TIMESTAMP('2021-06-08 18:19:30','YYYY-MM-DD HH24:MI:SS'),100)
@@ -114,4 +92,3 @@ UPDATE AD_Column SET IsIdentifier='Y', SeqNo=10,Updated=TO_TIMESTAMP('2021-06-08
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO t_alter_column values('externalsystem_config_alberta','ExternalSystemValue','VARCHAR(255)',null,null)
 ;
-
