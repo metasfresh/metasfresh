f919e5f0a857f0ad1c326c9a451eebd7680a11b6
Adrian <adrian.stefan@metasfresh.com>
2021-06-18 18:14:37 +0300

    #gh11330: fixed trls for TypeOfDestCountry values

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5589440_sys_gh11130_C_Tax_and_C_Tax_Category_column_changes.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5589440_sys_gh11130_C_Tax_and_C_Tax_Category_column_changes.sql
@@ -605,27 +605,6 @@ WHERE C_Country_ID <> To_Country_ID
   AND isToEuLocation <> 'Y'
 ;
 
--- 2021-05-21T15:16:16.860Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Ref_List
-SET Name='Domestic', Updated=TO_TIMESTAMP('2021-05-21 18:16:16', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
-WHERE AD_Ref_List_ID = 542610
-;
-
--- 2021-05-21T15:16:39.469Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Ref_List
-SET Name='EU-foreign', Updated=TO_TIMESTAMP('2021-05-21 18:16:39', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
-WHERE AD_Ref_List_ID = 542611
-;
-
--- 2021-05-21T15:22:08.613Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Ref_List
-SET Name='Non-EU country', Updated=TO_TIMESTAMP('2021-05-21 18:22:08', 'YYYY-MM-DD HH24:MI:SS'), UpdatedBy=100
-WHERE AD_Ref_List_ID = 542612
-;
-
 -- 2021-05-25T10:05:44.497Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Field (AD_Client_ID, AD_Column_ID, AD_Field_ID, AD_Org_ID, AD_Tab_ID, ColumnDisplayLength, Created, CreatedBy, Description, DisplayLength, EntityType, IncludedTabHeight, IsActive, IsDisplayed, IsDisplayedGrid, IsEncrypted, IsFieldOnly, IsHeading, IsReadOnly, IsSameLine, Name, SeqNo, SeqNoGrid, SortNo, SpanX, SpanY, Updated, UpdatedBy)
