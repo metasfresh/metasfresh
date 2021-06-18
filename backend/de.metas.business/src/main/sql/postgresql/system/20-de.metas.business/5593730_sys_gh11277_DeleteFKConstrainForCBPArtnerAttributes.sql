551c05a52df1c9f5831ef36fb27f5d30782fb16f
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-06-18 17:30:06 +0300

    #11277 Introduce C_BPartner_Attribute2, C_BPartner_Attribute3, C_BPartner_Attribute4, C_BPartner_Attribute5 Tables and Ref Lists https://github.com/metasfresh/metasfresh/issues/11277

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5593730_sys_gh11277_DeleteFKConstrainForCBPArtnerAttributes.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5593730_sys_gh11277_DeleteFKConstrainForCBPArtnerAttributes.sql
@@ -64,3 +64,94 @@ ALTER TABLE c_bpartner_attribute5
                 REFERENCES c_bpartner
                 ON DELETE CASCADE
 ;
+
+ALTER TABLE c_bpartner_contact_quickinput_attributes
+    DROP
+        CONSTRAINT cbpartnercontactquickinput_cbpartnercontactquickinputattributes
+;
+
+
+
+ALTER TABLE c_bpartner_contact_quickinput_attributes
+    ADD
+        CONSTRAINT cbpartnercontactquickinput_cbpartnercontactquickinputattributes
+            FOREIGN KEY (c_bpartner_contact_quickinput_ID)
+                REFERENCES c_bpartner_contact_quickinput
+                ON DELETE CASCADE
+;
+
+ALTER TABLE c_bpartner_quickinput_attributes
+    DROP
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes
+;
+
+
+
+ALTER TABLE c_bpartner_quickinput_attributes
+    ADD
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes
+            FOREIGN KEY (c_bpartner_quickinput_id)
+                REFERENCES c_bpartner_quickinput
+                ON DELETE CASCADE
+;
+
+
+ALTER TABLE c_bpartner_quickinput_attributes2
+    DROP
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes2
+;
+
+
+
+ALTER TABLE c_bpartner_quickinput_attributes2
+    ADD
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes2
+            FOREIGN KEY (c_bpartner_quickinput_id)
+                REFERENCES c_bpartner_quickinput
+                ON DELETE CASCADE
+;
+
+ALTER TABLE c_bpartner_quickinput_attributes3
+    DROP
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes3
+;
+
+
+
+ALTER TABLE c_bpartner_quickinput_attributes3
+    ADD
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes3
+            FOREIGN KEY (c_bpartner_quickinput_id)
+                REFERENCES c_bpartner_quickinput
+                ON DELETE CASCADE
+;
+
+ALTER TABLE c_bpartner_quickinput_attributes4
+    DROP
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes4
+;
+
+
+
+ALTER TABLE c_bpartner_quickinput_attributes4
+    ADD
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes4
+            FOREIGN KEY (c_bpartner_quickinput_id)
+                REFERENCES c_bpartner_quickinput
+                ON DELETE CASCADE
+;
+
+ALTER TABLE c_bpartner_quickinput_attributes5
+    DROP
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes5
+;
+
+
+
+ALTER TABLE c_bpartner_quickinput_attributes5
+    ADD
+        CONSTRAINT cbpartnerquickinput_cbpartnerquickinputattributes5
+            FOREIGN KEY (c_bpartner_quickinput_id)
+                REFERENCES c_bpartner_quickinput
+                ON DELETE CASCADE
+;
