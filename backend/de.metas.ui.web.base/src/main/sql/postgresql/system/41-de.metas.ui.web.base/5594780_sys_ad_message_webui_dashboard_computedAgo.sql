d9280756ef87d27c0194f24ff6d28f3f11cd45a5
Teo Sarca <teo.sarca@metasfresh.com>
2021-06-25 10:51:09 +0300

    webui.dashboard.computedAgo message trl

--- backend/de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5594780_sys_ad_message_webui_dashboard_computedAgo.sql
+++ backend/de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5594780_sys_ad_message_webui_dashboard_computedAgo.sql
@@ -1,25 +1,3 @@
-/*
- * #%L
- * de.metas.ui.web.base
- * %%
- * Copyright (C) 2021 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- 2021-06-25T07:41:32.059Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545039,0,TO_TIMESTAMP('2021-06-25 10:41:31','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Computed %(duration) ago','I',TO_TIMESTAMP('2021-06-25 10:41:31','YYYY-MM-DD HH24:MI:SS'),100,'webui.dashboard.computedAgo')
@@ -28,5 +6,25 @@ INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,E
 -- 2021-06-25T07:41:32.065Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545039 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2021-06-25T07:50:14.467Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='Berechnet vor %(duration)',Updated=TO_TIMESTAMP('2021-06-25 10:50:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545039
+;
+
+-- 2021-06-25T07:50:19.401Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Berechnet vor %(duration)',Updated=TO_TIMESTAMP('2021-06-25 10:50:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545039
+;
+
+-- 2021-06-25T07:50:24.501Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-06-25 10:50:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545039
+;
+
+-- 2021-06-25T07:50:29.383Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Berechnet vor %(duration)',Updated=TO_TIMESTAMP('2021-06-25 10:50:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545039
 ;
 
