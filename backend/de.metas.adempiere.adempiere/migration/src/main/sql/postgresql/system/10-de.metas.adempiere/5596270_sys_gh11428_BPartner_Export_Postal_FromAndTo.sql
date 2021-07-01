b4a407e873b4f69d4067770e90c28120ad40b48a
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-07-01 17:36:15 +0300

    BP Export Postal from-to filter https://github.com/metasfresh/metasfresh/issues/11428

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5596270_sys_gh11428_BPartner_Export_Postal_FromAndTo.sql
@@ -0,0 +1,40 @@
+-- 2021-07-01T14:28:56.778Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545043,0,TO_TIMESTAMP('2021-07-01 17:28:56','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Postal From','I',TO_TIMESTAMP('2021-07-01 17:28:56','YYYY-MM-DD HH24:MI:SS'),100,'PostalFrom')
+;
+
+-- 2021-07-01T14:28:56.794Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545043 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2021-07-01T14:29:19.334Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='PLZ von',Updated=TO_TIMESTAMP('2021-07-01 17:29:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545043
+;
+
+-- 2021-07-01T14:29:28.004Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='PLZ von',Updated=TO_TIMESTAMP('2021-07-01 17:29:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545043
+;
+
+-- 2021-07-01T14:29:41.079Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545044,0,TO_TIMESTAMP('2021-07-01 17:29:40','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Postal To','I',TO_TIMESTAMP('2021-07-01 17:29:40','YYYY-MM-DD HH24:MI:SS'),100,'PostalTo')
+;
+
+-- 2021-07-01T14:29:41.082Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Message_ID=545044 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- 2021-07-01T14:29:55.188Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Postal Bis',Updated=TO_TIMESTAMP('2021-07-01 17:29:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:30:03.464Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Postal Bis',Updated=TO_TIMESTAMP('2021-07-01 17:30:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545044
+;
+
