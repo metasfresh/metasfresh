6ac27fd54e3d46dec3dd1508192ed7764bacf7a3
metas-rc <ruxandra.craciunescu@metasfresh.com>
2021-07-01 17:56:49 +0300

    trl https://github.com/metasfresh/metasfresh/issues/11428

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5596270_sys_gh11428_BPartner_Export_Postal_FromAndTo.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5596270_sys_gh11428_BPartner_Export_Postal_FromAndTo.sql
@@ -38,3 +38,62 @@ UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Postal Bis',Updated=TO_TIME
 UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Postal Bis',Updated=TO_TIMESTAMP('2021-07-01 17:30:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545044
 ;
 
+
+
+
+
+-- 2021-07-01T14:54:55.627Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='PLZ von',Updated=TO_TIMESTAMP('2021-07-01 17:54:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545043
+;
+
+-- 2021-07-01T14:55:15.056Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='PLZ Bis',Updated=TO_TIMESTAMP('2021-07-01 17:55:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:17.942Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='PLZ Bis',Updated=TO_TIMESTAMP('2021-07-01 17:55:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:25.886Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='PLZ Bis',Updated=TO_TIMESTAMP('2021-07-01 17:55:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:37.788Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message SET MsgText='PLZ bis',Updated=TO_TIMESTAMP('2021-07-01 17:55:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:43.211Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='PLZ bis',Updated=TO_TIMESTAMP('2021-07-01 17:55:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:48.124Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='Postal to',Updated=TO_TIMESTAMP('2021-07-01 17:55:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:50.717Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='Postal to',Updated=TO_TIMESTAMP('2021-07-01 17:55:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:55:54.624Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='PLZ bis',Updated=TO_TIMESTAMP('2021-07-01 17:55:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545044
+;
+
+-- 2021-07-01T14:56:06.411Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='Postal from',Updated=TO_TIMESTAMP('2021-07-01 17:56:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545043
+;
+
+-- 2021-07-01T14:56:09.277Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Message_Trl SET MsgText='Postal from',Updated=TO_TIMESTAMP('2021-07-01 17:56:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Message_ID=545043
+;
+
