ba77997b399a6b8f53cc8df2f7af4af9f274b0e0
Adrian <adrian.stefan@metasfresh.com>
2021-07-06 09:01:28 +0300

    #gh11426: added AD_SQLColumn_SourceTableColumn entry

---
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5596730_sys_gh11426_add_SourceTableColumn_for_attachment.sql
@@ -0,0 +1,5 @@
+-- 2021-07-06T05:38:03.416Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_SQLColumn_SourceTableColumn (AD_Client_ID,AD_Column_ID,AD_Org_ID,AD_SQLColumn_SourceTableColumn_ID,AD_Table_ID,Created,CreatedBy,FetchTargetRecordsMethod,IsActive,Link_Column_ID,Source_Table_ID,Updated,UpdatedBy) VALUES (0,563224,0,540039,541144,TO_TIMESTAMP('2021-07-06 08:38:03','YYYY-MM-DD HH24:MI:SS'),100,'L','Y',557027,540833,TO_TIMESTAMP('2021-07-06 08:38:03','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
