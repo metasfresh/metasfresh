a34c98494ac9300a7fff4c09adbf047e353b9360
fp-ps <fp-ps@pevesoft.ro>
2021-07-06 15:18:17 +0300

    refs:https://github.com/metasfresh/metasfresh/issues/11453

---
+++ backend/de.metas.externalsystem/src/main/java/de/metas/externalsystem/ebay/ExternalSystemEbayConfig.java
@@ -0,0 +1,73 @@
+/*
+ * #%L
+ * de.metas.externalsystem
+ * %%
+ * Copyright (C) 2021 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.externalsystem.ebay;
+
+import de.metas.externalsystem.ExternalSystemParentConfigId;
+import de.metas.externalsystem.IExternalSystemChildConfig;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+public class ExternalSystemEbayConfig implements IExternalSystemChildConfig
+{
+	@NonNull
+	ExternalSystemEbayConfigId id;
+	@NonNull
+	ExternalSystemParentConfigId parentId;
+	@NonNull
+	String appId;
+	@NonNull
+	String certId;
+	@NonNull
+	String devId;
+	@NonNull
+	String redirectUrl;
+	@NonNull
+	ApiMode apiMode;
+
+	@Builder
+	public ExternalSystemEbayConfig(
+			@NonNull final ExternalSystemEbayConfigId id,
+			@NonNull final ExternalSystemParentConfigId parentId,
+			@NonNull final String appId,
+			@NonNull final String certId,
+			@NonNull final String devId,
+			@NonNull final String redirectUrl,
+			@NonNull final ApiMode apiMode)
+	{
+		this.id = id;
+		this.parentId = parentId;
+		this.appId = appId;
+		this.certId = certId;
+		this.devId = devId;
+		this.redirectUrl = redirectUrl;
+		this.apiMode = apiMode;
+	}
+
+	@NonNull
+	public static ExternalSystemEbayConfig cast(@NonNull final IExternalSystemChildConfig childConfig)
+	{
+		return (ExternalSystemEbayConfig)childConfig;
+	}
+}
