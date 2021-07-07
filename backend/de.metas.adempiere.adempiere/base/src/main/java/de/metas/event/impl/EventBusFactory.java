07c5e805f236e1ceba119bc8ed8f929dcd61f2ff
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-07-07 09:47:02 +0200

    after merge compile fix

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/event/impl/EventBusFactory.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/event/impl/EventBusFactory.java
@@ -37,6 +37,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class EventBusFactory implements IEventBusFactory
