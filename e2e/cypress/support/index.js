6ab8fee11cd9d5661163edc65e15d748661ff716
siemiatj <kuba@saskla.cz>
2021-07-14 15:49:55 +0200

    #11469 remove `languageSpecific` explicit wait as it should be handled by the login

--- e2e/cypress/support/index.js
+++ e2e/cypress/support/index.js
@@ -46,7 +46,7 @@ before(function () {
 
   cy.loginViaAPI().then(() => {
     cy.log('logged in successfully');
-  }, { timeout: 10000 });
+  }, { timeout: 20000 });
 
   // const autoLogin = function () {
   //   return cy.loginViaForm();
