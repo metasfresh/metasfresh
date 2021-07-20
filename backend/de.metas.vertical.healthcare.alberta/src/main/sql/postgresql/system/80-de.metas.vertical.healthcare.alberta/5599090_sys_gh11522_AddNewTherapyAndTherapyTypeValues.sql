397a1a36b794407813797a080c7b2d8b2b7aa0ea
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-07-20 11:12:44 +0300

    #11522 New Values for Alberta Therapy and Alberta Therapy Type https://github.com/metasfresh/metasfresh/issues/11522

--- backend/de.metas.vertical.healthcare.alberta/src/main/sql/postgresql/system/80-de.metas.vertical.healthcare.alberta/5599090_sys_gh11522_AddNewTherapyAndTherapyTypeValues.sql
+++ backend/de.metas.vertical.healthcare.alberta/src/main/sql/postgresql/system/80-de.metas.vertical.healthcare.alberta/5599090_sys_gh11522_AddNewTherapyAndTherapyTypeValues.sql
@@ -880,3 +880,8 @@ INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,C
 INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Ref_List_ID=542826 AND NOT EXISTS (SELECT 1 FROM AD_Ref_List_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Ref_List_ID=t.AD_Ref_List_ID)
 ;
 
+-- 2021-07-20T08:11:44.602Z
+-- URL zum Konzept
+UPDATE AD_Ref_List SET EntityType='D',Updated=TO_TIMESTAMP('2021-07-20 11:11:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=542802
+;
+
