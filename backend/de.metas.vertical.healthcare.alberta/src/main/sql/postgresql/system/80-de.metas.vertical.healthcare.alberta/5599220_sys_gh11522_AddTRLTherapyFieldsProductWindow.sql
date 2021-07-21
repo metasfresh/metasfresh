acef8c0c3f78a2872736b403568bf0c2822724bb
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-07-21 11:00:15 +0300

    #11522 New Values for Alberta Therapy and Alberta Therapy Type https://github.com/metasfresh/metasfresh/issues/11522

---
+++ backend/de.metas.vertical.healthcare.alberta/src/main/sql/postgresql/system/80-de.metas.vertical.healthcare.alberta/5599220_sys_gh11522_AddTRLTherapyFieldsProductWindow.sql
@@ -0,0 +1,10 @@
+-- 2021-07-21T07:56:31.916Z
+-- URL zum Konzept
+UPDATE AD_UI_Element SET Name='Therapie',Updated=TO_TIMESTAMP('2021-07-21 10:56:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=579773
+;
+
+-- 2021-07-21T07:57:43.859Z
+-- URL zum Konzept
+UPDATE AD_UI_Element SET Name='Abrechenbare Therapie',Updated=TO_TIMESTAMP('2021-07-21 10:57:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=579777
+;
+
