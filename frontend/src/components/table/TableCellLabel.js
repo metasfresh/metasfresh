7dbefc36bee11af8381ec59fbc5683db04adb802
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-07-27 14:31:27 +0300

    Show more feature(tooltip) for table cell rendered labels

--- frontend/src/components/table/TableCellLabel.js
+++ frontend/src/components/table/TableCellLabel.js
@@ -5,18 +5,38 @@ import PropTypes from 'prop-types';
 class TableCellLabel extends PureComponent {
   render() {
     const { tableCellData, rowId } = this.props;
+    const tableCellValues = tableCellData.value.values;
+    const commaSeparatedList =
+      tableCellValues.length > 1
+        ? tableCellValues.map((item) => item.caption).join(', ')
+        : '';
+
     return (
-      <React.Fragment>
-        {tableCellData &&
-          tableCellData.value.values.map((item, index) => (
+      <div>
+        {tableCellData && tableCellValues[0] && (
+          <div
+            className="table-cell-label-wrapper"
+            key={`${tableCellData.field}_${rowId}_0`}
+          >
             <Label
-              key={`${tableCellData.field}_${rowId}_${index}`}
-              label={item}
+              label={tableCellData.value.values[0]}
               readonly={true}
               hideCloseIcon={true}
             />
-          ))}
-      </React.Fragment>
+          </div>
+        )}
+
+        {tableCellData && tableCellValues.length > 1 && (
+          <div
+            data-toggle="tooltip"
+            data-placement="bottom"
+            title={commaSeparatedList}
+            className="table-cell-label-show-more"
+          >
+            ...
+          </div>
+        )}
+      </div>
     );
   }
 }
