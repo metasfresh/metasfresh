b935d4f71f0fc339f04558959171888560a818f0
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-08-19 15:23:14 +0300

    #11636 Modification to window Ingredients and Change Product Specification Report https://github.com/metasfresh/metasfresh/issues/11636 - modify product_specification view sql

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/views/product_specifications_v.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/views/product_specifications_v.sql
@@ -1,7 +1,7 @@
 --DROP VIEW "de.metas.fresh".product_specifications_v;
 
 CREATE OR REPLACE VIEW "de.metas.fresh".product_specifications_v AS
-  select
+select
     p.Name                                                  as productName,
     p.CustomerLabelName,
     p.additional_produktinfos,
@@ -17,26 +17,20 @@ CREATE OR REPLACE VIEW "de.metas.fresh".product_specifications_v AS
     nf.Name                                                 as nutritionName,
     pn.SeqNo                                                as NutritionSeqNo,
     pn.nutritionqty,
-    coalesce(NULLIF(TRIM(bomProduct.CustomerLabelName),''), bomProduct.Name) as componentName,
-    round(bomLine.qtybatch, 2)                              as qtybatch,
-    bomLine.componenttype,
-    bom_pc.ispackagingmaterial,
-    bomProduct.ingredients                                  as componentIngredients,
-    bp.companyname,
-    bpl.address,
+    pi.qty                              as qtybatch,
+    i.name as componentName,
+    (select name from m_product where m_product_id = i.m_product_id) as componentIngredients,
     p.M_product_ID
-  from m_product p
-    left outer join ad_orginfo oi on p.ad_org_id = oi.ad_org_id
-    left outer join C_BPartner_Location bpl on oi.orgbp_location_id = bpl.c_bpartner_location_id
-    left outer join C_BPartner bp on bpl.C_BPartner_ID = bp.C_BPartner_ID
-    left outer join c_location l on bpl.c_location_id = l.c_location_id
-    left outer join c_country c on l.c_country_id = c.c_country_id
-    left outer join M_Product_Allergen pa on pa.M_product_ID = p.M_product_ID
-    left outer join M_Allergen a on a.m_allergen_id = pa.m_allergen_id
-    left outer join M_Nutrition_Fact nf on 1 = 1
-    left outer join M_Product_Nutrition pn
-      on pn.M_product_ID = p.M_product_ID and nf.M_Nutrition_Fact_ID = pn.M_Nutrition_Fact_ID
-    left outer join PP_Product_BOM bom on bom.M_Product_ID = p.m_product_id
-    left outer join PP_Product_BOMLine bomLine on bomLine.pp_product_bom_id = bom.pp_product_bom_id and bomLine.isActive='Y'
-    left outer join m_product bomProduct on bomLine.m_product_id = bomProduct.m_product_id
-    left outer join m_product_category bom_pc on bomProduct.m_product_category_id = bom_pc.m_product_category_id;
+from m_product p
+         left outer join ad_orginfo oi on p.ad_org_id = oi.ad_org_id
+         left outer join C_BPartner_Location bpl on oi.orgbp_location_id = bpl.c_bpartner_location_id
+         left outer join C_BPartner bp on bpl.C_BPartner_ID = bp.C_BPartner_ID
+         left outer join c_location l on bpl.c_location_id = l.c_location_id
+         left outer join c_country c on l.c_country_id = c.c_country_id
+         left outer join M_Product_Allergen pa on pa.M_product_ID = p.M_product_ID
+         left outer join M_Allergen a on a.m_allergen_id = pa.m_allergen_id
+         left outer join M_Nutrition_Fact nf on 1 = 1
+         left outer join M_Product_Nutrition pn
+                         on pn.M_product_ID = p.M_product_ID and nf.M_Nutrition_Fact_ID = pn.M_Nutrition_Fact_ID
+         left outer join m_product_ingredients pi on pi.m_product_id = p.m_product_id
+         left outer JOIN m_ingredients i on pi.m_ingredients_id = i.m_ingredients_id;
