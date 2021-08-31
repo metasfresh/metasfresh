d9e0fb059243042e67c408a6695cba902978e10b
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-08-31 13:09:44 +0300

    #11654 Create Occupation Hierarchy tables and Validation Rules https://github.com/metasfresh/metasfresh/issues/11654

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5601910_sys_gh11654_OccupationValidationRules.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5601910_sys_gh11654_OccupationValidationRules.sql
@@ -139,3 +139,107 @@ UPDATE AD_Column SET AD_Val_Rule_ID=540553,Updated=TO_TIMESTAMP('2021-08-25 16:4
 -- URL zum Konzept
 UPDATE AD_Val_Rule SET Code='crm_occupation.jobType = ''Z'' and exists(select 1 from ad_user_occupation_specialization s where s.ad_user_id = @AD_User_ID@ and crm_occupation.crm_occupation_parent_id = s.crm_occupation_id)',Updated=TO_TIMESTAMP('2021-08-25 16:46:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=540553
 ;
+
+
+
+-- Add dependency to validation rules
+-- 2021-08-30T12:45:36.746Z
+-- URL zum Konzept
+INSERT INTO AD_Val_Rule_Dep (AD_Client_ID,AD_Org_ID,AD_Table_ID,AD_Val_Rule_Dep_ID,AD_Val_Rule_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,0,541772,540007,540552,TO_TIMESTAMP('2021-08-30 14:45:36','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2021-08-30 14:45:36','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2021-08-30T12:50:38.749Z
+-- URL zum Konzept
+INSERT INTO AD_Val_Rule_Dep (AD_Client_ID,AD_Org_ID,AD_Table_ID,AD_Val_Rule_Dep_ID,AD_Val_Rule_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (0,0,541774,540008,540553,TO_TIMESTAMP('2021-08-30 14:50:38','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2021-08-30 14:50:38','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Rename window
+-- 2021-08-30T13:08:01.076Z
+-- URL zum Konzept
+UPDATE AD_Element_Trl SET Name='Fachrichtung', PrintName='Fachrichtung',Updated=TO_TIMESTAMP('2021-08-30 16:08:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=579572 AND AD_Language='de_CH'
+;
+
+-- 2021-08-30T13:08:01.314Z
+-- URL zum Konzept
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579572,'de_CH')
+;
+
+-- 2021-08-30T13:08:06.593Z
+-- URL zum Konzept
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2021-08-30 16:08:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=579572 AND AD_Language='de_CH'
+;
+
+-- 2021-08-30T13:08:06.638Z
+-- URL zum Konzept
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579572,'de_CH')
+;
+
+-- 2021-08-30T13:09:12.398Z
+-- URL zum Konzept
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Fachrichtung', PrintName='Fachrichtung',Updated=TO_TIMESTAMP('2021-08-30 16:09:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=579572 AND AD_Language='de_DE'
+;
+
+-- 2021-08-30T13:09:12.439Z
+-- URL zum Konzept
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579572,'de_DE')
+;
+
+-- 2021-08-30T13:09:12.539Z
+-- URL zum Konzept
+/* DDL */  select update_ad_element_on_ad_element_trl_update(579572,'de_DE')
+;
+
+-- 2021-08-30T13:09:12.578Z
+-- URL zum Konzept
+UPDATE AD_Column SET ColumnName=NULL, Name='Fachrichtung', Description=NULL, Help=NULL WHERE AD_Element_ID=579572
+;
+
+-- 2021-08-30T13:09:12.615Z
+-- URL zum Konzept
+UPDATE AD_Process_Para SET ColumnName=NULL, Name='Fachrichtung', Description=NULL, Help=NULL WHERE AD_Element_ID=579572 AND IsCentrallyMaintained='Y'
+;
+
+-- 2021-08-30T13:09:12.654Z
+-- URL zum Konzept
+UPDATE AD_Field SET Name='Fachrichtung', Description=NULL, Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=579572) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 579572)
+;
+
+-- 2021-08-30T13:09:12.717Z
+-- URL zum Konzept
+UPDATE AD_PrintFormatItem pi SET PrintName='Fachrichtung', Name='Fachrichtung' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c  WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=579572)
+;
+
+-- 2021-08-30T13:09:12.768Z
+-- URL zum Konzept
+UPDATE AD_Tab SET Name='Fachrichtung', Description=NULL, Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 579572
+;
+
+-- 2021-08-30T13:09:12.816Z
+-- URL zum Konzept
+UPDATE AD_WINDOW SET Name='Fachrichtung', Description=NULL, Help=NULL WHERE AD_Element_ID = 579572
+;
+
+-- 2021-08-30T13:09:12.860Z
+-- URL zum Konzept
+UPDATE AD_Menu SET   Name = 'Fachrichtung', Description = NULL, WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 579572
+;
+
+-- 2021-08-30T13:09:27.186Z
+-- URL zum Konzept
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Speciality', PrintName='Speciality',Updated=TO_TIMESTAMP('2021-08-30 16:09:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=579572 AND AD_Language='en_US'
+;
+
+-- 2021-08-30T13:09:27.227Z
+-- URL zum Konzept
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579572,'en_US')
+;
+
+-- 2021-08-30T13:09:40.746Z
+-- URL zum Konzept
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Fachrichtung', PrintName='Fachrichtung',Updated=TO_TIMESTAMP('2021-08-30 16:09:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=579572 AND AD_Language='nl_NL'
+;
+
+-- 2021-08-30T13:09:40.787Z
+-- URL zum Konzept
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579572,'nl_NL')
+;
