f38b43cff50885661c7133015bde492374aa9097
Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
2021-08-31 12:45:40 +0300

    Gh11652 debug duplicate payment (#11664)
    * line with a paid invoice is still a line with invoice
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * cleanup
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * callout:set bank account from org
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * duplicate payment
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * typo
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * tests corrections
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * throw error for not found default bank account
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * infinite looop
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * corrections
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * corrections
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * we should never be able to set the "Duplicate Payment" action manually!
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * Make sure "Duplicate Payment" is set when the payment is older but not when it was just created
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * test correction
    Also formatting and cleanup
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * cleanup
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * cleanup: delete obsolete comment
    
    https://github.com/metasfresh/metasfresh/issues/11652
    
    * better error for exception
    
    https://github.com/metasfresh/metasfresh/issues/11652

--- backend/de.metas.payment.esr/src/test/java/de/metas/payment/esr/ESRValidationRuleTools.java
+++ backend/de.metas.payment.esr/src/test/java/de/metas/payment/esr/ESRValidationRuleTools.java
@@ -1,35 +1,12 @@
 package de.metas.payment.esr;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-/*
- * #%L
- * de.metas.payment.esr
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
-import org.adempiere.ad.validationRule.impl.PlainValidationContext;
-import org.compiere.util.NamePair;
-
 import de.metas.payment.esr.model.I_ESR_ImportLine;
 import de.metas.payment.esr.validationRule.ESRPaymentActionValidationRule;
 import de.metas.util.Check;
+import org.adempiere.ad.validationRule.impl.PlainValidationContext;
+import org.compiere.util.NamePair;
+
+import static org.assertj.core.api.Assertions.*;
 
 public final class ESRValidationRuleTools
 {
@@ -72,8 +49,7 @@ public final class ESRValidationRuleTools
 	}
 
 	/**
-	 *
-	 * @param action may not be {@code null}.
+	 * @param action             may not be {@code null}.
 	 * @param plainValidationCtx
 	 * @return
 	 */
@@ -87,8 +63,7 @@ public final class ESRValidationRuleTools
 	}
 
 	/**
-	 *
-	 * @param action may not be {@code null}.
+	 * @param action     may not be {@code null}.
 	 * @param importLine
 	 * @return
 	 */
