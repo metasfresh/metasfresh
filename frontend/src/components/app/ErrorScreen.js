9d4fc349c27171889003aa6ca75241e317158711
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-16 09:45:19 +0300

    Adapt ErrorScreen for multi usage
    (cherry picked from commit 65c2e05037e187cbe5d4d4a08ab01caa236d2b65)

--- frontend/src/components/app/ErrorScreen.js
+++ frontend/src/components/app/ErrorScreen.js
@@ -1,25 +1,52 @@
 import counterpart from 'counterpart';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const OFFLINE_MESSAGE_LINE1 = 'Connection lost.';
 const OFFLINE_MESSAGE_LINE2 =
   'There are some connection issues. ' +
   'Check connection and try to refresh the page.';
+const BAD_GATEWAY_LINE1 = 'Instance is not available';
+const BAD_GATEWAY_LINE2 = 'There are some connection issues.';
 
 class ErrorScreen extends Component {
   constructor(props) {
     super(props);
   }
-  render() {
-    let line1 = counterpart.translate('window.error.noStatus.title');
-    let line2 = counterpart.translate('window.error.noStatus.description');
 
-    if (!line1) {
-      line1 = OFFLINE_MESSAGE_LINE1;
+  getLineFromCounterpart = (targetLine) => {
+    const { errorType } = this.props;
+    switch (errorType) {
+      case 'badGateway':
+        return counterpart.translate(`window.error.badGateway.${targetLine}`);
+      default:
+        return counterpart.translate(`window.error.noStatus.${targetLine}`);
+    }
+  };
+
+  getLineMsg = (targetLine) => {
+    const { errorType } = this.props;
+    switch (errorType) {
+      case 'badGateway':
+        if (targetLine === 'line1') return BAD_GATEWAY_LINE1;
+        if (targetLine === 'line2') return BAD_GATEWAY_LINE2;
+        break;
+      default:
+        if (targetLine === 'line1') return OFFLINE_MESSAGE_LINE1;
+        if (targetLine === 'line2') return OFFLINE_MESSAGE_LINE2;
+    }
+  };
+
+  render() {
+    let line1 = this.getLineFromCounterpart('title');
+    let line2 = this.getLineFromCounterpart('description');
+
+    if (line1.includes('{lang.')) {
+      line1 = this.getLineMsg('line1');
     }
 
-    if (!line2) {
-      line2 = OFFLINE_MESSAGE_LINE2;
+    if (line2.includes('{lang.')) {
+      line2 = this.getLineMsg('line2');
     }
 
     return (
@@ -30,5 +57,9 @@ class ErrorScreen extends Component {
     );
   }
 }
+
+ErrorScreen.propTypes = {
+  errorType: PropTypes.string,
+};
 
 export default ErrorScreen;
