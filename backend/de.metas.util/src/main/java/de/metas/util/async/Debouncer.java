312b5dc583792cd56a11be54503d603885074404
cp-ps <cp-ps@pevesoft.ro>
2021-09-18 02:28:48 +0300

    remove `SystemTime.millis()` usages from Debouncer
    (cherry picked from commit a9057db18fff06c30ad57ef281814cf7605adcff)

--- backend/de.metas.util/src/main/java/de/metas/util/async/Debouncer.java
+++ backend/de.metas.util/src/main/java/de/metas/util/async/Debouncer.java
@@ -23,7 +23,6 @@
 package de.metas.util.async;
 
 import com.google.common.base.MoreObjects;
-import de.metas.common.util.time.SystemTime;
 import de.metas.util.Check;
 import lombok.Builder;
 import lombok.NonNull;
@@ -133,7 +132,7 @@ public final class Debouncer<T>
 		final int delayInMillisEffective = bufferMaxSize > 0 && buffer.size() >= bufferMaxSize
 				? 0 // ASAP
 				: delayInMillis;
-		dueTime = SystemTime.millis() + delayInMillisEffective;
+		dueTime = System.currentTimeMillis() + delayInMillisEffective;
 		//System.out.println(this + " - new dueTime=" + dueTime);
 
 		if (!taskWasAlreadyScheduled)
@@ -150,7 +149,7 @@ public final class Debouncer<T>
 
 		synchronized (lock)
 		{
-			remaining = dueTime - SystemTime.millis();
+			remaining = dueTime - System.currentTimeMillis();
 			bufferSize = buffer.size();
 
 			//
