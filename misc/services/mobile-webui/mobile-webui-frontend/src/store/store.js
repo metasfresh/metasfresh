36cda48f5d059039f626ea44a4a3a779b4900527
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-21 20:10:28 +0300

    Test actions

--- misc/services/mobile-webui/mobile-webui-frontend/src/store/store.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/store/store.js
@@ -5,11 +5,13 @@ import thunk from "redux-thunk"
 import createRootReducer from '../reducers'
 
 export const history = createBrowserHistory()
+const composeEnhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history),
-    preloadedState, compose(applyMiddleware(routerMiddleware(history), thunk)),
+    preloadedState, composeEnhancer(applyMiddleware(routerMiddleware(history), thunk)),
   )
   return store
 }
