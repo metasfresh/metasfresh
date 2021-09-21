223c9a4de76c922723a3d2a37a059115e7acfbcc
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-21 08:17:40 +0300

    Wire up bulma

---
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        const { appName } = this.props;
+        return (
+            <header className="p-4 navbar is-fixed-top is-flex is-align-items-center is-justify-content-space-around header">
+              <div className="header-title">
+                <h4 className="title is-4">{appName}</h4>
+              </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
--- misc/services/mobile-webui/mobile-webui-frontend/src/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/index.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route, Switch } from 'react-router';
 import { store, history } from './store/store';
+import './assets/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
