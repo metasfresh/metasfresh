61b959a061626f4e2c72e989994fd0f628064d06
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-21 08:49:17 +0300

    More on online/offline

--- misc/services/mobile-webui/mobile-webui-frontend/src/reducers/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { connectRouter } from 'connected-react-router';
+import appHandler from './appHandler';
 
 const createRootReducer = (history) => combineReducers({
     router: connectRouter(history),
+    appHandler,
 })
 
 export default createRootReducer;
