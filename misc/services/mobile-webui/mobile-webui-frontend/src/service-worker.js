74eb1c9ecccb6cb6c546d2e99ef2dd052465a09b
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-22 14:45:06 +0300

    Dont use the process.env.PUBLIC_URL

--- misc/services/mobile-webui/mobile-webui-frontend/src/service-worker.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/service-worker.js
@@ -46,7 +46,8 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  // createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL('./index.html')
 );
 
 // An example runtime caching route for requests that aren't handled by the
