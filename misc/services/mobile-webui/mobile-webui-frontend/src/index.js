c7658d848f5a8b09587231d8451e94aed20de24a
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-22 12:42:14 +0300

    Use broadcast api for broadcastimg msg

--- misc/services/mobile-webui/mobile-webui-frontend/src/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/index.js
@@ -40,6 +40,15 @@ serviceWorkerRegistration.register();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+const broadcast = new BroadcastChannel('network-status-channel');
+
+/**
+ * Listen to the response broadcasted by the service worker when the network status changes
+ * This is needed for the special case when user refreshes the page while being off
+ */
+broadcast.onmessage = (event) => {
+  event.data.payload === 'offline' && globalStore.dispatch(networkStatusOffline())
+};
 
 window.addEventListener('offline', () => {
   globalStore.dispatch(networkStatusOffline())
