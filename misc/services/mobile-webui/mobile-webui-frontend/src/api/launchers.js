6f74f59c245132854f90acbb08db05869e5d04ae
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-23 08:55:58 +0300

    Use variables defined in .env instead of config

--- misc/services/mobile-webui/mobile-webui-frontend/src/api/launchers.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/api/launchers.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export function getLaunchers() {
-  return axios.get(`${config.API_URL}/app/api/v2/userWorkflows/launchers`);
+  return axios.get(`${process.env.API_URL}/app/api/v2/userWorkflows/launchers`);
 }
