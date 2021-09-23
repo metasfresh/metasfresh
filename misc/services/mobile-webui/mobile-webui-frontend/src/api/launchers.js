3fefa1d3536c601a5eafa0070c4c120394dfb7de
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-23 09:09:57 +0300

    Add doc

--- misc/services/mobile-webui/mobile-webui-frontend/src/api/launchers.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/api/launchers.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+/**
+ * @method getLaunchers
+ * @summary Get the list of available launchers
+ * @param {object} `token` - The token to use for authentication 
+ * @returns 
+ */
 export function getLaunchers({ token }) {
   return axios.get(`${process.env.API_URL}/app/api/v2/userWorkflows/launchers`, {
     headers: {
