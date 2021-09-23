624200bcb2a8d7c0992c56c90fb077eab841b612
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-23 11:15:16 +0300

    Put URI in local config

--- misc/services/mobile-webui/mobile-webui-frontend/src/api/launchers.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/api/launchers.js
@@ -7,7 +7,7 @@ import axios from 'axios';
  * @returns 
  */
 export function getLaunchers({ token }) {
-  return axios.get(`${process.env.API_URL}/app/api/v2/userWorkflows/launchers`, {
+  return axios.get(`${window.config.API_URL}/userWorkflows/launchers`, {
     headers: {
       'Authorization': token,
       'accept': 'application/json',
