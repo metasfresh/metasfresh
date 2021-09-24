05e85374f44ad8a3fcfcb0b35d347f867825d6a4
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-24 12:10:59 +0300

    Unhook redux-offline

--- misc/services/mobile-webui/mobile-webui-frontend/src/store/store.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/store/store.js
@@ -3,8 +3,6 @@ import { routerMiddleware } from 'connected-react-router';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import createRootReducer from '../reducers';
-import { offline } from '@redux-offline/redux-offline';
-import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 
 export const history = createMemoryHistory();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -13,7 +11,7 @@ export const store = function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history),
     preloadedState,
-    composeEnhancer(applyMiddleware(routerMiddleware(history), offline(offlineConfig), thunk))
+    composeEnhancer(applyMiddleware(routerMiddleware(history), thunk))
   );
   return store;
 };
