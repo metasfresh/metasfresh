610b87c877833af04f96e7dafdd8ebc29bd8d202
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-24 14:54:02 +0300

    placeholders addition

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/BarcodeScanner.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/BarcodeScanner.jsx
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class BarcodeScanner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeScanning: false,
+    };
+  }
+
+  initiateScanning = () => this.setState({ activeScanning: true });
+
   render() {
     const { barcodeCaption } = this.props.componentProps;
+    const { activeScanning } = this.state;
     const scanBtnCaption = barcodeCaption || 'Scan';
     return (
       <div>
         <div className="buttons">
-          <button className="button scanner-btn-green">{scanBtnCaption}</button>
+          <button className="button scanner-btn-green" onClick={this.initiateScanning}>
+            {scanBtnCaption}
+          </button>
         </div>
+        {activeScanning && (
+          <div>
+            <div>Please scan a Barcode</div>
+            <div>Video Placeholder</div>
+          </div>
+        )}
       </div>
     );
   }
