b6cfc5453a7e09bbb556af083ee6506e6fbaa611
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-27 16:50:20 +0300

    Ditch redux-offline due to caching issues

--- misc/services/mobile-webui/mobile-webui-frontend/src/store/store.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/store/store.js
@@ -3,8 +3,8 @@ import { routerMiddleware } from 'connected-react-router';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import createRootReducer from '../reducers';
-import { offline } from '@redux-offline/redux-offline';
-import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
+// import { offline } from '@redux-offline/redux-offline';
+// import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 
 export const history = createMemoryHistory();
 const composeEnhancer =
@@ -14,7 +14,8 @@ export const store = function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history),
     preloadedState,
-    composeEnhancer(offline(offlineConfig), applyMiddleware(routerMiddleware(history), thunk))
+    // composeEnhancer(offline(offlineConfig), applyMiddleware(routerMiddleware(history), thunk)) // unhook offline due to caching issues
+    composeEnhancer(applyMiddleware(routerMiddleware(history), thunk))
   );
   return store;
 };
