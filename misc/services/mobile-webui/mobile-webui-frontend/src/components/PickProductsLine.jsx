e26b42fabb685edc93a37bd13d6c2b917febdbc6
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-27 12:41:36 +0300

    PickProductsLine scaffolding

---
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/PickProductsLine.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PickProductsLine extends Component {
+  render() {
+    const { id, caption } = this.props;
+    return (
+      <div key={id} className="ml-3 mr-3 is-light pick-product" onClick={() => this.handleClick(id)}>
+        <div className="box">
+          <div className="columns is-mobile">
+            <div className="column is-12">
+              <div className="columns">
+                <div className="column is-size-4-mobile no-p">{caption}</div>
+                <div className="column is-size-7 no-p">STEP</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+PickProductsLine.propTypes = {
+  id: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+};
+
+export default PickProductsLine;
