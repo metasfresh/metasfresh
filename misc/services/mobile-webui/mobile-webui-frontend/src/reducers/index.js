ce000d0c8ce7fba0d31f422817a3cec449838ff7
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-27 19:14:43 +0300

    Scaffolding for wfProcesses_status

--- misc/services/mobile-webui/mobile-webui-frontend/src/reducers/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/reducers/index.js
@@ -3,6 +3,7 @@ import { connectRouter } from 'connected-react-router';
 import appHandler from './appHandler';
 import launchers from './launchers';
 import wfProcesses from './wfProcesses';
+import wfProcesses_status from './wfProcesses_status';
 
 const createRootReducer = (history) =>
   combineReducers({
@@ -10,6 +11,7 @@ const createRootReducer = (history) =>
     appHandler,
     launchers,
     wfProcesses,
+    wfProcesses_status,
   });
 
 export default createRootReducer;
