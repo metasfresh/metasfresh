2b55163220d545b9fee26c348e5c1f5bdb0481f2
siemiatj <kuba@saskla.cz>
2021-09-28 23:51:14 +0200

    #11757 styling to login form, better error handling

--- misc/services/mobile-webui/mobile-webui-frontend/src/App.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/App.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-import { ProvideAuth } from './hooks/useAuth';
+import { useAuth } from './hooks/useAuth';
+import useConstructor from './hooks/useConstructor';
 import Routes from './routes';
 
 import './App.css';
 
 function App() {
+  const auth = useAuth();
+
+  useConstructor(() => {
+    axios.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        /*
+         * Authorization error
+         */
+        if (error.response.status == 401) {
+          const history = useHistory();
+
+          auth.logout().finally(() => {
+            history.push('/login');
+          });
+        } else {
+          return Promise.reject(error);
+        }
+      }
+    );
+  });
+
   return (
-    <ProvideAuth>
+    <div className="application section">
       <Routes />
-    </ProvideAuth>
+    </div>
   );
 }
 
