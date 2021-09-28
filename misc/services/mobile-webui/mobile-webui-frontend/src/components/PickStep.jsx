7149b13391baec26156963512b5079d267046451
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-28 15:56:11 +0300

    Hardcoded mobileUI due to infrastructure issues with REACT_APP_WEBSITE_NAME env setup

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/PickStep.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/PickStep.jsx
@@ -2,21 +2,41 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 class PickStep extends PureComponent {
-  handleClick = (id) => {
-    console.log(id);
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePickingStep: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ activePickingStep: true });
+    window.scrollTo(0, 0);
   };
+  goBackToPickingSteps = () => this.setState({ activePickingStep: false });
 
   render() {
+    const { activePickingStep } = this.state;
     const { id, locatorName, productName, qtyPicked, qtyToPick, uom } = this.props;
     return (
-      <div key={id} className="ml-3 mr-3 is-light launcher" onClick={() => this.handleClick(id)}>
-        <div className="box">
-          <div className="columns is-mobile">
-            <div className="column is-12">
-              <div className="columns">
-                <div className="column is-size-4-mobile no-p">Product: {productName}</div>
-                <div className="column is-size-7 no-p">
-                  To Pick: {qtyToPick} Quantity picked: {qtyPicked} UOM: {uom} Locator Name: {locatorName}
+      <div>
+        {activePickingStep && (
+          <div className="scanner-container">
+            <div className="subtitle centered-text is-size-4">
+              Pick Item <button onClick={this.goBackToPickingSteps}>Go back</button>
+            </div>
+            <video className="viewport scanner-window" id="video" />
+          </div>
+        )}
+        <div key={id} className="ml-3 mr-3 is-light launcher" onClick={() => this.handleClick(id)}>
+          <div className="box">
+            <div className="columns is-mobile">
+              <div className="column is-12">
+                <div className="columns">
+                  <div className="column is-size-4-mobile no-p">Product: {productName}</div>
+                  <div className="column is-size-7 no-p">
+                    To Pick: {qtyToPick} Quantity picked: {qtyPicked} UOM: {uom} Locator Name: {locatorName}
+                  </div>
                 </div>
               </div>
             </div>
