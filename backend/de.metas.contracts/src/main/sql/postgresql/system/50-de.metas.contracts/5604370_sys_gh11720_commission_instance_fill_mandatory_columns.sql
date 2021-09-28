100ee5e34326258a1f2af37672ff74c725711cb7
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-09-28 05:43:06 +0200

    fix SQL problem

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5604370_sys_gh11720_commission_instance_fill_mandatory_columns.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5604370_sys_gh11720_commission_instance_fill_mandatory_columns.sql
@@ -33,6 +33,9 @@ WHERE instance.commissiontrigger_type = 'MediatedOrder'
   AND mediatedOrderLine.c_orderline_id = instance.c_orderline_id
 ;
 
+-- avoid ERROR:  cannot ALTER TABLE "c_commission_instance" because it has pending trigger events
+COMMIT;
+
 -- 2021-09-13T16:38:41.722Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 INSERT INTO t_alter_column values('c_commission_instance','Qty',null,'NOT NULL',null)
