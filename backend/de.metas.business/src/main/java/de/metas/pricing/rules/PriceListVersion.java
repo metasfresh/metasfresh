d98a04b7203e3da30e1be1c2d3cfd9051649405d
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-09-29 06:42:10 +0200

    fix AD_Org_ID of possibly migrated C_BPartner_Locations
    also minor code improvements

--- backend/de.metas.business/src/main/java/de/metas/pricing/rules/PriceListVersion.java
+++ backend/de.metas.business/src/main/java/de/metas/pricing/rules/PriceListVersion.java
@@ -31,6 +31,8 @@ import de.metas.uom.UomId;
 import de.metas.util.Services;
 import lombok.NonNull;
 
+import javax.annotation.Nullable;
+
 /**
  * Calculate Price using Price List Version
  *
@@ -47,7 +49,7 @@ public class PriceListVersion extends AbstractPriceListBasedRule
 	private final IOrgDAO orgDAO = Services.get(IOrgDAO.class);
 
 	@Override
-	public void calculate(final IPricingContext pricingCtx, final IPricingResult result)
+	public void calculate(@NonNull final IPricingContext pricingCtx, @NonNull final IPricingResult result)
 	{
 		if (!applies(pricingCtx, result))
 		{
@@ -140,6 +142,7 @@ public class PriceListVersion extends AbstractPriceListBasedRule
 		return priceListsRepo.getPriceListVersionById(priceListVersionId);
 	}
 
+	@Nullable
 	private I_M_PriceList_Version getPriceListVersionEffective(final IPricingContext pricingCtx)
 	{
 		final I_M_PriceList_Version contextPLV = pricingCtx.getM_PriceList_Version();
@@ -151,7 +154,7 @@ public class PriceListVersion extends AbstractPriceListBasedRule
 		final I_M_PriceList_Version plv = priceListsRepo.retrievePriceListVersionOrNull(
 				pricingCtx.getPriceListId(),
 				TimeUtil.asZonedDateTime(pricingCtx.getPriceDate(), SystemTime.zoneId()),
-				(Boolean)null // processed
+				null // processed
 		);
 
 		return plv != null && plv.isActive() ? plv : null;
