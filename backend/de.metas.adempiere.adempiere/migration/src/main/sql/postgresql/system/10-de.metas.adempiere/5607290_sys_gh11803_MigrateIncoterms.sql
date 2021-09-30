b2f5c6769ee4875e261a434a0016994684991122
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-09-30 18:55:10 +0300

    #11803 Create Incoterms Tables and window https://github.com/metasfresh/metasfresh/issues/11803

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5607290_sys_gh11803_MigrateIncoterms.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5607290_sys_gh11803_MigrateIncoterms.sql
@@ -1,25 +1,3 @@
-/*
- * #%L
- * de.metas.adempiere.adempiere.migration-sql
- * %%
- * Copyright (C) 2021 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 INSERT INTO c_incoterms (ad_client_id, ad_org_id, created, createdby, c_incoterms_id, isactive, description, name, updated, updatedby, value)
     (
         SELECT ad_client_id, ad_org_id, created, '99', nextval('C_INCOTERMS_SEQ'), 'Y', null, name, now(), '99', value
