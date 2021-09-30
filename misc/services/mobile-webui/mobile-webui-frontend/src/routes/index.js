26c9616e11ac60b32e4ac42a1e229f05c475f684
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-30 12:21:07 +0300

    Prereq for push

--- misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router';
 import Main from '../components/Main';
 import LoginView from '../components/LoginView';
 import Dashboard from '../components/Dashboard';
+import LineScreen from '../components/LineScreen';
 import WorkflowProcess from '../containers/WFProcess';
 
 import PrivateRoute from './PrivateRoute';
@@ -19,6 +20,7 @@ const Routes = () => {
           <PrivateRoute path="/">
             <Route exact path="/" component={Dashboard} />
             <Route exact path="/workflow/:workflowId" component={WorkflowProcess} />
+            <Route exact path="/workflow/:workflowId/activityId/:activityId/lineId/:lineId" component={LineScreen} />
           </PrivateRoute>
         </Switch>
       </Main>
