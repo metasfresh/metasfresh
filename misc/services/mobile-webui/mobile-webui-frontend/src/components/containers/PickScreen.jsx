2780e69f93d9c212d016ad4467c51938e7504dad
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-30 19:37:19 +0300

    Get the properties from the store

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/PickScreen.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/PickScreen.jsx
@@ -5,33 +5,37 @@ import BarcodeScanner from './BarcodeScanner';
 
 class PickScreen extends Component {
   render() {
-    let huBarcode = 'test';
+    console.log('stepProps:', this.props);
+
+    const {
+      stepProps: { productName, locatorName, uom, huBarcode, qtyToPick, qtyPicked },
+    } = this.props;
     return (
       <div className="picking-step-container">
         <div className="picking-step-details centered-text is-size-5">
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Product:</div>
-            <div className="column is-half has-text-left">productName</div>
+            <div className="column is-half has-text-left">{productName}</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Locator:</div>
-            <div className="column is-half has-text-left">locatorName</div>
+            <div className="column is-half has-text-left">{locatorName}</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">UOM:</div>
-            <div className="column is-half has-text-left">uom</div>
+            <div className="column is-half has-text-left">{uom}</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Barcode:</div>
-            <div className="column is-half has-text-left">huBarcode</div>
+            <div className="column is-half has-text-left">{huBarcode}</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Quantity to pick:</div>
-            <div className="column is-half has-text-left">qtyToPick</div>
+            <div className="column is-half has-text-left">{qtyToPick}</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Quantity picked:</div>
-            <div className="column is-half has-text-left">qtyPicked</div>
+            <div className="column is-half has-text-left">{qtyPicked}</div>
           </div>
           <BarcodeScanner id={huBarcode} componentProps={{ caption: 'Scan' }} />
         </div>
@@ -41,16 +45,16 @@ class PickScreen extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(ownProps.match.params);
-  const { workflowId: wfProcessId, activityId, lineId } = ownProps.match.params;
+  const { workflowId: wfProcessId, activityId, lineId, stepId } = ownProps.match.params;
 
   const targetActivity = state.wfProcesses[wfProcessId].activities.filter(
     (activity) => activity.activityId === activityId
   );
   return {
-    lineProps: targetActivity[0].componentProps.lines[lineId],
+    stepProps: targetActivity[0].componentProps.lines[lineId].steps[stepId],
     activityId,
     wfProcessId,
+    stepId,
     lineIndex: Number(lineId),
   };
 };
@@ -59,7 +63,7 @@ PickScreen.propTypes = {
   activityId: PropTypes.string.isRequired,
   wfProcessId: PropTypes.string.isRequired,
   lineIndex: PropTypes.number.isRequired,
-  lineProps: PropTypes.object.isRequired,
+  stepProps: PropTypes.object.isRequired,
 };
 
 export default connect(mapStateToProps, {})(PickScreen);
