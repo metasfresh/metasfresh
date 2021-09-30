1238da426de1f69a74ff33646124ad7c56ded71e
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-30 15:28:30 +0300

    Ditch the loading of the wf component in the dashboard

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/Dashboard.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/Dashboard.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useStore } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Launchers from '../containers/Launchers';
-import WFProcess from '../components/containers/WFProcess';
 
 const Dashboard = () => {
-  const state = useStore().getState();
-  const { wfProcesses, wfProcesses_status } = state;
-
   return (
     <>
       <Launchers />
-      <WFProcess
-        wfProcessId="picking-7f42317d-0782-466c-a192-cb5ad7d3cce0"
-        {...wfProcesses['picking-7f42317d-0782-466c-a192-cb5ad7d3cce0']}
-        status={wfProcesses_status['picking-7f42317d-0782-466c-a192-cb5ad7d3cce0']}
-      />
       <Link to="/test">Go to test</Link>
     </>
   );
