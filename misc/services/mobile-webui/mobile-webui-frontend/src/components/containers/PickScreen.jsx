918933bd8db83128a4ec3db3d31c9018ec276e29
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-09-30 19:23:04 +0300

    Modify container for picking

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/PickScreen.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/PickScreen.jsx
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import BarcodeScanner from './BarcodeScanner';
 
 class PickScreen extends Component {
   render() {
+    let huBarcode = 'test';
     return (
       <div className="picking-step-container">
-        PICK SCREEN
-        {/* <div className="picking-step-details centered-text is-size-5">
+        <div className="picking-step-details centered-text is-size-5">
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Product:</div>
-            <div className="column is-half has-text-left">{productName}</div>
+            <div className="column is-half has-text-left">productName</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Locator:</div>
-            <div className="column is-half has-text-left">{locatorName}</div>
+            <div className="column is-half has-text-left">locatorName</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">UOM:</div>
-            <div className="column is-half has-text-left">{uom}</div>
+            <div className="column is-half has-text-left">uom</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Barcode:</div>
-            <div className="column is-half has-text-left">{huBarcode}</div>
+            <div className="column is-half has-text-left">huBarcode</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Quantity to pick:</div>
-            <div className="column is-half has-text-left">{qtyToPick}</div>
+            <div className="column is-half has-text-left">qtyToPick</div>
           </div>
           <div className="columns is-mobile">
             <div className="column is-half has-text-right has-text-weight-bold">Quantity picked:</div>
-            <div className="column is-half has-text-left">{qtyPicked}</div>
+            <div className="column is-half has-text-left">qtyPicked</div>
           </div>
           <BarcodeScanner id={huBarcode} componentProps={{ caption: 'Scan' }} />
-        </div> */}
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps.match.params);
   const { workflowId: wfProcessId, activityId, lineId } = ownProps.match.params;
 
   const targetActivity = state.wfProcesses[wfProcessId].activities.filter(
@@ -54,7 +56,6 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 PickScreen.propTypes = {
-  push: PropTypes.func.isRequired,
   activityId: PropTypes.string.isRequired,
   wfProcessId: PropTypes.string.isRequired,
   lineIndex: PropTypes.number.isRequired,
