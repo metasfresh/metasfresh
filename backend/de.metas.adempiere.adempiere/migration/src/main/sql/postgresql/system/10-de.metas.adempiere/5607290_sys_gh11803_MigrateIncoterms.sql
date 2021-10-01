b1d8546272843ca3bb5c49f1eb7137620081b9ec
BodorAnnamaria <annamaria.bodor@metasfresh.com>
2021-10-01 15:59:24 +0300

    #11803 Create Incoterms Tables and window https://github.com/metasfresh/metasfresh/issues/11803

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5607290_sys_gh11803_MigrateIncoterms.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5607290_sys_gh11803_MigrateIncoterms.sql
@@ -2,7 +2,7 @@ INSERT INTO c_incoterms (ad_client_id, ad_org_id, created, createdby, c_incoterm
     (
         SELECT ad_client_id, ad_org_id, created, '99', nextval('C_INCOTERMS_SEQ'), isactive, null, name, now(), '99', value
 
-        FROM ad_ref_list where ad_reference_id = 501599
+        FROM ad_ref_list where ad_reference_id = 501599 and value in ('DAF', 'DES', 'DEQ', 'DDU')
     )
 ;
 
@@ -10,6 +10,6 @@ INSERT INTO c_incoterms_trl (ad_client_id, ad_org_id, created, createdby, c_inco
     (
         SELECT ad_client_id, ad_org_id, created, '99', nextval('C_INCOTERMS_TRL_SEQ'), isactive, description, name, now(), '99',  (select c_incoterms_id from c_incoterms where value ILIKE substring(ad_ref_list_trl.name, 0, position(' - ' in  ad_ref_list_trl.name))), ad_language
 
-        FROM ad_ref_list_trl where ad_ref_list_id in  (select ad_ref_list_id from ad_ref_list where ad_reference_id = 501599)
+        FROM ad_ref_list_trl where ad_ref_list_id in  (select ad_ref_list_id from ad_ref_list where ad_reference_id = 501599 and value in ('DAF', 'DES', 'DEQ', 'DDU'))
     )
 ;
