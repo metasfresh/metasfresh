f5f4f29f270dbe93e50e73b96aab1ada9af5e6e5
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-01 08:39:42 +0300

    Some order in the actions and types

--- misc/services/mobile-webui/mobile-webui-frontend/src/actions/Actions.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/actions/Actions.js
@@ -2,26 +2,4 @@ export * from './NetworkActions';
 export * from './LauncherActions';
 export * from './PickingActions';
 export * from './ScanActions';
-
-import { SET_TOKEN, CLEAR_TOKEN } from '../constants/ActionTypes';
-
-/**
- * @method setToken
- * @summary sets user's authentication token
- */
-export function setToken(token) {
-  return {
-    type: SET_TOKEN,
-    payload: { token },
-  };
-}
-
-/**
- * @method clearToken
- * @summary clear user's authentication token
- */
-export function clearToken() {
-  return {
-    type: CLEAR_TOKEN,
-  };
-}
+export * from './TokenActions';
---
+++ misc/services/mobile-webui/mobile-webui-frontend/src/constants/WorkflowTypes.js
@@ -0,0 +1,17 @@
+/**
+ * @constant
+ * @type {string} clears the authentication token
+ */
+export const START_WORKFLOW_PROCESS = 'START_WORKFLOW_PROCESS';
+
+/**
+ * @constant
+ * @type {string} clears the authentication token
+ */
+export const CONTINUE_WORKFLOW_PROCESS = 'CONTINUE_WORKFLOW_PROCESS';
+
+/**
+ * @constant
+ * @type {string} Adds started workflow's activities to a temporary structure in the tree
+ */
+export const ADD_WORKFLOW_STATUS = 'ADD_WORKFLOW_STATUS';
