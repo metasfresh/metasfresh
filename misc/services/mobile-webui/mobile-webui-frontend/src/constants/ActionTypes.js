5ec7257380b88f257b9a74ec2cbeb11e7e5c0a4d
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-01 08:39:42 +0300

    Some order in the actions and types

--- misc/services/mobile-webui/mobile-webui-frontend/src/constants/ActionTypes.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/constants/ActionTypes.js
@@ -2,33 +2,5 @@ export * from './NetworkTypes';
 export * from './LaunchersTypes';
 export * from './PickingTypes';
 export * from './ScanTypes';
-
-/**
- * @constant
- * @type {string} sets the authentication token
- */
-export const SET_TOKEN = 'SET_TOKEN';
-
-/**
- * @constant
- * @type {string} clears the authentication token
- */
-export const CLEAR_TOKEN = 'CLEAR_TOKEN';
-
-/**
- * @constant
- * @type {string} clears the authentication token
- */
-export const START_WORKFLOW_PROCESS = 'START_WORKFLOW_PROCESS';
-
-/**
- * @constant
- * @type {string} clears the authentication token
- */
-export const CONTINUE_WORKFLOW_PROCESS = 'CONTINUE_WORKFLOW_PROCESS';
-
-/**
- * @constant
- * @type {string} Adds started workflow's activities to a temporary structure in the tree
- */
-export const ADD_WORKFLOW_STATUS = 'ADD_WORKFLOW_STATUS';
+export * from './TokenTypes';
+export * from './WorkflowTypes';
