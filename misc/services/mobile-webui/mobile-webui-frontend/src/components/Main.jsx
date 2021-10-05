b2de04aa8bfaa53fe33aa028850ec2888a69bdeb
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-05 13:41:56 +0300

    Change app name

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/Main.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/Main.jsx
@@ -6,7 +6,7 @@ import Header from './Header';
 function Main({ children }) {
   return (
     <>
-      <Header appName="webUI app" />
+      <Header appName="Picking" />
       {children}
     </>
   );
