43084ac0a981737eae892e6a4c9a7259a2b1574c
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-05 09:18:43 +0300

    cleanup the store

--- misc/services/mobile-webui/mobile-webui-frontend/src/reducers/wfProcesses_status.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/reducers/wfProcesses_status.js
@@ -1,46 +1,7 @@
 import { produce } from 'immer';
 import * as types from '../constants/ActionTypes';
 
-export const initialState = {
-  'picking-7f42317d-0782-466c-a192-cb5ad7d3cce0': {
-    isSentToBackend: false,
-    activities: {
-      1: {
-        activityId: '1',
-        isComplete: false,
-        dataStored: {},
-      },
-      2: {
-        activityId: '2',
-        dataStored: {
-          isComplete: false,
-          isLinesListVisible: true,
-          lines: [
-            {
-              caption: 'TestProduct',
-              steps: [
-                {
-                  productName: 'TestProduct',
-                  locatorName: 'Hauptlager',
-                  huBarcode: '1000001',
-                  uom: 'Stk',
-                  qtyToPick: 7,
-                  qtyPicked: 0,
-                },
-              ],
-            },
-          ],
-        },
-      },
-      3: {
-        activityId: '3',
-        dataStored: {
-          isComplete: false,
-        },
-      },
-    },
-  },
-};
+export const initialState = {};
 
 const reducer = produce((draftState, action) => {
   switch (action.type) {
