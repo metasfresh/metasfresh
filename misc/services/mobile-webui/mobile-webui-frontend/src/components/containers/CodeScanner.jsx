9eac34c805b36ff12b0b8577439ad53fb103a3e4
siemiatj <kuba@saskla.cz>
2021-10-08 02:05:04 +0200

    - export scanner button to a separate component - add path for scanner - remove unused props

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/CodeScanner.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/containers/CodeScanner.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserMultiFormatReader, DecodeHintType, BarcodeFormat } from '@zxing/library';
 import { connect } from 'react-redux';
+
 import { startScanning } from '../../actions/ScanActions';
-import ButtonWithIndicator from '../ButtonWithIndicator';
 
 class CodeScanner extends Component {
   _isMounted = false;
@@ -40,6 +40,7 @@ class CodeScanner extends Component {
 
   decodeContinuously = (selectedDeviceId) => {
     const { onDetection, activityId } = this.props;
+
     this.codeReader.decodeFromInputVideoDeviceContinuously(selectedDeviceId, 'video', (result, err) => {
       if (result) {
         // properly decoded the code
@@ -63,6 +64,10 @@ class CodeScanner extends Component {
 
   componentDidMount() {
     this._isMounted = true;
+
+    const { selectedDeviceId } = this.state;
+
+    this.decodeContinuously(selectedDeviceId);
 
     console.log('CodeScanner initialized');
     this.codeReader
@@ -89,14 +94,6 @@ class CodeScanner extends Component {
       });
   }
 
-  initiateScanning = () => {
-    const { selectedDeviceId } = this.state;
-    const { startScanning } = this.props;
-    startScanning();
-    this.decodeContinuously(selectedDeviceId);
-    // window.scrollTo(0, 0);
-  };
-
   componentWillUnmount() {
     this._isMounted = false;
   }
@@ -104,23 +101,12 @@ class CodeScanner extends Component {
   render() {
     const {
       scanner: { active },
-      caption,
-      scanButtonStatus,
     } = this.props;
-
-    let scanBtnCaption = caption || 'Scan';
 
     !active && this.codeReader.stopContinuousDecode();
 
     return (
       <div>
-        {!active && (
-          <>
-            <button className="button is-outlined complete-btn" onClick={this.initiateScanning}>
-              <ButtonWithIndicator caption={scanBtnCaption} indicatorType={scanButtonStatus} />
-            </button>
-          </>
-        )}
         {active && (
           <div className="scanner-container">
             {/* Select video source */}
--- misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
@@ -9,6 +9,7 @@ import LoginView from '../components/LoginView';
 import Launchers from '../containers/Launchers';
 import LineScreen from '../components/containers/LineScreen';
 import PickScreen from '../components/containers/PickScreen';
+import ScanScreen from '../components/containers/ScanScreen';
 import WorkflowProcess from '../containers/WFProcess';
 
 import PrivateRoute from './PrivateRoute';
@@ -17,6 +18,7 @@ import { history } from '../store/store';
 const routesArray = [
   { path: '/', Component: Launchers },
   { path: '/workflow/:workflowId', Component: WorkflowProcess },
+  { path: '/workflow/:workflowId/activityId/:activityId/scanner', Component: ScanScreen },
   { path: '/workflow/:workflowId/activityId/:activityId/lineId/:lineId', Component: LineScreen },
   { path: '/workflow/:workflowId/activityId/:activityId/lineId/:lineId/stepId/:stepId', Component: PickScreen },
 ];
