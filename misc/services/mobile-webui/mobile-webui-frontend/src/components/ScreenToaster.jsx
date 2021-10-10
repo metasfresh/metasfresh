7d0e418a78ca2f5e8f8d8a0d78d8b143878a8585
Teo Sarca <teo.sarca@metasfresh.com>
2021-10-10 17:45:51 +0300

    limit the number of toasts displayed on screen to one

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/ScreenToaster.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/ScreenToaster.jsx
@@ -1,7 +1,22 @@
-import { Toaster } from 'react-hot-toast';
-import React from 'react';
+import React, { useEffect } from 'react';
+import toast, { Toaster, useToasterStore } from 'react-hot-toast';
+
+const TOASTS_LIMIT = 1;
 
 const ScreenToaster = () => {
+  //
+  // Limit the number of toasts displayed on screen
+  // (thx to https://github.com/timolins/react-hot-toast/issues/31#issuecomment-803359550)
+  if (TOASTS_LIMIT > 0) {
+    const { toasts } = useToasterStore();
+    useEffect(() => {
+      toasts
+        .filter((t) => t.visible)
+        .filter((_, i) => i >= TOASTS_LIMIT)
+        .forEach((t) => toast.dismiss(t.id));
+    }, [toasts]);
+  }
+
   return (
     <Toaster
       position="bottom-center"
