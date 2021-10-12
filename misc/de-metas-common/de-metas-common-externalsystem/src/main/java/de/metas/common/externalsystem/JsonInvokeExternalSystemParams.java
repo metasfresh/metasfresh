df8e2e5fa4fe3e66144e3011e8db9bd65880b86f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2021-10-12 14:40:37 +0200

    minor: add API-doc

--- misc/de-metas-common/de-metas-common-externalsystem/src/main/java/de/metas/common/externalsystem/JsonInvokeExternalSystemParams.java
+++ misc/de-metas-common/de-metas-common-externalsystem/src/main/java/de/metas/common/externalsystem/JsonInvokeExternalSystemParams.java
@@ -24,6 +24,7 @@ package de.metas.common.externalsystem;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
@@ -33,6 +34,7 @@ import java.util.Map;
 @Value
 public class JsonInvokeExternalSystemParams
 {
+	@ApiModelProperty("Parameters to the process action. The parameter keys translate to `AD_Process_Para.ColumnName`s")
 	Map<String, String> params;
 
 	@Builder
