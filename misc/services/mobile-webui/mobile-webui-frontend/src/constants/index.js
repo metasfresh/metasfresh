450462775f6137d21d3377e6b4e1de4b1f5473a7
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-15 15:51:46 +0300

    Add doc

--- misc/services/mobile-webui/mobile-webui-frontend/src/constants/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/constants/index.js
@@ -1,4 +1,8 @@
 export const apiBasePath = `${config.SERVER_URL}/api/v2`;
 export const stompUrl = `${config.SERVER_URL}/stomp`;
 
-export const UPDATE_CHECK_INTERVAL = 60000; // check every min if there is a new version
+/**
+ * @constant
+ * @type {int} used normally to check every min if there is a new version, can be customised as needed
+ */
+export const UPDATE_CHECK_INTERVAL = 60000;
