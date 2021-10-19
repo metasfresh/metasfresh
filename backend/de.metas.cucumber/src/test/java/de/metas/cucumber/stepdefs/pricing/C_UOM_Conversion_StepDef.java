212501f51b3b1fc07e8763106b9ef16f21bbb808
dm-ps <dragos.moca@pevesoft.ro>
2021-10-19 13:07:24 +0300

    cucumber refs: https://github.com/metasfresh/metasfresh/issues/11866

---
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/pricing/C_UOM_Conversion_StepDef.java
@@ -0,0 +1,92 @@
+/*
+ * #%L
+ * de.metas.cucumber
+ * %%
+ * Copyright (C) 2021 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.cucumber.stepdefs.pricing;
+
+import de.metas.cucumber.stepdefs.DataTableUtil;
+import de.metas.cucumber.stepdefs.StepDefConstants;
+import de.metas.cucumber.stepdefs.StepDefData;
+import de.metas.product.ProductId;
+import de.metas.uom.CreateUOMConversionRequest;
+import de.metas.uom.IUOMConversionDAO;
+import de.metas.uom.IUOMDAO;
+import de.metas.uom.UomId;
+import de.metas.uom.X12DE355;
+import de.metas.util.Services;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import lombok.NonNull;
+import org.compiere.model.I_C_UOM;
+import org.compiere.model.I_C_UOM_Conversion;
+import org.compiere.model.I_M_Product;
+import org.compiere.model.I_M_ProductPrice;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class C_UOM_Conversion_StepDef
+{
+	private final StepDefData<I_M_Product> productTable;
+	private final IUOMDAO uomDAO = Services.get(IUOMDAO.class);
+	private final IUOMConversionDAO uomConversionDAO = Services.get(IUOMConversionDAO.class);
+
+	public C_UOM_Conversion_StepDef(@NonNull final StepDefData<I_M_Product> productTable)
+	{
+		this.productTable = productTable;
+	}
+
+	@And("metasfresh contains C_UOM_Conversions")
+	public void add_C_UOM_Conversions(@NonNull final DataTable dataTable)
+	{
+		final List<Map<String, String>> tableRows = dataTable.asMaps();
+		for (final Map<String, String> tableRow : tableRows)
+		{
+			create_C_UOM_Conversions(tableRow);
+		}
+	}
+
+	private void create_C_UOM_Conversions(@NonNull final Map<String, String> tableRow)
+	{
+		final String productIdentifier = DataTableUtil.extractStringForColumnName(tableRow, I_M_ProductPrice.COLUMNNAME_M_Product_ID + "." + StepDefConstants.TABLECOLUMN_IDENTIFIER);
+		final I_M_Product product = productTable.get(productIdentifier);
+
+		final String fromX12de355Code = DataTableUtil.extractStringForColumnName(tableRow, "FROM_" + I_C_UOM.COLUMNNAME_C_UOM_ID + "." + X12DE355.class.getSimpleName());
+		final X12DE355 fromX12DE355 = X12DE355.ofCode(fromX12de355Code);
+		final UomId fromUomId = uomDAO.getUomIdByX12DE355(fromX12DE355);
+
+		final String toX12de355Code = DataTableUtil.extractStringForColumnName(tableRow, "TO_" + I_C_UOM.COLUMNNAME_C_UOM_ID + "." + X12DE355.class.getSimpleName());
+		final X12DE355 toX12DE355 = X12DE355.ofCode(toX12de355Code);
+		final UomId toUomId = uomDAO.getUomIdByX12DE355(toX12DE355);
+
+		final BigDecimal fromToMultiplier = DataTableUtil.extractBigDecimalForColumnName(tableRow, I_C_UOM_Conversion.COLUMNNAME_MultiplyRate);
+
+		final CreateUOMConversionRequest uomConversionRequest = CreateUOMConversionRequest.builder()
+				.productId(ProductId.ofRepoId(product.getM_Product_ID()))
+				.fromUomId(fromUomId)
+				.toUomId(toUomId)
+				.fromToMultiplier(fromToMultiplier)
+				.build();
+
+		uomConversionDAO.createUOMConversion(uomConversionRequest);
+	}
+}
--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipment/M_InOut_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipment/M_InOut_StepDef.java
@@ -23,25 +23,45 @@
 package de.metas.cucumber.stepdefs.shipment;
 
 import de.metas.cucumber.stepdefs.DataTableUtil;
+import de.metas.cucumber.stepdefs.StepDefConstants;
 import de.metas.cucumber.stepdefs.StepDefData;
+import de.metas.cucumber.stepdefs.StepDefUtil;
+import de.metas.handlingunits.model.I_M_ShipmentSchedule;
+import de.metas.handlingunits.shipmentschedule.api.M_ShipmentSchedule_QuantityTypeToUse;
+import de.metas.handlingunits.shipmentschedule.api.ShipmentScheduleEnqueuer;
+import de.metas.inout.model.I_M_InOutLine;
+import de.metas.inoutcandidate.model.I_M_ShipmentSchedule_QtyPicked;
+import de.metas.process.IADPInstanceDAO;
+import de.metas.util.Services;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import lombok.NonNull;
+import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.dao.IQueryFilter;
 import org.compiere.model.I_M_InOut;
+import org.compiere.util.Env;
+import org.compiere.util.Trx;
 
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class M_InOut_StepDef
 {
 	private final StepDefData<I_M_InOut> shipmentTable;
+	private final StepDefData<I_M_ShipmentSchedule> shipmentScheduleTable;
+	private final IQueryBL queryBL = Services.get(IQueryBL.class);
+	private final IADPInstanceDAO pinstanceDAO = Services.get(IADPInstanceDAO.class);
 
-	public M_InOut_StepDef(@NonNull final StepDefData<I_M_InOut> shipmentTable)
+	public M_InOut_StepDef(
+			@NonNull final StepDefData<I_M_InOut> shipmentTable,
+			@NonNull final StepDefData<I_M_ShipmentSchedule> shipmentScheduleTable)
 	{
 		this.shipmentTable = shipmentTable;
+		this.shipmentScheduleTable = shipmentScheduleTable;
 	}
 
 	@And("validate created shipments")
@@ -67,5 +87,97 @@ public class M_InOut_StepDef
 			assertThat(shipment.isProcessed()).isEqualTo(processed);
 			assertThat(shipment.getDocStatus()).isEqualTo(docStatus);
 		}
+	}
+
+	@And("generate shipments process is invoked")
+	public void invokeGenerateShipmentsProcess(@NonNull final DataTable table)
+	{
+		final Map<String, String> tableRow = table.asMaps().get(0);
+
+		final String shipmentScheduleIdentifier = DataTableUtil.extractStringForColumnName(tableRow, I_M_ShipmentSchedule.COLUMNNAME_M_ShipmentSchedule_ID + "." + StepDefConstants.TABLECOLUMN_IDENTIFIER);
+
+		final String quantityType = DataTableUtil.extractStringForColumnName(tableRow, ShipmentScheduleEnqueuer.ShipmentScheduleWorkPackageParameters.PARAM_QuantityType);
+		final boolean isCompleteShipments = DataTableUtil.extractBooleanForColumnName(tableRow, ShipmentScheduleEnqueuer.ShipmentScheduleWorkPackageParameters.PARAM_IsCompleteShipments);
+		final boolean isShipToday = DataTableUtil.extractBooleanForColumnName(tableRow, ShipmentScheduleEnqueuer.ShipmentScheduleWorkPackageParameters.PARAM_IsShipmentDateToday);
+
+		final de.metas.inoutcandidate.model.I_M_ShipmentSchedule shipmentSchedule = shipmentScheduleTable.get(shipmentScheduleIdentifier);
+
+		final IQueryFilter<I_M_ShipmentSchedule> queryFilter = queryBL.createCompositeQueryFilter(I_M_ShipmentSchedule.class)
+				.addOnlyActiveRecordsFilter()
+				.addEqualsFilter(I_M_ShipmentSchedule.COLUMNNAME_M_ShipmentSchedule_ID, shipmentSchedule.getM_ShipmentSchedule_ID());
+
+		final ShipmentScheduleEnqueuer.ShipmentScheduleWorkPackageParameters workPackageParameters = ShipmentScheduleEnqueuer.ShipmentScheduleWorkPackageParameters.builder()
+				.adPInstanceId(pinstanceDAO.createSelectionId())
+				.queryFilters(queryFilter)
+				.quantityType(M_ShipmentSchedule_QuantityTypeToUse.ofCode(quantityType))
+				.completeShipments(isCompleteShipments)
+				.isShipmentDateToday(isShipToday)
+				.build();
+
+		final ShipmentScheduleEnqueuer.Result result = new ShipmentScheduleEnqueuer()
+				.setContext(Env.getCtx(), Trx.TRXNAME_None)
+				.createWorkpackages(workPackageParameters);
+
+		assertThat(result.getEnqueuedPackagesCount()).isEqualTo(1);
+	}
+
+	@And("^after not more than (.*)s, M_InOut is found:$")
+	public void shipmentIsFound(final int timeoutSec, @NonNull final DataTable dataTable) throws InterruptedException
+	{
+		final Map<String, String> tableRow = dataTable.asMaps().get(0);
+
+		final String shipmentScheduleIdentifier = DataTableUtil.extractStringForColumnName(tableRow, I_M_ShipmentSchedule.COLUMNNAME_M_ShipmentSchedule_ID + "." + StepDefConstants.TABLECOLUMN_IDENTIFIER);
+		final String shipmentIdentifier = DataTableUtil.extractStringForColumnName(tableRow, I_M_InOut.COLUMNNAME_M_InOut_ID + "." + StepDefConstants.TABLECOLUMN_IDENTIFIER);
+
+		final de.metas.inoutcandidate.model.I_M_ShipmentSchedule shipmentSchedule = shipmentScheduleTable.get(shipmentScheduleIdentifier);
+
+		final Supplier<Boolean> isShipmentCreated = () -> {
+
+			final I_M_ShipmentSchedule_QtyPicked qtyPickedRecord = queryBL
+					.createQueryBuilder(I_M_ShipmentSchedule_QtyPicked.class)
+					.addOnlyActiveRecordsFilter()
+					.addEqualsFilter(I_M_ShipmentSchedule_QtyPicked.COLUMNNAME_M_ShipmentSchedule_ID, shipmentSchedule.getM_ShipmentSchedule_ID())
+					.create()
+					.firstOnly(I_M_ShipmentSchedule_QtyPicked.class);
+
+			if (qtyPickedRecord == null)
+			{
+				return false;
+			}
+
+			if (qtyPickedRecord.getM_InOutLine_ID() <= 0)
+			{
+				return false;
+			}
+
+			final I_M_InOutLine shipmentLine = queryBL
+					.createQueryBuilder(I_M_InOutLine.class)
+					.addOnlyActiveRecordsFilter()
+					.addEqualsFilter(I_M_InOutLine.COLUMNNAME_M_InOutLine_ID, qtyPickedRecord.getM_InOutLine_ID())
+					.create()
+					.firstOnly(I_M_InOutLine.class);
+
+			if (shipmentLine == null)
+			{
+				return false;
+			}
+
+			final I_M_InOut shipment = queryBL
+					.createQueryBuilder(I_M_InOut.class)
+					.addOnlyActiveRecordsFilter()
+					.addEqualsFilter(I_M_InOut.COLUMNNAME_M_InOut_ID, shipmentLine.getM_InOut_ID())
+					.create()
+					.firstOnly(I_M_InOut.class);
+
+			if (shipment != null)
+			{
+				shipmentTable.put(shipmentIdentifier, shipment);
+				return true;
+			}
+
+			return false;
+		};
+
+		StepDefUtil.tryAndWait(timeoutSec, 500, isShipmentCreated);
 	}
 }
