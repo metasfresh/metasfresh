5a4b4afdff1074ffa8591db768cda245fa1844bc
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-19 17:39:10 +0300

    Fix linter err

--- misc/services/mobile-webui/mobile-webui-frontend/src/serviceWorkerRegistration.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/serviceWorkerRegistration.js
@@ -37,7 +37,7 @@ export function register(config) {
       const swUrl = `./service-worker.js`;
 
       console.log('[ServiceWorker] - on page load event');
-      
+
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
