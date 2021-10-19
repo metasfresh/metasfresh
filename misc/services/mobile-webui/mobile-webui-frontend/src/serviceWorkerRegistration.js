dd8e340511cd010adbf0f1441971461ecf6a1f66
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-10-19 16:35:23 +0300

    Rem check condition for prod

--- misc/services/mobile-webui/mobile-webui-frontend/src/serviceWorkerRegistration.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/serviceWorkerRegistration.js
@@ -19,7 +19,8 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  console.log('[ServiceWorker] - Call register()');
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     // const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     // if (publicUrl.origin !== window.location.origin) {
