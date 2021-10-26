metasfresh/metasfresh

commit 0da219bbb3768d5c215c2b79cf0f8fa31aca782c

Payment Discount not allocated when using Prepay Order fixed (#11443)
* Payment Discount not allocated when using Prepay Order fixed
* refs: https://github.com/metasfresh/metasfresh/issues/11442

backend/de.metas.business/src/main/java/de/metas/payment/C_Payment.java
@@ -1,0 +1,110 @@
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2021 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.payment;
+
+import de.metas.currency.Currency;
+import de.metas.currency.CurrencyRepository;
+import de.metas.order.IOrderDAO;
+import de.metas.order.OrderId;
+import de.metas.payment.api.IPaymentBL;
+import de.metas.payment.paymentterm.IPaymentTermRepository;
+import de.metas.payment.paymentterm.PaymentTermId;
+import de.metas.util.Services;
+import de.metas.util.lang.Percent;
+import lombok.NonNull;
+import org.adempiere.ad.callout.annotations.Callout;
+import org.adempiere.ad.callout.annotations.CalloutMethod;
+import org.adempiere.ad.callout.spi.IProgramaticCalloutProvider;
+import org.adempiere.ad.modelvalidator.annotations.Init;
+import org.adempiere.ad.modelvalidator.annotations.Interceptor;
+import org.adempiere.ad.modelvalidator.annotations.ModelChange;
+import org.compiere.model.I_C_Order;
+import org.compiere.model.I_C_Payment;
+import org.compiere.model.ModelValidator;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Callout(I_C_Payment.class)
+@Interceptor(I_C_Payment.class)
+@Component
+public class C_Payment
+{
+	private final IPaymentTermRepository paymentTermRepository = Services.get(IPaymentTermRepository.class);
+	private final IOrderDAO orderDAO = Services.get(IOrderDAO.class);
+	private final IPaymentBL paymentBL = Services.get(IPaymentBL.class);
+
+	private final CurrencyRepository currencyRepository;
+
+	public C_Payment(@NonNull final CurrencyRepository currencyRepository)
+	{
+		this.currencyRepository = currencyRepository;
+	}
+
+	@Init
+	public void registerCallout()
+	{
+		Services.get(IProgramaticCalloutProvider.class).registerAnnotatedCallout(this);
+	}
+
+	/**
+	 * Note that because of C_Payment.C_Order_ID's validation rule we can one have only prepay orders in this field.
+	 * Before the payment is made, the prepay-order is not even completed, so the payment's date is effectively also the order's date.
+	 * So in case of a prepay order there is no need to take the term's DiscountDays into account.
+	 */
+	@CalloutMethod(columnNames = I_C_Payment.COLUMNNAME_C_Order_ID)
+	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_CHANGE, ModelValidator.TYPE_BEFORE_NEW }, ifColumnsChanged = I_C_Payment.COLUMNNAME_C_Order_ID)
+	public void updateDiscountAndPaymentAmount(@NonNull final I_C_Payment record)
+	{
+		final OrderId orderId = OrderId.ofRepoIdOrNull(record.getC_Order_ID());
+
+		if (orderId == null)
+		{
+			record.setPayAmt(BigDecimal.ZERO);
+			record.setDiscountAmt(null);
+
+			return;
+		}
+
+		final I_C_Order order = orderDAO.getById(orderId);
+
+		final PaymentTermId paymentTermId = PaymentTermId.ofRepoId(order.getC_PaymentTerm_ID());
+		final Percent paymentTermDiscountPercent = paymentTermRepository.getPaymentTermDiscount(paymentTermId);
+		final Currency currency = currencyRepository.getById(record.getC_Currency_ID());
+
+		final BigDecimal priceActual = paymentTermDiscountPercent.subtractFromBase(order.getGrandTotal(), currency.getPrecision().toInt());
+		final BigDecimal discountAmount = paymentTermDiscountPercent.computePercentageOf(order.getGrandTotal(), currency.getPrecision().toInt());
+
+		record.setC_Invoice(null);
+		record.setC_Charge_ID(0);
+		record.setIsPrepayment(true);
+		record.setWriteOffAmt(BigDecimal.ZERO);
+		record.setIsOverUnderPayment(false);
+		record.setOverUnderAmt(BigDecimal.ZERO);
+
+		//
+		record.setPayAmt(priceActual);
+		record.setDiscountAmt(discountAmount);
+		paymentBL.validateDocTypeIsInSync(record);
+	}
+}

backend/de.metas.business/src/main/java-legacy/org/compiere/model/CalloutPayment.java
@@ -16,24 +16,21 @@
  *****************************************************************************/
 package org.compiere.model;
 
+import de.metas.document.IDocTypeDAO;
+import de.metas.document.sequence.IDocumentNoBuilderFactory;
+import de.metas.document.sequence.impl.IDocumentNoInfo;
+import de.metas.payment.api.IPaymentBL;
+import de.metas.util.Services;
+import org.adempiere.ad.callout.api.ICalloutField;
+import org.adempiere.exceptions.DBException;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.compiere.util.DB;
+
 import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-
-import org.adempiere.ad.callout.api.ICalloutField;
-import org.adempiere.exceptions.AdempiereException;
-import org.adempiere.exceptions.DBException;
-import org.adempiere.model.InterfaceWrapperHelper;
-import org.compiere.util.DB;
-
-import de.metas.document.IDocTypeDAO;
-import de.metas.document.sequence.IDocumentNoBuilderFactory;
-import de.metas.document.sequence.impl.IDocumentNoInfo;
-import de.metas.invoice.service.IInvoiceBL;
-import de.metas.payment.api.IPaymentBL;
-import de.metas.util.Services;
 
 /**
  * Payment Callouts. org.compiere.model.CalloutPayment.*
@@ -47,6 +44,7 @@ import de.metas.util.Services;
  */
 public class CalloutPayment extends CalloutEngine
 {
+	private final IPaymentBL paymentBL = Services.get(IPaymentBL.class);
 
 	/**
 	 * Payment_Invoice. when Invoice selected - set C_Currency_ID -
@@ -156,77 +154,6 @@ public class CalloutPayment extends CalloutEngine
 		return docType(calloutField);
 	} // invoice
 
-	/**
-	 * Payment_Order. when Waiting Payment Order selected - set C_Currency_ID -
-	 * C_BPartner_ID - DiscountAmt = C_Invoice_Discount (ID, DateTrx) - PayAmt =
-	 * invoiceOpen (ID) - Discount - WriteOffAmt = 0
-	 */
-	public String order(final ICalloutField calloutField)
-	{
-		if (isCalloutActive())
-		{
-			return NO_ERROR;
-		}
-
-		final I_C_Payment payment = calloutField.getModel(I_C_Payment.class);
-		final int C_Order_ID = payment.getC_Order_ID();
-		if (C_Order_ID <= 0) // assuming it is resetting value
-		{
-			return NO_ERROR;
-		}
-
-		payment.setC_Invoice(null);
-		payment.setC_Charge_ID(0);
-		payment.setIsPrepayment(true);
-		//
-		payment.setDiscountAmt(BigDecimal.ZERO);
-		payment.setWriteOffAmt(BigDecimal.ZERO);
-		payment.setIsOverUnderPayment(false);
-		payment.setOverUnderAmt(BigDecimal.ZERO);
-		// Payment Date
-		Timestamp ts = payment.getDateTrx();
-		if (ts == null)
-			ts = new Timestamp(System.currentTimeMillis());
-		//
-		String sql = "SELECT COALESCE(Bill_BPartner_ID, C_BPartner_ID) as C_BPartner_ID "
-				+ ", C_Currency_ID "
-				+ ", GrandTotal "
-				+ "FROM C_Order WHERE C_Order_ID=?"; // #1
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try
-		{
-			pstmt = DB.prepareStatement(sql, null);
-			pstmt.setInt(1, C_Order_ID);
-			rs = pstmt.executeQuery();
-			if (rs.next())
-			{
-				final int bpartnerId = rs.getInt(1);
-				payment.setC_BPartner_ID(bpartnerId);
-
-				final int C_Currency_ID = rs.getInt(2); // Set Order Currency
-				payment.setC_Currency_ID(C_Currency_ID);
-
-				//
-				BigDecimal GrandTotal = rs.getBigDecimal(3); // Set Pay
-				// Amount
-				if (GrandTotal == null)
-					GrandTotal = BigDecimal.ZERO;
-				payment.setPayAmt(GrandTotal);
-			}
-		}
-		catch (SQLException e)
-		{
-			throw new DBException(e, sql);
-		}
-		finally
-		{
-			DB.close(rs, pstmt);
-		}
-
-		return docType(calloutField);
-	} // order
-
 	// 2008/07/18 Globalqss [ 2021745 ]
 	// Deleted project method
 
@@ -279,13 +206,11 @@ public class CalloutPayment extends CalloutEngine
 		final I_C_Payment payment = calloutField.getModel(I_C_Payment.class);
 
 		final I_C_DocType docType = InterfaceWrapperHelper.load(payment.getC_DocType_ID(), I_C_DocType.class);
-		if (docType != null)
-		{
-			calloutField.putWindowContext("IsSOTrx", docType.isSOTrx());
-		}
 
 		if (docType != null)
 		{
+			calloutField.putWindowContext("IsSOTrx", docType.isSOTrx());
+
 			final IDocumentNoInfo documentNoInfo = Services.get(IDocumentNoBuilderFactory.class)
 					.createPreliminaryDocumentNoBuilder()
 					.setNewDocType(docType)
@@ -299,32 +224,7 @@ public class CalloutPayment extends CalloutEngine
 			}
 		}
 
-		// Invoice
-		if (payment.getC_Invoice_ID() > 0 && docType != null)
-		{
-			final I_C_Invoice inv = payment.getC_Invoice();
-			if (inv.isSOTrx() != docType.isSOTrx())
-			{
-				// task: 07564 the SOtrx flags don't match, but that's OK *if* the invoice i a credit memo (either for the vendor or customer side)
-				if (!Services.get(IInvoiceBL.class).isCreditMemo(inv))
-				{
-					throw new AdempiereException("@PaymentDocTypeInvoiceInconsistent@");
-				}
-			}
-		}
-		// globalqss - Allow prepayment to Purchase Orders
-		// Order Waiting Payment (can only be SO)
-		// if (C_Order_ID != 0 && dt != null && !dt.isSOTrx())
-		// return "PaymentDocTypeInvoiceInconsistent";
-		// Order
-		if (payment.getC_Order_ID() > 0 && docType != null)
-		{
-			final I_C_Order ord = payment.getC_Order();
-			if (ord.isSOTrx() != docType.isSOTrx())
-			{
-				throw new AdempiereException("@PaymentDocTypeInvoiceInconsistent@");
-			}
-		}
+		paymentBL.validateDocTypeIsInSync(payment);
 		return NO_ERROR;
 	} // docType
 
@@ -342,7 +242,7 @@ public class CalloutPayment extends CalloutEngine
 		}
 
 		final I_C_Payment payment = calloutField.getModel(I_C_Payment.class);
-		Services.get(IPaymentBL.class).updateAmounts(payment, calloutField.getColumnName(), true);
+		paymentBL.updateAmounts(payment, calloutField.getColumnName(), true);
 		return NO_ERROR;
 	} // amounts
 } // CalloutPayment

backend/de.metas.business/src/main/java/de/metas/allocation/api/impl/AllocationBL.java
@@ -232,13 +232,9 @@ public class AllocationBL implements IAllocationBL
 				.dateAcct(dateAcct)
 				.dateTrx(dateTrx);
 
-		BigDecimal sumAmt = BigDecimal.ZERO;
-
 		{
 			final PaymentId paymentId = PaymentId.ofRepoId(payment.getC_Payment_ID());
 			final BigDecimal currentAmt = paymentDAO.getAvailableAmount(paymentId);
-
-			sumAmt = sumAmt.add(currentAmt);
 
 			Check.assume(invoice.getC_BPartner_ID() == payment.getC_BPartner_ID(), "{} and {} have the same C_BPartner_ID", invoice, payment);
 
@@ -248,17 +244,19 @@ public class AllocationBL implements IAllocationBL
 					.invoiceId(invoice.getC_Invoice_ID())
 					.paymentId(payment.getC_Payment_ID());
 
-			if (sumAmt.compareTo(invoiceOpenAmt) < 0)
+			final BigDecimal amountToAllocate = currentAmt.min(invoiceOpenAmt);
+
+			if (payment.getC_Invoice_ID() == invoice.getC_Invoice_ID())
 			{
 				allocBuilder = lineBuilder
-						.amount(currentAmt)
+						.amount(amountToAllocate)
+						.discountAmt(payment.getDiscountAmt())
 						.lineDone();
 			}
 			else
 			{
-				// make sure the allocated amt is not bigger than the open amt of the invoice
 				allocBuilder = lineBuilder
-						.amount(invoiceOpenAmt.subtract(sumAmt.subtract(currentAmt)))
+						.amount(amountToAllocate)
 						.lineDone();
 			}
 		}

backend/de.metas.business/src/main/java/de/metas/invoice/InvoiceId.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Objects;
+import java.util.Optional;
+
 import com.google.common.collect.ImmutableSet;
 
 import de.metas.util.Check;
@@ -46,6 +48,13 @@ public class InvoiceId implements RepoIdAware
 	{
 		return repoId > 0 ? new InvoiceId(repoId) : null;
 	}
+
+	@NonNull
+	public static Optional<InvoiceId> ofRepoIdOptional(final int repoId)
+	{
+		return Optional.ofNullable(ofRepoIdOrNull(repoId));
+	}
+
 
 	int repoId;
 

backend/de.metas.business/src/main/java/de/metas/payment/api/IPaymentBL.java
@@ -153,4 +153,6 @@ public interface IPaymentBL extends ISingletonService
 	Optional<PaymentId> getByExtIdOrgId(ExternalId externalId, OrgId orgId);
 
 	CurrencyConversionContext extractCurrencyConversionContext(@NonNull I_C_Payment payment);
+
+	void validateDocTypeIsInSync(@NonNull final I_C_Payment payment);
 }

backend/de.metas.business/src/main/java/de/metas/payment/api/impl/PaymentBL.java
@@ -41,6 +41,7 @@ import de.metas.document.IDocTypeBL;
 import de.metas.document.IDocTypeDAO;
 import de.metas.document.engine.DocStatus;
 import de.metas.invoice.InvoiceId;
+import de.metas.invoice.service.IInvoiceBL;
 import de.metas.invoice.service.IInvoiceDAO;
 import de.metas.logging.LogManager;
 import de.metas.money.CurrencyConversionTypeId;
@@ -113,6 +114,7 @@ public class PaymentBL implements IPaymentBL
 	private final ISysConfigBL sysConfigBL = Services.get(ISysConfigBL.class);
 	private final ITrxManager trxManager = Services.get(ITrxManager.class);
 	private final IDocTypeDAO docTypeDAO = Services.get(IDocTypeDAO.class);
+	private final IInvoiceBL invoiceBL = Services.get(IInvoiceBL.class);
 
 	@Override
 	public I_C_Payment getById(@NonNull final PaymentId paymentId)
@@ -859,5 +861,51 @@ public class PaymentBL implements IPaymentBL
 		}
 
 		return conversionCtx;
+	}
+
+	@Override
+	public void validateDocTypeIsInSync(@NonNull final I_C_Payment payment)
+	{
+		final DocTypeId docTypeId = DocTypeId.ofRepoIdOrNull(payment.getC_DocType_ID());
+
+		if (docTypeId == null)
+		{
+			return;
+		}
+
+		final I_C_DocType docType = docTypeBL.getById(docTypeId);
+
+		// Invoice
+		final I_C_Invoice invoice = InvoiceId.ofRepoIdOptional(payment.getC_Invoice_ID())
+				.map(invoiceBL::getById)
+				.orElse(null);
+
+		if (invoice != null && invoice.isSOTrx() != docType.isSOTrx())
+		{
+			// task: 07564 the SOtrx flags don't match, but that's OK *if* the invoice i a credit memo (either for the vendor or customer side)
+			if (!invoiceBL.isCreditMemo(invoice))
+			{
+				throw new AdempiereException("@PaymentDocTypeInvoiceInconsistent@");
+			}
+		}
+
+		// globalqss - Allow prepayment to Purchase Orders
+		// Order Waiting Payment (can only be SO)
+		// if (C_Order_ID != 0 && dt != null && !dt.isSOTrx())
+		// return "PaymentDocTypeInvoiceInconsistent";
+		// Order
+		final OrderId orderId = OrderId.ofRepoIdOrNull(payment.getC_Order_ID());
+
+		if (orderId == null)
+		{
+			return;
+		}
+
+		final I_C_Order order = orderDAO.getById(orderId);
+
+		if (order.isSOTrx() != docType.isSOTrx())
+		{
+			throw new AdempiereException("@PaymentDocTypeInvoiceInconsistent@");
+		}
 	}
 }

backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5596320_sys_gh11442_PaymentDiscount_Callout_Column_Deleted.sql
@@ -1,0 +1,5 @@
+-- 2021-07-01T17:36:02.550Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_ColumnCallout WHERE AD_ColumnCallout_ID=50088
+;
+

commit ddbd10da5d604433caf4eca816384f25ab4bc764

fix bug that was introduced in
https://github.com/metasfresh/metasfresh/commit/0da219bbb3768d5c215c2b79cf0f8fa31aca782c

https://github.com/metasfresh/metasfresh/issues/11888

backend/de.metas.business/src/main/java/de/metas/payment/C_Payment.java
@@ -45,6 +45,8 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+import static de.metas.common.util.CoalesceUtil.firstGreaterThanZero;
+
 @Callout(I_C_Payment.class)
 @Interceptor(I_C_Payment.class)
 @Component
@@ -74,7 +76,7 @@ public class C_Payment
 	 */
 	@CalloutMethod(columnNames = I_C_Payment.COLUMNNAME_C_Order_ID)
 	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_CHANGE, ModelValidator.TYPE_BEFORE_NEW }, ifColumnsChanged = I_C_Payment.COLUMNNAME_C_Order_ID)
-	public void updateDiscountAndPaymentAmount(@NonNull final I_C_Payment record)
+	public void updateFromOrder(@NonNull final I_C_Payment record)
 	{
 		final OrderId orderId = OrderId.ofRepoIdOrNull(record.getC_Order_ID());
 
@@ -92,9 +94,13 @@ public class C_Payment
 		final BigDecimal priceActual = paymentTermDiscountPercent.subtractFromBase(order.getGrandTotal(), currency.getPrecision().toInt());
 		final BigDecimal discountAmount = paymentTermDiscountPercent.computePercentageOf(order.getGrandTotal(), currency.getPrecision().toInt());
 
+		record.setC_BPartner_ID(firstGreaterThanZero(order.getBill_BPartner_ID(), order.getC_BPartner_ID()));
+		record.setC_Currency_ID(order.getC_Currency_ID());
+
 		record.setC_Invoice(null);
 		record.setC_Charge_ID(0);
 		record.setIsPrepayment(true);
+
 		record.setWriteOffAmt(BigDecimal.ZERO);
 		record.setIsOverUnderPayment(false);
 		record.setOverUnderAmt(BigDecimal.ZERO);