68cd1cc6eb71cadedeef852a0383cf3cb1768877
Petrica Nanca <petrica_nanca@yahoo.com>
2021-10-27 15:04:13 +0300

    Introduce aplication screen (#11883)
    * Application scafolding
    
    * prepare translation of the keys
    
    * Dynamic listing of apps
    
    * Link to launchers
    
    * Prerequisites for fetching the launchers by using a parameter
    
    * Sync also ws with correct path
    
    * - polish design of applications screen
    - small refactoring
    
    * - store selected application, update header and keep up to date
    
    Co-authored-by: Petrica Nanca <petrica.nanca@metasfresh.com>
    Co-authored-by: siemiatj <kuba@saskla.cz>

--- misc/services/mobile-webui/mobile-webui-frontend/src/reducers/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/reducers/index.js
@@ -3,6 +3,7 @@ import { connectRouter } from 'connected-react-router';
 import appHandler from './appHandler';
 import headers from './headers';
 import launchers from './launchers';
+import applications from './applications';
 import update from './update';
 import wfProcesses_status from './wfProcesses_status/index';
 
@@ -12,6 +13,7 @@ const createRootReducer = (history) =>
     router: connectRouter(history),
     appHandler,
     headers,
+    applications,
     launchers,
     wfProcesses_status,
   });
--- misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
@@ -13,14 +13,16 @@ import ScanScreen from '../containers/activities/scan/ScanScreen';
 import PickLineScreen from '../containers/activities/picking/PickLineScreen';
 import PickStepScreen from '../containers/activities/picking/PickStepScreen';
 import PickStepScanHUScreen from '../containers/activities/picking/PickStepScanHUScreen';
+import ApplicationsScreen from '../containers/applicationScreen/ApplicationsScreen';
 
 import PrivateRoute from './PrivateRoute';
 import { history } from '../store/store';
 import ScreenToaster from '../components/ScreenToaster';
 
 const routesArray = [
-  { path: '/', Component: WFLaunchersScreen },
+  { path: '/', Component: ApplicationsScreen },
   { path: '/workflow/:workflowId', Component: WFProcessScreen },
+  { path: '/launchers/:applicationId', Component: WFLaunchersScreen },
   { path: '/workflow/:workflowId/activityId/:activityId/scanner', Component: ScanScreen },
   { path: '/workflow/:workflowId/activityId/:activityId/lineId/:lineId', Component: PickLineScreen },
   { path: '/workflow/:workflowId/activityId/:activityId/lineId/:lineId/stepId/:stepId', Component: PickStepScreen },
