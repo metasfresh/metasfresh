cdcec204c322eca3ad26e7cb97e58228738aeb7e
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-01 11:54:52 +0200

    Fix DRY

--- misc/services/mobile-webui/mobile-webui-frontend/src/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { REGISTER_SERVICE_WORKER } from './constants';
 import { ProvideAuth } from './hooks/useAuth';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -49,9 +48,7 @@ navigator.serviceWorker
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
     // Learn more about service workers: https://cra.link/PWA
-    if (REGISTER_SERVICE_WORKER) {
-      serviceWorkerRegistration.register();
-    }
+    serviceWorkerRegistration.register();
   })
   .catch(function (err) {
     console.log('Service Worker registration failed: ', err);
