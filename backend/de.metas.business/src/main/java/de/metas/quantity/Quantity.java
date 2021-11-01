0b491bb1c34d8bb18c42b9cbbecc2b5e47f7f150
Teo Sarca <teo.sarca@metasfresh.com>
2021-11-01 09:39:50 +0200

    Quantity.isPositive

--- backend/de.metas.business/src/main/java/de/metas/quantity/Quantity.java
+++ backend/de.metas.business/src/main/java/de/metas/quantity/Quantity.java
@@ -546,6 +546,11 @@ public final class Quantity implements Comparable<Quantity>
 		return ONE.compareTo(qty) == 0;
 	}
 
+	public boolean isPositive()
+	{
+		return signum() > 0;
+	}
+
 	/**
 	 * Adds given quantity and returns the result.
 	 * Assumes that the UOMs are equal.
