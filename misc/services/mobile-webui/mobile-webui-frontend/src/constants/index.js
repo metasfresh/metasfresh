d6f04be1a62ac35f403eda54e4dcb6f3b29eb6e8
Petrica Nanca <petrica_nanca@yahoo.com>
2021-11-01 12:38:20 +0200

    Unregister service worker based on config (#11910)
    * Unregister service worker based on config
    
    * Fix DRY
    
    Co-authored-by: Petrica Nanca <petrica.nanca@metasfresh.com>

--- misc/services/mobile-webui/mobile-webui-frontend/src/constants/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/constants/index.js
@@ -6,3 +6,9 @@ export const stompUrl = `${config.SERVER_URL}/stomp`;
  * @type {int} used normally to check every min if there is a new version, can be customised as needed
  */
 export const UPDATE_CHECK_INTERVAL = 60000;
+
+/**
+ * @constant
+ * @type {boolean} used to set or not the service worker registration
+ */
+export const REGISTER_SERVICE_WORKER = false;
