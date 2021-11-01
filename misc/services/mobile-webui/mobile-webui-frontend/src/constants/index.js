c4aeb6e8c15787448f98fcbaf730330b0e23e21e
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-01 08:20:53 +0200

    Update interval to 1 min

--- misc/services/mobile-webui/mobile-webui-frontend/src/constants/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/constants/index.js
@@ -5,4 +5,4 @@ export const stompUrl = `${config.SERVER_URL}/stomp`;
  * @constant
  * @type {int} used normally to check every min if there is a new version, can be customised as needed
  */
-export const UPDATE_CHECK_INTERVAL = 3000;
+export const UPDATE_CHECK_INTERVAL = 60000;
