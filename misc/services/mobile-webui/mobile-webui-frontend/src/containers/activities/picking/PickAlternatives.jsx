e10ab2157a684a7a48ed7bb9a5978f03bc5c7e8c
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-08 11:22:17 +0200

    Alternatives reducer

--- misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/picking/PickAlternatives.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/picking/PickAlternatives.jsx
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 class PickAlternatives extends Component {
+  renderAlternatives = ({ diffToPick }) => {
+    const { pickFromAlternativeIds, pickFromAlternatives } = this.props;
+
+    let totalQty = 0;
+
+    const alternativeHUs = pickFromAlternatives.reduce((accumulator, item) => {
+      if (pickFromAlternativeIds.includes(item.id) && totalQty < diffToPick && diffToPick) {
+        accumulator.push(item);
+        totalQty += item.qtyAvailable;
+      }
+      return accumulator;
+    }, []);
+
+    console.log('DIFFtoPick:', diffToPick);
+    console.log('ALTHUs', alternativeHUs);
+
+    // calculate here - filter to achieve the amount
+    return <div>DIFF: {diffToPick}</div>;
+  };
+
   render() {
-    return <div>PickAlternatives</div>;
+    const { qtyPicked, qtyToPick } = this.props;
+    const diffToPick = qtyToPick - qtyPicked;
+
+    return (
+      <div>
+        PickAlternatives remaining to pick {diffToPick}
+        {this.renderAlternatives({ diffToPick })}
+      </div>
+    );
   }
 }
 
 PickAlternatives.propTypes = {
   stepId: PropTypes.string.isRequired,
+  qtyPicked: PropTypes.number.isRequired,
+  qtyToPick: PropTypes.number.isRequired,
+  pickFromAlternativeIds: PropTypes.array,
+  pickFromAlternatives: PropTypes.array,
 };
 
 const mapStateToProps = (state, ownProps) => {
