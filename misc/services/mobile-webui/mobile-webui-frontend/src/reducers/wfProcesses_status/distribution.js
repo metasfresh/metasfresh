1e8eb3afb230f706cf722980866fa27fac5a9bbb
siemiatj <kuba@saskla.cz>
2021-11-09 12:30:16 +0100

    - remove duplicated code

--- misc/services/mobile-webui/mobile-webui-frontend/src/reducers/wfProcesses_status/distribution.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/reducers/wfProcesses_status/distribution.js
@@ -1,9 +1,7 @@
-import { original } from 'immer';
-
 import * as types from '../../constants/DistributionActionTypes';
-import { updateUserEditable } from './utils';
 import * as CompleteStatus from '../../constants/CompleteStatus';
 import { registerHandler } from './activityStateHandlers';
+import { computeLineStatus, updateActivityStatusFromLines } from './picking';
 
 const COMPONENT_TYPE = 'distribution/move';
 
@@ -82,76 +80,6 @@ const updateLineStatusFromSteps = ({ draftWFProcess, activityId, lineId }) => {
   // Rollup:
   updateActivityStatusFromLines({ draftWFProcess, activityId });
 };
-
-const computeLineStatus = ({ draftLine }) => {
-  const stepIds = Object.keys(original(draftLine.steps));
-
-  if (stepIds.length > 0) {
-    let countStepsCompleted = 0;
-    for (let stepId of stepIds) {
-      const draftStep = draftLine.steps[stepId];
-      const stepCompleteStatus = draftStep.completeStatus || CompleteStatus.NOT_STARTED;
-
-      if (stepCompleteStatus === CompleteStatus.COMPLETED) {
-        countStepsCompleted++;
-      }
-    }
-
-    if (countStepsCompleted === 0) {
-      return CompleteStatus.NOT_STARTED;
-    } else if (countStepsCompleted === stepIds.length) {
-      return CompleteStatus.COMPLETED;
-    } else {
-      return CompleteStatus.IN_PROGRESS;
-    }
-  } else {
-    // corner case, shall not happen: there are no steps in current line => consider it completed
-    return CompleteStatus.COMPLETED;
-  }
-};
-
-const updateActivityStatusFromLines = ({ draftWFProcess, activityId }) => {
-  const draftActivity = draftWFProcess.activities[activityId];
-  draftActivity.dataStored.completeStatus = computeActivityStatusFromLines({ draftActivity });
-  console.log(`Update activity [${activityId} ]: completeStatus=${draftActivity.dataStored.completeStatus}`);
-
-  //
-  // Rollup:
-  updateUserEditable({ draftWFProcess });
-};
-
-const computeActivityStatusFromLines = ({ draftActivity }) => {
-  const lineIds = Object.keys(original(draftActivity.dataStored.lines));
-
-  if (lineIds.length > 0) {
-    let countLinesCompleted = 0;
-    for (let lineId of lineIds) {
-      const draftLine = draftActivity.dataStored.lines[lineId];
-      const lineCompleteStatus = draftLine.completeStatus || CompleteStatus.NOT_STARTED;
-
-      if (lineCompleteStatus === CompleteStatus.COMPLETED) {
-        countLinesCompleted++;
-      }
-    }
-
-    if (countLinesCompleted === 0) {
-      return CompleteStatus.NOT_STARTED;
-    } else if (countLinesCompleted === lineIds.length) {
-      return CompleteStatus.COMPLETED;
-    } else {
-      return CompleteStatus.IN_PROGRESS;
-    }
-  } else {
-    // corner case, shall not happen: there are no lines in current activity => consider it completed
-    return CompleteStatus.COMPLETED;
-  }
-};
-
-//
-//
-// ----------------------------------------------------------------------------
-//
-//
 
 const normalizeLines = (lines) => {
   return lines.map((line) => {
