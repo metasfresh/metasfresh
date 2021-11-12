51ac218240a4e7a4a3561e9cbfddab32290665a5
metas_rc <ruxandra.craciunescu@metasfresh.com>
2021-11-12 12:08:56 +0200

    process trl https://github.com/metasfresh/metasfresh/issues/11906

---
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5613300_sys_gh11906_Process_C_Order_CreateForAllMembers_TRL.sql
@@ -0,0 +1,43 @@
+-- 2021-11-12T09:57:28.298Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Create Membership Contracts',Updated=TO_TIMESTAMP('2021-11-12 11:57:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584929
+;
+
+-- 2021-11-12T09:57:28.308Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Create Membership Contracts',Updated=TO_TIMESTAMP('2021-11-12 11:57:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541832
+;
+
+-- 2021-11-12T09:57:38.031Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Name='Mitgliedsverträge erstellen',Updated=TO_TIMESTAMP('2021-11-12 11:57:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=584929
+;
+
+-- 2021-11-12T09:57:38.034Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET Description=NULL, IsActive='Y', Name='Mitgliedsverträge erstellen',Updated=TO_TIMESTAMP('2021-11-12 11:57:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=541832
+;
+
+-- 2021-11-12T09:57:46.927Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Mitgliedsverträge erstellen',Updated=TO_TIMESTAMP('2021-11-12 11:57:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=584929
+;
+
+-- 2021-11-12T09:57:54.093Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Mitgliedsverträge erstellen',Updated=TO_TIMESTAMP('2021-11-12 11:57:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=584929
+;
+
+-- 2021-11-12T09:57:59.388Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET IsTranslated='Y', Name='Create Membership Contracts',Updated=TO_TIMESTAMP('2021-11-12 11:57:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=584929
+;
+
+-- 2021-11-12T09:58:04.607Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Trl SET Name='Create Membership Contracts',Updated=TO_TIMESTAMP('2021-11-12 11:58:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=584929
+;
+
+
+
+
