7c860e00d37fa69e2d108a5c478c814fdd513b9a
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-16 09:36:48 +0200

    Skel for testing

--- misc/services/mobile-webui/mobile-webui-frontend/src/__tests__/reducers/picking.test.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/__tests__/reducers/picking.test.js
@@ -1,19 +1,20 @@
 import rawstateJson from '../fixtures/rawstate.json';
 import { produce } from 'immer';
-// import generateAlternativeSteps from '../../reducers/wfProcesses_status/picking';
+import generateAlternativeSteps from '../../reducers/wfProcesses_status/picking';
 
 describe('picking unit tests', () => {
   describe('generate alternative steps function', () => {
     it('should generate no steps when ...', () => {
       const initialState = produce(rawstateJson, (draftState) => {
-        // draftState = generateAlternativeSteps({
-        //     draftState,
-        //     wfProcessId: 'picking-1000001',
-        //     activityId: 'A2',
-        //     lineId: '0',
-        //     stepId: '1000001',
-        //     qtyToAllocate: 20,
-        // });
+        generateAlternativeSteps({
+          draftState,
+          wfProcessId: 'picking-1000001',
+          activityId: 'A2',
+          lineId: '0',
+          stepId: '1000001',
+          qtyToAllocate: 0,
+        });
+
         return draftState;
       });
       console.log('initialState:', initialState);
