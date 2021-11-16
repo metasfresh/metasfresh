7be4772f45fad2fcf09d4f5b7adaf0d89b23a7a7
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-16 11:30:23 +0200

    Pass the correct draft

--- misc/services/mobile-webui/mobile-webui-frontend/src/__tests__/reducers/picking.test.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/__tests__/reducers/picking.test.js
@@ -1,13 +1,14 @@
 import rawstateJson from '../fixtures/rawstate.json';
 import { produce } from 'immer';
-import generateAlternativeSteps from '../../reducers/wfProcesses_status/picking';
+import { generateAlternativeSteps } from '../../reducers/wfProcesses_status/picking';
 
 describe('picking unit tests', () => {
   describe('generate alternative steps function', () => {
     it('should generate no steps when ...', () => {
       const initialState = produce(rawstateJson, (draftState) => {
+        const draftStateWFProcesses = draftState['wfProcesses_status'];
         draftState = generateAlternativeSteps({
-          draftState,
+          draftState: draftStateWFProcesses,
           wfProcessId: 'picking-1000001',
           activityId: 'A2',
           lineId: '0',
