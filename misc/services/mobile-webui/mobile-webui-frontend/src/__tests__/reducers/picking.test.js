17faffdfc639d662368d936195b37fea8c8eb934
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-16 09:05:48 +0200

    Produce initial state from rawstateJson

--- misc/services/mobile-webui/mobile-webui-frontend/src/__tests__/reducers/picking.test.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/__tests__/reducers/picking.test.js
@@ -1,5 +1,22 @@
+import rawstateJson from '../fixtures/rawstate.json';
+import { produce } from 'immer';
+// import generateAlternativeSteps from '../../reducers/wfProcesses_status/picking';
+
 describe('picking unit tests', () => {
   describe('generate alternative steps function', () => {
-    it('should generate no steps when ...', () => {});
+    it('should generate no steps when ...', () => {
+      const initialState = produce(rawstateJson, (draftState) => {
+        // draftState = generateAlternativeSteps({
+        //     draftState,
+        //     wfProcessId: 'picking-1000001',
+        //     activityId: 'A2',
+        //     lineId: '0',
+        //     stepId: '1000001',
+        //     qtyToAllocate: 20,
+        // });
+        return draftState;
+      });
+      console.log('initialState:', initialState);
+    });
   });
 });
