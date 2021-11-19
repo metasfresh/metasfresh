026c648a1a1987a6c7bed634fc9fb80f1ade3b9d
Petrica Nanca <petrica.nanca@metasfresh.com>
2021-11-19 05:09:22 +0200

    Revert pickFromAlternatives proptype

--- misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/picking/PickStepButton.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/picking/PickStepButton.jsx
@@ -120,7 +120,7 @@ PickStepButton.propTypes = {
   qtyPicked: PropTypes.number,
   qtyToPick: PropTypes.number.isRequired,
   stepState: PropTypes.object,
-  pickFromAlternatives: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+  pickFromAlternatives: PropTypes.array,
   //
   // Actions/Functions
   dispatch: PropTypes.func.isRequired,
