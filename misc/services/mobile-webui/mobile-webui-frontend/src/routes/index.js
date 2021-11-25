773e6ccdacc3417257e5d3fcd953c41d6bfdbf0c
Teo Sarca <teo.sarca@metasfresh.com>
2021-11-25 05:24:17 +0200

    inline childRoutes

--- misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/routes/index.js
@@ -16,26 +16,24 @@ import ScreenToaster from '../components/ScreenToaster';
 
 const routesArray = [...commonRoutes, ...distributionRoutes, ...manufacturingRoutes];
 
-const childRoutes = (
-  <div>
-    {routesArray.map(({ path, Component }) => (
-      <Route key={path} exact path={path}>
-        <Header appName="metasfresh mobile" hidden />
-        <ViewHeader />
-        <Component />
-        <ScreenToaster />
-      </Route>
-    ))}
-  </div>
-);
-
 const Routes = () => {
   return (
     <ConnectedRouter history={history} basename="./">
       <Main>
         <Switch>
           <Route exact path="/login" component={LoginView} />
-          <PrivateRoute path="/">{childRoutes}</PrivateRoute>
+          <PrivateRoute path="/">
+            <div>
+              {routesArray.map(({ path, Component }) => (
+                <Route key={path} exact path={path}>
+                  <Header appName="metasfresh mobile" hidden />
+                  <ViewHeader />
+                  <Component />
+                  <ScreenToaster />
+                </Route>
+              ))}
+            </div>
+          </PrivateRoute>
         </Switch>
       </Main>
     </ConnectedRouter>
