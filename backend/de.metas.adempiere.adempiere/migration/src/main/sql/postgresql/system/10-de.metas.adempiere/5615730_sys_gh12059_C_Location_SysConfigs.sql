500b11594790d0f4a705c9c1273fe7b9ba3086c8
metas_rc <ruxandra.craciunescu@metasfresh.com>
2021-11-25 21:45:48 +0200

    BP Quick Input https://github.com/metasfresh/metasfresh/issues/12059

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5615730_sys_gh12059_C_Location_SysConfigs.sql
@@ -0,0 +1,35 @@
+
+-- 2021-11-25T17:39:30.068Z
+-- URL zum Konzept
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541429,'S',TO_TIMESTAMP('2021-11-25 19:39:29','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.ui.web.address.AddressDescriptorFactory.Street.IsDisplay',TO_TIMESTAMP('2021-11-25 19:39:29','YYYY-MM-DD HH24:MI:SS'),100,'N')
+;
+
+-- 2021-11-25T17:40:10.601Z
+-- URL zum Konzept
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541430,'S',TO_TIMESTAMP('2021-11-25 19:40:10','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.ui.web.address.AddressDescriptorFactory.DHL_PostId.IsDisplay',TO_TIMESTAMP('2021-11-25 19:40:10','YYYY-MM-DD HH24:MI:SS'),100,'N')
+;
+
+-- 2021-11-25T17:40:40.109Z
+-- URL zum Konzept
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541431,'S',TO_TIMESTAMP('2021-11-25 19:40:39','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.ui.web.address.AddressDescriptorFactory.HouseNumber.IsDisplay',TO_TIMESTAMP('2021-11-25 19:40:39','YYYY-MM-DD HH24:MI:SS'),100,'N')
+;
+
+
+
+
+
+-- 2021-11-25T17:44:36.901Z
+-- URL zum Konzept
+UPDATE AD_SysConfig SET ConfigurationLevel='C',Updated=TO_TIMESTAMP('2021-11-25 19:44:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541430
+;
+
+-- 2021-11-25T17:44:44.438Z
+-- URL zum Konzept
+UPDATE AD_SysConfig SET ConfigurationLevel='C',Updated=TO_TIMESTAMP('2021-11-25 19:44:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541431
+;
+
+-- 2021-11-25T17:45:00.174Z
+-- URL zum Konzept
+UPDATE AD_SysConfig SET ConfigurationLevel='C',Updated=TO_TIMESTAMP('2021-11-25 19:45:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541429
+;
+
