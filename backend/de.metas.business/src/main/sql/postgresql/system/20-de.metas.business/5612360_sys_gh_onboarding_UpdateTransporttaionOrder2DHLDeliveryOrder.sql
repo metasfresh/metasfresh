0f822abceeccb6849be7181d8021baf45cfd61d1
Chems Eddine Nouioua <chems.nouioua@gmail.com>
2021-11-30 19:26:29 +0100

    #remove unnecessary changes from branch

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5612360_sys_gh_onboarding_UpdateTransporttaionOrder2DHLDeliveryOrder.sql
+++
@@ -1,7 +0,0 @@
-
---Transportation Order -> DHL Shipment Delivery
--- 2021-11-04T10:50:43.833Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_RelationType SET IsActive='Y',Updated=TO_TIMESTAMP('2021-11-04 11:50:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540230
-;
-
--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5612420_sys_gh_onboarding_DeactivateUnnecessaryRelations.sql
+++
@@ -1,3 +0,0 @@
-
--- Soft deletion
-update ad_relationtype  set isactive  = 'N' where ad_relationtype_id  in (540215, 540212);
