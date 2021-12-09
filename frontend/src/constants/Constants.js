37d4b119755426d3a1f875810de6434727c9095f
Petrica Nanca <petrica_nanca@yahoo.com>
2021-12-09 16:13:15 +0200

    11732 bad gateway (#12082)
    * Adapt ErrorScreen for multi usage
    
    (cherry picked from commit 65c2e05037e187cbe5d4d4a08ab01caa236d2b65)
    
    * Prerequisites for rendering using offline counterpart
    
    * Add special file for storing the offlineMessages
    
    * prerequisites for triggering the display of the overlay component on login when there is no connection due to bad gw
    
    * Add some debug to check on the actual instance
    
    * Export store as few month ago a commit changed that
    
    * get the status
    
    * Add badGatewayStatus detection logic
    
    * Map the connectionErrorType
    
    * Refresh the page when there is connectivity
    
    * Ditch onPingSuccess
    
    * Export the store from index.jsx
    
    * Test with global
    
    * Use global
    
    * Have the store separately in its rightful place to be able to import it properly at ws level
    
    * Add some debug
    
    * Some cleanup
    
    * Add extra safety check for notif msg
    
    * Silent notif for 502 in order to not get a bunch of notifications
    
    * Some cleaning
    
    * Fix path
    
    * Fix typo
    
    * registerTranslations of offlineMesages in the same place
    
    * dont use intervalId from the state
    
    * use string literal
    
    * change the constant to CONNECTION_ERROR_RETRY_INTERVAL_MILLIS
    
    * Refactor the noConnection add errorType param
    
    * Fix proptype
    
    * More adapts
    
    * ditch userSessionResp
    
    * Set interval for any error type
    
    * Fallback to offline
    
    * Remove accidentaly added space
    
    * Update doc dor extractLanguageFromLocale
    
    * Add getCurrentActiveLanguage to utils
    
    * Add separate translations for offline
    
    * delete unused action
    
    * Post review changes
    
    * Added more info to prevent other devs for changing that constant
    
    * same logic in ws but diff errorType
    
    * const title and description
    
    * Remove getLineFromCounterpart
    
    * Ren noConnection to connectionError
    
    * rename to connectionErrorType
    
    * move connectionErrorType to apphandler
    
    * add connectionErrorType as string
    
    * Use same pattern structure for the offline keys
    
    * Removed the debug line
    
    Co-authored-by: Petrica Nanca <petrica.nanca@metasfresh.com>

--- frontend/src/constants/Constants.js
+++ frontend/src/constants/Constants.js
@@ -1,5 +1,24 @@
 /**
  * @constant
+ * @type {string} Type of `connectionErrorType` to be used in the redux store when we receive 502 error from the server.
+ */
+export const BAD_GATEWAY_ERROR = 'badGateway';
+
+/**
+ * @constant
+ * @type {string} Type of connectionErrorType when no error is present (internet down).
+ * Note: do not change this to another value because this matches the key in the translation messages (as it comes from the BE).
+ *       (see ErrorScreen component for more details)
+ */
+export const NO_CONNECTION_ERROR = 'noStatus';
+
+/**
+ * @type {integer} The number of milliseconds to wait before retrying to ping the server
+ */
+export const CONNECTION_ERROR_RETRY_INTERVAL_MILLIS = 5000;
+
+/**
+ * @constant
  * @type {string} ToDo: Description for the constant.
  */
 export const VIEW_EDITOR_RENDER_MODES_NEVER = 'never';
