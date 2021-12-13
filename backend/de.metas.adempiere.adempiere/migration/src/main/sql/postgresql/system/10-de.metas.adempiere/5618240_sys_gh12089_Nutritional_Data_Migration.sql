5d68fd7e0f52c6017c3a15d6169fbd5fabd0b7bd
Riad MENNOUR <riad.mennour@owiin.com>
2021-12-13 12:56:31 +0100

    Nutritional Data Migration

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5618240_sys_gh12089_Nutritional_Data_Migration.sql
@@ -0,0 +1,11 @@
+CREATE TABLE backup.M_Nutrition_Fact_gh12089_20211210
+
+AS
+
+SELECT * FROM M_Nutrition_Fact;
+
+CREATE TABLE backup.M_Ingredients_gh12089_20211210
+
+AS
+
+SELECT * FROM M_Ingredients;
