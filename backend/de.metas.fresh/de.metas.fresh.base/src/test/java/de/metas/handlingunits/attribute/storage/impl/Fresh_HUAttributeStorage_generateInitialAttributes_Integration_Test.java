b4a84e613d326724b8604adfee6a116f2d20a1d8
Teo Sarca <teo.sarca@metasfresh.com>
2021-12-15 06:33:19 +0200

    fix compile error on Fresh_HUAttributeStorage_generateInitialAttributes_Integration_Test

--- backend/de.metas.fresh/de.metas.fresh.base/src/test/java/de/metas/handlingunits/attribute/storage/impl/Fresh_HUAttributeStorage_generateInitialAttributes_Integration_Test.java
+++ backend/de.metas.fresh/de.metas.fresh.base/src/test/java/de/metas/handlingunits/attribute/storage/impl/Fresh_HUAttributeStorage_generateInitialAttributes_Integration_Test.java
@@ -10,30 +10,29 @@ package de.metas.handlingunits.attribute.storage.impl;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-import org.adempiere.mm.attributes.spi.IAttributeValueGenerator;
-
 import de.metas.vertical.pharma.securpharm.attribute.HUScannedAttributeHandler;
 import de.metas.vertical.pharma.securpharm.attribute.SecurPharmAttributesStatus;
-import org.junit.jupiter.api.Test;
+import org.adempiere.mm.attributes.spi.IAttributeValueGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * Runs {@link HUAttributeStorage_generateInitialAttributes_Integration_Test} using endcustomer project's classpath,
  * because we want to test ALL {@link IAttributeValueGenerator}.
- * 
- * @author tsa
  *
+ * @author tsa
  */
 public class Fresh_HUAttributeStorage_generateInitialAttributes_Integration_Test extends HUAttributeStorage_generateInitialAttributes_Integration_Test
 {
@@ -44,11 +43,12 @@ public class Fresh_HUAttributeStorage_generateInitialAttributes_Integration_Test
 		super.initialize();
 	}
 
+	@ParameterizedTest
+	@ValueSource(classes = { HUScannedAttributeHandler.class })
 	@Override
-	@Test
-	public void test()
+	public void test(final Class<? extends IAttributeValueGenerator> attributeValueGeneratorClass)
 	{
-		test(HUScannedAttributeHandler.class);
+		super.test(attributeValueGeneratorClass);
 	}
 
 }
