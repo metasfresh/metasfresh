a7ae7a44f8a8140217ed5411fef69ed6018a9622
cp-ps <cp-ps@pevesoft.ro>
2021-12-21 23:12:30 +0200

    API_Audit_Config.isForceProcessedAsync description

--- backend/de.metas.business.rest-api-impl/src/main/sql/postgresql/system/50-de.metas.business.rest-api-impl/5619800_sys_gh12122_API_Audit_Config_Trl.sql
+++ backend/de.metas.business.rest-api-impl/src/main/sql/postgresql/system/50-de.metas.business.rest-api-impl/5619800_sys_gh12122_API_Audit_Config_Trl.sql
@@ -153,3 +153,83 @@ UPDATE AD_Element_Trl SET Description='Wenn "true", wird der HTTP-Aufruf asynchr
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580404,'de_CH') 
 ;
 
+-- 2021-12-21T21:00:40.426Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='If ticked, the HTTP call will be processed asynchronously and the response will consist only of a "requestId".',Updated=TO_TIMESTAMP('2021-12-21 23:00:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580404 AND AD_Language='en_US'
+;
+
+-- 2021-12-21T21:00:40.480Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580404,'en_US')
+;
+
+-- 2021-12-21T21:01:10.809Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".',Updated=TO_TIMESTAMP('2021-12-21 23:01:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580404 AND AD_Language='nl_NL'
+;
+
+-- 2021-12-21T21:01:10.813Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580404,'nl_NL')
+;
+
+-- 2021-12-21T21:01:17.698Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".',Updated=TO_TIMESTAMP('2021-12-21 23:01:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580404 AND AD_Language='de_DE'
+;
+
+-- 2021-12-21T21:01:17.702Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580404,'de_DE')
+;
+
+-- 2021-12-21T21:01:17.788Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580404,'de_DE')
+;
+
+-- 2021-12-21T21:01:17.792Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='IsForceProcessedAsync', Name='Asynchrone Verarbeitung erzwingen', Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', Help=NULL WHERE AD_Element_ID=580404
+;
+
+-- 2021-12-21T21:01:17.795Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IsForceProcessedAsync', Name='Asynchrone Verarbeitung erzwingen', Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', Help=NULL, AD_Element_ID=580404 WHERE UPPER(ColumnName)='ISFORCEPROCESSEDASYNC' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2021-12-21T21:01:17.798Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='IsForceProcessedAsync', Name='Asynchrone Verarbeitung erzwingen', Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', Help=NULL WHERE AD_Element_ID=580404 AND IsCentrallyMaintained='Y'
+;
+
+-- 2021-12-21T21:01:17.800Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Asynchrone Verarbeitung erzwingen', Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=580404) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 580404)
+;
+
+-- 2021-12-21T21:01:17.865Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Asynchrone Verarbeitung erzwingen', Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 580404
+;
+
+-- 2021-12-21T21:01:17.869Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_WINDOW SET Name='Asynchrone Verarbeitung erzwingen', Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', Help=NULL WHERE AD_Element_ID = 580404
+;
+
+-- 2021-12-21T21:01:17.870Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET   Name = 'Asynchrone Verarbeitung erzwingen', Description = 'Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".', WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 580404
+;
+
+-- 2021-12-21T21:01:23.942Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='Wenn angehakt, wird der HTTP-Aufruf asynchron verarbeitet und die Antwort besteht nur aus einer "requestId".',Updated=TO_TIMESTAMP('2021-12-21 23:01:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580404 AND AD_Language='de_CH'
+;
+
+-- 2021-12-21T21:01:23.946Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580404,'de_CH')
+;
+
