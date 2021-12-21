89b68ce224c1024136600588ba317509749a2a42
Adrian <adrian.stefan@metasfresh.com>
2021-12-21 13:22:01 +0200

    #11961 changed relation type for invoices via ref_invoice_id to only consider invoices with opposite SoTrx

---
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5619740_sys_gh11961_changed_relation_between_c_invoices.sql
@@ -0,0 +1,22 @@
+-- Changed the relation between c_invoices via c_invoice.ref_invoice_id to require invoices with different SoTrx values.
+
+-- 2021-12-21T10:54:51.824Z
+-- URL zum Konzept
+UPDATE AD_RelationType SET AD_Reference_Source_ID=540739,Updated=TO_TIMESTAMP('2021-12-21 12:54:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540186
+;
+
+-- 2021-12-21T10:55:03.796Z
+-- URL zum Konzept
+UPDATE AD_RelationType SET AD_Reference_Source_ID=540744,Updated=TO_TIMESTAMP('2021-12-21 12:55:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540184
+;
+
+-- 2021-12-21T10:57:01.678Z
+-- URL zum Konzept
+UPDATE AD_RelationType SET AD_Reference_Source_ID=540744,Updated=TO_TIMESTAMP('2021-12-21 12:57:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540185
+;
+
+-- 2021-12-21T10:57:11.721Z
+-- URL zum Konzept
+UPDATE AD_RelationType SET AD_Reference_Source_ID=540739,Updated=TO_TIMESTAMP('2021-12-21 12:57:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540187
+;
+
