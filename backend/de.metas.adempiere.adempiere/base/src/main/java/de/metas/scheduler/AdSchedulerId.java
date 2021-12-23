823eabf83e408be9b900fe0cc1d773b3f5cf2a5e
dm-ps <dragos.moca@pevesoft.ro>
2021-12-23 20:22:31 +0200

    Add an action to scheduler so that we can start and stop them from the webui refs: https://github.com/metasfresh/metasfresh/issues/12237

---
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/scheduler/AdSchedulerId.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2021 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.scheduler;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import de.metas.util.Check;
+import de.metas.util.lang.RepoIdAware;
+import lombok.Value;
+
+@Value
+public class AdSchedulerId implements RepoIdAware
+{
+	@JsonCreator
+	public static AdSchedulerId ofRepoId(final int repoId)
+	{
+		return new AdSchedulerId(repoId);
+	}
+
+	int repoId;
+
+	private AdSchedulerId(final int repoId)
+	{
+		this.repoId = Check.assumeGreaterThanZero(repoId, "AD_Scheduler_ID");
+	}
+
+	@Override
+	@JsonValue
+	public int getRepoId()
+	{
+		return repoId;
+	}
+}
