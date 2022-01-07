0029be5ab00b57401d8fa41c05a115f6443a4f45
Teo Sarca <teo.sarca@metasfresh.com>
2022-01-07 11:56:36 +0200

    PrintingQueuePDFConcatenateWorkpackageProcessor QA

--- backend/de.metas.printing/de.metas.printing.base/src/main/java/de/metas/printing/async/spi/impl/PrintingQueuePDFConcatenateWorkpackageProcessor.java
+++ backend/de.metas.printing/de.metas.printing.base/src/main/java/de/metas/printing/async/spi/impl/PrintingQueuePDFConcatenateWorkpackageProcessor.java
@@ -3,6 +3,7 @@ package de.metas.printing.async.spi.impl;
 import ch.qos.logback.classic.Level;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.BadPdfFormatException;
 import com.lowagie.text.pdf.PdfCopy;
 import com.lowagie.text.pdf.PdfReader;
 import de.metas.async.api.IQueueDAO;
@@ -12,13 +13,14 @@ import de.metas.async.spi.IWorkpackageProcessor;
 import de.metas.attachments.AttachmentEntryService;
 import de.metas.logging.LogManager;
 import de.metas.logging.TableRecordMDC;
-import de.metas.printing.api.IPrintingDAO;
 import de.metas.printing.api.IPrintingQueueBL;
 import de.metas.printing.model.I_C_Printing_Queue;
 import de.metas.util.Check;
 import de.metas.util.Loggables;
 import de.metas.util.Services;
+import lombok.NonNull;
 import org.adempiere.archive.api.IArchiveBL;
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.SpringContextHolder;
 import org.compiere.model.I_AD_Archive;
 import org.slf4j.Logger;
@@ -27,6 +29,7 @@ import org.slf4j.MDC;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -43,49 +46,40 @@ public class PrintingQueuePDFConcatenateWorkpackageProcessor implements IWorkpac
 	private final Logger logger = LogManager.getLogger(PrintingQueuePDFConcatenateWorkpackageProcessor.class);
 
 	private final AttachmentEntryService attachmentEntryService = SpringContextHolder.instance.getBean(AttachmentEntryService.class);
-	private final IPrintingDAO dao = Services.get(IPrintingDAO.class);
 	private final IQueueDAO queueDAO = Services.get(IQueueDAO.class);
 	private final IPrintingQueueBL printingQueueBL = Services.get(IPrintingQueueBL.class);
-	private final IArchiveBL archiveBl = Services.get(IArchiveBL.class);
-
-	private I_C_Async_Batch asyncBatch;
+	private final IArchiveBL archiveBL = Services.get(IArchiveBL.class);
 
 	@Override
-	public Result processWorkPackage(final I_C_Queue_WorkPackage workpackage, final String localTrxName_NOTUSED)
+	public Result processWorkPackage(
+			final @NonNull I_C_Queue_WorkPackage workpackage,
+			final String localTrxName_NOTUSED)
 	{
-		File outputFile = null;
 		try
 		{
-			outputFile = concatenateFiles(workpackage);
+			final File outputFile = concatenateFiles(workpackage);
+			attachmentEntryService.createNewAttachment(workpackage.getC_Async_Batch(), outputFile);
 		}
-		catch (IOException e)
+		catch (Exception ex)
 		{
-			Loggables.withLogger(logger, Level.ERROR).addLog(e.getLocalizedMessage());
+			logger.warn("Failed concatenating files", ex);
+			Loggables.withLogger(logger, Level.ERROR).addLog(ex.getLocalizedMessage());
 		}
 
-		attachmentEntryService.createNewAttachment(workpackage.getC_Async_Batch(), outputFile);
 		return Result.SUCCESS;
 	}
 
-	private File concatenateFiles(final I_C_Queue_WorkPackage workpackage) throws IOException
+	private File concatenateFiles(final I_C_Queue_WorkPackage workpackage) throws IOException, DocumentException
 	{
-		this.asyncBatch = workpackage.getC_Async_Batch();
-		Check.assumeNotNull(asyncBatch, "Async batch is not null");
-
-		final String fileName = "PDF_" + asyncBatch.getC_Async_Batch_ID();
-
-		final File file = File.createTempFile(fileName, ".pdf");
 		final Document document = new Document();
-		final FileOutputStream fos = new FileOutputStream(file, false);
 
-		try
+		final File file = createNewTemporaryPDFFile(workpackage);
+		try (final FileOutputStream fos = new FileOutputStream(file, false))
 		{
 			final PdfCopy copy = new PdfCopy(document, fos);
-
 			document.open();
 
 			final List<I_C_Printing_Queue> pqs = queueDAO.retrieveAllItems(workpackage, I_C_Printing_Queue.class);
-
 			for (final I_C_Printing_Queue pq : pqs)
 			{
 				try (final MDC.MDCCloseable ignored = TableRecordMDC.putTableRecordReference(pq))
@@ -95,16 +89,10 @@ public class PrintingQueuePDFConcatenateWorkpackageProcessor implements IWorkpac
 						Loggables.withLogger(logger, Level.DEBUG).addLog("*** Printing queue is already processed. Skipping it: {}", pq);
 						continue;
 					}
-					final I_AD_Archive archive = pq.getAD_Archive();
-					Check.assumeNotNull(archive, "Archive references an AD_Archive record");
 
-					final byte[] data = archiveBl.getBinaryData(archive);
-					final PdfReader reader = new PdfReader(data);
+					final PdfReader reader = getPdfReader(pq);
+					appendToPdf(copy, reader);
 
-					for (int page = 0; page < reader.getNumberOfPages(); )
-					{
-						copy.addPage(copy.getImportedPage(reader, ++page));
-					}
 					copy.freeReader(reader);
 					reader.close();
 
@@ -112,17 +100,61 @@ public class PrintingQueuePDFConcatenateWorkpackageProcessor implements IWorkpac
 				}
 			}
 
-		}
-		catch (IOException | DocumentException e)
-		{
-			Loggables.withLogger(logger, Level.ERROR).addLog(e.getLocalizedMessage());
+			return file;
 		}
 		finally
 		{
 			document.close();
-			fos.close();
-			return file;
 		}
 
+	}
+
+	@NonNull
+	private File createNewTemporaryPDFFile(final I_C_Queue_WorkPackage workpackage)
+	{
+		final I_C_Async_Batch asyncBatch = workpackage.getC_Async_Batch();
+		Check.assumeNotNull(asyncBatch, "Async batch is not null");
+		final String fileName = "PDF_" + asyncBatch.getC_Async_Batch_ID();
+
+		try
+		{
+			return File.createTempFile(fileName, ".pdf");
+		}
+		catch (Exception ex)
+		{
+			throw new AdempiereException("Failed to create temporary file with prefix: " + fileName, ex);
+		}
+	}
+
+	@NonNull
+	private PdfReader getPdfReader(final I_C_Printing_Queue pq)
+	{
+		final I_AD_Archive archive = pq.getAD_Archive();
+		Check.assumeNotNull(archive, "Archive references an AD_Archive record");
+
+		final InputStream data = archiveBL.getBinaryDataAsStream(archive);
+		try
+		{
+			return new PdfReader(data);
+		}
+		catch (IOException e)
+		{
+			throw new AdempiereException("Failed to create PDF reader from archive=" + archive + ", printing queue=" + pq, e);
+		}
+	}
+
+	private void appendToPdf(final PdfCopy pdf, final PdfReader from)
+	{
+		try
+		{
+			for (int page = 0; page < from.getNumberOfPages(); )
+			{
+				pdf.addPage(pdf.getImportedPage(from, ++page));
+			}
+		}
+		catch (IOException | BadPdfFormatException e)
+		{
+			throw new AdempiereException("Failed appending to pdf=" + pdf + " from " + from, e);
+		}
 	}
 }
