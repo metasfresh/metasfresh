43e8fba94604567c3f0ed4ea9f72f47786a60456
Adrian <adrian.stefan@metasfresh.com>
2022-01-21 17:30:10 +0200

    #12360 add migration scripts

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5622670_sys_gh12360_define_shipping_costs_by_country_area.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5622670_sys_gh12360_define_shipping_costs_by_country_area.sql
@@ -1,6 +1,6 @@
 -- 2022-01-19T11:05:00.169Z
 -- URL zum Konzept
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,DDL_NoForeignKey,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,579097,541761,0,13,540005,'C_CountryArea_ID','SELECT ca.C_CountryArea_ID from C_CountryArea_Assign ca where ca.C_Country_ID = coalesce(C_Country_ID,-1) ORDER BY C_CountryArea_ID DESC LIMIT 1',TO_TIMESTAMP('2022-01-19 13:04:59','YYYY-MM-DD HH24:MI:SS'),100,'N','de.metas.swat',0,10,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','N','N',0,'Country Area',0,0,TO_TIMESTAMP('2022-01-19 13:04:59','YYYY-MM-DD HH24:MI:SS'),100,0)
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,DDL_NoForeignKey,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,579097,541761,0,13,540005,'C_CountryArea_ID','SELECT ca.C_CountryArea_ID from C_CountryArea_Assign ca where ca.C_Country_ID = coalesce(C_Country_ID,-1) ORDER BY C_CountryArea_ID DESC LIMIT 1',TO_TIMESTAMP('2022-01-19 13:04:59','YYYY-MM-DD HH24:MI:SS'),100,'N','de.metas.swat',0,10,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N',0,'Country Area',0,0,TO_TIMESTAMP('2022-01-19 13:04:59','YYYY-MM-DD HH24:MI:SS'),100,0)
 ;
 
 -- 2022-01-19T11:05:00.204Z
@@ -15,7 +15,7 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 
 -- 2022-01-19T11:05:59.243Z
 -- URL zum Konzept
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,579097,677145,0,541820,0,TO_TIMESTAMP('2022-01-19 13:05:58','YYYY-MM-DD HH24:MI:SS'),100,0,'D',0,'Y','Y','Y','N','N','N','Y','N','Country Area',0,60,0,1,1,TO_TIMESTAMP('2022-01-19 13:05:58','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,579097,677145,0,541820,0,TO_TIMESTAMP('2022-01-19 13:05:58','YYYY-MM-DD HH24:MI:SS'),100,0,'D',0,'Y','Y','Y','N','N','N','N','N','Country Area',0,60,0,1,1,TO_TIMESTAMP('2022-01-19 13:05:58','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2022-01-19T11:05:59.281Z
@@ -40,7 +40,7 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=677145
 
 -- 2022-01-19T11:06:38.302Z
 -- URL zum Konzept
-INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,677145,0,541820,542676,599544,'F',TO_TIMESTAMP('2022-01-19 13:06:37','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Country Area',60,0,0,TO_TIMESTAMP('2022-01-19 13:06:37','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,677145,0,541820,542676,599544,'F',TO_TIMESTAMP('2022-01-19 13:06:37','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Country Area',25,0,0,TO_TIMESTAMP('2022-01-19 13:06:37','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2022-01-19T11:06:49.016Z
@@ -56,11 +56,6 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP(
 -- 2022-01-19T11:49:27.254Z
 -- URL zum Konzept
 UPDATE AD_Column SET AD_Reference_ID=19, IsExcludeFromZoomTargets='N',Updated=TO_TIMESTAMP('2022-01-19 13:49:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=579097
-;
-
--- 2022-01-19T12:50:45.751Z
--- URL zum Konzept
-UPDATE AD_Column SET ColumnSQL='', IsLazyLoading='N',Updated=TO_TIMESTAMP('2022-01-19 14:50:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=579097
 ;
 
 -- 2022-01-19T12:51:49.013Z
