41d639bfc0099da9bbd95e90d659fcf44ee8c774
Teo Sarca <teo.sarca@metasfresh.com>
2022-01-21 20:04:19 +0200

    mobile UI: cleanup UI

--- misc/services/mobile-webui/mobile-webui-frontend/src/components/buttons/ConfirmButton.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/components/buttons/ConfirmButton.jsx
@@ -1,81 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import counterpart from 'counterpart';
 import Button from './Button';
 
-class ConfirmButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isPromptDialogOpen: false,
-    };
-  }
+const ConfirmButton = ({ caption, promptQuestion, isUserEditable, isCancelMode, onUserConfirmed }) => {
+  const [isDialogDisplayed, setDialogDisplayed] = useState(false);
 
-  showConfirmDialog = () => this.setState({ isPromptDialogOpen: true });
-
-  hideConfirmDialog = () => this.setState({ isPromptDialogOpen: false });
-
-  /**
-   * Execute any function passed as onConfirm prop
-   */
-  onDialogYes = () => {
-    const { onUserConfirmed } = this.props;
-    this.hideConfirmDialog();
+  const onDialogYes = () => {
+    setDialogDisplayed(false);
     onUserConfirmed();
   };
 
-  onDialogNo = () => {
-    this.hideConfirmDialog();
+  const onDialogNo = () => {
+    setDialogDisplayed(false);
   };
 
-  render() {
-    const { caption, isUserEditable, isCancelMode } = this.props;
-    const { isPromptDialogOpen } = this.state;
-    const captionEffective = caption ? caption : counterpart.translate('activities.confirmButton.default.caption');
+  const captionEffective = caption ? caption : counterpart.translate('activities.confirmButton.default.caption');
 
-    return (
-      <>
-        {isPromptDialogOpen && this.renderDialog(captionEffective)}
-        <Button
-          caption={captionEffective}
-          disabled={!isUserEditable}
-          isDanger={isCancelMode}
-          onClick={this.showConfirmDialog}
-        />
-      </>
-    );
-  }
-
-  renderDialog = (captionEffective) => {
-    const { promptQuestion } = this.props;
-
-    const promptQuestionEffective = promptQuestion
-      ? promptQuestion
-      : counterpart.translate('activities.confirmButton.default.promptQuestion');
-
-    return (
-      <div className="prompt-dialog-screen">
-        <article className="message confirm-box is-dark">
-          <div className="message-header">
-            <p>{captionEffective}</p>
-          </div>
-          <div className="message-body">
-            <strong>{promptQuestionEffective}</strong>
-            <div>&nbsp;</div>
-            <div className="buttons is-centered">
-              <button className="button is-success confirm-button" onClick={this.onDialogYes}>
-                {counterpart.translate('activities.confirmButton.default.yes')}
-              </button>
-              <button className="button is-danger confirm-button" onClick={this.onDialogNo}>
-                {counterpart.translate('activities.confirmButton.default.no')}
-              </button>
-            </div>
-          </div>
-        </article>
-      </div>
-    );
-  };
-}
+  return (
+    <>
+      {isDialogDisplayed && (
+        <Dialog caption={captionEffective} promptQuestion={promptQuestion} onYes={onDialogYes} onNo={onDialogNo} />
+      )}
+      <Button
+        caption={captionEffective}
+        disabled={!isUserEditable || isDialogDisplayed}
+        isDanger={isCancelMode}
+        onClick={() => setDialogDisplayed(true)}
+      />
+    </>
+  );
+};
 
 /**
  * @typedef {object} Props Component props
@@ -92,6 +47,41 @@ ConfirmButton.propTypes = {
   isCancelMode: PropTypes.bool,
   //
   onUserConfirmed: PropTypes.func.isRequired,
+};
+
+const Dialog = ({ caption, promptQuestion, onYes, onNo }) => {
+  const promptQuestionEffective = promptQuestion
+    ? promptQuestion
+    : counterpart.translate('activities.confirmButton.default.promptQuestion');
+
+  return (
+    <div className="prompt-dialog-screen">
+      <article className="message confirm-box is-dark">
+        <div className="message-header">
+          <p>{caption}</p>
+        </div>
+        <div className="message-body">
+          <strong>{promptQuestionEffective}</strong>
+          <div>&nbsp;</div>
+          <div className="buttons is-centered">
+            <button className="button is-success confirm-button" onClick={onYes}>
+              {counterpart.translate('activities.confirmButton.default.yes')}
+            </button>
+            <button className="button is-danger confirm-button" onClick={onNo}>
+              {counterpart.translate('activities.confirmButton.default.no')}
+            </button>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+Dialog.propTypes = {
+  caption: PropTypes.string.isRequired,
+  promptQuestion: PropTypes.string,
+  onYes: PropTypes.func.isRequired,
+  onNo: PropTypes.func.isRequired,
 };
 
 export default ConfirmButton;
