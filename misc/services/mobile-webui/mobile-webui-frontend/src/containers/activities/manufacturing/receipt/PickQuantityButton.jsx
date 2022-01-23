a7442fadabf3a2bc8b8971d79267568ca6976d2f
Teo Sarca <teo.sarca@metasfresh.com>
2022-01-23 05:32:25 +0200

    QA, L&F

--- misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/manufacturing/receipt/PickQuantityButton.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/manufacturing/receipt/PickQuantityButton.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import counterpart from 'counterpart';
 
@@ -7,70 +7,45 @@ import GetQuantityDialog from '../../../../components/dialogs/GetQuantityDialog'
 
 import { toastError } from '../../../../utils/toast';
 
-class PickQuantityButton extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDialogOpen: false,
-    };
-  }
+const PickQuantityButton = ({ qtyTarget, uom, caption, isDisabled, onClick }) => {
+  const [isDialogOpen, setDialogOpen] = React.useState(false);
 
-  onQtyPickedChanged = (qty) => {
-    const { onClick } = this.props;
-
-    const inputQty = parseFloat(qty);
-    if (isNaN(inputQty)) {
+  const onQtyPickedChanged = (qty) => {
+    const qtyEntered = parseFloat(qty);
+    if (isNaN(qtyEntered)) {
       return;
     }
 
-    const isValidQty = this.validateQtyInput(inputQty);
-    if (isValidQty) {
-      this.setState({ isDialogOpen: false });
-      onClick(inputQty);
+    if (qtyEntered >= 0 && qtyEntered <= qtyTarget) {
+      setDialogOpen(false);
+      onClick(qtyEntered);
     } else {
       toastError({ messageKey: 'activities.picking.invalidQtyPicked' });
     }
   };
 
-  validateQtyInput = (numberInput) => {
-    const { qtyTarget } = this.props;
-
-    return numberInput >= 0 && numberInput <= qtyTarget;
-  };
-
-  showDialog = () => this.setState({ isDialogOpen: true });
-
-  hideDialog = () => this.setState({ isDialogOpen: false });
-
-  render() {
-    const { qtyTarget, uom, caption, isDisabled } = this.props;
-    const { isDialogOpen } = this.state;
-
-    return (
-      <>
-        {isDialogOpen && (
-          <GetQuantityDialog
-            qtyCaption={counterpart.translate('activities.mfg.receipts.pickPromptTitle')}
-            qtyTarget={qtyTarget}
-            uom={uom}
-            onQtyChange={this.onQtyPickedChanged}
-            onCloseDialog={this.hideDialog}
-          />
-        )}
-        <Button caption={caption} onClick={this.showDialog} disabled={isDisabled} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {isDialogOpen && (
+        <GetQuantityDialog
+          qtyCaption={counterpart.translate('activities.mfg.receipts.pickPromptTitle')}
+          qtyTarget={qtyTarget}
+          uom={uom}
+          onQtyChange={onQtyPickedChanged}
+          onCloseDialog={() => setDialogOpen(false)}
+        />
+      )}
+      <Button caption={caption} onClick={() => setDialogOpen(true)} disabled={isDisabled} />
+    </>
+  );
+};
 
 PickQuantityButton.propTypes = {
-  //
-  // Props
-  onClick: PropTypes.func.isRequired,
   qtyTarget: PropTypes.number.isRequired,
   uom: PropTypes.string.isRequired,
   caption: PropTypes.string.isRequired,
   isDisabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default PickQuantityButton;
