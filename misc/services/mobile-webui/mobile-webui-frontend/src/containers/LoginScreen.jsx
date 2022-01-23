157bbdf546b68d91618ae4dd01ca2fc4e7be448a
Teo Sarca <teo.sarca@metasfresh.com>
2022-01-23 17:32:28 +0200

    QA, L&F

--- misc/services/mobile-webui/mobile-webui-frontend/src/containers/LoginScreen.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/containers/LoginScreen.jsx
@@ -5,22 +5,24 @@ import Cookies from 'js-cookie';
 import { useAuth } from '../hooks/useAuth';
 import counterpart from 'counterpart';
 import ScreenToaster from '../components/ScreenToaster';
-import { toastError } from '../utils/toast';
+import { extractUserFriendlyErrorMessageFromAxiosError } from '../utils/toast';
 
 import LogoHeader from '../components/LogoHeader';
 
 const LoginScreen = () => {
-  const history = useHistory();
-  const location = useLocation();
-  const auth = useAuth();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loginPending, setLoginPending] = useState(false);
+
+  const history = useHistory();
+  const auth = useAuth();
+  const location = useLocation();
   const { from } = location.state || { from: { pathname: '/' } };
-
   const usernameFieldRef = useRef(null);
-
   useEffect(() => {
     const token = Cookies.get('Token');
+    console.log('useEffect: token', token);
 
     if (token) {
       auth.localLogin({ token }).then(() => history.replace(from));
@@ -30,35 +32,39 @@ const LoginScreen = () => {
     }
   }, []);
 
-  const changeUsername = (e) => setUsername(e.target.value);
-  const changePassword = (e) => setPassword(e.target.value);
-
   const submitForm = (e) => {
     e.preventDefault();
-    if (username && password) {
-      auth
-        .login(username, password)
-        .then(() => history.replace(from))
-        .catch((axiosError) => toastError({ axiosError }));
-    }
+
+    setLoginPending(true);
+    auth
+      .login(username, password)
+      .then(() => history.replace(from))
+      .catch((axiosError) => {
+        setLoginPending(false);
+        setErrorMessage(extractUserFriendlyErrorMessageFromAxiosError({ axiosError }));
+      });
+    // .finally(() => setLoginPending(false)); // don't set it here because at this point the component is already unmounted
   };
 
   return (
-    <div>
+    <div className="login-view">
       <LogoHeader />
-      <div className="section login_screen">
-        <div className="container p-6 login-view">
+      <div className="section is-size-5">
+        <div className="container px-6">
           <form>
+            <p className="help is-danger is-size-6 login-error">{errorMessage}</p>
             <div className="field">
               <p className="control has-icons-left">
                 <input
                   className="input is-medium"
                   type="text"
+                  id={username}
                   name="username"
                   value={username}
                   autoComplete="username"
                   ref={usernameFieldRef}
-                  onChange={changeUsername}
+                  disabled={loginPending}
+                  onInput={(e) => setUsername(e.target.value)}
                 />
                 <span className="icon is-small is-left">
                   <i className="fas fa-user" />
@@ -74,7 +80,8 @@ const LoginScreen = () => {
                   name="password"
                   value={password}
                   autoComplete="current-password"
-                  onChange={changePassword}
+                  disabled={loginPending}
+                  onInput={(e) => setPassword(e.target.value)}
                 />
                 <span className="icon is-small is-left">
                   <i className="fas fa-lock" />
@@ -83,7 +90,12 @@ const LoginScreen = () => {
             </div>
             <div className="field">
               <div className="control">
-                <button type="submit" className="button is-medium btn-green" onClick={submitForm}>
+                <button
+                  type="submit"
+                  className="button is-medium btn-green"
+                  disabled={loginPending}
+                  onClick={submitForm}
+                >
                   {counterpart.translate('login.submitButton')}
                 </button>
               </div>
