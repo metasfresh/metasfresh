1d40fb7727016497c4b295c64dc1185452794154
adi-stefan <60300682+adi-stefan@users.noreply.github.com>
2022-01-24 07:53:39 -0800

    #12360 implemented freight cost by country area, not just by country (#12370)
    * #12360 implemented freight cost by country area, not just by country
    
    * #12360 add migration scripts
    
    * #12360 add FreightCostDetail.SeqNo

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/adempiere/model/X_M_FreightCostDetail.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/adempiere/model/X_M_FreightCostDetail.java
@@ -1,51 +1,88 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+// Generated Model - DO NOT CHANGE
 package org.adempiere.model;
 
-/** Generated Model - DO NOT CHANGE */
-
-
+import javax.annotation.Nullable;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.Properties;
 
 /** Generated Model for M_FreightCostDetail
- *  @author Adempiere (generated)
+ *  @author metasfresh (generated)
  */
-@SuppressWarnings("javadoc")
+@SuppressWarnings("unused")
 public class X_M_FreightCostDetail extends org.compiere.model.PO implements I_M_FreightCostDetail, org.compiere.model.I_Persistent
 {
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 142781241L;
+	private static final long serialVersionUID = 1629601490L;
 
     /** Standard Constructor */
-    public X_M_FreightCostDetail (Properties ctx, int M_FreightCostDetail_ID, String trxName)
+    public X_M_FreightCostDetail (final Properties ctx, final int M_FreightCostDetail_ID, @Nullable final String trxName)
     {
       super (ctx, M_FreightCostDetail_ID, trxName);
-      /** if (M_FreightCostDetail_ID == 0)
-        {
-			setFreightAmt (BigDecimal.ZERO);
-			setM_FreightCostDetail_ID (0);
-			setM_FreightCostShipper_ID (0);
-			setShipmentValueAmt (BigDecimal.ZERO);
-        } */
     }
 
     /** Load Constructor */
-    public X_M_FreightCostDetail (Properties ctx, ResultSet rs, String trxName)
+    public X_M_FreightCostDetail (final Properties ctx, final ResultSet rs, @Nullable final String trxName)
     {
       super (ctx, rs, trxName);
     }
 
 
-    /** Load Meta Data */
-    @Override
-    protected org.compiere.model.POInfo initPO (Properties ctx)
-    {
-      org.compiere.model.POInfo poi = org.compiere.model.POInfo.getPOInfo (ctx, Table_Name, get_TrxName());
-      return poi;
-    }
+	/** Load Meta Data */
+	@Override
+	protected org.compiere.model.POInfo initPO(final Properties ctx)
+	{
+		return org.compiere.model.POInfo.getPOInfo(Table_Name);
+	}
+
+	@Override
+	public org.compiere.model.I_C_CountryArea getC_CountryArea()
+	{
+		return get_ValueAsPO(COLUMNNAME_C_CountryArea_ID, org.compiere.model.I_C_CountryArea.class);
+	}
+
+	@Override
+	public void setC_CountryArea(final org.compiere.model.I_C_CountryArea C_CountryArea)
+	{
+		set_ValueFromPO(COLUMNNAME_C_CountryArea_ID, org.compiere.model.I_C_CountryArea.class, C_CountryArea);
+	}
+
+	@Override
+	public void setC_CountryArea_ID (final int C_CountryArea_ID)
+	{
+		if (C_CountryArea_ID < 1)
+			set_ValueNoCheck (COLUMNNAME_C_CountryArea_ID, null);
+		else
+			set_ValueNoCheck (COLUMNNAME_C_CountryArea_ID, C_CountryArea_ID);
+	}
+
+	@Override
+	public int getC_CountryArea_ID()
+	{
+		return get_ValueAsInt(COLUMNNAME_C_CountryArea_ID);
+	}
 
 	@Override
 	public org.compiere.model.I_C_Country getC_Country()
@@ -54,103 +91,67 @@ public class X_M_FreightCostDetail extends org.compiere.model.PO implements I_M_
 	}
 
 	@Override
-	public void setC_Country(org.compiere.model.I_C_Country C_Country)
+	public void setC_Country(final org.compiere.model.I_C_Country C_Country)
 	{
 		set_ValueFromPO(COLUMNNAME_C_Country_ID, org.compiere.model.I_C_Country.class, C_Country);
 	}
 
-	/** Set Land.
-		@param C_Country_ID
-		Land
-	  */
 	@Override
-	public void setC_Country_ID (int C_Country_ID)
+	public void setC_Country_ID (final int C_Country_ID)
 	{
 		if (C_Country_ID < 1)
 			set_Value (COLUMNNAME_C_Country_ID, null);
 		else
-			set_Value (COLUMNNAME_C_Country_ID, Integer.valueOf(C_Country_ID));
+			set_Value (COLUMNNAME_C_Country_ID, C_Country_ID);
 	}
 
-	/** Get Land.
-		@return Land
-	  */
 	@Override
-	public int getC_Country_ID ()
+	public int getC_Country_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_C_Country_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_C_Country_ID);
 	}
 
-	/** Set Währung.
-		@param C_Currency_ID
-		Die Währung für diesen Eintrag
-	  */
 	@Override
-	public void setC_Currency_ID (int C_Currency_ID)
+	public void setC_Currency_ID (final int C_Currency_ID)
 	{
 		if (C_Currency_ID < 1)
 			set_Value (COLUMNNAME_C_Currency_ID, null);
 		else
-			set_Value (COLUMNNAME_C_Currency_ID, Integer.valueOf(C_Currency_ID));
+			set_Value (COLUMNNAME_C_Currency_ID, C_Currency_ID);
 	}
 
-	/** Get Währung.
-		@return Die Währung für diesen Eintrag
-	  */
 	@Override
-	public int getC_Currency_ID ()
+	public int getC_Currency_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_C_Currency_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_C_Currency_ID);
 	}
 
-	/** Set Frachtbetrag.
-		@param FreightAmt
-		Frachtbetrag
-	  */
 	@Override
-	public void setFreightAmt (java.math.BigDecimal FreightAmt)
+	public void setFreightAmt (final BigDecimal FreightAmt)
 	{
 		set_Value (COLUMNNAME_FreightAmt, FreightAmt);
 	}
 
-	/** Get Frachtbetrag.
-		@return Frachtbetrag
-	  */
 	@Override
-	public java.math.BigDecimal getFreightAmt ()
+	public BigDecimal getFreightAmt()
 	{
-		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_FreightAmt);
-		if (bd == null)
-			 return BigDecimal.ZERO;
-		return bd;
+		final BigDecimal bd = get_ValueAsBigDecimal(COLUMNNAME_FreightAmt);
+		return bd != null ? bd : BigDecimal.ZERO;
 	}
 
-	/** Set Frachtkostendetail.
-		@param M_FreightCostDetail_ID Frachtkostendetail	  */
 	@Override
-	public void setM_FreightCostDetail_ID (int M_FreightCostDetail_ID)
+	public void setM_FreightCostDetail_ID (final int M_FreightCostDetail_ID)
 	{
 		if (M_FreightCostDetail_ID < 1)
 			set_ValueNoCheck (COLUMNNAME_M_FreightCostDetail_ID, null);
 		else
-			set_ValueNoCheck (COLUMNNAME_M_FreightCostDetail_ID, Integer.valueOf(M_FreightCostDetail_ID));
+			set_ValueNoCheck (COLUMNNAME_M_FreightCostDetail_ID, M_FreightCostDetail_ID);
 	}
 
-	/** Get Frachtkostendetail.
-		@return Frachtkostendetail	  */
 	@Override
-	public int getM_FreightCostDetail_ID ()
+	public int getM_FreightCostDetail_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_M_FreightCostDetail_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_M_FreightCostDetail_ID);
 	}
 
 	@Override
@@ -160,49 +161,48 @@ public class X_M_FreightCostDetail extends org.compiere.model.PO implements I_M_
 	}
 
 	@Override
-	public void setM_FreightCostShipper(org.adempiere.model.I_M_FreightCostShipper M_FreightCostShipper)
+	public void setM_FreightCostShipper(final org.adempiere.model.I_M_FreightCostShipper M_FreightCostShipper)
 	{
 		set_ValueFromPO(COLUMNNAME_M_FreightCostShipper_ID, org.adempiere.model.I_M_FreightCostShipper.class, M_FreightCostShipper);
 	}
 
-	/** Set Lieferweg-Versandkosten.
-		@param M_FreightCostShipper_ID Lieferweg-Versandkosten	  */
 	@Override
-	public void setM_FreightCostShipper_ID (int M_FreightCostShipper_ID)
+	public void setM_FreightCostShipper_ID (final int M_FreightCostShipper_ID)
 	{
 		if (M_FreightCostShipper_ID < 1)
 			set_ValueNoCheck (COLUMNNAME_M_FreightCostShipper_ID, null);
 		else
-			set_ValueNoCheck (COLUMNNAME_M_FreightCostShipper_ID, Integer.valueOf(M_FreightCostShipper_ID));
+			set_ValueNoCheck (COLUMNNAME_M_FreightCostShipper_ID, M_FreightCostShipper_ID);
 	}
 
-	/** Get Lieferweg-Versandkosten.
-		@return Lieferweg-Versandkosten	  */
 	@Override
-	public int getM_FreightCostShipper_ID ()
+	public int getM_FreightCostShipper_ID()
 	{
-		Integer ii = (Integer)get_Value(COLUMNNAME_M_FreightCostShipper_ID);
-		if (ii == null)
-			 return 0;
-		return ii.intValue();
+		return get_ValueAsInt(COLUMNNAME_M_FreightCostShipper_ID);
 	}
 
-	/** Set Lieferwert.
-		@param ShipmentValueAmt Lieferwert	  */
 	@Override
-	public void setShipmentValueAmt (java.math.BigDecimal ShipmentValueAmt)
+	public void setSeqNo (final int SeqNo)
+	{
+		set_Value (COLUMNNAME_SeqNo, SeqNo);
+	}
+
+	@Override
+	public int getSeqNo()
+	{
+		return get_ValueAsInt(COLUMNNAME_SeqNo);
+	}
+
+	@Override
+	public void setShipmentValueAmt (final BigDecimal ShipmentValueAmt)
 	{
 		set_Value (COLUMNNAME_ShipmentValueAmt, ShipmentValueAmt);
 	}
 
-	/** Get Lieferwert.
-		@return Lieferwert	  */
 	@Override
-	public java.math.BigDecimal getShipmentValueAmt ()
+	public BigDecimal getShipmentValueAmt()
 	{
-		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_ShipmentValueAmt);
-		if (bd == null)
-			 return BigDecimal.ZERO;
-		return bd;
+		final BigDecimal bd = get_ValueAsBigDecimal(COLUMNNAME_ShipmentValueAmt);
+		return bd != null ? bd : BigDecimal.ZERO;
 	}
 }
