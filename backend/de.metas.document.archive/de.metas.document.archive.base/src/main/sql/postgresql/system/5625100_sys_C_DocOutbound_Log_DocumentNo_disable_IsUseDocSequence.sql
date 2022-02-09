95de6f42e94698736861b1184f866b423d38fbe0
Teo Sarca <teo.sarca@metasfresh.com>
2022-02-09 14:25:03 +0200

    avoid errors when C_DocOutbound_Log.DocumentNo is not set
    i.e. avoid
    ```
    022-02-09 14:06:17.360  WARN 23548 --- [nio-8282-exec-5] d.m.d.sequence.impl.DocumentNoBuilder    : Failed building the sequence. Returning NO_DOCUMENTNO
    
    de.metas.document.DocumentNoBuilderException: Ihr Test hat einen bisher unentdeckten Fehler offengelegt.
    Bitte leiten Sie diese Meldung an metas weiter:
    
    Assumption failure: C_DocType_ID > 0
        at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
        at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)
        at java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
        at java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)
        at java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)
        at de.metas.util.Check.mkEx(Check.java:112)
        at de.metas.util.Check.throwOrLogEx(Check.java:126)
        at de.metas.util.Check.assume(Check.java:172)
        at de.metas.document.sequence.impl.DocumentNoBuilder.retrieveDocumentSequenceInfoByDocTypeId(DocumentNoBuilder.java:497)
        at de.metas.document.sequence.impl.DocumentNoBuilder.lambda$setDocumentSequenceByDocTypeId$1(DocumentNoBuilder.java:478)
        at de.metas.document.sequence.impl.DocumentNoBuilder.getDocumentSequenceInfo(DocumentNoBuilder.java:460)
        at de.metas.document.sequence.impl.DocumentNoBuilder.buildParts(DocumentNoBuilder.java:129)
        at de.metas.document.sequence.impl.DocumentNoBuilder.build(DocumentNoBuilder.java:102)
        at org.compiere.model.PO.saveNew(PO.java:3597)
        at org.compiere.model.PO.save0(PO.java:2983)
        at org.compiere.model.PO.access$100(PO.java:148)
        at org.compiere.model.PO$1.run(PO.java:2886)
        at org.adempiere.ad.trx.api.impl.TrxCallableWrappers$3.call(TrxCallableWrappers.java:147)
        at org.adempiere.ad.trx.api.impl.TrxCallableWrappers$3.call(TrxCallableWrappers.java:137)
        at org.adempiere.ad.trx.api.impl.AbstractTrxManager.call0(AbstractTrxManager.java:753)
        at org.adempiere.ad.trx.api.impl.AbstractTrxManager.call(AbstractTrxManager.java:666)
        at org.adempiere.ad.trx.api.impl.AbstractTrxManager.call(AbstractTrxManager.java:567)
        at org.adempiere.ad.trx.api.impl.AbstractTrxManager.call(AbstractTrxManager.java:498)
        at org.adempiere.ad.trx.api.impl.AbstractTrxManager.run(AbstractTrxManager.java:484)
        at org.compiere.model.PO.saveEx(PO.java:2880)
        at org.adempiere.model.POWrapper.save(POWrapper.java:794)
        at org.adempiere.model.InterfaceWrapperHelper.save(InterfaceWrapperHelper.java:615)
        at de.metas.document.archive.spi.impl.DocOutboundArchiveEventListener.createLog(DocOutboundArchiveEventListener.java:251)
        at de.metas.document.archive.spi.impl.DocOutboundArchiveEventListener.createLogLine(DocOutboundArchiveEventListener.java:187)
        at de.metas.document.archive.spi.impl.DocOutboundArchiveEventListener.onPrintOut(DocOutboundArchiveEventListener.java:147)
    ```

---
+++ backend/de.metas.document.archive/de.metas.document.archive.base/src/main/sql/postgresql/system/5625100_sys_C_DocOutbound_Log_DocumentNo_disable_IsUseDocSequence.sql
@@ -0,0 +1,10 @@
+-- 2022-02-09T12:14:57.353Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsUseDocSequence='N',Updated=TO_TIMESTAMP('2022-02-09 14:14:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=548162
+;
+
+-- 2022-02-09T12:15:48.181Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsUseDocSequence='N',Updated=TO_TIMESTAMP('2022-02-09 14:15:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=548179
+;
+
