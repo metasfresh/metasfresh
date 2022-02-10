d7521b56ddfc254f25be8a919c2074b4c422dbf1
Cristina Ghita <cristina.ghita@metasfresh.com>
2022-02-10 18:56:07 +0200

    Implement simple java process

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_PrintQRCodeLabel.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_PrintQRCodeLabel.java
@@ -4,15 +4,15 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import de.metas.Profiles;
 import de.metas.handlingunits.HuId;
+import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.qrcodes.model.HUQRCode;
 import de.metas.handlingunits.qrcodes.service.HUQRCodeGenerateForExistingHUsRequest;
 import de.metas.handlingunits.qrcodes.service.HUQRCodesService;
-import de.metas.handlingunits.report.HUToReport;
-import de.metas.process.IProcessPrecondition;
-import de.metas.process.ProcessPreconditionsResolution;
+import de.metas.process.JavaProcess;
 import de.metas.report.server.OutputType;
-import de.metas.ui.web.handlingunits.HUEditorProcessTemplate;
+import de.metas.util.Services;
 import lombok.NonNull;
+import org.adempiere.ad.dao.IQueryBL;
 import org.compiere.SpringContextHolder;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
@@ -39,32 +39,10 @@ import org.springframework.core.io.Resource;
  * #L%
  */
 @Profile(Profiles.PROFILE_Webui)
-public class WEBUI_M_HU_PrintQRCodeLabel
-		extends HUEditorProcessTemplate
-		implements IProcessPrecondition
+public class WEBUI_M_HU_PrintQRCodeLabel extends JavaProcess
 {
+	final IQueryBL queryBL = Services.get(IQueryBL.class);
 	private final HUQRCodesService huQRCodesService = SpringContextHolder.instance.getBean(HUQRCodesService.class);
-
-	@Override
-	public ProcessPreconditionsResolution checkPreconditionsApplicable()
-	{
-		if (!isHUEditorView())
-		{
-			return ProcessPreconditionsResolution.rejectWithInternalReason("not the HU view");
-		}
-
-		if (!getSelectedRowIds().isSingleDocumentId())
-		{
-			return ProcessPreconditionsResolution.rejectBecauseNoSelection();
-		}
-
-		if (getHuIdsToPrint().isEmpty())
-		{
-			return ProcessPreconditionsResolution.rejectWithInternalReason("current HU's type does not match the receipt label process");
-		}
-
-		return ProcessPreconditionsResolution.accept();
-	}
 
 	@Override
 	protected String doIt()
@@ -80,12 +58,10 @@ public class WEBUI_M_HU_PrintQRCodeLabel
 
 	private ImmutableSet<HuId> getHuIdsToPrint()
 	{
-		return getSingleSelectedRow()
-				.getAsHUToReport()
-				.streamRecursively()
-				.map(HUToReport::getHUId)
-				.distinct()
-				.collect(ImmutableSet.toImmutableSet());
+		return queryBL.createQueryBuilder(I_M_HU.class).setOnlySelection(getPinstanceId())
+				.orderBy(I_M_HU.COLUMNNAME_M_HU_ID)
+				.create()
+				.listIds(HuId::ofRepoId);
 	}
 
 	private ImmutableList<HUQRCode> generateQrCodes(@NonNull final ImmutableSet<HuId> huIds)
