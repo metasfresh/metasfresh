0aa303b69660833eaa143a80deffc12bf9e9dd9d
Teo Sarca <teo.sarca@metasfresh.com>
2022-02-11 09:42:29 +0200

    QA

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/report/HUReportService.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/report/HUReportService.java
@@ -1,20 +1,7 @@
 package de.metas.handlingunits.report;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-
-import javax.annotation.Nullable;
-
-import org.adempiere.service.ISysConfigBL;
-import org.compiere.util.DisplayType;
-import org.compiere.util.Env;
-import org.slf4j.Logger;
-
-import com.google.common.collect.ImmutableList;
-
 import ch.qos.logback.classic.Level;
+import com.google.common.collect.ImmutableList;
 import de.metas.handlingunits.model.X_M_HU_PI_Version;
 import de.metas.handlingunits.process.api.HUProcessDescriptor;
 import de.metas.handlingunits.process.api.IMHUProcessDAO;
@@ -23,7 +10,17 @@ import de.metas.process.AdProcessId;
 import de.metas.util.ILoggable;
 import de.metas.util.Loggables;
 import de.metas.util.Services;
+import de.metas.util.StringUtils;
 import lombok.NonNull;
+import org.adempiere.service.ISysConfigBL;
+import org.compiere.util.Env;
+import org.slf4j.Logger;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
 /*
  * #%L
@@ -79,11 +76,6 @@ public class HUReportService
 	{
 	}
 
-	/**
-	 * Return the AD_Process_ID that was configured via {@code SysConfig} {@value #SYSCONFIG_RECEIPT_LABEL_AUTO_PRINT_PROCESS_ID}, if any.
-	 *
-	 * @return AD_Process_ID or <code>-1</code>
-	 */
 	public AdProcessId retrievePrintReceiptLabelProcessIdOrNull()
 	{
 		return retrieveProcessIdBySysConfig(SYSCONFIG_RECEIPT_LABEL_PROCESS_ID);
@@ -142,15 +134,13 @@ public class HUReportService
 	{
 		final ISysConfigBL sysConfigBL = Services.get(ISysConfigBL.class);
 		final Properties ctx = Env.getCtx();
-		final int copies = sysConfigBL.getIntValue(sysConfigName, 1, Env.getAD_Client_ID(ctx), Env.getAD_Org_ID(ctx));
-		return copies;
+		return sysConfigBL.getIntValue(sysConfigName, 1, Env.getAD_Client_ID(ctx), Env.getAD_Org_ID(ctx));
 	}
 
 	/**
 	 * Checks the sysconfig and returns true or if receipt label auto printing is enabled in general or for the given HU's C_BPartner_ID.
 	 *
 	 * @param vendorBPartnerId the original vendor. By now, might not be the same as M_HU.C_BPartner_ID anymore
-	 * @return
 	 */
 	public boolean isReceiptLabelAutoPrintEnabled(final int vendorBPartnerId)
 	{
@@ -163,13 +153,13 @@ public class HUReportService
 
 		if (!"NOT_SET".equals(valueForBPartner))
 		{
-			return DisplayType.toBoolean(valueForBPartner, false);
+			return StringUtils.toBoolean(valueForBPartner);
 		}
 
 		final String genericValue = sysConfigBL.getValue(SYSCONFIG_RECEIPT_LABEL_AUTO_PRINT_ENABLED, "N", Env.getAD_Client_ID(ctx), Env.getAD_Org_ID(ctx));
 		logger.info("SysConfig {}={};", SYSCONFIG_RECEIPT_LABEL_AUTO_PRINT_ENABLED, genericValue);
 
-		return DisplayType.toBoolean(genericValue, false);
+		return StringUtils.toBoolean(genericValue);
 	}
 
 	public boolean isPickingLabelAutoPrintEnabled()
@@ -181,7 +171,7 @@ public class HUReportService
 		final String genericValue = sysConfigBL.getValue(SYSCONFIG_PICKING_LABEL_AUTO_PRINT_ENABLED, "N", Env.getAD_Client_ID(ctx), Env.getAD_Org_ID(ctx));
 		logger.info("SysConfig {}={};", SYSCONFIG_PICKING_LABEL_AUTO_PRINT_ENABLED, genericValue);
 
-		return DisplayType.toBoolean(genericValue, false);
+		return StringUtils.toBoolean(genericValue);
 	}
 
 	public List<HUToReport> getHUsToProcess(@NonNull final Set<HUToReport> husToCheck)
@@ -242,10 +232,6 @@ public class HUReportService
 	 * This will happen even though we have, for instance, just 1 TU and some LUs selected.
 	 *
 	 * The HUs to have the processes applied will be the 1 TU and the included TUs of the selected LUs
-	 *
-	 * @param huUnitType
-	 * @param luHUs
-	 * @param tuHUs
 	 */
 	private static List<HUToReport> extractHUsToProcess(final String huUnitType, final List<HUToReport> luHUs, final List<HUToReport> tuHUs, final List<HUToReport> cuHUs)
 	{
@@ -255,7 +241,7 @@ public class HUReportService
 			return cuHUs;
 		}
 
-		// In case we the unit type is LU we just have to process the LUs
+		// In case the unit type is LU we just have to process the LUs
 		else if (X_M_HU_PI_Version.HU_UNITTYPE_LoadLogistiqueUnit.equals(huUnitType))
 		{
 			return luHUs;
@@ -264,7 +250,7 @@ public class HUReportService
 		// In case the unit type is TU we have 2 possibilities:
 		else
 		{
-			// In case the are no selected LUs, simply return the TUs
+			// In case there are no selected LUs, simply return the TUs
 			if (luHUs.isEmpty())
 			{
 				return tuHUs;
