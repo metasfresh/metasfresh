1a16d0ba20d34e1225e7f2c5a68b4e0a7829c9c4
Cristina Ghita <cristina.ghita@metasfresh.com>
2022-02-14 14:44:06 +0200

    #12518 Add incoterms into aggregation

---
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/47-de.metas.invoicecandidate/5625780_sys_gh12518_AddIncotermsIntoAggregation.sql
@@ -0,0 +1,20 @@
+-- 2022-02-14T12:39:13.353Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO C_AggregationItem (AD_Client_ID,AD_Column_ID,AD_Org_ID,C_Aggregation_ID,C_AggregationItem_ID,Created,CreatedBy,EntityType,IsActive,Type,Updated,UpdatedBy) VALUES (1000000,578583,1000000,1000000,540051,TO_TIMESTAMP('2022-02-14 14:39:13','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.invoicecandidate','Y','COL',TO_TIMESTAMP('2022-02-14 14:39:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-02-14T12:39:13.545Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO C_Queue_Block (AD_Client_ID,AD_Org_ID,C_Queue_Block_ID,C_Queue_PackageProcessor_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (1000000,1000000,562067,540028,TO_TIMESTAMP('2022-02-14 14:39:13','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2022-02-14 14:39:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-02-14T12:42:18.684Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO C_AggregationItem (AD_Client_ID,AD_Column_ID,AD_Org_ID,C_Aggregation_ID,C_AggregationItem_ID,Created,CreatedBy,EntityType,IsActive,Type,Updated,UpdatedBy) VALUES (1000000,579246,1000000,1000000,540052,TO_TIMESTAMP('2022-02-14 14:42:18','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.invoicecandidate','Y','COL',TO_TIMESTAMP('2022-02-14 14:42:18','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-02-14T12:42:18.778Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO C_Queue_Block (AD_Client_ID,AD_Org_ID,C_Queue_Block_ID,C_Queue_PackageProcessor_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (1000000,1000000,562068,540028,TO_TIMESTAMP('2022-02-14 14:42:18','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2022-02-14 14:42:18','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
