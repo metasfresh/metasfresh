3620bf9d9f58ee1988e72aa20092534b9b94180a
fp-ps <fp-ps@pevesoft.ro>
2022-02-15 18:00:53 +0200

    refs: https://github.com/metasfresh/metasfresh/issues/12529

--- backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5625970_sys_gh12529_ExternalSystem_Config_RabbitMQ_HTTP_auto_send_configs.sql
+++ backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5625970_sys_gh12529_ExternalSystem_Config_RabbitMQ_HTTP_auto_send_configs.sql
@@ -363,3 +363,8 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,Description,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,680055,0,544410,601119,546533,'F',TO_TIMESTAMP('2022-02-15 09:38:38','YYYY-MM-DD HH24:MI:SS'),100,'Wenn ein Geschäftspartner von einem Benutzer aus der angegebenen Gruppe erstellt wurde, wird er automatisch gesendet.','Y','N','N','Y','N','N','N',0,'Erstellt von Nutzergrp. autom. senden',30,0,0,TO_TIMESTAMP('2022-02-15 09:38:38','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+-- 2022-02-15T15:34:42.045Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET DisplayLogic='@IsSyncBPartnersToRabbitMQ@=''Y''',Updated=TO_TIMESTAMP('2022-02-15 17:34:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=680055
+;
+
