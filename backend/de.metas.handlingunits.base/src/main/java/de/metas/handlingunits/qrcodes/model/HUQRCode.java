2e90b8c603375ec7eec1e46edb3716b38ea77141
Teo Sarca <teo.sarca@metasfresh.com>
2022-02-17 13:23:24 +0200

    M_HU_QR_Code: introduce RenderedQRCode and DisplayableQRCode
    (cherry picked from commit 0ae9100b437ceb2429129842fd9180ef977677b7)

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/qrcodes/model/HUQRCode.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/qrcodes/model/HUQRCode.java
@@ -2,6 +2,7 @@ package de.metas.handlingunits.qrcodes.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.collect.ImmutableList;
+import de.metas.global_qrcodes.GlobalQRCode;
 import de.metas.global_qrcodes.PrintableQRCode;
 import de.metas.handlingunits.qrcodes.model.json.HUQRCodeJsonConverter;
 import de.metas.handlingunits.qrcodes.model.json.JsonRenderedHUQRCode;
@@ -33,10 +34,25 @@ public class HUQRCode
 		return Objects.equals(o1, o2);
 	}
 
+	public static HUQRCode fromGlobalQRCodeJsonString(@NonNull final String qrCodeString)
+	{
+		return HUQRCodeJsonConverter.fromGlobalQRCodeJsonString(qrCodeString);
+	}
+
 	@JsonIgnore
 	public JsonRenderedHUQRCode toRenderedJson()
 	{
 		return HUQRCodeJsonConverter.toRenderedJson(this);
+	}
+
+	public GlobalQRCode toGlobalQRCode()
+	{
+		return HUQRCodeJsonConverter.toGlobalQRCode(this);
+	}
+
+	public String toDisplayableQRCode()
+	{
+		return id.getDisplayableSuffix();
 	}
 
 	public PrintableQRCode toPrintableQRCode()
@@ -69,6 +85,6 @@ public class HUQRCode
 
 	private static String extractPrintableBottomText(final HUQRCode qrCode)
 	{
-		return qrCode.getPackingInfo().getHuUnitType().getShortDisplayName() + " ..." + qrCode.getId().getDisplayableSuffix();
+		return qrCode.getPackingInfo().getHuUnitType().getShortDisplayName() + " ..." + qrCode.toDisplayableQRCode();
 	}
 }
