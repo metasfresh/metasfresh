07ed17badb5a3fe1bb09c316945a6561335da22b
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-02-18 14:51:16 +0200

    Import prices improvements https://github.com/metasfresh/metasfresh/issues/12127

--- backend/de.metas.business/src/main/java/de/metas/product/impexp/MProductImportTableSqlUpdater.java
+++ backend/de.metas.business/src/main/java/de/metas/product/impexp/MProductImportTableSqlUpdater.java
@@ -601,6 +601,7 @@ public class MProductImportTableSqlUpdater
 				.append(targetTableName + " i ")
 				.append(" SET " + COLUMNNAME_I_IsImported + "='E', " + COLUMNNAME_I_ErrorMsg + "=" + COLUMNNAME_I_ErrorMsg + "||'ERR=UPC not unique,' ")
 				.append("WHERE " + COLUMNNAME_I_IsImported + "<>'Y'")
+				.append(" AND ").append(I_I_Product.COLUMNNAME_IsScalePrice).append(" <>'Y'")
 				.append(" AND UPC IN (SELECT UPC FROM I_Product ii WHERE i.AD_Client_ID=ii.AD_Client_ID GROUP BY UPC HAVING COUNT(*) > 1)")
 				.append(selection.toSqlWhereClause("i"));
 		DB.executeUpdateEx(sql.toString(), ITrx.TRXNAME_ThreadInherited);
