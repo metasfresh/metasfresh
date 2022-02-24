b0122ffee5b728f5a1ba5c53c18ce8290018cccd
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-02-24 17:14:03 +0100

    Fixes for dropship delivery and material tracking
    * M_ReceiptSchedule_Generate_M_InOuts fix bug with diverging trxNames
    * don't try to validate HUs if there are none because the receipt-schedule is packaging-only
    * fix NPE when checking if AbstractProductionMaterial is a byProduct
    * Fix bpartner in dropship purchase order lines
      * For dropship-*purchase*-orders, the dropship-partner may not be propagate to the porder lines
    * ..also minor improvements

--- backend/de.metas.business/src/main/java/de/metas/order/location/adapter/OrderLineMainLocationAdapter.java
+++ backend/de.metas.business/src/main/java/de/metas/order/location/adapter/OrderLineMainLocationAdapter.java
@@ -101,7 +101,9 @@ public class OrderLineMainLocationAdapter implements IDocumentLocationAdapter
 
 	public void setFromOrderHeader(@NonNull final I_C_Order order)
 	{
-		final DocumentLocation orderLocation = order.isDropShip()
+		final boolean useDropshiplocation = order.isDropShip() && order.isSOTrx();
+		
+		final DocumentLocation orderLocation = useDropshiplocation
 				? OrderDocumentLocationAdapterFactory.deliveryLocationAdapter(order).toDocumentLocation()
 				: OrderDocumentLocationAdapterFactory.locationAdapter(order).toDocumentLocation();
 
