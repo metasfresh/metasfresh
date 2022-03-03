5b799dd0e077e6190c3dd8ca1629e97d72e3a17c
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-03-03 18:19:48 +0200

    Bring back standard invoice cand aggregator
    https://github.com/metasfresh/metasfresh/issues/12624

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5628710_sys_gh12624_Bring_Back_Standard_InvoiceCand_Aggregator.sql
@@ -0,0 +1,14 @@
+
+
+-- 2022-03-03T15:26:18.401Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_Invoice_Candidate_Agg SET Classname='de.metas.invoicecandidate.spi.impl.aggregator.standard.DefaultAggregator',Updated=TO_TIMESTAMP('2022-03-03 17:26:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Invoice_Candidate_Agg_ID=540000
+;
+
+
+
+-- 2022-03-03T15:27:38.876Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_Invoice_Candidate_Agg SET SeqNo=80,Updated=TO_TIMESTAMP('2022-03-03 17:27:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Invoice_Candidate_Agg_ID=540000
+;
+
