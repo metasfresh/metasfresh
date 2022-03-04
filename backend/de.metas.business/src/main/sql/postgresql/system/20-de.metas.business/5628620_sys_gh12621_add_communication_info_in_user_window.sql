ae71cdde5735a71331ca5622edfc14dfadeb33d5
Chems Eddine Nouioua <chems.nouioua@gmail.com>
2022-03-04 12:27:38 +0100

    fix migration script

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5628620_sys_gh12621_add_communication_info_in_user_window.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5628620_sys_gh12621_add_communication_info_in_user_window.sql
@@ -1,11 +1,33 @@
+-- 2022-03-04T11:16:19.982Z
+-- URL zum Konzept
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,580648,0,TO_TIMESTAMP('2022-03-04 12:16:19','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Mobilnummer','Mobilnummer',TO_TIMESTAMP('2022-03-04 12:16:19','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-03-04T11:16:19.985Z
+-- URL zum Konzept
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=580648 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+
+-- 2022-03-04T11:21:25.806Z
+-- URL zum Konzept
+UPDATE AD_Element_Trl SET Name='Mobile', PrintName='Mobile',Updated=TO_TIMESTAMP('2022-03-04 12:21:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580648 AND AD_Language='en_US'
+;
+
+-- 2022-03-04T11:21:25.837Z
+-- URL zum Konzept
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580648,'en_US')
+;
+
+
 -- 2022-03-03T11:21:37.020Z
 -- URL zum Konzept
-UPDATE AD_Field SET AD_Name_ID=580513, Description=NULL, Help=NULL, IsDisplayed='Y', Name='Mobilnummer',Updated=TO_TIMESTAMP('2022-03-03 12:21:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=565535
+UPDATE AD_Field SET AD_Name_ID=580648, Description=NULL, Help=NULL, IsDisplayed='Y', Name='Mobilnummer',Updated=TO_TIMESTAMP('2022-03-03 12:21:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=565535
 ;
 
 -- 2022-03-03T11:21:37.050Z
 -- URL zum Konzept
-/* DDL */  select update_FieldTranslation_From_AD_Name_Element(580513) 
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(580648)
 ;
 
 -- 2022-03-03T11:21:37.056Z
@@ -62,4 +84,3 @@ UPDATE AD_UI_Element SET SeqNo=42,Updated=TO_TIMESTAMP('2022-03-03 12:33:04','YY
 -- URL zum Konzept
 UPDATE AD_UI_Element SET SeqNo=43,Updated=TO_TIMESTAMP('2022-03-03 12:33:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=604660
 ;
-
