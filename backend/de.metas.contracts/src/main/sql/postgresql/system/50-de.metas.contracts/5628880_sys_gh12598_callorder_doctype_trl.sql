250f03e4b1858fee0588f04ac5896f956abc3d13
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-03-07 07:49:25 +0100

    Update 5628880_sys_gh12598_callorder_doctype_trl.sql
    fix spelling in call-off order

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5628880_sys_gh12598_callorder_doctype_trl.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5628880_sys_gh12598_callorder_doctype_trl.sql
@@ -5,7 +5,7 @@ UPDATE C_DocType SET Name='Abrufauftrag',Updated=TO_TIMESTAMP('2022-03-06 21:44:
 
 -- 2022-03-06T19:44:00.783Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE C_DocType_Trl trl SET Description=NULL, DocumentNote=NULL, Name='Abrufauftrag', PrintName='Call Order'  WHERE C_DocType_ID=541028 AND ( trl.isTranslated = 'N'  OR  exists(select 1 from ad_language lang where lang.ad_language = trl.ad_language and lang.isBaseLanguage = 'Y') )
+UPDATE C_DocType_Trl trl SET Description=NULL, DocumentNote=NULL, Name='Abrufauftrag', PrintName='Call-off Order'  WHERE C_DocType_ID=541028 AND ( trl.isTranslated = 'N'  OR  exists(select 1 from ad_language lang where lang.ad_language = trl.ad_language and lang.isBaseLanguage = 'Y') )
 ;
 
 -- 2022-03-06T19:44:02.404Z
@@ -20,11 +20,11 @@ UPDATE C_DocType_Trl trl SET Description=NULL, DocumentNote=NULL, Name='Abrufauf
 
 -- 2022-03-06T19:44:23.787Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE C_DocType_Trl SET Name='Call-of order',Updated=TO_TIMESTAMP('2022-03-06 21:44:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541028
+UPDATE C_DocType_Trl SET Name='Call-off order',Updated=TO_TIMESTAMP('2022-03-06 21:44:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541028
 ;
 
 -- 2022-03-06T19:45:34.019Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE C_DocType_Trl SET PrintName='Call-of order',Updated=TO_TIMESTAMP('2022-03-06 21:45:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541028
+UPDATE C_DocType_Trl SET PrintName='Call-off order',Updated=TO_TIMESTAMP('2022-03-06 21:45:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541028
 ;
 
