22e4259d8c789cfc820881f9c4e4f539befbf98e
vera-k234 <98325903+vera-k234@users.noreply.github.com>
2022-03-18 13:53:56 +0100

    Update 5630730_sys_gh12679_translation_fixes.sql

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5630730_sys_gh12679_translation_fixes.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5630730_sys_gh12679_translation_fixes.sql
@@ -245,28 +245,6 @@ UPDATE AD_Field SET Name='Primärer Lieferort', Description='BParter location of
 UPDATE AD_PrintFormatItem pi SET PrintName='Primärer Lieferort', Name='PrimÃ¤rer Lieferort' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c  WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=542728)
 ;
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.migration-sql
- * %%
- * Copyright (C) 2022 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- 2022-03-10T06:46:22.928Z
 -- URL zum Konzept
 UPDATE AD_Tab SET Name='Primärer Lieferort', Description='BParter location of first shipment/receipt', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 542728
