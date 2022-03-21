b9efec2ff87814ef7bd954491cde1327a018a97c
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-03-21 16:57:12 +0200

    script improvement
    https://github.com/metasfresh/metasfresh/issues/12663

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5630830_sys_gh12663_ExplicitCostPrice.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5630830_sys_gh12663_ExplicitCostPrice.sql
@@ -483,7 +483,7 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 
 
 
-ALTER TABLE i_inventory DROP COLUMN isexplicitcostprice;
+/* DDL */ SELECT public.db_alter_table('I_Inventory','ALTER TABLE i_inventory DROP COLUMN isexplicitcostprice');
 
 
 
@@ -535,4 +535,34 @@ DELETE FROM AD_Column WHERE AD_Column_ID=582464
 
 
 
+
+-- 2022-03-21T14:26:22.227Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,582467,691417,0,481,0,TO_TIMESTAMP('2022-03-21 16:26:21','YYYY-MM-DD HH24:MI:SS'),100,0,'D',0,'Y','Y','Y','N','N','N','N','N','Explizite Kosten',0,330,0,1,1,TO_TIMESTAMP('2022-03-21 16:26:21','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-03-21T14:26:22.232Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Field_ID=691417 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2022-03-21T14:26:22.234Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(580718) 
+;
+
+-- 2022-03-21T14:26:22.238Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=691417
+;
+
+-- 2022-03-21T14:26:22.239Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */ select AD_Element_Link_Create_Missing_Field(691417)
+;
+
+-- 2022-03-21T14:27:01.751Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,691417,0,481,543224,605255,'F',TO_TIMESTAMP('2022-03-21 16:27:01','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Explizite Kosten',229,0,0,TO_TIMESTAMP('2022-03-21 16:27:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
 
