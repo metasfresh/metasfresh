3830c6a93321e1cfabe8abe528ab2d9f50cc93c6
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-03-25 10:08:02 +0100

    minor description fix

--- backend/de.metas.payment.sepa/base/src/main/sql/postgresql/system/70-de.metas.payment.sepa/5631720_sys_gh12673_rename_sepaexport_param.sql
+++ backend/de.metas.payment.sepa/base/src/main/sql/postgresql/system/70-de.metas.payment.sepa/5631720_sys_gh12673_rename_sepaexport_param.sql
@@ -20,7 +20,7 @@ UPDATE AD_Process_Para SET ColumnName='ReferenceAsEndToEndId', Name='Sammelüber
 
 -- 2022-03-24T10:25:27.351Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Element_Trl SET Description='If set, the pay reference (or NOTPROVIDED) will be used for the EndToEndId. Otherwise a unique identifier will be generated instead.', Name='Reference as EndToEndId', PrintName='Reference as EndToEndId',Updated=TO_TIMESTAMP('2022-03-24 12:25:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='en_US'
+UPDATE AD_Element_Trl SET Description='If set, the payment reference (or NOTPROVIDED) will be used for the EndToEndId. Otherwise a unique identifier will be generated instead.', Name='Reference as EndToEndId', PrintName='Reference as EndToEndId',Updated=TO_TIMESTAMP('2022-03-24 12:25:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='en_US'
 ;
 
 -- 2022-03-24T10:25:27.384Z
@@ -105,7 +105,7 @@ UPDATE AD_Element_Trl SET Name='Referenz als EndToEndId', PrintName='Referenz al
 
 -- 2022-03-24T10:25:39.385Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.',Updated=TO_TIMESTAMP('2022-03-24 12:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='de_CH'
+UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.',Updated=TO_TIMESTAMP('2022-03-24 12:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='de_CH'
 ;
 
 -- 2022-03-24T10:25:39.386Z
@@ -115,7 +115,7 @@ UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Lohnreferenz (oder
 
 -- 2022-03-24T10:25:40.743Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.',Updated=TO_TIMESTAMP('2022-03-24 12:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='de_DE'
+UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.',Updated=TO_TIMESTAMP('2022-03-24 12:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='de_DE'
 ;
 
 -- 2022-03-24T10:25:40.744Z
@@ -130,42 +130,42 @@ UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Lohnreferenz (oder
 
 -- 2022-03-24T10:25:40.750Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET ColumnName='ReferenceAsEndToEndId', Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE AD_Element_ID=580715
+UPDATE AD_Column SET ColumnName='ReferenceAsEndToEndId', Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE AD_Element_ID=580715
 ;
 
 -- 2022-03-24T10:25:40.751Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Process_Para SET ColumnName='ReferenceAsEndToEndId', Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL, AD_Element_ID=580715 WHERE UPPER(ColumnName)='REFERENCEASENDTOENDID' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+UPDATE AD_Process_Para SET ColumnName='ReferenceAsEndToEndId', Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL, AD_Element_ID=580715 WHERE UPPER(ColumnName)='REFERENCEASENDTOENDID' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
 ;
 
 -- 2022-03-24T10:25:40.751Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Process_Para SET ColumnName='ReferenceAsEndToEndId', Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE AD_Element_ID=580715 AND IsCentrallyMaintained='Y'
+UPDATE AD_Process_Para SET ColumnName='ReferenceAsEndToEndId', Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE AD_Element_ID=580715 AND IsCentrallyMaintained='Y'
 ;
 
 -- 2022-03-24T10:25:40.752Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Field SET Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=580715) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 580715)
+UPDATE AD_Field SET Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=580715) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 580715)
 ;
 
 -- 2022-03-24T10:25:40.757Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Tab SET Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 580715
+UPDATE AD_Tab SET Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 580715
 ;
 
 -- 2022-03-24T10:25:40.758Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_WINDOW SET Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE AD_Element_ID = 580715
+UPDATE AD_WINDOW SET Name='Referenz als EndToEndId', Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', Help=NULL WHERE AD_Element_ID = 580715
 ;
 
 -- 2022-03-24T10:25:40.758Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Menu SET   Name = 'Referenz als EndToEndId', Description = 'Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 580715
+UPDATE AD_Menu SET   Name = 'Referenz als EndToEndId', Description = 'Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.', WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 580715
 ;
 
 -- 2022-03-24T10:25:44.078Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Lohnreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.',Updated=TO_TIMESTAMP('2022-03-24 12:25:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='nl_NL'
+UPDATE AD_Element_Trl SET Description='Wenn gesetzt, wird die Zahlungsreferenz (oder NOTPROVIDED) für die EndToEndId verwendet. Andernfalls wird stattdessen ein eindeutiger Bezeichner generiert.',Updated=TO_TIMESTAMP('2022-03-24 12:25:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=580715 AND AD_Language='nl_NL'
 ;
 
 -- 2022-03-24T10:25:44.080Z
