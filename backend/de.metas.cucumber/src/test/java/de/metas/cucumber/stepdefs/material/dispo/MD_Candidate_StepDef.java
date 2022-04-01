1fb0523bd939a46b4791a039c9f7e011f6252907
dm-ps <dragos.moca@pevesoft.ro>
2022-04-01 16:38:56 +0300

    physical inventory and disposal cucumber refs: https://github.com/metasfresh/metasfresh/issues/11943

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/material/dispo/MD_Candidate_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/material/dispo/MD_Candidate_StepDef.java
@@ -30,6 +30,7 @@ import de.metas.cucumber.stepdefs.StepDefConstants;
 import de.metas.cucumber.stepdefs.StepDefUtil;
 import de.metas.cucumber.stepdefs.attribute.M_AttributeSetInstance_StepDefData;
 import de.metas.cucumber.stepdefs.material.dispo.MD_Candidate_StepDefTable.MaterialDispoTableRow;
+import de.metas.logging.LogManager;
 import de.metas.material.dispo.commons.candidate.Candidate;
 import de.metas.material.dispo.commons.candidate.CandidateBusinessCase;
 import de.metas.material.dispo.commons.candidate.CandidateId;
@@ -76,6 +77,7 @@ import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.TimeUtil;
 import org.eevolution.model.I_PP_Order_BOMLine;
+import org.slf4j.Logger;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -85,13 +87,19 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 import static de.metas.cucumber.stepdefs.StepDefConstants.TABLECOLUMN_IDENTIFIER;
+import static de.metas.material.dispo.model.I_MD_Candidate.COLUMNNAME_DateProjected;
+import static de.metas.material.dispo.model.I_MD_Candidate.COLUMNNAME_MD_Candidate_BusinessCase;
 import static de.metas.material.dispo.model.I_MD_Candidate.COLUMNNAME_MD_Candidate_ID;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static de.metas.material.dispo.model.I_MD_Candidate.COLUMNNAME_MD_Candidate_Type;
+import static de.metas.material.dispo.model.I_MD_Candidate.COLUMNNAME_Qty;
+import static de.metas.material.dispo.model.I_MD_Candidate.COLUMNNAME_Qty_AvailableToPromise;
+import static org.assertj.core.api.Assertions.*;
+import static org.compiere.model.I_C_Order.COLUMNNAME_M_Product_ID;
 import static org.eevolution.model.I_PP_Product_Planning.COLUMNNAME_M_AttributeSetInstance_ID;
 
 public class MD_Candidate_StepDef
 {
+	private final static Logger logger = LogManager.getLogger(MD_Candidate_StepDef.class);
 	private final IQueryBL queryBL = Services.get(IQueryBL.class);
 
 	private PostMaterialEventService postMaterialEventService;
@@ -401,7 +409,7 @@ public class MD_Candidate_StepDef
 					.filter(materialDispoRecord -> materialDispoRecord.getAtp().equals(tableRow.getAtp()))
 					.filter(materialDispoRecord -> materialDispoRecord.getMaterialDescriptor().getDate().equals(tableRow.getTime()))
 					.findFirst();
-			final MaterialDispoDataItem materialDispoRecord = StepDefUtil.tryAndWaitForItem(timeoutSec, 1000, candidateCreated);
+			final MaterialDispoDataItem materialDispoRecord = StepDefUtil.tryAndWaitForItem(timeoutSec, 1000, candidateCreated, () -> logCurrentContext(tableRow));
 
 			assertThat(materialDispoRecord).isNotNull();
 
@@ -450,6 +458,21 @@ public class MD_Candidate_StepDef
 		{
 			validate_md_candidate_stock(tableRow);
 		}
+	}
+
+	@And("^after not more than (.*)s, the MD_Candidate table has only the following records$")
+	public void validate_md_candidate_records(final int timeoutSec, @NonNull final MD_Candidate_StepDefTable table) throws InterruptedException
+	{
+		validate_md_candidates(timeoutSec, table);
+
+		final int storedCandidatesSize = queryBL.createQueryBuilder(I_MD_Candidate.class)
+				.create()
+				.count();
+
+		// expected count is twice the number of rows bc we integrated the stock md_candidate as a column in step def
+		final int expectedCandidateAndStocks = table.getRows().size() * 2;
+
+		assertThat(storedCandidatesSize).isEqualTo(expectedCandidateAndStocks);
 	}
 
 	private void validate_md_candidate_stock(@NonNull final Map<String, String> tableRow)
@@ -525,5 +548,35 @@ public class MD_Candidate_StepDef
 
 			assertThat(mdAttributesKeys).isEqualTo(expectedAttributesKey);
 		}
+	}
+
+	private void logCurrentContext(@NonNull final MaterialDispoTableRow tableRow)
+	{
+		final StringBuilder message = new StringBuilder();
+
+		message.append("Looking for instance with:").append("\n")
+				.append(COLUMNNAME_MD_Candidate_Type).append(" : ").append(tableRow.getType().getCode()).append("\n")
+				.append(COLUMNNAME_M_Product_ID).append(" : ").append(tableRow.getProductId().getRepoId()).append("\n")
+				.append(COLUMNNAME_DateProjected).append(" : ").append(tableRow.getTime()).append("\n")
+				.append(COLUMNNAME_Qty).append(" : ").append(tableRow.getQty()).append("\n")
+				.append(COLUMNNAME_Qty_AvailableToPromise).append(" : ").append(tableRow.getAtp()).append("\n")
+				.append(COLUMNNAME_MD_Candidate_BusinessCase).append(" : ").append(tableRow.getBusinessCase()).append("\n");
+
+		message.append("MD_Candidate records:").append("\n");
+
+		queryBL.createQueryBuilder(I_MD_Candidate.class)
+				.create()
+				.stream(I_MD_Candidate.class)
+				.forEach(candidateRecord -> message
+						.append(COLUMNNAME_MD_Candidate_ID).append(" : ").append(candidateRecord.getMD_Candidate_ID()).append(" ; ")
+						.append(COLUMNNAME_MD_Candidate_Type).append(" : ").append(candidateRecord.getMD_Candidate_Type()).append(" ; ")
+						.append(COLUMNNAME_M_Product_ID).append(" : ").append(candidateRecord.getM_Product_ID()).append(" ; ")
+						.append(COLUMNNAME_DateProjected).append(" : ").append(candidateRecord.getDateProjected()).append(" ; ")
+						.append(COLUMNNAME_Qty).append(" : ").append(candidateRecord.getQty()).append(" ; ")
+						.append(COLUMNNAME_Qty_AvailableToPromise).append(" : ").append(candidateRecord.getQty_AvailableToPromise()).append(" ; ")
+						.append(COLUMNNAME_MD_Candidate_BusinessCase).append(" : ").append(candidateRecord.getMD_Candidate_BusinessCase()).append(" ; ")
+						.append("\n"));
+
+		logger.error("*** Error while looking for MD_Candidate records, see current context: \n" + message);
 	}
 }
---
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/scheduler/AD_Scheduler_StepDef.java
@@ -0,0 +1,72 @@
+/*
+ * #%L
+ * de.metas.cucumber
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.cucumber.stepdefs.scheduler;
+
+import de.metas.process.AdProcessId;
+import de.metas.process.IADProcessDAO;
+import de.metas.scheduler.SchedulerAction;
+import de.metas.scheduler.SchedulerSearchKey;
+import de.metas.scheduler.eventbus.ManageSchedulerRequest;
+import de.metas.scheduler.eventbus.SchedulerEventBusService;
+import de.metas.util.Check;
+import de.metas.util.Services;
+import io.cucumber.java.en.And;
+import lombok.NonNull;
+import org.compiere.SpringContextHolder;
+import org.compiere.util.Env;
+
+public class AD_Scheduler_StepDef
+{
+	private final SchedulerEventBusService schedulerEventBusService = SpringContextHolder.instance.getBean(SchedulerEventBusService.class);
+	private final IADProcessDAO adProcessDAO = Services.get(IADProcessDAO.class);
+
+	@And("AD_Scheduler for classname {string} is disabled")
+	public void disable_AD_Scheduler_for_className(@NonNull final String className)
+	{
+		final AdProcessId targetProcessId = adProcessDAO.retrieveProcessIdByClassIfUnique(className);
+
+		Check.assumeNotNull(targetProcessId, "There should always be an AD_Process record for classname:" + className);
+
+		schedulerEventBusService.postRequest(ManageSchedulerRequest.builder()
+													 .schedulerSearchKey(SchedulerSearchKey.of(targetProcessId))
+													 .clientId(Env.getClientId())
+													 .schedulerAction(SchedulerAction.DISABLE)
+													 .supervisorAction(ManageSchedulerRequest.SupervisorAction.DISABLE)
+													 .build());
+	}
+
+	@And("AD_Scheduler for classname {string} is enabled")
+	public void enable_AD_Scheduler_for_className(@NonNull final String className)
+	{
+		final AdProcessId targetProcessId = adProcessDAO.retrieveProcessIdByClassIfUnique(className);
+
+		Check.assumeNotNull(targetProcessId, "There should always be an AD_Process record for classname:" + className);
+
+		schedulerEventBusService.postRequest(ManageSchedulerRequest.builder()
+													 .schedulerSearchKey(SchedulerSearchKey.of(targetProcessId))
+													 .clientId(Env.getClientId())
+													 .schedulerAction(SchedulerAction.ENABLE)
+													 .supervisorAction(ManageSchedulerRequest.SupervisorAction.ENABLE)
+													 .build());
+	}
+}
