88882b0aad5305a4545c658e0ec96c541f27f140
mpeelen <mpeelen@gmx.de>
2022-04-07 10:16:24 +0200

    added partial builds

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-docker build -t my-maven-build .
+docker build -f Dockerfile.common -t mvn-common:latest .
+docker build -f Dockerfile.backend -t mvn-backend:latest .
+docker build -f Dockerfile.camel -t mvn-camel:latest .
+
+docker build -f Dockerfile.frontend -t yarn-frontend:latest .
