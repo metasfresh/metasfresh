3c75327748ba8138e2fa1a6728d6ac2116ea232f
riadmen <riad.mennour@metasfresh.com>
2022-04-08 16:52:26 +0100

    procurement-webui-frontend builds fail with TypeScript error

--- misc/services/procurement-webui/procurement-webui-frontend/buildfile.groovy
+++ misc/services/procurement-webui/procurement-webui-frontend/buildfile.groovy
@@ -15,7 +15,10 @@ Map build(final Map scmVars,
     final misc = new Misc()
 
     final def resultsMap = [:]
-    resultsMap.buildDescription = "<h4>procurement-webui-frontend</h4>"
+    if(true)
+    {
+        resultsMap.buildDescription = """${resultsMap.buildDescription}<b>SKIPPING DUES TO BUILD ERROR - WE ARE ON IT</b>"""
+    }
 
     final String dockerLatestTag = "${misc.mkDockerTag(env.BRANCH_NAME)}_LATEST"
 
