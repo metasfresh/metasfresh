6245df1551aab0324e7473f2d0498037d541d3cf
mpeelen <mpeelen@gmx.de>
2022-04-12 10:59:13 +0200

    added frontend runtime; added procurement frontend

--- build.cmd
+++ build.cmd
@@ -6,3 +6,5 @@ docker build -f Dockerfile.backend.api -t my-api:latest .
 docker build -f Dockerfile.backend.app -t my-app:latest .
 docker build -f Dockerfile.camel.externalsystems -t my-externalsystems:latest .
 docker build -f Dockerfile.frontend -t yarn-frontend:latest .
+
+docker build -f Dockerfile.procurement.frontend -t yarn-frontend:latest .
