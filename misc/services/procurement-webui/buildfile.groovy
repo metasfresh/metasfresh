d4850ea7ae9d169d0e583ed65a16457d2d294016
mpeelen <mpeelen@gmx.de>
2022-04-12 11:11:09 +0200

    updated remaining maven executions to use local maven repository in jenkins home directory

--- misc/services/procurement-webui/buildfile.groovy
+++ misc/services/procurement-webui/buildfile.groovy
@@ -50,7 +50,7 @@ def build(final MvnConf mvnConf,
         nginxDockerImage = results.dockerImage
     }
 
-    withMaven(jdk: 'java-14', maven: 'maven-3.6.3', mavenLocalRepo: '.repository', mavenOpts: '-Xmx1536M', options: [artifactsPublisher(disabled: true)]) {
+    withMaven(jdk: 'java-14', maven: 'maven-3.6.3', mavenLocalRepo: "${env.HOME}/m2-local-repository", mavenOpts: '-Xmx1536M', options: [artifactsPublisher(disabled: true)]) {
         dir('procurement-webui-backend') {
             final def buildFile = load('buildfile.groovy')
             final Map results = buildFile.build(mvnConf, scmVars, forceBuild)
@@ -77,7 +77,7 @@ def build(final MvnConf mvnConf,
     String dockerComposeGroupId='de.metas.procurement'
     String dockerComposeArtifactId='procurement-webui'
     String dockerComposeClassifier='docker-compose'
-    withMaven(jdk: 'java-14', maven: 'maven-3.6.3', mavenLocalRepo: '.repository', options: [artifactsPublisher(disabled: true)]) {
+    withMaven(jdk: 'java-14', maven: 'maven-3.6.3', mavenLocalRepo: "${env.HOME}/m2-local-repository", options: [artifactsPublisher(disabled: true)]) {
         sh "mvn --settings ${mvnConf.settingsFile} ${mvnConf.resolveParams} -Dfile=docker-compose/docker-compose.yml -Durl=${mvnConf.deployRepoURL} -DrepositoryId=${mvnConf.MF_MAVEN_REPO_ID} -DgroupId=${dockerComposeGroupId} -DartifactId=${dockerComposeArtifactId} -Dversion=${env.MF_VERSION} -Dclassifier=${dockerComposeClassifier} -Dpackaging=yml -DgeneratePom=true org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file"
     }
 
