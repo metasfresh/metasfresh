24f4f0cb1da9703a339883541d2f2cfd4e2aa93d
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-04-13 17:57:53 +0300

    Display the isCloseInvoiceCandidate flag to org change log
    https://github.com/metasfresh/metasfresh/issues/12815

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5634910_sys_gh12815_Contract_Voiding_IsCloseInvoiceCandidate.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5634910_sys_gh12815_Contract_Voiding_IsCloseInvoiceCandidate.sql
@@ -98,3 +98,13 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=691671
 /* DDL */ select AD_Element_Link_Create_Missing_Field(691671)
 ;
 
+-- 2022-04-13T13:19:41.866Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,691671,0,543611,545217,605325,'F',TO_TIMESTAMP('2022-04-13 16:19:41','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Rechnungskandidat schließen',90,0,0,TO_TIMESTAMP('2022-04-13 16:19:41','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-04-13T13:19:53.504Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET SeqNo=35,Updated=TO_TIMESTAMP('2022-04-13 16:19:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=605325
+;
+
