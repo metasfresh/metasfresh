887a4296f22bb804f01d04119a7f9e22b5ab5d2d
Manuel Zorn <mpeelen@gmx.de>
2022-04-13 15:44:29 +0200

    added the most basic error handling to build cmd

--- build.cmd
+++ build.cmd
@@ -1,10 +1,24 @@
-docker build -f Dockerfile.common -t mvn-common:latest .
-docker build -f Dockerfile.backend -t mvn-backend:latest .
-docker build -f Dockerfile.camel -t mvn-camel:latest .
+docker build -f Dockerfile.common -t mvn-common:latest . || @goto error
 
-docker build -f Dockerfile.backend.api -t my-api:latest .
-docker build -f Dockerfile.backend.app -t my-app:latest .
-docker build -f Dockerfile.camel.externalsystems -t my-externalsystems:latest .
-docker build -f Dockerfile.frontend -t yarn-frontend:latest .
+docker build -f Dockerfile.backend -t mvn-backend:latest . || @goto error
+docker build -f Dockerfile.camel -t mvn-camel:latest . || @goto error
 
-docker build -f Dockerfile.procurement.frontend -t yarn-frontend:latest .
+docker build -f Dockerfile.backend.api -t my-api:latest . || @goto error
+docker build -f Dockerfile.backend.app -t my-app:latest . || @goto error
+docker build -f Dockerfile.camel.externalsystems -t my-externalsystems:latest . || @goto error
+docker build -f Dockerfile.frontend -t yarn-frontend:latest . || @goto error
+
+@REM docker build -f Dockerfile.procurement.frontend -t yarn-frontend:latest . || @goto error
+
+:success
+@echo.
+@echo --------------------------
+@echo success
+@echo --------------------------
+@exit
+
+:error
+@echo.
+@echo --------------------------
+@echo failure
+@echo --------------------------
