10461a399a52e5ec41653321a11d00b168e32696
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-04-13 11:37:34 +0300

    Parameter for closing the invoice candidates when canceling a contract
    https://github.com/metasfresh/metasfresh/issues/12815

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_OrgChange_History.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_OrgChange_History.java
@@ -12,7 +12,7 @@ import javax.annotation.Nullable;
 public class X_AD_OrgChange_History extends org.compiere.model.PO implements I_AD_OrgChange_History, org.compiere.model.I_Persistent 
 {
 
-	private static final long serialVersionUID = -1755835842L;
+	private static final long serialVersionUID = 1044968022L;
 
     /** Standard Constructor */
     public X_AD_OrgChange_History (final Properties ctx, final int AD_OrgChange_History_ID, @Nullable final String trxName)
@@ -146,5 +146,17 @@ public class X_AD_OrgChange_History extends org.compiere.model.PO implements I_A
 	public java.sql.Timestamp getDate_OrgChange() 
 	{
 		return get_ValueAsTimestamp(COLUMNNAME_Date_OrgChange);
+	}
+
+	@Override
+	public void setIsCloseInvoiceCandidate (final boolean IsCloseInvoiceCandidate)
+	{
+		set_Value (COLUMNNAME_IsCloseInvoiceCandidate, IsCloseInvoiceCandidate);
+	}
+
+	@Override
+	public boolean isCloseInvoiceCandidate() 
+	{
+		return get_ValueAsBoolean(COLUMNNAME_IsCloseInvoiceCandidate);
 	}
 }
