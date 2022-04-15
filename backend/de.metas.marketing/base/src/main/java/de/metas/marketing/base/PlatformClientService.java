f1ecbcb80dd09cab8853cc88a57275a0db978541
Teo Sarca <teo.sarca@metasfresh.com>
2022-04-15 12:21:20 +0300

    PlatformClientService: QA

--- backend/de.metas.marketing/base/src/main/java/de/metas/marketing/base/PlatformClientService.java
+++ backend/de.metas.marketing/base/src/main/java/de/metas/marketing/base/PlatformClientService.java
@@ -52,14 +52,12 @@ public class PlatformClientService
 		final Platform platform = platformRepository.getById(platformId);
 		final String platformGatewayId = platform.getPlatformGatewayId();
 
-		Check.errorUnless(platformClientFactoryRegistry.hasGatewaySupport(
-				platformGatewayId),
+		Check.errorUnless(platformClientFactoryRegistry.hasGatewaySupport(platformGatewayId),
 				"There is no support for the platformGatewayId={} of this platform; platform={}",
 				platformGatewayId, platform);
 
-		final PlatformClientFactory platformClientFactory = platformClientFactoryRegistry.getPlatformClientFactory(platformGatewayId);
-		final PlatformClient platformClient = platformClientFactory.newClientForPlatformId(platformId);
-
-		return platformClient;
+		return platformClientFactoryRegistry
+				.getPlatformClientFactory(platformGatewayId)
+				.newClientForPlatformId(platformId);
 	}
 }
