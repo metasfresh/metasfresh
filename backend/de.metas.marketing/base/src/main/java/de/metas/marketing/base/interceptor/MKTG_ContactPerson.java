3fded95f1111d8333eb9e0454f8a6d954a96c64a
Teo Sarca <teo.sarca@metasfresh.com>
2022-04-16 15:40:35 +0300

    QA

--- backend/de.metas.marketing/base/src/main/java/de/metas/marketing/base/interceptor/MKTG_ContactPerson.java
+++ backend/de.metas.marketing/base/src/main/java/de/metas/marketing/base/interceptor/MKTG_ContactPerson.java
@@ -6,6 +6,7 @@ import de.metas.marketing.base.model.ContactPerson;
 import de.metas.marketing.base.model.ContactPersonRepository;
 import de.metas.marketing.base.model.I_MKTG_Campaign_ContactPerson;
 import de.metas.marketing.base.model.I_MKTG_ContactPerson;
+import de.metas.user.UserId;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.dao.IQueryBL;
@@ -43,6 +44,7 @@ import org.springframework.stereotype.Component;
 @Interceptor(I_MKTG_ContactPerson.class)
 public class MKTG_ContactPerson
 {
+	private final IQueryBL queryBL = Services.get(IQueryBL.class);
 	private final ContactPersonService contactPersonService;
 
 	private MKTG_ContactPerson(@NonNull final ContactPersonService contactPersonService)
@@ -56,45 +58,37 @@ public class MKTG_ContactPerson
 	@ModelChange(timings = ModelValidator.TYPE_BEFORE_CHANGE, ifColumnsChanged = I_MKTG_ContactPerson.COLUMNNAME_AD_User_ID)
 	public void updateCampaignContactPersonAdUserId(@NonNull final I_MKTG_ContactPerson contactPerson)
 	{
-		final IQueryBL queryBL = Services.get(IQueryBL.class);
-
-		// note AD_User_ID=0 needs special treatment
-		final Integer newAdUserID = contactPerson.getAD_User_ID() <= 0 ? null : contactPerson.getAD_User_ID();
+		final UserId newAdUserId = UserId.ofRepoIdOrNullIfSystem(contactPerson.getAD_User_ID());
 
 		final IQueryUpdater<I_MKTG_Campaign_ContactPerson> updater = queryBL
 				.createCompositeQueryUpdater(I_MKTG_Campaign_ContactPerson.class)
-				.addSetColumnValue(I_MKTG_Campaign_ContactPerson.COLUMNNAME_AD_User_ID, newAdUserID);
+				.addSetColumnValue(I_MKTG_Campaign_ContactPerson.COLUMNNAME_AD_User_ID, newAdUserId);
 
-		createContactPersonQuery(contactPerson)
-				.update(updater);
+		queryCampaignContactPersonAssignment(contactPerson).update(updater);
 	}
 
 	@ModelChange(timings = ModelValidator.TYPE_BEFORE_DELETE)
 	public void deleteCampaignContactPersonAdUserId(@NonNull final I_MKTG_ContactPerson contactPerson)
 	{
-		createContactPersonQuery(contactPerson)
-				.delete();
+		queryCampaignContactPersonAssignment(contactPerson).delete();
 	}
 
-	private IQuery<I_MKTG_Campaign_ContactPerson> createContactPersonQuery(@NonNull final I_MKTG_ContactPerson contactPerson)
+	private IQuery<I_MKTG_Campaign_ContactPerson> queryCampaignContactPersonAssignment(@NonNull final I_MKTG_ContactPerson contactPerson)
 	{
-		final IQueryBL queryBL = Services.get(IQueryBL.class);
-
 		return queryBL.createQueryBuilder(I_MKTG_Campaign_ContactPerson.class)
 				.addOnlyActiveRecordsFilter()
 				.addEqualsFilter(I_MKTG_Campaign_ContactPerson.COLUMN_MKTG_ContactPerson_ID, contactPerson.getMKTG_ContactPerson_ID())
 				.create();
 	}
 
-	@ModelChange( //
-			timings = { ModelValidator.TYPE_AFTER_NEW, ModelValidator.TYPE_AFTER_CHANGE }, //
+	@ModelChange(
+			timings = { ModelValidator.TYPE_AFTER_NEW, ModelValidator.TYPE_AFTER_CHANGE },
 			ifColumnsChanged = { I_MKTG_ContactPerson.COLUMNNAME_EMail, I_MKTG_ContactPerson.COLUMNNAME_AD_Language })
 	public void updateUserFromContactPerson(final I_MKTG_ContactPerson contactPersonRecord)
 	{
-		final I_MKTG_ContactPerson oldContactPerson = InterfaceWrapperHelper.createOld(contactPersonRecord, I_MKTG_ContactPerson.class);
-
-		final String oldContactPersonMail = oldContactPerson.getEMail();
-		final Language oldContactPersonLanguage = Language.asLanguage(oldContactPerson.getAD_Language());
+		final I_MKTG_ContactPerson oldContactPersonRecord = InterfaceWrapperHelper.createOld(contactPersonRecord, I_MKTG_ContactPerson.class);
+		final String oldContactPersonMail = oldContactPersonRecord.getEMail();
+		final Language oldContactPersonLanguage = Language.asLanguage(oldContactPersonRecord.getAD_Language());
 
 		final ContactPerson contactPerson = ContactPersonRepository.toContactPerson(contactPersonRecord);
 
