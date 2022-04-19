a3422519315ad17444e56bb7f86b2afc5c86897f
Manuel Zorn <mpeelen@gmx.de>
2022-04-19 15:05:47 +0200

    cleaned up image names and build script

--- Dockerfile.backend.app
+++ Dockerfile.backend.app
@@ -4,8 +4,8 @@ WORKDIR /opt/metasfresh
 
 COPY --chmod=700 docker-builds/app/start.sh .
 
-COPY --from=mvn-backend:latest /backend/metasfresh-dist/dist/target/docker/app/metasfresh_server.jar ./metasfresh-app.jar
-COPY --from=mvn-backend:latest /backend/metasfresh-dist/dist/target/docker/app/configs/* ./
-COPY --from=mvn-backend:latest /backend/metasfresh-dist/dist/target/docker/app/reports/* reports/
+COPY --from=metas-mvn-backend:latest /backend/metasfresh-dist/dist/target/docker/app/metasfresh_server.jar ./metasfresh-app.jar
+COPY --from=metas-mvn-backend:latest /backend/metasfresh-dist/dist/target/docker/app/configs/* ./
+COPY --from=metas-mvn-backend:latest /backend/metasfresh-dist/dist/target/docker/app/reports/* reports/
 
 CMD ["/opt/metasfresh/start.sh"]
--- build.cmd
+++ build.cmd
@@ -1,23 +1,33 @@
-docker build -f Dockerfile.common -t mvn-common:latest . || @goto error
+@echo.
+@echo --------------------------
+@echo building maven artifacts
+@echo --------------------------
 
-docker build -f Dockerfile.backend -t mvn-backend:latest . || @goto error
-docker build -f Dockerfile.camel -t mvn-camel:latest . || @goto error
+docker build -f Dockerfile.common -t metas-mvn-common:latest . || @goto error
+docker build -f Dockerfile.backend -t metas-mvn-backend:latest . || @goto error
+docker build -f Dockerfile.camel -t metas-mvn-camel:latest . || @goto error
 
-docker build -f Dockerfile.backend.api -t my-api:latest . || @goto error
-docker build -f Dockerfile.backend.app -t my-app:latest . || @goto error
-docker build -f Dockerfile.camel.externalsystems -t my-externalsystems:latest . || @goto error
-docker build -f Dockerfile.frontend -t my-frontend:latest . || @goto error
+@echo.
+@echo --------------------------
+@echo building deployables
+@echo --------------------------
 
-docker build -f Dockerfile.db -t my-db:latest . || @goto error
-docker build -f Dockerfile.db-init -t my-db-init:latest . || @goto error
+docker build -f Dockerfile.backend.api -t metas-api:latest . || @goto error
+docker build -f Dockerfile.backend.app -t metas-app:latest . || @goto error
+docker build -f Dockerfile.camel.externalsystems -t metas-externalsystems:latest . || @goto error
+docker build -f Dockerfile.frontend -t metas-frontend:latest . || @goto error
+docker build -f Dockerfile.db -t metas-db:latest . || @goto error
+docker build -f Dockerfile.db-init -t metas-db-init:latest . || @goto error
 
-@REM docker build -f Dockerfile.procurement.frontend -t my-procurement-frontend:latest . || @goto error
+@REM docker build -f Dockerfile.procurement.frontend -t metas-procurement-frontend:latest . || @goto error
 
 :success
 @echo.
 @echo --------------------------
 @echo success
 @echo --------------------------
+@docker images --filter=reference="metas-*"
+@echo.
 @exit
 
 :error
