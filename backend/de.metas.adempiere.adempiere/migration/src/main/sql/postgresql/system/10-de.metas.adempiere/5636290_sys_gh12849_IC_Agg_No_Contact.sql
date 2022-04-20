6b8ce9f037afd8f8f91bfe96213d3cd585b27259
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-04-20 18:50:25 +0300

    deactivate contact form the invoice candidate aggregator
    https://github.com/metasfresh/metasfresh/issues/12849

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5636290_sys_gh12849_IC_Agg_No_Contact.sql
@@ -0,0 +1,20 @@
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_AggregationItem SET IsActive='N',Updated=TO_TIMESTAMP('2022-04-20 18:39:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=2188223 WHERE C_AggregationItem_ID=1000015
+;
+
+
+-- 2022-04-20T15:39:06.160442200Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_AggregationItem SET IsActive='Y',Updated=TO_TIMESTAMP('2022-04-20 18:39:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=2188223 WHERE C_AggregationItem_ID=1000015
+;
+
+
+-- 2022-04-20T15:40:15.038351100Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_AggregationItem SET Description='If the invoice candidates have different contacts, the resulting invoice will have none.',Updated=TO_TIMESTAMP('2022-04-20 18:40:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=2188223 WHERE C_AggregationItem_ID=1000015
+;
+
+-- 2022-04-20T15:40:15.143571600Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE C_AggregationItem SET IsActive='N',Updated=TO_TIMESTAMP('2022-04-20 18:40:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=2188223 WHERE C_AggregationItem_ID=1000015
+;
