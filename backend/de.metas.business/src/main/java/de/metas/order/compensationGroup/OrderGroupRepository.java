5968fdda184841e6b7aef8759e2b046c6977a34b
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-04-20 15:50:48 +0200

    fix after-merge-compile error

--- backend/de.metas.business/src/main/java/de/metas/order/compensationGroup/OrderGroupRepository.java
+++ backend/de.metas.business/src/main/java/de/metas/order/compensationGroup/OrderGroupRepository.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.order.compensationGroup;
 
 import com.google.common.collect.ImmutableList;
@@ -57,28 +79,6 @@ import static org.adempiere.model.InterfaceWrapperHelper.load;
 import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
 import static org.adempiere.model.InterfaceWrapperHelper.save;
 import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
-
-/*
- * #%L
- * de.metas.business
- * %%
- * Copyright (C) 2017 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 @Component
 public class OrderGroupRepository implements GroupRepository
@@ -615,7 +615,7 @@ public class OrderGroupRepository implements GroupRepository
 			@Nullable final GroupId groupId)
 	{
 		//dev-note: needed to make sure `de.metas.activity.model.validator.C_OrderLine.updateActivity` doesn't fail
-		final List<I_C_OrderLine> compensationLinesLast = regularOrderLines.stream()
+		final List<I_C_OrderLine> compensationLinesLast = orderLines.stream()
 				.sorted(Comparator.comparing(I_C_OrderLine::isGroupCompensationLine))
 				.collect(Collectors.toList());
 		for (final I_C_OrderLine regularLinePO : compensationLinesLast)
