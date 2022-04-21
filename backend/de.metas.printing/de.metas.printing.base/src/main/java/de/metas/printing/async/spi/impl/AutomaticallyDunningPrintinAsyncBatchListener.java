f42ac654bc09a9925d46b99056a4745ca9c7cc23
Cristina Ghita <cristina.ghita@metasfresh.com>
2022-04-21 17:24:20 +0300

    #12881 Implement dunning pdf listener

--- backend/de.metas.printing/de.metas.printing.base/src/main/java/de/metas/printing/async/spi/impl/AutomaticallyDunningPrintinAsyncBatchListener.java
+++ backend/de.metas.printing/de.metas.printing.base/src/main/java/de/metas/printing/async/spi/impl/AutomaticallyDunningPrintinAsyncBatchListener.java
@@ -1,18 +1,13 @@
 package de.metas.printing.async.spi.impl;
 
-import de.metas.async.Async_Constants;
 import de.metas.async.api.IAsyncBatchBL;
-import de.metas.async.api.IAsyncBatchDAO;
-import de.metas.async.api.IQueueDAO;
 import de.metas.async.model.I_C_Async_Batch;
-import de.metas.async.model.I_C_Async_Batch_Type;
 import de.metas.async.spi.IAsyncBatchListener;
 import de.metas.i18n.AdMessageKey;
 import de.metas.notification.INotificationBL;
 import de.metas.notification.UserNotificationRequest;
 import de.metas.notification.UserNotificationRequest.TargetRecordAction;
 import de.metas.printing.Printing_Constants;
-import de.metas.printing.api.IPrintingDAO;
 import de.metas.user.UserId;
 import de.metas.util.Services;
 import lombok.NonNull;
@@ -20,14 +15,14 @@ import org.adempiere.ad.element.api.AdWindowId;
 import org.adempiere.util.lang.impl.TableRecordReference;
 
 /**
- * Listener for notifying the user when a invoice PDF concatenating was done
+ * Listener for notifying the user when a dunning PDF concatenating was done
  * <ul>
  * when the workpackage was processed, the listener is triggered
  * </ul>
  */
-public class AutomaticallyInvoicePdfPrintinAsyncBatchListener implements IAsyncBatchListener
+public class AutomaticallyDunningPrintinAsyncBatchListener implements IAsyncBatchListener
 {
-	private static final AdMessageKey MSG_Event_PDFGenerated = AdMessageKey.of("AutomaticallyInvoicePdfPrintinAsyncBatchListener_Pdf_Done");
+	private static final AdMessageKey MSG_Event_DunningPDFGenerated = AdMessageKey.of("AutomaticallyDunningPdfPrintinAsyncBatchListener_Pdf_Done");
 	private static final AdWindowId WINDOW_ID_C_Async_Batch = AdWindowId.ofRepoId(540231);
 
 	// services
@@ -37,7 +32,7 @@ public class AutomaticallyInvoicePdfPrintinAsyncBatchListener implements IAsyncB
 	@Override
 	public void createNotice(@NonNull final I_C_Async_Batch asyncBatch)
 	{
-		if(asyncBatchBL.isAsyncBatchForAutomaticallyInvoicePDFPrinting(asyncBatch))
+		if(asyncBatchBL.isAsyncBatchForAutomaticallyDunningPDFPrinting(asyncBatch))
 		{
 			sendUserNotifications(asyncBatch);
 		}
@@ -49,7 +44,7 @@ public class AutomaticallyInvoicePdfPrintinAsyncBatchListener implements IAsyncB
 		notificationBL.send(UserNotificationRequest.builder()
 									.topic(Printing_Constants.USER_NOTIFICATIONS_TOPIC)
 									.recipientUserId(UserId.ofRepoId(asyncBatch.getCreatedBy()))
-									.contentADMessage(MSG_Event_PDFGenerated)
+									.contentADMessage(MSG_Event_DunningPDFGenerated)
 									.contentADMessageParam(asyncBatch.getCountProcessed())
 									.targetAction(TargetRecordAction.ofRecordAndWindow(asyncBatchItemRef, WINDOW_ID_C_Async_Batch))
 									.build());
