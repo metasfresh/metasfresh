bda7d6281131e490838b746f38a290c5ee6b997e
riadmen <riad.mennour@metasfresh.com>
2022-04-22 11:23:18 +0100

    Fix BankAccount SwiftCode (done by 5622960_sys_gh12385_Replace_BIC_SwiftCode.sql)

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5635250_sys_gh12817_Fix_BankAccount_SwiftCode.sql
+++
@@ -1,5 +0,0 @@
-
-UPDATE AD_Column
-SET ColumnName ='SwiftCode'
-WHERE AD_Column_ID = 575110
-;
--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5635260_sys_gh12817_Fix_BankAccount_SwiftCode.sql
+++
@@ -1,7 +0,0 @@
-UPDATE Revolut_Payment_Export
-SET SwiftCode = BIC
-;
-
-ALTER TABLE Revolut_Payment_Export
-DROP COLUMN BIC
-;
