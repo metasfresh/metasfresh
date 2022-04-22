556588e6f2ed3d17e291573dc648c01220539542
riadmen <riad.mennour@metasfresh.com>
2022-04-22 10:43:47 +0100

    Fix BankAccount SwiftCode

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5635250_sys_gh12817_Fix_BankAccount_SwiftCode.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5635250_sys_gh12817_Fix_BankAccount_SwiftCode.sql
@@ -3,7 +3,3 @@ UPDATE AD_Column
 SET ColumnName ='SwiftCode'
 WHERE AD_Column_ID = 575110
 ;
-
-
-/* DDL */ SELECT public.db_alter_table('Revolut_Payment_Export','ALTER TABLE public.Revolut_Payment_Export ADD COLUMN SwiftCode VARCHAR(20)')
-;
