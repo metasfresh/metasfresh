3dac3f05de5cdf044a7205b6c2a12c209647a5ef
av-ps <89453802+av-ps@users.noreply.github.com>
2022-04-28 11:08:47 +0300

    Improve Async QueuePackageProcessor caching (#12842)
    * https://github.com/metasfresh/metasfresh/issues/12591
    
    * Refactor int usage of C_Queue_Processor_ID
    
    Co-authored-by: Tobias Schöneberg <metas-ts@users.noreply.github.com>

--- backend/de.metas.async/src/main/java/de/metas/async/processor/impl/QueueProcessorFactory.java
+++ backend/de.metas.async/src/main/java/de/metas/async/processor/impl/QueueProcessorFactory.java
@@ -2,20 +2,22 @@ package de.metas.async.processor.impl;
 
 import de.metas.async.api.IWorkPackageQueue;
 import de.metas.async.api.IWorkpackageLogsRepository;
-import de.metas.async.model.I_C_Queue_Processor;
 import de.metas.async.processor.IQueueProcessor;
 import de.metas.async.processor.IQueueProcessorEventDispatcher;
 import de.metas.async.processor.IQueueProcessorFactory;
 import de.metas.async.processor.IWorkPackageQueueFactory;
 import de.metas.async.processor.QueuePackageProcessorId;
+import de.metas.async.processor.descriptor.QueueProcessorDescriptorRepository;
+import de.metas.async.processor.descriptor.model.QueueProcessorDescriptor;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.compiere.SpringContextHolder;
 
 public class QueueProcessorFactory implements IQueueProcessorFactory
 {
-	private final QueueProcessorDescriptorIndex queueProcessorDescriptorIndex = QueueProcessorDescriptorIndex.getInstance();
 	private final IWorkPackageQueueFactory workPackageQueueFactory = Services.get(IWorkPackageQueueFactory.class);
+
+	private final QueueProcessorDescriptorRepository queueProcessorDescriptorRepository = QueueProcessorDescriptorRepository.getInstance();
 
 	private IWorkpackageLogsRepository getLogsRepository()
 	{
@@ -23,7 +25,7 @@ public class QueueProcessorFactory implements IQueueProcessorFactory
 	}
 
 	@Override
-	public IQueueProcessor createAsynchronousQueueProcessor(final I_C_Queue_Processor config, final IWorkPackageQueue queue)
+	public IQueueProcessor createAsynchronousQueueProcessor(@NonNull final QueueProcessorDescriptor config, final IWorkPackageQueue queue)
 	{
 		final IWorkpackageLogsRepository logsRepository = getLogsRepository();
 		return new ThreadPoolQueueProcessor(config, queue, logsRepository);
@@ -40,7 +42,7 @@ public class QueueProcessorFactory implements IQueueProcessorFactory
 	@Override
 	public IQueueProcessor createAsynchronousQueueProcessor(@NonNull final QueuePackageProcessorId packageProcessorId)
 	{
-		final I_C_Queue_Processor queueProcessorConfig = queueProcessorDescriptorIndex.getQueueProcessor(packageProcessorId);
+		final QueueProcessorDescriptor queueProcessorConfig = queueProcessorDescriptorRepository.getQueueProcessor(packageProcessorId);
 
 		final IWorkPackageQueue queue = workPackageQueueFactory.getQueueForPackageProcessing(queueProcessorConfig);
 
