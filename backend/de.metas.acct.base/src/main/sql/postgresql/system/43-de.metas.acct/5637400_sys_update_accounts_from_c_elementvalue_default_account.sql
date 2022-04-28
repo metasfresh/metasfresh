d11422cf383dd2206c0865ada082bf8020cdf5e2
Teo Sarca <teo.sarca@metasfresh.com>
2022-04-28 21:17:52 +0300

    fix CoA related db functions

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5637400_sys_update_accounts_from_c_elementvalue_default_account.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5637400_sys_update_accounts_from_c_elementvalue_default_account.sql
@@ -125,6 +125,29 @@ $$
 
 
 /*
+ * #%L
+ * de.metas.acct.base
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+
+/*
 SELECT de_metas_acct.update_accounts_from_c_elementvalue_default_account(
                p_C_AcctSchema_ID := 1000000,
                p_DryRun := 'N'
