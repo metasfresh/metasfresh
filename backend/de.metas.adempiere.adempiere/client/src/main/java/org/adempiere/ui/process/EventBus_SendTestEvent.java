ebb05b4b93bcb32a7fed6beee5fc2e00c3725817
dm-ps <83770997+dm-ps@users.noreply.github.com>
2022-05-04 12:29:13 +0300

    Master gh12826 (#12833)
    * MetasfreshEventBusService - always post events to rabbitMQ
    refs: https://github.com/metasfresh/metasfresh/issues/12826
    
    Co-authored-by: Tobias Schöneberg <metas-ts@users.noreply.github.com>

--- backend/de.metas.adempiere.adempiere/client/src/main/java/org/adempiere/ui/process/EventBus_SendTestEvent.java
+++ backend/de.metas.adempiere.adempiere/client/src/main/java/org/adempiere/ui/process/EventBus_SendTestEvent.java
@@ -58,7 +58,7 @@ public class EventBus_SendTestEvent extends JavaProcess
 	{
 		final Topic topic = Topic.builder()
 				.name(p_TopicName)
-				.type(Type.REMOTE)
+				.type(Type.DISTRIBUTED)
 				.build();
 
 		final IEventBus eventBus = Services.get(IEventBusFactory.class)
@@ -71,7 +71,7 @@ public class EventBus_SendTestEvent extends JavaProcess
 					+ "\n Recipient: " + p_AD_User_ID
 					+ "\n Sender: " + getAD_User_ID()
 					+ "\n Summary: " + p_Summary;
-			eventBus.postEvent(Event.builder()
+			eventBus.enqueueEvent(Event.builder()
 					.setSummary(p_Summary)
 					.setDetailPlain(detail)
 					.addRecipient_User_ID(p_AD_User_ID)
--- backend/de.metas.material/dispo-commons/src/main/java/de/metas/material/dispo/commons/RequestMaterialOrderService.java
+++ backend/de.metas.material/dispo-commons/src/main/java/de/metas/material/dispo/commons/RequestMaterialOrderService.java
@@ -129,7 +129,7 @@ public class RequestMaterialOrderService
 	private void createAndFirePPOrderRequestedEvent(@NonNull final List<Candidate> group)
 	{
 		final PPOrderRequestedEvent ppOrderRequestEvent = createPPOrderRequestedEvent(group);
-		materialEventService.postEventAsync(ppOrderRequestEvent);
+		materialEventService.enqueueEventNow(ppOrderRequestEvent);
 	}
 
 	@VisibleForTesting
@@ -212,7 +212,7 @@ public class RequestMaterialOrderService
 	private void createAndFireDDOrderRequestedEvent(@NonNull final List<Candidate> group, @Nullable final String traceId)
 	{
 		final DDOrderRequestedEvent ddOrderRequestEvent = createDDOrderRequestEvent(group, traceId);
-		materialEventService.postEventAsync(ddOrderRequestEvent);
+		materialEventService.enqueueEventNow(ddOrderRequestEvent);
 	}
 
 	@VisibleForTesting
@@ -282,7 +282,7 @@ public class RequestMaterialOrderService
 	private void createAndFirePurchaseCandidateRequestedEvent(@NonNull final List<Candidate> group, @Nullable final String traceId)
 	{
 		final PurchaseCandidateRequestedEvent purchaseCandidateRequestedEvent = createPurchaseCandidateRequestedEvent(group, traceId);
-		materialEventService.postEventAfterNextCommit(purchaseCandidateRequestedEvent);
+		materialEventService.enqueueEventAfterNextCommit(purchaseCandidateRequestedEvent);
 	}
 
 	private PurchaseCandidateRequestedEvent createPurchaseCandidateRequestedEvent(@NonNull final List<Candidate> group, @Nullable final String traceId)
@@ -319,7 +319,7 @@ public class RequestMaterialOrderService
 	private void createAndFireForecastRequestedEvent(@NonNull final List<Candidate> group)
 	{
 		final PurchaseCandidateRequestedEvent purchaseCandidateRequestedEvent = createForecastRequestedEvent(group);
-		materialEventService.postEventAfterNextCommit(purchaseCandidateRequestedEvent);
+		materialEventService.enqueueEventAfterNextCommit(purchaseCandidateRequestedEvent);
 	}
 
 	private PurchaseCandidateRequestedEvent createForecastRequestedEvent(@NonNull final List<Candidate> group)
