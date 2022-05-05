3cfcc7b2b85330aea4b6d2cd8d99d9d25dfc6bea
Lars Sander <lars.sander@metasfresh.com>
2022-05-05 14:33:16 +0200

    Added a description and better name https://github.com/metasfresh/me03/issues/11302

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5637900_sys_gh11302_process_remove_old_hu.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5637900_sys_gh11302_process_remove_old_hu.sql
@@ -118,3 +118,70 @@ UPDATE AD_Process SET Description='Removes all uandling unit entries that have a
 UPDATE AD_Process_Para SET DefaultValue='100', IsMandatory='Y',Updated=TO_TIMESTAMP('2022-05-05 10:07:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=542257
 ;
 
+-- 2022-05-05T07:17:33.239Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,IsUseBPartnerLanguage,LockWaitTimeout,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,SpreadsheetFormat,SQLStatement,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,585049,'Y','de.metas.process.ExecuteUpdateSQL','N',TO_TIMESTAMP('2022-05-05 09:17:32','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N','N','Y','N','N','N','Y','Y',0,'remove_hu_entries_between_dates','json','N','N','xls','SELECT remove_hu_entries_between_dates(%months%, %placeholder%);','SQL',TO_TIMESTAMP('2022-05-05 09:17:32','YYYY-MM-DD HH24:MI:SS'),100,'remove_hu_entries_between_dates')
+;
+
+-- 2022-05-05T07:17:33.247Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Process t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_ID=585049 AND NOT EXISTS (SELECT 1 FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- 2022-05-05T07:20:06.527Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,1113,0,585049,542256,22,'NoMonths',TO_TIMESTAMP('2022-05-05 09:20:06','YYYY-MM-DD HH24:MI:SS'),100,'D',2,'Y','N','Y','N','Y','N','Number of Months',10,TO_TIMESTAMP('2022-05-05 09:20:06','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-05T07:20:06.531Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_Para_ID=542256 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2022-05-05T07:21:58.967Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,Updated,UpdatedBy) VALUES (0,542140,0,585049,542257,22,'M_HU_ID',TO_TIMESTAMP('2022-05-05 09:21:58','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.handlingunits',0,'Y','N','Y','N','N','N','Handling Unit',20,TO_TIMESTAMP('2022-05-05 09:21:58','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-05T07:21:58.970Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Process_Para_ID=542257 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
+;
+
+-- 2022-05-05T07:38:13.545Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Scheduler (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Role_ID,AD_Scheduler_ID,Created,CreatedBy,EntityType,Frequency,FrequencyType,IsActive,IsIgnoreProcessingTime,KeepLogDays,ManageScheduler,Name,Processing,SchedulerProcessType,ScheduleType,Status,Supervisor_ID,Updated,UpdatedBy) VALUES (0,0,585049,0,550087,TO_TIMESTAMP('2022-05-05 09:38:13','YYYY-MM-DD HH24:MI:SS'),100,'D',1,'D','Y','N',7,'N','remove_hu_entries_between_dates','N','P','F','NEW',100,TO_TIMESTAMP('2022-05-05 09:38:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-05T07:38:33.308Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Scheduler_Para (AD_Client_ID,AD_Org_ID,AD_Process_Para_ID,AD_Scheduler_ID,AD_Scheduler_Para_ID,Created,CreatedBy,IsActive,ParameterDefault,Updated,UpdatedBy) VALUES (0,0,542257,550087,540038,TO_TIMESTAMP('2022-05-05 09:38:33','YYYY-MM-DD HH24:MI:SS'),100,'Y','100',TO_TIMESTAMP('2022-05-05 09:38:33','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-05T07:38:56.861Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Scheduler_Para (AD_Client_ID,AD_Org_ID,AD_Process_Para_ID,AD_Scheduler_ID,AD_Scheduler_Para_ID,Created,CreatedBy,IsActive,ParameterDefault,Updated,UpdatedBy) VALUES (0,0,542256,550087,540039,TO_TIMESTAMP('2022-05-05 09:38:56','YYYY-MM-DD HH24:MI:SS'),100,'Y','24',TO_TIMESTAMP('2022-05-05 09:38:56','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-05T08:07:28.389Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process SET Description='Removes all uandling unit entries that have an inaktiv status and are older than the specified months. The placeholder is used in other tables linking to the removed handling unit.',Updated=TO_TIMESTAMP('2022-05-05 10:07:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585049
+;
+
+-- 2022-05-05T08:07:49.128Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET DefaultValue='100', IsMandatory='Y',Updated=TO_TIMESTAMP('2022-05-05 10:07:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=542257
+;
+
+-- 2022-05-05T12:12:35.109Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Scheduler SET CronPattern=NULL, Description='Removes old, inactive handling units', Name='Remove old Handling Units',Updated=TO_TIMESTAMP('2022-05-05 14:12:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Scheduler_ID=550087
+;
+
+-- 2022-05-05T12:12:48.784Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Scheduler SET CronPattern=NULL, IsActive='N',Updated=TO_TIMESTAMP('2022-05-05 14:12:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Scheduler_ID=550087
+;
+
+
+
