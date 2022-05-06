df7f1252c8d7d5039b482c21fb153d8690c86f43
Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
2022-05-06 09:45:56 +0300

    Scary plum ic agg (#12914)
    * deactivate contact form the invoice candidate aggregator
    
    https://github.com/metasfresh/metasfresh/issues/12849
    
    * proto start
    
    https://github.com/metasfresh/metasfresh/issues/12849
    
    * invoice contact null when different
    
    https://github.com/metasfresh/metasfresh/issues/12849
    
    * switch base type for negative amts invoices
    
    https://github.com/metasfresh/metasfresh/issues/12849
    
    * switch base type for negative amts invoices
    
    https://github.com/metasfresh/metasfresh/issues/12849
    
    * switch base type for negative amts invoices
    
    https://github.com/metasfresh/metasfresh/issues/12849
    
    * switch base type for negative amts invoices
    
    https://github.com/metasfresh/metasfresh/issues/12849
    # Conflicts:
    #	backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/bpartner/BPartnerContactId.java

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/AggregationEngine.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/AggregationEngine.java
@@ -630,24 +630,34 @@ public final class AggregationEngine
 	private/* static */void setDocBaseType(final InvoiceHeaderImpl invoiceHeader)
 	{
 		final boolean invoiceIsSOTrx = invoiceHeader.isSOTrx();
+		final I_C_DocType invoiceDocType = invoiceHeader.getC_DocTypeInvoice();
+		final Money totalAmt = invoiceHeader.calculateTotalNetAmtFromLines();
+
 		final InvoiceDocBaseType docBaseType;
 
 		//
 		// Case: Invoice DocType was preset
 		if (invoiceHeader.getC_DocTypeInvoice() != null)
 		{
-			final I_C_DocType invoiceDocType = invoiceHeader.getC_DocTypeInvoice();
 			Check.assume(invoiceIsSOTrx == invoiceDocType.isSOTrx(), "InvoiceHeader's IsSOTrx={} shall match document type {}", invoiceIsSOTrx, invoiceDocType);
 
-			docBaseType = InvoiceDocBaseType.ofCode(invoiceDocType.getDocBaseType());
+			final InvoiceDocBaseType invoiceDocBaseType = InvoiceDocBaseType.ofCode(invoiceDocType.getDocBaseType());
+
+			// handle negative amounts: switch the base type to credit memo, based on the IsSOTrx
+			if (totalAmt.signum() < 0)
+			{
+				docBaseType = flipDocBaseType(invoiceDocBaseType, invoiceIsSOTrx);
+			}
+			else
+			{
+				docBaseType = invoiceDocBaseType;
+			}
 		}
 		//
 		// Case: no invoice DocType was set
 		// We need to find out the DocBaseType based on Total Amount and IsSOTrx
 		else
 		{
-			final Money totalAmt = invoiceHeader.calculateTotalNetAmtFromLines();
-
 			if (invoiceIsSOTrx)
 			{
 				if (totalAmt.signum() < 0)
@@ -683,6 +693,28 @@ public final class AggregationEngine
 
 		invoiceHeader.setDocBaseType(docBaseType);
 		invoiceHeader.setPaymentTermId(getPaymentTermId(invoiceHeader).orElse(null));
+	}
+
+	private InvoiceDocBaseType flipDocBaseType(final InvoiceDocBaseType docBaseType, final boolean invoiceIsSOTrx)
+	{
+		if (docBaseType.isCreditMemo())
+		{
+			if (invoiceIsSOTrx)
+			{
+				return InvoiceDocBaseType.CustomerInvoice;
+			}
+			else
+			{
+				return InvoiceDocBaseType.VendorInvoice;
+			}
+		}
+
+		if (invoiceIsSOTrx)
+		{
+			return InvoiceDocBaseType.CustomerCreditMemo;
+		}
+
+		return InvoiceDocBaseType.VendorCreditMemo;
 	}
 
 	private Optional<PaymentTermId> getPaymentTermId(final InvoiceHeaderImpl invoiceHeader)
