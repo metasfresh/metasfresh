da80af0997abaf2da779ea75cd8718eb9adf200f
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-05-12 11:13:51 +0300

    * this index (mind the parameters order) improves performance more https://github.com/metasfresh/metasfresh/issues/12945

--- backend/de.metas.payment.esr/src/main/sql/postgresql/system/5638430_sys_gh12945_ImproveImportPerformace.sql
+++ backend/de.metas.payment.esr/src/main/sql/postgresql/system/5638430_sys_gh12945_ImproveImportPerformace.sql
@@ -1,12 +1,9 @@
 
-
-DROP INDEX IF EXISTS "c_referenceno_ESR";
-
+drop index if exists "c_referenceno_ESR";
 
 CREATE INDEX "c_referenceno_ESR"
-    ON c_referenceno (c_referenceno_type_id, ad_org_id, referenceno)
+    ON c_referenceno ( ad_org_id, c_referenceno_type_id, referenceno)
 ;
-
 
 
 SELECT public.db_alter_table('ESR_ImportLine','ALTER TABLE esr_importline ALTER COLUMN esrlinetext TYPE varchar(20000) USING esrlinetext::varchar(20000)')
