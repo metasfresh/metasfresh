61bc954c43e4c5545c24ea8f6cba15f477bb65a9
Teo Sarca <teo.sarca@metasfresh.com>
2022-05-18 08:36:31 +0300

    ModalButton QA

--- frontend/src/components/app/ModalButton.js
+++ frontend/src/components/app/ModalButton.js
@@ -1,49 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-/**
- * @file Function based component.
- * @module ModalButton
- * @param {object} props
- */
-const ModalButton = (props) => {
-  const {
-    name,
-    onShowTooltip,
-    onHideTooltip,
-    children,
-    onClick,
-    tabIndex,
-    disabled,
-  } = props;
-
-  /**
-   * @func handleClick
-   * @summary Call the `onClick` on mouse click
-   */
-  const handleClick = () => onClick(name);
-
-  /**
-   * @func handleShowTooltip
-   * @summary Call the `onShowTooltip` on mouse enter
-   */
-  const handleShowTooltip = () => onShowTooltip(name);
-
-  /**
-   * @method handleHideTooltip
-   * @summary Call the `onHideTooltip` on mouse leave
-   */
-  const handleHideTooltip = () => onHideTooltip(name);
-
+const ModalButton = ({
+  name,
+  onShowTooltip,
+  onHideTooltip,
+  children,
+  onClick,
+  tabIndex,
+  disabled,
+}) => {
   return (
     <button
       key={`rawmodal-button-${name}`}
       name={name}
       className="btn btn-meta-outline-secondary btn-distance-3 btn-md"
-      onClick={handleClick}
       tabIndex={tabIndex}
-      onMouseEnter={handleShowTooltip}
-      onMouseLeave={handleHideTooltip}
+      onClick={() => onClick && onClick(name)}
+      onMouseEnter={() => onShowTooltip && onShowTooltip(name)}
+      onMouseLeave={() => onHideTooltip && onHideTooltip(name)}
       disabled={disabled}
     >
       {children}
@@ -54,20 +29,21 @@ const ModalButton = (props) => {
 /**
  * @typedef {object} Props Component props
  * @prop {node} [children]
- * @prop {*} [name]
- * @prop {*} [onShowTooltip]
- * @prop {*} [onHideTooltip]
- * @prop {*} [onClick]
- * @prop {*} [tabIndex]
+ * @prop {string} [name] internal name of the button; used as param when functions like onClick are called
+ * @prop {number} [tabIndex]
+ * @prop {bool} [disabled]
+ * @prop {function} [onShowTooltip]
+ * @prop {function} [onHideTooltip]
+ * @prop {function} [onClick]
  */
 ModalButton.propTypes = {
-  children: PropTypes.node,
-  name: PropTypes.any,
-  onShowTooltip: PropTypes.any,
-  onHideTooltip: PropTypes.any,
-  onClick: PropTypes.any,
-  tabIndex: PropTypes.any,
+  children: PropTypes.node.isRequired,
+  name: PropTypes.string,
+  tabIndex: PropTypes.number,
   disabled: PropTypes.bool,
+  onShowTooltip: PropTypes.func,
+  onHideTooltip: PropTypes.func,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default ModalButton;
