c2f92684297862bf29d8dd369964b0e1dccea654
Teo Sarca <teo.sarca@metasfresh.com>
2022-05-25 17:14:02 +0300

    implement Calendar menu action

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5640800_sys_AD_Element_Link_window_fixes.sql
@@ -0,0 +1,337 @@
+-- Field: Application Elements -> Übersetzung - Element -> System-Element
+-- Column: AD_Element_Trl.AD_Element_ID
+-- 2022-05-25T11:15:55.169Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=10,Updated=TO_TIMESTAMP('2022-05-25 14:15:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570194
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Sprache
+-- Column: AD_Element_Trl.AD_Language
+-- 2022-05-25T11:15:55.177Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2022-05-25 14:15:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570195
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Spaltenname
+-- Column: AD_Element_Trl.ColumnName
+-- 2022-05-25T11:15:55.181Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2022-05-25 14:15:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570212
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> System-Element
+-- Column: AD_Element_Trl.AD_Element_ID
+-- 2022-05-25T11:17:03.576Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=10,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570194
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Sprache
+-- Column: AD_Element_Trl.AD_Language
+-- 2022-05-25T11:17:03.580Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=20,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570195
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Übersetzt
+-- Column: AD_Element_Trl.IsTranslated
+-- 2022-05-25T11:17:03.585Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=30,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570202
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Spaltenname
+-- Column: AD_Element_Trl.ColumnName
+-- 2022-05-25T11:17:03.589Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=40,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570212
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Name
+-- Column: AD_Element_Trl.Name
+-- 2022-05-25T11:17:03.592Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=50,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570199
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Drucktext
+-- Column: AD_Element_Trl.PrintName
+-- 2022-05-25T11:17:03.596Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=60,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570203
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Beschreibung
+-- Column: AD_Element_Trl.Description
+-- 2022-05-25T11:17:03.600Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=70,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570200
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Kommentar/Hilfe
+-- Column: AD_Element_Trl.Help
+-- 2022-05-25T11:17:03.603Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=80,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570201
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Name
+-- Column: AD_Element_Trl.PO_Name
+-- 2022-05-25T11:17:03.607Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=90,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570206
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Print name
+-- Column: AD_Element_Trl.PO_PrintName
+-- 2022-05-25T11:17:03.610Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=100,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570207
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Description
+-- Column: AD_Element_Trl.PO_Description
+-- 2022-05-25T11:17:03.612Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=110,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570204
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Help
+-- Column: AD_Element_Trl.PO_Help
+-- 2022-05-25T11:17:03.615Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=120,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570205
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Speicherwarnung
+-- Column: AD_Element_Trl.CommitWarning
+-- 2022-05-25T11:17:03.618Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570208
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Browse name
+-- Column: AD_Element_Trl.WEBUI_NameBrowse
+-- 2022-05-25T11:17:03.620Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=140,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570209
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> New record name
+-- Column: AD_Element_Trl.WEBUI_NameNew
+-- 2022-05-25T11:17:03.622Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=150,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570210
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> New record name (breadcrumb)
+-- Column: AD_Element_Trl.WEBUI_NameNewBreadcrumb
+-- 2022-05-25T11:17:03.625Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=160,Updated=TO_TIMESTAMP('2022-05-25 14:17:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570211
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Mandant
+-- Column: AD_Element_Trl.AD_Client_ID
+-- 2022-05-25T11:17:12.012Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570196
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Organisation
+-- Column: AD_Element_Trl.AD_Org_ID
+-- 2022-05-25T11:17:12.017Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570197
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Aktiv
+-- Column: AD_Element_Trl.IsActive
+-- 2022-05-25T11:17:12.020Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570198
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Name
+-- Column: AD_Element_Trl.Name
+-- 2022-05-25T11:17:12.023Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570199
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Beschreibung
+-- Column: AD_Element_Trl.Description
+-- 2022-05-25T11:17:12.025Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570200
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Kommentar/Hilfe
+-- Column: AD_Element_Trl.Help
+-- 2022-05-25T11:17:12.027Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570201
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Übersetzt
+-- Column: AD_Element_Trl.IsTranslated
+-- 2022-05-25T11:17:12.029Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570202
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Drucktext
+-- Column: AD_Element_Trl.PrintName
+-- 2022-05-25T11:17:12.032Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570203
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Description
+-- Column: AD_Element_Trl.PO_Description
+-- 2022-05-25T11:17:12.035Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=110,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570204
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Help
+-- Column: AD_Element_Trl.PO_Help
+-- 2022-05-25T11:17:12.037Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=120,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570205
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Name
+-- Column: AD_Element_Trl.PO_Name
+-- 2022-05-25T11:17:12.038Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=90,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570206
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> PO Print name
+-- Column: AD_Element_Trl.PO_PrintName
+-- 2022-05-25T11:17:12.040Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570207
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Speicherwarnung
+-- Column: AD_Element_Trl.CommitWarning
+-- 2022-05-25T11:17:12.041Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=130,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570208
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Browse name
+-- Column: AD_Element_Trl.WEBUI_NameBrowse
+-- 2022-05-25T11:17:12.043Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=140,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570209
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> New record name
+-- Column: AD_Element_Trl.WEBUI_NameNew
+-- 2022-05-25T11:17:12.044Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=150,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570210
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> New record name (breadcrumb)
+-- Column: AD_Element_Trl.WEBUI_NameNewBreadcrumb
+-- 2022-05-25T11:17:12.046Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=160,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570211
+;
+
+-- Field: Application Elements -> Übersetzung - Element -> Spaltenname
+-- Column: AD_Element_Trl.ColumnName
+-- 2022-05-25T11:17:12.049Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2022-05-25 14:17:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570212
+;
+
+-- Field: Application Elements -> AD_Element_Link -> System-Element
+-- Column: AD_Element_Link.AD_Element_ID
+-- 2022-05-25T11:17:54.913Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=10,Updated=TO_TIMESTAMP('2022-05-25 14:17:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570217
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Fenster
+-- Column: AD_Element_Link.AD_Window_ID
+-- 2022-05-25T11:17:54.917Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=20,Updated=TO_TIMESTAMP('2022-05-25 14:17:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570218
+;
+
+-- Field: Application Elements -> AD_Element_Link -> System-Element
+-- Column: AD_Element_Link.AD_Element_ID
+-- 2022-05-25T11:18:00.566Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=10,Updated=TO_TIMESTAMP('2022-05-25 14:18:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570217
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Fenster
+-- Column: AD_Element_Link.AD_Window_ID
+-- 2022-05-25T11:18:00.569Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2022-05-25 14:18:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570218
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Register
+-- Column: AD_Element_Link.AD_Tab_ID
+-- 2022-05-25T11:18:16.358Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,563573,696463,0,541366,TO_TIMESTAMP('2022-05-25 14:18:16','YYYY-MM-DD HH24:MI:SS'),100,'Register auf einem Fenster',10,'D','"Register" definiert ein Register zur Anzeige auf einem Fenster.','Y','N','N','N','N','N','N','N','Register',TO_TIMESTAMP('2022-05-25 14:18:16','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-25T11:18:16.359Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Field_ID=696463 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2022-05-25T11:18:16.406Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(125) 
+;
+
+-- 2022-05-25T11:18:16.431Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=696463
+;
+
+-- 2022-05-25T11:18:16.440Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(696463)
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Feld
+-- Column: AD_Element_Link.AD_Field_ID
+-- 2022-05-25T11:18:16.557Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,563574,696464,0,541366,TO_TIMESTAMP('2022-05-25 14:18:16','YYYY-MM-DD HH24:MI:SS'),100,'Ein Feld einer Datenbanktabelle',10,'D','"Feld" bezeichnet ein Feld einer Datenbanktabelle.','Y','N','N','N','N','N','N','N','Feld',TO_TIMESTAMP('2022-05-25 14:18:16','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-05-25T11:18:16.558Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Field_ID=696464 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2022-05-25T11:18:16.561Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(107) 
+;
+
+-- 2022-05-25T11:18:16.577Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=696464
+;
+
+-- 2022-05-25T11:18:16.578Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(696464)
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Register
+-- Column: AD_Element_Link.AD_Tab_ID
+-- 2022-05-25T11:18:27.263Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=30,Updated=TO_TIMESTAMP('2022-05-25 14:18:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=696463
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Feld
+-- Column: AD_Element_Link.AD_Field_ID
+-- 2022-05-25T11:18:27.267Z
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=40,Updated=TO_TIMESTAMP('2022-05-25 14:18:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=696464
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Mandant
+-- Column: AD_Element_Link.AD_Client_ID
+-- 2022-05-25T11:18:40.060Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:18:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570213
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Organisation
+-- Column: AD_Element_Link.AD_Org_ID
+-- 2022-05-25T11:18:40.065Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:18:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570214
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Aktiv
+-- Column: AD_Element_Link.IsActive
+-- 2022-05-25T11:18:40.067Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:18:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570215
+;
+
+-- Field: Application Elements -> AD_Element_Link -> AD_Element_Link
+-- Column: AD_Element_Link.AD_Element_Link_ID
+-- 2022-05-25T11:18:40.071Z
+UPDATE AD_Field SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2022-05-25 14:18:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=570216
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Register
+-- Column: AD_Element_Link.AD_Tab_ID
+-- 2022-05-25T11:18:40.075Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2022-05-25 14:18:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=696463
+;
+
+-- Field: Application Elements -> AD_Element_Link -> Feld
+-- Column: AD_Element_Link.AD_Field_ID
+-- 2022-05-25T11:18:40.078Z
+UPDATE AD_Field SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2022-05-25 14:18:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=696464
+;
+
+-- Table: AD_Element_Link
+-- 2022-05-25T11:19:03.618Z
+UPDATE AD_Table SET AD_Window_ID=540524,Updated=TO_TIMESTAMP('2022-05-25 14:19:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=541153
+;
+
