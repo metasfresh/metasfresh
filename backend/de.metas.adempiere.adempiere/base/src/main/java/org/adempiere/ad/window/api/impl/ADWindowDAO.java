5ca989a6a4dbfc8c0e20091fd54da3eb82201ed4
Teo Sarca <teo.sarca@metasfresh.com>
2022-05-30 11:25:32 +0300

    ADWindowDAO QA

--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/window/api/impl/ADWindowDAO.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/window/api/impl/ADWindowDAO.java
@@ -42,7 +42,6 @@ import org.compiere.util.Env;
 import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -102,6 +101,10 @@ public class ADWindowDAO implements IADWindowDAO
 				.addEqualsFilter(I_AD_Window.COLUMNNAME_AD_Window_ID, adWindowId)
 				.create()
 				.firstOnly(I_AD_Window.class);
+		if (window == null)
+		{
+			throw new AdempiereException("No window found for " + adWindowId);
+		}
 		return window.getInternalName();
 	}
 
@@ -639,7 +642,7 @@ public class ADWindowDAO implements IADWindowDAO
 
 		for (final I_AD_UI_Element sourceUIElement : sourceUIElements)
 		{
-			if(isValidSourceUIElement(sourceUIElement))
+			if (isValidSourceUIElement(sourceUIElement))
 			{
 				final I_AD_UI_Element existingTargetElement = existingTargetUIElements.get(sourceUIElement.getName());
 				copyUIElement(copyCtx, targetUIElementGroup, existingTargetElement, sourceUIElement);
@@ -649,12 +652,12 @@ public class ADWindowDAO implements IADWindowDAO
 
 	private boolean isValidSourceUIElement(final I_AD_UI_Element sourceUIElements)
 	{
-		if(sourceUIElements.getAD_UI_ElementType().equals(X_AD_UI_Element.AD_UI_ELEMENTTYPE_Labels))
+		if (sourceUIElements.getAD_UI_ElementType().equals(X_AD_UI_Element.AD_UI_ELEMENTTYPE_Labels))
 		{
 			return sourceUIElements.getLabels_Tab_ID() > 0 && sourceUIElements.getLabels_Tab().isActive();
 		}
 
-		if(sourceUIElements.getAD_UI_ElementType().equals(X_AD_UI_Element.AD_UI_ELEMENTTYPE_InlineTab))
+		if (sourceUIElements.getAD_UI_ElementType().equals(X_AD_UI_Element.AD_UI_ELEMENTTYPE_InlineTab))
 		{
 			return sourceUIElements.getInline_Tab_ID() > 0 && sourceUIElements.getInline_Tab().isActive();
 		}
@@ -705,27 +708,27 @@ public class ADWindowDAO implements IADWindowDAO
 		//
 		// Labels
 		{
-			final int sourceLabelsTabId = sourceElement.getLabels_Tab_ID();
-			final int targetLabelsTabId = sourceLabelsTabId > 0
+			final AdTabId sourceLabelsTabId = AdTabId.ofRepoIdOrNull(sourceElement.getLabels_Tab_ID());
+			final AdTabId targetLabelsTabId = sourceLabelsTabId != null
 					? copyCtx.getTargetTabIdBySourceTabId(sourceLabelsTabId)
-					: -1;
-			targetElement.setLabels_Tab_ID(targetLabelsTabId);
+					: null;
+			targetElement.setLabels_Tab_ID(AdTabId.toRepoId(targetLabelsTabId));
 
-			final int sourceLabelsSelectorFieldId = sourceElement.getLabels_Selector_Field_ID();
-			final int targetLabelsSelectorFieldId = sourceLabelsSelectorFieldId > 0
+			final AdFieldId sourceLabelsSelectorFieldId = AdFieldId.ofRepoIdOrNull(sourceElement.getLabels_Selector_Field_ID());
+			final AdFieldId targetLabelsSelectorFieldId = sourceLabelsSelectorFieldId != null
 					? copyCtx.getTargetFieldIdBySourceFieldId(sourceLabelsSelectorFieldId)
-					: -1;
-			targetElement.setLabels_Selector_Field_ID(targetLabelsSelectorFieldId);
+					: null;
+			targetElement.setLabels_Selector_Field_ID(AdFieldId.toRepoId(targetLabelsSelectorFieldId));
 		}
 
 		//
 		// Inline Tab
 		{
-			final int sourceInlineTabId = sourceElement.getInline_Tab_ID();
-			final int targetInlineTabId = sourceInlineTabId > 0
+			final AdTabId sourceInlineTabId = AdTabId.ofRepoIdOrNull(sourceElement.getInline_Tab_ID());
+			final AdTabId targetInlineTabId = sourceInlineTabId != null
 					? copyCtx.getTargetTabIdBySourceTabId(sourceInlineTabId)
-					: -1;
-			targetElement.setInline_Tab_ID(targetInlineTabId);
+					: null;
+			targetElement.setInline_Tab_ID(AdTabId.toRepoId(targetInlineTabId));
 		}
 
 		if (targetElement.getSeqNo() <= 0)
@@ -833,21 +836,18 @@ public class ADWindowDAO implements IADWindowDAO
 	@ToString
 	private static class CopyContext
 	{
-		private final HashMap<Integer, Integer> targetFieldIdsBySourceFieldId = new HashMap<>();
-		private final HashMap<Integer, Integer> targetTabIdsBySourceTabId = new HashMap<>();
+		private final HashMap<AdFieldId, AdFieldId> targetFieldIdsBySourceFieldId = new HashMap<>();
+		private final HashMap<AdTabId, AdTabId> targetTabIdsBySourceTabId = new HashMap<>();
 
-		public void put_SourceTabId_And_TargetTabId(final int sourceTabId, final int targetTabId)
+		public void put_SourceTabId_And_TargetTabId(@NonNull final AdTabId sourceTabId, @NonNull final AdTabId targetTabId)
 		{
-			Check.assumeGreaterThanZero(sourceTabId, "sourceTabId");
-			Check.assumeGreaterThanZero(targetTabId, "targetTabId");
-
 			targetTabIdsBySourceTabId.put(sourceTabId, targetTabId);
 		}
 
-		public int getTargetTabIdBySourceTabId(final int sourceTabId)
+		public AdTabId getTargetTabIdBySourceTabId(final AdTabId sourceTabId)
 		{
-			final Integer targetTabId = targetTabIdsBySourceTabId.get(sourceTabId);
-			if (targetTabId == null || targetTabId <= 0)
+			final AdTabId targetTabId = targetTabIdsBySourceTabId.get(sourceTabId);
+			if (targetTabId == null)
 			{
 				throw new AdempiereException("no Target tab ID found for sourceTabId=" + sourceTabId)
 						.setParameter("targetTabIdsBySourceTabId", targetTabIdsBySourceTabId)
@@ -856,18 +856,15 @@ public class ADWindowDAO implements IADWindowDAO
 			return targetTabId;
 		}
 
-		public void put_SourceFieldId_And_TargetFieldId(final int sourceFieldId, final int targetFieldId)
+		public void put_SourceFieldId_And_TargetFieldId(@NonNull final AdFieldId sourceFieldId, @NonNull final AdFieldId targetFieldId)
 		{
-			Check.assumeGreaterThanZero(sourceFieldId, "sourceFieldId");
-			Check.assumeGreaterThanZero(targetFieldId, "targetFieldId");
-
 			targetFieldIdsBySourceFieldId.put(sourceFieldId, targetFieldId);
 		}
 
-		public int getTargetFieldIdBySourceFieldId(final int sourceFieldId)
+		public AdFieldId getTargetFieldIdBySourceFieldId(final AdFieldId sourceFieldId)
 		{
-			final Integer targetFieldId = targetFieldIdsBySourceFieldId.get(sourceFieldId);
-			if (targetFieldId == null || targetFieldId <= 0)
+			final AdFieldId targetFieldId = targetFieldIdsBySourceFieldId.get(sourceFieldId);
+			if (targetFieldId == null)
 			{
 				throw new AdempiereException("no Target field ID found for sourceFieldId=" + sourceFieldId)
 						.setParameter("targetFieldIdsBySourceFieldId", targetFieldIdsBySourceFieldId)
@@ -929,7 +926,7 @@ public class ADWindowDAO implements IADWindowDAO
 		logger.debug("Copying tab {} to {}", sourceTab, targetWindow);
 
 		final I_AD_Tab targetTab = createOrUpdateTab(targetWindow, existingTargetTab, sourceTab);
-		copyCtx.put_SourceTabId_And_TargetTabId(sourceTab.getAD_Tab_ID(), targetTab.getAD_Tab_ID());
+		copyCtx.put_SourceTabId_And_TargetTabId(AdTabId.ofRepoId(sourceTab.getAD_Tab_ID()), AdTabId.ofRepoId(targetTab.getAD_Tab_ID()));
 
 		copyTabTrl(targetTab.getAD_Tab_ID(), sourceTab.getAD_Tab_ID());
 		copyTabCallouts(targetTab, sourceTab);
@@ -1070,7 +1067,7 @@ public class ADWindowDAO implements IADWindowDAO
 
 		save(targetField);
 
-		copyCtx.put_SourceFieldId_And_TargetFieldId(sourceField.getAD_Field_ID(), targetField.getAD_Field_ID());
+		copyCtx.put_SourceFieldId_And_TargetFieldId(AdFieldId.ofRepoId(sourceField.getAD_Field_ID()), AdFieldId.ofRepoId(targetField.getAD_Field_ID()));
 
 		copyFieldTrl(targetField.getAD_Field_ID(), sourceField.getAD_Field_ID());
 	}
@@ -1231,9 +1228,9 @@ public class ADWindowDAO implements IADWindowDAO
 		switch (soTrx)
 		{
 			case SALES:
-				return CoalesceUtil.coalesce(AdWindowId.ofRepoIdOrNull(adTableRecord.getAD_Window_ID()), defaultValue);
+				return CoalesceUtil.coalesceNotNull(AdWindowId.ofRepoIdOrNull(adTableRecord.getAD_Window_ID()), defaultValue);
 			case PURCHASE:
-				return CoalesceUtil.coalesce(AdWindowId.ofRepoIdOrNull(adTableRecord.getPO_Window_ID()), defaultValue);
+				return CoalesceUtil.coalesceNotNull(AdWindowId.ofRepoIdOrNull(adTableRecord.getPO_Window_ID()), defaultValue);
 			default:
 				throw new AdempiereException("Param 'soTrx' has an unspupported value; soTrx=" + soTrx);
 		}
