d12eeaa5b1169840717293b350b182e03220fd11
fp-ps <fp-ps@pevesoft.ro>
2022-06-01 09:33:54 +0300

    display advance edit accordingly

--- backend/de.metas.purchasecandidate.base/src/main/sql/postgresql/system/5641430_sys_gh13085_ad_window_purchaseDisposition_display_productDescription_field.sql
+++ backend/de.metas.purchasecandidate.base/src/main/sql/postgresql/system/5641430_sys_gh13085_ad_window_purchaseDisposition_display_productDescription_field.sql
@@ -39,3 +39,45 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2022-05-31 10:05:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=608280
 ;
 
+
+-- 2022-06-01T05:47:00.577Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Section (AD_Client_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy,Value) VALUES (0,0,540894,544963,TO_TIMESTAMP('2022-06-01 08:47:00','YYYY-MM-DD HH24:MI:SS'),100,'Y',20,TO_TIMESTAMP('2022-06-01 08:47:00','YYYY-MM-DD HH24:MI:SS'),100,'advanced edit')
+;
+
+-- 2022-06-01T05:47:00.580Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Section_Trl (AD_Language,AD_UI_Section_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_UI_Section_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_UI_Section t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_UI_Section_ID=544963 AND NOT EXISTS (SELECT 1 FROM AD_UI_Section_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_UI_Section_ID=t.AD_UI_Section_ID)
+;
+
+-- 2022-06-01T05:47:05.431Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Column (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_Section_ID,Created,CreatedBy,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,0,546008,544963,TO_TIMESTAMP('2022-06-01 08:47:05','YYYY-MM-DD HH24:MI:SS'),100,'Y',10,TO_TIMESTAMP('2022-06-01 08:47:05','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-06-01T05:47:13.816Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,546008,549260,TO_TIMESTAMP('2022-06-01 08:47:13','YYYY-MM-DD HH24:MI:SS'),100,'Y','product',10,TO_TIMESTAMP('2022-06-01 08:47:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-06-01T05:47:24.080Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,Description,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,697945,0,540894,608705,549260,'F',TO_TIMESTAMP('2022-06-01 08:47:23','YYYY-MM-DD HH24:MI:SS'),100,'Produktbeschreibung','Y','N','N','Y','N','N','N',0,'Produktbeschreibung',10,0,0,TO_TIMESTAMP('2022-06-01 08:47:23','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-06-01T05:48:36.682Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=608280
+;
+
+-- 2022-06-01T05:48:40.654Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_ElementGroup WHERE AD_UI_ElementGroup_ID=549184
+;
+
+-- 2022-06-01T05:50:16.414Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsAdvancedField='Y',Updated=TO_TIMESTAMP('2022-06-01 08:50:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=608705
+;
+
+
