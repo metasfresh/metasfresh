7ecdb57dcd9650a37dd138fbc9dbf038d5334c54
Manuel Zorn <mpeelen@gmx.de>
2022-06-07 14:35:49 +0200

    claened up local build; aligned local and github builds image tags

--- build.cmd
+++ build.cmd
@@ -3,36 +3,35 @@
 @echo building maven artifacts
 @echo --------------------------
 
-docker build -f Dockerfile.common -t metas-mvn-common:latest . || @goto error
-docker build -f Dockerfile.backend -t metas-mvn-backend:latest . || @goto error
-docker build -f Dockerfile.camel -t metas-mvn-camel:latest . || @goto error
+docker build -f Dockerfile.common -t mazorn/metas-mvn-common:local . || @goto error
+docker build -f Dockerfile.backend -t mazorn/metas-mvn-backend:local . || @goto error
+docker build -f Dockerfile.camel -t mazorn/metas-mvn-camel:local . || @goto error
 
-docker build -f Dockerfile.cucumber -t metas-cucumber:latest . || @goto error
-docker build -f Dockerfile.e2e -t metas-e2e:latest . || @goto error
-	
+docker build -f Dockerfile.junit -t mazorn/metas-junit:local . || @goto error
+
 @echo.
 @echo --------------------------
 @echo building deployables
 @echo --------------------------
 
-docker build -f Dockerfile.backend.api -t metas-api:latest . || @goto error
-docker build -f Dockerfile.backend.app -t metas-app:latest . || @goto error
-docker build -f Dockerfile.camel.externalsystems -t metas-externalsystems:latest . || @goto error
-docker build -f Dockerfile.frontend -t metas-frontend:latest . || @goto error
-docker build -f Dockerfile.mobile -t metas-mobile:latest . || @goto error
-docker build -f Dockerfile.db-standalone -t metas-db:latest . || @goto error
-docker build -f Dockerfile.db-preloaded -t metas-db:preloaded . || @goto error
+docker build -f Dockerfile.backend.api -t mazorn/metas-api:local . || @goto error
+docker build -f Dockerfile.backend.app -t mazorn/metas-app:local . || @goto error
+docker build -f Dockerfile.camel.externalsystems -t mazorn/metas-externalsystems:local . || @goto error
+docker build -f Dockerfile.frontend -t mazorn/metas-frontend:local . || @goto error
+docker build -f Dockerfile.mobile -t mazorn/metas-mobile:local . || @goto error
+docker build -f Dockerfile.db-standalone -t mazorn/metas-db:local . || @goto error
+docker build -f Dockerfile.db-preloaded -t mazorn/metas-db:local-preloaded . || @goto error
 
-@REM docker build -f Dockerfile.procurement.frontend -t metas-procurement-frontend:latest . || @goto error
-@REM docker build -f Dockerfile.db -t metas-db:latest . || @goto error
-@REM docker build -f Dockerfile.db-init -t metas-db-init:latest . || @goto error
+@REM docker build -f Dockerfile.procurement.frontend -t mazorn/metas-procurement-frontend:local . || @goto error
+@REM docker build -f Dockerfile.cucumber -t mazorn/metas-cucumber:local . || @goto error
+@REM docker build -f Dockerfile.e2e -t mazorn/metas-e2e:local . || @goto error
 
 :success
 @echo.
 @echo --------------------------
 @echo success
 @echo --------------------------
-@docker images --filter=reference="metas-*"
+@docker images --filter=reference="mazorn/metas-*"
 @echo.
 @exit
 
