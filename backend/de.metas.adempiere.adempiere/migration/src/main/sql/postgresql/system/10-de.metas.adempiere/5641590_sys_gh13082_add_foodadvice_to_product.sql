427493a5e63d10b938f64c7a2604ada704b1f0fd
Lars Sander <lars.sander@metasfresh.com>
2022-06-09 14:41:41 +0200

    Added foodadvice to gridview https://github.com/metasfresh/metasfresh/issues/13082

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5641590_sys_gh13082_add_foodadvice_to_product.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5641590_sys_gh13082_add_foodadvice_to_product.sql
@@ -532,3 +532,8 @@ UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2022-05-31 16:4
 UPDATE AD_Table_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2022-05-31 16:43:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Table_ID=542162
 ;
 
+-- 2022-06-09T12:23:10.456Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=150,Updated=TO_TIMESTAMP('2022-06-09 14:23:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=545297
+;
+
