c2c623e7628440edefd18464e51d70db546fc91f
Chems Nouioua <chems.nouioua@gmail.com>
2022-06-09 17:00:34 +0100

    fix c_incoterms_trls by dropping c_incoterms_trl_id column

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5642860_sys_gh13132_add_fr_CH_as_Sys_language_and_Trls.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5642860_sys_gh13132_add_fr_CH_as_Sys_language_and_Trls.sql
@@ -3,4 +3,13 @@
 UPDATE AD_Language SET IsActive = 'Y', IsSystemLanguage='Y',Updated=TO_TIMESTAMP('2022-06-09 14:38:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language_ID=156
 ;
 
+-- 2022-06-09T15:09:45.033Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET IsActive='N', IsUpdateable='N',Updated=TO_TIMESTAMP('2022-06-09 16:09:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=577189
+;
+
+
+ALTER TABLE  C_Incoterms_Trl DROP COLUMN c_incoterms_trl_id
+;
+
 
