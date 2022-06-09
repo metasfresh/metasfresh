cc18378b710fb515ccff84ecccfa5da6be7326c5
Chems Nouioua <chems.nouioua@gmail.com>
2022-06-09 16:59:28 +0100

    #add fr_CH as sys language

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5642860_sys_gh13132_add_fr_CH_as_Sys_language_and_Trls.sql
@@ -0,0 +1,6 @@
+-- 2022-06-09T13:38:21.432Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Language SET IsActive = 'Y', IsSystemLanguage='Y',Updated=TO_TIMESTAMP('2022-06-09 14:38:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language_ID=156
+;
+
+
