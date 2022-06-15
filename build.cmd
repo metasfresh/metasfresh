4df3fb1a1c888356a6a2f7c67f4de79f7fbc2af7
Manuel Zorn <mpeelen@gmx.de>
2022-06-15 15:26:03 +0200

    added external systems to local compose

--- build.cmd
+++ build.cmd
@@ -1,14 +1,17 @@
+set "tag=local"
+set "registry=mazorn"
+
 @echo.
 @echo --------------------------
 @echo building maven artifacts
 @echo --------------------------
 
-docker build -f docker-builds/Dockerfile.common -t mazorn/metas-mvn-common:local . || @goto error
-docker build -f docker-builds/Dockerfile.backend -t mazorn/metas-mvn-backend:local . || @goto error
-docker build -f docker-builds/Dockerfile.camel -t mazorn/metas-mvn-camel:local . || @goto error
+docker build -f docker-builds/Dockerfile.common -t %registry%/metas-mvn-common:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.backend -t %registry%/metas-mvn-backend:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.camel -t %registry%/metas-mvn-camel:%tag% . || @goto error
 
-docker build -f docker-builds/Dockerfile.junit -t mazorn/metas-junit:local . || @goto error
-docker build -f docker-builds/Dockerfile.cucumber -t mazorn/metas-cucumber:local . || @goto error
+docker build -f docker-builds/Dockerfile.junit -t %registry%/metas-junit:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.cucumber -t %registry%/metas-cucumber:%tag% . || @goto error
 
 
 @echo.
@@ -16,18 +19,18 @@ docker build -f docker-builds/Dockerfile.cucumber -t mazorn/metas-cucumber:local
 @echo building deployables
 @echo --------------------------
 
-docker build -f docker-builds/Dockerfile.backend.api -t mazorn/metas-api:local . || @goto error
-docker build -f docker-builds/Dockerfile.backend.app -t mazorn/metas-app:local . || @goto error
-docker build -f docker-builds/Dockerfile.camel.externalsystems -t mazorn/metas-externalsystems:local . || @goto error
-docker build -f docker-builds/Dockerfile.frontend -t mazorn/metas-frontend:local . || @goto error
-docker build -f docker-builds/Dockerfile.mobile -t mazorn/metas-mobile:local . || @goto error
-docker build -f docker-builds/Dockerfile.db-standalone -t mazorn/metas-db:local . || @goto error
-docker build -f docker-builds/Dockerfile.db-preloaded -t mazorn/metas-db:local-preloaded . || @goto error
+docker build -f docker-builds/Dockerfile.backend.api -t %registry%/metas-api:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.backend.app -t %registry%/metas-app:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.camel.externalsystems -t %registry%/metas-externalsystems:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.frontend -t %registry%/metas-frontend:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.mobile -t %registry%/metas-mobile:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.db-standalone -t %registry%/metas-db:%tag% . || @goto error
+docker build -f docker-builds/Dockerfile.db-preloaded -t %registry%/metas-db:%tag%-preloaded . || @goto error
 
 
 @REM ----- for a rainy day -----
-@REM docker build -f docker-builds/Dockerfile.e2e -t mazorn/metas-e2e:local . || @goto error
-@REM docker build -f docker-builds/Dockerfile.procurement.frontend -t mazorn/metas-procurement-frontend:local . || @goto error
+@REM docker build -f docker-builds/Dockerfile.e2e -t %registry%/metas-e2e:%tag% . || @goto error
+@REM docker build -f docker-builds/Dockerfile.procurement.frontend -t %registry%/metas-procurement-frontend:%tag% . || @goto error
 @REM ---------------------------
 
 
@@ -36,7 +39,7 @@ docker build -f docker-builds/Dockerfile.db-preloaded -t mazorn/metas-db:local-p
 @echo --------------------------
 @echo success
 @echo --------------------------
-@docker images --filter=reference="mazorn/metas-*"
+@docker images --filter=reference="%registry%/metas-*"
 @echo.
 @exit
 
