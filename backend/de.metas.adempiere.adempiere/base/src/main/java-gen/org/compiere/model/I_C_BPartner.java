ed55d9895b9a20e4c2a42ffe8065664b7a2300e8
Chems NOUIOUA <chems.nouioua@gmail.com>
2022-06-16 09:55:15 +0100

    Gh13153 add firmenbuch nr to business partner window (#13155)
    * add CommercialRegisterNumber  to partner window
    
    * #generating model for c_bpartner

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_C_BPartner.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_C_BPartner.java
@@ -53,8 +53,8 @@ public interface I_C_BPartner
 	String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
 
 	/**
-	 * Set Straße und Nr..
-	 * Adresszeile 1 für diesen Standort
+	 * Set Street & House No..
+	 * Address line 1 for this location
 	 *
 	 * <br>Type: String
 	 * <br>Mandatory: false
@@ -65,8 +65,8 @@ public interface I_C_BPartner
 	void setAddress1 (@Nullable java.lang.String Address1);
 
 	/**
-	 * Get Straße und Nr..
-	 * Adresszeile 1 für diesen Standort
+	 * Get Street & House No..
+	 * Address line 1 for this location
 	 *
 	 * <br>Type: String
 	 * <br>Mandatory: false
@@ -518,6 +518,27 @@ public interface I_C_BPartner
 	String COLUMNNAME_City = "City";
 
 	/**
+	 * Set Commercial Register Number.
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setCommercialRegisterNumber (@Nullable java.lang.String CommercialRegisterNumber);
+
+	/**
+	 * Get Commercial Register Number.
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getCommercialRegisterNumber();
+
+	ModelColumn<I_C_BPartner, Object> COLUMN_CommercialRegisterNumber = new ModelColumn<>(I_C_BPartner.class, "CommercialRegisterNumber", null);
+	String COLUMNNAME_CommercialRegisterNumber = "CommercialRegisterNumber";
+
+	/**
 	 * Set Company Name.
 	 *
 	 * <br>Type: String
@@ -676,7 +697,7 @@ public interface I_C_BPartner
 	String COLUMNNAME_CreditLimitIndicator = "CreditLimitIndicator";
 
 	/**
-	 * Set Kreditoren-Nr.
+	 * Set Creditor ID.
 	 *
 	 * <br>Type: Integer
 	 * <br>Mandatory: false
@@ -685,7 +706,7 @@ public interface I_C_BPartner
 	void setCreditorId (int CreditorId);
 
 	/**
-	 * Get Kreditoren-Nr.
+	 * Get Creditor ID.
 	 *
 	 * <br>Type: Integer
 	 * <br>Mandatory: false
@@ -764,7 +785,7 @@ public interface I_C_BPartner
 	String COLUMNNAME_DateHaddexCheck = "DateHaddexCheck";
 
 	/**
-	 * Set Debitoren-Nr.
+	 * Set Debtor ID.
 	 *
 	 * <br>Type: Integer
 	 * <br>Mandatory: false
@@ -773,7 +794,7 @@ public interface I_C_BPartner
 	void setDebtorId (int DebtorId);
 
 	/**
-	 * Get Debitoren-Nr.
+	 * Get Debtor ID.
 	 *
 	 * <br>Type: Integer
 	 * <br>Mandatory: false
@@ -858,8 +879,8 @@ public interface I_C_BPartner
 	String COLUMNNAME_DeliveryRule = "DeliveryRule";
 
 	/**
-	 * Set Lieferung.
-	 * Wie der Auftrag geliefert wird
+	 * Set Delivery Via.
+	 * How the order will be delivered
 	 *
 	 * <br>Type: List
 	 * <br>Mandatory: false
@@ -868,8 +889,8 @@ public interface I_C_BPartner
 	void setDeliveryViaRule (@Nullable java.lang.String DeliveryViaRule);
 
 	/**
-	 * Get Lieferung.
-	 * Wie der Auftrag geliefert wird
+	 * Get Delivery Via.
+	 * How the order will be delivered
 	 *
 	 * <br>Type: List
 	 * <br>Mandatory: false
@@ -990,6 +1011,27 @@ public interface I_C_BPartner
 
 	ModelColumn<I_C_BPartner, Object> COLUMN_EMail = new ModelColumn<>(I_C_BPartner.class, "EMail", null);
 	String COLUMNNAME_EMail = "EMail";
+
+	/**
+	 * Set EORI Number.
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setEORI (@Nullable java.lang.String EORI);
+
+	/**
+	 * Get EORI Number.
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getEORI();
+
+	ModelColumn<I_C_BPartner, Object> COLUMN_EORI = new ModelColumn<>(I_C_BPartner.class, "EORI", null);
+	String COLUMNNAME_EORI = "EORI";
 
 	/**
 	 * Set Exclude From Promotions.
@@ -1750,7 +1792,7 @@ public interface I_C_BPartner
 	String COLUMNNAME_IsShippingNotificationEmail = "IsShippingNotificationEmail";
 
 	/**
-	 * Set Zusammenfassungseintrag.
+	 * Set Summary Level.
 	 * This is a summary entity
 	 *
 	 * <br>Type: YesNo
@@ -1760,7 +1802,7 @@ public interface I_C_BPartner
 	void setIsSummary (boolean IsSummary);
 
 	/**
-	 * Get Zusammenfassungseintrag.
+	 * Get Summary Level.
 	 * This is a summary entity
 	 *
 	 * <br>Type: YesNo
@@ -2068,7 +2110,7 @@ public interface I_C_BPartner
 	String COLUMNNAME_MKTG_Campaign_ID = "MKTG_Campaign_ID";
 
 	/**
-	 * Set Preisliste.
+	 * Set Price List.
 	 * Unique identifier of a Price List
 	 *
 	 * <br>Type: TableDir
@@ -2078,7 +2120,7 @@ public interface I_C_BPartner
 	void setM_PriceList_ID (int M_PriceList_ID);
 
 	/**
-	 * Get Preisliste.
+	 * Get Price List.
 	 * Unique identifier of a Price List
 	 *
 	 * <br>Type: TableDir
@@ -2291,6 +2333,71 @@ public interface I_C_BPartner
 
 	ModelColumn<I_C_BPartner, Object> COLUMN_NumberEmployees = new ModelColumn<>(I_C_BPartner.class, "NumberEmployees", null);
 	String COLUMNNAME_NumberEmployees = "NumberEmployees";
+
+	/**
+	 * Set Old Value.
+	 * The old file data
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setOldValue (@Nullable java.lang.String OldValue);
+
+	/**
+	 * Get Old Value.
+	 * The old file data
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getOldValue();
+
+	ModelColumn<I_C_BPartner, Object> COLUMN_OldValue = new ModelColumn<>(I_C_BPartner.class, "OldValue", null);
+	String COLUMNNAME_OldValue = "OldValue";
+
+	/**
+	 * Set Old Value (Customer).
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setOld_Value_Customer (@Nullable java.lang.String Old_Value_Customer);
+
+	/**
+	 * Get Old Value (Customer).
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getOld_Value_Customer();
+
+	ModelColumn<I_C_BPartner, Object> COLUMN_Old_Value_Customer = new ModelColumn<>(I_C_BPartner.class, "Old_Value_Customer", null);
+	String COLUMNNAME_Old_Value_Customer = "Old_Value_Customer";
+
+	/**
+	 * Set Old Value (Vendor).
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setOld_Value_Vendor (@Nullable java.lang.String Old_Value_Vendor);
+
+	/**
+	 * Get Old Value (Vendor).
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getOld_Value_Vendor();
+
+	ModelColumn<I_C_BPartner, Object> COLUMN_Old_Value_Vendor = new ModelColumn<>(I_C_BPartner.class, "Old_Value_Vendor", null);
+	String COLUMNNAME_Old_Value_Vendor = "Old_Value_Vendor";
 
 	/**
 	 * Set Payment Rule.
