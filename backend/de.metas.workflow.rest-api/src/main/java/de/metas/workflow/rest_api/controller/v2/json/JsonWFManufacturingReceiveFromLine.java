30c9f3a204d34b0e3bc12a790fbda432e897b43c
mi-ps <mihai.ispas@pevesoft.ro>
2022-06-18 00:22:09 +0300

    refs:https://github.com/metasfresh/metasfresh/issues/13144

---
+++ backend/de.metas.workflow.rest-api/src/main/java/de/metas/workflow/rest_api/controller/v2/json/JsonWFManufacturingReceiveFromLine.java
@@ -0,0 +1,61 @@
+/*
+ * #%L
+ * de.metas.workflow.rest-api
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.workflow.rest_api.controller.v2.json;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+@Value
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonWFManufacturingReceiveFromLine
+{
+	@JsonProperty("id")
+	@NonNull String id;
+
+	@JsonProperty("qtyToReceive")
+	@NonNull BigDecimal qtyToReceive;
+
+	@JsonProperty("availableReceivingTargets")
+	@NonNull JsonWFManufacturingAvailableReceivingTargets availableReceivingTargets;
+
+	@Builder
+	@JsonCreator
+	public JsonWFManufacturingReceiveFromLine(
+			@JsonProperty("id") @NonNull final String id,
+			@JsonProperty("qtyToReceive") @NonNull final BigDecimal qtyToReceive,
+			@JsonProperty("availableReceivingTargets") @NonNull final JsonWFManufacturingAvailableReceivingTargets availableReceivingTargets
+	)
+	{
+		this.id = id;
+		this.qtyToReceive = qtyToReceive;
+		this.availableReceivingTargets = availableReceivingTargets;
+	}
+}
