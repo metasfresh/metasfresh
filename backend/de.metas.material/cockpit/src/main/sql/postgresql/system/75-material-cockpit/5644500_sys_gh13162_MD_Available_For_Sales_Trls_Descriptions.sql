728a1430f104f2453c41422bfba816fd950e054f
dm-ps <dragos.moca@pevesoft.ro>
2022-06-21 18:29:29 +0300

    trls updated refs: https://github.com/metasfresh/metasfresh/issues/13162

--- backend/de.metas.material/cockpit/src/main/sql/postgresql/system/75-material-cockpit/5644500_sys_gh13162_MD_Available_For_Sales_Trls_Descriptions.sql
+++ backend/de.metas.material/cockpit/src/main/sql/postgresql/system/75-material-cockpit/5644500_sys_gh13162_MD_Available_For_Sales_Trls_Descriptions.sql
@@ -146,3 +146,86 @@ UPDATE AD_Element_Trl SET Description='Quantity that according to the respective
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576514,'en_US') 
 ;
 
+-- 2022-06-21T15:18:53.249Z
+UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.',Updated=TO_TIMESTAMP('2022-06-21 18:18:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581040 AND AD_Language='de_CH'
+;
+
+-- 2022-06-21T15:18:53.331Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581040,'de_CH')
+;
+
+-- 2022-06-21T15:18:56.954Z
+UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.',Updated=TO_TIMESTAMP('2022-06-21 18:18:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581040 AND AD_Language='de_DE'
+;
+
+-- 2022-06-21T15:18:56.957Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581040,'de_DE')
+;
+
+-- 2022-06-21T15:18:56.993Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581040,'de_DE')
+;
+
+-- 2022-06-21T15:18:56.995Z
+UPDATE AD_Column SET ColumnName='IsSyncStockToShopware6', Name='Sync verfügb. Menge nach Shopware6', Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', Help=NULL WHERE AD_Element_ID=581040
+;
+
+-- 2022-06-21T15:18:56.998Z
+UPDATE AD_Process_Para SET ColumnName='IsSyncStockToShopware6', Name='Sync verfügb. Menge nach Shopware6', Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', Help=NULL, AD_Element_ID=581040 WHERE UPPER(ColumnName)='ISSYNCSTOCKTOSHOPWARE6' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2022-06-21T15:18:57.001Z
+UPDATE AD_Process_Para SET ColumnName='IsSyncStockToShopware6', Name='Sync verfügb. Menge nach Shopware6', Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', Help=NULL WHERE AD_Element_ID=581040 AND IsCentrallyMaintained='Y'
+;
+
+-- 2022-06-21T15:18:57.003Z
+UPDATE AD_Field SET Name='Sync verfügb. Menge nach Shopware6', Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=581040) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 581040)
+;
+
+-- 2022-06-21T15:18:57.048Z
+UPDATE AD_Tab SET Name='Sync verfügb. Menge nach Shopware6', Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 581040
+;
+
+-- 2022-06-21T15:18:57.050Z
+UPDATE AD_WINDOW SET Name='Sync verfügb. Menge nach Shopware6', Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', Help=NULL WHERE AD_Element_ID = 581040
+;
+
+-- 2022-06-21T15:18:57.052Z
+UPDATE AD_Menu SET   Name = 'Sync verfügb. Menge nach Shopware6', Description = 'Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.', WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 581040
+;
+
+-- 2022-06-21T15:18:59.391Z
+UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, wird automatisch die aktuell für den Verkauf der Produkte verfügbare Planmenge an Shopware6 gesendet.',Updated=TO_TIMESTAMP('2022-06-21 18:18:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581040 AND AD_Language='nl_NL'
+;
+
+-- 2022-06-21T15:18:59.393Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581040,'nl_NL')
+;
+
+-- 2022-06-21T15:19:19.487Z
+UPDATE AD_Element_Trl SET Description='If checked, the current planned quantity available for sales is automatically sent to Shopware6.',Updated=TO_TIMESTAMP('2022-06-21 18:19:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581040 AND AD_Language='en_US'
+;
+
+-- 2022-06-21T15:19:19.488Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581040,'en_US')
+;
+
+-- 2022-06-21T15:20:08.681Z
+UPDATE AD_Process_Trl SET Description='Berechnet die aktuell für den Verkauf verfügbaren Mengen für alle gelagerten und verkauften Produkte. Hinweis: Die Mengen werden zwischen den Prozessausführungen automatisch synchronisiert, d.h. die Mengen in der Tabelle "Für den Verkauf verfügbar" sind immer aktuell.',Updated=TO_TIMESTAMP('2022-06-21 18:20:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=585064
+;
+
+-- 2022-06-21T15:20:11.145Z
+UPDATE AD_Process SET Description='Berechnet die aktuell für den Verkauf verfügbaren Mengen für alle gelagerten und verkauften Produkte. Hinweis: Die Mengen werden zwischen den Prozessausführungen automatisch synchronisiert, d.h. die Mengen in der Tabelle "Für den Verkauf verfügbar" sind immer aktuell.', Help=NULL, Name='Tabelle aktualisieren',Updated=TO_TIMESTAMP('2022-06-21 18:20:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585064
+;
+
+-- 2022-06-21T15:20:11.016Z
+UPDATE AD_Process_Trl SET Description='Berechnet die aktuell für den Verkauf verfügbaren Mengen für alle gelagerten und verkauften Produkte. Hinweis: Die Mengen werden zwischen den Prozessausführungen automatisch synchronisiert, d.h. die Mengen in der Tabelle "Für den Verkauf verfügbar" sind immer aktuell.',Updated=TO_TIMESTAMP('2022-06-21 18:20:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=585064
+;
+
+-- 2022-06-21T15:20:13.752Z
+UPDATE AD_Process_Trl SET Description='Berechnet die aktuell für den Verkauf verfügbaren Mengen für alle gelagerten und verkauften Produkte. Hinweis: Die Mengen werden zwischen den Prozessausführungen automatisch synchronisiert, d.h. die Mengen in der Tabelle "Für den Verkauf verfügbar" sind immer aktuell.',Updated=TO_TIMESTAMP('2022-06-21 18:20:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=585064
+;
+
+-- 2022-06-21T15:20:19.681Z
+UPDATE AD_Process_Trl SET Description='Computes the current available for sales quantities for all stocked and sold products. Note: the quantities are automatically kept in sync between process executions, meaning the quantities in the available for sales table are always up-to-date.',Updated=TO_TIMESTAMP('2022-06-21 18:20:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=585064
+;
