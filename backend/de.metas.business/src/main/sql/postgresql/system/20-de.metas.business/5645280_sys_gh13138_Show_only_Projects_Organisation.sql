6327333265a1edb34d93ff614929586bc382a80a
Chems Nouioua <chems.nouioua@gmail.com>
2022-06-27 18:30:27 +0100

    backup ad_val_rule table

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5645280_sys_gh13138_Show_only_Projects_Organisation.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5645280_sys_gh13138_Show_only_Projects_Organisation.sql
@@ -6,3 +6,12 @@ SELECT * FROM ad_column
 -- test the backup
 SELECT * FROM backup.BKP_ad_column_gh13138_27062022
 ;
+
+CREATE TABLE backup.BKP_ad_val_rule_gh13138_27062022
+AS
+SELECT * FROM ad_val_rule
+;
+
+-- test the backup
+SELECT * FROM backup.BKP_ad_val_rule_gh13138_27062022
+;
