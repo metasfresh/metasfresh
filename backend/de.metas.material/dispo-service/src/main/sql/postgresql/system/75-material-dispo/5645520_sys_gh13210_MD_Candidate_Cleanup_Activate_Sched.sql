5fc1de8edcbbd2a49d30909d40fa86394a87e36a
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-06-29 19:16:01 +0300

    Activate the material candidate cleanup scheduler https://github.com/pevesoft/metasfresh/issues/310

---
+++ backend/de.metas.material/dispo-service/src/main/sql/postgresql/system/75-material-dispo/5645520_sys_gh13210_MD_Candidate_Cleanup_Activate_Sched.sql
@@ -0,0 +1,5 @@
+-- 2022-06-29T16:08:33.607Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Scheduler SET IsActive='Y',Updated=TO_TIMESTAMP('2022-06-29 19:08:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Scheduler_ID=550097
+;
+
