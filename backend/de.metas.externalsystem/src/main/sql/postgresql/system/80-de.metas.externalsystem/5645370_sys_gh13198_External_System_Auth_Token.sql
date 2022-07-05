212c2edf2319e99bdfe3625e10c1d0a9686b7e35
mi-ps <mihai.ispas@pevesoft.ro>
2022-07-05 14:15:59 +0300

    refs : https://github.com/metasfresh/metasfresh/issues/13198 (migration script updates)

--- backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5645370_sys_gh13198_External_System_Auth_Token.sql
+++ backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5645370_sys_gh13198_External_System_Auth_Token.sql
@@ -62,3 +62,35 @@ INSERT INTO AD_UserGroup (AD_Client_ID,AD_Org_ID,AD_UserGroup_ID,Created,Created
 INSERT INTO AD_UserGroup_User_Assign (AD_Client_ID,AD_Org_ID,AD_User_ID,AD_UserGroup_ID,AD_UserGroup_User_Assign_ID,Created,CreatedBy,IsActive,Updated,UpdatedBy) VALUES (1000000,1000000,100,540005,540003,TO_TIMESTAMP('2022-07-01 17:27:15','YYYY-MM-DD HH24:MI:SS'),100,'Y',TO_TIMESTAMP('2022-07-01 17:27:15','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+-- 2022-07-05T09:17:19.378Z
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541465,'S',TO_TIMESTAMP('2022-07-05 12:17:19','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.externalsystem.externalservice.authorization.notificationUserGroupId',TO_TIMESTAMP('2022-07-05 12:17:19','YYYY-MM-DD HH24:MI:SS'),100,'540005')
+;
+
+-- 2022-07-05T09:38:01.459Z
+UPDATE AD_Message SET MsgText='The authorization token could not be verified by metasfresh using sysconfig "de.metas.externalsystem.externalservice.authorization.authToken". Error details : "{0}" .',Updated=TO_TIMESTAMP('2022-07-05 12:38:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545137
+;
+
+-- 2022-07-05T09:41:16.989Z
+UPDATE AD_Message SET EntityType='D',Updated=TO_TIMESTAMP('2022-07-05 12:41:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545137
+;
+
+-- 2022-07-05T09:42:03.048Z
+UPDATE AD_Message SET EntityType='D',Updated=TO_TIMESTAMP('2022-07-05 12:42:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545138
+;
+
+-- 2022-07-05T10:42:44.220Z
+UPDATE AD_Message SET MsgText='Something went wrong when trying to send an authorization token to Camel-ExternalSystem. {0}
+',Updated=TO_TIMESTAMP('2022-07-05 13:42:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545137
+;
+
+-- 2022-07-05T10:44:06.523Z
+UPDATE AD_Message_Trl SET MsgText='Beim Versuch, ein Autorisierungstoken an Camel-ExternalSystem zu senden, ist ein Fehler aufgetreten. {0}',Updated=TO_TIMESTAMP('2022-07-05 13:44:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545137
+;
+
+-- 2022-07-05T10:45:57.254Z
+UPDATE AD_Message SET MsgText='The sysconfig "de.metas.externalsystem.externalservice.authorization.authToken" is not set. So Camel-ExternalSystem will not be able to authenticate into metasfresh.',Updated=TO_TIMESTAMP('2022-07-05 13:45:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545138
+;
+
+-- 2022-07-05T10:46:06.188Z
+UPDATE AD_Message_Trl SET MsgText='Die Sysconfig "de.metas.externalsystem.externalservice.authorization.authToken" ist nicht gesetzt. Daher kann sich Camel-ExternalSystem nicht bei metasfresh authentifizieren.',Updated=TO_TIMESTAMP('2022-07-05 13:46:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545138
+;
