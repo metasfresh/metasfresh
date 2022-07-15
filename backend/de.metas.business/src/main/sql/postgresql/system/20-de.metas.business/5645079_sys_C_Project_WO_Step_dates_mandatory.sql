edd8563a3fb702e9ca2d6ec4506e610241810707
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-07-15 21:32:46 +0200

    fix sql migration problem
    WO projects - sandbox simulation #13214

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5645079_sys_C_Project_WO_Step_dates_mandatory.sql
@@ -0,0 +1,3 @@
+
+UPDATE c_project_wo_step SET DateStart='2000-01-01', updatedby=99, updated='2022-06-20 21:28' where DateStart is null;
+UPDATE c_project_wo_step SET DateEnd='2000-01-01', updatedby=99, updated='2022-06-20 21:28' where DateEnd is null;
