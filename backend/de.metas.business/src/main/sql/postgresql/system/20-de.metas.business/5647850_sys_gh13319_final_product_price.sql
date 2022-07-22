1e16e7a759cc8dc6ffa5ca63dcf172bf2265a4e1
Chems Nouioua <chems.nouioua@gmail.com>
2022-07-22 18:59:32 +0100

    #13313 clean scripts

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5647850_sys_gh13319_final_product_price.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5647850_sys_gh13319_final_product_price.sql
@@ -18,3 +18,14 @@ UPDATE AD_Column SET IsUseDocSequence='N',Updated=TO_TIMESTAMP('2022-07-22 08:31
 UPDATE AD_Column SET IsUseDocSequence='Y',Updated=TO_TIMESTAMP('2022-07-22 08:32:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=583760
 ;
 
+
+ALTER TABLE m_costrevaluation DROP COLUMN name;
+ALTER TABLE m_costrevaluation DROP COLUMN description;
+
+ALTER TABLE m_costrevaluationline DROP COLUMN name;
+ALTER TABLE m_costrevaluationline DROP COLUMN description;
+ALTER TABLE m_costrevaluationline DROP COLUMN cm_template_id;
+ALTER TABLE m_costrevaluationline DROP COLUMN otherclause;
+ALTER TABLE m_costrevaluationline DROP COLUMN whereclause;
+ALTER TABLE m_costrevaluationline DROP COLUMN ad_table_id;
+
