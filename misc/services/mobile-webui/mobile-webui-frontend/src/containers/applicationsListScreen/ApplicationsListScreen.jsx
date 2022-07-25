42050f29d3861cf5dcd901c88f710d8d5087281b
Teo Sarca <teo.sarca@metasfresh.com>
2022-07-25 10:59:17 +0300

    make ApplicationButton a pure visual component

--- misc/services/mobile-webui/mobile-webui-frontend/src/containers/applicationsListScreen/ApplicationsListScreen.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/containers/applicationsListScreen/ApplicationsListScreen.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { getAvailableApplicationsArray } from '../../reducers/applications';
 
 import ScreenToaster from '../../components/ScreenToaster';
 import ApplicationButton from './ApplicationButton';
 import LogoHeader from '../../components/LogoHeader';
+import { useHistory } from 'react-router-dom';
+import { getApplicationStartFunction } from '../../apps';
+import { appLaunchersLocation } from '../../routes/launchers';
 
 const ApplicationsListScreen = () => {
   const applications = useSelector((state) => getAvailableApplicationsArray(state));
@@ -14,6 +17,17 @@ const ApplicationsListScreen = () => {
     document.title = 'mobile UI';
   }, []);
 
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleAppClick = (applicationId) => {
+    const startApplicationFunc = getApplicationStartFunction(applicationId);
+    if (startApplicationFunc) {
+      dispatch(startApplicationFunc());
+    } else {
+      history.push(appLaunchersLocation({ applicationId }));
+    }
+  };
+
   return (
     <div>
       <LogoHeader />
@@ -21,9 +35,9 @@ const ApplicationsListScreen = () => {
         {applications.map((app) => (
           <ApplicationButton
             key={app.id}
-            applicationId={app.id}
             caption={app.caption}
             iconClassNames={app.iconClassNames}
+            onClick={() => handleAppClick(app.id)}
           />
         ))}
       </div>
