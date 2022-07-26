ffa65d74aa38136d2358cd15ccd8409373e73b9e
Chems Nouioua <chems.nouioua@gmail.com>
2022-07-26 18:12:46 +0100

    gh13319 fix DocNo problem

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5648110_sys_gh13319_final_product_price.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5648110_sys_gh13319_final_product_price.sql
@@ -85,12 +85,3 @@ UPDATE AD_Table SET IsEnableRemoteCacheInvalidation='N',Updated=TO_TIMESTAMP('20
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2022-07-25 19:06:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=702158
 ;
-
-
-
--- 2022-07-25T18:39:43.777Z
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET DefaultValue='< >',Updated=TO_TIMESTAMP('2022-07-25 19:39:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=583760
-;
-
-
