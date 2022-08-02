02dc17c5f5964094806d3f731651d0e2a2244018
Teo Sarca <teo.sarca@metasfresh.com>
2022-08-02 19:32:14 +0300

    fix migration script

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5648720_sys_PaymentView_Launcher_From_PaySelection_process.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5648720_sys_PaymentView_Launcher_From_PaySelection_process.sql
@@ -1,6 +1,9 @@
 -- 2022-08-01T13:23:01.073Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,IsUpdateExportDate,IsUseBPartnerLanguage,LockWaitTimeout,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,SpreadsheetFormat,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,585082,'Y','de.metas.ui.web.payment_allocation.process.PaymentView_Launcher_From_PaySelection','N',TO_TIMESTAMP('2022-08-01 16:23:00','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment','Y','N','N','N','Y','N','N','N','Y','N','Y',0,'Zahlung-Zuordnung','json','N','N','xls','Java',TO_TIMESTAMP('2022-08-01 16:23:00','YYYY-MM-DD HH24:MI:SS'),100,'PaymentView_Launcher_From_PaySelection')
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,
+                        IsUseBPartnerLanguage,LockWaitTimeout,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,SpreadsheetFormat,Type,Updated,UpdatedBy,Value)
+VALUES ('3',0,0,585082,'Y','de.metas.ui.web.payment_allocation.process.PaymentView_Launcher_From_PaySelection','N',TO_TIMESTAMP('2022-08-01 16:23:00','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.payment','Y','N','N','N','Y','N','N','N','Y',
+        'Y',0,'Zahlung-Zuordnung','json','N','N','xls','Java',TO_TIMESTAMP('2022-08-01 16:23:00','YYYY-MM-DD HH24:MI:SS'),100,'PaymentView_Launcher_From_PaySelection')
 ;
 
 -- 2022-08-01T13:23:01.077Z
