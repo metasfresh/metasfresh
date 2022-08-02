6484611ec69a9f8e1e6353d3f49990dde0501f82
Peter Wyss <peter.wyss@kommunikativ.ch>
2022-08-02 16:56:12 +0200

    update from master

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_C_BP_BankAccount.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_C_BP_BankAccount.java
@@ -430,7 +430,19 @@ public class X_C_BP_BankAccount extends org.compiere.model.PO implements I_C_BP_
 	{
 		return get_ValueAsString(COLUMNNAME_IBAN);
 	}
+	
+	@Override
+	public void setSwiftCode (final @Nullable java.lang.String SwiftCode)
+	{
+		set_Value (COLUMNNAME_SwiftCode, SwiftCode);
+	}
 
+	@Override
+	public java.lang.String getSwiftCode()
+	{
+		return get_ValueAsString(COLUMNNAME_SwiftCode);
+	}
+	
 	@Override
 	public void setIsACH (final boolean IsACH)
 	{
