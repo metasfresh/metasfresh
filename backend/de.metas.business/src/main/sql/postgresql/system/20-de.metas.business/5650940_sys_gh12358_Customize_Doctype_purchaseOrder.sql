ff5c02a1d1635ee220c62b164fcbbffe7ad01be0
MHNV <95350690+MHNV@users.noreply.github.com>
2022-08-15 15:23:46 +0200

    Changed C_Order Reference
    - Changed C_Order Reference
    
    https://github.com/metasfresh/ic114/issues/1487

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5650940_sys_gh12358_Customize_Doctype_purchaseOrder.sql
@@ -0,0 +1,4 @@
+-- 2022-08-15T12:39:50.670Z
+UPDATE AD_Ref_Table SET OrderByClause='C_Order.DocumentNo ASC, C_DocType.DocSubType DESC',Updated=TO_TIMESTAMP('2022-08-15 14:39:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=290
+;
+
