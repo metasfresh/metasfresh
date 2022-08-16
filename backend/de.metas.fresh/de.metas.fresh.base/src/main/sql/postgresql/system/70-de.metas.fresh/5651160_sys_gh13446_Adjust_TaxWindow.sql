153480d7ac650c095a92c4d70f53359cf88e9e6f
Cristina Ghita <cristina.ghita@metasfresh.com>
2022-08-16 19:00:49 +0300

    #13446 Adjust Tax window

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5651160_sys_gh13446_Adjust_TaxWindow.sql
@@ -0,0 +1,61 @@
+-- 2022-08-16T10:03:28.529491400Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_ID=10,Updated=TO_TIMESTAMP('2022-08-16 13:03:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=583559
+;
+
+-- 2022-08-16T10:03:30.764555900Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('i_product','IsWear_Tear_Part_cw105','VARCHAR(1)',null,'0')
+;
+
+-- 2022-08-16T10:03:30.784063300Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE I_Product SET IsWear_Tear_Part_cw105='0' WHERE IsWear_Tear_Part_cw105 IS NULL
+;
+
+-- 2022-08-16T10:04:31.605105100Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('i_product','IsSpare_Part_cw105','VARCHAR(1)',null,'0')
+;
+
+-- 2022-08-16T10:04:31.608210300Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE I_Product SET IsSpare_Part_cw105='0' WHERE IsSpare_Part_cw105 IS NULL
+;
+
+
+
+
+
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+
+
+
+-- 2022-08-16T13:59:44.173994200Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=540521, SeqNo=20,Updated=TO_TIMESTAMP('2022-08-16 16:59:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=584675
+;
+
+-- 2022-08-16T14:00:29.327662400Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,12489,0,174,540521,611780,'F',TO_TIMESTAMP('2022-08-16 17:00:29','YYYY-MM-DD HH24:MI:SS'),100,'Dies ist eine VK Steuer','If selected AP tax is handled as expense, otherwise it is handeled as a VAT credit.','Y','N','N','Y','N','N','N',0,'VK Steuer',30,0,0,TO_TIMESTAMP('2022-08-16 17:00:29','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-08-16T14:06:04.656255900Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=544934
+;
+
+-- 2022-08-16T14:07:51.711119600Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=540521, SeqNo=40,Updated=TO_TIMESTAMP('2022-08-16 17:07:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=584460
+;
+
+-- 2022-08-16T14:12:05.960384800Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=544935
+;
+
