0a8aca9f12fd7414edf685be610974c243c61c9e
MHNV <95350690+MHNV@users.noreply.github.com>
2022-08-16 09:42:52 +0200

    changed to default flag in windows fields
    - changed to default flag in windows fields
    
    https://github.com/metasfresh/ic114/issues/1487

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5650940_sys_gh12358_Customize_Doctype_purchaseOrder.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5650940_sys_gh12358_Customize_Doctype_purchaseOrder.sql
@@ -1,4 +1,17 @@
--- 2022-08-15T12:39:50.670Z
-UPDATE AD_Ref_Table SET OrderByClause='C_Order.DocumentNo ASC, C_DocType.DocSubType DESC',Updated=TO_TIMESTAMP('2022-08-15 14:39:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=290
+-- Column: AD_Column.IsAutoApplyValidationRule
+-- 2022-08-16T06:46:31.918Z
+UPDATE AD_Column SET DefaultValue='Y',Updated=TO_TIMESTAMP('2022-08-16 08:46:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=563397
+;
+
+-- Field: Bestellung -> Bestellung -> Belegart
+-- Column: C_Order.C_DocTypeTarget_ID
+-- 2022-08-16T07:27:13.391Z
+UPDATE AD_Field SET DefaultValue='1000016',Updated=TO_TIMESTAMP('2022-08-16 09:27:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3428
+;
+
+-- Field: Auftrag -> Auftrag -> Belegart
+-- Column: C_Order.C_DocTypeTarget_ID
+-- 2022-08-16T07:27:51.386Z
+UPDATE AD_Field SET DefaultValue='1000030',Updated=TO_TIMESTAMP('2022-08-16 09:27:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1085
 ;
 
