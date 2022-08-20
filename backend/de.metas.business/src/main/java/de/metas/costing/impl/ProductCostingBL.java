add9666665a79e805d95068fc28829c2c46b0a90
Teo Sarca <teo.sarca@metasfresh.com>
2022-08-20 12:30:10 +0300

    IProductDAO.retrieveProductAndCategoryIdsByProductIds

--- backend/de.metas.business/src/main/java/de/metas/costing/impl/ProductCostingBL.java
+++ backend/de.metas.business/src/main/java/de/metas/costing/impl/ProductCostingBL.java
@@ -1,15 +1,8 @@
 package de.metas.costing.impl;
 
-import javax.annotation.Nullable;
-
-import org.adempiere.ad.dao.IQueryBL;
-import org.adempiere.model.InterfaceWrapperHelper;
-import org.compiere.model.IQuery;
-import org.compiere.model.I_C_AcctSchema_Default;
-import org.compiere.model.I_M_Product;
-import org.compiere.model.I_M_Product_Category;
-import org.compiere.model.I_M_Product_Category_Acct;
-
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
 import de.metas.acct.api.AcctSchema;
 import de.metas.acct.api.AcctSchemaId;
 import de.metas.acct.api.IAcctSchemaDAO;
@@ -19,12 +12,29 @@ import de.metas.costing.CostingLevel;
 import de.metas.costing.CostingMethod;
 import de.metas.costing.IProductCostingBL;
 import de.metas.product.IProductDAO;
+import de.metas.product.ProductAndCategoryId;
 import de.metas.product.ProductCategoryId;
 import de.metas.product.ProductId;
 import de.metas.util.Services;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
+import org.adempiere.ad.dao.ICompositeQueryFilter;
+import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.dao.IQueryBuilder;
+import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.compiere.model.I_C_AcctSchema_Default;
+import org.compiere.model.I_M_Product;
+import org.compiere.model.I_M_Product_Category;
+import org.compiere.model.I_M_Product_Category_Acct;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 /*
  * #%L
@@ -36,12 +46,12 @@ import lombok.Value;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -54,7 +64,7 @@ public class ProductCostingBL implements IProductCostingBL
 	private final IProductDAO productDAO = Services.get(IProductDAO.class);
 	private final IAcctSchemaDAO acctSchemaDAO = Services.get(IAcctSchemaDAO.class);
 
-	private final CCache<ProductCategoryAcctKey, ProductCategoryAcct> productCategoryAcctCache = CCache.<ProductCategoryAcctKey, ProductCategoryAcct> builder()
+	private final CCache<ProductCategoryAcctKey, ProductCategoryAcct> productCategoryAcctCache = CCache.<ProductCategoryAcctKey, ProductCategoryAcct>builder()
 			.tableName(I_M_Product_Category_Acct.Table_Name)
 			.cacheMapType(CacheMapType.LRU)
 			.initialCapacity(100)
@@ -87,6 +97,38 @@ public class ProductCostingBL implements IProductCostingBL
 	{
 		final I_M_Product product = productDAO.getById(productId);
 		return getCostingLevel(product, acctSchema);
+	}
+
+	@Override
+	public @NonNull Map<ProductId, CostingLevel> getCostingLevels(final Set<ProductId> productIds, final AcctSchema acctSchema)
+	{
+		if (productIds.isEmpty())
+		{
+			return ImmutableMap.of();
+		}
+
+		final ImmutableSetMultimap<ProductCategoryId, ProductId> productIdsByProductCategoryId = productDAO.retrieveProductAndCategoryIdsByProductIds(productIds)
+				.stream()
+				.collect(ImmutableSetMultimap.toImmutableSetMultimap(
+						ProductAndCategoryId::getProductCategoryId,
+						ProductAndCategoryId::getProductId));
+
+		final ImmutableSet<ProductCategoryId> productCategoryIds = productIdsByProductCategoryId.keySet();
+		final Collection<ProductCategoryAcct> productCategoryAccts = getProductCategoryAccts(productCategoryIds, acctSchema.getId());
+
+		final HashMap<ProductId, CostingLevel> costingLevelsByProductId = new HashMap<>(productIds.size());
+		for (final ProductCategoryAcct productCategoryAcct : productCategoryAccts)
+		{
+			final ProductCategoryId productCategoryId = productCategoryAcct.getProductCategoryId();
+			final CostingLevel costingLevel = productCategoryAcct.getCostingLevel();
+
+			for (final ProductId productId : productIdsByProductCategoryId.get(productCategoryId))
+			{
+				costingLevelsByProductId.put(productId, costingLevel);
+			}
+		}
+
+		return costingLevelsByProductId;
 	}
 
 	@Override
@@ -123,36 +165,97 @@ public class ProductCostingBL implements IProductCostingBL
 		return productCategoryAcctCache.getOrLoad(key, this::retrieveOrCreateAcct);
 	}
 
+	private Collection<ProductCategoryAcct> getProductCategoryAccts(final Set<ProductCategoryId> productCategoryIds, final AcctSchemaId acctSchemaId)
+	{
+		final ImmutableSet<ProductCategoryAcctKey> keys = productCategoryIds.stream()
+				.map(productCategoryId -> ProductCategoryAcctKey.of(productCategoryId, acctSchemaId))
+				.collect(ImmutableSet.toImmutableSet());
+		return productCategoryAcctCache.getAllOrLoad(keys, this::retrieveOrCreateAcctsByKeys);
+	}
+
 	private ProductCategoryAcct retrieveOrCreateAcct(@NonNull final ProductCategoryAcctKey key)
 	{
-		// NOTE: because we currently have some bugs with "ctx" on server side, we are not filtering the query by AD_Client_ID
-		// but we just rely on M_Product_Category_ID/C_AcctSchema_ID.
-
-		final IQuery<I_M_Product_Category_Acct> query = queryBL
-				.createQueryBuilderOutOfTrx(I_M_Product_Category_Acct.class)
-				.addEqualsFilter(I_M_Product_Category_Acct.COLUMNNAME_M_Product_Category_ID, key.getProductCategoryId())
-				.addEqualsFilter(I_M_Product_Category_Acct.COLUMNNAME_C_AcctSchema_ID, key.getAcctSchemaId())
-				.addOnlyActiveRecordsFilter()
-				.create();
-		I_M_Product_Category_Acct productCategoryAcct = query.firstOnly(I_M_Product_Category_Acct.class);
+		I_M_Product_Category_Acct record = retrieveProductCategoryAcctRecord(key.getProductCategoryId(), key.getAcctSchemaId()).orElse(null);
 
 		//
 		// If no product category accounting record was not found and we are asked to create one on fly
 		// We are passing the MProductCategory to create all that are missing
-		if (productCategoryAcct == null)
+		if (record == null)
 		{
-			final I_M_Product_Category pc = productDAO.getProductCategoryById(key.getProductCategoryId());
-			InterfaceWrapperHelper.getPO(pc).insert_Accounting(
-					I_M_Product_Category_Acct.Table_Name,
-					I_C_AcctSchema_Default.Table_Name,
-					null // whereClause
-			);
-
-			// Run the query again => we expect our product category acct record to be created
-			productCategoryAcct = query.firstOnlyNotNull(I_M_Product_Category_Acct.class);
+			createMissingProductCategoryAccts(key.getProductCategoryId());
+			record = retrieveProductCategoryAcctRecord(key.getProductCategoryId(), key.getAcctSchemaId())
+					.orElseThrow(() -> new AdempiereException("No M_Product_Category_Acct record was found/created for " + key));
 		}
 
-		return toProductCategoryAcct(productCategoryAcct);
+		return toProductCategoryAcct(record);
+	}
+
+	private Map<ProductCategoryAcctKey, ProductCategoryAcct> retrieveOrCreateAcctsByKeys(@NonNull final Set<ProductCategoryAcctKey> keys)
+	{
+		if (keys.isEmpty())
+		{
+			return ImmutableMap.of();
+		}
+
+		final IQueryBuilder<I_M_Product_Category_Acct> queryBuilder = queryBL.createQueryBuilderOutOfTrx(I_M_Product_Category_Acct.class)
+				.addOnlyActiveRecordsFilter();
+
+		final ICompositeQueryFilter<I_M_Product_Category_Acct> keysFilter = queryBuilder.addCompositeQueryFilter().setJoinOr();
+		for (ProductCategoryAcctKey key : keys)
+		{
+			keysFilter.addCompositeQueryFilter()
+					.addEqualsFilter(I_M_Product_Category_Acct.COLUMNNAME_M_Product_Category_ID, key.getProductCategoryId())
+					.addEqualsFilter(I_M_Product_Category_Acct.COLUMNNAME_C_AcctSchema_ID, key.getAcctSchemaId())
+					.addOnlyActiveRecordsFilter();
+		}
+
+		final ImmutableMap<ProductCategoryAcctKey, I_M_Product_Category_Acct> recordsByKey = queryBuilder.stream()
+				.collect(ImmutableMap.toImmutableMap(
+						record -> ProductCategoryAcctKey.of(ProductCategoryId.ofRepoId(record.getM_Product_Category_ID()), AcctSchemaId.ofRepoId(record.getC_AcctSchema_ID())),
+						record -> record
+				));
+
+		final HashMap<ProductCategoryAcctKey, ProductCategoryAcct> productCategoryAccts = new HashMap<>();
+		for (ProductCategoryAcctKey key : keys)
+		{
+			I_M_Product_Category_Acct record = recordsByKey.get(key);
+
+			//
+			// If no product category accounting record was not found and we are asked to create one on fly
+			// We are passing the MProductCategory to create all that are missing
+			if (record == null)
+			{
+				createMissingProductCategoryAccts(key.getProductCategoryId());
+				record = retrieveProductCategoryAcctRecord(key.getProductCategoryId(), key.getAcctSchemaId())
+						.orElseThrow(() -> new AdempiereException("No M_Product_Category_Acct record was found/created for " + key));
+			}
+
+			productCategoryAccts.put(key, toProductCategoryAcct(record));
+		}
+
+		return productCategoryAccts;
+	}
+
+	private void createMissingProductCategoryAccts(@NonNull final ProductCategoryId productCategoryId)
+	{
+		final I_M_Product_Category pc = productDAO.getProductCategoryById(productCategoryId);
+		InterfaceWrapperHelper.getPO(pc).insert_Accounting(
+				I_M_Product_Category_Acct.Table_Name,
+				I_C_AcctSchema_Default.Table_Name,
+				null // whereClause
+		);
+	}
+
+	private Optional<I_M_Product_Category_Acct> retrieveProductCategoryAcctRecord(
+			@NonNull final ProductCategoryId productCategoryId,
+			@NonNull final AcctSchemaId acctSchemaId
+	)
+	{
+		return queryBL.createQueryBuilderOutOfTrx(I_M_Product_Category_Acct.class)
+				.addOnlyActiveRecordsFilter()
+				.addEqualsFilter(I_M_Product_Category_Acct.COLUMNNAME_M_Product_Category_ID, productCategoryId)
+				.addEqualsFilter(I_M_Product_Category_Acct.COLUMNNAME_C_AcctSchema_ID, acctSchemaId)
+				.firstOptional();
 	}
 
 	private static ProductCategoryAcct toProductCategoryAcct(@NonNull final I_M_Product_Category_Acct record)
