bc4664809bebb186bb070bc2ce1e46ee0950e261
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-08-23 15:07:59 +0300

    update trl proto https://github.com/metasfresh/metasfresh/issues/13480

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5652540_sys_gh13480_Duplicate_TRL_Corrections_From_AD_Element.sql
@@ -0,0 +1,12 @@
+-- 2022-08-23T09:12:54.547Z
+UPDATE C_DocType_Trl SET Name='Call-off purchase order',Updated=TO_TIMESTAMP('2022-08-23 12:12:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541034
+;
+
+-- 2022-08-23T09:13:10.663Z
+UPDATE C_DocType_Trl SET Name='Call-off sales order', PrintName='Call-off sales order',Updated=TO_TIMESTAMP('2022-08-23 12:13:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541028
+;
+
+-- 2022-08-23T09:13:19.372Z
+UPDATE C_DocType_Trl SET PrintName='Call-off purchase order',Updated=TO_TIMESTAMP('2022-08-23 12:13:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=541034
+;
+
