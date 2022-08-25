6c70e7e4046bd0e333e5a0a483db12184f0cebe7
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-08-25 15:15:49 +0300

    update trl proto https://github.com/metasfresh/metasfresh/issues/13480

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_TRL_Tables_On_AD_Element_TRL_Update.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_TRL_Tables_On_AD_Element_TRL_Update.sql
@@ -21,68 +21,13 @@ BEGIN
 
 	--
 	-- AD_Process_Para_Trl
-	UPDATE AD_Process_Para_Trl t
-	SET
-		IsTranslated = x.IsTranslated,
-		Name = x.Name,
-		Description = x.Description,
-		Help = x.Help
-	FROM
-	(
-		select
-			pp.AD_Process_Para_ID,
-			etrl.AD_Element_ID,
-			etrl.ad_language,
-			etrl.IsTranslated,
-			etrl.Name,
-			etrl.Description,
-			etrl.Help
-		from AD_Element_Trl_Effective_v etrl
-			join AD_Process_Para pp on pp.AD_Element_ID = etrl.AD_Element_ID
-		where
-			etrl.AD_Element_ID = p_AD_Element_ID
-			and pp.IsCentrallyMaintained = 'Y'
-			and (p_AD_Language is null OR etrl.AD_Language = p_AD_Language)
-	) x
-	WHERE
-		t.AD_Process_Para_ID = x.AD_Process_Para_ID 
-		and t.AD_Language = x.AD_Language
-	;
-	--
-	GET DIAGNOSTICS update_count = ROW_COUNT;
-	raise notice 'Update % AD_Process_Para_Trl rows using AD_Element_ID=%, AD_Language=%', update_count, p_AD_Element_ID, p_AD_Language;
+    PERFORM update_Process_Para_Translation_From_AD_Element (p_AD_Element_ID, p_AD_Language);
 
 
 --
 -- AD_PrintFormatItem_TRL
-	UPDATE  AD_PrintFormatItem_Trl t
-	SET
-		IsTranslated = x.IsTranslated,
-		PrintName = x.PrintName
-	FROM
-	(
-		select
-			pfi.AD_PrintFormatItem_ID,
-			etrl.AD_Element_ID,
-			etrl.AD_Language,
-			etrl.IsTranslated,
-			etrl.PrintName
-		from AD_Element_Trl_Effective_v etrl
-			join AD_Column c on c.AD_Element_ID = etrl.AD_Element_ID
-			join AD_PrintFormatItem pfi on c.AD_Column_ID = pfi.AD_Column_ID
-		where
-			etrl.AD_Element_ID = p_AD_Element_ID
-			and pfi.IsCentrallyMaintained = 'Y'
-			and (p_AD_Language is null OR etrl.AD_Language = p_AD_Language)
-	) x
-	WHERE
-		t.AD_PrintFormatItem_ID = x.AD_PrintFormatItem_ID
-		and t.AD_Language = x.AD_Language
-	;
-	--
-	GET DIAGNOSTICS update_count = ROW_COUNT;
-	raise notice 'Update % AD_PrintFormatItem_Trl rows using AD_Element_ID=%, AD_Language=%', update_count, p_AD_Element_ID, p_AD_Language;
 
+	PERFORM update_PrintFormatItem_Translation_From_AD_Element(p_AD_Element_ID, p_AD_Language);
 	--
 	-- AD_Tab_TRL
 	PERFORM update_Tab_Translation_From_AD_Element(p_AD_Element_ID, p_AD_Language);
