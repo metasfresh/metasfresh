a2d5b27f124828fdd445f02e3a0ec9b492435b86
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-09-01 22:00:14 +0300

    set AD_Column's name and description from the base language https://github.com/metasfresh/metasfresh/issues/13480"

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_Column_Translation_From_AD_Element.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_Column_Translation_From_AD_Element.sql
@@ -27,18 +27,19 @@ BEGIN
     GET DIAGNOSTICS update_count = ROW_COUNT;
     RAISE NOTICE 'Update % AD_Column_Trl rows using AD_Element_ID=%, AD_Language=%', update_count, p_AD_Element_ID, p_AD_Language;
 
-    IF (p_AD_Language IS NULL OR isBaseAD_Language(p_AD_Language) = 'Y') THEN
+   -- IF (p_AD_Language IS NULL OR isBaseAD_Language(p_AD_Language) = 'Y') THEN
         UPDATE AD_Column c
         SET Name        = e_trl.Name,
             Description = e_trl.Description
         FROM AD_Element_Trl_Effective_v e_trl
         WHERE (p_AD_Element_ID IS NULL OR e_trl.AD_Element_ID = p_AD_Element_ID)
           AND (p_AD_Language IS NULL OR e_trl.AD_Language = p_AD_Language)
-          AND c.ad_element_id = e_trl.ad_element_id;
+          AND c.ad_element_id = e_trl.ad_element_id
+        and isbasead_language(e_trl.ad_language) = 'Y';
         --
         GET DIAGNOSTICS update_count = ROW_COUNT;
         RAISE NOTICE 'Update % AD_Column rows using AD_Element_ID=%, AD_Language=%', update_count, p_AD_Element_ID, p_AD_Language;
-    END IF;
+  --  END IF;
 END;
 $BODY$
     LANGUAGE plpgsql
