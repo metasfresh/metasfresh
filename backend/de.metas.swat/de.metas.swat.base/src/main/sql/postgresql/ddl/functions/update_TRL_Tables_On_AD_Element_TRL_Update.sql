0c9877a189c506fc9efa36e928349d29e65e7b5b
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-09-02 10:39:35 +0300

    change comment on the update_trl_tables_on_ad_element_trl_update function https://github.com/metasfresh/metasfresh/issues/13480"

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_TRL_Tables_On_AD_Element_TRL_Update.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/functions/update_TRL_Tables_On_AD_Element_TRL_Update.sql
@@ -39,22 +39,5 @@ $BODY$
 ;
 
 COMMENT ON FUNCTION update_trl_tables_on_ad_element_trl_update(numeric, character varying) IS
-    'When the AD_Element_trl has one of its values changed, the change shall also propagate to the linked table entries:
-    -AD_Column_TRL -- name, isTranslated
-    -AD_Process_Para_TRL -- name, description, help, isTranslated
-    -AD_Field_TRL --name, description, help, isTranslated
-    -AD_PrintFormatItem_TRL -- printname, isTranslated
-    -AD_Tab_TRL -- name, isTranslated, description, help, commitwarning
-    -AD_Window_TRL -- name, isTranslated, description, help
-    -AD_Menu_TRL-- Name, IsTranslated, description, help, commitwarning, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb
-    The following column are updated from the AD_Element_trl of the base language:
-    -AD_Element -- name , printname, description, help, commitwarning, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb, po_name, po_printname, po_description, po_help
-    -AD_Column -- name, isTranslated
-    -AD_Process_Para -- name, description, help, isTranslated
-    -AD_Field --name, description, help, isTranslated
-    -AD_PrintFormatItem -- printname, isTranslated
-    -AD_Tab -- name, isTranslated, description, help, commitwarning
-    -AD_Window -- name, isTranslated, description, help
-    -AD_Menu -- Name, IsTranslated, description, help, commitwarning, webui_namebrowse, webui_namenew, webui_namenewbreadcrumb
-    '
+    'Propagates AD_Element_Trl changes to all depending entities (e.g. AD_Field, AD_Field_Trl, AD_Window, AD_Window_Trl, etc).'
 ;
