f906469f09c1a99b90bfb7d1d06674183e0d27b0
Teo Sarca <teo.sarca@metasfresh.com>
2022-09-03 10:45:29 +0300

    POInfo.getPOInfoNotNull (copy from master)

--- backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/POInfo.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/POInfo.java
@@ -81,9 +81,20 @@ public final class POInfo implements Serializable, ColumnDisplayTypeProvider
 	}
 
 	@Nullable
-	public static POInfo getPOInfo(final String tableName)
+	public static POInfo getPOInfo(@NonNull final String tableName)
 	{
 		return getPOInfoMap().getByTableNameOrNull(tableName);
+	}
+
+	@NonNull
+	public static POInfo getPOInfoNotNull(@NonNull final String tableName)
+	{
+		final POInfo poInfo = getPOInfoMap().getByTableNameOrNull(tableName);
+		if(poInfo == null)
+		{
+			throw new AdempiereException("No POInfo found for "+tableName);
+		}
+		return poInfo;
 	}
 
 	public static Optional<POInfo> getPOInfoIfPresent(@NonNull final String tableName)
