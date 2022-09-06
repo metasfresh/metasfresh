035068248e7fd6e6db2e1917bf1291df7afa0e92
riadm <riad.mennour@owiin.com>
2022-09-06 11:53:33 +0100

    Fix Zoom Across where Project reference is pointing to itself

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5654680_gh13555_Fix_Project_Zoom_Across.sql
@@ -0,0 +1,9 @@
+-- Window: Projekt (webUI), InternalName=Projekt (webUI)
+-- 2022-09-06T09:59:29.630Z
+UPDATE AD_Window SET IsEnableRemoteCacheInvalidation='Y', IsExcludeFromZoomTargets='Y',Updated=TO_TIMESTAMP('2022-09-06 10:59:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540668
+;
+
+-- 2022-09-06T10:12:40.909Z
+UPDATE AD_Ref_Table SET AD_Window_ID=540668,Updated=TO_TIMESTAMP('2022-09-06 11:12:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=541136
+;
+
