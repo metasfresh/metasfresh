20c385fc7b51dde0b26442eb73eb7141871f898b
riadm <riad.mennour@owiin.com>
2022-09-06 16:04:19 +0100

    Fix Zoom Across where Project reference is pointing to itself

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5654680_gh13555_Fix_Project_Zoom_Across.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5654680_gh13555_Fix_Project_Zoom_Across.sql
@@ -3,7 +3,26 @@
 UPDATE AD_Window SET IsEnableRemoteCacheInvalidation='Y', IsExcludeFromZoomTargets='Y',Updated=TO_TIMESTAMP('2022-09-06 10:59:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540668
 ;
 
--- 2022-09-06T10:12:40.909Z
-UPDATE AD_Ref_Table SET AD_Window_ID=540668,Updated=TO_TIMESTAMP('2022-09-06 11:12:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=541136
+UPDATE ad_window SET zoomintopriority = 1 WHERE ad_window_id=540668
+;
+
+-- Table: C_Project
+-- 2022-09-06T14:33:57.260Z
+UPDATE AD_Table SET AD_Window_ID=540668,Updated=TO_TIMESTAMP('2022-09-06 15:33:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=203
+;
+
+-- Table: C_Project
+-- 2022-09-06T14:48:33.627Z
+UPDATE AD_Table SET PO_Window_ID=540668,Updated=TO_TIMESTAMP('2022-09-06 15:48:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=203
+;
+
+-- Window: Projekt (Verkauf), InternalName=286
+-- 2022-09-06T14:58:40.334Z
+UPDATE AD_Window SET IsExcludeFromZoomTargets='Y',Updated=TO_TIMESTAMP('2022-09-06 15:58:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=286
+;
+
+-- Window: Seminar, InternalName=Seminar
+-- 2022-09-06T15:00:10.527Z
+UPDATE AD_Window SET IsExcludeFromZoomTargets='Y',Updated=TO_TIMESTAMP('2022-09-06 16:00:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=540680
 ;
 
