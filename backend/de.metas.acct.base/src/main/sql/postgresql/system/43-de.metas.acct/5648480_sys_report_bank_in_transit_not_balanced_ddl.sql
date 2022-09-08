b72ca1a9e2fb6bdb9d648ebc53e4907d6f9cbc26
Teo Sarca <teo.sarca@metasfresh.com>
2022-09-08 10:28:15 +0300

    report_bank_in_transit_not_balanced: introduce p_C_BankStatement_ID param

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5648480_sys_report_bank_in_transit_not_balanced_ddl.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5648480_sys_report_bank_in_transit_not_balanced_ddl.sql
@@ -1,25 +1,3 @@
-/*
- * #%L
- * de.metas.acct.base
- * %%
- * Copyright (C) 2022 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 DROP FUNCTION IF EXISTS de_metas_acct.report_bank_in_transit_not_balanced
 (
     p_EndDate    date,
@@ -27,9 +5,19 @@ DROP FUNCTION IF EXISTS de_metas_acct.report_bank_in_transit_not_balanced
 )
 ;
 
+DROP FUNCTION IF EXISTS de_metas_acct.report_bank_in_transit_not_balanced
+(
+    p_EndDate            date,
+    p_Account_ID         numeric,
+    p_C_BankStatement_ID numeric
+)
+;
+
+
 CREATE OR REPLACE FUNCTION de_metas_acct.report_bank_in_transit_not_balanced(
-    p_EndDate    date = NULL,
-    p_Account_ID numeric = NULL
+    p_EndDate            date = NULL,
+    p_Account_ID         numeric = NULL,
+    p_C_BankStatement_ID numeric = NULL
 )
     RETURNS table
             (
@@ -51,6 +39,7 @@ DECLARE
     v_rowcount    integer;
 BEGIN
     RAISE NOTICE 'Using p_EndDate=%', p_EndDate;
+    RAISE NOTICE 'Using p_C_BankStatement_ID=%', p_C_BankStatement_ID;
 
     IF (p_Account_ID IS NOT NULL AND p_Account_ID > 0) THEN
         v_Account_IDs := ARRAY [p_Account_ID];
@@ -68,6 +57,66 @@ BEGIN
 
     --
     --
+    --
+    RAISE NOTICE 'Pre-selecting Fact_Acct records...';
+    DROP TABLE IF EXISTS tmp_fact_acct_bankInTransit_prefiltered;
+    CREATE TEMPORARY TABLE tmp_fact_acct_bankInTransit_prefiltered
+    (
+        fact_acct_id numeric,
+        tablename    text,
+        c_payment_id numeric
+    );
+    --
+    IF (p_C_BankStatement_ID IS NOT NULL) THEN
+        INSERT INTO tmp_fact_acct_bankInTransit_prefiltered (fact_acct_id, tablename, c_payment_id)
+        SELECT fa.fact_acct_id, 'C_Payment', p.c_payment_id
+        FROM c_payment p
+                 INNER JOIN fact_acct fa ON fa.ad_table_id = get_table_id('C_Payment') AND fa.record_id = p.c_payment_id
+        WHERE p.c_bankstatement_id = p_C_BankStatement_ID
+          AND fa.account_id = ANY (v_Account_IDs)
+          AND (p_EndDate IS NULL OR fa.dateacct::date <= p_EndDate);
+        --
+        GET DIAGNOSTICS v_rowcount = ROW_COUNT;
+        RAISE NOTICE 'Pre-selected % Fact_Acct rows (from C_Payment into tmp_fact_acct_bankInTransit_prefiltered)', v_rowcount;
+
+        INSERT INTO tmp_fact_acct_bankInTransit_prefiltered (fact_acct_id, tablename, c_payment_id)
+        SELECT fa.fact_acct_id,
+               'C_BankStatement',
+               (CASE
+                    WHEN fa.subline_id IS NOT NULL THEN (SELECT bslr.c_payment_id FROM c_bankstatementline_ref bslr WHERE bslr.c_bankstatementline_ref_id = fa.subline_id)
+                    WHEN fa.line_id IS NOT NULL    THEN (SELECT bsl.c_payment_id FROM c_bankstatementline bsl WHERE bsl.c_bankstatementline_id = fa.line_id)
+                END) AS c_payment_id
+        FROM fact_acct fa
+        WHERE fa.ad_table_id = get_table_id('C_BankStatement')
+          AND fa.record_id = p_C_BankStatement_ID
+          AND fa.account_id = ANY (v_Account_IDs)
+          AND (p_EndDate IS NULL OR fa.dateacct::date <= p_EndDate);
+        --
+        GET DIAGNOSTICS v_rowcount = ROW_COUNT;
+        RAISE NOTICE 'Pre-selected % Fact_Acct rows (from C_BankStatement into tmp_fact_acct_bankInTransit_prefiltered)', v_rowcount;
+    ELSE
+        INSERT INTO tmp_fact_acct_bankInTransit_prefiltered (fact_acct_id, tablename, c_payment_id)
+        SELECT fa.fact_acct_id,
+               t.tablename,
+               (CASE
+                    WHEN t.tablename = 'C_Payment'                                     THEN fa.record_id
+                    WHEN t.tablename = 'C_BankStatement' AND fa.subline_id IS NOT NULL THEN (SELECT bslr.c_payment_id FROM c_bankstatementline_ref bslr WHERE bslr.c_bankstatementline_ref_id = fa.subline_id)
+                    WHEN t.tablename = 'C_BankStatement' AND fa.line_id IS NOT NULL    THEN (SELECT bsl.c_payment_id FROM c_bankstatementline bsl WHERE bsl.c_bankstatementline_id = fa.line_id)
+                END) AS c_payment_id
+        FROM fact_acct fa
+                 INNER JOIN ad_table t ON t.ad_table_id = fa.ad_table_id
+        WHERE --
+                fa.account_id = ANY (v_Account_IDs)
+          AND (p_EndDate IS NULL OR fa.dateacct::date <= p_EndDate);
+        --
+        GET DIAGNOSTICS v_rowcount = ROW_COUNT;
+        RAISE NOTICE 'Pre-selected % Fact_Acct rows (into tmp_fact_acct_bankInTransit_prefiltered)', v_rowcount;
+    END IF;
+    --
+    CREATE UNIQUE INDEX ON tmp_fact_acct_bankInTransit_prefiltered (fact_acct_id);
+
+    --
+    --
     RAISE NOTICE 'Selecting Fact_Acct records...';
     DROP TABLE IF EXISTS tmp_fact_acct_bankInTransit;
 
@@ -77,20 +126,16 @@ BEGIN
                                                 ELSE (t.tablename || '_' || t.record_id)::text
             END) AS aggregationKey,
            t.*
-    FROM (SELECT (CASE
-                      WHEN t.tablename = 'C_Payment'                                     THEN fa.record_id
-                      WHEN t.tablename = 'C_BankStatement' AND fa.subline_id IS NOT NULL THEN (SELECT bslr.c_payment_id FROM c_bankstatementline_ref bslr WHERE bslr.c_bankstatementline_ref_id = fa.subline_id)
-                      WHEN t.tablename = 'C_BankStatement' AND fa.line_id IS NOT NULL    THEN (SELECT bsl.c_payment_id FROM c_bankstatementline bsl WHERE bsl.c_bankstatementline_id = fa.line_id)
-                  END)    AS c_payment_id,
-                 t.tablename,
+    FROM (SELECT sel.c_payment_id,
+                 sel.tablename,
                  ev.value AS accountno,
                  ev.name  AS accountname,
                  fa.*
-          FROM fact_acct fa
-                   INNER JOIN ad_table t ON t.ad_table_id = fa.ad_table_id
+          FROM tmp_fact_acct_bankInTransit_prefiltered sel
+                   INNER JOIN fact_acct fa ON (fa.fact_acct_id = sel.fact_acct_id)
                    INNER JOIN c_elementvalue ev ON ev.c_elementvalue_id = fa.account_id
-          WHERE fa.account_id = ANY (v_Account_IDs)
-            AND (p_EndDate IS NULL OR fa.dateacct::date <= p_EndDate)) t
+             --
+         ) t
              LEFT OUTER JOIN c_payment p ON p.c_payment_id = t.c_payment_id;
     --
     GET DIAGNOSTICS v_rowcount = ROW_COUNT;
