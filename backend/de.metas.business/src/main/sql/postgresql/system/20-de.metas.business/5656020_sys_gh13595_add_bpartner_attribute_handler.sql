2bd1803b89057954897ba208b1e27be882b4ef54
Chems Nouioua <chems.nouioua@gmail.com>
2022-09-13 19:50:17 +0100

    gh13595 add sysconfig to config business partner attribute value handler cache

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5656020_sys_gh13595_add_bpartner_attribute_handler.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5656020_sys_gh13595_add_bpartner_attribute_handler.sql
@@ -1,3 +1,7 @@
+-- 2022-09-13T18:18:10.822Z
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541481,'S',TO_TIMESTAMP('2022-09-13 19:18:10','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','org.adempiere.mm.attributes.spi.impl.HUVendorBPartnerAttributeValuesProvider.maxBusinessPartners',TO_TIMESTAMP('2022-09-13 19:18:10','YYYY-MM-DD HH24:MI:SS'),100,'20')
+;
+
 -- 2022-09-13T18:30:35.209Z
 INSERT INTO AD_JavaClass (AD_Client_ID,AD_JavaClass_ID,AD_JavaClass_Type_ID,AD_Org_ID,Classname,Created,CreatedBy,EntityType,IsActive,IsInterface,Name,Updated,UpdatedBy) VALUES (0,540070,540026,0,'org.adempiere.mm.attributes.spi.impl.HUBusinessPartnerAttributeHandler',TO_TIMESTAMP('2022-09-13 19:30:35','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.handlingunits','Y','N','Business Partner Attribute Value Handler',TO_TIMESTAMP('2022-09-13 19:30:35','YYYY-MM-DD HH24:MI:SS'),100)
 ;
