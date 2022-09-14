93c9b5f9ade82b8e370157dc9895625e85628e43
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-09-14 14:11:16 +0200

    solve "pending trigger event" error
    https://github.com/metasfresh/metasfresh/issues/13538

--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5646030_sys_gh13538_migrate_ediDesadvlinePack_to_ediDesadvPackWithItems.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5646030_sys_gh13538_migrate_ediDesadvlinePack_to_ediDesadvPackWithItems.sql
@@ -1,24 +1,3 @@
-/*
- * #%L
- * de.metas.edi
- * %%
- * Copyright (C) 2022 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 alter table edi_desadv_pack
     add edi_desadvline_pack_id numeric(10)
@@ -75,8 +54,4 @@ select nextval('edi_desadv_pack_item_seq'),
        oldPackLine.m_hu_packagingcode_tu_id
 from edi_desadvline_pack oldPackLine
          inner join edi_desadv_pack pack on oldPackLine.edi_desadvline_pack_id = pack.edi_desadvline_pack_id
-;
-
-alter table edi_desadv_pack
-    drop column edi_desadvline_pack_id
 ;
