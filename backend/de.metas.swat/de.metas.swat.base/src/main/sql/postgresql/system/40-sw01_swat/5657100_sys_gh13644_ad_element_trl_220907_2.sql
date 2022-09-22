6330d60279eb60d40451b5efd5c67c384ea63844
Chems NOUIOUA <chems.nouioua@gmail.com>
2022-09-22 10:51:49 +0100

    gh13644 import fr_CH element translations (#13645)
    * gh13644 import fr_CH element translations
    
    * gh13644 enable fr_CH as system language

---
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5657100_sys_gh13644_ad_element_trl_220907_2.sql
@@ -0,0 +1,38 @@
+
+CREATE TABLE migration_data.ad_element_trl_220907_2
+(
+    ad_element_id                numeric,
+    name_de                      text,
+    name_trad                    text,
+    name_trl                     text,
+    description_de               text,
+    description_trad             text,
+    description_trl              text,
+    po_name_de                   text,
+    po_name_trad                 text,
+    po_name_trl                  text,
+    po_printname_de              text,
+    po_printname_trad            text,
+    po_printname_trl             text,
+    po_description_de            text,
+    po_description_trad          text,
+    po_description_trl           text,
+    po_help_de                   text,
+    po_help_trad                 text,
+    po_help_trl                  text,
+    webui_namebrowse_de          text,
+    webui_namebrowse_trad        text,
+    webui_namebrowse_trl         text,
+    webui_namenew_de             text,
+    webui_namenew_trad           text,
+    webui_namenew_trl            text,
+    webui_namenewbreadcrumb_de   text,
+    webui_namenewbreadcrumb_trad text,
+    webui_namenewbreadcrumb_trl  text
+)
+;
+
+ALTER TABLE  migration_data.ad_element_trl_220907_2
+    OWNER TO metasfresh
+;
+
