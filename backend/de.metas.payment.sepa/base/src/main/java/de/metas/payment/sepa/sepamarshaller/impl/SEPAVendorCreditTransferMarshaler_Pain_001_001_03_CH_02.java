9524db8625c5465cd2571cbb4eb5b720c6381b4f
Cristina Ghita <cristina.ghita@metasfresh.com>
2022-09-29 11:38:11 +0300

    Use isBlank
    Co-authored-by: Tobias Schöneberg <metas-ts@users.noreply.github.com>

--- backend/de.metas.payment.sepa/base/src/main/java/de/metas/payment/sepa/sepamarshaller/impl/SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02.java
+++ backend/de.metas.payment.sepa/base/src/main/java/de/metas/payment/sepa/sepamarshaller/impl/SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02.java
@@ -960,7 +960,7 @@ public class SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02 implements 
 				de.metas.payment.esr.model.I_C_BP_BankAccount.class);
 
 		final String paymentMode;
-		if (bPBankAccount.isEsrAccount() && !Check.isEmpty(line.getStructuredRemittanceInfo(), true) && bPBankAccount.getQR_IBAN() == null)
+		if (bPBankAccount.isEsrAccount() && !Check.isEmpty(line.getStructuredRemittanceInfo(), true) && Check.isBlank(bPBankAccount.getQR_IBAN()))
 		{
 			paymentMode = PAYMENT_TYPE_1;
 		}
