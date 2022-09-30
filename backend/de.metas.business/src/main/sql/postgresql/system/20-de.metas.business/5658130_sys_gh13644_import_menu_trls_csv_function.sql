8147b799abab19344eb7206c782f7809b545cd0a
Chems Nouioua <chems.nouioua@gmail.com>
2022-09-30 15:03:13 +0100

    gh13644 fix query

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5658130_sys_gh13644_import_menu_trls_csv_function.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5658130_sys_gh13644_import_menu_trls_csv_function.sql
@@ -1,38 +1,19 @@
 DO
 $$
-    DECLARE
-        p_ad_element_id numeric;
-        p_ad_menu_id    numeric;
     BEGIN
+        UPDATE ad_element_trl
+        SET name                    = data.name_trl
+          , printname               = data.name_trl
+          , description             = data.description_trl
+          , webui_namenew           = data.webui_namenew_trl
+          , webui_namebrowse        = data.webui_namebrowse_trl
+          , webui_namenewbreadcrumb = data.webui_namenewbreadcrumb_trl
+        FROM migration_data."AD_Menu_Trad" data
+                 INNER JOIN ad_menu m ON data.ad_menu_id = m.ad_menu_id
+        WHERE ad_element_trl.ad_element_id = m.ad_element_id
+          AND ad_language IN ('fr_CH', 'fr_FR');
 
-        PERFORM backup_table('AD_Element_Trl');
-        PERFORM backup_table('AD_Menu_Trl');
-
-        FOR p_ad_menu_id IN SELECT DISTINCT ad_menu_id FROM migration_data."AD_Menu_Trad" WHERE LENGTH(TRIM(name_trl)) > 0 ORDER BY 1
-            LOOP
-                BEGIN
-                    -- Get corresponding element
-                    SELECT ad_element_id INTO p_ad_element_id FROM ad_menu WHERE ad_menu_id = p_ad_menu_id;
-
-                    -- Update element translation
-                    UPDATE ad_element_trl
-                    SET name                    = data.name_trl
-                      , printname               = data.name_trl
-                      , description             = data.description_trl
-                      , webui_namenew           = data.webui_namenew_trl
-                      , webui_namebrowse        = data.webui_namebrowse_trl
-                      , webui_namenewbreadcrumb = data.webui_namenewbreadcrumb_trl
-                    FROM migration_data."AD_Menu_Trad" data
-                    WHERE ad_element_trl.ad_element_id = p_ad_element_id
-                      AND ad_language IN ('fr_CH', 'fr_FR')
-                      AND LENGTH(TRIM(data.name_trl)) > 0;
-
-                END;
-            END LOOP;
-
-        -- sync elements translation, including menu
         PERFORM update_trl_tables_on_ad_element_trl_update(NULL, NULL);
     END;
 $$
 ;
-
