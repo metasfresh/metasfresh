6d7a96ec18548a262bc83ae57456d594d96fad17
Chems NOUIOUA <chems.nouioua@gmail.com>
2022-10-10 18:14:34 +0100

    gh13644 import config tables French translations (#13713)
    * gh13644 import config tables French translations
    
    * gh13644 import config  tables translations
    
    * gh13644 fix script name

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5658750_gh13644_c_currency_trad_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO migration_data.c_currency_trad (c_currency_id, description_de, description_trad, description_trl) VALUES (102, 'Euro (100 Cent)', 'Euro (100 Cent)', 'Euro (100 Cent)');
+INSERT INTO migration_data.c_currency_trad (c_currency_id, description_de, description_trad, description_trl) VALUES (100, 'Dollar (100 Cents)', 'Dollar (100 Cents)', 'Dollar (100 Cents)');
+INSERT INTO migration_data.c_currency_trad (c_currency_id, description_de, description_trad, description_trl) VALUES (318, 'Schweizer Franken (100 Rappen)', 'Schweizer Franken (100 Rappen)', 'Francs Suisses (100 centimes)');
