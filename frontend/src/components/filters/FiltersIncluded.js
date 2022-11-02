9897383f8c9d5a0bd0df3d61f7fb3fe866fa486c
Chems NOUIOUA <chems.nouioua@gmail.com>
2022-11-02 20:32:54 +0100

    #13784 provide translation for Filter caption (#13864)
    * #13784 provide translation for Filter caption
    
    * #13784 fix formatting
    
    * #13784 fix formatting
    
    (cherry picked from commit 45aa05deba4f534b8de3b9daf6ab76b3540176e5)

--- frontend/src/components/filters/FiltersIncluded.js
+++ frontend/src/components/filters/FiltersIncluded.js
@@ -100,7 +100,11 @@ class FiltersIncluded extends PureComponent {
         activeFiltersCaptions[activeFilter.filterId]) ||
       [];
     let panelCaption = activeFilter.isActive ? activeFilter.caption : '';
-    let buttonCaption = activeFilter.isActive ? activeFilter.caption : 'Filter';
+    let buttonCaption = activeFilter.isActive
+      ? activeFilter.caption
+      : counterpart.translate('window.filters.noActiveFilter.caption', {
+          fallback: 'Filter',
+        });
 
     if (captions.length) {
       buttonCaption = captions[0];
@@ -140,7 +144,9 @@ class FiltersIncluded extends PureComponent {
               )}: ${buttonCaption}`
             )
           ) : (
-            'Filter'
+            `${counterpart.translate('window.filters.noActiveFilter.caption', {
+              fallback: 'Filter',
+            })}`
           )}
         </button>
 
