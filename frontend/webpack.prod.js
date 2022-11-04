a1c07ed31cc085a7f9feb184c1bede056a3177a1
Teo Sarca <teo.sarca@metasfresh.com>
2022-11-04 14:08:22 +0200

    webpack: consider *.scss files too

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -85,7 +85,7 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
+        test: /\.(css|scss)$/,
         use: [
           'style-loader',
           { loader: 'css-loader', options: { importLoaders: 1 } },
