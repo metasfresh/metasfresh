4ea137807d940fe398c236f2e403326805f8cd51
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-11-08 14:32:25 +0100

    fix merge-compile-error

--- backend/de.metas.payment.sepa/base/src/test/java/de/metas/payment/sepa/sepamarshaller/impl/SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02_Test.java
+++ backend/de.metas.payment.sepa/base/src/test/java/de/metas/payment/sepa/sepamarshaller/impl/SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02_Test.java
@@ -132,7 +132,6 @@ public class SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02_Test
 		assertThat(xmlDocument.getCstmrCdtTrfInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.isBtchBookg()).isTrue());
 	}
 
-
 	@Test
 	public void createDocument_batch_noCollectiveTransfer_noRef() throws Exception
 	{
@@ -234,39 +233,6 @@ public class SEPAVendorCreditTransferMarshaler_Pain_001_001_03_CH_02_Test
 				.filter(Objects::nonNull)
 				.collect(Collectors.toSet());
 		assertThat(endToEndIds).containsExactlyInAnyOrder("Ref123", "Ref456", "Ref789");
-
-		assertThat(xmlDocument.getCstmrCdtTrfInitn().getPmtInf()).hasSize(2);
-		assertThat(xmlDocument.getCstmrCdtTrfInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.isBtchBookg()).isTrue());
-	}
-
-	@Test
-	public void createDocument_batch_with_QR_IBAN() throws Exception
-	{
-		final I_SEPA_Export sepaExport = createSEPAExport(
-				"org", // SEPA_CreditorName
-				"12345", // SEPA_CreditorIdentifier
-				"INGBNL2A" // bic
-		);
-		createSEPAExportLineQRVersion(sepaExport,
-							 "001",// SEPA_MandateRefNo
-							 "NL31INGB0000000044",// IBAN
-							 "INGBNL2A", // BIC
-							 new BigDecimal("100"), // amount
-							 eur, "210000000003139471430009017");
-
-		createSEPAExportLineQRVersion(sepaExport,
-							 "002", // SEPA_MandateRefNo
-							 "NL31INGB0000000044", // IBAN
-							 "INGBNL2A",// BIC
-							 new BigDecimal("40"), // amount
-							 chf, "210000000003139471430009017");
-
-		// invoke the method under test
-		xmlDocument = xmlGenerator.createDocument(sepaExport);
-
-		assertThat(xmlDocument.getCstmrCdtTrfInitn().getGrpHdr().getCtrlSum()).isEqualByComparingTo("140");
-		assertThat(xmlDocument.getCstmrCdtTrfInitn().getGrpHdr().getNbOfTxs()).isEqualTo("2");
-		assertThat(xmlDocument.getCstmrCdtTrfInitn().getGrpHdr().getInitgPty().getNm()).isEqualTo(sepaExport.getSEPA_CreditorName());
 
 		assertThat(xmlDocument.getCstmrCdtTrfInitn().getPmtInf()).hasSize(2);
 		assertThat(xmlDocument.getCstmrCdtTrfInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.isBtchBookg()).isTrue());
