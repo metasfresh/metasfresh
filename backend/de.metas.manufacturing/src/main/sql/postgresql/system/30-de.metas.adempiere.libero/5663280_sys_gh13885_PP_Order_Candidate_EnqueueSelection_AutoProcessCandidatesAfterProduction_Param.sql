6df13d9323722ae6ec77c10ab0e64429a62c2f0a
av-ps <alexandra.vezendan@pevesoft.ro>
2022-11-08 17:03:32 +0200

    update trls

--- backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5663280_sys_gh13885_PP_Order_Candidate_EnqueueSelection_AutoProcessCandidatesAfterProduction_Param.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5663280_sys_gh13885_PP_Order_Candidate_EnqueueSelection_AutoProcessCandidatesAfterProduction_Param.sql
@@ -210,3 +210,198 @@ UPDATE AD_Element_Trl SET Description='If true, selected candidates will be mark
 UPDATE AD_Process_Para SET AD_Element_ID=581640, DisplayLogic='1=0', ReadOnlyLogic='',Updated=TO_TIMESTAMP('2022-11-07 13:09:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=542360
 ;
 
+-- 2022-11-08T14:51:52.617Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Always mark as processed ',Updated=TO_TIMESTAMP('2022-11-08 16:51:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='en_US'
+;
+
+-- 2022-11-08T14:51:52.650Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'en_US')
+;
+
+-- 2022-11-08T14:52:05.169Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='If ticked, selected candidates will be marked as processed immediately after the manufacturing order was created, even if it''s a partial quantity.', Name='Always mark as processed ',Updated=TO_TIMESTAMP('2022-11-08 16:52:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='nl_NL'
+;
+
+-- 2022-11-08T14:52:05.170Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'nl_NL')
+;
+
+-- 2022-11-08T14:52:07.086Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='If ticked, selected candidates will be marked as processed immediately after the manufacturing order was created, even if it''s a partial quantity.',Updated=TO_TIMESTAMP('2022-11-08 16:52:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='en_US'
+;
+
+-- 2022-11-08T14:52:07.087Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'en_US')
+;
+
+-- 2022-11-08T14:52:11.031Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET PrintName='Always mark as processed ',Updated=TO_TIMESTAMP('2022-11-08 16:52:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='en_US'
+;
+
+-- 2022-11-08T14:52:11.032Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'en_US')
+;
+
+-- 2022-11-08T14:52:13.804Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET PrintName='Always mark as processed ',Updated=TO_TIMESTAMP('2022-11-08 16:52:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='nl_NL'
+;
+
+-- 2022-11-08T14:52:13.807Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'nl_NL')
+;
+
+-- 2022-11-08T14:52:23.949Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Immer als verarbeitet markieren',Updated=TO_TIMESTAMP('2022-11-08 16:52:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='de_DE'
+;
+
+-- 2022-11-08T14:52:23.951Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'de_DE')
+;
+
+-- 2022-11-08T14:52:23.960Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581640,'de_DE')
+;
+
+-- 2022-11-08T14:52:23.964Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='AutoProcessCandidatesAfterProduction', Name='Immer als verarbeitet markieren', Description='Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE AD_Element_ID=581640
+;
+
+-- 2022-11-08T14:52:23.965Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='AutoProcessCandidatesAfterProduction', Name='Immer als verarbeitet markieren', Description='Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', Help=NULL, AD_Element_ID=581640 WHERE UPPER(ColumnName)='AUTOPROCESSCANDIDATESAFTERPRODUCTION' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2022-11-08T14:52:23.967Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='AutoProcessCandidatesAfterProduction', Name='Immer als verarbeitet markieren', Description='Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE AD_Element_ID=581640 AND IsCentrallyMaintained='Y'
+;
+
+-- 2022-11-08T14:52:23.968Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Immer als verarbeitet markieren', Description='Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=581640) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 581640)
+;
+
+-- 2022-11-08T14:52:23.989Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Autoprozess-Kandidaten nach der Produktion', Name='Immer als verarbeitet markieren' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c  WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=581640)
+;
+
+-- 2022-11-08T14:52:23.991Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Immer als verarbeitet markieren', Description='Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 581640
+;
+
+-- 2022-11-08T14:52:23.993Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_WINDOW SET Name='Immer als verarbeitet markieren', Description='Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE AD_Element_ID = 581640
+;
+
+-- 2022-11-08T14:52:23.994Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET   Name = 'Immer als verarbeitet markieren', Description = 'Wenn dies der Fall ist, werden die ausgewählten Kandidaten sofort als bearbeitet markiert, nachdem die Menge des Kandidaten bei der Erstellung des Produktionsauftrags berücksichtigt wurde, auch wenn es sich um eine Teilmenge handelt.', WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 581640
+;
+
+-- 2022-11-08T14:52:25.608Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Name='Immer als verarbeitet markieren',Updated=TO_TIMESTAMP('2022-11-08 16:52:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='de_CH'
+;
+
+-- 2022-11-08T14:52:25.610Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'de_CH')
+;
+
+-- 2022-11-08T14:52:27.210Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET PrintName='Immer als verarbeitet markieren',Updated=TO_TIMESTAMP('2022-11-08 16:52:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='de_DE'
+;
+
+-- 2022-11-08T14:52:27.211Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'de_DE')
+;
+
+-- 2022-11-08T14:52:27.221Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581640,'de_DE')
+;
+
+-- 2022-11-08T14:52:27.222Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_PrintFormatItem pi SET PrintName='Immer als verarbeitet markieren', Name='Immer als verarbeitet markieren' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c  WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=581640)
+;
+
+-- 2022-11-08T14:52:34.498Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', PrintName='Immer als verarbeitet markieren',Updated=TO_TIMESTAMP('2022-11-08 16:52:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='de_CH'
+;
+
+-- 2022-11-08T14:52:34.499Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'de_CH')
+;
+
+-- 2022-11-08T14:52:37.230Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Element_Trl SET Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.',Updated=TO_TIMESTAMP('2022-11-08 16:52:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581640 AND AD_Language='de_DE'
+;
+
+-- 2022-11-08T14:52:37.231Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581640,'de_DE')
+;
+
+-- 2022-11-08T14:52:37.244Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581640,'de_DE')
+;
+
+-- 2022-11-08T14:52:37.246Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET ColumnName='AutoProcessCandidatesAfterProduction', Name='Immer als verarbeitet markieren', Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE AD_Element_ID=581640
+;
+
+-- 2022-11-08T14:52:37.246Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='AutoProcessCandidatesAfterProduction', Name='Immer als verarbeitet markieren', Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', Help=NULL, AD_Element_ID=581640 WHERE UPPER(ColumnName)='AUTOPROCESSCANDIDATESAFTERPRODUCTION' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- 2022-11-08T14:52:37.248Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Process_Para SET ColumnName='AutoProcessCandidatesAfterProduction', Name='Immer als verarbeitet markieren', Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE AD_Element_ID=581640 AND IsCentrallyMaintained='Y'
+;
+
+-- 2022-11-08T14:52:37.249Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET Name='Immer als verarbeitet markieren', Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE (AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=581640) AND AD_Name_ID IS NULL ) OR (AD_Name_ID = 581640)
+;
+
+-- 2022-11-08T14:52:37.267Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET Name='Immer als verarbeitet markieren', Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 581640
+;
+
+-- 2022-11-08T14:52:37.269Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_WINDOW SET Name='Immer als verarbeitet markieren', Description='Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', Help=NULL WHERE AD_Element_ID = 581640
+;
+
+-- 2022-11-08T14:52:37.270Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Menu SET   Name = 'Immer als verarbeitet markieren', Description = 'Wenn angehakt werden die ausgewählten Kandidaten nach dem Erstellen des Produktionsauftrags sofort als bearbeitet markiert, auch wenn es sich um eine Teilmenge handelt.', WEBUI_NameBrowse = NULL, WEBUI_NameNew = NULL, WEBUI_NameNewBreadcrumb = NULL WHERE AD_Element_ID = 581640
+;
+
