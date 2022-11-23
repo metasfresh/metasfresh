e3c4a57187d9b73b8a1e24d7c3f36157080f6b66
cp-ps <cp-ps@pevesoft.ro>
2022-11-23 12:13:09 +0200

    fix known duplicated trls

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5658129_sys_gh13644_import_menu_trls_csv_function_fix_known_dups.sql
@@ -0,0 +1,6 @@
+
+update ad_element_trl set name = 'Partenaires commerciaux Dist-Orgs' where ad_element_id = 574339 and ad_language = 'fr_CH'
+;
+
+update ad_element_trl set name = 'Planification des livraisons - Révision des exportations' where ad_element_id = 577793 and ad_language = 'fr_CH'
+;
