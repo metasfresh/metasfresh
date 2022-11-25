e4b26adbc3f794c93747b2cac292fd6b2d0e0255
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-11-25 16:27:22 +0200

    ad_names https://github.com/metasfresh/metasfresh/issues/13942

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5666170_sys_gh13942_Delivery_Planning_AD_Names.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5666170_sys_gh13942_Delivery_Planning_AD_Names.sql
@@ -498,3 +498,35 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=708109
 /* DDL */ select AD_Element_Link_Create_Missing_Field(708109)
 ;
 
+
+
+-- 2022-11-25T14:26:15.030Z
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,581748,0,TO_TIMESTAMP('2022-11-25 16:26:14','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','Material','Material',TO_TIMESTAMP('2022-11-25 16:26:14','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 2022-11-25T14:26:15.031Z
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=581748 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- Field: Delivery Planning(541632,D) -> Delivery Planning(546674,D) -> Material
+-- Column: M_Delivery_Planning.M_Product_ID
+-- 2022-11-25T14:26:31.218Z
+UPDATE AD_Field SET AD_Name_ID=581748, Description=NULL, Help=NULL, Name='Material',Updated=TO_TIMESTAMP('2022-11-25 16:26:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=708110
+;
+
+-- 2022-11-25T14:26:31.219Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(581748) 
+;
+
+-- 2022-11-25T14:26:31.225Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=708110
+;
+
+-- 2022-11-25T14:26:31.226Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(708110)
+;
+
+
+
+
+
