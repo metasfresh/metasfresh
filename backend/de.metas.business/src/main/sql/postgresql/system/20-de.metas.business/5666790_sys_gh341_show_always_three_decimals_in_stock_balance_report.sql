7abd0ab5db00ede9bd9600d78f0c1235b03bbbe0
Chems Nouioua <chems.nouioua@gmail.com>
2022-11-30 16:34:26 +0100

    #13942 Delivery Planning Master Data fixes

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5666790_sys_gh341_show_always_three_decimals_in_stock_balance_report.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION formatQtyToDisplay(p_qty numeric) RETURNS character varying
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    precision int;
+BEGIN
+    SELECT get_sysconfig_value('webui.frontend.widget.Quantity.defaultPrecision', '2')::INTEGER INTO precision;
+    RETURN to_char(coalesce(p_qty, 0), '99999990.' || repeat('0', precision));
+END;
+$$
+;
+
+ALTER FUNCTION formatQtyToDisplay(p_qty numeric ) OWNER TO metasfresh
+;
