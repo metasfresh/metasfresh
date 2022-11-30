dbe98896889080b252cada5c966960a3dfa37839
Chems Nouioua <chems.nouioua@gmail.com>
2022-11-30 16:53:31 +0100

    #13942 Delivery Planning Master Data fixes

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5666790_sys_gh341_show_always_three_decimals_in_stock_balance_report.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5666790_sys_gh341_show_always_three_decimals_in_stock_balance_report.sql
@@ -6,10 +6,14 @@ DECLARE
     precision int;
 BEGIN
     SELECT get_sysconfig_value('webui.frontend.widget.Quantity.defaultPrecision', '2')::INTEGER INTO precision;
-    RETURN to_char(coalesce(p_qty, 0), '99999990.' || repeat('0', precision));
+    RETURN TO_CHAR(COALESCE(p_qty, 0), '99999990.' || REPEAT('0', precision));
 END;
 $$
 ;
 
 ALTER FUNCTION formatQtyToDisplay(p_qty numeric ) OWNER TO metasfresh
+;
+
+COMMENT ON FUNCTION formatQtyToDisplay(p_qty numeric) IS
+    'This function formats the provided qty as a parameter using the webui.frontend.widget.Quantity.defaultPrecision SysConfig'
 ;
