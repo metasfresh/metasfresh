ead2d32e2b509c537edb068715f9cd70131f42e8
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2022-12-01 09:34:02 +0100

    Update backend/de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_Flatrate_Term.java

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_Flatrate_Term.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_Flatrate_Term.java
@@ -185,7 +185,7 @@ public class C_Flatrate_Term
 		final I_C_Flatrate_Data flatrateData = term.getC_Flatrate_Data();
 
 		if (term.getBill_BPartner_ID() == 0)
-		{
+		{ // make sure not to override a Bill_BPartner_ID that is already set via extend-flatrate-term
 			term.setBill_BPartner_ID(flatrateData.getC_BPartner_ID());
 		}
 
