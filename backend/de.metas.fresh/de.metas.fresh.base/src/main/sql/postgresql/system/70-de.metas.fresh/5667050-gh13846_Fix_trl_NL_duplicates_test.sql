09b0de0eaf0194ed8cda386c793a31045f4a5995
MHNV <95350690+MHNV@users.noreply.github.com>
2022-12-05 11:23:32 +0100

    Fix window_trl Duplicates NL name:Test
    Fix window_trl Duplicates NL name:Test
    
    https://github.com/metasfresh/mf15/issues/1999

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5667050-gh13846_Fix_trl_NL_duplicates_test.sql
@@ -0,0 +1,3 @@
+UPDATE ad_window_trl SET updated = now(), updatedby = 99, name = 'Test01' WHERE ad_window_id = 127 AND ad_language = 'nl_NL';
+UPDATE ad_window_trl SET updated = now(), updatedby = 99, name = 'Test01' WHERE ad_window_id = 127 AND ad_language = 'fr_CH';
+
