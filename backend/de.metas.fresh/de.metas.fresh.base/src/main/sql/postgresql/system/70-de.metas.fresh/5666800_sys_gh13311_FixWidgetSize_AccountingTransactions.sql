908aa6926130c0d841913c3cd22dc895a137f643
MHNV <95350690+MHNV@users.noreply.github.com>
2022-12-05 09:37:45 +0100

    Fix WidgetSize
    Fix WidgetSize for every ui_element that has the `''` as size instead of null
    
    https://github.com/metasfresh/ab182/issues/237

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5666800_sys_gh13311_FixWidgetSize_AccountingTransactions.sql
@@ -0,0 +1,6 @@
+UPDATE ad_ui_element SET   updated = now(), updatedby = 99, widgetsize = null WHERE ad_ui_element_id = 543370;
+UPDATE ad_ui_element SET   updated = now(), updatedby = 99, widgetsize = null WHERE ad_ui_element_id = 548015;
+UPDATE ad_ui_element SET   updated = now(), updatedby = 99, widgetsize = null WHERE ad_ui_element_id = 575353;
+UPDATE ad_ui_element SET   updated = now(), updatedby = 99, widgetsize = null WHERE ad_ui_element_id = 612272;
+UPDATE ad_ui_element SET   updated = now(), updatedby = 99, widgetsize = null WHERE ad_ui_element_id = 613238;
+
