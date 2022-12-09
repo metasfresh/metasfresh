faefc3b17f939bef6c9792c4f88e9689183157f2
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2022-12-09 15:56:11 +0100

    fix SQL errors

--- backend/de-metas-salesorder/src/main/sql/postgresql/system/70-de-metas-salesorder/5611419_sys_gh11947_fix_sql_folder_names.sql
+++ backend/de-metas-salesorder/src/main/sql/postgresql/system/70-de-metas-salesorder/5611419_sys_gh11947_fix_sql_folder_names.sql
@@ -8,17 +8,17 @@ $$
     BEGIN
         IF EXISTS(SELECT 1 FROM ad_migrationscript WHERE name = '${migration-sql-basedir}->5611420_sys_gh11947_entity_de_metas_salesorder.sql')
         THEN
-            SELECT migrationscript_ignore('70-de-metas-salesorder/5611420_sys_gh11947_entity_de_metas_salesorder.sql');
+            PERFORM migrationscript_ignore('70-de-metas-salesorder/5611420_sys_gh11947_entity_de_metas_salesorder.sql');
         END IF;
 
         IF EXISTS(SELECT 1 FROM ad_migrationscript WHERE name = '${migration-sql-basedir}->5611420_sys_gh11947_entity_de_metas_salesorder.sql')
         THEN
-            SELECT migrationscript_ignore('70-de-metas-salesorder/5611420_sys_gh11947_entity_de_metas_salesorder.sql');
+            PERFORM migrationscript_ignore('70-de-metas-salesorder/5611420_sys_gh11947_entity_de_metas_salesorder.sql');
         END IF;
 
         IF EXISTS(SELECT 1 FROM ad_migrationscript WHERE name = '${migration-sql-basedir}->5612270_sys_gh11947_wp_ProcessOLCandsWorkpackageProcessor.sql')
         THEN
-            SELECT migrationscript_ignore('70-de-metas-salesorder/5612270_sys_gh11947_wp_ProcessOLCandsWorkpackageProcessor.sql');
+            PERFORM migrationscript_ignore('70-de-metas-salesorder/5612270_sys_gh11947_wp_ProcessOLCandsWorkpackageProcessor.sql');
         END IF;
     END;
 $$
