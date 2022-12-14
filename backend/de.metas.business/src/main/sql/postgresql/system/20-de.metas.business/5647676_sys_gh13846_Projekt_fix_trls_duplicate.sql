fea8b072d30234797a94a47c4c3d590fa6359c14
MHNV <95350690+MHNV@users.noreply.github.com>
2022-12-14 14:55:11 +0100

    Fixing more trls Problems
    Fixing more trls Problems:
    - Projekt
    
    https://github.com/metasfresh/mf15/issues/1999

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5647676_sys_gh13846_Projekt_fix_trls_duplicate.sql
@@ -0,0 +1,3 @@
+UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Projekt - it_CH'  WHERE ad_element_id = 2161 AND ad_language = 'it_CH';
+UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Projekt - it_CH'  WHERE ad_element_id = 2161 AND ad_language = 'en_GB';
+
