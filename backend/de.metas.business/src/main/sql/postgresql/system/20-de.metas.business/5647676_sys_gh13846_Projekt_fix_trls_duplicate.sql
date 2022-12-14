ea452a245cc336bf10a7cde4908107763d587e0b
MHNV <95350690+MHNV@users.noreply.github.com>
2022-12-14 15:49:16 +0100

    Update 5647676_sys_gh13846_Projekt_fix_trls_duplicate.sql
    https://github.com/metasfresh/mf15/issues/2016

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5647676_sys_gh13846_Projekt_fix_trls_duplicate.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5647676_sys_gh13846_Projekt_fix_trls_duplicate.sql
@@ -1,3 +1,7 @@
 UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Projekt - it_CH'  WHERE ad_element_id = 2161 AND ad_language = 'it_CH';
-UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Projekt - it_CH'  WHERE ad_element_id = 2161 AND ad_language = 'en_GB';
+UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Projekt - en_GB'  WHERE ad_element_id = 2161 AND ad_language = 'en_GB';
+
+UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Finanzprojekt' WHERE ad_element_id = 208 AND ad_language = 'de_CH';
+UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Finanzprojekt' WHERE ad_element_id = 208 AND ad_language = 'nl_NL';
+UPDATE ad_element_trl SET updated = now(), updatedby = 99, name = 'Finanzprojekt' WHERE ad_element_id = 208 AND ad_language = 'de_DE';
 
