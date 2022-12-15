eeb1c497541af4d5121f1e332dd04c2c6a590265
Teo Sarca <teo.sarca@metasfresh.com>
2022-12-15 21:36:46 +0200

    copy Tab Callout improvements from master

--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/ui/spi/TabCallout.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/ui/spi/TabCallout.java
@@ -1,35 +1,41 @@
-package org.adempiere.ad.ui.spi;
-
-import org.adempiere.ad.callout.api.ICalloutRecord;
-
-import javax.annotation.Nullable;
-
 /*
  * #%L
  * de.metas.adempiere.adempiere.base
  * %%
- * Copyright (C) 2016 metas GmbH
+ * Copyright (C) 2022 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
 
-public interface IStatefulTabCallout extends ITabCallout
+package org.adempiere.ad.ui.spi;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+@Component
+public @interface TabCallout
 {
 	/**
-	 * Called after {@link ICalloutRecord} was initialized.
+	 * model class
 	 */
-	void onInit(@Nullable ICalloutRecord calloutRecord);
+	Class<?> value();
 }
