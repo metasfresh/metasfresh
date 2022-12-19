d20e776fb91b33f231cc75eb4125678ad2287f82
metas_rc <ruxandra.craciunescu@metasfresh.com>
2022-12-19 22:07:12 +0200

    cleanup for tests https://github.com/metasfresh/metasfresh/issues/14164

--- backend/de.metas.material/dispo-commons/src/main/java/de/metas/document/dimension/MDCandidateDimensionFactory.java
+++ backend/de.metas.material/dispo-commons/src/main/java/de/metas/document/dimension/MDCandidateDimensionFactory.java
@@ -48,7 +48,6 @@ public class MDCandidateDimensionFactory implements DimensionFactory<I_MD_Candid
 				.campaignId(record.getC_Campaign_ID())
 				.activityId(ActivityId.ofRepoIdOrNull(record.getC_Activity_ID()))
 				.orderId(OrderId.ofRepoIdOrNull(record.getC_OrderSO_ID()))
-				.productId(ProductId.ofRepoIdOrNull(record.getM_Product_ID()))
 				.userElementString1(record.getUserElementString1())
 				.userElementString2(record.getUserElementString2())
 				.userElementString3(record.getUserElementString3())
@@ -66,7 +65,6 @@ public class MDCandidateDimensionFactory implements DimensionFactory<I_MD_Candid
 		record.setC_Campaign_ID(from.getCampaignId());
 		record.setC_Activity_ID(ActivityId.toRepoId(from.getActivityId()));
 		record.setC_OrderSO_ID(OrderId.toRepoId(from.getOrderId()));
-		record.setM_Product_ID(ProductId.toRepoId(from.getProductId()));
 		record.setUserElementString1(from.getUserElementString1());
 		record.setUserElementString2(from.getUserElementString2());
 		record.setUserElementString3(from.getUserElementString3());
