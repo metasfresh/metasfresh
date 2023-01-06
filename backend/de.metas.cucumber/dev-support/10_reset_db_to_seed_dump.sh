ef8c8821a8630298b28fe2f5c186936f1b810167
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2023-01-06 14:29:07 +0100

    don't fail 10_reset_db_to_seed_dump.sh if the volumes to delete...
     ...don't exist
     Also improve readability or all 3 files

--- backend/de.metas.cucumber/dev-support/10_reset_db_to_seed_dump.sh
+++ backend/de.metas.cucumber/dev-support/10_reset_db_to_seed_dump.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-#
-# %L
-# de.metas.cucumber
-# %%
-# Copyright (C) 2022 metas GmbH
-# %%
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as
-# published by the Free Software Foundation, either version 2 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public
-# License along with this program. If not, see
-# <http://www.gnu.org/licenses/gpl-2.0.html>.
-# L%
-#
-
 set -e
 set -u # this will fail this script if no branch name is given as parameter
 
@@ -33,8 +11,12 @@ ENV_FILE=../../../misc/dev-support/docker/infrastructure/env-files/${BRANCH_NAME
 
 # reset the database
 docker-compose --file ${COMPOSE_FILE} --env-file ${ENV_FILE} --project-name ${BRANCH_NAME}_infrastructure down
+
+set +e # if the volumes don't exist yet, then it's also fine..so, don't fail the script in that case
 docker volume rm ${BRANCH_NAME}_metasfresh_postgres
 docker volume rm ${BRANCH_NAME}_metasfresh_elasticsearch
+set -e
+
 docker-compose --file ${COMPOSE_FILE} --env-file ${ENV_FILE} build --pull
 docker-compose --file ${COMPOSE_FILE} --env-file ${ENV_FILE} --project-name ${BRANCH_NAME}_infrastructure up -d
 
