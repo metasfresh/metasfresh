e02a0b48abad4e1110ddbd7fd61ffe574a9d9ec9
Chems Nouioua <chems.nouioua@gmail.com>
2023-01-11 13:38:49 +0100

    #14346 customize material receipt window

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5671210_sys_gh14346_customize_material_receipt_window.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5671210_sys_gh14346_customize_material_receipt_window.sql
@@ -4,10 +4,4 @@ UPDATE AD_Column SET AD_Reference_ID=10, IsExcludeFromZoomTargets='Y',Updated=TO
 ;
 
 
--- Field: Wareneingang_OLD -> Wareneingang -> Referenz
--- Column: M_InOut.POReference
--- 2023-01-11T10:57:13.070Z
-UPDATE AD_Field SET AD_Reference_ID=10,Updated=TO_TIMESTAMP('2023-01-11 11:57:13.069','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=3502
-;
-
 
