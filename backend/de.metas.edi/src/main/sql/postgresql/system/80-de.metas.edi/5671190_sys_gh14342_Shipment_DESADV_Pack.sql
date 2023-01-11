d1397392ebbbbea4129f0db25ac24f4f743cccd2
riadmen <riad.mennour@owiin.com>
2023-01-11 11:05:01 +0100

    Fixing M_InOut to EDI_Desadv_Pack Relation

--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5671190_sys_gh14342_Shipment_DESADV_Pack.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5671190_sys_gh14342_Shipment_DESADV_Pack.sql
@@ -1,9 +1,9 @@
 -- 2023-01-09T11:58:53.080Z
-INSERT INTO AD_RelationType (AD_Client_ID,AD_Org_ID,AD_Reference_Source_ID,AD_RelationType_ID,Created,CreatedBy,EntityType,IsActive,IsTableRecordIdTarget,Name,Updated,UpdatedBy) VALUES (0,0,540670,540380,TO_TIMESTAMP('2023-01-09 12:58:52.206','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.intercheese','Y','N','M_InOut (SOTrx) -> EDI_Desadv_Pack',TO_TIMESTAMP('2023-01-09 12:58:52.206','YYYY-MM-DD HH24:MI:SS.US'),100)
+INSERT INTO AD_RelationType (AD_Client_ID,AD_Org_ID,AD_Reference_Source_ID,AD_RelationType_ID,Created,CreatedBy,EntityType,IsActive,IsTableRecordIdTarget,Name,Updated,UpdatedBy) VALUES (0,0,540670,540380,TO_TIMESTAMP('2023-01-09 12:58:52.206','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.esb.edi','Y','N','M_InOut (SOTrx) -> EDI_Desadv_Pack',TO_TIMESTAMP('2023-01-09 12:58:52.206','YYYY-MM-DD HH24:MI:SS.US'),100)
 ;
 
 -- 2023-01-09T12:02:11.873Z
-INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,541704,TO_TIMESTAMP('2023-01-09 13:02:10.657','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.intercheese','Y','N','EDI_Desadv_Pack_Target_For_M_InOut',TO_TIMESTAMP('2023-01-09 13:02:10.657','YYYY-MM-DD HH24:MI:SS.US'),100,'T')
+INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,541704,TO_TIMESTAMP('2023-01-09 13:02:10.657','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.esb.edi','Y','N','EDI_Desadv_Pack_Target_For_M_InOut',TO_TIMESTAMP('2023-01-09 13:02:10.657','YYYY-MM-DD HH24:MI:SS.US'),100,'T')
 ;
 
 -- 2023-01-09T12:02:12.062Z
@@ -11,7 +11,7 @@ INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name
 ;
 
 -- 2023-01-09T14:56:47.488Z
-INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,ShowInactiveValues,Updated,UpdatedBy,WhereClause) VALUES (0,583425,0,541704,542170,TO_TIMESTAMP('2023-01-09 15:56:47.201','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.intercheese','Y','N','N',TO_TIMESTAMP('2023-01-09 15:56:47.201','YYYY-MM-DD HH24:MI:SS.US'),100,'edi_desadv_pack_id IN  ( SELECT pi.edi_desadv_pack_id from edi_desadv_pack_item pi  where pi.M_InOut_ID = @M_InOut_ID / -1@ )')
+INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,ShowInactiveValues,Updated,UpdatedBy,WhereClause) VALUES (0,583425,0,541704,542170,TO_TIMESTAMP('2023-01-09 15:56:47.201','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.esb.edi','Y','N','N',TO_TIMESTAMP('2023-01-09 15:56:47.201','YYYY-MM-DD HH24:MI:SS.US'),100,'edi_desadv_pack_id IN  ( SELECT pi.edi_desadv_pack_id from edi_desadv_pack_item pi  where pi.M_InOut_ID = @M_InOut_ID / -1@ )')
 ;
 
 -- 2023-01-09T14:58:26.332Z
