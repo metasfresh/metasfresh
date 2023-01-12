8377e902a6e455d7fae7258e2f325910bb91ded0
mrie04 <max.rieck@web.de>
2023-01-12 10:59:45 +0100

    update to elasticsearch 7.17.8

--- backend/de.metas.business/src/test/java/de/metas/project/service/ProjectServiceTest.java
+++ backend/de.metas.business/src/test/java/de/metas/project/service/ProjectServiceTest.java
@@ -44,7 +44,7 @@ import org.compiere.model.I_C_Project;
 import org.compiere.model.I_C_ProjectType;
 import org.compiere.model.I_C_UOM;
 import org.compiere.model.I_R_StatusCategory;
-import org.elasticsearch.common.collect.List;
+import org.elasticsearch.core.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
