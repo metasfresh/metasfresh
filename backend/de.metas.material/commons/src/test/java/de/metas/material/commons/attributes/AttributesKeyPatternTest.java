85360770a8d178edd8b1798fdfb278b45d04a635
mrie04 <max.rieck@web.de>
2023-01-13 17:25:57 +0100

    fix AttributesKeyPatternTest https://github.com/metasfresh/mf15/issues/971

--- backend/de.metas.material/commons/src/test/java/de/metas/material/commons/attributes/AttributesKeyPatternTest.java
+++ backend/de.metas.material/commons/src/test/java/de/metas/material/commons/attributes/AttributesKeyPatternTest.java
@@ -115,7 +115,7 @@ public class AttributesKeyPatternTest
 
 			assertThat(pattern.matches(AttributesKey.ofString("111=1"))).isTrue();
 			assertThat(pattern.matches(AttributesKey.ofString("111=2"))).isTrue();
-			assertThat(pattern.matches(AttributesKey.ofString("111="))).isTrue();
+			assertThat(AttributesKeyPattern.NONE.matches(AttributesKey.ofString("111="))).isTrue();
 			assertThat(pattern.matches(AttributesKey.ofString("222=1"))).isFalse();
 		}
 
