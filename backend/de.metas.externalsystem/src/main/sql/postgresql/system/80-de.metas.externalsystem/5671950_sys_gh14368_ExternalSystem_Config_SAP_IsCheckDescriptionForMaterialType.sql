fa846fe549f89886e5f2b650f7caeb2a01c58b4c
fp-ps <fp-ps@pevesoft.ro>
2023-01-18 17:06:31 +0200

    update trls for isCheckDescriptionForMaterialType

--- backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5671950_sys_gh14368_ExternalSystem_Config_SAP_IsCheckDescriptionForMaterialType.sql
+++ backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5671950_sys_gh14368_ExternalSystem_Config_SAP_IsCheckDescriptionForMaterialType.sql
@@ -1,5 +1,5 @@
 -- 2023-01-16T12:47:16.812Z
-INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,581923,0,'IsCheckDescriptionForMaterialType',TO_TIMESTAMP('2023-01-16 14:47:16','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.externalsystem','Y','Check description for material type','Check description for material type',TO_TIMESTAMP('2023-01-16 14:47:16','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,581923,0,'IsCheckDescriptionForMaterialType',TO_TIMESTAMP('2023-01-16 14:47:16','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.externalsystem','Y','Check also description for material type','Check also description for material type',TO_TIMESTAMP('2023-01-16 14:47:16','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2023-01-16T12:47:16.817Z
@@ -8,7 +8,7 @@ INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description
 
 -- Column: ExternalSystem_Config_SAP.IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:47:40.381Z
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,DDL_NoForeignKey,DefaultValue,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRestAPICustomColumn,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,585491,581923,0,20,542238,'IsCheckDescriptionForMaterialType',TO_TIMESTAMP('2023-01-16 14:47:40','YYYY-MM-DD HH24:MI:SS'),100,'N','N','de.metas.externalsystem',0,1,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N',0,'Check description for material type',0,0,TO_TIMESTAMP('2023-01-16 14:47:40','YYYY-MM-DD HH24:MI:SS'),100,0)
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,DDL_NoForeignKey,DefaultValue,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRestAPICustomColumn,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,585491,581923,0,20,542238,'IsCheckDescriptionForMaterialType',TO_TIMESTAMP('2023-01-16 14:47:40','YYYY-MM-DD HH24:MI:SS'),100,'N','N','de.metas.externalsystem',0,1,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N',0,'Check also description for material type',0,0,TO_TIMESTAMP('2023-01-16 14:47:40','YYYY-MM-DD HH24:MI:SS'),100,0)
 ;
 
 -- 2023-01-16T12:47:40.385Z
@@ -23,10 +23,10 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 /* DDL */ SELECT public.db_alter_table('ExternalSystem_Config_SAP','ALTER TABLE public.ExternalSystem_Config_SAP ADD COLUMN IsCheckDescriptionForMaterialType CHAR(1) DEFAULT ''N'' CHECK (IsCheckDescriptionForMaterialType IN (''Y'',''N''))')
 ;
 
--- Field: External system config SAP(541631,de.metas.externalsystem) -> ExternalSystem_Config_SAP(546671,de.metas.externalsystem) -> Check description for material type
+-- Field: External system config SAP(541631,de.metas.externalsystem) -> ExternalSystem_Config_SAP(546671,de.metas.externalsystem) -> Check also description for material type
 -- Column: ExternalSystem_Config_SAP.IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:48:24.073Z
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,585491,710160,0,546671,TO_TIMESTAMP('2023-01-16 14:48:23','YYYY-MM-DD HH24:MI:SS'),100,1,'de.metas.externalsystem','Y','N','N','N','N','N','N','N','Check description for material type',TO_TIMESTAMP('2023-01-16 14:48:23','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,585491,710160,0,546671,TO_TIMESTAMP('2023-01-16 14:48:23','YYYY-MM-DD HH24:MI:SS'),100,1,'de.metas.externalsystem','Y','N','N','N','N','N','N','N','Check also description for material type',TO_TIMESTAMP('2023-01-16 14:48:23','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2023-01-16T12:48:24.079Z
@@ -45,63 +45,63 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=710160
 /* DDL */ select AD_Element_Link_Create_Missing_Field(710160)
 ;
 
--- UI Element: External system config SAP(541631,de.metas.externalsystem) -> ExternalSystem_Config_SAP(546671,de.metas.externalsystem) -> main -> 20 -> flag.Check description for material type
+-- UI Element: External system config SAP(541631,de.metas.externalsystem) -> ExternalSystem_Config_SAP(546671,de.metas.externalsystem) -> main -> 20 -> flag.Check also description for material type
 -- Column: ExternalSystem_Config_SAP.IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:48:45.381Z
-INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,710160,0,546671,614659,550017,'F',TO_TIMESTAMP('2023-01-16 14:48:45','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Check description for material type',20,0,0,TO_TIMESTAMP('2023-01-16 14:48:45','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_Element_ID,AD_UI_ElementGroup_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayed_SideList,IsDisplayedGrid,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNo_SideList,SeqNoGrid,Updated,UpdatedBy) VALUES (0,710160,0,546671,614659,550017,'F',TO_TIMESTAMP('2023-01-16 14:48:45','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','N','Y','N','N','N',0,'Check also description for material type',20,0,0,TO_TIMESTAMP('2023-01-16 14:48:45','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2023-01-16T12:49:53.518Z
-UPDATE AD_Element SET Description='If enabled, the process also checks the product description when trying to lookup material type.',Updated=TO_TIMESTAMP('2023-01-16 14:49:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923
+UPDATE AD_Element SET Description='If enabled, the process also looks into the dat file''s product description column for material types that are mapped to a product category.',Updated=TO_TIMESTAMP('2023-01-16 14:49:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923
 ;
 
 -- 2023-01-16T12:49:53.526Z
-/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'de_DE') 
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'de_DE')
 ;
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:50:07.100Z
-UPDATE AD_Element_Trl SET Description='If enabled, the process also checks the product description when trying to lookup material type.',Updated=TO_TIMESTAMP('2023-01-16 14:50:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_CH'
+UPDATE AD_Element_Trl SET Description='If enabled, the process also looks into the dat file''s product description column for material types that are mapped to a product category.',Updated=TO_TIMESTAMP('2023-01-16 14:50:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_CH'
 ;
 
 -- 2023-01-16T12:50:07.102Z
-/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'de_CH') 
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'de_CH')
 ;
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:50:11.630Z
-UPDATE AD_Element_Trl SET Description='If enabled, the process also checks the product description when trying to lookup material type.',Updated=TO_TIMESTAMP('2023-01-16 14:50:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_DE'
+UPDATE AD_Element_Trl SET Description='If enabled, the process also looks into the dat file''s product description column for material types that are mapped to a product category.',Updated=TO_TIMESTAMP('2023-01-16 14:50:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_DE'
 ;
 
 -- 2023-01-16T12:50:11.633Z
-/* DDL */  select update_ad_element_on_ad_element_trl_update(581923,'de_DE') 
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581923,'de_DE')
 ;
 
 -- 2023-01-16T12:50:11.634Z
-/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'de_DE') 
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'de_DE')
 ;
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:50:13.423Z
-UPDATE AD_Element_Trl SET Description='If enabled, the process also checks the product description when trying to lookup material type.',Updated=TO_TIMESTAMP('2023-01-16 14:50:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='en_US'
+UPDATE AD_Element_Trl SET Description='If enabled, the process also looks into the dat file''s product description column for material types that are mapped to a product category.',Updated=TO_TIMESTAMP('2023-01-16 14:50:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='en_US'
 ;
 
 -- 2023-01-16T12:50:13.425Z
-/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'en_US') 
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'en_US')
 ;
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:50:15.641Z
-UPDATE AD_Element_Trl SET Description='If enabled, the process also checks the product description when trying to lookup material type.',Updated=TO_TIMESTAMP('2023-01-16 14:50:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='fr_CH'
+UPDATE AD_Element_Trl SET Description='If enabled, the process also looks into the dat file''s product description column for material types that are mapped to a product category.',Updated=TO_TIMESTAMP('2023-01-16 14:50:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='fr_CH'
 ;
 
 -- 2023-01-16T12:50:15.642Z
-/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'fr_CH') 
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581923,'fr_CH')
 ;
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T12:50:18.616Z
-UPDATE AD_Element_Trl SET Description='If enabled, the process also checks the product description when trying to lookup material type.',Updated=TO_TIMESTAMP('2023-01-16 14:50:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='nl_NL'
+UPDATE AD_Element_Trl SET Description='If enabled, the process also looks into the dat file''s product description column for material types that are mapped to a product category.',Updated=TO_TIMESTAMP('2023-01-16 14:50:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='nl_NL'
 ;
 
 -- 2023-01-16T12:50:18.618Z
@@ -110,7 +110,7 @@ UPDATE AD_Element_Trl SET Description='If enabled, the process also checks the p
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T16:00:50.141Z
-UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, prüft der Prozess auch die Produktbeschreibung, wenn er versucht, nach der Materialart zu suchen.', Name='Beschreibung für Materialtyp prüfen', PrintName='Beschreibung für Materialtyp prüfen',Updated=TO_TIMESTAMP('2023-01-16 18:00:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_CH'
+UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, sucht der Prozess auch in der Produktbeschreibungsspalte der dat-Datei nach Materialtypen, die einer Produktkategorie zugeordnet sind.', Name='Auch Beschreibung für Materialtyp prüfen', PrintName='Auch Beschreibung für Materialtyp prüfen',Updated=TO_TIMESTAMP('2023-01-16 18:00:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_CH'
 ;
 
 -- 2023-01-16T16:00:50.191Z
@@ -119,7 +119,7 @@ UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, prüft d
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T16:01:01.206Z
-UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, prüft der Prozess auch die Produktbeschreibung, wenn er versucht, nach der Materialart zu suchen.', Name='Beschreibung für Materialtyp prüfen', PrintName='Beschreibung für Materialtyp prüfen',Updated=TO_TIMESTAMP('2023-01-16 18:01:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_DE'
+UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, sucht der Prozess auch in der Produktbeschreibungsspalte der dat-Datei nach Materialtypen, die einer Produktkategorie zugeordnet sind.', Name='Auch Beschreibung für Materialtyp prüfen', PrintName='Auch Beschreibung für Materialtyp prüfen',Updated=TO_TIMESTAMP('2023-01-16 18:01:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='de_DE'
 ;
 
 -- 2023-01-16T16:01:01.208Z
@@ -132,7 +132,7 @@ UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, prüft d
 
 -- Element: IsCheckDescriptionForMaterialType
 -- 2023-01-16T16:01:09.166Z
-UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, prüft der Prozess auch die Produktbeschreibung, wenn er versucht, nach der Materialart zu suchen.', Name='Beschreibung für Materialtyp prüfen', PrintName='Beschreibung für Materialtyp prüfen',Updated=TO_TIMESTAMP('2023-01-16 18:01:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='fr_CH'
+UPDATE AD_Element_Trl SET Description='Wenn diese Option aktiviert ist, sucht der Prozess auch in der Produktbeschreibungsspalte der dat-Datei nach Materialtypen, die einer Produktkategorie zugeordnet sind.', Name='Auch Beschreibung für Materialtyp prüfen', PrintName='Auch Beschreibung für Materialtyp prüfen',Updated=TO_TIMESTAMP('2023-01-16 18:01:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=581923 AND AD_Language='fr_CH'
 ;
 
 -- 2023-01-16T16:01:09.171Z
