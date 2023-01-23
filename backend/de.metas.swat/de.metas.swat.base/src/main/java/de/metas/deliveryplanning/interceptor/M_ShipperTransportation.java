f2153678f736205a8b7be259a1726c13ba4388a8
Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
2023-01-23 10:23:38 +0200

    Adorable badger uat delivery instruction notification (#14420)
    * notification registering  https://github.com/metasfresh/metasfresh/issues/14215
    
    * set shipper transportation to delivery planning  https://github.com/metasfresh/metasfresh/issues/14215
    
    * set shipper transportation to delivery planning  https://github.com/metasfresh/metasfresh/issues/14215

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/deliveryplanning/interceptor/M_ShipperTransportation.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/deliveryplanning/interceptor/M_ShipperTransportation.java
@@ -1,9 +1,13 @@
 package de.metas.deliveryplanning.interceptor;
 
+import de.metas.deliveryplanning.DeliveryInstructionUserNotificationsProducer;
 import de.metas.deliveryplanning.DeliveryPlanningService;
+import de.metas.event.IEventBusFactory;
 import de.metas.shipping.model.I_M_ShipperTransportation;
+import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.modelvalidator.annotations.DocValidate;
+import org.adempiere.ad.modelvalidator.annotations.Init;
 import org.adempiere.ad.modelvalidator.annotations.Interceptor;
 import org.compiere.model.ModelValidator;
 import org.springframework.stereotype.Component;
@@ -13,6 +17,12 @@ import org.springframework.stereotype.Component;
 public class M_ShipperTransportation
 {
 	private final DeliveryPlanningService deliveryPlanningService;
+	@Init
+	public void onInit()
+	{
+		// Setup event bus topics on which client notification listener shall subscribe
+		Services.get(IEventBusFactory.class).addAvailableUserNotificationsTopic(DeliveryInstructionUserNotificationsProducer.EVENTBUS_TOPIC);
+	}
 
 	public M_ShipperTransportation(@NonNull final DeliveryPlanningService deliveryPlanningService)
 	{
