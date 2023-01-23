9bb90d15b33cffa04511f96ceffdebcea76e7830
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-01-23 13:25:41 +0200

    #14422 Add means of transportation

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_DeliveryInstructions_Description.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_DeliveryInstructions_Description.sql
@@ -22,7 +22,8 @@ CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_DeliveryInstr
             orderno              varchar,
             referenceno          varchar,
             incoterms            varchar,
-            incotermlocation     varchar
+            incotermlocation     varchar,
+            meansoftransport     text
         )
     STABLE
     LANGUAGE sql
@@ -42,7 +43,8 @@ SELECT f.*,
        o.documentno  AS orderno,
        o.poreference AS referenceno,
        ic.name       AS incoterms,
-       st.incotermlocation
+       st.incotermlocation,
+       mt.name       AS meansoftransport
 
 FROM de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_LoadingAddress(p_m_shippertransportation_id) AS l,
      de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_Forwarder(p_m_shippertransportation_id) AS f,
@@ -52,6 +54,7 @@ FROM de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_LoadingAddress
          JOIN m_delivery_planning dp ON dp.releaseno = st.documentno
          JOIN C_order o ON o.c_order_id = dp.c_order_id
          JOIN c_incoterms ic ON ic.c_incoterms_id = st.c_incoterms_id
+         JOIN m_meansoftransportation mt ON mt.m_meansoftransportation_id = st.m_meansoftransportation_id
 WHERE st.m_shippertransportation_id = p_m_shippertransportation_id
 $$
 ;
