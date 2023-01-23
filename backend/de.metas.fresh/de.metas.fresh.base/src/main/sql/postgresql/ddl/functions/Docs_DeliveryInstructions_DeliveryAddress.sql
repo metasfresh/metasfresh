f7ff7d5b9a39707f5ddb75c01373728c9b0ef553
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-01-23 14:24:55 +0200

    #14422 Fix script regarding delivery instructions

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_DeliveryInstructions_DeliveryAddress.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_DeliveryInstructions_DeliveryAddress.sql
@@ -10,10 +10,9 @@ CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_DeliveryInstr
     LANGUAGE sql
 AS
 $$
-SELECT (COALESCE(bp.name || E'\n', '') || COALESCE(bpl.address, '') || COALESCE(wh.excisenumber, '')) AS address, u.name, u.phone
+SELECT (COALESCE(bp.name || E'\n', '') || COALESCE(bpl.address, '') ) AS address, u.name, u.phone
 FROM M_ShipperTransportation st
          JOIN C_BPartner_location bpl ON bpl.C_BPartner_location_id = st.c_bpartner_location_delivery_id
-         LEFT JOIN m_warehouse wh ON wh.m_warehouse_id = bpl.m_warehouse_id
          JOIN c_bpartner bp ON bpl.c_bpartner_id = bp.c_bpartner_id
          LEFT JOIN ad_user u ON bp.c_bpartner_id = u.c_bpartner_id AND u.isshiptocontact_default='Y'
 
