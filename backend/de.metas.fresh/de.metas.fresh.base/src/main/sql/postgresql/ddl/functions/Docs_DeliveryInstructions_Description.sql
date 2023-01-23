01eeadbbdbece1f92242a295f20595e6928a4342
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-01-23 12:57:11 +0200

    #14422 Add ne delivery instruction document

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_DeliveryInstructions_Description.sql
@@ -0,0 +1,53 @@
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_Description(p_m_shippertransportation_id numeric);
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_Description(p_m_shippertransportation_id numeric)
+    RETURNS
+        Table
+        (
+            Forwarderaddress     text,
+            transportdetails     text,
+            deliveryaddress      text,
+            deliverycontactname  varchar,
+            deliverycontactphone varchar,
+            LoadingAddress       text,
+            loadingdate          timestamp,
+            loadingtime          varchar,
+            deliverydate         timestamp,
+            documentno           varchar,
+            Creator              varchar,
+            CreatorPhone         varchar,
+            CreatorFax           varchar,
+            CreatorEmail         varchar,
+            orderno              varchar,
+            incoterms            varchar,
+            incotermlocation     varchar
+        )
+    STABLE
+    LANGUAGE sql
+AS
+$$
+SELECT f.*,
+       d.*,
+       l.*,
+       st.loadingdate,
+       st.loadingtime,
+       st.deliverydate,
+       st.documentno,
+       u.name       AS Creator,
+       u.phone      AS CreatorPhone,
+       u.fax        AS CreatorFax,
+       u.email      AS CreatorEmail,
+       o.documentno AS orderno,
+       ic.name      AS incoterms,
+       st.incotermlocation
+
+FROM de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_LoadingAddress(p_m_shippertransportation_id) AS l,
+     de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_Forwarder(p_m_shippertransportation_id) AS f,
+     de_metas_endcustomer_fresh_reports.Docs_DeliveryInstructions_DeliveryAddress(p_m_shippertransportation_id) AS d,
+     M_ShipperTransportation st
+         JOIN ad_user u ON st.createdby = u.ad_user_id
+         JOIN m_delivery_planning dp ON dp.releaseno = st.documentno
+         JOIN C_order o ON o.c_order_id = dp.c_order_id
+         JOIN c_incoterms ic ON ic.c_incoterms_id = st.c_incoterms_id
+WHERE st.m_shippertransportation_id = p_m_shippertransportation_id
+$$
+;
---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5672970_sys_gh14422_AddNewJasperForDeliveryInstructions.sql
@@ -0,0 +1,11 @@
+-- Value: Delivery instructions(Jasper)
+-- Classname: de.metas.report.jasper.client.process.JasperReportStarter
+-- JasperReport: @PREFIX@de/metas/docs/deliveryinstructions/report.jasper
+-- 2023-01-23T10:53:36.356Z
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsLogWarning,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,IsUpdateExportDate,IsUseBPartnerLanguage,JasperReport,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,585190,'N','de.metas.report.jasper.client.process.JasperReportStarter',TO_TIMESTAMP('2023-01-23 12:53:36','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','N','Y','Y','Y','N','N','N','Y','Y','N','Y','@PREFIX@de/metas/docs/deliveryinstructions/report.jasper','Delivery instructions(Jasper)','json','N','N','JasperReportsSQL',TO_TIMESTAMP('2023-01-23 12:53:36','YYYY-MM-DD HH24:MI:SS'),100,'Delivery instructions(Jasper)')
+;
+
+-- 2023-01-23T10:53:36.358Z
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Process t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Process_ID=585190 AND NOT EXISTS (SELECT 1 FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
