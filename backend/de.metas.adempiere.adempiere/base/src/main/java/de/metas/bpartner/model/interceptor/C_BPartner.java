13495331e501d4335d1f0757dc714024ab6a7a82
Chems NOUIOUA <chems.nouioua@gmail.com>
2023-01-24 19:47:19 +0100

    #14433 Add M_SectionCode_ID to C_BPartner_Stats (#14434)
    * #14433 Add M_SectionCode_ID to C_BPartner_Stats
    
    * #14433 regenerate PO
    
    * #14433 fix process
    
    * #14433 fix process
    
    * #14433 copy section code from partner and update it when possible
    
    * #14433 copy section code from partner and update it when possible
    
    * #14433 review fixes
    
    * #14433 review fixes
    
    * #14433 review fixes
    
    * #14433 review fixes
    
    * #14433 review fixes
    
    * #14433 fix recursive definition
    
    * #14433 update stats on section code update
    
    * #14433 update stats on section code update
    
    * #14433 update stats on section code update

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/bpartner/model/interceptor/C_BPartner.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/bpartner/model/interceptor/C_BPartner.java
@@ -71,10 +71,11 @@ import java.util.List;
 public class C_BPartner
 {
 	private static final String MSG_CycleDetectedError = "CycleDetectedError";
-
 	private final IBPartnerDAO bPartnerDAO = Services.get(IBPartnerDAO.class);
 	private final IBPartnerBL bPartnerBL = Services.get(IBPartnerBL.class);
 	private final IQueryBL queryBL = Services.get(IQueryBL.class);
+	private final IBPartnerStatisticsUpdater partnerStatisticsUpdater = Services.get(IBPartnerStatisticsUpdater.class);
+
 
 	private final static transient Logger logger = LogManager.getLogger(C_BPartner.class);
 
@@ -235,4 +236,15 @@ public class C_BPartner
 							.build());
 		}
 	}
+
+	@ModelChange(timings = ModelValidator.TYPE_AFTER_CHANGE, ifColumnsChanged = I_C_BPartner.COLUMNNAME_M_SectionCode_ID)
+	public void updateStatsSectionCode(@NonNull final I_C_BPartner bpartner)
+	{
+		partnerStatisticsUpdater.updateBPartnerStatistics(BPartnerStatisticsUpdateRequest.builder()
+																  .bpartnerId(bpartner.getC_BPartner_ID())
+																  .build());
+	}
+
+
+
 }
