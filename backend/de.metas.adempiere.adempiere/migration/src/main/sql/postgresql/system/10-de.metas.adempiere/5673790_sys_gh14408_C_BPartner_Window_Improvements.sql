2bf44fe1d3be92b49e2e10b7511660611da96ccb
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-01-26 20:23:15 +0200

    window improvements https://github.com/metasfresh/metasfresh/issues/14408

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5673790_sys_gh14408_C_BPartner_Window_Improvements.sql
@@ -0,0 +1,47 @@
+-- Column: C_BPartner_CreditLimit.IsActive
+-- 2023-01-26T17:27:58.987Z
+UPDATE AD_Column SET ReadOnlyLogic='@IsActive@=''N''',Updated=TO_TIMESTAMP('2023-01-26 19:27:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=558972
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Delivery Credit Status
+-- Column: C_BPartner_Stats.Delivery_CreditStatus
+-- 2023-01-26T17:41:44.527Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=614859
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Delivery Credit Used
+-- Column: C_BPartner_Stats.Delivery_CreditUsed
+-- 2023-01-26T17:41:44.536Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=614738
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Offene Posten
+-- Column: C_BPartner_Stats.OpenItems
+-- 2023-01-26T17:41:44.544Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000362
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Section Code
+-- Column: C_BPartner_Stats.M_SectionCode_ID
+-- 2023-01-26T17:41:44.553Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=614780
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Währung
+-- Column: C_BPartner_Stats.C_Currency_ID
+-- 2023-01-26T17:41:44.562Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=550943
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Aktiv
+-- Column: C_BPartner_Stats.IsActive
+-- 2023-01-26T17:41:44.570Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=90,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000358
+;
+
+-- UI Element: Geschäftspartner(123,D) -> Statistik(540739,D) -> main -> 10 -> default.Sektion
+-- Column: C_BPartner_Stats.AD_Org_ID
+-- 2023-01-26T17:41:44.580Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2023-01-26 19:41:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=546570
+;
+
