cfedc2d1c291fd02d75cc170278e506eb983d27d
Teo Sarca <teo.sarca@metasfresh.com>
2023-01-27 15:25:32 +0200

    QA

--- backend/de.metas.business/src/main/java/de/metas/forex/ForexContractRef.java
+++ backend/de.metas.business/src/main/java/de/metas/forex/ForexContractRef.java
@@ -1,8 +1,6 @@
 package de.metas.forex;
 
-import de.metas.common.util.CoalesceUtil;
 import de.metas.money.CurrencyId;
-import de.metas.util.NumberUtils;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
@@ -19,7 +17,7 @@ public class ForexContractRef
 	@NonNull CurrencyId orderCurrencyId;
 	@NonNull CurrencyId fromCurrencyId;
 	@NonNull CurrencyId toCurrencyId;
-	@Nullable BigDecimal currencyRate;
+	@NonNull BigDecimal currencyRate;
 
 	@Builder
 	@Jacksonized
@@ -28,16 +26,11 @@ public class ForexContractRef
 			@NonNull final CurrencyId orderCurrencyId,
 			@NonNull final CurrencyId fromCurrencyId,
 			@NonNull final CurrencyId toCurrencyId,
-			@Nullable final BigDecimal currencyRate)
+			@NonNull final BigDecimal currencyRate)
 	{
-		final BigDecimal currencyRateNorm = NumberUtils.zeroToNull(currencyRate);
-		if (CoalesceUtil.countNotNulls(forexContractId, currencyRateNorm) == 0)
+		if (currencyRate.signum() <= 0)
 		{
-			throw new AdempiereException("At least of of forexContractId or currencyRate shall be set");
-		}
-		if (currencyRateNorm != null && currencyRateNorm.signum() < 0)
-		{
-			throw new AdempiereException("Invalid negative currency rate: " + currencyRateNorm);
+			throw new AdempiereException("Currency rate shall be greater than zero");
 		}
 		if (CurrencyId.equals(fromCurrencyId, toCurrencyId))
 		{
@@ -48,14 +41,6 @@ public class ForexContractRef
 		this.orderCurrencyId = orderCurrencyId;
 		this.fromCurrencyId = fromCurrencyId;
 		this.toCurrencyId = toCurrencyId;
-		this.currencyRate = currencyRateNorm;
-	}
-
-	@Nullable
-	public static ForexContractRef ofNullableForexContractId(@Nullable final ForexContractId forexContractId)
-	{
-		return forexContractId != null
-				? builder().forexContractId(forexContractId).build()
-				: null;
+		this.currencyRate = currencyRate;
 	}
 }
