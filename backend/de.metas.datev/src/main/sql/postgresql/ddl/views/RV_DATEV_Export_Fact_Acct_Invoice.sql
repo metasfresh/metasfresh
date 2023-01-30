08e06e8acfcc542e359b3936a3a1cabdc303ecfc
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2023-01-30 16:15:33 +0200

    updated taxcode column

--- backend/de.metas.datev/src/main/sql/postgresql/ddl/views/RV_DATEV_Export_Fact_Acct_Invoice.sql
+++ backend/de.metas.datev/src/main/sql/postgresql/ddl/views/RV_DATEV_Export_Fact_Acct_Invoice.sql
@@ -1,9 +1,14 @@
+DROP VIEW IF EXISTS RV_DATEV_Export_Fact_Acct_Invoice_PerTax
+;
+DROP VIEW IF EXISTS RV_DATEV_Export_Fact_Acct_Invoice_All
+;
 DROP VIEW IF EXISTS RV_DATEV_Export_Fact_Acct_Invoice
 ;
 
 CREATE OR REPLACE VIEW RV_DATEV_Export_Fact_Acct_Invoice
             (DebitOrCreditIndicator, Currency, dr_account, cr_account, amt, GrandTotal, taxamt, activityname, c_activity_id, documentno, dateacct, bpvalue, bpname, duedate, description, c_bpartner_id, c_invoice_id, docbasetype,
              c_tax_rate,
+             vatCode,
              c_doctype_name,
              fact_acct_id, rv_datev_export_fact_acct_invoice_id, ad_client_id, ad_org_id)
 AS
@@ -26,6 +31,7 @@ SELECT DebitOrCreditIndicator,
        c_invoice_id,
        docbasetype,
        c_tax_rate,
+       vatCode,
        c_doctype_name,
        fact_acct_id,
        rv_datev_export_fact_acct_invoice_id,
@@ -52,6 +58,7 @@ FROM (
                 c_invoice_id,
                 docbasetype,
                 c_tax_rate,
+                vatCode,
                 c_doctype_name,
                 fact_acct_id,
                 rv_datev_export_fact_acct_invoice_id,
@@ -79,6 +86,7 @@ FROM (
                 c_invoice_id,
                 docbasetype,
                 c_tax_rate,
+                vatCode,
                 c_doctype_name,
                 fact_acct_id,
                 rv_datev_export_fact_acct_invoice_id,
