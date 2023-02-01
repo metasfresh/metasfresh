b5574f3db1583c13d591bf59597e4b0086b7102b
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-02-01 10:47:41 +0200

    credit limit view proto

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/public/functions/getSOCreditUsedForSectionGroupPartner.sql
@@ -0,0 +1,25 @@
+
+
+DROP FUNCTION IF EXISTS getSOCreditUsedForSectionGroupPartner(numeric, numeric);
+
+
+CREATE FUNCTION getSOCreditUsedForSectionGroupPartner(p_section_group_partner_id numeric,
+p_M_Department_ID numeric) RETURNS numeric
+    STABLE
+    LANGUAGE sql
+AS
+$$ SELECT SUM(openView.openInvoiceAmt + openView.unallocatedPaymentAmt+ openView.openOrderAmt)
+
+FROM C_BPartner sectionGroupPartner
+         JOIN C_BPartner sectionPartner ON sectionGroupPartner.c_bpartner_id = sectionPartner.section_group_partner_id
+
+         JOIN M_SectionCode sectionCode ON sectionPartner.m_sectioncode_id = sectionCode.m_sectioncode_id
+         JOIN m_department_sectioncode depSectionCode ON sectionCode.m_sectioncode_id = depSectionCode.m_sectioncode_id
+         JOIN M_Department dep ON depSectionCode.m_department_id = dep.m_department_id
+         LEFT JOIN C_BPartner_OpenAmounts_v openView ON sectionPartner.c_bpartner_id = openView.c_bpartner_id
+WHERE sectionGroupPartner.c_bpartner_id = p_section_group_partner_id
+    AND dep.m_department_id = p_M_Department_ID
+GROUP BY sectionPartner.section_group_partner_id, dep.m_department_id
+
+$$
+;
