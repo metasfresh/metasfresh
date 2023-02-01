e600471e5c8efeb549746fe00addf93e517a9a69
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-02-01 21:33:32 +0200

    credit limit view proto

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5674930_sys_gh14408_C_BPartner_CreditLimit_Departments_V.sql
@@ -0,0 +1,30 @@
+-- DROP VIEW IF EXISTS C_BPartner_CreditLimit_Departments_V;
+
+
+CREATE OR REPLACE VIEW C_BPartner_CreditLimit_Departments_V
+AS
+SELECT sectionGroupPartner.c_bpartner_id                                                                            AS Section_Group_Partner_ID,
+       sectionPartner.c_bpartner_id                                                                                 AS C_BPartner_ID,
+       sectionCode.m_sectioncode_id,
+       dep.m_department_id,
+       ac.C_Currency_ID                                                                                        AS C_Currency_ID,
+
+       getSOCreditUsedForSectionGroupPartnerDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)      AS SO_CreditUsed,
+       getOpenOrderAmtForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)             AS M_Department_Order_OpenAmt,
+       getDeliveryCreditUsedForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)       AS Delivery_CrediytUsed,
+       getTotalOrderValueForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)          AS TotalOrderValue,
+       getOpenBalanceForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)              AS OpenOrderAmt,
+       getCreditLimitForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id, NOW()::date) AS CreditLimit
+
+
+FROM C_BPartner sectionGroupPartner
+         JOIN C_BPartner sectionPartner ON sectionGroupPartner.c_bpartner_id = sectionPartner.section_group_partner_id
+
+         JOIN M_SectionCode sectionCode ON sectionPartner.m_sectioncode_id = sectionCode.m_sectioncode_id
+         JOIN m_department_sectioncode depSectionCode ON sectionCode.m_sectioncode_id = depSectionCode.m_sectioncode_id
+         JOIN M_Department dep ON depSectionCode.m_department_id = dep.m_department_id
+
+         JOIN ad_clientinfo ci ON sectionGroupPartner.ad_client_id = ci.ad_client_id
+         JOIN c_acctschema ac ON ci.c_acctschema1_id = ac.c_acctschema_id
+
+;
