97e255806cf5ca1ffd526d14088c2f539d907c45
Adrian <adrian.stefan@metasfresh.com>
2023-02-01 22:20:09 +0200

    #14481 added export process

---
+++ backend/de.metas.deliveryplanning.base/src/main/sql/postgresql/system/43-de.metas.deliveryplanning/5674860_sys_gh14481_Export_Delivery_Planning_Lines_process.sql
@@ -0,0 +1,80 @@
+-- Value: Delivery Planning Export (Excel)
+-- Classname: de.metas.report.jasper.client.process.JasperReportStarter
+-- JasperReport: @PREFIX@/de/metas/docs/deliveryplanning/deliveryplanning.xls
+-- 2023-02-01T16:18:42.371Z
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,IsUpdateExportDate,IsUseBPartnerLanguage,JasperReport,LockWaitTimeout,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,SpreadsheetFormat,SQLStatement,TechnicalNote,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,585207,'Y','de.metas.report.jasper.client.process.JasperReportStarter','N',TO_TIMESTAMP('2023-02-01 18:18:42','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N','Y','Y','N','N','Y','Y','N','Y','@PREFIX@/de/metas/docs/deliveryplanning/deliveryplanning.xls',0,'Delivery Planning Export Excel','json','N','N','xls','
+SELECT div.documentno,
+       COALESCE(deliveryL.bpartnername, deliveryBP.name) || E''\n'' || deliveryL.address AS ship_to_location,
+       p.name                                                                          AS product_name,
+       p.value                                                                         AS product_code,
+       w.name                                                                          AS warehouse_name,
+       fromC.name                                                                      AS from_country,
+       TO_CHAR(div.deliverydate, ''YYYY-MM-DD'')                                         AS deliverydate,
+       dp.batch,
+       dp.releaseno,
+       TO_CHAR(dp.plannedloadingdate, ''YYYY-MM-DD'')                                    AS plannedloadingdate,
+       TO_CHAR(dp.actualloadingdate, ''YYYY-MM-DD'')                                     AS actualloadingdate,
+       dp.plannedloadedquantity,
+       dp.actualloadqty,
+       TO_CHAR(dp.planneddeliverydate, ''YYYY-MM-DD'')                                   AS planneddeliverydate,
+       TO_CHAR(dp.actualdeliverydate, ''YYYY-MM-DD'')                                    AS actualdeliverydate,
+       dp.planneddischargequantity,
+       dp.actualdischargequantity
+FROM m_delivery_planning_delivery_instructions_v div
+         INNER JOIN m_delivery_planning dp ON dp.m_delivery_planning_id = div.m_delivery_planning_id
+         INNER JOIN c_bpartner_location deliveryL ON div.c_bpartner_location_delivery_id = deliveryL.c_bpartner_location_id
+         INNER JOIN c_bpartner_location loadingL ON div.c_bpartner_location_loading_id = loadingL.c_bpartner_location_id
+         INNER JOIN c_bpartner deliveryBP ON deliveryL.c_bpartner_id = deliveryBP.c_bpartner_id
+         LEFT JOIN m_product p ON dp.m_product_id = p.m_product_id
+         LEFT JOIN m_warehouse w ON dp.m_warehouse_id = w.m_warehouse_id
+         LEFT JOIN c_country fromC ON dp.c_origincountry_id = fromC.c_country_id
+;','Still to add a WHERE clause for the current selection','JasperReportsSQL',TO_TIMESTAMP('2023-02-01 18:18:42','YYYY-MM-DD HH24:MI:SS'),100,'Delivery Planning Export (Excel)')
+;
+--FIXME add proper where clause
+
+-- 2023-02-01T16:18:42.373Z
+INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Process_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Process t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Process_ID=585207 AND NOT EXISTS (SELECT 1 FROM AD_Process_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_ID=t.AD_Process_ID)
+;
+
+-- Value: Export Delivery Planning Lines (Jasper)
+-- Classname: de.metas.report.jasper.client.process.JasperReportStarter
+-- JasperReport: @PREFIX@/de/metas/docs/deliveryplanning/deliveryplanning.xls
+-- 2023-02-01T16:20:23.386Z
+UPDATE AD_Process SET Name='Export Delivery Planning Lines', Value='Export Delivery Planning Lines (Jasper)',Updated=TO_TIMESTAMP('2023-02-01 18:20:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585207
+;
+
+-- 2023-02-01T16:20:23.387Z
+UPDATE AD_Process_Trl trl SET Name='Export Delivery Planning Lines' WHERE AD_Process_ID=585207 AND AD_Language='de_DE'
+;
+
+-- Process: Export Delivery Planning Lines (Jasper)(de.metas.report.jasper.client.process.JasperReportStarter)
+-- 2023-02-01T16:20:42.250Z
+UPDATE AD_Process_Trl SET Name='Export Delivery Planning Lines',Updated=TO_TIMESTAMP('2023-02-01 18:20:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Process_ID=585207
+;
+
+-- Process: Export Delivery Planning Lines (Jasper)(de.metas.report.jasper.client.process.JasperReportStarter)
+-- 2023-02-01T16:22:30.211Z
+UPDATE AD_Process_Trl SET Name='Export Lieferplanungszeilen',Updated=TO_TIMESTAMP('2023-02-01 18:22:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=585207
+;
+
+-- Process: Export Delivery Planning Lines (Jasper)(de.metas.report.jasper.client.process.JasperReportStarter)
+-- 2023-02-01T16:22:32.058Z
+UPDATE AD_Process_Trl SET Name='Export Lieferplanungszeilen',Updated=TO_TIMESTAMP('2023-02-01 18:22:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=585207
+;
+
+-- 2023-02-01T16:22:32.059Z
+UPDATE AD_Process SET Name='Export Lieferplanungszeilen' WHERE AD_Process_ID=585207
+;
+
+-- Process: Export Delivery Planning Lines (Jasper)(de.metas.report.jasper.client.process.JasperReportStarter)
+-- 2023-02-01T16:22:33.722Z
+UPDATE AD_Process_Trl SET Name='Export Lieferplanungszeilen',Updated=TO_TIMESTAMP('2023-02-01 18:22:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=585207
+;
+
+-- Process: Export Delivery Planning Lines (Jasper)(de.metas.report.jasper.client.process.JasperReportStarter)
+-- Table: M_Delivery_Planning
+-- EntityType: D
+-- 2023-02-01T18:07:29.419Z
+INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,AD_Table_Process_ID,Created,CreatedBy,EntityType,IsActive,Updated,UpdatedBy,WEBUI_DocumentAction,WEBUI_IncludedTabTopAction,WEBUI_ViewAction,WEBUI_ViewQuickAction,WEBUI_ViewQuickAction_Default) VALUES (0,0,585207,542259,541348,TO_TIMESTAMP('2023-02-01 20:07:29','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',TO_TIMESTAMP('2023-02-01 20:07:29','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','N')
+;
+
