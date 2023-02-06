c157d5bd8bc89060701510a10d2265d4b854ed8c
Chems Nouioua <chems.nouioua@gmail.com>
2023-02-06 21:12:04 +0100

    #14500  fix missing elements

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5675660_sys_gh14500_cl_usage_window_and_tabs.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5675660_sys_gh14500_cl_usage_window_and_tabs.sql
@@ -67,14 +67,6 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 /* DDL */  select update_Column_Translation_From_AD_Element(187) 
 ;
 
--- 2023-02-03T16:07:10.781Z
-INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,582015,0,'partner_code',TO_TIMESTAMP('2023-02-03 17:07:10','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','partner_code','partner_code',TO_TIMESTAMP('2023-02-03 17:07:10','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2023-02-03T16:07:10.782Z
-INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=582015 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
-;
-
 -- Column: C_BPartner_Creditlimit_Usage_V.partner_code
 -- 2023-02-03T16:07:11.092Z
 INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsTranslated,IsUpdateable,Name,Updated,UpdatedBy,Version) VALUES (0,585845,582015,0,10,542293,'partner_code',TO_TIMESTAMP('2023-02-03 17:07:10','YYYY-MM-DD HH24:MI:SS'),100,'D',40,'Y','Y','N','N','N','N','N','N','N','N','N','partner_code',TO_TIMESTAMP('2023-02-03 17:07:10','YYYY-MM-DD HH24:MI:SS'),100,0)
