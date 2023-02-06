cb75aa66b443fe5ce2804f1376355a88c12cda17
Chems Nouioua <chems.nouioua@gmail.com>
2023-02-06 14:47:36 +0100

    #14500 review fixes

--- backend/de.metas.business/src/main/sql/postgresql/ddl/functions/getcreditlimitfordepartment.sql
+++ backend/de.metas.business/src/main/sql/postgresql/ddl/functions/getcreditlimitfordepartment.sql
@@ -18,6 +18,8 @@ FROM C_BPartner partner
 
 
 WHERE dep.m_department_id = p_m_department_ID
+
+  -- 2.4.13 AC14 – Increased Credit Limit in Window “Business Partner” for the section partner can be decreased again https://github.com/metasfresh/me03/issues/14325#issuecomment-1406979127
   AND ((lim.processed = 'Y' AND lim.isactive = 'Y') OR (lim.processed = 'N' AND lim.isactive = 'N'))
   AND lim.datefrom <= p_date
   AND NOT EXISTS(SELECT 1
