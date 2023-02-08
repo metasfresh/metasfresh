5ef5a185d338456e3dcfebe58bb4778464a35d41
Chems Nouioua <chems.nouioua@gmail.com>
2023-02-08 23:25:23 +0100

    #14500  cl usage feedback fixes

--- backend/de.metas.business/src/main/sql/postgresql/ddl/views/C_BPartner_Creditlimit_Usage_V.sql
+++ backend/de.metas.business/src/main/sql/postgresql/ddl/views/C_BPartner_Creditlimit_Usage_V.sql
@@ -1,8 +1,7 @@
-
-DROP VIEW IF EXISTS c_bpartner_creditlimit_usage_v
+DROP VIEW IF EXISTS C_BPartner_Creditlimit_Usage_V
 ;
 
-CREATE VIEW C_BPartner_Creditlimit_Usage_V
+CREATE OR REPLACE VIEW C_BPartner_Creditlimit_Usage_V
             (
              C_BPartner_Creditlimit_Usage_V_ID,
              c_bpartner_id,
@@ -11,7 +10,6 @@ CREATE VIEW C_BPartner_Creditlimit_Usage_V
              m_sectioncode_id,
              creditlimit,
              creditlimit_by_department,
-             creditlimit_by_sectioncode,
              cl_indicator_order,
              cl_indicator_delivery,
              created,
@@ -23,31 +21,54 @@ CREATE VIEW C_BPartner_Creditlimit_Usage_V
              ad_org_id
                 )
 AS
-SELECT bpcl.c_bpartner_creditlimit_id                                                                              AS C_BPartner_Creditlimit_Usage_V_ID,
-       bp.c_bpartner_id,
-       bp.value                                                                                                    AS partner_code,
-       d.m_department_id,
-       sc.m_sectioncode_id,
-       getcreditlimitforpartner(bp.c_bpartner_id, NOW()::date)                                 AS creditlimit,
-       getcreditlimitfordepartment(d.m_department_id, NOW()::date)                          AS creditlimit_by_department,
-       getcreditlimitforsectioncode(bp.m_sectioncode_id, NOW()::date)                        AS creditlimit_by_sectioncode,
-       (SELECT MAX(creditlimitindicator) FROM C_BPartner_Stats s WHERE s.C_BPartner_ID = bp.c_bpartner_id)         AS cl_indicator_order,
-       (SELECT MAX(deliverycreditlimitindicator) FROM C_BPartner_Stats s WHERE s.C_BPartner_ID = bp.c_bpartner_id) AS cl_indicator_delivery,
-       bpcl.created                                                                                                AS created,
-       bpcl.updated                                                                                                AS updated,
-       bpcl.createdby                                                                                              AS createdby,
-       bpcl.updatedby                                                                                              AS updatedby,
-       'Y'                                                                                                         AS isactive,
-       bp.ad_client_id,
-       bp.ad_org_id
-FROM c_bpartner bp
-         JOIN m_sectioncode sc ON bp.m_sectioncode_id = sc.m_sectioncode_id
-         JOIN m_department_sectioncode dsc ON bp.m_sectioncode_id = dsc.m_sectioncode_id
-         JOIN m_department d ON dsc.m_department_id = d.m_department_id
-         JOIN c_bpartner_creditlimit bpcl ON bp.c_bpartner_id = bpcl.c_bpartner_id
+SELECT *
+FROM (SELECT 1000000 + ROW_NUMBER() OVER ()                                                                              AS C_BPartner_Creditlimit_Usage_V_ID,
+             bp.c_bpartner_id,
+             bp.value                                                                                                    AS partner_code,
+             dsc.m_department_id,
+             bp.m_sectioncode_id,
+             concat(getcreditlimitforpartner(bp.c_bpartner_id, NOW()::date), ' ', c.iso_code::text ) AS creditlimit,
+             getCreditLimitForSectionPartnerByDepartment(bp.c_bpartner_id, dsc.m_department_id, NOW()::date)             AS creditlimit_by_department,
+             (SELECT MAX(creditlimitindicator) FROM C_BPartner_Stats s WHERE s.C_BPartner_ID = bp.c_bpartner_id)         AS cl_indicator_order,
+             (SELECT MAX(deliverycreditlimitindicator) FROM C_BPartner_Stats s WHERE s.C_BPartner_ID = bp.c_bpartner_id) AS cl_indicator_delivery,
+             dsc.created                                                                                                 AS created,
+             dsc.updated                                                                                                 AS updated,
+             dsc.createdby                                                                                               AS createdby,
+             dsc.updatedby                                                                                               AS updatedby,
+             'Y'                                                                                                         AS isactive,
+             dsc.ad_client_id,
+             dsc.ad_org_id
+      FROM c_bpartner bp
+               INNER JOIN m_department_sectioncode dsc ON bp.m_sectioncode_id = dsc.m_sectioncode_id
+               INNER JOIN ad_clientinfo ci ON bp.ad_client_id = ci.ad_client_id
+               INNER JOIN c_acctschema ac ON ci.c_acctschema1_id = ac.c_acctschema_id
+               INNER JOIN c_currency c on ac.c_currency_id = c.c_currency_id
 
-ORDER BY d.m_department_id, bp.c_bpartner_id
+      UNION ALL
+
+      (
+          (SELECT 2000000 + ROW_NUMBER() OVER ()                                                                                                               AS c_bpartner_creditlimit_usage_v_id,
+                  NULL                                                                                                                                         AS c_bpartner_id,
+                  NULL                                                                                                                                         AS partner_code,
+                  d.m_department_id,
+                  NULL                                                                                                                                         AS m_sectioncode_id,
+                  NULL                                                                                                                                         AS creditlimit,
+                  coalesce(getCreditLimitForDepartment(d.m_department_id, NOW()::date),0)                                                                                  AS creditlimit_by_department,
+                  coalesce(ROUND((getSOCreditUsedForDepartment(d.m_department_id) / getcreditlimitfordepartment(d.m_department_id, NOW()::date)) * 100, 1),0 ) || '%'       AS cl_indicator_order,
+                  coalesce(ROUND((getDeliveryCreditUsedForDepartment(d.m_department_id) / getcreditlimitfordepartment(d.m_department_id, NOW()::date)) * 100, 1),0 ) || '%' AS cl_indicator_delivery,
+                  d.created,
+                  d.updated,
+                  d.createdby,
+                  d.updatedby,
+                  'Y'                                                                                                                                          AS isactive,
+                  d.ad_client_id,
+                  d.ad_org_id
+           FROM m_department d)
+      )
+     ) cl
+ORDER BY cl.m_department_id, cl.C_BPartner_Creditlimit_Usage_V_ID, cl.c_bpartner_id
 ;
+
 
 ALTER TABLE C_BPartner_Creditlimit_Usage_V
     OWNER TO metasfresh
