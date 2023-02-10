038a0e319bdbf058f9b413c16a1aa17ba2ebfd2f
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-02-10 21:52:22 +0200

    section group partner credit limit indicators https://github.com/metasfresh/metasfresh/issues/14408

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5676650_sys_gh14408_Section_Group_BPartner_CreditLimit_VirtualColumns.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5676650_sys_gh14408_Section_Group_BPartner_CreditLimit_VirtualColumns.sql
@@ -10,3 +10,9 @@ UPDATE AD_Column SET ColumnSQL='(SELECT CASE          WHEN C_BPartner.IsSectionG
 UPDATE AD_Column SET ColumnSQL='(SELECT CASE             WHEN C_BPartner.IsSectionGroupPartner = ''Y'' THEN (SELECT getDeliveryCreditUsedPercentForSectionGroupPartner(C_BPartner.C_BPartner_ID, now()::date))                                                         ELSE (SELECT MAX(CreditLimitIndicator) from C_BPartner_Stats S where S.C_BPartner_ID = C_BPartner.C_BPartner_ID)         END)',Updated=TO_TIMESTAMP('2023-02-10 21:01:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=585625
 ;
 
+-- Column: C_BPartner.DeliveryCreditLimitIndicator
+-- Column SQL (old): (SELECT CASE             WHEN C_BPartner.IsSectionGroupPartner = 'Y' THEN (SELECT getDeliveryCreditUsedPercentForSectionGroupPartner(C_BPartner.C_BPartner_ID, now()::date))                                                         ELSE (SELECT MAX(CreditLimitIndicator) from C_BPartner_Stats S where S.C_BPartner_ID = C_BPartner.C_BPartner_ID)         END)
+-- 2023-02-10T19:51:53.812Z
+UPDATE AD_Column SET ColumnSQL='(SELECT CASE             WHEN C_BPartner.IsSectionGroupPartner = ''Y'' THEN (SELECT getDeliveryCreditUsedPercentForSectionGroupPartner(C_BPartner.C_BPartner_ID, now()::date))                                                         ELSE (SELECT MAX(DeliveryCreditLimitIndicator) from C_BPartner_Stats S where S.C_BPartner_ID = C_BPartner.C_BPartner_ID)         END)',Updated=TO_TIMESTAMP('2023-02-10 21:51:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=585625
+;
+
