9d8faff22f54fa19e9c733b065015782c1c137bf
Chems Nouioua <chems.nouioua@gmail.com>
2023-02-13 20:36:19 +0100

    #14500 cl usage feedback fixes

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5676860_sys_gh14500_getOverdueInvoicesWithOpenOrders.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5676860_sys_gh14500_getOverdueInvoicesWithOpenOrders.sql
@@ -1,4 +1,3 @@
-
 DROP FUNCTION IF EXISTS getOverdueInvoicesWithOpenOrders(numeric,
                                                          numeric,
                                                          numeric,
@@ -45,43 +44,51 @@ SELECT dep.value || '_' || dep.name                        AS "Department",
 
 
 FROM M_Department dep
-         JOIN M_Department_SectionCode depSC ON dep.m_department_id = depSC.m_department_id
+         JOIN M_Department_SectionCode depSC ON dep.m_department_id = depSC.m_department_id AND depSC.isactive = 'Y'
          JOIN M_SectionCode sc ON depSC.m_sectioncode_id = sc.m_sectioncode_id
          JOIN C_BPartner bp ON sc.m_sectioncode_id = bp.m_sectioncode_id
          JOIN C_Order o ON bp.c_bpartner_id = o.c_bpartner_id
          JOIN C_OrderLine ol ON o.c_order_id = ol.c_order_id
          LEFT JOIN M_Delivery_Planning dp ON ol.c_orderLine_id = dp.c_orderLine_id
-         JOIN C_Invoice i ON bp.c_bpartner_id = i.c_bpartner_id
-         JOIN C_PaymentTerm pt ON i.c_paymentterm_id = pt.c_paymentterm_id
+
 
 WHERE TRUE
   AND (dep.m_department_id = p_m_department_id OR p_m_department_id IS NULL)   -- Department
   AND (sc.m_sectioncode_id = p_m_sectioncode_id OR p_m_sectioncode_id IS NULL) -- Section Code
   AND (bp.c_bpartner_id = p_c_bpartner_id OR p_c_bpartner_id IS NULL)          -- Business partner
-  AND (bp.c_bpartner_id = p_c_bpartner_id OR p_c_bpartner_id IS NULL)
   AND (dp.plannedloadingdate BETWEEN p_plannedLoadingDateFrom AND p_plannedLoadingDateTo -- Planned loading date (range)
     OR (p_plannedLoadingDateFrom IS NULL AND p_plannedLoadingDateTo IS NULL))
   AND (dp.planneddeliverydate BETWEEN p_plannedDeliveryDateFrom AND p_plannedDeliveryDateTo -- Planned discharge date (range)
     OR (p_plannedDeliveryDateFrom IS NULL AND p_plannedDeliveryDateTo IS NULL))
 
   AND o.IsSOTrx = 'Y'
-  AND i.docstatus IN ('CO', 'CL')
-  AND i.ispaid = 'N'
-  AND ((i.dateinvoiced + netdays)
-    < NOW()::DATE)
-  AND ol.qtyenteredinpriceuom
-    > (SELECT COALESCE(SUM(sp.actualloadqty)
-                  , 0)
-       FROM m_shippingpackage sp
-                JOIN M_ShipperTransportation st
-                     ON sp.m_shippertransportation_id = st.m_shippertransportation_id
-       WHERE sp.c_orderline_id = ol.c_orderline_id
-         AND st.docstatus IN ('CO', 'CL'))
-    ;
+  AND o.docstatus IN ('CO', 'CL')
 
+  AND ol.qtyenteredinpriceuom > (SELECT COALESCE(SUM(sp.actualloadqty), 0)
+                                 FROM m_shippingpackage sp
+                                          JOIN M_ShipperTransportation st ON sp.m_shippertransportation_id = st.m_shippertransportation_id
+                                 WHERE sp.c_orderline_id = ol.c_orderline_id
+                                   AND st.docstatus IN ('CO', 'CL'))
+
+  AND EXISTS(SELECT 1
+             FROM C_Invoice i
+                      JOIN C_PaymentTerm pt ON i.c_paymentterm_id = pt.c_paymentterm_id
+             WHERE bp.c_bpartner_id = i.c_bpartner_id
+               AND i.docstatus IN ('CO', 'CL')
+               AND i.ispaid = 'N'
+               AND ((i.dateinvoiced + netdays) < NOW()::DATE));
 
 $$
 ;
 
 ALTER FUNCTION getOverdueInvoicesWithOpenOrders() OWNER TO metasfresh
 ;
+
+
+
+
+
+
+
+
+
