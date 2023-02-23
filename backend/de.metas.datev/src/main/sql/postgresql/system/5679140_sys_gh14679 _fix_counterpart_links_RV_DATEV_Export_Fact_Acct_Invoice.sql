541d9463e5b75c9fe297ce1c9624d0272000944d
Adrian <adrian.stefan@metasfresh.com>
2023-02-23 20:00:31 +0200

    #14679 drop RV_DATEV_Export_Fact_Acct_Invoice and recreate afterwards
    (cherry picked from commit 5c02e0990191f7df8cdaa947a6c15e4340b929a4)

--- backend/de.metas.datev/src/main/sql/postgresql/system/5679140_sys_gh14679 _fix_counterpart_links_RV_DATEV_Export_Fact_Acct_Invoice.sql
+++ backend/de.metas.datev/src/main/sql/postgresql/system/5679140_sys_gh14679 _fix_counterpart_links_RV_DATEV_Export_Fact_Acct_Invoice.sql
@@ -1,3 +1,6 @@
+DROP VIEW IF EXISTS RV_DATEV_Export_Fact_Acct_Invoice
+;
+
 DROP VIEW IF EXISTS RV_DATEV_Export_Fact_Acct_Invoice_PerTax
 ;
 
@@ -195,4 +198,96 @@ FROM fact_acct fa
          JOIN c_doctype dt ON dt.c_doctype_id = fa.c_doctype_id
 
 WHERE fa.ad_table_id = get_table_id('C_Invoice'::character varying)
+;
+
+CREATE OR REPLACE VIEW RV_DATEV_Export_Fact_Acct_Invoice
+            (DebitOrCreditIndicator, Currency, dr_account, cr_account, amt, GrandTotal, taxamt, activityname, c_activity_id, documentno, dateacct, bpvalue, bpname, duedate, description, c_bpartner_id, c_invoice_id, docbasetype,
+             c_tax_rate,
+             vatCode,
+             c_doctype_name,
+             fact_acct_id, rv_datev_export_fact_acct_invoice_id, ad_client_id, ad_org_id)
+AS
+SELECT DebitOrCreditIndicator,
+       Currency,
+       dr_account,
+       cr_account,
+       amt,
+       GrandTotal,
+       taxamt,
+       activityname,
+       c_activity_id,
+       documentno,
+       dateacct,
+       bpvalue,
+       bpname,
+       duedate,
+       description,
+       c_bpartner_id,
+       c_invoice_id,
+       docbasetype,
+       c_tax_rate,
+       vatCode,
+       c_doctype_name,
+       fact_acct_id,
+       rv_datev_export_fact_acct_invoice_id,
+       ad_client_id,
+       ad_org_id
+FROM (
+         SELECT 'Y' AS IsOneLinePerInvoiceTax,
+                DebitOrCreditIndicator,
+                Currency,
+                dr_account,
+                cr_account,
+                amt,
+                GrandTotal,
+                taxamt,
+                activityname,
+                c_activity_id,
+                documentno,
+                dateacct,
+                bpvalue,
+                bpname,
+                duedate,
+                description,
+                c_bpartner_id,
+                c_invoice_id,
+                docbasetype,
+                c_tax_rate,
+                vatCode,
+                c_doctype_name,
+                fact_acct_id,
+                rv_datev_export_fact_acct_invoice_id,
+                ad_client_id,
+                ad_org_id
+         FROM RV_DATEV_Export_Fact_Acct_Invoice_PerTax
+         UNION
+         SELECT 'N' AS IsOneLinePerInvoiceTax,
+                DebitOrCreditIndicator,
+                Currency,
+                dr_account,
+                cr_account,
+                amt,
+                GrandTotal,
+                taxamt,
+                activityname,
+                c_activity_id,
+                documentno,
+                dateacct,
+                bpvalue,
+                bpname,
+                duedate,
+                description,
+                c_bpartner_id,
+                c_invoice_id,
+                docbasetype,
+                c_tax_rate,
+                vatCode,
+                c_doctype_name,
+                fact_acct_id,
+                rv_datev_export_fact_acct_invoice_id,
+                ad_client_id,
+                ad_org_id
+         FROM RV_DATEV_Export_Fact_Acct_Invoice_All
+     ) AS invoices
+WHERE IsOneLinePerInvoiceTax = get_sysconfig_value('DATEVExportLines_OneLinePerInvoiceTax', 'N')
 ;
