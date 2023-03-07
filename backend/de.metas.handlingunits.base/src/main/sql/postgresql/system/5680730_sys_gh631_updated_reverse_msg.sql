648fb2de4bada9a9d197e89ebe4df9bfd94af5d4
Cristina Stefan <50820854+cristinastefan90@users.noreply.github.com>
2023-03-07 16:48:24 +0200

    fixed message (#14797)

---
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5680730_sys_gh631_updated_reverse_msg.sql
@@ -0,0 +1,16 @@
+-- 2023-03-07T14:21:21.746Z
+UPDATE AD_Message_Trl SET MsgText='The return cannot be reversed while the associated project is still active.',Updated=TO_TIMESTAMP('2023-03-07 16:21:21.643','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545250
+;
+
+-- 2023-03-07T14:21:33.841Z
+UPDATE AD_Message_Trl SET MsgText='Die Serviceannahme kann nicht rückgängig gemacht werden, solange das zugehörige Projekt noch aktiv ist.',Updated=TO_TIMESTAMP('2023-03-07 16:21:33.743','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545250
+;
+
+-- 2023-03-07T14:21:40.589Z
+UPDATE AD_Message_Trl SET MsgText='Die Serviceannahme kann nicht rückgängig gemacht werden, solange das zugehörige Projekt noch aktiv ist.',Updated=TO_TIMESTAMP('2023-03-07 16:21:40.492','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Message_ID=545250
+;
+
+-- 2023-03-07T14:21:46.242Z
+UPDATE AD_Message_Trl SET MsgText='Die Serviceannahme kann nicht rückgängig gemacht werden, solange das zugehörige Projekt noch aktiv ist.',Updated=TO_TIMESTAMP('2023-03-07 16:21:46.145','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545250
+;
+
