6b1715b08c94142df32a59608dcbc85cc269c0b7
Ispas Mihai <mihai.ispas@pevesoft.ro>
2023-03-10 16:59:04 +0200

    remove unnecessary function

--- backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5681170_sys_gh14858_Order_By_SeqNo_PP_Product_BOM_Recursive_Excel_Report.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5681170_sys_gh14858_Order_By_SeqNo_PP_Product_BOM_Recursive_Excel_Report.sql
@@ -115,24 +115,3 @@ $$
 
 ALTER FUNCTION pp_product_bom_recursive(numeric, varchar) OWNER TO metasfresh
 ;
-
-CREATE OR REPLACE FUNCTION pp_product_bom_recursive_report(p_pp_product_bom_id numeric)
-    RETURNS TABLE(line text, productvalue character varying, productname character varying, qtybom numeric, percentage numeric, uomsymbol character varying)
-    STABLE
-    LANGUAGE sql
-AS
-$$
-select
-    t.Line,
-    t.ProductValue,
-    t.ProductName,
-    t.QtyBOM,
-    t.Percentage,
-    t.UOMSymbol
-from PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, null) t
-order by t.path
-$$
-;
-
-ALTER FUNCTION pp_product_bom_recursive_report(numeric) OWNER TO metasfresh
-;
