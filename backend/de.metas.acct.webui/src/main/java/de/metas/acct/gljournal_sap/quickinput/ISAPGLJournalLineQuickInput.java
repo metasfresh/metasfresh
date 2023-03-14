e7f8ec0c5b7ad1d6ad9b9e1ec7b7b475a1b874d7
Chems NOUIOUA <chems.nouioua@gmail.com>
2023-03-14 16:04:39 +0100

    #14841 (#14842)
    * #14841
    
    * #14841
    
    * #14841 update it only when it's not null, since M_SectionCode_ID is mandatory
    
    * #14841 fix null pointer exception when setting  C_BPartner2_ID and m_docLine is null
    
    * QA
    
    * QA (2)
    
    * #14841 review fixes
    
    * #14841 review fixes
    
    * #14841 review fixes
    
    ---------
    
    Co-authored-by: Teo Sarca <teo.sarca@metasfresh.com>
    (cherry picked from commit 7d7acb54cdcc9654847a0ced299110cb4b4f545d)

--- backend/de.metas.acct.webui/src/main/java/de/metas/acct/gljournal_sap/quickinput/ISAPGLJournalLineQuickInput.java
+++ backend/de.metas.acct.webui/src/main/java/de/metas/acct/gljournal_sap/quickinput/ISAPGLJournalLineQuickInput.java
@@ -7,16 +7,18 @@ public interface ISAPGLJournalLineQuickInput
 	String COLUMNNAME_PostingSign = "PostingSign";
 
 	java.lang.String getPostingSign();
+	String COLUMNNAME_GL_Account_ID = "GL_Account_ID";
 
-	String COLUMNNAME_C_ValidCombination_ID = "C_ValidCombination_ID";
-
-	int getC_ValidCombination_ID();
+	int getGL_Account_ID();
 
 	String COLUMNNAME_Amount = "Amount";
 
 	BigDecimal getAmount();
 
 	String COLUMNNAME_C_Tax_ID = "C_Tax_ID";
+	String COLUMNNAME_M_SectionCode_ID = "M_SectionCode_ID";
+
+	int getM_SectionCode_ID();
 
 	int getC_Tax_ID();
 }
