ca459d552d3b8bccecef875a9a14bd02cbfa3fa0
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-03-15 11:31:08 +0200

    Script correction after merge from release

--- backend/de.metas.async/src/main/sql/postgresql/system/80-async-dlm/5624381_sys_gh12591_migrate_c_queue_package_processor_ID.sql
+++ backend/de.metas.async/src/main/sql/postgresql/system/80-async-dlm/5624381_sys_gh12591_migrate_c_queue_package_processor_ID.sql
@@ -25,7 +25,7 @@ FROM c_queue_element
 
 -- DELETE invalid data
 
-CREATE TEMP TABLE workPackageIDsToDelete AS
+CREATE TABLE backup.workPackageIDsToDelete_15032023 AS
 SELECT c_queue_workpackage_ID
 FROM c_queue_workpackage wp
 WHERE EXISTS(SELECT 1
@@ -37,13 +37,13 @@ WHERE EXISTS(SELECT 1
 
 DELETE
 FROM c_queue_element el
-WHERE EXISTS(SELECT 1 FROM workPackageIDsToDelete wpd WHERE el.c_queue_workpackage_id = wpd.c_queue_workpackage_ID)
+WHERE EXISTS(SELECT 1 FROM backup.workPackageIDsToDelete_15032023 wpd WHERE el.c_queue_workpackage_id = wpd.c_queue_workpackage_ID)
 ;
 
 DELETE
 FROM c_queue_workpackage wp
 WHERE EXISTS(SELECT 1
-             FROM workPackageIDsToDelete wpd WHERE wp.c_queue_workpackage_id = wpd.c_queue_workpackage_ID)
+             FROM backup.workPackageIDsToDelete_15032023 wpd WHERE wp.c_queue_workpackage_id = wpd.c_queue_workpackage_ID)
 ;
 
 DELETE
@@ -52,17 +52,8 @@ WHERE NOT EXISTS(SELECT 1 FROM c_queue_packageprocessor WHERE c_queue_packagepro
 ;
 
 
-DELETE
-FROM dlm.c_queue_workpackage_archived wp
-WHERE EXISTS(SELECT 1
-             FROM c_queue_block b
-             WHERE b.c_queue_block_id = wp.c_queue_block_id
-               AND NOT EXISTS(SELECT 1 FROM c_queue_packageprocessor proc WHERE proc.c_queue_packageprocessor_id = b.c_queue_packageprocessor_id))
-;
-
-
 -- Actual migration
-38
+
 update c_queue_workpackage
 set c_queue_packageprocessor_id = block.c_queue_packageprocessor_id, updatedBy=99, updated=TO_TIMESTAMP('2022-02-04 12:16:30', 'YYYY-MM-DD HH24:MI:SS')
 from c_queue_block block
