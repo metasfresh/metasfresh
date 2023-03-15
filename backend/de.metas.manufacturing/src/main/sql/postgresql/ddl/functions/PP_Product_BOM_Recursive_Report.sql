4a653a3d0b757954df840421ecf6024c9521fe76
Ispas Mihai <mihai.ispas@pevesoft.ro>
2023-03-15 12:46:29 +0200

    refactor code and left join

--- backend/de.metas.manufacturing/src/main/sql/postgresql/ddl/functions/PP_Product_BOM_Recursive_Report.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/ddl/functions/PP_Product_BOM_Recursive_Report.sql
@@ -1,19 +1,27 @@
 CREATE OR REPLACE FUNCTION pp_product_bom_recursive_report(p_pp_product_bom_id numeric)
-    RETURNS TABLE(line text, productvalue character varying, productname character varying, qtybom numeric, percentage numeric, uomsymbol character varying, supplier text)
+    RETURNS TABLE
+            (
+                line         text,
+                productvalue character varying,
+                productname  character varying,
+                qtybom       numeric,
+                percentage   numeric,
+                uomsymbol    character varying,
+                supplier     text
+            )
     STABLE
     LANGUAGE sql
 AS
 $$
-select
-    t.Line,
-    t.ProductValue,
-    t.ProductName,
-    t.QtyBOM,
-    t.Percentage,
-    t.UOMSymbol,
-    t.Supplier
-from PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, null) t
-order by t.path
+SELECT t.Line,
+       t.ProductValue,
+       t.ProductName,
+       t.QtyBOM,
+       t.Percentage,
+       t.UOMSymbol,
+       t.Supplier
+FROM PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, NULL) t
+ORDER BY t.path
 $$
 ;
 
