c2cb0c6c659e4e11ccd47cc1ef4c09cf2ef768e0
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2023-03-21 14:18:56 +0100

    Also ignore inactive ICs (don't ignore inactive masterdata though)

--- backend/de.metas.printing/de.metas.printing.base/src/main/java/de/metas/printing/process/C_Invoice_Candidate_EnqueueSelectionForInvoicingAndPDFConcatenating.java
+++ backend/de.metas.printing/de.metas.printing.base/src/main/java/de/metas/printing/process/C_Invoice_Candidate_EnqueueSelectionForInvoicingAndPDFConcatenating.java
@@ -83,7 +83,7 @@ public class C_Invoice_Candidate_EnqueueSelectionForInvoicingAndPDFConcatenating
 		final IParams params = getParameterAsIParams();
 		this.invoicingParams = new InvoicingParams(params);
 
-		int selectionCount = createSelection();
+		final int selectionCount = createSelection();
 
 		if (selectionCount <= 0)
 		{
@@ -163,6 +163,7 @@ public class C_Invoice_Candidate_EnqueueSelectionForInvoicingAndPDFConcatenating
 
 		return queryBL
 				.createQueryBuilder(I_C_Invoice_Candidate.class, getCtx(), ITrx.TRXNAME_None)
+				.addOnlyActiveRecordsFilter()
 				.addEqualsFilter(I_C_Invoice_Candidate.COLUMNNAME_AD_Org_ID, p_OrgId)
 				.addEqualsFilter(I_C_Invoice_Candidate.COLUMNNAME_Processed, false)
 				.addInSubQueryFilter()
