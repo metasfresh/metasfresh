e6ebd1b677bf3f7fe7ce5a588c024ea87be40448
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-04-08 10:07:33 +0300

    #14602 Separate scripts (#15092)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5684560_sys_gh14602_MigrateInvoiceCandidates.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5684560_sys_gh14602_MigrateInvoiceCandidates.sql
@@ -37,16 +37,6 @@ UPDATE c_invoice_candidate
 SET c_paymentterm_id= (select c_paymentterm_id from c_paymentterm pt where pt.isdefault='Y' and pt.isactive='Y' and pt.AD_Client_ID=c_invoice_candidate.AD_Client_ID) where c_paymentterm_id IS NULL;
 
 
--- make it mandatory
-
-
--- 2023-04-07T10:25:52.683Z
-INSERT INTO t_alter_column values('c_invoice_candidate','C_PaymentTerm_ID','NUMERIC(10)',null,null)
-;
-
--- 2023-04-07T10:25:52.693Z
-INSERT INTO t_alter_column values('c_invoice_candidate','C_PaymentTerm_ID',null,'NOT NULL',null)
-;
 
 
 --- drop unneeded parameter
