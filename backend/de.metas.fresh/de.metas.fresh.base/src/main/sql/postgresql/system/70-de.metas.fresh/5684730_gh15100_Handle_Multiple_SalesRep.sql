a411bcc164f737aab48dd2e91e5f285c2c3a4a35
riadmen <riad.mennour@owiin.com>
2023-04-11 11:36:30 +0100

    If the invoice candidates have different Sales Represent, the resulting invoice will have none

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_gh15100_Handle_Multiple_SalesRep.sql
@@ -0,0 +1,10 @@
+--
+-- Script: C:\Users\riadm\.metasfresh\migration_scripts\5684730_migration_2023-04-11_postgresql.sql
+-- User: metasfresh
+-- OS user: riadm
+--
+
+-- 2023-04-11T10:34:03.783Z
+INSERT INTO C_AggregationItem (AD_Client_ID,AD_Column_ID,AD_Org_ID,C_Aggregation_ID,C_AggregationItem_ID,Created,CreatedBy,Description,EntityType,IsActive,Type,Updated,UpdatedBy) VALUES (1000000,586468,1000000,1000000,540071,TO_TIMESTAMP('2023-04-11 11:34:03','YYYY-MM-DD HH24:MI:SS'),100,'If the invoice candidates have different Sales Represent, the resulting invoice will have none.','de.metas.invoicecandidate','N','COL',TO_TIMESTAMP('2023-04-11 11:34:03','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
