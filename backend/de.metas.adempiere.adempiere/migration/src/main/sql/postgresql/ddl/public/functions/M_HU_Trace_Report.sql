05b9dfdff33ed38308104225b1abd52e7636806a
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-04-12 20:59:38 +0300

    M_HU_Trace_Report function: adjust inout qtys

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/public/functions/M_HU_Trace_Report.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/ddl/public/functions/M_HU_Trace_Report.sql
@@ -7,17 +7,17 @@ CREATE OR REPLACE FUNCTION public.M_HU_Trace_Report(p_AD_PInstance_ID numeric)
                 LotNumber     character varying,
                 HUTraceType   character varying,
                 Product       character varying,
-                "InOut"         character varying,
+                "InOut"       character varying,
                 CostCollector character varying,
                 PPOrder       character varying,
                 Inventory     character varying,
-                DocumentDate  timestamp with time zone,
+                DocumentDate  timestamp WITH TIME ZONE,
                 Qty           numeric,
                 UOM           character varying
             )
 AS
 $BODY$
-    --
+
 
 SELECT t.LotNumber              AS LotNumber,
        'Current Stock'          AS HUTraceType,
@@ -53,7 +53,39 @@ WHERE t.hutracetype IN ('PRODUCTION_ISSUE',
 
 UNION
 
+(SELECT DISTINCT ON (t.m_inout_ID) t.LotNumber                                                                                 AS LotNumber,
+                                   t.hutracetype                                                                               AS HUTraceType,
+                                   p.value || '_' || p.name                                                                    AS Product,
+                                   io.documentno                                                                               AS InOut,
+                                   NULL                                                                                        AS CostCollector,
+                                   NULL                                                                                        AS PPOrder,
+                                   NULL                                                                                        AS Inventory,
+                                   io.movementdate                                                                             AS DocumentDate,
 
+                                   (SELECT SUM(uomconvert(p_m_product_id := p.m_product_id, p_c_uom_from_id := iol.c_uom_id, p_c_uom_to_id := p.c_uom_id, p_qty := iol.movementqty))
+                                    FROM metasfresh.public.m_inoutline iol
+                                             LEFT JOIN m_attributesetinstance asi ON iol.m_attributesetinstance_id = asi.m_attributesetinstance_id
+                                             LEFT JOIN M_AttributeInstance ai ON asi.m_attributesetinstance_id = ai.m_attributesetinstance_id
+                                    WHERE io.m_inout_id = iol.m_inout_id
+                                      AND iol.m_product_id = p.m_product_id
+                                      AND ((t.lotnumber IS NULL) OR (ai.m_attribute_id = 1000017 AND ai.value = t.lotnumber))) AS Qty,
+                                   u.uomsymbol                                                                                 AS UOM
+ FROM M_HU_Trace t
+          JOIN M_Product p
+               ON t.m_product_id = p.m_product_id
+          JOIN C_UOM u ON t.C_UOM_ID = u.c_uom_id
+          LEFT JOIN M_InOut io ON t.m_inout_id = io.m_inout_id
+          LEFT JOIN PP_cost_collector cc ON t.pp_cost_collector_id = cc.pp_cost_collector_id
+          LEFT JOIN PP_Order po ON t.pp_order_id = po.pp_order_id
+          LEFT JOIN M_Inventory i ON t.M_Inventory_ID = i.m_inventory_id
+ WHERE t.hutracetype IN ('MATERIAL_RECEIPT', 'MATERIAL_SHIPMENT')
+   AND EXISTS (SELECT 1
+               FROM T_Selection s
+               WHERE s.AD_PInstance_ID = p_AD_PInstance_ID
+                 AND s.T_Selection_ID = t.m_hu_trace_id))
+
+
+UNION
 (SELECT t.LotNumber                                                                 AS LotNumber,
         t.hutracetype                                                               AS HUTraceType,
         p.value || '_' || p.name                                                    AS Product,
@@ -72,13 +104,13 @@ UNION
           LEFT JOIN PP_cost_collector cc ON t.pp_cost_collector_id = cc.pp_cost_collector_id
           LEFT JOIN PP_Order po ON t.pp_order_id = po.pp_order_id
           LEFT JOIN M_Inventory i ON t.M_Inventory_ID = i.m_inventory_id
- WHERE t.hutracetype IN ('PRODUCTION_ISSUE', 'PRODUCTION_RECEIPT', 'MATERIAL_RECEIPT', 'MATERIAL_SHIPMENT', 'MATERIAL_INVENTORY')
+ WHERE t.hutracetype IN ('PRODUCTION_ISSUE', 'PRODUCTION_RECEIPT', 'MATERIAL_INVENTORY')
    AND EXISTS (SELECT 1
                FROM T_Selection s
                WHERE s.AD_PInstance_ID = p_AD_PInstance_ID
-                 AND s.T_Selection_ID = t.m_hu_trace_id)
+                 AND s.T_Selection_ID = t.m_hu_trace_id))
 
- ORDER BY t.lotnumber, DocumentDate)
+ORDER BY LotNumber, HUTraceType, DocumentDate
 
     ;
 $BODY$
@@ -94,3 +126,4 @@ COMMENT ON FUNCTION public.M_HU_Trace_Report(numeric) IS 'Returns all M_HU_Trace
 --
 -- Test it
 -- select * from M_HU_Trace_Report(1000000) ;
+
