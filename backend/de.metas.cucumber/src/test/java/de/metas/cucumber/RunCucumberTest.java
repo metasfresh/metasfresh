aed9d1db2b87d735e6dd1c4c1b561cb9b8c4a361
Chems Nouioua <chems.nouioua@gmail.com>
2023-04-17 14:30:02 +0100

    fix unwanted change

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/RunCucumberTest.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/RunCucumberTest.java
@@ -29,8 +29,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		glue = "de.metas.cucumber.stepdefs",
-		//tags = "not @ignore",
-		tags = "@ordersWithSameSalesRep",
+		tags = "not @ignore", // use this tag to temporarily ignore single scenarios
+		//tags = "@dev:runThisOne", // use this tag to run only particular scenarios
 		plugin = {
 				"html:target/cucumber.html",
 				"json:target/cucumber.json" /* this json-output is needed for the Jenkins plugin that's supposed to publish it */,
