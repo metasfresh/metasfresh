975dc940463ffa98d9778704d3aa880dcc65d9f0
riadmen <riad.mennour@owiin.com>
2023-04-18 15:25:36 +0100

    Handle multiple SalesRep in invoices

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_gh15100_Handle_Multiple_SalesRep.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_gh15100_Handle_Multiple_SalesRep.sql
@@ -1,5 +1,15 @@
-
--- 2023-04-11T10:34:03.783Z
-INSERT INTO C_AggregationItem (AD_Client_ID, AD_Column_ID, AD_Org_ID, C_Aggregation_ID, C_AggregationItem_ID, Created, CreatedBy, Description, EntityType, IsActive, Type, Updated, UpdatedBy)
-VALUES (1000000, 572811, 1000000, 1000000, 540071, TO_TIMESTAMP('2023-04-11 11:34:03', 'YYYY-MM-DD HH24:MI:SS'), 100, 'If the invoice candidates have different Sales Represent, the resulting invoice will have none.', 'de.metas.invoicecandidate', 'N', 'COL', TO_TIMESTAMP('2023-04-11 11:34:03', 'YYYY-MM-DD HH24:MI:SS'), 100)
+-- 2021-02-15T12:54:07.014Z
+-- #298 changing anz. stellen
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,DDL_NoForeignKey,Description,EntityType,FacetFilterSeqNo,FieldLength,Help,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRangeFilter,IsSelectionColumn,IsShowFilterIncrementButtons,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,572811,1063,0,18,190,540270,'SalesRep_ID','(select bp.SalesRep_ID from C_BPartner bp where bp.c_bpartner_id = C_Invoice_Candidate.c_bpartner_id)',TO_TIMESTAMP('2021-02-15 14:54:06','YYYY-MM-DD HH24:MI:SS'),100,'N','','de.metas.invoicecandidate',0,10,'','Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N',0,'Kundenbetreuer',0,0,TO_TIMESTAMP('2021-02-15 14:54:06','YYYY-MM-DD HH24:MI:SS'),100,0)
 ;
+
+-- 2021-02-15T12:54:07.052Z
+-- #298 changing anz. stellen
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Column_ID=572811 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 2021-02-15T12:54:07.087Z
+-- #298 changing anz. stellen
+/* DDL */  select update_Column_Translation_From_AD_Element(1063)
+;
+
