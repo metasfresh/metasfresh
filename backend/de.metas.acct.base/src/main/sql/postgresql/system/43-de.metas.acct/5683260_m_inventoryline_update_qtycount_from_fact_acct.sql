03ba3fbe20486b618fcaa42de01099bc3dfc7bdd
Teo Sarca <teo.sarca@metasfresh.com>
2023-04-18 18:42:07 +0300

    Improve cost calculation support/troubleshoot DB functions (#15021)
    * m_inventoryline_update_qtycount_from_fact_acct: support DateAcctFrom and DateAcctTo params (optional)
    
    * accountable_docs_and_lines_v: fine tune how we order documents when we process them (for costing)
    
    * accountable_docs_and_lines_v fine tune priorities in case of inventory: if document no is 'init' -> take it first, we assume that the cost initialization
    
    * accounting_docs_to_repost_reorder: group documents quaternary to avoid negative costing

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5683260_m_inventoryline_update_qtycount_from_fact_acct.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5683260_m_inventoryline_update_qtycount_from_fact_acct.sql
@@ -2,6 +2,14 @@ DROP FUNCTION IF EXISTS de_metas_acct.m_inventoryline_update_qtycount_from_fact_
     p_M_Inventory_ID      numeric,
     p_ProductAssetAccount varchar,
     p_RecreateLines       char(1),
+    p_DryRun              char(1)
+)
+;
+
+DROP FUNCTION IF EXISTS de_metas_acct.m_inventoryline_update_qtycount_from_fact_acct(
+    p_M_Inventory_ID      numeric,
+    p_ProductAssetAccount varchar,
+    p_RecreateLines       char(1),
     p_DryRun              char(1),
     p_DateAcctFrom        date,
     p_DateAcctTo          date
