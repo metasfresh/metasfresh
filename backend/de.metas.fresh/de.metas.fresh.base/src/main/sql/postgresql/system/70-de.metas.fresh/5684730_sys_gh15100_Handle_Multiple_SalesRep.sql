e8a4bda4dccbceb1355a983490df27d54f5010f7
dm-ps <83770997+dm-ps@users.noreply.github.com>
2023-04-24 13:45:01 +0300

    fix SalesRep_ID ColumnSQL (#15157)

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_sys_gh15100_Handle_Multiple_SalesRep.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_sys_gh15100_Handle_Multiple_SalesRep.sql
@@ -1,7 +1,7 @@
 DO $$
 BEGIN
     IF exists(select 1 from ad_column where ad_column_id = 572811 and columnname = 'SalesRep_ID') THEN
-        update ad_column SET ColumnSQL = '(select o.SalesRep_ID from C_Order o where o.C_Order_ID = C_Invoice_Candidate.C_Order_ID', updated = now() where ad_column_id = 572811;
+        update ad_column SET ColumnSQL = '(select o.SalesRep_ID from C_Order o where o.C_Order_ID = C_Invoice_Candidate.C_Order_ID)', updated = now() where ad_column_id = 572811;
     ELSE
         INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,DDL_NoForeignKey,Description,EntityType,FacetFilterSeqNo,FieldLength,Help,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRangeFilter,IsSelectionColumn,IsShowFilterIncrementButtons,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,572811,1063,0,18,190,540270,'SalesRep_ID','(select o.SalesRep_ID from C_Order o where o.C_Order_ID = C_Invoice_Candidate.C_Order_ID)',TO_TIMESTAMP('2021-02-15 14:54:06','YYYY-MM-DD HH24:MI:SS'),100,'N','','de.metas.invoicecandidate',0,10,'','Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N',0,'Kundenbetreuer',0,0,TO_TIMESTAMP('2021-02-15 14:54:06','YYYY-MM-DD HH24:MI:SS'),100,0)
         ;
