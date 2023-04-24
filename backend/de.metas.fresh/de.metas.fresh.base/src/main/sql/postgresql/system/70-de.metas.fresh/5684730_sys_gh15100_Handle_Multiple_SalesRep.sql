20c321189b7f072e91d5b5d977ad1a910836ddd9
dm-ps <83770997+dm-ps@users.noreply.github.com>
2023-04-24 14:43:33 +0300

    Yoyo uat gh15100 fix sales rep id column sql 2 (#15160)
    * fix SalesRep_ID ColumnSQL

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_sys_gh15100_Handle_Multiple_SalesRep.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684730_sys_gh15100_Handle_Multiple_SalesRep.sql
@@ -9,7 +9,7 @@ BEGIN
         INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N') AND t.AD_Column_ID=572811 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
         ;
 
-        /* DDL */  select update_Column_Translation_From_AD_Element(1063)
+        /* DDL */  perform update_Column_Translation_From_AD_Element(1063)
         ;
     END IF;
 END $$;
