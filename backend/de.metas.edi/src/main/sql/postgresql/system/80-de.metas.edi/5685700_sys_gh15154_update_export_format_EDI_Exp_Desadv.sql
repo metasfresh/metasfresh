b1e6c799fd358934aee18a57956aa3c2238556d9
fp-ps <fp-ps@pevesoft.com>
2023-04-24 17:06:02 +0300

    refs: https://github.com/metasfresh/metasfresh/issues/15154

---
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5685700_sys_gh15154_update_export_format_EDI_Exp_Desadv.sql
@@ -0,0 +1,4 @@
+-- 2023-04-24T13:00:43.597Z
+INSERT INTO EXP_FormatLine (AD_Client_ID,AD_Column_ID,AD_Org_ID,AD_Reference_Override_ID,Created,CreatedBy,EntityType,EXP_Format_ID,EXP_FormatLine_ID,IsActive,IsMandatory,IsPartUniqueIndex,Name,Position,Type,Updated,UpdatedBy,Value) VALUES (0,586492,0,10,TO_TIMESTAMP('2023-04-24 16:00:43','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.esb.edi',540405,550592,'Y','N','N','ShipmentDocumentNo',85,'E',TO_TIMESTAMP('2023-04-24 16:00:43','YYYY-MM-DD HH24:MI:SS'),100,'ShipmentDocumentNo')
+;
+
