c25b3bf454546baf985d87b4aef357ec9aa36336
dm-ps <dragos.moca@pevesoft.ro>
2023-04-25 13:08:37 +0300

    fix SalesRep_ID ColumnSQL

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5685670_sys_gh15100_Handle_Multiple_SalesRep.sql
@@ -0,0 +1,6 @@
+DO $$
+    BEGIN
+        IF exists(select 1 from ad_column where ad_column_id = 572811 and columnname = 'SalesRep_ID' and ColumnSQL != '(select o.SalesRep_ID from C_Order o where o.C_Order_ID = C_Invoice_Candidate.C_Order_ID)') THEN
+            update ad_column SET ColumnSQL = '(select o.SalesRep_ID from C_Order o where o.C_Order_ID = C_Invoice_Candidate.C_Order_ID)', updated = now() where ad_column_id = 572811;
+        END IF;
+    END $$;
