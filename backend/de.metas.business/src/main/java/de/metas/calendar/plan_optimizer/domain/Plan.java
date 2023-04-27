e7b250a32b96aaa4f9365276e3c1b0f4f58f9987
Teo Sarca <teo.sarca@metasfresh.com>
2023-04-27 12:48:44 +0300

    improve scoring (use BendableScore)
    (cherry picked from commit 99b89b8414600da71ed1c65b853efe4d39f850b5)

--- backend/de.metas.business/src/main/java/de/metas/calendar/plan_optimizer/domain/Plan.java
+++ backend/de.metas.business/src/main/java/de/metas/calendar/plan_optimizer/domain/Plan.java
@@ -1,5 +1,6 @@
 package de.metas.calendar.plan_optimizer.domain;
 
+import de.metas.calendar.plan_optimizer.solver.PlanConstraintProvider;
 import de.metas.calendar.simulation.SimulationPlanId;
 import lombok.Data;
 import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
@@ -8,7 +9,7 @@ import org.optaplanner.core.api.domain.solution.PlanningSolution;
 import org.optaplanner.core.api.domain.valuerange.CountableValueRange;
 import org.optaplanner.core.api.domain.valuerange.ValueRangeFactory;
 import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
-import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.score.buildin.bendable.BendableScore;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -28,8 +29,8 @@ public class Plan
 	@PlanningEntityCollectionProperty
 	private ArrayList<Step> stepsList;
 
-	@PlanningScore
-	private HardSoftScore score;
+	@PlanningScore(bendableHardLevelsSize = PlanConstraintProvider.HARD_LEVELS_SIZE, bendableSoftLevelsSize = PlanConstraintProvider.SOFT_LEVELS_SIZE)
+	private BendableScore score;
 	private boolean isFinalSolution;
 	private Duration timeSpent = Duration.ZERO;
 
