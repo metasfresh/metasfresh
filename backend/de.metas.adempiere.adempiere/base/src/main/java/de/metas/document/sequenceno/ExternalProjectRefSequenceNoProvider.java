c1c1deea1b1101712cefe84f4650678853785a0e
MHNV <95350690+MHNV@users.noreply.github.com>
2023-05-08 08:10:50 +0200

    Revert "Delete ExternalProjectRefSequenceNoProvider.java"
    This reverts commit c948cb4e780d0d8ca9d46f4e16e6dd590ea0a334.

---
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/document/sequenceno/ExternalProjectRefSequenceNoProvider.java
@@ -0,0 +1,67 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.document.sequenceno;
+
+import de.metas.common.util.time.SystemTime;
+import de.metas.document.DocumentSequenceInfo;
+import lombok.NonNull;
+import org.compiere.util.Evaluatee;
+
+import java.text.DecimalFormat;
+import java.util.function.Supplier;
+
+public class ExternalProjectRefSequenceNoProvider implements CustomSequenceNoProvider
+{
+	@Override
+	public boolean isApplicable(@NonNull final Evaluatee context, @NonNull final DocumentSequenceInfo docSeqInfo)
+	{
+		return true;
+	}
+
+	@Override
+	public @NonNull String provideSeqNo(
+			@NonNull final Supplier<String> incrementalSeqNoSupplier,
+			@NonNull final Evaluatee context,
+			@NonNull final DocumentSequenceInfo documentSequenceInfo)
+	{
+		final String incrementalSeqNo = incrementalSeqNoSupplier.get();
+		final String customPart = getCustomPart();
+
+		final String decimalPattern = documentSequenceInfo.getDecimalPattern();
+		if (decimalPattern == null)
+		{
+			return customPart + "-" + incrementalSeqNo;
+		}
+
+		final int incrementalSeqNoInt = Integer.parseInt(incrementalSeqNo);
+
+		return customPart + "-" + new DecimalFormat(decimalPattern).format(incrementalSeqNoInt);
+	}
+
+	@NonNull
+	protected static String getCustomPart()
+	{
+		return String.valueOf(SystemTime.asLocalDate().getYear())
+				.substring(2);
+	}
+}
