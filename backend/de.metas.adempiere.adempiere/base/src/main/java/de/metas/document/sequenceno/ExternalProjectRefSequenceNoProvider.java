425909d51208384443e80e4134b2990f59646036
Manh Hung Nguyen Vuong <95350690+MHNV@users.noreply.github.com>
2023-05-08 11:43:54 +0200

    new class for projectRefSeq Changes (#15233)
    * new class for projectRefSeq Changes
    
    new class for projectRefSeq Changes
    
    https://github.com/metasfresh/ip180/issues/210
    
    * Update ExternalProjectRefSequenceChanger.java
    
    changed seq
    
    * Update ExternalProjectRefBeginningSequenceNoProvider.java
    
    code refactoring
    
    * Update ExternalProjectRefBeginningSequenceNoProvider.java
    
    * updated classes
    
    updated so that its not overcomplicated
    
    * Update ExternalProjectRefBeginningSequenceNoProvider.java
    
    * created abstract basis class and renamed custom class
    
    created abstract basis class and renamed custom class
    
    * Delete ExternalProjectRefSequenceNoProvider.java
    
    * Revert "Delete ExternalProjectRefSequenceNoProvider.java"
    
    This reverts commit c948cb4e780d0d8ca9d46f4e16e6dd590ea0a334.
    
    * Update ExternalProjectRefSequenceNoProvider.java
    
    * add custom classes to return and reverse the seq
    
    add custom classes to return and reverse the seq
    https://github.com/metasfresh/ip180/issues/210
    
    * #15338 Refactor
    
    * #15338 Rename files after merging
    
    ---------
    
    Co-authored-by: Cristina Ghita <cristina.ghita@metasfresh.com>
    (cherry picked from commit 519d4b885c2ffea1993de70b1c5293974eeb9893)

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/document/sequenceno/ExternalProjectRefSequenceNoProvider.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/document/sequenceno/ExternalProjectRefSequenceNoProvider.java
@@ -22,46 +22,17 @@
 
 package de.metas.document.sequenceno;
 
-import de.metas.common.util.time.SystemTime;
-import de.metas.document.DocumentSequenceInfo;
 import lombok.NonNull;
-import org.compiere.util.Evaluatee;
 
 import java.text.DecimalFormat;
-import java.util.function.Supplier;
 
-public class ExternalProjectRefSequenceNoProvider implements CustomSequenceNoProvider
+public class ExternalProjectRefSequenceNoProvider extends AbstractExternalProjectRefSequenceNoProvider
 {
-	@Override
-	public boolean isApplicable(@NonNull final Evaluatee context, @NonNull final DocumentSequenceInfo docSeqInfo)
-	{
-		return true;
-	}
-
-	@Override
-	public @NonNull String provideSeqNo(
-			@NonNull final Supplier<String> incrementalSeqNoSupplier,
-			@NonNull final Evaluatee context,
-			@NonNull final DocumentSequenceInfo documentSequenceInfo)
-	{
-		final String incrementalSeqNo = incrementalSeqNoSupplier.get();
-		final String customPart = getCustomPart();
-
-		final String decimalPattern = documentSequenceInfo.getDecimalPattern();
-		if (decimalPattern == null)
-		{
-			return customPart + "-" + incrementalSeqNo;
-		}
-
-		final int incrementalSeqNoInt = Integer.parseInt(incrementalSeqNo);
-
-		return customPart + "-" + new DecimalFormat(decimalPattern).format(incrementalSeqNoInt);
-	}
-
 	@NonNull
-	private static String getCustomPart()
+	@Override
+	protected String formatSeqNo(@NonNull final String decimalPattern, final int incrementalSeqNoInt)
 	{
-		return String.valueOf(SystemTime.asLocalDate().getYear())
-				.substring(2);
+		final String customPart = getCustomPart();
+		return customPart + "-" + new DecimalFormat(decimalPattern).format(incrementalSeqNoInt);
 	}
 }
