9e5726f447b89624b37329491ce8bbc987b0db93
riadmen <riad.mennour@owiin.com>
2023-05-09 21:29:59 +0100

    Update C_Campaign_Price callout

--- backend/de.metas.business/src/main/java/de/metas/pricing/rules/campaign_price/callout/C_Campaign_Price.java
+++ backend/de.metas.business/src/main/java/de/metas/pricing/rules/campaign_price/callout/C_Campaign_Price.java
@@ -1,5 +1,20 @@
 package de.metas.pricing.rules.campaign_price.callout;
 
+import de.metas.bpartner.BPGroupId;
+import de.metas.bpartner.BPartnerId;
+import de.metas.bpartner.service.IBPartnerDAO;
+import de.metas.lang.SOTrx;
+import de.metas.location.CountryId;
+import de.metas.location.ICountryDAO;
+import de.metas.pricing.PriceListId;
+import de.metas.pricing.PricingSystemId;
+import de.metas.pricing.service.IPriceListDAO;
+import de.metas.pricing.service.ProductPrices;
+import de.metas.pricing.service.impl.PlainPriceListDAO;
+import de.metas.product.IProductBL;
+import de.metas.product.ProductId;
+import de.metas.uom.UomId;
+import de.metas.util.Services;
 import org.adempiere.ad.callout.annotations.Callout;
 import org.adempiere.ad.callout.annotations.Callout.RecursionAvoidanceLevel;
 import org.adempiere.ad.callout.annotations.CalloutMethod;
@@ -10,18 +25,10 @@ import org.adempiere.ad.modelvalidator.annotations.ModelChange;
 import org.adempiere.exceptions.FillMandatoryException;
 import org.adempiere.model.CopyRecordFactory;
 import org.compiere.model.I_C_Campaign_Price;
+import org.compiere.model.I_M_PriceList_Version;
+import org.compiere.model.I_M_ProductPrice;
 import org.compiere.model.ModelValidator;
 import org.springframework.stereotype.Component;
-
-import de.metas.bpartner.BPGroupId;
-import de.metas.bpartner.BPartnerId;
-import de.metas.location.CountryId;
-import de.metas.location.ICountryDAO;
-import de.metas.money.CurrencyId;
-import de.metas.product.IProductBL;
-import de.metas.product.ProductId;
-import de.metas.uom.UomId;
-import de.metas.util.Services;
 
 /*
  * #%L
@@ -70,8 +77,41 @@ public class C_Campaign_Price
 		{
 			return;
 		}
-
+		updatePricingInfo(record);
 		record.setC_BP_Group_ID(-1);
+	}
+
+	private static void updatePricingInfo(final I_C_Campaign_Price record)
+	{
+		final BPartnerId bpartnerId = BPartnerId.ofRepoIdOrNull(record.getC_BPartner_ID());
+		final IBPartnerDAO bpartnerRepo = Services.get(IBPartnerDAO.class);
+		final CountryId countryId = bpartnerRepo.getDefaultShipToLocationCountryIdOrNull(bpartnerId);
+		if (countryId != null)
+		{
+			record.setC_Country_ID(countryId.getRepoId());
+			updateCurrency(record, countryId);
+		}
+		final PricingSystemId pricingSystemId = bpartnerRepo.retrievePricingSystemIdOrNull(bpartnerId, SOTrx.SALES);
+		if (pricingSystemId != null)
+		{
+			record.setM_PricingSystem_ID(pricingSystemId.getRepoId());
+			final IPriceListDAO priceListDAO = new PlainPriceListDAO();
+			final PriceListId priceListId = priceListDAO.retrievePriceListIdByPricingSyst(pricingSystemId, countryId, SOTrx.SALES);
+			if (priceListId != null)
+			{
+				final I_M_PriceList_Version newestPriceListVersion = priceListDAO.retrieveNewestPriceListVersion(priceListId);
+				final ProductId productId = ProductId.ofRepoIdOrNull(record.getM_Product_ID());
+				final I_M_ProductPrice productPrice = ProductPrices.retrieveMainProductPriceOrNull(newestPriceListVersion, productId);
+				if (productPrice != null)
+				{
+					record.setC_TaxCategory_ID(productPrice.getC_TaxCategory_ID());
+					record.setInvoicableQtyBasedOn(productPrice.getInvoicableQtyBasedOn());
+				}
+			}
+		}
+		else {
+			record.setM_PricingSystem_ID(-1);
+		}
 	}
 
 	@CalloutMethod(columnNames = I_C_Campaign_Price.COLUMNNAME_C_BP_Group_ID, skipIfCopying = true)
@@ -99,12 +139,13 @@ public class C_Campaign_Price
 			return;
 		}
 
+		updateCurrency(record, countryId);
+	}
+
+	private static void updateCurrency(final I_C_Campaign_Price record, final CountryId countryId)
+	{
 		final ICountryDAO countriesRepo = Services.get(ICountryDAO.class);
-		final CurrencyId currencyId = countriesRepo.getCountryCurrencyId(countryId).orElse(null);
-		if (currencyId != null)
-		{
-			record.setC_Currency_ID(currencyId.getRepoId());
-		}
+		countriesRepo.getCountryCurrencyId(countryId).ifPresent(currencyId -> record.setC_Currency_ID(currencyId.getRepoId()));
 	}
 
 	@CalloutMethod(columnNames = I_C_Campaign_Price.COLUMNNAME_M_Product_ID, skipIfCopying = true)
@@ -118,6 +159,7 @@ public class C_Campaign_Price
 
 		final UomId stockUomId = Services.get(IProductBL.class).getStockUOMId(productId);
 		record.setC_UOM_ID(stockUomId.getRepoId());
+		updatePricingInfo(record);
 	}
 
 	@ModelChange(timings = { ModelValidator.TYPE_BEFORE_NEW, ModelValidator.TYPE_BEFORE_CHANGE }, ifColumnsChanged = { I_C_Campaign_Price.COLUMNNAME_C_BPartner_ID, I_C_Campaign_Price.COLUMNNAME_C_BP_Group_ID, I_C_Campaign_Price.COLUMNNAME_M_PricingSystem_ID })
