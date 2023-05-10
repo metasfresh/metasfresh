df33c10eafd6c1782bd16a5bbab1b94ab5bf4381
riadmen <riad.mennour@owiin.com>
2023-05-10 23:25:11 +0100

    master to koala_yawn_uat merge

---
+++ backend/de.metas.async/src/main/sql/postgresql/system/42-de.metas.async/5687610_sys_gh15136_C_Queue_Processor_C_InvoiceCandidateQueue.sql
@@ -0,0 +1,8 @@
+-- 2022-04-07T08:07:35.885Z
+UPDATE C_Queue_Processor SET IsActive='N' WHERE C_Queue_Processor_ID=540072
+;
+
+-- 2022-04-07T08:12:37.393Z
+UPDATE C_Queue_Processor SET IsActive='N' WHERE C_Queue_Processor_ID=540029
+;
+
