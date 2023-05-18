c4e72cbf0fe7a5709dfafeec048b812a52a930b8
riadmen <riad.mennour@owiin.com>
2023-05-18 09:39:09 +0100

    Fix calendar remaining trl
    (cherry picked from commit 518fb4186ebe4faf0958f99c4889ded5fd0c63cd)

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5688300_sys_gh15343_Fix_Calendrer_Remaining_Trl.sql
@@ -0,0 +1,272 @@
+-- Element: C_Project_WO_Step_Simulation_ID
+-- 2023-05-17T14:38:53.816042100Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Prüfschritt Simulation', PrintName='Prüfschritt Simulation',Updated=TO_TIMESTAMP('2023-05-17 15:38:53.815','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581065 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T14:38:53.923164400Z
+UPDATE AD_Element SET Name='Prüfschritt Simulation', PrintName='Prüfschritt Simulation' WHERE AD_Element_ID=581065
+;
+
+-- 2023-05-17T14:39:01.689860300Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581065,'de_DE') 
+;
+
+-- 2023-05-17T14:39:01.837791400Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581065,'de_DE') 
+;
+
+-- Element: C_Project_WO_Resource_Simulation_ID
+-- 2023-05-17T14:40:43.440794300Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Projekt Ressource Simulation', PrintName='Projekt Ressource Simulation',Updated=TO_TIMESTAMP('2023-05-17 15:40:43.44','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581066 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T14:40:43.542908700Z
+UPDATE AD_Element SET Name='Projekt Ressource Simulation', PrintName='Projekt Ressource Simulation' WHERE AD_Element_ID=581066
+;
+
+-- 2023-05-17T14:40:55.148337Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581066,'de_DE') 
+;
+
+-- 2023-05-17T14:40:55.290890400Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581066,'de_DE') 
+;
+
+-- Element: C_Project_Resource_Budget_Simulation_ID
+-- 2023-05-17T14:42:07.222765Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Projekt Ressource Budget Simulation', PrintName='Projekt Ressource Budget Simulation',Updated=TO_TIMESTAMP('2023-05-17 15:42:07.222','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581067 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T14:42:07.332085Z
+UPDATE AD_Element SET Name='Projekt Ressource Budget Simulation', PrintName='Projekt Ressource Budget Simulation' WHERE AD_Element_ID=581067
+;
+
+-- 2023-05-17T14:42:13.627969300Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581067,'de_DE') 
+;
+
+-- 2023-05-17T14:42:13.735928800Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581067,'de_DE') 
+;
+
+-- Element: C_Project_WO_Step_ID
+-- 2023-05-17T14:47:47.876108800Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Prüfschritt', PrintName='Prüfschritt',Updated=TO_TIMESTAMP('2023-05-17 15:47:47.876','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=580965 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T14:47:48.011899600Z
+UPDATE AD_Element SET Name='Prüfschritt', PrintName='Prüfschritt' WHERE AD_Element_ID=580965
+;
+
+-- 2023-05-17T14:47:56.377783100Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580965,'de_DE') 
+;
+
+-- 2023-05-17T14:47:56.543826Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580965,'de_DE') 
+;
+
+-- Element: C_Project_WO_Resource_ID
+-- 2023-05-17T16:25:13.628761700Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Projekt Ressource', PrintName='Projekt Ressource',Updated=TO_TIMESTAMP('2023-05-17 17:25:13.628','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=580966 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:25:13.750201400Z
+UPDATE AD_Element SET Name='Projekt Ressource', PrintName='Projekt Ressource' WHERE AD_Element_ID=580966
+;
+
+-- 2023-05-17T16:25:20.026082700Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580966,'de_DE') 
+;
+
+-- 2023-05-17T16:25:20.189489Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580966,'de_DE') 
+;
+
+-- Element: IsAllDay
+-- 2023-05-17T16:29:03.660757900Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='ganztägig', PrintName='ganztägig',Updated=TO_TIMESTAMP('2023-05-17 17:29:03.66','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=580861 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:29:03.758010400Z
+UPDATE AD_Element SET Name='ganztägig', PrintName='ganztägig' WHERE AD_Element_ID=580861
+;
+
+-- 2023-05-17T16:29:10.055094Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580861,'de_DE') 
+;
+
+-- 2023-05-17T16:29:10.179260500Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580861,'de_DE') 
+;
+
+-- Element: IsAllDay
+-- 2023-05-17T16:30:09.348269900Z
+UPDATE AD_Element_Trl SET Name='Ganztägig', PrintName='Ganztägig',Updated=TO_TIMESTAMP('2023-05-17 17:30:09.348','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=580861 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:30:09.442288300Z
+UPDATE AD_Element SET Name='Ganztägig', PrintName='Ganztägig' WHERE AD_Element_ID=580861
+;
+
+-- 2023-05-17T16:30:12.972728800Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580861,'de_DE') 
+;
+
+-- 2023-05-17T16:30:13.082259Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580861,'de_DE') 
+;
+
+-- Element: IsAllDay
+-- 2023-05-17T16:32:07.812195100Z
+UPDATE AD_Element_Trl SET Name='ganztägig', PrintName='ganztägig',Updated=TO_TIMESTAMP('2023-05-17 17:32:07.811','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=580861 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:32:07.910522800Z
+UPDATE AD_Element SET Name='ganztägig', PrintName='ganztägig' WHERE AD_Element_ID=580861
+;
+
+-- 2023-05-17T16:32:15.197485Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580861,'de_DE') 
+;
+
+-- 2023-05-17T16:32:15.695654Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580861,'de_DE') 
+;
+
+-- Element: IsAllDay_Prev
+-- 2023-05-17T16:34:18.642541900Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='ganztägig (zuvor)', PrintName='ganztägig (zuvor)',Updated=TO_TIMESTAMP('2023-05-17 17:34:18.642','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581138 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:34:18.743926100Z
+UPDATE AD_Element SET Name='ganztägig (zuvor)', PrintName='ganztägig (zuvor)' WHERE AD_Element_ID=581138
+;
+
+-- 2023-05-17T16:34:26.348214500Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581138,'de_DE') 
+;
+
+-- 2023-05-17T16:34:26.449810300Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581138,'de_DE') 
+;
+
+-- Element: AssignDateFrom_Prev
+-- 2023-05-17T16:35:20.745917700Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Zuordnung von (zuvor)', PrintName='Zuordnung von (zuvor)',Updated=TO_TIMESTAMP('2023-05-17 17:35:20.745','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581136 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:35:20.841482900Z
+UPDATE AD_Element SET Name='Zuordnung von (zuvor)', PrintName='Zuordnung von (zuvor)' WHERE AD_Element_ID=581136
+;
+
+-- 2023-05-17T16:35:28.459812700Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581136,'de_DE') 
+;
+
+-- 2023-05-17T16:35:28.553085100Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581136,'de_DE') 
+;
+
+-- Element: AssignDateTo_Prev
+-- 2023-05-17T16:36:02.143366600Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Zuordnung bis (zuvor)', PrintName='Zuordnung bis (zuvor)',Updated=TO_TIMESTAMP('2023-05-17 17:36:02.143','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581137 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:36:02.236347100Z
+UPDATE AD_Element SET Name='Zuordnung bis (zuvor)', PrintName='Zuordnung bis (zuvor)' WHERE AD_Element_ID=581137
+;
+
+-- 2023-05-17T16:36:07.152271300Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581137,'de_DE') 
+;
+
+-- 2023-05-17T16:36:07.284244100Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581137,'de_DE') 
+;
+
+-- Element: C_Project_Resource_Budget_ID
+-- 2023-05-17T16:40:35.753955200Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Projekt Ressource Budget', PrintName='Projekt Ressource Budget',Updated=TO_TIMESTAMP('2023-05-17 17:40:35.753','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=580947 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:40:35.843146100Z
+UPDATE AD_Element SET Name='Projekt Ressource Budget', PrintName='Projekt Ressource Budget' WHERE AD_Element_ID=580947
+;
+
+-- 2023-05-17T16:40:41.488655800Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(580947,'de_DE') 
+;
+
+-- 2023-05-17T16:40:41.576204200Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(580947,'de_DE') 
+;
+
+-- Element: DateFinishPlan
+-- 2023-05-17T16:41:34.827501500Z
+UPDATE AD_Element_Trl SET Description='', Help='', IsTranslated='Y', Name='Ende Plan', PrintName='Ende Plan',Updated=TO_TIMESTAMP('2023-05-17 17:41:34.827','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=541244 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:41:34.925618100Z
+UPDATE AD_Element SET Description='', Help='', Name='Ende Plan', PrintName='Ende Plan' WHERE AD_Element_ID=541244
+;
+
+-- 2023-05-17T16:41:39.236264900Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(541244,'de_DE') 
+;
+
+-- 2023-05-17T16:41:39.363587Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(541244,'de_DE') 
+;
+
+-- Element: C_Project_WO_Resource2_ID
+-- 2023-05-17T16:46:18.338173700Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Projekt Ressource', PrintName='Projekt Ressource',Updated=TO_TIMESTAMP('2023-05-17 17:46:18.338','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581127 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:46:18.444096900Z
+UPDATE AD_Element SET Name='Projekt Ressource', PrintName='Projekt Ressource' WHERE AD_Element_ID=581127
+;
+
+-- 2023-05-17T16:46:24.961502Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581127,'de_DE') 
+;
+
+-- 2023-05-17T16:46:25.062126200Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581127,'de_DE') 
+;
+
+-- Reference Item: C_Project_WO_Resource_Conflict_Status -> C_Conflict
+-- 2023-05-17T16:47:46.383197900Z
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='Konflikt',Updated=TO_TIMESTAMP('2023-05-17 17:47:46.383','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Ref_List_ID=543241
+;
+
+-- 2023-05-17T16:47:46.490466200Z
+UPDATE AD_Ref_List SET Name='Konflikt' WHERE AD_Ref_List_ID=543241
+;
+
+-- Reference Item: C_Project_WO_Resource_Conflict_Status -> R_Resolved
+-- 2023-05-17T16:48:16.987993600Z
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='Aufgelöst',Updated=TO_TIMESTAMP('2023-05-17 17:48:16.987','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Ref_List_ID=543242
+;
+
+-- 2023-05-17T16:48:17.129478100Z
+UPDATE AD_Ref_List SET Name='Aufgelöst' WHERE AD_Ref_List_ID=543242
+;
+
+-- Element: C_Project_WO_Resource_Simulation_ID
+-- 2023-05-17T16:53:54.474654900Z
+UPDATE AD_Element_Trl SET Name='Prüfschritt Projekt Ressource Simulation', PrintName='Prüfschritt Projekt Ressource Simulation',Updated=TO_TIMESTAMP('2023-05-17 17:53:54.474','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=581066 AND AD_Language='de_DE'
+;
+
+-- 2023-05-17T16:53:54.574141600Z
+UPDATE AD_Element SET Name='Prüfschritt Projekt Ressource Simulation', PrintName='Prüfschritt Projekt Ressource Simulation' WHERE AD_Element_ID=581066
+;
+
+-- 2023-05-17T16:54:13.582046400Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(581066,'de_DE') 
+;
+
+-- 2023-05-17T16:54:13.676997600Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(581066,'de_DE') 
+;
