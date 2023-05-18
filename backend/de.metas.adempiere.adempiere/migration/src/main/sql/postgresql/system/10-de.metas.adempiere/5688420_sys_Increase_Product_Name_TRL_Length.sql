aa64944b04bf36fa3caf4d445c059633d70e0f8c
Adrian <adrian.stefan@metasfresh.com>
2023-05-18 13:49:09 +0300

    make m_product.name & m_product_trl.name have the same length once again

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5688420_sys_Increase_Product_Name_TRL_Length.sql
@@ -0,0 +1,24 @@
+--duplicates 5633190_sys_Increase_Product_Name_TRL_Length.sql
+--while reversing the effects of 5638600_sys_Increase_Product_Name_TRL_Length_510.sql
+-- so that m_product and m_product_trl have the same length
+
+-- 2022-04-04T12:33:02.081Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET FieldLength=600,Updated=TO_TIMESTAMP('2022-04-04 15:33:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3330
+;
+
+-- 2022-04-04T12:33:03.192Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_product_trl','Name','VARCHAR(600)',null,null)
+;
+
+
+
+
+-- 2022-04-04T12:33:02.081Z	-- 2022-04-04T12:33:02.081Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator	-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET FieldLength=600,Updated=TO_TIMESTAMP('2022-04-04 15:33:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1410; -- M_Product.Name
+
+-- 2022-04-04T12:33:03.192Z	-- 2022-04-04T12:33:03.192Z
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator	-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO t_alter_column values('m_product','Name','VARCHAR(600)',null,null);
