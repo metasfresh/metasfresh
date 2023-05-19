ab41b2700628cece8acfd37d4aeb3e9637df224f
mrie04 <max.rieck@web.de>
2023-05-19 14:43:56 +0200

    improve description of info fields

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5685540_sys_gh15155_LotForLot.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5685540_sys_gh15155_LotForLot.sql
@@ -167,7 +167,7 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 ;
 
 -- 2023-04-27T07:24:16.291Z
-INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,Created,CreatedBy,Description,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,582267,0,TO_TIMESTAMP('2023-04-27 09:24:16.12','YYYY-MM-DD HH24:MI:SS.US'),100,'Gibt an ob bei der Anzahl Lot for Lot verwendet wurde.','U','Y','Lot for Lot','Lot for Lot',TO_TIMESTAMP('2023-04-27 09:24:16.12','YYYY-MM-DD HH24:MI:SS.US'),100)
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,Created,CreatedBy,Description,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,582267,0,TO_TIMESTAMP('2023-04-27 09:24:16.12','YYYY-MM-DD HH24:MI:SS.US'),100,'Gibt an ob bei der Berechnung der Anzahl Lot for Lot verwendet wurde, welches in den Produkt Plandaten konfiguriert wird. Gibt leer an, wenn nicht eindeutig bestimmbar oder nicht via Produkt Plandaten erstellt.','U','Y','Lot for Lot','Lot for Lot',TO_TIMESTAMP('2023-04-27 09:24:16.12','YYYY-MM-DD HH24:MI:SS.US'),100)
 ;
 
 -- 2023-04-27T07:24:16.297Z
@@ -195,7 +195,7 @@ UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2023-04-27 09:2
 ;
 
 -- 2023-04-27T07:25:49.272Z
-UPDATE AD_Element_Trl SET Description='Indicates if the Quantity is based on Lot for Lot.', IsTranslated='Y',Updated=TO_TIMESTAMP('2023-04-27 09:25:49.27','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582267 AND AD_Language='en_US'
+UPDATE AD_Element_Trl SET Description='Indicates whether Lot for Lot was used in the calculation of the number, which is configured in the product planning. Indicates none, if not clearly determinable or not created via product planning.', IsTranslated='Y',Updated=TO_TIMESTAMP('2023-04-27 09:25:49.27','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582267 AND AD_Language='en_US'
 ;
 
 -- 2023-04-27T07:25:49.273Z
@@ -205,7 +205,7 @@ UPDATE AD_Element_Trl SET Description='Indicates if the Quantity is based on Lot
 -- Field: Material Disposition -> Materialdispo -> Lot for Lot
 -- Column: MD_Candidate.IsLotForLot
 -- 2023-04-27T07:27:47.563Z
-UPDATE AD_Field SET AD_Name_ID=582267, Description='Gibt an ob bei der Anzahl Lot for Lot verwendet wurde.', Help=NULL, Name='Lot for Lot',Updated=TO_TIMESTAMP('2023-04-27 09:27:47.563','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=715028
+UPDATE AD_Field SET AD_Name_ID=582267, Description='Gibt an ob bei der Berechnung der Anzahl Lot for Lot verwendet wurde, welches in den Produkt Plandaten konfiguriert wird. Gibt leer an, wenn nicht eindeutig bestimmbar oder nicht via Produkt Plandaten erstellt.', Help=NULL, Name='Lot for Lot',Updated=TO_TIMESTAMP('2023-04-27 09:27:47.563','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=715028
 ;
 
 -- 2023-04-27T07:27:47.565Z
@@ -223,7 +223,7 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=715028
 -- UI Element: Material Disposition -> Materialdispo.Lot for Lot
 -- Column: MD_Candidate.IsLotForLot
 -- 2023-04-27T07:31:48.417Z
-UPDATE AD_UI_Element SET Description='Gibt an ob bei der Anzahl Lot for Lot verwendet wurde.',Updated=TO_TIMESTAMP('2023-04-27 09:31:48.417','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=617259
+UPDATE AD_UI_Element SET Description='Gibt an ob bei der Berechnung der Anzahl Lot for Lot verwendet wurde, welches in den Produkt Plandaten konfiguriert wird. Gibt leer an, wenn nicht eindeutig bestimmbar oder nicht via Produkt Plandaten erstellt.',Updated=TO_TIMESTAMP('2023-04-27 09:31:48.417','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=617259
 ;
 
 -- Column: PP_Order_Candidate.IsLotForLot
@@ -271,3 +271,20 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=715333
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,715333,0,544794,547107,617432,'F',TO_TIMESTAMP('2023-05-17 14:18:05.009','YYYY-MM-DD HH24:MI:SS.US'),100,'Erzwingt, dass die Menge gleich der bestellten Menge ist.','Y','N','N','Y','N','N','N',0,'Lot for Lot',30,0,0,TO_TIMESTAMP('2023-05-17 14:18:05.009','YYYY-MM-DD HH24:MI:SS.US'),100)
 ;
 
+-- Field: Produktionsdisposition -> Produktionsdisposition -> Lot for Lot
+-- Column: PP_Order_Candidate.IsLotForLot
+-- 2023-05-17T12:18:48.277Z
+UPDATE AD_Field SET AD_Name_ID=582267, Description='Gibt an ob bei der Berechnung der Anzahl Lot for Lot verwendet wurde, welches in den Produkt Plandaten konfiguriert wird. Gibt leer an, wenn nicht eindeutig bestimmbar oder nicht via Produkt Plandaten erstellt.', Help=NULL, Name='Lot for Lot',Updated=TO_TIMESTAMP('2023-05-17 14:18:48.277','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=715333
+;
+
+-- 2023-05-17T12:18:48.280Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(582267)
+;
+
+-- 2023-05-17T12:18:48.282Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=715333
+;
+
+-- 2023-05-17T12:18:48.283Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(715333)
+;
