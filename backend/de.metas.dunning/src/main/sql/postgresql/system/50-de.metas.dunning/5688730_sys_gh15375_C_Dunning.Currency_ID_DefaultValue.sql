d3ceb71ff4e441f7336fc8100804983cf91d9720
dm-ps <dragos.moca@pevesoft.ro>
2023-05-22 21:16:55 +0300

    Use correct currency in dunning mail refs: https://github.com/metasfresh/metasfresh/issues/15375

---
+++ backend/de.metas.dunning/src/main/sql/postgresql/system/50-de.metas.dunning/5688730_sys_gh15375_C_Dunning.Currency_ID_DefaultValue.sql
@@ -0,0 +1,28 @@
+/*
+ * #%L
+ * de.metas.dunning
+ * %%
+ * Copyright (C) 2023 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+-- Column: C_Dunning.C_Currency_ID
+-- Column: C_Dunning.C_Currency_ID
+-- 2023-05-22T17:20:51.922Z
+UPDATE AD_Column SET DefaultValue='-1',Updated=TO_TIMESTAMP('2023-05-22 20:20:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=547343
+;
+
