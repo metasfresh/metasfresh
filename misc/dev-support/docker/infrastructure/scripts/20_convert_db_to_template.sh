545f9a70e451dd1ab2d10b93fe7661a1beacb81c
mrie04 <max.rieck@web.de>
2023-05-25 11:48:31 +0200

    update dev scripts

--- misc/dev-support/docker/infrastructure/scripts/20_convert_db_to_template.sh
+++ misc/dev-support/docker/infrastructure/scripts/20_convert_db_to_template.sh
@@ -2,9 +2,9 @@
 
 #
 # %L
-# de.metas.cucumber
+# metasfresh
 # %%
-# Copyright (C) 2022 metas GmbH
+# Copyright (C) 2023 metas GmbH
 # %%
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as
@@ -23,20 +23,28 @@
 #
 
 set -e
-set -u # this will fail this script if no branch name is given as parameter
 
 # We assume that the stuff was started with 10_reset_db_to_seed_dump.sh
-BRANCH_NAME=$1
+# We assume that in the folder misc/dev-support/docker/infrastructure/env-files/ there exists an env file named ${BRANCH_NAME}.env
+if ! [ -z "$1" ]; then
+    BRANCH_NAME=$1
+else
+    echo "!! The first parameter needs do correspond to an env-File !!"
+    echo "!! E.g. to use the env-file env-files/release.env, run 20_convert_db_to_template.sh release !!" 
+    exit
+fi
+
+set -u
 
 # the winpty is needed to avoid an error when running the script in git bash on windows
 
-winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "alter database metasfresh rename to metasfresh_template_master_integration;"
-winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "alter database metasfresh_template_master_integration is_template true;"
+winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "alter database metasfresh rename to metasfresh_template_${BRANCH_NAME};"
+winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "alter database metasfresh_template_${BRANCH_NAME} is_template true;"
 
 echo "The local database has been converted to a template database."
-echo "You can drop this template database by running the following commands:"
+echo "You can drop this template database by running "
 echo ""
-echo "winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c \"UPDATE pg_database SET datistemplate='false' WHERE datname='metasfresh_template_master_integration';\""
-echo "winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c \"drop database if exists metasfresh_template_master_integration;\""
+echo "./21_drop_template.sh ${BRANCH_NAME}"
+
 echo ""
 echo "You can now proceed with creating the actual database from this template"
