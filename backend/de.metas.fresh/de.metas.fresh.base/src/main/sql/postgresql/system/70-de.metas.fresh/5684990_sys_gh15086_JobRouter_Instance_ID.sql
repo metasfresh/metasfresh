d4d3b94542048f96ff3852e9239ab53778889d76
MHNV <95350690+MHNV@users.noreply.github.com>
2023-05-30 15:01:48 +0200

    Update 5684990_sys_gh15086_JobRouter_Instance_ID.sql
    changed the AD_InputDataSource_ID to 1000006
    since the 1000005 ID already exist for Ebay.
    
    https://github.com/metasfresh/mf15/issues/2159

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684990_sys_gh15086_JobRouter_Instance_ID.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5684990_sys_gh15086_JobRouter_Instance_ID.sql
@@ -6,5 +6,5 @@
 
 
 -- 2023-04-13T14:52:11.876Z
-INSERT INTO AD_InputDataSource (AD_Client_ID,AD_InputDataSource_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,IsDestination,IsEdiEnabled,Name,Updated,UpdatedBy,Value,internalname) VALUES (1000000,1000005,1000000,TO_TIMESTAMP('2023-04-13 15:52:11','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N','SOURCE.de.metas.rest_api.v2.invoice.InvoicesRestController',TO_TIMESTAMP('2023-04-13 15:52:11','YYYY-MM-DD HH24:MI:SS'),100,'SOURCE.de.metas.rest_api.v2.invoice.InvoicesRestController','SOURCE.de.metas.rest_api.v2.invoice.InvoicesRestController')
+INSERT INTO AD_InputDataSource (AD_Client_ID,AD_InputDataSource_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,IsDestination,IsEdiEnabled,Name,Updated,UpdatedBy,Value,internalname) VALUES (1000000,1000006,1000000,TO_TIMESTAMP('2023-04-13 15:52:11','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N','SOURCE.de.metas.rest_api.v2.invoice.InvoicesRestController',TO_TIMESTAMP('2023-04-13 15:52:11','YYYY-MM-DD HH24:MI:SS'),100,'SOURCE.de.metas.rest_api.v2.invoice.InvoicesRestController','SOURCE.de.metas.rest_api.v2.invoice.InvoicesRestController')
 ;
