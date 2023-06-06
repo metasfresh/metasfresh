df5f8400a30d4472b35ab9925ce9cfa3ec2a9a5c
Adrian <adrian.stefan@metasfresh.com>
2023-06-06 18:50:51 +0300

    set isExternalProperty

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/model/validator/M_InOut.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/model/validator/M_InOut.java
@@ -340,6 +340,7 @@ public class M_InOut
 		// the handling units are already created
 		if (!existingHandlingUnits.isEmpty())
 		{
+			existingHandlingUnits.forEach(hu -> hu.setIsExternalProperty(true));
 			final IContextAware contextProvider = InterfaceWrapperHelper.getContextAware(customerReturn);
 
 			//make sure they all have status active
