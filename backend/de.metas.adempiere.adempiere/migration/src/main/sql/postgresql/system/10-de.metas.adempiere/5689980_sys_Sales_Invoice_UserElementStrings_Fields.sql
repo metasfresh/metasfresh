92488e5776c6cc806060400b5dc2931600fc987b
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-06-07 15:52:46 +0300

    script correction

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5689980_sys_Sales_Invoice_UserElementStrings_Fields.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5689980_sys_Sales_Invoice_UserElementStrings_Fields.sql
@@ -64,15 +64,6 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=716158
 /* DDL */ select AD_Element_Link_Create_Missing_Field(716158)
 ;
 
--- Field: Rechnung(167,D) -> Rechnung(263,D) -> Datum Fälligkeit
--- Column: C_Invoice.DueDate
--- 2023-05-31T15:30:28.434Z
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,584270,716159,0,263,TO_TIMESTAMP('2023-05-31 18:30:28','YYYY-MM-DD HH24:MI:SS'),100,'Datum, zu dem Zahlung fällig wird',7,'D','Datum, zu dem Zahlung ohne Abzüge oder Rabattierung fällig wird.','Y','Y','N','N','N','N','N','Datum Fälligkeit',TO_TIMESTAMP('2023-05-31 18:30:28','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2023-05-31T15:30:28.435Z
-INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=716159 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
-;
 
 -- 2023-05-31T15:30:28.436Z
 /* DDL */  select update_FieldTranslation_From_AD_Name_Element(2000) 
