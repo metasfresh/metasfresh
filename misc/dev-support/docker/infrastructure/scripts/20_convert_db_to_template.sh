c504f6a1638758714f52bb442252d8b8140afadb
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2023-06-07 15:01:27 +0200

    get improvements from master branch

--- misc/dev-support/docker/infrastructure/scripts/20_convert_db_to_template.sh
+++ misc/dev-support/docker/infrastructure/scripts/20_convert_db_to_template.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+#
+# %L
+# metasfresh
+# %%
+# Copyright (C) 2023 metas GmbH
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/gpl-2.0.html>.
+# L%
+#
+
 set -e
 
 # We assume that the stuff was started with 10_reset_db_to_seed_dump.sh
@@ -23,5 +45,6 @@ echo "The local database has been converted to a template database."
 echo "You can drop this template database by running "
 echo ""
 echo "./21_drop_template.sh ${BRANCH_NAME}"
+
 echo ""
 echo "You can now proceed with creating the actual database from this template"
