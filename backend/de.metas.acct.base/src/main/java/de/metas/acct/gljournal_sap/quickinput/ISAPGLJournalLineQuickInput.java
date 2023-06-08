c4851d19396757a0307dc9dfc8705cb098cf55ab
Teo Sarca <teo.sarca@metasfresh.com>
2023-06-08 10:33:54 +0300

    SAP GL Journal quick input: handle C_Activity_ID, make it configurable via sysconfig

--- backend/de.metas.acct.base/src/main/java/de/metas/acct/gljournal_sap/quickinput/ISAPGLJournalLineQuickInput.java
+++ backend/de.metas.acct.base/src/main/java/de/metas/acct/gljournal_sap/quickinput/ISAPGLJournalLineQuickInput.java
@@ -1,5 +1,7 @@
 package de.metas.acct.gljournal_sap.quickinput;
 
+import com.google.common.collect.ImmutableSet;
+
 import java.math.BigDecimal;
 
 public interface ISAPGLJournalLineQuickInput
@@ -7,6 +9,7 @@ public interface ISAPGLJournalLineQuickInput
 	String COLUMNNAME_PostingSign = "PostingSign";
 
 	java.lang.String getPostingSign();
+
 	String COLUMNNAME_GL_Account_ID = "GL_Account_ID";
 
 	int getGL_Account_ID();
@@ -17,8 +20,23 @@ public interface ISAPGLJournalLineQuickInput
 
 	String COLUMNNAME_C_Tax_ID = "C_Tax_ID";
 	String COLUMNNAME_M_SectionCode_ID = "M_SectionCode_ID";
+	String COLUMNNAME_C_Activity_ID = "C_Activity_ID";
 
 	int getM_SectionCode_ID();
 
 	int getC_Tax_ID();
+
+	int getC_Activity_ID();
+
+	//
+	//
+	//
+	ImmutableSet<String> COLUMNNAMES = ImmutableSet.of(
+			COLUMNNAME_PostingSign,
+			COLUMNNAME_GL_Account_ID,
+			COLUMNNAME_Amount,
+			COLUMNNAME_M_SectionCode_ID,
+			COLUMNNAME_C_Activity_ID,
+			COLUMNNAME_C_Tax_ID
+	);
 }
