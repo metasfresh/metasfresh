b5dcba08f078d5eb8be4b26bcdd89f03168be3c5
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-06-12 10:43:29 +0300

    avoid duplicate

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5689660_sys_C_InvoiceCandidate_UserElementStrings_Fields.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5689660_sys_C_InvoiceCandidate_UserElementStrings_Fields.sql
@@ -195,16 +195,16 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=716035
 -- 2023-05-29T09:10:33.956Z
 /* DDL */ select AD_Element_Link_Create_Missing_Field(716035)
 ;
-
--- Field: Sales Invoice Candidates_OLD(540092,de.metas.invoicecandidate) -> Invoice Candidates(540279,de.metas.invoicecandidate) -> Project
--- Column: C_Invoice_Candidate.C_Project_ID
--- 2023-05-29T09:10:34.648Z
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,572537,716036,0,540279,TO_TIMESTAMP('2023-05-29 12:10:34','YYYY-MM-DD HH24:MI:SS'),100,'Financial Project',10,'de.metas.invoicecandidate','A Project allows you to track and control internal or external activities.','Y','Y','N','N','N','N','N','Project',TO_TIMESTAMP('2023-05-29 12:10:34','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2023-05-29T09:10:34.692Z
-INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=716036 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
-;
+--
+-- -- Field: Sales Invoice Candidates_OLD(540092,de.metas.invoicecandidate) -> Invoice Candidates(540279,de.metas.invoicecandidate) -> Project
+-- -- Column: C_Invoice_Candidate.C_Project_ID
+-- -- 2023-05-29T09:10:34.648Z
+-- INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,572537,716036,0,540279,TO_TIMESTAMP('2023-05-29 12:10:34','YYYY-MM-DD HH24:MI:SS'),100,'Financial Project',10,'de.metas.invoicecandidate','A Project allows you to track and control internal or external activities.','Y','Y','N','N','N','N','N','Project',TO_TIMESTAMP('2023-05-29 12:10:34','YYYY-MM-DD HH24:MI:SS'),100)
+-- ;
+--
+-- -- 2023-05-29T09:10:34.692Z
+-- INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=716036 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+-- ;
 
 -- 2023-05-29T09:10:34.736Z
 /* DDL */  select update_FieldTranslation_From_AD_Name_Element(208) 
