371be9f025c689bafbb094bde45f1b0085a9ed48
alexp <no_user_email@email.com>
2023-06-13 18:04:33 +0300

    https://github.com/metasfresh/metasfresh/issues/15528
    (cherry picked from commit 8ed9defebc7b0e43587d23f1b01811fb940c8d25)

---
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5691075_sys_gh_15528_add_translations_for_AllocateToFEC_process.sql
@@ -0,0 +1,8 @@
+update ad_element set name='Foreign Exchange Contract',printname='Foreign Exchange Contract'
+                  FROM ad_element where ad_ELEMENT_id = 581935;
+
+update ad_element set name='Allocated Amount',printname='Allocated Amt'
+                  FROM ad_element where ad_ELEMENT_id = 2677;
+
+update ad_element set name='Remaining FEC',printname='Remaining FEC'
+                  FROM ad_element where ad_ELEMENT_id = 582401
