e99dedcbbbbd107c1682ae75162450f740b4fc7a
cp-ps <77785468+cp-ps@users.noreply.github.com>
2023-06-14 09:58:25 +0300

    Update 5691075_sys_gh_15528_add_translations_for_AllocateToFEC_process.sql

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5691075_sys_gh_15528_add_translations_for_AllocateToFEC_process.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5691075_sys_gh_15528_add_translations_for_AllocateToFEC_process.sql
@@ -1,8 +1,8 @@
 update ad_element set name='Foreign Exchange Contract',printname='Foreign Exchange Contract'
-                  FROM ad_element where ad_ELEMENT_id = 581935;
+                  FROM ad_element where AD_ELEMENT_ID = 581935;
 
 update ad_element set name='Allocated Amount',printname='Allocated Amt'
-                  FROM ad_element where ad_ELEMENT_id = 2677;
+                  FROM ad_element where AD_ELEMENT_ID = 2677;
 
 update ad_element set name='Remaining FEC',printname='Remaining FEC'
-                  FROM ad_element where ad_ELEMENT_id = 582401
+                  FROM ad_element where AD_ELEMENT_ID = 582401;
