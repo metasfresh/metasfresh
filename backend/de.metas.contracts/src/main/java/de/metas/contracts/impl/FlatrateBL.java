3dd8c583bcfdf240ffc1b65ded47bdb9e000dd90
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2023-07-01 17:40:30 +0200

    Fix "SpringApplicationContext not configured yet" error (#15786)
    (also remove two unused fields)
    
    Caused by: org.adempiere.exceptions.AdempiereException: SpringApplicationContext not configured yet
    Additional parameters:
     requiredType: class de.metas.cache.model.ModelCacheInvalidationService
       at org.compiere.SpringContextHolder.throwExceptionIfNull(SpringContextHolder.java:219)
       at org.compiere.SpringContextHolder.getBean(SpringContextHolder.java:125)
       at de.metas.cache.model.ModelCacheInvalidationService.get(ModelCacheInvalidationService.java:65)
       at de.metas.contracts.impl.FlatrateBL.<init>(FlatrateBL.java:191)
       at sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
       at sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)
       at sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
       at java.lang.reflect.Constructor.newInstance(Constructor.java:423)
       at de.metas.util.Services.findAndLoadService(Services.java:250)
    
    (cherry picked from commit 3716c87a3519efabccf16047ef9be2df8ad4572a)

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/impl/FlatrateBL.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/impl/FlatrateBL.java
@@ -187,8 +187,6 @@ public class FlatrateBL implements IFlatrateBL
 	private final IADTableDAO adTableDAO = Services.get(IADTableDAO.class);
 	private final IOrgDAO orgDAO = Services.get(IOrgDAO.class);
 	private final IInvoiceCandidateHandlerBL invoiceCandidateHandlerBL = Services.get(IInvoiceCandidateHandlerBL.class);
-
-	private final transient ModelCacheInvalidationService modelCacheInvalidationService = ModelCacheInvalidationService.get();
 	private final IUOMDAO uomDAO = Services.get(IUOMDAO.class);
 	private final IInvoiceCandDAO invoiceCandDAO = Services.get(IInvoiceCandDAO.class);
 	private final IProductDAO productDAO = Services.get(IProductDAO.class);
@@ -2117,6 +2115,10 @@ public class FlatrateBL implements IFlatrateBL
 			updateBillBPartnerForInvoiceCandidate(request);
 			invoiceCandidateHandlerBL.invalidateCandidatesFor(term);
 		}
+
+		// we can't have this as a field,
+		// because metasfresh might try to instantiate FlatrateBL before the spring context is ready
+		final ModelCacheInvalidationService modelCacheInvalidationService = ModelCacheInvalidationService.get();
 
 		modelCacheInvalidationService.invalidate(
 				CacheInvalidateMultiRequest.of(
