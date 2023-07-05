c1b504e4157e5efc052848b275efac095d9b8254
riadmen <riad.mennour@owiin.com>
2023-07-05 15:11:37 +0100

    Account Open Items flag
    (cherry picked from commit e38178c51c463fb3050ba09de2fd7898e991e8a8)

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5694640_sys_gh15802_Account_Open_Items_Flag.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5694640_sys_gh15802_Account_Open_Items_Flag.sql
@@ -69,14 +69,6 @@ UPDATE AD_Element_Trl SET Name='Offener Posten Verwaltet', PrintName='Offener Po
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,716633,0,542127,543188,618219,'F',TO_TIMESTAMP('2023-07-05 11:27:45','YYYY-MM-DD HH24:MI:SS'),100,'This indicator shows that the account selected is an open item managed account.','Y','N','N','Y','N','N','N',0,'Open Item Managed',70,0,0,TO_TIMESTAMP('2023-07-05 11:27:45','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
--- 2023-07-05T10:28:36.227Z
-INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,582535,0,TO_TIMESTAMP('2023-07-05 11:28:35','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','OI','OI',TO_TIMESTAMP('2023-07-05 11:28:35','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
--- 2023-07-05T10:28:36.307Z
-INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=582535 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
-;
-
 -- Field: GL Journal (SAP)(541693,de.metas.ab182) -> GL Journal Line (SAP)(546899,de.metas.ab182) -> Open Item Managed
 -- Column: SAP_GLJournalLine.IsOpenItem
 -- 2023-07-05T10:33:07.435Z
