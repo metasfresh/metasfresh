3a26c62d60dbf5b77a4185c57546d1be849e91f5
Teo Sarca <teo.sarca@metasfresh.com>
2023-07-06 14:15:52 +0300

    in case of sales order use the price and vat from sales invoice

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/java/de/metas/gplr/source/model/SourceInvoiceLine.java
@@ -0,0 +1,22 @@
+package de.metas.gplr.source.model;
+
+import de.metas.currency.Amount;
+import de.metas.invoice.InvoiceLineId;
+import de.metas.order.OrderLineId;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+import javax.annotation.Nullable;
+
+@Value
+@Builder
+public class SourceInvoiceLine
+{
+	@NonNull InvoiceLineId id;
+	@NonNull Amount lineNetAmtFC;
+	@NonNull Amount taxAmtFC;
+	@NonNull SourceTaxInfo tax;
+
+	@Nullable OrderLineId orderLineId;
+}
