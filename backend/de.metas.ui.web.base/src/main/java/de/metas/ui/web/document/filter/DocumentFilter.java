92ace1215c9ceb7d7af5693aae2fee0ef1b9ffc0
Teo Sarca <teo.sarca@metasfresh.com>
2023-07-07 10:25:33 +0300

    DocumentFilter.getParameterValueAsRefListOrNull

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/document/filter/DocumentFilter.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/document/filter/DocumentFilter.java
@@ -8,6 +8,7 @@ import de.metas.i18n.TranslatableStrings;
 import de.metas.ui.web.document.filter.DocumentFilterParam.Operator;
 import de.metas.util.Check;
 import de.metas.util.GuavaCollectors;
+import de.metas.util.lang.ReferenceListAwareEnum;
 import de.metas.util.lang.RepoIdAware;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -23,6 +24,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.IntFunction;
 
 /*
@@ -66,10 +68,10 @@ public final class DocumentFilter
 		return builder()
 				.filterId(filterId)
 				.parameter(DocumentFilterParam.builder()
-								   .setFieldName(fieldName)
-								   .setOperator(operator)
-								   .setValue(value)
-								   .build())
+						.setFieldName(fieldName)
+						.setOperator(operator)
+						.setValue(value)
+						.build())
 				.build();
 	}
 
@@ -83,10 +85,10 @@ public final class DocumentFilter
 		return builder()
 				.filterId(filterId)
 				.parameter(DocumentFilterParam.builder()
-								   .setFieldName(fieldName)
-								   .setOperator(Operator.IN_ARRAY)
-								   .setValue(ImmutableList.copyOf(values))
-								   .build())
+						.setFieldName(fieldName)
+						.setOperator(Operator.IN_ARRAY)
+						.setValue(ImmutableList.copyOf(values))
+						.build())
 				.build();
 	}
 
@@ -106,10 +108,10 @@ public final class DocumentFilter
 		return builder()
 				.filterId(filterId)
 				.parameter(DocumentFilterParam.builder()
-								   .setFieldName(fieldName)
-								   .setOperator(Operator.EQUAL)
-								   .setValue(value)
-								   .build())
+						.setFieldName(fieldName)
+						.setOperator(Operator.EQUAL)
+						.setValue(value)
+						.build())
 				.build();
 	}
 
@@ -285,7 +287,6 @@ public final class DocumentFilter
 		return param.getValueAsInstant();
 	}
 
-
 	@Nullable
 	public <T extends RepoIdAware> T getParameterValueAsRepoIdOrNull(@NonNull final String parameterName, @NonNull final IntFunction<T> repoIdMapper)
 	{
@@ -299,6 +300,18 @@ public final class DocumentFilter
 	}
 
 	@Nullable
+	public <T extends ReferenceListAwareEnum> T getParameterValueAsRefListOrNull(@NonNull final String parameterName, @NonNull final Function<String, T> mapper)
+	{
+		final DocumentFilterParam param = getParameterOrNull(parameterName);
+		if (param == null)
+		{
+			return null;
+		}
+
+		return param.getParameterValueAsRefListOrNull(mapper);
+	}
+
+	@Nullable
 	public <T> T getParameterValueAs(@NonNull final String parameterName)
 	{
 		final DocumentFilterParam param = getParameterOrNull(parameterName);
@@ -307,8 +320,7 @@ public final class DocumentFilter
 			return null;
 		}
 
-		@SuppressWarnings("unchecked")
-		final T value = (T)param.getValue();
+		@SuppressWarnings("unchecked") final T value = (T)param.getValue();
 		return value;
 	}
 
