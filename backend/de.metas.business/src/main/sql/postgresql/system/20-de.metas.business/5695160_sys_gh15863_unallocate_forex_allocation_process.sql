3360c13b104c66e1fdb5acdaa2803cc4de4f422b
fp-ps <fp-ps@pevesoft.com>
2023-07-11 13:42:56 +0300

    update DE trls

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5695160_sys_gh15863_unallocate_forex_allocation_process.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5695160_sys_gh15863_unallocate_forex_allocation_process.sql
@@ -1,7 +1,7 @@
 -- Value: C_Order_UnallocateFromForexContract
 -- Classname: de.metas.forex.webui.process.C_Order_UnallocateFromForexContract
 -- 2023-07-10T14:10:45.518Z
-INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,Help,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,IsUpdateExportDate,IsUseBPartnerLanguage,LockWaitTimeout,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,SpreadsheetFormat,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,585282,'Y','de.metas.forex.webui.process.C_Order_UnallocateFromForexContract','N',TO_TIMESTAMP('2023-07-10 17:10:45','YYYY-MM-DD HH24:MI:SS'),100,'D','Allocated amount from selected lines will be unallocated from FEC','Y','N','N','N','Y','N','N','N','Y','N','Y',0,'Unallocate from FEC','json','N','Y','xls','Java',TO_TIMESTAMP('2023-07-10 17:10:45','YYYY-MM-DD HH24:MI:SS'),100,'C_Order_UnallocateFromForexContract')
+INSERT INTO AD_Process (AccessLevel,AD_Client_ID,AD_Org_ID,AD_Process_ID,AllowProcessReRun,Classname,CopyFromProcess,Created,CreatedBy,EntityType,Help,IsActive,IsApplySecuritySettings,IsBetaFunctionality,IsDirectPrint,IsFormatExcelFile,IsNotifyUserAfterExecution,IsOneInstanceOnly,IsReport,IsTranslateExcelHeaders,IsUpdateExportDate,IsUseBPartnerLanguage,LockWaitTimeout,Name,PostgrestResponseFormat,RefreshAllAfterExecution,ShowHelp,SpreadsheetFormat,Type,Updated,UpdatedBy,Value) VALUES ('3',0,0,585282,'Y','de.metas.forex.webui.process.C_Order_UnallocateFromForexContract','N',TO_TIMESTAMP('2023-07-10 17:10:45','YYYY-MM-DD HH24:MI:SS'),100,'D','The allocated amount of the selected lines will be removed from the Foreign Exchange Contract (FEC).','Y','N','N','N','Y','N','N','N','Y','N','Y',0,'Remove allocation from FEC','json','N','Y','xls','Java',TO_TIMESTAMP('2023-07-10 17:10:45','YYYY-MM-DD HH24:MI:SS'),100,'C_Order_UnallocateFromForexContract')
 ;
 
 -- 2023-07-10T14:10:45.518Z
@@ -11,11 +11,11 @@ INSERT INTO AD_Process_Trl (AD_Language,AD_Process_ID, Description,Help,Name, Is
 -- Value: C_Order_UnallocateFromForexContract
 -- Classname: de.metas.forex.webui.process.C_Order_UnallocateFromForexContract
 -- 2023-07-11T06:57:05.591Z
-UPDATE AD_Process SET Description='Allocated amount from selected lines will be unallocated from FEC',Updated=TO_TIMESTAMP('2023-07-11 09:57:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585282
+UPDATE AD_Process SET Description='The allocated amount of the selected lines will be removed from the Foreign Exchange Contract (FEC).',Updated=TO_TIMESTAMP('2023-07-11 09:57:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585282
 ;
 
 -- 2023-07-11T06:57:05.600Z
-UPDATE AD_Process_Trl trl SET Description='Allocated amount from selected lines will be unallocated from FEC' WHERE AD_Process_ID=585282 AND AD_Language='en_US'
+UPDATE AD_Process_Trl trl SET Description='The allocated amount of the selected lines will be removed from the Foreign Exchange Contract (FEC).' WHERE AD_Process_ID=585282 AND AD_Language='en_US'
 ;
 
 
@@ -32,21 +32,21 @@ INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,A
 
 -- Process: C_Order_UnallocateFromForexContract(de.metas.forex.webui.process.C_Order_UnallocateFromForexContract)
 -- 2023-07-11T09:17:47.633Z
-UPDATE AD_Process_Trl SET Description='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Help='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Name='Freigabe von FEC',Updated=TO_TIMESTAMP('2023-07-11 12:17:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=585282
+UPDATE AD_Process_Trl SET Description='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Help='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Name='Zuordnung zum Devisenvertrag (FEC) aufheben',Updated=TO_TIMESTAMP('2023-07-11 12:17:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=585282
 ;
 
 -- Process: C_Order_UnallocateFromForexContract(de.metas.forex.webui.process.C_Order_UnallocateFromForexContract)
 -- 2023-07-11T09:17:58.691Z
-UPDATE AD_Process_Trl SET Description='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Help='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Name='Freigabe von FEC',Updated=TO_TIMESTAMP('2023-07-11 12:17:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=585282
+UPDATE AD_Process_Trl SET Description='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Help='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Name='Zuordnung zum Devisenvertrag (FEC) aufheben',Updated=TO_TIMESTAMP('2023-07-11 12:17:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=585282
 ;
 
 -- Process: C_Order_UnallocateFromForexContract(de.metas.forex.webui.process.C_Order_UnallocateFromForexContract)
 -- 2023-07-11T09:18:09.400Z
-UPDATE AD_Process_Trl SET Description='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Help='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Name='Freigabe von FEC',Updated=TO_TIMESTAMP('2023-07-11 12:18:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Process_ID=585282
+UPDATE AD_Process_Trl SET Description='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Help='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Name='Zuordnung zum Devisenvertrag (FEC) aufheben',Updated=TO_TIMESTAMP('2023-07-11 12:18:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Process_ID=585282
 ;
 
 -- Process: C_Order_UnallocateFromForexContract(de.metas.forex.webui.process.C_Order_UnallocateFromForexContract)
 -- 2023-07-11T09:18:19.609Z
-UPDATE AD_Process_Trl SET Description='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Help='Zugewiesene Beträge aus ausgewählten Zeilen werden aus dem FEC herausgenommen.', Name='Freigabe von FEC',Updated=TO_TIMESTAMP('2023-07-11 12:18:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=585282
+UPDATE AD_Process_Trl SET Description='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Help='Der zugeordnete Betrag der ausgewählten Positionen wird aus dem Devisenvertrag entfernt (engl.: Foreign Exchange Contract, FEC).', Name='Zuordnung zum Devisenvertrag (FEC) aufheben',Updated=TO_TIMESTAMP('2023-07-11 12:18:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=585282
 ;
 
