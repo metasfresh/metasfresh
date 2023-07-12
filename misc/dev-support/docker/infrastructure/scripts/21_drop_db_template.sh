8980fdbbd054312328b135c4730ff846c6af006b
Max Rieck <32903793+mrie04@users.noreply.github.com>
2023-07-12 13:08:50 +0200

    update dev-infrastructure (#15871)
    Co-authored-by: mrie04 <max.rieck@web.de>

--- misc/dev-support/docker/infrastructure/scripts/21_drop_db_template.sh
+++ misc/dev-support/docker/infrastructure/scripts/21_drop_db_template.sh
@@ -2,7 +2,7 @@
 
 #
 # %L
-# metasfresh
+# master
 # %%
 # Copyright (C) 2023 metas GmbH
 # %%
@@ -30,7 +30,7 @@ if ! [ -z "$1" ]; then
     BRANCH_NAME=$1
 else
     echo "!! The first parameter needs do correspond to an env-File !!"
-    echo "!! E.g. to use the env-file env-files/release.env, run 30_reset_db_to_template.sh release !!" 
+    echo "!! E.g. to use the env-file env-files/release.env, run 20_convert_db_to_template.sh release !!"
     exit
 fi
 
@@ -38,8 +38,7 @@ set -u
 
 # the winpty is needed to avoid an error when running the script in git bash on windows
 
-winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "drop database if exists metasfresh;"
-winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "create database metasfresh template metasfresh_template_${BRANCH_NAME};"
+winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "UPDATE pg_database SET datistemplate='false' WHERE datname='metasfresh_template_${BRANCH_NAME}';"
+winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "drop database if exists metasfresh_template_${BRANCH_NAME};"
 
-echo "The local database has been recreated from the template database."
-echo "You can rerun this script to reset the local database to the template database."
+echo "The template-DB metasfresh_template_${BRANCH_NAME} was dropped and can be recreated with 20_convert_db_to_template.sh"
