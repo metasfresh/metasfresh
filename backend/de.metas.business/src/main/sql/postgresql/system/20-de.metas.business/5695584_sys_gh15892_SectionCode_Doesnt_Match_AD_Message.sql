ba972fd6327941a3d76ee2af323bb6737e37323f
cp-ps <77785468+cp-ps@users.noreply.github.com>
2023-07-14 21:16:44 +0300

    remove "'" from ad_message as it messes up with the parameters (#15895)
    refs https://github.com/metasfresh/metasfresh/issues/15892

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5695584_sys_gh15892_SectionCode_Doesnt_Match_AD_Message.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5695584_sys_gh15892_SectionCode_Doesnt_Match_AD_Message.sql
@@ -1,7 +1,7 @@
 
 -- Value: FEC_SectionCode_Doesnt_Match_With_Document
 -- 2023-07-14T14:58:43.251Z
-INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545291,0,TO_TIMESTAMP('2023-07-14 17:58:42','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.acct','Y','ForexContract.SectionCode ({0}) doesn''t match the Document.SectionCode ({1})','E',TO_TIMESTAMP('2023-07-14 17:58:42','YYYY-MM-DD HH24:MI:SS'),100,'FEC_SectionCode_Doesnt_Match_With_Document')
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545291,0,TO_TIMESTAMP('2023-07-14 17:58:42','YYYY-MM-DD HH24:MI:SS'),100,'de.metas.acct','Y','ForexContract.SectionCode ({0}) does not match the Document.SectionCode ({1})','E',TO_TIMESTAMP('2023-07-14 17:58:42','YYYY-MM-DD HH24:MI:SS'),100,'FEC_SectionCode_Doesnt_Match_With_Document')
 ;
 
 -- 2023-07-14T14:58:43.270Z
