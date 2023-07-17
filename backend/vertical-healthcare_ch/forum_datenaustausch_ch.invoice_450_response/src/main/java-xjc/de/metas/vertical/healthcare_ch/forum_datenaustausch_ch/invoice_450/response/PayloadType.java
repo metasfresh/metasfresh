1a9677a81e54e58bab089c46dd20dcbe898db8e4
riadmen <riad.mennour@owiin.com>
2023-07-17 15:56:03 +0100

    Show response_timestamp in Invoice Rejection Detail window

--- backend/vertical-healthcare_ch/forum_datenaustausch_ch.invoice_450_response/src/main/java-xjc/de/metas/vertical/healthcare_ch/forum_datenaustausch_ch/invoice_450/response/PayloadType.java
+++ backend/vertical-healthcare_ch/forum_datenaustausch_ch.invoice_450_response/src/main/java-xjc/de/metas/vertical/healthcare_ch/forum_datenaustausch_ch/invoice_450/response/PayloadType.java
@@ -15,7 +15,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import java.math.BigInteger;
 
 /**
  * <p>Java class for payloadType complex type.
