11210814d30b2197d0862a1c21acb060092f3d30
fp-ps <fp-ps@pevesoft.com>
2023-07-19 14:15:29 +0300

    modular contract log trls

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5696190_sys_gh15910_modular_contract_settings_trls_adjustments.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5696190_sys_gh15910_modular_contract_settings_trls_adjustments.sql
@@ -53,3 +53,113 @@ UPDATE AD_Element_Trl SET Name='Modules', PrintName='Modules',Updated=TO_TIMESTA
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582426,'en_US') 
 ;
 
+
+
+-- Element: null
+-- 2023-07-19T11:03:30.923465900Z
+UPDATE AD_Element_Trl SET Name='Modular Contract Log', PrintName='Modular Contract Log',Updated=TO_TIMESTAMP('2023-07-19 14:03:30.923','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='en_US'
+;
+
+-- 2023-07-19T11:03:30.932233600Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'en_US')
+;
+
+-- Element: null
+-- 2023-07-19T11:03:37.161552300Z
+UPDATE AD_Element_Trl SET Name='Log für modulare Verträge', PrintName='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:03:37.161','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='fr_CH'
+;
+
+-- 2023-07-19T11:03:37.163156500Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'fr_CH')
+;
+
+-- Element: null
+-- 2023-07-19T11:03:40.532195900Z
+UPDATE AD_Element_Trl SET Name='Log für modulare Verträge', PrintName='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:03:40.532','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='it_IT'
+;
+
+-- 2023-07-19T11:03:40.535894800Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'it_IT')
+;
+
+-- Element: null
+-- 2023-07-19T11:03:45.958118600Z
+UPDATE AD_Element_Trl SET Name='Log für modulare Verträge', PrintName='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:03:45.958','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='de_CH'
+;
+
+-- 2023-07-19T11:03:45.960117300Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'de_CH')
+;
+
+-- Element: null
+-- 2023-07-19T11:03:49.721596800Z
+UPDATE AD_Element_Trl SET Name='Log für modulare Verträge', PrintName='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:03:49.721','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='de_DE'
+;
+
+-- 2023-07-19T11:03:49.722632700Z
+UPDATE AD_Element SET Name='Log für modulare Verträge', PrintName='Log für modulare Verträge' WHERE AD_Element_ID=582418
+;
+
+-- 2023-07-19T11:03:50.198357100Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(582418,'de_DE')
+;
+
+-- 2023-07-19T11:03:50.200012400Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'de_DE')
+;
+
+
+
+-- Element: null
+-- 2023-07-19T11:12:05.732317100Z
+UPDATE AD_Element_Trl SET WEBUI_NameBrowse='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:12:05.732','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='de_CH'
+;
+
+-- 2023-07-19T11:12:05.736316800Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'de_CH')
+;
+
+-- Element: null
+-- 2023-07-19T11:12:08.509135100Z
+UPDATE AD_Element_Trl SET WEBUI_NameBrowse='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:12:08.509','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='de_DE'
+;
+
+-- 2023-07-19T11:12:08.510358900Z
+UPDATE AD_Element SET WEBUI_NameBrowse='Log für modulare Verträge' WHERE AD_Element_ID=582418
+;
+
+-- 2023-07-19T11:12:08.915429600Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(582418,'de_DE')
+;
+
+-- 2023-07-19T11:12:08.916446Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'de_DE')
+;
+
+-- Element: null
+-- 2023-07-19T11:12:14.835031700Z
+UPDATE AD_Element_Trl SET WEBUI_NameBrowse='Modular Contract Log',Updated=TO_TIMESTAMP('2023-07-19 14:12:14.835','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='en_US'
+;
+
+-- 2023-07-19T11:12:14.837029200Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'en_US')
+;
+
+-- Element: null
+-- 2023-07-19T11:12:20.920332800Z
+UPDATE AD_Element_Trl SET WEBUI_NameBrowse='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:12:20.919','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='fr_CH'
+;
+
+-- 2023-07-19T11:12:20.922330700Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'fr_CH')
+;
+
+-- Element: null
+-- 2023-07-19T11:12:26.989489800Z
+UPDATE AD_Element_Trl SET WEBUI_NameBrowse='Log für modulare Verträge',Updated=TO_TIMESTAMP('2023-07-19 14:12:26.989','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=582418 AND AD_Language='it_IT'
+;
+
+-- 2023-07-19T11:12:26.990574100Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582418,'it_IT')
+;
+
