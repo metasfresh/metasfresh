e4f0eaf312925287fef6fcb35e46a6c9da5205cf
cp-ps <no-email@email.com>
2023-07-24 13:55:58 +0300

    tryout 9

--- backend/de.metas.async/src/main/sql/postgresql/system/80-async-dlm/5624381_sys_gh12591_migrate_c_queue_package_processor_ID.sql
+++ backend/de.metas.async/src/main/sql/postgresql/system/80-async-dlm/5624381_sys_gh12591_migrate_c_queue_package_processor_ID.sql
@@ -59,12 +59,14 @@ WHERE c_queue_workpackage_id IN (SELECT c_queue_workpackage_id FROM backup.c_que
 update c_queue_workpackage
 set c_queue_packageprocessor_id = block.c_queue_packageprocessor_id, updatedBy=99, updated=TO_TIMESTAMP('2022-02-04 12:16:30', 'YYYY-MM-DD HH24:MI:SS')
 from c_queue_block block
+    inner join c_queue_packageprocessor packageProcessor on packageProcessor.c_queue_packageprocessor_id = block.c_queue_packageprocessor_id
 where block.c_queue_block_id = c_queue_workpackage.c_queue_block_id
 ;
 
 update dlm.c_queue_workpackage_archived
 set c_queue_packageprocessor_id = block.c_queue_packageprocessor_id, updatedBy=99, updated=TO_TIMESTAMP('2022-02-04 12:16:30', 'YYYY-MM-DD HH24:MI:SS')
 from c_queue_block block
+         inner join c_queue_packageprocessor packageProcessor on packageProcessor.c_queue_packageprocessor_id = block.c_queue_packageprocessor_id
 where block.c_queue_block_id = dlm.c_queue_workpackage_archived.c_queue_block_id
 ;
 
@@ -74,6 +76,9 @@ create index c_queue_block_archived_c_queue_id on dlm.c_queue_block_archived (c_
 
 
 UPDATE dlm.c_queue_workpackage_archived wp
-SET c_queue_packageprocessor_id=(SELECT b.c_queue_packageprocessor_id FROM dlm.c_queue_block_archived b WHERE b.c_queue_block_id = wp.c_queue_block_id)
+SET c_queue_packageprocessor_id=(SELECT b.c_queue_packageprocessor_id
+                                 FROM dlm.c_queue_block_archived b
+                                          inner join c_queue_packageprocessor packageProcessor on packageProcessor.c_queue_packageprocessor_id = b.c_queue_packageprocessor_id
+                                 WHERE b.c_queue_block_id = wp.c_queue_block_id)
 WHERE c_queue_packageprocessor_id IS NULL
 ;
