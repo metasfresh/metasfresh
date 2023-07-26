5d83216cb0ac32d73a9f2acd477682a20c7f39a3
cp-ps <no-email@email.com>
2023-07-26 07:18:03 +0300

    Revert "tryout 3 (#15929)"
    This reverts commit 25bcfdf602e16e3acd5ff7568609eee73a54bc31.

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5606340_sys_cleanup_x_mrp_tables_and_related_process.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5606340_sys_cleanup_x_mrp_tables_and_related_process.sql
@@ -1,35 +1,35 @@
--- DELETE
--- FROM ad_table
--- WHERE tablename = 'X_MRP_ProductInfo_Detail_MV'
--- ;
---
--- DELETE
--- FROM ad_user_sortpref_line
--- WHERE ad_user_sortpref_hdr_id in (SELECT ad_user_sortpref_hdr_id FROM ad_user_sortpref_hdr WHERE ad_infowindow_id in (SELECT ad_infowindow_id FROM ad_infowindow WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V')))
--- ;
---
--- DELETE
--- FROM ad_user_sortpref_hdr
--- WHERE ad_infowindow_id in (SELECT ad_infowindow_id FROM ad_infowindow WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V'))
--- ;
---
--- DELETE
--- FROM ad_infowindow
--- WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V')
--- ;
---
--- DELETE
--- FROM ad_table
--- WHERE tablename = 'X_MRP_ProductInfo_V'
--- ;
---
--- DELETE from ad_table_process where ad_process_id = (
---     SELECT ad_process_id
---     FROM ad_process
---     WHERE value = 'Fresh_QtyOnHand_UpdateSeqNo_And_Export_SortPref')
--- ;
---
--- DELETE
--- FROM ad_process
--- WHERE value = 'Fresh_QtyOnHand_UpdateSeqNo_And_Export_SortPref'
--- ;
+DELETE
+FROM ad_table
+WHERE tablename = 'X_MRP_ProductInfo_Detail_MV'
+;
+
+DELETE
+FROM ad_user_sortpref_line
+WHERE ad_user_sortpref_hdr_id in (SELECT ad_user_sortpref_hdr_id FROM ad_user_sortpref_hdr WHERE ad_infowindow_id in (SELECT ad_infowindow_id FROM ad_infowindow WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V')))
+;
+
+DELETE
+FROM ad_user_sortpref_hdr
+WHERE ad_infowindow_id in (SELECT ad_infowindow_id FROM ad_infowindow WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V'))
+;
+
+DELETE
+FROM ad_infowindow
+WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V')
+;
+
+DELETE
+FROM ad_table
+WHERE tablename = 'X_MRP_ProductInfo_V'
+;
+
+DELETE from ad_table_process where ad_process_id = (
+    SELECT ad_process_id
+    FROM ad_process
+    WHERE value = 'Fresh_QtyOnHand_UpdateSeqNo_And_Export_SortPref')
+;
+
+DELETE
+FROM ad_process
+WHERE value = 'Fresh_QtyOnHand_UpdateSeqNo_And_Export_SortPref'
+;
