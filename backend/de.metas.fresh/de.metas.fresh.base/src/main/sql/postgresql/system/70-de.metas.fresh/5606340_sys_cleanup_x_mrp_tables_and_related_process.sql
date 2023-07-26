a12dac1b9c22ba0ea11a1205946a44a788e00c6e
cp-ps <no-email@email.com>
2023-07-26 07:18:09 +0300

    Revert "trying to fix the build (#15925)"
    This reverts commit 962aace6a527633d25ee147a42f37638b38744b9.

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5606340_sys_cleanup_x_mrp_tables_and_related_process.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5606340_sys_cleanup_x_mrp_tables_and_related_process.sql
@@ -1,29 +1,9 @@
-DELETE
-FROM ad_table
-WHERE tablename = 'X_MRP_ProductInfo_Detail_MV'
-;
 
-DELETE
-FROM ad_user_sortpref_line
-WHERE ad_user_sortpref_hdr_id in (SELECT ad_user_sortpref_hdr_id FROM ad_user_sortpref_hdr WHERE ad_infowindow_id in (SELECT ad_infowindow_id FROM ad_infowindow WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V')))
-;
+delete from ad_table where tablename='X_MRP_ProductInfo_Detail_MV';
 
-DELETE
-FROM ad_user_sortpref_hdr
-WHERE ad_infowindow_id in (SELECT ad_infowindow_id FROM ad_infowindow WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V'))
-;
+delete from ad_user_sortpref_line where ad_user_sortpref_hdr_id=(select ad_user_sortpref_hdr_id from ad_user_sortpref_hdr where ad_infowindow_id=(select ad_infowindow_id from ad_infowindow where ad_table_id=get_table_id('X_MRP_ProductInfo_V')));
+delete from ad_user_sortpref_hdr where ad_infowindow_id=(select ad_infowindow_id from ad_infowindow where ad_table_id=get_table_id('X_MRP_ProductInfo_V'));
+delete from ad_infowindow where ad_table_id=get_table_id('X_MRP_ProductInfo_V');
+delete from ad_table where tablename='X_MRP_ProductInfo_V';
 
-DELETE
-FROM ad_infowindow
-WHERE ad_table_id = get_table_id('X_MRP_ProductInfo_V')
-;
-
-DELETE
-FROM ad_table
-WHERE tablename = 'X_MRP_ProductInfo_V'
-;
-
-DELETE
-FROM ad_process
-WHERE value = 'Fresh_QtyOnHand_UpdateSeqNo_And_Export_SortPref'
-;
+delete from ad_process where value='Fresh_QtyOnHand_UpdateSeqNo_And_Export_SortPref'; 
