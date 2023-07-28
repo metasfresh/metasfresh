4b5b69e9a78a802f3807ba2cd559c5c7ab9f6028
mrie04 <max.rieck@metasfresh.com>
2023-07-28 10:17:27 +0200

    printing client config fixes

--- backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5697070_sys_printing_client_add_config.sql
+++ backend/de.metas.externalsystem/src/main/sql/postgresql/system/80-de.metas.externalsystem/5697070_sys_printing_client_add_config.sql
@@ -3,7 +3,7 @@ INSERT INTO ExternalSystem_Config (AD_Client_ID,AD_Org_ID,AuditFileFolder,Create
 ;
 
 -- 2023-07-26T18:59:34.271Z
-INSERT INTO ExternalSystem_Config_PrintingClient (AD_Client_ID,AD_Org_ID,Created,CreatedBy,ExternalSystem_Config_ID,ExternalSystem_Config_PrintingClient_ID,ExternalSystemValue,IsActive,Target_Directory,Updated,UpdatedBy) VALUES (1000000,1000000,TO_TIMESTAMP('2023-07-26 20:59:34','YYYY-MM-DD HH24:MI:SS'),100,540016,540000,'PrintingClient','Y','\opt\metasfresh\printingclient',TO_TIMESTAMP('2023-07-26 20:59:34','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO ExternalSystem_Config_PrintingClient (AD_Client_ID,AD_Org_ID,Created,CreatedBy,ExternalSystem_Config_ID,ExternalSystem_Config_PrintingClient_ID,ExternalSystemValue,IsActive,Target_Directory,Updated,UpdatedBy) VALUES (1000000,1000000,TO_TIMESTAMP('2023-07-26 20:59:34','YYYY-MM-DD HH24:MI:SS'),100,540016,540000,'PrintingClient','Y','/opt/metasfresh/printingclient',TO_TIMESTAMP('2023-07-26 20:59:34','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 2023-07-26T18:59:54.109Z
