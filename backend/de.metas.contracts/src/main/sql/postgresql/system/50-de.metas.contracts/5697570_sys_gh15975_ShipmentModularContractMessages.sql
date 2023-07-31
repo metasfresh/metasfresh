e075e3e849994f81fbef95a29d3f85a43c95b112
riad.mennour <riad.mennour@gmail.com>
2023-07-31 16:05:16 +0100

    Shipment-> contract module log relation implementation

---
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5697570_sys_gh15975_ShipmentModularContractMessages.sql
@@ -0,0 +1,28 @@
+-- Value: de.metas.contracts.ShipmentCompleted
+-- 2023-07-31T14:58:09.767550200Z
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545311,0,TO_TIMESTAMP('2023-07-31 15:58:09.46','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','Lieferung abgeschlossen','I',TO_TIMESTAMP('2023-07-31 15:58:09.46','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts.ShipmentCompleted')
+;
+
+-- 2023-07-31T14:58:09.772769400Z
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Message_ID=545311 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Value: de.metas.contracts.ShipmentCompleted
+-- 2023-07-31T14:59:08.801745Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Shipment completed',Updated=TO_TIMESTAMP('2023-07-31 15:59:08.801','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545311
+;
+
+-- Value: de.metas.contracts.ShipmentReversed
+-- 2023-07-31T15:00:49.330527200Z
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545312,0,TO_TIMESTAMP('2023-07-31 16:00:49.034','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','Sendung storniert','I',TO_TIMESTAMP('2023-07-31 16:00:49.034','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts.ShipmentReversed')
+;
+
+-- 2023-07-31T15:00:49.331569800Z
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Message_ID=545312 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Value: de.metas.contracts.ShipmentReversed
+-- 2023-07-31T15:01:33.007831800Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Shipment reversed',Updated=TO_TIMESTAMP('2023-07-31 16:01:33.007','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545312
+;
+
