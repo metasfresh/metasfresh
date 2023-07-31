06c9fa4bb601d1bfdbed0f482abb49fd5a4c0fb2
riad.mennour <riad.mennour@gmail.com>
2023-07-31 18:12:53 +0100

    Shipment-> contract module log relation implementation

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5697570_sys_gh15975_ShipmentModularContractMessages.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5697570_sys_gh15975_ShipmentModularContractMessages.sql
@@ -26,3 +26,16 @@ INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTransla
 UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Shipment reversed',Updated=TO_TIMESTAMP('2023-07-31 16:01:33.007','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545312
 ;
 
+-- Value: de.metas.contracts.DocActionUnsupported
+-- 2023-07-31T15:22:15.500397900Z
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545313,0,TO_TIMESTAMP('2023-07-31 16:22:15.142','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','Nicht unterstützte Modellaktion!','E',TO_TIMESTAMP('2023-07-31 16:22:15.142','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts.DocActionUnsupported')
+;
+
+-- 2023-07-31T15:22:15.505163400Z
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Message_ID=545313 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Value: de.metas.contracts.DocActionUnsupported
+-- 2023-07-31T15:22:32.247876Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Unsupported model action!',Updated=TO_TIMESTAMP('2023-07-31 16:22:32.247','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545313
+;
