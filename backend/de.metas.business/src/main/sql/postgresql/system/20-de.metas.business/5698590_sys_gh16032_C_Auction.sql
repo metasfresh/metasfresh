5577d2d9af8b55b8aa2b4b9aeca304546c8ea1b2
dm-ps <dragos.moca@pevesoft.ro>
2023-08-10 11:22:10 +0300

    Sammelstellen/ Verladeorte/ Annahmestellen refs: https://github.com/metasfresh/metasfresh/issues/16032

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5698590_sys_gh16032_C_Auction.sql
@@ -0,0 +1,9 @@
+-- Column: C_Order.C_Auction_ID
+-- 2023-08-09T13:56:30.632206700Z
+UPDATE AD_Column SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2023-08-09 16:56:30.632','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587270
+;
+
+-- 2023-08-09T13:56:32.713640300Z
+INSERT INTO t_alter_column values('c_order','C_Auction_ID','NUMERIC(10)',null,null)
+;
+
