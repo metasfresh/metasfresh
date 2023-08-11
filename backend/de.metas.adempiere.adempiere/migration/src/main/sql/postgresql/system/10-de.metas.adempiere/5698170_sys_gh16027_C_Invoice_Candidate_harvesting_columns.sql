0b7f173e6c6521666523debae698f63897610377
fp-ps <fp-ps@pevesoft.com>
2023-08-11 15:52:12 +0300

    small adjustments

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5698170_sys_gh16027_C_Invoice_Candidate_harvesting_columns.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5698170_sys_gh16027_C_Invoice_Candidate_harvesting_columns.sql
@@ -238,30 +238,6 @@ UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=330,Updated=TO_
 
 
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.migration-sql
- * %%
- * Copyright (C) 2023 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
-
-
 -- Column: C_Invoice_Candidate.C_Harvesting_Calendar_ID
 -- 2023-08-08T10:11:37.441345800Z
 UPDATE AD_Column SET FilterOperator='E',Updated=TO_TIMESTAMP('2023-08-08 13:11:37.441','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587248
--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5698220_sys_gh16027_C_Invoice_harvesting_columns.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5698220_sys_gh16027_C_Invoice_harvesting_columns.sql
@@ -92,31 +92,21 @@ UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=90,Updated=TO_T
 UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=100,Updated=TO_TIMESTAMP('2023-08-08 13:09:04.882','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587251
 ;
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.migration-sql
- * %%
- * Copyright (C) 2023 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- Column: C_Invoice.M_Warehouse_ID
 -- 2023-08-08T10:09:05.369343900Z
 UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=110,Updated=TO_TIMESTAMP('2023-08-08 13:09:05.369','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=571520
 ;
+
+
+-- Column: C_Invoice.M_Warehouse_ID
+-- 2023-08-11T12:48:57.561321400Z
+UPDATE AD_Column SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2023-08-11 15:48:57.561','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=571520
+;
+
+-- 2023-08-11T12:49:00.604617600Z
+INSERT INTO t_alter_column values('c_invoice','M_Warehouse_ID','NUMERIC(10)',null,null)
+;
+
+
 
 
