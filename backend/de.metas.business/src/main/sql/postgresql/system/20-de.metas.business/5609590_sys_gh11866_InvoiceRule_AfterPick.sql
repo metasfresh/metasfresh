e6ea50f8713d3eb3e84eb4db6355bdb47af9739b
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2023-08-21 07:43:45 +0200

    Apply suggestions from code review

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5609590_sys_gh11866_InvoiceRule_AfterPick.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5609590_sys_gh11866_InvoiceRule_AfterPick.sql
@@ -452,28 +452,6 @@ UPDATE AD_PrintFormatItem pi SET PrintName='Auf Packzettel', Name='Auf Packzette
 UPDATE AD_Tab SET Name='Auf Packzettel', Description=NULL, Help=NULL, CommitWarning = NULL WHERE AD_Element_ID = 580033
 ;
 
-/*
- * #%L
- * de.metas.business
- * %%
- * Copyright (C) 2023 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- 2021-10-19T07:46:51.099Z
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_WINDOW SET Name='Auf Packzettel', Description=NULL, Help=NULL WHERE AD_Element_ID = 580033
