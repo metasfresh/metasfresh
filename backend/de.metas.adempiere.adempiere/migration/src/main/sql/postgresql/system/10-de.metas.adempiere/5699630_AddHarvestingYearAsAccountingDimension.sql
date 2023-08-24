afe1d4fbd0177f0d7c92f1f7750b0a9cdb8682eb
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-08-24 11:27:56 +0300

    Do not validate year in account element

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5699630_AddHarvestingYearAsAccountingDimension.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5699630_AddHarvestingYearAsAccountingDimension.sql
@@ -963,3 +963,8 @@ UPDATE AD_Column SET IsAutoApplyValidationRule='N',Updated=TO_TIMESTAMP('2023-08
 UPDATE AD_Column SET IsAutoApplyValidationRule='N',Updated=TO_TIMESTAMP('2023-08-23 15:01:21.946','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587311
 ;
 
+-- Column: C_AcctSchema_Element.Harvesting_Year_ID
+-- 2023-08-24T08:23:34.946097600Z
+UPDATE AD_Column SET AD_Val_Rule_ID=NULL,Updated=TO_TIMESTAMP('2023-08-24 11:23:34.946','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587304
+;
+
