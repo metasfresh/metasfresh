d52a0272ce59d8c27e87665002cde9804094b419
Ispas Mihai <mihai.ispas@pevesoft.ro>
2023-08-31 12:17:45 +0300

    after review updates

--- backend/de.metas.business/src/main/sql/postgresql/ddl/functions/C_BPartner_Adv_Search_update.sql
+++ backend/de.metas.business/src/main/sql/postgresql/ddl/functions/C_BPartner_Adv_Search_update.sql
@@ -1,23 +1,60 @@
 DROP FUNCTION IF EXISTS C_BPartner_Adv_Search_Update()
 ;
 
+CREATE FUNCTION c_bpartner_adv_search_update() RETURNS void
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    rowcount numeric;
+BEGIN
+    DELETE FROM c_bpartner_adv_search WHERE TRUE;
+    GET DIAGNOSTICS rowcount = ROW_COUNT;
+    RAISE NOTICE 'Removed % rows', rowcount;
+
+    INSERT INTO c_bpartner_adv_search (ad_client_id, ad_org_id, address1, c_bp_contact_id, c_bpartner_id, c_bpartner_location_id, city, created, createdby, es_documentid, firstname, isactive, iscompany, lastname, name, companyname, postal, updated, updatedby, value, externalid)
+    SELECT v.ad_client_id,
+           v.ad_org_id,
+           v.address1,
+           v.c_bp_contact_id,
+           v.c_bpartner_id,
+           v.c_bpartner_location_id,
+           v.city,
+           v.created,
+           v.createdby,
+           v.es_documentid,
+           v.firstname,
+           v.isactive,
+           v.iscompany,
+           v.lastname,
+           v.name,
+           v.companyname,
+           v.postal,
+           v.updated,
+           v.updatedby,
+           v.value,
+           v.externalid
+    FROM c_bpartner_adv_search_v v;
+    GET DIAGNOSTICS rowcount = ROW_COUNT;
+    RAISE NOTICE 'Inserted % rows', rowcount;
+END;
+$$
+;
 
 DROP FUNCTION IF EXISTS C_BPartner_Adv_Search_Update(
     p_C_BPartner_IDs numeric[]
 )
 ;
 
-
-CREATE OR REPLACE FUNCTION C_BPartner_Adv_Search_Update(
-    p_C_BPartner_IDs numeric[]
-)
-    RETURNS table
+CREATE FUNCTION c_bpartner_adv_search_update(p_c_bpartner_ids numeric[])
+    RETURNS TABLE
             (
-                operation     char(1),
-                es_documentid varchar(255)
+                operation     character,
+                es_documentid character varying
             )
+    LANGUAGE plpgsql
 AS
-$BODY$
+$$
 DECLARE
     v_transactionId        text;
     rowcount               integer;
@@ -38,8 +75,7 @@ BEGIN
         DELETE FROM C_BPartner_Adv_Search t
             WHERE
                 (p_C_BPartner_IDs IS NULL OR t.C_BPartner_ID = ANY (p_C_BPartner_IDs))
-            RETURNING t.es_documentid
-    )
+            RETURNING t.es_documentid)
     SELECT ARRAY_AGG(deleted_rows.es_documentid)
     INTO v_deleted_documentIds
     FROM deleted_rows;
@@ -47,7 +83,7 @@ BEGIN
     --
     -- Insert new rows
     WITH inserted_rows AS (
-        INSERT INTO c_bpartner_adv_search (ad_client_id, ad_org_id, address1, c_bp_contact_id, c_bpartner_id, c_bpartner_location_id, city, created, createdby, es_documentid, firstname, isactive, iscompany, lastname, name, postal, updated, updatedby, value)
+        INSERT INTO c_bpartner_adv_search (ad_client_id, ad_org_id, address1, c_bp_contact_id, c_bpartner_id, c_bpartner_location_id, city, created, createdby, es_documentid, firstname, isactive, iscompany, lastname, name, companyname, postal, updated, updatedby, value, externalid)
             SELECT v.ad_client_id,
                    v.ad_org_id,
                    v.address1,
@@ -63,14 +99,15 @@ BEGIN
                    v.iscompany,
                    v.lastname,
                    v.name,
+                   v.companyname,
                    v.postal,
                    v.updated,
                    v.updatedby,
-                   v.value
+                   v.value,
+                   v.externalid
             FROM c_bpartner_adv_search_v v
             WHERE (p_C_BPartner_IDs IS NULL OR v.C_BPartner_ID = ANY (p_C_BPartner_IDs))
-            RETURNING c_bpartner_adv_search.es_documentid
-    )
+            RETURNING c_bpartner_adv_search.es_documentid)
     SELECT ARRAY_AGG(inserted_rows.es_documentid)
     INTO v_inserted_documentIds
     FROM inserted_rows;
@@ -92,51 +129,6 @@ BEGIN
                t.t          AS es_documentid
         FROM UNNEST(v_inserted_documentIds) t;
 END;
-$BODY$
-    LANGUAGE plpgsql
-    VOLATILE
-    COST 100
-;
-
-
-CREATE OR REPLACE FUNCTION C_BPartner_Adv_Search_Update()
-    RETURNS void
-AS
-$BODY$
-DECLARE
-    rowcount numeric;
-BEGIN
-    DELETE FROM c_bpartner_adv_search WHERE 1 = 1;
-    GET DIAGNOSTICS rowcount = ROW_COUNT;
-    RAISE NOTICE 'Removed % rows', rowcount;
-
-    INSERT INTO c_bpartner_adv_search (ad_client_id, ad_org_id, address1, c_bp_contact_id, c_bpartner_id, c_bpartner_location_id, city, created, createdby, es_documentid, firstname, isactive, iscompany, lastname, name, postal, updated, updatedby, value)
-    SELECT v.ad_client_id,
-           v.ad_org_id,
-           v.address1,
-           v.c_bp_contact_id,
-           v.c_bpartner_id,
-           v.c_bpartner_location_id,
-           v.city,
-           v.created,
-           v.createdby,
-           v.es_documentid,
-           v.firstname,
-           v.isactive,
-           v.iscompany,
-           v.lastname,
-           v.name,
-           v.postal,
-           v.updated,
-           v.updatedby,
-           v.value
-    FROM c_bpartner_adv_search_v v;
-    GET DIAGNOSTICS rowcount = ROW_COUNT;
-    RAISE NOTICE 'Inserted % rows', rowcount;
-END;
-$BODY$
-    LANGUAGE plpgsql
-    VOLATILE
-    COST 100
+$$
 ;
 
