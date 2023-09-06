f0eb202d3562fa2747c46f94d8e7223005f82fa3
riad.mennour <riad.mennour@gmail.com>
2023-09-06 14:17:46 +0100

    Update the de_metas_endcustomer_fresh_reports.hasFreeEUTaxes Function

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5701710_sys_gh16164_hasEUTaxes.sql
@@ -0,0 +1,29 @@
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.hasFreeEUTaxes(numeric) CASCADE
+;
+
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.hasFreeEUTaxes(IN p_C_Invoice_ID numeric)
+    RETURNS char(1)
+AS
+$BODY$
+DECLARE
+
+    hasEUTax char(1);
+
+BEGIN
+
+    hasEUTax := (SELECT CASE WHEN (COUNT(1) > 0) THEN 'Y' ELSE 'N' END
+                 FROM c_invoiceline il
+                          JOIN C_Tax t ON il.c_tax_id = t.c_tax_id
+                 WHERE t.TypeOfDestCountry = 'WITHIN_COUNTRY_AREA'
+                   AND t.isTaxexempt = 'Y'
+                   AND il.c_invoice_id = p_C_Invoice_ID);
+
+    IF hasEUTax IS NOT NULL THEN
+        RETURN hasEUTax;
+    ELSE
+        RETURN 'N';
+    END IF;
+END;
+$BODY$
+    LANGUAGE plpgsql
+;
