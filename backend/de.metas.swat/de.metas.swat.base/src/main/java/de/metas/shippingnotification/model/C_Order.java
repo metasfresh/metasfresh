c72d00047d4059cbbfa0cd2ebdc076d58a935fc6
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-09-13 16:55:34 +0300

    Reverse shipping notifications when voiding order

---
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/shippingnotification/model/C_Order.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * de.metas.swat.base
+ * %%
+ * Copyright (C) 2022 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.shippingnotification.model;
+
+import de.metas.order.OrderId;
+import de.metas.shippingnotification.ShippingNotificationService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.adempiere.ad.modelvalidator.annotations.DocValidate;
+import org.adempiere.ad.modelvalidator.annotations.Interceptor;
+import org.compiere.model.I_C_Order;
+import org.compiere.model.ModelValidator;
+import org.springframework.stereotype.Component;
+
+@Interceptor(I_C_Order.class)
+@Component
+@RequiredArgsConstructor
+public class C_Order
+{
+
+	private final ShippingNotificationService shippingNotificationService;
+
+	@DocValidate(timings = ModelValidator.TIMING_AFTER_VOID)
+	public void beforeSave_updateRenderedAddressesAndCapturedLocations(@NonNull final I_C_Order order)
+	{
+		shippingNotificationService.reverseIfExistsShippingNotifications(OrderId.ofRepoId(order.getC_Order_ID()));
+	}
+}
