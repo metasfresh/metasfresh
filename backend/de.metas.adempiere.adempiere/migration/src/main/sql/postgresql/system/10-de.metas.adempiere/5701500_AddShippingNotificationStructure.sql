b8f6b916b0909cf6857da18e0d27c6600433dfcc
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-09-14 09:01:07 +0300

    Do not copy the date

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5701500_AddShippingNotificationStructure.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5701500_AddShippingNotificationStructure.sql
@@ -3870,3 +3870,8 @@ INSERT INTO t_alter_column values('m_shipping_notification','BPartnerAddress','T
 INSERT INTO t_alter_column values('m_shipping_notification','BPartnerAddress',null,'NULL',null)
 ;
 
+-- Column: C_Order.PhysicalClearanceDate
+-- 2023-09-14T06:00:46.688469300Z
+UPDATE AD_Column SET IsCalculated='Y',Updated=TO_TIMESTAMP('2023-09-14 09:00:46.687','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587455
+;
+
