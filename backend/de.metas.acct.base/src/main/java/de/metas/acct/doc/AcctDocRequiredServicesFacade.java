8e26b0722dc2c3d04a84729f99771e760a25b9e7
Teo Sarca <teo.sarca@metasfresh.com>
2023-09-18 12:01:39 +0300

    QA/cleanup

--- backend/de.metas.acct.base/src/main/java/de/metas/acct/doc/AcctDocRequiredServicesFacade.java
+++ backend/de.metas.acct.base/src/main/java/de/metas/acct/doc/AcctDocRequiredServicesFacade.java
@@ -72,7 +72,6 @@ import de.metas.uom.UomId;
 import de.metas.util.Services;
 import lombok.Getter;
 import lombok.NonNull;
-import org.adempiere.acct.api.IFactAcctBL;
 import org.adempiere.ad.trx.api.ITrxManager;
 import org.adempiere.service.ClientId;
 import org.adempiere.service.ISysConfigBL;
