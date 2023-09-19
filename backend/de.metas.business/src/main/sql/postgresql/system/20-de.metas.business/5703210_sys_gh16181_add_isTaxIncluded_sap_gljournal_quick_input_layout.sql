7f37b679fa996c3e5137ee03fa1e06916df311e1
Chems Nouioua <chems.nouioua@gmail.com>
2023-09-19 12:25:15 +0100

    adjust quick input layout

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703210_sys_gh16181_add_isTaxIncluded_sap_gljournal_quick_input_layout.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703210_sys_gh16181_add_isTaxIncluded_sap_gljournal_quick_input_layout.sql
@@ -1,6 +1,6 @@
 -- Add the IsTaxIncluded to the QuickInput
 UPDATE ad_sysconfig
-SET value     = 'PostingSign,GL_Account_ID,Amount,M_SectionCode_ID,C_Tax_ID?,IsTaxIncluded?'
+SET value     = 'PostingSign,GL_Account_ID,Amount,M_SectionCode_ID,IsTaxIncluded?,C_Tax_ID?'
   , updated   = NOW()
   , updatedby = 99
 WHERE name = 'SAP_GLJournalLine.quickInput.layout'
