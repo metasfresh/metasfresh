73f85bc1c3c08ef80ea487fd4e56fc2c1a9af7e0
Chems Nouioua <chems.nouioua@gmail.com>
2023-09-20 20:16:05 +0100

    code review fixes: use set_sysconfig_value function

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703210_sys_gh16181_add_isTaxIncluded_sap_gljournal_quick_input_layout.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703210_sys_gh16181_add_isTaxIncluded_sap_gljournal_quick_input_layout.sql
@@ -1,7 +1,3 @@
 -- Add the IsTaxIncluded to the QuickInput
-UPDATE ad_sysconfig
-SET value     = 'PostingSign,GL_Account_ID,Amount,M_SectionCode_ID,IsTaxIncluded?,C_Tax_ID?'
-  , updated   = NOW()
-  , updatedby = 99
-WHERE name = 'SAP_GLJournalLine.quickInput.layout'
+SELECT set_sysconfig_value('SAP_GLJournalLine.quickInput.layout', 'PostingSign,GL_Account_ID,Amount,M_SectionCode_ID,IsTaxIncluded?,C_Tax_ID?')
 ;
