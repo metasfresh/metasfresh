a3955c8aa0b48f1dab43b1437a15369442779d2b
riad.mennour <riad.mennour@gmail.com>
2023-09-20 13:57:18 +0100

    Shipment -> Modular Contract Log (Sales)

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Shipping_Notification_Root.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Shipping_Notification_Root.sql
@@ -37,7 +37,6 @@ CREATE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Shipping_Notification_Ro
                 printname                            varchar(60),
                 ad_language                          text,
                 ishidepackingmaterialinshipmentprint char,
-                email                                varchar(50),
                 issourcesupplycert                   char
             )
     STABLE
@@ -57,23 +56,11 @@ SELECT sn.ad_user_id,
        END                                                             AS printname,
        CASE WHEN sn.DocStatus IN ('DR', 'IP') THEN 'de_CH' ELSE $2 END AS AD_Language,
        bp.isHidePackingMaterialInShipmentPrint,
-       mb.email,
        bp.issourcesupplycert
 FROM M_Shipping_Notification sn
          JOIN C_BPartner bp ON sn.C_BPartner_ID = bp.C_BPartner_ID
          INNER JOIN C_DocType dt ON sn.C_DocType_ID = dt.C_DocType_ID
          LEFT OUTER JOIN C_DocType_Trl dtt ON dt.C_DocType_ID = dtt.C_DocType_ID AND dtt.AD_Language = $2 AND dtt.isActive = 'Y'
-    -- get the email from AD_MailConfig for org, docbasetype and docsubtype, with fallback to org, docbasetype
-         LEFT OUTER JOIN (SELECT mb.email, sne.M_Shipping_Notification_ID
-                          FROM M_Shipping_Notification sne
-                                   INNER JOIN C_DocType dt ON sne.C_DocType_ID = dt.C_DocType_ID
-                                   LEFT OUTER JOIN AD_MailConfig mc1 ON sne.AD_Org_ID = mc1.AD_Org_ID AND mc1.DocBaseType = dt.DocBaseType AND mc1.DocSubType = dt.DocSubType
-                                   LEFT OUTER JOIN AD_MailConfig mc2 ON sne.AD_Org_ID = mc2.AD_Org_ID AND mc2.DocBaseType = dt.DocBaseType AND mc2.DocSubType IS NULL
-                                   LEFT OUTER JOIN AD_MailConfig mc3 ON sne.AD_Org_ID = mc3.AD_Org_ID AND mc3.DocBaseType IS NULL AND mc3.DocSubType IS NULL
-                                   LEFT OUTER JOIN AD_Mailbox mb ON COALESCE(COALESCE(mc1.AD_Mailbox_ID, mc2.AD_Mailbox_ID), mc3.AD_Mailbox_ID) = mb.AD_Mailbox_ID
-
-                          WHERE sne.M_Shipping_Notification_ID = $1
-                          LIMIT 1) mb ON mb.M_Shipping_Notification_ID = sn.M_Shipping_Notification_ID
 
 WHERE sn.M_Shipping_Notification_ID = $1
   AND sn.isActive = 'Y'
