5927b04055d3ca9ad9ee8326685616f032143ac1
Chems NOUIOUA <chems.nouioua@gmail.com>
2023-09-21 18:47:36 +0100

    #16181 : fix IsIncludedTax translation (#16268)

---
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703640_sys_gh16181_fix_isIncludedTax_translation.sql
@@ -0,0 +1,42 @@
+-- Run mode: SWING_CLIENT
+
+-- Element: IsTaxIncluded
+-- 2023-09-21T15:27:44.937Z
+UPDATE AD_Element_Trl SET Description_Customized='Steuern inklusive', IsUseCustomization='Y', Name_Customized='Steuern inklusive',Updated=TO_TIMESTAMP('2023-09-21 16:27:44.937','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1065 AND AD_Language='de_DE'
+;
+
+-- 2023-09-21T15:27:44.952Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(1065,'de_DE')
+;
+
+-- 2023-09-21T15:27:44.958Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1065,'de_DE')
+;
+
+-- Element: IsTaxIncluded
+-- 2023-09-21T16:38:35.178Z
+UPDATE AD_Element_Trl SET Description_Customized='Tax Included', IsUseCustomization='Y', Name_Customized='Tax Included',Updated=TO_TIMESTAMP('2023-09-21 17:38:35.177','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1065 AND AD_Language='en_US'
+;
+
+-- 2023-09-21T16:38:35.193Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1065,'en_US')
+;
+
+-- Element: IsTaxIncluded
+-- 2023-09-21T16:39:21.755Z
+UPDATE AD_Element_Trl SET Description_Customized='Tax Included', IsUseCustomization='Y', Name_Customized='Tax Included',Updated=TO_TIMESTAMP('2023-09-21 17:39:21.755','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1065 AND AD_Language='en_GB'
+;
+
+-- 2023-09-21T16:39:21.757Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1065,'en_GB')
+;
+
+-- Element: IsTaxIncluded
+-- 2023-09-21T16:39:37.378Z
+UPDATE AD_Element_Trl SET Description_Customized='Steuern inklusive', IsUseCustomization='Y', Name_Customized='Steuern inklusive',Updated=TO_TIMESTAMP('2023-09-21 17:39:37.378','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1065 AND AD_Language='de_CH'
+;
+
+-- 2023-09-21T16:39:37.382Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1065,'de_CH')
+;
+
