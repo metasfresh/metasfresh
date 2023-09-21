ce4440a46b34060253d549922cba2e8b9a7a64f7
Chems Nouioua <chems.nouioua@gmail.com>
2023-09-21 11:02:30 +0100

    code review fixes: prevent changing metasfresh layout

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703210_sys_gh16181_add_isTaxIncluded_sap_gljournal_quick_input_layout.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703210_sys_gh16181_add_isTaxIncluded_sap_gljournal_quick_input_layout.sql
@@ -1,3 +1,3 @@
 -- Add the IsTaxIncluded to the QuickInput
-SELECT set_sysconfig_value('SAP_GLJournalLine.quickInput.layout', 'PostingSign,GL_Account_ID,Amount,M_SectionCode_ID,IsTaxIncluded?,C_Tax_ID?')
-;
+--SELECT set_sysconfig_value('SAP_GLJournalLine.quickInput.layout', 'PostingSign,GL_Account_ID,Amount,M_SectionCode_ID,IsTaxIncluded?,C_Tax_ID?')
+--;
