e869cadc3e3cf34d5d2738317099a08e474b3e89
Chems Nouioua <chems.nouioua@gmail.com>
2023-09-21 19:49:58 +0100

    #16181 : fix IsIncludedTax translation

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703640_sys_gh16181_fix_isIncludedTax_translation.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5703640_sys_gh16181_fix_isIncludedTax_translation.sql
@@ -40,3 +40,27 @@ UPDATE AD_Element_Trl SET Description_Customized='Steuern inklusive', IsUseCusto
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1065,'de_CH')
 ;
 
+-- Run mode: SWING_CLIENT
+
+-- Field: GL Journal (SAP)(541656,de.metas.acct) -> GL Journal Line (SAP)(546731,de.metas.acct) -> Steuern inklusive
+-- Column: SAP_GLJournalLine.IsTaxIncluded
+-- 2023-09-21T18:40:52.138Z
+UPDATE AD_Field SET AD_Name_ID=NULL, Description='Steuern inklusive', Help=NULL, Name='Steuern inklusive',Updated=TO_TIMESTAMP('2023-09-21 19:40:52.138','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=720484
+;
+
+-- 2023-09-21T18:40:52.142Z
+UPDATE AD_Field_Trl trl SET Description='Steuern inklusive',Name='Steuern inklusive' WHERE AD_Field_ID=720484 AND AD_Language='de_DE'
+;
+
+-- 2023-09-21T18:40:52.161Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(1065)
+;
+
+-- 2023-09-21T18:40:52.185Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=720484
+;
+
+-- 2023-09-21T18:40:52.190Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(720484)
+;
+
