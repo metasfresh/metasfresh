96374053480304114b59ac300d92a85521335c95
riad.mennour <riad.mennour@gmail.com>
2023-10-11 08:52:44 +0100

    Products Proposal modal update
    (cherry picked from commit d7def7138da00ceb3733f21e98d2d46fd80237e5)

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5705850_sys_gh16430_Missing_Base_PLV_Msg.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5705850_sys_gh16430_Missing_Base_PLV_Msg.sql
@@ -30,26 +30,16 @@ UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Keine Basispreisliste für 
 =======
 >>>>>>> 62861f561ce (Products Proposal modal update)
 -- 2023-10-10T18:05:56.608Z
-UPDATE AD_Message_Trl SET MsgText='Preisliste hat keine Basis Preisliste',Updated=TO_TIMESTAMP('2023-10-10 19:05:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545352
-;
-
--- Value: WEBUI_Missing_Base_PriceList_Version
--- 2023-10-10T18:06:03.702Z
-UPDATE AD_Message SET MsgText='Preisliste hat keine Basis Preisliste',Updated=TO_TIMESTAMP('2023-10-10 19:06:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Message_ID=545352
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Preisliste hat keine Basis Preisliste',Updated=TO_TIMESTAMP('2023-10-10 19:05:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545352
 ;
 
 -- Value: WEBUI_Missing_Base_PriceList_Version
 -- 2023-10-10T18:06:47.440Z
-UPDATE AD_Message_Trl SET MsgText='Pricelist has no Base Pricelist',Updated=TO_TIMESTAMP('2023-10-10 19:06:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545352
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Preisliste hat keine Basis Preisliste',Updated=TO_TIMESTAMP('2023-10-10 19:06:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545352
 ;
 
 -- Value: WEBUI_Missing_Base_PriceList_Version
 -- 2023-10-10T18:06:53.914Z
 UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Pricelist has no Base Pricelist',Updated=TO_TIMESTAMP('2023-10-10 19:06:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545352
-;
-
--- Value: WEBUI_Missing_Base_PriceList_Version
--- 2023-10-10T18:06:59.408Z
-UPDATE AD_Message_Trl SET MsgText='Pricelist has no Base Pricelist',Updated=TO_TIMESTAMP('2023-10-10 19:06:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Message_ID=545352
 ;
 
