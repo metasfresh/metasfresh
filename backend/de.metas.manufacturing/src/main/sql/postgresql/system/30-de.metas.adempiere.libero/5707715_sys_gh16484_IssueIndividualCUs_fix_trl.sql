43ac33f56593547a40fa75de4ca012f4ebadb698
cp-ps <cp-ps@pevesoft.ro>
2023-10-19 19:36:56 +0300

    fix trls, ref https://github.com/metasfresh/metasfresh/issues/16484

---
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5707715_sys_gh16484_IssueIndividualCUs_fix_trl.sql
@@ -0,0 +1,29 @@
+
+
+-- 2023-10-19T16:34:18.356Z
+UPDATE AD_Element_Trl SET Name='CUs zählen', PrintName='CUs zählen',Updated=TO_TIMESTAMP('2023-10-19 19:34:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=582770 AND AD_Language='de_CH'
+;
+
+-- 2023-10-19T16:34:18.529Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582770,'de_CH')
+;
+
+-- 2023-10-19T16:34:24.679Z
+UPDATE AD_Element_Trl SET Name='CUs zählen', PrintName='CUs zählen',Updated=TO_TIMESTAMP('2023-10-19 19:34:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=582770 AND AD_Language='de_DE'
+;
+
+-- 2023-10-19T16:34:24.683Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(582770,'de_DE')
+;
+
+-- 2023-10-19T16:34:24.704Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582770,'de_DE')
+;
+
+-- 2023-10-19T16:34:43.097Z
+UPDATE AD_Element_Trl SET Name='CUs zählen', PrintName='CUs zählen',Updated=TO_TIMESTAMP('2023-10-19 19:34:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=582770 AND AD_Language='nl_NL'
+;
+
+-- 2023-10-19T16:34:43.099Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(582770,'nl_NL')
+;
