0e016d01cbe6550ff45c6c83d0ca9b51d1a29da3
cp-ps <77785468+cp-ps@users.noreply.github.com>
2023-10-19 15:08:25 +0300

    refs Serial Number Production Automation - Implementation #16484 (#16485)
    * refs Serial Number Production Automation - Implementation #16484
    https://github.com/metasfresh/metasfresh/issues/16484

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/pporder/process/WEBUI_PP_Order_Receipt.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/pporder/process/WEBUI_PP_Order_Receipt.java
@@ -24,7 +24,6 @@ package de.metas.ui.web.pporder.process;
 
 import de.metas.handlingunits.attribute.IHUAttributesBL;
 import de.metas.handlingunits.impl.IDocumentLUTUConfigurationManager;
-import de.metas.handlingunits.model.I_M_HU_LUTU_Configuration;
 import de.metas.handlingunits.model.I_M_HU_PI_Item;
 import de.metas.handlingunits.model.I_M_HU_PI_Item_Product;
 import de.metas.handlingunits.model.I_PP_Order;
@@ -39,6 +38,7 @@ import de.metas.process.Param;
 import de.metas.process.ProcessPreconditionsResolution;
 import de.metas.process.RunOutOfTrx;
 import de.metas.product.IProductDAO;
+import de.metas.quantity.Quantity;
 import de.metas.ui.web.pporder.PPOrderLineRow;
 import de.metas.ui.web.pporder.PPOrderLineType;
 import de.metas.ui.web.pporder.PPOrderLinesView;
@@ -82,6 +82,12 @@ public class WEBUI_PP_Order_Receipt
 	@Param(parameterName = PackingInfoProcessParams.PARAM_QtyLU)
 	private BigDecimal p_QtyLU;
 
+	@Param(parameterName = "NumberOfCUs")
+	private int p_NumberOfCUs;
+
+	@Param(parameterName = PackingInfoProcessParams.PARAM_IsReceiveIndividualCUs)
+	private boolean isReceiveIndividualCUs;
+
 	private transient PackingInfoProcessParams _packingInfoParams;
 
 	/**
@@ -94,6 +100,7 @@ public class WEBUI_PP_Order_Receipt
 			_packingInfoParams = PackingInfoProcessParams.builder()
 					.defaultLUTUConfigManager(createDefaultLUTUConfigManager(getSingleSelectedRow()))
 					.enforcePhysicalTU(false) // allow to to produce just the CU, without a TU etc..maybe later we'll add a sysconfig for this
+					.selectedRow(getSingleSelectedRow())
 					.build();
 		}
 
@@ -211,15 +218,21 @@ public class WEBUI_PP_Order_Receipt
 
 	@Override
 	@RunOutOfTrx
-	protected String doIt() throws Exception
+	protected String doIt()
 	{
-		// Calculate and set the LU/TU config from packing info params and defaults
-		final I_M_HU_LUTU_Configuration lutuConfig = getPackingInfoParams().createAndSaveNewLUTUConfig();
+		final IPPOrderReceiptHUProducer producer = newReceiptCandidatesProducer()
+				.bestBeforeDate(computeBestBeforeDate());
 
-		newReceiptCandidatesProducer()
-				.packUsingLUTUConfiguration(lutuConfig)
-				.bestBeforeDate(computeBestBeforeDate())
-				.createDraftReceiptCandidatesAndPlanningHUs();
+		if (isReceiveIndividualCUs)
+		{
+			producer.withPPOrderLocatorId()
+					.receiveIndividualPlanningCUs(Quantity.of(p_NumberOfCUs, getSingleSelectedRow().getUomNotNull()));
+		}
+		else
+		{
+			producer.packUsingLUTUConfiguration(getPackingInfoParams().createAndSaveNewLUTUConfig())
+					.createDraftReceiptCandidatesAndPlanningHUs();
+		}
 
 		return MSG_OK;
 	}
