7e74edee44573133001396d36d191a53658cb77c
riad.mennour <riad.mennour@gmail.com>
2023-10-23 11:54:50 +0100

    Include crop year & crop calendar in Purchase Order

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5708260_sys_gh16510_Purchase_Order_Year_Msg.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5708260_sys_gh16510_Purchase_Order_Year_Msg.sql
@@ -2,7 +2,7 @@
 
 -- Value: de.metas.contracts.modular.interceptor.C_Order.HarvestingDetailsChangeNotAllowed_PurchaseOrder
 -- 2023-10-23T09:54:43.027Z
-INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545354,0,TO_TIMESTAMP('2023-10-23 10:54:42.723','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','Das Erntejahr kann nicht geändert werden, wenn der Auftrag Zeilen mit Vertragsbedingungen enthält.','E',TO_TIMESTAMP('2023-10-23 10:54:42.723','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts.modular.interceptor.C_Order.HarvestingDetailsChangeNotAllowed_PurchaseOrder')
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545354,0,TO_TIMESTAMP('2023-10-23 10:54:42.723','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','Das Erntejahr kann nicht geändert werden, da der Auftrag Positionen enthält, die an Vertragsbedingungen gebunden sind.','E',TO_TIMESTAMP('2023-10-23 10:54:42.723','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts.modular.interceptor.C_Order.HarvestingDetailsChangeNotAllowed_PurchaseOrder')
 ;
 
 -- 2023-10-23T09:54:43.035Z
@@ -11,6 +11,6 @@ INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTransla
 
 -- Value: de.metas.contracts.modular.interceptor.C_Order.HarvestingDetailsChangeNotAllowed_PurchaseOrder
 -- 2023-10-23T09:54:59.122Z
-UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='The harvesting year can not be changed when the order has lines with contract terms.',Updated=TO_TIMESTAMP('2023-10-23 10:54:59.122','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545354
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='The crop year cannot be changed because the sales order contains items subject to contract terms.',Updated=TO_TIMESTAMP('2023-10-23 10:54:59.122','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545354
 ;
 
