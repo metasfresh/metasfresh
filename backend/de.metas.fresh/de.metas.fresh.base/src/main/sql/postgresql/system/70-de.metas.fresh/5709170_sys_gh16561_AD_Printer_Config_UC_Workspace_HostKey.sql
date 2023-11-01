b7db8760fa87920c0f316641ceb4e02e9bf79a83
Riad MENNOUR <93593841+riadmen@users.noreply.github.com>
2023-11-01 17:41:27 +0100

    Support the mapping of printers to C_Workplace (#16578)

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5709170_sys_gh16561_AD_Printer_Config_UC_Workspace_HostKey.sql
@@ -0,0 +1,3 @@
+-- 2023-10-29T18:26:58.801Z
+UPDATE AD_Index_Table_Trl SET ErrorMsg='Only one record is permitted per workplace and host key', IsTranslated='Y',Updated=TO_TIMESTAMP('2023-10-29 19:26:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Index_Table_ID=540772 AND AD_Language='en_US'
+;
