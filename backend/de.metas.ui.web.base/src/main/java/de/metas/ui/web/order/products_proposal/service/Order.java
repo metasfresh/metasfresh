2570f0aefbb2746437efd09bf9b24a40f77d676e
metas_rc <ruxandra.craciunescu@metasfresh.com>
2023-11-03 10:20:05 +0200

    cleanup after review

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/order/products_proposal/service/Order.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/order/products_proposal/service/Order.java
@@ -3,21 +3,18 @@ package de.metas.ui.web.order.products_proposal.service;
 import com.google.common.collect.ImmutableList;
 import de.metas.bpartner.BPartnerId;
 import de.metas.currency.Currency;
-import de.metas.handlingunits.HUPIItemProductId;
 import de.metas.lang.SOTrx;
 import de.metas.location.CountryId;
 import de.metas.order.OrderId;
 import de.metas.pricing.PriceListId;
 import de.metas.pricing.PriceListVersionId;
 import de.metas.pricing.PricingSystemId;
-import de.metas.product.ProductId;
 import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
 import javax.annotation.Nullable;
 import java.time.ZonedDateTime;
-import java.util.Optional;
 
 /*
  * #%L
