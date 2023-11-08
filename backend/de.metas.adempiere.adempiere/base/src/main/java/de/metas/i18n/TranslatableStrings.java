36d978e527b83f7133cc457853fec4c5ed98e5ac
Teo Sarca <teo.sarca@metasfresh.com>
2023-11-08 14:35:33 +0200

    ImmutableTranslatableString.ofMap()

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/i18n/TranslatableStrings.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/i18n/TranslatableStrings.java
@@ -283,7 +283,7 @@ public class TranslatableStrings
 		}
 		else
 		{
-			return new ImmutableTranslatableString(trlMap, ConstantTranslatableString.EMPTY.getDefaultValue());
+			return ImmutableTranslatableString.ofMap(trlMap, ConstantTranslatableString.EMPTY.getDefaultValue());
 		}
 	}
 
@@ -295,7 +295,7 @@ public class TranslatableStrings
 		}
 		else
 		{
-			return new ImmutableTranslatableString(trlMap, defaultValue);
+			return ImmutableTranslatableString.ofMap(trlMap, defaultValue);
 		}
 	}
 
