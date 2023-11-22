2df57e1f8fcc2d6473e640be0da016f025312d85
Max Rieck <max.rieck@metasfresh.com>
2023-11-22 18:33:20 +0100

    QA

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/order/products_proposal/process/WEBUI_ProductsProposal_Launcher_Template.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/order/products_proposal/process/WEBUI_ProductsProposal_Launcher_Template.java
@@ -20,12 +20,12 @@ import org.compiere.SpringContextHolder;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -50,9 +50,9 @@ abstract class WEBUI_ProductsProposal_Launcher_Template extends JavaProcess
 		final ViewId viewId = view.getViewId();
 
 		getResult().setWebuiViewToOpen(WebuiViewToOpen.builder()
-				.viewId(viewId.toJson())
-				.target(ViewOpenTarget.ModalOverlay)
-				.build());
+											   .viewId(viewId.toJson())
+											   .target(ViewOpenTarget.ModalOverlay)
+											   .build());
 
 		return MSG_OK;
 	}
