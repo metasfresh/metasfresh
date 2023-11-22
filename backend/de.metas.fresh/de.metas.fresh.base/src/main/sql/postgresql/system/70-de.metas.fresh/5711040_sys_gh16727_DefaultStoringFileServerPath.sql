bb0c9fb4f46170dc05f5b3f9583eb952ad1a9ba6
riad.mennour <riad.mennour@gmail.com>
2023-11-22 10:54:08 +0100

    predefined path where to to store the process-result file

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5711040_sys_gh16727_DefaultStoringFileServerPath.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5711040_sys_gh16727_DefaultStoringFileServerPath.sql
@@ -1,6 +1,7 @@
 -- SysConfig Name: de.metas.process.DefaultStoringFileServerPath
 -- SysConfig Value: /opt/metasfresh/process_created_files/default
 -- 2023-11-20T10:08:46.016170400Z
-INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (1000000,1000000,541655,'O',TO_TIMESTAMP('2023-11-20 11:08:45.997','YYYY-MM-DD HH24:MI:SS.US'),100,'U','Y','de.metas.process.DefaultStoringFileServerPath',TO_TIMESTAMP('2023-11-20 11:08:45.997','YYYY-MM-DD HH24:MI:SS.US'),100,'/opt/metasfresh/process_created_files/default')
+INSERT INTO AD_SysConfig (AD_Client_ID, AD_Org_ID, AD_SysConfig_ID, ConfigurationLevel, Created, CreatedBy, EntityType, IsActive, Name, Updated, UpdatedBy, Value)
+VALUES (0, 0, 541655, 'S', TO_TIMESTAMP('2023-11-20 11:08:45.997', 'YYYY-MM-DD HH24:MI:SS.US'), 100, 'D', 'Y', 'de.metas.process.DefaultStoringFileServerPath', TO_TIMESTAMP('2023-11-20 11:08:45.997', 'YYYY-MM-DD HH24:MI:SS.US'), 100, '/opt/metasfresh/process_created_files/default')
 ;
 
