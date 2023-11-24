ca096174d6211d4d7b4eaef94427fa24d9dcf530
Adrian <adrian.stefan@metasfresh.com>
2023-11-24 10:12:30 +0200

    Add C_DunningDoc to getDocumentPOReference(ad_table_id, record_id)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5711570_sys_dunningDoc_to_getDocumentPOReference.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5711570_sys_dunningDoc_to_getDocumentPOReference.sql
@@ -6,23 +6,21 @@
 CREATE OR REPLACE FUNCTION getDocumentPOReference(ad_table_id numeric, record_id numeric)
   RETURNS character AS
 $BODY$
-	SELECT
-		(CASE
-			WHEN $1=318 -- C_Invoice
-				THEN (
-					SELECT i.POReference
-					FROM C_Invoice i WHERE i.C_Invoice_ID=$2)
-			WHEN $1=319 -- M_InOut
-				THEN (
-					SELECT io.POReference
-					FROM M_InOut io WHERE io.M_InOut_ID=$2)
-			ELSE NULL
-            WHEN $1=540401 -- C_DunningDoc
-            THEN (
-            SELECT dd.POReference
-            FROM C_DunningDoc dd WHERE dd.C_DunningDoc_ID=$2)
-            ELSE NULL
-		END) POReference
+SELECT (CASE
+            WHEN $1 = 318 -- C_Invoice
+                THEN (SELECT i.POReference
+                      FROM C_Invoice i
+                      WHERE i.C_Invoice_ID = $2)
+            WHEN $1 = 319 -- M_InOut
+                THEN (SELECT io.POReference
+                      FROM M_InOut io
+                      WHERE io.M_InOut_ID = $2)
+            WHEN $1 = 540401 -- C_DunningDoc
+                THEN (SELECT dd.POReference
+                      FROM C_DunningDoc dd
+                      WHERE dd.C_DunningDoc_ID = $2)
+                ELSE NULL
+        END) POReference
 
 $BODY$
   LANGUAGE sql VOLATILE
