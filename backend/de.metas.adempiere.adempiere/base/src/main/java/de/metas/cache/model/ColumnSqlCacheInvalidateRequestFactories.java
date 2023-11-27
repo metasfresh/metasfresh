da2f9bbfb1f8345527a795b1d96de10a9a4fa45a
Teo Sarca <teo.sarca@metasfresh.com>
2023-11-27 17:14:41 +0200

    Fix not working cache invalidation on source column changed (#16794)
    * ColumnSqlCacheInvalidateRequestFactories - hotfix how we fetch the target records in case a source column changed
    
    (cherry picked from commit 3af3e392b58f3362c6b7a3d7eb0b246e3cc57e6b)

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/cache/model/ColumnSqlCacheInvalidateRequestFactories.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/cache/model/ColumnSqlCacheInvalidateRequestFactories.java
@@ -14,6 +14,8 @@ import org.adempiere.ad.expression.api.impl.StringExpressionCompiler;
 import org.adempiere.ad.table.api.ColumnSqlSourceDescriptor;
 import org.adempiere.ad.table.api.ColumnSqlSourceDescriptor.FetchTargetRecordsMethod;
 import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.compiere.model.POInfo;
 import org.compiere.util.DB;
 import org.compiere.util.Evaluatee;
 import org.compiere.util.Evaluatees;
@@ -31,12 +33,12 @@ import java.util.List;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -49,24 +51,34 @@ final class ColumnSqlCacheInvalidateRequestFactories
 	public static ModelCacheInvalidateRequestFactory ofDescriptor(@NonNull final ColumnSqlSourceDescriptor descriptor)
 	{
 		final FetchTargetRecordsMethod fetchTargetRecordsMethod = descriptor.getFetchTargetRecordsMethod();
+		final String targetTableName = descriptor.getTargetTableName();
+		final String sqlToGetTargetRecordIdBySourceRecordId;
 		if (fetchTargetRecordsMethod == FetchTargetRecordsMethod.SQL)
 		{
-			return FetchFromSQL.builder()
-					.targetTableName(descriptor.getTargetTableName())
-					.sqlToGetTargetRecordIdBySourceRecordId(descriptor.getSqlToGetTargetRecordIdBySourceRecordId())
-					.build();
+			sqlToGetTargetRecordIdBySourceRecordId = descriptor.getSqlToGetTargetRecordIdBySourceRecordId();
 		}
 		else if (fetchTargetRecordsMethod == FetchTargetRecordsMethod.LINK_COLUMN)
 		{
-			return FetchUsingLinkColumn.builder()
-					.targetTableName(descriptor.getTargetTableName())
-					.linkColumnName(descriptor.getLinkColumnName())
-					.build();
+			final POInfo targetTableInfo = POInfo.getPOInfoNotNull(targetTableName);
+			final String targetTableKeyColumnName = targetTableInfo.getSingleKeyColumnName();
+			final String linkColumnName = descriptor.getLinkColumnNameNotNull();
+			if (!targetTableInfo.isPhysicalColumn(linkColumnName))
+			{
+				throw new AdempiereException("Column " + targetTableName + "." + linkColumnName + " it's expected to exist and be a physical column");
+			}
+			sqlToGetTargetRecordIdBySourceRecordId = "SELECT " + targetTableKeyColumnName
+					+ " FROM " + targetTableName
+					+ " WHERE " + linkColumnName + "=@Record_ID@";
 		}
 		else
 		{
 			throw new AdempiereException("Fetch method not supported for " + descriptor);
 		}
+
+		return FetchFromSQL.builder()
+				.targetTableName(targetTableName)
+				.sqlToGetTargetRecordIdBySourceRecordId(sqlToGetTargetRecordIdBySourceRecordId)
+				.build();
 	}
 
 	@ToString
@@ -95,12 +107,8 @@ final class ColumnSqlCacheInvalidateRequestFactories
 		public List<CacheInvalidateRequest> createRequestsFromModel(@NonNull final ICacheSourceModel sourceModel, final ModelCacheInvalidationTiming timing_NOTNUSED)
 		{
 			final int sourceRecordId = sourceModel.getRecordId();
-			return createRequestsFromSourceRecordId(sourceRecordId);
-		}
-
-		private List<CacheInvalidateRequest> createRequestsFromSourceRecordId(final int sourceRecordId)
-		{
-			if (sourceRecordId <= 0)
+			final String keyColumnName = InterfaceWrapperHelper.getKeyColumnName(sourceModel.getTableName());
+			if (sourceRecordId < InterfaceWrapperHelper.getFirstValidIdByColumnName(keyColumnName))
 			{
 				return ImmutableList.of();
 			}
@@ -138,40 +146,6 @@ final class ColumnSqlCacheInvalidateRequestFactories
 					});
 
 			return targetRecordIds.build();
-		}
-	}
-
-	@ToString
-	@EqualsAndHashCode
-	private static class FetchUsingLinkColumn implements ModelCacheInvalidateRequestFactory
-	{
-		private final String targetTableName;
-		private final String linkColumnName;
-
-		@Builder
-		private FetchUsingLinkColumn(
-				@NonNull final String targetTableName,
-				@Nullable final String linkColumnName)
-		{
-			Check.assumeNotEmpty(targetTableName, "targetTableName is not empty");
-			Check.assumeNotEmpty(linkColumnName, "linkColumnName is not empty");
-
-			this.targetTableName = targetTableName;
-			this.linkColumnName = linkColumnName;
-		}
-
-		@Override
-		public List<CacheInvalidateRequest> createRequestsFromModel(@NonNull final ICacheSourceModel sourceModel, final ModelCacheInvalidationTiming timing_NOTUSED)
-		{
-			final int targetRecordId = sourceModel.getValueAsInt(linkColumnName, -1);
-			if (targetRecordId < 0)
-			{
-				return ImmutableList.of();
-			}
-			else
-			{
-				return ImmutableList.of(CacheInvalidateRequest.rootRecord(targetTableName, targetRecordId));
-			}
 		}
 	}
 }
