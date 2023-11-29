ae21af825741f92e487b8a31b8f5a51c4ea14a49
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-11-29 13:50:19 +0200

    Fix Invoice service - no async batch usage

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoice/InvoiceService.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoice/InvoiceService.java
@@ -25,17 +25,11 @@ package de.metas.invoice;
 import ch.qos.logback.classic.Level;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import de.metas.async.AsyncBatchId;
-import de.metas.async.api.IAsyncBatchBL;
-import de.metas.async.api.IEnqueueResult;
-import de.metas.async.service.AsyncBatchService;
 import de.metas.invoicecandidate.InvoiceCandidateId;
 import de.metas.invoicecandidate.api.IInvoiceCandBL;
 import de.metas.invoicecandidate.api.IInvoiceCandDAO;
-import de.metas.invoicecandidate.api.IInvoiceCandidateEnqueuer;
-import de.metas.invoicecandidate.api.IInvoicingParams;
-import de.metas.invoicecandidate.api.impl.PlainInvoicingParams;
 import de.metas.invoicecandidate.model.I_C_Invoice_Candidate;
+import de.metas.invoicecandidate.process.params.InvoicingParams;
 import de.metas.logging.LogManager;
 import de.metas.process.PInstanceId;
 import de.metas.util.Loggables;
@@ -50,7 +44,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Supplier;
 
 import static org.compiere.util.Env.getCtx;
 
@@ -61,13 +54,6 @@ public class InvoiceService
 
 	private final IInvoiceCandBL invoiceCandBL = Services.get(IInvoiceCandBL.class);
 	private final IInvoiceCandDAO invoiceCandDAO = Services.get(IInvoiceCandDAO.class);
-
-	private final AsyncBatchService asyncBatchService;
-
-	public InvoiceService(@NonNull final AsyncBatchService asyncBatchService)
-	{
-		this.asyncBatchService = asyncBatchService;
-	}
 
 	@NonNull
 	public Set<InvoiceId> generateInvoicesFromShipmentLines(@NonNull final List<I_M_InOutLine> shipmentLines)
@@ -103,18 +89,12 @@ public class InvoiceService
 	{
 		final PInstanceId invoiceCandidatesSelectionId = DB.createT_Selection(invoiceCandidateIds, Trx.TRXNAME_None);
 
-		final IInvoiceCandidateEnqueuer enqueuer = invoiceCandBL.enqueueForInvoicing()
+		invoiceCandBL.enqueueForInvoicing()
 				.setContext(getCtx())
 				.setInvoicingParams(createDefaultIInvoicingParams())
-				.setFailIfNothingEnqueued(true);
-
-		// this creates workpackages
-		enqueuer.prepareSelection(invoiceCandidatesSelectionId);
-
-		final Supplier<IEnqueueResult> enqueueInvoiceCandidates = () -> enqueuer
+				.setFailIfNothingEnqueued(true)
 				.enqueueSelection(invoiceCandidatesSelectionId);
 
-		asyncBatchService.executeBatch(enqueueInvoiceCandidates, AsyncBatchId.NONE_ASYNC_BATCH_ID);
 	}
 
 	@NonNull
