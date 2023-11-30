15d002defb6cc2c2502adc5352804f018ec01324
Max Rieck <max.rieck@metasfresh.com>
2023-11-30 16:18:21 +0100

    update docker compose and dev-scripts

--- misc/dev-support/docker/infrastructure/scripts/95_start.sh
+++ misc/dev-support/docker/infrastructure/scripts/95_start.sh
@@ -2,7 +2,7 @@
 
 #
 # %L
-# metasfresh
+# master
 # %%
 # Copyright (C) 2023 metas GmbH
 # %%
@@ -24,22 +24,18 @@
 
 set -e
 
-# We assume that the stuff was started with 10_reset_db_to_seed_dump.sh
 # We assume that in the folder misc/dev-support/docker/infrastructure/env-files/ there exists an env file named ${BRANCH_NAME}.env
 if ! [ -z "$1" ]; then
     BRANCH_NAME=$1
 else
     echo "!! The first parameter needs do correspond to an env-File !!"
-    echo "!! E.g. to use the env-file env-files/release.env, run 30_reset_db_to_template.sh release !!" 
+    echo "!! E.g. to use the env-file env-files/release.env, run 10_reset_db_to_seek_Dump.sh release !!" 
     exit
 fi
 
 set -u
 
-# the winpty is needed to avoid an error when running the script in git bash on windows
+COMPOSE_FILE=../docker-compose.yml
+ENV_FILE=../env-files/${BRANCH_NAME}.env
 
-winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "drop database if exists metasfresh;"
-winpty docker exec -it ${BRANCH_NAME}_db  psql -U postgres -c "create database metasfresh template metasfresh_template_${BRANCH_NAME};"
-
-echo "The local database has been recreated from the template database."
-echo "You can rerun this script to reset the local database to the template database."
+docker-compose --file ${COMPOSE_FILE} --env-file ${ENV_FILE} --project-name ${BRANCH_NAME}_infrastructure start
