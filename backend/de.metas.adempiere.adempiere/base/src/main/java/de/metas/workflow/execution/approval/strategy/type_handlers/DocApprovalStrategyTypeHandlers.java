5de40e555c4fc3472552ebea9b7b72fe3bf671bf
Teo Sarca <teo.sarca@metasfresh.com>
2023-12-13 20:31:01 +0200

    throw exception if no users found for job

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/workflow/execution/approval/strategy/type_handlers/DocApprovalStrategyTypeHandlers.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/workflow/execution/approval/strategy/type_handlers/DocApprovalStrategyTypeHandlers.java
@@ -1,15 +1,17 @@
 package de.metas.workflow.execution.approval.strategy.type_handlers;
 
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DocApprovalStrategyTypeHandlers
 {
 	@NonNull private final WorkflowResponsibleHandler workflowResponsibleHandler = new WorkflowResponsibleHandler();
 	@NonNull private final RequestorHandler requestorHandler = new RequestorHandler();
 	@NonNull private final ProjectManagerHandler projectManagerHandler = new ProjectManagerHandler();
-	@NonNull private final JobHandler jobHandler = new JobHandler();
+	@NonNull private final JobHandler jobHandler;
 
 	public DocApprovalStrategyTypeHandler getHandler(@NonNull final DocApprovalStrategyType type)
 	{
