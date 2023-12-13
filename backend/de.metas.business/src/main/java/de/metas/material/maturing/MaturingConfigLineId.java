a5e906de12a870ef2cf76af0019a4e60341c7e81
Cristina Ghita <cristina.ghita@metasfresh.com>
2023-12-13 17:02:01 +0200

    Add basic POJOs and Repo for Maturingconfig

---
+++ backend/de.metas.business/src/main/java/de/metas/material/maturing/MaturingConfigLineId.java
@@ -0,0 +1,58 @@
+package de.metas.material.maturing;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import de.metas.material.planning.ProductPlanningId;
+import de.metas.util.Check;
+import de.metas.util.lang.RepoIdAware;
+import lombok.Value;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+/*
+ * #%L
+ * metasfresh-material-planning
+ * %%
+ * Copyright (C) 2019 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+@Value
+public class MaturingConfigLineId implements RepoIdAware
+{
+	int repoId;
+	@JsonCreator
+	public static MaturingConfigLineId ofRepoId(final int repoId)
+	{
+		return new MaturingConfigLineId(repoId);
+	}
+
+	public static MaturingConfigLineId ofRepoIdOrNull(@Nullable final Integer repoId)
+	{
+		return repoId != null && repoId > 0 ? new MaturingConfigLineId(repoId) : null;
+	}
+
+	public static int toRepoId(final MaturingConfigLineId id)
+	{
+		return id != null ? id.getRepoId() : -1;
+	}
+	public static boolean equals(@Nullable final ProductPlanningId id1, @Nullable final ProductPlanningId id2) {return Objects.equals(id1, id2);}
+	private MaturingConfigLineId(final int repoId)
+	{
+		this.repoId = Check.assumeGreaterThanZero(repoId, "M_MaturingConfig_Line_ID");
+	}
+}
