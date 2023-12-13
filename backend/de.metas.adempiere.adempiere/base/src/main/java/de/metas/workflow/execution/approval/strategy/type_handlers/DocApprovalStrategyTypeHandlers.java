95c9b27bd3e4dd78470908286883af571291cbdb
Teo Sarca <teo.sarca@metasfresh.com>
2023-12-13 21:13:09 +0200

    junit tests

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/workflow/execution/approval/strategy/type_handlers/DocApprovalStrategyTypeHandlers.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/workflow/execution/approval/strategy/type_handlers/DocApprovalStrategyTypeHandlers.java
@@ -1,7 +1,9 @@
 package de.metas.workflow.execution.approval.strategy.type_handlers;
 
+import de.metas.job.JobService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.compiere.Adempiere;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +14,12 @@ public class DocApprovalStrategyTypeHandlers
 	@NonNull private final RequestorHandler requestorHandler = new RequestorHandler();
 	@NonNull private final ProjectManagerHandler projectManagerHandler = new ProjectManagerHandler();
 	@NonNull private final JobHandler jobHandler;
+
+	public static DocApprovalStrategyTypeHandlers newInstanceForUnitTesting()
+	{
+		Adempiere.assertUnitTestMode();
+		return new DocApprovalStrategyTypeHandlers(new JobHandler(JobService.newInstanceForUnitTesting()));
+	}
 
 	public DocApprovalStrategyTypeHandler getHandler(@NonNull final DocApprovalStrategyType type)
 	{
