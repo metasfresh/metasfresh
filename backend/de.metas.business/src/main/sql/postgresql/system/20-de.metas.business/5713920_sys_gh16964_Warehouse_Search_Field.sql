6db5cc049a05e9ff298453c17c477b40925ea375
dm-ps <dragos.moca@pevesoft.ro>
2023-12-18 16:47:06 +0200

    Warehouse in Shipment Disposition as search field refs: https://github.com/metasfresh/metasfresh/issues/16964

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5713920_sys_gh16964_Warehouse_Search_Field.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5713920_sys_gh16964_Warehouse_Search_Field.sql
@@ -11,3 +11,13 @@ UPDATE AD_Column SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2023-12-18 14:28:2
 INSERT INTO t_alter_column values('m_shipmentschedule','M_Warehouse_ID','NUMERIC(10)',null,null)
 ;
 
+-- Run mode: SWING_CLIENT
+
+-- Column: M_ShipmentSchedule.M_Warehouse_Override_ID
+-- 2023-12-18T14:44:12.139Z
+UPDATE AD_Column SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2023-12-18 16:44:12.139','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=65576
+;
+
+-- 2023-12-18T14:44:37.505Z
+INSERT INTO t_alter_column values('m_shipmentschedule','M_Warehouse_Override_ID','NUMERIC(10)',null,null)
+;
