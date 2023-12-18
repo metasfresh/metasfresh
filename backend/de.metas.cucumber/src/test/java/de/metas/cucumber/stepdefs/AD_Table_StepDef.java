1788e27178a17479aeee1fa61153d458c75ae70e
Teo Sarca <teo.sarca@metasfresh.com>
2023-12-18 08:54:15 +0200

    adTable.feature improve test

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/AD_Table_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/AD_Table_StepDef.java
@@ -4,21 +4,26 @@ import com.google.common.collect.ImmutableSet;
 import de.metas.logging.LogManager;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
+import lombok.Builder;
 import lombok.NonNull;
+import lombok.Value;
 import org.adempiere.ad.column.ColumnSql;
 import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.exceptions.DBException;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.compiere.model.POInfo;
 import org.compiere.model.POInfoColumn;
 import org.compiere.util.DB;
 import org.slf4j.Logger;
 
+import javax.annotation.Nullable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.LinkedHashSet;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.LinkedHashMap;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class AD_Table_StepDef
 {
@@ -29,11 +34,11 @@ public class AD_Table_StepDef
 	{
 		final TableNamesSkipList tableNamesSkipList = TableNamesSkipList.ofDataTable(dataTable);
 
-		final AtomicInteger countOK = new AtomicInteger(0);
-		final LinkedHashSet<String> tableNamesWithError = new LinkedHashSet<>();
+		final Progress progress = new Progress();
+		final SoftAssertions softly = new SoftAssertions();
 
-		POInfo.getPOInfoMap()
-				.stream()
+		final POInfo.POInfoMap poInfoMap = POInfo.getPOInfoMap();
+		poInfoMap.stream()
 				.forEach(poInfo -> {
 					final String tableName = poInfo.getTableName();
 
@@ -43,30 +48,29 @@ public class AD_Table_StepDef
 						return;
 					}
 
-					try
-					{
-						tryRetrieveFirstRow(poInfo);
-						logger.info("{} is OK", tableName);
-						countOK.incrementAndGet();
-					}
-					catch (Exception ex)
-					{
-						logger.warn("{} has errors", tableName, ex);
-						tableNamesWithError.add(tableName);
-					}
+					softly.assertThatCode(() -> tryRetrieveFirstRow(poInfo, progress))
+							.as(() -> "Expect table " + tableName + " is valid")
+							.doesNotThrowAnyException();
 				});
 
-		final int countErrors = tableNamesWithError.size();
-		logger.info("Checked all tables. {} are OK, {} are with errors.", countOK, countErrors);
-		if (countErrors > 0)
-		{
-			logger.info("Tables with errors are: {}", tableNamesWithError);
-		}
+		//
+		// Hygiene: make sure the skip rules are still matching.
+		// If not, advice the developer to remote the rule.
+		tableNamesSkipList.forEachTableNameUC(tableNameUC -> {
+			softly.assertThatCode(() -> {
+						final POInfo poInfo = poInfoMap.getByTableName(tableNameUC);
+						tryRetrieveFirstRow(poInfo, null); // expect failure
+					})
+					.as(() -> "Expected table " + tableNameUC + " is still invalid but it's OK. GREAT! Consider removing this exclude rule.")
+					.isInstanceOf(Throwable.class);
+		});
 
-		Assertions.assertThat(countErrors).as("AD_Table(s) with errors found").isZero();
+		progress.logSummary();
+
+		Assertions.assertThat(progress.countErrors()).as("Expect no errors").isZero();
 	}
 
-	private static void tryRetrieveFirstRow(final @NonNull POInfo poInfo)
+	private static void tryRetrieveFirstRow(final @NonNull POInfo poInfo, @Nullable final Progress progress)
 	{
 		final String sql = getSqlSelectFirstRow(poInfo);
 		PreparedStatement pstmt = null;
@@ -75,10 +79,21 @@ public class AD_Table_StepDef
 		{
 			pstmt = DB.prepareStatement(sql, ITrx.TRXNAME_ThreadInherited);
 			rs = pstmt.executeQuery();
+
+			if (progress != null)
+			{
+				progress.addOK(poInfo.getTableName());
+			}
 		}
 		catch (SQLException ex)
 		{
-			throw new DBException(ex, sql);
+			final DBException dbException = new DBException(ex, sql);
+			if (progress != null)
+			{
+				progress.addError(poInfo.getTableName(), dbException);
+			}
+
+			throw dbException;
 		}
 		finally
 		{
@@ -109,7 +124,11 @@ public class AD_Table_StepDef
 	{
 		@NonNull private final ImmutableSet<String> tableNamesUC;
 
-		private TableNamesSkipList(@NonNull ImmutableSet<String> tableNamesUC) {this.tableNamesUC = tableNamesUC;}
+		private TableNamesSkipList(@NonNull ImmutableSet<String> tableNamesUC)
+		{
+			this.tableNamesUC = tableNamesUC;
+			System.out.println("tableNamesUC=" + this.tableNamesUC);
+		}
 
 		public static TableNamesSkipList ofDataTable(@NonNull final DataTable dataTable)
 		{
@@ -126,5 +145,63 @@ public class AD_Table_StepDef
 		{
 			return tableNamesUC.contains(tableName.toUpperCase());
 		}
+
+		public void forEachTableNameUC(final Consumer<String> consumer) {tableNamesUC.forEach(consumer);}
+	}
+
+	private static class Progress
+	{
+		private int countAll = 0;
+		private final LinkedHashMap<String, ErrorEntry> errors = new LinkedHashMap<>();
+
+		public void addOK(@NonNull final String tableName)
+		{
+			countAll++;
+			logger.info("{} is OK", tableName);
+		}
+
+		public void addError(@NonNull final String tableName, @Nullable final Exception ex)
+		{
+			countAll++;
+			logger.warn("{} has errors", tableName, ex);
+			errors.put(tableName, ErrorEntry.builder()
+					.tableName(tableName)
+					.error(ex != null ? ex.getLocalizedMessage() : "")
+					.build());
+		}
+
+		public void logSummary()
+		{
+			final int countErrors = countErrors();
+			logger.info("Checked all {} tables. Found {} are with errors.", countAll, countErrors);
+			if (countErrors > 0)
+			{
+				logger.info("Tables with errors are: {}", errors.keySet());
+				logger.info("To ignore them pls add following to your ignore table:\n"
+						+ getErrorsAsCucumberTable()
+				);
+			}
+
+		}
+
+		private String getErrorsAsCucumberTable()
+		{
+			return errors.values().stream()
+					.map(error -> " | " + error.getTableName() + " | " + error.getError() + " | ")
+					.collect(Collectors.joining("\n"));
+		}
+
+		private int countErrors()
+		{
+			return errors.size();
+		}
+	}
+
+	@Value
+	@Builder
+	private static class ErrorEntry
+	{
+		String tableName;
+		String error;
 	}
 }
