a40d50e04bb4acf284aa17b31d52bf2efcdd4254
Teo Sarca <teo.sarca@metasfresh.com>
2023-12-20 21:31:46 +0200

    IADProcessDAO.retrieveAllActiveAdProcesIds

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/impl/ADProcessDAO.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/impl/ADProcessDAO.java
@@ -672,11 +672,19 @@ public class ADProcessDAO implements IADProcessDAO
 				ITrx.TRXNAME_ThreadInherited);
 	}
 
-
 	@Override
 	public ProcessType retrieveProcessType(@NonNull final AdProcessId processId)
 	{
-		final I_AD_Process process =  InterfaceWrapperHelper.loadOutOfTrx(processId, I_AD_Process.class);
+		final I_AD_Process process = InterfaceWrapperHelper.loadOutOfTrx(processId, I_AD_Process.class);
 		return ProcessType.ofCode(process.getType());
+	}
+
+	@Override
+	public ImmutableSet<AdProcessId> retrieveAllActiveAdProcesIds()
+	{
+		return queryBL.createQueryBuilderOutOfTrx(I_AD_Process.class)
+				.addOnlyActiveRecordsFilter()
+				.create()
+				.listIds(AdProcessId::ofRepoId);
 	}
 }
