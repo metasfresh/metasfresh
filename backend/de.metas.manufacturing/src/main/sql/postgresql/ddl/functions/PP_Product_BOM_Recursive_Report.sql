b12b1e042df61bdaa4234d207144aa7e7979b673
dm-ps <dragos.moca@pevesoft.ro>
2024-01-08 18:17:53 +0200

    Port the fix for PP_Product_Bom_Recursive to release refs: https://github.com/metasfresh/metasfresh/issues/17066

--- backend/de.metas.manufacturing/src/main/sql/postgresql/ddl/functions/PP_Product_BOM_Recursive_Report.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/ddl/functions/PP_Product_BOM_Recursive_Report.sql
@@ -1,29 +1,19 @@
-CREATE OR REPLACE FUNCTION pp_product_bom_recursive_report(p_pp_product_bom_id numeric)
-    RETURNS TABLE
-            (
-                line         text,
-                productvalue character varying,
-                productname  character varying,
-                qtybom       numeric,
-                percentage   numeric,
-                uomsymbol    character varying,
-                supplier     text
-            )
-    STABLE
-    LANGUAGE sql
-AS
+drop function if exists pp_product_bom_recursive_report(p_pp_product_bom_id numeric,referencedate timestamp without time zone);
+create function pp_product_bom_recursive_report(p_pp_product_bom_id numeric,referencedate timestamp without time zone)
+    returns TABLE(line text, productvalue character varying, productname character varying, qtybom numeric, percentage numeric, uomsymbol character varying)
+    stable
+    language sql
+as
 $$
-SELECT t.Line,
-       t.ProductValue,
-       t.ProductName,
-       t.QtyBOM,
-       t.Percentage,
-       t.UOMSymbol,
-       t.Supplier
-FROM PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, NULL) t
-ORDER BY t.path
-$$
-;
+select
+    t.Line,
+    t.ProductValue,
+    t.ProductName,
+    t.QtyBOM,
+    t.Percentage,
+    t.UOMSymbol
+from PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, null,referencedate) t
+order by t.path
+$$;
 
-ALTER FUNCTION pp_product_bom_recursive_report(numeric) OWNER TO metasfresh
-;
+alter function pp_product_bom_recursive_report(numeric,timestamp without time zone) owner to metasfresh;
--- backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5714830_sys_gh17066_pp_product_bom_recursive_and_pp_product_bom_recursive_report_f.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5714830_sys_gh17066_pp_product_bom_recursive_and_pp_product_bom_recursive_report_f.sql
@@ -1,3 +1,5 @@
+drop function if exists pp_product_bom_recursive(p_pp_product_bom_id numeric, p_ad_language character varying);
+
 drop function if exists pp_product_bom_recursive(p_pp_product_bom_id numeric, p_ad_language character varying,referencedate timestamp without time zone);
 create function pp_product_bom_recursive(p_pp_product_bom_id numeric, p_ad_language character varying,referencedate timestamp without time zone DEFAULT now())
     returns TABLE(line text, parent_line text, productvalue character varying, productname character varying, qtybom numeric, percentage numeric, uomsymbol character varying, depth integer, m_product_id numeric, isqtypercentage character, c_uom_id numeric, path integer[])
@@ -90,3 +92,26 @@ order by path
 $$;
 
 alter function pp_product_bom_recursive(numeric, varchar,timestamp without time zone) owner to metasfresh;
+
+
+drop function if exists pp_product_bom_recursive_report(p_pp_product_bom_id numeric);
+
+drop function if exists pp_product_bom_recursive_report(p_pp_product_bom_id numeric,referencedate timestamp without time zone);
+create function pp_product_bom_recursive_report(p_pp_product_bom_id numeric,referencedate timestamp without time zone)
+    returns TABLE(line text, productvalue character varying, productname character varying, qtybom numeric, percentage numeric, uomsymbol character varying)
+    stable
+    language sql
+as
+$$
+select
+    t.Line,
+    t.ProductValue,
+    t.ProductName,
+    t.QtyBOM,
+    t.Percentage,
+    t.UOMSymbol
+from PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, null,referencedate) t
+order by t.path
+$$;
+
+alter function pp_product_bom_recursive_report(numeric,timestamp without time zone) owner to metasfresh;
