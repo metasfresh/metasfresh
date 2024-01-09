05e999030e543627d846744739da1b3ef9ab9d2c
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2024-01-09 07:43:24 +0100

    format code for comparability

--- backend/de.metas.manufacturing/src/main/sql/postgresql/ddl/functions/PP_Product_BOM_Recursive_Report.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/ddl/functions/PP_Product_BOM_Recursive_Report.sql
@@ -1,19 +1,29 @@
-drop function if exists pp_product_bom_recursive_report(p_pp_product_bom_id numeric,referencedate timestamp without time zone);
-create function pp_product_bom_recursive_report(p_pp_product_bom_id numeric,referencedate timestamp without time zone)
-    returns TABLE(line text, productvalue character varying, productname character varying, qtybom numeric, percentage numeric, uomsymbol character varying)
-    stable
-    language sql
-as
-$$
-select
-    t.Line,
-    t.ProductValue,
-    t.ProductName,
-    t.QtyBOM,
-    t.Percentage,
-    t.UOMSymbol
-from PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, null,referencedate) t
-order by t.path
-$$;
+DROP FUNCTION IF EXISTS pp_product_bom_recursive_report(p_pp_product_bom_id numeric,
+                                                        referencedate       timestamp WITHOUT TIME ZONE)
+;
 
-alter function pp_product_bom_recursive_report(numeric,timestamp without time zone) owner to metasfresh;
+CREATE FUNCTION pp_product_bom_recursive_report(p_pp_product_bom_id numeric,
+                                                referencedate       timestamp WITHOUT TIME ZONE)
+    RETURNS TABLE
+            (
+                line         text,
+                productvalue character varying,
+                productname  character varying,
+                qtybom       numeric,
+                percentage   numeric,
+                uomsymbol    character varying
+            )
+    STABLE
+    LANGUAGE sql
+AS
+$$
+SELECT t.Line,
+       t.ProductValue,
+       t.ProductName,
+       t.QtyBOM,
+       t.Percentage,
+       t.UOMSymbol
+FROM PP_Product_BOM_Recursive(PP_Product_BOM_Recursive_Report.p_PP_Product_BOM_ID, NULL, referencedate) t
+ORDER BY t.path
+$$
+;
