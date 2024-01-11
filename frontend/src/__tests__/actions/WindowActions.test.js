abf4c055b825879c9f66e5705f29ba0153b337b0
Teo Sarca <teo.sarca@metasfresh.com>
2024-01-11 22:35:14 +0200

    FE Load documents correctly when going back and forward to/from profile and a given page (#17111)
    * windowHandler.js - INIT_DATA_SUCCESS - don't change current state
    
    * windowHandler.js - INIT_DATA_SUCCESS - reset the layout in case we are setting data for a different windowId
    
    * fix unit tests

--- frontend/src/__tests__/actions/WindowActions.test.js
+++ frontend/src/__tests__/actions/WindowActions.test.js
@@ -73,6 +73,7 @@ describe('WindowActions thunks', () => {
                 { type: ACTION_TYPES.INIT_WINDOW },
                 {
                   type: ACTION_TYPES.INIT_DATA_SUCCESS,
+                  windowId: "143",
                   data: {},
                   docId: undefined,
                   hasComments: undefined,
@@ -240,6 +241,7 @@ describe('WindowActions thunks', () => {
               { type: ACTION_TYPES.INIT_WINDOW },
               {
                 type: ACTION_TYPES.INIT_DATA_SUCCESS,
+                windowId,
                 data: parseToDisplay(dataResponse[0].fieldsByName),
                 docId,
                 saveStatus: dataResponse[0].saveStatus,
