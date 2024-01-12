metasfresh/metasfresh

commit a10f5799891f9693d10a7d55d87a39baf7c95b84

TableFilter.js: convert to functional component; fixes

frontend/src/components/table/TableFilter.js
@@ -1,7 +1,7 @@
 import counterpart from 'counterpart';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import keymap from '../../shortcuts/keymap';
 import { TableFilterContextShortcuts } from '../keyshortcuts';
@@ -11,128 +11,93 @@ import { addNotification } from '../../actions/AppActions';
 
 import Tooltips from '../tooltips/Tooltips';
 import TableQuickInput from './TableQuickInput';
-import { getSelection, getTableId } from '../../reducers/tables';
+import { useSelectedRowIds } from '../../reducers/tables';
 
 /**
  * Component displayed above included tab header and contains buttons like 'Add new', 'Batch entry', top actions etc.
  */
-class TableFilter extends PureComponent {
-  static propTypes = {
-    tabIndex: PropTypes.number.isRequired,
-    modalVisible: PropTypes.bool.isRequired,
-    fetchTopActions: PropTypes.func.isRequired,
-    deleteTopActions: PropTypes.func.isRequired,
-    forceHeight: PropTypes.number,
-    tabId: PropTypes.string,
-    docType: PropTypes.string,
-    docId: PropTypes.string,
-    selectedRowIds: PropTypes.array,
-    openModal: PropTypes.func.isRequired,
-    toggleFullScreen: PropTypes.func,
-    fullScreen: PropTypes.any,
-    wrapperHeight: PropTypes.number,
-    isBatchEntry: PropTypes.bool,
-    handleBatchEntryToggle: PropTypes.func,
-    quickInputSupport: PropTypes.object,
-    newRecordInputMode: PropTypes.string,
-    allowCreateNew: PropTypes.bool,
-    openTableModal: PropTypes.func,
-    addNotification: PropTypes.func.isRequired,
-    pending: PropTypes.bool,
-  };
+const TableFilter = ({
+  tabIndex: tabIndexProp,
+  docType: windowId,
+  tabId,
+  docId,
+  toggleFullScreen,
+  fullScreen,
+  wrapperHeight,
+  isBatchEntry: isBatchEntryActive,
+  handleBatchEntryToggle,
+  quickInputSupport,
+  newRecordInputMode,
+  allowCreateNew,
+  openTableModal,
+  pending,
+}) => {
+  const modalVisible = useSelector(
+    (state) => state.windowHandler.modal.visible
+  );
+  const selectedRowIds = useSelectedRowIds({ windowId, tabId, docId });
 
-  constructor(props) {
-    super(props);
+  const [topActions, setTopActions] = useState([]);
+  const [shortcutActions, setShortcutActions] = useState([]);
+  const [isTooltipShow, setIsTooltipShow] = useState(null);
 
-    this.state = {
-      topActions: [],
-      isTooltipShow: false,
-      shortcutActions: [],
-    };
-  }
+  const dispatch = useDispatch();
 
-  componentDidMount() {
-    this.loadTopActions();
-    this.openCloseBatchEntryIfNeeded();
-  }
-
-  componentWillUnmount() {
-    this.props.deleteTopActions();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.allowCreateNew !== prevProps.allowCreateNew ||
-      this.props.quickInputSupport !== prevProps.quickInputSupport ||
-      this.props.newRecordInputMode !== prevProps.newRecordInputMode
-    ) {
-      this.openCloseBatchEntryIfNeeded();
+  useEffect(() => {
+    if (windowId && tabId && docId) {
+      dispatch(fetchTopActions(windowId, docId, tabId)).then((topActions) => {
+        setTopActions(topActions);
+        setShortcutActions(
+          generateShortcutActionsArray(topActions, handleTopActionClick)
+        );
+      });
     }
-  }
+    return () => {
+      dispatch(deleteTopActions());
+    };
+  }, []);
 
-  openCloseBatchEntryIfNeeded = () => {
-    const {
-      allowCreateNew,
-      quickInputSupport,
-      newRecordInputMode,
-      isBatchEntry,
-      handleBatchEntryToggle,
-    } = this.props;
+  useEffect(() => {
+    openCloseBatchEntryIfNeeded();
+  }, [allowCreateNew, quickInputSupport, newRecordInputMode]);
 
+  const openCloseBatchEntryIfNeeded = () => {
     //
     // Automatically open batch entry (if not already opened) when we can add new entries
     if (
       quickInputSupport &&
       newRecordInputMode === 'QUICK_INPUT_ONLY' &&
       allowCreateNew &&
-      !isBatchEntry
+      !isBatchEntryActive
     ) {
       handleBatchEntryToggle();
     }
     //
     // Close batch entry if open but creating new entries is no longer allowed
-    else if (quickInputSupport && !allowCreateNew && isBatchEntry) {
+    else if (quickInputSupport && !allowCreateNew && isBatchEntryActive) {
       handleBatchEntryToggle();
     }
   };
 
-  /**
-   * @method loadTopActions
-   * @summary fetch top actions for the table
-   */
-  loadTopActions = () => {
-    const { tabId, docType, docId, fetchTopActions } = this.props;
-
-    if (tabId && docType && docId) {
-      fetchTopActions(docType, docId, tabId).then((topActions) => {
-        this.setState({ topActions });
-        this.generateShortcuts(topActions);
-      });
-    }
-  };
-
-  handleTopActionClick = (action) => {
-    const { openModal, selectedRowIds } = this.props;
-
+  const handleTopActionClick = (action) => {
     if (action.disabled) {
       return;
     }
 
-    openModal({
-      title: action.caption,
-      windowId: action.processId,
-      modalType: 'process',
-      viewDocumentIds: selectedRowIds,
-    });
+    dispatch(
+      openModal({
+        title: action.caption,
+        windowId: action.processId,
+        modalType: 'process',
+        viewDocumentIds: selectedRowIds,
+      })
+    );
   };
 
   /**
    * @summary create and store buttons for actions once, so that we won't redo this on each render
    */
-  renderTopActionButtons = () => {
-    const { tabIndex } = this.props;
-    const { topActions, isTooltipShow } = this.state;
-
+  const renderTopActionButtons = () => {
     if (!topActions || !topActions.length) {
       return null;
     }
@@ -143,9 +108,10 @@ class TableFilter extends PureComponent {
         tabIndex={tabIndex}
         caption={action.caption}
         description={action.description}
-        onClick={() => this.handleTopActionClick(action)}
-        showTooltip={() => this.showTooltip(action.processId)}
-        hideTooltip={this.hideTooltip}
+        onClick={() => handleTopActionClick(action)}
+        showTooltip={() => showTooltip(action.processId)}
+        hideTooltip={hideTooltip}
+        disabled={pending}
       >
         {isTooltipShow === action.processId && (
           <Tooltips
@@ -158,160 +124,159 @@ class TableFilter extends PureComponent {
     ));
   };
 
-  /**
-   * @method generateShortcuts
-   * @summary generate table filters shortcuts and store them in state. We're doing
-   * this to force a re-render once we have the actions data available.
-   */
-  generateShortcuts = (actions) => {
-    const shortcutActions = [];
-
-    for (let i = 0; i < actions.length; i += 1) {
-      const action = actions[i];
-
-      shortcutActions.push({
-        name: `FILTER_ACTION_${i}`,
-        handler: () => this.handleTopActionClick(action),
-        shortcut: action.shortcut ? action.shortcut.replace('-', '+') : '',
-      });
-    }
-
-    this.setState({ shortcutActions });
+  const showTooltip = (name) => {
+    setIsTooltipShow(name);
   };
 
-  showTooltip = (name) => {
-    this.setState({ isTooltipShow: name });
+  const hideTooltip = () => {
+    setIsTooltipShow(null);
   };
 
-  hideTooltip = () => {
-    this.setState({ isTooltipShow: null });
-  };
+  const tabIndex = fullScreen || modalVisible ? -1 : tabIndexProp;
 
-  render() {
-    const {
-      openTableModal,
-      toggleFullScreen,
-      fullScreen,
-      docType,
-      docId,
-      tabId,
-      isBatchEntry,
-      newRecordInputMode,
-      handleBatchEntryToggle,
-      quickInputSupport,
-      allowCreateNew,
-      modalVisible,
-      wrapperHeight,
-      addNotification,
-      pending,
-    } = this.props;
-    const { isTooltipShow, shortcutActions } = this.state;
-    const tabIndex = fullScreen || modalVisible ? -1 : this.props.tabIndex;
+  const showNewButton =
+    newRecordInputMode === 'ALL_METHODS' && // input mode allows it
+    allowCreateNew && // we are allowed to create a new record
+    !isBatchEntryActive; // batch entry is not already opened
 
-    const showNewButton =
-      newRecordInputMode === 'ALL_METHODS' && // input mode allows it
-      allowCreateNew && // we are allowed to create a new record
-      !isBatchEntry; // batch entry is not already opened
+  const showBatchEntryButton =
+    (newRecordInputMode === 'ALL_METHODS' ||
+      newRecordInputMode === 'QUICK_INPUT_ONLY') && // input mode allows it
+    quickInputSupport && // batch entry is supported by backend
+    allowCreateNew && // we are allowed to create a new record
+    !fullScreen; // included tab is not in full screen mode
 
-    const showBatchEntryButton =
-      (newRecordInputMode === 'ALL_METHODS' ||
-        newRecordInputMode === 'QUICK_INPUT_ONLY') && // input mode allows it
-      quickInputSupport && // batch entry is supported by backend
-      allowCreateNew && // we are allowed to create a new record
-      !fullScreen; // included tab is not in full screen mode
-
-    return (
-      <div className="table-filter-line">
-        <div className="form-flex-align">
-          <div className="row filter-panel-buttons">
-            {showNewButton && (
-              <button
-                className="btn btn-meta-outline-secondary btn-distance btn-sm"
-                onClick={openTableModal}
-                tabIndex={tabIndex}
-                disabled={pending}
-              >
-                {counterpart.translate('window.addNew.caption')}
-              </button>
-            )}
-            {showBatchEntryButton && (
-              <button
-                className="btn btn-meta-outline-secondary btn-distance btn-sm close-batch-entry"
-                onClick={handleBatchEntryToggle}
-                onMouseEnter={() => this.showTooltip(keymap.TOGGLE_QUICK_INPUT)}
-                onMouseLeave={this.hideTooltip}
-                tabIndex={tabIndex}
-                disabled={pending}
-              >
-                {isBatchEntry
-                  ? quickInputSupport.closeButtonCaption
-                  : quickInputSupport.openButtonCaption}
-                {isTooltipShow === keymap.TOGGLE_QUICK_INPUT && (
-                  <Tooltips
-                    name={keymap.TOGGLE_QUICK_INPUT}
-                    action={
-                      isBatchEntry
-                        ? quickInputSupport.closeButtonCaption
-                        : quickInputSupport.openButtonCaption
-                    }
-                    type={''}
-                  />
-                )}
-              </button>
-            )}
-            {!isBatchEntry && this.renderTopActionButtons()}
-            {!isBatchEntry &&
-              (shortcutActions.length ? (
-                <TableFilterContextShortcuts
-                  shortcutActions={shortcutActions}
+  return (
+    <div className="table-filter-line">
+      <div className="form-flex-align">
+        <div className="row filter-panel-buttons">
+          {showNewButton && (
+            <button
+              className="btn btn-meta-outline-secondary btn-distance btn-sm"
+              onClick={openTableModal}
+              tabIndex={tabIndex}
+              disabled={pending}
+            >
+              {counterpart.translate('window.addNew.caption')}
+            </button>
+          )}
+          {showBatchEntryButton && (
+            <button
+              className="btn btn-meta-outline-secondary btn-distance btn-sm close-batch-entry"
+              onClick={handleBatchEntryToggle}
+              onMouseEnter={() => showTooltip(keymap.TOGGLE_QUICK_INPUT)}
+              onMouseLeave={hideTooltip}
+              tabIndex={tabIndex}
+              disabled={pending}
+            >
+              {isBatchEntryActive
+                ? quickInputSupport.closeButtonCaption
+                : quickInputSupport.openButtonCaption}
+              {isTooltipShow === keymap.TOGGLE_QUICK_INPUT && (
+                <Tooltips
+                  name={keymap.TOGGLE_QUICK_INPUT}
+                  action={
+                    isBatchEntryActive
+                      ? quickInputSupport.closeButtonCaption
+                      : quickInputSupport.openButtonCaption
+                  }
+                  type={''}
                 />
-              ) : null)}
-          </div>
-          {quickInputSupport &&
-            (isBatchEntry || fullScreen) &&
-            allowCreateNew && (
-              <TableQuickInput
-                docType={docType}
-                docId={docId}
-                tabId={tabId}
-                forceHeight={wrapperHeight ? wrapperHeight : null}
-                closeBatchEntry={handleBatchEntryToggle}
-                addNotification={addNotification}
-              />
-            )}
+              )}
+            </button>
+          )}
+          {!isBatchEntryActive && renderTopActionButtons()}
+          {!isBatchEntryActive &&
+            (shortcutActions.length ? (
+              <TableFilterContextShortcuts shortcutActions={shortcutActions} />
+            ) : null)}
         </div>
-
-        {
-          <button
-            className="btn-icon btn-meta-outline-secondary pointer btn-fullscreen"
-            onClick={toggleFullScreen}
-            onMouseEnter={() => this.showTooltip(keymap.TOGGLE_EXPAND)}
-            onMouseLeave={this.hideTooltip}
-            tabIndex="-1"
-          >
-            {fullScreen ? (
-              <i className="meta-icon-collapse" />
-            ) : (
-              <i className="meta-icon-fullscreen" />
-            )}
-
-            {isTooltipShow === keymap.TOGGLE_EXPAND && (
-              <Tooltips
-                name={keymap.TOGGLE_EXPAND}
-                action={
-                  fullScreen
-                    ? counterpart.translate('window.table.collapse')
-                    : counterpart.translate('window.table.expand')
-                }
-                type={''}
-              />
-            )}
-          </button>
-        }
+        {quickInputSupport &&
+          (isBatchEntryActive || fullScreen) &&
+          allowCreateNew && (
+            <TableQuickInput
+              docType={windowId}
+              docId={docId}
+              tabId={tabId}
+              forceHeight={wrapperHeight ? wrapperHeight : null}
+              closeBatchEntry={handleBatchEntryToggle}
+              addNotification={(...args) => dispatch(addNotification(...args))}
+            />
+          )}
       </div>
-    );
+
+      {
+        <button
+          className="btn-icon btn-meta-outline-secondary pointer btn-fullscreen"
+          onClick={toggleFullScreen}
+          onMouseEnter={() => showTooltip(keymap.TOGGLE_EXPAND)}
+          onMouseLeave={hideTooltip}
+          tabIndex="-1"
+        >
+          {fullScreen ? (
+            <i className="meta-icon-collapse" />
+          ) : (
+            <i className="meta-icon-fullscreen" />
+          )}
+
+          {isTooltipShow === keymap.TOGGLE_EXPAND && (
+            <Tooltips
+              name={keymap.TOGGLE_EXPAND}
+              action={
+                fullScreen
+                  ? counterpart.translate('window.table.collapse')
+                  : counterpart.translate('window.table.expand')
+              }
+              type={''}
+            />
+          )}
+        </button>
+      }
+    </div>
+  );
+};
+
+TableFilter.propTypes = {
+  tabIndex: PropTypes.number.isRequired,
+  forceHeight: PropTypes.number,
+  docType: PropTypes.string,
+  tabId: PropTypes.string,
+  docId: PropTypes.string,
+  toggleFullScreen: PropTypes.func,
+  fullScreen: PropTypes.any,
+  wrapperHeight: PropTypes.number,
+  isBatchEntry: PropTypes.bool,
+  handleBatchEntryToggle: PropTypes.func,
+  quickInputSupport: PropTypes.object,
+  newRecordInputMode: PropTypes.string,
+  allowCreateNew: PropTypes.bool,
+  openTableModal: PropTypes.func,
+  pending: PropTypes.bool,
+};
+
+const generateShortcutActionsArray = (actions, handleTopActionClick) => {
+  const shortcutActions = [];
+
+  for (let i = 0; i < actions.length; i += 1) {
+    const action = actions[i];
+
+    shortcutActions.push({
+      name: `FILTER_ACTION_${i}`,
+      handler: () => handleTopActionClick(action),
+      shortcut: action.shortcut ? action.shortcut.replace('-', '+') : '',
+    });
   }
-}
+
+  return shortcutActions;
+};
+
+//
+//
+//
+// -----------------------------------------
+//
+//
+//
 
 const ActionButton = ({
   caption,
@@ -321,6 +286,7 @@ const ActionButton = ({
   onClick,
   showTooltip,
   hideTooltip,
+  disabled = false,
 }) => {
   return (
     <button
@@ -330,6 +296,7 @@ const ActionButton = ({
       title={description}
       onMouseEnter={showTooltip}
       onMouseLeave={hideTooltip}
+      disabled={disabled}
     >
       {caption}
       {children}
@@ -345,22 +312,7 @@ ActionButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   showTooltip: PropTypes.func,
   hideTooltip: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
-const mapStateToProps = (state, props) => {
-  const { docType: windowId, docId, tabId } = props;
-
-  const tableId = getTableId({ windowId, docId, tabId });
-
-  return {
-    modalVisible: state.windowHandler.modal.visible,
-    selectedRowIds: getSelection()(state, tableId),
-  };
-};
-
-export default connect(mapStateToProps, {
-  fetchTopActions,
-  deleteTopActions,
-  openModal,
-  addNotification,
-})(TableFilter);
+export default TableFilter;

frontend/src/reducers/tables.js
@@ -5,6 +5,7 @@ import { merge } from 'merge-anything';
 
 import * as types from '../constants/ActionTypes';
 import { doesSelectionExist } from '../utils/documentListHelper';
+import { useSelector } from 'react-redux';
 
 export const initialTableState = {
   windowId: null,
@@ -62,6 +63,11 @@ export const getTable = createSelector(selectTableHelper, (table) => table);
 
 const getSelectionData = (state, tableId) =>
   selectTableHelper(state, tableId).selected;
+
+export const useSelectedRowIds = ({ windowId, tabId, docId }) => {
+  const tableId = getTableId({ windowId, docId, tabId });
+  return useSelector((state) => getSelectionData(state, tableId));
+};
 
 /**
  * @method getSelection

commit 06188ad81e927252663ddc37e663d358faa09eb9

fix typo introduced by https://github.com/metasfresh/metasfresh/commit/a10f5799891f9693d10a7d55d87a39baf7c95b84

frontend/src/components/table/TableFilter.js
@@ -197,7 +197,7 @@ const TableFilter = ({
         </div>
         {quickInputSupport && isBatchEntryActive && allowCreateNew && (
           <TableQuickInput
-            docType={windowId}
+            windowId={windowId}
             docId={docId}
             tabId={tabId}
             forceHeight={wrapperHeight ? wrapperHeight : null}