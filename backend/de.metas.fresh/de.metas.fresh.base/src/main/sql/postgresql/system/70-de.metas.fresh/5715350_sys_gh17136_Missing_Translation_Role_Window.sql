554dbc59469757ede0cb7a4f1f861d39c9b4121a
riad.mennour <riad.mennour@gmail.com>
2024-01-15 16:45:55 +0100

    Set the missing translations in Role window

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5715350_sys_gh17136_Missing_Translation_Role_Window.sql
@@ -0,0 +1,165 @@
+-- Run mode: SWING_CLIENT
+
+-- Element: AD_Role_Record_Access_Config_ID
+-- 2024-01-15T15:16:25.295Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Rollen Datensatz-Zugriff Konfiguration', PrintName='Rollen Datensatz-Zugriff Konfiguration',Updated=TO_TIMESTAMP('2024-01-15 16:16:25.295','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=576597 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:16:25.429Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576597,'de_CH')
+;
+
+-- Element: AD_Role_Record_Access_Config_ID
+-- 2024-01-15T15:16:54.121Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Rollen Datensatz-Zugriff Konfiguration', PrintName='Rollen Datensatz-Zugriff Konfiguration',Updated=TO_TIMESTAMP('2024-01-15 16:16:54.121','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=576597 AND AD_Language='de_DE'
+;
+
+-- 2024-01-15T15:16:54.177Z
+UPDATE AD_Element SET Name='Rollen Datensatz-Zugriff Konfiguration', PrintName='Rollen Datensatz-Zugriff Konfiguration' WHERE AD_Element_ID=576597
+;
+
+-- 2024-01-15T15:16:55.620Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(576597,'de_DE')
+;
+
+-- 2024-01-15T15:16:55.673Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(576597,'de_DE')
+;
+
+-- Element: null
+-- 2024-01-15T15:19:04.118Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Berechtigung Anfrage', PrintName='Berechtigung Anfrage',Updated=TO_TIMESTAMP('2024-01-15 16:19:04.118','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=573461 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:19:04.220Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573461,'de_CH')
+;
+
+-- Element: null
+-- 2024-01-15T15:19:43.810Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Berechtigung verweigert', PrintName='Berechtigung verweigert',Updated=TO_TIMESTAMP('2024-01-15 16:19:43.81','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=573538 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:19:43.911Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573538,'de_CH')
+;
+
+-- Element: null
+-- 2024-01-15T15:21:23.501Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Benachrichtigungsgruppen', PrintName='Benachrichtigungsgruppen',Updated=TO_TIMESTAMP('2024-01-15 16:21:23.501','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=573597 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:21:23.601Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573597,'de_CH')
+;
+
+-- Element: null
+-- 2024-01-15T15:21:45.321Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Benachrichtigungsgruppen', PrintName='Benachrichtigungsgruppen',Updated=TO_TIMESTAMP('2024-01-15 16:21:45.321','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=573597 AND AD_Language='de_DE'
+;
+
+-- 2024-01-15T15:21:45.372Z
+UPDATE AD_Element SET Name='Benachrichtigungsgruppen', PrintName='Benachrichtigungsgruppen' WHERE AD_Element_ID=573597
+;
+
+-- 2024-01-15T15:21:46.663Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(573597,'de_DE')
+;
+
+-- 2024-01-15T15:21:46.717Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(573597,'de_DE')
+;
+
+-- Element: AD_Role_TableOrg_Access_ID
+-- 2024-01-15T15:23:54.747Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Rollentabelle Organisation Zugriff', PrintName='Rollentabelle Organisation Zugriff',Updated=TO_TIMESTAMP('2024-01-15 16:23:54.747','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=579926 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:23:54.849Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579926,'de_CH')
+;
+
+-- Element: AD_Role_TableOrg_Access_ID
+-- 2024-01-15T15:24:05.801Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Rollentabelle Organisation Zugriff', PrintName='Rollentabelle Organisation Zugriff',Updated=TO_TIMESTAMP('2024-01-15 16:24:05.801','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=579926 AND AD_Language='de_DE'
+;
+
+-- 2024-01-15T15:24:05.851Z
+UPDATE AD_Element SET Name='Rollentabelle Organisation Zugriff', PrintName='Rollentabelle Organisation Zugriff' WHERE AD_Element_ID=579926
+;
+
+-- 2024-01-15T15:24:07.584Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(579926,'de_DE')
+;
+
+-- 2024-01-15T15:24:07.637Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(579926,'de_DE')
+;
+
+-- Element: Included_Role_ID
+-- 2024-01-15T15:28:05.350Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Enthaltene Rolle', PrintName='Enthaltene Rolle',Updated=TO_TIMESTAMP('2024-01-15 16:28:05.35','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=53896 AND AD_Language='de_DE'
+;
+
+-- 2024-01-15T15:28:05.402Z
+UPDATE AD_Element SET Name='Enthaltene Rolle', PrintName='Enthaltene Rolle' WHERE AD_Element_ID=53896
+;
+
+-- 2024-01-15T15:28:06.714Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(53896,'de_DE')
+;
+
+-- 2024-01-15T15:28:06.765Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(53896,'de_DE')
+;
+
+-- Element: Included_Role_ID
+-- 2024-01-15T15:28:15.800Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Enthaltene Rolle', PrintName='Enthaltene Rolle',Updated=TO_TIMESTAMP('2024-01-15 16:28:15.8','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=53896 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:28:15.903Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(53896,'de_CH')
+;
+
+-- Element: IsAutoRoleLogin
+-- 2024-01-15T15:35:46.942Z
+UPDATE AD_Element_Trl SET Description='Überspringen Sie die Anmeldeseite für die Rolle und übernehmen Sie die Standardeinstellungen', IsTranslated='Y', Name='Rollen-Anmeldeseite überspringen', PrintName='Rollen-Anmeldeseite überspringen',Updated=TO_TIMESTAMP('2024-01-15 16:35:46.94','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=54151 AND AD_Language='de_DE'
+;
+
+-- 2024-01-15T15:35:46.993Z
+UPDATE AD_Element SET Description='Überspringen Sie die Anmeldeseite für die Rolle und übernehmen Sie die Standardeinstellungen', Name='Rollen-Anmeldeseite überspringen', PrintName='Rollen-Anmeldeseite überspringen' WHERE AD_Element_ID=54151
+;
+
+-- 2024-01-15T15:35:48.244Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(54151,'de_DE')
+;
+
+-- 2024-01-15T15:35:48.295Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(54151,'de_DE')
+;
+
+-- Element: IsAutoRoleLogin
+-- 2024-01-15T15:36:12.538Z
+UPDATE AD_Element_Trl SET Description='Überspringen Sie die Anmeldeseite für die Rolle und übernehmen Sie die Standardeinstellungen', IsTranslated='Y', Name='Rollen-Anmeldeseite überspringen', PrintName='Rollen-Anmeldeseite überspringen',Updated=TO_TIMESTAMP('2024-01-15 16:36:12.538','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=54151 AND AD_Language='de_CH'
+;
+
+-- 2024-01-15T15:36:12.642Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(54151,'de_CH')
+;
+
+-- Value: webui.window.addNew.caption
+-- 2024-01-15T15:31:19.748Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Neu hinzufügen',Updated=TO_TIMESTAMP('2024-01-15 16:31:19.748','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=544351
+;
+
+-- Value: webui.window.logOut.caption
+-- 2024-01-15T10:44:11.491Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Abmelden',Updated=TO_TIMESTAMP('2024-01-15 11:44:11.491','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=544345
+;
+
+-- Value: webui.window.settings.caption
+-- 2024-01-15T10:45:03.748Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Einstellungen',Updated=TO_TIMESTAMP('2024-01-15 11:45:03.748','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=544344
+;
+
