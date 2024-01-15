2e46a5f84e81daddbe1c493b87187d58cbd6a2a2
Max Rieck <max.rieck@metasfresh.com>
2024-01-15 10:45:48 +0100

    fix java test

--- backend/de.metas.adempiere.adempiere/base/src/test/java/de/metas/process/JavaProcessTests.java
+++ backend/de.metas.adempiere.adempiere/base/src/test/java/de/metas/process/JavaProcessTests.java
@@ -1,8 +1,31 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2024 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.process;
 
-import static org.adempiere.model.InterfaceWrapperHelper.newInstanceOutOfTrx;
-import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
-
+import de.metas.process.JavaProcess.ProcessCanceledException;
+import de.metas.process.impl.ADPInstanceDAO;
+import de.metas.user.UserId;
+import de.metas.util.Services;
 import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.ad.trx.api.ITrxManager;
 import org.adempiere.ad.trx.api.impl.PlainTrxManager;
@@ -18,10 +41,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.metas.process.JavaProcess.ProcessCanceledException;
-import de.metas.process.impl.ADPInstanceDAO;
-import de.metas.user.UserId;
-import de.metas.util.Services;
+import static org.adempiere.model.InterfaceWrapperHelper.newInstanceOutOfTrx;
+import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
 
 /**
  * Tests {@link JavaProcess} life-cycle management.
@@ -49,7 +70,7 @@ public class JavaProcessTests
 		// TODO: we need to validate AD_PInstance values only when running from ProcessCtl
 		public boolean validateAD_PInstance_Values = false;
 
-		protected MockedJavaProcess()
+		public MockedJavaProcess()
 		{
 			this(false, false); // expectPrepareOutOfTrx=false, expectDoItOutOfTrx=false
 		}
