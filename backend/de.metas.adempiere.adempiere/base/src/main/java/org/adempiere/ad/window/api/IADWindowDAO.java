52ee09dfaf7630bec7e846decebf0f68bb773f48
Max Rieck <32903793+mrie04@users.noreply.github.com>
2024-01-16 16:03:11 +0100

    add health check pipeline (#17133)
    * add health check endpoints
    
    * add health check pipeline
    
    * fix java test
    
    * ClassInstanceProvider.provideInstance: make it work with java17
    
    * JavaProcessTests: sync from master
    
    * extend docker-compose summary
    
    * fix java tests
    
    * fix view keyColumns
    
    ---------
    
    Co-authored-by: Teo Sarca <teo.sarca@metasfresh.com>

--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/window/api/IADWindowDAO.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/window/api/IADWindowDAO.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2024 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package org.adempiere.ad.window.api;
 
 import com.google.common.collect.ImmutableSet;
@@ -114,6 +136,8 @@ public interface IADWindowDAO extends ISingletonService
 	AdTabId copyTabToWindow(I_AD_Tab sourceTab, AdWindowId targetWindowId);
 
 	ImmutableSet<AdWindowId> retrieveAllAdWindowIdsByTableId(AdTableId adTableId);
+
+	ImmutableSet<AdWindowId> retrieveAllActiveAdWindowIds();
 
 	Optional<QuickInputConfigLayout> getQuickInputConfigLayout(AdTabId adTabId);
 }
