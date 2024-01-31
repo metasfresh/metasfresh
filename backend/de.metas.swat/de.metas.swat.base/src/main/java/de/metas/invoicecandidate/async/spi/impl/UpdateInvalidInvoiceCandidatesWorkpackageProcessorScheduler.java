b2e8e4868b44646e5914c5d2f53ddec977a59154
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2024-01-31 08:59:54 +0100

    Address two sources for unneeded workpackages
    ..that can cause users havig to wait very long for invoice candidates.

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/async/spi/impl/UpdateInvalidInvoiceCandidatesWorkpackageProcessorScheduler.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/async/spi/impl/UpdateInvalidInvoiceCandidatesWorkpackageProcessorScheduler.java
@@ -1,9 +1,19 @@
 package de.metas.invoicecandidate.async.spi.impl;
 
+import com.google.common.collect.ImmutableSet;
+import de.metas.async.api.IQueueDAO;
+import de.metas.async.api.IWorkPackageQuery;
+import de.metas.async.api.impl.WorkPackageQuery;
+import de.metas.async.model.I_C_Queue_WorkPackage;
+import de.metas.async.processor.QueuePackageProcessorId;
 import de.metas.async.spi.WorkpackagesOnCommitSchedulerTemplate;
+import de.metas.common.util.Check;
 import de.metas.invoicecandidate.api.IInvoiceCandUpdateSchedulerRequest;
+import de.metas.util.Services;
+import lombok.NonNull;
+import org.compiere.model.IQuery;
 
-import java.util.Optional;
+import javax.annotation.Nullable;
 import java.util.Properties;
 
 /*
@@ -37,19 +47,19 @@ class UpdateInvalidInvoiceCandidatesWorkpackageProcessorScheduler extends Workpa
 	}
 
 	@Override
-	protected Properties extractCtxFromItem(final IInvoiceCandUpdateSchedulerRequest item)
+	protected Properties extractCtxFromItem(@NonNull final IInvoiceCandUpdateSchedulerRequest item)
 	{
 		return item.getCtx();
 	}
 
 	@Override
-	protected String extractTrxNameFromItem(final IInvoiceCandUpdateSchedulerRequest item)
+	protected String extractTrxNameFromItem(@NonNull final IInvoiceCandUpdateSchedulerRequest item)
 	{
 		return item.getTrxName();
 	}
 
 	@Override
-	protected Object extractModelToEnqueueFromItem(final Collector collector, final IInvoiceCandUpdateSchedulerRequest item)
+	protected Object extractModelToEnqueueFromItem(@Nullable final Collector collector, @Nullable final IInvoiceCandUpdateSchedulerRequest item)
 	{
 		return null; // there is no actual model to be collected
 	}
@@ -58,5 +68,34 @@ class UpdateInvalidInvoiceCandidatesWorkpackageProcessorScheduler extends Workpa
 	protected boolean isEnqueueWorkpackageWhenNoModelsEnqueued()
 	{
 		return true;
+	}
+
+	/**
+	 * Return {@code true} only if there is not yet an existing unprocessed workpackage for the {@link UpdateInvalidInvoiceCandidatesWorkpackageProcessor}.
+	 * This is important to avoid making metasfresh to deal with a large number of such WBs where actually the first one already did all the work.
+	 */
+	protected boolean isEligibleForScheduling(@NonNull final IInvoiceCandUpdateSchedulerRequest item)
+	{
+		final IQueueDAO queueDAO = Services.get(IQueueDAO.class);
+
+		final String classname = UpdateInvalidInvoiceCandidatesWorkpackageProcessor.class.getName();
+		final QueuePackageProcessorId packageProcessorId = queueDAO.retrieveQueuePackageProcessorIdFor(classname);
+		;
+
+		// if there is already a workpackage, don't enqueue another one
+		final ImmutableSet<QueuePackageProcessorId> packageProcessorIds = ImmutableSet.of(
+				Check.assumeNotNull(
+						packageProcessorId,
+						"There is an active C_Queue_PackageProcessor record for classname {}." + classname));
+
+		final IWorkPackageQuery existingWorkpackageQuery = new WorkPackageQuery()
+				.setPackageProcessorIds(packageProcessorIds)
+				.setProcessed(false)
+				.setError(false)
+				.setReadyForProcessing(true);
+
+		final IQuery<I_C_Queue_WorkPackage> query = queueDAO.createQuery(item.getCtx(), existingWorkpackageQuery);
+
+		return !query.anyMatch();
 	}
 }
