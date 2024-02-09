a56bf39a69ae24b4c46a543eddc186e97d38efa4
cp-ps <cp-ps@pevesoft.ro>
2024-02-09 18:32:37 +0200

    update trl

--- backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5716850_sys_gh17310_fix_trl.sql
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5716850_sys_gh17310_fix_trl.sql
@@ -1,22 +1,22 @@
 -- Reference: Create Shipment Policy
 -- Value: CL
 -- ValueName: CREATE_COMPLETE_CLOSE
--- 2024-02-09T16:25:58.921Z
-UPDATE AD_Ref_List SET Name='Erstellen, Ausfüllen und Abschließen',Updated=TO_TIMESTAMP('2024-02-09 18:25:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=543626
+-- 2024-02-09T16:31:59.869Z
+UPDATE AD_Ref_List SET Name='Erstellen, fertigstellen und Lieferdispo schliessen',Updated=TO_TIMESTAMP('2024-02-09 18:31:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=543626
 ;
 
 -- Reference Item: Create Shipment Policy -> CL_CREATE_COMPLETE_CLOSE
--- 2024-02-09T16:26:04.415Z
-UPDATE AD_Ref_List_Trl SET Name='Erstellen, Ausfüllen und Abschließen',Updated=TO_TIMESTAMP('2024-02-09 18:26:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Ref_List_ID=543626
+-- 2024-02-09T16:32:04.800Z
+UPDATE AD_Ref_List_Trl SET Name='Erstellen, fertigstellen und Lieferdispo schliessen',Updated=TO_TIMESTAMP('2024-02-09 18:32:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Ref_List_ID=543626
 ;
 
 -- Reference Item: Create Shipment Policy -> CL_CREATE_COMPLETE_CLOSE
--- 2024-02-09T16:26:07.880Z
-UPDATE AD_Ref_List_Trl SET Name='Erstellen, Ausfüllen und Abschließen',Updated=TO_TIMESTAMP('2024-02-09 18:26:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Ref_List_ID=543626
+-- 2024-02-09T16:32:08.338Z
+UPDATE AD_Ref_List_Trl SET Name='Erstellen, fertigstellen und Lieferdispo schliessen',Updated=TO_TIMESTAMP('2024-02-09 18:32:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Ref_List_ID=543626
 ;
 
 -- Reference Item: Create Shipment Policy -> CL_CREATE_COMPLETE_CLOSE
--- 2024-02-09T16:26:11.004Z
-UPDATE AD_Ref_List_Trl SET Name='Erstellen, Ausfüllen und Abschließen',Updated=TO_TIMESTAMP('2024-02-09 18:26:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Ref_List_ID=543626
+-- 2024-02-09T16:32:11.226Z
+UPDATE AD_Ref_List_Trl SET Name='Erstellen, fertigstellen und Lieferdispo schliessen',Updated=TO_TIMESTAMP('2024-02-09 18:32:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Ref_List_ID=543626
 ;
 
