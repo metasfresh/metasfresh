1bd38971a2f472456f1a317c1406fc474c276a09
Teo Sarca <teo.sarca@metasfresh.com>
2024-02-13 16:53:02 +0200

    mobile UI: picking: workplace scanner - be more robust on errors (#17365)

--- misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/picking/WorkplaceScanner.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/containers/activities/picking/WorkplaceScanner.jsx
@@ -4,12 +4,15 @@ import { parseWorkplaceQRCodeString } from '../../../utils/qrCode/workplace';
 import PropTypes from 'prop-types';
 import { trl } from '../../../utils/translations';
 import { assignWorkplace } from '../../../apps/workplaceManager/api';
+import { toastError } from '../../../utils/toast';
 
 const WorkplaceScanner = ({ onComplete }) => {
   const resolveScannedBarcode = ({ scannedBarcode }) => parseWorkplaceQRCodeString(scannedBarcode);
 
   const onResolvedResult = ({ workplaceId }) => {
-    assignWorkplace(workplaceId).then(() => onComplete());
+    assignWorkplace(workplaceId)
+      .then(() => onComplete())
+      .catch((axiosError) => toastError({ axiosError }));
   };
 
   return (
@@ -17,6 +20,7 @@ const WorkplaceScanner = ({ onComplete }) => {
       resolveScannedBarcode={resolveScannedBarcode}
       onResolvedResult={onResolvedResult}
       inputPlaceholderText={trl('components.BarcodeScannerComponent.scanWorkplacePlaceholder')}
+      continuousRunning={true}
     />
   );
 };
