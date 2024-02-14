0cf1a87120ae4c1811f76fef9db4ce95b3c4fd89
riad.mennour <riad.mennour@gmail.com>
2024-02-14 15:36:17 +0100

    Add currency in Dunning Type window

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5717270_sys_gh17633_Add_Currency_Dunning_Type_Window.sql
@@ -0,0 +1,8 @@
+-- Run mode: SWING_CLIENT
+
+-- UI Element: Mahnart(159,de.metas.dunning) -> Mahnart(231,D) -> main -> 10 -> description.Currency
+-- Column: C_Dunning.C_Currency_ID
+-- 2024-02-14T14:21:54.253Z
+UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2024-02-14 14:21:54.253000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=560117
+;
+
