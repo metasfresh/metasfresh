5d84bf3f68262ae16a6acbfcdc395baee2c6ae8e
riad.mennour <riad.mennour@gmail.com>
2024-02-15 17:39:09 +0100

    merging `intensive_care_uat`

---
+++ misc/services/mobile-webui/mobile-webui-frontend/src/apps/workplaceManager/actionTypes.js
@@ -0,0 +1,1 @@
+export const SET_WORKPLACE = 'workplaceManager/SET_WORKPLACE';
--- misc/services/mobile-webui/mobile-webui-frontend/src/apps/workplaceManager/containers/WorkplaceManagerScreen.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/apps/workplaceManager/containers/WorkplaceManagerScreen.js
@@ -19,27 +19,30 @@
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getWorkplaceInfoFromGlobalState } from '../reducers';
 import WorkplaceInfoComponent from '../components/WorkplaceInfoComponent';
 import { useRouteMatch } from 'react-router-dom';
 import { pushHeaderEntry } from '../../../actions/HeaderActions';
 import ButtonWithIndicator from '../../../components/buttons/ButtonWithIndicator';
 import { trl } from '../../../utils/translations';
-import BarcodeScannerComponent from '../../../components/BarcodeScannerComponent';
-import * as api from '../../../api/workplace';
-import { toastError } from '../../../utils/toast';
+import * as api from '../api';
+import { setWorkplace } from '../actions';
+import { push } from 'connected-react-router';
+import { scanAnythingLocation } from '../../scanAnything/routes';
 
 const WorkplaceManagerScreen = () => {
   const { url } = useRouteMatch();
   const dispatch = useDispatch();
-
-  const [workplace, setWorkplace] = useState();
-  console.log('ctor', { workplace });
+  const workplaceInfo = useSelector((state) => getWorkplaceInfoFromGlobalState(state));
+  if (!workplaceInfo) {
+    dispatch(push(scanAnythingLocation()));
+  }
 
   useEffect(() => {
     // IMPORTANT, else it won't restore the title when we move back to this screen
-    dispatch(pushHeaderEntry({ location: url }));
+    dispatch(pushHeaderEntry({ caption: trl('workplaceManager.title'), location: url }));
   }, []);
 
   const onBarcodeScanned = ({ scannedBarcode }) => {
@@ -54,37 +57,27 @@ const WorkplaceManagerScreen = () => {
   };
 
   const onAssignClick = () => {
-    api
-      .assignWorkplace(workplace.id)
-      .then((workplace) => setWorkplace(workplace))
-      .catch((axiosError) => toastError({ axiosError }));
+    api.assignWorkplace(workplaceInfo.id).then((workplaceInfo) => dispatch(setWorkplace({ workplaceInfo })));
   };
 
   const onScanAgainClick = () => {
-    setWorkplace(null);
+    dispatch(push(scanAnythingLocation()));
   };
 
-  if (workplace) {
-    return (
-      <>
-        <WorkplaceInfoComponent workplaceInfo={workplace} />
-        <div className="pt-3 section">
-          {!workplace.userAssigned && (
-            <ButtonWithIndicator
-              caption={trl('workplaceManager.action.assign.buttonCaption')}
-              onClick={onAssignClick}
-            />
-          )}
-          <ButtonWithIndicator
-            caption={trl('workplaceManager.action.scanAgain.buttonCaption')}
-            onClick={onScanAgainClick}
-          />
-        </div>
-      </>
-    );
-  } else {
-    return <BarcodeScannerComponent onResolvedResult={onBarcodeScanned} continuousRunning={true} />;
-  }
+  return (
+    <>
+      <WorkplaceInfoComponent workplaceInfo={workplaceInfo} />
+      <div className="pt-3 section">
+        {!workplaceInfo.userAssigned && (
+          <ButtonWithIndicator caption={trl('workplaceManager.action.assign.buttonCaption')} onClick={onAssignClick} />
+        )}
+        <ButtonWithIndicator
+          caption={trl('workplaceManager.action.scanAgain.buttonCaption')}
+          onClick={onScanAgainClick}
+        />
+      </div>
+    </>
+  );
 };
 
 export default WorkplaceManagerScreen;
