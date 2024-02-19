0da5d2da43026e2f6c5ca11912d7376dc32d16b9
dm-ps <dragos.moca@pevesoft.ro>
2024-02-19 21:57:28 +0200

    Add description in DE refs: https://github.com/metasfresh/metasfresh/issues/17393

---
+++ backend/de.metas.invoice.webui/src/main/sql/postgresql/system/45-de.metas.invoice/5717620_sys_gh17393_EnqueueForInvoicing_Description.sql
@@ -0,0 +1,26 @@
+-- Run mode: SWING_CLIENT
+
+-- Process: C_Invoice_Candidate_EnqueueSelectionForInvoicing(de.metas.invoicecandidate.process.C_Invoice_Candidate_EnqueueSelectionForInvoicing)
+-- 2024-02-19T19:47:48.595Z
+UPDATE AD_Process_Trl SET Description='Das Feld „Überschriebenes Fälligkeitsdatum“ wird nur verwendet, wenn für die entsprechende Zahlungsbedingung das Flag „Überschreiben des Fälligkeitsdatums zulassen“ auf „Ja“ gesetzt ist.',Updated=TO_TIMESTAMP('2024-02-19 19:47:48.595000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=540304
+;
+
+-- 2024-02-19T19:47:48.606Z
+UPDATE AD_Process SET Description='Das Feld „Überschriebenes Fälligkeitsdatum“ wird nur verwendet, wenn für die entsprechende Zahlungsbedingung das Flag „Überschreiben des Fälligkeitsdatums zulassen“ auf „Ja“ gesetzt ist.', Updated=TO_TIMESTAMP('2024-02-19 19:47:48.605000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC' WHERE AD_Process_ID=540304
+;
+
+-- Process: C_Invoice_Candidate_EnqueueSelectionForInvoicing(de.metas.invoicecandidate.process.C_Invoice_Candidate_EnqueueSelectionForInvoicing)
+-- 2024-02-19T19:47:53.385Z
+UPDATE AD_Process_Trl SET Description='Das Feld „Überschriebenes Fälligkeitsdatum“ wird nur verwendet, wenn für die entsprechende Zahlungsbedingung das Flag „Überschreiben des Fälligkeitsdatums zulassen“ auf „Ja“ gesetzt ist.',Updated=TO_TIMESTAMP('2024-02-19 19:47:53.385000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Process_ID=540304
+;
+
+-- Process: C_Invoice_Candidate_EnqueueSelectionForInvoicing(de.metas.invoicecandidate.process.C_Invoice_Candidate_EnqueueSelectionForInvoicing)
+-- 2024-02-19T19:47:57.439Z
+UPDATE AD_Process_Trl SET Description='Das Feld „Überschriebenes Fälligkeitsdatum“ wird nur verwendet, wenn für die entsprechende Zahlungsbedingung das Flag „Überschreiben des Fälligkeitsdatums zulassen“ auf „Ja“ gesetzt ist.',Updated=TO_TIMESTAMP('2024-02-19 19:47:57.438000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='it_CH' AND AD_Process_ID=540304
+;
+
+-- Process: C_Invoice_Candidate_EnqueueSelectionForInvoicing(de.metas.invoicecandidate.process.C_Invoice_Candidate_EnqueueSelectionForInvoicing)
+-- 2024-02-19T19:48:00.914Z
+UPDATE AD_Process_Trl SET Description='Das Feld „Überschriebenes Fälligkeitsdatum“ wird nur verwendet, wenn für die entsprechende Zahlungsbedingung das Flag „Überschreiben des Fälligkeitsdatums zulassen“ auf „Ja“ gesetzt ist.',Updated=TO_TIMESTAMP('2024-02-19 19:48:00.914000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='it_IT' AND AD_Process_ID=540304
+;
+
