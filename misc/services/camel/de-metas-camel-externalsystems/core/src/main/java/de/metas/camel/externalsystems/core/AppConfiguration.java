ec46539cf91949772bdb9c2f2fc3aa28d899a662
Max Rieck <32903793+mrie04@users.noreply.github.com>
2024-02-28 17:03:39 +0100

    fix external system startup (#17496)
    (cherry picked from commit f9e9dc2cbb8dbcd7bd170da031e2157205503940)
    
    # Conflicts:
    #	misc/services/camel/de-metas-camel-externalsystems/core/src/main/java/de/metas/camel/externalsystems/core/AppConfiguration.java

--- misc/services/camel/de-metas-camel-externalsystems/core/src/main/java/de/metas/camel/externalsystems/core/AppConfiguration.java
+++ misc/services/camel/de-metas-camel-externalsystems/core/src/main/java/de/metas/camel/externalsystems/core/AppConfiguration.java
@@ -22,13 +22,25 @@
 
 package de.metas.camel.externalsystems.core;
 
+<<<<<<< HEAD
+=======
+import de.metas.camel.externalsystems.common.ExternalSystemCamelConstants;
+import de.metas.camel.externalsystems.core.authorization.MetasfreshAuthorizationTokenNotifier;
+import de.metas.camel.externalsystems.core.authorization.provider.MetasfreshAuthProvider;
+import de.metas.common.util.Check;
+import lombok.NonNull;
+>>>>>>> f9e9dc2cbb8 (fix external system startup (#17496))
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+<<<<<<< HEAD
 import javax.annotation.PostConstruct;
+=======
+import static de.metas.camel.externalsystems.core.authorization.CustomMessageToMFRouteBuilder.CUSTOM_TO_MF_ROUTE_ID;
+>>>>>>> f9e9dc2cbb8 (fix external system startup (#17496))
 
 @Configuration
 public class AppConfiguration
@@ -36,7 +48,9 @@ public class AppConfiguration
 	private final ApplicationContext context;
 	private final CamelContext camelContext;
 
-	public AppConfiguration(final ApplicationContext context, final CamelContext camelContext)
+	public AppConfiguration(
+			final ApplicationContext context,
+			final CamelContext camelContext)
 	{
 		this.context = context;
 		this.camelContext = camelContext;
@@ -48,10 +62,56 @@ public class AppConfiguration
 		return camelContext.createProducerTemplate();
 	}
 
+<<<<<<< HEAD
 	@PostConstruct
 	public void auditEventNotifier()
 	{
 		camelContext.getManagementStrategy()
 				.addEventNotifier(new AuditEventNotifier(context.getBean(ProducerTemplate.class)));
+=======
+	@Bean
+	public CustomRouteController customRouteController()
+	{
+		return new CustomRouteController(this.camelContext);
+	}
+
+	@Bean
+	CamelContextConfiguration contextConfiguration(
+			@NonNull final MetasfreshAuthProvider metasfreshAuthProvider,
+			@NonNull final CustomRouteController customRouteController,
+			@NonNull final ProducerTemplate producerTemplate
+			)
+	{
+		return new CamelContextConfiguration()
+		{
+			@Override
+			public void beforeApplicationStart(final CamelContext camelContext)
+			{
+				camelContext.setAutoStartup(false);
+
+				final String metasfreshAPIBaseURL = context.getEnvironment().getProperty(ExternalSystemCamelConstants.MF_API_BASE_URL_PROPERTY);
+
+				if (Check.isBlank(metasfreshAPIBaseURL))
+				{
+					throw new RuntimeException("Missing mandatory property! property = " + ExternalSystemCamelConstants.MF_API_BASE_URL_PROPERTY);
+				}
+
+				camelContext.getManagementStrategy()
+						.addEventNotifier(new MetasfreshAuthorizationTokenNotifier(metasfreshAuthProvider, metasfreshAPIBaseURL, customRouteController, producerTemplate));
+
+				camelContext.getManagementStrategy()
+						.addEventNotifier(new AuditEventNotifier(producerTemplate));
+			}
+
+			@Override
+			public void afterApplicationStart(final CamelContext camelContext)
+			{
+				customRouteController().startAlwaysRunningRoutes();
+
+				producerTemplate
+						.sendBody("direct:" + CUSTOM_TO_MF_ROUTE_ID, "Trigger external system authentication!");
+			}
+		};
+>>>>>>> f9e9dc2cbb8 (fix external system startup (#17496))
 	}
 }
