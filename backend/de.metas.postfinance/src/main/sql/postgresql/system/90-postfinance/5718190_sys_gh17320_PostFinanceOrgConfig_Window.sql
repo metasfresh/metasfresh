7daee65501956628d1ba75c03a0441860e7f9e2a
Max Rieck <max.rieck@metasfresh.com>
2024-03-06 07:40:33 +0100

    adjust config to simple fixed file-url

--- backend/de.metas.postfinance/src/main/sql/postgresql/system/90-postfinance/5718190_sys_gh17320_PostFinanceOrgConfig_Window.sql
+++ backend/de.metas.postfinance/src/main/sql/postgresql/system/90-postfinance/5718190_sys_gh17320_PostFinanceOrgConfig_Window.sql
@@ -109,3 +109,9 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP(
 UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2024-02-29 19:17:03.288','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=622121
 ;
 
+-- Tab: PostFinance Kunden-Registrierungsnachricht(541769,D) -> PostFinance Kunden-Registrierungsnachricht
+-- Table: PostFinance_Customer_Registration_Message
+-- 2024-03-01T14:33:21.103Z
+UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_TIMESTAMP('2024-03-01 16:33:21.103','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Tab_ID=547410
+;
+
