eaf00e24c247dcea7d8c6097ba7ff2ffd31210c4
Riad MENNOUR <93593841+riadmen@users.noreply.github.com>
2024-03-07 17:14:26 +0100

    Improve API-Request-Logging during retry (#17486) (#17552)
    * Improve logging for API-requests if no user is in context
    
    * Skipping execution of the housekeeping tasks
    
    ---------
    
    Co-authored-by: Tobias Schöneberg <metas-ts@users.noreply.github.com>

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5718710_sys_gh17549_SkipHouseKeeping_SysConfig.sql
@@ -0,0 +1,4 @@
+-- 2024-03-07T09:12:02.570Z
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541712,'S',TO_TIMESTAMP('2024-03-07 10:12:02','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.housekeeping.SkipHouseKeeping.ApiAuditHouseKeepingTask',TO_TIMESTAMP('2024-03-07 10:12:02','YYYY-MM-DD HH24:MI:SS'),100,'Y')
+;
+
