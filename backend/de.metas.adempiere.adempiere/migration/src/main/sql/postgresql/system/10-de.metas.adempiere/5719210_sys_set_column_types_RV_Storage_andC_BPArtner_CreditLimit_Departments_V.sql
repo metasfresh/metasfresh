e479867629605c26ecea1283705d6487b66739d1
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2024-03-15 16:14:19 +0100

    Specify column-types for views C_BPartner_CreditLimit_Departments_V and RV_Storage (#17611)
    * specify view column types
    
    (cherry picked from commit e21aad79649aa12b97b59383563bb9933956b13d)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5719210_sys_set_column_types_RV_Storage_andC_BPArtner_CreditLimit_Departments_V.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5719210_sys_set_column_types_RV_Storage_andC_BPArtner_CreditLimit_Departments_V.sql
@@ -1,6 +1,46 @@
--- DROP VIEW IF EXISTS C_BPartner_CreditLimit_Departments_V;
 
+DROP VIEW RV_Storage;
 
+CREATE OR REPLACE VIEW RV_Storage AS
+SELECT s.ad_client_id,
+       s.ad_org_id,
+       s.m_product_id,
+       p.value,
+       p.name,
+       p.description,
+       p.upc,
+       p.sku,
+       p.c_uom_id,
+       p.m_product_category_id,
+       p.classification,
+       p.weight,
+       p.volume,
+       p.versionno,
+       p.guaranteedays,
+       p.guaranteedaysmin,
+       s.m_locator_id,
+       l.m_warehouse_id,
+       l.x,
+       l.y,
+       l.z,
+       s.qtyonhand,
+       s.qtyreserved,
+       s.qtyonhand - s.qtyreserved AS qtyavailable,
+       s.qtyordered,
+       s.datelastinventory,
+       s.m_attributesetinstance_id,
+       asi.m_attributeset_id,
+       NULL::numeric               AS shelflifedays,
+       NULL::numeric               AS goodfordays,
+       NULL::numeric
+                                   AS shelfliferemainingpct
+FROM m_storage s
+         JOIN m_locator l ON s.m_locator_id = l.m_locator_id
+         JOIN m_product p ON s.m_product_id = p.m_product_id
+         LEFT JOIN m_attributesetinstance asi ON s.m_attributesetinstance_id = asi.m_attributesetinstance_id
+;
+
+DROP VIEW IF EXISTS C_BPartner_CreditLimit_Departments_V;
 CREATE OR REPLACE VIEW C_BPartner_CreditLimit_Departments_V
 AS
 SELECT sectionGroupPartner.c_bpartner_id                                                                            AS Section_Group_Partner_ID,
@@ -9,7 +49,7 @@ SELECT sectionGroupPartner.c_bpartner_id                                        
        dep.m_department_id,
        ac.C_Currency_ID                                                                                        AS C_Currency_ID,
 
-	   getSOCreditUsedForSectionGroupPartnerDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)                   AS SO_CreditUsed,
+       getSOCreditUsedForSectionGroupPartnerDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)                   AS SO_CreditUsed,
        getOpenOrderAmtForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)                          AS M_Department_Order_OpenAmt,
        getDeliveryCreditUsedForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id)                    AS Delivery_CreditUsed,
        COALESCE(getTotalOrderValueForSectionGroupDepartment(sectionGroupPartner.c_bpartner_id, dep.m_department_id), 0)          AS TotalOrderValue,
