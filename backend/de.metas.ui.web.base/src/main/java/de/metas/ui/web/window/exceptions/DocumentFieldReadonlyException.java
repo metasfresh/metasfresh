7cefe885d532e7cc032122ccc5c2ba5ddbe8f6f2
Riad MENNOUR <93593841+riadmen@users.noreply.github.com>
2024-03-18 10:13:15 +0100

    Adding `DocumentFieldReadonlyException` error message (#17620)
    * Adding DocumentFieldReadonlyException error message
    
    * Update backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5719270_sys_gh17619_DocumentFieldReadonlyException_Error_Msg.sql
    
    Co-authored-by: Max Rieck <32903793+mrie04@users.noreply.github.com>
    
    * Update backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5719270_sys_gh17619_DocumentFieldReadonlyException_Error_Msg.sql
    
    Co-authored-by: Max Rieck <32903793+mrie04@users.noreply.github.com>
    
    * Simplify DocumentFieldReadonlyException methode
    
    ---------
    
    Co-authored-by: Max Rieck <32903793+mrie04@users.noreply.github.com>

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/window/exceptions/DocumentFieldReadonlyException.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/window/exceptions/DocumentFieldReadonlyException.java
@@ -1,8 +1,12 @@
 package de.metas.ui.web.window.exceptions;
 
+import de.metas.i18n.AdMessageKey;
+import lombok.NonNull;
 import org.adempiere.exceptions.AdempiereException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.annotation.Nullable;
 
 /*
  * #%L
@@ -30,13 +34,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class DocumentFieldReadonlyException extends AdempiereException
 {
-	public DocumentFieldReadonlyException(final String fieldName, final Object value)
-	{
-		super(buildMsg(fieldName, value));
-	}
+	public static final AdMessageKey CHANGING_READONLY_FIELD_NOT_ALLOWED_ERROR = AdMessageKey.of("de.metas.ui.web.window.exceptions.DocumentFieldReadonlyException.Msg");
 
-	private static String buildMsg(final String fieldName, final Object value)
+	public DocumentFieldReadonlyException(@NonNull final String fieldName, @Nullable final Object value)
 	{
-		return "Changing " + fieldName + " to '" + value + "' is not allowed because the field is readonly";
+		super(CHANGING_READONLY_FIELD_NOT_ALLOWED_ERROR, fieldName, value);
 	}
 }
+
