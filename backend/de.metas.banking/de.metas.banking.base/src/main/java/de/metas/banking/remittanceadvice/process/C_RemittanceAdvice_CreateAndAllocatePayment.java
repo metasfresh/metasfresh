610912398a46913739f33df7221dce83e59820ea
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-03-19 17:00:03 +0200

    refactoring

--- backend/de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/remittanceadvice/process/C_RemittanceAdvice_CreateAndAllocatePayment.java
+++ backend/de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/remittanceadvice/process/C_RemittanceAdvice_CreateAndAllocatePayment.java
@@ -34,6 +34,7 @@ import de.metas.banking.payment.paymentallocation.service.PaymentAllocationResul
 import de.metas.banking.payment.paymentallocation.service.PaymentAllocationService;
 import de.metas.bpartner.BPartnerBankAccountId;
 import de.metas.bpartner.BPartnerId;
+import de.metas.common.util.CoalesceUtil;
 import de.metas.currency.Amount;
 import de.metas.invoice.InvoiceAmtMultiplier;
 import de.metas.invoice.InvoiceId;
@@ -260,7 +261,7 @@ public class C_RemittanceAdvice_CreateAndAllocatePayment extends JavaProcess
 				.currencyId(remittanceAdvice.getRemittedAmountCurrencyId())
 				.payAmt(remittanceAdvice.getRemittedAmountSum())
 				.dateAcct(CoalesceUtil.coalesce(TimeUtil.asLocalDate(remittanceAdvice.getPaymentDate()),
-						 TimeUtil.asLocalDate(remittanceAdvice.getDocumentDate())))
+												TimeUtil.asLocalDate(remittanceAdvice.getDocumentDate())))
 				.dateTrx(TimeUtil.asLocalDate(remittanceAdvice.getDocumentDate()))
 				.tenderType(TenderType.DirectDeposit)
 				.createAndProcess();
