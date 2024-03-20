8e6683cb2a7ff795b03a3795ac271980b03d454d
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2024-03-20 18:02:14 +0100

    port api oome fix from master (#17640)
    * Cache API improvements for observability (REST API) and configuration (#16625)
    
    Restrict the sizes of our CCaches
    refs: https://github.com/metasfresh/metasfresh/issues/16705
    
    (cherry picked from commit 0eed8b1baf60afa65dbb554feaadd6c29a22d2b7)
    
    solved Conflicts:
    	backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/InvoiceCandBLCreateInvoices.java
    
    ------
    
    * minor unrelated fix
    * fix cucumber-test (port from master)
    
    ---------
    
    Co-authored-by: Teo Sarca <teo.sarca@metasfresh.com>

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/i18n/impl/MsgBL.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/i18n/impl/MsgBL.java
@@ -1,5 +1,6 @@
 package de.metas.i18n.impl;
 
+import de.metas.i18n.AdMessageId;
 import de.metas.i18n.AdMessageKey;
 import de.metas.i18n.IMsgBL;
 import de.metas.i18n.ITranslatableString;
@@ -13,12 +14,11 @@ import org.compiere.util.Env;
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 
 /**
- *
  * This implementation delegates to {@link Msg} and is therefore coupled with the database.
- *
  */
 @SuppressWarnings("deprecation")
 public class MsgBL implements IMsgBL
@@ -129,7 +129,24 @@ public class MsgBL implements IMsgBL
 	@Override
 	public String getBaseLanguageMsg(@NonNull final AdMessageKey adMessage, @Nullable final Object... msgParameters)
 	{
-		return TranslatableStrings.adMessage(adMessage, msgParameters)
-				.translate(Language.getBaseAD_Language());
+		return TranslatableStrings.adMessage(adMessage, msgParameters).translate(Language.getBaseAD_Language());
+	}
+
+	@Override
+	public Optional<AdMessageId> getIdByAdMessage(@NonNull final AdMessageKey adMessage)
+	{
+		return Msg.toMap().getIdByAdMessage(adMessage);
+	}
+
+	@Override
+	public boolean isMessageExists(AdMessageKey adMessage)
+	{
+		return Msg.toMap().isMessageExists(adMessage);
+	}
+
+	@Override
+	public Optional<AdMessageKey> getAdMessageKeyById(final AdMessageId adMessageId)
+	{
+		return Msg.toMap().getAdMessageKeyById(adMessageId);
 	}
 }
