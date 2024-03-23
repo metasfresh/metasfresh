b87ac18470feca622087d027c381e74e13601ba5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2024-03-23 18:54:16 +0100

    cleanup

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/externalreference/S_ExternalReference_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/externalreference/S_ExternalReference_StepDef.java
@@ -299,16 +299,6 @@ public class S_ExternalReference_StepDef
 		}
 	}
 
-	// @And("metasfresh contains S_ExternalReferences:")
-	// public void metasfresh_contains_s_external_reference(@NonNull final DataTable dataTable)
-	// {
-	// 	final List<Map<String, String>> tableRows = dataTable.asMaps(String.class, String.class);
-	// 	for (final Map<String, String> tableRow : tableRows)
-	// 	{
-	// 		createS_ExternalReference(tableRow);
-	// 	}
-	// }
-
 	@And("remove external reference if exists:")
 	public void remove_external_reference_if_exists(@NonNull final DataTable dataTable)
 	{
@@ -318,91 +308,6 @@ public class S_ExternalReference_StepDef
 			removeExternalReferenceIfExists(tableRow);
 		}
 	}
-
-	// @Given("metasfresh contains S_ExternalReferences")
-	// public void theUserAddsBpartnerExternalReference(@NonNull final DataTable dataTable)
-	// {
-	// 	final List<Map<String, String>> dataTableEntries = dataTable.asMaps();
-	//
-	// 	final IQueryOrderBy orderBy =
-	// 			queryBL.createQueryOrderByBuilder(I_C_BPartner.class)
-	// 					.addColumn(I_C_BPartner.COLUMN_C_BPartner_ID)
-	// 					.createQueryOrderBy();
-	//
-	// 	final List<JsonMetasfreshId> bPartnerIds = queryBL.createQueryBuilder(I_C_BPartner.class)
-	// 			.addOnlyActiveRecordsFilter()
-	// 			.create()
-	// 			.setOrderBy(orderBy)
-	// 			.list()
-	// 			.stream()
-	// 			.map(bPartner -> JsonMetasfreshId.of(bPartner.getC_BPartner_ID())).collect(Collectors.toList());
-	//
-	// 	for (final Map<String, String> dataTableEntry : dataTableEntries)
-	// 	{
-	// 		final String externalSystemName = DataTableUtil.extractStringForColumnName(dataTableEntry, I_S_ExternalReference.COLUMNNAME_ExternalSystem);
-	// 		final String externalId = DataTableUtil.extractStringForColumnName(dataTableEntry, I_S_ExternalReference.COLUMNNAME_ExternalReference);
-	// 		final IExternalReferenceType externalReferenceType = getExternalReferenceType(DataTableUtil.extractStringForColumnName(dataTableEntry, I_S_ExternalReference.COLUMNNAME_Type));
-	//
-	// 		final JsonMetasfreshId metasfreshId;
-	// 		if (externalReferenceType.equals(BPartnerExternalReferenceType.BPARTNER))
-	// 		{
-	// 			metasfreshId = bPartnerIds.get(dataTableEntries.indexOf(dataTableEntry));
-	// 		}
-	// 		else
-	// 		{
-	// 			throw new AdempiereException("No implementation for external reference type.");
-	// 		}
-	//
-	// 		final IExternalSystem externalSystem = externalSystems.ofCode(externalSystemName)
-	// 				.orElseThrow(() -> new InvalidIdentifierException("systemName", externalSystemName));
-	//
-	// 		final ExternalReference externalReference = ExternalReference.builder()
-	// 				.orgId(ORG_ID)
-	// 				.externalSystem(externalSystem)
-	// 				.externalReference(externalId)
-	// 				.externalReferenceType(externalReferenceType)
-	// 				.recordId(metasfreshId.getValue())
-	// 				.build();
-	//
-	// 		externalReferenceRepository.save(externalReference);
-	// 	}
-	// }
-
-	// private void createS_ExternalReference(@NonNull final Map<String, String> tableRow)
-	// {
-	// 	final String externalSystemCode = DataTableUtil.extractStringForColumnName(tableRow, "ExternalSystem.Code");
-	// 	final String externalId = DataTableUtil.extractStringForColumnName(tableRow, "ExternalReference");
-	// 	final String referenceType = DataTableUtil.extractStringForColumnName(tableRow, "ExternalReferenceType.Code");
-	// 	final String recordIdentifier = DataTableUtil.extractStringForColumnName(tableRow, "RecordId.Identifier");
-	//
-	// 	final IExternalSystem externalSystem = externalSystems.ofCode(externalSystemCode)
-	// 			.orElseThrow(() -> new InvalidIdentifierException("systemName", externalSystemCode));
-	//
-	// 	final IExternalReferenceType externalReferenceType = externalReferenceTypes.ofCode(referenceType)
-	// 			.orElseThrow(() -> new InvalidIdentifierException("externalReferenceType", externalSystemCode));
-	//
-	// 	final int recordId;
-	//
-	// 	if (externalReferenceType.equals(ProductExternalReferenceType.PRODUCT))
-	// 	{
-	// 		final I_M_Product product = productTable.get(recordIdentifier);
-	// 		recordId = product.getM_Product_ID();
-	// 	}
-	// 	else
-	// 	{
-	// 		throw new RuntimeException("External reference type not covered by this step def!");
-	// 	}
-	//
-	// 	final ExternalReference externalReference = ExternalReference.builder()
-	// 			.orgId(defaultOrgId)
-	// 			.externalSystem(externalSystem)
-	// 			.externalReference(externalId)
-	// 			.externalReferenceType(externalReferenceType)
-	// 			.recordId(recordId)
-	// 			.build();
-	//
-	// 	externalReferenceRepository.save(externalReference);
-	// }
 
 	private void removeExternalReferenceIfExists(@NonNull final Map<String, String> tableRow)
 	{
@@ -418,19 +323,4 @@ public class S_ExternalReference_StepDef
 				.create()
 				.delete();
 	}
-
-	// private IExternalReferenceType getExternalReferenceType(final String type)
-	// {
-	// 	switch (type)
-	// 	{
-	// 		case TYPE_Product:
-	// 			return ProductExternalReferenceType.PRODUCT;
-	// 		case TYPE_Bpartner:
-	// 			return BPartnerExternalReferenceType.BPARTNER;
-	// 		case TYPE_ProductCategory:
-	// 			return ProductCategoryExternalReferenceType.PRODUCT_CATEGORY;
-	// 		default:
-	// 			throw new AdempiereException("Bad external reference type: " + type);
-	// 	}
-	// }
 }
