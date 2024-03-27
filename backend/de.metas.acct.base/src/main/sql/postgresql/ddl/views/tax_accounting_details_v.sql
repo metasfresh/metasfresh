80205e15700a285153867fee4c3a980b4938f4f8
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-03-27 18:45:23 +0200

    Show baseamt in booking currency

--- backend/de.metas.acct.base/src/main/sql/postgresql/ddl/views/tax_accounting_details_v.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/ddl/views/tax_accounting_details_v.sql
@@ -1,46 +1,57 @@
+DROP VIEW de_metas_acct.tax_accounting_details_v;
+
 CREATE OR REPLACE VIEW de_metas_acct.tax_accounting_details_v
 AS
-SELECT ev.value      AS kontono,
-       ev.name       AS kontoname,
+SELECT ev.value          AS kontono,
+       ev.name           AS kontoname,
        fa.dateacct,
        fa.documentno,
 
-       tax.name      AS taxname,
-       tax.rate      AS taxrate,
+       tax.name          AS taxname,
+       tax.rate          AS taxrate,
 
-       bp.name       AS bpName,
+       bp.name           AS bpName,
 
-       i.taxbaseamt  AS inv_baseamt,
-       gl.taxbaseamt AS gl_baseamt,
+       i.taxbaseamt      AS inv_baseamt,
+       gl.taxbaseamt     AS gl_baseamt,
        (CASE
             WHEN al.c_allocationline_id IS NULL THEN NULL
-            WHEN tax.IsWholeTax = 'Y'           THEN 0
-            WHEN tax.Rate = 0                   THEN 0
-                                                ELSE ROUND((fa.AmtAcctDr - fa.AmtAcctCr) * 100 / tax.Rate, 2)
-        END)         AS alloc_baseamt,
+            WHEN tax.IsWholeTax = 'Y' THEN 0
+            WHEN tax.Rate = 0 THEN 0
+            ELSE ROUND((fa.AmtAcctDr - fa.AmtAcctCr) * 100 / tax.Rate, 2)
+           END)          AS alloc_baseamt,
 
-       i.taxamt      AS inv_taxamt,
-       gl.taxamt     AS gl_taxamt,
+       i.taxamt          AS inv_taxamt,
+       gl.taxamt         AS gl_taxamt,
        (CASE
             WHEN al.c_allocationline_id IS NOT NULL
                 THEN fa.AmtAcctDr - fa.AmtAcctCr
-        END)         AS alloc_taxamt,
-
-       (CASE
-            WHEN fa.line_id IS NULL AND fa.C_Tax_id IS NOT NULL
-                THEN (fa.amtacctdr - fa.amtacctcr)
-                ELSE 0
-        END)         AS taxamtperaccount,
+           END)          AS alloc_taxamt,
 
        c.iso_code,
 
        (CASE
             WHEN fa.line_id IS NULL AND fa.C_Tax_id IS NOT NULL
-                THEN 'Y'
-                ELSE 'N'
-        END)         AS IsTaxLine,
+                THEN (fa.amtacctdr - fa.amtacctcr)
+            ELSE 0
+           END)          AS taxamtperaccount,
+       (CASE
+            WHEN fa.line_id IS NULL AND fa.C_Tax_id IS NOT NULL
+                THEN
+                (select cfa.amtacctdr - cfa.amtacctcr
+                 FROM fact_acct cfa
+                 where cfa.record_id = fa.record_id and cfa.ad_table_id=fa.ad_table_id and cfa.line_id is not null and cfa.c_tax_id=fa.c_tax_id)
+            ELSE 0
+           END)          AS total,
+       acct_cur.iso_code as schema_currency,
 
-       fa.vatcode    AS vatcode,
+       (CASE
+            WHEN fa.line_id IS NULL AND fa.C_Tax_id IS NOT NULL
+                THEN 'Y'
+            ELSE 'N'
+           END)          AS IsTaxLine,
+
+       fa.vatcode        AS vatcode,
        fa.C_Tax_ID,
        fa.account_id,
        fa.postingtype,
@@ -51,6 +62,9 @@ SELECT ev.value      AS kontono,
 
 FROM fact_acct fa
 
+         LEFT OUTER JOIN c_acctschema cas on fa.c_acctschema_id = cas.c_acctschema_id
+         LEFT OUTER JOIN C_Currency acct_cur ON cas.c_Currency_ID = acct_cur.C_Currency_ID
+
          LEFT OUTER JOIN c_elementvalue ev ON ev.c_elementvalue_id = fa.account_id
          LEFT OUTER JOIN c_tax tax ON fa.c_tax_id = tax.c_tax_id
 
@@ -60,18 +74,20 @@ FROM fact_acct fa
     -- if invoice
          LEFT OUTER JOIN (SELECT (CASE
                                       WHEN dt.docbasetype <> 'APC' THEN inv_tax.taxbaseamt
-                                                                   ELSE (-1) * inv_tax.taxbaseamt
-                                  END) AS taxbaseamt,
+                                      ELSE (-1) * inv_tax.taxbaseamt
+    END)                                  AS taxbaseamt,
                                  (CASE
                                       WHEN dt.docbasetype <> 'APC'
                                           THEN inv_tax.taxamt
-                                          ELSE (-1) * inv_tax.taxamt
-                                  END) AS taxamt,
+                                      ELSE (-1) * inv_tax.taxamt
+                                     END) AS taxamt,
                                  i.c_invoice_id,
                                  inv_tax.c_tax_id
                           FROM c_invoice i
                                    JOIN C_InvoiceTax inv_tax ON i.c_invoice_id = inv_tax.c_invoice_id
-                                   JOIN C_DocType dt ON dt.C_DocType_ID = i.C_DocTypeTarget_id) i ON (fa.record_id = i.c_invoice_id AND fa.ad_table_id = get_Table_Id('C_Invoice') AND i.c_tax_id = fa.c_tax_id)
+                                   JOIN C_DocType dt ON dt.C_DocType_ID = i.C_DocTypeTarget_id) i
+                         ON (fa.record_id = i.c_invoice_id AND fa.ad_table_id = get_Table_Id('C_Invoice') AND
+                             i.c_tax_id = fa.c_tax_id)
     --
     -- if gl journal
          LEFT OUTER JOIN (SELECT (CASE
@@ -79,21 +95,25 @@ FROM fact_acct fa
                                           THEN gll.dr_taxbaseamt
                                       WHEN cr_autotaxaccount = 'Y'
                                           THEN gll.cr_taxbaseamt
-                                  END)                                  AS taxbaseamt,
+    END)                                                                AS taxbaseamt,
                                  (CASE
                                       WHEN gll.dr_autotaxaccount = 'Y'
                                           THEN gll.dr_taxamt
                                       WHEN cr_autotaxaccount = 'Y'
                                           THEN gll.cr_taxamt
-                                  END)                                  AS taxamt,
+                                     END)                               AS taxamt,
                                  gl.gl_journal_id,
                                  gll.gl_journalline_id,
                                  COALESCE(gll.dr_tax_id, gll.cr_tax_id) AS tax_id
                           FROM gl_journal gl
                                    JOIN GL_JournalLine gll
-                                        ON gl.gl_journal_id = gll.gl_journal_id) gl ON (fa.record_id = gl.gl_journal_id AND fa.ad_table_id = get_Table_Id('GL_Journal') AND gl.gl_journalline_id = fa.line_id AND gl.tax_id = fa.c_tax_id)
+                                        ON gl.gl_journal_id = gll.gl_journal_id) gl
+                         ON (fa.record_id = gl.gl_journal_id AND fa.ad_table_id = get_Table_Id('GL_Journal') AND
+                             gl.gl_journalline_id = fa.line_id AND gl.tax_id = fa.c_tax_id)
     --
     -- if allocationHdr
-         LEFT OUTER JOIN c_allocationline al ON (al.c_allocationline_id = fa.line_id AND al.c_allocationhdr_id = fa.record_id AND fa.ad_table_id = get_Table_Id('C_AllocationHdr'))
+         LEFT OUTER JOIN c_allocationline al
+                         ON (al.c_allocationline_id = fa.line_id AND al.c_allocationhdr_id = fa.record_id AND
+                             fa.ad_table_id = get_Table_Id('C_AllocationHdr'))
 ;
 
