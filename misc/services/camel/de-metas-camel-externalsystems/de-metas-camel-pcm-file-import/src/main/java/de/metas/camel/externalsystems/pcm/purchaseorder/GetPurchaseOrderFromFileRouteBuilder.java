087da88f9a30cede98847123d1e1fc73d778f002
cp-ps <no-email@email.com>
2024-03-28 11:14:31 +0200

    import purchase orders rfs: https://github.com/metasfresh/metasfresh/issues/17735

--- misc/services/camel/de-metas-camel-externalsystems/de-metas-camel-pcm-file-import/src/main/java/de/metas/camel/externalsystems/pcm/purchaseorder/GetPurchaseOrderFromFileRouteBuilder.java
+++ misc/services/camel/de-metas-camel-externalsystems/de-metas-camel-pcm-file-import/src/main/java/de/metas/camel/externalsystems/pcm/purchaseorder/GetPurchaseOrderFromFileRouteBuilder.java
@@ -4,28 +4,34 @@ import de.metas.camel.externalsystems.common.IdAwareRouteBuilder;
 import de.metas.camel.externalsystems.common.PInstanceLogger;
 import de.metas.camel.externalsystems.common.PInstanceUtil;
 import de.metas.camel.externalsystems.common.ProcessLogger;
+import de.metas.camel.externalsystems.common.v2.PurchaseCandidateCamelRequest;
+import de.metas.camel.externalsystems.pcm.SkipFirstLinePredicate;
 import de.metas.camel.externalsystems.pcm.config.PurchaseOrderFileEndpointConfig;
+import de.metas.camel.externalsystems.pcm.purchaseorder.model.PurchaseOrderRow;
 import de.metas.common.externalsystem.JsonExternalSystemRequest;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+
+import static de.metas.camel.externalsystems.common.ExternalSystemCamelConstants.MF_CREATE_PURCHASE_CANDIDATE_V2_CAMEL_URI;
+import static de.metas.camel.externalsystems.common.ExternalSystemCamelConstants.MF_ERROR_ROUTE_ID;
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.direct;
 
 public class GetPurchaseOrderFromFileRouteBuilder extends IdAwareRouteBuilder
 {
+	public static final String UPSERT_ORDER_PROCESSOR_ID = "GetPurchaseOrderFromFileRouteBuilder.UPSERT_ORDER_PROCESSOR_ID";
+	public static final String UPSERT_ORDER_ENDPOINT_ID = "GetPurchaseOrderFromFileRouteBuilder.UPSERT_ORDER_ENDPOINT_ID";
+
 	@NonNull
 	private final PurchaseOrderFileEndpointConfig fileEndpointConfig;
 
 	@Getter
-	@NonNull
-	private final String routeId;
-
-	@NonNull
-	private final JsonExternalSystemRequest enabledByExternalSystemRequest;
-
-	@NonNull
-	private final ProcessLogger processLogger;
+	@NonNull private final String routeId;
+	@NonNull private final JsonExternalSystemRequest enabledByExternalSystemRequest;
+	@NonNull private final PInstanceLogger pInstanceLogger;
 
 	@Builder
 	private GetPurchaseOrderFromFileRouteBuilder(
@@ -39,7 +45,10 @@ public class GetPurchaseOrderFromFileRouteBuilder extends IdAwareRouteBuilder
 		this.fileEndpointConfig = fileEndpointConfig;
 		this.routeId = routeId;
 		this.enabledByExternalSystemRequest = enabledByExternalSystemRequest;
-		this.processLogger = processLogger;
+		this.pInstanceLogger = PInstanceLogger.builder()
+				.processLogger(processLogger)
+				.pInstanceId(enabledByExternalSystemRequest.getAdPInstanceId())
+				.build();
 	}
 
 	@Override
@@ -53,18 +62,32 @@ public class GetPurchaseOrderFromFileRouteBuilder extends IdAwareRouteBuilder
 				.process(exchange -> PInstanceUtil.setPInstanceHeader(exchange, enabledByExternalSystemRequest))
 				.split(body().tokenize("\n"))
 					.streaming()
-					.process(this::logFileContent)
+					.filter(new SkipFirstLinePredicate())
+					.doTry()
+						.unmarshal(new BindyCsvDataFormat(PurchaseOrderRow.class))
+						.process(getPurchaseOrderUpsertProcessor()).id(UPSERT_ORDER_PROCESSOR_ID)
+						.choice()
+							.when(bodyAs(PurchaseCandidateCamelRequest.class).isNull())
+								.log(LoggingLevel.INFO, "Nothing to do! No order to upsert!")
+							.otherwise()
+								.log(LoggingLevel.DEBUG, "Calling metasfresh-api to upsert purchase order: ${body}")
+								.to(direct(MF_CREATE_PURCHASE_CANDIDATE_V2_CAMEL_URI)).id(UPSERT_ORDER_ENDPOINT_ID)
+							.endChoice()
+						.end()
+					.endDoTry()
+					.doCatch(Throwable.class)
+						.to(direct(MF_ERROR_ROUTE_ID))
+					.end()
 				.end();
 		//@formatter:on
 	}
 
-	private void logFileContent(@NonNull final Exchange exchange)
+	@NonNull
+	private PurchaseOrderUpsertProcessor getPurchaseOrderUpsertProcessor()
 	{
-		final PInstanceLogger pInstanceLogger = PInstanceLogger.builder()
-				.processLogger(processLogger)
-				.pInstanceId(enabledByExternalSystemRequest.getAdPInstanceId())
+		return PurchaseOrderUpsertProcessor.builder()
+				.externalSystemRequest(enabledByExternalSystemRequest)
+				.pInstanceLogger(pInstanceLogger)
 				.build();
-
-		pInstanceLogger.logMessage(exchange.getIn().getBody(String.class));
 	}
 }
