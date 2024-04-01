623ddf99e2707139e5b8aefd433302be3344a0b2
dm-ps <dragos.moca@pevesoft.ro>
2024-04-01 12:22:16 +0300

    refactoring

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5719160_sys_gh17606_Timezone_Europe_Berlin.sql
+++
@@ -1,6 +0,0 @@
--- Run mode: WEBUI
-
--- 2024-03-13T19:18:23.802Z
-UPDATE AD_OrgInfo SET TimeZone='Europe/Berlin',Updated=TO_TIMESTAMP('2024-03-13 19:18:23.795000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_OrgInfo_ID=1000000
-;
-
