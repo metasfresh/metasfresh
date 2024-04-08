1b973e817141a95395304e08bddcb54be27e02f3
cp-ps <no-email@email.com>
2024-04-08 13:13:08 +0300

    add trl https://github.com/metasfresh/metasfresh/issues/17779

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5721030_sys_gh17779_add_trls.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5721030_sys_gh17779_add_trls.sql
@@ -134,3 +134,59 @@ UPDATE AD_Element_Trl SET Name='Projekt Zeit',Updated=TO_TIMESTAMP('2024-04-08 1
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583074,'de_CH') 
 ;
 
+-- 2024-04-08T10:07:44.040Z
+UPDATE AD_Process_Trl SET Name='Als fakturiert einstellen',Updated=TO_TIMESTAMP('2024-04-08 13:07:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=585376
+;
+
+-- 2024-04-08T10:07:47.351Z
+UPDATE AD_Process SET Description=NULL, Help=NULL, Name='Als fakturiert einstellen',Updated=TO_TIMESTAMP('2024-04-08 13:07:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585376
+;
+
+-- 2024-04-08T10:07:47.321Z
+UPDATE AD_Process_Trl SET Name='Als fakturiert einstellen',Updated=TO_TIMESTAMP('2024-04-08 13:07:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=585376
+;
+
+-- 2024-04-08T10:07:50.982Z
+UPDATE AD_Process_Trl SET Name='Als fakturiert einstellen',Updated=TO_TIMESTAMP('2024-04-08 13:07:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='fr_CH' AND AD_Process_ID=585376
+;
+
+-- 2024-04-08T10:08:03.155Z
+UPDATE AD_Process_Trl SET Name='Als fakturiert einstellen',Updated=TO_TIMESTAMP('2024-04-08 13:08:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='nl_NL' AND AD_Process_ID=585376
+;
+
+-- 2024-04-08T10:11:23.989Z
+UPDATE AD_Element_Trl SET Name='Projektzeit - Admin', PrintName='Projektzeit - Admin',Updated=TO_TIMESTAMP('2024-04-08 13:11:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=583075 AND AD_Language='de_CH'
+;
+
+-- 2024-04-08T10:11:23.994Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583075,'de_CH')
+;
+
+-- 2024-04-08T10:11:26.559Z
+UPDATE AD_Element_Trl SET Name='Projektzeit - Admin', PrintName='Projektzeit - Admin',Updated=TO_TIMESTAMP('2024-04-08 13:11:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=583075 AND AD_Language='de_DE'
+;
+
+-- 2024-04-08T10:11:26.562Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583075,'de_DE')
+;
+
+-- 2024-04-08T10:11:26.563Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583075,'de_DE')
+;
+
+-- 2024-04-08T10:11:31.734Z
+UPDATE AD_Element_Trl SET Name='Projektzeit - Admin', PrintName='Projektzeit - Admin',Updated=TO_TIMESTAMP('2024-04-08 13:11:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=583075 AND AD_Language='fr_CH'
+;
+
+-- 2024-04-08T10:11:31.736Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583075,'fr_CH')
+;
+
+-- 2024-04-08T10:11:34.876Z
+UPDATE AD_Element_Trl SET Name='Projektzeit - Admin', PrintName='Projektzeit - Admin',Updated=TO_TIMESTAMP('2024-04-08 13:11:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=583075 AND AD_Language='nl_NL'
+;
+
+-- 2024-04-08T10:11:34.878Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583075,'nl_NL')
+;
+
