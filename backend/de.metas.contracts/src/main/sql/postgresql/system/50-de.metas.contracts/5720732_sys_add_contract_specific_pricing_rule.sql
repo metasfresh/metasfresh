c58572f312b73126086214a99c1151d71ff70992
Adrian <adrian.stefan@metasfresh.com>
2024-04-09 11:09:23 +0300

    remove ContractSpecificPricingRule. The approach is to use the price from logs directly when creating the NewInvoiceCandidate

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5720732_sys_add_contract_specific_pricing_rule.sql
+++
@@ -1,5 +0,0 @@
--- Run mode: SWING_CLIENT
-
--- 2024-04-05T15:53:37.128Z
-INSERT INTO C_PricingRule (AD_Client_ID,AD_Org_ID,Classname,C_PricingRule_ID,Created,CreatedBy,EntityType,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,'de.metas.contracts.pricing.ContractSpecificPricingRule',540019,TO_TIMESTAMP('2024-04-05 18:53:36.986','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','de.metas.contracts ContractSpecific',100,TO_TIMESTAMP('2024-04-05 18:53:36.986','YYYY-MM-DD HH24:MI:SS.US'),100)
-;
