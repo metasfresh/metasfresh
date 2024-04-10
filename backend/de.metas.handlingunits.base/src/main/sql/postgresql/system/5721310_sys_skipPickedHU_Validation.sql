fc413c1c699bf0cb9ab871d63f967e22df450977
cp-ps <77785468+cp-ps@users.noreply.github.com>
2024-04-10 16:11:12 +0300

    MobileUI - HU cannot be picked (#17821)
    https://github.com/metasfresh/metasfresh/issues/17820

---
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5721310_sys_skipPickedHU_Validation.sql
@@ -0,0 +1,3 @@
+-- 2024-04-10T12:56:57.452Z
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541717,'S',TO_TIMESTAMP('2024-04-10 15:56:57','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','PickingJobPickCommand.SYS_CONFIG_SKIP_PICKED_HU_VALIDATION',TO_TIMESTAMP('2024-04-10 15:56:57','YYYY-MM-DD HH24:MI:SS'),100,'N')
+;
