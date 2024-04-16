42a5ef0ab1cda55a27f6e757cb61eab64caa99c7
Teodor Sarca <teo.sarca@metasfresh.com>
2024-04-16 11:03:48 +0300

    when compacting IDs, start from 1000000 and not from 1
    (cherry picked from commit 7120c7410c8d6ce554b956ec86a1a2bb2db8d2e1)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5722020_040_AD_Workflow_Access_compact_ids.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5722020_040_AD_Workflow_Access_compact_ids.sql
@@ -12,7 +12,7 @@ ALTER TABLE AD_Workflow_Access
 -- Update ID 
 UPDATE AD_Workflow_Access t -- intermediate unique violations are ignored now
 SET AD_Workflow_Access_ID = t1.new_id
-FROM (SELECT AD_Workflow_Access_ID, ROW_NUMBER() OVER (ORDER BY AD_Workflow_Access_ID) AS new_id FROM AD_Workflow_Access) t1
+FROM (SELECT AD_Workflow_Access_ID, 1000000 - 1 + ROW_NUMBER() OVER (ORDER BY AD_Workflow_Access_ID) AS new_id FROM AD_Workflow_Access) t1
 WHERE t.AD_Workflow_Access_ID = t1.AD_Workflow_Access_ID
 ;
 
