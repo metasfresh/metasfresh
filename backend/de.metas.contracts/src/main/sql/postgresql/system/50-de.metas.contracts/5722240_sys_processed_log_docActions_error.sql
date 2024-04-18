229e5612e7c78c2b04a29cf0b68158ed57e85be7
Max Rieck <max.rieck@metasfresh.com>
2024-04-18 12:50:33 +0200

    add german trls

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5722240_sys_processed_log_docActions_error.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5722240_sys_processed_log_docActions_error.sql
@@ -48,30 +48,13 @@ UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-04-18 10:4
 UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-04-18 10:45:34.473','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545391
 ;
 
-/*
- * #%L
- * de.metas.contracts
- * %%
- * Copyright (C) 2024 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- Value: de.metas.contracts.modular.calculation.CalculationMethodService.DocActionNotAllowedForProcessedLogsError
 -- 2024-04-18T08:46:31.313Z
 UPDATE AD_Message_Trl SET MsgText='Document action is not allowed, because modular logs related to this document are already processed.',Updated=TO_TIMESTAMP('2024-04-18 10:46:31.313','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545391
+;
+
+-- Value: de.metas.contracts.modular.calculation.CalculationMethodService.DocActionNotAllowedForProcessedLogsError
+-- 2024-04-18T10:31:44.718Z
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-04-18 12:31:44.718','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545391
 ;
 
