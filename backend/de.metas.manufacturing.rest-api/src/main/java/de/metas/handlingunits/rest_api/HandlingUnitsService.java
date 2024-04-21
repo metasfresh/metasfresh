8a0f3e55edb8194099afb1a167434b8b005aa4db
Teodor Sarca <teo.sarca@metasfresh.com>
2024-04-21 12:17:43 +0300

    JsonHU: nicely format dates

--- backend/de.metas.manufacturing.rest-api/src/main/java/de/metas/handlingunits/rest_api/HandlingUnitsService.java
+++ backend/de.metas.manufacturing.rest-api/src/main/java/de/metas/handlingunits/rest_api/HandlingUnitsService.java
@@ -81,6 +81,9 @@ import org.compiere.util.Env;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nullable;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -149,7 +152,7 @@ public class HandlingUnitsService
 
 		final boolean isAggregatedTU = handlingUnitsBL.isAggregateHU(hu);
 
-		final JsonHUAttributes jsonHUAttributes = toJsonHUAttributes(huContext, hu);
+		final JsonHUAttributes jsonHUAttributes = toJsonHUAttributes(huContext, hu, adLanguage);
 
 		final JsonHU.JsonHUBuilder jsonHUBuilder = JsonHU.builder()
 				.id(String.valueOf(huId.getRepoId()))
@@ -279,7 +282,8 @@ public class HandlingUnitsService
 	@NonNull
 	private JsonHUAttributes toJsonHUAttributes(
 			@NonNull final IMutableHUContext huContext,
-			@NonNull final I_M_HU hu)
+			@NonNull final I_M_HU hu,
+			@NonNull final String adLanguage)
 	{
 		final ImmutableAttributeSet huAttributes = huContext.getHUAttributeStorageFactory()
 				.getImmutableAttributeSet(hu);
@@ -293,7 +297,7 @@ public class HandlingUnitsService
 			list.add(JsonHUAttribute.builder()
 					.code(attributeCode.getCode())
 					.caption(attribute.getName())
-					.value(value)
+					.value(toJsonHUAttributeValue(value, adLanguage))
 					.build());
 		}
 
@@ -307,6 +311,50 @@ public class HandlingUnitsService
 		}
 
 		return JsonHUAttributes.builder().list(ImmutableList.copyOf(list)).build();
+	}
+
+	@NonNull
+	private static Object toJsonHUAttributeValue(@Nullable final Object value, @NonNull final String adLanguage)
+	{
+		if (value == null)
+		{
+			return null;
+		}
+		else if (value instanceof java.sql.Timestamp)
+		{
+			final LocalDateTime dateTime = ((Timestamp)value).toLocalDateTime();
+			return toJsonHUAttributeValue_fromLocalDateTime(dateTime, adLanguage);
+		}
+		else if (value instanceof LocalDateTime)
+		{
+			return toJsonHUAttributeValue_fromLocalDateTime((LocalDateTime)value, adLanguage);
+		}
+		else if (value instanceof LocalDate)
+		{
+			return toJsonHUAttributeValue_fromLocalDate((LocalDate)value, adLanguage);
+		}
+		else
+		{
+			return value;
+		}
+	}
+
+	private static Object toJsonHUAttributeValue_fromLocalDateTime(@NonNull final LocalDateTime value, @NonNull final String adLanguage)
+	{
+		final LocalDate date = value.toLocalDate();
+		if (value.equals(date.atStartOfDay()))
+		{
+			return toJsonHUAttributeValue_fromLocalDate(date, adLanguage);
+		}
+		else
+		{
+			return TranslatableStrings.dateAndTime(value).translate(adLanguage);
+		}
+	}
+
+	private static Object toJsonHUAttributeValue_fromLocalDate(@NonNull final LocalDate value, @NonNull final String adLanguage)
+	{
+		return TranslatableStrings.date(value).translate(adLanguage);
 	}
 
 	@NonNull
