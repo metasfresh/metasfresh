c4a9fc3b951c063ea58f4b35477f37f24ea6f4fa
Adrian <adrian.stefan@metasfresh.com>
2024-04-23 21:42:04 +0300

    add ModCntr_Compute_Interest process

---
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/modular/interim/invoice/process/ModCntr_Compute_Interest.java
@@ -0,0 +1,59 @@
+/*
+ * #%L
+ * de.metas.contracts
+ * %%
+ * Copyright (C) 2024 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.contracts.modular.interim.invoice.process;
+
+import de.metas.process.IProcessPrecondition;
+import de.metas.process.IProcessPreconditionsContext;
+import de.metas.process.JavaProcess;
+import de.metas.process.Param;
+import de.metas.process.ProcessPreconditionsResolution;
+import lombok.NonNull;
+
+import java.util.Date;
+
+public class ModCntr_Compute_Interest extends JavaProcess implements IProcessPrecondition
+{
+	@Param(parameterName = "InterimDate")
+	private Date p_InterimDate;
+
+	@Param(parameterName = "BillingDate")
+	Date p_BillingDate;
+
+	@Override
+	public ProcessPreconditionsResolution checkPreconditionsApplicable(final @NonNull IProcessPreconditionsContext context)
+	{
+		if (context.isNoSelection())
+		{
+			return ProcessPreconditionsResolution.rejectBecauseNoSelection();
+		}
+		return ProcessPreconditionsResolution.accept();
+	}
+
+	@Override
+	protected String doIt() throws Exception
+	{
+		return MSG_OK;
+	}
+
+
+}
