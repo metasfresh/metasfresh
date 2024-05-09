28471448117ca6739786048e1b9124f70ea593ae
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-05-09 15:48:44 +0300

    script correction: informative modules must be k=marked as processed

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5723420_sys_ModCntr_Settings_InsertInformativeModule.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5723420_sys_ModCntr_Settings_InsertInformativeModule.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.modcntr_module (ad_client_id, ad_org_id, created, createdby, invoicinggroup, isactive, modcntr_module_id, modcntr_settings_id, modcntr_type_id,
-                                   m_product_id, name, seqno, updated, updatedby)
+                                   m_product_id, name, seqno, updated, updatedby, processed)
 
 SELECT 1000000,
        1000000,
@@ -14,7 +14,8 @@ SELECT 1000000,
        'Informative Logs',
        0,
        '2024-05-08 14:32:59.239000 +00:00',
-       100
+       100,
+       'Y'
 FROM modcntr_settings s
 WHERE NOT EXISTS (SELECT 1 FROM modcntr_module m WHERE m.modcntr_settings_id = s.modcntr_settings_id AND m.modcntr_type_id = 540008)
 ;
