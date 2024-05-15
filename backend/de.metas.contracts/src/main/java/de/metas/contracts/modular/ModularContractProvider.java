0f01cd85f73d42bf7bbacd9ac6927170289234f5
Max Rieck <max.rieck@metasfresh.com>
2024-05-15 11:46:01 +0200

    QA use CoalesceUtil.coalesceNotNull

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/modular/ModularContractProvider.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/modular/ModularContractProvider.java
@@ -25,11 +25,13 @@ package de.metas.contracts.modular;
 import de.metas.calendar.standard.CalendarId;
 import de.metas.calendar.standard.YearAndCalendarId;
 import de.metas.calendar.standard.YearId;
+import de.metas.common.util.CoalesceUtil;
 import de.metas.contracts.FlatrateTermId;
 import de.metas.contracts.FlatrateTermRequest.ModularFlatrateTermQuery;
 import de.metas.contracts.IFlatrateBL;
 import de.metas.contracts.model.I_C_Flatrate_Term;
 import de.metas.contracts.modular.settings.ModularContractSettings;
+import de.metas.contracts.modular.settings.ModularContractSettingsBL;
 import de.metas.contracts.modular.settings.ModularContractSettingsQuery;
 import de.metas.inout.IInOutDAO;
 import de.metas.inout.InOutId;
@@ -199,7 +201,7 @@ public class ModularContractProvider
 			return Stream.of(flatrateTermId);
 		}
 		else if (flatrateBL.isInterimContract(flatrateTermId))
-		{//todo cp
+		{
 			final I_C_Flatrate_Term interimContractRecord = flatrateBL.getById(flatrateTermId);
 			return Stream.of(FlatrateTermId.ofRepoId(interimContractRecord.getModular_Flatrate_Term_ID()));
 		}
@@ -258,8 +260,7 @@ public class ModularContractProvider
 																													 .build());
 
 		final ProductId settingsProductId = CollectionUtils.extractSingleElementOrDefault(settings, ModularContractSettings::getRawProductId, null);
-		final ProductId productIdToUse;
-        productIdToUse = settingsProductId != null ? settingsProductId : inOutProductId;
+		final ProductId productIdToUse = CoalesceUtil.coalesceNotNull(settingsProductId, inOutProductId);
 
 		final ModularFlatrateTermQuery query = ModularFlatrateTermQuery.builder()
 				.bPartnerId(warehouseBL.getBPartnerId(warehouseId))
