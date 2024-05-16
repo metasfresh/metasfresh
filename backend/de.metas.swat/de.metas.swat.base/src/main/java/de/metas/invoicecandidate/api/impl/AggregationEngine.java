704ea2f40d0e472b6c12cee3685981008482bbbc
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2024-05-16 13:34:20 +0200

    Port small part of PR-15101 so solve a concrete invoicing problem (#18013)
    This comes from PR https://github.com/metasfresh/metasfresh/pull/15101
    Also add some simple improvements to make merging towards master easier

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/AggregationEngine.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/api/impl/AggregationEngine.java
@@ -66,7 +66,6 @@ import org.adempiere.ad.table.api.IADTableDAO;
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.SpringContextHolder;
-import org.compiere.SpringContextHolder;
 import org.compiere.model.I_C_DocType;
 import org.compiere.model.I_C_Order;
 import org.compiere.model.I_M_InOutLine;
@@ -480,7 +479,7 @@ public final class AggregationEngine
 					else
 					{
 						invoiceHeader.setDocTypeInvoiceId(null, false);
-						invoiceHeader.setIsTakeDocTypeFromPool(true);
+						invoiceHeader.setTakeDocTypeFromPool(true);
 					}
 				}
 
