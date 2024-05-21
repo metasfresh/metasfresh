a92a58b76067acfb0b532172c85e34faef26a203
Adrian <adrian.stefan@metasfresh.com>
2024-05-22 00:00:17 +0300

    added ModCntr_Interest_V view

---
+++ backend/de.metas.contracts/src/main/sql/postgresql/ddl/views/ModCntr_Interest_V.sql
@@ -0,0 +1,62 @@
+CREATE OR REPLACE VIEW ModCntr_Interest_V AS
+WITH interimAmts AS (SELECT SUM(amount
+                            ) AS interim_amt,
+                            c_flatrate_term_id,
+                            l.modcntr_module_id,
+                            t.modularcontracthandlertype
+                     FROM modcntr_log l
+                              INNER JOIN modcntr_module m ON l.modcntr_module_id = m.modcntr_module_id
+                              INNER JOIN modcntr_type t ON l.modcntr_type_id = t.modcntr_type_id
+                     WHERE isbillable = 'Y'
+                       AND ad_table_id = get_table_id('C_InvoiceLine'
+                                         )
+                     GROUP BY c_flatrate_term_id, l.modcntr_module_id, t.modularcontracthandlertype),
+     matchedAmts AS (SELECT SUM(mi.matchedamt
+                               ) OVER (PARTITION BY c_flatrate_term_id, modcntr_module_id
+                                ORDER BY datetrx,modcntr_interest_id ROWS UNBOUNDED PRECEDING
+                                ) AS matched_amt,
+                            mi.modcntr_interest_id,
+                            c_flatrate_term_id,
+                            modcntr_module_id,
+                            datetrx
+                     FROM modcntr_interest mi
+                              INNER JOIN modcntr_log l ON mi.shippingnotification_modcntr_log_id = l.modcntr_log_id AND l.isbillable = 'Y'
+                     WHERE mi.interiminvoice_modcntr_log_id IS NOT NULL
+                       AND finalinterest != 0
+                     ORDER BY datetrx, modcntr_interest_id)
+SELECT ir.modcntr_interest_run_id,
+       l.c_flatrate_term_id,
+       bp.value                  AS Bill_BPartner_Value,
+       bp.name                   AS Bill_BPartner_Name,
+       ig.name                   AS InvoicingGroup_Name,
+       interimInvoice.documentno AS InterimInvoice_documentNo,
+       ir.interimdate,
+       ir.billingdate,
+       ir.totalinterest,
+       s.addinterestdays,
+       s.interestrate,
+       l.datetrx,
+       ABS(interim_amt)          AS InterimAmt,
+       mi.matchedamt,
+       matchedAmts.matched_amt   AS TotalAmt,
+       mi.interestdays,
+       mi.interestscore,
+       mi.finalinterest
+
+FROM modcntr_interest mi
+         INNER JOIN modCntr_log l ON mi.shippingnotification_modcntr_log_id = l.modcntr_log_id AND l.isbillable = 'Y'
+         INNER JOIN modcntr_invoicinggroup ig ON l.modcntr_invoicinggroup_id = ig.modcntr_invoicinggroup_id
+         INNER JOIN modcntr_interest_run ir ON ir.modcntr_interest_run_id = mi.modcntr_interest_run_id
+         INNER JOIN c_bpartner bp ON l.bill_bpartner_id = bp.c_bpartner_id
+         INNER JOIN modCntr_module m ON l.modcntr_module_id = m.modcntr_module_id
+         INNER JOIN modcntr_type t ON l.modcntr_type_id = t.modcntr_type_id
+         INNER JOIN modcntr_settings s ON m.modcntr_settings_id = s.modcntr_settings_id
+         INNER JOIN interimAmts ON l.c_flatrate_term_id = interimAmts.c_flatrate_term_id AND l.modcntr_module_id = interimAmts.modcntr_module_id
+         INNER JOIN matchedAmts ON matchedamts.modcntr_interest_id = mi.modcntr_interest_id
+         LEFT JOIN modcntr_log interimInvoiceLineLog ON mi.interiminvoice_modcntr_log_id = interimInvoiceLineLog.modcntr_log_id
+         LEFT JOIN c_invoiceline il ON interimInvoiceLineLog.record_id = il.c_invoiceline_id
+         LEFT JOIN c_invoice interimInvoice ON il.c_invoice_id = interimInvoice.c_invoice_id
+WHERE mi.finalinterest != 0
+ORDER BY bp.c_bpartner_id,
+         mi.created
+;
