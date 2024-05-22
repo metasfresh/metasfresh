47524c5a75c30b1e060e7b7f0911969c9d0bb33a
Teodor Sarca <teo.sarca@metasfresh.com>
2024-05-22 18:11:06 +0300

    DocumentIdsSelection - copy from master; improvements

---
+++ backend/de.metas.ui.web.base/src/test/java/de/metas/ui/web/window/datatypes/DocumentIdsSelectionTest.java
@@ -0,0 +1,58 @@
+package de.metas.ui.web.window.datatypes;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static de.metas.ui.web.window.datatypes.DocumentIdsSelection.ALL;
+import static de.metas.ui.web.window.datatypes.DocumentIdsSelection.EMPTY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DocumentIdsSelectionTest
+{
+	@Nested
+	class addAll
+	{
+		@Test
+		void EMPTY_and_ALL()
+		{
+			assertThat(EMPTY.addAll(EMPTY)).isSameAs(EMPTY);
+			assertThat(EMPTY.addAll(ALL)).isSameAs(ALL);
+			assertThat(ALL.addAll(EMPTY)).isSameAs(ALL);
+			assertThat(ALL.addAll(ALL)).isSameAs(ALL);
+		}
+
+		@Test
+		void EMPTY_and_fixedSet()
+		{
+			final DocumentIdsSelection fixedSet = DocumentIdsSelection.ofCommaSeparatedString("1,2,3");
+			assertThat(EMPTY.addAll(fixedSet)).isSameAs(fixedSet);
+			assertThat(fixedSet.addAll(EMPTY)).isSameAs(fixedSet);
+			assertThat(fixedSet.addAll(fixedSet)).isSameAs(fixedSet);
+		}
+
+		@Test
+		void ALL_and_fixedSet()
+		{
+			final DocumentIdsSelection fixedSet = DocumentIdsSelection.ofCommaSeparatedString("1,2,3");
+			assertThat(ALL.addAll(fixedSet)).isSameAs(ALL);
+			assertThat(fixedSet.addAll(ALL)).isSameAs(ALL);
+		}
+
+		@Test
+		void overlapping_fixedSets()
+		{
+			final DocumentIdsSelection fixedSet1 = DocumentIdsSelection.ofCommaSeparatedString("1,2,3");
+			final DocumentIdsSelection fixedSet2 = DocumentIdsSelection.ofCommaSeparatedString("3,4,5");
+			assertThat(fixedSet1.addAll(fixedSet2)).isSameAs(DocumentIdsSelection.ofCommaSeparatedString("1,2,3,4,5"));
+		}
+
+		@Test
+		void disjunct_fixedSets()
+		{
+			final DocumentIdsSelection fixedSet1 = DocumentIdsSelection.ofCommaSeparatedString("1,2,3");
+			final DocumentIdsSelection fixedSet2 = DocumentIdsSelection.ofCommaSeparatedString("7,8,9");
+			assertThat(fixedSet1.addAll(fixedSet2)).isSameAs(DocumentIdsSelection.ofCommaSeparatedString("1,2,3,7,8,9"));
+		}
+
+	}
+}
