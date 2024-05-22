ad71c50d50db6e48c870f7ccc5537bac30f73d4f
Adrian <adrian.stefan@metasfresh.com>
2024-05-22 10:20:07 +0300

    updated ModCntr_Interest_V to include reference to include final invoice, currency and handler type

--- backend/de.metas.contracts/src/main/sql/postgresql/ddl/views/ModCntr_Interest_V.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/ddl/views/ModCntr_Interest_V.sql
@@ -1,3 +1,6 @@
+-- DROP VIEW IF EXISTS ModCntr_Interest_V
+-- ;
+
 CREATE OR REPLACE VIEW ModCntr_Interest_V AS
 WITH interimAmts AS (SELECT SUM(amount
                             ) AS interim_amt,
@@ -8,8 +11,7 @@ WITH interimAmts AS (SELECT SUM(amount
                               INNER JOIN modcntr_module m ON l.modcntr_module_id = m.modcntr_module_id
                               INNER JOIN modcntr_type t ON l.modcntr_type_id = t.modcntr_type_id
                      WHERE isbillable = 'Y'
-                       AND ad_table_id = get_table_id('C_InvoiceLine'
-                                         )
+                       AND ad_table_id = get_table_id('C_InvoiceLine')
                      GROUP BY c_flatrate_term_id, l.modcntr_module_id, t.modularcontracthandlertype),
      matchedAmts AS (SELECT SUM(mi.matchedamt
                                ) OVER (PARTITION BY c_flatrate_term_id, modcntr_module_id
@@ -24,14 +26,17 @@ WITH interimAmts AS (SELECT SUM(amount
                      WHERE mi.interiminvoice_modcntr_log_id IS NOT NULL
                        AND finalinterest != 0
                      ORDER BY datetrx, modcntr_interest_id)
-SELECT ir.modcntr_interest_run_id,
+SELECT finalIL.C_Invoice_ID as C_FinalInvoice_ID,
+       ir.modcntr_interest_run_id,
        l.c_flatrate_term_id,
+       t.modularcontracthandlertype,
        bp.value                  AS Bill_BPartner_Value,
        bp.name                   AS Bill_BPartner_Name,
        ig.name                   AS InvoicingGroup_Name,
        interimInvoice.documentno AS InterimInvoice_documentNo,
        ir.interimdate,
        ir.billingdate,
+       ir.c_currency_id,
        ir.totalinterest,
        s.addinterestdays,
        s.interestrate,
@@ -42,7 +47,6 @@ SELECT ir.modcntr_interest_run_id,
        mi.interestdays,
        mi.interestscore,
        mi.finalinterest
-
 FROM modcntr_interest mi
          INNER JOIN modCntr_log l ON mi.shippingnotification_modcntr_log_id = l.modcntr_log_id AND l.isbillable = 'Y'
          INNER JOIN modcntr_invoicinggroup ig ON l.modcntr_invoicinggroup_id = ig.modcntr_invoicinggroup_id
@@ -56,6 +60,9 @@ FROM modcntr_interest mi
          LEFT JOIN modcntr_log interimInvoiceLineLog ON mi.interiminvoice_modcntr_log_id = interimInvoiceLineLog.modcntr_log_id
          LEFT JOIN c_invoiceline il ON interimInvoiceLineLog.record_id = il.c_invoiceline_id
          LEFT JOIN c_invoice interimInvoice ON il.c_invoice_id = interimInvoice.c_invoice_id
+         left join c_invoice_candidate finalIC on l.c_invoice_candidate_id = finalIC.c_invoice_candidate_id
+         left join C_Invoice_Line_Alloc finalILA on finalIC.c_invoice_candidate_id = finalILA.c_invoice_candidate_id
+         left join c_invoiceline finalIL on finalIL.c_invoiceline_id = finalILA.c_invoiceline_id
 WHERE mi.finalinterest != 0
 ORDER BY bp.c_bpartner_id,
          mi.created
