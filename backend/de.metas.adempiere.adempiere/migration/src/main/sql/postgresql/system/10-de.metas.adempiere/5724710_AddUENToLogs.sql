5045ce93b82992be3a064eff94bcac9100f707b4
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-05-28 18:16:20 +0300

    Add UEN to window

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5724710_AddUENToLogs.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5724710_AddUENToLogs.sql
@@ -81,3 +81,111 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 /* DDL */ SELECT public.db_alter_table('ModCntr_Log','ALTER TABLE public.ModCntr_Log ADD COLUMN UserElementNumber2 NUMERIC')
 ;
 
+-- 2024-05-28T15:13:59.565Z
+INSERT INTO t_alter_column values('modcntr_log','UserElementNumber2','NUMERIC',null,null)
+;
+
+-- 2024-05-28T15:14:02.927Z
+INSERT INTO t_alter_column values('modcntr_log','UserElementNumber1','NUMERIC',null,null)
+;
+
+-- Field: Log für modulare Verträge(541711,de.metas.contracts) -> Contract Module Log(547012,de.metas.contracts) -> Manufacturing Cost Collector
+-- Column: ModCntr_Log.PP_Cost_Collector_ID
+-- 2024-05-28T15:14:15.006Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,587472,728765,0,547012,TO_TIMESTAMP('2024-05-28 18:14:14.89','YYYY-MM-DD HH24:MI:SS.US'),100,10,'de.metas.contracts','Y','N','N','N','N','N','N','N','Manufacturing Cost Collector',TO_TIMESTAMP('2024-05-28 18:14:14.89','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-28T15:14:15.009Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=728765 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-05-28T15:14:15.011Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(53310)
+;
+
+-- 2024-05-28T15:14:15.023Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=728765
+;
+
+-- 2024-05-28T15:14:15.025Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(728765)
+;
+
+-- Field: Log für modulare Verträge(541711,de.metas.contracts) -> Contract Module Log(547012,de.metas.contracts) -> Ursprüngliches Produkt
+-- Column: ModCntr_Log.Initial_Product_ID
+-- 2024-05-28T15:14:15.127Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,588192,728766,0,547012,TO_TIMESTAMP('2024-05-28 18:14:15.03','YYYY-MM-DD HH24:MI:SS.US'),100,10,'de.metas.contracts','Y','N','N','N','N','N','N','N','Ursprüngliches Produkt',TO_TIMESTAMP('2024-05-28 18:14:15.03','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-28T15:14:15.129Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=728766 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-05-28T15:14:15.130Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(583085)
+;
+
+-- 2024-05-28T15:14:15.133Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=728766
+;
+
+-- 2024-05-28T15:14:15.133Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(728766)
+;
+
+-- Field: Log für modulare Verträge(541711,de.metas.contracts) -> Contract Module Log(547012,de.metas.contracts) -> HL
+-- Column: ModCntr_Log.UserElementNumber1
+-- 2024-05-28T15:14:15.215Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,588324,728767,0,547012,TO_TIMESTAMP('2024-05-28 18:14:15.136','YYYY-MM-DD HH24:MI:SS.US'),100,10,'de.metas.contracts','Y','N','N','N','N','N','N','N','HL',TO_TIMESTAMP('2024-05-28 18:14:15.136','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-28T15:14:15.216Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=728767 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-05-28T15:14:15.218Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(583112)
+;
+
+-- 2024-05-28T15:14:15.220Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=728767
+;
+
+-- 2024-05-28T15:14:15.221Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(728767)
+;
+
+-- Field: Log für modulare Verträge(541711,de.metas.contracts) -> Contract Module Log(547012,de.metas.contracts) -> Protein
+-- Column: ModCntr_Log.UserElementNumber2
+-- 2024-05-28T15:14:15.309Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,588325,728768,0,547012,TO_TIMESTAMP('2024-05-28 18:14:15.223','YYYY-MM-DD HH24:MI:SS.US'),100,10,'de.metas.contracts','Y','N','N','N','N','N','N','N','Protein',TO_TIMESTAMP('2024-05-28 18:14:15.223','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-28T15:14:15.312Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=728768 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-05-28T15:14:15.313Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(583113)
+;
+
+-- 2024-05-28T15:14:15.316Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=728768
+;
+
+-- 2024-05-28T15:14:15.316Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(728768)
+;
+
+-- UI Element: Log für modulare Verträge(541711,de.metas.contracts) -> Contract Module Log(547012,de.metas.contracts) -> main -> 10 -> details.HL
+-- Column: ModCntr_Log.UserElementNumber1
+-- 2024-05-28T15:15:31.106Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,728767,0,547012,550777,624780,'F',TO_TIMESTAMP('2024-05-28 18:15:31.003','YYYY-MM-DD HH24:MI:SS.US'),100,'Y','N','N','Y','N','N','N',0,'HL',60,0,0,TO_TIMESTAMP('2024-05-28 18:15:31.003','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- UI Element: Log für modulare Verträge(541711,de.metas.contracts) -> Contract Module Log(547012,de.metas.contracts) -> main -> 10 -> details.Protein
+-- Column: ModCntr_Log.UserElementNumber2
+-- 2024-05-28T15:15:39.651Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,728768,0,547012,550777,624781,'F',TO_TIMESTAMP('2024-05-28 18:15:39.528','YYYY-MM-DD HH24:MI:SS.US'),100,'Y','N','N','Y','N','N','N',0,'Protein',70,0,0,TO_TIMESTAMP('2024-05-28 18:15:39.528','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
