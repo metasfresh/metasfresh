ee49feb30bbf0b54c449599da81f3afffe76d250
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-05-29 16:37:41 +0300

    Minor fixes

--- backend/de.metas.business/src/main/java/de/metas/document/dimension/Dimension.java
+++ backend/de.metas.business/src/main/java/de/metas/document/dimension/Dimension.java
@@ -114,7 +114,6 @@ public class Dimension implements Comparable<Dimension>
 				.userElementString5(CoalesceUtil.coalesce(this.userElementString5, other.userElementString5))
 				.userElementString6(CoalesceUtil.coalesce(this.userElementString6, other.userElementString6))
 				.userElementString7(CoalesceUtil.coalesce(this.userElementString7, other.userElementString7))
-				.userElement1Id(CoalesceUtil.firstGreaterThanZero(this.userElement1Id, other.userElement1Id))
 				.userElementNumber1(CoalesceUtil.coalesce(this.userElementNumber1, other.userElementNumber1))
 				.userElementNumber2(CoalesceUtil.coalesce(this.userElementNumber2, other.userElementNumber2))
 				.harvestingYearAndCalendarId(CoalesceUtil.coalesce(this.harvestingYearAndCalendarId, other.harvestingYearAndCalendarId))
