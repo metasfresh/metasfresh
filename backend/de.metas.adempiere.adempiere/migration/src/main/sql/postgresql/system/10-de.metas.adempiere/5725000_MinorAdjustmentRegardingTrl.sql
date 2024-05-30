4f2c4f28cdce64e3cd75843e09f9b36ef5ad3359
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-05-30 16:51:57 +0300

    Remove unneeded columns

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725000_MinorAdjustmentRegardingTrl.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725000_MinorAdjustmentRegardingTrl.sql
@@ -48,3 +48,32 @@ UPDATE M_ProductPrice SET ScalePriceQuantityFrom='Q' WHERE ScalePriceQuantityFro
 INSERT INTO t_alter_column values('m_productprice','ScalePriceQuantityFrom',null,'NOT NULL',null)
 ;
 
+
+-- Value: MSG_ModularContractPrice_NoScalePrice
+-- 2024-05-30T13:47:08.462Z
+INSERT INTO AD_Message (AD_Client_ID,AD_Message_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,MsgText,MsgType,Updated,UpdatedBy,Value) VALUES (0,545417,0,TO_TIMESTAMP('2024-05-30 16:47:08.284','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Y','Es konnte kein Staffelpreis.','E',TO_TIMESTAMP('2024-05-30 16:47:08.284','YYYY-MM-DD HH24:MI:SS.US'),100,'MSG_ModularContractPrice_NoScalePrice')
+;
+
+-- 2024-05-30T13:47:08.494Z
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Message t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Message_ID=545417 AND NOT EXISTS (SELECT 1 FROM AD_Message_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Message_ID=t.AD_Message_ID)
+;
+
+-- Value: MSG_ModularContractPrice_NoScalePrice
+-- 2024-05-30T13:47:16.320Z
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-05-30 16:47:16.32','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545417
+;
+
+-- Value: MSG_ModularContractPrice_NoScalePrice
+-- 2024-05-30T13:47:24.176Z
+UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='There is no scale price.',Updated=TO_TIMESTAMP('2024-05-30 16:47:24.176','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545417
+;
+
+-- Value: MSG_ModularContractPrice_NoScalePrice
+-- 2024-05-30T13:47:28.347Z
+UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-05-30 16:47:28.347','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Message_ID=545417
+;
+
+--------------------
+
+
+
