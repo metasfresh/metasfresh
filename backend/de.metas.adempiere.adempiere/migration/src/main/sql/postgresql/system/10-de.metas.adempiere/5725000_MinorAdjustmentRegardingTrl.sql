8072b08fb8f46d620a26f7d80fd75929256e7083
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-05-30 18:17:29 +0300

    Translate index

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725000_MinorAdjustmentRegardingTrl.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725000_MinorAdjustmentRegardingTrl.sql
@@ -77,3 +77,49 @@ UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-05-30 16:4
 
 
 
+DROP INDEX ModCntr_Specific_Price_MinValue;
+
+
+-- 2024-05-30T15:11:25.427Z
+INSERT INTO AD_Index_Table (AD_Client_ID,AD_Index_Table_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,EntityType,ErrorMsg,IsActive,IsUnique,Name,Processing,Updated,UpdatedBy,WhereClause) VALUES (0,540793,0,542405,TO_TIMESTAMP('2024-05-30 18:11:25.242','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Shall be one price with minimum value for one contract.','Y','Y','ModCntr_Specific_Price_MinValue','N',TO_TIMESTAMP('2024-05-30 18:11:25.242','YYYY-MM-DD HH24:MI:SS.US'),100,'')
+;
+
+-- 2024-05-30T15:11:25.431Z
+INSERT INTO AD_Index_Table_Trl (AD_Language,AD_Index_Table_ID, ErrorMsg, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Index_Table_ID, t.ErrorMsg, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Index_Table t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Index_Table_ID=540793 AND NOT EXISTS (SELECT 1 FROM AD_Index_Table_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Index_Table_ID=t.AD_Index_Table_ID)
+;
+
+-- 2024-05-30T15:11:43.948Z
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,588321,541402,540793,0,TO_TIMESTAMP('2024-05-30 18:11:43.813','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Y',10,TO_TIMESTAMP('2024-05-30 18:11:43.813','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-30T15:12:15.711Z
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,588099,541403,540793,0,TO_TIMESTAMP('2024-05-30 18:12:15.584','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Y',20,TO_TIMESTAMP('2024-05-30 18:12:15.584','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-30T15:12:24.997Z
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,588320,541404,540793,0,TO_TIMESTAMP('2024-05-30 18:12:24.88','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Y',30,TO_TIMESTAMP('2024-05-30 18:12:24.88','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-30T15:12:32.473Z
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,588100,541405,540793,0,TO_TIMESTAMP('2024-05-30 18:12:32.356','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Y',40,TO_TIMESTAMP('2024-05-30 18:12:32.356','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-30T15:12:43.076Z
+INSERT INTO AD_Index_Column (AD_Client_ID,AD_Column_ID,AD_Index_Column_ID,AD_Index_Table_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,SeqNo,Updated,UpdatedBy) VALUES (0,588097,541406,540793,0,TO_TIMESTAMP('2024-05-30 18:12:42.959','YYYY-MM-DD HH24:MI:SS.US'),100,'de.metas.contracts','Y',50,TO_TIMESTAMP('2024-05-30 18:12:42.959','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-05-30T15:12:48.003Z
+DROP INDEX IF EXISTS modcntr_specific_price_minvalue
+;
+
+-- 2024-05-30T15:12:48.010Z
+CREATE UNIQUE INDEX ModCntr_Specific_Price_MinValue ON ModCntr_Specific_Price (MinValue,ModCntr_Module_ID,IsScalePrice,M_Product_ID,C_Flatrate_Term_ID)
+;
+
+
+UPDATE ad_index_table_trl
+SET errormsg = 'Shall be one price with minimum value for one contract.', istranslated = 'Y'
+WHERE ad_index_table_id = 540793
+  AND ad_language IN ('de_CH', 'de_DE')
+;
+
