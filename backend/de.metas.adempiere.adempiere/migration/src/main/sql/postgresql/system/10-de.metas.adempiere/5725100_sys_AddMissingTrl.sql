4fffc5e7bfae1ad923fd248c3ab742bd271659ae
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-05-31 11:16:10 +0300

    Add missing Trl

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725100_sys_AddMissingTrl.sql
@@ -0,0 +1,45 @@
+
+-- Element: AsNewPrice
+-- 2024-05-31T08:13:15.097Z
+UPDATE AD_Element_Trl SET Name='As New Price',Updated=TO_TIMESTAMP('2024-05-31 11:13:15.097','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583127 AND AD_Language='fr_CH'
+;
+
+-- 2024-05-31T08:13:15.102Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583127,'fr_CH')
+;
+
+-- Element: AsNewPrice
+-- 2024-05-31T08:13:18.503Z
+UPDATE AD_Element_Trl SET Name='As New Price',Updated=TO_TIMESTAMP('2024-05-31 11:13:18.503','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583127 AND AD_Language='en_US'
+;
+
+-- 2024-05-31T08:13:18.507Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583127,'en_US')
+;
+
+-- Element: AsNewPrice
+-- 2024-05-31T08:13:24.723Z
+UPDATE AD_Element_Trl SET Name='Neupreis',Updated=TO_TIMESTAMP('2024-05-31 11:13:24.723','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583127 AND AD_Language='de_DE'
+;
+
+-- 2024-05-31T08:13:24.724Z
+UPDATE AD_Element SET Name='Neupreis' WHERE AD_Element_ID=583127
+;
+
+-- 2024-05-31T08:13:25.181Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583127,'de_DE')
+;
+
+-- 2024-05-31T08:13:25.182Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583127,'de_DE')
+;
+
+-- Element: AsNewPrice
+-- 2024-05-31T08:13:28.417Z
+UPDATE AD_Element_Trl SET Name='Neupreis',Updated=TO_TIMESTAMP('2024-05-31 11:13:28.417','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583127 AND AD_Language='de_CH'
+;
+
+-- 2024-05-31T08:13:28.421Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583127,'de_CH')
+;
+
