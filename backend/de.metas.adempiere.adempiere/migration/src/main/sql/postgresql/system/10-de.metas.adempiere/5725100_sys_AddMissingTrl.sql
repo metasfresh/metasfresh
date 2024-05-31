5d08cd133e765d8d2556621728eb6fc311540934
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-05-31 11:24:20 +0300

    AddAttributeDependant trl

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725100_sys_AddMissingTrl.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5725100_sys_AddMissingTrl.sql
@@ -43,3 +43,46 @@ UPDATE AD_Element_Trl SET Name='Neupreis',Updated=TO_TIMESTAMP('2024-05-31 11:13
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583127,'de_CH')
 ;
 
+
+
+-- Element: IsAttributeDependant
+-- 2024-05-31T08:21:57.469Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Attribute Dependant', PrintName='Attribute Dependant',Updated=TO_TIMESTAMP('2024-05-31 11:21:57.468','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542237 AND AD_Language='en_GB'
+;
+
+-- 2024-05-31T08:21:57.473Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542237,'en_GB')
+;
+
+-- Element: IsAttributeDependant
+-- 2024-05-31T08:22:09.326Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Attributabhängig', PrintName='Attributabhängig',Updated=TO_TIMESTAMP('2024-05-31 11:22:09.326','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542237 AND AD_Language='de_CH'
+;
+
+-- 2024-05-31T08:22:09.333Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542237,'de_CH')
+;
+
+-- Element: IsAttributeDependant
+-- 2024-05-31T08:22:19.106Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Attribute Dependant', PrintName='Attribute Dependant',Updated=TO_TIMESTAMP('2024-05-31 11:22:19.106','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542237 AND AD_Language='en_US'
+;
+
+-- 2024-05-31T08:22:19.110Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542237,'en_US')
+;
+
+-- Element: IsAttributeDependant
+-- 2024-05-31T08:22:22.387Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-05-31 11:22:22.387','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542237 AND AD_Language='de_DE'
+;
+
+-- 2024-05-31T08:22:22.390Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(542237,'de_DE')
+;
+
+-- 2024-05-31T08:22:22.393Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542237,'de_DE')
+;
+
+
