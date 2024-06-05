e2935f94497273e8c8970d58041cd5141c087d0f
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2024-06-05 15:57:45 +0200

    SQL fixes

---
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5725770_sys_gh17995_fix_AD_Menu_C_Flatrate_Transistion.sql
@@ -0,0 +1,12 @@
+-- 2024-06-05T13:17:54.380Z
+UPDATE AD_Menu SET AD_Element_ID=574261, Description=NULL, Name='Vertragslaufzeit', WEBUI_NameBrowse=NULL, WEBUI_NameNew=NULL, WEBUI_NameNewBreadcrumb=NULL,Updated=TO_TIMESTAMP('2024-06-05 15:17:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540884
+;
+
+-- 2024-06-05T13:17:54.414Z
+/* DDL */  select update_menu_translation_from_ad_element(574261) 
+;
+
+-- 2024-06-05T13:18:04.981Z
+UPDATE AD_Menu SET InternalName='_C_Flatrate_Transition',Updated=TO_TIMESTAMP('2024-06-05 15:18:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=540884
+;
+
