26ba2dadfb584414784fd4730ab6316e1734231a
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-06-06 09:35:06 +0300

    Set back negation for all credit memos

--- backend/de.metas.business/src/main/java/de/metas/allocation/api/impl/AllocationDAO.java
+++ backend/de.metas.business/src/main/java/de/metas/allocation/api/impl/AllocationDAO.java
@@ -88,7 +88,7 @@ public class AllocationDAO implements IAllocationDAO
 			openAmt = invoice.getGrandTotal();
 		}
 
-		if (creditMemoAdjusted && invoiceBL.isARCreditMemo(invoice))
+		if (creditMemoAdjusted && invoiceBL.isCreditMemo(invoice))
 		{
 			return Money.of(openAmt.negate(), invoiceCurrencyId);
 		}
