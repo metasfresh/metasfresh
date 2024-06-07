53cd59dea69336ae81876cda2d261f3e8051a10e
Teodor Sarca <teo.sarca@metasfresh.com>
2024-06-07 10:40:29 +0300

    Modal.js: close printing dialog after print

--- frontend/src/components/app/Modal.js
+++ frontend/src/components/app/Modal.js
@@ -406,7 +406,7 @@ class Modal extends Component {
    * @summary Handle closing modal when the `done` button is clicked or `{esc}` key pressed
    */
   handleClose = () => {
-    const { modalSaveStatus, modalType, dispatch } = this.props;
+    const { modalSaveStatus, modalType } = this.props;
 
     if (modalType === 'process') {
       return this.closeModal(modalSaveStatus);
@@ -415,7 +415,6 @@ class Modal extends Component {
     if (modalSaveStatus || window.confirm('Do you really want to leave?')) {
       this.closeModal(modalSaveStatus);
     }
-    dispatch(resetPrintingOptions());
   };
 
   /**
@@ -486,8 +485,13 @@ class Modal extends Component {
    * @summary before printing we check the available parameters from the store and we use those for forming the final printing URI
    */
   handlePrinting = () => {
-    const { windowId, modalViewDocumentIds, dataId, printingOptions } =
-      this.props;
+    const {
+      windowId,
+      modalViewDocumentIds,
+      dataId,
+      printingOptions,
+      dispatch,
+    } = this.props;
     const documentId = dataId;
     const documentNo = modalViewDocumentIds[0] ?? documentId;
 
@@ -503,7 +507,10 @@ class Modal extends Component {
       options,
     });
 
-    this.handleClose();
+    this.closeModal(true);
+    dispatch(resetPrintingOptions());
+
+    return true; // stopPropagation to avoid calling the global alt-P handler
   };
 
   /**
