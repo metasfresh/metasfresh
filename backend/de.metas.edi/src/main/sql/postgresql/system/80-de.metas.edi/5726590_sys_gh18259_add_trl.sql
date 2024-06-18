e8907b940cd0e37f79340ab0aa45bc613b980c21
cp-ps <cp-ps@pevesoft.ro>
2024-06-18 13:41:57 +0300

    add process trl

---
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5726590_sys_gh18259_add_trl.sql
@@ -0,0 +1,16 @@
+-- Value: EDI_Desadv_InOut_EnqueueForExport
+-- Classname: de.metas.edi.process.EDI_Desadv_InOut_EnqueueForExport
+-- 2024-06-18T10:40:43.659Z
+UPDATE AD_Process SET Name='EDI DESADV Auswahl senden',Updated=TO_TIMESTAMP('2024-06-18 10:40:43.658000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585400
+;
+
+-- Process: EDI_Desadv_InOut_EnqueueForExport(de.metas.edi.process.EDI_Desadv_InOut_EnqueueForExport)
+-- 2024-06-18T10:40:47.906Z
+UPDATE AD_Process_Trl SET Name='EDI DESADV Auswahl senden',Updated=TO_TIMESTAMP('2024-06-18 10:40:47.906000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Process_ID=585400
+;
+
+-- Process: EDI_Desadv_InOut_EnqueueForExport(de.metas.edi.process.EDI_Desadv_InOut_EnqueueForExport)
+-- 2024-06-18T10:40:51.392Z
+UPDATE AD_Process_Trl SET Name='EDI DESADV Auswahl senden',Updated=TO_TIMESTAMP('2024-06-18 10:40:51.391000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Process_ID=585400
+;
+
