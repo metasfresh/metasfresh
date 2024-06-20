4950f837a850727a05705ef0a4573daf75d26cbb
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-06-20 11:59:09 +0300

    Fix preselection of contact types (#18287)

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5726630_sys_AddProcess_ModCntr_Specific_Price_Update_Selection.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5726630_sys_AddProcess_ModCntr_Specific_Price_Update_Selection.sql
@@ -355,3 +355,39 @@ UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='Auswahl beinhaltet untersch
 UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-06-20 10:01:03.833','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545426
 ;
 
+
+-- Element: Price_Old
+-- 2024-06-20T07:22:22.514Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Preis alt', PrintName='Preis alt',Updated=TO_TIMESTAMP('2024-06-20 10:22:22.514','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=543096 AND AD_Language='de_CH'
+;
+
+-- 2024-06-20T07:22:22.521Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(543096,'de_CH')
+;
+
+-- Element: Price_Old
+-- 2024-06-20T07:22:26.071Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-06-20 10:22:26.071','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=543096 AND AD_Language='en_US'
+;
+
+-- 2024-06-20T07:22:26.075Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(543096,'en_US')
+;
+
+-- Element: Price_Old
+-- 2024-06-20T07:22:29.840Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Preis alt', PrintName='Preis alt',Updated=TO_TIMESTAMP('2024-06-20 10:22:29.84','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=543096 AND AD_Language='de_DE'
+;
+
+-- 2024-06-20T07:22:29.842Z
+UPDATE AD_Element SET Name='Preis alt', PrintName='Preis alt' WHERE AD_Element_ID=543096
+;
+
+-- 2024-06-20T07:22:30.301Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(543096,'de_DE')
+;
+
+-- 2024-06-20T07:22:30.303Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(543096,'de_DE')
+;
+
