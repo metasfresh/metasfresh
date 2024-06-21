a88303fa60e4d09533d03e65d8f416b289d859bb
dm-ps <dragos.moca@pevesoft.ro>
2024-06-21 15:08:02 +0300

    mobile UI: Production - Switch to DateStartSchedule refs: https://github.com/metasfresh/metasfresh/issues/18288

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5727040_sys_gh18288_update_sysconfig_mobileuiDefaultFilters.sql
@@ -0,0 +1,7 @@
+-- 2024-06-21T11:49:46.412Z
+UPDATE AD_SysConfig SET ConfigurationLevel='S', Description='Describes which filters to be applied to available manufacturing jobs for logged user in mobile UI.
+Following values are available:
+* UserPlant -show only those manufacturing order which are for a plant which is associated with logged user
+ TodayDateStartSchedule - show only those which have the DateStartSchedule as today.',Updated=TO_TIMESTAMP('2024-06-21 14:49:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541519
+;
+
