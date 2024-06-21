c772c8301bbfa716931e679c63962f5a111695d5
riad.mennour <riad.mennour@gmail.com>
2024-06-21 10:23:37 +0100

    Exclude cases where the column reference = search and field reference != search. Then we know it was set on purpose

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5726940_sys_gh18285_Use_Lookup_Instead_Listview.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5726940_sys_gh18285_Use_Lookup_Instead_Listview.sql
@@ -1,12 +1,15 @@
 select backup_table('ad_column'); -- backup.ad_column_bkp_20240620_114638_818
 select backup_table('ad_field'); -- backup.ad_field_bkp_20240620_114658_431
 
-UPDATE AD_Column
+UPDATE AD_Field f
 SET AD_Reference_ID = 30
-WHERE AD_Reference_ID IN (18, 19)
+WHERE f.AD_Reference_ID IN (18, 19)
+  AND f.AD_Column_ID NOT IN (SELECT AD_Column_ID
+                             FROM AD_Column
+                             WHERE AD_Reference_ID = 30)
 ;
 
-UPDATE AD_Field
+UPDATE AD_Column
 SET AD_Reference_ID = 30
 WHERE AD_Reference_ID IN (18, 19)
 ;
