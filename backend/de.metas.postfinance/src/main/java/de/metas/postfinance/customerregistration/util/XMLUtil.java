467fa199d21945a85ae1cb4606b93973869557a3
Max Rieck <max.rieck@metasfresh.com>
2024-06-28 12:19:15 +0200

    use provided postFinance xsds

--- backend/de.metas.postfinance/src/main/java/de/metas/postfinance/customerregistration/util/XMLUtil.java
+++ backend/de.metas.postfinance/src/main/java/de/metas/postfinance/customerregistration/util/XMLUtil.java
@@ -22,48 +22,34 @@
 
 package de.metas.postfinance.customerregistration.util;
 
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import de.metas.postfinance.ConfiguredXmlMapper;
-import de.metas.postfinance.customerregistration.model.XmlCustomerRegistrationMessage;
+import de.metas.postfinance.customerregistration.CustomerRegistrationMessage;
 import de.metas.postfinance.jaxb.DownloadFile;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.adempiere.exceptions.AdempiereException;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.StringWriter;
-
-import static de.metas.postfinance.PostFinanceConstants.XML_PROPERTY_VALUE_YES;
 
 @UtilityClass
 public class XMLUtil
 {
 	@NonNull
-	public XmlCustomerRegistrationMessage getXmlCustomerRegistrationMessage(@NonNull final DownloadFile downloadFile)
+	public CustomerRegistrationMessage getCustomerRegistrationMessage(@NonNull final DownloadFile downloadFile)
 	{
 		try
 		{
-			final Document document = readFromDownloadFile(downloadFile);
-			final String documentAsString = toString(document);
+			final JAXBContext jc = JAXBContext.newInstance(CustomerRegistrationMessage.class);
+			final Unmarshaller unmarshaller = jc.createUnmarshaller();
+			final ByteArrayInputStream inputStream = new ByteArrayInputStream(downloadFile.getData().getValue());
 
-			final XmlMapper xmlMapper = ConfiguredXmlMapper.get();
-			return xmlMapper.readValue(documentAsString, XmlCustomerRegistrationMessage.class);
+			return (CustomerRegistrationMessage)unmarshaller.unmarshal(inputStream);
 		}
-		catch (final Exception e)
+		catch (final JAXBException e)
 		{
-			throw new AdempiereException(e);
+			throw AdempiereException.wrapIfNeeded(e);
 		}
 	}
 
@@ -72,33 +58,5 @@ public class XMLUtil
 	{
 		final String name = filename.trim().toLowerCase();
 		return name.endsWith(".xml");
-	}
-	
-	@NonNull
-	private Document readFromDownloadFile(@NonNull final DownloadFile downloadFile) throws ParserConfigurationException, IOException, SAXException
-	{
-		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		final DocumentBuilder builder = factory.newDocumentBuilder();
-		final Document document = builder.parse(new ByteArrayInputStream(downloadFile.getData().getValue()));
-
-		//dev-note: ensure that the document hierarchy isn't affected by any extra white spaces or new lines within nodes.
-		document.getDocumentElement().normalize();
-
-		return document;
-	}
-
-	@NonNull
-	private String toString(@NonNull final Document document) throws TransformerException
-	{
-		final TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		final Transformer transformer = transformerFactory.newTransformer();
-		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, XML_PROPERTY_VALUE_YES);
-
-		final StringWriter buffer = new StringWriter();
-
-		transformer.transform(new DOMSource(document.getDocumentElement()),
-							  new StreamResult(buffer));
-
-		return buffer.toString();
 	}
 }
