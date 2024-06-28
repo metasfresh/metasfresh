cb97cbf73ec71baa176791b7a7873f3fb48b81af
riad.mennour <riad.mennour@gmail.com>
2024-06-28 10:06:29 +0100

    Exclude fields (leave them as list) where fields != search and column=search

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5726940_sys_gh18285_Use_Lookup_Instead_Listview.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5726940_sys_gh18285_Use_Lookup_Instead_Listview.sql
@@ -1,12 +1,11 @@
 select backup_table('ad_column'); -- backup.ad_column_bkp_20240620_114638_818
 select backup_table('ad_field'); -- backup.ad_field_bkp_20240620_114658_431
 
-UPDATE AD_Field f
-SET AD_Reference_ID = 30
-WHERE f.AD_Reference_ID IN (18, 19)
-  AND f.AD_Column_ID NOT IN (SELECT AD_Column_ID
-                             FROM AD_Column
-                             WHERE AD_Reference_ID = 30)
+UPDATE AD_Field f SET AD_Reference_ID = 30
+FROM AD_Column c
+WHERE f.AD_Column_ID=c.AD_Column_ID
+  AND f.AD_Reference_ID IS NOT NULL
+  AND f.AD_Reference_ID=c.AD_Reference_ID
 ;
 
 UPDATE AD_Column
