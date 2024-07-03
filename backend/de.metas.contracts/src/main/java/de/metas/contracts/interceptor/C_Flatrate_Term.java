6950e5400a017da51266dcee6b1b5e04682fe700
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-07-03 12:26:29 +0300

    Fix moel validator

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_Flatrate_Term.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/interceptor/C_Flatrate_Term.java
@@ -250,19 +250,16 @@ public class C_Flatrate_Term
 			}
 			else
 			{
-				final LocalDateAndOrgId firstPeriodStartDate = LocalDateAndOrgId.ofTimestamp(periodsOfTerm.get(0).getStartDate(), orgId, orgDAO::getTimeZone);
-				if (firstPeriodStartDate.compareTo(startDate) >= 0)
+				if (periodsOfTerm.get(0).getStartDate().after(term.getStartDate()))
 				{
 					errors.add(msgBL.getMsg(ctx, MSG_TERM_ERROR_PERIOD_START_DATE_AFTER_TERM_START_DATE_2P,
 							new Object[] { term.getStartDate(), invoicingCal.getName() }));
 				}
-
-				final LocalDateAndOrgId lastPeriodEndDate = LocalDateAndOrgId.ofTimestamp(periodsOfTerm.get(periodsOfTerm.size() - 1).getEndDate(), orgId, orgDAO::getTimeZone);
-				
-				if (lastPeriodEndDate.compareTo(endDate) <= 0)
+				final I_C_Period lastPeriodOfTerm = periodsOfTerm.get(periodsOfTerm.size() - 1);
+				if (lastPeriodOfTerm.getEndDate().before(term.getEndDate()))
 				{
 					errors.add(msgBL.getMsg(ctx, MSG_TERM_ERROR_PERIOD_END_DATE_BEFORE_TERM_END_DATE_2P,
-							new Object[] { lastPeriodEndDate.toLocalDate(), invoicingCal.getName() }));
+							new Object[] { lastPeriodOfTerm.getEndDate(), invoicingCal.getName() }));
 				}
 			}
 		}
