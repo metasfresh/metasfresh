8740c28d31dc52f36182ac4b6b0fa612ba5526bf
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2024-07-05 15:39:11 +0200

    Add ILIKE operator so we can also lookup via substrings
    also a few minor improvements
    
    #18408

--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5727930_sys_gh18408_C_BPartner_Lookup_Label.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5727930_sys_gh18408_C_BPartner_Lookup_Label.sql
@@ -1,3 +1,21 @@
+-- 2024-07-05T06:58:39.096Z
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,583167,0,'Lookup_Label',TO_TIMESTAMP('2024-07-05 06:58:38.909000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','Unterscheidsungs-Label','Unterscheidsungs-Label',TO_TIMESTAMP('2024-07-05 06:58:38.909000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2024-07-05T06:58:39.101Z
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=583167 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- Element: Lookup_Label
+-- 2024-07-05T07:00:15.225Z
+UPDATE AD_Element_Trl SET Description='Dient der Unterscheidung, wenn verschiedene Datensätze ansonsten gleich Lookup-Merkmale haben', IsTranslated='Y',Updated=TO_TIMESTAMP('2024-07-05 07:00:15.225000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583167 AND AD_Language='de_CH'
+;
+
+-- 2024-07-05T07:00:15.266Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583167,'de_CH')
+;
+
+
 -- Element: Lookup_Label
 -- 2024-07-05T07:28:00.869Z
 UPDATE AD_Element_Trl SET Description='Dient der Unterscheidung, wenn verschiedene Datensätze ansonsten gleiche Lookup-Merkmale haben.', Name='Unterscheidungs-Label', PrintName='Unterscheidungs-Label',Updated=TO_TIMESTAMP('2024-07-05 07:28:00.869000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583167 AND AD_Language='de_CH'
