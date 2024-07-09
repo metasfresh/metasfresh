ace4c3655ff240d7fad070be667204a36d1a26ab
Teodor Sarca <teo.sarca@metasfresh.com>
2024-07-09 12:04:39 +0300

    fix after merge

--- backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5728090_cli_M_Picking_Job_Step_PickedHU_Picked_RenderedQRCode_migrate.sql
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5728090_cli_M_Picking_Job_Step_PickedHU_Picked_RenderedQRCode_migrate.sql
@@ -1,2 +1,7 @@
-update M_Picking_Job_Step_PickedHU p set Picked_RenderedQRCode=(select qr.renderedqrcode from M_HU_QRCode qr where qr.m_hu_id=p.picked_hu_id)
-where Picked_RenderedQRCode is null;
+UPDATE M_Picking_Job_Step_PickedHU p
+SET Picked_RenderedQRCode=(SELECT qr.renderedqrcode
+                           FROM M_HU_QRCode_Assignment a
+                                    INNER JOIN M_HU_QRCode qr ON qr.m_hu_qrcode_id = a.m_hu_qrcode_id
+                           WHERE a.m_hu_id = p.picked_hu_id)
+WHERE Picked_RenderedQRCode IS NULL
+;
