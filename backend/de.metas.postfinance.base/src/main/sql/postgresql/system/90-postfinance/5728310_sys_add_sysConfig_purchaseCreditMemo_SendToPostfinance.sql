792e045552785d89f4cfd5c0248d15d0a6dc307b
Max Rieck <32903793+mrie04@users.noreply.github.com>
2024-07-11 09:35:35 +0200

    Add postFinance purchase credit memo support (#18432)
    * Add postFinance purchase credit memo support
    
    * fix sysCfg
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ backend/de.metas.postfinance.base/src/main/sql/postgresql/system/90-postfinance/5728310_sys_add_sysConfig_purchaseCreditMemo_SendToPostfinance.sql
@@ -0,0 +1,8 @@
+-- Run mode: SWING_CLIENT
+
+-- SysConfig Name: de.metas.postfinance.document.export.impl.PurchaseCreditMemoPostFinanceYbInvoiceHandler.isActive
+-- SysConfig Value: N
+-- 2024-07-10T13:32:06.666Z
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541726,'S',TO_TIMESTAMP('2024-07-10 15:32:06.461','YYYY-MM-DD HH24:MI:SS.US'),100,'Sending of purchase credit memos to postfinance can be turned on/off','de.metas.postfinance','Y','de.metas.postfinance.document.export.impl.PurchaseCreditMemoPostFinanceYbInvoiceHandler.isActive',TO_TIMESTAMP('2024-07-10 15:32:06.461','YYYY-MM-DD HH24:MI:SS.US'),100,'N')
+;
+
