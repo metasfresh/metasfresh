48c47446d8de5a98a4bcbf06ac72f7b582b94318
riad.mennour <riad.mennour@gmail.com>
2024-07-16 10:22:35 +0100

    Using getSingleSelectedRecordId instead of getSelectedModel

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/split_shipment/SplitShipmentView_Launcher.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/split_shipment/SplitShipmentView_Launcher.java
@@ -2,6 +2,7 @@ package de.metas.ui.web.split_shipment;
 
 import de.metas.i18n.AdMessageKey;
 import de.metas.inout.ShipmentScheduleId;
+import de.metas.inoutcandidate.api.IShipmentScheduleBL;
 import de.metas.inoutcandidate.model.I_M_ShipmentSchedule;
 import de.metas.process.IProcessPrecondition;
 import de.metas.process.IProcessPreconditionsContext;
@@ -10,12 +11,14 @@ import de.metas.process.ProcessExecutionResult;
 import de.metas.process.ProcessPreconditionsResolution;
 import de.metas.ui.web.view.IViewsRepository;
 import de.metas.ui.web.view.ViewId;
+import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.SpringContextHolder;
 
 public class SplitShipmentView_Launcher extends JavaProcess implements IProcessPrecondition
 {
+	private final IShipmentScheduleBL shipmentScheduleBL = Services.get(IShipmentScheduleBL.class);
 	private final IViewsRepository viewsFactory = SpringContextHolder.instance.getBean(IViewsRepository.class);
 	private static final AdMessageKey PRECONDITION_MSG_ONLY_OPEN_STATUS = AdMessageKey.of("de.metas.ui.web.split_shipment.SplitShipmentView_Launcher.OnlyOpenedStatusSelection");
 
@@ -27,9 +30,10 @@ public class SplitShipmentView_Launcher extends JavaProcess implements IProcessP
 			return ProcessPreconditionsResolution.rejectBecauseNotSingleSelection().toInternal();
 		}
 
-		final I_M_ShipmentSchedule shipmentScheduleRecord = context.getSelectedModel(I_M_ShipmentSchedule.class);
+		final ShipmentScheduleId shipmentScheduleId = context.getSingleSelectedRecordId(ShipmentScheduleId.class);
+		final I_M_ShipmentSchedule shipmentScheduleRecord = shipmentScheduleBL.getById(shipmentScheduleId);
 
-		if(shipmentScheduleRecord.isClosed())
+		if (shipmentScheduleRecord.isClosed())
 		{
 			return ProcessPreconditionsResolution.reject(PRECONDITION_MSG_ONLY_OPEN_STATUS);
 		}
@@ -45,9 +49,9 @@ public class SplitShipmentView_Launcher extends JavaProcess implements IProcessP
 				.getViewId();
 
 		getResult().setWebuiViewToOpen(ProcessExecutionResult.WebuiViewToOpen.builder()
-				.viewId(viewId.getViewId())
-				.target(ProcessExecutionResult.ViewOpenTarget.ModalOverlay)
-				.build());
+											   .viewId(viewId.getViewId())
+											   .target(ProcessExecutionResult.ViewOpenTarget.ModalOverlay)
+											   .build());
 
 		return MSG_OK;
 	}
