9fabd6d1559961851b69c64feff3a359e1e5260c
Max Rieck <max.rieck@metasfresh.com>
2024-07-24 08:11:52 +0200

    compare qty on md_candidate change

--- backend/de.metas.material/dispo-service/src/main/java/de/metas/material/dispo/service/interceptor/MD_Candidate.java
+++ backend/de.metas.material/dispo-service/src/main/java/de/metas/material/dispo/service/interceptor/MD_Candidate.java
@@ -221,7 +221,8 @@ public class MD_Candidate
 		return !candidateRecord.getDateProjected().equals(oldCandidateRecord.getDateProjected())
 				|| !candidateRecord.getStorageAttributesKey().equals(oldCandidateRecord.getStorageAttributesKey())
 				|| oldCandidateRecord.getM_Warehouse_ID() != candidateRecord.getM_Warehouse_ID()
-				|| oldCandidateRecord.getM_Product_ID() != candidateRecord.getM_Product_ID();
+				|| oldCandidateRecord.getM_Product_ID() != candidateRecord.getM_Product_ID()
+				|| oldCandidateRecord.getQty().compareTo(candidateRecord.getQty()) != 0;
 	}
 	
 	private static boolean isUpdateOldStockRequired(
