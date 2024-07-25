6150f374279d5de6b1e53396729cedf3a5cb07d0
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-07-25 23:24:09 +0300

    interest window corrections

---
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5730130_sys_Hide_InterimContract_From_Interest.sql
@@ -0,0 +1,134 @@
+-- Run mode: SWING_CLIENT
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Akonto-vertrag
+-- Column: ModCntr_Interest_V.C_Interim_Flatrate_Term_ID
+-- 2024-07-25T10:50:45.531Z
+UPDATE AD_UI_Element SET IsDisplayed='N',Updated=TO_TIMESTAMP('2024-07-25 13:50:45.531','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624977
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Rechnungsgruppe
+-- Column: ModCntr_Interest_V.ModCntr_InvoicingGroup_ID
+-- 2024-07-25T10:55:26.561Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=10,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.561','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624755
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Pauschale - Vertragsperiode
+-- Column: ModCntr_Interest_V.C_Flatrate_Term_ID
+-- 2024-07-25T10:55:26.570Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.57','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624751
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Shipping Notification
+-- Column: ModCntr_Interest_V.M_Shipping_Notification_ID
+-- 2024-07-25T10:55:26.577Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.577','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624978
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Ursprüngliches Produkt
+-- Column: ModCntr_Interest_V.Initial_Product_ID
+-- 2024-07-25T10:55:26.583Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.583','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624750
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Handlertyp für modularen Vertrag
+-- Column: ModCntr_Interest_V.ModularContractHandlerType
+-- 2024-07-25T10:55:26.588Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.588','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624752
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Rechnungspartner
+-- Column: ModCntr_Interest_V.Bill_BPartner_ID
+-- 2024-07-25T10:55:26.594Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.594','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624768
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Endgültige Zinsen
+-- Column: ModCntr_Interest_V.FinalInterest
+-- 2024-07-25T10:55:26.600Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.6','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624767
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Schlussabrechnung
+-- Column: ModCntr_Interest_V.C_FinalInvoice_ID
+-- 2024-07-25T10:55:26.606Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.606','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624749
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Vorgangsdatum
+-- Column: ModCntr_Interest_V.DateTrx
+-- 2024-07-25T10:55:26.612Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=90,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.612','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624757
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Menge
+-- Column: ModCntr_Interest_V.Qty
+-- 2024-07-25T10:55:26.617Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.617','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624758
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Maßeinheit
+-- Column: ModCntr_Interest_V.C_UOM_ID
+-- 2024-07-25T10:55:26.621Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=110,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.621','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624759
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Zusätzliche Zinstage
+-- Column: ModCntr_Interest_V.AddInterestDays
+-- 2024-07-25T10:55:26.626Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=120,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.626','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624760
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Zinssatz
+-- Column: ModCntr_Interest_V.InterestRate
+-- 2024-07-25T10:55:26.631Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=130,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.631','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624761
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Vorfinanzierungsbetrag
+-- Column: ModCntr_Interest_V.InterimAmt
+-- 2024-07-25T10:55:26.636Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=140,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.636','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624762
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Übereinstimmender Betrag
+-- Column: ModCntr_Interest_V.MatchedAmt
+-- 2024-07-25T10:55:26.641Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=150,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.641','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624763
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Gesamtbetrag
+-- Column: ModCntr_Interest_V.TotalAmt
+-- 2024-07-25T10:55:26.646Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=160,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.646','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624764
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Zinstage
+-- Column: ModCntr_Interest_V.InterestDays
+-- 2024-07-25T10:55:26.651Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=170,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.651','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624765
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Zinsnummer
+-- Column: ModCntr_Interest_V.InterestScore
+-- 2024-07-25T10:55:26.656Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=180,Updated=TO_TIMESTAMP('2024-07-25 13:55:26.656','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624766
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Gesamtbetrag
+-- Column: ModCntr_Interest_V.TotalAmt
+-- 2024-07-25T10:58:09.220Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2024-07-25 13:58:09.22','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624764
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Zinstage
+-- Column: ModCntr_Interest_V.InterestDays
+-- 2024-07-25T10:58:09.225Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=160,Updated=TO_TIMESTAMP('2024-07-25 13:58:09.225','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624765
+;
+
+-- UI Element: Zinsberechnungslauf(541801,de.metas.contracts) -> Zins(547546,de.metas.contracts) -> 1000029 -> 10 -> main.Zinsnummer
+-- Column: ModCntr_Interest_V.InterestScore
+-- 2024-07-25T10:58:09.230Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=170,Updated=TO_TIMESTAMP('2024-07-25 13:58:09.23','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=624766
+;
+
