4f7b6c8aaffdfba99600788da364256bcee7d761
Teodor Sarca <teo.sarca@metasfresh.com>
2024-07-26 02:12:42 +0300

    fix cucumber tests

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipper/M_Shipper_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipper/M_Shipper_StepDef.java
@@ -25,6 +25,7 @@ package de.metas.cucumber.stepdefs.shipper;
 import de.metas.common.util.CoalesceUtil;
 import de.metas.cucumber.stepdefs.DataTableRow;
 import de.metas.cucumber.stepdefs.DataTableRows;
+import de.metas.cucumber.stepdefs.DataTableUtil;
 import de.metas.cucumber.stepdefs.ValueAndName;
 import de.metas.organization.OrgId;
 import de.metas.shipping.IShipperDAO;
@@ -34,9 +35,12 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.model.I_M_Shipper;
 import org.compiere.util.Env;
+
+import static de.metas.cucumber.stepdefs.StepDefConstants.TABLECOLUMN_IDENTIFIER;
 
 @RequiredArgsConstructor
 public class M_Shipper_StepDef
@@ -44,6 +48,23 @@ public class M_Shipper_StepDef
 	private final IShipperDAO shipperDAO = Services.get(IShipperDAO.class);
 
 	@NonNull private final M_Shipper_StepDefData shipperTable;
+
+	@And("load M_Shipper:")
+	public void load_M_Shipper(@NonNull final DataTable dataTable)
+	{
+		DataTableRows.of(dataTable).forEach(this::loadM_Shipper);
+	}
+
+	private void loadM_Shipper(@NonNull final DataTableRow row)
+	{
+
+		final String name = row.getAsString(I_M_Shipper.COLUMNNAME_Name);
+		final I_M_Shipper shipperRecord = shipperDAO.getByName(name).orElseThrow(() -> new AdempiereException("No shipper found for " + name));
+		row.getAsOptionalString(I_M_Shipper.COLUMNNAME_InternalName).ifPresent(shipperRecord::setInternalName);
+		InterfaceWrapperHelper.saveRecord(shipperRecord);
+
+		shipperTable.put(row.getAsIdentifier(), shipperRecord);
+	}
 
 	@And("contains M_Shippers")
 	public void createOrUpdateShippers(@NonNull final DataTable dataTable)
