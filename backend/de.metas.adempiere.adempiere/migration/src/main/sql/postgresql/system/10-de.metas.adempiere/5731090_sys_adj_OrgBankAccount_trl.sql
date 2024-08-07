b89ff490e9b865eeffff18ccbd8a83cc7c6b753c
cp-ps <77785468+cp-ps@users.noreply.github.com>
2024-08-07 12:31:09 +0200

    adjust orgBpAccount trl

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5731090_sys_adj_OrgBankAccount_trl.sql
@@ -0,0 +1,28 @@
+-- Run mode: SWING_CLIENT
+
+-- Element: Org_BP_Account_ID
+-- 2024-08-07T14:27:13.628Z
+UPDATE AD_Element_Trl SET Name='Organisation Bankkonto', PrintName='Organisation Bankkonto',Updated=TO_TIMESTAMP('2024-08-07 16:27:13.628','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583191 AND AD_Language='de_CH'
+;
+
+-- 2024-08-07T14:27:13.651Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583191,'de_CH')
+;
+
+-- Element: Org_BP_Account_ID
+-- 2024-08-07T14:27:27.912Z
+UPDATE AD_Element_Trl SET Name='Organisation Bankkonto', PrintName='Organisation Bankkonto',Updated=TO_TIMESTAMP('2024-08-07 16:27:27.912','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583191 AND AD_Language='de_DE'
+;
+
+-- 2024-08-07T14:27:27.914Z
+UPDATE AD_Element SET Name='Organisation Bankkonto', PrintName='Organisation Bankkonto' WHERE AD_Element_ID=583191
+;
+
+-- 2024-08-07T14:27:28.284Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583191,'de_DE')
+;
+
+-- 2024-08-07T14:27:28.286Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583191,'de_DE')
+;
+
