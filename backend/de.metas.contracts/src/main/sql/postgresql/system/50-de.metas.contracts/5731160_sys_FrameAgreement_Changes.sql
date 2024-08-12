52f3e2fcbd2cca7f34ac480986b46c760c21327d
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-08-12 17:11:30 +0300

    remove mandatory logic that makes no sense

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5731160_sys_FrameAgreement_Changes.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5731160_sys_FrameAgreement_Changes.sql
@@ -263,3 +263,12 @@ UPDATE AD_Val_Rule SET Code='C_Flatrate_Term.Bill_BPartner_ID=@C_BPartner_ID@ AN
 
 
 
+
+-- Column: C_Order.C_FrameAgreement_Order_ID
+-- 2024-08-12T13:57:03.372Z
+UPDATE AD_Column SET MandatoryLogic='',Updated=TO_TIMESTAMP('2024-08-12 16:57:03.372','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=573002
+;
+
+
+
+
