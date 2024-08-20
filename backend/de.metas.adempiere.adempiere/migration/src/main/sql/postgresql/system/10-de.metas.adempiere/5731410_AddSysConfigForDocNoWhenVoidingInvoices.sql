044d0e26f37a774342b967c88de13090fc1dc923
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-08-20 18:38:30 +0300

    Remode updating sys config to N

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5731410_AddSysConfigForDocNoWhenVoidingInvoices.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5731410_AddSysConfigForDocNoWhenVoidingInvoices.sql
@@ -4,13 +4,6 @@
 INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541730,'S',TO_TIMESTAMP('2024-08-20 17:52:42.139','YYYY-MM-DD HH24:MI:SS.US'),100,'If set to Y, then metasfresh will  he documentno will be added to description when the invoice gets voided.','D','Y','org.compiere.model.MInvoice.ANNOTATE_DOCNO_INVOICE',TO_TIMESTAMP('2024-08-20 17:52:42.139','YYYY-MM-DD HH24:MI:SS.US'),100,'Y')
 ;
 
--- SysConfig Name: org.compiere.model.MInvoice.ANNOTATE_DOCNO_INVOICE
--- SysConfig Value: N
--- SysConfig Value (old): Y
--- 2024-08-20T14:52:45.495Z
-UPDATE AD_SysConfig SET ConfigurationLevel='S', Value='N',Updated=TO_TIMESTAMP('2024-08-20 17:52:45.493','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_SysConfig_ID=541730
-;
-
 -- SysConfig Name: org.compiere.model.MInvoice.ANNOTATE_DOCNO_INVOICE_TO_DESCRIPTION
 -- SysConfig Value: N
 -- 2024-08-20T15:32:18.853Z
