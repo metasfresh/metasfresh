c3603ebbad7623d64669a7e441676e2735ce2f86
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-08-21 11:24:25 +0300

    script cleanup

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5731450_sys_HarvestingDetails_Order_Windows.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5731450_sys_HarvestingDetails_Order_Windows.sql
@@ -70,6 +70,94 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Field: Bestellung(181,D) -> Bestellung(294,D) -> Erntekalender
+-- Column: C_Order.C_Harvesting_Calendar_ID
+-- 2024-08-20T10:55:29.771Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,FacetFilterSeqNo,
+IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsForbidNewRecordCreation,IsHeading,IsOverrideFilterDefaultValue,IsReadOnly,IsSameLine,
+MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,587165,729798,0,294,0,
+TO_TIMESTAMP('2024-08-20 10:55:29.650000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,0,'de.metas.contracts',0,0,'Y','Y','Y','N','N','N','N','N','N',
+'N',0,'Erntekalender',0,0,230,0,1,1,TO_TIMESTAMP('2024-08-20 10:55:29.650000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2024-08-20T10:55:29.772Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=729798 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-08-20T10:55:29.774Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(581157)
+;
+
+-- 2024-08-20T10:55:29.778Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=729798
+;
+
+-- 2024-08-20T10:55:29.779Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(729798)
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Field: Bestellung(181,D) -> Bestellung(294,D) -> Erntejahr
+-- Column: C_Order.Harvesting_Year_ID
+-- 2024-08-20T10:55:39.381Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,FacetFilterSeqNo,IncludedTabHeight,
+IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsForbidNewRecordCreation,IsHeading,IsOverrideFilterDefaultValue,IsReadOnly,IsSameLine,MaxFacetsToFetch,Name,
+SelectionColumnSeqNo,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,587166,729799,0,294,0,
+TO_TIMESTAMP('2024-08-20 10:55:39.261000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,0,'de.metas.contracts',0,0,'Y','Y','Y','N','N','N','N','N','N','N',
+0,'Erntejahr',0,0,240,0,1,1,TO_TIMESTAMP('2024-08-20 10:55:39.261000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2024-08-20T10:55:39.382Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=729799 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-08-20T10:55:39.383Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(582471)
+;
+
+-- 2024-08-20T10:55:39.386Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=729799
+;
+
+-- 2024-08-20T10:55:39.387Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(729799)
+;
+
+
+
 -- UI Element: Bestellung(181,D) -> Bestellung(294,D) -> advanced edit -> 10 -> advanced edit.Erntekalender
 -- Column: C_Order.C_Harvesting_Calendar_ID
 -- 2024-08-20T13:42:37.929Z
