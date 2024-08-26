2c2dfd45a1ca9199409d6f7d6ad1b11b30359323
Max Rieck <max.rieck@metasfresh.com>
2024-08-26 09:28:19 +0200

    Replace PP_Order dateOrdered filter with dateStartSchedule

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5731650_sys_pp_order_filter_startDateSchedule.sql
@@ -0,0 +1,12 @@
+-- Column: PP_Order.DateOrdered
+-- Column: PP_Order.DateOrdered
+-- 2024-08-26T06:39:54.146Z
+UPDATE AD_Column SET IsSelectionColumn='N',Updated=TO_TIMESTAMP('2024-08-26 08:39:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53640
+;
+
+-- Column: PP_Order.DateStartSchedule
+-- Column: PP_Order.DateStartSchedule
+-- 2024-08-26T06:40:08.291Z
+UPDATE AD_Column SET FilterOperator='B', IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2024-08-26 08:40:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53643
+;
+
