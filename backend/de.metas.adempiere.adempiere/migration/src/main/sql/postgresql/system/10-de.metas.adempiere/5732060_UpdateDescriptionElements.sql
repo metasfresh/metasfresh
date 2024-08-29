2a810f94372fed2d174fcf152bf664462c254922
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-08-29 14:51:18 +0300

    Add some trls

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5732060_UpdateDescriptionElements.sql
@@ -0,0 +1,37 @@
+-- Run mode: SWING_CLIENT
+
+-- Element: AD_PrintFormat_ID
+-- 2024-08-29T11:11:06.480Z
+UPDATE AD_Element_Trl SET Description='Das Druckformat legt fest, wie die Daten für den Druck aufbereitet werden.',Updated=TO_TIMESTAMP('2024-08-29 14:11:06.48','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1790 AND AD_Language='de_CH'
+;
+
+-- 2024-08-29T11:11:06.511Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1790,'de_CH')
+;
+
+-- Element: AD_PrintFormat_ID
+-- 2024-08-29T11:11:11.950Z
+UPDATE AD_Element_Trl SET Description='The print format determines how data is rendered for print.',Updated=TO_TIMESTAMP('2024-08-29 14:11:11.95','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1790 AND AD_Language='en_US'
+;
+
+-- 2024-08-29T11:11:11.953Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1790,'en_US')
+;
+
+-- Element: AD_PrintFormat_ID
+-- 2024-08-29T11:11:18.958Z
+UPDATE AD_Element_Trl SET Description='Das Druckformat legt fest, wie die Daten für den Druck aufbereitet werden.',Updated=TO_TIMESTAMP('2024-08-29 14:11:18.958','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=1790 AND AD_Language='de_DE'
+;
+
+-- 2024-08-29T11:11:18.959Z
+UPDATE AD_Element SET Description='Das Druckformat legt fest, wie die Daten für den Druck aufbereitet werden.' WHERE AD_Element_ID=1790
+;
+
+-- 2024-08-29T11:11:19.243Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(1790,'de_DE')
+;
+
+-- 2024-08-29T11:11:19.246Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1790,'de_DE')
+;
+
