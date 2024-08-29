8f1d35b9ee91558cb5902a54e67573c8d3c1b0dc
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-08-29 15:14:24 +0300

    Add more description and adjust window

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5732060_UpdateDescriptionElements.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5732060_UpdateDescriptionElements.sql
@@ -35,4 +35,185 @@ UPDATE AD_Element SET Description='Das Druckformat legt fest, wie die Daten für
 /* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(1790,'de_DE')
 ;
 
+-- Element: CCPath
+-- 2024-08-29T11:13:24.263Z
+UPDATE AD_Element_Trl SET Description='File Path used to store the file',Updated=TO_TIMESTAMP('2024-08-29 14:13:24.263','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542348 AND AD_Language='en_US'
+;
+
+-- 2024-08-29T11:13:24.266Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542348,'en_US')
+;
+
+-- Element: CCPath
+-- 2024-08-29T11:13:27.310Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:13:27.31','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542348 AND AD_Language='en_US'
+;
+
+-- 2024-08-29T11:13:27.313Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542348,'en_US')
+;
+
+-- Element: CCPath
+-- 2024-08-29T11:13:40.125Z
+UPDATE AD_Element_Trl SET Description='Dateipfad, der zum Speichern der Datei verwendet wird', IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:13:40.125','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542348 AND AD_Language='de_DE'
+;
+
+-- 2024-08-29T11:13:40.127Z
+UPDATE AD_Element SET Description='Dateipfad, der zum Speichern der Datei verwendet wird' WHERE AD_Element_ID=542348
+;
+
+-- 2024-08-29T11:13:40.351Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(542348,'de_DE')
+;
+
+-- 2024-08-29T11:13:40.352Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542348,'de_DE')
+;
+
+-- Element: C_Doc_Outbound_Config_CC_ID
+-- 2024-08-29T11:14:18.077Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:14:18.076','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583227 AND AD_Language='de_CH'
+;
+
+-- 2024-08-29T11:14:18.080Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583227,'de_CH')
+;
+
+-- Element: C_Doc_Outbound_Config_CC_ID
+-- 2024-08-29T11:14:20.102Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:14:20.102','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583227 AND AD_Language='de_DE'
+;
+
+-- 2024-08-29T11:14:20.105Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583227,'de_DE')
+;
+
+-- 2024-08-29T11:14:20.106Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583227,'de_DE')
+;
+
+-- Element: C_Doc_Outbound_Config_CC_ID
+-- 2024-08-29T11:15:37.878Z
+UPDATE AD_Element_Trl SET Description='Table used to add several printformats that shall be used for the same table', IsTranslated='Y', Name='Outbound Doc Config CC', PrintName='Outbound Doc Config CC',Updated=TO_TIMESTAMP('2024-08-29 14:15:37.877','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583227 AND AD_Language='en_US'
+;
+
+-- 2024-08-29T11:15:37.882Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583227,'en_US')
+;
+
+-- Element: IsDirectEnqueue
+-- 2024-08-29T11:16:42.701Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:16:42.701','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542117 AND AD_Language='de_CH'
+;
+
+-- 2024-08-29T11:16:42.704Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542117,'de_CH')
+;
+
+-- Element: IsDirectEnqueue
+-- 2024-08-29T11:17:15.064Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:17:15.064','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542117 AND AD_Language='de_DE'
+;
+
+-- 2024-08-29T11:17:15.067Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(542117,'de_DE')
+;
+
+-- 2024-08-29T11:17:15.068Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542117,'de_DE')
+;
+
+-- Element: IsDirectEnqueue
+-- 2024-08-29T11:17:39.063Z
+UPDATE AD_Element_Trl SET Description='Decides whether a print queue record should be created automatically when the print item (archive) is created', PrintName='Is Direct Enqueue',Updated=TO_TIMESTAMP('2024-08-29 14:17:39.063','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542117 AND AD_Language='en_US'
+;
+
+-- 2024-08-29T11:17:39.066Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542117,'en_US')
+;
+
+-- Element: C_Doc_Outbound_Config_CC_ID
+-- 2024-08-29T11:18:42.263Z
+UPDATE AD_Element_Trl SET Description='Table used to add several print formats that shall be used for the same table',Updated=TO_TIMESTAMP('2024-08-29 14:18:42.262','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583227 AND AD_Language='en_US'
+;
+
+-- 2024-08-29T11:18:42.266Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583227,'en_US')
+;
+
+-- Element: C_Doc_Outbound_Config_CC_ID
+-- 2024-08-29T11:19:02.274Z
+UPDATE AD_Element_Trl SET Description='Tabelle zum Hinzufügen mehrerer Druckformate, die für dieselbe Tabelle verwendet werden sollen',Updated=TO_TIMESTAMP('2024-08-29 14:19:02.274','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583227 AND AD_Language='de_DE'
+;
+
+-- 2024-08-29T11:19:02.276Z
+UPDATE AD_Element SET Description='Tabelle zum Hinzufügen mehrerer Druckformate, die für dieselbe Tabelle verwendet werden sollen' WHERE AD_Element_ID=583227
+;
+
+-- 2024-08-29T11:19:02.553Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583227,'de_DE')
+;
+
+-- 2024-08-29T11:19:02.555Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583227,'de_DE')
+;
+
+-- Element: C_Doc_Outbound_Config_CC_ID
+-- 2024-08-29T11:19:12.600Z
+UPDATE AD_Element_Trl SET Description='Tabelle zum Hinzufügen mehrerer Druckformate, die für dieselbe Tabelle verwendet werden sollen',Updated=TO_TIMESTAMP('2024-08-29 14:19:12.599','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=583227 AND AD_Language='de_CH'
+;
+
+-- 2024-08-29T11:19:12.603Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583227,'de_CH')
+;
+
+-- Element: IsDirectProcessQueueItem
+-- 2024-08-29T11:19:45.541Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-08-29 14:19:45.541','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Element_ID=542655 AND AD_Language='en_GB'
+;
+
+-- 2024-08-29T11:19:45.544Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(542655,'en_GB')
+;
+
+-- 2024-08-29T11:23:25.415Z
+INSERT INTO AD_PrintFormat (AD_Client_ID,AD_Org_ID,AD_PrintColor_ID,AD_PrintFont_ID,AD_Printformat_Group,AD_PrintFormat_ID,AD_PrintPaper_ID,AD_Table_ID,CreateCopy,Created,CreatedBy,FooterMargin,HeaderMargin,IsActive,IsDefault,IsForm,IsStandardHeaderFooter,IsTableBased,JasperProcess_ID,Name,Updated,UpdatedBy) VALUES (0,0,100,540006,'None',540136,102,542365,'N',TO_TIMESTAMP('2024-08-29 14:23:25.287','YYYY-MM-DD HH24:MI:SS.US'),100,0,0,'Y','N','N','Y','Y',585317,'Shipping Notification',TO_TIMESTAMP('2024-08-29 14:23:25.287','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- Field: Lieferavis(541734,de.metas.shippingnotification) -> Lieferavis(547218,de.metas.shippingnotification) -> Lieferung von
+-- Column: M_Shipping_Notification.ShipFrom_Partner_ID
+-- 2024-08-29T12:09:02.140Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsForbidNewRecordCreation,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,588928,729841,0,547218,0,TO_TIMESTAMP('2024-08-29 15:09:01.937','YYYY-MM-DD HH24:MI:SS.US'),100,0,'D',0,'Y','Y','Y','N','N','N','N','N','N','Lieferung von',0,110,0,1,1,TO_TIMESTAMP('2024-08-29 15:09:01.937','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-08-29T12:09:02.148Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=729841 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-08-29T12:09:02.151Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(583229)
+;
+
+-- 2024-08-29T12:09:02.162Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=729841
+;
+
+-- 2024-08-29T12:09:02.165Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(729841)
+;
+
+-- UI Element: Lieferavis(541734,de.metas.shippingnotification) -> Lieferavis(547218,de.metas.shippingnotification) -> main -> 10 -> default.Lieferung von
+-- Column: M_Shipping_Notification.ShipFrom_Partner_ID
+-- 2024-08-29T12:09:19.566Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,729841,0,547218,551131,625300,'F',TO_TIMESTAMP('2024-08-29 15:09:19.425','YYYY-MM-DD HH24:MI:SS.US'),100,'Y','N','N','Y','N','N','N',0,'Lieferung von',80,0,0,TO_TIMESTAMP('2024-08-29 15:09:19.425','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- UI Element: Lieferavis(541734,de.metas.shippingnotification) -> Lieferavis(547218,de.metas.shippingnotification) -> main -> 10 -> default.Lieferung von
+-- Column: M_Shipping_Notification.ShipFrom_Partner_ID
+-- 2024-08-29T12:10:11.030Z
+UPDATE AD_UI_Element SET SeqNo=49,Updated=TO_TIMESTAMP('2024-08-29 15:10:11.03','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=625300
+;
+
+
 UPDATE public.c_doctype SET ad_printformat_id = 540136, name = 'Lieferavis' WHERE c_doctype_id = 541109;
+
