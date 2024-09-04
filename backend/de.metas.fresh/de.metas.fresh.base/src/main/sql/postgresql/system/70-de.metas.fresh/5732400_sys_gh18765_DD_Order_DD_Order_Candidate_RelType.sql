b2de365dd472593e19e859a523cc4670c6f36990
riad.mennour <riad.mennour@gmail.com>
2024-09-04 15:48:27 +0100

    Correcting the DD Order standard window

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5732400_sys_gh18765_DD_Order_DD_Order_Candidate_RelType.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5732400_sys_gh18765_DD_Order_DD_Order_Candidate_RelType.sql
@@ -11,7 +11,7 @@ INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name
 -- Table: DD_Order
 -- Key: DD_Order.DD_Order_ID
 -- 2024-09-04T08:20:34.251Z
-INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,AD_Window_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,ShowInactiveValues,Updated,UpdatedBy) VALUES (0,53883,0,541883,53037,541266,TO_TIMESTAMP('2024-09-04 09:20:33','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N',TO_TIMESTAMP('2024-09-04 09:20:33','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,AD_Window_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,ShowInactiveValues,Updated,UpdatedBy) VALUES (0,53883,0,541883,53037,53012,TO_TIMESTAMP('2024-09-04 09:20:33','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','N','N',TO_TIMESTAMP('2024-09-04 09:20:33','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- Name: RelType DD_Order->DD_Order_Candidate
