8f69cd9f3f9b1370ee69330bbd0e3fcff57776fe
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-09-05 12:08:15 +0300

    Add new reference for PP_Product_BOMVersions

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5732490_Make_PP_Product_Planning_PP_Product_BOMVersions_ID_SearchField.sql
@@ -0,0 +1,27 @@
+--
+-- Script: /tmp/webui_migration_scripts_2024-08-26_8376368427608254311/5732490_migration_2024-09-05_postgresql.sql
+-- User: metasfresh
+-- OS user: root
+--
+
+-- Name: PP_Product_BOMVersions_
+-- 2024-09-05T08:59:58.131Z
+INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,541887,TO_TIMESTAMP('2024-09-05 11:59:57.942','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','N','PP_Product_BOMVersions_',TO_TIMESTAMP('2024-09-05 11:59:57.942','YYYY-MM-DD HH24:MI:SS.US'),100,'T')
+;
+
+-- 2024-09-05T08:59:58.141Z
+INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Reference_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Reference t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Reference_ID=541887 AND NOT EXISTS (SELECT 1 FROM AD_Reference_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Reference_ID=t.AD_Reference_ID)
+;
+
+
+-- Column: PP_Product_Planning.PP_Product_BOMVersions_ID
+-- Column: PP_Product_Planning.PP_Product_BOMVersions_ID
+-- 2024-09-05T08:18:23.451Z
+UPDATE AD_Column SET AD_Reference_ID=30,Updated=TO_TIMESTAMP('2024-09-05 10:18:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53397
+;
+
+-- Column: PP_Product_Planning.PP_Product_BOMVersions_ID
+-- Column: PP_Product_Planning.PP_Product_BOMVersions_ID
+-- 2024-09-05T08:19:11.258Z
+UPDATE AD_Column SET AD_Reference_Value_ID=541887, IsExcludeFromZoomTargets='Y',Updated=TO_TIMESTAMP('2024-09-05 10:19:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53397
+;
