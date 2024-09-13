1f986737ebd135e7469ab36e1ae19eaec9eecc7b
Teodor Sarca <teo.sarca@metasfresh.com>
2024-09-13 18:09:15 +0300

    partial cleanup MPOS

--- backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/MPOS.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/compiere/model/MPOS.java
@@ -16,101 +16,60 @@
  *****************************************************************************/
 package org.compiere.model;
 
-import java.sql.ResultSet;
-import java.util.Properties;
-
+import de.metas.util.Services;
 import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.warehouse.WarehouseId;
 import org.adempiere.warehouse.api.IWarehouseDAO;
 
-import de.metas.cache.CCache;
-import de.metas.util.Services;
-
+import java.sql.ResultSet;
+import java.util.Properties;
 
 /**
- *	POS Terminal definition
- *	
- *  @author Jorg Janke
- *  @version $Id: MPOS.java,v 1.3 2006/07/30 00:51:05 jjanke Exp $
+ * POS Terminal definition
+ *
+ * @author Jorg Janke
+ * @version $Id: MPOS.java,v 1.3 2006/07/30 00:51:05 jjanke Exp $
  */
 public class MPOS extends X_C_POS
 {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -1568195843844720536L;
 
-	/**
-	 * 	Get POS from Cache
-	 *	@param ctx context
-	 *	@param C_POS_ID id
-	 *	@return MPOS
-	 */
-	public static MPOS get (Properties ctx, int C_POS_ID)
+	public MPOS(Properties ctx, int C_POS_ID, String trxName)
 	{
-		Integer key = new Integer (C_POS_ID);
-		MPOS retValue = s_cache.get (key);
-		if (retValue != null)
-			return retValue;
-		retValue = new MPOS (ctx, C_POS_ID, null);
-		if (retValue.get_ID () != 0)
-			s_cache.put (key, retValue);
-		return retValue;
-	} //	get
-	
-	/**	Cache						*/
-	private static CCache<Integer,MPOS> s_cache = new CCache<>("C_POS", 20);
-
-	/**
-	 * 	Standard Constructor
-	 *	@param ctx context
-	 *	@param C_POS_ID id
-	 *	@param trxName transaction
-	 */
-	public MPOS (Properties ctx, int C_POS_ID, String trxName)
-	{
-		super (ctx, C_POS_ID, trxName);
-		if (C_POS_ID == 0)
+		super(ctx, C_POS_ID, trxName);
+		if (is_new())
 		{
-		//	setName (null);
-		//	setSalesRep_ID (0);
-		//	setC_CashBook_ID (0);
-		//	setM_PriceList_ID (0);
-			setIsModifyPrice (false);	// N
-		//	setM_Warehouse_ID (0);
-		}	
-	}	//	MPOS
+			//	setName (null);
+			//	setSalesRep_ID (0);
+			//	setC_CashBook_ID (0);
+			//	setM_PriceList_ID (0);
+			setIsModifyPrice(false);    // N
+			//	setM_Warehouse_ID (0);
+		}
+	}
 
-	/**
-	 * 	Load Constructor
-	 *	@param ctx context
-	 *	@param rs result set
-	 *	@param trxName transaction
-	 */
-	public MPOS (Properties ctx, ResultSet rs, String trxName)
+	@SuppressWarnings("unused")
+	public MPOS(Properties ctx, ResultSet rs, String trxName)
 	{
 		super(ctx, rs, trxName);
-	}	//	MPOS
-	
-	
-	/**
-	 * 	Before Save
-	 *	@param newRecord new
-	 *	@return true
-	 */
+	}
+
 	@Override
-	protected boolean beforeSave (boolean newRecord)
+	protected boolean beforeSave(boolean newRecord)
 	{
 		//	Org Consistency
-		if (newRecord 
-			|| is_ValueChanged("C_CashBook_ID") || is_ValueChanged("M_Warehouse_ID"))
+		if (newRecord
+				|| is_ValueChanged(COLUMNNAME_C_CashBook_ID) || is_ValueChanged(COLUMNNAME_M_Warehouse_ID))
 		{
 			MCashBook cb = MCashBook.get(getCtx(), getC_CashBook_ID(), get_TrxName());
 			if (cb.getAD_Org_ID() != getAD_Org_ID())
 			{
 				throw new AdempiereException("@AD_Org_ID@: @C_CashBook_ID@");
 			}
-			
+
 			final I_M_Warehouse wh = Services.get(IWarehouseDAO.class).getById(WarehouseId.ofRepoId(getM_Warehouse_ID()));
 			if (wh.getAD_Org_ID() != getAD_Org_ID())
 			{
@@ -118,11 +77,13 @@ public class MPOS extends X_C_POS
 			}
 		}
 		return true;
-	}	//	beforeSave
+	}    //	beforeSave
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return super.getName();
-		}
-	
-}	//	MPOS
+	}
+
+}
+
