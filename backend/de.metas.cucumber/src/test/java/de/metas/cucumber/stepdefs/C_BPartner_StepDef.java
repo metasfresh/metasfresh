a46e54d5fcbf56ee1835c0704fc34a2cc54cb758
riad.mennour <riad.mennour@gmail.com>
2024-09-19 15:55:10 +0100

    master merge

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/C_BPartner_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/C_BPartner_StepDef.java
@@ -24,6 +24,7 @@ package de.metas.cucumber.stepdefs;
 
 import de.metas.aggregation.model.I_C_Aggregation;
 import de.metas.bpartner.BPGroupId;
+import de.metas.bpartner.BPartnerId;
 import de.metas.bpartner.BPartnerLocationId;
 import de.metas.bpartner.service.IBPartnerDAO;
 import de.metas.common.rest_api.common.JsonMetasfreshId;
@@ -35,6 +36,7 @@ import de.metas.cucumber.stepdefs.aggregation.C_Aggregation_StepDefData;
 import de.metas.cucumber.stepdefs.discountschema.M_DiscountSchema_StepDefData;
 import de.metas.cucumber.stepdefs.dunning.C_Dunning_StepDefData;
 import de.metas.cucumber.stepdefs.org.AD_Org_StepDefData;
+import de.metas.cucumber.stepdefs.paymentterm.C_PaymentTerm_StepDefData;
 import de.metas.cucumber.stepdefs.pricing.M_PricingSystem_StepDefData;
 import de.metas.externalreference.ExternalIdentifier;
 import de.metas.externalreference.bpartner.BPartnerExternalReferenceType;
@@ -81,7 +83,7 @@ import static de.metas.cucumber.stepdefs.StepDefConstants.ORG_ID;
 import static de.metas.cucumber.stepdefs.StepDefConstants.TABLECOLUMN_IDENTIFIER;
 import static de.metas.edi.model.I_C_BPartner.COLUMNNAME_IsEdiInvoicRecipient;
 import static de.metas.invoicecandidate.model.I_C_BPartner.COLUMNNAME_SO_Invoice_Aggregation_ID;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.compiere.model.I_C_BPartner.COLUMNNAME_AD_Language;
 import static org.compiere.model.I_C_BPartner.COLUMNNAME_C_BP_Group_ID;
 import static org.compiere.model.I_C_BPartner.COLUMNNAME_C_BPartner_ID;
@@ -108,14 +110,15 @@ public class C_BPartner_StepDef
 {
 	public static final int BP_GROUP_ID = BPGroupId.ofRepoId(1000000).getRepoId();
 
-	private final C_BPartner_StepDefData bPartnerTable;
-	private final C_BPartner_Location_StepDefData bPartnerLocationTable;
-	private final M_PricingSystem_StepDefData pricingSystemTable;
-	private final M_Product_StepDefData productTable;
-	private final M_DiscountSchema_StepDefData discountSchemaTable;
-	private final C_Dunning_StepDefData dunningTable;
-	private final AD_Org_StepDefData orgTable;
-	private final C_Aggregation_StepDefData aggregationTable;
+	@NonNull private final C_BPartner_StepDefData bPartnerTable;
+	@NonNull private final C_BPartner_Location_StepDefData bPartnerLocationTable;
+	@NonNull private final M_PricingSystem_StepDefData pricingSystemTable;
+	@NonNull private final M_Product_StepDefData productTable;
+	@NonNull private final M_DiscountSchema_StepDefData discountSchemaTable;
+	@NonNull private final C_Dunning_StepDefData dunningTable;
+	@NonNull private final C_PaymentTerm_StepDefData paymentTermTable;
+	@NonNull private final AD_Org_StepDefData orgTable;
+	@NonNull private final C_Aggregation_StepDefData aggregationTable;
 	private final IBPartnerDAO bpartnerDAO = Services.get(IBPartnerDAO.class);
 	private final IProductDAO productDAO = Services.get(IProductDAO.class);
 	private final IQueryBL queryBL = Services.get(IQueryBL.class);
@@ -170,12 +173,9 @@ public class C_BPartner_StepDef
 	@And("the following c_bpartner is changed")
 	public void change_bpartner(@NonNull final DataTable dataTable)
 	{
-		final List<Map<String, String>> dataRows = dataTable.asMaps();
-
-		for (final Map<String, String> row : dataRows)
-		{
-			changeBPartner(row);
-		}
+		DataTableRows.of(dataTable)
+				.setAdditionalRowIdentifierColumnName(COLUMNNAME_C_BPartner_ID)
+				.forEach(this::changeBPartner);
 	}
 
 	@And("locate bpartner by external identifier")
@@ -346,56 +346,25 @@ public class C_BPartner_StepDef
 				.ifPresent(recordIdentifier -> bPartnerTable.putOrReplace(recordIdentifier, bPartnerRecord));
 	}
 
-	private void changeBPartner(@NonNull final Map<String, String> row)
+	private void changeBPartner(@NonNull final DataTableRow row)
 	{
-		final String bPartnerIdentifier = DataTableUtil.extractStringForColumnName(row, COLUMNNAME_C_BPartner_ID + ".Identifier");
+		final StepDefDataIdentifier bpartnerIdentifier = row.getAsIdentifier();
+		final BPartnerId bpartnerId = bPartnerTable.getIdOptional(bpartnerIdentifier)
+				.orElseGet(() -> bpartnerIdentifier.getAsId(BPartnerId.class));
 
-		final Integer bPartnerId = bPartnerTable.getOptional(bPartnerIdentifier)
-				.map(I_C_BPartner::getC_BPartner_ID)
-				.orElseGet(() -> Integer.parseInt(bPartnerIdentifier));
+		final de.metas.edi.model.I_C_BPartner bPartnerRecord = bpartnerDAO.getById(bpartnerId, de.metas.edi.model.I_C_BPartner.class);
 
-		final de.metas.edi.model.I_C_BPartner bPartnerRecord = InterfaceWrapperHelper.load(bPartnerId, de.metas.edi.model.I_C_BPartner.class);
+		row.getAsOptionalString(I_C_BPartner.COLUMNNAME_Name2).ifPresent(bPartnerRecord::setName2);
+		row.getAsOptionalString(I_C_BPartner.COLUMNNAME_VATaxID).ifPresent(vaTaxId -> bPartnerRecord.setVATaxID(DataTableUtil.nullToken2Null(vaTaxId)));
 
-		final String name2 = DataTableUtil.extractNullableStringForColumnName(row, "OPT." + I_C_BPartner.COLUMNNAME_Name2);
+		bPartnerRecord.setIsEdiDesadvRecipient(row.getAsOptionalBoolean(de.metas.edi.model.I_C_BPartner.COLUMNNAME_IsEdiDesadvRecipient).orElseFalse());
 
-		if (Check.isNotBlank(name2))
-		{
-			bPartnerRecord.setName2(DataTableUtil.nullToken2Null(name2));
-		}
+		row.getAsOptionalString(de.metas.edi.model.I_C_BPartner.COLUMNNAME_EdiDesadvRecipientGLN).ifPresent(ediDesadvRecipientGLN -> bPartnerRecord.setEdiDesadvRecipientGLN(DataTableUtil.nullToken2Null(ediDesadvRecipientGLN)));
+		row.getAsOptionalString(de.metas.edi.model.I_C_BPartner.COLUMNNAME_EdiInvoicRecipientGLN).ifPresent(ediInvoicRecipientGLN -> bPartnerRecord.setEdiInvoicRecipientGLN(DataTableUtil.nullToken2Null(ediInvoicRecipientGLN)));
 
-		final String vaTaxId = DataTableUtil.extractNullableStringForColumnName(row, "OPT." + I_C_BPartner.COLUMNNAME_VATaxID);
+		bPartnerRecord.setIsEdiInvoicRecipient(row.getAsOptionalBoolean(de.metas.edi.model.I_C_BPartner.COLUMNNAME_IsEdiInvoicRecipient).orElseFalse());
 
-		if (Check.isNotBlank(vaTaxId))
-		{
-			bPartnerRecord.setVATaxID(DataTableUtil.nullToken2Null(vaTaxId));
-		}
-
-		final boolean isDesadvRecipient = DataTableUtil.extractBooleanForColumnNameOr(row, "OPT." + de.metas.edi.model.I_C_BPartner.COLUMNNAME_IsEdiDesadvRecipient, false);
-		bPartnerRecord.setIsEdiDesadvRecipient(isDesadvRecipient);
-
-		final String ediDesadvRecipientGLN = DataTableUtil.extractNullableStringForColumnName(row, "OPT." + de.metas.edi.model.I_C_BPartner.COLUMNNAME_EdiDesadvRecipientGLN);
-
-		if (Check.isNotBlank(ediDesadvRecipientGLN))
-		{
-			bPartnerRecord.setEdiDesadvRecipientGLN(DataTableUtil.nullToken2Null(ediDesadvRecipientGLN));
-		}
-
-		final String ediInvoicRecipientGLN = DataTableUtil.extractNullableStringForColumnName(row, "OPT." + de.metas.edi.model.I_C_BPartner.COLUMNNAME_EdiInvoicRecipientGLN);
-
-		if (Check.isNotBlank(ediInvoicRecipientGLN))
-		{
-			bPartnerRecord.setEdiInvoicRecipientGLN(DataTableUtil.nullToken2Null(ediInvoicRecipientGLN));
-		}
-
-		final boolean isInvoicRecipient = DataTableUtil.extractBooleanForColumnNameOr(row, "OPT." + de.metas.edi.model.I_C_BPartner.COLUMNNAME_IsEdiInvoicRecipient, false);
-		bPartnerRecord.setIsEdiInvoicRecipient(isInvoicRecipient);
-
-		final String deliveryRule = DataTableUtil.extractNullableStringForColumnName(row, "OPT." + de.metas.edi.model.I_C_BPartner.COLUMNNAME_DeliveryRule);
-
-		if (Check.isNotBlank(deliveryRule))
-		{
-			bPartnerRecord.setDeliveryRule(DataTableUtil.nullToken2Null(deliveryRule));
-		}
+		row.getAsOptionalString(de.metas.edi.model.I_C_BPartner.COLUMNNAME_DeliveryRule).ifPresent(deliveryRule -> bPartnerRecord.setDeliveryRule(DataTableUtil.nullToken2Null(deliveryRule)));
 
 		InterfaceWrapperHelper.save(bPartnerRecord);
 	}
@@ -512,6 +481,13 @@ public class C_BPartner_StepDef
 
 			final Boolean isManuallyCreated = DataTableUtil.extractBooleanForColumnNameOr(row, "OPT." + I_C_BPartner.COLUMNNAME_IsManuallyCreated, false);
 			softly.assertThat(bPartnerRecord.isManuallyCreated()).as("IsManuallyCreated").isEqualTo(isManuallyCreated);
+
+			final String paymentTermIdentifier = DataTableUtil.extractStringOrNullForColumnName(row, "OPT.C_PaymentTerm_ID" + "." + TABLECOLUMN_IDENTIFIER);
+			if (Check.isNotBlank(paymentTermIdentifier))
+			{
+				final I_C_PaymentTerm paymentTerm = paymentTermTable.get(paymentTermIdentifier);
+				softly.assertThat(paymentTerm.getC_PaymentTerm_ID()).as("C_PaymentTerm_ID").isEqualTo(bPartnerRecord.getC_PaymentTerm_ID());
+			}
 		}
 
 		softly.assertAll();
