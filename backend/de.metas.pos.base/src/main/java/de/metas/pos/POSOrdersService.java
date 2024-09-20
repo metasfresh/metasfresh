3bd1987c52106bc82a7c830af7db409a52bf57b4
Teodor Sarca <teo.sarca@metasfresh.com>
2024-09-20 21:58:55 +0300

    prototyping

--- backend/de.metas.pos.base/src/main/java/de/metas/pos/POSOrdersService.java
+++ backend/de.metas.pos.base/src/main/java/de/metas/pos/POSOrdersService.java
@@ -3,20 +3,8 @@ package de.metas.pos;
 import de.metas.common.util.time.SystemTime;
 import de.metas.currency.CurrencyPrecision;
 import de.metas.currency.CurrencyRepository;
-import de.metas.lang.SOTrx;
-import de.metas.pos.POSOrderLine.POSOrderLineBuilder;
-import de.metas.pos.POSPayment.POSPaymentBuilder;
 import de.metas.pos.remote.RemotePOSOrder;
-import de.metas.pos.remote.RemotePOSOrderLine;
-import de.metas.pos.remote.RemotePOSPayment;
-import de.metas.quantity.Quantity;
-import de.metas.quantity.Quantitys;
 import de.metas.tax.api.ITaxDAO;
-import de.metas.tax.api.Tax;
-import de.metas.tax.api.TaxCategoryId;
-import de.metas.tax.api.TaxId;
-import de.metas.tax.api.TaxNotFoundException;
-import de.metas.tax.api.TaxQuery;
 import de.metas.user.UserId;
 import de.metas.util.Services;
 import lombok.NonNull;
@@ -24,10 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.adempiere.exceptions.AdempiereException;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -72,6 +56,18 @@ public class POSOrdersService
 				});
 	}
 
+	private void updateOrderFromRemote(final POSOrder order, final @NonNull RemotePOSOrder remoteOrder)
+	{
+
+		POSOrderUpdateFromRemoteCommand.builder()
+				.taxDAO(taxDAO)
+				.order(order)
+				.remoteOrder(remoteOrder)
+				.currencyPrecision(currencyRepository.getStdPrecision(order.getCurrencyId()))
+				.build()
+				.execute();
+	}
+
 	private POSOrder newPOSOrder(@NonNull final POSOrderExternalId externalId, @NonNull final UserId userId)
 	{
 		final POSConfig config = configService.getConfig();
@@ -90,142 +86,4 @@ public class POSOrdersService
 				.configId(config.getId())
 				.build();
 	}
-
-	private void updateOrderFromRemote(final POSOrder order, final RemotePOSOrder remoteOrder)
-	{
-		if (!POSOrderExternalId.equals(order.getExternalId(), remoteOrder.getUuid()))
-		{
-			throw new AdempiereException("Expected externalIds to match");
-		}
-
-		//
-		// Update order lines
-		{
-			final ArrayList<String> lineExternalIdsInOrder = new ArrayList<>();
-			for (final RemotePOSOrderLine remoteOrderLine : remoteOrder.getLines())
-			{
-				createOrUpdateOrderLineFromRemote(order, remoteOrderLine);
-				lineExternalIdsInOrder.add(remoteOrderLine.getUuid());
-			}
-
-			order.preserveOnlyLineExternalIdsInOrder(lineExternalIdsInOrder);
-		}
-
-		//
-		// Update payments
-		{
-			final HashSet<String> paymentExternalIdsToKeep = new HashSet<>();
-			final List<RemotePOSPayment> remotePayments = remoteOrder.getPayments();
-			if (!remotePayments.isEmpty())
-			{
-				for (final RemotePOSPayment remotePayment : remotePayments)
-				{
-					createOrUpdatePaymentFromRemote(order, remotePayment);
-					paymentExternalIdsToKeep.add(remotePayment.getUuid());
-				}
-			}
-
-			order.preserveOnlyPaymentExternalIds(paymentExternalIdsToKeep);
-		}
-
-	}
-
-	private void createOrUpdateOrderLineFromRemote(
-			@NonNull final POSOrder order,
-			@NonNull final RemotePOSOrderLine remoteOrderLine)
-	{
-		order.createOrUpdateLine(remoteOrderLine.getUuid(), existingLine -> {
-			//
-			// TaxId
-			final TaxCategoryId taxCategoryId = remoteOrderLine.getTaxCategoryId();
-			final TaxId taxId;
-			Tax tax = null; // lazy loaded
-			if (existingLine != null)
-			{
-				taxId = existingLine.getTaxId();
-			}
-			else
-			{
-				tax = findTax(order, taxCategoryId);
-				taxId = tax.getTaxId();
-			}
-
-			//
-			// Amount, TaxAmt
-			final Quantity qty = extractQty(remoteOrderLine);
-			final BigDecimal price = remoteOrderLine.getPrice();
-			final CurrencyPrecision currencyPrecision = currencyRepository.getStdPrecision(order.getCurrencyId());
-			final BigDecimal amount = currencyPrecision.round(qty.toBigDecimal().multiply(price));
-			final BigDecimal amountPrev = existingLine != null ? existingLine.getAmount() : BigDecimal.ZERO;
-			final BigDecimal taxAmt;
-			if (existingLine == null || amount.compareTo(amountPrev) != 0)
-			{
-				tax = tax != null ? tax : taxDAO.getTaxById(taxId); // load tax if needed
-				taxAmt = tax.calculateTax(amount, order.isTaxIncluded(), currencyPrecision.toInt());
-			}
-			else
-			{
-				taxAmt = existingLine.getTaxAmt();
-			}
-
-			final POSOrderLineBuilder builder = existingLine != null
-					? existingLine.toBuilder()
-					: POSOrderLine.builder();
-
-			return builder.externalId(remoteOrderLine.getUuid())
-					.productId(remoteOrderLine.getProductId())
-					.productName(remoteOrderLine.getProductName())
-					.taxCategoryId(taxCategoryId)
-					.taxId(taxId)
-					.qty(qty)
-					.price(price)
-					.amount(amount)
-					.taxAmt(taxAmt)
-					.build();
-		});
-	}
-
-	private Quantity extractQty(@NonNull final RemotePOSOrderLine remoteOrderLine)
-	{
-		return Quantitys.create(remoteOrderLine.getQty(), remoteOrderLine.getUomId());
-	}
-
-	private Tax findTax(final POSOrder order, final TaxCategoryId taxCategoryId)
-	{
-		final TaxQuery taxQuery = TaxQuery.builder()
-				.fromCountryId(order.getShipFrom().getCountryId())
-				.orgId(order.getShipFrom().getOrgId())
-				.bPartnerLocationId(order.getShipToCustomerAndLocationId())
-				.dateOfInterest(Timestamp.from(order.getDate()))
-				.taxCategoryId(taxCategoryId)
-				.soTrx(SOTrx.SALES)
-				.build();
-
-		final Tax tax = taxDAO.getByIfPresent(taxQuery).orElseThrow(() -> TaxNotFoundException.ofQuery(taxQuery));
-		if (tax.isDocumentLevel())
-		{
-			throw new AdempiereException("POS tax shall be all line level")
-					.setParameter("tax", tax);
-		}
-		return tax;
-	}
-
-	//
-	//
-	//
-
-	private void createOrUpdatePaymentFromRemote(final POSOrder order, final RemotePOSPayment remotePayment)
-	{
-		order.createOrUpdatePayment(remotePayment.getUuid(), existingPayment -> {
-			final POSPaymentBuilder builder = existingPayment != null
-					? existingPayment.toBuilder()
-					: POSPayment.builder();
-
-			return builder.externalId(remotePayment.getUuid())
-					.paymentMethod(remotePayment.getPaymentMethod())
-					.amount(remotePayment.getAmount())
-					.build();
-		});
-	}
-
 }
