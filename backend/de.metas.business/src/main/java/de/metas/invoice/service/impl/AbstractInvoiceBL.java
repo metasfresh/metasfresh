a135991df3c9302c094cc07486372ab54ce3992a
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-09-24 14:52:57 +0300

    Get order form the invoice (#18947)

--- backend/de.metas.business/src/main/java/de/metas/invoice/service/impl/AbstractInvoiceBL.java
+++ backend/de.metas.business/src/main/java/de/metas/invoice/service/impl/AbstractInvoiceBL.java
@@ -877,7 +877,7 @@ public abstract class AbstractInvoiceBL implements IInvoiceBL
 
 		if (copyDocumentNote.isCopyDocumentNoteFromOrder() && invoice.getC_Order_ID() > 0)
 		{
-        	final String descriptionBottom = orderBL.getDescripttionBottomById(OrderId.ofRepoId(invoice.getC_Invoice_ID()));
+        	final String descriptionBottom = orderBL.getDescripttionBottomById(OrderId.ofRepoId(invoice.getC_Order_ID()));
 			invoice.setDescriptionBottom(descriptionBottom);
 		}
 		else
