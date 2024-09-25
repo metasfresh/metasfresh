f2773a9f0f0b1bf391ff96268fb093d20718bda3
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-09-25 16:14:14 +0300

    Add french trl

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5734770_addPaymentMethodFrenchTrl.sql
@@ -0,0 +1,8 @@
+UPDATE ad_ref_list_trl SET name = 'Espèces' , istranslated='Y' WHERE ad_ref_list_id = 332 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Carte de crédit' , istranslated='Y'  WHERE ad_ref_list_id = 333 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Transfert' , istranslated='Y'  WHERE ad_ref_list_id = 334 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Chèque' , istranslated='Y'  WHERE ad_ref_list_id = 335 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'À crédit' , istranslated='Y'  WHERE ad_ref_list_id = 337 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Prélèvement automatique' , istranslated='Y'  WHERE ad_ref_list_id = 652 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Carte de crédit' , istranslated='Y'  WHERE ad_ref_list_id = 543126 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Transfert instantané' , istranslated='Y'  WHERE ad_ref_list_id = 543127 AND ad_language = 'fr_CH';
