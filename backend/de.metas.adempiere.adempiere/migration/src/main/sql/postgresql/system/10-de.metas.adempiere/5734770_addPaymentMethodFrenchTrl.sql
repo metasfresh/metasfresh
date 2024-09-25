cd0b3b0520fb1e5056508a9024c27b5ebf731a24
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-09-25 16:49:31 +0300

    Translate freight cost ruleas

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5734770_addPaymentMethodFrenchTrl.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5734770_addPaymentMethodFrenchTrl.sql
@@ -6,3 +6,9 @@ UPDATE ad_ref_list_trl SET name = 'À crédit' , istranslated='Y'  WHERE ad_ref_
 UPDATE ad_ref_list_trl SET name = 'Prélèvement automatique' , istranslated='Y'  WHERE ad_ref_list_id = 652 AND ad_language = 'fr_CH';
 UPDATE ad_ref_list_trl SET name = 'Carte de crédit' , istranslated='Y'  WHERE ad_ref_list_id = 543126 AND ad_language = 'fr_CH';
 UPDATE ad_ref_list_trl SET name = 'Transfert instantané' , istranslated='Y'  WHERE ad_ref_list_id = 543127 AND ad_language = 'fr_CH';
+
+
+UPDATE ad_ref_list_trl SET name = 'Livraison gratuite', istranslated='Y' WHERE ad_ref_list_id = 225 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Prix fixe', istranslated='Y'  WHERE ad_ref_list_id = 226 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Calculé' , istranslated='Y' WHERE ad_ref_list_id = 227 AND ad_language = 'fr_CH';
+UPDATE ad_ref_list_trl SET name = 'Frais d''expédition forfaitaires', istranslated='Y' WHERE ad_ref_list_id = 540000 AND ad_language = 'fr_CH';
