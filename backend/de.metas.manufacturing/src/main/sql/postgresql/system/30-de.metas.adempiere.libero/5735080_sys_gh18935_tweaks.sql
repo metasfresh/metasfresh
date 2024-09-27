aee771720f2f923b1dcc966c5e90edac5c24a74a
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2024-09-27 08:47:03 +0200

    customize ad_relationtype target role

--- backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5735080_sys_gh18935_tweaks.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5735080_sys_gh18935_tweaks.sql
@@ -44,3 +44,34 @@ INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Display,AD_Key,AD_Org_ID,AD_Reference_
 INSERT INTO AD_RelationType (AD_Client_ID,AD_Org_ID,AD_Reference_Source_ID,AD_Reference_Target_ID,AD_RelationType_ID,Created,CreatedBy,EntityType,IsActive,IsTableRecordIdTarget,Name,Updated,UpdatedBy) VALUES (0,0,541894,541895,540444,TO_TIMESTAMP('2024-09-26 17:41:12','YYYY-MM-DD HH24:MI:SS'),100,'EE01','Y','N','PP_Order_Candidate_Parent -> PP_Order_Candidate',TO_TIMESTAMP('2024-09-26 17:41:12','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
+-- Reference: AD_RelationType Role
+-- Value: PP_OrderCandidate_Children
+-- ValueName: Vorher zu produzierende Kandidaten
+-- 2024-09-27T06:43:55.806Z
+INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value,ValueName) VALUES (0,0,53331,543727,TO_TIMESTAMP('2024-09-27 08:43:55','YYYY-MM-DD HH24:MI:SS'),100,'EE01','Y','Vorher zu produzierende Kandidaten',TO_TIMESTAMP('2024-09-27 08:43:55','YYYY-MM-DD HH24:MI:SS'),100,'PP_OrderCandidate_Children','Vorher zu produzierende Kandidaten')
+;
+
+-- 2024-09-27T06:43:55.814Z
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Ref_List_ID=543727 AND NOT EXISTS (SELECT 1 FROM AD_Ref_List_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Ref_List_ID=t.AD_Ref_List_ID)
+;
+
+-- Reference Item: AD_RelationType Role -> PP_OrderCandidate_Children_Vorher zu produzierende Kandidaten
+-- 2024-09-27T06:43:59.599Z
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-09-27 08:43:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Ref_List_ID=543727
+;
+
+-- Reference Item: AD_RelationType Role -> PP_OrderCandidate_Children_Vorher zu produzierende Kandidaten
+-- 2024-09-27T06:44:04.978Z
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2024-09-27 08:44:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Ref_List_ID=543727
+;
+
+-- Reference Item: AD_RelationType Role -> PP_OrderCandidate_Children_Vorher zu produzierende Kandidaten
+-- 2024-09-27T06:44:51.057Z
+UPDATE AD_Ref_List_Trl SET IsTranslated='Y', Name='Candidates to be produced beforehand',Updated=TO_TIMESTAMP('2024-09-27 08:44:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Ref_List_ID=543727
+;
+
+-- 2024-09-27T06:45:11.795Z
+UPDATE AD_RelationType SET Role_Target='PP_OrderCandidate_Children',Updated=TO_TIMESTAMP('2024-09-27 08:45:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_RelationType_ID=540444
+;
+
+
