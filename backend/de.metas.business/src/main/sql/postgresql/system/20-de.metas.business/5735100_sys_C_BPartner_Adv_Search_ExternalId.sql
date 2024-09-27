4bad943606c851e4686053f8f743e8ec8a325d87
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-09-27 12:19:55 +0300

    Insert the ExternalId column and let the script fail if it already exists

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5735100_sys_C_BPartner_Adv_Search_ExternalId.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5735100_sys_C_BPartner_Adv_Search_ExternalId.sql
@@ -1,42 +1,21 @@
--- Run mode: SWING_CLIENT
+
+
+
+-- Please, ignore or comment out the content of this script if the column already exists!
 
 
 
 
--- Extra precaution
 
-
-create table backup.C_Bpartner_Adv_Search_Columns_27092024 AS SELECT * FROM AD_COLUMN WHERE ad_table_ID = 541761;
-
-
--- DROP  clumn in case it exists on a private repo
-
-
-DELETE FROM AD_Column WHERE ad_table_id = 541761 AND Columnname ilike 'ExternalId';
-
--- 2024-09-27T07:58:24.661Z
-/* DDL */ SELECT public.db_alter_table('C_BPartner_Adv_Search','ALTER TABLE public.C_BPartner_Adv_Search DROP COLUMN IF EXISTS ExternalId')
+INSERT INTO public.ad_column (ad_column_id, ad_client_id, ad_org_id, isactive, created, updated, createdby, updatedby, name, description, help, version, entitytype, columnname, ad_table_id, ad_reference_id, ad_reference_value_id, ad_val_rule_id, fieldlength, defaultvalue, iskey, isparent, ismandatory, isupdateable, readonlylogic, isidentifier, seqno, istranslated, isencrypted, callout, vformat,
+                              valuemin, valuemax, isselectioncolumn, ad_element_id, ad_process_id, issyncdatabase, isalwaysupdateable, columnsql, mandatorylogic, infofactoryclass, isautocomplete, isallowlogging, formatpattern, isadvancedtext, islazyloading, iscalculated, isgenericzoomorigin, isgenericzoomkeycolumn, isusedocsequence, isstaleable, ddl_noforeignkey, isdimension,
+                              isdlmpartitionboundary, cacheinvalidateparent, selectioncolumnseqno, israngefilter, isshowfilterincrementbuttons, filterdefaultvalue, isforceincludeingeneratedmodel, technicalnote, personaldatacategory, allowzoomto, isautoapplyvalidationrule, isfacetfilter, maxfacetstofetch, facetfilterseqno, isshowfilterinline, filteroperator, isexcludefromzoomtargets,
+                              isrestapicustomcolumn, filter_val_rule_id, ad_sequence_id)
+VALUES (587016, 0, 0, 'Y', '2023-06-29 16:38:12.626000 +02:00', '2021-06-23 14:13:33.000000 +02:00', 100, 100, 'Externe ID', NULL, NULL, 0, 'D', 'ExternalId', 541761, 10, NULL, NULL, 255, '', 'N', 'N', 'N', 'Y', NULL, 'N', 0, 'N', 'N', NULL, NULL, NULL, NULL, 'N', 543939, NULL, 'N', 'N', NULL, NULL, NULL, 'N', 'Y', NULL, 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N',
+        'Y', 0, 'N', 'N', NULL, 'N', NULL, NULL, NULL, 'N', 'N', 0, 0, 'N', NULL, 'Y', 'N', NULL, NULL)
 ;
 
 
 
-
--- insert column again
-
--- Column: C_BPartner_Adv_Search.ExternalId
--- 2024-09-27T07:54:18.227Z
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,DDL_NoForeignKey,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRestAPICustomColumn,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,589170,543939,0,10,541761,'ExternalId',TO_TIMESTAMP('2024-09-27 10:54:17.958','YYYY-MM-DD HH24:MI:SS.US'),100,'N','D',0,255,'Y','N','Y','N','N','N','Y','N','N','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N',0,'Externe ID',0,0,TO_TIMESTAMP('2024-09-27 10:54:17.958','YYYY-MM-DD HH24:MI:SS.US'),100,0)
+SELECT public.db_alter_table('C_BPartner_Adv_Search', 'ALTER TABLE public.C_BPartner_Adv_Search ADD COLUMN ExternalId VARCHAR(255)')
 ;
-
--- 2024-09-27T07:54:18.233Z
-INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Column_ID=589170 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
-;
-
--- 2024-09-27T07:54:18.272Z
-/* DDL */  select update_Column_Translation_From_AD_Element(543939)
-;
-
--- 2024-09-27T07:58:24.661Z
-/* DDL */ SELECT public.db_alter_table('C_BPartner_Adv_Search','ALTER TABLE public.C_BPartner_Adv_Search ADD COLUMN ExternalId VARCHAR(255)')
-;
-
