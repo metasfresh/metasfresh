d080569d4129669ce867f25d60dfbd0c91ea5400
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-09-27 16:20:17 +0300

    AD_User CC German translation

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5734590_sys_AD_User_CC.sql
@@ -0,0 +1,84 @@
+-- Element: CC_User_ID
+-- 2024-09-27T09:36:42.047Z
+UPDATE AD_Element_Trl SET Description='Selet a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.', Help='Selet a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.',Updated=TO_TIMESTAMP('2024-09-27 09:36:42.047000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='en_US'
+;
+
+-- 2024-09-27T09:36:42.075Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'en_US') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T09:39:31.490Z
+UPDATE AD_Element_Trl SET Description='Select a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.', Help='Select a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.',Updated=TO_TIMESTAMP('2024-09-27 09:39:31.490000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='en_US'
+;
+
+-- 2024-09-27T09:39:31.493Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'en_US') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T09:40:21.402Z
+UPDATE AD_Element_Trl SET Description='Select a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.', Help='Select a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.',Updated=TO_TIMESTAMP('2024-09-27 09:40:21.402000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='de_DE'
+;
+
+-- 2024-09-27T09:40:21.404Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583286,'de_DE') 
+;
+
+-- 2024-09-27T09:40:21.405Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'de_DE') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T09:40:47.189Z
+UPDATE AD_Element_Trl SET Description='Select a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.', Help='Select a user that will receive emails as CC  if the current user receives a mail. This is only applied to emails and not notifications.',Updated=TO_TIMESTAMP('2024-09-27 09:40:47.189000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='de_CH'
+;
+
+-- 2024-09-27T09:40:47.191Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'de_CH') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T13:18:27.263Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Mail-CC an Nutzer', PrintName='Mail-CC an Nutzer',Updated=TO_TIMESTAMP('2024-09-27 13:18:27.262000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='de_DE'
+;
+
+-- 2024-09-27T13:18:27.267Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583286,'de_DE') 
+;
+
+-- 2024-09-27T13:18:27.272Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'de_DE') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T13:18:39.672Z
+UPDATE AD_Element_Trl SET Name='Mail-CC an Nutzer', PrintName='Mail-CC an Nutzer',Updated=TO_TIMESTAMP('2024-09-27 13:18:39.672000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='de_CH'
+;
+
+-- 2024-09-27T13:18:39.675Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'de_CH') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T13:18:54.701Z
+UPDATE AD_Element_Trl SET Description='Wählen Sie einen Benutzer aus, der E-Mails als CC erhalten soll, wenn der aktuelle Benutzer eine E-Mail erhält. Dies wird nur auf E-Mails und nicht auf Benachrichtigungen angewendet.`', Help='Wählen Sie einen Benutzer aus, der E-Mails als CC erhalten soll, wenn der aktuelle Benutzer eine E-Mail erhält. Dies wird nur auf E-Mails und nicht auf Benachrichtigungen angewendet.`',Updated=TO_TIMESTAMP('2024-09-27 13:18:54.699000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='de_CH'
+;
+
+-- 2024-09-27T13:18:54.712Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'de_CH') 
+;
+
+-- Element: CC_User_ID
+-- 2024-09-27T13:19:02.412Z
+UPDATE AD_Element_Trl SET Description='Wählen Sie einen Benutzer aus, der E-Mails als CC erhalten soll, wenn der aktuelle Benutzer eine E-Mail erhält. Dies wird nur auf E-Mails und nicht auf Benachrichtigungen angewendet.`', Help='Wählen Sie einen Benutzer aus, der E-Mails als CC erhalten soll, wenn der aktuelle Benutzer eine E-Mail erhält. Dies wird nur auf E-Mails und nicht auf Benachrichtigungen angewendet.`',Updated=TO_TIMESTAMP('2024-09-27 13:19:02.412000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583286 AND AD_Language='de_DE'
+;
+
+-- 2024-09-27T13:19:02.415Z
+/* DDL */  select update_ad_element_on_ad_element_trl_update(583286,'de_DE') 
+;
+
+-- 2024-09-27T13:19:02.417Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583286,'de_DE') 
+;
+
