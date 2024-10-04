56cd9aa1d18dca33f8068916b39e1ccabea10677
Teodor Sarca <teo.sarca@metasfresh.com>
2024-10-04 12:25:15 +0300

    fix after merge - in jackson-databind-2.13.4 key deserializers must be String deserializer (not Object)

--- backend/de.metas.business/src/main/java/de/metas/inout/ShipmentScheduleId.java
+++ backend/de.metas.business/src/main/java/de/metas/inout/ShipmentScheduleId.java
@@ -52,11 +52,11 @@ public class ShipmentScheduleId implements RepoIdAware
 		return repoId > 0 ? ofRepoId(repoId) : null;
 	}
 
-	// NOTE: we need this object deserializer in order to use ShipmentScheduleId as a map key in a map that needs to deserialized from json
+	// NOTE: we need this String deserializer in order to use ShipmentScheduleId as a map key in a map that needs to deserialized from json
 	@JsonCreator
-	public static ShipmentScheduleId ofObject(@NonNull final Object repoIdObj)
+	public static ShipmentScheduleId ofString(@NonNull final String repoIdStr)
 	{
-		return RepoIdAwares.ofObject(repoIdObj, ShipmentScheduleId.class, ShipmentScheduleId::ofRepoId);
+		return RepoIdAwares.ofObject(repoIdStr, ShipmentScheduleId.class, ShipmentScheduleId::ofRepoId);
 	}
 
 	public static ImmutableSet<Integer> toIntSet(@NonNull final Collection<ShipmentScheduleId> ids)
