904e5b4435e6bb25f548e77f2793d39528b13329
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-10-04 13:52:41 +0300

    Clean imports

--- backend/de.metas.document.archive/de.metas.document.archive.base/src/main/java/de/metas/document/archive/interceptor/DocOutboundProducerValidator.java
+++ backend/de.metas.document.archive/de.metas.document.archive.base/src/main/java/de/metas/document/archive/interceptor/DocOutboundProducerValidator.java
@@ -1,13 +1,5 @@
 package de.metas.document.archive.interceptor;
 
-import org.adempiere.service.IClientDAO;
-import org.compiere.model.I_AD_Client;
-import org.compiere.model.I_C_Doc_Outbound_Config;
-import org.compiere.model.MClient;
-import org.compiere.model.ModelValidationEngine;
-import org.compiere.model.ModelValidator;
-import org.compiere.model.PO;
-
 import de.metas.document.archive.api.IDocOutboundProducerService;
 import de.metas.document.archive.async.spi.impl.DocOutboundWorkpackageProcessor;
 import de.metas.document.archive.spi.impl.AbstractDocOutboundProducer;
@@ -18,6 +10,7 @@ import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.service.IClientDAO;
 import org.compiere.model.I_AD_Client;
+import org.compiere.model.I_C_Doc_Outbound_Config;
 import org.compiere.model.MClient;
 import org.compiere.model.ModelValidationEngine;
 import org.compiere.model.ModelValidator;
