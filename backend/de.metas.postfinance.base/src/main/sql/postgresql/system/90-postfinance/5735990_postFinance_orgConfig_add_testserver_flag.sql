ef2a11657d68bd082b8b0d0e95b94420f3b34211
Max Rieck <max.rieck@metasfresh.com>
2024-10-07 18:36:47 +0200

    PostFinance tables and columns update entities

--- backend/de.metas.postfinance.base/src/main/sql/postgresql/system/90-postfinance/5735990_postFinance_orgConfig_add_testserver_flag.sql
+++ backend/de.metas.postfinance.base/src/main/sql/postgresql/system/90-postfinance/5735990_postFinance_orgConfig_add_testserver_flag.sql
@@ -41,28 +41,6 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 DELETE FROM AD_Element_Link WHERE AD_Field_ID=731849
 ;
 
-/*
- * #%L
- * de.metas.postfinance.base
- * %%
- * Copyright (C) 2024 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 -- 2024-10-07T10:48:51.581Z
 /* DDL */ select AD_Element_Link_Create_Missing_Field(731849)
 ;
