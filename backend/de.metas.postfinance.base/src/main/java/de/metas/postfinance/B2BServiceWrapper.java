6e6d625cbbb3efdbc049e62509c24da8bdde6706
Max Rieck <max.rieck@metasfresh.com>
2024-10-07 13:45:32 +0200

    PostFinance fix prod connection and refactor

--- backend/de.metas.postfinance.base/src/main/java/de/metas/postfinance/B2BServiceWrapper.java
+++ backend/de.metas.postfinance.base/src/main/java/de/metas/postfinance/B2BServiceWrapper.java
@@ -23,10 +23,9 @@
 package de.metas.postfinance;
 
 import com.google.common.collect.ImmutableList;
-import de.metas.bpartner.postfinance.PostFinanceOrgConfig;
-import de.metas.bpartner.postfinance.PostFinanceOrgConfigRepository;
 import de.metas.organization.OrgId;
 import de.metas.postfinance.customerregistration.util.XMLUtil;
+import de.metas.postfinance.document.export.PostFinanceExportException;
 import de.metas.postfinance.document.export.PostFinanceYbInvoiceResponse;
 import de.metas.postfinance.jaxb.ArrayOfInvoice;
 import de.metas.postfinance.jaxb.ArrayOfProcessedInvoice;
@@ -36,10 +35,15 @@ import de.metas.postfinance.jaxb.B2BService_Service;
 import de.metas.postfinance.jaxb.DownloadFile;
 import de.metas.postfinance.jaxb.ObjectFactory;
 import de.metas.postfinance.jaxb.ProtocolReport;
+import de.metas.postfinance.orgconfig.PostFinanceOrgConfig;
+import de.metas.postfinance.orgconfig.PostFinanceOrgConfigRepository;
+import de.metas.util.collections.CollectionUtils;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -52,6 +56,9 @@ public class B2BServiceWrapper
 	@NonNull
 	private final PostFinanceOrgConfigRepository postFinanceOrgConfigRepository;
 	public static final ObjectFactory B2B_SERVICE_OBJECT_FACTORY = new ObjectFactory();
+	private static final String B2B_SERVICE_WSDL_URL_TEST = "https://ebill-ki.postfinance.ch/B2BService/B2BService.svc?wsdl";
+
+	private static final String B2B_SERVICE_WSDL_URL_PROD = "https://ebill.postfinance.ch/B2BService/B2BService.svc?wsdl";
 
 	@NonNull
 	public List<DownloadFile> getCustomerRegistrationMessageFiles(@NonNull final OrgId orgId)
@@ -60,7 +67,7 @@ public class B2BServiceWrapper
 		final String billerId = postFinanceOrgConfig.getBillerId();
 		final boolean isArchiveData = postFinanceOrgConfig.isArchiveData();
 
-		final B2BService port = new B2BService_Service().getUserNamePassword();
+		final B2BService port = getB2BService(postFinanceOrgConfig.isTestserver()).getUserNamePassword();
 		final ArrayOfProtocolReport arrayOfProtocolReport = port.getRegistrationProtocolList(billerId, isArchiveData);
 
 		return arrayOfProtocolReport.getProtocolReport()
@@ -77,7 +84,15 @@ public class B2BServiceWrapper
 
 	public ArrayOfProcessedInvoice uploadFilesReport(@NonNull final String billerId, @NonNull final List<PostFinanceYbInvoiceResponse> invoices)
 	{
-		final B2BService port = new B2BService_Service().getUserNamePassword();
+		if(invoices.isEmpty())
+		{
+			return new ArrayOfProcessedInvoice();
+		}
+
+		final OrgId orgId = CollectionUtils.extractSingleElement(invoices, PostFinanceYbInvoiceResponse::getOrgId);
+
+		final PostFinanceOrgConfig postFinanceOrgConfig = postFinanceOrgConfigRepository.getByOrgIdOrError(orgId);
+		final B2BService port = getB2BService(postFinanceOrgConfig.isTestserver()).getUserNamePassword();
 		final ArrayOfInvoice arrayOfInvoice = B2B_SERVICE_OBJECT_FACTORY.createArrayOfInvoice();
 		invoices.forEach(invoice -> arrayOfInvoice.getInvoice().add(invoice.getInvoice()));
 
@@ -93,7 +108,7 @@ public class B2BServiceWrapper
 		// note: if true, use already downloaded data. False = never downloaded data only
 		final boolean isArchiveData = postFinanceOrgConfig.isArchiveData();
 
-		final B2BService port = new B2BService_Service().getUserNamePassword();
+		final B2BService port = getB2BService(postFinanceOrgConfig.isTestserver()).getUserNamePassword();
 
 		final ArrayOfProtocolReport arrayOfProtocolReport = port.getProcessProtocolList(billerId, isArchiveData);
 
@@ -107,6 +122,21 @@ public class B2BServiceWrapper
 				.flatMap(List::stream)
 				.filter(downloadFile -> XMLUtil.isXML(downloadFile.getFilename().getValue()))
 				.collect(ImmutableList.toImmutableList());
+
+	}
+
+	private B2BService_Service getB2BService(final boolean isTestServer)
+	{
+		final String wsdlURL = isTestServer ? B2B_SERVICE_WSDL_URL_TEST : B2B_SERVICE_WSDL_URL_PROD;
+
+		try
+		{
+			return new B2BService_Service(new URL(wsdlURL));
+		}
+		catch (final MalformedURLException e)
+		{
+			throw new PostFinanceExportException("MalformedURLException on static variable, this shouldn't happen!", e);
+		}
 
 	}
 }
---
+++ backend/de.metas.postfinance.base/src/main/sql/postgresql/system/90-postfinance/5735990_postFinance_orgConfig_add_testserver_flag.sql
@@ -0,0 +1,99 @@
+-- 2024-10-07T10:47:30.099Z
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,583307,0,'IsTestserver',TO_TIMESTAMP('2024-10-07 12:47:29.941','YYYY-MM-DD HH24:MI:SS.US'),100,'D','Y','Testserver','Testserver',TO_TIMESTAMP('2024-10-07 12:47:29.941','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-10-07T10:47:30.105Z
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=583307 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- Column: PostFinance_Org_Config.IsTestserver
+-- 2024-10-07T10:48:06.581Z
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,DDL_NoForeignKey,DefaultValue,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRestAPICustomColumn,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,589244,583307,0,20,542386,'IsTestserver',TO_TIMESTAMP('2024-10-07 12:48:06.466','YYYY-MM-DD HH24:MI:SS.US'),100,'N','Y','D',0,1,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','Y','N',0,'Testserver',0,0,TO_TIMESTAMP('2024-10-07 12:48:06.466','YYYY-MM-DD HH24:MI:SS.US'),100,0)
+;
+
+-- 2024-10-07T10:48:06.584Z
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Column_ID=589244 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 2024-10-07T10:48:06.609Z
+/* DDL */  select update_Column_Translation_From_AD_Element(583307)
+;
+
+-- 2024-10-07T10:48:16.777Z
+/* DDL */ SELECT public.db_alter_table('PostFinance_Org_Config','ALTER TABLE public.PostFinance_Org_Config ADD COLUMN IsTestserver CHAR(1) DEFAULT ''Y'' CHECK (IsTestserver IN (''Y'',''N'')) NOT NULL')
+;
+
+-- Field: Organisation(110,D) -> PostFinance(547355,D) -> Testserver
+-- Column: PostFinance_Org_Config.IsTestserver
+-- 2024-10-07T10:48:51.570Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,589244,731849,0,547355,TO_TIMESTAMP('2024-10-07 12:48:51.453','YYYY-MM-DD HH24:MI:SS.US'),100,1,'D','Y','N','N','N','N','N','N','N','Testserver',TO_TIMESTAMP('2024-10-07 12:48:51.453','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- 2024-10-07T10:48:51.572Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=731849 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2024-10-07T10:48:51.574Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(583307)
+;
+
+-- 2024-10-07T10:48:51.580Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=731849
+;
+
+/*
+ * #%L
+ * de.metas.postfinance.base
+ * %%
+ * Copyright (C) 2024 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+-- 2024-10-07T10:48:51.581Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(731849)
+;
+
+-- UI Element: Organisation(110,D) -> PostFinance(547355,D) -> main -> 10 -> default.Testserver
+-- Column: PostFinance_Org_Config.IsTestserver
+-- 2024-10-07T10:49:47.136Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,731849,0,547355,551424,626151,'F',TO_TIMESTAMP('2024-10-07 12:49:47.023','YYYY-MM-DD HH24:MI:SS.US'),100,'Y','N','N','Y','N','N','N',0,'Testserver',15,0,0,TO_TIMESTAMP('2024-10-07 12:49:47.023','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- UI Element: Organisation(110,D) -> PostFinance(547355,D) -> main -> 10 -> default.Testserver
+-- Column: PostFinance_Org_Config.IsTestserver
+-- 2024-10-07T10:50:00.210Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2024-10-07 12:50:00.21','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=626151
+;
+
+-- UI Element: Organisation(110,D) -> PostFinance(547355,D) -> main -> 10 -> default.Verwenden Sie den Papierrechnungsservice
+-- Column: PostFinance_Org_Config.IsUsePaperBill
+-- 2024-10-07T10:50:00.217Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2024-10-07 12:50:00.217','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=622118
+;
+
+-- UI Element: Organisation(110,D) -> PostFinance(547355,D) -> main -> 10 -> default.Daten archivieren
+-- Column: PostFinance_Org_Config.IsArchiveData
+-- 2024-10-07T10:50:00.221Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2024-10-07 12:50:00.221','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=623014
+;
+
+-- UI Element: Organisation(110,D) -> PostFinance(547355,D) -> main -> 10 -> default.Aktiv
+-- Column: PostFinance_Org_Config.IsActive
+-- 2024-10-07T10:50:00.227Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2024-10-07 12:50:00.227','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=622121
+;
+
