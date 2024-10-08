00d53219f10b304eb3296eef0d0166fd92203782
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2024-10-08 15:15:47 +0200

    DESADV - Support different Products on one LU (#19042)
    - DESADV - allow also TUs to be top-level HUs (#18775)
      Also some minor code improvements, mainly in ExportHelper
      (cherry picked from commit 3f908369de4551c925e24a8495b24eefcc9b83a2)
    - adapt column/fieldnames and added line field to pack-item
    - extract CreateEDIDesadvPackItemRequest to be usable stand-alone
    - start implementing to not always create a new pack for each new item
    - also:
      - change shipment creating stepdef to reflect what is does
      - add javadoc to M_HU_StepDef#transformCUtoNewTUs
    - play with jetbrains Contract annotation to avoid pointless warnings (#17049)
       E.g. avoid a warning saying that a parameter might be null although Check.isNotBlank returned false for that parameter
        (cherry picked from commit d2ef0e60dc50e7b43255dd8871d771b4ad6c02ea)
        solved Conflicts:
    	backend/de.metas.adempiere.adempiere/base/src/main/java-legacy/org/adempiere/exceptions/AdempiereException.java
    	misc/de-metas-common/de-metas-common-util/pom.xml
    - SSCC18AttributeValueGenerator: don't use the HU's ID, see comment
    - relax timeout from 60s to 120s
    - port another flaky-fix from master

--- backend/de.metas.util/src/main/java/de/metas/util/collections/CollectionUtils.java
+++ backend/de.metas.util/src/main/java/de/metas/util/collections/CollectionUtils.java
@@ -9,6 +9,7 @@ import com.google.common.collect.SetMultimap;
 import de.metas.util.Check;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -204,6 +205,7 @@ public final class CollectionUtils
 	 * @param defaultValue value to be returned in case there are more than one element or no element
 	 * @see de.metas.util.reducers.Reducers#singleValue()
 	 */
+	@Contract("_, !null -> !null")
 	@Nullable
 	public static <T> T singleElementOrDefault(final Collection<T> collection, @Nullable final T defaultValue)
 	{
@@ -238,6 +240,7 @@ public final class CollectionUtils
 	/**
 	 * @see de.metas.util.reducers.Reducers#singleValue()
 	 */
+	@Contract("_, _, !null -> !null")
 	@Nullable
 	public static <T, R> R extractSingleElementOrDefault(
 			@NonNull final Collection<T> collection,
