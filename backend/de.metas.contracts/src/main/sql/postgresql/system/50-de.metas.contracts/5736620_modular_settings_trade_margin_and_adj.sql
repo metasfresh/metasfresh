c1859de12cd351edc2f91c6779df2ac089ca10e4
Max Rieck <max.rieck@metasfresh.com>
2024-10-14 16:24:32 +0200

    add shipped qty sales computing methods

--- backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5736620_modular_settings_trade_margin_and_adj.sql
+++ backend/de.metas.contracts/src/main/sql/postgresql/system/50-de.metas.contracts/5736620_modular_settings_trade_margin_and_adj.sql
@@ -136,3 +136,7 @@ UPDATE AD_Field SET DisplayLogic='@IsSOTrx/''''@=''N''', IsDisplayed='Y',Updated
 UPDATE AD_Column SET FilterOperator='E', IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2024-10-14 13:39:37.81','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=587164
 ;
 
+-- Column: ModCntr_Module.ModCntr_Type_ID
+-- 2024-10-14T13:54:03.563Z
+UPDATE AD_Column SET IsIdentifier='Y', SeqNo=4,Updated=TO_TIMESTAMP('2024-10-14 15:54:03.563','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=586809
+;
