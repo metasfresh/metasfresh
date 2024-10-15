6f5acbd09626242173cfe9bd42568647e84fc6a7
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-10-15 17:51:45 +0300

    cleanup after merge

--- misc/de-metas-common/de-metas-common-externalsystem/src/main/java/de/metas/common/externalsystem/ExternalSystemConstants.java
+++ misc/de-metas-common/de-metas-common-externalsystem/src/main/java/de/metas/common/externalsystem/ExternalSystemConstants.java
@@ -107,17 +107,6 @@ public class ExternalSystemConstants
 	public static final String PARAM_TARGET_DIRECTORY = "TargetDirectory";
 	public static final String PARAM_PRINTING_QUEUE_ID = "PrintingQueueId";
 
-	public static final String PARAM_JSON_AVAILABLE_FOR_SALES = "JsonAvailableForSales";
-
-	public static final int DEFAULT_SW6_ORDER_PAGE_SIZE = 100;
-
-	public static final String API_REQUEST_AUDIT_TABLE_NAME = "API_Request_Audit";
-
-	public static final String PARAM_JSON_AVAILABLE_STOCK = "JsonAvailableStock";
-
-	public static final String PARAM_FEEDBACK_RESOURCE_URL = "FeedbackResourceURL";
-	public static final String PARAM_FEEDBACK_RESOURCE_AUTH_TOKEN = "FeedbackResourceAuthToken";
-
 	public static final String PARAM_LOCAL_FILE_ROOT_LOCATION = "LocalFileRootLocation";
 	public static final String PARAM_LOCAL_FILE_PROCESSED_DIRECTORY = "LocalFileProcessedDirectory";
 	public static final String PARAM_LOCAL_FILE_ERRORED_DIRECTORY = "LocalFileErroredDirectory";
