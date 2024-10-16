1287792fbc5eb563fc9a46f6dda932ed46be4a08
Teodor Sarca <teo.sarca@metasfresh.com>
2024-10-16 13:38:02 +0300

    GetCatchWeightModal: make sure weight is valid

--- misc/services/mobile-webui/mobile-webui-frontend/src/apps/pos/containers/order_panel/GetCatchWeightModal.jsx
+++ misc/services/mobile-webui/mobile-webui-frontend/src/apps/pos/containers/order_panel/GetCatchWeightModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import useEscapeKey from '../../../../hooks/useEscapeKey';
 import { trl } from '../../../../utils/translations';
@@ -6,13 +6,23 @@ import { trl } from '../../../../utils/translations';
 const _ = (key, args = {}) => trl(`pos.currentOrder.getWeightModal.${key}`, args);
 
 const GetCatchWeightModal = ({ catchWeightUomSymbol, onOk, onCancel }) => {
+  const [weightStr, setWeightStr] = useState(0);
   const weightRef = useRef();
+
+  const weight = Number(weightStr);
+  const isValid = weight > 0;
 
   useEffect(() => {
     weightRef?.current?.focus();
+    weightRef?.current?.select();
   }, [weightRef.current]);
 
   useEscapeKey(onCancel);
+
+  const handleOKClick = () => {
+    if (!isValid) return;
+    onOk({ catchWeight: weight });
+  };
 
   return (
     <div className="modal is-active">
@@ -24,17 +34,19 @@ const GetCatchWeightModal = ({ catchWeightUomSymbol, onOk, onCancel }) => {
         </header>
         <section className="modal-card-body">
           <div className="control">
-            <input ref={weightRef} className="input is-large" type="text" placeholder={_('weightPlaceholder')} />
+            <input
+              ref={weightRef}
+              className="input is-large"
+              type="text"
+              placeholder={_('weightPlaceholder')}
+              value={weightStr}
+              onChange={(e) => setWeightStr(e.target.value)}
+            />
           </div>
         </section>
         <footer className="modal-card-foot">
           <div className="buttons">
-            <button
-              className="button is-success"
-              onClick={() => {
-                onOk({ catchWeight: Number(weightRef.current.value) });
-              }}
-            >
+            <button className="button is-success" disabled={!isValid} onClick={handleOKClick}>
               {_('actions.ok')}
             </button>
           </div>
