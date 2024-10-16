d0da4d32104f62e0a310d5c9e081539442ec5b68
Teodor Sarca <teo.sarca@metasfresh.com>
2024-10-16 14:09:34 +0300

    trls

--- misc/services/mobile-webui/mobile-webui-frontend/src/apps/pos/utils/payments.js
+++ misc/services/mobile-webui/mobile-webui-frontend/src/apps/pos/utils/payments.js
@@ -1,9 +1,12 @@
 import { getPaymentMethodCaption } from '../constants/paymentMethods';
+import { trl } from '../../../utils/translations';
 
 export const getPaymentSummaryLine = ({ paymentMethod, status, statusDetails }) => {
+  const statusStr = status ? trl(`pos.payment.status.${status}`) : null;
+
   return (
     getPaymentMethodCaption({ paymentMethod }) +
     (statusDetails ? ' ' + statusDetails : '') +
-    (status ? ' (' + status + ')' : '')
+    (statusStr ? ' (' + statusStr + ')' : '')
   );
 };
