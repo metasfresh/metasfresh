338c93115679aa2274de664ee9d65a784ed0378c
dm-ps <dragos.moca@pevesoft.ro>
2024-10-17 19:45:13 +0300

    QA

--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5734550_sys_gh18966_EDI_DESADV_Customizations.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5734550_sys_gh18966_EDI_DESADV_Customizations.sql
@@ -18,7 +18,7 @@ UPDATE AD_Element_Trl SET Name='Shipments not sent', PrintName='Shipments not se
 ;
 
 -- 2024-09-24T15:58:20.118Z
-/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583285,'en_US') 
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583285,'en_US')
 ;
 
 -- Column: EDI_Desadv.IsShipmentsNotSent
@@ -33,7 +33,7 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 ;
 
 -- 2024-09-24T16:08:46.068Z
-/* DDL */  select update_Column_Translation_From_AD_Element(583285) 
+/* DDL */  select update_Column_Translation_From_AD_Element(583285)
 ;
 
 -- Column: EDI_Desadv.IsShipmentsNotSent
@@ -98,94 +98,40 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=150,Updated=TO_TIMESTAMP
 UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=160,Updated=TO_TIMESTAMP('2024-09-24 17:21:57.576000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=540617
 ;
 
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:04:20.712Z
-UPDATE AD_Field SET AD_Reference_ID=17, AD_Reference_Value_ID=540381, FilterOperator='E', IsFilterField='Y', IsShowFilterInline='N', SelectionColumnSeqNo=10.000000000000,Updated=TO_TIMESTAMP('2024-09-25 12:04:20.712000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- 2024-09-25T09:48:02.466Z
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,583287,0,'EDI_ExportStatus_VirtualColumn',TO_TIMESTAMP('2024-09-25 09:48:02.342000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'de.metas.esb.edi','Y','EDI-Sendestatus','EDI-Sendestatus',TO_TIMESTAMP('2024-09-25 09:48:02.342000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:05:20.824Z
-UPDATE AD_Field SET SelectionColumnSeqNo=NULL,Updated=TO_TIMESTAMP('2024-09-25 12:05:20.823000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- 2024-09-25T09:48:02.471Z
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=583287 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
 ;
 
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:05:58.039Z
-UPDATE AD_Field SET IsShowFilterInline='Y',Updated=TO_TIMESTAMP('2024-09-25 12:05:58.038000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- Element: EDI_ExportStatus_VirtualColumn
+-- 2024-09-25T09:48:14.072Z
+UPDATE AD_Element_Trl SET Name='EDI export status', PrintName='EDI export status',Updated=TO_TIMESTAMP('2024-09-25 09:48:14.072000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=583287 AND AD_Language='en_US'
 ;
 
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:07:20.553Z
-UPDATE AD_Field SET IsShowFilterInline='N',Updated=TO_TIMESTAMP('2024-09-25 12:07:20.552000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- 2024-09-25T09:48:14.129Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(583287,'en_US')
 ;
 
-/*
- * #%L
- * de.metas.edi
- * %%
- * Copyright (C) 2024 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:07:52.962Z
-UPDATE AD_Field SET SelectionColumnSeqNo=10.000000000000,Updated=TO_TIMESTAMP('2024-09-25 12:07:52.962000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- Column: M_InOut.EDI_ExportStatus_VirtualColumn
+-- Column SQL (old): null
+-- Column: M_InOut.EDI_ExportStatus_VirtualColumn
+-- 2024-09-25T09:50:05.875Z
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,ColumnSQL,Created,CreatedBy,DDL_NoForeignKey,EntityType,FacetFilterSeqNo,FieldLength,FilterOperator,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRestAPICustomColumn,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version) VALUES (0,589166,583287,0,10,319,'EDI_ExportStatus_VirtualColumn','(select M_InOut.EDI_ExportStatus)',TO_TIMESTAMP('2024-09-25 09:50:05.688000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'N','de.metas.esb.edi',0,1,'E','Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','Y','N','N','N','Y','N','N','N','N','N','N','N',0,'EDI-Sendestatus',0,0,TO_TIMESTAMP('2024-09-25 09:50:05.688000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,0)
 ;
 
--- Column: M_InOut.EDI_ExportStatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:08:51.449Z
-UPDATE AD_Column SET FilterOperator='E', IsSelectionColumn='Y',Updated=TO_TIMESTAMP('2024-09-25 12:08:51.449000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=549871
+-- 2024-09-25T09:50:05.878Z
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Column t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y') AND t.AD_Column_ID=589166 AND NOT EXISTS (SELECT 1 FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
 
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:11:21.481Z
-UPDATE AD_Field SET SelectionColumnSeqNo=60.000000000000,Updated=TO_TIMESTAMP('2024-09-25 12:11:21.480000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- 2024-09-25T09:50:05.882Z
+/* DDL */  select update_Column_Translation_From_AD_Element(583287)
 ;
 
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:11:55.004Z
-UPDATE AD_Field SET SelectionColumnSeqNo=100.000000000000,Updated=TO_TIMESTAMP('2024-09-25 12:11:55.004000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
-;
-
--- Field: Lieferung -> Lieferung -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- Field: Lieferung(169,D) -> Lieferung(257,D) -> EDI-Sendestatus
--- Column: M_InOut.EDI_ExportStatus
--- 2024-09-25T12:12:44.990Z
-UPDATE AD_Field SET SelectionColumnSeqNo=140.000000000000,Updated=TO_TIMESTAMP('2024-09-25 12:12:44.989000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=553214
+-- Column: M_InOut.EDI_ExportStatus_VirtualColumn
+-- Column: M_InOut.EDI_ExportStatus_VirtualColumn
+-- 2024-09-25T09:52:30.301Z
+UPDATE AD_Column SET AD_Reference_ID=17, AD_Reference_Value_ID=540381,Updated=TO_TIMESTAMP('2024-09-25 09:52:30.301000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=589166
 ;
