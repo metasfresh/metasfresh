b455523b2ba33e84a970d2639c7e3e9109d8eb34
dm-ps <dragos.moca@pevesoft.ro>
2024-10-17 14:54:18 +0300

    handle isDeliveryClosed in case fully delivered in multiple steps but no export was done

--- backend/de.metas.edi/src/main/java/de/metas/edi/api/impl/EDIDesadvInOutLineDAO.java
+++ backend/de.metas.edi/src/main/java/de/metas/edi/api/impl/EDIDesadvInOutLineDAO.java
@@ -22,11 +22,19 @@
 
 package de.metas.edi.api.impl;
 
+import de.metas.edi.api.DesadvInOutLine;
+import de.metas.edi.api.EDIDesadvLineId;
 import de.metas.esb.edi.model.I_EDI_DesadvLine_InOutLine;
 import de.metas.inout.InOutLineId;
+import de.metas.organization.OrgId;
+import de.metas.product.ProductId;
+import de.metas.quantity.Quantitys;
+import de.metas.quantity.StockQtyAndUOMQtys;
+import de.metas.uom.UomId;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.exceptions.AdempiereException;
 import org.adempiere.model.InterfaceWrapperHelper;
 import org.springframework.stereotype.Repository;
 
@@ -38,7 +46,52 @@ public class EDIDesadvInOutLineDAO
 	private final IQueryBL queryBL = Services.get(IQueryBL.class);
 
 	@NonNull
-	public Optional<I_EDI_DesadvLine_InOutLine> getByInOutLineId(@NonNull final InOutLineId shipmentLineId)
+	public Optional<DesadvInOutLine> getByInOutLineId(@NonNull final InOutLineId shipmentLineId)
+	{
+		return getRecordByInOutLineIdIfExists(shipmentLineId)
+				.map(EDIDesadvInOutLineDAO::ofRecord);
+	}
+
+	public void save(@NonNull final DesadvInOutLine desadvInOutLine)
+	{
+		final I_EDI_DesadvLine_InOutLine record = getRecordByInOutLineIdIfExists(desadvInOutLine.getShipmentLineId())
+				.orElseGet(() -> InterfaceWrapperHelper.newInstance(I_EDI_DesadvLine_InOutLine.class));
+
+		record.setAD_Org_ID(desadvInOutLine.getOrgId().getRepoId());
+		record.setM_InOutLine_ID(desadvInOutLine.getShipmentLineId().getRepoId());
+		record.setEDI_DesadvLine_ID(desadvInOutLine.getDesadvLineId().getRepoId());
+
+		record.setM_Product_ID(desadvInOutLine.getProductId().getRepoId());
+		record.setQtyDeliveredInStockingUOM(desadvInOutLine.getQtyDeliveredInStockingUOM().toBigDecimal());
+		record.setDesadvLineTotalQtyDelivered(desadvInOutLine.getDesadvTotalQtyDeliveredInStockingUOM().toBigDecimal());
+
+		record.setC_UOM_ID(desadvInOutLine.getQtyDeliveredInUOM().getUomId().getRepoId());
+		record.setQtyDeliveredInUOM(desadvInOutLine.getQtyDeliveredInUOM().toBigDecimal());
+
+		if (desadvInOutLine.getQtyDeliveredInInvoiceUOM() != null)
+		{
+			record.setC_UOM_Invoice_ID(desadvInOutLine.getQtyDeliveredInInvoiceUOM().getUomId().getRepoId());
+			record.setQtyDeliveredInInvoiceUOM(desadvInOutLine.getQtyDeliveredInInvoiceUOM().toBigDecimal());
+		}
+
+		if (desadvInOutLine.getQtyEnteredInBPartnerUOM() != null)
+		{
+			record.setC_UOM_BPartner_ID(desadvInOutLine.getQtyEnteredInBPartnerUOM().getUomId().getRepoId());
+			record.setQtyEnteredInBPartnerUOM(desadvInOutLine.getQtyEnteredInBPartnerUOM().toBigDecimal());
+		}
+
+		InterfaceWrapperHelper.save(record);
+	}
+
+	public void delete(@NonNull final DesadvInOutLine desadvInOutLine)
+	{
+		final I_EDI_DesadvLine_InOutLine desadvLineInOutLine = getRecordByInOutLineIdIfExists(desadvInOutLine.getShipmentLineId())
+				.orElseThrow(() -> new AdempiereException("No EDI_DesadvLine_InOutLine found for M_InOutLine_ID = " + desadvInOutLine.getShipmentLineId().getRepoId()));
+		InterfaceWrapperHelper.delete(desadvLineInOutLine);
+	}
+
+	@NonNull
+	private Optional<I_EDI_DesadvLine_InOutLine> getRecordByInOutLineIdIfExists(@NonNull final InOutLineId shipmentLineId)
 	{
 		return queryBL.createQueryBuilder(I_EDI_DesadvLine_InOutLine.class)
 				.addEqualsFilter(I_EDI_DesadvLine_InOutLine.COLUMNNAME_M_InOutLine_ID, shipmentLineId)
@@ -46,19 +99,31 @@ public class EDIDesadvInOutLineDAO
 				.firstOnlyOptional(I_EDI_DesadvLine_InOutLine.class);
 	}
 
-	public void save(@NonNull final I_EDI_DesadvLine_InOutLine record)
-	{
-		InterfaceWrapperHelper.save(record);
-	}
-
 	@NonNull
-	public I_EDI_DesadvLine_InOutLine newInstance()
+	private static DesadvInOutLine ofRecord(@NonNull final I_EDI_DesadvLine_InOutLine record)
 	{
-		return InterfaceWrapperHelper.newInstance(I_EDI_DesadvLine_InOutLine.class);
-	}
+		final ProductId productId = ProductId.ofRepoId(record.getM_Product_ID());
 
-	public void delete(@NonNull final I_EDI_DesadvLine_InOutLine record)
-	{
-		InterfaceWrapperHelper.delete(record);
+		return DesadvInOutLine.builder()
+				.orgId(OrgId.ofRepoId(record.getAD_Org_ID()))
+				.desadvLineId(EDIDesadvLineId.ofRepoId(record.getEDI_DesadvLine_ID()))
+				.shipmentLineId(InOutLineId.ofRepoId(record.getM_InOutLine_ID()))
+				.productId(ProductId.ofRepoId(record.getM_Product_ID()))
+				.qtyEnteredInBPartnerUOM(Optional.of(record.getC_UOM_BPartner_ID())
+												 .map(UomId::ofRepoIdOrNull)
+												 .map(bpartnerUOMId -> Quantitys.create(record.getQtyEnteredInBPartnerUOM(), bpartnerUOMId))
+												 .orElse(null))
+				.qtyDeliveredInStockingUOM(StockQtyAndUOMQtys
+												   .ofQtyInStockUOM(record.getQtyDeliveredInStockingUOM(), productId)
+												   .getStockQty())
+				.qtyDeliveredInUOM(Quantitys.create(record.getQtyDeliveredInUOM(), UomId.ofRepoId(record.getC_UOM_ID())))
+				.qtyDeliveredInInvoiceUOM(Optional.of(record.getC_UOM_Invoice_ID())
+												  .map(UomId::ofRepoIdOrNull)
+												  .map(invoiceUOMId -> Quantitys.create(record.getQtyDeliveredInInvoiceUOM(), invoiceUOMId))
+												  .orElse(null))
+				.desadvTotalQtyDeliveredInStockingUOM(StockQtyAndUOMQtys
+															  .ofQtyInStockUOM(record.getDesadvLineTotalQtyDelivered(), productId)
+															  .getStockQty())
+				.build();
 	}
 }
