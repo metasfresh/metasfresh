3b36cdd5cd884e936745649ce7e220c109944af1
cp-ps <77785468+cp-ps@users.noreply.github.com>
2024-10-18 14:52:22 +0300

    Don't create variance cost collector if qty issued is within tolerance (#19187)
    * refs: https://github.com/metasfresh/metasfresh/issues/19186

--- backend/de.metas.manufacturing/src/main/java/org/eevolution/api/impl/PPCostCollectorBL.java
+++ backend/de.metas.manufacturing/src/main/java/org/eevolution/api/impl/PPCostCollectorBL.java
@@ -60,6 +60,7 @@ import org.eevolution.api.ComponentIssueCreateRequest;
 import org.eevolution.api.CostCollectorType;
 import org.eevolution.api.IPPCostCollectorBL;
 import org.eevolution.api.IPPCostCollectorDAO;
+import org.eevolution.api.IPPOrderBL;
 import org.eevolution.api.PPCostCollectorId;
 import org.eevolution.api.PPCostCollectorQuantities;
 import org.eevolution.api.PPOrderBOMLineId;
@@ -391,6 +392,16 @@ public class PPCostCollectorBL implements IPPCostCollectorBL
 		if (componentType.isVariant())
 		{
 			return;
+		}
+
+		final OrderBOMLineQuantities orderBOMLineQuantities = ppOrderBOMBL.getQuantities(line);
+		if (orderBOMLineQuantities.getIssuingToleranceSpec() != null)
+		{
+			final Quantity scaleRounding = Services.get(IPPOrderBL.class).getRoundingToScale(PPOrderId.ofRepoId(ppOrder.getPP_Order_ID())).orElse(null);
+			if (orderBOMLineQuantities.isQtyIssuedWithinTolerance(scaleRounding))
+			{
+				return;
+			}
 		}
 
 		final PPOrderBOMLineId ppOrderBOMLineId = PPOrderBOMLineId.ofRepoId(line.getPP_Order_BOMLine_ID());
