cb1628b5bc567441a6a551640ff017942772fe99
Teodor Sarca <teo.sarca@metasfresh.com>
2024-10-22 10:30:09 +0300

    more unit tests

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/qrcodes/gs1/GS1HUQRCode.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/qrcodes/gs1/GS1HUQRCode.java
@@ -7,6 +7,7 @@ import de.metas.handlingunits.qrcodes.model.IHUQRCode;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
+import org.adempiere.exceptions.AdempiereException;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -28,8 +29,18 @@ public class GS1HUQRCode implements IHUQRCode
 		final GS1Elements elements = GS1Parser.parseElementsOrNull(string);
 		return elements != null ? new GS1HUQRCode(elements) : null;
 	}
-	
-	public Optional<GTIN> getGTIN() { return elements.getGTIN();}
+
+	public static GS1HUQRCode fromString(@NonNull final String string)
+	{
+		final GS1HUQRCode code = fromStringOrNullIfNotHandled(string);
+		if (code == null)
+		{
+			throw new AdempiereException("Invalid GS1 code: " + string);
+		}
+		return code;
+	}
+
+	public Optional<GTIN> getGTIN() {return elements.getGTIN();}
 
 	@Override
 	public Optional<BigDecimal> getWeightInKg() {return elements.getWeightInKg();}
