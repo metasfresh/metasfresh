82535f5c069b7080cd5df31a15a765fe05ec912f
dm-ps <dragos.moca@pevesoft.ro>
2024-10-25 16:08:39 +0300

    Function that create Partner, Project and external project reference refs: https://github.com/metasfresh/metasfresh/issues/19234

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5737810_sys_gh19234_New_Project_Onboarding.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5737810_sys_gh19234_New_Project_Onboarding.sql
@@ -1,85 +1,47 @@
--- Run mode: SWING_CLIENT
-
--- Tab: Inventur(168,D) -> Bestandszählung
--- Table: M_Inventory
--- 2024-10-25T10:15:01.081Z
-UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_TIMESTAMP('2024-10-25 10:15:01.081000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Tab_ID=255
+DROP FUNCTION IF EXISTS New_Project_Onboarding
+(
+    /* p_project_value */ varchar
+, /* p_project_name */    varchar
+, /* p_partner_value */   varchar
+, /* p_partner_name */    varchar
+)
 ;
 
--- Tab: Inventur(168,D) -> Bestandszählung
--- Table: M_Inventory
--- 2024-10-25T10:15:03.783Z
-UPDATE AD_Tab SET IsInsertRecord='Y',Updated=TO_TIMESTAMP('2024-10-25 10:15:03.783000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Tab_ID=255
+CREATE OR REPLACE FUNCTION New_Project_Onboarding(p_project_value varchar,
+                                                  p_project_name  varchar,
+                                                  p_partner_value varchar,
+                                                  p_partner_name  varchar)
+    RETURNS void
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    _partner_id           numeric := NEXTVAL(''c_bpartner_seq '');
+    _project_id           numeric := NEXTVAL(''c_project_seq '');
+    _location_id          numeric := NEXTVAL(''c_location_seq '');
+    _bpartner_location_id numeric := NEXTVAL(''c_bpartner_location_seq '');
+    _AD_Client_ID         numeric := 1000000;
+    _AD_Org_ID            numeric := 1000000;
+BEGIN
+
+    INSERT INTO C_BPartner(c_bpartner_id, c_bp_group_id, ad_client_id, ad_org_id, created, createdby, updated, updatedby, value, name, url)
+    VALUES (_partner_id, 1000000, _AD_Client_ID, _AD_Org_ID, NOW(), 99, NOW(), 99, p_partner_value, p_partner_name, CONCAT('' git @ github.com:metasfresh / '', p_project_value, ''.git ''));
+
+    INSERT INTO c_location(c_location_id, ad_client_id, ad_org_id, created, createdby, updated, updatedby, c_country_id)
+    VALUES (_location_id, _AD_Client_ID, _AD_Org_ID, NOW(), 99, NOW(), 99, 101);
+
+    INSERT INTO c_bpartner_location(c_bpartner_location_id, c_bpartner_id, c_location_id, ad_client_id, ad_org_id, created, createdby, updated, updatedby)
+    VALUES (_bpartner_location_id, _partner_id, _location_id, _AD_Client_ID, _AD_Org_ID, NOW(), 99, NOW(), 99);
+
+    INSERT INTO c_project(c_project_id, c_bpartner_id, c_bpartner_location_id, ad_client_id, ad_org_id, created, createdby, updated, updatedby, value, name, c_currency_id, r_statuscategory_id)
+    VALUES (_project_id, _partner_id, _bpartner_location_id, _AD_Client_ID, _AD_Org_ID, NOW(), 99, NOW(), 99, p_project_value, p_project_name, 102, 540009);
+
+    INSERT INTO s_externalprojectreference(s_externalprojectreference_id, isactive, ad_client_id, ad_org_id, created, createdby, updated, updatedby, c_project_id, projecttype, externalsystem, externalreference, externalprojectowner)
+    VALUES (NEXTVAL(''s_externalprojectreference_seq ''), ''Y'', _AD_Client_ID, _AD_Org_ID, NOW(), 99, NOW(), 99, _project_id, ''Budget'', ''Github'', p_project_value, ''metasfresh'');
+
+END;
+$$
 ;
 
--- Process: M_Inventory_Mark_As_Counted(de.metas.inventory.process.M_Inventory_Mark_As_Counted)
--- Table: M_Inventory
--- EntityType: D
--- 2024-10-25T10:42:34.564Z
-UPDATE AD_Table_Process SET WEBUI_ViewAction='N',Updated=TO_TIMESTAMP('2024-10-25 10:42:34.564000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=541387
+COMMENT ON FUNCTION New_Project_Onboarding(varchar, varchar, varchar, varchar) IS ''Automated Customer Onboarding: CREATE the NEW customer project AND the necessary entries''
 ;
-
--- Process: M_Inventory_Mark_As_Counted(de.metas.inventory.process.M_Inventory_Mark_As_Counted)
--- Table: M_Inventory
--- EntityType: D
--- 2024-10-25T10:43:35.411Z
-UPDATE AD_Table_Process SET WEBUI_ViewAction='Y',Updated=TO_TIMESTAMP('2024-10-25 10:43:35.411000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=541387
-;
-
--- Process: M_Inventory_Mark_As_Counted(de.metas.inventory.process.M_Inventory_Mark_As_Counted)
--- Table: M_Inventory
--- EntityType: D
--- 2024-10-25T10:52:41.640Z
-UPDATE AD_Table_Process SET WEBUI_ViewQuickAction_Default='Y',Updated=TO_TIMESTAMP('2024-10-25 10:52:41.639000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=541387
-;
-
--- Process: WEBUI_Order_ProductsProposal_Launcher(de.metas.ui.web.order.products_proposal.process.WEBUI_Order_ProductsProposal_Launcher)
--- Table: C_Order
--- Tab: Auftrag(143,D) -> Auftragsposition(187,D)
--- Window: Auftrag(143,D)
--- EntityType: D
--- 2024-10-25T11:00:13.022Z
-UPDATE AD_Table_Process SET WEBUI_IncludedTabTopAction='N',Updated=TO_TIMESTAMP('2024-10-25 11:00:13.022000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=540665
-;
-
--- Process: WEBUI_Order_ProductsProposal_Launcher(de.metas.ui.web.order.products_proposal.process.WEBUI_Order_ProductsProposal_Launcher)
--- Table: C_Order
--- Tab: Auftrag(143,D) -> Auftragsposition(187,D)
--- Window: Auftrag(143,D)
--- EntityType: D
--- 2024-10-25T11:00:51.180Z
-UPDATE AD_Table_Process SET WEBUI_IncludedTabTopAction='Y',Updated=TO_TIMESTAMP('2024-10-25 11:00:51.179000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=540665
-;
-
--- Process: M_Inventory_Mark_As_Counted(de.metas.inventory.process.M_Inventory_Mark_As_Counted)
--- Table: M_Inventory
--- EntityType: D
--- 2024-10-25T11:01:20.779Z
-UPDATE AD_Table_Process SET WEBUI_DocumentAction='N', WEBUI_IncludedTabTopAction='Y', WEBUI_ViewAction='N', WEBUI_ViewQuickAction='N',Updated=TO_TIMESTAMP('2024-10-25 11:01:20.778000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=541387
-;
-
--- Value: M_Inventory_Mark_As_Counted
--- Classname: de.metas.inventory.process.M_Inventory_Mark_As_Counted
--- 2024-10-25T11:33:06.701Z
-UPDATE AD_Process SET AllowProcessReRun='N',Updated=TO_TIMESTAMP('2024-10-25 11:33:06.699000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585274
-;
-
--- Process: M_Inventory_Mark_As_Counted(de.metas.inventory.process.M_Inventory_Mark_As_Counted)
--- Table: M_Inventory
--- EntityType: D
--- 2024-10-25T11:33:31.982Z
-UPDATE AD_Table_Process SET WEBUI_DocumentAction='Y', WEBUI_IncludedTabTopAction='N',Updated=TO_TIMESTAMP('2024-10-25 11:33:31.982000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_Process_ID=541387
-;
-
--- Value: M_Inventory_CreateLines_ForLocatorAndProduct
--- Classname: de.metas.handlingunits.inventory.draftlinescreator.process.M_Inventory_CreateLines_ForLocatorAndProduct
--- 2024-10-25T11:35:25.440Z
-UPDATE AD_Process SET AllowProcessReRun='Y',Updated=TO_TIMESTAMP('2024-10-25 11:35:25.439000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=540935
-;
-
--- Value: M_Inventory_CreateLines_ForLocatorAndProduct
--- Classname: de.metas.handlingunits.inventory.draftlinescreator.process.M_Inventory_CreateLines_ForLocatorAndProduct
--- 2024-10-25T11:36:40.920Z
-UPDATE AD_Process SET AllowProcessReRun='N', RefreshAllAfterExecution='Y',Updated=TO_TIMESTAMP('2024-10-25 11:36:40.920000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=540935
-;
-
