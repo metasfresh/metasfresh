97db9a39e10daaf69897bce8e3cf07dec9b1bc02
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-10-31 17:00:46 +0200

    qa

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5738270_Valrule_C_Flatrate_Conditions_OrderLine.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5738270_Valrule_C_Flatrate_Conditions_OrderLine.sql
@@ -23,3 +23,8 @@ UPDATE AD_SysConfig SET Value='540693',Updated=TO_TIMESTAMP('2024-10-31 16:24:29
 UPDATE AD_Val_Rule SET Code='(NOT EXISTS (SELECT 1 from C_DocType dt where dt.C_DocType_ID = @C_DocTypeTarget_ID/-1@  AND dt.docsubtype in (''PF'',''CAO'' )))  AND (C_Flatrate_Conditions.C_Flatrate_Conditions_ID IN ( (SELECT c.C_Flatrate_Conditions_ID FROM C_Flatrate_Conditions c INNER JOIN ModCntr_Settings mc ON @Harvesting_Year_ID/0@>0 AND mc.ModCntr_Settings_ID = c.ModCntr_Settings_ID AND mc.M_Raw_Product_ID = @M_Product_ID@ AND mc.C_Year_ID = @Harvesting_Year_ID/0@ AND mc.isSOTrx = ''@IsSOTrx@'' WHERE c.ModCntr_Settings_ID IS NOT NULL AND c.DocStatus = ''CO'' AND c.type_conditions !=''InterimInvoice'' ) UNION ALL (SELECT C_Flatrate_Conditions_ID FROM C_Flatrate_Conditions WHERE @Harvesting_Year_ID/0@=0 AND type_conditions NOT IN (''InterimInvoice'', ''ModularContract'') AND DocStatus = ''CO'' ) ) )',Updated=TO_TIMESTAMP('2024-10-31 16:51:08.404','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Val_Rule_ID=540693
 ;
 
+-- Name: C_Flatrate_Conditions_Modular_Settings
+-- 2024-10-31T13:43:52.292Z
+DELETE FROM AD_Val_Rule WHERE AD_Val_Rule_ID=540640
+;
+
