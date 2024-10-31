01c63c2b9f8b63a135c0cea4fd7baa25e1a6e108
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-10-31 16:36:00 +0200

    add the valrule in sys config for order line quickinput

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5738270_Valrule_C_Flatrate_Conditions_OrderLine.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5738270_Valrule_C_Flatrate_Conditions_OrderLine.sql
@@ -10,3 +10,16 @@ INSERT INTO AD_Val_Rule (AD_Client_ID,AD_Org_ID,AD_Val_Rule_ID,Code,Created,Crea
 UPDATE AD_Column SET AD_Val_Rule_ID=540693,Updated=TO_TIMESTAMP('2024-10-31 15:40:48.63','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=540003
 ;
 
+
+-- SysConfig Name: OrderLineQuickInputDescriptorFactory.FilterFlatrateConditionsADValRule
+-- SysConfig Value: 540693
+-- SysConfig Value (old): 540640
+-- 2024-10-31T14:24:29.437Z
+UPDATE AD_SysConfig SET Value='540693',Updated=TO_TIMESTAMP('2024-10-31 16:24:29.435','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_SysConfig_ID=541640
+;
+
+-- Name: C_Flatrate_Conditions for order line
+-- 2024-10-31T14:28:13.480Z
+UPDATE AD_Val_Rule SET Code='(@C_DocTypeTarget_ID/-1@ NOT IN (541034,541062,541113,541112)) AND (C_Flatrate_Conditions.C_Flatrate_Conditions_ID IN ( (SELECT c.C_Flatrate_Conditions_ID FROM C_Flatrate_Conditions c INNER JOIN ModCntr_Settings mc ON @Harvesting_Year_ID/0@>0 AND mc.ModCntr_Settings_ID = c.ModCntr_Settings_ID AND mc.M_Raw_Product_ID = @M_Product_ID@ AND mc.C_Year_ID = @Harvesting_Year_ID/0@ AND mc.isSOTrx = ''@IsSOTrx@'' WHERE c.ModCntr_Settings_ID IS NOT NULL AND c.DocStatus = ''CO'' AND c.type_conditions =  ''InterimInvoice'' ) UNION ALL (SELECT C_Flatrate_Conditions_ID FROM C_Flatrate_Conditions WHERE @Harvesting_Year_ID/0@=0 AND type_conditions NOT IN (''InterimInvoice'', ''ModularContract'') AND DocStatus = ''CO'' ) ) )',Updated=TO_TIMESTAMP('2024-10-31 16:28:13.477','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Val_Rule_ID=540693
+;
+
