d8032bae59dfa6f39ae7ae55cc0d59799ed053cf
Max Rieck <max.rieck@metasfresh.com>
2024-11-01 16:32:20 +0100

    Fix condition removal

--- backend/de.metas.business/src/main/java/de/metas/order/model/interceptor/C_Order.java
+++ backend/de.metas.business/src/main/java/de/metas/order/model/interceptor/C_Order.java
@@ -164,7 +164,7 @@ public class C_Order
 			ModelValidator.TYPE_BEFORE_CHANGE
 	}, ifColumnsChanged = {
 			I_C_Order.COLUMNNAME_C_DocTypeTarget_ID,
-			I_C_Order.COLUMNNAME_C_DocType_ID,
+			I_C_Order.COLUMNNAME_C_DocType_ID
 	})
 	public void removeFlatRateConditionsForCallOrderAndProformaOrder(final I_C_Order order)
 	{
