0d4007346ba9a7b9d0486521c697aa513b688cd4
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-11-04 16:39:28 +0200

    proto continued

--- backend/de.metas.contracts/src/main/java/de/metas/contracts/bpartner/process/C_BPartner_MoveToAnotherOrg_Mass.java
+++ backend/de.metas.contracts/src/main/java/de/metas/contracts/bpartner/process/C_BPartner_MoveToAnotherOrg_Mass.java
@@ -22,9 +22,7 @@
 
 package de.metas.contracts.bpartner.process;
 
-import com.google.common.collect.ImmutableSet;
 import de.metas.bpartner.BPartnerId;
-import de.metas.bpartner.service.IBPartnerBL;
 import de.metas.common.util.time.SystemTime;
 import de.metas.contracts.bpartner.service.OrgChangeRequest;
 import de.metas.contracts.bpartner.service.OrgChangeService;
@@ -34,12 +32,17 @@ import de.metas.process.IProcessDefaultParameter;
 import de.metas.process.IProcessDefaultParametersProvider;
 import de.metas.process.JavaProcess;
 import de.metas.process.Param;
-import de.metas.util.Services;
+import lombok.NonNull;
+import org.adempiere.ad.dao.impl.TypedSqlQuery;
+import org.adempiere.ad.trx.api.ITrx;
 import org.compiere.SpringContextHolder;
+import org.compiere.model.IQuery;
+import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_C_CompensationGroup_Schema_Category;
 
 import javax.annotation.Nullable;
 import java.time.Instant;
+import java.util.Iterator;
 
 public class C_BPartner_MoveToAnotherOrg_Mass extends JavaProcess implements
 		IProcessDefaultParametersProvider
@@ -49,40 +52,36 @@ public class C_BPartner_MoveToAnotherOrg_Mass extends JavaProcess implements
 	public static final String PARAM_AD_ORG_TARGET_ID = "AD_Org_Target_ID";
 	public static final String PARAM_GroupCategory_ID = I_C_CompensationGroup_Schema_Category.COLUMNNAME_C_CompensationGroup_Schema_Category_ID;
 	public static final String PARAM_DATE_ORG_CHANGE = "Date_OrgChange";
-	public static final String PARAM_IS_SHOW_MEMBERSHIP_PARAMETER = "IsShowMembershipParameter";
 	public static final String PARAM_IsCloseInvoiceCandidate = "IsCloseInvoiceCandidate";
+	public static final String PARAM_WhereClause = "WhereClause";
+
 	final OrgChangeService service = SpringContextHolder.instance.getBean(OrgChangeService.class);
-	final IBPartnerBL bpartnerBL = Services.get(IBPartnerBL.class);
-	@Param(parameterName = PARAM_AD_ORG_SOURCE_ID, mandatory = true)
-	protected OrgId p_orgSourceId;
 
 	@Param(parameterName = PARAM_AD_ORG_TARGET_ID, mandatory = true)
 	protected OrgId p_orgTargetId;
-
 	@Param(parameterName = PARAM_GroupCategory_ID)
 	protected GroupCategoryId p_groupCategoryId;
-
 	@Param(parameterName = PARAM_DATE_ORG_CHANGE, mandatory = true)
 	protected Instant p_startDate;
-
-	@Param(parameterName = PARAM_IS_SHOW_MEMBERSHIP_PARAMETER, mandatory = true)
-	protected boolean isShowMembershipParameter;
-
 	@Param(parameterName = PARAM_IsCloseInvoiceCandidate)
 	private boolean isCloseInvoiceCandidate;
+	@Param(parameterName = PARAM_WhereClause, mandatory = true)
+	protected String p_WhereClause;
 
 	@Override
 	protected String doIt() throws Exception
 	{
-		final ImmutableSet<BPartnerId> bPartnerIdsToMove = bpartnerBL.getBPartnerIdsForOrg(p_orgSourceId);
+		final Iterator<I_C_BPartner> partnerIterator = retrievePartnersToMove(p_WhereClause);
 
-		for (final BPartnerId bPartnerId : bPartnerIdsToMove)
+		while (partnerIterator.hasNext())
 		{
+			final I_C_BPartner partner = partnerIterator.next();
+
 			final OrgChangeRequest orgChangeRequest = OrgChangeRequest.builder()
-					.bpartnerId(bPartnerId)
+					.bpartnerId(BPartnerId.ofRepoId(partner.getC_BPartner_ID()))
 					.startDate(p_startDate)
 					.groupCategoryId(p_groupCategoryId)
-					.orgFromId(p_orgSourceId)
+					.orgFromId(OrgId.ofRepoId(partner.getAD_Org_ID()))
 					.orgToId(p_orgTargetId)
 					.isCloseInvoiceCandidate(isCloseInvoiceCandidate)
 					.build();
@@ -103,5 +102,13 @@ public class C_BPartner_MoveToAnotherOrg_Mass extends JavaProcess implements
 		}
 
 		return IProcessDefaultParametersProvider.DEFAULT_VALUE_NOTAVAILABLE;
+	}
+
+	private Iterator<I_C_BPartner> retrievePartnersToMove(@NonNull final String whereClause)
+	{
+		return new TypedSqlQuery<>(getCtx(), I_C_BPartner.class, whereClause, ITrx.TRXNAME_None)
+				.setOption(IQuery.OPTION_IteratorBufferSize, 1)
+				.iterate();
+
 	}
 }
