7cd9d4789c4a52c091073aea0b8823097f69dcec
riad.mennour <riad.mennour@gmail.com>
2024-11-26 16:22:22 +0100

    Update shipment footer sub report

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5740540_sys_gh19365_Update_Shipment_DocumentNote.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5740540_sys_gh19365_Update_Shipment_DocumentNote.sql
@@ -1,24 +1,17 @@
---
--- Script: /tmp/webui_migration_scripts_2024-11-26_3065392917516489212/5740540_migration_2024-11-26_postgresql.sql
--- User: metasfresh
--- OS user: root
---
-
-
 -- 2024-11-26T10:19:22.323Z
-UPDATE C_DocType SET DocumentNote='<b>Waren in einwandfreiem Zustand erhalten.</b>
+UPDATE C_DocType SET DocumentNote='<br><b>Waren in einwandfreiem Zustand erhalten.</b><br><br><br>
 
 <b>Unterschrift:</b>',Updated=TO_TIMESTAMP('2024-11-26 11:19:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_DocType_ID=1000011
 ;
 
 -- 2024-11-26T10:19:22.327Z
-UPDATE C_DocType_Trl trl SET Description=NULL, DocumentNote='<b>Waren in einwandfreiem Zustand erhalten.</b>
+UPDATE C_DocType_Trl trl SET Description=NULL, DocumentNote='<br><b>Waren in einwandfreiem Zustand erhalten.</b><br><br><br>
 
 <b>Unterschrift:</b>', Name='Lieferschein', PrintName='Lieferschein'  WHERE C_DocType_ID=1000011 AND ( trl.isTranslated = 'N'  OR  exists(select 1 from ad_language lang where lang.ad_language = trl.ad_language and lang.isBaseLanguage = 'Y') )
 ;
 
 -- 2024-11-26T10:19:47.448Z
-UPDATE C_DocType_Trl SET DocumentNote='<b>Goods received in perfect condition.</b>
+UPDATE C_DocType_Trl SET DocumentNote='<br><b>Goods received in perfect condition.</b><br><br><br>
 
 <b>Signature:</b>',Updated=TO_TIMESTAMP('2024-11-26 11:19:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Language='en_US' AND C_DocType_ID=1000011
 ;
