75570a0105e4e4190a45a1e5074263441839f4df
riad.mennour <riad.mennour@gmail.com>
2024-11-27 15:18:35 +0100

    Update generic reports

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5740570_sys_gh19365_Docs_Inout_Details_Footer.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5740570_sys_gh19365_Docs_Inout_Details_Footer.sql
@@ -23,55 +23,63 @@ AS
 $$
 
 SELECT footer.*,
-       COALESCE(inc_trl.name, inc.name)     AS Incoterms,
-       io.incotermlocation,
        COALESCE(o_dr_trl.name, o_dr.name)   AS deliveryrule,
        COALESCE(o_dvr_trl.name, o_dvr.name) AS deliveryviarule
 FROM (
          --Docnote DE
-         SELECT NULL                                                                                AS textleft,
+         SELECT NULL                                                                                        AS textleft,
                 CASE
                     WHEN io.descriptionbottom IS NOT NULL
                         THEN E'\n\n\n'
                         ELSE ''
-                END || dt.documentnote                                                              AS textcenter,
-                (SELECT AD_Language FROM AD_Language WHERE IsBaseLanguage = 'Y' AND isActive = 'Y') AS language,
-                io.m_inout_id                                                                       AS m_inout_id,
-                'docnote'                                                                           AS tag,
-                3                                                                                   AS pozition
+                END || dt.documentnote                                                                      AS textcenter,
+                (SELECT l.AD_Language FROM AD_Language l WHERE l.IsBaseLanguage = 'Y' AND l.isActive = 'Y') AS language,
+                io.m_inout_id                                                                               AS m_inout_id,
+                'docnote'                                                                                   AS tag,
+                3                                                                                           AS pozition,
+                COALESCE(inc_trl.name, inc.name)                                                            AS Incoterms,
+                io.incotermlocation
          FROM m_inout io
                   LEFT JOIN c_doctype dt ON io.c_doctype_id = dt.c_doctype_id AND dt.isActive = 'Y'
+                  LEFT OUTER JOIN C_Incoterms inc ON io.c_incoterms_id = inc.c_incoterms_id
+                  LEFT OUTER JOIN C_Incoterms_trl inc_trl ON inc.c_incoterms_id = inc_trl.c_incoterms_id AND inc_trl.ad_language = p_Language
          WHERE io.isActive = 'Y'
          UNION
          ---------------------------------------------------------------------------------------------
          --Docnote TRL
-         SELECT NULL                   AS textleft,
+         SELECT NULL                             AS textleft,
                 CASE
                     WHEN io.descriptionbottom IS NOT NULL
                         THEN E'\n\n\n'
                         ELSE ''
-                END || dt.documentnote AS textcenter,
-                dt.ad_language         AS language,
-                io.m_inout_id          AS m_inout_id,
-                'docnote'              AS tag,
-                3                      AS pozition
+                END || dt.documentnote           AS textcenter,
+                dt.ad_language                   AS language,
+                io.m_inout_id                    AS m_inout_id,
+                'docnote'                        AS tag,
+                3                                AS pozition,
+                COALESCE(inc_trl.name, inc.name) AS Incoterms,
+                io.incotermlocation
          FROM m_inout io
                   LEFT JOIN c_doctype_trl dt ON io.c_doctype_id = dt.c_doctype_id AND dt.isActive = 'Y'
+                  LEFT OUTER JOIN C_Incoterms inc ON io.c_incoterms_id = inc.c_incoterms_id
+                  LEFT OUTER JOIN C_Incoterms_trl inc_trl ON inc.c_incoterms_id = inc_trl.c_incoterms_id AND inc_trl.ad_language = p_Language
          WHERE io.isActive = 'Y'
          UNION
          ---------------------------------------------------------------------------------------------
          --Descriptionbottom
-         SELECT io.descriptionbottom AS textleft,
-                NULL                 AS textcenter,
-                NULL                 AS language,
-                io.m_inout_id        AS m_inout_id,
-                'descr'              AS tag,
-                2                    AS pozition
+         SELECT io.descriptionbottom             AS textleft,
+                NULL                             AS textcenter,
+                NULL                             AS language,
+                io.m_inout_id                    AS m_inout_id,
+                'descr'                          AS tag,
+                2                                AS pozition,
+                COALESCE(inc_trl.name, inc.name) AS Incoterms,
+                io.incotermlocation
          FROM m_inout io
+                  LEFT OUTER JOIN C_Incoterms inc ON io.c_incoterms_id = inc.c_incoterms_id
+                  LEFT OUTER JOIN C_Incoterms_trl inc_trl ON inc.c_incoterms_id = inc_trl.c_incoterms_id AND inc_trl.ad_language = p_Language
          WHERE io.isActive = 'Y') footer
          INNER JOIN m_inout io ON io.m_inout_id = footer.m_inout_id
-         LEFT OUTER JOIN C_Incoterms inc ON io.c_incoterms_id = inc.c_incoterms_id
-         LEFT OUTER JOIN C_Incoterms_trl inc_trl ON inc.c_incoterms_id = inc_trl.c_incoterms_id AND inc_trl.ad_language = p_Language
          LEFT JOIN AD_Ref_List o_dr ON o_dr.AD_Reference_ID = 151 AND o_dr.value = io.deliveryrule
          LEFT JOIN AD_Ref_List_Trl o_dr_trl ON o_dr.ad_ref_list_id = o_dr_trl.ad_ref_list_id AND o_dr_trl.ad_language = p_Language
          LEFT JOIN AD_Ref_List o_dvr ON o_dvr.AD_Reference_ID = 152 AND o_dvr.value = io.deliveryviarule
