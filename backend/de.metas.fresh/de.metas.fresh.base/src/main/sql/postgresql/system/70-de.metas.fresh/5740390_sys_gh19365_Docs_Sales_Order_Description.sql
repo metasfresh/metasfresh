fcf2f96feeefcb5eb280a8d25376398ad857616b
riad.mennour <riad.mennour@gmail.com>
2024-11-27 09:21:32 +0100

    Update generic reports

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5740390_sys_gh19365_Docs_Sales_Order_Description.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5740390_sys_gh19365_Docs_Sales_Order_Description.sql
@@ -29,7 +29,7 @@ CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Sales_Order_D
                 dt_description    text,
                 offer_documentno  character varying,
                 deliverytoaddress character varying,
-                validuntil        timestamp WITH TIME ZONE,
+                validuntil        timestamp,
                 versionno           character varying
             )
     STABLE
