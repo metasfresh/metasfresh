b21c8782b9cfbe10419c3b311c533623b14ae0fb
dm-ps <dragos.moca@pevesoft.ro>
2024-12-10 19:11:03 +0200

    Delete Outdated Period Controls refs: https://github.com/metasfresh/metasfresh/issues/19566

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5741410_sys_gh19584_Delete_Outdated_Period_Controls.sql
@@ -0,0 +1,3 @@
+-- DELETE ALL THE PERIOD CONTROLS THAT HAVE A DocBaseType which is not anymore in the C_DocType DocBaseType AD_Ref_List
+DELETE FROM c_periodcontrol
+WHERE docbasetype NOT IN (select value from ad_ref_list where ad_reference_id = 183);
