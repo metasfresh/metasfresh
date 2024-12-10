fb7457277fbdef798c7d1752cad8de07e3b3d3d1
metas-ts <tobias.schoeneberg@metasfresh.com>
2024-12-10 15:05:11 +0100

    Fix locking issue in C_OLCandEnqueueForSalesOrderCreation
    Also
    - fix unrelated NPE in WEBUI_Order_ProductsProposal_Launcher
    - minor improvement in ProcessInfo

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/ProcessInfo.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/ProcessInfo.java
@@ -79,6 +79,7 @@ import org.compiere.model.X_C_DocType;
 import org.compiere.util.Env;
 import org.compiere.util.Ini;
 import org.compiere.util.Util;
+import org.jetbrains.annotations.Contract;
 import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
@@ -676,7 +677,7 @@ public final class ProcessInfo implements Serializable
 	 * task 03685
 	 * @see JavaProcess#retrieveSelectedRecordsQueryBuilder(Class)
 	 */
-	@Nullable
+	@NonNull
 	public <T> IQueryFilter<T> getQueryFilterOrElseTrue()
 	{
 		// default: use a "neutral" filter that does not exclude anything
@@ -697,6 +698,7 @@ public final class ProcessInfo implements Serializable
 	 * @param defaultQueryFilter filter to be returned if this process info does not have a whereClause set.
 	 * @return a query filter for the current m_whereClause or if there is none, return <code>defaultQueryFilter</code>
 	 */
+	@Contract("!null -> !null")
 	@Nullable
 	public <T> IQueryFilter<T> getQueryFilterOrElse(@Nullable final IQueryFilter<T> defaultQueryFilter)
 	{
