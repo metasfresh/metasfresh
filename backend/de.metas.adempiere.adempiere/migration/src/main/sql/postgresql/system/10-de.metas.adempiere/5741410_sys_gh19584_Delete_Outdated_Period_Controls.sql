ec789751bb95634acc36df2fb925223995952339
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2024-12-11 07:44:02 +0100

    back up C_PeriodControl before cleanup
    Co-authored-by: Adrian <60300682+adi-stefan@users.noreply.github.com>

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5741410_sys_gh19584_Delete_Outdated_Period_Controls.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5741410_sys_gh19584_Delete_Outdated_Period_Controls.sql
@@ -1,3 +1,6 @@
 -- DELETE ALL THE PERIOD CONTROLS THAT HAVE A DocBaseType which is not anymore in the C_DocType DocBaseType AD_Ref_List
+SELECT backup_table('c_periodcontrol')
+;
+
 DELETE FROM c_periodcontrol
 WHERE docbasetype NOT IN (select value from ad_ref_list where ad_reference_id = 183);
