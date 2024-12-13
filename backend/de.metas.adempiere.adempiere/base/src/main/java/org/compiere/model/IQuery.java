0119911f2b401a2af99f688d642cd5c9d166ca8f
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-12-13 11:54:39 +0200

    Bring datev changes from master

--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/compiere/model/IQuery.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/compiere/model/IQuery.java
@@ -105,6 +105,8 @@ public interface IQuery<T>
 
 	String getTableName();
 
+	IQuery<T> setSqlFromParameter(@NonNull final String name, @Nullable final Object value);
+
 	List<T> list() throws DBException;
 
 	/**
