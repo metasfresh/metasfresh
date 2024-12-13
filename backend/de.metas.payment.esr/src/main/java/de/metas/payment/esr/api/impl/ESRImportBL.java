a1a110b0f1c0246d0244c1524ddc715d21a65018
Cristina Ghita <cristina.ghita@metasfresh.com>
2024-12-13 16:31:00 +0200

    Support SCOR type when Importing esr  files (#19560)
    * Support SCOR type when Importing esr  files
    
    * Validate file type
    
    * Support SCOR payments - do not create payment - just process the line with action UNKNOW Invoice
    
    * Add Unknown_Invoice ESR Action
    
    * Avoid NPE
    
    * Support SCO explicitelly
    
    * Format code
    
    * Avoid NPE
    
    * Improve processing ZIP files
    
    * Fix reference value
    
    * Set to valid and processed scor line

--- backend/de.metas.payment.esr/src/main/java/de/metas/payment/esr/api/impl/ESRImportBL.java
+++ backend/de.metas.payment.esr/src/main/java/de/metas/payment/esr/api/impl/ESRImportBL.java
@@ -44,6 +44,7 @@ import de.metas.payment.esr.dataimporter.ESRImportEnqueuer;
 import de.metas.payment.esr.dataimporter.ESRImportEnqueuerDataSource;
 import de.metas.payment.esr.dataimporter.ESRStatement;
 import de.metas.payment.esr.dataimporter.ESRTransaction;
+import de.metas.payment.esr.dataimporter.ESRType;
 import de.metas.payment.esr.dataimporter.IESRDataImporter;
 import de.metas.payment.esr.dataimporter.impl.v11.ESRTransactionLineMatcherUtil;
 import de.metas.payment.esr.exception.ESRImportLockedException;
@@ -604,6 +605,16 @@ public class ESRImportBL implements IESRImportBL
 			return true;
 		}
 
+		// Check for SCOR ESR Type
+		if (ESRTransactionLineMatcherUtil.isSCORLine(line))
+		{
+			line.setESR_Payment_Action(X_ESR_ImportLine.ESR_PAYMENT_ACTION_Unknown_Invoice);
+			line.setIsValid(true);
+			line.setProcessed(true);
+			esrImportDAO.save(line);
+			return true;
+		}
+
 		// skip reverse booking lines from regular processing
 		// the admin should deal with them manually
 		if (isReverseBookingLine(line))
@@ -664,6 +675,7 @@ public class ESRImportBL implements IESRImportBL
 
 		return existentPaymentId.orElse(null);
 	}
+
 
 	/**
 	 * @task https://github.com/metasfresh/metasfresh/issues/2118
