2895b4423499f93f52ed8424b457313fb60412b7
Max Rieck <max.rieck@metasfresh.com>
2024-12-13 15:40:48 +0100

    Update pipeline 0.3.6 to 0.3.9

--- docker-builds/db/scripts/adjust-config.sql
+++ docker-builds/db/scripts/adjust-config.sql
@@ -1,6 +1,3 @@
-
-update ad_sysconfig set value='http://metasfresh-app:8282/adempiereJasper/ReportServlet' where name='de.metas.adempiere.report.jasper.JRServerServlet';
-update ad_sysconfig set value='http://metasfresh-app:8282/adempiereJasper/BarcodeServlet' where name='de.metas.adempiere.report.barcode.BarcodeServlet';
 update ad_sysconfig set value='' where name='webui.frontend.url';
 
 insert into ad_user(
