183cfe40264d59ff1471d2893fba09fd47dc604e
metas_rc <ruxandra.craciunescu@metasfresh.com>
2024-12-17 11:40:23 +0200

    change interceptor

--- backend/de.metas.edi/src/main/java/de/metas/edi/model/validator/C_Doc_Outbound_Log.java
+++ backend/de.metas.edi/src/main/java/de/metas/edi/model/validator/C_Doc_Outbound_Log.java
@@ -43,7 +43,7 @@ public class C_Doc_Outbound_Log
 		this.ediDocOutBoundLogService = ediDocOutBoundLogService;
 	}
 
-	@ModelChange(timings = ModelValidator.TYPE_BEFORE_NEW)
+	@ModelChange(timings = ModelValidator.TYPE_AFTER_NEW)
 	public void setEdiExportStatusFromInvoice(@NonNull final I_C_Doc_Outbound_Log docOutboundLogRecord)
 	{
 		final TableRecordReference recordReference = TableRecordReference.ofReferenced(docOutboundLogRecord);
