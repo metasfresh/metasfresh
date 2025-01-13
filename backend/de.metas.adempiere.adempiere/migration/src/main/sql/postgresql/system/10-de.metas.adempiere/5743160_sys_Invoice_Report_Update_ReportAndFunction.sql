7da67f1739680288e8b886b42bade6a199940aa2
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-01-13 17:09:03 +0200

    additional column in sales invoice report

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5743160_sys_Invoice_Report_Update_ReportAndFunction.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5743160_sys_Invoice_Report_Update_ReportAndFunction.sql
@@ -19,7 +19,7 @@ SELECT DISTINCT dt.name                                                         
                 i.documentno                                                                                           AS InvoiceDocumentNo,
                 il.qtyentered * (CASE WHEN dt.docbasetype IN ('APC', 'ARC') THEN -1 ELSE 1 END)::NUMERIC               AS InvoicedQty,
                 il.linenetamt * (CASE WHEN dt.docbasetype IN ('APC', 'ARC') THEN -1 ELSE 1 END)::NUMERIC               AS LineNetAmt,
-                (il.taxamtinfo + il.linenetamt) * (CASE WHEN dt.docbasetype IN ('APC', 'ARC') THEN -1 ELSE 1 END)::NUMERIC AS LineGrossAmt
+                (il.taxamt + il.linenetamt) * (CASE WHEN dt.docbasetype IN ('APC', 'ARC') THEN -1 ELSE 1 END)::NUMERIC AS LineGrossAmt
 
 
 FROM c_invoice i
@@ -44,3 +44,85 @@ WHERE i.docstatus = 'CO'::bpchar
 ALTER TABLE rv_sales_invoice_report
     OWNER TO metasfresh
 ;
+
+
+
+
+
+
+
+
+DROP FUNCTION IF EXISTS sales_invoice_excel_report(date,
+                                                   date)
+;
+
+
+DROP FUNCTION IF EXISTS sales_invoice_excel_report(date,
+                                                   date,
+                                                   date,
+                                                   date)
+;
+
+CREATE FUNCTION sales_invoice_excel_report(p_DateInvoicedFrom date,
+                                           p_DateInvoicedTo   date,
+                                           p_DeliveryDateFrom date,
+                                           p_DeliveryDateTo   date)
+    RETURNS TABLE
+            (
+                DocTypeName       character varying,
+                ProductValue      character varying,
+                ProductName       character varying,
+                Postal            character varying,
+                ProductCategory   character varying,
+                BPValue           character varying,
+                BPName            character varying,
+                BPGroupName       character varying,
+                SalesRep_Name     character varying,
+                DeliveryDate      date,
+                DateInvoiced      date,
+                InvoiceDocumentNo character varying,
+                InvoicedQty       numeric,
+                LineNetAmt        numeric,
+                LineGrossAmt      numeric
+            )
+    STABLE
+    LANGUAGE sql
+AS
+$$
+
+SELECT x.DocTypeName       AS DocTypeName,
+       x.ProductValue      AS ProductValue,
+       x.ProductName       AS ProductName,
+       x.Postal            AS Postal,
+       x.ProductCategory   AS ProductCategory,
+       x.BPValue           AS BPValue,
+       x.BPName            AS BPName,
+       x.BPGroupName       AS BPGroupName,
+       x.SalesRep_Name     AS SalesRep_Name,
+       x.DeliveryDate      AS DeliveryDate,
+       x.DateInvoiced      AS DateInvoiced,
+       x.InvoiceDocumentNo AS InvoiceDocumentNo,
+       x.InvoicedQty       AS InvoicedQty,
+       x.LineNetAmt        AS LineNetAmt,
+       x.LineGrossAmt      AS LineGrossAmt
+
+FROM rv_sales_invoice_report x
+WHERE (p_DateInvoicedFrom <= x.DateInvoiced)
+  AND (p_DateInvoicedTo >= x.DateInvoiced)
+  AND (p_DeliveryDateFrom <= x.DeliveryDate)
+  AND (p_DeliveryDateTo >= x.DeliveryDate)
+
+$$
+;
+
+ALTER FUNCTION sales_invoice_excel_report(date, date, date, date) OWNER TO metasfresh
+;
+
+
+
+
+
+
+
+
+
