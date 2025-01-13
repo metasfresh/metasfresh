a08d33735806c2105f590521397a61064f3aab50
dm-ps <83770997+dm-ps@users.noreply.github.com>
2025-01-13 09:57:10 +0200

    Bestelldispo: UOM conversion not used correctly for products where stock UOM != price UOM (#19763)
    * Bestelldispo: UOM conversion not used correctly for products where stock UOM != price UOM
    refs: https://github.com/metasfresh/metasfresh/issues/19759

--- backend/de.metas.purchasecandidate.base/src/main/java/de/metas/purchasecandidate/purchaseordercreation/remotepurchaseitem/PurchaseOrderItem.java
+++ backend/de.metas.purchasecandidate.base/src/main/java/de/metas/purchasecandidate/purchaseordercreation/remotepurchaseitem/PurchaseOrderItem.java
@@ -11,6 +11,7 @@ import de.metas.purchasecandidate.PurchaseCandidate;
 import de.metas.purchasecandidate.PurchaseCandidateId;
 import de.metas.purchasecandidate.purchaseordercreation.remoteorder.NullVendorGatewayInvoker;
 import de.metas.quantity.Quantity;
+import de.metas.uom.UomId;
 import de.metas.util.Check;
 import de.metas.util.lang.ExternalId;
 import de.metas.util.lang.Percent;
@@ -241,6 +242,12 @@ public class PurchaseOrderItem implements PurchaseItem
 		return purchaseCandidate.getPriceEnteredEff();
 	}
 
+	@Nullable
+	public UomId getPriceUomId()
+	{
+		return purchaseCandidate.getPriceUomId();
+	}
+	
 	@Nullable
 	public Percent getDiscount()
 	{
