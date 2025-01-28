e9e3bb0d7f55813008919912508208ac84e1f571
dm-ps <dragos.moca@pevesoft.ro>
2025-01-28 13:13:15 +0200

    Display Destroyed HUs refs: https://github.com/metasfresh/metasfresh/issues/19940

---
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/5745040_sys_gh19940_Display_Destroyed_HUs.sql
@@ -0,0 +1,8 @@
+-- Tab: Handling Unit -> Handling Unit
+-- Table: M_HU
+-- Tab: Handling Unit(540189,de.metas.handlingunits) -> Handling Unit
+-- Table: M_HU
+-- 2025-01-28T10:56:51.905Z
+UPDATE AD_Tab SET WhereClause='',Updated=TO_TIMESTAMP('2025-01-28 10:56:51.905000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Tab_ID=540508
+;
+
