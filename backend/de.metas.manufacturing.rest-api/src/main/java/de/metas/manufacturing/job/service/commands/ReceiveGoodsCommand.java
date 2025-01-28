8dafe5b363891d4eaa875a3a6280039cfe08f56a
cp-ps <77785468+cp-ps@users.noreply.github.com>
2025-01-28 13:27:30 +0000

    after merge fix

--- backend/de.metas.manufacturing.rest-api/src/main/java/de/metas/manufacturing/job/service/commands/ReceiveGoodsCommand.java
+++ backend/de.metas.manufacturing.rest-api/src/main/java/de/metas/manufacturing/job/service/commands/ReceiveGoodsCommand.java
@@ -290,6 +290,6 @@ public class ReceiveGoodsCommand
 	@NonNull
 	private List<I_M_HU> receiveTUs(@NonNull final HUPIItemProductId tuPIItemProductId)
 	{
-		return newHUProducer().receiveTUs(getQtyToReceive(), tuPIItemProductId).getHus();
+		return newHUProducer().receiveTUs(getQtyToReceive(), tuPIItemProductId);
 	}
 }
