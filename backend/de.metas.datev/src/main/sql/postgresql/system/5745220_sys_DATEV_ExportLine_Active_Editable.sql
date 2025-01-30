439a79957d06ca304b5ae7c716a63a058d7c2a1e
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-01-30 18:33:27 +0200

    Datev Export Line IsActive Updateable

---
+++ backend/de.metas.datev/src/main/sql/postgresql/system/5745220_sys_DATEV_ExportLine_Active_Editable.sql
@@ -0,0 +1,204 @@
+
+
+
+
+-- Tab: Buchungen Export_OLD -> Lines
+-- Table: DATEV_ExportLine
+-- Tab: Buchungen Export_OLD(540413,de.metas.datev) -> Lines
+-- Table: DATEV_ExportLine
+-- 2025-01-30T16:24:26.525Z
+UPDATE AD_Tab SET IsReadOnly='N',Updated=TO_TIMESTAMP('2025-01-30 18:24:26.525','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Tab_ID=541037
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Sektion
+-- Column: DATEV_ExportLine.AD_Org_ID
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Sektion
+-- Column: DATEV_ExportLine.AD_Org_ID
+-- 2025-01-30T16:26:28.320Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:28.319','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562740
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Dokument Basis Typ
+-- Column: DATEV_ExportLine.DocBaseType
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Dokument Basis Typ
+-- Column: DATEV_ExportLine.DocBaseType
+-- 2025-01-30T16:26:30.017Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:30.017','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562754
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Nr.
+-- Column: DATEV_ExportLine.DocumentNo
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Nr.
+-- Column: DATEV_ExportLine.DocumentNo
+-- 2025-01-30T16:26:31.777Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:31.777','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562748
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> DATEV Export Line
+-- Column: DATEV_ExportLine.DATEV_ExportLine_ID
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> DATEV Export Line
+-- Column: DATEV_ExportLine.DATEV_ExportLine_ID
+-- 2025-01-30T16:26:33.417Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:33.417','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562742
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Name
+-- Column: DATEV_ExportLine.BPName
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Name
+-- Column: DATEV_ExportLine.BPName
+-- 2025-01-30T16:26:35.047Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:35.047','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562751
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Kostenstelle
+-- Column: DATEV_ExportLine.C_Activity_ID
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Kostenstelle
+-- Column: DATEV_ExportLine.C_Activity_ID
+-- 2025-01-30T16:26:36.706Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:36.706','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562747
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> DATEV Export
+-- Column: DATEV_ExportLine.DATEV_Export_ID
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> DATEV Export
+-- Column: DATEV_ExportLine.DATEV_Export_ID
+-- 2025-01-30T16:26:38.358Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:38.358','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562743
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Nr.
+-- Column: DATEV_ExportLine.BPValue
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Nr.
+-- Column: DATEV_ExportLine.BPValue
+-- 2025-01-30T16:26:40.239Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:40.239','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562750
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Name der Belegart
+-- Column: DATEV_ExportLine.C_DocType_Name
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Name der Belegart
+-- Column: DATEV_ExportLine.C_DocType_Name
+-- 2025-01-30T16:26:41.910Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:41.91','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=707329
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Steuersatz
+-- Column: DATEV_ExportLine.C_Tax_Rate
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Steuersatz
+-- Column: DATEV_ExportLine.C_Tax_Rate
+-- 2025-01-30T16:26:43.575Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:43.575','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=707328
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Beschreibung
+-- Column: DATEV_ExportLine.Description
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Beschreibung
+-- Column: DATEV_ExportLine.Description
+-- 2025-01-30T16:26:45.248Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:45.248','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562788
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Datum Fälligkeit
+-- Column: DATEV_ExportLine.DueDate
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Datum Fälligkeit
+-- Column: DATEV_ExportLine.DueDate
+-- 2025-01-30T16:26:46.926Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:46.926','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562787
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Buchungsdatum
+-- Column: DATEV_ExportLine.DateAcct
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Buchungsdatum
+-- Column: DATEV_ExportLine.DateAcct
+-- 2025-01-30T16:26:48.610Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:48.61','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562749
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Activity Name
+-- Column: DATEV_ExportLine.ActivityName
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Activity Name
+-- Column: DATEV_ExportLine.ActivityName
+-- 2025-01-30T16:26:50.297Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:50.296','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562786
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Rechnung
+-- Column: DATEV_ExportLine.C_Invoice_ID
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Rechnung
+-- Column: DATEV_ExportLine.C_Invoice_ID
+-- 2025-01-30T16:26:51.966Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:51.966','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562753
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Geschäftspartner
+-- Column: DATEV_ExportLine.C_BPartner_ID
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Geschäftspartner
+-- Column: DATEV_ExportLine.C_BPartner_ID
+-- 2025-01-30T16:26:53.635Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:53.635','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562752
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Betrag
+-- Column: DATEV_ExportLine.Amt
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Betrag
+-- Column: DATEV_ExportLine.Amt
+-- 2025-01-30T16:26:55.315Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:55.315','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562746
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Credit Account
+-- Column: DATEV_ExportLine.CR_Account
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Credit Account
+-- Column: DATEV_ExportLine.CR_Account
+-- 2025-01-30T16:26:57.142Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:57.142','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562745
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Debit Account
+-- Column: DATEV_ExportLine.DR_Account
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Debit Account
+-- Column: DATEV_ExportLine.DR_Account
+-- 2025-01-30T16:26:58.827Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:26:58.827','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=562744
+;
+
+-- Field: Buchungen Export_OLD -> Lines -> Währung
+-- Column: DATEV_ExportLine.Currency
+-- Field: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> Währung
+-- Column: DATEV_ExportLine.Currency
+-- 2025-01-30T16:27:51.093Z
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-01-30 18:27:51.093','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Field_ID=691475
+;
+
+-- UI Element: Buchungen Export_OLD -> Lines.Aktiv
+-- Column: DATEV_ExportLine.IsActive
+-- UI Element: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> main -> 10 -> default.Aktiv
+-- Column: DATEV_ExportLine.IsActive
+-- 2025-01-30T16:28:58.680Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,562741,0,541037,541479,628431,'F',TO_TIMESTAMP('2025-01-30 18:28:57.816','YYYY-MM-DD HH24:MI:SS.US'),100,'Der Eintrag ist im System aktiv','Es gibt zwei Möglichkeiten, einen Datensatz nicht mehr verfügbar zu machen: einer ist, ihn zu löschen; der andere, ihn zu deaktivieren. Ein deaktivierter Eintrag ist nicht mehr für eine Auswahl verfügbar, aber verfügbar für die Verwendung in Berichten. Es gibt zwei Gründe, Datensätze zu deaktivieren und nicht zu löschen: (1) Das System braucht den Datensatz für Revisionszwecke. (2) Der Datensatz wird von anderen Datensätzen referenziert. Z.B. können Sie keinen Geschäftspartner löschen, wenn es Rechnungen für diesen Geschäftspartner gibt. Sie deaktivieren den Geschäftspartner und verhindern, dass dieser Eintrag in zukünftigen Vorgängen verwendet wird.','Y','N','Y','N','N','Aktiv',240,0,0,TO_TIMESTAMP('2025-01-30 18:28:57.816','YYYY-MM-DD HH24:MI:SS.US'),100)
+;
+
+-- UI Element: Buchungen Export_OLD -> Lines.Aktiv
+-- Column: DATEV_ExportLine.IsActive
+-- UI Element: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> main -> 10 -> default.Aktiv
+-- Column: DATEV_ExportLine.IsActive
+-- 2025-01-30T16:29:30.854Z
+UPDATE AD_UI_Element SET SeqNo=5,Updated=TO_TIMESTAMP('2025-01-30 18:29:30.854','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=628431
+;
+
+-- UI Element: Buchungen Export_OLD -> Lines.Aktiv
+-- Column: DATEV_ExportLine.IsActive
+-- UI Element: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> main -> 10 -> default.Aktiv
+-- Column: DATEV_ExportLine.IsActive
+-- 2025-01-30T16:29:48.592Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=10,Updated=TO_TIMESTAMP('2025-01-30 18:29:48.592','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=628431
+;
+
+-- UI Element: Buchungen Export_OLD -> Lines.Sektion
+-- Column: DATEV_ExportLine.AD_Org_ID
+-- UI Element: Buchungen Export_OLD(540413,de.metas.datev) -> Lines(541037,de.metas.datev) -> main -> 10 -> default.Sektion
+-- Column: DATEV_ExportLine.AD_Org_ID
+-- 2025-01-30T16:29:48.924Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=120,Updated=TO_TIMESTAMP('2025-01-30 18:29:48.924','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=551051
+;
+
