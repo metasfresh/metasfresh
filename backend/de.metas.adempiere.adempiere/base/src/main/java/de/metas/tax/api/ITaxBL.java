4832cb55f47b84a31740c9dd334904661d62af82
dm-ps <dragos.moca@pevesoft.ro>
2025-01-31 11:48:23 +0200

    after merge fixes

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/tax/api/ITaxBL.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/tax/api/ITaxBL.java
@@ -71,17 +71,6 @@ public interface ITaxBL extends ISingletonService
 			BPartnerLocationAndCaptureId shipBPartnerLocationId,
 			SOTrx soTrx,
 			@Nullable VatCodeId vatCodeId);
-
-	@NonNull
-	TaxId getTaxOrNoTaxId(
-			@Nullable Object model,
-			@Nullable TaxCategoryId taxCategoryId,
-			int productId,
-			Timestamp shipDate,
-			@NonNull OrgId orgId,
-			@Nullable WarehouseId warehouseId,
-			BPartnerLocationAndCaptureId shipBPartnerLocationId,
-			SOTrx soTrx);
 	
 	/**
 	 * @param taxIncluded if true tax is included in given amount
