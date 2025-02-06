4c0d5d8b0252b517fec90cdd7382d8fe8bb62011
Teodor Sarca <teo.sarca@metasfresh.com>
2025-02-06 15:23:29 +0200

    check_is_system_language_tgfn QA

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5746030_enforce_standard_languages_to_be_active.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5746030_enforce_standard_languages_to_be_active.sql
@@ -12,11 +12,11 @@ $$
 DECLARE
     supported_languages CONSTANT TEXT[] := ARRAY ['de_DE', 'de_CH', 'en_US'];
 BEGIN
-    
+
     -- Make sure nobody is deactivating one of the languages that we have support on each instance we have
     IF NEW.ad_language = ANY (supported_languages) THEN
-        IF NEW.issystemlanguage != 'Y' THEN
-            RAISE EXCEPTION 'IsSystemLanguage must be ''Y'' for ad_language = %', NEW.ad_language;
+        IF NEW.issystemlanguage != 'Y' AND NEW.isbaselanguage != 'Y' THEN
+            RAISE EXCEPTION 'IsSystemLanguage or IsBaseLanguage must be ''Y'' for ad_language = %', NEW.ad_language;
         END IF;
         IF NEW.isactive != 'Y' THEN
             RAISE EXCEPTION 'IsActive must be ''Y'' for ad_language = %', NEW.ad_language;
@@ -35,3 +35,4 @@ CREATE TRIGGER check_is_system_language_tg
     FOR EACH ROW
 EXECUTE PROCEDURE check_is_system_language_tgfn()
 ;
+
