df74bb260423945accbdfd6756cb5d5062efdd4e
dm-ps <83770997+dm-ps@users.noreply.github.com>
2025-02-06 20:44:06 +0200

    fix order callout (#20021)

--- backend/de.metas.business/src/main/java/de/metas/tax/api/impl/TaxBL.java
+++ backend/de.metas.business/src/main/java/de/metas/tax/api/impl/TaxBL.java
@@ -67,16 +67,24 @@ public class TaxBL implements de.metas.tax.api.ITaxBL
 	{
 		if (taxCategoryId != null)
 		{
-			final Optional<TaxId> taxId = getTaxId(taxCategoryId,
-					shipDate,
-					orgId,
-					warehouseId,
-					shipBPartnerLocationId,
-					soTrx);
+			final CountryId countryFromId = Optional.ofNullable(warehouseId)
+				.map(warehouseBL::getCountryId)
+				.orElseGet(() -> Optional.ofNullable(bPartnerOrgBL.getOrgCountryId(orgId))
+						.orElseGet(countryDAO::getDefaultCountryId));
 
-			if (taxId.isPresent())
+			final Tax tax = taxDAO.getBy(TaxQuery.builder()
+					.fromCountryId(countryFromId)
+					.orgId(orgId)
+					.bPartnerLocationId(shipBPartnerLocationId)
+					.dateOfInterest(shipDate)
+					.taxCategoryId(taxCategoryId)
+					.warehouseId(warehouseId)
+					.soTrx(soTrx)
+					.build());
+
+			if (tax != null)
 			{
-				return taxId.get();
+				return tax.getTaxId();
 			}
 		}
 
@@ -98,27 +106,6 @@ public class TaxBL implements de.metas.tax.api.ITaxBL
 		// If we got here, it means that no tax was found to satisfy the conditions
 		// In this case, the Tax_Not_Found placeholder will be returned
 		return TaxId.ofRepoId(Tax.C_TAX_ID_NO_TAX_FOUND);
-	}
-
-	@Override
-	@NonNull
-	public TaxId getTaxOrNoTaxId(
-			@Nullable final Object model,
-			@Nullable final TaxCategoryId taxCategoryId,
-			final int productId,
-			@NonNull final Timestamp shipDate,
-			@NonNull final OrgId orgId,
-			@Nullable final WarehouseId warehouseId,
-			@NonNull final BPartnerLocationAndCaptureId shipBPartnerLocationId,
-			@NonNull final SOTrx soTrx)
-	{
-		return getTaxId(taxCategoryId,
-				shipDate,
-				orgId,
-				warehouseId,
-				shipBPartnerLocationId,
-				soTrx)
-				.orElseGet(() -> TaxId.ofRepoId(Tax.C_TAX_ID_NO_TAX_FOUND));
 	}
 
 	public CalculateTaxResult calculateTax(final I_C_Tax tax, final BigDecimal amount, final boolean taxIncluded, final int scale)
@@ -183,31 +170,5 @@ public class TaxBL implements de.metas.tax.api.ITaxBL
 				.firstOnlyOptional(I_C_TaxCategory.class)
 				.map(I_C_TaxCategory::getC_TaxCategory_ID)
 				.map(TaxCategoryId::ofRepoId);
-	}
-
-	@NonNull
-	private Optional<TaxId> getTaxId(
-			@Nullable final TaxCategoryId taxCategoryId,
-			@NonNull final Timestamp shipDate,
-			@NonNull final OrgId orgId,
-			@Nullable final WarehouseId warehouseId,
-			@NonNull final BPartnerLocationAndCaptureId shipBPartnerLocationId,
-			@NonNull final SOTrx soTrx)
-	{
-		final CountryId countryFromId = Optional.ofNullable(warehouseId)
-				.map(warehouseBL::getCountryId)
-				.orElseGet(() -> Optional.ofNullable(bPartnerOrgBL.getOrgCountryId(orgId))
-						.orElseGet(countryDAO::getDefaultCountryId));
-
-		return Optional.ofNullable(taxDAO.getBy(TaxQuery.builder()
-						.fromCountryId(countryFromId)
-						.orgId(orgId)
-						.bPartnerLocationId(shipBPartnerLocationId)
-						.dateOfInterest(shipDate)
-						.taxCategoryId(taxCategoryId)
-						.warehouseId(warehouseId)
-						.soTrx(soTrx)
-						.build()))
-				.map(Tax::getTaxId);
 	}
 }
