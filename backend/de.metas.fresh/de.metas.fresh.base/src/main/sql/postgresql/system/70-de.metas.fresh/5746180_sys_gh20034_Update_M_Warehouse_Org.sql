72663b06b811368a84281ed956dbb74417c5b50a
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2025-02-11 07:32:33 +0100

    Update backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5746180_sys_gh20034_Update_M_Warehouse_Org.sql

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5746180_sys_gh20034_Update_M_Warehouse_Org.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5746180_sys_gh20034_Update_M_Warehouse_Org.sql
@@ -13,6 +13,6 @@ COMMENT ON CONSTRAINT M_Warehouse_AD_Org_Check ON M_Warehouse
     IS 'Ensures that AD_Org_ID cannot be set to 0 to prevent MD_Stock_Update_From_M_HUs process to fail.'
 ;
 
-UPDATE AD_Table SET TechnicalNote='M_Warehouse_AD_Org_Check constraint ensures that AD_Org_ID cannot be set to 0 to prevent MD_Stock_Update_From_M_HUs process to fail.',Updated=TO_TIMESTAMP('2025-02-10 08:44:20.430000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_ID=190
+UPDATE AD_Table SET TechnicalNote='An additional DB-contraint named M_Warehouse_AD_Org_Check constraint ensures that AD_Org_ID cannot be set to 0 to prevent MD_Stock_Update_From_M_HUs process to fail.',Updated=TO_TIMESTAMP('2025-02-10 08:44:20.430000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Table_ID=190
 ;
 
