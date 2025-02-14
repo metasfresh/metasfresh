8bf1ac0d7618bc2ea4561300030d2aa659a32896
riad.mennour <riad.mennour@gmail.com>
2025-02-14 15:17:51 +0100

    `Distribution Order` window update

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5746790_sys_gh20059_DD_Order_Window_Update.sql
@@ -0,0 +1,7 @@
+-- Run mode: SWING_CLIENT
+
+-- Column: DD_Order.M_Warehouse_ID
+-- 2025-02-14T14:16:09.629Z
+UPDATE AD_Column SET AD_Reference_ID=30, AD_Reference_Value_ID=540420, IsExcludeFromZoomTargets='Y',Updated=TO_TIMESTAMP('2025-02-14 14:16:09.629000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=53906
+;
+
