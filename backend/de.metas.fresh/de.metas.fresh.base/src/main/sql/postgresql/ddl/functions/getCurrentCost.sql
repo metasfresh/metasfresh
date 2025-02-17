ac17da609ecd35e6717e950ae3f9c902685a6a13
Teodor Sarca <teo.sarca@metasfresh.com>
2025-02-17 17:08:19 +0200

    update getCurrentCost.sql with the version that we currently have in database

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/getCurrentCost.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/getCurrentCost.sql
@@ -1,94 +1,234 @@
-DROP FUNCTION IF EXISTS getCurrentCost(numeric,
-                                       date,
-                                       numeric,
-                                       numeric,
-                                       numeric,
-                                       numeric)
+DROP FUNCTION IF EXISTS getCurrentCost(
+    p_m_product_id     numeric,
+    p_c_uom_id         numeric,
+    p_date             date,
+    p_acctschema_id    numeric,
+    p_m_costelement_id numeric,
+    p_ad_client_id     numeric,
+    p_ad_org_id        numeric
+)
+;
+
+DROP FUNCTION IF EXISTS getCurrentCostInfo(
+    p_m_product_id     numeric,
+    p_c_uom_id         numeric,
+    p_date             date,
+    p_acctschema_id    numeric,
+    p_m_costelement_id numeric,
+    p_ad_client_id     numeric,
+    p_ad_org_id        numeric
+)
+;
+
+DROP TYPE IF EXISTS currentcostprice
+;
+
+--
+--
+--
+--
+--
+
+CREATE TYPE CurrentCostPrice AS
+(
+    c_currency_id       numeric,
+    owncostprice        numeric,
+    componentscostprice numeric,
+    c_uom_id            numeric
+)
+;
+
+COMMENT ON TYPE CurrentCostPrice IS 'The result of some getCurrentCostInfo function.'
 ;
 
 
-
-DROP FUNCTION IF EXISTS getCurrentCost(numeric,
-                                       numeric,
-                                       date,
-                                       numeric,
-                                       numeric,
-                                       numeric)
-;
-
-CREATE OR REPLACE FUNCTION getCurrentCost(p_M_Product_ID     numeric,
-                                          p_C_UOM_ID         numeric,
-                                          p_Date             date,
-                                          p_AcctSchema_ID    numeric,
-                                          p_M_CostElement_Id numeric,
-                                          p_AD_Client_ID     numeric,
-                                          p_AD_Org_ID        numeric)
-    RETURNS numeric
-AS
-$BODY$
-WITH x AS
-         (
-             SELECT m_costdetail_ID,
-                    cd.prev_currentcostprice,
-                    cd.M_Product_ID,
-                    COALESCE(mi.dateAcct,
-                             mpo.dateAcct,
-                             pp.dateAcct,
-                             inv.movementDate,
-                             m.MovementDate,
-                             io.dateAcct,
-                             NULL) AS dateAcct
-
-             FROM m_costdetail cd
-
-                      JOIN AD_Org org ON cd.ad_org_ID = org.ad_org_id
-
-
-                      LEFT JOIN M_MatchInv mi ON mi.m_matchinv_id = cd.m_matchinv_id
-                      LEFT JOIN M_MatchPO mpo ON mpo.M_MatchPO_ID = cd.m_MatchPO_ID
-                      LEFT JOIN PP_Cost_Collector pp ON pp.PP_Cost_Collector_ID = cd.pp_cost_collector_id
-                      LEFT JOIN m_inventoryline il ON il.m_inventoryline_id = cd.m_inventoryline_id
-                      LEFT JOIN M_Inventory inv ON inv.m_inventory_id = il.m_inventory_id
-                      LEFT JOIN M_MovementLine ml ON ml.m_movementline_id = cd.m_movementline_id
-                      LEFT JOIN M_Movement m ON m.m_movement_id = ml.m_movement_ID
-                      LEFT JOIN m_inoutline iol ON iol.m_inoutline_id = cd.m_inoutline_id
-                      LEFT JOIN M_InOut io ON iol.M_InOut_ID = io.M_InOut_ID
-
-             WHERE cd.ischangingcosts = 'Y'
-               AND cd.c_acctschema_id = p_acctSchema_ID
-               AND cd.m_costelement_id = p_M_CostElement_Id
-               AND cd.M_Product_ID = p_M_Product_ID
-               AND cd.ad_client_id = p_AD_Client_ID
-               AND cd.ad_org_ID = p_AD_Org_ID
-         )
-
-
-SELECT priceuomconvert(p.M_Product_ID,
-                       COALESCE((
-                                    SELECT prev_currentcostprice
-                                    FROM x
-                                    WHERE dateAcct > p_Date
-                                    ORDER BY dateAcct
-                                    LIMIT 1), cost.currentCostPrice),
-                       p.c_uom_id,
-                       p_c_uom_id,
-                       currency.costingprecision:: integer)
-           AS currentCostPrice
-
-FROM M_Cost cost
-         JOIN M_Product p ON cost.M_Product_ID = p.M_Product_ID
-         JOIN C_AcctSchema sch ON cost.c_acctschema_id = sch.c_acctschema_id
-         JOIN C_Currency currency ON sch.c_currency_id = currency.c_currency_id
-
-
-WHERE cost.m_product_id = p_M_Product_ID
-  AND cost.c_acctschema_id = p_acctSchema_ID
-  AND cost.m_costelement_id = p_M_CostElement_Id
-  AND cost.ad_client_id = p_AD_Client_ID
-  AND cost.AD_Org_Id = p_AD_Org_ID;
-
-
-$BODY$
-    LANGUAGE SQL
+CREATE OR REPLACE FUNCTION getCurrentCostInfo(
+    p_m_product_id     numeric,
+    p_c_uom_id         numeric,
+    p_date             date,
+    p_acctschema_id    numeric,
+    p_m_costelement_id numeric,
+    p_ad_client_id     numeric,
+    p_ad_org_id        numeric
+)
+    RETURNS CurrentCostPrice
     STABLE
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    v_context             record;
+    v_ad_org_id_effective numeric;
+    v_costdetail          record;
+    v_cost                record;
+    --
+    v_result              CurrentCostPrice;
+BEGIN
+    RAISE NOTICE 'getCurrentCostInfo: p_M_Product_ID=%, p_C_UOM_ID=%, p_Date=%, p_AcctSchema_ID=%, p_M_CostElement_ID=%, p_AD_Client_ID=%, p_AD_Org_ID=%',
+        p_M_Product_ID, p_C_UOM_ID, p_Date, p_AcctSchema_ID, p_M_CostElement_Id, p_AD_Client_ID, p_AD_Org_ID;
+
+    --
+    -- Fetch context (i.e. costing level)
+    --
+    SELECT COALESCE(pca.costinglevel, cas.costinglevel) AS costinglevel
+    INTO v_context
+    FROM m_product p
+             INNER JOIN m_product_category_acct pca ON pca.m_product_category_id = p.m_product_category_id AND pca.c_acctschema_id = p_AcctSchema_ID
+             INNER JOIN c_acctschema cas ON cas.c_acctschema_id = pca.c_acctschema_id
+    WHERE p.m_product_id = p_M_Product_ID;
+    RAISE NOTICE 'Context: %', v_context;
+    --
+    IF (v_context IS NULL) THEN
+        RAISE WARNING 'No info found for M_Product_ID=%, C_AcctSchema_ID=%', p_M_Product_ID, p_AcctSchema_ID;
+        RETURN NULL;
+    END IF;
+
+    --
+    -- Calculate the effective dimensions based on costing level
+    --
+    IF (v_context.costinglevel = 'C') THEN
+        v_ad_org_id_effective := NULL;
+    ELSIF (v_context.costinglevel = 'O') THEN
+        v_ad_org_id_effective := (CASE WHEN p_AD_Org_ID > 0 THEN p_AD_Org_ID END);
+    ELSE
+        RAISE EXCEPTION 'Costing level not handled: %', v_context.costinglevel;
+    END IF;
+    RAISE NOTICE 'v_ad_org_id_effective=%', v_ad_org_id_effective;
+
+
+    --
+    -- Check cost details:
+    --
+    SELECT cd.m_costdetail_id,
+           cd.prev_currentcostprice,
+           cd.prev_currentcostpricell,
+           cd.M_Product_ID,
+           cd.c_uom_id,
+           cd.c_currency_id,
+           currency.costingprecision::integer AS costingprecision
+    INTO v_costdetail
+    FROM m_costdetail cd
+             INNER JOIN c_currency currency ON currency.c_currency_id = cd.c_currency_id
+    WHERE cd.ischangingcosts = 'Y'
+      AND cd.c_acctschema_id = p_acctSchema_ID
+      AND cd.m_costelement_id = p_M_CostElement_Id
+      AND cd.M_Product_ID = p_M_Product_ID
+      AND cd.ad_client_id = p_AD_Client_ID
+      AND (v_ad_org_id_effective IS NULL OR cd.ad_org_ID = v_ad_org_id_effective)
+      -- AND cd.m_attributesetinstance_id = any
+      AND cd.dateAcct > p_Date
+    ORDER BY cd.dateacct, cd.m_costdetail_id
+    LIMIT 1;
+
+    IF (v_costdetail IS NOT NULL) THEN
+        v_result.C_Currency_ID := v_costdetail.c_currency_id;
+
+        IF (p_C_UOM_ID IS NULL OR p_C_UOM_ID <= 0 OR v_costdetail.c_uom_id = p_C_UOM_ID) THEN
+            v_result.C_UOM_ID = v_costdetail.c_uom_id;
+            v_result.OwnCostPrice := v_costdetail.prev_currentcostprice;
+            v_result.ComponentsCostPrice := v_costdetail.prev_currentcostpricell;
+        ELSE
+            v_result.C_UOM_ID = p_C_UOM_ID;
+            v_result.OwnCostPrice := priceuomconvert(
+                    p_m_product_id := v_costdetail.m_product_id,
+                    p_price := v_costdetail.prev_currentcostprice,
+                    p_price_uom_id := v_costdetail.c_uom_id,
+                    p_c_uom_to_id := p_C_UOM_ID,
+                    p_priceprecision := v_costdetail.costingprecision);
+            v_result.ComponentsCostPrice := priceuomconvert(
+                    p_m_product_id := v_costdetail.m_product_id,
+                    p_price := v_costdetail.prev_currentcostpricell,
+                    p_price_uom_id := v_costdetail.c_uom_id,
+                    p_c_uom_to_id := p_C_UOM_ID,
+                    p_priceprecision := v_costdetail.costingprecision);
+        END IF;
+
+        RAISE NOTICE 'Calculated cost price from cost detail: % (CD=%, p_C_UOM_ID=%)', v_result, v_costdetail, p_C_UOM_ID;
+        RETURN v_result;
+    END IF;
+
+    --
+    -- Check current cost:
+    --
+    SELECT cost.m_product_id,
+           cost.currentcostprice,
+           cost.currentcostpricell,
+           cost.c_uom_id,
+           cost.c_currency_id,
+           currency.costingprecision::integer AS costingprecision
+    INTO v_cost
+    FROM m_cost cost
+             INNER JOIN c_currency currency ON currency.c_currency_id = cost.c_currency_id
+    WHERE cost.c_acctschema_id = p_acctSchema_ID
+      AND cost.m_costelement_id = p_M_CostElement_Id
+      AND cost.M_Product_ID = p_M_Product_ID
+      AND cost.ad_client_id = p_AD_Client_ID
+      AND (v_ad_org_id_effective IS NULL OR cost.ad_org_ID = v_ad_org_id_effective)
+      AND cost.m_attributesetinstance_id = 0;
+
+    IF (v_cost IS NOT NULL) THEN
+        v_result.C_Currency_ID := v_cost.c_currency_id;
+
+        IF (p_C_UOM_ID IS NULL OR p_C_UOM_ID <= 0 OR v_cost.c_uom_id = p_C_UOM_ID) THEN
+            v_result.C_UOM_ID := v_cost.c_uom_id;
+            v_result.OwnCostPrice := v_cost.currentcostprice;
+            v_result.ComponentsCostPrice := v_cost.currentcostpricell;
+        ELSE
+            v_result.C_UOM_ID := p_C_UOM_ID;
+            v_result.OwnCostPrice := priceuomconvert(
+                    p_m_product_id := v_cost.m_product_id,
+                    p_price := v_cost.currentcostprice,
+                    p_price_uom_id := v_cost.c_uom_id,
+                    p_c_uom_to_id := p_C_UOM_ID,
+                    p_priceprecision := v_cost.costingprecision);
+            v_result.ComponentsCostPrice := priceuomconvert(
+                    p_m_product_id := v_cost.m_product_id,
+                    p_price := v_cost.currentcostpricell,
+                    p_price_uom_id := v_cost.c_uom_id,
+                    p_c_uom_to_id := p_C_UOM_ID,
+                    p_priceprecision := v_cost.costingprecision);
+        END IF;
+
+        RAISE NOTICE 'Calculated cost price from M_Cost: % (C=%, p_C_UOM_ID=%)', v_result, v_cost, p_C_UOM_ID;
+        RETURN v_result;
+    END IF;
+
+    --
+    -- Nothing found, shall not happen
+    RAISE WARNING 'No cost price found. Returning null';
+    RETURN NULL;
+END
+$$
+;
+
+CREATE FUNCTION getCurrentCost(
+    p_m_product_id     numeric,
+    p_c_uom_id         numeric,
+    p_date             date,
+    p_acctschema_id    numeric,
+    p_m_costelement_id numeric,
+    p_ad_client_id     numeric,
+    p_ad_org_id        numeric
+)
+    RETURNS numeric
+    STABLE
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    v_costPriceInfo CurrentCostPrice;
+BEGIN
+    v_costPriceInfo := getCurrentCostInfo(
+            p_M_Product_ID := p_M_Product_ID,
+            p_C_UOM_ID := p_C_UOM_ID,
+            p_Date := p_Date,
+            p_AcctSchema_ID := p_AcctSchema_ID,
+            p_M_CostElement_Id := p_M_CostElement_Id,
+            p_AD_Client_ID := p_AD_Client_ID,
+            p_AD_Org_ID := p_AD_Org_ID
+                       );
+
+    RETURN v_costPriceInfo.OwnCostPrice;
+END;
+$$
 ;
