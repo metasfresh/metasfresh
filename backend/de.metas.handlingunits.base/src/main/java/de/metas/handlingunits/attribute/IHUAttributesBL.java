64e2a40425a68a5926b406af9db0d12dc5ee234c
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-02-19 23:58:46 +0200

    cleanup after merge

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/attribute/IHUAttributesBL.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/attribute/IHUAttributesBL.java
@@ -113,7 +113,4 @@ public interface IHUAttributesBL extends ISingletonService
 	void transferAttributesForSingleProductHUs(@NonNull I_M_HU huFrom, @NonNull I_M_HU huTo);
 
 	void updateHUAttribute(@NonNull HuId huId, @NonNull AttributeCode attributeCode, @Nullable Object attributeValue);
-
-	@Nullable
-	String getHUAttributeValue(@NonNull I_M_HU hu, @NonNull AttributeCode attributeCode);
 }
