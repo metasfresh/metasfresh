731b271d065f20a2d9cad11594e78dc2123ee94a
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-02-21 12:14:19 +0200

    Fix Docs_Purchase_Invoice_Points

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Purchase_Invoice_Points.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Purchase_Invoice_Points.sql
@@ -22,12 +22,12 @@ CREATE TABLE de_metas_endcustomer_fresh_reports.Docs_Purchase_Invoice_Points
 );
 
 
-CREATE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Invoice_Points(IN p_record_ID numeric,
+CREATE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Purchase_Invoice_Points(IN p_record_ID numeric,
                                                                                 IN p_AD_Language Character Varying(6))
     RETURNS SETOF de_metas_endcustomer_fresh_reports.Docs_Purchase_Invoice_Points AS
 $$
-select c.C_Invoice_id as C_Invoice_id,
-       c.c_invoiceline_id as C_InvoiceLine_id,
+select c.c_invoice_commission_id as C_Invoice_id,
+       c.c_invoiceline_commission_id as C_InvoiceLine_id,
        bp.value                            as bpValue,
        bp.Name                             as bpName,
        bp.C_Bpartner_ID,
@@ -44,14 +44,14 @@ select c.C_Invoice_id as C_Invoice_id,
        c.percentofbasepoints
 from C_Commission_Overview_V c
          join c_bpartner bp on bp.c_bpartner_id = c.Bill_Bpartner_ID
-         join c_invoiceline il on il.c_invoiceline_id = c.c_invoiceline_id
+         join c_invoiceline il on il.c_invoiceline_id = c.c_invoiceline_commission_id
          join m_product pil on pil.m_product_id = il.m_product_id
          join m_product_trl pilt on pilt.m_product_id = pil.m_product_id and pilt.ad_language = p_AD_Language
          left join M_Product p on c.M_Product_Order_ID = p.m_product_id
          left join m_product_trl pt on pt.m_product_id = p.m_product_id and pt.ad_language = p_AD_Language
          left join C_Uom u on c.C_Uom_id = u.C_Uom_id
          left join C_Uom_trl ut on ut.C_Uom_id = u.C_Uom_id and ut.ad_language = p_AD_Language
-where c.c_invoice_id = p_record_ID
+where c.c_invoice_commission_id = p_record_ID
 order by C_Bpartner_ID, productValue, productName;
 $$
     LANGUAGE sql
