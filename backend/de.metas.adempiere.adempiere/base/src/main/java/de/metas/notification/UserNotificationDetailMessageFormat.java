3d84d1e5b3b894564b821a32b1a3d096647d89a2
Adrian <adrian.stefan@metasfresh.com>
2025-03-06 13:03:15 +0200

    don't escape html symbols in notification text

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/notification/UserNotificationDetailMessageFormat.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/notification/UserNotificationDetailMessageFormat.java
@@ -20,12 +20,12 @@ import org.slf4j.Logger;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -34,9 +34,8 @@ import org.slf4j.Logger;
 
 /**
  * {@link UserNotification}'s message formatter.
- * 
- * @author metas-dev <dev@metasfresh.com>
  *
+ * @author metas-dev <dev@metasfresh.com>
  */
 final class UserNotificationDetailMessageFormat extends EventMessageFormatTemplate
 {
@@ -68,12 +67,12 @@ final class UserNotificationDetailMessageFormat extends EventMessageFormatTempla
 			return "<" + recordRef.getRecord_ID() + ">";
 		}
 
-		if(record == null)
+		if (record == null)
 		{
 			logger.info("Failed retrieving record for " + recordRef);
 			return "<" + recordRef.getRecord_ID() + ">";
 		}
-		
+
 		final String documentNo = Services.get(IDocumentBL.class).getDocumentNo(record);
 		return documentNo;
 	}
@@ -81,11 +80,6 @@ final class UserNotificationDetailMessageFormat extends EventMessageFormatTempla
 	@Override
 	protected String formatText(final String text)
 	{
-		if (text == null || text.isEmpty())
-		{
-			return "";
-		}
-
-		return text;
+		return text == null ? "" : text;
 	}
 }
