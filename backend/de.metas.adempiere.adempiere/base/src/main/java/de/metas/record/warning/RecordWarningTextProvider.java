metasfresh/metasfresh

commit fe31eb91f02755ffaaf34e26262ff1d52deddfa4

Send user notification when a business rule is violated, make the business rule message an AD_Message (#20079)
* Send UserNotification when a business rule is violated

* notification should take user to warning, not to record itself

* fix script name

* fix

* QA/prototype

* QA/prototype

* QA/prototype

* CR Fixes
Rework to consider target record to be the max seqNo one

* add missing SQLs

* add missing ad_messge

* CR Fixes

* CR Fixes

---------

Co-authored-by: Teodor Sarca <teo.sarca@metasfresh.com>

backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/record/warning/RecordWarningTextProvider.java
@@ -1,0 +1,48 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.record.warning;
+
+import com.google.common.base.Optional;
+import de.metas.notification.spi.IRecordTextProvider;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.adempiere.util.lang.ITableRecordReference;
+import org.compiere.model.I_AD_Record_Warning;
+
+@RequiredArgsConstructor
+public class RecordWarningTextProvider implements IRecordTextProvider
+{
+	@NonNull private final RecordWarningRepository recordWarningRepository;
+
+	@Override
+	public Optional<String> getTextMessageIfApplies(final ITableRecordReference referencedRecord)
+	{
+		if (I_AD_Record_Warning.Table_Name.equals(referencedRecord.getTableName()))
+		{
+			return Optional.absent();
+		}
+		final RecordWarningId recordWarningId = RecordWarningId.ofRepoId(referencedRecord.getRecord_ID());
+		
+		return Optional.of(recordWarningRepository.getById(recordWarningId).getMsgText());
+	}
+}

backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_AD_BusinessRule.java
@@ -234,23 +234,22 @@ public interface I_AD_BusinessRule
 	String COLUMNNAME_Validation_Rule_ID = "Validation_Rule_ID";
 
 	/**
-	 * Set Warning Message.
+	 * Set Warning Message ID.
 	 *
-	 * <br>Type: TextLong
+	 * <br>Type: Search
 	 * <br>Mandatory: false
 	 * <br>Virtual Column: false
 	 */
-	void setWarningMessage (@Nullable java.lang.String WarningMessage);
+	void setWarning_Message_ID (int Warning_Message_ID);
 
 	/**
-	 * Get Warning Message.
+	 * Get Warning Message ID.
 	 *
-	 * <br>Type: TextLong
+	 * <br>Type: Search
 	 * <br>Mandatory: false
 	 * <br>Virtual Column: false
 	 */
-	@Nullable java.lang.String getWarningMessage();
+	int getWarning_Message_ID();
 
-	ModelColumn<I_AD_BusinessRule, Object> COLUMN_WarningMessage = new ModelColumn<>(I_AD_BusinessRule.class, "WarningMessage", null);
-	String COLUMNNAME_WarningMessage = "WarningMessage";
+	String COLUMNNAME_Warning_Message_ID = "Warning_Message_ID";
 }

backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_AD_BusinessRule_Event.java
@@ -273,6 +273,26 @@ public interface I_AD_BusinessRule_Event
 	String COLUMNNAME_Source_Table_ID = "Source_Table_ID";
 
 	/**
+	 * Set Triggering User ID.
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setTriggering_User_ID (int Triggering_User_ID);
+
+	/**
+	 * Get Triggering User ID.
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getTriggering_User_ID();
+
+	String COLUMNNAME_Triggering_User_ID = "Triggering_User_ID";
+
+	/**
 	 * Get Updated.
 	 * Date this record was updated
 	 *

backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_AD_Record_Warning.java
@@ -171,20 +171,20 @@ public interface I_AD_Record_Warning
 	 * Textual Informational, Menu or Error Message
 	 *
 	 * <br>Type: TextLong
-	 * <br>Mandatory: false
+	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
-	void setMsgText (@Nullable java.lang.String MsgText);
+	void setMsgText (java.lang.String MsgText);
 
 	/**
 	 * Get Message Text.
 	 * Textual Informational, Menu or Error Message
 	 *
 	 * <br>Type: TextLong
-	 * <br>Mandatory: false
+	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
-	@Nullable java.lang.String getMsgText();
+	java.lang.String getMsgText();
 
 	ModelColumn<I_AD_Record_Warning, Object> COLUMN_MsgText = new ModelColumn<>(I_AD_Record_Warning.class, "MsgText", null);
 	String COLUMNNAME_MsgText = "MsgText";
@@ -193,7 +193,7 @@ public interface I_AD_Record_Warning
 	 * Set Record ID.
 	 * Direct internal record ID
 	 *
-	 * <br>Type: ID
+	 * <br>Type: Button
 	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
@@ -203,7 +203,7 @@ public interface I_AD_Record_Warning
 	 * Get Record ID.
 	 * Direct internal record ID
 	 *
-	 * <br>Type: ID
+	 * <br>Type: Button
 	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */

backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_BusinessRule.java
@@ -12,7 +12,7 @@ import javax.annotation.Nullable;
 public class X_AD_BusinessRule extends org.compiere.model.PO implements I_AD_BusinessRule, org.compiere.model.I_Persistent 
 {
 
-	private static final long serialVersionUID = 472130672L;
+	private static final long serialVersionUID = -295086656L;
 
     /** Standard Constructor */
     public X_AD_BusinessRule (final Properties ctx, final int AD_BusinessRule_ID, @Nullable final String trxName)
@@ -116,14 +116,17 @@ public class X_AD_BusinessRule extends org.compiere.model.PO implements I_AD_Bus
 	}
 
 	@Override
-	public void setWarningMessage (final @Nullable java.lang.String WarningMessage)
+	public void setWarning_Message_ID (final int Warning_Message_ID)
 	{
-		set_Value (COLUMNNAME_WarningMessage, WarningMessage);
+		if (Warning_Message_ID < 1) 
+			set_Value (COLUMNNAME_Warning_Message_ID, null);
+		else 
+			set_Value (COLUMNNAME_Warning_Message_ID, Warning_Message_ID);
 	}
 
 	@Override
-	public java.lang.String getWarningMessage() 
+	public int getWarning_Message_ID() 
 	{
-		return get_ValueAsString(COLUMNNAME_WarningMessage);
+		return get_ValueAsInt(COLUMNNAME_Warning_Message_ID);
 	}
 }

backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_BusinessRule_Event.java
@@ -12,7 +12,7 @@ import javax.annotation.Nullable;
 public class X_AD_BusinessRule_Event extends org.compiere.model.PO implements I_AD_BusinessRule_Event, org.compiere.model.I_Persistent 
 {
 
-	private static final long serialVersionUID = -1166482534L;
+	private static final long serialVersionUID = -1371014578L;
 
     /** Standard Constructor */
     public X_AD_BusinessRule_Event (final Properties ctx, final int AD_BusinessRule_Event_ID, @Nullable final String trxName)
@@ -170,5 +170,20 @@ public class X_AD_BusinessRule_Event extends org.compiere.model.PO implements I_
 	public int getSource_Table_ID() 
 	{
 		return get_ValueAsInt(COLUMNNAME_Source_Table_ID);
+	}
+
+	@Override
+	public void setTriggering_User_ID (final int Triggering_User_ID)
+	{
+		if (Triggering_User_ID < 1) 
+			set_Value (COLUMNNAME_Triggering_User_ID, null);
+		else 
+			set_Value (COLUMNNAME_Triggering_User_ID, Triggering_User_ID);
+	}
+
+	@Override
+	public int getTriggering_User_ID() 
+	{
+		return get_ValueAsInt(COLUMNNAME_Triggering_User_ID);
 	}
 }

backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_AD_Record_Warning.java
@@ -12,7 +12,7 @@ import javax.annotation.Nullable;
 public class X_AD_Record_Warning extends org.compiere.model.PO implements I_AD_Record_Warning, org.compiere.model.I_Persistent 
 {
 
-	private static final long serialVersionUID = -1171640052L;
+	private static final long serialVersionUID = 1004276011L;
 
     /** Standard Constructor */
     public X_AD_Record_Warning (final Properties ctx, final int AD_Record_Warning_ID, @Nullable final String trxName)
@@ -92,7 +92,7 @@ public class X_AD_Record_Warning extends org.compiere.model.PO implements I_AD_R
 	}
 
 	@Override
-	public void setMsgText (final @Nullable java.lang.String MsgText)
+	public void setMsgText (final java.lang.String MsgText)
 	{
 		set_Value (COLUMNNAME_MsgText, MsgText);
 	}

backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/business_rule/descriptor/BusinessRuleLoader.java
@@ -13,6 +13,7 @@ import de.metas.business_rule.descriptor.model.TriggerTiming;
 import de.metas.business_rule.descriptor.model.Validation;
 import de.metas.business_rule.descriptor.model.ValidationType;
 import de.metas.business_rule.log.BusinessRuleLogLevel;
+import de.metas.i18n.AdMessageId;
 import de.metas.util.GuavaCollectors;
 import de.metas.util.StringUtils;
 import lombok.Builder;
@@ -136,7 +137,7 @@ class BusinessRuleLoader
 				.triggers(getTriggers(id).stream()
 						.map(BusinessRuleLoader::fromRecord)
 						.collect(ImmutableList.toImmutableList()))
-				.warningMessage(record.getWarningMessage())
+				.warningMessageId(AdMessageId.ofRepoId(record.getWarning_Message_ID()))
 				.logLevel(record.isDebug() ? BusinessRuleLogLevel.DEBUG : null)
 				.build();
 	}

commit f798d7370e3c6cf76ba8a03b9f7471c327158886

fix typo bug/NPE introduced by https://github.com/metasfresh/metasfresh/pull/20079

backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/record/warning/RecordWarningTextProvider.java
@@ -37,12 +37,12 @@ public class RecordWarningTextProvider implements IRecordTextProvider
 	@Override
 	public Optional<String> getTextMessageIfApplies(final ITableRecordReference referencedRecord)
 	{
-		if (I_AD_Record_Warning.Table_Name.equals(referencedRecord.getTableName()))
+		if (!I_AD_Record_Warning.Table_Name.equals(referencedRecord.getTableName()))
 		{
 			return Optional.absent();
 		}
-		final RecordWarningId recordWarningId = RecordWarningId.ofRepoId(referencedRecord.getRecord_ID());
 		
+		final RecordWarningId recordWarningId = RecordWarningId.ofRepoId(referencedRecord.getRecord_ID());
 		return Optional.of(recordWarningRepository.getById(recordWarningId).getMsgText());
 	}
 }