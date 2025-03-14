94cbc050b45566d8172abda73d5178688da52ff7
Riad MENNOUR <93593841+riadmen@users.noreply.github.com>
2025-03-14 19:26:53 +0100

    New `Commission Forecast` Report (#20364)

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5749270_sys_gh20325_Commission_Forecast_Process.sql
+++
@@ -1,6 +0,0 @@
--- Value: Commission Forecast (Excel)
--- Classname: de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess
--- 2025-03-14T14:06:26.275Z
-UPDATE AD_Process SET SQLStatement='select * from de_metas_endcustomer_fresh_reports.Docs_Purchase_Commission_Forecast(@C_BPartner_SalesRep_ID/NULL@,@CommissionDate_From/NULL@::date,@CommissionDate_To/NULL@::date, ''@#AD_Language@'')',Updated=TO_TIMESTAMP('2025-03-14 15:06:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=585456
-;
-
