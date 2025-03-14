07afa84d6e1a55ed1aa50df6b294c68c8c6a0600
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2025-03-14 08:44:43 +0100

    Update 5749150_sys_C_Invoice_Detail_add_perf_idx.sql

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5749150_sys_C_Invoice_Detail_add_perf_idx.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5749150_sys_C_Invoice_Detail_add_perf_idx.sql
@@ -1,5 +1,5 @@
 
--- this could be a common index
+-- useful in jasper when we have to display details that are not assigned to a certain invoice-line
 CREATE INDEX IF NOT EXISTS c_invoice_detail_c_invoice_id ON c_invoice_detail (c_invoice_id)
 ;
 
