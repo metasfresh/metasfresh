a7a5cc83fcd2503a39055e614d87b791098c306f
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-03-17 17:53:38 +0200

    Create Receipts with optional accounting date

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5749440_sys_CreateReceipt_AcctDate_NotMandatory.sql
@@ -0,0 +1,8 @@
+-- Run mode: SWING_CLIENT
+
+-- Process: WEBUI_M_HU_CreateReceipt_MovementAndAcctDateParams(de.metas.ui.web.handlingunits.process.WEBUI_M_HU_CreateReceipt_MovementAndAcctDateParams)
+-- ParameterName: DateAcct
+-- 2025-03-17T15:09:22.503Z
+UPDATE AD_Process_Para SET DefaultValue='', IsMandatory='N',Updated=TO_TIMESTAMP('2025-03-17 17:09:22.503','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Process_Para_ID=542819
+;
+
