89c11c6d879ef7c3fc77bf87e92dfb67b29c09c2
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-03-20 13:08:51 +0200

    Fix Typo

--- backend/de.metas.business/src/main/java/de/metas/product/impexp/MProductImportTableSqlUpdater.java
+++ backend/de.metas.business/src/main/java/de/metas/product/impexp/MProductImportTableSqlUpdater.java
@@ -343,7 +343,7 @@ public class MProductImportTableSqlUpdater
 		sql = new StringBuilder("UPDATE ")
 				.append(targetTableName + " i ")
 				.append(" SET QtyCU_UOM_ID = (SELECT C_UOM_ID FROM C_UOM u WHERE lower(u.uomsymbol)=lower(i.QtyCU_UOM_Code) AND u.AD_Client_ID IN (0,i.AD_Client_ID) AND u.IsActive='Y' ORDER BY u.AD_Client_ID DESC, u.C_UOM_ID ASC LIMIT 1) ")
-				.append("WHERE Package_UOM_ID IS NULL")
+				.append("WHERE QtyCU_UOM_ID IS NULL")
 				.append(" AND " + COLUMNNAME_I_IsImported + "<>'Y'")
 				.append(selection.toSqlWhereClause("i"));
 		no = DB.executeUpdateAndThrowExceptionOnFail(sql.toString(), ITrx.TRXNAME_ThreadInherited);
