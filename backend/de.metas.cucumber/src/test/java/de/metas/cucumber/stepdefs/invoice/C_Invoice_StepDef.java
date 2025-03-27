6bece7f5d1829b72166d03811944a0d6e1ae9f3f
Teodor Sarca <teo.sarca@metasfresh.com>
2025-03-27 18:22:11 +0200

    QA

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/invoice/C_Invoice_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/invoice/C_Invoice_StepDef.java
@@ -491,18 +491,10 @@ public class C_Invoice_StepDef
 					.ifPresent(invoiceIsPartiallyPaid -> softly.assertThat(invoice.isPartiallyPaid()).as("IsPartiallyPaid").isEqualTo(invoiceIsPartiallyPaid));
 			row.getAsOptionalBigDecimal("OpenAmt")
 					.ifPresent(expectedOpenAmt -> {
-						softly.assertThat(invoice.getOpenAmt()).as("C_Invoice.OpenAmt").isEqualByComparingTo(expectedOpenAmt);
+						softly.assertThat(invoice.getOpenAmt()).as("OpenAmt not matching C_Invoice.OpenAmt").isEqualByComparingTo(expectedOpenAmt);
 
-						final List<InvoiceToAllocate> invoiceToAllocates = paymentAllocationRepository
-								.retrieveInvoicesToAllocate(InvoiceToAllocateQuery.builder()
-										.evaluationDate(ZonedDateTime.now())
-										.onlyInvoiceId(InvoiceId.ofRepoId(invoice.getC_Invoice_ID()))
-										.build());
-						final InvoiceToAllocate invoiceToAllocate = invoiceToAllocates.isEmpty() ? null : invoiceToAllocates.get(0);
-						final BigDecimal actualOpenAmountConverted = invoiceToAllocate != null ? invoiceToAllocate.getOpenAmountConverted().getAsBigDecimal() : BigDecimal.ZERO;
-
-						final BigDecimal expectedOpenAmtConverted = (invoiceToAllocate != null && invoiceToAllocate.getDocBaseType().isCreditMemo()) ? expectedOpenAmt.negate() : expectedOpenAmt;
-						softly.assertThat(actualOpenAmountConverted).as("OpenAmountConverted").isEqualByComparingTo(expectedOpenAmtConverted);
+						final BigDecimal computedOpenAmt = computeOpenAmount(InvoiceId.ofRepoId(invoice.getC_Invoice_ID()));
+						softly.assertThat(computedOpenAmt).as("OpenAmt not matching computed value").isEqualByComparingTo(expectedOpenAmt);
 					});
 		}
 
@@ -544,6 +536,27 @@ public class C_Invoice_StepDef
 		}
 
 		softly.assertAll();
+	}
+
+	private BigDecimal computeOpenAmount(@NonNull final InvoiceId invoiceId)
+	{
+		final List<InvoiceToAllocate> invoiceToAllocates = paymentAllocationRepository
+				.retrieveInvoicesToAllocate(InvoiceToAllocateQuery.builder()
+						.evaluationDate(ZonedDateTime.now())
+						.onlyInvoiceId(invoiceId)
+						.build());
+
+		if (invoiceToAllocates.isEmpty())
+		{
+			return BigDecimal.ZERO;
+		}
+
+		final InvoiceToAllocate invoiceToAllocate = invoiceToAllocates.get(0);
+		return invoiceToAllocate.getOpenAmountConverted()
+				// because retrieved amount is already CM adjusted, we are "canceling" this adjustment here,
+				// because that's how the amount is expected to be returned
+				.negateIf(invoiceToAllocate.getDocBaseType().isCreditMemo())
+				.toBigDecimal();
 	}
 
 	public ProviderResult<List<I_C_Invoice>> loadInvoice(@NonNull final Map<String, String> row)
@@ -594,7 +607,7 @@ public class C_Invoice_StepDef
 
 		assertThat(invoices).isNotEmpty();
 		assertThat(invoices).hasSameSizeAs(invoiceIdentifiers);
-		
+
 		for (int invoiceIndex = 0; invoiceIndex < invoices.size(); invoiceIndex++)
 		{
 			invoiceTable.putOrReplace(invoiceIdentifiers.get(invoiceIndex), invoices.get(invoiceIndex));
