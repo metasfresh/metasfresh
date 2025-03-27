ad14e6d0590bf6112426ff19e2a1a99b6a705bb9
Adrian <adrian.stefan@metasfresh.com>
2025-03-27 12:33:28 +0200

    Fixed logic in invoicing of grouped compensation ICs, which is now decoupled from `isAllowSeparateInvoicing` ICs

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/de_metas_invoicecandidate/views/C_Invoice_Candidate_SelectionIncompleteGroups.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/ddl/de_metas_invoicecandidate/views/C_Invoice_Candidate_SelectionIncompleteGroups.sql
@@ -1,17 +1,19 @@
-drop view if exists C_Invoice_Candidate_SelectionIncompleteGroups;
-create or replace view C_Invoice_Candidate_SelectionIncompleteGroups as
-select distinct
-	s.AD_PInstance_ID,
-	ic.C_Order_ID,
-	o.DocumentNo as OrderDocumentNo,
-	ic.C_Order_CompensationGroup_ID
-from T_Selection s
-inner join C_Invoice_Candidate ic on (ic.C_Invoice_Candidate_ID=s.T_Selection_ID)
-inner join C_Invoice_Candidate ic2 on (ic2.C_Order_ID=ic.C_Order_ID and ic2.C_Order_CompensationGroup_ID=ic.C_Order_CompensationGroup_ID)
-inner join C_Order o on (o.C_Order_ID=ic.C_Order_ID)
-where true
-and ic.C_Order_CompensationGroup_ID is not null and ic.isAllowSeparateInvoicing='N'
-and not exists (select 1 from T_Selection s2 where s2.AD_PInstance_ID=s.AD_PInstance_ID and s2.T_Selection_ID=ic2.C_Invoice_Candidate_ID)
+DROP VIEW IF EXISTS C_Invoice_Candidate_SelectionIncompleteGroups
+;
+
+CREATE OR REPLACE VIEW C_Invoice_Candidate_SelectionIncompleteGroups AS
+SELECT DISTINCT s.AD_PInstance_ID,
+                ic.C_Order_ID,
+                o.DocumentNo AS OrderDocumentNo,
+                ic.C_Order_CompensationGroup_ID
+FROM T_Selection s
+         INNER JOIN C_Invoice_Candidate ic ON (ic.C_Invoice_Candidate_ID = s.T_Selection_ID)
+         INNER JOIN C_Invoice_Candidate ic2 ON (ic2.C_Order_ID = ic.C_Order_ID AND ic2.C_Order_CompensationGroup_ID = ic.C_Order_CompensationGroup_ID AND ic2.isAllowSeparateInvoicing = 'N')
+         INNER JOIN C_Order o ON (o.C_Order_ID = ic.C_Order_ID)
+WHERE TRUE
+  AND ic.C_Order_CompensationGroup_ID IS NOT NULL
+  AND ic.isAllowSeparateInvoicing = 'N'
+  AND NOT EXISTS (SELECT 1 FROM T_Selection s2 WHERE s2.AD_PInstance_ID = s.AD_PInstance_ID AND s2.T_Selection_ID = ic2.C_Invoice_Candidate_ID)
 ;
 
 -- select * from C_Invoice_Candidate_SelectionIncompleteGroups;
