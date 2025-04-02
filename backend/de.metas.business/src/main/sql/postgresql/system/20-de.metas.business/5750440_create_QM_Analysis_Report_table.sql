e73be5a51b01e166e1f9688c4d6da2663181f719
Adrian <adrian.stefan@metasfresh.com>
2025-04-02 10:48:38 +0300

    Implement QM_Analysis_Report table, window, docbasetype & doctype

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5750440_create_QM_Analysis_Report_table.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5750440_create_QM_Analysis_Report_table.sql
@@ -1028,3 +1028,28 @@ INSERT INTO t_alter_column values('qm_analysis_report','DocAction','CHAR(2)',nul
 -- 2025-04-01T11:56:42.224Z
 UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-04-01 11:56:42.224000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Field_ID=741853
 ;
+
+-- UI Element: Analysebericht(541876,D) -> QM_Analysis_Report(547945,D) -> main -> 20 -> org.Sektion
+-- Column: QM_Analysis_Report.AD_Org_ID
+-- 2025-04-02T07:46:33.128Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,741836,0,547945,552719,631297,'F',TO_TIMESTAMP('2025-04-02 07:46:32.761000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Organisatorische Einheit des Mandanten','Eine Organisation ist ein Bereich ihres Mandanten - z.B. Laden oder Abteilung. Sie können Daten über Organisationen hinweg gemeinsam verwenden.','Y','N','N','Y','N','N','N',0,'Sektion',10,0,0,TO_TIMESTAMP('2025-04-02 07:46:32.761000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- UI Element: Analysebericht(541876,D) -> QM_Analysis_Report(547945,D) -> main -> 20 -> other.Belegdatum
+-- Column: QM_Analysis_Report.DateDoc
+-- 2025-04-02T07:47:03.930Z
+DELETE FROM AD_UI_Element WHERE AD_UI_Element_ID=631290
+;
+
+-- UI Element: Analysebericht(541876,D) -> QM_Analysis_Report(547945,D) -> main -> 10 -> other.Belegdatum
+-- Column: QM_Analysis_Report.DateDoc
+-- 2025-04-02T07:47:28.373Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,Help,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,741846,0,547945,552720,631298,'F',TO_TIMESTAMP('2025-04-02 07:47:28.242000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Datum des Belegs','The Document Date indicates the date the document was generated.  It may or may not be the same as the accounting date.','Y','N','N','Y','N','N','N',0,'Belegdatum',10,0,0,TO_TIMESTAMP('2025-04-02 07:47:28.242000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- UI Element: Analysebericht(541876,D) -> QM_Analysis_Report(547945,D) -> main -> 10 -> other.Beschreibung
+-- Column: QM_Analysis_Report.Description
+-- 2025-04-02T07:47:38.498Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,741849,0,547945,552720,631299,'F',TO_TIMESTAMP('2025-04-02 07:47:38.343000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','N','Y','N','N','N',0,'Beschreibung',20,0,0,TO_TIMESTAMP('2025-04-02 07:47:38.343000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
