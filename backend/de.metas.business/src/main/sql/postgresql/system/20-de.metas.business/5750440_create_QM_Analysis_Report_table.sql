2d740b9590c829a56580f3e8d83b3ffd6d60a05f
Adrian <adrian.stefan@metasfresh.com>
2025-04-02 14:07:31 +0300

    Implement QM_Analysis_Report table, window, docbasetype & doctype

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5750440_create_QM_Analysis_Report_table.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5750440_create_QM_Analysis_Report_table.sql
@@ -1053,3 +1053,24 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,741849,0,547945,552720,631299,'F',TO_TIMESTAMP('2025-04-02 07:47:38.343000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','N','Y','N','N','N',0,'Beschreibung',20,0,0,TO_TIMESTAMP('2025-04-02 07:47:38.343000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
+-- Name: C_DocType QMA
+-- 2025-04-02T10:59:31.233Z
+INSERT INTO AD_Reference (AD_Client_ID,AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,IsOrderByValue,Name,Updated,UpdatedBy,ValidationType) VALUES (0,0,541933,TO_TIMESTAMP('2025-04-02 10:59:31.014000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','N','C_DocType QMA',TO_TIMESTAMP('2025-04-02 10:59:31.014000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'T')
+;
+
+-- 2025-04-02T10:59:31.235Z
+INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Reference_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Reference t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Reference_ID=541933 AND NOT EXISTS (SELECT 1 FROM AD_Reference_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Reference_ID=t.AD_Reference_ID)
+;
+
+-- Reference: C_DocType QMA
+-- Table: C_DocType
+-- Key: C_DocType.C_DocType_ID
+-- 2025-04-02T11:00:26.107Z
+INSERT INTO AD_Ref_Table (AD_Client_ID,AD_Display,AD_Key,AD_Org_ID,AD_Reference_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,IsValueDisplayed,ShowInactiveValues,Updated,UpdatedBy,WhereClause) VALUES (0,1509,1501,0,541933,217,TO_TIMESTAMP('2025-04-02 11:00:26.097000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','N','N',TO_TIMESTAMP('2025-04-02 11:00:26.097000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'C_DocType.DocBaseType = ''QMA'' AND C_DocType.AD_Client_ID=@#AD_Client_ID@')
+;
+
+-- Column: QM_Analysis_Report.C_DocType_ID
+-- 2025-04-02T11:00:43.905Z
+UPDATE AD_Column SET AD_Reference_Value_ID=541933,Updated=TO_TIMESTAMP('2025-04-02 11:00:43.905000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=589815
+;
+
