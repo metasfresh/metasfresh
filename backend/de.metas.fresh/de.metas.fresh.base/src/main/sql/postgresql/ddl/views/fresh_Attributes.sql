5e0c17b2fc667e57dcfd917edf174b304c5b1de1
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-04-04 14:59:26 +0300

    Exclude null cases in fresh_attributes

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/views/fresh_Attributes.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/views/fresh_Attributes.sql
@@ -18,7 +18,11 @@ FROM (
                                                                                                           THEN (SELECT mt.lot
                                                                                                                 FROM m_material_tracking mt
                                                                                                                 WHERE mt.m_material_tracking_id = ai.value::numeric)
-                                                                                                          ELSE coalesce( nullif(trim(printvalue_override), ''), av.name)::varchar -- default
+                    ELSE (
+                        CASE
+                            WHEN av.name IS NOT NULL AND av.IsNullFieldValue = 'N'
+                                THEN COALESCE(NULLIF(TRIM(printvalue_override), ''), av.name)::varchar
+                        END)
                 END                      AS ai_Value,
                 M_AttributeSetInstance_ID,
                 a.Value                  AS at_Value,
