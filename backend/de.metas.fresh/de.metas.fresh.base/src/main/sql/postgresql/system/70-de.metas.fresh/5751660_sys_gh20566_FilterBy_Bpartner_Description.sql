a99788e0fe642ab1be9e7079a59481883f6b688a
riad.mennour <riad.mennour@gmail.com>
2025-04-09 16:02:25 +0100

    Search by `BPartner Description`

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5751660_sys_gh20566_FilterBy_Bpartner_Description.sql
@@ -0,0 +1,9 @@
+-- Reference: C_BPartner (Trx)
+-- Table: C_BPartner
+-- Key: C_BPartner.C_BPartner_ID
+-- 2025-04-09T14:40:09.101Z
+UPDATE AD_Ref_Table SET AD_Display=NULL,Updated=TO_TIMESTAMP('2025-04-09 15:40:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=138
+;
+
+-------------------
+
