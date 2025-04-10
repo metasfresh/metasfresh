95533ca3c2e593640486d4b292645765727a346a
riad.mennour <riad.mennour@gmail.com>
2025-04-10 10:45:56 +0100

    Make `Auftragsposition` read only in `Lieferung` window

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5751720_sys_gh20571_Update_Shipment_Window.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5751720_sys_gh20571_Update_Shipment_Window.sql
@@ -9,5 +9,5 @@ UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2025-04-10 08:34:19.793
 -- Column: M_InOutLine.C_OrderLine_ID
 -- Column: M_InOutLine.C_OrderLine_ID
 -- 2025-04-10T09:39:34.294Z
-UPDATE AD_Column SET ReadOnlyLogic='1=2',Updated=TO_TIMESTAMP('2025-04-10 09:39:34.294000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=3811
+UPDATE AD_Column SET ReadOnlyLogic='1=1',Updated=TO_TIMESTAMP('2025-04-10 09:39:34.294000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=3811
 ;
