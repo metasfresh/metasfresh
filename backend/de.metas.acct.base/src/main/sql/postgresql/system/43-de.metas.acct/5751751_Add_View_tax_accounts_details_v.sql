964d86382f698f8e67220b6fcfb7cc4a610eaa45
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-04-11 13:31:06 +0300

    Take out unneeded c_period_id

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5751751_Add_View_tax_accounts_details_v.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5751751_Add_View_tax_accounts_details_v.sql
@@ -21,6 +21,29 @@
  */
 DROP VIEW IF EXISTS de_metas_acct.tax_accounts_details_v
 ;
+/*
+ * #%L
+ * de.metas.acct.base
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+-- DROP VIEW IF EXISTS de_metas_acct.tax_accounts_details_v
+-- ;
 
 CREATE OR REPLACE VIEW de_metas_acct.tax_accounts_details_v
 AS
@@ -47,7 +70,6 @@ SELECT fa.vatcode,
        fa.accountconceptualname,
        fa.c_currency_id,
        fa.source_currency_id,
-       fa.c_period_ID,
        fa.ad_table_id,
        fa.record_id,
        fa.ad_org_id
@@ -93,7 +115,6 @@ FROM (SELECT fa.vatcode                        AS vatcode,
              fa.accountconceptualname,
              fa.c_currency_id                  AS source_currency_id,
              cr.c_currency_id,
-             fa.c_period_ID,
              fa.ad_table_id,
              fa.record_id,
              fa.ad_org_id,
