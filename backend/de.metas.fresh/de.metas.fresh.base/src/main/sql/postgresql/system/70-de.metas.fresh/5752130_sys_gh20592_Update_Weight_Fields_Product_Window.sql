ffc57868886f122904338a4fa45b58c854c68213
riad.mennour <riad.mennour@gmail.com>
2025-04-16 09:41:41 +0100

    Rename `GrossWeight_UOM_ID` and adding weight validation rule

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5752130_sys_gh20592_Update_Weight_Fields_Product_Window.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5752130_sys_gh20592_Update_Weight_Fields_Product_Window.sql
@@ -3,6 +3,11 @@
 INSERT INTO AD_Val_Rule (AD_Client_ID,AD_Org_ID,AD_Val_Rule_ID,Code,Created,CreatedBy,EntityType,IsActive,Name,Type,Updated,UpdatedBy) VALUES (0,0,540706,'C_UOM.UOMType=''WE''',TO_TIMESTAMP('2025-04-16 07:11:58.478000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','C_UOM_Weigth','S',TO_TIMESTAMP('2025-04-16 07:11:58.478000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
+-- Name: C_UOM_Weight
+-- 2025-04-16T08:40:13.181Z
+UPDATE AD_Val_Rule SET Name='C_UOM_Weight',Updated=TO_TIMESTAMP('2025-04-16 08:40:12.957000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Val_Rule_ID=540706
+;
+
 -- Column: M_Product.GrossWeight_UOM_ID
 -- Column: M_Product.GrossWeight_UOM_ID
 -- 2025-04-16T07:13:39.838Z
