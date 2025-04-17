c598f47d9fcd63b94c9609f9cbe8c382d35db8e0
Adrian <60300682+adi-stefan@users.noreply.github.com>
2025-04-17 10:44:35 +0300

    Log DHL request/response as JSON. Added extra filters to the Shipment Order Log window. (#20645)

--- backend/de.metas.shipper.gateway.dhl/src/main/java/de/metas/shipper/gateway/dhl/DhlConstants.java
+++ backend/de.metas.shipper.gateway.dhl/src/main/java/de/metas/shipper/gateway/dhl/DhlConstants.java
@@ -22,8 +22,10 @@
 
 package de.metas.shipper.gateway.dhl;
 
+import lombok.experimental.UtilityClass;
 import org.compiere.model.X_M_Shipper;
 
+@UtilityClass
 public class DhlConstants
 {
 	public static final String SHIPPER_GATEWAY_ID = X_M_Shipper.SHIPPERGATEWAY_DHL;
@@ -34,4 +36,5 @@ public class DhlConstants
 	public static final String DHL_API_KEY_HTTP_HEADER = "Dhl-Api-Key";
 	public static final String STANDARD_GRUPPENPROFIL = "STANDARD_GRUPPENPROFIL";
 	public static final String REMOVED = "<<removed>>";
+	public static final String PARAM_RESPONSE_BODY_AS_STRING = "responseBodyAsString";
 }
