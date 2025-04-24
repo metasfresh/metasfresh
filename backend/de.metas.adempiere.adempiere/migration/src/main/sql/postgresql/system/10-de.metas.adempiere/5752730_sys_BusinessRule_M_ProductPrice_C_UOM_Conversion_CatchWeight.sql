53fcfca8f945231c72580ebcbb87708d1190d662
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-04-24 20:24:48 +0300

    improve rule

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5752730_sys_BusinessRule_M_ProductPrice_C_UOM_Conversion_CatchWeight.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5752730_sys_BusinessRule_M_ProductPrice_C_UOM_Conversion_CatchWeight.sql
@@ -75,3 +75,26 @@ INSERT INTO AD_BusinessRule (AD_BusinessRule_ID,AD_Client_ID,AD_Org_ID,AD_Table_
 INSERT INTO AD_BusinessRule_Trigger (AD_BusinessRule_ID,AD_BusinessRule_Trigger_ID,AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,OnDelete,OnNew,OnUpdate,Source_Table_ID,TargetRecordMappingSQL,Updated,UpdatedBy) VALUES (540015,540020,0,0,TO_TIMESTAMP('2025-04-24 11:02:45.080000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','Y','Y',251,'M_ProductPrice_ID',TO_TIMESTAMP('2025-04-24 11:02:45.080000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
+
+-- 2025-04-24T16:59:09.502Z
+INSERT INTO AD_BusinessRule_Precondition (AD_BusinessRule_ID,AD_BusinessRule_Precondition_ID,AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,PreconditionSQL,PreconditionType,Updated,UpdatedBy) VALUES (540015,540022,0,0,TO_TIMESTAMP('2025-04-24 16:59:09.486000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','M_ProductPrice.CatchWeight','S',TO_TIMESTAMP('2025-04-24 16:59:09.486000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2025-04-24T16:59:31.528Z
+UPDATE AD_BusinessRule_Precondition SET PreconditionSQL='M_ProductPrice.invoicableqtybasedon = ''CatchWeight''',Updated=TO_TIMESTAMP('2025-04-24 16:59:31.528000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_BusinessRule_Precondition_ID=540022
+;
+
+-- 2025-04-24T16:59:59.163Z
+UPDATE AD_BusinessRule_Trigger SET ConditionSQL='invoicableqtybasedon = ''CatchWeight''',Updated=TO_TIMESTAMP('2025-04-24 16:59:59.163000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_BusinessRule_Trigger_ID=540020
+;
+
+
+-- 2025-04-24T17:06:11.817Z
+INSERT INTO AD_BusinessRule_Trigger (AD_BusinessRule_ID,AD_BusinessRule_Trigger_ID,AD_Client_ID,AD_Org_ID,ConditionSQL,Created,CreatedBy,IsActive,OnDelete,OnNew,OnUpdate,Source_Table_ID,TargetRecordMappingSQL,Updated,UpdatedBy) VALUES (540015,540026,0,0,'.iscatchuomforproduct=''Y''',TO_TIMESTAMP('2025-04-24 17:06:11.803000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','Y','Y',175,'C_UOM_Conversion_ID',TO_TIMESTAMP('2025-04-24 17:06:11.803000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+
+-- 2025-04-24T17:13:28.823Z
+UPDATE AD_BusinessRule_Trigger SET ConditionSQL='iscatchuomforproduct=''Y''',Updated=TO_TIMESTAMP('2025-04-24 17:13:28.823000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_BusinessRule_Trigger_ID=540026
+;
+
