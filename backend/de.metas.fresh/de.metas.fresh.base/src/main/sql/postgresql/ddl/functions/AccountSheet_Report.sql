5ed98e8d1c03b643deaa7be488ebf3ec7a547b72
dm-ps <dragos.moca@pevesoft.ro>
2025-04-25 11:44:27 +0300

    improve accountsheet_report function

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/AccountSheet_Report.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/AccountSheet_Report.sql
@@ -7,66 +7,20 @@ DROP FUNCTION IF EXISTS de_metas_acct.AccountSheet_Report(
 ;
 
 DROP FUNCTION IF EXISTS de_metas_acct.AccountSheet_Report(
-    p_Account_IDs numeric[],
-    p_C_AcctSchema_ID numeric,
-    p_DateAcctFrom    DATE,
-    p_DateAcctTo      date
-)
-;
-
-CREATE OR REPLACE FUNCTION de_metas_acct.AccountSheet_Report(
-    p_Account_IDs numeric[],
-    p_C_AcctSchema_ID numeric,
-    p_DateAcctFrom    DATE,
-    p_DateAcctTo      date
-)
-    RETURNS
-        TABLE
-        (
-            konto         text,
-            gegenkonto    text,
-            soll          numeric,
-            haben         numeric,
-            soll_währung  numeric,
-            haben_währung numeric,
-            währung       text,
-            Belegdatum    text,
-            Buchungsdatum text,
-            "Nr"          text
-        )
-    LANGUAGE sql
-    STABLE
-
-
-AS
-$BODY$
-SELECT t.AccountValueAndName::text                                                            AS konto,
-       t.counterpart_AccountValueAndName::text                                                AS gegenkonto,
-       t.amtacctdr                                                                            AS soll,
-       t.amtacctcr                                                                            AS haben,
-       t.amtsourcedr                                                                          AS soll_währung,
-       t.amtsourcecr                                                                          AS haben_währung,
-       (SELECT cy.iso_code FROM c_currency cy WHERE cy.c_currency_id = t.c_currency_id)::text AS währung,
-       TO_CHAR(t.datetrx, 'DD.MM.YYYY')::text                                                 AS Belegdatum,
-       TO_CHAR(t.dateacct, 'DD.MM.YYYY')::text                                                AS Buchungsdatum,
-       t.documentno::text                                                                     AS "Nr"
-FROM de_metas_acct.RV_AccountSheet t
-WHERE TRUE
-  AND t.PostingType = 'A'
-  AND t.C_AcctSchema_ID = p_C_AcctSchema_ID
-  AND t.account_id = ANY((array_retain_positive(p_Account_IDs)))
-  AND (p_DateAcctFrom IS NULL OR t.dateacct >= p_DateAcctFrom)
-  AND (p_DateAcctTo IS NULL OR t.dateacct <= p_DateAcctTo)
-ORDER BY t.dateacct, t.ad_table_id, t.record_id, t.fact_acct_id
-    ;
-$BODY$
-;
-
-CREATE OR REPLACE FUNCTION de_metas_acct.AccountSheet_Report(
     p_Account_ID      numeric,
     p_C_AcctSchema_ID numeric,
     p_DateAcctFrom    DATE,
-    p_DateAcctTo      date
+    p_DateAcctTo      date,
+    p_Account_IDs     numeric[]
+)
+;
+
+CREATE OR REPLACE FUNCTION de_metas_acct.AccountSheet_Report(
+    p_Account_ID      numeric = NULL,
+    p_C_AcctSchema_ID numeric = NULL,
+    p_DateAcctFrom    DATE = NULL,
+    p_DateAcctTo      date = NULL,
+    p_Account_IDs     numeric[] = NULL
 )
     RETURNS
         TABLE
@@ -82,18 +36,44 @@ CREATE OR REPLACE FUNCTION de_metas_acct.AccountSheet_Report(
             Buchungsdatum text,
             "Nr"          text
         )
-    LANGUAGE sql
+    LANGUAGE plpgsql
     STABLE
 
 
 AS
 $BODY$
-SELECT * FROM de_metas_acct.AccountSheet_Report(
-        p_Account_IDs := ARRAY[p_Account_ID],
-        p_C_AcctSchema_ID := p_C_AcctSchema_ID,
-        p_DateAcctFrom := p_DateAcctFrom,
-        p_DateAcctTo := p_DateAcctTo
-    );
+DECLARE
+    v_Account_IDs numeric[];
+BEGIN
+    IF (p_Account_ID IS NOT NULL AND p_Account_ID > 0) THEN
+        v_Account_IDs := ARRAY [p_Account_ID];
+    ELSE
+        v_Account_IDs := p_Account_IDs;
+    END IF;
+
+    IF (v_Account_IDs IS NULL OR ARRAY_LENGTH(v_Account_IDs, 1) = 0) THEN
+        RAISE EXCEPTION 'No account(s) provided. p_Account_ID(=%) or p_Account_IDs(=%) must be set.', p_Account_ID, p_Account_IDs;
+    END IF;
+
+    RETURN QUERY
+        SELECT t.AccountValueAndName::text                                                            AS konto,
+               t.counterpart_AccountValueAndName::text                                                AS gegenkonto,
+               t.amtacctdr                                                                            AS soll,
+               t.amtacctcr                                                                            AS haben,
+               t.amtsourcedr                                                                          AS soll_währung,
+               t.amtsourcecr                                                                          AS haben_währung,
+               (SELECT cy.iso_code FROM c_currency cy WHERE cy.c_currency_id = t.c_currency_id)::text AS währung,
+               TO_CHAR(t.datetrx, 'DD.MM.YYYY')::text                                                 AS Belegdatum,
+               TO_CHAR(t.dateacct, 'DD.MM.YYYY')::text                                                AS Buchungsdatum,
+               t.documentno::text                                                                     AS "Nr"
+        FROM de_metas_acct.RV_AccountSheet t
+        WHERE TRUE
+          AND t.PostingType = 'A'
+          AND t.C_AcctSchema_ID = p_C_AcctSchema_ID
+          AND t.account_id = ANY ((array_retain_positive(v_Account_IDs)))
+          AND (p_DateAcctFrom IS NULL OR t.dateacct >= p_DateAcctFrom)
+          AND (p_DateAcctTo IS NULL OR t.dateacct <= p_DateAcctTo)
+        ORDER BY t.dateacct, t.ad_table_id, t.record_id, t.fact_acct_id;
+END;
 $BODY$
 ;
-
