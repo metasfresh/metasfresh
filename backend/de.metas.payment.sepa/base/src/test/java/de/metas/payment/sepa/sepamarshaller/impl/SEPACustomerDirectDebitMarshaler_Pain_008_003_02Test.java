71127e5faa20bb2b4e6b9b0ac783cfdb778ce005
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-04-30 15:47:56 +0300

    Take out country stuff from tests

--- backend/de.metas.payment.sepa/base/src/test/java/de/metas/payment/sepa/sepamarshaller/impl/SEPACustomerDirectDebitMarshaler_Pain_008_003_02Test.java
+++ backend/de.metas.payment.sepa/base/src/test/java/de/metas/payment/sepa/sepamarshaller/impl/SEPACustomerDirectDebitMarshaler_Pain_008_003_02Test.java
@@ -109,8 +109,6 @@ class SEPACustomerDirectDebitMarshaler_Pain_008_003_02Test
 		assertThat(xmlDocument.getCstmrDrctDbtInitn().getGrpHdr().getInitgPty().getNm()).isEqualTo("SEPA_CreditorName");
 
 		assertThat(xmlDocument.getCstmrDrctDbtInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.getCdtr().getNm()).isEqualTo("bankAccount.A_Name"));
-		assertThat(xmlDocument.getCstmrDrctDbtInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.getCdtr().getPstlAdr().getAdrLine().get(0)).isEqualTo("bankAccount.A_Street"));
-		assertThat(xmlDocument.getCstmrDrctDbtInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.getCdtr().getPstlAdr().getAdrLine().get(1)).isEqualTo("bankAccount.A_ZIP bankAccount.A_City"));
 		assertThat(xmlDocument.getCstmrDrctDbtInitn().getPmtInf()).allSatisfy(pmtInf -> assertThat(pmtInf.getCdtrSchmeId().getId().getPrvtId().getOthr().getId()).isEqualTo("SEPA_CreditorIdentifier"));
 	}
 
@@ -144,9 +142,6 @@ class SEPACustomerDirectDebitMarshaler_Pain_008_003_02Test
 		bankAccount.setIBAN(iban);
 		bankAccount.setIsEsrAccount(true);
 		bankAccount.setA_Name("bankAccount.A_Name");
-		bankAccount.setA_City("bankAccount.A_City");
-		bankAccount.setA_Street("bankAccount.A_Street");
-		bankAccount.setA_Zip("bankAccount.A_ZIP");
 		save(bankAccount);
 
 		final I_SEPA_Export_Line line = newInstance(I_SEPA_Export_Line.class);
