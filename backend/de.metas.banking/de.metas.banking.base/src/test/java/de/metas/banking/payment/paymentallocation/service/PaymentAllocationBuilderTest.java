2cff4e81fcc6a89a000057bfe0d73b25f5bf32fa
Max Rieck <max.rieck@metasfresh.com>
2025-05-06 17:05:51 +0200

    BUG: Grant discount twice during allocate payment process  after cp adjustments

--- backend/de.metas.banking/de.metas.banking.base/src/test/java/de/metas/banking/payment/paymentallocation/service/PaymentAllocationBuilderTest.java
+++ backend/de.metas.banking/de.metas.banking.base/src/test/java/de/metas/banking/payment/paymentallocation/service/PaymentAllocationBuilderTest.java
@@ -883,13 +883,13 @@ public class PaymentAllocationBuilderTest
 				allocation().type(InvoiceToCreditMemo)
 						.payableRef(customerInvoice3.getReference()) // smallest invoice first
 						.paymentRef(customerCreditMemo1.getReference()) // prefer allocating against creditMemos over allocating against payments
-						.allocatedAmt("500").discountAmt("100").writeOffAmt("0").overUnderAmt("1000")
+						.allocatedAmt("500").discountAmt("0").writeOffAmt("0").overUnderAmt("1100")
 						.date("2021-01-14")
 						.build(),
-				allocation().type(InvoiceToPayment) // what's lef to allocate is allocated against payment1
+				allocation().type(InvoiceToPayment) // what's left to allocate is allocated against payment1
 						.payableRef(customerInvoice3.getReference())
 						.paymentRef(inboundPayment1.getReference())
-						.allocatedAmt("1000").discountAmt("0").writeOffAmt("0").overUnderAmt("0").paymentOverUnderAmt("4000")
+						.allocatedAmt("1000").discountAmt("100").writeOffAmt("0").overUnderAmt("0").paymentOverUnderAmt("4000")
 						.date("2021-01-21")
 						.build(),
 				allocation().type(InvoiceToPayment)
@@ -901,13 +901,13 @@ public class PaymentAllocationBuilderTest
 				allocation().type(InvoiceToPayment)
 						.payableRef(customerInvoice1.getReference())
 						.paymentRef(inboundPayment1.getReference())
-						.allocatedAmt("1000").discountAmt("1599").writeOffAmt("1").overUnderAmt("5400").paymentOverUnderAmt("0")
+						.allocatedAmt("1000").discountAmt("0").writeOffAmt("1").overUnderAmt("6999").paymentOverUnderAmt("0")
 						.date("2021-01-21")
 						.build(),
 				allocation().type(InvoiceToPayment)
 						.payableRef(customerInvoice1.getReference())
 						.paymentRef(inboundPayment2.getReference())
-						.allocatedAmt("5000").discountAmt("0").writeOffAmt("0").overUnderAmt("400").paymentOverUnderAmt("0")
+						.allocatedAmt("5000").discountAmt("1599").writeOffAmt("0").overUnderAmt("400").paymentOverUnderAmt("0")
 						.date("2021-01-22")
 						.build());
 
@@ -988,13 +988,13 @@ public class PaymentAllocationBuilderTest
 				allocation().type(InvoiceToCreditMemo)
 						.payableRef(vendorInvoice1.getReference())
 						.paymentRef(vendorCreditMemo1.getReference())
-						.allocatedAmt("-80").discountAmt("-10").writeOffAmt("-5").overUnderAmt("-70")
+						.allocatedAmt("-80").writeOffAmt("-5").overUnderAmt("-80")
 						.date("2021-02-11")
 						.build(),
 				allocation().type(InvoiceToPayment)
 						.payableRef(vendorInvoice1.getReference())
 						.paymentRef(outboundPayment1.getReference())
-						.allocatedAmt("-20").overUnderAmt("-50")
+						.allocatedAmt("-20").discountAmt("-10").overUnderAmt("-50")
 						.date("2021-02-12")
 						.build());
 
