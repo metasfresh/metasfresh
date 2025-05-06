981a7d92e00eafd89da9b9cd921b935cae5cf3c6
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-05-06 19:24:57 +0300

    qa

--- backend/de.metas.business/src/main/java/de/metas/payment/api/impl/PaymentBL.java
+++ backend/de.metas.business/src/main/java/de/metas/payment/api/impl/PaymentBL.java
@@ -99,7 +99,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.math.BigDecimal.ZERO;
 
