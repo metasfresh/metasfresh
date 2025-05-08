161a0c8e40038b7efe0811a5cd3308b53739fc6b
Teodor Sarca <teo.sarca@metasfresh.com>
2025-05-08 09:21:46 +0300

    Scannable Code Format (prototype)

---
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/scannable_code/format/ScannableCodeFormatPartType.java
@@ -0,0 +1,28 @@
+package de.metas.scannable_code.format;
+
+import de.metas.util.lang.ReferenceListAwareEnum;
+import de.metas.util.lang.ReferenceListAwareEnums;
+import de.metas.util.lang.ReferenceListAwareEnums.ValuesIndex;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.compiere.model.X_C_ScannableCode_Format_Part;
+
+@RequiredArgsConstructor
+@Getter
+public enum ScannableCodeFormatPartType implements ReferenceListAwareEnum
+{
+	Ignored(X_C_ScannableCode_Format_Part.DATATYPE_Ignored, ScannableCodeFormatPartDataType.String),
+	ProductCode(X_C_ScannableCode_Format_Part.DATATYPE_ProductCode, ScannableCodeFormatPartDataType.String),
+	WeightInKg(X_C_ScannableCode_Format_Part.DATATYPE_WeightInKg, ScannableCodeFormatPartDataType.Number),
+	LotNo(X_C_ScannableCode_Format_Part.DATATYPE_LotNo, ScannableCodeFormatPartDataType.String),
+	BestBeforeDate(X_C_ScannableCode_Format_Part.DATATYPE_BestBeforeDate, ScannableCodeFormatPartDataType.Date),
+	;
+
+	@NonNull private static final ValuesIndex<ScannableCodeFormatPartType> index = ReferenceListAwareEnums.index(values());
+
+	@NonNull private final String code;
+	@NonNull private final ScannableCodeFormatPartDataType dataType;
+
+	public static ScannableCodeFormatPartType ofCode(@NonNull final String code) {return index.ofCode(code);}
+}
