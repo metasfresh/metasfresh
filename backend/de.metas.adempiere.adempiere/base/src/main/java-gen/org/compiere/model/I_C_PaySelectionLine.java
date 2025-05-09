3d190aa0d28e098982f9d9b0caa2f3e57132eb28
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-05-09 18:27:53 +0300

    Merge inner_silence_hotfix to uat (#20793)
    * Model Changes for Duplicate Payments (#20771)
    
    * C_PaySelectionLine Original Payment
    
    * C_Payment Original Payment
    
    * C_Payment isRefund
    
    * Support payments in Pay selection
    
    * use Interface List instead of ArrayList
    
    * Create payments and allocate them
    
    * Update Refund status and link payments
    
    * Take out payment writeoff
    
    * Add read only logic in pay selection and payment
    
    * Negate amounts
    
    * QA/fixes
    
    * Set back ArrayList
    
    * Set back ArrayList
    
    * Support any payments
    
    * Pay Selection: set proper bank account (#20792)
    
    * C_PaySelectionLine Original Payment
    
    * C_Payment Original Payment
    
    * C_Payment isRefund
    
    * Support payments in Pay selection
    
    * use Interface List instead of ArrayList
    
    * Create payments and allocate them
    
    * Update Refund status and link payments
    
    * Take out payment writeoff
    
    * Add read only logic in pay selection and payment
    
    * Negate amounts
    
    * QA/fixes
    
    * Set back ArrayList
    
    * Set back ArrayList
    
    * Support any payments
    
    * Set proper Bank Account in pay selection line
    
    * Fix where clause
    
    * Do not validate for esr account
    
    ---------
    
    Co-authored-by: metas_rc <ruxandra.craciunescu@metasfresh.com>
    Co-authored-by: Teodor Sarca <teo.sarca@metasfresh.com>
    
    ---------
    
    Co-authored-by: Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
    Co-authored-by: Teodor Sarca <teo.sarca@metasfresh.com>

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_C_PaySelectionLine.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_C_PaySelectionLine.java
@@ -187,7 +187,7 @@ public interface I_C_PaySelectionLine
 	 * Invoice Identifier
 	 *
 	 * <br>Type: Search
-	 * <br>Mandatory: true
+	 * <br>Mandatory: false
 	 * <br>Virtual Column: false
 	 */
 	void setC_Invoice_ID (int C_Invoice_ID);
@@ -197,7 +197,7 @@ public interface I_C_PaySelectionLine
 	 * Invoice Identifier
 	 *
 	 * <br>Type: Search
-	 * <br>Mandatory: true
+	 * <br>Mandatory: false
 	 * <br>Virtual Column: false
 	 */
 	int getC_Invoice_ID();
@@ -658,4 +658,25 @@ public interface I_C_PaySelectionLine
 	int getUpdatedBy();
 
 	String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/**
+	 * Set Original Payment.
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setOriginal_Payment_ID (int Original_Payment_ID);
+
+	/**
+	 * Get Original Payment.
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	int getOriginal_Payment_ID();
+
+	String COLUMNNAME_Original_Payment_ID = "Original_Payment_ID";
+
 }
