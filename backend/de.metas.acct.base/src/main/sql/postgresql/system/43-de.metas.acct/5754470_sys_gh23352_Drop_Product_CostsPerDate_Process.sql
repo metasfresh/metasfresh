5fad136fc166a4b09bd196cecd834a3bdddcdd8d
riad.mennour <riad.mennour@gmail.com>
2025-05-14 15:31:39 +0100

    Drop `Product Costs Per Date (Excel)` Process

--- backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5754470_sys_gh23352_Drop_Product_CostsPerDate_Process.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/system/43-de.metas.acct/5754470_sys_gh23352_Drop_Product_CostsPerDate_Process.sql
@@ -13,6 +13,21 @@ DELETE FROM AD_Menu WHERE AD_Menu_ID=542123
 DELETE FROM AD_TreeNodeMM n WHERE Node_ID=542123 AND EXISTS (SELECT * FROM AD_Tree t WHERE t.AD_Tree_ID=n.AD_Tree_ID AND t.AD_Table_ID=116)
 ;
 
+-- Name: Auswertung Produktkosten (Excel)
+-- Action Type: R
+-- Report: Product costs per date (Excel)(de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess)
+-- 2025-05-14T09:35:51.252Z
+DELETE FROM  AD_Menu_Trl WHERE AD_Menu_ID=542130
+;
+
+-- 2025-05-14T09:35:51.558Z
+DELETE FROM AD_Menu WHERE AD_Menu_ID=542130
+;
+
+-- 2025-05-14T09:35:51.708Z
+DELETE FROM AD_TreeNodeMM n WHERE Node_ID=542130 AND EXISTS (SELECT * FROM AD_Tree t WHERE t.AD_Tree_ID=n.AD_Tree_ID AND t.AD_Table_ID=116)
+;
+
 -- Value: Auswertung Produktkosten (Excel)
 -- Classname: de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess
 -- 2025-05-14T09:39:17.949Z
