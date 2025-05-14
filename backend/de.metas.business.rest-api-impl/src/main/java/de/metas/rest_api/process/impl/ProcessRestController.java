6172b2f2cbf7d5a60ab1b390c63edecdc3d39ca5
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-05-14 16:30:05 +0200

    - ProcessInfo    - add enum ProcessCalledFrom so that a process can tell if it was invoked via API    - It doesn't always have to be specified. The default is "Unknown". If you need it at a particular point, I think it will be easy to set.    - Concrete usecase: we need to store a process-result to disk if not invoked via API.

--- backend/de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/process/impl/ProcessRestController.java
+++ backend/de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/process/impl/ProcessRestController.java
@@ -28,6 +28,7 @@ import de.metas.logging.LogManager;
 import de.metas.process.AdProcessId;
 import de.metas.process.IADProcessDAO;
 import de.metas.process.ProcessBasicInfo;
+import de.metas.process.ProcessCalledFrom;
 import de.metas.process.ProcessExecutionResult;
 import de.metas.process.ProcessInfo;
 import de.metas.process.ProcessType;
@@ -79,7 +80,7 @@ import java.util.stream.Collectors;
 @Profile(Profiles.PROFILE_App)
 public class ProcessRestController
 {
-	private static final transient Logger logger = LogManager.getLogger(ADProcessDAO.class);
+	private static final Logger logger = LogManager.getLogger(ADProcessDAO.class);
 
 	private final IADProcessDAO adProcessDAO = Services.get(IADProcessDAO.class);
 	private final PermissionServiceFactory permissionServiceFactory = PermissionServiceFactories.currentContext();
@@ -111,8 +112,9 @@ public class ProcessRestController
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 		}
 
-		final ProcessInfo.ProcessInfoBuilder processInfoBuilder = ProcessInfo.builder();
-		processInfoBuilder.setAD_Process_ID(processId.get());
+		final ProcessInfo.ProcessInfoBuilder processInfoBuilder = ProcessInfo.builder()
+				.setAD_Process_ID(processId.get())
+				.setProcessCalledFrom(ProcessCalledFrom.API);
 
 		final boolean parametersPresent = request != null && !Check.isEmpty(request.getProcessParameters());
 
