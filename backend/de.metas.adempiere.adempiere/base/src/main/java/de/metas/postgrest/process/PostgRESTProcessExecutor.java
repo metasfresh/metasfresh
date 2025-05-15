20575535b55f7e0f9a70c8ebd5b45b7b21a03362
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-05-15 07:36:40 +0200

    - PostgRESTProcessExecutor   - now also sends the result to the browser if called via WebUI   - can be called directly as before, but also subclassed to add custom behavior.     - First use-Case: C_Invoice_EDI_Export_JSON   - AD_Process.ClassName is editable for PostgREST-Processes   - A MI verifies that the entered class is PostgRESTProcessExecutor or a subclass thereof

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/postgrest/process/PostgRESTProcessExecutor.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/postgrest/process/PostgRESTProcessExecutor.java
@@ -26,64 +26,153 @@ import de.metas.postgrest.client.GetRequest;
 import de.metas.postgrest.client.PostgRESTClient;
 import de.metas.postgrest.client.PostgRESTResponseFormat;
 import de.metas.postgrest.config.PostgRESTConfig;
+import de.metas.postgrest.config.PostgRESTConfigId;
 import de.metas.postgrest.config.PostgRESTConfigRepository;
 import de.metas.process.IADProcessDAO;
 import de.metas.process.JavaProcess;
 import de.metas.process.ProcessInfo;
 import de.metas.util.Services;
 import de.metas.util.StringUtils;
+import lombok.Builder;
 import lombok.NonNull;
+import lombok.Value;
 import org.adempiere.ad.expression.api.IExpressionEvaluator;
 import org.adempiere.ad.expression.api.IExpressionFactory;
 import org.adempiere.ad.expression.api.IStringExpression;
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.SpringContextHolder;
+import org.compiere.model.I_AD_Process;
 import org.compiere.util.Env;
 import org.compiere.util.Evaluatee;
 import org.compiere.util.Evaluatees;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Makes an invocation to the postgresql-service.
+ * Can be used as is or be subclassed if custom behavior is needed.
+ * If invoked via WebUI, then the result is sent to the browser, similar to the behavior of a report-process.
+ */
 public class PostgRESTProcessExecutor extends JavaProcess
 {
 	private final PostgRESTClient postgRESTClient = SpringContextHolder.instance.getBean(PostgRESTClient.class);
 	private final PostgRESTConfigRepository configRepository = SpringContextHolder.instance.getBean(PostgRESTConfigRepository.class);
+
 	private final IExpressionFactory expressionFactory = Services.get(IExpressionFactory.class);
 	private final IADProcessDAO processDAO = Services.get(IADProcessDAO.class);
 
 	@Override
-	protected String doIt() throws Exception
+	protected final String doIt() throws Exception
+	{
+		try
+		{
+			final CustomPostgRESTParameters parameters = beforePostgRESTCall();
+
+			performPostgRESTCallAndSetResult(parameters);
+
+			return afterPostgRESTCall();
+		}
+		catch (final Exception e)
+		{
+			throw handleException(e);
+		}
+	}
+
+	protected CustomPostgRESTParameters beforePostgRESTCall()
+	{
+		return CustomPostgRESTParameters.builder().storeJsonFile(false).build();
+	}
+
+	protected String afterPostgRESTCall()
+	{
+		return MSG_OK;
+	}
+
+	protected Exception handleException(final Exception e) {return e;}
+
+	private void performPostgRESTCallAndSetResult(@NonNull final CustomPostgRESTParameters parameters) throws IOException
 	{
 		final ProcessInfo processInfo = getProcessInfo();
 
-		final boolean jsonPathMissing = !processInfo.getJsonPath().isPresent();
-
-		if (jsonPathMissing)
-		{
-			throw new AdempiereException("JSONPath is missing!")
-					.appendParametersToMessage()
-					.setParameter("processInfo", processInfo);
-		}
+		final String jsonPathRaw = processInfo.getJsonPath()
+				.orElseThrow(() -> new AdempiereException("JSONPath is missing!")
+						.appendParametersToMessage()
+						.setParameter("processInfo", processInfo));
 
 		final PostgRESTConfig config = configRepository.getConfigFor(processInfo.getOrgId());
+		addLog("Using S_PostgREST_Config_ID={}", PostgRESTConfigId.toRepoId(config.getId()));
+		addLog("ProcessCalledFrom={}", processInfo.getProcessCalledFrom());
 
-		final PostgRESTResponseFormat responseFormat = PostgRESTResponseFormat.ofCode(processDAO.getById(getProcessInfo().getAdProcessId()).getPostgrestResponseFormat());
+		final I_AD_Process processRecord = processDAO.getById(getProcessInfo().getAdProcessId());
+		final PostgRESTResponseFormat responseFormat = PostgRESTResponseFormat.ofCode(processRecord.getPostgrestResponseFormat());
 
-		final String rawJSONPath = processInfo.getJsonPath().get();
-		final IStringExpression pathExpression = expressionFactory.compile(prepareJSONPath(rawJSONPath), IStringExpression.class);
-		final String path = pathExpression.evaluate(getEvalContext(), IExpressionEvaluator.OnVariableNotFound.Fail);
+		final IStringExpression jsonPathExpression = expressionFactory.compile(prepareJSONPath(jsonPathRaw), IStringExpression.class);
+		final String jsonPath = jsonPathExpression.evaluate(getEvalContext(), IExpressionEvaluator.OnVariableNotFound.Fail);
 
 		final GetRequest getRequest = GetRequest
 				.builder()
-				.baseURL(config.getBaseURL() + path)
+				.baseURL(createRequestURL(config, jsonPath))
 				.responseFormat(responseFormat)
 				.build();
 
-		final String postgRESTResponse = postgRESTClient.performGet(getRequest);
-		processInfo.getResult().setStringResult(postgRESTResponse, responseFormat.getContentType());
+		final String fileName = processInfo.getPinstanceId().getRepoId() + responseFormat.getFilenameExtension();
+		final String fileNameForReportData = processRecord.getValue() + "_" + fileName;
 
-		return MSG_OK;
+		final Resource postgRESTResponse = postgRESTClient.performGet(getRequest);
+		if (!postgRESTResponse.exists())
+		{
+			throw new AdempiereException("postgREST response does not exist: " + postgRESTResponse.getDescription())
+					.appendParametersToMessage()
+					.setParameter("getRequest", getRequest);
+		}
+
+		if (parameters.isStoreJsonFile())
+		{
+			final Path outputFilePath = Paths.get(
+					config.getResultDirectory(),
+					processRecord.getValue(),
+					fileName);
+
+			// store resource to disk
+			Files.createDirectories(outputFilePath.getParent());
+			try (final InputStream in = postgRESTResponse.getInputStream(); final OutputStream out = Files.newOutputStream(outputFilePath))
+			{
+				final byte[] buffer = new byte[8192];
+				int bytesRead;
+				while ((bytesRead = in.read(buffer)) != -1)
+				{
+					out.write(buffer, 0, bytesRead);
+				}
+			}
+			addLog("Stored postgREST result to {}", outputFilePath);
+			processInfo.getResult().setReportData(outputFilePath.toFile(), fileNameForReportData);
+		}
+		else
+		{
+			processInfo.getResult().setReportData(postgRESTResponse, fileNameForReportData, responseFormat.getContentType());
+		}
+	}
+
+	@NonNull
+	private static String createRequestURL(
+			@NonNull final PostgRESTConfig config,
+			@NonNull final String jsonPath)
+	{
+		final StringBuilder result = new StringBuilder(config.getBaseURL());
+		if (!config.getBaseURL().endsWith("/") && !jsonPath.startsWith("/"))
+		{
+			result.append("/");
+		}
+		result.append(jsonPath);
+		return result.toString();
 	}
 
 	private String prepareJSONPath(@NonNull final String rawJSONPath)
@@ -105,4 +194,10 @@ public class PostgRESTProcessExecutor extends JavaProcess
 		return Evaluatees.compose(contexts);
 	}
 
+	@Value
+	@Builder
+	protected static class CustomPostgRESTParameters
+	{
+		boolean storeJsonFile;
+	}
 }
