46c67c191ab47332ba3861958aee9fae7abafd47
dm-ps <dragos.moca@pevesoft.ro>
2025-05-21 00:22:36 +0300

    Allow EDI-Contractnumber to be set for BPartners

---
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5755460_sys_gh23452_BPartner-EDI_Merkmale_attribute_set.sql
@@ -0,0 +1,37 @@
+-- Run mode: WEBUI
+
+-- 2025-05-20T18:51:17.127Z
+INSERT INTO M_Attribute (AD_Client_ID,AD_Org_ID,AttributeValueType,Created,CreatedBy,IsActive,IsAlwaysUpdateable,IsAttrDocumentRelevant,IsHighVolume,IsInstanceAttribute,IsMandatory,IsPricingRelevant,IsPrintedInDocument,IsReadOnlyValues,IsStorageRelevant,IsTransferWhenNull,M_Attribute_ID,Name,Updated,UpdatedBy,Value) VALUES (1000000,1000000,'S',TO_TIMESTAMP('2025-05-20 18:51:17.108000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','N','N','N','N','N','N','N','N','N',540160,'Abkommensnummer',TO_TIMESTAMP('2025-05-20 18:51:17.108000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Abkommensnummer')
+;
+
+-- 2025-05-20T18:51:17.179Z
+UPDATE M_Attribute SET IsInstanceAttribute='Y',Updated=TO_TIMESTAMP('2025-05-20 18:51:17.179000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE M_Attribute_ID=540160
+;
+
+-- 2025-05-20T18:51:17.312Z
+UPDATE M_AttributeSet mas SET IsInstanceAttribute='Y' WHERE IsInstanceAttribute='N' AND EXISTS (SELECT * FROM M_AttributeUse mau WHERE mas.M_AttributeSet_ID=mau.M_AttributeSet_ID AND mau.M_Attribute_ID=540160)
+;
+
+-- Run mode: WEBUI
+
+-- 2025-05-20T18:53:34.070Z
+INSERT INTO M_AttributeSet (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,IsInstanceAttribute,M_AttributeSet_ID,MandatoryType,Name,Updated,UpdatedBy) VALUES (1000000,1000000,TO_TIMESTAMP('2025-05-20 18:53:34.067000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','Y',540019,'N','BPartner-EDI Merkmale',TO_TIMESTAMP('2025-05-20 18:53:34.067000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2025-05-20T18:53:34.086Z
+UPDATE M_AttributeSet mas SET IsInstanceAttribute='N' WHERE M_AttributeSet_ID=540019 AND IsInstanceAttribute='Y' AND NOT EXISTS (SELECT * FROM M_AttributeUse mau INNER JOIN M_Attribute ma ON (mau.M_Attribute_ID=ma.M_Attribute_ID) WHERE mau.M_AttributeSet_ID=mas.M_AttributeSet_ID AND mau.IsActive='Y' AND ma.IsActive='Y' AND ma.IsInstanceAttribute='Y')
+;
+
+-- Run mode: WEBUI
+
+-- 2025-05-20T19:07:13.701Z
+INSERT INTO M_AttributeUse (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,M_Attribute_ID,M_AttributeSet_ID,M_AttributeUse_ID,SeqNo,Updated,UpdatedBy) VALUES (1000000,1000000,TO_TIMESTAMP('2025-05-20 19:07:13.700000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y',540160,540019,540088,10,TO_TIMESTAMP('2025-05-20 19:07:13.700000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2025-05-20T19:07:13.708Z
+UPDATE M_AttributeSet mas SET IsInstanceAttribute='Y' WHERE M_AttributeSet_ID=540019 AND IsInstanceAttribute='N' AND (EXISTS (SELECT * FROM M_AttributeUse mau INNER JOIN M_Attribute ma ON (mau.M_Attribute_ID=ma.M_Attribute_ID) WHERE mau.M_AttributeSet_ID=mas.M_AttributeSet_ID AND mau.IsActive='Y' AND ma.IsActive='Y' AND ma.IsInstanceAttribute='Y'))
+;
+
+-- 2025-05-20T19:07:13.709Z
+UPDATE M_AttributeSet mas SET IsInstanceAttribute='N' WHERE M_AttributeSet_ID=540019 AND IsInstanceAttribute='Y' AND NOT EXISTS (SELECT * FROM M_AttributeUse mau INNER JOIN M_Attribute ma ON (mau.M_Attribute_ID=ma.M_Attribute_ID) WHERE mau.M_AttributeSet_ID=mas.M_AttributeSet_ID AND mau.IsActive='Y' AND ma.IsActive='Y' AND ma.IsInstanceAttribute='Y')
+;
