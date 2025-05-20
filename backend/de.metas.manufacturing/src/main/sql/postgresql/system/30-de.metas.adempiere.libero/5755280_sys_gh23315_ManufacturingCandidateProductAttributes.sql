7a69bcbbf487c3d3bbd13815d844e23b9893899e
dm-ps <dragos.moca@pevesoft.ro>
2025-05-20 13:41:50 +0300

    make attribute ui elements readonly

--- backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5755280_sys_gh23315_ManufacturingCandidateProductAttributes.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5755280_sys_gh23315_ManufacturingCandidateProductAttributes.sql
@@ -13,3 +13,29 @@ INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_El
 -- 2025-05-20T07:23:01.316Z
 UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=230,Updated=TO_TIMESTAMP('2025-05-20 07:23:01.316000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=632507
 ;
+
+-- Run mode: SWING_CLIENT
+
+-- UI Element: Produktionsdisposition(541316,EE01) -> Produktionsdisposition(544794,EE01) -> main -> 10 -> attributes.Product Attributes
+-- Column: PP_Order_Candidate.M_Product_ID
+-- 2025-05-20T10:35:27.284Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=200,Updated=TO_TIMESTAMP('2025-05-20 10:35:27.284000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=632507
+;
+
+-- UI Element: Produktionsdisposition(541316,EE01) -> Produktionsdisposition(544794,EE01) -> main -> 20 -> org.Sektion
+-- Column: PP_Order_Candidate.AD_Org_ID
+-- 2025-05-20T10:35:27.291Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=210,Updated=TO_TIMESTAMP('2025-05-20 10:35:27.291000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=594545
+;
+
+-- UI Element: Produktionsdisposition(541316,EE01) -> Produktionsdisposition(544794,EE01) -> main -> 20 -> org.Mandant
+-- Column: PP_Order_Candidate.AD_Client_ID
+-- 2025-05-20T10:35:27.299Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=220,Updated=TO_TIMESTAMP('2025-05-20 10:35:27.299000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=594546
+;
+
+-- UI Element: Produktionsdisposition(541316,EE01) -> Produktionsdisposition(544794,EE01) -> main -> 10 -> attributes.Resource Attributes
+-- Column: PP_Order_Candidate.S_Resource_ID
+-- 2025-05-20T10:35:27.304Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=230,Updated=TO_TIMESTAMP('2025-05-20 10:35:27.304000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=630840
+;
