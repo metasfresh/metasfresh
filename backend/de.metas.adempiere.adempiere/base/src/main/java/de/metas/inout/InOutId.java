47613e7de583951197099e386f9ad677eb66f635
Teodor Sarca <teo.sarca@metasfresh.com>
2025-05-21 01:51:19 +0300

    InOutId.toRecordRef

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/inout/InOutId.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/inout/InOutId.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import de.metas.util.Check;
 import de.metas.util.lang.RepoIdAware;
 import lombok.Value;
+import org.adempiere.util.lang.impl.TableRecordReference;
+import org.compiere.model.I_M_InOut;
 
 import javax.annotation.Nullable;
 import java.util.Objects;
@@ -20,12 +22,12 @@ import java.util.Optional;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -71,4 +73,7 @@ public class InOutId implements RepoIdAware
 	}
 
 	public static boolean equals(@Nullable InOutId id1, @Nullable InOutId id2) {return Objects.equals(id1, id2);}
+
+	public TableRecordReference toRecordRef() {return TableRecordReference.of(I_M_InOut.Table_Name, repoId);}
+
 }
