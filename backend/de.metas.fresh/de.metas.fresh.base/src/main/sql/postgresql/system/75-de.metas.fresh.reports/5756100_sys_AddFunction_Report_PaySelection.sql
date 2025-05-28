7f90bb2867aeffba40069a21006a402a0d227f20
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-05-28 12:09:02 +0300

    Add coalesce

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5756100_sys_AddFunction_Report_PaySelection.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5756100_sys_AddFunction_Report_PaySelection.sql
@@ -36,11 +36,7 @@ CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Report_PaySelectio
             )
 AS
 $$
-SELECT (SELECT COALESCE(org.name, '')
-        FROM AD_Org org
-
-        WHERE org.ad_org_ID = ps.ad_org_ID)                         AS org_name,
-
+SELECT COALESCE(org.name, '')                                       AS org_name,
        ps.PayDate,
        psl.C_PaySelectionLine_ID,
        bp.C_BPartner_ID,
@@ -55,58 +51,62 @@ SELECT (SELECT COALESCE(org.name, '')
        i.poreference                                                AS I_DocNo,
        COALESCE(i.Documentno, p.documentno)                         AS U_DocNo,
 
-       pt.NetDays,
-       pt.DiscountDays,
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN NULL ::timestamp with time zone
-                                             ELSE i.DueDate::timestamp with time zone
-        END)                                                        AS DueDate,
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN NULL
-                                             ELSE EXTRACT(DAY FROM (TRUNC(ps.PayDate) - i.DueDate))
-        END)                                                        AS DaysDue,
+       COALESCE(pt.NetDays, 0)                                      AS NetDays,
+       COALESCE(pt.DiscountDays, 0)                                 AS DiscountDays,
 
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN NULL ::timestamp with time zone
-                                             ELSE AddDays(i.DateInvoiced::timestamp with time zone, pt.DiscountDays)
-        END)                                                        AS DiscountDate,
+       CASE
+           WHEN psl.original_payment_id > 0 THEN NULL::timestamp with time zone
+                                            ELSE i.DueDate
+       END                                                          AS DueDate,
 
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN NULL
-                                             ELSE ROUND(i.GrandTotal * pt.Discount / 100::numeric, 2)
-        END)                                                        AS DiscountAmt,
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN rp.payamt
-                                             ELSE InvoicePaid(i.C_Invoice_ID, i.C_Currency_ID, 1::numeric)
-        END)                                                        AS PaidAAmt,
+       CASE
+           WHEN psl.original_payment_id > 0 THEN NULL
+                                            ELSE EXTRACT(DAY FROM TRUNC(ps.PayDate) - TRUNC(i.DueDate))
+       END                                                          AS DaysDue,
 
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN rp.payamt - p.payamt
-                                             ELSE InvoiceOpenToDate(i.C_Invoice_ID, 0, ps.PayDate)
-        END)                                                        AS OpenAmt,
+       CASE
+           WHEN psl.original_payment_id > 0 THEN NULL::timestamp with time zone
+                                            ELSE AddDays(i.DateInvoiced, pt.DiscountDays)
+       END                                                          AS DiscountDate,
 
-       (CASE
-            WHEN psl.original_payment_id > 0 THEN p.payamt
-                                             ELSE i.GrandTotal
-        END)                                                        AS GrandTotal,
+       CASE
+           WHEN psl.original_payment_id > 0 THEN 0
+                                            ELSE ROUND(i.GrandTotal * pt.Discount / 100.0, 2)
+       END                                                          AS DiscountAmt,
+
+       CASE
+           WHEN psl.original_payment_id > 0 THEN COALESCE(rp.payamt, 0)
+                                            ELSE InvoicePaid(i.C_Invoice_ID, i.C_Currency_ID, 1)
+       END                                                          AS PaidAmt,
+
+       CASE
+           WHEN psl.original_payment_id > 0 THEN COALESCE(rp.payamt, 0) - COALESCE(p.payamt, 0)
+                                            ELSE InvoiceOpenToDate(i.C_Invoice_ID, 0, ps.PayDate)
+       END                                                          AS OpenAmt,
+
+       CASE
+           WHEN psl.original_payment_id > 0 THEN COALESCE(p.payamt, 0)
+                                            ELSE i.GrandTotal
+       END                                                          AS GrandTotal,
+
        c.ISO_Code,
-       psl.DiscountAmt                                              AS ActualDiscountAmt,
-       psl.OpenAmt                                                  AS beforePay,
-       psl.DifferenceAmt                                            AS AfterPay,
-       psl.PayAmt                                                   AS PayAmt,
+       COALESCE(psl.DiscountAmt, 0)                                 AS ActualDiscountAmt,
+       COALESCE(psl.OpenAmt, 0)                                     AS beforePay,
+       COALESCE(psl.DifferenceAmt, 0)                               AS AfterPay,
+       COALESCE(psl.PayAmt, 0)                                      AS PayAmt,
        ps.processed != 'Y'                                          AS isDraft
-FROM
-    -- Payselection records
-    C_PaySelection ps
-        LEFT OUTER JOIN C_PaySelectionLine psl ON ps.C_PaySelection_ID = psl.C_PaySelection_ID
-        -- Documents
-        LEFT OUTER JOIN C_Invoice i ON psl.C_Invoice_ID = i.C_Invoice_ID
-        LEFT OUTER JOIN C_Payment p ON psl.original_payment_id = p.c_payment_id
-        LEFT OUTER JOIN C_Payment rp ON psl.c_payment_id = rp.c_payment_id
-        LEFT OUTER JOIN C_PaymentTerm pt ON i.C_PaymentTerm_ID = pt.C_PaymentTerm_ID
-        LEFT OUTER JOIN C_DocType dt ON COALESCE(i.C_DocType_ID, p.c_doctype_id) = dt.C_DocType_ID
-        LEFT OUTER JOIN C_Currency c ON COALESCE(i.C_Currency_ID, p.c_currency_id) = c.C_Currency_ID
-        LEFT OUTER JOIN C_BPartner bp ON COALESCE(i.C_BPartner_ID, psl.c_bpartner_id) = bp.C_BPartner_ID
+
+FROM C_PaySelection ps
+         LEFT JOIN AD_Org org ON org.AD_Org_ID = ps.AD_Org_ID
+         LEFT JOIN C_PaySelectionLine psl ON ps.C_PaySelection_ID = psl.C_PaySelection_ID
+         LEFT JOIN C_Invoice i ON psl.C_Invoice_ID = i.C_Invoice_ID
+         LEFT JOIN C_Payment p ON psl.original_payment_id = p.C_Payment_ID
+         LEFT JOIN C_Payment rp ON psl.C_Payment_ID = rp.C_Payment_ID
+         LEFT JOIN C_PaymentTerm pt ON i.C_PaymentTerm_ID = pt.C_PaymentTerm_ID
+         LEFT JOIN C_DocType dt ON COALESCE(i.C_DocType_ID, p.C_DocType_ID) = dt.C_DocType_ID
+         LEFT JOIN C_Currency c ON COALESCE(i.C_Currency_ID, p.C_Currency_ID) = c.C_Currency_ID
+         LEFT JOIN C_BPartner bp ON COALESCE(i.C_BPartner_ID, psl.C_BPartner_ID) = bp.C_BPartner_ID
+
 WHERE ps.C_PaySelection_ID = p_Record_ID
   AND (p_C_BPartner_ID IS NULL OR bp.C_BPartner_ID = p_C_BPartner_ID)
 
@@ -114,4 +114,5 @@ ORDER BY c.ISO_Code, bp.name, i.DateInvoiced;
 $$
     LANGUAGE sql
     STABLE
+    PARALLEL SAFE
 ;
