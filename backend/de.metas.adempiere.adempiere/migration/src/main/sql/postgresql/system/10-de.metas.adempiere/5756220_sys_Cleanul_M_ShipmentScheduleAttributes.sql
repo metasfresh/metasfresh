4c3a2a61c998ff5aa3eecf74a3dbaded2e84d9dc
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-05-29 21:04:48 +0300

    delete M_ShipmentSchedule_AttributeConfigs for attributes that don't need to be used in shipments

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5756220_sys_Cleanul_M_ShipmentScheduleAttributes.sql
@@ -0,0 +1,9 @@
+
+
+select backup_table('M_ShipmentSchedule_AttributeConfig'); 
+
+
+
+DELETE FROM M_ShipmentSchedule_AttributeConfig WHERE m_attribute_id = 540022;
+
+DELETE FROM M_ShipmentSchedule_AttributeConfig WHERE m_attribute_id = 1000025;
