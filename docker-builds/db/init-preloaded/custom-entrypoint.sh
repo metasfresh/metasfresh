b5f0a25eb22137d4e8df53603990f3f7af0582d4
Max Rieck <max.rieck@metasfresh.com>
2025-06-01 15:26:11 +0200

    Fix preloaded db image

--- docker-builds/db/init-preloaded/custom-entrypoint.sh
+++ docker-builds/db/init-preloaded/custom-entrypoint.sh
@@ -4,5 +4,5 @@ set -ex
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     cp -a /var/lib/postgresql/initdata/* $PGDATA
 fi
-
+# because of backwards compatibility with CMD ["postgres"] in custom dockerfiles we don't use /usr/local/bin/docker-entrypoint.sh postgres "$@"
 /usr/local/bin/docker-entrypoint.sh "$@"
