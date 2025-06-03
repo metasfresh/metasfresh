4ee28d25108470f121cc95d5324f0dc119feb8b4
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-06-03 18:08:12 +0300

    AD_Record_Warning Root Info Mandatory

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_AD_Record_Warning.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_AD_Record_Warning.java
@@ -259,7 +259,7 @@ public interface I_AD_Record_Warning
 	 * Set Root Table.
 	 *
 	 * <br>Type: Search
-	 * <br>Mandatory: false
+	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
 	void setRoot_AD_Table_ID (int Root_AD_Table_ID);
@@ -268,7 +268,7 @@ public interface I_AD_Record_Warning
 	 * Get Root Table.
 	 *
 	 * <br>Type: Search
-	 * <br>Mandatory: false
+	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
 	int getRoot_AD_Table_ID();
@@ -279,7 +279,7 @@ public interface I_AD_Record_Warning
 	 * Set Root Record.
 	 *
 	 * <br>Type: Button
-	 * <br>Mandatory: false
+	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
 	void setRoot_Record_ID (int Root_Record_ID);
@@ -288,7 +288,7 @@ public interface I_AD_Record_Warning
 	 * Get Root Record.
 	 *
 	 * <br>Type: Button
-	 * <br>Mandatory: false
+	 * <br>Mandatory: true
 	 * <br>Virtual Column: false
 	 */
 	int getRoot_Record_ID();
