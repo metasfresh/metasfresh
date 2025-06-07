1701d216ac66a52e6d51020856b4bb40c0e23cf7
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-06-07 22:49:05 +0200

    extend mvn_build.sh
    it now also builds camel and downloads all the sources

--- misc/dev-support/bootstrap-scripts/mvn_build.sh
+++ misc/dev-support/bootstrap-scripts/mvn_build.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 set -e
 
-export JAVA_HOME=/c/Users/tobia/.jdks/temurin-21.0.7 
+#needed if your locally installed "default" java is not java-21
+#export JAVA_HOME=c:/Users/tobia/.jdks/temurin-21.0.7 
 
-#mvn --file ../../parent-pom/pom.xml -DskipTests --settings ../maven/settings.xml clean install
+MULTITHREAD_PARAM='-T2C'
+#MULTITHREAD_PARAM=''
 
-#mvn --file ../../de-metas-common/pom.xml -DskipTests --settings ../maven/settings.xml clean install
+ADDITIONAL_PARAMS='-DskipTests'
+#ADDITIONAL_PARAMS='-DskipTests -Dmaven.gitcommitid.skip=true -Dlicense.skip=true'
+#ADDITIONAL_PARAMS='-Dmaven.test.skip=true -Dmaven.gitcommitid.skip=true -Dlicense.skip=true'
 
+mvn --file ../../parent-pom/pom.xml --settings ../maven/settings.xml $ADDITIONAL_PARAMS clean install
+mvn --file ../../parent-pom/pom.xml --settings ../maven/settings.xml dependency:sources
+
+mvn --file ../../de-metas-common/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS clean install
+mvn --file ../../de-metas-common/pom.xml --settings ../maven/settings.xml dependency:sources
+
+mvn --file ../../../backend/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS install
 mvn --file ../../../backend/pom.xml --settings ../maven/settings.xml dependency:sources
 
-#mvn --file ../../../backend/pom.xml -T2C -DskipTests --settings ../maven/settings.xml clean install
-#mvn --file ../../../backend/pom.xml -DskipTests --settings ../maven/settings.xml install -rf :de.metas.util.web
-
-
-#mvn --file ../../services/camel/pom.xml -T2C -DskipTests --settings ../maven/settings.xml clean install
+mvn --file ../../services/camel/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS clean install
+mvn --file ../../services/camel/pom.xml --settings ../maven/settings.xml dependency:sources
