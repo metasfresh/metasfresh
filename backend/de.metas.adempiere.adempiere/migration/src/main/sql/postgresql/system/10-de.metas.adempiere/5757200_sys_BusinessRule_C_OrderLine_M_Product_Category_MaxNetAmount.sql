66ff2eb0fec994530ce17242fc2305556ac9c5d8
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-06-10 20:04:05 +0300

    improve valrule

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5757200_sys_BusinessRule_C_OrderLine_M_Product_Category_MaxNetAmount.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5757200_sys_BusinessRule_C_OrderLine_M_Product_Category_MaxNetAmount.sql
@@ -25,17 +25,21 @@ INSERT INTO AD_BusinessRule (AD_BusinessRule_ID,AD_Client_ID,AD_Org_ID,AD_Table_
 
 
 
--- Name: C_OrderLine_M_Product_Category_MaxNetAmount
--- 2025-06-10T12:23:01.204Z
-UPDATE AD_Val_Rule SET Code='NOT EXISTS (SELECT 1 from
-M_Product_Category pc 
-JOIN M_Product p ON pc.M_Product_Category_ID = p.M_Product_Category_ID
-JOIN M_Product_Category_MaxNetAmount pcm ON pc.M_Product_Category_ID = pcm.M_Product_Category_ID
-WHERE p.M_Product_ID = C_OrderLine.M_Product_ID
-AND pcm.MaxNetAmount < C_OrderLine.LineNetAmt)',
-Updated=TO_TIMESTAMP('2025-06-10 12:23:01.204000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',
-UpdatedBy=100 WHERE AD_Val_Rule_ID=540732
-;
+-- -- Name: C_OrderLine_M_Product_Category_MaxNetAmount
+-- -- 2025-06-10T12:23:01.204Z
+-- UPDATE AD_Val_Rule SET Code='NOT EXISTS (SELECT 1 from
+-- M_Product_Category pc 
+-- JOIN M_Product p ON pc.M_Product_Category_ID = p.M_Product_Category_ID
+-- JOIN M_Product_Category_MaxNetAmount pcm ON pc.M_Product_Category_ID = pcm.M_Product_Category_ID
+-- WHERE p.M_Product_ID = C_OrderLine.M_Product_ID
+-- AND pcm.MaxNetAmount < C_OrderLine.LineNetAmt)',
+-- Updated=TO_TIMESTAMP('2025-06-10 12:23:01.204000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',
+-- UpdatedBy=100 WHERE AD_Val_Rule_ID=540732
+-- ;
+
+
+
+
 
 -- Name: C_OrderLine_M_Product_Category_MaxNetAmount
 -- 2025-06-10T12:23:10.685Z
@@ -96,4 +100,15 @@ UPDATE AD_Message base SET MsgText=trl.MsgText, Updated=trl.Updated, UpdatedBy=t
 
 
 
+
+-- Name: C_OrderLine_M_Product_Category_MaxNetAmount
+-- 2025-06-10T16:49:51.867Z
+UPDATE AD_Val_Rule SET Code='NOT EXISTS (SELECT 1 from
+M_Product_Category pc
+JOIN M_Product p ON pc.M_Product_Category_ID = p.M_Product_Category_ID
+JOIN M_Product_Category_MaxNetAmount pcm ON pc.M_Product_Category_ID = pcm.M_Product_Category_ID
+WHERE p.M_Product_ID = C_OrderLine.M_Product_ID
+AND pcm.C_Currency_ID = C_OrderLine.C_Currency_ID
+AND pcm.MaxNetAmount < C_OrderLine.LineNetAmt)',Updated=TO_TIMESTAMP('2025-06-10 16:49:51.867000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Val_Rule_ID=540732
+;
 
