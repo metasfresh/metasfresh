bd123d74c320b63609e15ed48a26d5f1b4723ccd
Teodor Sarca <teo.sarca@metasfresh.com>
2025-06-11 15:36:40 +0300

    improve playwright test

--- misc/services/mobile-webui/mobile-webui-frontend-testing/tests/spec/manufacturing/receive_using_customQRCodeFormat.spec.js
+++ misc/services/mobile-webui/mobile-webui-frontend-testing/tests/spec/manufacturing/receive_using_customQRCodeFormat.spec.js
@@ -6,7 +6,7 @@ import { ManufacturingJobsListScreen } from '../../utils/screens/manufacturing/M
 import { ManufacturingJobScreen } from '../../utils/screens/manufacturing/ManufacturingJobScreen';
 import { MaterialReceiptLineScreen } from '../../utils/screens/manufacturing/receipt/MaterialReceiptLineScreen';
 
-const createMasterdata = async ({ customQRCodeFormats }) => {
+const createMasterdata = async () => {
     return await Backend.createMasterdata({
         language: "en_US",
         request: {
@@ -45,30 +45,38 @@ const createMasterdata = async ({ customQRCodeFormats }) => {
                     datePromised: '2025-03-01T00:00:00.000+02:00',
                 },
             },
-            customQRCodeFormats,
+            customQRCodeFormats: [
+                {
+                    name: 'my custom code 1',
+                    parts: [
+                        { startPosition: 1, endPosition: 1, type: 'CONSTANT', constantValue: 'A' },
+                        { startPosition: 2, endPosition: 7, type: 'PRODUCT_CODE' },
+                        { startPosition: 8, endPosition: 8, type: 'CONSTANT', constantValue: 'G' },
+                        { startPosition: 9, endPosition: 14, type: 'WEIGHT_KG', decimalPointPosition: 3 },
+                        { startPosition: 15, endPosition: 15, type: 'CONSTANT', constantValue: 'C' },
+                        { startPosition: 16, endPosition: 23, type: 'LOT' },
+                        { startPosition: 24, endPosition: 24, type: 'CONSTANT', constantValue: 'E' },
+                        { startPosition: 25, endPosition: 30, type: 'IGNORE' },
+                    ],
+                },
+                {
+                    name: 'my custom code 2',
+                    parts: [
+                        { startPosition: 1, endPosition: 4, type: 'PRODUCT_CODE' },
+                        { startPosition: 5, endPosition: 10, type: 'WEIGHT_KG' },
+                        { startPosition: 11, endPosition: 18, type: 'LOT' },
+                        { startPosition: 19, endPosition: 24, type: 'IGNORE' },
+                        { startPosition: 25, endPosition: 30, type: 'BEST_BEFORE_DATE', dateFormat: 'yyMMdd' },
+                    ],
+                }
+            ],
         }
     });
 }
 
 // noinspection JSUnusedLocalSymbols
-test('Receive using custom QR Code format (1)', async ({ page }) => {
-    const masterdata = await createMasterdata({
-        customQRCodeFormats: [
-            {
-                name: 'my custom code 1',
-                parts: [
-                    { startPosition: 1, endPosition: 1, type: 'CONSTANT', constantValue: 'A' },
-                    { startPosition: 2, endPosition: 7, type: 'PRODUCT_CODE' },
-                    { startPosition: 8, endPosition: 8, type: 'CONSTANT', constantValue: 'G' },
-                    { startPosition: 9, endPosition: 14, type: 'WEIGHT_KG', decimalPointPosition: 3 },
-                    { startPosition: 15, endPosition: 15, type: 'CONSTANT', constantValue: 'C' },
-                    { startPosition: 16, endPosition: 23, type: 'LOT' },
-                    { startPosition: 24, endPosition: 24, type: 'CONSTANT', constantValue: 'E' },
-                    { startPosition: 25, endPosition: 30, type: 'IGNORE' },
-                ],
-            }
-        ]
-    });
+test('Receive using custom QR Code format', async ({ page }) => {
+    const masterdata = await createMasterdata();
 
     await LoginScreen.login(masterdata.login.user);
     await ApplicationsListScreen.expectVisible();
@@ -79,7 +87,7 @@ test('Receive using custom QR Code format (1)', async ({ page }) => {
 
     const bomQRCode = await ManufacturingJobScreen.generateSingleHUQRCode({ piTestId: masterdata.packingInstructions.BOM_PI.tuPITestId, numberOfHUs: 1 });
 
-    {
+    await test.step('Receive using custom code 1', async () => {
         await ManufacturingJobScreen.clickReceiveButton({ index: 1 });
         await MaterialReceiptLineScreen.selectExistingHUTarget({ huQRCode: bomQRCode });
         // NOTE: the product code in the QR code is not validated because, at the moment, product codes from any type of barcode are not being validated.
@@ -93,39 +101,9 @@ test('Receive using custom QR Code format (1)', async ({ page }) => {
                 }
             }
         });
-    }
-
-    await ManufacturingJobScreen.goBack();
-})
-;
-
-// noinspection JSUnusedLocalSymbols
-test('Receive using custom QR Code format (2)', async ({ page }) => {
-    const masterdata = await createMasterdata({
-        customQRCodeFormats: [
-            {
-                name: 'my custom code 2',
-                parts: [
-                    { startPosition: 1, endPosition: 4, type: 'PRODUCT_CODE' },
-                    { startPosition: 5, endPosition: 10, type: 'WEIGHT_KG' },
-                    { startPosition: 11, endPosition: 18, type: 'LOT' },
-                    { startPosition: 19, endPosition: 24, type: 'IGNORE' },
-                    { startPosition: 25, endPosition: 30, type: 'BEST_BEFORE_DATE', dateFormat: 'yyMMdd' },
-                ],
-            }
-        ]
     });
 
-    await LoginScreen.login(masterdata.login.user);
-    await ApplicationsListScreen.expectVisible();
-    await ApplicationsListScreen.startApplication('mfg');
-    await ManufacturingJobsListScreen.waitForScreen();
-
-    await ManufacturingJobsListScreen.startJob({ documentNo: masterdata.manufacturingOrders.PP1.documentNo });
-
-    const bomQRCode = await ManufacturingJobScreen.generateSingleHUQRCode({ piTestId: masterdata.packingInstructions.BOM_PI.tuPITestId, numberOfHUs: 1 });
-
-    {
+    await test.step('Receive using custom code 2', async () => {
         await ManufacturingJobScreen.clickReceiveButton({ index: 1 });
         await MaterialReceiptLineScreen.selectExistingHUTarget({ huQRCode: bomQRCode });
         // NOTE: the product code in the QR code is not validated because, at the moment, product codes from any type of barcode are not being validated.
@@ -134,12 +112,12 @@ test('Receive using custom QR Code format (2)', async ({ page }) => {
         await Backend.expect({
             hus: {
                 [bomQRCode]: {
-                    storages: { 'BOM': '1 PCE' },
-                    attributes: { 'WeightNet': '99.999' }
+                    storages: { 'BOM': '2 PCE' },
+                    attributes: { 'WeightNet': '100.383' } // 0.384 + 99.999
                 }
             }
         });
-    }
+    });
 
     await ManufacturingJobScreen.goBack();
 });
