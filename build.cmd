7a7bb9e75d9e412261409f02c73f3f942498e39d
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-06-26 13:49:35 +0200

    fix a few more docker-build-problems

--- build.cmd
+++ build.cmd
@@ -1,15 +1,23 @@
 @echo off
 
+@REM Simple Script to run a docker-based build - kindof like github-actions are doing it
+@REM
+@REM
 @REM This skript now builds against a local docker registry that runs on localhost:5000
 @REM
-@REM If you want to build against dockerhub instead, you need to
+@REM If instead you want to build against dockerhub, you need to
 @REM set "registry="
-
 echo --------------------------
 echo starting local docker-registry that persists data between script restarts
 echo --------------------------
 docker run -d -p 5000:5000 --name local-registry -v local-registry-data:/var/lib/registry registry:2
 set "registry=localhost:5000/"
+
+@REM This skript now runs with the maven build-settings from misc/dev-support/maven
+@REM
+@REM If instead you want to run with the maven-settings that the github-action runs with, you need to
+@REM set "maven_settings=docker-builds\mvn\settings.xml"
+set "maven_settings=misc\dev-support\maven\settings.xml"
 
 set "pubregistry=%registry%metasfresh"
 set "qualifier=local"
@@ -18,7 +26,7 @@ set /P version=<docker-builds/version.info
 for /F "tokens=*" %%g in ('powershell -Command "& {Get-Date}"') do (set buildtime=%%g)
 for /F "tokens=*" %%g in ('powershell -Command "& {Get-Date -Format """"yyMMddHHmm""""}"') do (set buildnr=%%g)
 
-if not exist docker-builds\mvn\local-settings.xml ( copy docker-builds\mvn\settings.xml docker-builds\mvn\local-settings.xml)
+if not exist docker-builds\mvn\local-settings.xml ( copy %maven_settings% docker-builds\mvn\local-settings.xml)
 
 (
 	echo build.name=local windows
@@ -53,15 +61,15 @@ echo.
 @echo building maven artifacts
 @echo --------------------------
 
-docker build -f docker-builds/Dockerfile.common --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-mvn-common:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.backend --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-mvn-backend:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.backend.dist --build-arg REGISTRY=%registry% -t %pubregistry%/metas-mvn-backend-dist:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.camel --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-mvn-camel:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.camel.dist --build-arg REGISTRY=%registry% -t %pubregistry%/metas-mvn-camel-dist:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.common --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-mvn-common:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.backend --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-mvn-backend:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.backend.dist --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-mvn-backend-dist:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.camel --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-mvn-camel:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.camel.dist --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-mvn-camel-dist:%qualifier% . || @goto error
 
-docker build -f docker-builds/Dockerfile.junit --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-junit:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.camel.junit --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-camel-junit:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.cucumber --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-cucumber:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.junit --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-junit:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.camel.junit --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-camel-junit:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.cucumber --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-cucumber:%qualifier% . || @goto error
 
 
 @echo.
@@ -69,30 +77,30 @@ docker build -f docker-builds/Dockerfile.cucumber --build-arg REGISTRY=%registry
 @echo building deployables
 @echo --------------------------
 
-docker build -f docker-builds/Dockerfile.backend.api --build-arg REGISTRY=%registry% -t %pubregistry%/metas-api:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.backend.app --build-arg REGISTRY=%registry% -t %pubregistry%/metas-app:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.camel.externalsystems --build-arg REGISTRY=%registry% -t %pubregistry%/metas-externalsystems:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.camel.edi --build-arg REGISTRY=%registry% -t %pubregistry%/metas-edi:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.frontend --build-arg REGISTRY=%registry% -t %pubregistry%/metas-frontend:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.mobile --build-arg REGISTRY=%registry% -t %pubregistry%/metas-mobile:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.mobile.test --build-arg REGISTRY=%registry% -t %pubregistry%/metas-mobile-test:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.db-init --build-arg REGISTRY=%registry% -t %pubregistry%/metas-db:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.db-migration-tool --build-arg REGISTRY=%registry% -t %pubregistry%/metas-db-migration-tool:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.db-preloaded --build-arg REGISTRY=%registry% -t %pubregistry%/metas-db:%qualifier%-preloaded . || @goto error
+docker build -f docker-builds/Dockerfile.backend.api --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-api:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.backend.app --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-app:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.camel.externalsystems --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-externalsystems:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.camel.edi --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-edi:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.frontend --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-frontend:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.mobile --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-mobile:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.mobile.test --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-mobile-test:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.db-init --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-db:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.db-migration-tool --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-db-migration-tool:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.db-preloaded --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-db:%qualifier%-preloaded . || @goto error
 
-docker build -f docker-builds/Dockerfile.procurement.backend --build-arg REGISTRY=%registry% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-procurement-backend:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.procurement.nginx --build-arg REGISTRY=%registry% -t %pubregistry%/metas-procurement-nginx:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.procurement.rabbitmq --build-arg REGISTRY=%registry% -t %pubregistry%/metas-procurement-rabbitmq:%qualifier% . || @goto error
-docker build -f docker-builds/Dockerfile.procurement.frontend --build-arg REGISTRY=%registry% -t %pubregistry%/metas-procurement-frontend:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.procurement.backend --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% --secret id=mvn-settings,src=docker-builds/mvn/local-settings.xml -t %pubregistry%/metas-procurement-backend:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.procurement.nginx --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-procurement-nginx:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.procurement.rabbitmq --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-procurement-rabbitmq:%qualifier% . || @goto error
+docker build -f docker-builds/Dockerfile.procurement.frontend --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-procurement-frontend:%qualifier% . || @goto error
 
 @echo --------------------------
 @echo building classic-compatible deployables
 @echo --------------------------
 
-docker build -f docker-builds/Dockerfile.backend.api.compat --build-arg REGISTRY=%registry% -t %pubregistry%/metas-api:%qualifier%-compat . || @goto error
-docker build -f docker-builds/Dockerfile.backend.app.compat --build-arg REGISTRY=%registry% -t %pubregistry%/metas-app:%qualifier%-compat . || @goto error
-docker build -f docker-builds/Dockerfile.mobile.compat --build-arg REGISTRY=%registry% -t %pubregistry%/metas-mobile:%qualifier%-compat . || @goto error
-docker build -f docker-builds/Dockerfile.frontend.compat --build-arg REGISTRY=%registry% -t %pubregistry%/metas-frontend:%qualifier%-compat . || @goto error
+docker build -f docker-builds/Dockerfile.backend.api.compat --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-api:%qualifier%-compat . || @goto error
+docker build -f docker-builds/Dockerfile.backend.app.compat --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-app:%qualifier%-compat . || @goto error
+docker build -f docker-builds/Dockerfile.mobile.compat --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-mobile:%qualifier%-compat . || @goto error
+docker build -f docker-builds/Dockerfile.frontend.compat --build-arg REGISTRY=%registry% --build-arg REFNAME=%qualifier% -t %pubregistry%/metas-frontend:%qualifier%-compat . || @goto error
 
 
 :success
