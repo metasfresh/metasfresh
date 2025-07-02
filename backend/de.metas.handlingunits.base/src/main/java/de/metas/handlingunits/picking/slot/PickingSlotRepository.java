f1b46fd0bd1b0522099360f6e20daf8558745a22
Teodor Sarca <teo.sarca@metasfresh.com>
2025-07-02 16:14:18 +0300

    picking slots API QA/improvements

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/slot/PickingSlotRepository.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/slot/PickingSlotRepository.java
@@ -1,9 +1,10 @@
 package de.metas.handlingunits.picking.slot;
 
-import de.metas.handlingunits.model.I_M_PickingSlot;
 import de.metas.picking.api.IPickingSlotDAO;
 import de.metas.picking.api.PickingSlotId;
 import de.metas.picking.api.PickingSlotIdAndCaption;
+import de.metas.picking.api.PickingSlotQuery;
+import de.metas.picking.model.I_M_PickingSlot;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.springframework.stereotype.Repository;
@@ -26,6 +27,11 @@ public class PickingSlotRepository
 		return pickingSlotDAO.getByIds(pickingSlotIds, I_M_PickingSlot.class);
 	}
 
+	public List<I_M_PickingSlot> list(@NonNull final PickingSlotQuery query)
+	{
+		return pickingSlotDAO.retrievePickingSlots(query);
+	}
+
 	public void save(final @NonNull I_M_PickingSlot pickingSlot)
 	{
 		pickingSlotDAO.save(pickingSlot);
@@ -39,6 +45,11 @@ public class PickingSlotRepository
 	public Set<PickingSlotIdAndCaption> getPickingSlotIdAndCaptions(@NonNull final Set<PickingSlotId> pickingSlotIds)
 	{
 		return pickingSlotDAO.getPickingSlotIdAndCaptions(pickingSlotIds);
+	}
+
+	public Set<PickingSlotIdAndCaption> getPickingSlotIdAndCaptions(@NonNull final PickingSlotQuery query)
+	{
+		return pickingSlotDAO.retrievePickingSlotIdAndCaptions(query);
 	}
 
 }
