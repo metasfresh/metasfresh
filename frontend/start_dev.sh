6285abeed0cfe430f7fc85216fdd9a73a4db0efe
Teodor Sarca <teo.sarca@metasfresh.com>
2025-07-09 22:03:00 +0300

    start_dev.sh: use node 20

--- frontend/start_dev.sh
+++ frontend/start_dev.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-NODE_VERSION=14
-#export NODE_OPTIONS=--openssl-legacy-provider
+NODE_VERSION=20
+echo "NODE version: ${NODE_VERSION}"
 
 set -e
 
+export NODE_OPTIONS=--openssl-legacy-provider
 npx "--package=node@${NODE_VERSION}" yarn install
 npx "--package=node@${NODE_VERSION}" yarn start
