e27dc2b81b4ff809dd105ee66f4b3afdab8e5954
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-07-18 12:44:13 +0200

    Update mvn_build script and clean env
    Revamped `mvn_build.sh` to include customizable multithreading, additional parameters, and enhanced build goals. Cleaned up the deep_tundra_uat.env file by removing an obsolete database image reference.

--- misc/dev-support/bootstrap-scripts/mvn_build.sh
+++ misc/dev-support/bootstrap-scripts/mvn_build.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
+#
+# %L
+# master
+# %%
+# Copyright (C) 2025 metas GmbH
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/gpl-2.0.html>.
+# L%
+#
+
 set -e
 
-mvn --file ../../parent-pom/pom.xml -DskipTests --settings ../maven/settings.xml clean install
+#needed if your locally installed "default" java is not the one expected by maven
+#export JAVA_HOME=c:/Users/tobia/.jdks/temurin-21.0.7 
+#export JAVA_HOME=c:/Users/tobia/.jdks/temurin-17.0.15
 
-mvn --file ../../de-metas-common/pom.xml -DskipTests --settings ../maven/settings.xml clean install
+#MULTITHREAD_PARAM=''
+MULTITHREAD_PARAM='-T2C'
 
-mvn --file ../../../backend/pom.xml -T2C -DskipTests --settings ../maven/settings.xml clean install
+ADDITIONAL_PARAMS='-Djib.skip=true -Dmaven.gitcommitid.skip=true -Dlicense.skip=true'
+#ADDITIONAL_PARAMS='${ADDITIONAL_PARAMS} -DskipTests'
+#ADDITIONAL_PARAMS='${ADDITIONAL_PARAMS} -Dmaven.test.skip=true'
+
+#GOALS='test'
+GOALS='clean install'
+
+mvn --file ../../parent-pom/pom.xml --settings ../maven/settings.xml $ADDITIONAL_PARAMS $GOALS && \
+
+mvn --file ../../de-metas-common/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS $GOALS && \
+
+mvn --file ../../../backend/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS $GOALS && \
+
+mvn --file ../../services/camel/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS $GOALS && \
+
+mvn --file ../../services/procurement-webui/procurement-webui-backend/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS $GOALS && \
+
+mvn --file ../../services/federated-rabbitmq/pom.xml --settings ../maven/settings.xml $MULTITHREAD_PARAM $ADDITIONAL_PARAMS $GOALS
+
+echo "$(date): Done"
