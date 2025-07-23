eb633cefec8b1a0dd473519af56e0392298f5014
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-07-23 17:50:23 +0300

    Set shipper in shipper only if the shipment is created manually

--- backend/de.metas.business/src/main/java/de/metas/shipment/model/interceptor/M_InOut.java
+++ backend/de.metas.business/src/main/java/de/metas/shipment/model/interceptor/M_InOut.java
@@ -14,6 +14,7 @@ import org.adempiere.ad.modelvalidator.annotations.DocValidate;
 import org.adempiere.ad.modelvalidator.annotations.Interceptor;
 import org.adempiere.ad.modelvalidator.annotations.ModelChange;
 import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.model.I_M_InOut;
 import org.compiere.model.ModelValidator;
 import org.slf4j.MDC.MDCCloseable;
@@ -113,7 +114,10 @@ public class M_InOut
 	})
 	public void onBPartnerLocation(final I_M_InOut inout)
 	{
-		inOutBL.setShipperId(inout);
+		if (InterfaceWrapperHelper.isUIAction(inout))
+		{
+			inOutBL.setShipperId(inout);
+		}
 	}
 
 }
