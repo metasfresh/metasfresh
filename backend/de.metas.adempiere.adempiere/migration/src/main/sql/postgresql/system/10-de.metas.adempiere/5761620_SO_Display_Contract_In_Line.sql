acaba65369ac9bfe00e750e608db2b6a78bb61d7
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-07-30 13:05:44 +0300

    Display contract in sales order line vanilla

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5761620_SO_Display_Contract_In_Line.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5761620_SO_Display_Contract_In_Line.sql
@@ -24,12 +24,6 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=280,Updated=TO_TIMESTAMP
 
 -- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Pauschale - Vertragsperiode
 -- Column: C_OrderLine.C_Flatrate_Term_ID
--- 2025-07-30T09:54:24.852Z
-UPDATE AD_UI_Element SET SeqNo=165,Updated=TO_TIMESTAMP('2025-07-30 09:54:24.852000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=601388
-;
-
--- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Pauschale - Vertragsperiode
--- Column: C_OrderLine.C_Flatrate_Term_ID
 -- 2025-07-30T09:54:34.783Z
 UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2025-07-30 09:54:34.783000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=601388
 ;
