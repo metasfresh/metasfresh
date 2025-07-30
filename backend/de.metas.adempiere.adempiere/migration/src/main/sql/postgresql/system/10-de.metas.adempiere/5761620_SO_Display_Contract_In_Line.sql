034a616d3beadd0f2fee620dd6a6a08b9690da51
metas_rc <ruxandra.craciunescu@metasfresh.com>
2025-07-30 12:58:54 +0300

    Display contract in sales order line vanilla

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5761620_SO_Display_Contract_In_Line.sql
@@ -0,0 +1,36 @@
+-- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Gruppen Zeile
+-- Column: C_OrderLine.IsGroupCompensationLine
+-- 2025-07-30T09:52:03.978Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=270,Updated=TO_TIMESTAMP('2025-07-30 09:52:03.978000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=549127
+;
+
+-- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Pauschale - Vertragsperiode
+-- Column: C_OrderLine.C_Flatrate_Term_ID
+-- 2025-07-30T09:52:04.358Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=280,Updated=TO_TIMESTAMP('2025-07-30 09:52:04.358000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=601388
+;
+
+-- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Pauschale - Vertragsperiode
+-- Column: C_OrderLine.C_Flatrate_Term_ID
+-- 2025-07-30T09:52:13.576Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=270,Updated=TO_TIMESTAMP('2025-07-30 09:52:13.576000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=601388
+;
+
+-- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Gruppen Zeile
+-- Column: C_OrderLine.IsGroupCompensationLine
+-- 2025-07-30T09:52:13.960Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=280,Updated=TO_TIMESTAMP('2025-07-30 09:52:13.960000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=549127
+;
+
+-- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Pauschale - Vertragsperiode
+-- Column: C_OrderLine.C_Flatrate_Term_ID
+-- 2025-07-30T09:54:24.852Z
+UPDATE AD_UI_Element SET SeqNo=165,Updated=TO_TIMESTAMP('2025-07-30 09:54:24.852000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=601388
+;
+
+-- UI Element: Auftrag_OLD(143,D) -> Auftragsposition(187,D) -> main -> 10 -> main.Pauschale - Vertragsperiode
+-- Column: C_OrderLine.C_Flatrate_Term_ID
+-- 2025-07-30T09:54:34.783Z
+UPDATE AD_UI_Element SET IsAdvancedField='N',Updated=TO_TIMESTAMP('2025-07-30 09:54:34.783000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=601388
+;
+
