f2f27d98652570e3f66b4abf1316a6a49d7cd445
dm-ps <83770997+dm-ps@users.noreply.github.com>
2025-08-06 15:21:33 +0300

    fix deleted process referenced in other records (#21165)
    * fix deleted process referenced in other records

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5761120_sys_gh24365_remove_outdated_process.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5761120_sys_gh24365_remove_outdated_process.sql
@@ -15,6 +15,11 @@ DELETE FROM AD_Menu WHERE AD_Menu_ID=540062
 DELETE FROM AD_TreeNodeMM n WHERE Node_ID=540062 AND EXISTS (SELECT * FROM AD_Tree t WHERE t.AD_Tree_ID=n.AD_Tree_ID AND t.AD_Table_ID=116)
 ;
 
+UPDATE ad_archive
+SET ad_process_id = 540061, Updated=TO_TIMESTAMP('2025-08-05 17:11:51.444000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=99
+WHERE ad_process_id = 540046
+;
+
 -- Value: Speditionsauftrag (Jasper)
 -- Classname: de.metas.report.jasper.client.process.JasperReportStarter
 -- JasperReport: @PREFIX@de/metas/docs/sales/shippingorder/report.jasper
