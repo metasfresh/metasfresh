bc5bc6769a13972b860d2b69ae3ff385d07cea4f
riad.mennour <riad.mennour@gmail.com>
2025-08-13 20:12:29 +0100

    `Add Costs` process update

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5762660_sys_gh24697_Add_Costs_Process_Update.sql
@@ -0,0 +1,8 @@
+-- Run mode: SWING_CLIENT
+
+-- Process: C_Order_CreateCost(de.metas.ui.web.order.costs.C_Order_CreateCost)
+-- ParameterName: C_BPartner_ID
+-- 2025-08-13T19:04:37.499Z
+UPDATE AD_Process_Para SET AD_Val_Rule_ID=540353,Updated=TO_TIMESTAMP('2025-08-13 19:04:37.499000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_Para_ID=542538
+;
+
