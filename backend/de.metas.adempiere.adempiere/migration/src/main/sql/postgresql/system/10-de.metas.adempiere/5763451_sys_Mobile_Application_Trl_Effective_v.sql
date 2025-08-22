762c93aea40abef0cb12d61ad28a081b9d56c4cd
metas-rc <ruxandra.craciunescu@metasfresh.com>
2025-08-22 15:45:35 +0300

    fallback on name if no custom is given

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5763451_sys_Mobile_Application_Trl_Effective_v.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5763451_sys_Mobile_Application_Trl_Effective_v.sql
@@ -6,8 +6,8 @@ SELECT
     IsTranslated, 
     -- IsUseCustomization,
     --
-    (case when IsUseCustomization='Y' then Name_Customized else Name end) as Name,
-    (case when IsUseCustomization='Y' then Description_Customized else Description end) as Description,
+    (case when IsUseCustomization='Y' then COALESCE(Name_Customized, Name) else Name end) as Name,
+    (case when IsUseCustomization='Y' then COALESCE(Description_Customized, Description) else Description end) as Description,
 
 	AD_Client_ID, AD_Org_ID, IsActive, Created, CreatedBy, Updated, UpdatedBy
 FROM Mobile_Application_Trl
