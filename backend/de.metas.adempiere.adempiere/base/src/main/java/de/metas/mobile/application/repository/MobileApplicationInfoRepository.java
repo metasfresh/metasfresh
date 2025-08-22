f1d1a6ec58ed9df211d66421c319d19195baf36a
Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
2025-08-22 14:29:14 +0300

    Custom Mobile Application Trl (#21202)
    * Mobile_Application_TRL use customization
    
    * proto
    
    * Proto
    
    * cleanup
    
    * merge MobileApplicationTranslationService.java into MobileApplicationInfoRepository
    
    * Use Mobile_Application_Trl.IsUseCustomization
    
    * cleanup
    
    ---------
    
    Co-authored-by: Teodor Sarca <teo.sarca@metasfresh.com>

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/mobile/application/repository/MobileApplicationInfoRepository.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/mobile/application/repository/MobileApplicationInfoRepository.java
@@ -1,10 +1,12 @@
 package de.metas.mobile.application.repository;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import de.metas.cache.CCache;
-import de.metas.i18n.IModelTranslationMap;
+import de.metas.i18n.ITranslatableString;
+import de.metas.i18n.TranslatableStrings;
 import de.metas.mobile.application.MobileApplicationId;
 import de.metas.mobile.application.MobileApplicationInfo;
 import de.metas.mobile.application.MobileApplicationRepoId;
@@ -12,11 +14,16 @@ import de.metas.util.GuavaCollectors;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.migration.logger.MigrationScriptFileLoggerHolder;
+import org.adempiere.ad.trx.api.ITrx;
 import org.adempiere.exceptions.AdempiereException;
-import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.model.I_Mobile_Application;
+import org.compiere.model.I_Mobile_Application_Trl;
+import org.compiere.util.DB;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -26,6 +33,8 @@ public class MobileApplicationInfoRepository
 	private final CCache<Integer, MobileApplicationInfoMap> cache = CCache.<Integer, MobileApplicationInfoMap>builder()
 			.tableName(I_Mobile_Application.Table_Name)
 			.build();
+
+	private static final String FUNCTION_update_Mobile_Application_TRLs = "update_Mobile_Application_TRLs";
 
 	public MobileApplicationInfo getById(final MobileApplicationId applicationId)
 	{
@@ -44,26 +53,72 @@ public class MobileApplicationInfoRepository
 
 	private MobileApplicationInfoMap retrieveMap()
 	{
-		return queryBL.createQueryBuilder(I_Mobile_Application.class)
+		final List<I_Mobile_Application> records = queryBL.createQueryBuilder(I_Mobile_Application.class)
+				.addOnlyActiveRecordsFilter()
+				.create()
+				.list();
+
+		final ImmutableListMultimap<MobileApplicationRepoId, I_Mobile_Application_Trl> recordTrls = queryBL.createQueryBuilder(I_Mobile_Application_Trl.class)
 				.addOnlyActiveRecordsFilter()
 				.create()
 				.stream()
-				.map(MobileApplicationInfoRepository::fromRecord)
+				.collect(ImmutableListMultimap.toImmutableListMultimap(
+						MobileApplicationInfoRepository::extractId,
+						recordTrl -> recordTrl
+				));
+
+		return records.stream()
+				.map(record -> fromRecord(record, recordTrls.get(extractId(record))))
 				.collect(GuavaCollectors.collectUsingListAccumulator(MobileApplicationInfoMap::new));
 	}
 
-	private static MobileApplicationInfo fromRecord(final I_Mobile_Application record)
+	private static MobileApplicationInfo fromRecord(
+			@NonNull final I_Mobile_Application record,
+			@NonNull final ImmutableList<I_Mobile_Application_Trl> recordTrls)
 	{
-		final IModelTranslationMap trls = InterfaceWrapperHelper.getModelTranslationMap(record);
-
 		return MobileApplicationInfo.builder()
-				.repoId(MobileApplicationRepoId.ofRepoId(record.getMobile_Application_ID()))
+				.repoId(extractId(record))
 				.id(MobileApplicationId.ofString(record.getValue()))
-				.caption(trls.getColumnTrl(I_Mobile_Application.COLUMNNAME_Name, record.getName()))
+				.caption(extractNameTrl(record, recordTrls))
 				.showInMainMenu(record.isShowInMainMenu())
 				.build();
 	}
 
+	private static ITranslatableString extractNameTrl(@NonNull final I_Mobile_Application record,
+													  @NonNull final ImmutableList<I_Mobile_Application_Trl> recordTrls)
+	{
+		final HashMap<String, String> trlMap = new HashMap<>();
+		for (final I_Mobile_Application_Trl recordTrl : recordTrls)
+		{
+			final String adLanguage = recordTrl.getAD_Language();
+			final String nameTrl = recordTrl.isUseCustomization() ? recordTrl.getName_Customized() : recordTrl.getName();
+			trlMap.put(adLanguage, nameTrl);
+		}
+
+		return TranslatableStrings.ofMap(trlMap, record.getName());
+	}
+
+	private static @NotNull MobileApplicationRepoId extractId(final I_Mobile_Application record) {return MobileApplicationRepoId.ofRepoId(record.getMobile_Application_ID());}
+
+	private static @NotNull MobileApplicationRepoId extractId(final I_Mobile_Application_Trl recordTrl) {return MobileApplicationRepoId.ofRepoId(recordTrl.getMobile_Application_ID());}
+
+	public void updateMobileApplicationTrl(final MobileApplicationRepoId mobileApplicationRepoId, @NonNull final String adLanguage)
+	{
+
+		DB.executeFunctionCallEx(
+				ITrx.TRXNAME_ThreadInherited,
+				addUpdateFunctionCall(FUNCTION_update_Mobile_Application_TRLs, mobileApplicationRepoId, adLanguage),
+				null);
+	}
+
+	@SuppressWarnings("SameParameterValue")
+	private String addUpdateFunctionCall(final String functionCall, final MobileApplicationRepoId mobileApplicationRepoId, final String adLanguage)
+	{
+		return MigrationScriptFileLoggerHolder.DDL_PREFIX + " select " + functionCall + "(" + mobileApplicationRepoId.getRepoId() + "," + DB.TO_STRING(adLanguage) + ") ";
+	}
+
+	//
+	//
 	//
 	//
 	//
