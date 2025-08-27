b9ca031fd07b5d561d4307f894fab6c07738bd46
Ruxandra Craciunescu <ruxandra.craciunescu@metasfresh.com>
2025-08-27 15:58:31 +0300

    use X12DE355 in the uom conversion json  (#21214)
    * X12DE355 instead of uom symbol in JsonMHUPIItemProduct
    
    * X12DE355 instead of uom symbol in JsonProductUOMConversion

--- backend/de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/v2/product/command/GetProductsCommand.java
+++ backend/de.metas.business.rest-api-impl/src/main/java/de/metas/rest_api/v2/product/command/GetProductsCommand.java
@@ -309,8 +309,8 @@ public class GetProductsCommand
 	private JsonProductUOMConversion toJsonProductUOMConversion(final UOMConversionRate record)
 	{
 		return JsonProductUOMConversion.builder()
-				.fromUomCode(servicesFacade.getUOMSymbol(record.getFromUomId()))
-				.toUomCode(servicesFacade.getUOMSymbol(record.getToUomId()))
+				.fromUomCode(servicesFacade.getX12DE355(record.getFromUomId()))
+				.toUomCode(servicesFacade.getX12DE355(record.getToUomId()))
 				.fromToMultiplier(record.getFromToMultiplier())
 				.catchUOMForProduct(record.isCatchUOMForProduct())
 				.build();
