04643a96fd8d38d33a9a1ca29d7b0bd48d1e71d1
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-09-01 16:26:17 +0200

    (Fix SQL from Insert to Update)

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5764750_sys_gh17549_SkipHouseKeeping_SysConfig.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5764750_sys_gh17549_SkipHouseKeeping_SysConfig.sql
@@ -2,7 +2,6 @@
 -- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5718710_sys_gh17549_SkipHouseKeeping_SysConfig.sql
 -- the value was set wrongly, so the sysconfig was ineffective
     
--- 2024-03-07T09:12:02.570Z
-INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541712,'S',TO_TIMESTAMP('2024-03-07 10:12:02','YYYY-MM-DD HH24:MI:SS'),100,'D','Y','de.metas.housekeeping.SkipHouseKeeping.ApiAuditHouseKeepingTask',TO_TIMESTAMP('2024-03-07 10:12:02','YYYY-MM-DD HH24:MI:SS'),100,'Y')
+-- 2025-09-01T09:12:02.570Z
+UPDATE AD_SysConfig SET ConfigurationLevel='S', Name='de.metas.housekeeping.SkipHouseKeeping.ApiAuditHouseKeepingTask',Updated=TO_TIMESTAMP('2024-09-01 16:25:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=541712
 ;
-
