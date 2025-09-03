f998fb8daeeaf5b3bd9b9d365fafb7cff39f3a5c
metas-rc <ruxandra.craciunescu@metasfresh.com>
2025-09-03 20:58:38 +0300

    Update Active Status Process

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5765020_sys_Process_M_ShipmentSchedule_QtyPicked_SwitchActiveStatus.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5765020_sys_Process_M_ShipmentSchedule_QtyPicked_SwitchActiveStatus.sql
@@ -172,3 +172,9 @@ UPDATE AD_Process SET SQLStatement='SELECT M_ShipmentSchedule_QtyPicked_SwitchIs
 UPDATE AD_Process SET Value='M_ShipmentSchedule_QtyPicked_SwitchActiveStatus',Updated=TO_TIMESTAMP('2025-09-03 15:55:20.444000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585495
 ;
 
+-- Value: M_ShipmentSchedule_QtyPicked_SwitchActiveStatus
+-- Classname: de.metas.process.ExecuteUpdateSQL
+-- 2025-09-03T17:45:46Z
+UPDATE AD_Process SET RefreshAllAfterExecution='Y',Updated=TO_TIMESTAMP('2025-09-03 17:45:45.998000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585495
+;
+
