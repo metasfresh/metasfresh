def7135b77fc9ca7f5845d74a9ec34bb36c2601e
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-09-10 16:48:52 +0200

    Make CacheMgt.reset() also reset remote cache

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/cache/CacheMgt.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/cache/CacheMgt.java
@@ -1,19 +1,24 @@
-/******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution *
- * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved. *
- * This program is free software; you can redistribute it and/or modify it *
- * under the terms version 2 of the GNU General Public License as published *
- * by the Free Software Foundation. This program is distributed in the hope *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. *
- * See the GNU General Public License for more details. *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the Free Software Foundation, Inc., *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA. *
- * For the text or an alternative of this public license, you may reach us *
- * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA *
- * or via info@compiere.org or http://www.compiere.org/license.html *
- *****************************************************************************/
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 package de.metas.cache;
 
 import com.google.common.base.MoreObjects;
@@ -176,6 +181,10 @@ public final class CacheMgt
 			lastCacheReset.incrementAndGet();
 
 			logger.info("Reset all: cache instances invalidated ({} cached items invalidated).", total);
+
+			CacheInvalidationRemoteHandler.instance.postEvent(CacheInvalidateMultiRequest.all());
+			logger.info("Reset all: posted invalidation request to message-bus");
+			
 			return total;
 		}
 		finally
