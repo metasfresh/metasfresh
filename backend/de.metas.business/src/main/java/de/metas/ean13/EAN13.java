edda7998617d2b423fabe117e9dd7c5cf14e9581
Teodor Sarca <teo.sarca@metasfresh.com>
2025-09-12 12:05:08 +0300

    javadoc

--- backend/de.metas.business/src/main/java/de/metas/ean13/EAN13.java
+++ backend/de.metas.business/src/main/java/de/metas/ean13/EAN13.java
@@ -1,5 +1,6 @@
 package de.metas.ean13;
 
+import de.metas.gs1.GTIN;
 import de.metas.i18n.ExplainedOptional;
 import de.metas.scannable_code.ScannedCode;
 import lombok.Builder;
@@ -11,9 +12,15 @@ import javax.annotation.Nullable;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+/**
+ * EAN-13 (European Article Number, 13 digits):
+ * A specific implementation of GTIN, namely GTIN-13.
+ * Always has 13 digits: a GS1 prefix (country/organization code), manufacturer code, product code, and a checksum.
+ * Used globally (not only in Europe any more) for retail products.
+ */
 @Builder
 @EqualsAndHashCode
-public class EAN13
+public final class EAN13
 {
 	@NonNull private final String barcode;
 	@Getter @NonNull private final EAN13Prefix prefix;
@@ -37,8 +44,17 @@ public class EAN13
 
 	public String getAsString() {return barcode;}
 
-	public Optional<BigDecimal> getWeightInKg()
-	{
-		return Optional.ofNullable(weightInKg);
-	}
+	/**
+	 * @return true if variable weight EAN13 (i.e. starts with prefix 28)
+	 */
+	public boolean isVariableWeight() {return prefix.isVariableWeight();}
+
+	/**
+	 * @return true if internal or variable measure EAN13 (i.e. starts with prefix 29)
+	 */
+	public boolean isInternalUseOrVariableMeasure() {return prefix.isInternalUseOrVariableMeasure();}
+
+	public Optional<BigDecimal> getWeightInKg() {return Optional.ofNullable(weightInKg);}
+
+	public GTIN toGTIN() {return GTIN.ofString(barcode);}
 }
--- backend/de.metas.business/src/main/java/de/metas/gs1/GTIN.java
+++ backend/de.metas.business/src/main/java/de/metas/gs1/GTIN.java
@@ -11,6 +11,18 @@ import org.adempiere.exceptions.AdempiereException;
 import javax.annotation.Nullable;
 import java.util.Optional;
 
+/**
+ * GTIN (Global Trade Item Number):
+ * <p>
+ * An umbrella term defined by GS1 for product identifiers used worldwide.
+ * Can have different lengths depending on the format:
+ * <ul>
+ * <li>GTIN-8 (8 digits)</li>
+ * <li>GTIN-12 (12 digits, typically UPC in the U.S.)</li>
+ * <li>GTIN-13 (13 digits, typically EAN-13 in Europe/most of the world)</li>
+ * <li>GTIN-14 (14 digits, used for cases/cartons).</li>
+ * </ul>
+ */
 @EqualsAndHashCode(doNotUseGetters = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 public final class GTIN
