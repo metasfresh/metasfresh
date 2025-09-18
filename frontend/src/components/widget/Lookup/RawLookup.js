ad4a5a26cb11bb8f79b8c4d9087cdcd053fb400e
Teodor Sarca <teo.sarca@metasfresh.com>
2025-09-18 22:40:46 +0300

    fix frontend compile errors

--- frontend/src/components/widget/Lookup/RawLookup.js
+++ frontend/src/components/widget/Lookup/RawLookup.js
@@ -1,19 +1,22 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import TetherComponent from "react-tether";
-import ReactDOM from "react-dom";
-import classnames from "classnames";
-import { debounce } from "throttle-debounce";
-import counterpart from "counterpart";
-import { LOOKUP_SHOW_MORE_PIXEL_NO } from "../../../constants/Constants";
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import TetherComponent from 'react-tether';
+import ReactDOM from 'react-dom';
+import classnames from 'classnames';
+import { debounce } from 'throttle-debounce';
+import counterpart from 'counterpart';
+import { LOOKUP_SHOW_MORE_PIXEL_NO } from '../../../constants/Constants';
 
-import { autocompleteModalRequest, autocompleteRequest } from "../../../actions/GenericActions";
-import { getViewAttributeTypeahead } from "../../../api";
-import { openModal } from "../../../actions/WindowActions";
-import SelectionDropdown from "../SelectionDropdown";
-import { isBlank } from "../../../utils";
-import { getViewFieldTypeahead } from "../../../api/view";
+import {
+  autocompleteModalRequest,
+  autocompleteRequest,
+} from '../../../actions/GenericActions';
+import { getViewAttributeTypeahead } from '../../../api';
+import { openModal } from '../../../actions/WindowActions';
+import SelectionDropdown from '../SelectionDropdown';
+import { isBlank } from '../../../utils';
+import { getViewFieldTypeahead } from '../../../api/view';
 
 const KEY_None = null;
 const KEY_New = 'NEW';
