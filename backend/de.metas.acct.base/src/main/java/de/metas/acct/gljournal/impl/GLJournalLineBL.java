2ad66db3590a0c559bdfab93533cbb375f8cc16e
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-09-19 15:55:24 +0300

    Clean code

--- backend/de.metas.acct.base/src/main/java/de/metas/acct/gljournal/impl/GLJournalLineBL.java
+++ backend/de.metas.acct.base/src/main/java/de/metas/acct/gljournal/impl/GLJournalLineBL.java
@@ -132,27 +132,16 @@ public class GLJournalLineBL implements IGLJournalLineBL
 		if (group == null)
 		{
 			final int lastGroupNo = glJournalLineDAO.retrieveLastGroupNo(glJournal);
-			groupNo = (lastGroupNo <= 0 ? 0 : lastGroupNo) + 10;
+			groupNo = (Math.max(lastGroupNo, 0)) + 10;
 			final boolean hasDebit  = glJournalLine.getAmtSourceDr().signum() != 0;
 			final boolean hasCredit = glJournalLine.getAmtSourceCr().signum() != 0;
 
-			if ( hasDebit && hasCredit)
-			{
-				allowAccountDr = true;
-				allowAccountCr = true;
+			allowAccountDr = hasDebit;
+			allowAccountCr = hasCredit;
 
-				// Split transaction: no, it's not a split transaction because we are just starting a new group.
-				// User would be able to enable it if he wants
-				isSplitAcctTrx = false;
-			}
-			else
-			{
-				// Only one side present:
-				// Mark as split transaction for detailed posting.
-				allowAccountDr = hasDebit;
-				allowAccountCr = hasCredit;
-				isSplitAcctTrx = true;
-			}
+			// Split transaction: no, it's not a split transaction because we are just starting a new group.
+			// User would be able to enable it if he wants
+			isSplitAcctTrx = !hasDebit || !hasCredit;
 		}
 		//
 		// Case: we found an unbalanced group, so this new journal line shall be part of that.
