533c34b076ca494d7fed194452e260a3a8c19b69
dm-ps <dragos.moca@pevesoft.ro>
2025-09-26 12:18:52 +0300

    Apply a general SQL

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5769025_sys_gh25379_fix_AD_Reference_Trls.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5769025_sys_gh25379_fix_AD_Reference_Trls.sql
@@ -1,29 +1,17 @@
-UPDATE AD_Reference_Trl
-SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='Fact_Acct_Transactions_View'
-WHERE AD_Reference_ID = 540773
-  AND AD_Language IN ('de_CH', 'de_DE')
-;
-
-UPDATE AD_Reference_Trl
-SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='R_Request Due Type'
-WHERE AD_Reference_ID = 222
-  AND AD_Language = 'fr_CH'
-;
-
-UPDATE AD_Reference_Trl
-SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='C_Queue_Workpackage'
-WHERE AD_Reference_ID = 540527
-  AND AD_Language = 'fr_CH'
-;
-
-UPDATE AD_Reference_Trl
-SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='C_Invoice_Candidate'
-WHERE AD_Reference_ID = 540266
-  AND AD_Language = 'fr_CH'
-;
-
-UPDATE AD_Reference_Trl
-SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='ItemName'
-WHERE AD_Reference_ID = 540735
-  AND AD_Language = 'fr_CH'
+UPDATE ad_reference_trl trl2
+SET Name = base2.name,
+    Updated = now(),
+    UpdatedBy = 99
+FROM ad_reference base
+         JOIN ad_reference_trl trl
+              ON base.ad_reference_id = trl.ad_reference_id
+         JOIN ad_reference_trl trl2_match
+              ON trl.ad_language = trl2_match.ad_language
+                  AND trl.name = trl2_match.name
+                  AND trl.ad_reference_id > trl2_match.ad_reference_id
+         JOIN ad_reference base2
+              ON base2.ad_reference_id = trl2_match.ad_reference_id
+WHERE trl2.ad_reference_id = trl2_match.ad_reference_id
+  AND trl2.ad_language = trl2_match.ad_language
+  AND trl2.name <> base2.name
 ;
