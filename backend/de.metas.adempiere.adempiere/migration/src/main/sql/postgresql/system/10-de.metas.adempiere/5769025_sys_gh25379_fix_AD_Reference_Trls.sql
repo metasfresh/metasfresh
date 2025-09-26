62f3c8fdbe819d681ff0c88c4add9fce1c1694d2
dm-ps <dragos.moca@pevesoft.ro>
2025-09-26 11:04:33 +0300

    Fix duplicated AD_Reference_Trl names

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5769025_sys_gh25379_fix_AD_Reference_Trls.sql
@@ -0,0 +1,29 @@
+UPDATE AD_Reference_Trl
+SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='Fact_Acct_Transactions_View'
+WHERE AD_Reference_ID = 540773
+  AND AD_Language IN ('de_CH', 'de_DE')
+;
+
+UPDATE AD_Reference_Trl
+SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='R_Request Due Type'
+WHERE AD_Reference_ID = 222
+  AND AD_Language = 'fr_CH'
+;
+
+UPDATE AD_Reference_Trl
+SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='C_Queue_Workpackage'
+WHERE AD_Reference_ID = 540527
+  AND AD_Language = 'fr_CH'
+;
+
+UPDATE AD_Reference_Trl
+SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='C_Invoice_Candidate'
+WHERE AD_Reference_ID = 540266
+  AND AD_Language = 'fr_CH'
+;
+
+UPDATE AD_Reference_Trl
+SET Updated='2025-09-26 14:43', UpdatedBy=99, Name='ItemName'
+WHERE AD_Reference_ID = 540735
+  AND AD_Language = 'fr_CH'
+;
