cbad2be8fa60bf44b8258a99e37845d1b12edd07
riad.mennour <riad.mennour@gmail.com>
2025-10-03 15:02:49 +0100

    Extend `Transport Order` and `Receipt Schedule` for Procurement

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5772340_sys_gh24946_Receipt_Schedule_Logistics_Window.sql
@@ -0,0 +1,110 @@
+-- Run mode: SWING_CLIENT
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: flags
+-- 2025-10-03T13:37:49.518Z
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,548501,553603,TO_TIMESTAMP('2025-10-03 13:37:48.565000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','flags',5,TO_TIMESTAMP('2025-10-03 13:37:48.565000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: doctype
+-- 2025-10-03T13:46:07.512Z
+INSERT INTO AD_UI_ElementGroup (AD_Client_ID,AD_Org_ID,AD_UI_Column_ID,AD_UI_ElementGroup_ID,Created,CreatedBy,IsActive,Name,SeqNo,Updated,UpdatedBy) VALUES (0,0,548501,553605,TO_TIMESTAMP('2025-10-03 13:46:06.608000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','doctype',30,TO_TIMESTAMP('2025-10-03 13:46:06.608000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> doctype.Belegart
+-- Column: M_ReceiptSchedule.C_DocType_ID
+-- 2025-10-03T13:47:37.504Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553605, SeqNo=10,Updated=TO_TIMESTAMP('2025-10-03 13:47:37.504000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637581
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> doctype.Bestellung
+-- Column: M_ReceiptSchedule.C_Order_ID
+-- 2025-10-03T13:48:03.229Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553605, SeqNo=20,Updated=TO_TIMESTAMP('2025-10-03 13:48:03.229000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637575
+;
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: doctype
+-- 2025-10-03T13:49:21.039Z
+UPDATE AD_UI_ElementGroup SET SeqNo=2,Updated=TO_TIMESTAMP('2025-10-03 13:49:21.039000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=553605
+;
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: doctype
+-- 2025-10-03T13:50:16.732Z
+UPDATE AD_UI_ElementGroup SET SeqNo=10,Updated=TO_TIMESTAMP('2025-10-03 13:50:16.732000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=553605
+;
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: flags
+-- 2025-10-03T13:50:22.247Z
+UPDATE AD_UI_ElementGroup SET SeqNo=20,Updated=TO_TIMESTAMP('2025-10-03 13:50:22.247000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=553603
+;
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: dates
+-- 2025-10-03T13:50:28.671Z
+UPDATE AD_UI_ElementGroup SET SeqNo=30,Updated=TO_TIMESTAMP('2025-10-03 13:50:28.671000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=553590
+;
+
+-- UI Column: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20
+-- UI Element Group: org
+-- 2025-10-03T13:50:36.600Z
+UPDATE AD_UI_ElementGroup SET SeqNo=40,Updated=TO_TIMESTAMP('2025-10-03 13:50:36.600000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_ElementGroup_ID=553591
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> dates.ETD
+-- Column: M_ReceiptSchedule.ETD
+-- 2025-10-03T13:54:03.513Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553590, SeqNo=40,Updated=TO_TIMESTAMP('2025-10-03 13:54:03.513000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637640
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> dates.ETA
+-- Column: M_ReceiptSchedule.ETA
+-- 2025-10-03T13:54:32.155Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553590, SeqNo=50,Updated=TO_TIMESTAMP('2025-10-03 13:54:32.155000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637641
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> dates.ATD
+-- Column: M_ReceiptSchedule.ATD
+-- 2025-10-03T13:54:52.810Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553590, SeqNo=60,Updated=TO_TIMESTAMP('2025-10-03 13:54:52.810000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637642
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> dates.ATA
+-- Column: M_ReceiptSchedule.ATA
+-- 2025-10-03T13:55:14.527Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553590, SeqNo=70,Updated=TO_TIMESTAMP('2025-10-03 13:55:14.526000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637643
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> dates.CRD
+-- Column: M_ReceiptSchedule.CRD
+-- 2025-10-03T13:55:34.923Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553590, SeqNo=80,Updated=TO_TIMESTAMP('2025-10-03 13:55:34.922000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637644
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> dates.B/L Datum
+-- Column: M_ReceiptSchedule.BLDate
+-- 2025-10-03T13:56:04.851Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553590, SeqNo=90,Updated=TO_TIMESTAMP('2025-10-03 13:56:04.851000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637649
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> flags.Buchung bestätigt
+-- Column: M_ReceiptSchedule.IsBookingConfirmed
+-- 2025-10-03T13:56:28.416Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553603, SeqNo=10,Updated=TO_TIMESTAMP('2025-10-03 13:56:28.416000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637645
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> flags.B/L erhalten
+-- Column: M_ReceiptSchedule.IsBLReceived
+-- 2025-10-03T13:56:57.082Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553603, SeqNo=20,Updated=TO_TIMESTAMP('2025-10-03 13:56:57.082000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637648
+;
+
+-- UI Element: Wareneingangsdisposition Logistik(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> main -> 20 -> flags.WE Avis
+-- Column: M_ReceiptSchedule.IsWENotice
+-- 2025-10-03T13:57:51.855Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=553603, SeqNo=30,Updated=TO_TIMESTAMP('2025-10-03 13:57:51.855000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637650
+;
+
