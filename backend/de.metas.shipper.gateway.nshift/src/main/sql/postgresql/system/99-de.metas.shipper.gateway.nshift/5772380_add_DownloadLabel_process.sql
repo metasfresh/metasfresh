acc6bcf303bc156439104e19f419217ad51a1cde
Adrian <adrian.stefan@metasfresh.com>
2025-10-03 19:33:46 +0300

    add serialization JUnits for JsonDeliveryRequest/JsonDeliveryResponse items

--- backend/de.metas.shipper.gateway.nshift/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.nshift/5772380_add_DownloadLabel_process.sql
+++ backend/de.metas.shipper.gateway.nshift/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.nshift/5772380_add_DownloadLabel_process.sql
@@ -59,3 +59,39 @@ INSERT INTO AD_Table_Process (AD_Client_ID,AD_Org_ID,AD_Process_ID,AD_Table_ID,A
 UPDATE AD_Process SET Classname='de.metas.shipper.gateway.commons.process.Carrier_ShipmentOrder_Parcel_Label_Download',Updated=TO_TIMESTAMP('2025-10-03 15:09:46.752000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585507
 ;
 
+-- UI Element: Frachtauftrag-Protokoll(541955,D) -> Carrier ShipmentOrder Log(548454,D) -> main -> 10 -> main.Anfrage-ID
+-- Column: Carrier_ShipmentOrder_Log.RequestID
+-- 2025-10-03T16:23:08.912Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,754675,0,548454,553595,637722,'F',TO_TIMESTAMP('2025-10-03 16:23:08.650000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','N','Y','N','N','N',0,'Anfrage-ID',70,0,0,TO_TIMESTAMP('2025-10-03 16:23:08.650000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- UI Element: Frachtauftrag-Protokoll(541955,D) -> Carrier ShipmentOrder Log(548454,D) -> main -> 10 -> main.Anfrage-ID
+-- Column: Carrier_ShipmentOrder_Log.RequestID
+-- 2025-10-03T16:23:15.503Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2025-10-03 16:23:15.503000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637722
+;
+
+-- UI Element: Frachtauftrag-Protokoll(541955,D) -> Carrier ShipmentOrder Log(548454,D) -> main -> 10 -> main.Shipment Order
+-- Column: Carrier_ShipmentOrder_Log.Carrier_ShipmentOrder_ID
+-- 2025-10-03T16:23:15.510Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2025-10-03 16:23:15.510000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637639
+;
+
+-- UI Element: Frachtauftrag-Protokoll(541955,D) -> Carrier ShipmentOrder Log(548454,D) -> main -> 10 -> main.Fehler
+-- Column: Carrier_ShipmentOrder_Log.IsError
+-- 2025-10-03T16:23:15.516Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2025-10-03 16:23:15.516000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637635
+;
+
+-- UI Element: Frachtauftrag-Protokoll(541955,D) -> Carrier ShipmentOrder Log(548454,D) -> main -> 10 -> main.Antwortnachricht
+-- Column: Carrier_ShipmentOrder_Log.ResponseMessage
+-- 2025-10-03T16:23:15.522Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2025-10-03 16:23:15.522000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637638
+;
+
+-- UI Element: Frachtauftrag-Protokoll(541955,D) -> Carrier ShipmentOrder Log(548454,D) -> main -> 10 -> main.Duration (ms)
+-- Column: Carrier_ShipmentOrder_Log.DurationMillis
+-- 2025-10-03T16:23:15.527Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2025-10-03 16:23:15.527000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=637636
+;
+
