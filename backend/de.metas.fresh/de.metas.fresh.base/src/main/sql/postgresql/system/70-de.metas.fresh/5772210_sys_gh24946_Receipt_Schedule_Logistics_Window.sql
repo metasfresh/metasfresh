7e984e43a948136283429d5600071390233b0fc6
riad.mennour <riad.mennour@gmail.com>
2025-10-05 09:04:19 +0100

    Fix `LowLevel` build error

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5772210_sys_gh24946_Receipt_Schedule_Logistics_Window.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5772210_sys_gh24946_Receipt_Schedule_Logistics_Window.sql
@@ -3763,6 +3763,10 @@ UPDATE AD_Window SET Overrides_Window_ID=NULL,Updated=TO_TIMESTAMP('2025-10-02 1
 UPDATE AD_Window_Trl SET Name='Wareneingangsdisposition Logistik',Updated=TO_TIMESTAMP('2025-10-02 18:36:33.560000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_DE' AND AD_Window_ID=541954
 ;
 
+-- 2025-10-02T18:36:33.871Z
+UPDATE AD_Window base SET Name=trl.Name, Updated=trl.Updated, UpdatedBy=trl.UpdatedBy FROM AD_Window_Trl trl  WHERE trl.AD_Window_ID=base.AD_Window_ID AND trl.AD_Language='de_DE' AND trl.AD_Language=getBaseLanguage()
+;
+
 -- 2025-10-02T18:36:46.270Z
 UPDATE AD_Window_Trl SET Name='Wareneingangsdisposition Logistik',Updated=TO_TIMESTAMP('2025-10-02 18:36:46.039000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Window_ID=541954
 ;
