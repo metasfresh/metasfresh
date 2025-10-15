8435362f2f0a84a8a29bb33af48359d0f521f84f
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-10-15 18:49:54 +0300

    Test changing status

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipment/M_ShipperTransportation_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipment/M_ShipperTransportation_StepDef.java
@@ -24,10 +24,17 @@ package de.metas.cucumber.stepdefs.shipment;
 
 import de.metas.cucumber.stepdefs.C_BPartner_Location_StepDefData;
 import de.metas.cucumber.stepdefs.C_BPartner_StepDefData;
+import de.metas.cucumber.stepdefs.C_Order_StepDefData;
+import de.metas.cucumber.stepdefs.DataTableRow;
+import de.metas.cucumber.stepdefs.DataTableRows;
 import de.metas.cucumber.stepdefs.DataTableUtil;
+import de.metas.cucumber.stepdefs.M_Package_StepDefData;
+import de.metas.cucumber.stepdefs.shipment.pickingterminal.M_ShippingPackage_StepDefData;
 import de.metas.cucumber.stepdefs.shipper.M_Shipper_StepDefData;
+import de.metas.shipping.api.IShipperTransportationDAO;
 import de.metas.shipping.model.I_M_ShipperTransportation;
 import de.metas.shipping.model.I_M_ShippingPackage;
+import de.metas.shipping.model.ShipperTransportationId;
 import de.metas.util.Check;
 import de.metas.util.Services;
 import io.cucumber.datatable.DataTable;
@@ -39,11 +46,15 @@ import org.assertj.core.api.SoftAssertions;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_C_BPartner_Location;
 import org.compiere.model.I_M_InOut;
+import org.compiere.model.I_M_Package;
 import org.compiere.model.I_M_Shipper;
 
+import java.sql.Timestamp;
 import java.util.Map;
 
 import static de.metas.cucumber.stepdefs.StepDefConstants.TABLECOLUMN_IDENTIFIER;
+import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
+import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @AllArgsConstructor
@@ -51,12 +62,16 @@ public class M_ShipperTransportation_StepDef
 {
 	private final M_ShipperTransportation_StepDefData deliveryInstructionTable;
 	private final M_Shipper_StepDefData shipperTable;
+	private final M_ShippingPackage_StepDefData shippingPackageTable;
+	private final M_Package_StepDefData packageTable;
 	private final C_BPartner_Location_StepDefData bPartnerLocationTable;
 	private final C_BPartner_StepDefData bPartnerTable;
 
 	private final M_InOut_StepDefData shipmentTable;
+	private final C_Order_StepDefData orderTable;
 
 	public final IQueryBL queryBL = Services.get(IQueryBL.class);
+	private final IShipperTransportationDAO shipperTransportationDAO = Services.get(IShipperTransportationDAO.class);
 
 	@And("validate M_ShipperTransportation:")
 	public void validateM_ShipperTransportation(@NonNull final DataTable dataTable)
@@ -113,5 +128,114 @@ public class M_ShipperTransportation_StepDef
 			final String shipperTransportationIdentifier = DataTableUtil.extractStringForColumnName(row, I_M_ShipperTransportation.COLUMNNAME_M_ShipperTransportation_ID + "." + TABLECOLUMN_IDENTIFIER);
 			deliveryInstructionTable.putOrReplace(shipperTransportationIdentifier, shipperTransportation);
 		}
+	}
+
+	@And("metasfresh contains M_ShipperTransportation")
+	public void add_M_ShipperTransportation(@NonNull final DataTable dataTable)
+	{
+		DataTableRows.of(dataTable)
+				.setAdditionalRowIdentifierColumnName(I_M_ShipperTransportation.COLUMNNAME_M_ShipperTransportation_ID)
+				.forEach(this::createM_ShipperTransportation);
+	}
+
+	public void createM_ShipperTransportation(@NonNull final DataTableRow row)
+	{
+		final I_M_ShipperTransportation shipperTransportationRecord = newInstance(I_M_ShipperTransportation.class);
+
+		row.getAsOptionalIdentifier(I_M_ShipperTransportation.COLUMNNAME_M_Shipper_ID)
+				.map(shipperTable::getId)
+				.ifPresent(id -> shipperTransportationRecord.setM_Shipper_ID(id.getRepoId()));
+
+		row.getAsOptionalIdentifier(I_M_ShipperTransportation.COLUMNNAME_Shipper_BPartner_ID)
+				.map(bPartnerTable::getId)
+				.ifPresent(id -> shipperTransportationRecord.setShipper_BPartner_ID(id.getRepoId()));
+
+		row.getAsOptionalIdentifier(I_M_ShipperTransportation.COLUMNNAME_Shipper_Location_ID)
+				.map(bPartnerLocationTable::getId)
+				.ifPresent(id -> shipperTransportationRecord.setShipper_Location_ID(id.getRepoId()));
+
+		saveRecord(shipperTransportationRecord);
+
+		deliveryInstructionTable.putOrReplace(row.getAsIdentifier(), shipperTransportationRecord);
+	}
+
+	@And("metasfresh contains M_ShippingPackage")
+	public void add_M_ShippingPackage(@NonNull final DataTable dataTable)
+	{
+		DataTableRows.of(dataTable)
+				.setAdditionalRowIdentifierColumnName(I_M_ShippingPackage.COLUMNNAME_M_ShippingPackage_ID)
+				.forEach(this::createM_ShippingPackage);
+	}
+
+	public void createM_ShippingPackage(@NonNull final DataTableRow row)
+	{
+		final I_M_ShippingPackage shippingPackageRecord = newInstance(I_M_ShippingPackage.class);
+
+		row.getAsOptionalIdentifier(I_M_ShipperTransportation.COLUMNNAME_M_ShipperTransportation_ID)
+				.map(deliveryInstructionTable::getId)
+				.ifPresent(id -> shippingPackageRecord.setM_ShipperTransportation_ID(id.getRepoId()));
+
+		row.getAsOptionalIdentifier(I_M_ShippingPackage.COLUMNNAME_C_Order_ID)
+				.map(orderTable::getId)
+				.ifPresent(id -> shippingPackageRecord.setC_Order_ID(id.getRepoId()));
+
+		row.getAsOptionalIdentifier(I_M_ShippingPackage.COLUMNNAME_M_InOut_ID)
+				.map(shipmentTable::getId)
+				.ifPresent(id -> shippingPackageRecord.setM_InOut_ID(id.getRepoId()));
+
+		row.getAsOptionalIdentifier(I_M_ShippingPackage.COLUMNNAME_M_Package_ID)
+				.map(packageTable::getId)
+				.ifPresent(id -> shippingPackageRecord.setM_Package_ID(id.getRepoId()));
+
+		row.getAsOptionalIdentifier(I_M_ShippingPackage.COLUMNNAME_C_BPartner_Location_ID)
+				.map(bPartnerLocationTable::getId)
+				.ifPresent(id -> shippingPackageRecord.setC_BPartner_Location_ID(id.getRepoId()));
+
+		saveRecord(shippingPackageRecord);
+
+		shippingPackageTable.putOrReplace(row.getAsIdentifier(), shippingPackageRecord);
+	}
+
+	@And("metasfresh contains M_Package")
+	public void add_M_Package(@NonNull final DataTable dataTable)
+	{
+		DataTableRows.of(dataTable)
+				.setAdditionalRowIdentifierColumnName(I_M_Package.COLUMNNAME_M_Package_ID)
+				.forEach(this::creat_M_Package);
+	}
+
+	private void creat_M_Package(@NonNull final DataTableRow row)
+	{
+		final I_M_Package packageRecord = newInstance(I_M_Package.class);
+
+		row.getAsOptionalIdentifier(I_M_Package.COLUMNNAME_M_Shipper_ID)
+				.map(shipperTable::getId)
+				.ifPresent(id -> packageRecord.setM_Shipper_ID(id.getRepoId()));
+		saveRecord(packageRecord);
+
+		packageTable.putOrReplace(row.getAsIdentifier(), packageRecord);
+	}
+
+	@And("update transport order")
+	public void update_order(@NonNull final DataTable dataTable)
+	{
+		DataTableRows.of(dataTable)
+				.setAdditionalRowIdentifierColumnName(I_M_ShipperTransportation.COLUMNNAME_M_ShipperTransportation_ID)
+				.forEach(this::updateTransportOrder);
+	}
+
+	private void updateTransportOrder(@NonNull final DataTableRow tableRow)
+	{
+		final ShipperTransportationId shipperTransportationId = tableRow.getAsIdentifier().lookupNotNullIdIn(deliveryInstructionTable);
+		final I_M_ShipperTransportation record = shipperTransportationDAO.getById(shipperTransportationId);
+
+		tableRow.getAsOptionalInstant(I_M_ShipperTransportation.COLUMNNAME_ETA)
+				.ifPresent(expected -> record.setETA(Timestamp.from(expected)));
+
+		tableRow.getAsOptionalInstant(I_M_ShipperTransportation.COLUMNNAME_BLDate)
+				.ifPresent(expected -> record.setBLDate(Timestamp.from(expected)));
+		saveRecord(record);
+
+		deliveryInstructionTable.putOrReplace(tableRow.getAsIdentifier(), record);
 	}
 }
--- backend/de.metas.cucumber/src/test/resources/de/metas/cucumber/features/purchaseOrder/purchaseOrderComplexPaymentTerm.feature
+++ backend/de.metas.cucumber/src/test/resources/de/metas/cucumber/features/purchaseOrder/purchaseOrderComplexPaymentTerm.feature
@@ -8,6 +8,9 @@ Feature: Purchase order with complex payment term
     And set sys config boolean value true for sys config SKIP_WP_PROCESSOR_FOR_AUTOMATION
     And set sys config boolean value false for sys config AUTO_SHIP_AND_INVOICE
     And metasfresh has date and time 2025-04-01T13:30:13+01:00[Europe/Berlin]
+    And load M_Shipper:
+      | Identifier  | Name |
+      | shipper_DHL | Dhl  |
     And metasfresh contains M_Warehouse:
       | M_Warehouse_ID |
       | wh             |
@@ -29,9 +32,11 @@ Feature: Purchase order with complex payment term
     And metasfresh contains C_BPartners without locations:
       | Identifier | IsVendor | IsCustomer | M_PricingSystem_ID |
       | vendor     | Y        | N          | ps_1               |
+      | shipper    | N        | N          | ps_1               |
     And metasfresh contains C_BPartner_Locations:
-      | Identifier     | C_BPartner_ID | C_Country_ID | IsShipToDefault | IsBillToDefault |
-      | vendorLocation | vendor        | CH           | Y               | Y               |
+      | Identifier      | C_BPartner_ID | C_Country_ID | IsShipToDefault | IsBillToDefault |
+      | vendorLocation  | vendor        | CH           | Y               | Y               |
+      | shipperLocation | shipper       | CH           | Y               | Y               |
     And metasfresh contains C_Tax
       | Identifier | C_TaxCategory_ID.InternalName | Name      | ValidFrom  | Rate | C_Country_ID.CountryCode | To_Country_ID.CountryCode |
       | de_ch_tax  | Normal                        | de_ch_tax | 2021-04-02 | 2.5  | DE                       | CH                        |
@@ -43,6 +48,15 @@ Feature: Purchase order with complex payment term
       | Identifier | C_PaymentTerm_ID | Percent | OffsetDays | ReferenceDateType | SeqNo |
       | PTB1       | pt_PO            | 25      | 1          | OD                | 10    |
       | PTB2       | pt_PO            | 75      | 0          | LC                | 20    |
+    And metasfresh contains C_PaymentTerm
+      | Identifier | IsComplex |
+      | pt_PO_2    | Y         |
+    And metasfresh contains C_PaymentTerm_Break
+      | Identifier | C_PaymentTerm_ID | Percent | OffsetDays | ReferenceDateType | SeqNo |
+      | PTB21      | pt_PO_2          | 25      | 1          | OD                | 10    |
+      | PTB22      | pt_PO_2          | 25      | 0          | LC                | 20    |
+      | PTB23      | pt_PO_2          | 25      | 0          | BL                | 30    |
+      | PTB24      | pt_PO_2          | 25      | 0          | ET                | 40    |
 
 
   @from:cucumber
@@ -59,11 +73,48 @@ Feature: Purchase order with complex payment term
       | C_PaymentTerm_Break_ID | DueDate    | DueAmt | Status |
       | PTB1                   | 2025-10-10 | 25.58  | WP     |
       | PTB2                   | 9999-01-01 | 76.72  | PR     |
+
+
+  @from:cucumber
+  Scenario: Order pay schedules are updated when LC date, BLDate, ETA Ddate are changed
+    When metasfresh contains C_Orders:
+      | Identifier | IsSOTrx | C_BPartner_ID | DateOrdered | DocBaseType | M_Warehouse_ID | C_PaymentTerm_ID |
+      | po2        | N       | vendor        | 2025-10-09  | POO         | wh             | pt_PO_2          |
+    And metasfresh contains C_OrderLines:
+      | Identifier | C_Order_ID | M_Product_ID | QtyEntered |
+      | po2_l1     | po2        | product      | 10         |
+    And the order identified by po2 is completed
+    Then the order identified by po2 has following pay schedules
+    # In the last line, dueamt is computed as total - previous due amounts, to avoid rounding issues
+      | C_PaymentTerm_Break_ID | DueDate    | DueAmt | Status |
+      | PTB21                  | 2025-10-10 | 25.58  | WP     |
+      | PTB22                  | 9999-01-01 | 25.58  | PR     |
+      | PTB23                  | 9999-01-01 | 25.58  | PR     |
+      | PTB24                  | 9999-01-01 | 25.56  | PR     |
     And update order
       | Identifier | LC_Date    |
-      | po1        | 2025-10-15 |
-    Then the order identified by po1 has following pay schedules
+      | po2        | 2025-10-15 |
+    Then the order identified by po2 has following pay schedules
       | C_PaymentTerm_Break_ID | DueDate    | DueAmt | Status |
-      | PTB1                   | 2025-10-10 | 25.58  | WP     |
-      | PTB2                   | 2025-10-15 | 76.72  | WP     |
-
+      | PTB21                  | 2025-10-10 | 25.58  | WP     |
+      | PTB22                  | 2025-10-15 | 25.58  | WP     |
+      | PTB23                  | 9999-01-01 | 25.58  | PR     |
+      | PTB24                  | 9999-01-01 | 25.56  | PR     |
+    And metasfresh contains M_ShipperTransportation
+      | Identifier | M_Shipper_ID | Shipper_BPartner_ID | Shipper_Location_ID |
+      | shipperTransp_1            | shipper_DHL  | shipper             | shipperLocation     |
+    And metasfresh contains M_Package
+      | Identifier | M_Shipper_ID |
+      | Pckg         | shipper_DHL  |
+    And metasfresh contains M_ShippingPackage
+      | Identifier | C_Order_ID | M_ShipperTransportation_ID | M_Package_ID | C_BPartner_Location_ID |
+      | shPckg     | po2        | shipperTransp_1            | Pckg         | shipperLocation        |
+    And update transport order
+      | M_ShipperTransportation_ID | ETA        | BLDate     |
+      | shipperTransp_1            | 2025-10-19 | 2025-10-25 |
+    Then the order identified by po2 has following pay schedules
+      | C_PaymentTerm_Break_ID | DueDate    | DueAmt | Status |
+      | PTB21                  | 2025-10-10 | 25.58  | WP     |
+      | PTB22                  | 2025-10-15 | 25.58  | WP     |
+      | PTB23                  | 2025-10-25 | 25.58  | WP     |
+      | PTB24                  | 2025-10-19 | 25.56  | WP     |
