3dfc476c3e9e2ac236e1ee2a18749b95e8717be2
Adrian <adrian.stefan@metasfresh.com>
2025-10-19 13:20:13 +0300

    default to 08:00 for pickuptimefrom and 17 for pickuptimeto if one is not defined at m_transportationOrder level

--- backend/de.metas.shipper.gateway.nshift/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.nshift/5773611_default_timeFrom_timeTo_columns.sql
+++ backend/de.metas.shipper.gateway.nshift/src/main/sql/postgresql/system/99-de.metas.shipper.gateway.nshift/5773611_default_timeFrom_timeTo_columns.sql
@@ -3,9 +3,10 @@ SELECT backup_table('carrier_shipmentorder')
 
 WITH st AS (SELECT * FROM m_shippertransportation)
 UPDATE carrier_shipmentorder
-SET pickuptimeto   = st.pickuptimeto,
-    pickuptimefrom = st.pickuptimefrom,
+SET pickuptimefrom = COALESCE(st.pickuptimefrom, TO_TIMESTAMP('2025-01-01 08:00:00', 'YYYY-MM-DD HH24:MI:SS')),
+    pickuptimeto   = COALESCE(st.pickuptimeto, TO_TIMESTAMP('2025-01-01 17:00:00', 'YYYY-MM-DD HH24:MI:SS')),
     updatedby=100,
     updated=TO_TIMESTAMP('2025-10-19 13:00:00', 'YYYY-MM-DD HH24:MI:SS')
+FROM st
 WHERE st.m_shippertransportation_id = carrier_shipmentorder.m_shippertransportation_id
 ;
