35e1f77e20f1c8de86b736895772e238ba13dc38
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2025-10-21 16:28:24 +0200

    Improve `JsonRequestBPartner`-API-Docu with language documentation (#21497)
    Also enhance `Language` utility with `@Contract` annotations

--- misc/de-metas-common/de-metas-common-bpartner/src/main/java/de/metas/common/bpartner/v2/request/JsonRequestBPartner.java
+++ misc/de-metas-common/de-metas-common-bpartner/src/main/java/de/metas/common/bpartner/v2/request/JsonRequestBPartner.java
@@ -2,7 +2,7 @@
  * #%L
  * de-metas-common-bpartner
  * %%
- * Copyright (C) 2021 metas GmbH
+ * Copyright (C) 2025 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -111,7 +111,8 @@ public class JsonRequestBPartner
 	@ApiModelProperty(hidden = true)
 	private boolean phoneSet;
 
-	@ApiModelProperty(position = 90)
+	@ApiModelProperty(position = 90, //
+			value = "Optional; the partner's language, like `de_DE` or `en_GB`" )
 	private String language;
 
 	@ApiModelProperty(hidden = true)
