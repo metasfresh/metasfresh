79bc58113d5b00749956e10817246ed2541e88cb
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-10-22 14:09:22 +0300

    Separate DDL from DML (#21501)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5773544_Make_C_Currency_ID_mandatory_into_C_InvoicePaySchedule.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5773544_Make_C_Currency_ID_mandatory_into_C_InvoicePaySchedule.sql
@@ -1,10 +1,3 @@
-UPDATE C_InvoicePaySchedule
-SET c_currency_id=i.c_currency_id, updatedby=999, updated=NOW()
-FROM c_invoice i
-WHERE i.c_invoice_id = C_InvoicePaySchedule.c_invoice_id;
-
-
-
 -- Column: C_InvoicePaySchedule.C_Currency_ID
 -- 2025-10-21T14:29:21.290Z
 UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2025-10-21 14:29:21.290000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591411
