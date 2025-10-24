4fed8369d06e743a1d8c725904e0b0172f3dcc4c
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-10-24 23:57:01 +0300

    Show Orgvalue field in the window

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5774550_AddOrgValueIntoImportAccount.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5774550_AddOrgValueIntoImportAccount.sql
@@ -15,3 +15,36 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 /* DDL */ SELECT public.db_alter_table('I_ElementValue','ALTER TABLE public.I_ElementValue ADD COLUMN OrgValue VARCHAR(60)')
 ;
 
+
+
+
+
+
+-- Field: Import Kontenrahmen(248,D) -> Import - Kontendefinition(443,D) -> Organisations-Schlüssel
+-- Column: I_ElementValue.OrgValue
+-- 2025-10-24T20:55:23.747Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,Description,DisplayLength,EntityType,FacetFilterSeqNo,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsHideGridColumnIfEmpty,IsOverrideFilterDefaultValue,IsReadOnly,IsSameLine,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,591437,755086,0,443,0,TO_TIMESTAMP('2025-10-24 20:55:23.556000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Suchschlüssel der Organisation',0,'D',0,0,'Y','Y','Y','N','N','N','N','N','N','N',0,'Organisations-Schlüssel',0,0,270,0,1,1,TO_TIMESTAMP('2025-10-24 20:55:23.556000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2025-10-24T20:55:23.761Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=755086 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2025-10-24T20:55:23.768Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(2115)
+;
+
+-- 2025-10-24T20:55:23.793Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=755086
+;
+
+-- 2025-10-24T20:55:23.801Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(755086)
+;
+
+-- UI Element: Import Kontenrahmen(248,D) -> Import - Kontendefinition(443,D) -> advanced edit -> 10 -> advanced edit.Organisations-Schlüssel
+-- Column: I_ElementValue.OrgValue
+-- 2025-10-24T20:56:29.790Z
+INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,Description,IsActive,IsAdvancedField,IsAllowFiltering,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,IsMultiLine,MultiLine_LinesCount,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy) VALUES (0,755086,0,443,541652,637951,'F',TO_TIMESTAMP('2025-10-24 20:56:29.606000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Suchschlüssel der Organisation','Y','N','N','Y','N','N','N',0,'Organisations-Schlüssel',170,0,0,TO_TIMESTAMP('2025-10-24 20:56:29.606000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
