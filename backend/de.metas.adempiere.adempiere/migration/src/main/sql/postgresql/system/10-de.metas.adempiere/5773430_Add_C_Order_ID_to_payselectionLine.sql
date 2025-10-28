c7042cc0a5ee6ebea58c6634d473e8a7042dda1b
metas-ts <tobias.schoeneberg@metasfresh.com>
2025-10-28 14:03:48 +0100

    Fix different problems that occured on an older customer-DB

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5773430_Add_C_Order_ID_to_payselectionLine.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5773430_Add_C_Order_ID_to_payselectionLine.sql
@@ -70,10 +70,17 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=754991
 /* DDL */ select AD_Element_Link_Create_Missing_Field(754991)
 ;
 
+-- on some DBs this field already exists - since 2015 - without a migration script
+UPDATE ad_element_link SET AD_Field_ID=754992 where AD_Field_ID=(select ad_field_id from ad_field where ad_tab_id=353 and ad_column_id=552758); 
+UPDATE AD_Field_Trl SET AD_Field_ID=754992 where AD_Field_ID=(select ad_field_id from ad_field where ad_tab_id=353 and ad_column_id=552758); 
+UPDATE AD_Field SET AD_Field_ID=754992 where ad_tab_id=353 and ad_column_id=552758;
+
 -- Field: Zahlung anweisen(206,D) -> Rechnung auswählen(353,D) -> Has Open Outgoing payments
 -- Column: C_PaySelectionLine.HasOpenOutgoingPayments
 -- 2025-10-16T13:09:33.691Z
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,552758,754992,0,353,TO_TIMESTAMP('2025-10-16 13:09:33.592000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,1,'D','Y','N','N','N','N','N','N','N','Has Open Outgoing payments',TO_TIMESTAMP('2025-10-16 13:09:33.592000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) 
+SELECT 0,552758,754992,0,353,TO_TIMESTAMP('2025-10-16 13:09:33.592000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,1,'D','Y','N','N','N','N','N','N','N','Has Open Outgoing payments',TO_TIMESTAMP('2025-10-16 13:09:33.592000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100
+WHERE NOT EXISTS (SELECT 1 FROM AD_Field WHERE AD_Field_ID=754992)
 ;
 
 -- 2025-10-16T13:09:33.696Z
@@ -218,6 +225,28 @@ UPDATE AD_Column SET MandatoryLogic='@C_Order_ID@ > 0',Updated=TO_TIMESTAMP('202
 -- 2025-10-17T12:31:39.741Z
 /* DDL */ SELECT public.db_alter_table('C_PaySelectionLine','ALTER TABLE public.C_PaySelectionLine ADD COLUMN C_OrderPaySchedule_ID NUMERIC(10)')
 ;
+
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.migration-sql
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 
 -- 2025-10-17T12:31:39.946Z
 ALTER TABLE C_PaySelectionLine ADD CONSTRAINT COrderPaySchedule_CPaySelectionLine FOREIGN KEY (C_OrderPaySchedule_ID) REFERENCES public.C_OrderPaySchedule DEFERRABLE INITIALLY DEFERRED
