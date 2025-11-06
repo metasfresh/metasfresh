6121ec85661f26002f0ea430184dabd2b5d6370e
Tobias Schöneberg <metas-ts@users.noreply.github.com>
2025-11-06 12:51:54 +0100

    Create a process that shall set QtyCount=QtyBooked - Korrektur für negative Mengen
    Merge pull request #21607 from metasfresh/intensive_care_hotfix_M_Inventor_Update_negative_qty_count

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/interceptor/M_InventoryLine.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/inventory/interceptor/M_InventoryLine.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.handlingunits.base
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.handlingunits.inventory.interceptor;
 
 import de.metas.handlingunits.inventory.InventoryLine;
@@ -24,28 +46,6 @@ import org.compiere.model.ModelValidator;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
-
-/*
- * #%L
- * de.metas.handlingunits.base
- * %%
- * Copyright (C) 2019 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 @Interceptor(I_M_InventoryLine.class)
 @Callout(I_M_InventoryLine.class)
@@ -83,18 +83,16 @@ public class M_InventoryLine
 		{
 			return; // nothing to do
 		}
-		else
-		{
-			final Quantity qtyCount = extractQtyCount(inventoryLineRecord);
 
-			final InventoryLine inventoryLine = inventoryLineRepository
-					.toInventoryLine(inventoryLineRecord)
-					.distributeQtyCountToHUs(qtyCount);
+		final Quantity qtyCount = extractQtyCount(inventoryLineRecord);
 
-			final InventoryId inventoryId = InventoryId.ofRepoId(inventoryLineRecord.getM_Inventory_ID());
+		final InventoryLine inventoryLine = inventoryLineRepository
+				.toInventoryLine(inventoryLineRecord)
+				.distributeQtyCountToHUs(qtyCount);
 
-			inventoryLineRepository.saveInventoryLineHURecords(inventoryLine, inventoryId);
-		}
+		final InventoryId inventoryId = InventoryId.ofRepoId(inventoryLineRecord.getM_Inventory_ID());
+
+		inventoryLineRepository.saveInventoryLineHURecords(inventoryLine, inventoryId);
 	}
 
 	private Quantity extractQtyCount(final I_M_InventoryLine inventoryLineRecord)
