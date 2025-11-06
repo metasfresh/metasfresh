e469aa93f808fa1e0c92cdaff3f011a445cf688b
Teodor Sarca <teo.sarca@metasfresh.com>
2025-11-06 02:44:11 +0200

    notifications: make sure we are not adding the same notification twice

--- frontend/src/reducers/appHandler.js
+++ frontend/src/reducers/appHandler.js
@@ -159,13 +159,27 @@ export default function appHandler(state = initialState, action) {
     }
 
     case types.NEW_NOTIFICATION: {
+      const { notification: newNotification, unreadCount } = action;
+      let added = false;
+      const notifications = state.inbox.notifications.map((item) => {
+        if (item.id === newNotification.id) {
+          added = true;
+          return newNotification;
+        } else {
+          return item;
+        }
+      });
+      if (!added) {
+        notifications.unshift(newNotification);
+      }
+
       return update(state, {
         inbox: {
           notifications: {
-            $unshift: [action.notification],
+            $set: notifications,
           },
           unreadCount: {
-            $set: action.unreadCount,
+            $set: unreadCount,
           },
         },
       });
