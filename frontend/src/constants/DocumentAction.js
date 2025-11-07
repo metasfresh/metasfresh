7ef262fb9f5653c22335f9101e4c227e2a322e06
Teodor Sarca <teo.sarca@metasfresh.com>
2025-11-07 21:54:51 +0200

    webui: hide New and Clone actions if they are not allowed for Document or View

---
+++ frontend/src/constants/DocumentAction.js
@@ -0,0 +1,18 @@
+// NOTE to dev: keep in sync with de.metas.ui.web.window.model.DocumentStandardAction
+
+export const DocumentAction = Object.freeze({
+  BREADCRUMB_CLICK: 'breadcrumbClick',
+  TOGGLE_EDIT_MODE: 'toggleEditMode',
+  ABOUT_DOCUMENT: 'aboutDocument',
+  DOWNLOAD_SELECTED: 'downloadSelected',
+  // Standard actions:
+  // keep in sync with de.metas.ui.web.window.model.DocumentStandardAction
+  NEW_DOCUMENT: 'new',
+  OPEN_ADVANCED_EDIT: 'advancedEdit',
+  CLONE_DOCUMENT: 'clone',
+  OPEN_EMAIL: 'email',
+  OPEN_LETTER: 'letter',
+  OPEN_PRINT_RAPORT: 'print',
+  DELETE_DOCUMENT: 'delete',
+  OPEN_COMMENTS: 'comments',
+});
