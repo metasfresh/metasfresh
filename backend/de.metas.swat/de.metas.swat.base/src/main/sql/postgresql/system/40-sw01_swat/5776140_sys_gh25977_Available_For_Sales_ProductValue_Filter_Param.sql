2f18517b5c4491bbd77bdc50f051eaaf297bec0f
dm-ps <dragos.moca@pevesoft.ro>
2025-11-10 17:30:35 +0200

    add ProductExternalReference filter param

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5776140_sys_gh25977_Available_For_Sales_ProductValue_Filter_Param.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/40-sw01_swat/5776140_sys_gh25977_Available_For_Sales_ProductValue_Filter_Param.sql
@@ -10,12 +10,6 @@ INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,
 INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Process_Para_ID=543029 AND NOT EXISTS (SELECT 1 FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
 ;
 
--- Value: Available_For_Sales_JSON
--- Classname: de.metas.postgrest.process.PostgRESTProcessExecutor
--- 2025-11-10T10:39:01.717Z
-UPDATE AD_Process SET JSONPath='/available_for_sales_json_v?ExternalSystem=ilike.@ExternalSystem/%@&WarehouseCode=ilike.@WarehouseCode/%@&ProductValue=ilike.@ProductValue/%@&limit=@Limit/2000@&offset=@Offset/0@',Updated=TO_TIMESTAMP('2025-11-10 10:39:01.716000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585498
-;
-
 -- Run mode: SWING_CLIENT
 
 -- Process: Available_For_Sales_JSON(de.metas.postgrest.process.PostgRESTProcessExecutor)
@@ -86,4 +80,10 @@ UPDATE AD_Process_Para SET SeqNo=16,Updated=TO_TIMESTAMP('2025-11-10 15:07:37.04
 -- ParameterName: ProductExternalReference
 -- 2025-11-10T15:07:41.910Z
 UPDATE AD_Process_Para SET SeqNo=17,Updated=TO_TIMESTAMP('2025-11-10 15:07:41.910000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_Para_ID=543030
+;
+
+-- Value: Available_For_Sales_JSON
+-- Classname: de.metas.postgrest.process.PostgRESTProcessExecutor
+-- 2025-11-10T15:29:31.706Z
+UPDATE AD_Process SET JSONPath='/available_for_sales_json_v?ExternalSystem=ilike.@ExternalSystem/%@&WarehouseCode=ilike.@WarehouseCode/%@&ProductValue=ilike.@ProductValue/%@&ProductExternalReference=ilike.@ProductExternalReference/%@&limit=@Limit/2000@&offset=@Offset/0@',Updated=TO_TIMESTAMP('2025-11-10 15:29:31.706000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585498
 ;
