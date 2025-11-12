8f434c12d799cdcdfbbe699f46142b57a35e32db
Max Rieck <max.rieck@metasfresh.com>
2025-11-12 13:30:40 +0100

    fix paymentRule to use generated value

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/payment/PaymentRule.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/payment/PaymentRule.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.adempiere.adempiere.base
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.payment;
 
 import com.google.common.collect.ImmutableMap;
@@ -12,28 +34,6 @@ import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.Optional;
 
-/*
- * #%L
- * de.metas.business
- * %%
- * Copyright (C) 2019 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
 public enum PaymentRule implements ReferenceListAwareEnum
 {
 	Cash(X_C_Order.PAYMENTRULE_Cash), // B
@@ -47,8 +47,8 @@ public enum PaymentRule implements ReferenceListAwareEnum
 	PayPalExtern(X_C_Order.PAYMENTRULE_PayPalExtern), // V
 	CreditCardExtern(X_C_Order.PAYMENTRULE_KreditkarteExtern), // U
 	InstantBankTransfer(X_C_Order.PAYMENTRULE_Sofortueberweisung), // R
-	Reimbursement(X_C_Order.PAYMENTRULE_Reimbursement), // E
-	Settlement(X_C_Order.PAYMENTRULE_Settlement) // F
+	Reimbursement(X_C_Order.PAYMENTRULE_Rueckerstattung), // E
+	Settlement(X_C_Order.PAYMENTRULE_Verrechnung) // F
 	;
 
 	@Getter
