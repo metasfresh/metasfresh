2e7a6a4ef3bea73bf2fdd9328d51f8e58b0eda3a
riad.mennour <riad.mennour@gmail.com>
2025-11-17 09:06:01 +0100

    `Business Partner Account Sheet Report` process update

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5777250_sys_gh26293_BusinessPartnerAccountSheetReport_Process_Update.sql
@@ -0,0 +1,7 @@
+-- Run mode: SWING_CLIENT
+
+-- Process: BusinessPartnerAccountSheetReport(de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess)
+-- ParameterName: IsSOTrx
+-- 2025-11-14T19:39:34.665Z
+UPDATE AD_Process_Para SET AD_Reference_ID=17, AD_Reference_Value_ID=540528,Updated=TO_TIMESTAMP('2025-11-14 20:39:34.665','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Process_Para_ID=541756
+;
