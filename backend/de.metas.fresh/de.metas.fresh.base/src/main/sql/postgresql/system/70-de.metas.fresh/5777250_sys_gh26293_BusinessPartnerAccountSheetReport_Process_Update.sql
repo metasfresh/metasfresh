186ad37454fc72302a76fbc6d337ed18bd002be7
riad.mennour <riad.mennour@gmail.com>
2025-11-17 09:06:59 +0100

    `Business Partner Account Sheet Report` process update

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5777250_sys_gh26293_BusinessPartnerAccountSheetReport_Process_Update.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5777250_sys_gh26293_BusinessPartnerAccountSheetReport_Process_Update.sql
@@ -1,7 +1,7 @@
 -- Run mode: SWING_CLIENT
 
--- Process: BusinessPartnerAccountSheetReport(de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess)
--- ParameterName: IsSOTrx
--- 2025-11-14T19:39:34.665Z
-UPDATE AD_Process_Para SET AD_Reference_ID=17, AD_Reference_Value_ID=540528,Updated=TO_TIMESTAMP('2025-11-14 20:39:34.665','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Process_Para_ID=541756
+-- Value: BusinessPartnerAccountSheetReport
+-- Classname: de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess
+-- 2025-11-14T15:17:16.070Z
+UPDATE AD_Process SET SQLStatement='SELECT * FROM BusinessPartnerAccountSheetReport_Union(p_c_bpartner_id => @C_BPartner_ID@,p_dateFrom => ''@DateFrom@''::date, p_dateTo => ''@DateTo@''::date, p_ad_client_id => @#AD_Client_ID@, p_ad_org_id => @AD_Org_ID@, p_isSoTrx => @IsSOTrx/quotedIfNotDefault/NULL@, p_ad_language => ''@#AD_Language@'')',Updated=TO_TIMESTAMP('2025-11-14 16:17:15.878','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Process_ID=584661
 ;
