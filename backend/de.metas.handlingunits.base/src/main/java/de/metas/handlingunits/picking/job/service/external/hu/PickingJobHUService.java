01bd863e306e2501e0295cae53d9c0a19839e87b
Teodor Sarca <teo.sarca@metasfresh.com>
2025-11-18 01:35:51 +0200

    remove: unused static import in PickingJobHUService

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/job/service/external/hu/PickingJobHUService.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/job/service/external/hu/PickingJobHUService.java
@@ -65,8 +65,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.adempiere.mm.attributes.api.AttributeConstants.ATTR_BestBeforeDate;
-
 @Service
 @RequiredArgsConstructor
 public class PickingJobHUService
