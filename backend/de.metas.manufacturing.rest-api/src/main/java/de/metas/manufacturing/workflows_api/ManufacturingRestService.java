5d32284a2ea1bdc9b74626b5fe5320dcef52d814
Teodor Sarca <teo.sarca@metasfresh.com>
2025-11-19 10:29:32 +0200

    Remove unused import `SelectedReceivingTarget` from ManufacturingRestService.

--- backend/de.metas.manufacturing.rest-api/src/main/java/de/metas/manufacturing/workflows_api/ManufacturingRestService.java
+++ backend/de.metas.manufacturing.rest-api/src/main/java/de/metas/manufacturing/workflows_api/ManufacturingRestService.java
@@ -12,7 +12,6 @@ import de.metas.manufacturing.job.model.ManufacturingJobFacets;
 import de.metas.manufacturing.job.model.ManufacturingJobReference;
 import de.metas.manufacturing.job.service.ManufacturingJobReferenceQuery;
 import de.metas.manufacturing.job.service.ManufacturingJobService;
-import de.metas.manufacturing.job.service.commands.SelectedReceivingTarget;
 import de.metas.manufacturing.workflows_api.activity_handlers.callExternalSystem.CallExternalSystemActivityHandler;
 import de.metas.manufacturing.workflows_api.activity_handlers.confirmation.ConfirmationActivityHandler;
 import de.metas.manufacturing.workflows_api.activity_handlers.generateHUQRCodes.GenerateHUQRCodesActivityHandler;
