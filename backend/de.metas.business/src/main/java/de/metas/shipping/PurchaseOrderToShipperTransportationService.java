044a2ef9194d590107f2ba1142b6b40761f89b60
Adrian <60300682+adi-stefan@users.noreply.github.com>
2025-11-19 18:17:19 +0200

    Simplified deletion of shipping packages logic (#21732)
    (cherry picked from commit 11f5ac32c7c825423023fcd8cbf5bc6bd3685b1f)

--- backend/de.metas.business/src/main/java/de/metas/shipping/PurchaseOrderToShipperTransportationService.java
+++ backend/de.metas.business/src/main/java/de/metas/shipping/PurchaseOrderToShipperTransportationService.java
@@ -31,6 +31,7 @@ import de.metas.bpartner.BPartnerLocationId;
 import de.metas.document.engine.DocStatus;
 import de.metas.handlingunits.ILUQtyProvider;
 import de.metas.handlingunits.impl.ShipperTransportationQuery;
+import de.metas.i18n.AdMessageKey;
 import de.metas.interfaces.I_C_OrderLine;
 import de.metas.order.IOrderBL;
 import de.metas.order.IOrderDAO;
@@ -53,6 +54,7 @@ import de.metas.util.Services;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.I_C_Order;
 import org.compiere.util.Env;
 import org.springframework.stereotype.Service;
@@ -69,6 +71,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PurchaseOrderToShipperTransportationService
 {
+	private static final AdMessageKey MSG_RECEIPT_SCHEDULE_ASSIGNED_TO_PROCESSED_TRANSPORTATION_ORDER = AdMessageKey.of("ReceiptScheduleAssignedToProcessedTransportationOrder");
 	@NonNull private final PurchaseOrderToShipperTransportationRepository repo;
 
 	private final IOrderDAO orderDAO = Services.get(IOrderDAO.class);
@@ -154,7 +157,6 @@ public class PurchaseOrderToShipperTransportationService
 		final List<I_C_OrderLine> orderLinesWithoutLUQty = getUnassignedOrderLines(orderLines.stream()
 				.filter(ol -> !orderLinesWithLUQty.contains(ol) && !ol.isPackagingMaterial())
 				.collect(Collectors.toList()));
-		getUnassignedOrderLines(orderLinesWithoutLUQty);
 
 		final BPartnerId bPartnerId = BPartnerId.ofRepoId(order.getC_BPartner_ID());
 		final BPartnerLocationId bPartnerLocationId = BPartnerLocationId.ofRepoId(bPartnerId, order.getC_BPartner_Location_ID());
@@ -279,7 +281,7 @@ public class PurchaseOrderToShipperTransportationService
 		return isDeletePossible;
 	}
 
-	public boolean deleteShippingPackagesForOrderLinesIfPossible(@NonNull final Collection<OrderLineId> orderLineIds)
+	public void deleteShippingPackagesForOrderLines(@NonNull final Collection<OrderLineId> orderLineIds)
 	{
 		final boolean isDeletePossible = !shipperTransportationDAO.anyMatch(ShipperTransportationQuery.builder()
 				.orderLineIds(orderLineIds)
@@ -289,6 +291,9 @@ public class PurchaseOrderToShipperTransportationService
 		{
 			repo.deleteBy(ShippingPackageQuery.builder().orderLineIds(orderLineIds).build());
 		}
-		return isDeletePossible;
+		else
+		{
+			throw new AdempiereException(MSG_RECEIPT_SCHEDULE_ASSIGNED_TO_PROCESSED_TRANSPORTATION_ORDER);
+		}
 	}
 }
