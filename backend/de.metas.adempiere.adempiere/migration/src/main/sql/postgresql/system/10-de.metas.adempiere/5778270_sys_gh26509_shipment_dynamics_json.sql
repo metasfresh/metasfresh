613d2b4425e1859d02c61090ef0c341eb3ad8ecb
dm-ps <dragos.moca@pevesoft.ro>
2025-11-25 17:21:52 +0200

    Export Shipments in Dynamics365 format

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5778270_sys_gh26509_shipment_dynamics_json.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5778270_sys_gh26509_shipment_dynamics_json.sql
@@ -1,12 +1,13 @@
 SELECT db_drop_functions('*.shipment_dynamics_json')
 ;
 
-CREATE OR REPLACE FUNCTION shipment_dynamics_json(p_m_inout_id numeric)
+CREATE OR REPLACE FUNCTION shipment_dynamics_json(p_m_inout_id text)
     RETURNS TABLE
             (
                 "orgCode"            text,
                 "deliveryNoteNumber" text,
                 "poReference"        text,
+
                 "dateShip"           date,
                 "partnerIdentifier"  text,
                 "partnerValue"       text,
@@ -32,10 +33,12 @@ WITH dynamics_system AS (SELECT externalsystem_id
                                  INNER JOIN m_hu hu ON assignment.m_hu_id = hu.m_hu_id
                                  INNER JOIN m_hu_attribute attribute ON attribute.m_hu_id = hu.m_hu_id
                         WHERE assignment.ad_table_id = get_table_id('M_InOutLine')
-                          AND attribute.m_attribute_id = (SELECT m_attribute_id FROM m_attribute WHERE m_attribute.value = 'SerialNo')
+                          AND attribute.m_attribute_id = (SELECT m_attribute_id
+                                                          FROM m_attribute
+                                                          WHERE m_attribute.value = 'SerialNo')
                           AND assignment.record_id IN (SELECT m_inoutline_id
                                                        FROM m_inoutline
-                                                       WHERE m_inout_id = p_m_inout_id)
+                                                       WHERE m_inout_id = p_m_inout_id::numeric)
                         GROUP BY assignment.record_id),
      shipment_lines AS (SELECT line.m_inout_id,
                                COALESCE(
@@ -54,7 +57,7 @@ WITH dynamics_system AS (SELECT externalsystem_id
                                  INNER JOIN m_product product ON product.m_product_id = line.m_product_id
                                  INNER JOIN c_uom ouom ON ouom.c_uom_id = line.c_uom_id
                                  LEFT JOIN shipping_items items ON items.record_id = line.m_inoutline_id
-                        WHERE line.m_inout_id = p_m_inout_id
+                        WHERE line.m_inout_id = p_m_inout_id::numeric
                         GROUP BY line.m_inout_id)
 SELECT org.value,
        shipment.documentno,
@@ -76,7 +79,7 @@ FROM m_inout shipment
     LIMIT 1
     ) ext_ref ON TRUE
          LEFT JOIN shipment_lines lines ON lines.m_inout_id = shipment.m_inout_id
-WHERE shipment.m_inout_id = p_m_inout_id
+WHERE shipment.m_inout_id = p_m_inout_id::numeric
   AND shipment.issotrx = 'Y';
 $$
 ;
