d1453ea458c79cec44c0eb9e2a88a96253d48290
riad.mennour <riad.mennour@gmail.com>
2025-11-26 16:47:42 +0100

    Adding `Customs Tariff` to `Receipt Schedule` window

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5778600_sys_gh26454_ReceiptSchedule_Table_Update.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5778600_sys_gh26454_ReceiptSchedule_Table_Update.sql
@@ -28,3 +28,25 @@ UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=260,Updated=TO_
 -- 2025-11-26T15:11:21.662Z
 UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=270,Updated=TO_TIMESTAMP('2025-11-26 15:11:21.662000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=549481
 ;
+
+-- Field: Wareneingangsdisposition - Einkauf_OLD(541954,de.metas.inoutcandidate) -> Wareneingangsdisposition(548451,de.metas.inoutcandidate) -> Customs Tariff
+-- Column: M_ReceiptSchedule.M_CustomsTariff_ID
+-- 2025-11-26T15:47:09.770Z
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Name_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,DisplayLength,EntityType,FacetFilterSeqNo,IncludedTabHeight,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsHideGridColumnIfEmpty,IsOverrideFilterDefaultValue,IsReadOnly,IsSameLine,MaxFacetsToFetch,Name,SelectionColumnSeqNo,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,591601,758433,577257,0,548451,0,TO_TIMESTAMP('2025-11-26 15:47:08.956000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,0,'D',0,0,'Y','Y','Y','N','N','N','N','N','N','N',0,'Customs Tariff',0,0,410,0,1,1,TO_TIMESTAMP('2025-11-26 15:47:08.956000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2025-11-26T15:47:09.849Z
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Field t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Field_ID=758433 AND NOT EXISTS (SELECT 1 FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 2025-11-26T15:47:09.951Z
+/* DDL */  select update_FieldTranslation_From_AD_Name_Element(577257)
+;
+
+-- 2025-11-26T15:47:10.035Z
+DELETE FROM AD_Element_Link WHERE AD_Field_ID=758433
+;
+
+-- 2025-11-26T15:47:10.107Z
+/* DDL */ select AD_Element_Link_Create_Missing_Field(758433)
+;
