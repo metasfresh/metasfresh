598efa974f1266c5a39ee0e22c62b19a78bc900b
Teodor Sarca <teo.sarca@metasfresh.com>
2025-11-27 23:04:06 +0200

    Remove unused class `JsonValidateProductScannedCodeResponse` from mobile distribution REST API.

--- backend/de.metas.distribution.rest-api/src/main/java/de/metas/distribution/mobileui/rest_api/json/JsonValidateProductScannedCodeResponse.java
+++
@@ -1,9 +0,0 @@
-package de.metas.distribution.mobileui.rest_api.json;
-
-import de.metas.product.ProductId;
-import lombok.NonNull;
-
-public class JsonValidateProductScannedCodeResponse
-{
-	@NonNull ProductId productId;
-}
