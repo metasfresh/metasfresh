b4de95f1052a2c39668f5de164e55090ca41edc5
Adrian <adrian.stefan@metasfresh.com>
2025-11-28 17:14:34 +0200

    Changed to use softAssertions

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/request/R_Request_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/request/R_Request_StepDef.java
@@ -22,27 +22,31 @@
 
 package de.metas.cucumber.stepdefs.request;
 
-import de.metas.common.util.time.SystemTime;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.metas.common.rest_api.common.JsonMetasfreshId;
 import de.metas.cucumber.stepdefs.DataTableRow;
 import de.metas.cucumber.stepdefs.DataTableRows;
+import de.metas.cucumber.stepdefs.context.TestContext;
+import de.metas.cucumber.stepdefs.order.C_Order_StepDefData;
 import de.metas.cucumber.stepdefs.org.AD_Org_StepDefData;
 import de.metas.organization.OrgId;
 import de.metas.request.RequestTypeId;
 import de.metas.request.api.IRequestTypeDAO;
+import de.metas.rest_api.request.JsonRRequest;
 import de.metas.util.Services;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.adempiere.ad.dao.IQueryBL;
-import org.adempiere.ad.dao.impl.CompareQueryFilter;
+import org.adempiere.ad.table.api.impl.TableIdsCache;
 import org.adempiere.exceptions.AdempiereException;
-import org.compiere.model.IQuery;
+import org.assertj.core.api.SoftAssertions;
+import org.compiere.model.I_C_Order;
 import org.compiere.model.I_R_Request;
 import org.compiere.util.TimeUtil;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 @RequiredArgsConstructor
 public class R_Request_StepDef
@@ -51,15 +55,23 @@ public class R_Request_StepDef
 	private final IRequestTypeDAO requestTypeDAO = Services.get(IRequestTypeDAO.class);
 
 	@NonNull private final AD_Org_StepDefData orgTable;
+	@NonNull private final TestContext restTestContext;
+	@NonNull private final C_Order_StepDefData orderTable;
 
-	@And("a new request exists:")
-	public void newRequestExists(@NonNull final DataTable dataTable)
+	@And("validate a new request has been created:")
+	public void newRequestExists(@NonNull final DataTable dataTable) throws JsonProcessingException
 	{
-		DataTableRows.of(dataTable)
-				.forEach(this::assertNewRequest);
+		final DataTableRows dataTableRows = DataTableRows.of(dataTable);
+		if (dataTableRows.size() != 1)
+		{
+			throw new AdempiereException("This does not support multiple rows");
+		}
+		final JsonRRequest request = restTestContext.getApiResponseBodyAs(JsonRRequest.class);
+
+		assertNewRequest(dataTableRows.singleRow(), request.getRequestId());
 	}
 
-	private void assertNewRequest(@NonNull final DataTableRow dataTableRow, final Integer ignored)
+	private void assertNewRequest(@NonNull final DataTableRow dataTableRow, final JsonMetasfreshId requestId)
 	{
 		final OrgId orgId = dataTableRow.getAsIdentifier(I_R_Request.COLUMNNAME_AD_Org_ID).lookupIdIn(orgTable);
 		final RequestTypeId requestTypeId = requestTypeDAO.retrieveRequestTypeIdByInternalName(dataTableRow.getAsString(I_R_Request.COLUMNNAME_R_RequestType_ID));
@@ -68,22 +80,27 @@ public class R_Request_StepDef
 		final String priority = dataTableRow.getAsString(I_R_Request.COLUMNNAME_Priority);
 		final String confidentialType = dataTableRow.getAsString(I_R_Request.COLUMNNAME_ConfidentialType);
 
-		final IQuery<I_R_Request> requestQuery = queryBL.createQueryBuilder(I_R_Request.class)
-				.addEqualsFilter(I_R_Request.COLUMN_AD_Org_ID, orgId)
-				.addEqualsFilter(I_R_Request.COLUMNNAME_R_RequestType_ID, requestTypeId)
-				.addEqualsFilter(I_R_Request.COLUMNNAME_Summary, summary)
-				.addEqualsFilter(I_R_Request.COLUMNNAME_DateDelivered, dateDelivered)
-				.addEqualsFilter(I_R_Request.COLUMNNAME_Priority, priority)
-				.addEqualsFilter(I_R_Request.COLUMNNAME_ConfidentialType, confidentialType)
-				.addCompareFilter(I_R_Request.COLUMN_Created, CompareQueryFilter.Operator.EQUAL, SystemTime.asTimestamp())
-				.create();
-		final List<I_R_Request> requests = requestQuery.list();
+		final I_R_Request request = queryBL.createQueryBuilder(I_R_Request.class)
+				.addEqualsFilter(I_R_Request.COLUMNNAME_R_Request_ID, requestId.getValue())
+				.create()
+				.firstOnly();
+		final SoftAssertions softAssertions = new SoftAssertions();
+		softAssertions.assertThat(orgId).as(I_R_Request.COLUMNNAME_AD_Org_ID).isEqualTo(OrgId.ofRepoId(request.getAD_Org_ID()));
+		softAssertions.assertThat(requestTypeId).as(I_R_Request.COLUMNNAME_R_RequestType_ID).isEqualTo(RequestTypeId.ofRepoIdOrNull(request.getR_RequestType_ID()));
+		softAssertions.assertThat(summary).as(I_R_Request.COLUMNNAME_Summary).isEqualTo(request.getSummary());
+		softAssertions.assertThat(dateDelivered).as(I_R_Request.COLUMNNAME_DateDelivered).isEqualTo(request.getDateDelivered());
+		softAssertions.assertThat(priority).as(I_R_Request.COLUMNNAME_Priority).isEqualTo(request.getPriority());
+		softAssertions.assertThat(confidentialType).as(I_R_Request.COLUMNNAME_ConfidentialType).isEqualTo(request.getConfidentialType());
 
-		if (requests.isEmpty())
-		{
-			throw new AdempiereException("No new request found")
-					.setParameter("query", requestQuery);
-		}
+		dataTableRow.getAsOptionalIdentifier(I_C_Order.COLUMNNAME_C_Order_ID)
+				.map(orderTable::getId)
+				.ifPresent(orderId -> {
+					softAssertions.assertThat(orderId.getRepoId()).as(I_R_Request.COLUMNNAME_C_Order_ID).isEqualTo(request.getC_Order_ID());
+					softAssertions.assertThat(TableIdsCache.instance.getTableIdNotNull(I_C_Order.Table_Name).getRepoId()).as(I_R_Request.COLUMNNAME_AD_Table_ID).isEqualTo(request.getAD_Table_ID());
+					softAssertions.assertThat(orderId.getRepoId()).as(I_R_Request.COLUMNNAME_Record_ID).isEqualTo(request.getRecord_ID());
+				});
+
+		softAssertions.assertAll();
 
 	}
 }
--- backend/de.metas.cucumber/src/test/resources/de/metas/cucumber/features/request/request.feature
+++ backend/de.metas.cucumber/src/test/resources/de/metas/cucumber/features/request/request.feature
@@ -13,17 +13,38 @@ Feature: R_Request upsert and retrieval via API
     And metasfresh contains M_Products:
       | Identifier | Value   |
       | product    | reqProd |
+    And metasfresh contains M_Products:
+      | Identifier | Name        |
+      | p1         | testProduct |
+    And metasfresh contains M_PricingSystems
+      | Identifier |
+      | ps1        |
+    And metasfresh contains M_PriceLists
+      | Identifier | M_PricingSystem_ID | OPT.C_Country.CountryCode | C_Currency.ISO_Code | SOTrx |
+      | pl_so      | ps1                | DE                        | EUR                 | true  |
+    And metasfresh contains M_PriceList_Versions
+      | Identifier | M_PriceList_ID.Identifier | ValidFrom  |
+      | plv_so     | pl_so                     | 2022-01-20 |
+    And metasfresh contains M_ProductPrices
+      | M_PriceList_Version_ID | M_Product_ID | PriceStd | C_TaxCategory_ID.InternalName | C_UOM_ID.X12DE355 |
+      | plv_so                 | p1           | 10.0     | Normal                        | PCE               |
     And metasfresh contains C_BPartners:
-      | Identifier | IsVendor | IsCustomer | Value |
-      | customer   | N        | Y          | reqBP |
+      | Identifier | IsVendor | IsCustomer | Value | M_PricingSystem_ID |
+      | customer   | N        | Y          | reqBP | ps1                |
     And metasfresh contains AD_Users:
       | Identifier | AD_User_ID | Name     |
       | someUser   | 12345      | someUser |
 
   @from:cucumber
-  @Id:S0476_010
-  @Id:S0477_010
   Scenario: R_Request upsert
+    And metasfresh contains C_Orders:
+      | Identifier | IsSOTrx | C_BPartner_ID | DateOrdered | PreparationDate      | REST.Context |
+      | o1         | true    | customer      | 2024-03-30  | 2024-03-29T21:00:00Z | orderId      |
+    And metasfresh contains C_OrderLines:
+      | Identifier | C_Order_ID | M_Product_ID | QtyEntered |
+      | ol_1       | o1         | p1           | 3          |
+    And the order identified by o1 is completed
+
     When a 'POST' request with the below payload is sent to the metasfresh REST-API 'api/request' and fulfills with '201' status code
 """
 {
@@ -32,10 +53,11 @@ Feature: R_Request upsert and retrieval via API
   "summary" : "Another summary",
   "dateDelivered" : "2025-06-15",
   "priority" : "Medium",
-  "confidentialityLevel" : "PartnerConfidential"
+  "confidentialityLevel" : "PartnerConfidential",
+  "orderIdentifier" : "@orderId@"
 }
 """
 
-    Then a new request exists:
-      | AD_Org_ID | R_RequestType_ID    | Summary         | DateDelivered | Priority | ConfidentialType |
-      | 002       | A_CustomerComplaint | Another summary | 2025-06-15    | 5        | C                |
+    Then validate a new request has been created:
+      | AD_Org_ID | R_RequestType_ID    | Summary         | DateDelivered | Priority | ConfidentialType | C_Order_ID |
+      | 002       | A_CustomerComplaint | Another summary | 2025-06-15    | 5        | C                | o1         |
