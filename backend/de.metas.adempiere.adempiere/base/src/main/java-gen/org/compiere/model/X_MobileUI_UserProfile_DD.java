bfd305711c20eda46caa38400abe1c1b0b121e04
Teo Sarca <teo.sarca@metasfresh.com>
2025-12-03 15:32:13 +0200

    mobile UI distribution:  new configuration for navigating to the Jobs List after order fully picked; bugfix pick by scanning HU ID or External Barcode attribute (#21818)
    * Add 'Navigate to Jobs List After Pick From Complete' configuration and logic
    
    * Add e2e test for navigating to Jobs List after completing pick
    
    * Add e2e tests for scanning HU by QR code, ID, and external attribute; update HU code resolution in backend
    
    * Add e2e test for bypassing picked qty prompt when qty is one
    
    * Update e2e test to use `huId` instead of `id` for HU QR code scanning
    
    * Fix typo in e2e test step description in `navigateToJobsListAfterPickFromComplete.spec.js`
    
    * Update e2e test to use `qtyToMove: 1` in `pickFrom_validate_GTIN.spec.js`

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_MobileUI_UserProfile_DD.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/X_MobileUI_UserProfile_DD.java
@@ -12,7 +12,7 @@ import javax.annotation.Nullable;
 public class X_MobileUI_UserProfile_DD extends org.compiere.model.PO implements I_MobileUI_UserProfile_DD, org.compiere.model.I_Persistent 
 {
 
-	private static final long serialVersionUID = -653375513L;
+	private static final long serialVersionUID = 703928833L;
 
     /** Standard Constructor */
     public X_MobileUI_UserProfile_DD (final Properties ctx, final int MobileUI_UserProfile_DD_ID, @Nullable final String trxName)
@@ -68,6 +68,18 @@ public class X_MobileUI_UserProfile_DD extends org.compiere.model.PO implements 
 	public boolean isCompleteJobAutomatically() 
 	{
 		return get_ValueAsBoolean(COLUMNNAME_IsCompleteJobAutomatically);
+	}
+
+	@Override
+	public void setIsNavigateToJobsListAfterPickFromComplete (final boolean IsNavigateToJobsListAfterPickFromComplete)
+	{
+		set_Value (COLUMNNAME_IsNavigateToJobsListAfterPickFromComplete, IsNavigateToJobsListAfterPickFromComplete);
+	}
+
+	@Override
+	public boolean isNavigateToJobsListAfterPickFromComplete() 
+	{
+		return get_ValueAsBoolean(COLUMNNAME_IsNavigateToJobsListAfterPickFromComplete);
 	}
 
 	@Override
