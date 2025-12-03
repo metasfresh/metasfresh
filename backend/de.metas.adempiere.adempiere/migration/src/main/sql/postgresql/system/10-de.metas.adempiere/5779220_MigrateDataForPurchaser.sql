f2dcf78f6e1b6bd9b9d8c1fdeb6ee8082bf78d57
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-12-03 13:37:24 +0200

    Refactor SQL to replace hardcoded values with `AD_Client_ID` and `AD_Org_ID` for better flexibility and reusability.

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5779220_MigrateDataForPurchaser.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5779220_MigrateDataForPurchaser.sql
@@ -1,7 +1,7 @@
 INSERT INTO ad_user_purchaser (ad_client_id, ad_org_id, ad_user_id, ad_user_purchaser_id, created, createdby, isactive, updated, updatedby, c_bp_group_id)
 
-SELECT 1000000,
-       1000000,
+SELECT AD_Client_ID,
+       AD_Org_ID,
        Purchaser_User_ID,
        NEXTVAL('ad_user_purchaser_seq'),
        '2025-12-02 14:39:02.246000 +01:00',
