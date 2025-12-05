b9a8b17bfe90130ae89322e50a0d3f1bc3bf53c6
Teodor Sarca <teo.sarca@metasfresh.com>
2025-12-05 11:39:22 +0200

    Refactor `GetNextEligibleLineToPackCommand` for enhanced modularity and performance; add line eligibility predicates and caching of resolved HU details.

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/job/service/commands/get_next_eligible_line/GetNextEligibleLineToPackCommand.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/job/service/commands/get_next_eligible_line/GetNextEligibleLineToPackCommand.java
@@ -10,7 +10,7 @@ import de.metas.handlingunits.picking.job.service.PickingJobService;
 import de.metas.handlingunits.picking.job.service.external.hu.PickingJobHUService;
 import de.metas.handlingunits.picking.job.service.external.shipmentschedule.ShipmentScheduleInfoLoadingCache;
 import de.metas.handlingunits.qrcodes.model.IHUQRCode;
-import de.metas.i18n.AdMessageKey;
+import de.metas.i18n.BooleanWithReason;
 import de.metas.i18n.ExplainedOptional;
 import de.metas.inout.ShipmentScheduleId;
 import de.metas.logging.LogManager;
@@ -21,11 +21,12 @@ import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.function.Function;
 
 public class GetNextEligibleLineToPackCommand
 {
-	public final static AdMessageKey ERR_QR_ProductNotMatching = AdMessageKey.of("de.metas.handlingunits.picking.job.QR_CODE_PRODUCT_ERROR_MSG");
-
 	// services
 	@NonNull private static final Logger logger = LogManager.getLogger(GetNextEligibleLineToPackCommand.class);
 	@NonNull private final PickingJobService pickingJobService;
@@ -36,6 +37,9 @@ public class GetNextEligibleLineToPackCommand
 	@NonNull private final GetNextEligibleLineToPackRequest request;
 
 	// state
+	@Nullable private IHUQRCode _huQRCode;
+	@Nullable private PickingJob _job;
+	@NonNull private final HashMap<PickingJobLineId, ExplainedOptional<HUInfo>> resolvedHUInfos = new HashMap<>();
 	@NonNull private final ArrayList<String> logs = new ArrayList<>();
 
 	@Builder
@@ -53,10 +57,10 @@ public class GetNextEligibleLineToPackCommand
 
 	public GetNextEligibleLineToPackResponse execute()
 	{
-		final IHUQRCode huQRCode = huService.parsePickFromScannedCode(request.getHuScannedCode());
+		final IHUQRCode huQRCode = getHUQRCode();
 		log("Parsed QR code: " + huQRCode.getAsString());
 
-		@NonNull final PickingJob job = pickingJobService.getById(request.getPickingJobId());
+		@NonNull final PickingJob job = getJob();
 		job.assertCanBeEditedBy(request.getCallerId());
 
 		if (!job.isAllowPickingAnyHU())
@@ -67,65 +71,129 @@ public class GetNextEligibleLineToPackCommand
 
 		shipmentSchedules.warmUpByIds(job.getScheduleIds().getShipmentScheduleIds());
 
-		for (final PickingJobLine line : job.getLines())
-		{
-			if (line.isFullyPicked())
-			{
-				log(line, "Skip because fully picked");
-				continue;
-			}
+		final ArrayList<PickingJobLine> lines = new ArrayList<>(job.getLines());
 
+		//
+		// First, try matching not fully picked lines
+		GetNextEligibleLineToPackResponse response = resolveOverLines(
+				lines,
+				line -> BooleanWithReason.falseIf(line.isFullyPicked(), "fully picked")
+		);
+		if (response.isFound())
+		{
+			return response;
+		}
+
+		//
+		// Next, try matching not fully picked lines (excluding rejected qty)
+		log("No line found that is not fully picked. Trying to find one that is not fully picked (excluding rejected qty)");
+		response = resolveOverLines(
+				lines,
+				line -> BooleanWithReason.falseIf(line.isFullyPickedExcludingRejectedQty(), "fully picked (even when not considering rejected qty)")
+		);
+
+		return response;
+	}
+
+	private GetNextEligibleLineToPackResponse resolveOverLines(
+			@NonNull final ArrayList<PickingJobLine> lines,
+			@NonNull final Function<PickingJobLine, BooleanWithReason> eligiblePredicate)
+	{
+		for (final Iterator<PickingJobLine> it = lines.iterator(); it.hasNext(); )
+		{
+			final PickingJobLine line = it.next();
 			if (request.getExcludeLineId() != null && PickingJobLineId.equals(request.getExcludeLineId(), line.getId()))
 			{
+				it.remove(); // remove it from the list because it will never be eligible
 				log(line, "Skip because it is the one we are excluding");
 				continue;
 			}
 
-			final ExplainedOptional<HUInfo> resolveResult = resolvePickFromHUQRCode(huQRCode, line, job);
-			if (!resolveResult.isPresent())
+			final BooleanWithReason eligible = eligiblePredicate.apply(line);
+			if (eligible.isFalse())
 			{
-				log(line, "Skip because it cannot be picked from " + huQRCode + ": " + resolveResult.getExplanationAsString());
+				log(line, "Skip because " + eligible.getReasonAsString());
 				continue;
 			}
 
-			return GetNextEligibleLineToPackResponse.builder()
-					.lineId(line.getId())
-					.huInfo(resolveResult.get())
-					.build();
+			final ExplainedOptional<HUInfo> resolveResult = resolvePickFromHUQRCode(line);
+			if (!resolveResult.isPresent())
+			{
+				log(line, "Skip because HU QR Code is not matching the line: " + resolveResult.getExplanationAsString());
+				it.remove();
+				continue;
+			}
+
+			return found(line, resolveResult.get());
 		}
 
 		return notFound();
+
 	}
 
 	private GetNextEligibleLineToPackResponse notFound()
 	{
-		return GetNextEligibleLineToPackResponse.builder().logs(logs).build();
+		return GetNextEligibleLineToPackResponse.builder()
+				.lineId(null)
+				.logs(logs)
+				.build();
 	}
 
-	private ExplainedOptional<HUInfo> resolvePickFromHUQRCode(
-			@NonNull final IHUQRCode huQRCode,
-			@NonNull final PickingJobLine line,
-			@NonNull final PickingJob job)
+	private GetNextEligibleLineToPackResponse found(PickingJobLine line, HUInfo huInfo)
 	{
-		return huService.resolvePickFromHUQRCode(
-				huQRCode,
-				line.getProductId(),
-				getCustomerId(job, line),
-				getWarehouseId(line)
+		return GetNextEligibleLineToPackResponse.builder()
+				.lineId(line.getId())
+				.huInfo(huInfo)
+				.logs(logs)
+				.build();
+	}
+
+	@NonNull
+	private PickingJob getJob()
+	{
+		if (_job == null)
+		{
+			_job = pickingJobService.getById(request.getPickingJobId());
+		}
+		return _job;
+	}
+
+	private IHUQRCode getHUQRCode()
+	{
+		if (_huQRCode == null)
+		{
+			_huQRCode = huService.parsePickFromScannedCode(request.getHuScannedCode());
+			log("Parsed QR code: " + _huQRCode.getAsString());
+		}
+		return _huQRCode;
+	}
+
+	private ExplainedOptional<HUInfo> resolvePickFromHUQRCode(@NonNull final PickingJobLine line)
+	{
+		return resolvedHUInfos.computeIfAbsent(
+				line.getId(),
+				k -> huService.resolvePickFromHUQRCode(
+						getHUQRCode(),
+						line.getProductId(),
+						getCustomerId(line),
+						getWarehouseId(line)
+				));
+	}
+
+	@NonNull
+	private BPartnerId getCustomerId(@NonNull final PickingJobLine line)
+	{
+		return CoalesceUtil.coalesceSuppliersNotNull(
+				line::getCustomerId,
+				() -> getJob().getCustomerId()
 		);
 	}
 
 	@NonNull
-	private WarehouseId getWarehouseId(final PickingJobLine line)
+	private WarehouseId getWarehouseId(@NonNull final PickingJobLine line)
 	{
 		final ShipmentScheduleId shipmentScheduleId = line.getScheduleId().getShipmentScheduleId();
 		return shipmentSchedules.getById(shipmentScheduleId).getWarehouseId();
-	}
-
-	@NonNull
-	private static BPartnerId getCustomerId(@NonNull final PickingJob job, final @NonNull PickingJobLine line)
-	{
-		return CoalesceUtil.coalesceNotNull(line.getCustomerId(), job.getCustomerId());
 	}
 
 	private void log(@NonNull String message)
--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/job/service/commands/get_next_eligible_line/GetNextEligibleLineToPackResponse.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/picking/job/service/commands/get_next_eligible_line/GetNextEligibleLineToPackResponse.java
@@ -4,18 +4,18 @@ import de.metas.handlingunits.picking.job.model.HUInfo;
 import de.metas.handlingunits.picking.job.model.PickingJobLineId;
 import lombok.Builder;
 import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
 @Value
 @Builder
-@Jacksonized
 public class GetNextEligibleLineToPackResponse
 {
 	@Nullable PickingJobLineId lineId;
 	@Nullable HUInfo huInfo;
 
 	@Nullable List<String> logs;
+
+	public boolean isFound() {return lineId != null;}
 }
