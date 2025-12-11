8145c02f632e22634b8e477bf7e0ae7031f419f1
Adrian <60300682+adi-stefan@users.noreply.github.com>
2025-12-11 18:56:50 +0200

    fix DD_Order_Candidate aggregation migrationscript typo (#21867)

--- backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5780430_DDOrderAggregation_sysconfigs.sql
+++ backend/de.metas.manufacturing/src/main/sql/postgresql/system/30-de.metas.adempiere.libero/5780430_DDOrderAggregation_sysconfigs.sql
@@ -3,7 +3,7 @@
 -- SysConfig Name: DDOrderAggregation.header.bySalesOrderId
 -- SysConfig Value: Y
 -- 2025-12-10T15:47:44.193Z
-INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541780,'O',TO_TIMESTAMP('2025-12-10 15:47:43.888000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'If enabled, prevents DD_Order_Candidates of different sales orders from ending up in the same DD_Order.','D','Y','DDOrderAggregation.header.bySalesOrderId ',TO_TIMESTAMP('2025-12-10 15:47:43.888000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y')
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,541780,'O',TO_TIMESTAMP('2025-12-10 15:47:43.888000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'If enabled, prevents DD_Order_Candidates of different sales orders from ending up in the same DD_Order.','D','Y','DDOrderAggregation.header.bySalesOrderId',TO_TIMESTAMP('2025-12-10 15:47:43.888000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y')
 ;
 
 -- SysConfig Name: DDOrderAggregation.header.byPPOrderRef
