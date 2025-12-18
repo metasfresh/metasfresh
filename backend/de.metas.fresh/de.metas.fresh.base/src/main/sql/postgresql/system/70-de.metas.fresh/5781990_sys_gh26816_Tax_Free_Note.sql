69935d71e4db03903a018dc9b1e294481fa8f64b
riad.mennour <riad.mennour@gmail.com>
2025-12-18 17:09:03 +0100

    `Intra-Community Acquisition - Tax` Note

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5781990_sys_gh26816_Tax_Free_Note.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5781990_sys_gh26816_Tax_Free_Note.sql
@@ -12,7 +12,7 @@ INSERT INTO AD_BoilerPlate (AD_BoilerPlate_ID,AD_Client_ID,AD_Org_ID,Created,Cre
 ;
 
 -- 2025-12-18T12:25:22.455Z
-INSERT INTO AD_BoilerPlate_Trl (AD_Language,AD_BoilerPlate_ID, Subject,TextSnippet, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_BoilerPlate_ID, t.Subject,t.TextSnippet, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_BoilerPlate t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_BoilerPlate_ID=540034 AND NOT EXISTS (SELECT 1 FROM AD_BoilerPlate_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_BoilerPlate_ID=t.AD_BoilerPlate_ID)
+INSERT INTO AD_BoilerPlate_Trl (AD_Language,AD_BoilerPlate_ID, TextSnippet, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_BoilerPlate_ID, t.TextSnippet, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_BoilerPlate t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_BoilerPlate_ID=540034 AND NOT EXISTS (SELECT 1 FROM AD_BoilerPlate_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_BoilerPlate_ID=t.AD_BoilerPlate_ID)
 ;
 
 -- 2025-12-18T12:25:22.455Z
