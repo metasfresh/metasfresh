8ae5e5f32e7ec5294df83b2934b040177a145bbf
riad.mennour <riad.mennour@gmail.com>
2025-12-18 19:25:40 +0100

    `Intra-Community Acquisition - Tax` Note

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5782020_sys_gh26816_TaxNote.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5782020_sys_gh26816_TaxNote.sql
@@ -40,3 +40,19 @@ END;
 $BODY$
     LANGUAGE plpgsql
 ;
+
+-- Refactor legacy function
+
+CREATE OR REPLACE FUNCTION report.TaxNote(
+    IN p_c_invoice_id numeric,
+    IN p_ad_language  character varying DEFAULT 'de_DE'
+)
+    RETURNS text
+    LANGUAGE plpgsql
+AS
+$BODY$
+BEGIN
+    RETURN report.TaxNote(NULL, p_c_invoice_id, p_ad_language);
+END;
+$BODY$;
+
