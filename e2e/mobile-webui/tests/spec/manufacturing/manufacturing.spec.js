063431951abb08f838e757b9281356be1d2bfad6
Teodor Sarca <teo.sarca@metasfresh.com>
2025-12-18 22:46:28 +0200

    improve playwright test

--- e2e/mobile-webui/tests/spec/manufacturing/manufacturing.spec.js
+++ e2e/mobile-webui/tests/spec/manufacturing/manufacturing.spec.js
@@ -21,7 +21,7 @@ const createMasterdata = async () => {
             products: {
                 "COMP1": {},
                 "COMP2": {},
-                "BOM": {
+                "FINISHED_GOOD": {
                     bom: {
                         lines: [
                             { product: 'COMP1', qty: 1 },
@@ -31,7 +31,7 @@ const createMasterdata = async () => {
                 },
             },
             packingInstructions: {
-                "PI": { lu: "LU", qtyTUsPerLU: 20, tu: "TU", product: "BOM", qtyCUsPerTU: 4 },
+                "PI": { lu: "LU", qtyTUsPerLU: 20, tu: "TU", product: "FINISHED_GOOD", qtyCUsPerTU: 4 },
             },
             handlingUnits: {
                 "HU_COMP1": { product: 'COMP1', warehouse: 'wh', qty: 100 },
@@ -40,7 +40,7 @@ const createMasterdata = async () => {
             manufacturingOrders: {
                 "PP1": {
                     warehouse: 'wh',
-                    product: 'BOM',
+                    product: 'FINISHED_GOOD',
                     qty: 5,
                     datePromised: '2025-03-01T00:00:00.000+02:00',
                 }
@@ -59,24 +59,34 @@ test('Simple manufacturing test', async ({ page }) => {
     await ManufacturingJobsListScreen.waitForScreen();
     const { jobId } = await ManufacturingJobsListScreen.startJob({ documentNo: masterdata.manufacturingOrders.PP1.documentNo });
 
-    await ManufacturingJobScreen.clickIssueButton({ index: 1 });
-    await RawMaterialIssueLineScreen.scanQRCode({ qrCode: masterdata.handlingUnits.HU_COMP1.qrCode, expectQtyEntered: '5' });
-    await RawMaterialIssueLineScreen.goBack();
+    await test.step('Issue COMP1', async () => {
+        await ManufacturingJobScreen.expectIssueButton({ index: 1, qtyToIssue: '5 Stk', qtyIssued: '0 Stk' });
+        await ManufacturingJobScreen.clickIssueButton({ index: 1 });
+        await RawMaterialIssueLineScreen.scanQRCode({ qrCode: masterdata.handlingUnits.HU_COMP1.qrCode, expectQtyEntered: '5' });
+        await RawMaterialIssueLineScreen.goBack();
+        await ManufacturingJobScreen.expectIssueButton({ index: 1, qtyToIssue: '5 Stk', qtyIssued: '5 Stk' });
+    });
 
-    await ManufacturingJobScreen.clickIssueButton({ index: 2 });
-    await RawMaterialIssueLineScreen.scanQRCode({ qrCode: masterdata.handlingUnits.HU_COMP2.qrCode, expectQtyEntered: '10' });
-    await RawMaterialIssueLineScreen.goBack();
+    await test.step('Issue COMP2', async () => {
+        await ManufacturingJobScreen.expectIssueButton({ index: 2, qtyToIssue: '10 Stk', qtyIssued: '0 Stk' });
+        await ManufacturingJobScreen.clickIssueButton({ index: 2 });
+        await RawMaterialIssueLineScreen.scanQRCode({ qrCode: masterdata.handlingUnits.HU_COMP2.qrCode, expectQtyEntered: '10' });
+        await RawMaterialIssueLineScreen.goBack();
+        await ManufacturingJobScreen.expectIssueButton({ index: 2, qtyToIssue: '10 Stk', qtyIssued: '10 Stk' });
+    });
 
-    await ManufacturingJobScreen.clickReceiveButton({ index: 1 });
-    await MaterialReceiptLineScreen.selectNewLUTarget({ luPIItemTestId: masterdata.packingInstructions.PI.luPIItemTestId })
-    await MaterialReceiptLineScreen.receiveQty({
-        expectQtyEntered: '5',
-        qtyEntered: '1',
-    })
+    await test.step('Receive finished goods', async () => {
+        await ManufacturingJobScreen.expectReceiveButton({ index: 1, qtyToReceive: '5 Stk', qtyReceived: '0 Stk' });
+        await ManufacturingJobScreen.clickReceiveButton({ index: 1 });
+        await MaterialReceiptLineScreen.selectNewLUTarget({ luPIItemTestId: masterdata.packingInstructions.PI.luPIItemTestId })
+        await MaterialReceiptLineScreen.receiveQty({ expectQtyEntered: '5', qtyEntered: '1' });
+        await ManufacturingJobScreen.expectReceiveButton({ index: 1, qtyToReceive: '5 Stk', qtyReceived: '1 Stk' });
+    });
 
     await ManufacturingJobScreen.complete();
-
+    
     await Backend.expect({
+        title: 'HUs after completing the manufacturing order',
         manufacturings: {
             [jobId]: {
                 receivedHUs: [
@@ -85,7 +95,7 @@ test('Simple manufacturing test', async ({ page }) => {
             }
         },
         hus: {
-            'lu1': { huStatus: 'A', storages: { 'BOM': '1 PCE' } },
+            'lu1': { huStatus: 'A', storages: { 'FINISHED_GOOD': '1 PCE' } },
             'HU_COMP1': { huStatus: 'A', storages: { 'COMP1': '95 PCE' } },
             'HU_COMP2': { huStatus: 'A', storages: { 'COMP2': '90 PCE' } },
         }
