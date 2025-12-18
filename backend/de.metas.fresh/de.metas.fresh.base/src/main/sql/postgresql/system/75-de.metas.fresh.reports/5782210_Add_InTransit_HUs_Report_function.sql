adb861c0298a40b10e633849e4321f71d47a0692
Cristina Ghita <cristina.ghita@metasfresh.com>
2025-12-18 21:12:13 +0200

    Refine InTransit HUs Report: Adjust resource bundle paths, improve SQL join logic, and replace redundant SQL statements.

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5782210_Add_InTransit_HUs_Report_function.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5782210_Add_InTransit_HUs_Report_function.sql
@@ -1,9 +1,7 @@
-DROP FUNCTION IF EXISTS report.InTransit_HUs_Report(IN p_M_Locator_ID numeric,
-                                                                        IN p_AD_Language Character Varying(6))
-;
+SELECT db_drop_functions('*.InTransit_HUs_Report');
 
 CREATE OR REPLACE FUNCTION report.InTransit_HUs_Report(IN p_M_Locator_ID numeric,
-                                                                           IN p_AD_Language Character Varying(6))
+                                                       IN p_AD_Language  Character Varying(6))
     RETURNS TABLE
             (
                 ProductName         character varying(255),
@@ -27,9 +25,9 @@ SELECT COALESCE(pt.Name, p.name) AS ProductName,
        p.value                   AS p_value,
 
        CASE
-           WHEN ddom.qtypicked > 0 THEN ddom.qtypicked
-           ELSE ddol.qtyentered
-           END                   AS QtyEntered,
+           WHEN ddolhc.qtypicked > 0 THEN ddolhc.qtypicked
+                                     ELSE ddol.qtyentered
+       END                       AS QtyEntered,
 
        uomt.uomsymbol            AS UOMSymbol,
        ddo.documentno            AS documentno,
@@ -47,41 +45,37 @@ FROM M_HU hu
 
          -- Join to get HUs on the specified locator
          INNER JOIN M_Locator loc ON hu.M_Locator_ID = loc.M_Locator_ID
+         -- Warehouse information
+         INNER JOIN m_warehouse w ON loc.m_warehouse_id = w.m_warehouse_id
 
-    -- Join DD Order data via DD_OrderLine_HU_Candidate (as per requirement)
-         INNER JOIN DD_OrderLine_HU_Candidate ddolhc ON ddolhc.PickFrom_HU_ID = hu.M_HU_ID
+         -- Join DD Order data via DD_OrderLine_HU_Candidate (as per requirement)
+         INNER JOIN DD_OrderLine_HU_Candidate ddolhc ON ddolhc.M_HU_ID = hu.M_HU_ID
          INNER JOIN DD_OrderLine ddol ON ddolhc.DD_OrderLine_ID = ddol.DD_OrderLine_ID
          INNER JOIN DD_Order ddo ON ddol.DD_Order_ID = ddo.DD_Order_ID
 
-    -- Product information
-         INNER JOIN M_Product p ON ddol.M_Product_ID = p.M_Product_ID AND p.isActive = 'Y'
+         -- Product information
+         INNER JOIN M_Product p ON ddol.M_Product_ID = p.M_Product_ID
          INNER JOIN M_Product_Trl pt
-                    ON ddol.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = p_AD_Language AND pt.isActive = 'Y'
+                    ON ddol.M_Product_ID = pt.M_Product_ID AND pt.AD_Language = p_AD_Language
 
-    -- UOM information
+         -- UOM information
          INNER JOIN C_UOM uom ON ddol.C_UOM_ID = uom.C_UOM_ID
          INNER JOIN C_UOM_Trl uomt ON ddol.C_UOM_ID = uomt.C_UOM_ID AND uomt.AD_Language = p_AD_Language
 
-    -- Warehouse information
-         INNER JOIN m_warehouse w ON loc.m_warehouse_id = w.m_warehouse_id
-
-    -- Locator To information
+         -- Locator To information
          INNER JOIN m_locator loc_to ON ddol.m_locatorto_id = loc_to.m_locator_id
 
-    -- Sales Order information
+          -- Sales Order information
          LEFT JOIN C_OrderLine ol ON ddol.C_OrderLineSO_ID = ol.C_OrderLine_ID
          LEFT JOIN C_Order o ON ol.C_Order_ID = o.C_Order_ID
          LEFT JOIN c_bpartner bp ON o.c_bpartner_id = bp.c_bpartner_id
 
-    -- Production Order information
+         -- Production Order information
          LEFT JOIN pp_order pp ON ddo.forward_pp_order_id = pp.pp_order_id
 
-    -- Move Schedule information
-         LEFT JOIN DD_Order_MoveSchedule ddom ON ddom.DD_OrderLine_id = ddol.DD_OrderLine_ID AND ddom.status = 'IP'
-
-    -- QR Code information
-         LEFT JOIN m_hu_qrcode_assignment qr_assign ON qr_assign.m_hu_id = hu.m_hu_id
-         LEFT JOIN M_HU_QRCode qr ON qr_assign.m_hu_qrcode_id = qr.m_hu_qrcode_id
+         -- QR Code information
+         INNER JOIN m_hu_qrcode_assignment qr_assign ON qr_assign.m_hu_id = hu.m_hu_id
+         INNER JOIN M_HU_QRCode qr ON qr_assign.m_hu_qrcode_id = qr.m_hu_qrcode_id
 
 WHERE hu.M_Locator_ID = p_M_Locator_ID
   AND hu.HUStatus IN ('A', 'S', 'I')
