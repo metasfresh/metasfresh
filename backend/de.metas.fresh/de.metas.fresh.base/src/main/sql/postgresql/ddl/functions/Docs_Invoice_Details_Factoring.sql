ba2611b847c14c4432837fd5bcb7496d486a7e9d
riad.mennour <riad.mennour@gmail.com>
2026-01-14 22:03:17 +0100

    Support for `Silent Factoring`

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/Docs_Invoice_Details_Factoring.sql
@@ -0,0 +1,60 @@
+/*
+ * #%L
+ * de.metas.fresh.base
+ * %%
+ * Copyright (C) 2026 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Invoice_Details_Factoring(IN p_Record_ID numeric)
+;
+
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Invoice_Details_Factoring(IN p_Record_ID numeric)
+    RETURNS TABLE
+            (
+                bp_name       character varying,
+                bp_address    character varying,
+                bank_acct     character varying,
+                bank_currency character varying,
+                bank_code     character varying,
+                bank_name     character varying,
+                bank_iban     character varying,
+                bank_swift    character varying
+            )
+    STABLE
+    LANGUAGE sql
+AS
+$$
+SELECT bp.name                                             AS bp_name,
+       REGEXP_REPLACE(i.BPartnerAddress, E'\n', ', ', 'g') AS bp_address,
+       COALESCE(bpb.accountno, '')                         AS bank_acct,
+       c.iso_code                                          AS bank_currency,
+       COALESCE(bank.routingno, '')                        AS bank_code,
+       COALESCE(bank.name, '')                             AS bank_name,
+       COALESCE(bpb.iban, '')                              AS bank_iban,
+       COALESCE(bank.swiftcode, '')                        AS bank_swift
+FROM C_Invoice i
+         INNER JOIN C_BPartner bp ON i.C_BPartner_ID = bp.C_BPartner_ID
+         LEFT OUTER JOIN C_BPartner bpf ON bpf.IsFactorer = 'Y' AND i.AD_Org_ID = bpf.AD_Org_ID
+         LEFT OUTER JOIN c_bp_bankaccount bpb ON bpf.c_bpartner_id = bpb.c_bpartner_id
+         LEFT OUTER JOIN c_bank bank ON bpb.c_bank_id = bank.c_bank_id
+         LEFT JOIN C_Currency c ON bpb.C_Currency_ID = c.C_Currency_ID
+
+WHERE i.C_Invoice_ID = p_Record_ID
+$$
+;
+
