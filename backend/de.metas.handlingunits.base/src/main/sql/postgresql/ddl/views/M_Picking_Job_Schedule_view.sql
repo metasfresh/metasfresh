f59938f6f0f08073e53033071b25c6ba6e9575be
Max Rieck <max.rieck@metasfresh.com>
2026-01-21 14:13:24 +0100

    additional Traffic Management column qtyPicked proto

--- backend/de.metas.handlingunits.base/src/main/sql/postgresql/ddl/views/M_Picking_Job_Schedule_view.sql
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/ddl/views/M_Picking_Job_Schedule_view.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS M_Picking_Job_Schedule_view
 ;
 
 CREATE OR REPLACE VIEW M_Picking_Job_Schedule_view AS
-WITH base_shipment AS (SELECT s.m_shipmentschedule_id,
+WITH base_schedule AS (SELECT s.m_shipmentschedule_id,
 
                               s.c_bpartner_customer_id,
                               s.c_bpartner_location_id,
@@ -64,24 +64,28 @@ WITH base_shipment AS (SELECT s.m_shipmentschedule_id,
 SELECT b.*,
        j.m_picking_job_schedule_id, -- Composed Key together with m_shipmentschedule_id
        j.qtytopick,
+       (SELECT COALESCE(sum(sqp.qtypicked), 0)
+       FROM m_shipmentschedule_qtypicked sqp
+       WHERE sqp.m_picking_job_schedule_id = j.m_picking_job_schedule_id
+       AND sqp.processed = 'Y') AS qtypicked,
        j.c_workplace_id,
        j.processed,
        'N' AS isreschedule,
        'Y' AS isassigned
-FROM base_shipment b
-         JOIN M_Picking_Job_Schedule j
-              ON j.m_shipmentschedule_id = b.m_shipmentschedule_id
+FROM base_schedule b
+         JOIN M_Picking_Job_Schedule j ON j.m_shipmentschedule_id = b.m_shipmentschedule_id
 
 UNION ALL
 
 -- Synthetic "to be scheduled" row
 SELECT b.*,
-       0                                                           AS m_picking_job_schedule_id, -- Composed Key together with m_shipmentschedule_id
-       NULL                                                        AS qtytopick,
-       NULL                                                        AS c_workplace_id,
-       'N'                                                         AS processed,
-       (b.qtydelivered <> b.qtyscheduledforpickingofprocessed)     AS isreschedule,
-       'N'                                                         AS isassigned
-FROM base_shipment b
+       0                                                       AS m_picking_job_schedule_id, -- Composed Key together with m_shipmentschedule_id
+       NULL                                                    AS qtytopick,
+       NULL                                                    AS qtypicked,
+       NULL                                                    AS c_workplace_id,
+       'N'                                                     AS processed,
+       (b.qtydelivered <> b.qtyscheduledforpickingofprocessed) AS isreschedule,
+       'N'                                                     AS isassigned
+FROM base_schedule b
 WHERE b.qtytoscheduleforpicking > 0
 ;
