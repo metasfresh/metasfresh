0db6ecbf3429f9ccba7715fa8b5d7b0c7a165c0c
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-01-27 12:03:25 +0200

    Fix CR_TaxTotalAmt  not null (#22145)

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5784950_Extend_I_GLJOurnal_Table.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5784950_Extend_I_GLJOurnal_Table.sql
@@ -53,7 +53,7 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 ;
 
 -- 2026-01-22T10:09:36.432Z
-/* DDL */ SELECT public.db_alter_table('I_GLJournal','ALTER TABLE public.I_GLJournal ADD COLUMN CR_TaxTotalAmt NUMERIC NOT NULL')
+/* DDL */ SELECT public.db_alter_table('I_GLJournal','ALTER TABLE public.I_GLJournal ADD COLUMN CR_TaxTotalAmt NUMERIC')
 ;
 
 -- Column: I_GLJournal.Taxname
@@ -97,7 +97,7 @@ ALTER TABLE I_GLJournal ADD CONSTRAINT CTax_IGLJournal FOREIGN KEY (C_Tax_ID) RE
 -- 2026-01-22T11:36:11.645Z
 INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,584450,0,'C_ValidCombinationTaxFrom_ID',TO_TIMESTAMP('2026-01-22 11:36:11.514000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','Valid Combination Tax From','Valid Combination Tax From',TO_TIMESTAMP('2026-01-22 11:36:11.514000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
-
+    
 -- 2026-01-22T11:36:11.652Z
 INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=584450 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
 ;
