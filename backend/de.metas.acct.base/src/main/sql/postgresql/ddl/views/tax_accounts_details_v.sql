a72bf79a27c0cd21520fece807c8280face02f86
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-01-28 20:15:49 +0200

    Enhance tax accounts reporting and Jasper integration (#22185)
    Improved SQL logic for tax accounts functions and views to better handle VAT code and previous year calculations. Updated JasperReports templates for compatibility with Jaspersoft Studio 7.0.0 and added new data adapter configuration. Introduced new SQL function and view for more flexible tax reporting, including support for detailed and recap levels.

--- backend/de.metas.acct.base/src/main/sql/postgresql/ddl/views/tax_accounts_details_v.sql
+++ backend/de.metas.acct.base/src/main/sql/postgresql/ddl/views/tax_accounts_details_v.sql
@@ -28,7 +28,13 @@ AS
 WITH tax_accounts AS (SELECT DISTINCT vc.Account_ID AS C_ElementValue_ID
                       FROM C_Tax_Acct ta
                                INNER JOIN C_ValidCombination vc
-                                          ON vc.C_ValidCombination_ID IN (ta.T_Due_Acct, ta.T_Credit_Acct))
+                                          ON vc.C_ValidCombination_ID IN (ta.T_Due_Acct, ta.T_Credit_Acct)
+                      UNION
+
+                        SELECT C_ElementValue_ID from C_ElementValue
+                        WHERE AccountConceptualName IN ('T_Due_Acct', 'T_Credit_Acct')
+
+                      )
 SELECT fa.vatcode,
        fa.accountno,
        fa.accountname,
@@ -114,7 +120,6 @@ FROM (SELECT fa.vatcode                    AS vatcode,
                INNER JOIN AD_ClientInfo ci ON ci.AD_Client_ID = fa.ad_client_id
                INNER JOIN C_AcctSchema acs ON acs.C_AcctSchema_ID = ci.C_AcctSchema1_ID
                INNER JOIN C_Currency cr ON acs.C_Currency_ID = cr.C_Currency_ID) AS fa
-         LEFT OUTER JOIN tax_accounts ON (tax_accounts.C_ElementValue_ID = fa.account_id AND fa.ad_table_id IN (get_Table_Id('SAP_GLJournal'), get_Table_Id('GL_Journal')))
-WHERE (fa.ad_table_id IN (get_Table_Id('SAP_GLJournal'), get_Table_Id('GL_Journal')) AND tax_accounts.C_ElementValue_ID IS NOT NULL)
+    WHERE (exists (select 1 from tax_accounts where tax_accounts.C_ElementValue_ID = fa.account_id) )
    OR (fa.ad_table_id IN (get_Table_Id('C_Invoice'), get_Table_Id('C_AllocationHdr')) AND fa.accountconceptualname IN ('T_Due_Acct', 'T_Credit_Acct'))
 ;
