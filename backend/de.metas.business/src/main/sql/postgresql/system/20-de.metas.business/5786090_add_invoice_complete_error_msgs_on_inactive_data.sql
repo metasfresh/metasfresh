0dd06485bbe864b8647d44049b5790f3569c3f3d
Max Rieck <max.rieck@metasfresh.com>
2026-01-30 14:14:18 +0100

    fix error msgs script

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5786090_add_invoice_complete_error_msgs_on_inactive_data.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5786090_add_invoice_complete_error_msgs_on_inactive_data.sql
@@ -14,10 +14,6 @@ INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTransla
 UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='The invoice can not be completed, because of inactive Invoice Partner',Updated=TO_TIMESTAMP('2026-01-30 12:09:32.080000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545624
 ;
 
--- 2026-01-30T12:09:32.089Z
-UPDATE AD_Message base SET MsgText=trl.MsgText, Updated=trl.Updated, UpdatedBy=trl.UpdatedBy FROM AD_Message_Trl trl  WHERE trl.AD_Message_ID=base.AD_Message_ID AND trl.AD_Language='en_US' AND trl.AD_Language=getBaseLanguage()
-;
-
 -- Value: InactivePartnerOnInvoiceComplete
 -- 2026-01-30T12:09:32.908Z
 UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2026-01-30 12:09:32.908000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545624
@@ -42,10 +38,6 @@ INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTransla
 UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='The invoice can not be completed, because the invvoiceaddress is inactive',Updated=TO_TIMESTAMP('2026-01-30 12:11:12.355000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545625
 ;
 
--- 2026-01-30T12:11:12.356Z
-UPDATE AD_Message base SET MsgText=trl.MsgText, Updated=trl.Updated, UpdatedBy=trl.UpdatedBy FROM AD_Message_Trl trl  WHERE trl.AD_Message_ID=base.AD_Message_ID AND trl.AD_Language='en_US' AND trl.AD_Language=getBaseLanguage()
-;
-
 -- Value: InactiveAddressOnInvoiceComplete
 -- 2026-01-30T12:11:13.442Z
 UPDATE AD_Message_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2026-01-30 12:11:13.442000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='de_CH' AND AD_Message_ID=545625
@@ -68,10 +60,6 @@ INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTransla
 -- Value: InactiveProductOnInvoiceComplete
 -- 2026-01-30T12:12:50.200Z
 UPDATE AD_Message_Trl SET IsTranslated='Y', MsgText='The invoice can not be completed, because it contains at least one inactive product',Updated=TO_TIMESTAMP('2026-01-30 12:12:50.200000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Language='en_US' AND AD_Message_ID=545626
-;
-
--- 2026-01-30T12:12:50.201Z
-UPDATE AD_Message base SET MsgText=trl.MsgText, Updated=trl.Updated, UpdatedBy=trl.UpdatedBy FROM AD_Message_Trl trl  WHERE trl.AD_Message_ID=base.AD_Message_ID AND trl.AD_Language='en_US' AND trl.AD_Language=getBaseLanguage()
 ;
 
 -- Value: InactiveProductOnInvoiceComplete
