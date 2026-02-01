8f7d289098feede006da9d1f2e20e83089645b77
metas-ts <tobias.schoeneberg@metasfresh.com>
2026-02-01 15:45:17 +0100

    fix SQL-problem

--- backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5786220_sys_gh27537_SalesInvoiceDetails_fix_process_name_descr.sql
+++ backend/de.metas.business/src/main/sql/postgresql/system/20-de.metas.business/5786220_sys_gh27537_SalesInvoiceDetails_fix_process_name_descr.sql
@@ -1,6 +1,6 @@
 -- Run mode: SWING_CLIENT
 
-UPDATE AD_Process SET VALUE = 'Sales_Invoice_Line_Details (Excel)',Updated=TO_TIMESTAMP('2026-02-01 11:51:10.541000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 AD_Process_ID=585439;
+UPDATE AD_Process SET VALUE = 'Sales_Invoice_Line_Details (Excel)',Updated=TO_TIMESTAMP('2026-02-01 11:51:10.541000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585439;
 
 
 -- Process: Sales Invoice Report (Excel)(de.metas.impexp.spreadsheet.process.ExportToSpreadsheetProcess)
