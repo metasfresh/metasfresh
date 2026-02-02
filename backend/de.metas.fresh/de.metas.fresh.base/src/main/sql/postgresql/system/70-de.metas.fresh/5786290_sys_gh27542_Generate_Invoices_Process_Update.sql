f5d6969b7f73407e087b0a4e66a96966ca088253
riad.mennour <riad.mennour@gmail.com>
2026-02-02 11:32:35 +0100

    `Generate invoices` process update

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5786290_sys_gh27542_Generate_Invoices_Process_Update.sql
@@ -0,0 +1,8 @@
+-- Run mode: SWING_CLIENT
+
+-- Process: C_Invoice_Candidate_EnqueueSelectionForInvoicing(de.metas.invoicecandidate.process.C_Invoice_Candidate_EnqueueSelectionForInvoicing)
+-- ParameterName: IsUpdateLocationAndContactForInvoice
+-- 2026-02-02T10:28:03.228Z
+UPDATE AD_Process_Para SET DefaultValue='Y',Updated=TO_TIMESTAMP('2026-02-02 10:28:03.228000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_Para_ID=541615
+;
+
