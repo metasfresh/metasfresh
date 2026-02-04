07c49095df3fa9b1319559db35c54de61d2592d8
metas-ts <tobias.schoeneberg@metasfresh.com>
2026-02-04 22:37:09 +0100

    add delivery status color code to m_Shipmentschedule window

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5786840_sys_gh27905_m_shipmentSchedule_grid_DeliverableStatus.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inoutcandidate/5786840_sys_gh27905_m_shipmentSchedule_grid_DeliverableStatus.sql
@@ -122,4 +122,9 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP(
 UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2026-02-04 21:10:32.512000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=540638
 ;
 
+-- UI Element: Lieferdisposition(500221,de.metas.inoutcandidate) -> Auslieferplan(500221,de.metas.inoutcandidate) -> advanced edit -> 10 -> advanced edit.Delivery Status_DeliveryStatusColor_ID
+-- Column: M_ShipmentSchedule.DeliveryStatusColor_ID
+-- 2026-02-04T21:35:01.737Z
+UPDATE AD_UI_Element SET AD_UI_ElementGroup_ID=540052, SeqNo=410,Updated=TO_TIMESTAMP('2026-02-04 21:35:01.737000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_UI_Element_ID=646828
+;
 
