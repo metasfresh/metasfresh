e02fdec3e1ca496ce80437d8c35983a66ec1e632
riad.mennour <riad.mennour@gmail.com>
2026-02-06 11:12:00 +0100

    `Custom Invoice` report update

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/docs_sales_customs_invoice_footer.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/ddl/functions/docs_sales_customs_invoice_footer.sql
@@ -1,26 +1,56 @@
+/*
+ * #%L
+ * de.metas.fresh.base
+ * %%
+ * Copyright (C) 2026 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.docs_sales_customs_invoice_footer(NUMERIC,
                                                                                              character varying)
 ;
 
-CREATE FUNCTION de_metas_endcustomer_fresh_reports.docs_sales_customs_invoice_footer(p_c_customs_invoice_id numeric,
-                                                                                     p_ad_language          character varying)
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.docs_sales_customs_invoice_footer(p_c_customs_invoice_ID NUMERIC,
+                                                                                                p_ad_language          character varying)
     RETURNS TABLE
             (
-                nettototalweight  numeric,
+                nettoTotalWeight  numeric,
                 uom               character varying,
-                qtypattern        text,
-                bruttototalweight numeric,
-                country           character varying
+                QtyPattern        text,
+                bruttoTotalWeight numeric,
+                country           character varying,
+                containernumber   character varying,
+                sealnumber        character varying,
+                Incoterms         character varying,
+                ModeOfTransport   character varying
             )
-    LANGUAGE sql
+    LANGUAGE 'sql'
 AS
-$$
+$BODY$
 
 SELECT SUM(catchweight) AS nettoTotalWeight,
        UOM,
        QtyPattern,
        SUM(bruttweight) AS bruttoTotalWeight,
-       country
+       country,
+       containernumber,
+       sealnumber,
+       Incoterms,
+       ModeOfTransport
 FROM (SELECT (CASE
                   WHEN il.c_uom_id = 540017 -- harcoded kg
                       THEN il.InvoicedQty
@@ -36,10 +66,14 @@ FROM (SELECT (CASE
              CASE
                  WHEN report.IsHiddenReportElement(ci.C_DocType_ID, 'Customs_Invoice_Note') = 'N' THEN
                      COALESCE(ct.name, co.name)
-             END                                                                                                       AS country
+             END                                                                                                       AS country,
+             ci.containernumber                                                                                        AS containernumber,
+             ci.sealnumber                                                                                             AS sealnumber,
+             COALESCE(inc_trl.name, inc.name)                                                                          AS Incoterms,
+             COALESCE(modt_trl.name, modt.name)                                                                        AS ModeOfTransport
 
       FROM C_Customs_Invoice_Line il
-               INNER JOIN C_Customs_Invoice ci ON il.c_customs_invoice_id = ci.c_customs_invoice_id
+               INNER JOIN C_Customs_Invoice ci ON ci.c_customs_invoice_ID = il.c_customs_invoice_ID
                LEFT OUTER JOIN C_UOM uom ON uom.C_UOM_ID = 540017 -- harcoded kg
                LEFT OUTER JOIN C_UOM_Trl uomt ON uomt.c_UOM_ID = uom.C_UOM_ID AND uomt.AD_Language = p_AD_Language
                LEFT OUTER JOIN M_InOutLine_To_C_Customs_Invoice_Line io_to_ci ON io_to_ci.C_Customs_Invoice_Line_ID = il.C_Customs_Invoice_Line_ID
@@ -48,8 +82,12 @@ FROM (SELECT (CASE
                LEFT OUTER JOIN C_Location loc ON ilbl.c_location_id = loc.c_location_id
                LEFT OUTER JOIN C_Country co ON loc.c_country_id = co.c_country_id
                LEFT OUTER JOIN C_Country_Trl ct ON ct.c_country_id = co.c_country_id AND ct.ad_language = p_AD_Language
+               LEFT OUTER JOIN C_Incoterms inc ON ci.c_incoterms_id = inc.c_incoterms_id
+               LEFT OUTER JOIN C_Incoterms_trl inc_trl ON inc.c_incoterms_id = inc_trl.c_incoterms_id AND inc_trl.ad_language = p_AD_Language
+               LEFT OUTER JOIN AD_Ref_List modt ON modt.AD_Reference_ID = 542040 AND modt.value = ci.ModeOfTransport
+               LEFT OUTER JOIN AD_Ref_List_Trl modt_trl ON modt.ad_ref_list_id = modt_trl.ad_ref_list_id AND modt_trl.ad_language = p_AD_Language
       WHERE il.C_Customs_Invoice_Id = p_c_customs_invoice_ID) AS d
-GROUP BY UOM, QtyPattern, country;
-$$
+GROUP BY UOM, QtyPattern, country, containernumber, sealnumber, Incoterms, ModeOfTransport;
+$BODY$
 ;
 
