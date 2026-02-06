22a35fa521db3474b0a71ad64fc97316099c0598
Adrian <adrian.stefan@metasfresh.com>
2026-02-06 14:20:19 +0200

    Skip validation for order changes triggered by the application instead of the user.

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/modelvalidator/C_Order.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/modelvalidator/C_Order.java
@@ -14,6 +14,7 @@ import org.adempiere.ad.modelvalidator.annotations.DocValidate;
 import org.adempiere.ad.modelvalidator.annotations.Interceptor;
 import org.adempiere.ad.modelvalidator.annotations.ModelChange;
 import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.model.ModelValidator;
 
 /*
@@ -112,13 +113,20 @@ public class C_Order
 					I_C_Order.COLUMNNAME_UpdatedBy })
 	public void assertChangeAllowed(@NonNull final I_C_Order order)
 	{
+		if (!InterfaceWrapperHelper.isUIAction(order))
+		{
+			// do nothing if the modification was triggered from the application, not by the user
+			return;
+		}
 		if (order.getQtyMoved().signum() == 0 || orderBL.isSalesOrder(order))
 		{
+			// not a PO or has no receipts
 			return;
 		}
 		final DocStatus docStatus = DocStatus.ofCode(order.getDocStatus());
 		if (!docStatus.isInProgress())
 		{
+			// document has not been reactivated
 			return;
 		}
 
