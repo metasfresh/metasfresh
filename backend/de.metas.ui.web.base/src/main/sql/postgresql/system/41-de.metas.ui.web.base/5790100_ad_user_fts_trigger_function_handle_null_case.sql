a203d3cc65407997762aa386e5cb4fba7f601ea0
Max Rieck <max.rieck@metasfresh.com>
2026-02-24 16:07:01 +0100

    User update performance fix - Prevent full postgresFTS reindex with partner not set

--- backend/de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5790100_ad_user_fts_trigger_function_handle_null_case.sql
+++ backend/de.metas.ui.web.base/src/main/sql/postgresql/system/41-de.metas.ui.web.base/5790100_ad_user_fts_trigger_function_handle_null_case.sql
@@ -2,7 +2,7 @@
  * #%L
  * de.metas.ui.web.base
  * %%
- * Copyright (C) 2025 metas GmbH
+ * Copyright (C) 2026 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
