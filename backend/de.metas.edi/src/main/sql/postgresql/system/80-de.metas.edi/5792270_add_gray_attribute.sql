43e8e2e827b2048cbc874124b4e7fa74e76eba02
Max Rieck <max.rieck@metasfresh.com>
2026-03-05 19:18:59 +0100

    add gray attribute

---
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5792270_add_gray_attribute.sql
@@ -0,0 +1,6 @@
+-- Run mode: WEBUI
+
+-- 2026-03-05T18:02:41.209Z
+INSERT INTO M_Attribute (AD_Client_ID,AD_Org_ID,AttributeValuesOrderBy,AttributeValueType,Created,CreatedBy,IsActive,IsAlwaysUpdateable,IsAttrDocumentRelevant,IsHighVolume,IsInstanceAttribute,IsMandatory,IsPricingRelevant,IsPrintedInDocument,IsReadOnlyValues,IsStorageRelevant,IsTransferWhenNull,M_Attribute_ID,Name,Updated,UpdatedBy,Value) VALUES (1000000,1000000,'V','S',TO_TIMESTAMP('2026-03-05 18:02:41.193000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','N','N','Y','N','N','N','N','N','N',540192,'GRAI',TO_TIMESTAMP('2026-03-05 18:02:41.193000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'GRAI')
+;
+
