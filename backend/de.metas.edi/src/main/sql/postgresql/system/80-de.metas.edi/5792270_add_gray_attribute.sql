c2b46244928d94cb0b6dd7a7fadbc3b291d7924a
Max Rieck <max.rieck@metasfresh.com>
2026-03-05 19:36:49 +0100

    add gray attribute

--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5792270_add_gray_attribute.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5792270_add_gray_attribute.sql
@@ -4,3 +4,10 @@
 INSERT INTO M_Attribute (AD_Client_ID,AD_Org_ID,AttributeValuesOrderBy,AttributeValueType,Created,CreatedBy,IsActive,IsAlwaysUpdateable,IsAttrDocumentRelevant,IsHighVolume,IsInstanceAttribute,IsMandatory,IsPricingRelevant,IsPrintedInDocument,IsReadOnlyValues,IsStorageRelevant,IsTransferWhenNull,M_Attribute_ID,Name,Updated,UpdatedBy,Value) VALUES (1000000,1000000,'V','S',TO_TIMESTAMP('2026-03-05 18:02:41.193000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','N','N','Y','N','N','N','N','N','N',540192,'GRAI',TO_TIMESTAMP('2026-03-05 18:02:41.193000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'GRAI')
 ;
 
+-- 2026-03-05T18:03:34.354Z
+INSERT INTO M_HU_PI_Attribute (AD_Client_ID,AD_Org_ID,Created,CreatedBy,HU_TansferStrategy_JavaClass_ID,IsActive,IsDisplayed,IsInstanceAttribute,IsMandatory,IsOnlyIfInProductAttributeSet,IsReadOnly,M_Attribute_ID,M_HU_PI_Attribute_ID,M_HU_PI_Version_ID,PropagationType,SeqNo,Updated,UpdatedBy,UseInASI) VALUES (1000000,1000000,TO_TIMESTAMP('2026-03-05 18:03:34.352000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,540027,'Y','Y','N','N','N','N',540192,540139,2001855,'NONE',10,TO_TIMESTAMP('2026-03-05 18:03:34.352000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y')
+;
+
+-- 2026-03-05T18:03:45.748Z
+UPDATE M_HU_PI_Attribute SET UseInASI='N',Updated=TO_TIMESTAMP('2026-03-05 18:03:45.748000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE M_HU_PI_Attribute_ID=540139
+;
