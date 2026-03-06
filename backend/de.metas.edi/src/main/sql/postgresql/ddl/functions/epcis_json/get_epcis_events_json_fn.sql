386a701a00e3aa992746017b336bedf366f476c1
Max Rieck <max.rieck@metasfresh.com>
2026-03-06 13:37:47 +0100

    adjust get_epcis_events_json_fn to provide tu-ean

--- backend/de.metas.edi/src/main/sql/postgresql/ddl/functions/epcis_json/get_epcis_events_json_fn.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/ddl/functions/epcis_json/get_epcis_events_json_fn.sql
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.edi
+ * %%
+ * Copyright (C) 2026 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 -- Function to assemble EPCIS event data as JSON for a given M_InOut (shipment).
 -- Returns a flat JSON structure that the JavaScript transform converts to EPCIS 1.2 XML.
 --
@@ -86,7 +108,7 @@ BEGIN
                      LEFT JOIN LATERAL (
                 SELECT JSONB_AGG(
                                JSONB_BUILD_OBJECT(
-                                       'productGTIN', prod.gtin,
+                                       'tuGTIN', pi_item.gtin_tu_packingmaterial,
                                        'productValue', prod.value,
                                        'lotNumber', pi_item.lotnumber,
                                        'bestBeforeDate', TO_CHAR(pi_item.bestbeforedate, 'YYYY-MM-DD'),
--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5791100_sys_get_epcis_events_json_fn.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5791100_sys_get_epcis_events_json_fn.sql
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.edi
+ * %%
+ * Copyright (C) 2026 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 -- Function for EPCIS event JSON assembly from shipment data
 -- See ddl/functions/epcis_json/get_epcis_events_json_fn.sql for the canonical DDL
 
@@ -59,7 +81,7 @@ BEGIN
                      LEFT JOIN LATERAL (
                 SELECT JSONB_AGG(
                                JSONB_BUILD_OBJECT(
-                                       'productGTIN', prod.gtin,
+                                       'tuGTIN', pi_item.gtin_tu_packingmaterial,
                                        'productValue', prod.value,
                                        'lotNumber', pi_item.lotnumber,
                                        'bestBeforeDate', TO_CHAR(pi_item.bestbeforedate, 'YYYY-MM-DD'),
