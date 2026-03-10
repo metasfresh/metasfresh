8820d840ba815bf3a44f633fd2ddf788d46ef460
Adrian <60300682+adi-stefan@users.noreply.github.com>
2026-03-10 17:26:37 +0200

    Change INSERT statement for M_Product.SyncEANAndGTIN
    Updated the insertion method for M_Product.SyncEANAndGTIN configuration.

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5793550_sys_gh28612_SyncEANAndGTIN_sysconfig.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5793550_sys_gh28612_SyncEANAndGTIN_sysconfig.sql
@@ -2,10 +2,6 @@
 -- When Y (default), GTIN/UPC/EAN_CU fields are synced automatically on M_Product and C_BPartner_Product.
 -- When N, fields can be maintained independently.
 
-INSERT INTO AD_SysConfig (AD_SysConfig_ID, AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy, IsActive,
-                          Name, Value, Description, EntityType, ConfigurationLevel)
-SELECT nextval('ad_sysconfig_seq'), 0, 0, now(), 100, now(), 100, 'Y',
-       'M_Product.SyncEANAndGTIN', 'Y',
-       'When Y, GTIN/UPC/EAN_CU fields are automatically synchronized on M_Product and C_BPartner_Product. When N, fields can be maintained independently.',
-       'de.metas.product', 'O'
-WHERE NOT EXISTS (SELECT 1 FROM AD_SysConfig WHERE Name = 'M_Product.SyncEANAndGTIN' AND AD_Client_ID = 0 AND AD_Org_ID = 0);
+INSERT INTO AD_SysConfig (AD_SysConfig_ID, AD_Client_ID, AD_Org_ID, Created, CreatedBy, Updated, UpdatedBy, IsActive, Name, Value, Description, EntityType, ConfigurationLevel)
+VALUES (541799, 0, 0, TO_TIMESTAMP('2026-03-10 11:11:11.000000', 'YYYY-MM-DD HH24:MI:SS.US'), 100, TO_TIMESTAMP('2026-03-10 11:11:11.000000', 'YYYY-MM-DD HH24:MI:SS.US'), 100, 'Y', 'M_Product.SyncEANAndGTIN', 'Y', 'When Y, GTIN/UPC/EAN_CU fields are automatically synchronized on M_Product and C_BPartner_Product. When N, fields can be maintained independently.', 'de.metas.product', 'O')
+;
