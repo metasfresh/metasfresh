4d2698f2c64747877b2ce32d53609dbd47e85ab1
riad.mennour <riad.mennour@gmail.com>
2026-03-10 20:27:06 +0100

    `Receipt Schedule` window update

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5793740_sys_gh28614_Receipt_Schedule_Window_Update.sql
@@ -0,0 +1,112 @@
+-- Run mode: SWING_CLIENT
+
+-- Column: M_ReceiptSchedule.Processed
+-- 2026-03-10T19:19:58.101Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=50,Updated=TO_TIMESTAMP('2026-03-10 19:19:58.100000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=549513
+;
+
+-- Column: M_ReceiptSchedule.POReference
+-- 2026-03-10T19:20:05.170Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=60,Updated=TO_TIMESTAMP('2026-03-10 19:20:05.170000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=588327
+;
+
+-- Column: M_ReceiptSchedule.C_BPartner_ID
+-- 2026-03-10T19:20:12.452Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=70,Updated=TO_TIMESTAMP('2026-03-10 19:20:12.452000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=549499
+;
+
+-- Column: M_ReceiptSchedule.C_BP_Group_ID
+-- 2026-03-10T19:20:18.169Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=80,Updated=TO_TIMESTAMP('2026-03-10 19:20:18.169000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591609
+;
+
+-- Column: M_ReceiptSchedule.C_Order_ID
+-- 2026-03-10T19:20:25.486Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=90,Updated=TO_TIMESTAMP('2026-03-10 19:20:25.486000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=549488
+;
+
+-- Column: M_ReceiptSchedule.M_Product_ID
+-- 2026-03-10T19:20:31.663Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=100,Updated=TO_TIMESTAMP('2026-03-10 19:20:31.663000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=549508
+;
+
+-- Column: M_ReceiptSchedule.M_Warehouse_Effective_ID
+-- 2026-03-10T19:20:39.743Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=110,Updated=TO_TIMESTAMP('2026-03-10 19:20:39.743000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=570612
+;
+
+-- Column: M_ReceiptSchedule.ExportStatus
+-- 2026-03-10T19:20:46.671Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=120,Updated=TO_TIMESTAMP('2026-03-10 19:20:46.671000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=571008
+;
+
+-- Column: M_ReceiptSchedule.Shipper_BPartner_ID
+-- 2026-03-10T19:20:52.611Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=130,Updated=TO_TIMESTAMP('2026-03-10 19:20:52.611000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591271
+;
+
+-- Column: M_ReceiptSchedule.ETD
+-- 2026-03-10T19:21:00.243Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=140,Updated=TO_TIMESTAMP('2026-03-10 19:21:00.243000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591259
+;
+
+-- Column: M_ReceiptSchedule.ETA
+-- 2026-03-10T19:21:10.709Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=150,Updated=TO_TIMESTAMP('2026-03-10 19:21:10.709000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591265
+;
+
+-- Column: M_ReceiptSchedule.ATD
+-- 2026-03-10T19:21:18.134Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=160,Updated=TO_TIMESTAMP('2026-03-10 19:21:18.134000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591261
+;
+
+-- Column: M_ReceiptSchedule.ATA
+-- 2026-03-10T19:21:27.139Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=170,Updated=TO_TIMESTAMP('2026-03-10 19:21:27.139000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591260
+;
+
+-- Column: M_ReceiptSchedule.CRD
+-- 2026-03-10T19:21:33.785Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=180,Updated=TO_TIMESTAMP('2026-03-10 19:21:33.785000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591264
+;
+
+-- Column: M_ReceiptSchedule.ContainerNo
+-- 2026-03-10T19:21:42.827Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=190,Updated=TO_TIMESTAMP('2026-03-10 19:21:42.827000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591263
+;
+
+-- Column: M_ReceiptSchedule.TrackingID
+-- 2026-03-10T19:21:51.531Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=200,Updated=TO_TIMESTAMP('2026-03-10 19:21:51.531000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591272
+;
+
+-- Column: M_ReceiptSchedule.IsBLReceived
+-- 2026-03-10T19:21:59.197Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=210,Updated=TO_TIMESTAMP('2026-03-10 19:21:59.197000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591266
+;
+
+-- Column: M_ReceiptSchedule.IsBookingConfirmed
+-- 2026-03-10T19:22:10.101Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=220,Updated=TO_TIMESTAMP('2026-03-10 19:22:10.101000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591267
+;
+
+-- Column: M_ReceiptSchedule.IsWENotice
+-- 2026-03-10T19:22:18.987Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=230,Updated=TO_TIMESTAMP('2026-03-10 19:22:18.987000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591268
+;
+
+-- Column: M_ReceiptSchedule.VesselName
+-- 2026-03-10T19:22:28.079Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=240,Updated=TO_TIMESTAMP('2026-03-10 19:22:28.079000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591273
+;
+
+-- Column: M_ReceiptSchedule.POL_ID
+-- 2026-03-10T19:22:41.024Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=250,Updated=TO_TIMESTAMP('2026-03-10 19:22:41.024000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591270
+;
+
+-- Column: M_ReceiptSchedule.POD_ID
+-- 2026-03-10T19:22:50.012Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=260,Updated=TO_TIMESTAMP('2026-03-10 19:22:50.012000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Column_ID=591269
+;
+
