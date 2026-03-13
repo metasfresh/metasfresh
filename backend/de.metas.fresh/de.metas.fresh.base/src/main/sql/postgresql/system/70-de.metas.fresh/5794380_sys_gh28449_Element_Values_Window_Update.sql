99a0f8558976f4379c4575ba78f398df4231033c
riad.mennour <riad.mennour@gmail.com>
2026-03-13 11:11:42 +0100

    `Element Values` window update

---
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5794380_sys_gh28449_Element_Values_Window_Update.sql
@@ -0,0 +1,126 @@
+-- Run mode: SWING_CLIENT
+
+-- Column: C_ElementValue.AccountConceptualName
+-- 2026-03-13T10:04:32.058Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=10,Updated=TO_TIMESTAMP('2026-03-13 11:04:32.058','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=589915
+;
+
+-- Column: C_ElementValue.AD_Org_ID
+-- 2026-03-13T10:04:39.166Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=20,Updated=TO_TIMESTAMP('2026-03-13 11:04:39.165','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1129
+;
+
+-- Column: C_ElementValue.Name
+-- 2026-03-13T10:04:42.677Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=30,Updated=TO_TIMESTAMP('2026-03-13 11:04:42.677','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1135
+;
+
+-- Column: C_ElementValue.Value
+-- 2026-03-13T10:04:47.290Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=40,Updated=TO_TIMESTAMP('2026-03-13 11:04:47.29','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1234
+;
+
+-- Column: C_ElementValue.AccountType
+-- 2026-03-13T10:04:50.280Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=50,Updated=TO_TIMESTAMP('2026-03-13 11:04:50.28','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1127
+;
+
+-- Column: C_ElementValue.C_Element_ID
+-- 2026-03-13T10:04:53.296Z
+UPDATE AD_Column SET IsSelectionColumn='Y', IsUpdateable='N', SelectionColumnSeqNo=60,Updated=TO_TIMESTAMP('2026-03-13 11:04:53.296','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1137
+;
+
+-- Column: C_ElementValue.IsActive
+-- 2026-03-13T10:04:56.953Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=70,Updated=TO_TIMESTAMP('2026-03-13 11:04:56.953','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1130
+;
+
+-- Column: C_ElementValue.Name
+-- 2026-03-13T10:05:26.222Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=10,Updated=TO_TIMESTAMP('2026-03-13 11:05:26.222','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1135
+;
+
+-- Column: C_ElementValue.Value
+-- 2026-03-13T10:05:30.815Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=20,Updated=TO_TIMESTAMP('2026-03-13 11:05:30.814','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1234
+;
+
+-- Column: C_ElementValue.AccountType
+-- 2026-03-13T10:05:34.795Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=30,Updated=TO_TIMESTAMP('2026-03-13 11:05:34.795','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1127
+;
+
+-- Column: C_ElementValue.C_Element_ID
+-- 2026-03-13T10:05:37.842Z
+UPDATE AD_Column SET IsSelectionColumn='Y', IsUpdateable='N', SelectionColumnSeqNo=40,Updated=TO_TIMESTAMP('2026-03-13 11:05:37.842','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1137
+;
+
+-- Column: C_ElementValue.AccountConceptualName
+-- 2026-03-13T10:05:40.721Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=50,Updated=TO_TIMESTAMP('2026-03-13 11:05:40.721','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=589915
+;
+
+-- Column: C_ElementValue.IsActive
+-- 2026-03-13T10:05:43.404Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=60,Updated=TO_TIMESTAMP('2026-03-13 11:05:43.404','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1130
+;
+
+-- Column: C_ElementValue.AD_Org_ID
+-- 2026-03-13T10:05:46.150Z
+UPDATE AD_Column SET IsSelectionColumn='Y', SelectionColumnSeqNo=70,Updated=TO_TIMESTAMP('2026-03-13 11:05:46.15','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_Column_ID=1129
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 20 -> flags.Aktiv
+-- Column: C_ElementValue.IsActive
+-- 2026-03-13T10:06:12.496Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2026-03-13 11:06:12.496','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564077
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 10 -> default.Suchschlüssel
+-- Column: C_ElementValue.Value
+-- 2026-03-13T10:06:13.013Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2026-03-13 11:06:13.013','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564072
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 10 -> default.Name
+-- Column: C_ElementValue.Name
+-- 2026-03-13T10:06:13.519Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2026-03-13 11:06:13.519','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564073
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 10 -> desc.Beschreibung
+-- Column: C_ElementValue.Description
+-- 2026-03-13T10:06:14.026Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2026-03-13 11:06:14.026','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564074
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 20 -> flags.Zusammenfassungseintrag
+-- Column: C_ElementValue.IsSummary
+-- 2026-03-13T10:06:14.537Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2026-03-13 11:06:14.537','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564078
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 10 -> desc.Kontenart
+-- Column: C_ElementValue.AccountType
+-- 2026-03-13T10:06:15.043Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2026-03-13 11:06:15.043','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564075
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 10 -> desc.Kontovorzeichen
+-- Column: C_ElementValue.AccountSign
+-- 2026-03-13T10:06:15.565Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2026-03-13 11:06:15.565','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564076
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 20 -> orgs.Sektion
+-- Column: C_ElementValue.AD_Org_ID
+-- 2026-03-13T10:06:16.069Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=90,Updated=TO_TIMESTAMP('2026-03-13 11:06:16.069','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564083
+;
+
+-- UI Element: Konten(540761,D) -> Kontenart(542127,D) -> main -> 20 -> orgs.Mandant
+-- Column: C_ElementValue.AD_Client_ID
+-- 2026-03-13T10:06:16.549Z
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2026-03-13 11:06:16.549','YYYY-MM-DD HH24:MI:SS.US'),UpdatedBy=100 WHERE AD_UI_Element_ID=564084
+;
+
