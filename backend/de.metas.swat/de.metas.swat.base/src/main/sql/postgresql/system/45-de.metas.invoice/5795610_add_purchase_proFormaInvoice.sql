c0de2793926c4b6f5191357214a20bde892f56bf
Max Rieck <max.rieck@metasfresh.com>
2026-03-26 14:53:33 +0100

    add isFinancial flag

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.invoice/5795610_add_purchase_proFormaInvoice.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.invoice/5795610_add_purchase_proFormaInvoice.sql
@@ -89,3 +89,8 @@ UPDATE ad_ref_list
 SET valuename = 'ShipperTransportation',Updated=TO_TIMESTAMP('2026-03-25 15:38:04.053000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100
 WHERE ad_ref_list_id = 540166
 ;
+
+-- Name: C_DocType AR/AP normal Invoices and Credit Memos
+-- 2026-03-26T10:31:36.154Z
+UPDATE AD_Val_Rule SET Code='( C_DocType.DocBaseType IN (''API'',''APC'',''APF'') OR (C_DocType.DocBaseType IN (''ARC'',''ARI'') AND C_DocType.DocSubType IS NULL) /*only the default types*/ OR (C_DocType.DocBaseType=''ARC'' AND C_DocType.DocSubType = ''CS'') /*only the RMA-credit-memo*/ ) AND C_DocType.IsSOTrx=''@IsSOTrx@'' AND C_DocType.AD_Org_ID IN (@AD_Org_ID/-1@, 0)',Updated=TO_TIMESTAMP('2026-03-26 10:31:36.152000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Val_Rule_ID=540294
+;
