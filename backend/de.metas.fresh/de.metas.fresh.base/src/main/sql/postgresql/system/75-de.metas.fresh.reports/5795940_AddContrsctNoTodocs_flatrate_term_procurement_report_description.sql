a552822f5847a203b56ad45e0314662054ea59a8
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-03-26 17:45:08 +0200

    Rename report function and parameterize args
    Rename function to Docs_Flatrate_Term_Procurement_Report_Description and change input parameters to p_record_id / p_ad_language. Replace positional $1/$2 usage with the named parameters throughout the query (joins and WHERE). Remove several isActive filters from JOINs and the ft.isActive check to broaden/select rows as intended. Apply same updates to both the DDL function and the corresponding system migration SQL.

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5795940_AddContrsctNoTodocs_flatrate_term_procurement_report_description.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5795940_AddContrsctNoTodocs_flatrate_term_procurement_report_description.sql
@@ -1,9 +1,9 @@
-DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.docs_flatrate_term_procurement_report_description(IN record_id   numeric,
-                                                                                                             IN ad_language character varying)
+DROP FUNCTION IF EXISTS de_metas_endcustomer_fresh_reports.Docs_Flatrate_Term_Procurement_Report_Description(IN p_record_id   numeric,
+                                                                                                             IN p_ad_language character varying)
 ;
 
-CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.docs_flatrate_term_procurement_report_description(IN record_id   numeric,
-                                                                                                                IN ad_language character varying)
+CREATE OR REPLACE FUNCTION de_metas_endcustomer_fresh_reports.Docs_Flatrate_Term_Procurement_Report_Description(IN p_record_id   numeric,
+                                                                                                                IN p_ad_language character varying)
     RETURNS TABLE
             (
                 ContractNo    character varying,
@@ -31,24 +31,23 @@ SELECT
     y_end.FiscalYear
 
 FROM C_Flatrate_Term ft
-         JOIN C_Flatrate_Conditions fc ON ft.C_Flatrate_Conditions_id = fc.C_Flatrate_Conditions_ID AND fc.isActive = 'Y'
-         JOIN PMM_Product pmm ON ft.PMM_Product_ID = pmm.PMM_Product_ID AND pmm.isActive = 'Y'
-         JOIN C_BPartner billBP ON ft.bill_bpartner_id = billBP.C_BPartner_ID AND billBP.isActive = 'Y'
-         JOIN AD_User u ON ft.ad_user_incharge_ID = u.AD_User_ID AND u.isActive = 'Y'
-         JOIN C_UOM uom ON ft.C_UOM_ID = uom.C_UOM_ID AND uom.isActive = 'Y'
-         LEFT OUTER JOIN C_UOM_trl uom_trl ON uom.C_UOM_ID = uom_trl.C_UOM_ID AND uom_trl.ad_language = $2 AND uom_trl.isActive = 'Y'
+         JOIN C_Flatrate_Conditions fc ON ft.C_Flatrate_Conditions_id = fc.C_Flatrate_Conditions_ID
+         JOIN PMM_Product pmm ON ft.PMM_Product_ID = pmm.PMM_Product_ID
+         JOIN C_BPartner billBP ON ft.bill_bpartner_id = billBP.C_BPartner_ID
+         JOIN AD_User u ON ft.ad_user_incharge_ID = u.AD_User_ID
+         JOIN C_UOM uom ON ft.C_UOM_ID = uom.C_UOM_ID
+         LEFT OUTER JOIN C_UOM_trl uom_trl ON uom.C_UOM_ID = uom_trl.C_UOM_ID AND uom_trl.ad_language = p_ad_language
 
-         JOIN c_period p_param_st ON ft.startdate >= p_param_st.startdate AND ft.startdate <= p_param_st.enddate AND p_param_st.isActive = 'Y'
-         LEFT OUTER JOIN C_Period_trl period_trl_st ON p_param_st.c_period_ID = period_trl_st.c_period_ID AND period_trl_st.ad_language = $2 AND period_trl_st.isActive = 'Y'
-         JOIN c_period p_param_end ON ft.enddate >= p_param_end.startdate AND ft.enddate <= p_param_end.enddate AND p_param_end.isActive = 'Y'
-         LEFT OUTER JOIN C_Period_trl period_trl_end ON p_param_end.c_period_ID = period_trl_end.c_period_ID AND period_trl_end.ad_language = $2 AND period_trl_end.isActive = 'Y'
+         JOIN c_period p_param_st ON ft.startdate >= p_param_st.startdate AND ft.startdate <= p_param_st.enddate
+         LEFT OUTER JOIN C_Period_trl period_trl_st ON p_param_st.c_period_ID = period_trl_st.c_period_ID AND period_trl_st.ad_language = p_ad_language
+         JOIN c_period p_param_end ON ft.enddate >= p_param_end.startdate AND ft.enddate <= p_param_end.enddate
+         LEFT OUTER JOIN C_Period_trl period_trl_end ON p_param_end.c_period_ID = period_trl_end.c_period_ID AND period_trl_end.ad_language = p_ad_language
 
-         JOIN C_Year y_st ON p_param_st.C_Year_ID = y_st.C_Year_ID AND y_st.isActive = 'Y'
-         JOIN C_Year y_end ON p_param_end.C_Year_ID = y_end.C_Year_ID AND y_end.isActive = 'Y'
+         JOIN C_Year y_st ON p_param_st.C_Year_ID = y_st.C_Year_ID
+         JOIN C_Year y_end ON p_param_end.C_Year_ID = y_end.C_Year_ID
 
-WHERE ft.C_Flatrate_Term_id = $1
+WHERE ft.C_Flatrate_Term_id = p_record_id
   AND fc.type_conditions = 'Procuremnt'
-  AND ft.isActive = 'Y'
 
 $$
     LANGUAGE sql STABLE
