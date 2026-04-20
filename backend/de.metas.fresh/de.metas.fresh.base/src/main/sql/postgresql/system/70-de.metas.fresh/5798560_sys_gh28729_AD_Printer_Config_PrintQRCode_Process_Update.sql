973799542f3df3d58d28fd3234f28311da4b99c0
riad.mennour <riad.mennour@gmail.com>
2026-04-20 11:12:07 +0100

    `QR-Codes drucken` reports update

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5798560_sys_gh28729_AD_Printer_Config_PrintQRCode_Process_Update.sql
+++
@@ -1,9 +0,0 @@
--- Run mode: SWING_CLIENT
-
--- Value: AD_Printer_Config_PrintQRCode
--- Classname: de.metas.report.jasper.client.process.JasperReportStarter
--- JasperReport: @PREFIX@de/metas/docs/label/printer_config/Label.jasper
--- 2026-04-20T09:53:20.453Z
-UPDATE AD_Process SET JasperReport='@PREFIX@de/metas/docs/label/printer_config/Label.jasper',Updated=TO_TIMESTAMP('2026-04-20 09:53:20.451000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Process_ID=585517
-;
-
