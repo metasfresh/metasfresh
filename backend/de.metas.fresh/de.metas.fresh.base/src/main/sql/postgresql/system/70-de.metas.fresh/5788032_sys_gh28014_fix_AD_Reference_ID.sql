89f07406e6cdf4b3d662162c892765618c885983
metas-rc <ruxandra.craciunescu@metasfresh.com>
2026-04-22 14:47:32 +0300

    Avoid duplicate references where the correction was already done

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5788032_sys_gh28014_fix_AD_Reference_ID.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5788032_sys_gh28014_fix_AD_Reference_ID.sql
@@ -8,7 +8,7 @@
 -- 2nd: NOW: add this file to update DBs where the SQL with the wrong ID was already applied     
 --
 
-UPDATE AD_Reference SET AD_Reference_ID=542058 WHERE AD_Reference_ID=542051;
-UPDATE AD_Reference_Trl SET AD_Reference_ID=542058 WHERE AD_Reference_ID=542051;
+UPDATE AD_Reference SET AD_Reference_ID=542058 WHERE AD_Reference_ID=542051 and not exists (select 1 from AD_Reference where AD_Reference_ID=542058);
+UPDATE AD_Reference_Trl SET AD_Reference_ID=542058 WHERE AD_Reference_ID=542051 and not exists (select 1 from AD_Reference_Trl where AD_Reference_ID=542058);
 UPDATE AD_Ref_Table SET AD_Reference_ID=542058 WHERE AD_Reference_ID=542051;
 UPDATE AD_Process_Para SET AD_Reference_Value_ID=542058 WHERE AD_Reference_Value_ID=542051;
