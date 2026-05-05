8f9255094d1cb0f643d127d42aaba60c22b6b71d
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-05-05 21:30:43 +0300

    Fix missing AD_Element

--- backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5800891_AddMissingColumnAndMinorFixes.sql
+++ backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5800891_AddMissingColumnAndMinorFixes.sql
@@ -53,6 +53,51 @@ INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Clien
 ;
 
 
+-- 2026-05-05T13:02:39.509Z
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,584825,0,TO_TIMESTAMP('2026-05-05 13:02:39.369000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','ExportEXTF','Export as EXTF File',TO_TIMESTAMP('2026-05-05 13:02:39.369000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+;
+
+-- 2026-05-05T13:02:39.515Z
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=584825 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- Element: null
+-- 2026-05-05T13:03:44.172Z
+UPDATE AD_Element_Trl SET IsTranslated='Y', Name='Als EXTF-Datei exportieren', PrintName='Als EXTF-Datei exportieren',Updated=TO_TIMESTAMP('2026-05-05 13:03:44.172000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=584825 AND AD_Language='de_CH'
+;
+
+-- 2026-05-05T13:03:44.173Z
+UPDATE AD_Element base SET Name=trl.Name, PrintName=trl.PrintName, Updated=trl.Updated, UpdatedBy=trl.UpdatedBy FROM AD_Element_Trl trl  WHERE trl.AD_Element_ID=base.AD_Element_ID AND trl.AD_Language='de_CH' AND trl.AD_Language=getBaseLanguage()
+;
+
+-- 2026-05-05T13:03:44.375Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(584825,'de_CH')
+;
+
+-- Element: null
+-- 2026-05-05T13:03:48.376Z
+UPDATE AD_Element_Trl SET IsTranslated='Y',Updated=TO_TIMESTAMP('2026-05-05 13:03:48.376000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=584825 AND AD_Language='en_US'
+;
+
+-- 2026-05-05T13:03:48.380Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(584825,'en_US')
+;
+
+-- 2026-05-05T13:04:23.689Z
+UPDATE AD_Element SET ColumnName='ExportEXTF',Updated=TO_TIMESTAMP('2026-05-05 13:04:23.689000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_Element_ID=584825
+;
+
+-- 2026-05-05T13:04:23.690Z
+UPDATE AD_Column SET ColumnName='ExportEXTF' WHERE AD_Element_ID=584825
+;
+
+-- 2026-05-05T13:04:23.691Z
+UPDATE AD_Process_Para SET ColumnName='ExportEXTF' WHERE AD_Element_ID=584825
+;
+
+-- 2026-05-05T13:04:23.694Z
+/* DDL */  select update_TRL_Tables_On_AD_Element_TRL_Update(584825,'de_DE')
+;
 
 -- Process: DATEV_ExportFile(de.metas.datev.process.DATEV_ExportFile)
 -- ParameterName: ExportEXTF
