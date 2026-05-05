a2dd78b2685d058a20aa986d225090b02142b0c7
metas-ts <tobias.schoeneberg@metasfresh.com>
2026-05-05 14:36:25 +0200

    Fix: callout works even if no C_BPartner_Stats record exists

--- backend/de.metas.business/src/main/java-legacy/org/compiere/model/CalloutInvoice.java
+++ backend/de.metas.business/src/main/java-legacy/org/compiere/model/CalloutInvoice.java
@@ -113,23 +113,18 @@ public class CalloutInvoice extends CalloutEngine
 
 		final boolean isSOTrx = invoice.isSOTrx();
 
-		final StringBuilder sql = new StringBuilder().append("SELECT p.AD_Language,p.C_PaymentTerm_ID,"
-																	 + " COALESCE(p.M_PriceList_ID,g.M_PriceList_ID) AS M_PriceList_ID, p.PaymentRule,p.POReference,"
-																	 + " p.SO_Description,p.IsDiscountPrinted, "
-																	 + " stats." + I_C_BPartner_Stats.COLUMNNAME_SO_CreditUsed + ", "
-																	 + " l.C_BPartner_Location_ID,c.AD_User_ID,"
-																	 + " COALESCE(p.PO_PriceList_ID,g.PO_PriceList_ID) AS PO_PriceList_ID, p.PaymentRulePO,p.PO_PaymentTerm_ID "
-																	 + "FROM C_BPartner p"
-																	 + " INNER JOIN "
-																	 + I_C_BPartner_Stats.Table_Name
-																	 + " stats ON (p."
-																	 + I_C_BPartner.COLUMNNAME_C_BPartner_ID
-																	 + " = stats."
-																	 + I_C_BPartner_Stats.COLUMNNAME_C_BPartner_ID
-																	 + ")"
-																	 + " INNER JOIN C_BP_Group g ON (p.C_BP_Group_ID=g.C_BP_Group_ID)"
-																	 + " LEFT OUTER JOIN C_BPartner_Location l ON (p.C_BPartner_ID=l.C_BPartner_ID AND l.IsBillTo='Y' AND l.IsActive='Y')"
-																	 + " LEFT OUTER JOIN AD_User c ON (p.C_BPartner_ID=c.C_BPartner_ID) ");
+		final StringBuilder sql = new StringBuilder()
+				.append("SELECT p.AD_Language,p.C_PaymentTerm_ID,"
+						+ " COALESCE(p.M_PriceList_ID,g.M_PriceList_ID) AS M_PriceList_ID, p.PaymentRule,p.POReference,"
+						+ " p.SO_Description,p.IsDiscountPrinted, "
+						+ " stats." + I_C_BPartner_Stats.COLUMNNAME_SO_CreditUsed + ", "
+						+ " l.C_BPartner_Location_ID,c.AD_User_ID,"
+						+ " COALESCE(p.PO_PriceList_ID,g.PO_PriceList_ID) AS PO_PriceList_ID, p.PaymentRulePO,p.PO_PaymentTerm_ID "
+						+ "FROM C_BPartner p"
+						+ " INNER JOIN C_BP_Group g ON (p.C_BP_Group_ID=g.C_BP_Group_ID)"
+						+ " LEFT OUTER JOIN " + I_C_BPartner_Stats.Table_Name + " stats ON (p." + I_C_BPartner.COLUMNNAME_C_BPartner_ID + " = stats." + I_C_BPartner_Stats.COLUMNNAME_C_BPartner_ID + ")"
+						+ " LEFT OUTER JOIN C_BPartner_Location l ON (p.C_BPartner_ID=l.C_BPartner_ID AND l.IsBillTo='Y' AND l.IsActive='Y')"
+						+ " LEFT OUTER JOIN AD_User c ON (p.C_BPartner_ID=c.C_BPartner_ID) ");
 
 		if (isAllowOnlyBillToDefault_Contact && isSOTrx)
 		{
@@ -237,7 +232,7 @@ public class CalloutInvoice extends CalloutEngine
 					final BigDecimal CreditLimit = creditLimitRepo.retrieveCreditLimitByBPartnerId(bPartnerID, invoice.getDateInvoiced());
 					if (CreditLimit.signum() > 0)
 					{
-						final double creditUsed = rs.getDouble("SO_CreditUsed");
+						final double creditUsed = rs.getDouble(I_C_BPartner_Stats.COLUMNNAME_SO_CreditUsed); // if column=null then double=0
 						final BigDecimal CreditAvailable = CreditLimit.subtract(BigDecimal.valueOf(creditUsed));
 						if (!rs.wasNull() && CreditAvailable.signum() < 0)
 						{
