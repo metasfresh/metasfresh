metasfresh/metasfresh

commit a9d3375b38fd7ffecb2f6c985abdbbbd7a9925b2

major fix claude slop

backend/de.metas.business/src/main/java/de/metas/order/paymentschedule/core/repository/OrderPayScheduleLoaderAndSaver.java
@@ -1,0 +1,253 @@
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.order.paymentschedule.core.repository;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import de.metas.inout.InOutId;
+import de.metas.invoice.InvoiceId;
+import de.metas.money.CurrencyId;
+import de.metas.money.Money;
+import de.metas.order.OrderId;
+import de.metas.order.paymentschedule.core.OrderPaySchedule;
+import de.metas.order.paymentschedule.core.OrderPayScheduleId;
+import de.metas.order.paymentschedule.core.OrderPayScheduleLine;
+import de.metas.order.paymentschedule.core.OrderPayScheduleStatus;
+import de.metas.payment.paymentterm.PaymentTermBreakId;
+import de.metas.payment.paymentterm.ReferenceDateType;
+import de.metas.util.GuavaCollectors;
+import de.metas.util.collections.CollectionUtils;
+import de.metas.util.lang.Percent;
+import de.metas.util.lang.SeqNoProvider;
+import lombok.Builder;
+import lombok.NonNull;
+import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.trx.api.ITrxManager;
+import org.adempiere.exceptions.AdempiereException;
+import org.adempiere.model.InterfaceWrapperHelper;
+import org.compiere.model.I_C_OrderPaySchedule;
+import org.compiere.util.TimeUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+@Builder
+public class OrderPayScheduleLoaderAndSaver
+{
+	@NonNull private final ITrxManager trxManager;
+	@NonNull private final IQueryBL queryBL;
+
+	private final HashMap<OrderId, ImmutableList<I_C_OrderPaySchedule>> recordsByOrderId = new HashMap<>();
+
+	public Optional<OrderPaySchedule> loadByOrderId(@NonNull final OrderId orderId)
+	{
+		final List<I_C_OrderPaySchedule> records = getRecordsByOrderId(orderId);
+		if (records.isEmpty())
+		{
+			return Optional.empty();
+		}
+
+		final ImmutableList<OrderPayScheduleLine> lines = records.stream()
+				.map(OrderPayScheduleLoaderAndSaver::fromRecord)
+				.collect(ImmutableList.toImmutableList());
+
+		return Optional.of(OrderPaySchedule.ofList(orderId, lines));
+	}
+
+	private void warmUpByOrderIds(final Set<OrderId> orderIds)
+	{
+		CollectionUtils.getAllOrLoad(recordsByOrderId, orderIds, this::retrieveRecordsByOrderId);
+	}
+
+	private List<I_C_OrderPaySchedule> getRecordsByOrderId(@NonNull final OrderId orderId)
+	{
+		return CollectionUtils.getOrLoad(recordsByOrderId, orderId, this::retrieveRecordsByOrderId);
+	}
+
+	private Map<OrderId, ImmutableList<I_C_OrderPaySchedule>> retrieveRecordsByOrderId(@NonNull final Set<OrderId> orderIds)
+	{
+		if (orderIds.isEmpty())
+		{
+			return ImmutableMap.of();
+		}
+
+		final Map<OrderId, ImmutableList<I_C_OrderPaySchedule>> recordsByOrderId = queryBL.createQueryBuilder(I_C_OrderPaySchedule.class)
+				.addInArrayFilter(I_C_OrderPaySchedule.COLUMNNAME_C_Order_ID, orderIds)
+				.orderBy(I_C_OrderPaySchedule.COLUMNNAME_SeqNo)
+				.create()
+				.stream()
+				.collect(Collectors.groupingBy(
+						OrderPayScheduleLoaderAndSaver::extractOrderId,
+						ImmutableList.toImmutableList()
+				));
+
+		final HashMap<OrderId, ImmutableList<I_C_OrderPaySchedule>> result = new HashMap<>();
+		for (final OrderId orderId : orderIds)
+		{
+			final ImmutableList<I_C_OrderPaySchedule> records = recordsByOrderId.get(orderId);
+			result.put(orderId, records != null ? records : ImmutableList.of());
+		}
+
+		return result;
+	}
+
+	private void invalidateCache(final OrderId orderId)
+	{
+		recordsByOrderId.remove(orderId);
+	}
+
+	private static OrderId extractOrderId(@NonNull final I_C_OrderPaySchedule record)
+	{
+		return OrderId.ofRepoId(record.getC_Order_ID());
+	}
+
+	@NonNull
+	private static OrderPayScheduleLine fromRecord(@NonNull final I_C_OrderPaySchedule record)
+	{
+		final CurrencyId currencyId = CurrencyId.ofRepoId(record.getC_Currency_ID());
+		final BigDecimal dueAmtActualBD = InterfaceWrapperHelper.getValueAsBigDecimalOrNull(record, I_C_OrderPaySchedule.COLUMNNAME_DueAmt_Actual);
+		return OrderPayScheduleLine.builder()
+				.id(extractId(record))
+				.orderId(OrderId.ofRepoId(record.getC_Order_ID()))
+				//
+				.paymentTermBreakId(PaymentTermBreakId.ofRepoId(record.getC_PaymentTerm_ID(), record.getC_PaymentTerm_Break_ID()))
+				.referenceDateType(ReferenceDateType.ofCode(record.getReferenceDateType()))
+				.percent(Percent.of(record.getPercent()))
+				.offsetDays(record.getOffsetDays())
+				//
+				.status(OrderPayScheduleStatus.ofCode(record.getStatus()))
+				.isPaid(record.isPaid())
+				.referenceDate(TimeUtil.asLocalDate(record.getReferenceDate()))
+				.dueDate(TimeUtil.asLocalDate(record.getDueDate()))
+				.baseAmount(Money.of(record.getBaseAmt(), currencyId))
+				.dueAmount(Money.of(record.getDueAmt(), currencyId))
+				.dueAmountActual(dueAmtActualBD != null ? Money.of(dueAmtActualBD, currencyId) : null)
+				//
+				.invoiceId(InvoiceId.ofRepoIdOrNull(record.getC_Invoice_ID()))
+				.inoutId(InOutId.ofRepoIdOrNull(record.getM_InOut_ID()))
+				//
+				.build();
+	}
+
+	private static @NotNull OrderPayScheduleId extractId(final @NotNull I_C_OrderPaySchedule record)
+	{
+		return OrderPayScheduleId.ofRepoId(record.getC_OrderPaySchedule_ID());
+	}
+
+	//
+	//
+	//
+	// --- Saving and Updating --------------------------------------------------
+	//
+	//
+	//
+
+	public void save(@NonNull final OrderPaySchedule orderPaySchedule)
+	{
+		trxManager.runInThreadInheritedTrx(() -> save0(orderPaySchedule));
+	}
+
+	private void save0(@NonNull final OrderPaySchedule orderPaySchedule)
+	{
+		final OrderId orderId = orderPaySchedule.getOrderId();
+		final HashMap<OrderPayScheduleId, I_C_OrderPaySchedule> records = getRecordsByOrderId(orderId)
+				.stream()
+				.collect(GuavaCollectors.toHashMapByKey(OrderPayScheduleLoaderAndSaver::extractId));
+		final SeqNoProvider seqNoProvider = SeqNoProvider.start();
+
+		for (final OrderPayScheduleLine line : orderPaySchedule.getLines())
+		{
+			final I_C_OrderPaySchedule record = line.getId() == null
+					? InterfaceWrapperHelper.newInstance(I_C_OrderPaySchedule.class)
+					: records.remove(line.getId());
+
+			if (record == null)
+			{
+				throw new AdempiereException("No record found by " + line.getId());
+			}
+
+			updateRecord(record, line, seqNoProvider);
+			InterfaceWrapperHelper.save(record);
+			line.markSaved(extractId(record));
+		}
+
+		InterfaceWrapperHelper.deleteAll(records.values());
+
+		invalidateCache(orderId);
+	}
+
+	private static void updateRecord(
+			@NonNull final I_C_OrderPaySchedule record,
+			@NonNull final OrderPayScheduleLine from,
+			@NonNull final SeqNoProvider seqNoProvider)
+	{
+		record.setC_Order_ID(from.getOrderId().getRepoId());
+		record.setC_PaymentTerm_ID(from.getPaymentTermBreakId().getPaymentTermId().getRepoId());
+		record.setC_PaymentTerm_Break_ID(from.getPaymentTermBreakId().getRepoId());
+		record.setDueAmt(from.getDueAmount().toBigDecimal());
+		record.setC_Currency_ID(from.getDueAmount().getCurrencyId().getRepoId());
+		record.setDueDate(TimeUtil.asTimestamp(from.getDueDate()));
+		record.setPercent(from.getPercent().toInt());
+		record.setOffsetDays(from.getOffsetDays());
+		record.setReferenceDateType(from.getReferenceDateType().getCode());
+		record.setSeqNo(seqNoProvider.getAndIncrement().toInt());
+		record.setStatus(OrderPayScheduleStatus.toCodeOrNull(from.getStatus()));
+		record.setDueAmt_Actual(from.getDueAmountActual() != null ? from.getDueAmountActual().toBigDecimal() : null);
+		record.setReferenceDate(TimeUtil.asTimestamp(from.getReferenceDate()));
+		record.setIsPaid(from.isPaid());
+	}
+
+	public void updateByIds(@NonNull final Set<OrderId> orderIds, @NonNull final Consumer<OrderPaySchedule> updater)
+	{
+		if (orderIds.isEmpty()) {return;}
+
+		trxManager.runInThreadInheritedTrx(() -> {
+			warmUpByOrderIds(orderIds);
+			orderIds.forEach(orderId -> updateById0(orderId, updater));
+		});
+	}
+
+	public void updateById(@NonNull final OrderId orderId, @NonNull final Consumer<OrderPaySchedule> updater)
+	{
+		trxManager.runInThreadInheritedTrx(() -> updateById0(orderId, updater));
+	}
+
+	private void updateById0(@NonNull final OrderId orderId, @NonNull final Consumer<OrderPaySchedule> updater)
+	{
+		final OrderPaySchedule paySchedules = loadByOrderId(orderId).orElse(null);
+		if (paySchedules == null)
+		{
+			return;
+		}
+
+		updater.accept(paySchedules);
+		save(paySchedules);
+	}
+}

backend/de.metas.business/src/main/java/de/metas/order/paymentschedule/repository/OrderPayScheduleLoaderAndSaver.java
@@ -1,238 +1,0 @@
-/*
- * #%L
- * de.metas.business
- * %%
- * Copyright (C) 2025 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
-
-package de.metas.order.paymentschedule.repository;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import de.metas.invoice.InvoiceId;
-import de.metas.money.CurrencyId;
-import de.metas.money.Money;
-import de.metas.order.OrderId;
-import de.metas.order.paymentschedule.OrderPaySchedule;
-import de.metas.order.paymentschedule.OrderPayScheduleId;
-import de.metas.order.paymentschedule.OrderPayScheduleLine;
-import de.metas.order.paymentschedule.OrderPayScheduleStatus;
-import de.metas.payment.paymentterm.PaymentTermBreakId;
-import de.metas.payment.paymentterm.ReferenceDateType;
-import de.metas.util.GuavaCollectors;
-import de.metas.util.collections.CollectionUtils;
-import de.metas.util.lang.Percent;
-import de.metas.util.lang.SeqNo;
-import lombok.Builder;
-import lombok.NonNull;
-import org.adempiere.ad.dao.IQueryBL;
-import org.adempiere.ad.trx.api.ITrxManager;
-import org.adempiere.exceptions.AdempiereException;
-import org.adempiere.model.InterfaceWrapperHelper;
-import org.compiere.model.I_C_OrderPaySchedule;
-import org.compiere.util.TimeUtil;
-import org.jetbrains.annotations.NotNull;
-
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-
-@Builder
-public class OrderPayScheduleLoaderAndSaver
-{
-	@NonNull private final ITrxManager trxManager;
-	@NonNull private final IQueryBL queryBL;
-
-	private final HashMap<OrderId, ImmutableList<I_C_OrderPaySchedule>> recordsByOrderId = new HashMap<>();
-
-	public Optional<OrderPaySchedule> loadByOrderId(@NonNull final OrderId orderId)
-	{
-		final List<I_C_OrderPaySchedule> records = getRecordsByOrderId(orderId);
-		if (records.isEmpty())
-		{
-			return Optional.empty();
-		}
-
-		final ImmutableList<OrderPayScheduleLine> lines = records.stream()
-				.map(OrderPayScheduleLoaderAndSaver::fromRecord)
-				.collect(ImmutableList.toImmutableList());
-
-		return Optional.of(OrderPaySchedule.ofList(orderId, lines));
-	}
-
-	private void warmUpByOrderIds(final Set<OrderId> orderIds)
-	{
-		CollectionUtils.getAllOrLoad(recordsByOrderId, orderIds, this::retrieveRecordsByOrderId);
-	}
-
-	private List<I_C_OrderPaySchedule> getRecordsByOrderId(@NonNull final OrderId orderId)
-	{
-		return CollectionUtils.getOrLoad(recordsByOrderId, orderId, this::retrieveRecordsByOrderId);
-	}
-
-	private Map<OrderId, ImmutableList<I_C_OrderPaySchedule>> retrieveRecordsByOrderId(@NonNull final Set<OrderId> orderIds)
-	{
-		if (orderIds.isEmpty())
-		{
-			return ImmutableMap.of();
-		}
-
-		final Map<OrderId, ImmutableList<I_C_OrderPaySchedule>> recordsByOrderId = queryBL.createQueryBuilder(I_C_OrderPaySchedule.class)
-				.addInArrayFilter(I_C_OrderPaySchedule.COLUMNNAME_C_Order_ID, orderIds)
-				.orderBy(I_C_OrderPaySchedule.COLUMNNAME_SeqNo)
-				.create()
-				.stream()
-				.collect(Collectors.groupingBy(
-						OrderPayScheduleLoaderAndSaver::extractOrderId,
-						ImmutableList.toImmutableList()
-				));
-
-		final HashMap<OrderId, ImmutableList<I_C_OrderPaySchedule>> result = new HashMap<>();
-		for (final OrderId orderId : orderIds)
-		{
-			final ImmutableList<I_C_OrderPaySchedule> records = recordsByOrderId.get(orderId);
-			result.put(orderId, records != null ? records : ImmutableList.of());
-		}
-
-		return result;
-	}
-
-	private void invalidateCache(final OrderId orderId)
-	{
-		recordsByOrderId.remove(orderId);
-	}
-
-	private static OrderId extractOrderId(@NonNull final I_C_OrderPaySchedule record)
-	{
-		return OrderId.ofRepoId(record.getC_Order_ID());
-	}
-
-	@NonNull
-	private static OrderPayScheduleLine fromRecord(@NonNull final I_C_OrderPaySchedule record)
-	{
-		final CurrencyId currencyId = CurrencyId.ofRepoId(record.getC_Currency_ID());
-		final BigDecimal dueAmtActualBD = InterfaceWrapperHelper.getValueAsBigDecimalOrNull(record, I_C_OrderPaySchedule.COLUMNNAME_DueAmt_Actual);
-		return OrderPayScheduleLine.builder()
-				.id(OrderPayScheduleId.ofRepoId(record.getC_OrderPaySchedule_ID()))
-				.orderId(OrderId.ofRepoId(record.getC_Order_ID()))
-				.seqNo(SeqNo.ofInt(record.getSeqNo()))
-				//
-				.paymentTermBreakId(PaymentTermBreakId.ofRepoId(record.getC_PaymentTerm_ID(), record.getC_PaymentTerm_Break_ID()))
-				.referenceDateType(ReferenceDateType.ofCode(record.getReferenceDateType()))
-				.percent(Percent.of(record.getPercent()))
-				.offsetDays(record.getOffsetDays())
-				//
-				.status(OrderPayScheduleStatus.ofCode(record.getStatus()))
-				.isPaid(record.isPaid())
-				.referenceDate(TimeUtil.asLocalDate(record.getReferenceDate()))
-				.dueDate(TimeUtil.asLocalDate(record.getDueDate()))
-				.baseAmount(Money.of(record.getBaseAmt(), currencyId))
-				.dueAmount(Money.of(record.getDueAmt(), currencyId))
-				.dueAmtActual(dueAmtActualBD != null ? Money.of(dueAmtActualBD, currencyId) : null)
-				//
-				.invoiceId(InvoiceId.ofRepoIdOrNull(record.getC_Invoice_ID()))
-				.build();
-	}
-
-	//
-	//
-	//
-	// --- Saving and Updating --------------------------------------------------
-	//
-	//
-	//
-
-	public void save(@NonNull final OrderPaySchedule orderPaySchedule)
-	{
-		trxManager.runInThreadInheritedTrx(() -> save0(orderPaySchedule));
-	}
-
-	private void save0(@NonNull final OrderPaySchedule orderPaySchedule)
-	{
-		final OrderId orderId = orderPaySchedule.getOrderId();
-		final HashMap<OrderPayScheduleId, I_C_OrderPaySchedule> records = getRecordsByOrderId(orderId)
-				.stream()
-				.collect(GuavaCollectors.toHashMapByKey(record -> OrderPayScheduleId.ofRepoId(record.getC_OrderPaySchedule_ID())));
-
-		for (final OrderPayScheduleLine line : orderPaySchedule.getLines())
-		{
-			final I_C_OrderPaySchedule record = records.remove(line.getId());
-			if (record == null)
-			{
-				throw new AdempiereException("No record found by " + line.getId());
-			}
-
-			updateRecord(record, line);
-			InterfaceWrapperHelper.save(record);
-		}
-
-		InterfaceWrapperHelper.deleteAll(records.values());
-
-		invalidateCache(orderId);
-	}
-
-	private static void updateRecord(final I_C_OrderPaySchedule record, final @NotNull OrderPayScheduleLine from)
-	{
-		record.setC_Order_ID(from.getOrderId().getRepoId());
-		record.setC_PaymentTerm_ID(from.getPaymentTermBreakId().getPaymentTermId().getRepoId());
-		record.setC_PaymentTerm_Break_ID(from.getPaymentTermBreakId().getRepoId());
-		record.setDueAmt(from.getDueAmount().toBigDecimal());
-		record.setC_Currency_ID(from.getDueAmount().getCurrencyId().getRepoId());
-		record.setDueDate(TimeUtil.asTimestamp(from.getDueDate()));
-		record.setPercent(from.getPercent().toInt());
-		record.setOffsetDays(from.getOffsetDays());
-		record.setReferenceDateType(from.getReferenceDateType().getCode());
-		record.setSeqNo(from.getSeqNo().toInt());
-		record.setStatus(OrderPayScheduleStatus.toCodeOrNull(from.getStatus()));
-		record.setDueAmt_Actual(from.getDueAmtActual() != null ? from.getDueAmtActual().toBigDecimal() : null);
-		record.setReferenceDate(TimeUtil.asTimestamp(from.getReferenceDate()));
-		record.setIsPaid(from.isPaid());
-	}
-
-	public void updateByIds(@NonNull final Set<OrderId> orderIds, @NonNull final Consumer<OrderPaySchedule> updater)
-	{
-		if (orderIds.isEmpty()) {return;}
-
-		trxManager.runInThreadInheritedTrx(() -> {
-			warmUpByOrderIds(orderIds);
-			orderIds.forEach(orderId -> updateById0(orderId, updater));
-		});
-	}
-
-	public void updateById(@NonNull final OrderId orderId, @NonNull final Consumer<OrderPaySchedule> updater)
-	{
-		trxManager.runInThreadInheritedTrx(() -> updateById0(orderId, updater));
-	}
-
-	private void updateById0(@NonNull final OrderId orderId, @NonNull final Consumer<OrderPaySchedule> updater)
-	{
-		final OrderPaySchedule paySchedules = loadByOrderId(orderId).orElse(null);
-		if (paySchedules == null)
-		{
-			return;
-		}
-
-		updater.accept(paySchedules);
-		save(paySchedules);
-	}
-}

commit 0206e7898297e929a640a649609350094c996652

fix(29369): preserve iter-3 columns in OrderPayScheduleLoaderAndSaver.updateRecord
The loader (`fromRecord` lines 132-157) reads BaseAmt, M_InOut_ID, and
C_Invoice_ID, but `updateRecord` (lines 206-225) was not writing them.
This made the round-trip asymmetric and caused inserts of new
C_OrderPaySchedule rows from `recomputeDeliverySteps` to crash with
`null value in column "baseamt" violates not-null constraint`.

Fixes the regression introduced by `a9d3375b38f` ("major fix claude
slop"). New round-trip test in `OrderPayScheduleLoaderAndSaverTest`
proves the symmetry.

Refs: https://github.com/metasfresh/me03/issues/29369

backend/de.metas.business/src/main/java/de/metas/order/paymentschedule/core/repository/OrderPayScheduleLoaderAndSaver.java
@@ -211,12 +211,15 @@ public class OrderPayScheduleLoaderAndSaver
 		record.setC_Order_ID(from.getOrderId().getRepoId());
 		record.setC_PaymentTerm_ID(from.getPaymentTermBreakId().getPaymentTermId().getRepoId());
 		record.setC_PaymentTerm_Break_ID(from.getPaymentTermBreakId().getRepoId());
+		record.setBaseAmt(from.getBaseAmount() != null ? from.getBaseAmount().toBigDecimal() : null);
 		record.setDueAmt(from.getDueAmount().toBigDecimal());
 		record.setC_Currency_ID(from.getDueAmount().getCurrencyId().getRepoId());
 		record.setDueDate(TimeUtil.asTimestamp(from.getDueDate()));
 		record.setPercent(from.getPercent().toInt());
 		record.setOffsetDays(from.getOffsetDays());
 		record.setReferenceDateType(from.getReferenceDateType().getCode());
+		record.setM_InOut_ID(from.getInoutId() != null ? from.getInoutId().getRepoId() : 0);
+		record.setC_Invoice_ID(from.getInvoiceId() != null ? from.getInvoiceId().getRepoId() : 0);
 		record.setSeqNo(seqNoProvider.getAndIncrement().toInt());
 		record.setStatus(OrderPayScheduleStatus.toCodeOrNull(from.getStatus()));
 		record.setDueAmt_Actual(from.getDueAmountActual() != null ? from.getDueAmountActual().toBigDecimal() : null);

backend/de.metas.business/src/test/java/de/metas/order/paymentschedule/core/repository/OrderPayScheduleLoaderAndSaverTest.java
@@ -1,0 +1,135 @@
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2026 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package de.metas.order.paymentschedule.core.repository;
+
+import de.metas.inout.InOutId;
+import de.metas.invoice.InvoiceId;
+import de.metas.money.CurrencyId;
+import de.metas.money.Money;
+import de.metas.order.OrderId;
+import de.metas.order.paymentschedule.core.OrderPaySchedule;
+import de.metas.order.paymentschedule.core.OrderPayScheduleLine;
+import de.metas.order.paymentschedule.core.OrderPayScheduleStatus;
+import de.metas.payment.paymentterm.PaymentTermBreakId;
+import de.metas.payment.paymentterm.PaymentTermId;
+import de.metas.payment.paymentterm.ReferenceDateType;
+import de.metas.pricing.tax.ProductTaxCategoryRepository;
+import de.metas.pricing.tax.ProductTaxCategoryService;
+import de.metas.util.lang.Percent;
+import org.adempiere.test.AdempiereTestHelper;
+import org.compiere.SpringContextHolder;
+import org.compiere.model.I_C_Order;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.adempiere.model.InterfaceWrapperHelper.newInstance;
+import static org.adempiere.model.InterfaceWrapperHelper.saveRecord;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Round-trip test for {@link OrderPayScheduleLoaderAndSaver}: proves that
+ * {@code updateRecord} writes every field that {@code fromRecord} reads.
+ *
+ * @see <a href="https://github.com/metasfresh/me03/issues/29369">me03 #29369 Split-Payment Iter 3</a>
+ */
+class OrderPayScheduleLoaderAndSaverTest
+{
+	private static final CurrencyId EUR = CurrencyId.ofRepoId(318);
+
+	private OrderPayScheduleRepository repository;
+
+	@BeforeEach
+	void beforeEach()
+	{
+		AdempiereTestHelper.get().init();
+		SpringContextHolder.registerJUnitBean(new ProductTaxCategoryService(new ProductTaxCategoryRepository()));
+		repository = new OrderPayScheduleRepository();
+	}
+
+	/**
+	 * Saves an {@link OrderPayScheduleLine} with iter-3 columns set
+	 * ({@code baseAmount}, {@code inoutId}, {@code invoiceId}),
+	 * reloads it by order, and asserts all three values survived the round-trip.
+	 * <p>
+	 * Before the fix, {@code updateRecord} was not calling
+	 * {@code record.setBaseAmt}, {@code record.setM_InOut_ID}, or
+	 * {@code record.setC_Invoice_ID}, so the reloaded line had null/0 values.
+	 */
+	@Test
+	void testRoundTripPreservesIter3Columns()
+	{
+		// --- arrange ---
+		final I_C_Order order = newInstance(I_C_Order.class);
+		saveRecord(order);
+		final OrderId orderId = OrderId.ofRepoId(order.getC_Order_ID());
+
+		final PaymentTermId paymentTermId = PaymentTermId.ofRepoId(1001);
+		final PaymentTermBreakId breakId = PaymentTermBreakId.ofRepoId(paymentTermId, 1002);
+
+		final Money baseAmount = Money.of(BigDecimal.valueOf(68654.40), EUR);
+		final InOutId inoutId = InOutId.ofRepoId(1234);
+		final InvoiceId invoiceId = InvoiceId.ofRepoId(5678);
+
+		final OrderPayScheduleLine line = OrderPayScheduleLine.builder()
+				.orderId(orderId)
+				.paymentTermBreakId(breakId)
+				.referenceDateType(ReferenceDateType.OrderDate)
+				.percent(Percent.of(100))
+				.offsetDays(0)
+				.status(OrderPayScheduleStatus.Pending)
+				.dueDate(LocalDate.of(2026, 6, 1))
+				.baseAmount(baseAmount)
+				.dueAmount(Money.of(BigDecimal.valueOf(68654.40), EUR))
+				.inoutId(inoutId)
+				.invoiceId(invoiceId)
+				.build();
+
+		final OrderPaySchedule schedule = OrderPaySchedule.ofList(orderId, Collections.singletonList(line));
+
+		// --- act: save ---
+		repository.save(schedule);
+
+		// --- act: reload ---
+		final Optional<OrderPaySchedule> reloaded = repository.getByOrderId(orderId);
+		assertThat(reloaded).isPresent();
+		final OrderPayScheduleLine reloadedLine = reloaded.get().getLines().get(0);
+
+		// --- assert iter-3 columns round-tripped ---
+		assertThat(reloadedLine.getBaseAmount())
+				.as("baseAmount must survive the round-trip")
+				.isEqualTo(baseAmount);
+
+		assertThat(reloadedLine.getInoutId())
+				.as("inoutId must survive the round-trip")
+				.isEqualTo(inoutId);
+
+		assertThat(reloadedLine.getInvoiceId())
+				.as("invoiceId must survive the round-trip")
+				.isEqualTo(invoiceId);
+	}
+}