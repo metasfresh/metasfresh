317ddde559fee12c0b6e281c4387435b8da34487
Adrian <adrian.stefan@metasfresh.com>
2026-05-08 08:22:19 +0300

    Add translations to `GRAIRequired` AD_Column and update translation logic

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5795291_sys_fix_GRAI_GRAIRequired_AD_Column.sql
+++
@@ -1,8 +0,0 @@
---Add name to GRAIRequired ad_column
-
-UPDATE ad_column
-SET Name      = 'GRAIRequired',
-    updated   = TO_TIMESTAMP('2026-05-07 12:00', 'YYYY-MM-DD HH24:MI'),
-    updatedBy = 100
-WHERE ad_column_id = 592261
-;
