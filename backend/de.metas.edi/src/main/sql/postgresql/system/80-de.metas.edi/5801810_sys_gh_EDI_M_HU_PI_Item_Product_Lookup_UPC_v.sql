27d6d30dc821ea5bdfe11d04593c8c70b7a9add3
metas-ts <tobias.schoeneberg@metasfresh.com>
2026-05-12 11:54:26 +0200

    For a BPartner, prefer M_HU_PI_Item_Products with that partner over others with no C_BPartner_ID

--- backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5801810_sys_gh_EDI_M_HU_PI_Item_Product_Lookup_UPC_v.sql
+++ backend/de.metas.edi/src/main/sql/postgresql/system/80-de.metas.edi/5801810_sys_gh_EDI_M_HU_PI_Item_Product_Lookup_UPC_v.sql
@@ -1,3 +1,26 @@
+/*
+ * #%L
+ * de.metas.edi
+ * %%
+ * Copyright (C) 2026 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+
 CREATE OR REPLACE VIEW EDI_M_HU_PI_Item_Product_Lookup_UPC_v AS
 SELECT DISTINCT ON (lookup.UPC, bpLookup.GLN, bpLookup.storegln) lookup.M_HU_PI_Item_Product_ID,
                                                                  --    lookup.M_Product_ID, not outputting M_Product_ID because we didn't do it in the past; just added M_Product_ID brelow so we can exclude inactive and discontinued products
