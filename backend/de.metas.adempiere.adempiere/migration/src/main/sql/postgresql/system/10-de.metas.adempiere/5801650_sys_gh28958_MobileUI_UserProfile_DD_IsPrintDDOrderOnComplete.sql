89e5bc1eb6557d220dadf4665c04619736381563
Max Rieck <max.rieck@metasfresh.com>
2026-05-12 17:19:34 +0200

    fix migration

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5801650_sys_gh28958_MobileUI_UserProfile_DD_IsPrintDDOrderOnComplete.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5801650_sys_gh28958_MobileUI_UserProfile_DD_IsPrintDDOrderOnComplete.sql
@@ -4,7 +4,7 @@
 --
 -- AD_Element: IsPrintDDOrderOnComplete
 INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy)
-VALUES (0,584852 /*From ID Server*/,0,'IsPrintDDOrderOnComplete',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','Print DD_Order on Complete','Print DD_Order on Complete',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100)
+VALUES (0,584852 /*From ID Server*/,0,'IsPrintDDOrderOnComplete',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,'D','Y','Print Distribution Order on Complete','Print Distribution Order on Complete',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
 INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive)
@@ -17,20 +17,20 @@ WHERE l.IsActive='Y' AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y')
 
 UPDATE AD_Element_Trl SET Name='Distributionsauftrag bei Abschluss drucken', PrintName='Distributionsauftrag bei Abschluss drucken',
   Description='Wenn aktiviert, wird der Distributionsauftrag automatisch gedruckt, sobald ein Mobile-Distributionsauftrag abgeschlossen wird.',
-  IsTranslated='Y', Updated=TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC', UpdatedBy=100
+  IsTranslated='Y', Updated=TO_TIMESTAMP('2026-05-11 14:00:12','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC', UpdatedBy=100
 WHERE AD_Element_ID=584852 AND AD_Language IN ('de_DE','de_CH')
 ;
 
-UPDATE AD_Element_Trl SET Name='Print DD_Order on Complete', PrintName='Print DD_Order on Complete',
+UPDATE AD_Element_Trl SET Name='Print Distribution Order on Complete', PrintName='Print Distribution Order on Complete',
   Description='If enabled, the Distribution Order document is printed automatically when a mobile distribution job is completed.',
-  IsTranslated='Y', Updated=TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC', UpdatedBy=100
+  IsTranslated='Y', Updated=TO_TIMESTAMP('2026-05-11 14:00:18','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC', UpdatedBy=100
 WHERE AD_Element_ID=584852 AND AD_Language IN ('en_US','en_GB')
 ;
 
 --
 -- AD_Column: MobileUI_UserProfile_DD.IsPrintDDOrderOnComplete
 INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,CloningStrategy,ColumnName,Created,CreatedBy,DDL_NoForeignKey,DefaultValue,EntityType,FacetFilterSeqNo,FieldLength,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutoApplyValidationRule,IsAutocomplete,IsCalculated,IsDimension,IsDLMPartitionBoundary,IsEncrypted,IsExcludeFromZoomTargets,IsFacetFilter,IsForceIncludeInGeneratedModel,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsRestAPICustomColumn,IsSelectionColumn,IsShowFilterIncrementButtons,IsShowFilterInline,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,MaxFacetsToFetch,Name,PersonalDataCategory,SelectionColumnSeqNo,SeqNo,Updated,UpdatedBy,Version)
-VALUES (0,592506 /*From ID Server*/,584852,0,20,542462,'XX','IsPrintDDOrderOnComplete',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,'N','Y','D',0,1,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','Y','N',0,'Print DD_Order on Complete','NP',0,0,TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,0)
+VALUES (0,592506 /*From ID Server*/,584852,0,20,542462,'XX','IsPrintDDOrderOnComplete',TO_TIMESTAMP('2026-05-11 14:01:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,'N','Y','D',0,1,'Y','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','Y','N','N','N','N','N','N','N','N','Y','N',0,'Print Distribution Order on Complete','NP',0,0,TO_TIMESTAMP('2026-05-11 14:01:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,0)
 ;
 
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive)
@@ -52,7 +52,7 @@ WHERE l.IsActive='Y' AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y')
 --
 -- AD_Field on the Mobile Distribution Profile tab (547735)
 INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy)
-VALUES (0,592506,779178 /*From ID Server*/,0,547735,TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,1,'D','Y','Y','N','N','N','N','N','N','Print DD_Order on Complete',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100)
+VALUES (0,592506,779178 /*From ID Server*/,0,547735,TO_TIMESTAMP('2026-05-11 14:02:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,1,'D','Y','Y','N','N','N','N','N','N','Print Distribution Order on Complete',TO_TIMESTAMP('2026-05-11 14:02:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
 INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive)
@@ -75,7 +75,7 @@ DELETE FROM AD_Element_Link WHERE AD_Field_ID=779178
 --
 -- UI placement: section 546319 / column 547725 (second column) / group 553846 (job) / SeqNo 30 (after IsRequireScanningProductCode=10, IsCompleteJobAutomatically=20)
 INSERT INTO AD_UI_Element (AD_Client_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,AD_UI_ElementGroup_ID,AD_UI_Element_ID,AD_UI_ElementType,Created,CreatedBy,IsActive,IsAdvancedField,IsDisplayed,IsDisplayedGrid,IsDisplayed_SideList,Name,SeqNo,SeqNoGrid,SeqNo_SideList,Updated,UpdatedBy)
-VALUES (0,779178,0,547735,553846,651162 /*From ID Server*/,'F',TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','Y','N','N','Print DD_Order on Complete',30,0,0,TO_TIMESTAMP('2026-05-11 14:00:00','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100)
+VALUES (0,779178,0,547735,553846,651162 /*From ID Server*/,'F',TO_TIMESTAMP('2026-05-11 14:02:30','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100,'Y','N','Y','N','N','Print Distribution Order on Complete',30,0,0,TO_TIMESTAMP('2026-05-11 14:02:30','YYYY-MM-DD HH24:MI:SS')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
 --
