0775b355e698f0e79a9a7d561bd0c82644a03761
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-05-12 09:27:46 +0300

    Separate scripts

--- backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5801620_MigrateC_PeriodForDatevExport.sql
+++ backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5801620_MigrateC_PeriodForDatevExport.sql
@@ -6,12 +6,3 @@ SET C_Period_ID = report.Get_Period((SELECT C_Calendar_ID FROM AD_ClientInfo ci 
 WHERE C_Period_ID IS NULL
 ;
 
--- 2026-05-11T07:57:50.473Z
-INSERT INTO t_alter_column values('datev_export','C_Period_ID','NUMERIC(10)',null,null)
-;
-
--- 2026-05-11T07:57:50.477Z
-INSERT INTO t_alter_column values('datev_export','C_Period_ID',null,'NOT NULL',null)
-;
-
-
---
+++ backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5801621_MigrateC_PeriodForDatevExport.sql
@@ -0,0 +1,9 @@
+-- 2026-05-11T07:57:50.473Z
+INSERT INTO t_alter_column values('datev_export','C_Period_ID','NUMERIC(10)',null,null)
+;
+
+-- 2026-05-11T07:57:50.477Z
+INSERT INTO t_alter_column values('datev_export','C_Period_ID',null,'NOT NULL',null)
+;
+
+
