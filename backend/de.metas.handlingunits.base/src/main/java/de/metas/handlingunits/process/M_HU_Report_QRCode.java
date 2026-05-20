75a2afdbcff34bb1c188599a986da12d5dab42e5
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-05-20 22:24:29 +0300

    Refactor HU QRCode process: lazy init & params
    Defer HUQRCodesService initialization and simplify parameter handling in M_HU_Report_QRCode.
    
    Changes:
    - Remove IProcessPrecondition implementation and its precondition check; use prepare() to lazily obtain HUQRCodesService via SpringContextHolder to avoid reflection/instantiation issues.
    - Rename parameters: p_ProcessId -> processId, p_isPrintPreview -> isPrintPreview.
    - Use getProcessInfo().isPrintPreview() and derive PrintCopies from ProcessInfoParameter stream instead of the old p_PrintCopies field usage.
    - Drop unused DAOs, selection-building and DB calls; operate on existing PInstanceId selection.
    - Clean up imports and field initialization.
    
    These changes make service lookup deferred, simplify parameter access, and remove redundant selection/DB manipulation from the process.

--- backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/process/M_HU_Report_QRCode.java
+++ backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/process/M_HU_Report_QRCode.java
@@ -1,29 +1,16 @@
 package de.metas.handlingunits.process;
 
-import com.google.common.collect.ImmutableSet;
 import de.metas.global_qrcodes.service.QRCodePDFResource;
-import de.metas.handlingunits.HuId;
-import de.metas.handlingunits.IHandlingUnitsDAO;
-import de.metas.handlingunits.model.I_M_HU;
 import de.metas.handlingunits.qrcodes.service.HUQRCodesService;
-import de.metas.handlingunits.report.HUToReport;
-import de.metas.handlingunits.report.HUToReportWrapper;
 import de.metas.printing.IMassPrintingService;
 import de.metas.process.AdProcessId;
-import de.metas.process.IProcessPrecondition;
-import de.metas.process.IProcessPreconditionsContext;
 import de.metas.process.JavaProcess;
 import de.metas.process.PInstanceId;
 import de.metas.process.Param;
-import de.metas.process.ProcessPreconditionsResolution;
+import de.metas.process.ProcessInfoParameter;
 import de.metas.process.RunOutOfTrx;
 import de.metas.report.PrintCopies;
-import de.metas.util.Services;
-import lombok.NonNull;
-import org.adempiere.ad.trx.api.ITrx;
-import org.adempiere.exceptions.AdempiereException;
 import org.compiere.SpringContextHolder;
-import org.compiere.util.DB;
 
 /*
  * #%L
@@ -52,33 +39,26 @@ import org.compiere.util.DB;
  * It takes M_HU_IDs from T_Selection, gets/generates QR-Codes for them
  * and then generate the PDF.
  */
-public class M_HU_Report_QRCode extends JavaProcess implements IProcessPrecondition
+public class M_HU_Report_QRCode extends JavaProcess
 {
-	@NonNull
-	private final HUQRCodesService huQRCodesService = SpringContextHolder.instance.getBean(HUQRCodesService.class);
-	@NonNull
-	private final IHandlingUnitsDAO handlingUnitsDAO = Services.get(IHandlingUnitsDAO.class);
+	private HUQRCodesService huQRCodesService;
 
 	private static final String PARAM_AD_Process_ID = "AD_Process_ID";
 
 	@Param(parameterName = PARAM_AD_Process_ID)
-	private int p_ProcessId;
+	private int processId;
 
 	@Param(parameterName = "IsPrintPreview")
-	private boolean p_isPrintPreview;
+	private boolean isPrintPreview;
 
 	@Param(parameterName = IMassPrintingService.PARAM_PrintCopies)
 	private int p_PrintCopies;
 
 	@Override
-	public ProcessPreconditionsResolution checkPreconditionsApplicable(final @NonNull IProcessPreconditionsContext context)
+	protected void prepare()
 	{
-		if (context.isNoSelection())
-		{
-			return ProcessPreconditionsResolution.rejectBecauseNoSelection();
-		}
-
-		return ProcessPreconditionsResolution.accept();
+		// Deferred so plain-reflection instantiation during precondition checking does not throw
+		huQRCodesService = SpringContextHolder.instance.getBean(HUQRCodesService.class);
 	}
 
 	@Override
@@ -86,26 +66,24 @@ public class M_HU_Report_QRCode extends JavaProcess implements IProcessPrecondit
 	protected String doIt()
 	{
 		final PInstanceId selectionId = getPinstanceId();
-		final AdProcessId qrCodeProcessId = AdProcessId.ofRepoIdOrNull(p_ProcessId);
+		final AdProcessId qrCodeProcessId = AdProcessId.ofRepoIdOrNull(processId);
 
-		final ImmutableSet<HuId> huIdSet = handlingUnitsDAO.streamByQuery(
-						retrieveSelectedRecordsQueryBuilder(I_M_HU.class, false), HUToReportWrapper::of)
-				.map(HUToReport::getHUId)
-				.collect(ImmutableSet.toImmutableSet());
-
-		if (huIdSet.isEmpty())
-			throw new AdempiereException("No HUs");
-
-		DB.createT_Selection(selectionId, HuId.toRepoIds(huIdSet), ITrx.TRXNAME_None);
-
-		if (p_isPrintPreview)
+		if (getProcessInfo().isPrintPreview())
 		{
 			final QRCodePDFResource pdf = huQRCodesService.createPdfForSelectionOfHUIds(selectionId, qrCodeProcessId);
 			getResult().setReportData(pdf, pdf.getFilename(), pdf.getContentType());
 		}
 		else
 		{
-			huQRCodesService.printForSelectionOfHUIds(selectionId, qrCodeProcessId, PrintCopies.ofIntOrOne(p_PrintCopies));
+			final PrintCopies printCopies = getParameters().stream()
+					.filter(p -> IMassPrintingService.PARAM_PrintCopies.equals(p.getParameterName()))
+					.findFirst()
+					.map(ProcessInfoParameter::getParameterAsInt)
+					.filter(n -> n > 0)
+					.map(PrintCopies::ofInt)
+					.orElse(PrintCopies.ONE);
+
+			huQRCodesService.printForSelectionOfHUIds(selectionId, qrCodeProcessId, printCopies);
 		}
 
 		return MSG_OK;
