25c36906c91fc9648f1ecdaaa7880df319cd6275
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-05-27 14:26:56 +0300

    Add comments

--- backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/70-de.metas.handlingunits/5804760_sys_M_HU_Report_QRCode_register_in_M_HU_Process.sql
+++ backend/de.metas.handlingunits.base/src/main/sql/postgresql/system/70-de.metas.handlingunits/5804760_sys_M_HU_Report_QRCode_register_in_M_HU_Process.sql
@@ -3,6 +3,8 @@
 
 INSERT INTO m_hu_process (ad_client_id, ad_org_id, ad_process_id, created, createdby, isactive, isapplytolus, isapplytotus, isapplytocus, m_hu_pi_id, m_hu_process_id, updated, updatedby, isprovideasuseraction, isapplytotoplevelhusonly) VALUES (0, 0, 584980, '2022-02-10 17:33:55.000000 +01:00', 100, 'Y', 'Y', 'Y', 'Y', null, 540019, '2022-02-10 18:53:24.000000 +01:00', 100, 'Y', 'N');
 
+-- delete the process from AD_Table_Process because is registered in M_HU_Process
 delete from AD_Table_Process  where ad_process_id = 584980;
 
+-- delete copy parameter from AD_Process_Para because is registered in M_HU_Process
 delete from ad_process_para where ad_process_para_id = 543183;
