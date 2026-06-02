7074035fb70e4227b5df2a6b512b2d25bcfb5d9e
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-06-02 23:51:08 +0300

    Fix parameter

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5805790_sys_goods_purchase_report_add_bp_group_param.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/75-de.metas.fresh.reports/5805790_sys_goods_purchase_report_add_bp_group_param.sql
@@ -2,7 +2,7 @@
 -- ParameterName: C_BP_Group_ID
 -- 2026-06-02T00:00:00.000Z
 INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Process_Para_ID,AD_Reference_ID,AD_Reference_Value_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAutocomplete,IsCentrallyMaintained,IsEncrypted,IsMandatory,IsRange,Name,SeqNo,ShowInactiveValues,Updated,UpdatedBy)
-VALUES (0,908,0,585628,543237 /*From ID Server*/,19,NULL,'C_BP_Group_ID',TO_TIMESTAMP('2026-06-02 00:00:00.000000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Geschäftspartnergruppe','D',0,NULL,'Y','N','Y','N','N','N','Geschäftspartnergruppe',25,'N',TO_TIMESTAMP('2026-06-02 00:00:00.000000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
+VALUES (0,1383,0,585628,543237 /*From ID Server*/,19,NULL,'C_BP_Group_ID',TO_TIMESTAMP('2026-06-02 00:00:00.000000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100,'Geschäftspartnergruppe','D',0,NULL,'Y','N','Y','N','N','N','Geschäftspartnergruppe',25,'N',TO_TIMESTAMP('2026-06-02 00:00:00.000000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',100)
 ;
 
 -- 2026-06-02T00:00:00.100Z
