31fd58e058f0cbd937a0fd244f1295e5da3f339b
Adrian <adrian.stefan@metasfresh.com>
2026-06-05 20:39:48 +0300

    Limit `AD_Column` virtual column logic to a single `TrackingURL` record for `M_InOut`.

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inout/5805580_sys_fix_virtualCol_logic.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inout/5805580_sys_fix_virtualCol_logic.sql
@@ -1,5 +1,5 @@
 UPDATE AD_Column
-SET columnsql='( SELECT sop.TrackingURL FROM M_ShippingPackage p INNER JOIN Carrier_ShipmentOrder_Parcel sop ON sop.M_Package_ID = p.M_Package_ID WHERE p.M_InOut_ID = M_InOut.M_InOut_ID )',
+SET columnsql='( SELECT sop.TrackingURL FROM M_ShippingPackage p INNER JOIN Carrier_ShipmentOrder_Parcel sop ON sop.M_Package_ID = p.M_Package_ID WHERE p.M_InOut_ID = M_InOut.M_InOut_ID LIMIT 1 )',
     updatedby=99,
     updated=TO_TIMESTAMP('2026-06-05 10:20:30','YYYY-MM-DD HH24:MI:SS')
 WHERE AD_Column_ID = 592668
