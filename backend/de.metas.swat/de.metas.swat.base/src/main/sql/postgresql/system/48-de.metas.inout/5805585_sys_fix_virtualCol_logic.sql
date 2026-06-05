6a13ef44c271d0199d663727a117f41affa47855
Adrian <adrian.stefan@metasfresh.com>
2026-06-05 22:57:34 +0300

    Fix casing in `columnsql` logic for `TrackingURL` in `M_InOut` virtual column

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inout/5805585_sys_fix_virtualCol_logic.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/48-de.metas.inout/5805585_sys_fix_virtualCol_logic.sql
@@ -1,5 +1,5 @@
 UPDATE AD_Column
-SET columnsql='( SELECT sop.TrackingURL FROM M_ShippingPackage p INNER JOIN Carrier_ShipmentOrder_Parcel sop ON sop.M_Package_ID = p.M_Package_ID WHERE p.M_InOut_ID = M_InOut.M_InOut_ID LIMIT 1 )',
+SET columnsql='( select sop.TrackingURL from M_ShippingPackage p inner join Carrier_ShipmentOrder_Parcel sop on sop.M_Package_ID = p.M_Package_ID where p.M_InOut_ID = M_InOut.M_InOut_ID limit 1 )',
     updatedby=100,
     updated=TO_TIMESTAMP('2026-06-05 10:20:30','YYYY-MM-DD HH24:MI:SS')
 WHERE AD_Column_ID = 592668
