metasfresh/metasfresh

commit 7a10029b652b2a3cfe185abdfdc9170eac169a1e

Make sure that shipment and invoice-creation uses dedicated asyncBatchIds (#21683)
...and not existing ones that might be already processed
Also change from C_Async_Batch to AsyncBatchId where actually just this ID is needed in the end

* also, minor improvements

backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/shipmentschedule/api/ShipmentService.java
@@ -69,6 +69,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Nullable;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -353,6 +354,10 @@ public class ShipmentService implements IShipmentService
 		return trxManager.callInNewTrx(() -> groupSchedulesByAsyncBatch0(scheduleIds));
 	}
 
+	/**
+	 * Group the given shipmentschedules according to their async-batch-id, <b>but</b> create a new async-batch-id for each old one.
+	 * That's because we will want to register an async-batch-observer for each group, and therefore we need new "unspend" async batches.
+	 */
 	@NonNull
 	private ImmutableMap<AsyncBatchId, ShipmentScheduleAndJobScheduleIdSet> groupSchedulesByAsyncBatch0(@NonNull final ShipmentScheduleAndJobScheduleIdSet scheduleIds2)
 	{
@@ -362,6 +367,8 @@ public class ShipmentService implements IShipmentService
 		final HashSet<ShipmentScheduleAndJobScheduleId> scheduleIdsWithoutAsyncBatchId = new HashSet<>();
 		final ArrayList<de.metas.inoutcandidate.model.I_M_ShipmentSchedule> shipmentSchedulesWithoutAsyncBatchId = new ArrayList<>();
 
+		final HashMap<AsyncBatchId,AsyncBatchId> oldToNewAsyncBatchId = new HashMap<>();
+		
 		for (final ShipmentScheduleAndJobScheduleId shipmentScheduleAndJobScheduleId : scheduleIds2)
 		{
 			final ShipmentScheduleId shipmentScheduleId = shipmentScheduleAndJobScheduleId.getShipmentScheduleId();
@@ -371,15 +378,16 @@ public class ShipmentService implements IShipmentService
 				continue;
 			}
 
-			final AsyncBatchId asyncBatchId = AsyncBatchId.ofRepoIdOrNull(shipmentSchedule.getC_Async_Batch_ID());
-			if (asyncBatchId == null)
+			final AsyncBatchId oldAsyncBatchId = AsyncBatchId.ofRepoIdOrNull(shipmentSchedule.getC_Async_Batch_ID());
+			if (oldAsyncBatchId == null)
 			{
 				scheduleIdsWithoutAsyncBatchId.add(shipmentScheduleAndJobScheduleId);
 				shipmentSchedulesWithoutAsyncBatchId.add(shipmentSchedule);
 			}
 			else
 			{
-				scheduleIdsByAsyncBatchId.put(asyncBatchId, shipmentScheduleAndJobScheduleId);
+				final AsyncBatchId newAsyncBatchId = oldToNewAsyncBatchId.computeIfAbsent(oldAsyncBatchId, k -> asyncBatchBL.newAsyncBatch(C_Async_Batch_InternalName_ShipmentSchedule));
+				scheduleIdsByAsyncBatchId.put(newAsyncBatchId, shipmentScheduleAndJobScheduleId);
 			}
 		}
 

backend/de-metas-salesorder/src/main/java/de/metas/salesorder/candidate/AutoProcessingOLCandService.java
@@ -50,8 +50,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static de.metas.async.Async_Constants.C_Async_Batch_InternalName_InvoiceCandidate_Processing;
-
 @Service
 public class AutoProcessingOLCandService
 {
@@ -119,9 +117,7 @@ public class AutoProcessingOLCandService
 			final List<I_M_InOutLine> shipmentLines = inOutDAO.retrieveShipmentLinesForOrderId(orderIds);
 			Loggables.withLogger(logger, Level.INFO).addLog("Retrieved {} M_InOutLines for {} C_Order_IDs", shipmentLines.size(), orderIds.size());
 
-			// again, now we need a new async-batch
-			final AsyncBatchId asyncBatchId = asyncBatchBL.newAsyncBatch(C_Async_Batch_InternalName_InvoiceCandidate_Processing);
-			invoiceService.generateInvoicesFromShipmentLines(shipmentLines, asyncBatchId);
+			invoiceService.generateInvoicesFromShipmentLines(shipmentLines);
 		}
 
 		if (request.isCloseOrder())

backend/de-metas-salesorder/src/main/java/de/metas/salesorder/candidate/ProcessOLCandsWorkpackageEnqueuer.java
@@ -2,7 +2,7 @@
  * #%L
  * de-metas-salesorder
  * %%
- * Copyright (C) 2021 metas GmbH
+ * Copyright (C) 2025 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -46,7 +46,7 @@ public class ProcessOLCandsWorkpackageEnqueuer
 		workPackageQueueFactory.getQueueForEnqueuing(getCtx(), ProcessOLCandsWorkpackageProcessor.class)
 				.newWorkPackage()
 				.bindToThreadInheritedTrx()
-				.setC_Async_Batch_ID(processOLCandsBatchId)
+				.setAsyncBatchId(processOLCandsBatchId)
 				.parameter(WP_PARAM_VALID_OLCANDIDS_SELECTIONID, request.getPInstanceId())
 				.parameter(WP_PARAM_SHIP, request.isShip())
 				.parameter(WP_PARAM_INVOICE, request.isInvoice())

backend/de-metas-salesorder/src/main/java/de/metas/salesorder/service/AutoProcessingOrderService.java
@@ -24,8 +24,6 @@ package de.metas.salesorder.service;
 
 import ch.qos.logback.classic.Level;
 import com.google.common.collect.ImmutableSet;
-import de.metas.async.AsyncBatchId;
-import de.metas.async.api.IAsyncBatchBL;
 import de.metas.bpartner.BPartnerLocationAndCaptureId;
 import de.metas.handlingunits.shipmentschedule.api.GenerateShipmentsForSchedulesRequest;
 import de.metas.handlingunits.shipmentschedule.api.M_ShipmentSchedule_QuantityTypeToUse;
@@ -40,25 +38,19 @@ import de.metas.invoicecandidate.InvoiceCandidateId;
 import de.metas.invoicecandidate.api.IInvoiceCandBL;
 import de.metas.invoicecandidate.model.I_C_Invoice_Candidate;
 import de.metas.logging.LogManager;
-import de.metas.order.IOrderBL;
 import de.metas.order.OrderId;
-import de.metas.organization.OrgId;
 import de.metas.util.Loggables;
 import de.metas.util.Services;
 import lombok.NonNull;
 import org.adempiere.exceptions.AdempiereException;
-import org.compiere.model.I_C_Order;
 import org.compiere.model.I_M_InOut;
 import org.compiere.model.I_M_InOutLine;
-import org.compiere.util.Env;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import static de.metas.async.Async_Constants.C_Async_Batch_InternalName_InvoiceCandidate_Processing;
 
 @Service
 public class AutoProcessingOrderService
@@ -67,8 +59,6 @@ public class AutoProcessingOrderService
 
 	private final IInOutDAO inOutDAO = Services.get(IInOutDAO.class);
 	private final IInvoiceCandBL invoiceCandBL = Services.get(IInvoiceCandBL.class);
-	private final IOrderBL orderBL = Services.get(IOrderBL.class);
-	private final IAsyncBatchBL asyncBatchBL = Services.get(IAsyncBatchBL.class);
 
 	private final OrderService orderService;
 	private final ShipmentService shipmentService;
@@ -144,27 +134,7 @@ public class AutoProcessingOrderService
 				.map(InvoiceCandidateId::ofRepoId)
 				.collect(ImmutableSet.toImmutableSet());
 
-		final AsyncBatchId asyncBatchId = extractOrCreateAsyncBatchId(orderId);
-		invoiceService.generateInvoicesFromInvoiceCandidateIds(invoiceCandidateIds, asyncBatchId);
-	}
-
-	@NonNull
-	private AsyncBatchId extractOrCreateAsyncBatchId(final @NonNull OrderId orderId)
-	{
-		final I_C_Order orderRecord = orderBL.getById(orderId);
-		final int asyncBatchIdInt = orderRecord.getC_Async_Batch_ID();
-
-		if (orderRecord.getC_Async_Batch_ID() > 0)
-		{
-			return AsyncBatchId.ofRepoId(asyncBatchIdInt);
-		}
-
-		return asyncBatchBL.newAsyncBatch()
-				.setContext(Env.getCtx())
-				.setC_Async_Batch_Type(C_Async_Batch_InternalName_InvoiceCandidate_Processing)
-				.setName("Process ICs for C_Order_ID=" + OrderId.toRepoId(orderId))
-				.setOrgId(OrgId.ofRepoId(orderRecord.getAD_Org_ID()))
-				.buildAndEnqueue();
+		invoiceService.generateInvoicesFromInvoiceCandidateIds(invoiceCandidateIds);
 	}
 
 	private boolean sameShippingAndBillingAddress(

backend/de.metas.async/src/main/java/de/metas/async/Async_Constants.java
@@ -1,10 +1,8 @@
-package de.metas.async;
-
 /*
  * #%L
  * de.metas.async
  * %%
- * Copyright (C) 2021 metas GmbH
+ * Copyright (C) 2025 metas GmbH
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -13,14 +11,16 @@ package de.metas.async;
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
+ * License along with this program. If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
  */
+
+package de.metas.async;
 
 import de.metas.async.event.WorkpackageProcessedEvent;
 import de.metas.async.event.WorkpackagesProcessedWaiter;
@@ -53,7 +53,7 @@ public final class Async_Constants
 			.build();
 
 	/**
-	 * metasfresh posts {@link WorkpackageProcessedEvent}s to this topic when a workpackage that has a corelation-id set was processed. Also see {@link WorkpackagesProcessedWaiter}.
+	 * metasfresh posts {@link WorkpackageProcessedEvent}s to this topic when a workpackage that has a correlation-id set was processed. Also see {@link WorkpackagesProcessedWaiter}.
 	 */
 	public static final Topic WORKPACKAGE_LIFECYCLE_TOPIC = Topic.builder()
 			.name("de.metas.async.WorkpackageLifeCycle")

backend/de.metas.async/src/main/java/de/metas/async/api/IWorkPackageBuilder.java
@@ -1,3 +1,25 @@
+/*
+ * #%L
+ * de.metas.async
+ * %%
+ * Copyright (C) 2025 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
 package de.metas.async.api;
 
 import de.metas.async.AsyncBatchId;
@@ -18,28 +40,6 @@ import org.adempiere.util.lang.impl.TableRecordReferenceSet;
 import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.UUID;
-
-/*
- * #%L
- * de.metas.async
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 public interface IWorkPackageBuilder
 {
@@ -168,7 +168,7 @@ public interface IWorkPackageBuilder
 	 * Overloading set async batch, to enable setting async batch also by id (optional).
 	 * If the asyncBatchId is not set, it will be inherited.
 	 */
-	IWorkPackageBuilder setC_Async_Batch_ID(@Nullable AsyncBatchId asyncBatchId);
+	IWorkPackageBuilder setAsyncBatchId(@Nullable AsyncBatchId asyncBatchId);
 
 	I_C_Queue_WorkPackage buildWithPackageProcessor();
 

backend/de.metas.async/src/main/java/de/metas/async/api/impl/WorkPackageBuilder.java
@@ -264,7 +264,7 @@ import static org.adempiere.model.InterfaceWrapperHelper.setTrxName;
 
 		final AsyncBatchId asyncBatchId = AsyncBatchId.ofRepoId(asyncBatch.getC_Async_Batch_ID());
 
-		return setC_Async_Batch_ID(asyncBatchId);
+		return setAsyncBatchId(asyncBatchId);
 	}
 
 	@Override
@@ -276,7 +276,7 @@ import static org.adempiere.model.InterfaceWrapperHelper.setTrxName;
 	}
 
 	@Override
-	public WorkPackageBuilder setC_Async_Batch_ID(@Nullable final AsyncBatchId asyncBatchId)
+	public WorkPackageBuilder setAsyncBatchId(@Nullable final AsyncBatchId asyncBatchId)
 	{
 		if (asyncBatchId == null)
 		{

commit a1eeecfb1bc2a340212a81197cde0de7d5ec6b9d

Mass Printing — shipments per customer CreateShipmentPolicy
- Fix self-deadlock in ShipmentService.groupSchedulesByAsyncBatch: changed
  callInNewTrx → callInThreadInheritedTrx to avoid deadlock when called from
  PickingJobCompleteCommand which already holds RowExclusiveLocks on
  M_ShipmentSchedule (regressed by commit 7a10029b652)
- Add per-customer shipment-policy cucumber step in MobileUIPickingUserProfile_StepDef
- Add 4 new MassPrintingLabels scenarios covering CREATE_DRAFT (DR), CREATE_AND_COMPLETE (CO),
  CREATE_COMPLETE_CLOSE (CO + closed schedule), and mixed-customer scan
  (one customer DO_NOT_CREATE, another CREATE_DRAFT)

backend/de.metas.handlingunits.base/src/main/java/de/metas/handlingunits/shipmentschedule/api/ShipmentService.java
@@ -351,7 +351,11 @@ public class ShipmentService implements IShipmentService
 	@NonNull
 	public ImmutableMap<AsyncBatchId, ShipmentScheduleAndJobScheduleIdSet> groupSchedulesByAsyncBatch(@NonNull final ShipmentScheduleAndJobScheduleIdSet scheduleIds)
 	{
-		return trxManager.callInNewTrx(() -> groupSchedulesByAsyncBatch0(scheduleIds));
+		// NOTE: Use callInThreadInheritedTrx (not callInNewTrx) to avoid a self-deadlock:
+		// When called from PickingJobCompleteCommand.executeInTrx (which already holds RowExclusiveLocks
+		// on M_ShipmentSchedule rows), a callInNewTrx would open a second DB connection and attempt to
+		// UPDATE M_ShipmentSchedule — which blocks on the outer transaction's lock, causing a deadlock.
+		return trxManager.callInThreadInheritedTrx(() -> groupSchedulesByAsyncBatch0(scheduleIds));
 	}
 
 	/**

backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/picking/MobileUIPickingUserProfile_StepDef.java
@@ -22,22 +22,37 @@
 
 package de.metas.cucumber.stepdefs.picking;
 
+import com.google.common.collect.ImmutableList;
+import de.metas.bpartner.BPartnerId;
+import de.metas.cucumber.stepdefs.C_BPartner_StepDefData;
 import de.metas.cucumber.stepdefs.DataTableRow;
+import de.metas.cucumber.stepdefs.DataTableRows;
 import de.metas.handlingunits.picking.config.mobileui.MobileUIPickingUserProfile;
 import de.metas.handlingunits.picking.config.mobileui.MobileUIPickingUserProfileService;
+import de.metas.handlingunits.picking.config.mobileui.PickingCustomerConfig;
+import de.metas.handlingunits.picking.config.mobileui.PickingCustomerConfigsCollection;
 import de.metas.handlingunits.picking.config.mobileui.PickingJobOptions.PickingJobOptionsBuilder;
+import de.metas.handlingunits.picking.config.mobileui.PickingJobOptionsId;
 import de.metas.handlingunits.picking.job.service.CreateShipmentPolicy;
 import de.metas.logging.LogManager;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.adempiere.model.InterfaceWrapperHelper;
 import org.compiere.SpringContextHolder;
 import org.compiere.model.I_MobileUI_UserProfile_Picking;
+import org.compiere.model.I_MobileUI_UserProfile_Picking_Job;
 import org.slf4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 public class MobileUIPickingUserProfile_StepDef
 {
 	private static final Logger logger = LogManager.getLogger(MobileUIPickingUserProfile_StepDef.class);
+	@NonNull private final C_BPartner_StepDefData bPartnerTable;
 	private final MobileUIPickingUserProfileService profileService = SpringContextHolder.instance.getBean(MobileUIPickingUserProfileService.class);
 
 	@And("set mobile UI picking profile")
@@ -62,5 +77,69 @@ public class MobileUIPickingUserProfile_StepDef
 		});
 
 		logger.info("Profile updated: {}", profileService.getProfile());
+	}
+
+	/**
+	 * Sets a per-customer {@code CreateShipmentPolicy} override on the picking profile.
+	 * Creates a {@code MobileUI_UserProfile_Picking_Job} record for each row and links it to the
+	 * given customer via {@code MobileUI_UserProfile_Picking_BPartner}.
+	 *
+	 * <p>Required columns:
+	 * <ul>
+	 *   <li>{@code C_BPartner_ID} — identifier of the customer</li>
+	 *   <li>{@code CreateShipmentPolicy} — policy code: DO_NOT_CREATE / CREATE_DRAFT / CREATE_AND_COMPLETE / CREATE_COMPLETE_CLOSE</li>
+	 * </ul>
+	 *
+	 * <p>Example:
+	 * <pre>
+	 * And set per-customer mobile UI shipment policy:
+	 *   | C_BPartner_ID | CreateShipmentPolicy |
+	 *   | customer1     | CREATE_DRAFT         |
+	 *   | customer2     | DO_NOT_CREATE        |
+	 * </pre>
+	 */
+	@And("set per-customer mobile UI shipment policy:")
+	public void setPerCustomerShipmentPolicy(@NonNull final DataTable dataTable)
+	{
+		// Build per-customer PickingJobOptionsId entries, creating a MobileUI_UserProfile_Picking_Job
+		// record per row so the PickingJobOptionsCollection cache can load them from DB.
+		final List<PickingCustomerConfig> newConfigs = new ArrayList<>();
+		DataTableRows.of(dataTable).forEach(row -> {
+			final BPartnerId customerId = bPartnerTable.getId(row.getAsIdentifier("C_BPartner_ID"));
+			final CreateShipmentPolicy policy = CreateShipmentPolicy.ofCodeOrName(row.getAsString("CreateShipmentPolicy"));
+
+			// Insert a MobileUI_UserProfile_Picking_Job record carrying only the shipment policy;
+			// all other options fall back to the profile default via PickingJobOptions.fallbackTo().
+			final I_MobileUI_UserProfile_Picking_Job jobRecord = InterfaceWrapperHelper.newInstance(I_MobileUI_UserProfile_Picking_Job.class);
+			jobRecord.setIsActive(true);
+			jobRecord.setName("per-customer-" + customerId.getRepoId());
+			jobRecord.setCreateShipmentPolicy(policy.getCode());
+			jobRecord.setIsAllowCompletingPartialPickingJob(true);
+			InterfaceWrapperHelper.saveRecord(jobRecord);
+
+			final PickingJobOptionsId optionsId = PickingJobOptionsId.ofRepoId(jobRecord.getMobileUI_UserProfile_Picking_Job_ID());
+			newConfigs.add(PickingCustomerConfig.builder()
+					.customerId(customerId)
+					.pickingJobOptionsId(optionsId)
+					.build());
+		});
+
+		// Update the profile's customer configs (persists MobileUI_UserProfile_Picking_BPartner records).
+		// This invalidates the profileCache (backed on BPartner table) and the pickingJobOptionsCache
+		// (backed on MobileUI_UserProfile_Picking_Job) so both reload fresh on next access.
+		profileService.update(profile -> {
+			final List<PickingCustomerConfig> merged = new ArrayList<>(ImmutableList.copyOf(profile.getCustomerConfigs()));
+			// Replace or add entries for the customers defined in this step.
+			for (final PickingCustomerConfig newConfig : newConfigs)
+			{
+				merged.removeIf(existing -> existing.getCustomerId().equals(newConfig.getCustomerId()));
+				merged.add(newConfig);
+			}
+			return profile.toBuilder()
+					.customerConfigs(PickingCustomerConfigsCollection.ofCollection(merged))
+					.build();
+		});
+
+		logger.info("Per-customer shipment policies set: {}", newConfigs);
 	}
 }

backend/de.metas.cucumber/src/test/resources/de/metas/cucumber/features/mobileui/massprinting/MassPrintingLabels.feature
@@ -169,3 +169,177 @@ Feature: Mass Printing Labels — https://github.com/metasfresh/me03/issues/2994
     And mass-printing produced box HUs
       | boxHUCount | qtyPerBoxHU |
       | 3          | 1           |
+
+  @from:cucumber
+  @allure.label.epic:E0105_Picking
+  @allure.label.feature:F00230_MobileUI_Picking
+  Scenario: Shipment policy CREATE_DRAFT — scan creates a draft shipment per packed order
+    # Background sets policy to DO_NOT_CREATE; override it here to CREATE_DRAFT.
+    And set mobile UI picking profile
+      | CreateShipmentPolicy |
+      | CREATE_DRAFT         |
+
+    And metasfresh contains C_Orders:
+      | Identifier | IsSOTrx | C_BPartner_ID.Identifier | DateOrdered | OPT.PreparationDate |
+      | SO         | true    | customer                 | 2026-06-01  | 2026-06-02          |
+    And metasfresh contains C_OrderLines:
+      | C_Order_ID.Identifier | Identifier | M_Product_ID.Identifier | QtyEntered |
+      | SO                    | OL         | selfPackedPrd           | 1          |
+    And the order identified by SO is completed
+    And after not more than 60s, M_ShipmentSchedules are found:
+      | Identifier | C_OrderLine_ID.Identifier | IsToRecompute |
+      | SS         | OL                        | N             |
+
+    When mass-printing scans LU
+      | LU |
+      | lu |
+    Then mass-printing result is
+      | boxesPacked | OPT.unitsLeftOnLU |
+      | 1           | 2                 |
+    And mass-printing produced box HUs
+      | boxHUCount | qtyPerBoxHU |
+      | 1          | 1           |
+
+    # Shipment generated in draft — async WP processor creates it; poll until it appears.
+    Then after not more than 60s, M_InOut is found:
+      | M_ShipmentSchedule_ID.Identifier | M_InOut_ID.Identifier |
+      | SS                               | shipment              |
+    And validate M_In_Out status
+      | M_InOut_ID | DocStatus |
+      | shipment   | DR        |
+
+  @from:cucumber
+  @allure.label.epic:E0105_Picking
+  @allure.label.feature:F00230_MobileUI_Picking
+  Scenario: Shipment policy CREATE_AND_COMPLETE — scan creates and completes a shipment per packed order
+    # Background sets policy to DO_NOT_CREATE; override it here to CREATE_AND_COMPLETE.
+    And set mobile UI picking profile
+      | CreateShipmentPolicy |
+      | CREATE_AND_COMPLETE  |
+
+    And metasfresh contains C_Orders:
+      | Identifier | IsSOTrx | C_BPartner_ID.Identifier | DateOrdered | OPT.PreparationDate |
+      | SO         | true    | customer                 | 2026-06-01  | 2026-06-02          |
+    And metasfresh contains C_OrderLines:
+      | C_Order_ID.Identifier | Identifier | M_Product_ID.Identifier | QtyEntered |
+      | SO                    | OL         | selfPackedPrd           | 1          |
+    And the order identified by SO is completed
+    And after not more than 60s, M_ShipmentSchedules are found:
+      | Identifier | C_OrderLine_ID.Identifier | IsToRecompute |
+      | SS         | OL                        | N             |
+
+    When mass-printing scans LU
+      | LU |
+      | lu |
+    Then mass-printing result is
+      | boxesPacked | OPT.unitsLeftOnLU |
+      | 1           | 2                 |
+    And mass-printing produced box HUs
+      | boxHUCount | qtyPerBoxHU |
+      | 1          | 1           |
+
+    # Shipment generated and completed — poll until it appears in CO status.
+    Then after not more than 60s, M_InOut is found:
+      | M_ShipmentSchedule_ID.Identifier | M_InOut_ID.Identifier |
+      | SS                               | shipment              |
+    And validate M_In_Out status
+      | M_InOut_ID | DocStatus |
+      | shipment   | CO        |
+
+  @from:cucumber
+  @allure.label.epic:E0105_Picking
+  @allure.label.feature:F00230_MobileUI_Picking
+  Scenario: Shipment policy CREATE_COMPLETE_CLOSE — scan creates a completed shipment and closes the schedule
+    # Background sets policy to DO_NOT_CREATE; override it here to CREATE_COMPLETE_CLOSE.
+    And set mobile UI picking profile
+      | CreateShipmentPolicy    |
+      | CREATE_COMPLETE_CLOSE   |
+
+    And metasfresh contains C_Orders:
+      | Identifier | IsSOTrx | C_BPartner_ID.Identifier | DateOrdered | OPT.PreparationDate |
+      | SO         | true    | customer                 | 2026-06-01  | 2026-06-02          |
+    And metasfresh contains C_OrderLines:
+      | C_Order_ID.Identifier | Identifier | M_Product_ID.Identifier | QtyEntered |
+      | SO                    | OL         | selfPackedPrd           | 1          |
+    And the order identified by SO is completed
+    And after not more than 60s, M_ShipmentSchedules are found:
+      | Identifier | C_OrderLine_ID.Identifier | IsToRecompute |
+      | SS         | OL                        | N             |
+
+    When mass-printing scans LU
+      | LU |
+      | lu |
+    Then mass-printing result is
+      | boxesPacked | OPT.unitsLeftOnLU |
+      | 1           | 2                 |
+    And mass-printing produced box HUs
+      | boxHUCount | qtyPerBoxHU |
+      | 1          | 1           |
+
+    # Shipment generated and completed; schedule also closed.
+    Then after not more than 60s, M_InOut is found:
+      | M_ShipmentSchedule_ID.Identifier | M_InOut_ID.Identifier |
+      | SS                               | shipment              |
+    And validate M_In_Out status
+      | M_InOut_ID | DocStatus |
+      | shipment   | CO        |
+    And the shipment-schedule is closed
+      | M_ShipmentSchedule_ID.Identifier |
+      | SS                               |
+
+  @from:cucumber
+  @allure.label.epic:E0105_Picking
+  @allure.label.feature:F00230_MobileUI_Picking
+  Scenario: Mixed-customer scan — each customer's shipment follows its own policy within one scan
+    # Two customers with different policies on one scan.
+    # customer (from Background) → keeps default DO_NOT_CREATE (no shipment).
+    # customer2 → CREATE_DRAFT (shipment in draft).
+    # LU has 3 units: 1 unit goes to customer, 1 unit goes to customer2, 1 unit leftover.
+
+    # Set up second customer with its own pricing.
+    And metasfresh contains C_BPartners without locations:
+      | Identifier | Name      | OPT.IsVendor | OPT.IsCustomer | M_PricingSystem_ID.Identifier |
+      | customer2  | customer2 | N            | Y              | PS                            |
+    And metasfresh contains C_BPartner_Locations:
+      | Identifier        | GLN           | C_BPartner_ID.Identifier | OPT.IsBillToDefault | OPT.IsShipTo |
+      | customer2Location | 9876500000001 | customer2                | true                | true         |
+
+    # Per-customer policy: customer2 → CREATE_DRAFT; customer keeps profile default (DO_NOT_CREATE).
+    And set per-customer mobile UI shipment policy:
+      | C_BPartner_ID | CreateShipmentPolicy |
+      | customer2     | CREATE_DRAFT         |
+
+    # One single-unit order per customer (FIFO: customer gets prep date 2026-06-02, customer2 gets 2026-06-03).
+    And metasfresh contains C_Orders:
+      | Identifier | IsSOTrx | C_BPartner_ID.Identifier | DateOrdered | OPT.PreparationDate |
+      | SO_c1      | true    | customer                 | 2026-06-01  | 2026-06-02          |
+      | SO_c2      | true    | customer2                | 2026-06-01  | 2026-06-03          |
+    And metasfresh contains C_OrderLines:
+      | C_Order_ID.Identifier | Identifier | M_Product_ID.Identifier | QtyEntered |
+      | SO_c1                 | OL_c1      | selfPackedPrd           | 1          |
+      | SO_c2                 | OL_c2      | selfPackedPrd           | 1          |
+    And the order identified by SO_c1 is completed
+    And the order identified by SO_c2 is completed
+    And after not more than 60s, M_ShipmentSchedules are found:
+      | Identifier | C_OrderLine_ID.Identifier | IsToRecompute |
+      | SS_c1      | OL_c1                     | N             |
+      | SS_c2      | OL_c2                     | N             |
+
+    # Scan: 2 units packed (1 per customer), 1 unit leftover on LU.
+    When mass-printing scans LU
+      | LU |
+      | lu |
+    Then mass-printing result is
+      | boxesPacked | OPT.unitsLeftOnLU |
+      | 2           | 1                 |
+
+    # customer (DO_NOT_CREATE): no M_InOut created for its order.
+    And validate no M_InOut found for C_Order identified by SO_c1
+
+    # customer2 (CREATE_DRAFT): shipment created in draft.
+    Then after not more than 60s, M_InOut is found:
+      | M_ShipmentSchedule_ID.Identifier | M_InOut_ID.Identifier |
+      | SS_c2                            | shipment_c2           |
+    And validate M_In_Out status
+      | M_InOut_ID  | DocStatus |
+      | shipment_c2 | DR        |