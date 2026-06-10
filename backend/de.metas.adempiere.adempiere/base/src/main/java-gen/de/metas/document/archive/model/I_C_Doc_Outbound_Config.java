bb15a57de5c4a3e61796a240e8c9958a5256e82e
metas-ts <tobias.schoeneberg@metasfresh.com>
2026-06-10 22:03:35 +0200

    DocuWare export - C_Doc_Outbound_Config.ExternalSystem_Config_ID link to external system

---
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/de/metas/document/archive/model/I_C_Doc_Outbound_Config.java
@@ -0,0 +1,329 @@
+package de.metas.document.archive.model;
+
+import javax.annotation.Nullable;
+import org.adempiere.model.ModelColumn;
+
+/** Generated Interface for C_Doc_Outbound_Config
+ *  @author metasfresh (generated) 
+ */
+@SuppressWarnings("unused")
+public interface I_C_Doc_Outbound_Config 
+{
+
+	String Table_Name = "C_Doc_Outbound_Config";
+
+//	/** AD_Table_ID=540434 */
+//	int Table_ID = org.compiere.model.MTable.getTable_ID(Table_Name);
+
+
+	/**
+	 * Get Client.
+	 * Client/Tenant for this installation.
+	 *
+	 * <br>Type: TableDir
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getAD_Client_ID();
+
+	String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/**
+	 * Set Organisation.
+	 * Organisational entity within client
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setAD_Org_ID (int AD_Org_ID);
+
+	/**
+	 * Get Organisation.
+	 * Organisational entity within client
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getAD_Org_ID();
+
+	String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/**
+	 * Set Print Format.
+	 * Data Print Format
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setAD_PrintFormat_ID (int AD_PrintFormat_ID);
+
+	/**
+	 * Get Print Format.
+	 * Data Print Format
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	int getAD_PrintFormat_ID();
+
+	@Nullable org.compiere.model.I_AD_PrintFormat getAD_PrintFormat();
+
+	void setAD_PrintFormat(@Nullable org.compiere.model.I_AD_PrintFormat AD_PrintFormat);
+
+	ModelColumn<I_C_Doc_Outbound_Config, org.compiere.model.I_AD_PrintFormat> COLUMN_AD_PrintFormat_ID = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "AD_PrintFormat_ID", org.compiere.model.I_AD_PrintFormat.class);
+	String COLUMNNAME_AD_PrintFormat_ID = "AD_PrintFormat_ID";
+
+	/**
+	 * Set Table.
+	 * Database Table information
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setAD_Table_ID (int AD_Table_ID);
+
+	/**
+	 * Get Table.
+	 * Database Table information
+	 *
+	 * <br>Type: Search
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getAD_Table_ID();
+
+	String COLUMNNAME_AD_Table_ID = "AD_Table_ID";
+
+	/**
+	 * Set CC Path.
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setCCPath (@Nullable java.lang.String CCPath);
+
+	/**
+	 * Get CC Path.
+	 *
+	 * <br>Type: String
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getCCPath();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_CCPath = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "CCPath", null);
+	String COLUMNNAME_CCPath = "CCPath";
+
+	/**
+	 * Set Ausgehende Belege Konfig.
+	 *
+	 * <br>Type: ID
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setC_Doc_Outbound_Config_ID (int C_Doc_Outbound_Config_ID);
+
+	/**
+	 * Get Ausgehende Belege Konfig.
+	 *
+	 * <br>Type: ID
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getC_Doc_Outbound_Config_ID();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_C_Doc_Outbound_Config_ID = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "C_Doc_Outbound_Config_ID", null);
+	String COLUMNNAME_C_Doc_Outbound_Config_ID = "C_Doc_Outbound_Config_ID";
+
+	/**
+	 * Get Created.
+	 * Date this record was created
+	 *
+	 * <br>Type: DateTime
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	java.sql.Timestamp getCreated();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_Created = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "Created", null);
+	String COLUMNNAME_Created = "Created";
+
+	/**
+	 * Get Created By.
+	 * User who created this records
+	 *
+	 * <br>Type: Table
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getCreatedBy();
+
+	String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/**
+	 * Set Document Base Type.
+	 * Logical type of document
+	 *
+	 * <br>Type: List
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setDocBaseType (@Nullable java.lang.String DocBaseType);
+
+	/**
+	 * Get Document Base Type.
+	 * Logical type of document
+	 *
+	 * <br>Type: List
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	@Nullable java.lang.String getDocBaseType();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_DocBaseType = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "DocBaseType", null);
+	String COLUMNNAME_DocBaseType = "DocBaseType";
+
+	/**
+	 * Set External System Config.
+	 * External-system configuration this outbound config is linked to.
+	 *
+	 * <br>Type: TableDir
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	void setExternalSystem_Config_ID (int ExternalSystem_Config_ID);
+
+	/**
+	 * Get External System Config.
+	 * External-system configuration this outbound config is linked to.
+	 *
+	 * <br>Type: TableDir
+	 * <br>Mandatory: false
+	 * <br>Virtual Column: false
+	 */
+	int getExternalSystem_Config_ID();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_ExternalSystem_Config_ID = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "ExternalSystem_Config_ID", null);
+	String COLUMNNAME_ExternalSystem_Config_ID = "ExternalSystem_Config_ID";
+
+	/**
+	 * Set Active.
+	 * The record is active in the system
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setIsActive (boolean IsActive);
+
+	/**
+	 * Get Active.
+	 * The record is active in the system
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	boolean isActive();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_IsActive = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "IsActive", null);
+	String COLUMNNAME_IsActive = "IsActive";
+
+	/**
+	 * Set Beleg autom. versenden.
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setIsAutoSendDocument (boolean IsAutoSendDocument);
+
+	/**
+	 * Get Beleg autom. versenden.
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	boolean isAutoSendDocument();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_IsAutoSendDocument = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "IsAutoSendDocument", null);
+	String COLUMNNAME_IsAutoSendDocument = "IsAutoSendDocument";
+
+	/**
+	 * Set Is Direct Enqueue.
+	 * Entscheidet, ob beim erstellen des Druckstücks (Archiv) automatisch eine Druck-Warteschlange-Datensatz erstellt werden soll
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setIsDirectEnqueue (boolean IsDirectEnqueue);
+
+	/**
+	 * Get Is Direct Enqueue.
+	 * Entscheidet, ob beim erstellen des Druckstücks (Archiv) automatisch eine Druck-Warteschlange-Datensatz erstellt werden soll
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	boolean isDirectEnqueue();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_IsDirectEnqueue = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "IsDirectEnqueue", null);
+	String COLUMNNAME_IsDirectEnqueue = "IsDirectEnqueue";
+
+	/**
+	 * Set Directly proces queue item.
+	 * Decides whether according to config a print-job is created or the PDF is stored locally right away.
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	void setIsDirectProcessQueueItem (boolean IsDirectProcessQueueItem);
+
+	/**
+	 * Get Directly proces queue item.
+	 * Decides whether according to config a print-job is created or the PDF is stored locally right away.
+	 *
+	 * <br>Type: YesNo
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	boolean isDirectProcessQueueItem();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_IsDirectProcessQueueItem = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "IsDirectProcessQueueItem", null);
+	String COLUMNNAME_IsDirectProcessQueueItem = "IsDirectProcessQueueItem";
+
+	/**
+	 * Get Updated.
+	 * Date this record was updated
+	 *
+	 * <br>Type: DateTime
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	java.sql.Timestamp getUpdated();
+
+	ModelColumn<I_C_Doc_Outbound_Config, Object> COLUMN_Updated = new ModelColumn<>(I_C_Doc_Outbound_Config.class, "Updated", null);
+	String COLUMNNAME_Updated = "Updated";
+
+	/**
+	 * Get Updated By.
+	 * User who updated this records
+	 *
+	 * <br>Type: Table
+	 * <br>Mandatory: true
+	 * <br>Virtual Column: false
+	 */
+	int getUpdatedBy();
+
+	String COLUMNNAME_UpdatedBy = "UpdatedBy";
+}
