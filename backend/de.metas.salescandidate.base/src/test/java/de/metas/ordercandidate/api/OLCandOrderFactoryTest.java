724fec3d2053c697b0bcb048d4ebff5e873cd77b
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-06-12 23:50:27 +0300

    gh30235 Fix Spring instantiation: single constructor + update OLCandOrderFactoryTest
    Two constructors without @Autowired caused Spring to fall back to the
    missing no-arg default constructor, crashing the application context.
    Remove the 2-arg delegate and keep only the 3-arg constructor Spring
    can auto-wire (IBPartnerBL + BPartnerOrderParamsRepository + IWarehouseDAO).
    Update OLCandOrderFactoryTest to pass IWarehouseDAO as the third argument.

--- backend/de.metas.salescandidate.base/src/test/java/de/metas/ordercandidate/api/OLCandOrderFactoryTest.java
+++ backend/de.metas.salescandidate.base/src/test/java/de/metas/ordercandidate/api/OLCandOrderFactoryTest.java
@@ -39,6 +39,7 @@ import de.metas.order.compensationGroup.OrderGroupRepository;
 import de.metas.order.location.adapter.OrderDocumentLocationAdapterFactory;
 import de.metas.ordercandidate.api.impl.OLCandBL;
 import de.metas.ordercandidate.location.adapter.OLCandDocumentLocationAdapterFactory;
+import de.metas.util.Services;
 import de.metas.ordercandidate.model.I_C_OLCand;
 import de.metas.ordercandidate.spi.NullOLCandListener;
 import de.metas.product.ProductId;
@@ -56,6 +57,7 @@ import org.compiere.model.I_AD_User;
 import org.compiere.model.I_C_BP_Group;
 import org.compiere.model.I_C_BPartner;
 import org.compiere.model.I_C_BPartner_Location;
+import org.adempiere.warehouse.api.IWarehouseDAO;
 import org.compiere.model.I_C_Country;
 import org.compiere.model.I_C_Location;
 import org.compiere.model.I_C_UOM;
@@ -98,7 +100,8 @@ class OLCandOrderFactoryTest
 				IOLCandBL.class,
 				new OLCandBL(
 						bpartnerBL,
-						BPartnerOrderParamsRepository.newInstanceForUnitTesting()
+						BPartnerOrderParamsRepository.newInstanceForUnitTesting(),
+						Services.get(IWarehouseDAO.class)
 				)
 		);
 
