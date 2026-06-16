02e4cce659e4ac0e4dce2ca132255e24eb522b58
Teodor Sarca <teo.sarca@metasfresh.com>
2026-06-17 00:47:27 +0300

    fix performances of ProductAvailableStockPerLocator

--- backend/de.metas.distribution.rest-api/src/main/java/de/metas/distribution/mobileui/external_services/warehouse/GroundLocatorByPriorityAndStockResolver.java
+++ backend/de.metas.distribution.rest-api/src/main/java/de/metas/distribution/mobileui/external_services/warehouse/GroundLocatorByPriorityAndStockResolver.java
@@ -1,12 +1,11 @@
 package de.metas.distribution.mobileui.external_services.warehouse;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableList;
 import de.metas.handlingunits.IHandlingUnitsBL;
+import de.metas.handlingunits.storage.LocatorIdAndQty;
 import de.metas.handlingunits.storage.ProductAvailableStockPerLocator;
-import de.metas.handlingunits.storage.ProductQtyOnHandByLocator;
 import de.metas.product.ProductId;
 import de.metas.util.Services;
-import de.metas.util.collections.CollectionUtils;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.adempiere.exceptions.AdempiereException;
@@ -15,7 +14,6 @@ import org.adempiere.warehouse.Warehouse;
 import org.adempiere.warehouse.WarehouseRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
 import java.util.List;
 
 @Component
@@ -36,20 +34,35 @@ public class GroundLocatorByPriorityAndStockResolver implements NextPickFromLoca
 			throw new AdempiereException(MSG_NO_ALTERNATIVE);
 		}
 
-		final ProductQtyOnHandByLocator qtyOnHandByLocator = getQtyOnHandByLocator(productId, locatorIds);
+		final List<LocatorId> candidatesInRoundRobinOrder = orderedRoundRobinAfter(locatorIds, currentLocatorId);
 
-		final LocatorId nextLocatorId = CollectionUtils.getNextRoundRobin(locatorIds, currentLocatorId, qtyOnHandByLocator::hasStock);
-		if (nextLocatorId == null)
-		{
-			throw new AdempiereException(MSG_NO_ALTERNATIVE);
-		}
-
-		return nextLocatorId;
+		return ProductAvailableStockPerLocator.newInstance(handlingUnitsBL)
+				.streamLocatorQtyOnHandOrdered(productId, 10, candidatesInRoundRobinOrder)
+				.map(LocatorIdAndQty::getLocatorId)
+				.findFirst()
+				.orElseThrow(() -> new AdempiereException(MSG_NO_ALTERNATIVE));
 	}
 
-	private ProductQtyOnHandByLocator getQtyOnHandByLocator(@NonNull final ProductId productId, @NonNull final Collection<LocatorId> sourceLocatorIds)
+	/**
+	 * Returns the {@code locatorIds} reordered round-robin starting at {@code currentLocatorId + 1},
+	 * excluding {@code currentLocatorId} itself. Empty if {@code currentLocatorId} is not in the list.
+	 */
+	private static List<LocatorId> orderedRoundRobinAfter(
+			@NonNull final List<LocatorId> locatorIds,
+			@NonNull final LocatorId currentLocatorId)
 	{
-		final ProductAvailableStockPerLocator productAvailableStockPerLocator = ProductAvailableStockPerLocator.newInstance(handlingUnitsBL);
-		return productAvailableStockPerLocator.getQtyOnHandByLocator(productId, ImmutableSet.copyOf(sourceLocatorIds));
+		final int startIdx = locatorIds.indexOf(currentLocatorId);
+		if (startIdx < 0)
+		{
+			return ImmutableList.of();
+		}
+
+		final int size = locatorIds.size();
+		final ImmutableList.Builder<LocatorId> result = ImmutableList.builderWithExpectedSize(size - 1);
+		for (int step = 1; step < size; step++)
+		{
+			result.add(locatorIds.get((startIdx + step) % size));
+		}
+		return result.build();
 	}
 }
