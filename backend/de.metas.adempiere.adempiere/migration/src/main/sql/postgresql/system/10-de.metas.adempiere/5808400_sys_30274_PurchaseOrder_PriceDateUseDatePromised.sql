474d8080c7745cff4f61d9746cf25f6ddd6b4efd
Adrian <60300682+adi-stefan@users.noreply.github.com>
2026-06-17 19:08:08 +0200

    Fix SQL update statement for PurchaseOrder pricing config

--- backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5808400_sys_30274_PurchaseOrder_PriceDateUseDatePromised.sql
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5808400_sys_30274_PurchaseOrder_PriceDateUseDatePromised.sql
@@ -6,6 +6,5 @@ INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLe
 
 -- SysConfig Name: de.metas.order.PurchaseOrder.UseDatePromisedForPricing
 -- 2026-06-17T14:26:39.346Z
-UPDATE AD_SysConfig SET ConfigurationLevel='C', Description='Controls which date is used to select the price list version on purchase orders. Y = DatePromised (Zugesagter Te
-rmin); N = DateOrdered (Auftragsdatum, default).', Value='N',Updated=TO_TIMESTAMP('2026-06-17 14:26:39.261000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_SysConfig_ID=541822
+UPDATE AD_SysConfig SET ConfigurationLevel='C', Description='Controls which date is used to select the price list version on purchase orders. Y = DatePromised (Zugesagter Termin); N = DateOrdered (Auftragsdatum, default).', Value='N',Updated=TO_TIMESTAMP('2026-06-17 14:26:39.261000','YYYY-MM-DD HH24:MI:SS.US')::timestamp without time zone AT TIME ZONE 'UTC',UpdatedBy=100 WHERE AD_SysConfig_ID=541822
 ;
