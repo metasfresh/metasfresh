6c28f6713cf1b0d2a0cd963d746f3081b7fd0049
Max Rieck <max.rieck@metasfresh.com>
2026-06-18 09:28:08 +0200

    nShift advise — use method reference instead of trivial lambda (review)

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipper/M_Shipper_StepDef.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/stepdefs/shipper/M_Shipper_StepDef.java
@@ -107,7 +107,7 @@ public class M_Shipper_StepDef
 				.ifPresent(record::setShipperGateway);
 
 		row.getAsOptionalBoolean(I_M_Shipper.COLUMNNAME_IsApiCarrierAdvise)
-				.ifPresent(value -> record.setIsApiCarrierAdvise(value));
+				.ifPresent(record::setIsApiCarrierAdvise);
 
 		InterfaceWrapperHelper.save(record);
 
