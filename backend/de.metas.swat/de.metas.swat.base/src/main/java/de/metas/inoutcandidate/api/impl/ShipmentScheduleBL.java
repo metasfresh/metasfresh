e50c5ffc2b12e414d6dae1391b82be89a65976ee
Teo Sarca <teo.sarca@metasfresh.com>
2026-07-13 20:03:36 +0200

    Add missing java.util.HashMap import (soft_panda_hotfix lacked it; used by the ported close method)

--- backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/api/impl/ShipmentScheduleBL.java
+++ backend/de.metas.swat/de.metas.swat.base/src/main/java/de/metas/inoutcandidate/api/impl/ShipmentScheduleBL.java
@@ -98,6 +98,7 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
