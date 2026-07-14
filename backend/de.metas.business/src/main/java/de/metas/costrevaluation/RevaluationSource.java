1d0456e3c4bb73b0d0e5f38cd3a4cb0ff8d318d4
Teo Sarca <teo.sarca@metasfresh.com>
2026-07-14 10:37:10 +0200

    Cost Revaluation — RevaluationSource enum (ReferenceListAwareEnum)
    Maps AD_Ref_List codes Calculated/CopyFromCostElement; ofCode round-trip + unknown-code guard.

---
+++ backend/de.metas.business/src/main/java/de/metas/costrevaluation/RevaluationSource.java
@@ -0,0 +1,84 @@
+package de.metas.costrevaluation;
+
+import com.google.common.collect.ImmutableMap;
+import de.metas.util.lang.ReferenceListAwareEnum;
+import de.metas.util.lang.ReferenceListAwareEnums;
+import lombok.Getter;
+import lombok.NonNull;
+import org.adempiere.exceptions.AdempiereException;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2024 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+/**
+ * NOTE to developers: Please keep in sync with list reference "M_CostRevaluation RevaluationSource" {@code AD_Reference_ID=542117}
+ */
+public enum RevaluationSource implements ReferenceListAwareEnum
+{
+	/** Cost is recalculated for this revaluation (default, existing behaviour). */
+	Calculated("Calculated"), //
+	/** Cost is copied unchanged from the cost element selected under CopyFrom_M_CostElement_ID (value-neutral). */
+	CopyFromCostElement("CopyFromCostElement") //
+	;
+
+	@Getter
+	private final String code;
+
+	RevaluationSource(final String code)
+	{
+		this.code = code;
+	}
+
+	public static RevaluationSource ofCode(@NonNull final String code)
+	{
+		final RevaluationSource source = typesByCode.get(code);
+		if (source == null)
+		{
+			throw new AdempiereException("No " + RevaluationSource.class + " found for code: " + code);
+		}
+		return source;
+	}
+
+	@Nullable
+	public static RevaluationSource ofNullableCode(@Nullable final String code)
+	{
+		return code != null ? ofCode(code) : null;
+	}
+
+	@Nullable
+	public static String toCodeOrNull(@Nullable final RevaluationSource source)
+	{
+		return source != null ? source.getCode() : null;
+	}
+
+	private static final ImmutableMap<String, RevaluationSource> typesByCode = ReferenceListAwareEnums.indexByCode(values());
+
+	public static boolean equals(@Nullable final RevaluationSource o1, @Nullable final RevaluationSource o2)
+	{
+		return Objects.equals(o1, o2);
+	}
+
+	public boolean isCopyFromCostElement() {return this == CopyFromCostElement;}
+}
---
+++ backend/de.metas.business/src/test/java/de/metas/costrevaluation/RevaluationSourceTest.java
@@ -0,0 +1,55 @@
+package de.metas.costrevaluation;
+
+import org.adempiere.exceptions.AdempiereException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/*
+ * #%L
+ * de.metas.business
+ * %%
+ * Copyright (C) 2024 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+public class RevaluationSourceTest
+{
+	@Test
+	public void ofCode_returns_matching_enum_value()
+	{
+		assertThat(RevaluationSource.ofCode("CopyFromCostElement")).isSameAs(RevaluationSource.CopyFromCostElement);
+		assertThat(RevaluationSource.ofCode("Calculated")).isSameAs(RevaluationSource.Calculated);
+	}
+
+	@Test
+	public void getCode_roundtrips()
+	{
+		for (final RevaluationSource source : RevaluationSource.values())
+		{
+			assertThat(RevaluationSource.ofCode(source.getCode())).isSameAs(source);
+		}
+	}
+
+	@Test
+	public void ofCode_throws_for_unknown_code()
+	{
+		assertThatThrownBy(() -> RevaluationSource.ofCode("SomeUnknownCode"))
+				.isInstanceOf(AdempiereException.class);
+	}
+}
