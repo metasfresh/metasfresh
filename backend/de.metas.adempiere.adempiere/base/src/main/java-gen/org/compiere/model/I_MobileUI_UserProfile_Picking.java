0400d934d91085658080b80b096ef786c3eb8865
Cristina Ghita <cristina.ghita@metasfresh.com>
2026-07-24 17:20:10 +0300

    Fix duplicates

--- backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_MobileUI_UserProfile_Picking.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java-gen/org/compiere/model/I_MobileUI_UserProfile_Picking.java
@@ -646,7 +646,6 @@ public interface I_MobileUI_UserProfile_Picking
 	String COLUMNNAME_IsShowLastPickedBestBeforeDateForLines = "IsShowLastPickedBestBeforeDateForLines";
 
 	/**
-	 /**
 	 * Set Warn when guaranteed remaining shelf life is undercut.
 	 *
 	 * <br>Type: YesNo
@@ -655,7 +654,7 @@ public interface I_MobileUI_UserProfile_Picking
 	 */
 	 void setIsWarnShelfLifeUndercut (boolean IsWarnShelfLifeUndercut);
 
-	 /**
+	/**
 	 * Get Warn when guaranteed remaining shelf life is undercut.
 	 *
 	 * <br>Type: YesNo
@@ -667,7 +666,7 @@ public interface I_MobileUI_UserProfile_Picking
 	 ModelColumn<I_MobileUI_UserProfile_Picking, Object> COLUMN_IsWarnShelfLifeUndercut = new ModelColumn<>(I_MobileUI_UserProfile_Picking.class, "IsWarnShelfLifeUndercut", null);
 	 String COLUMNNAME_IsWarnShelfLifeUndercut = "IsWarnShelfLifeUndercut";
 
-	 /**
+	/**
 	 * Set Show available qty per line.
 	 *
 	 * <br>Type: YesNo
@@ -676,7 +675,7 @@ public interface I_MobileUI_UserProfile_Picking
 	 */
 	 void setIsShowQtyAvailableForLines (boolean IsShowQtyAvailableForLines);
 
-	 /**
+	/**
 	 * Get Show available qty per line.
 	 *
 	 * <br>Type: YesNo
@@ -687,75 +686,6 @@ public interface I_MobileUI_UserProfile_Picking
 
 	 ModelColumn<I_MobileUI_UserProfile_Picking, Object> COLUMN_IsShowQtyAvailableForLines = new ModelColumn<>(I_MobileUI_UserProfile_Picking.class, "IsShowQtyAvailableForLines", null);
 	 String COLUMNNAME_IsShowQtyAvailableForLines = "IsShowQtyAvailableForLines";
-	 /*
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	 void setIsWarnShelfLifeUndercut (boolean IsWarnShelfLifeUndercut);
-
-	 /**
-	 * Get Warn when guaranteed remaining shelf life is undercut.
-	 *
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	 boolean isWarnShelfLifeUndercut();
-
-	 ModelColumn<I_MobileUI_UserProfile_Picking, Object> COLUMN_IsWarnShelfLifeUndercut = new ModelColumn<>(I_MobileUI_UserProfile_Picking.class, "IsWarnShelfLifeUndercut", null);
-	 String COLUMNNAME_IsWarnShelfLifeUndercut = "IsWarnShelfLifeUndercut";
-
-	 /**
-	 * Set Show available qty per line.
-	 *
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	 void setIsShowQtyAvailableForLines (boolean IsShowQtyAvailableForLines);
-
-	 /**
-	 * Get Show available qty per line.
-	 *
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	 boolean isShowQtyAvailableForLines();
-
-	 ModelColumn<I_MobileUI_UserProfile_Picking, Object> COLUMN_IsShowQtyAvailableForLines = new ModelColumn<>(I_MobileUI_UserProfile_Picking.class, "IsShowQtyAvailableForLines", null);
-	 String COLUMNNAME_IsShowQtyAvailableForLines = "IsShowQtyAvailableForLines";
-	 /*
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	boolean isWarnShelfLifeUndercut();
-
-	ModelColumn<I_MobileUI_UserProfile_Picking, Object> COLUMN_IsWarnShelfLifeUndercut = new ModelColumn<>(I_MobileUI_UserProfile_Picking.class, "IsWarnShelfLifeUndercut", null);
-	String COLUMNNAME_IsWarnShelfLifeUndercut = "IsWarnShelfLifeUndercut";
-
-	/**
-	 * Set Show available qty per line.
-	 *
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	void setIsShowQtyAvailableForLines (boolean IsShowQtyAvailableForLines);
-
-	/**
-	 * Get Show available qty per line.
-	 *
-	 * <br>Type: YesNo
-	 * <br>Mandatory: true
-	 * <br>Virtual Column: false
-	 */
-	boolean isShowQtyAvailableForLines();
-
-	ModelColumn<I_MobileUI_UserProfile_Picking, Object> COLUMN_IsShowQtyAvailableForLines = new ModelColumn<>(I_MobileUI_UserProfile_Picking.class, "IsShowQtyAvailableForLines", null);
-	String COLUMNNAME_IsShowQtyAvailableForLines = "IsShowQtyAvailableForLines";
 
 	/**
 	 * Set Lot number.
