30bb4d8910173c025bed1239c55eafead16d262f
Max Rieck <max.rieck@metasfresh.com>
2026-07-27 20:04:25 +0200

    nShift: trim OrderNo padding comment to the external constraint only

--- backend/de.metas.shipper.client.nshift/src/main/java/de/metas/shipper/client/nshift/NShiftShipmentService.java
+++ backend/de.metas.shipper.client.nshift/src/main/java/de/metas/shipper/client/nshift/NShiftShipmentService.java
@@ -173,7 +173,7 @@ public class NShiftShipmentService
 
 		final JsonShipmentData.JsonShipmentDataBuilder dataBuilder = JsonShipmentData.builder()
 				.actorCSID(Integer.valueOf(actorId))
-				// nShift requires OrderNo (its refNo) to be 8..35 chars long; a delivery-order id is an int (<= 10 digits), so left-pad with zeros to reach the 8-char minimum
+				// nShift requires OrderNo (its refNo) to be 8..35 chars long
 				.orderNo(String.format("%08d", deliveryRequest.getDeliveryOrderId()))
 				.pickupDt(LocalDate.parse(deliveryRequest.getPickupDate()));
 
