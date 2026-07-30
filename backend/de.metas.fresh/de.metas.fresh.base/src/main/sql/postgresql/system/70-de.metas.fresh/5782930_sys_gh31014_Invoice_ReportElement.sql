ad9cc6e554a1a61cb3c5baff501e54702a99e053
riad.mennour <riad.mennour@gmail.com>
2026-07-30 11:17:12 +0100

    Merging `new_dawn_uat` into `task_force_release`

--- backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5782930_sys_gh31014_Invoice_ReportElement.sql
+++ backend/de.metas.fresh/de.metas.fresh.base/src/main/sql/postgresql/system/70-de.metas.fresh/5782930_sys_gh31014_Invoice_ReportElement.sql
@@ -6,5 +6,9 @@ WHERE columnname = 'PackingInstruction'
 DELETE
 FROM AD_Ref_List
 WHERE value = 'Delivery_Via_Rule'
-   OR value = 'Net_Sum'
+;
+
+DELETE
+FROM AD_Ref_List
+WHERE AD_Ref_List_ID=544107
 ;
