607f99ec97f4556379b198adc2766dd07c2865e5
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2026-08-05 07:55:00 +0200

    InvoiceCandBL timeout SysConfig migration: reword comment to describe behaviour, not the Java method (code-review LOW)

--- backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.invoice/5817560_sys_InvoiceCandBL_WaitForRecompute_Timeout_SysConfig.sql
+++ backend/de.metas.swat/de.metas.swat.base/src/main/sql/postgresql/system/45-de.metas.invoice/5817560_sys_InvoiceCandBL_WaitForRecompute_Timeout_SysConfig.sql
@@ -1,6 +1,6 @@
--- SysConfig backing InvoiceCandBL.waitForInvoiceCandidatesUpdated: the max number of seconds the wait
--- polls for a selection's invoice candidates to be recomputed before aborting. Default 3600 (one hour);
--- lower it to make a wedged recompute fail fast instead of blocking for the full hour.
+-- SysConfig: max number of seconds the invoice-candidate recompute wait polls for a selection's invoice
+-- candidates to be recomputed before aborting. Default 3600 (one hour); lower it to make a wedged
+-- recompute fail fast instead of blocking for the full hour.
 INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,EntityType,IsActive,Name,Description,Updated,UpdatedBy,Value)
 SELECT 0,0,541846 /*From ID Server*/,'S',
        TO_TIMESTAMP('2026-08-05 09:00:00','YYYY-MM-DD HH24:MI:SS'),100,'D','Y',
