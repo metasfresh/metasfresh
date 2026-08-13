364fdb4f9a9542ec4ff6e6239f7cf203bbd65e15
tobias <tobias.schoeneberg@metasfresh.com>
2026-08-13 14:14:08 +0200

    ci: retire Testspace + adopt Allure reporting on hard_encoded_uat (sweep port from hard_encoded_hotfix)

--- backend/de.metas.cucumber/src/test/java/de/metas/cucumber/RunCucumberTest.java
+++ backend/de.metas.cucumber/src/test/java/de/metas/cucumber/RunCucumberTest.java
@@ -41,7 +41,8 @@ import org.junit.runner.RunWith;
 				"html:target/cucumber.html",
 				"json:target/cucumber.1json" /* this json-output is needed for the Jenkins plugin that's supposed to publish it */,
 				"junit:target/cucumber-junit.xml" /* thx to https://stackoverflow.com/a/52676659/1012103 */,
-				"message:target/cucumber.message"
+				"message:target/cucumber.message",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 		})
 public class RunCucumberTest
 {
