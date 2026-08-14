6b560f5529f434eee6e33790cce27efd3bc168a8
riad.mennour <riad.mennour@gmail.com>
2026-08-14 18:03:16 +0100

    Fix duplicated SQL files

--- backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5801271_AddOriginIntoDatevExport.sql
+++ backend/de.metas.datev/src/main/sql/postgresql/system/45-de.metas.datev/5801271_AddOriginIntoDatevExport.sql
@@ -9,7 +9,6 @@ ON CONFLICT (AD_Element_ID) DO NOTHING;
 -- 2026-03-26T18:40:26.268Z
 INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, CommitWarning,Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName,WEBUI_NameBrowse,WEBUI_NameNew,WEBUI_NameNewBreadcrumb, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy,IsActive) SELECT l.AD_Language, t.AD_Element_ID, t.CommitWarning,t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName,t.WEBUI_NameBrowse,t.WEBUI_NameNew,t.WEBUI_NameNewBreadcrumb, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy,'Y' FROM AD_Language l, AD_Element t WHERE l.IsActive='Y'AND (l.IsSystemLanguage='Y' OR l.IsBaseLanguage='Y') AND t.AD_Element_ID=584703 AND NOT EXISTS (SELECT 1 FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
 ;
-;
 
 -- Element: Origin
 -- 2026-03-26T18:43:17.560Z
