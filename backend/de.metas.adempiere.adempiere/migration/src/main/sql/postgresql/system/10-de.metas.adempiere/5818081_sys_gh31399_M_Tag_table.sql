84e1df295fb03cbb55a8f4ef1a6ce5c72581c6d3
Riad MENNOUR <93593841+riadmen@users.noreply.github.com>
2026-08-21 16:33:18 +0100

    `Tag Table`: Sequence fix (#25587)
    * `Tag Table`: Sequence fix
    
    * `Tag Table`: Sequence fix

---
+++ backend/de.metas.adempiere.adempiere/migration/src/main/sql/postgresql/system/10-de.metas.adempiere/5818081_sys_gh31399_M_Tag_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO AD_Sequence (CurrentNext,IsAudited,IsActive,IsTableID,Created,CreatedBy,IsAutoSequence,StartNo,IncrementNo,CurrentNextSys,Updated,UpdatedBy,AD_Sequence_ID,AD_Client_ID,Name,AD_Org_ID,Description) VALUES (1000000,'N','Y','Y',TO_TIMESTAMP('2026-08-10 15:20:00','YYYY-MM-DD HH24:MI:SS'),100,'Y',1000000,1,50000,TO_TIMESTAMP('2026-08-10 15:20:00','YYYY-MM-DD HH24:MI:SS'),100,581987,0,'M_Tag',0,'Table M_Tag')
+;
