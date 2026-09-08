a81f2c0a32356f003287b795e1116dfb63a7a710
Adrian Stefan <adrian.stefan@metasfresh.com>
2026-09-08 16:43:23 +0200

    ATP reconciliation — add SourceDocumentStatus

---
+++ backend/de.metas.material/dispo-commons/src/test/java/de/metas/material/dispo/commons/reconcile/SourceDocumentStatusTest.java
@@ -0,0 +1,48 @@
+package de.metas.material.dispo.commons.reconcile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * #%L
+ * metasfresh-material-dispo-commons
+ * %%
+ * Copyright (C) 2017 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+public class SourceDocumentStatusTest
+{
+	@Test
+	public void testStillOpenIsContributingToAtp()
+	{
+		assertThat(SourceDocumentStatus.STILL_OPEN.isContributingToAtp()).isTrue();
+	}
+
+	@Test
+	public void testClosedIsNotContributingToAtp()
+	{
+		assertThat(SourceDocumentStatus.CLOSED.isContributingToAtp()).isFalse();
+	}
+
+	@Test
+	public void testNoSourceDocumentIsNotContributingToAtp()
+	{
+		assertThat(SourceDocumentStatus.NO_SOURCE_DOCUMENT.isContributingToAtp()).isFalse();
+	}
+}
